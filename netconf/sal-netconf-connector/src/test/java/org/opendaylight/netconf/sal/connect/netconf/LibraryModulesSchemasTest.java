f81c24fb8b11d47bee724c3097afa49573f0d8bb
Maros Marsalek <mmarsale@cisco.com>
2015-07-19 13:19:47 +0200

    Bug 2343 - Sideload of models for nodes from remote yang sources repository
    Add support for sideloading schemas for devices from remote yang
    schemas sources repositories.
    
    Remote repository is queried for list of provided models and URLs
    representing their schemas resources. These schemas are preregistered
    to device schema registry as fallback schemas.
    
    Change-Id: Iba3d77dd1a868a89a2f260ed751e36250fa30a47
    Signed-off-by: Maros Marsalek <mmarsale@cisco.com>
    Signed-off-by: Jakub Morvay <jmorvay@cisco.com>

---
+++ netconf/sal-netconf-connector/src/test/java/org/opendaylight/netconf/sal/connect/netconf/LibraryModulesSchemasTest.java
@@ -0,0 +1,84 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.netconf.sal.connect.netconf;
+
+import static org.hamcrest.CoreMatchers.is;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Collections;
+import java.util.Map;
+import org.hamcrest.CoreMatchers;
+import org.junit.Assert;
+import org.junit.Test;
+import org.opendaylight.netconf.sal.connect.netconf.LibraryModulesSchemas;
+import org.opendaylight.yangtools.yang.model.repo.api.SourceIdentifier;
+
+public class LibraryModulesSchemasTest {
+
+    @Test
+    public void testCreate() throws Exception {
+        // test create from xml
+        LibraryModulesSchemas libraryModulesSchemas =
+                LibraryModulesSchemas.create(getClass().getResource("/yang-library.xml").toString());
+
+        verifySchemas(libraryModulesSchemas);
+
+        // test create from json
+        LibraryModulesSchemas libraryModuleSchemas =
+                LibraryModulesSchemas.create(getClass().getResource("/yang-library.json").toString());
+
+        verifySchemas(libraryModulesSchemas);
+    }
+
+
+    private void verifySchemas(final LibraryModulesSchemas libraryModulesSchemas) throws MalformedURLException {
+        final Map<SourceIdentifier, URL> resolvedModulesSchema = libraryModulesSchemas.getAvailableModels();
+        Assert.assertThat(resolvedModulesSchema.size(), is(3));
+
+        Assert.assertTrue(resolvedModulesSchema.containsKey(new SourceIdentifier("module-with-revision", "2014-04-08")));
+        Assert.assertThat(resolvedModulesSchema.get(
+                new SourceIdentifier("module-with-revision", "2014-04-08")),
+                is(new URL("http://localhost:8181/yanglib/schemas/module-with-revision/2014-04-08")));
+
+        Assert.assertTrue(resolvedModulesSchema.containsKey(
+                new SourceIdentifier("another-module-with-revision", "2013-10-21")));
+        Assert.assertThat(resolvedModulesSchema.get(
+                new SourceIdentifier("another-module-with-revision", "2013-10-21")),
+                is(new URL("http://localhost:8181/yanglib/schemas/another-module-with-revision/2013-10-21")));
+
+        Assert.assertTrue(resolvedModulesSchema.containsKey(new SourceIdentifier("module-without-revision")));
+        Assert.assertThat(resolvedModulesSchema.get(
+                new SourceIdentifier("module-without-revision")),
+                is(new URL("http://localhost:8181/yanglib/schemas/module-without-revision/")));
+    }
+
+    @Test
+    public void testCreateInvalidModulesEntries() throws Exception {
+        LibraryModulesSchemas libraryModulesSchemas =
+                LibraryModulesSchemas.create(getClass().getResource("/yang-library-fail.xml").toString());
+
+        final Map<SourceIdentifier, URL> resolvedModulesSchema = libraryModulesSchemas.getAvailableModels();
+        Assert.assertThat(resolvedModulesSchema.size(), is(1));
+
+        Assert.assertFalse(resolvedModulesSchema.containsKey(new SourceIdentifier("module-with-bad-url")));
+        Assert.assertFalse(resolvedModulesSchema.containsKey(
+                new SourceIdentifier("module-with-bad-revision", "bad-revision")));
+        Assert.assertTrue(resolvedModulesSchema.containsKey(new SourceIdentifier("good-ol-module")));
+    }
+
+
+    @Test
+    public void testCreateFromInvalidAll() throws Exception {
+        // test bad yang lib url
+        LibraryModulesSchemas libraryModulesSchemas = LibraryModulesSchemas.create("ObviouslyBadUrl");
+        Assert.assertThat(libraryModulesSchemas.getAvailableModels(), is(Collections.emptyMap()));
+
+        // TODO test also fail on json and xml parsing. But can we fail not on runtime exceptions?
+    }
+}
