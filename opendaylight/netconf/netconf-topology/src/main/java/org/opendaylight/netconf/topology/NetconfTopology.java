ae59747c07225dc30011da4a206ddc63fb134608
Tomas Cere <tcere@cisco.com>
2015-08-17 15:14:29 +0200

    BUG 2920: Extend and simplify netconf connector topology model.
    Add shared schema repository and netconf topology binding.
    Create a default schema repository instance.
    Add topology config.
    Simplifiy netconf connector model to use new netconf
    topology with backwards compatibility.
    Bump netconf connector schema revision.
    
    Change-Id: I3978a0a7e6e3c08dba52e65912bfe33ad46b4cf8
    Signed-off-by: Tomas Cere <tcere@cisco.com>

---
+++ opendaylight/netconf/netconf-topology/src/main/java/org/opendaylight/netconf/topology/NetconfTopology.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.netconf.topology;
+
+import com.google.common.util.concurrent.ListenableFuture;
+import org.opendaylight.netconf.sal.connect.netconf.listener.NetconfDeviceCapabilities;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.NodeId;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.Node;
+
+// TODO maybe rename to NetconfTopologyDispatcher?
+public interface NetconfTopology {
+
+    String getTopologyId();
+
+    ListenableFuture<NetconfDeviceCapabilities> connectNode(NodeId nodeId, Node configNode);
+
+    ListenableFuture<Void> disconnectNode(NodeId nodeId);
+}
