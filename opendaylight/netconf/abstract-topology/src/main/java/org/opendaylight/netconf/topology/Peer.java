427731d1c08d897e96094cb6674f66cd8cf3be44
Maros Marsalek <mmarsale@cisco.com>
2015-09-09 15:50:01 +0200

    Abstract-topology draft
    Change-Id: I6712bf05b9267bec2160a0addf148e911e1b79e5
    Signed-off-by: Maros Marsalek <mmarsale@cisco.com>
    Signed-off-by: Tomas Cere <tcere@cisco.com>

---
+++ opendaylight/netconf/abstract-topology/src/main/java/org/opendaylight/netconf/topology/Peer.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2015 Cisco Systems, Inc. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.netconf.topology;
+
+import com.google.common.annotations.Beta;
+
+/**
+ *
+ */
+@Beta
+public interface Peer<T extends Peer<T>> {
+
+    boolean isMaster();
+
+    Iterable<T> getPeers();
+
+    /**
+     * Used for communication between NodeAdministratorCallbacks
+     */
+    public interface PeerContext<M> {
+
+        // TODO the message needs to be serialized and sent through AKKA, how to achieve it ?
+        void notifyPeers(M msg);
+
+    }
+}
