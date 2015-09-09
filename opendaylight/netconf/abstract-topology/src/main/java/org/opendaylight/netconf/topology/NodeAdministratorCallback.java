dc0ebbaa4abc1ea6bba602906f053f081d1e88ab
Maros Marsalek <mmarsale@cisco.com>
2015-09-09 15:50:01 +0200

    Abstract-topology draft
    Change-Id: I6712bf05b9267bec2160a0addf148e911e1b79e5
    Signed-off-by: Maros Marsalek <mmarsale@cisco.com>
    Signed-off-by: Tomas Cere <tcere@cisco.com>

---
+++ opendaylight/netconf/abstract-topology/src/main/java/org/opendaylight/netconf/topology/NodeAdministratorCallback.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2015 Cisco Systems, Inc. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.netconf.topology;
+
+import com.google.common.annotations.Beta;
+
+@Beta
+public interface NodeAdministratorCallback<M> extends NodeAdministrator {
+
+    // TODO how to handle peer communication on NodeAdmin callback level ?
+
+    void setPeerContext(Peer.PeerContext<M> peerContext);
+
+    void handle(M msg);
+
+    interface NodeAdministratorCallbackFactory<M> {
+        NodeAdministratorCallback<M> create();
+    }
+}
