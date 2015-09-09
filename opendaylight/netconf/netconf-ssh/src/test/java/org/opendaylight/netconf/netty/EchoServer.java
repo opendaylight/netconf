14ae873b96978b227eed84c875341930ecd990dc
Tomas Cere <tcere@cisco.com>
2015-09-09 16:30:51 +0200

    Rename netconf-ssh test package
    Change-Id: I0a19631cdb4ce567f6d8a2f222b49b38641751a9
    Signed-off-by: Tomas Cere <tcere@cisco.com>

--- opendaylight/netconf/netconf-ssh/src/test/java/org/opendaylight/netconf/netty/EchoServer.java
+++ opendaylight/netconf/netconf-ssh/src/test/java/org/opendaylight/netconf/netty/EchoServer.java
@@ -6,7 +6,7 @@
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
 
-package org.opendaylight.controller.netconf.netty;
+package org.opendaylight.netconf.netty;
 
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.channel.ChannelFuture;
