529b64401bb6d9cb88d128d4670f3eed3c52c77f
Tomas Cere <tcere@cisco.com>
2015-09-23 16:17:46 +0200

    RoleChangeStrategy first draft
    Change-Id: I932d01437ff79d627da8ab8ffe42a46ce202c950
    Signed-off-by: Tomas Cere <tcere@cisco.com>

---
+++ opendaylight/netconf/abstract-topology/src/main/java/org/opendaylight/netconf/topology/util/NodeRoleChangeStrategy.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) 2015 Cisco Systems, Inc. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.netconf.topology.util;
+
+import org.opendaylight.controller.md.sal.common.api.clustering.CandidateAlreadyRegisteredException;
+import org.opendaylight.controller.md.sal.common.api.clustering.Entity;
+import org.opendaylight.controller.md.sal.common.api.clustering.EntityOwnershipChange;
+import org.opendaylight.controller.md.sal.common.api.clustering.EntityOwnershipListener;
+import org.opendaylight.controller.md.sal.common.api.clustering.EntityOwnershipService;
+import org.opendaylight.netconf.topology.RoleChangeStrategy;
+import org.opendaylight.netconf.topology.NodeListener;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class NodeRoleChangeStrategy implements RoleChangeStrategy, EntityOwnershipListener {
+
+    private static final Logger LOG = LoggerFactory.getLogger(NodeRoleChangeStrategy.class);
+
+    private final EntityOwnershipService entityOwnershipService;
+    private final String entityType;
+    private final String entityName;
+    private NodeListener ownershipCandidate;
+
+    public NodeRoleChangeStrategy(final EntityOwnershipService entityOwnershipService,
+                                  final String entityType,
+                                  final String entityName) {
+        this.entityOwnershipService = entityOwnershipService;
+        this.entityType = entityType;
+        this.entityName = entityName;
+    }
+
+    @Override
+    public void registerRoleCandidate(NodeListener electionCandidate) {
+        this.ownershipCandidate = electionCandidate;
+        try {
+            entityOwnershipService.registerCandidate(new Entity(entityType, entityName));
+            entityOwnershipService.registerListener(entityType, this);
+        } catch (CandidateAlreadyRegisteredException e) {
+            LOG.error("Candidate already registered for election", e);
+            throw new IllegalStateException("Candidate already registered for election", e);
+        }
+    }
+
+    @Override
+    public void onRoleChanged(RoleChangeDTO roleChangeDTO) {
+        ownershipCandidate.onRoleChanged(roleChangeDTO);
+    }
+
+    @Override
+    public void ownershipChanged(EntityOwnershipChange ownershipChange) {
+        ownershipCandidate.onRoleChanged(new RoleChangeDTO(ownershipChange.wasOwner(), ownershipChange.isOwner(), ownershipChange.hasOwner()));
+    }
+}
