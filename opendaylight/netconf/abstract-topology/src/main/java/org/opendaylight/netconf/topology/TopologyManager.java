b434a89ec14d915db4a62b48f1c969ef7fd8c8b0
Tomas Cere <tcere@cisco.com>
2015-10-15 15:52:03 +0200

    Abstract topology framework apis
    Change-Id: Ib3d8b9fab7132a25313094fe31c9a77028bb4ea0
    Signed-off-by: Tomas Cere <tcere@cisco.com>

---
+++ opendaylight/netconf/abstract-topology/src/main/java/org/opendaylight/netconf/topology/TopologyManager.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.netconf.topology;
+
+import akka.actor.TypedActor.Receiver;
+import com.google.common.annotations.Beta;
+import javax.annotation.Nonnull;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.NodeId;
+
+/**
+ * Top level topology manager that handles comunication between nodes, aggregates results, and handles writes into the datastore
+ */
+@Beta
+public interface TopologyManager extends NodeListener, Receiver, RemoteNodeListener{
+
+    void notifyNodeStatusChange(NodeId nodeId);
+
+    // DEBUG helper methods
+    @Nonnull
+    String getTopologyId();
+
+    int getId();
+
+    boolean hasAllPeersUp();
+
+    /* Add useful getters to retrieve nodes and the topology
+    @Nonnull
+    TA getTopology();
+
+    @Nonnull
+    TA getTopology(@Nonnull final LogicalDatastoreType type);
+
+    @Nonnull
+    NA getNode(@Nonnull final NodeId nodeId);
+
+    @Nonnull
+    NA getNode(@Nonnull final NodeId nodeId, @Nonnull final LogicalDatastoreType type);
+    */
+
+}
