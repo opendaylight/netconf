3c9f727fce799a055a162ae068ae8b8f5121f32b
Tomas Cere <tcere@cisco.com>
2015-10-15 16:21:46 +0200

    Netconf clustering topology implementation
    Change-Id: I52b5d2f309789402dc72319d59fb75a3ac4c8df1
    Signed-off-by: Tomas Cere <tcere@cisco.com>

---
+++ opendaylight/netconf/abstract-topology/src/main/java/org/opendaylight/netconf/topology/example/ExampleNodeManagerCallback.java
@@ -0,0 +1,210 @@
+/*
+ * Copyright (c) 2015 Cisco Systems, Inc. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.netconf.topology.example;
+
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+import com.google.common.base.Function;
+import com.google.common.util.concurrent.FutureCallback;
+import com.google.common.util.concurrent.Futures;
+import com.google.common.util.concurrent.ListenableFuture;
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import org.opendaylight.controller.md.sal.dom.api.DOMNotification;
+import org.opendaylight.controller.md.sal.dom.api.DOMRpcService;
+import org.opendaylight.netconf.sal.connect.api.RemoteDeviceHandler;
+import org.opendaylight.netconf.sal.connect.netconf.listener.NetconfDeviceCapabilities;
+import org.opendaylight.netconf.sal.connect.netconf.listener.NetconfSessionPreferences;
+import org.opendaylight.netconf.topology.NetconfTopology;
+import org.opendaylight.netconf.topology.NodeManagerCallback;
+import org.opendaylight.netconf.topology.RoleChangeStrategy;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netconf.node.topology.rev150114.NetconfNode;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netconf.node.topology.rev150114.NetconfNodeBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netconf.node.topology.rev150114.NetconfNodeFields;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.NodeId;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.Node;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.NodeBuilder;
+import org.opendaylight.yangtools.yang.model.api.SchemaContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ExampleNodeManagerCallback implements NodeManagerCallback, RemoteDeviceHandler<NetconfSessionPreferences>{
+
+    private static final Logger LOG = LoggerFactory.getLogger(ExampleNodeManagerCallback.class);
+
+    private boolean isMaster = false;
+    private NetconfTopology topologyDispatcher;
+    private final ActorSystem actorSystem;
+
+    private final RoleChangeStrategy roleChangeStrategy;
+
+    private String nodeId;
+    private String topologyId;
+
+    public ExampleNodeManagerCallback(final String nodeId,
+                                      final String topologyId,
+                                      final ActorSystem actorSystem,
+                                      final NetconfTopology topologyDispatcher,
+                                      final RoleChangeStrategy roleChangeStrategy) {
+        this.nodeId = nodeId;
+        this.topologyId = topologyId;
+        this.actorSystem = actorSystem;
+        this.topologyDispatcher = topologyDispatcher;
+        this.roleChangeStrategy = roleChangeStrategy;
+    }
+
+
+    @Nonnull
+    @Override public Node getInitialState(@Nonnull final NodeId nodeId,
+                                          @Nonnull final Node configNode) {
+        return new NodeBuilder().addAugmentation(NetconfNode.class,
+                new NetconfNodeBuilder().setConnectionStatus(NetconfNodeFields.ConnectionStatus.Connecting).build()).build();
+    }
+
+    @Nonnull @Override public Node getFailedState(@Nonnull final NodeId nodeId,
+                                                  @Nonnull final Node configNode) {
+        return new NodeBuilder().addAugmentation(NetconfNode.class,
+                new NetconfNodeBuilder().setConnectionStatus(NetconfNodeFields.ConnectionStatus.UnableToConnect).build()).build();
+    }
+
+    @Nonnull @Override public ListenableFuture<Node> nodeCreated(@Nonnull final NodeId nodeId,
+                                                                 @Nonnull final Node configNode) {
+        this.nodeId = nodeId.getValue();
+        // connect magic
+        // User logic goes here, f.ex connect your device
+        final ListenableFuture<NetconfDeviceCapabilities> connectionFuture = topologyDispatcher.connectNode(nodeId, configNode);
+
+        Futures.addCallback(connectionFuture, new FutureCallback<NetconfDeviceCapabilities>() {
+            @Override
+            public void onSuccess(@Nullable NetconfDeviceCapabilities result) {
+//                roleChangeStrategy.registerRoleCandidate(parentNodeManager);
+                topologyDispatcher.registerConnectionStatusListener(nodeId, ExampleNodeManagerCallback.this);
+            }
+
+            @Override
+            public void onFailure(Throwable t) {
+                LOG.error("Connection to device failed", t);
+            }
+        });
+
+        final NetconfNode netconfNode = configNode.getAugmentation(NetconfNode.class);
+
+        // transform future result into state that gets written into datastore
+        return Futures.transform(connectionFuture, new Function<NetconfDeviceCapabilities, Node>() {
+            @Nullable
+            @Override
+            public Node apply(NetconfDeviceCapabilities input) {
+                // build state data
+                return new NodeBuilder().addAugmentation(NetconfNode.class,
+                        new NetconfNodeBuilder()
+                                .setConnectionStatus(NetconfNodeFields.ConnectionStatus.Connected)
+                                .setHost(netconfNode.getHost())
+                                .setPort(netconfNode.getPort())
+                                .build()).build();
+            }
+        });
+    }
+
+    @Nonnull
+    @Override
+    public ListenableFuture<Node> nodeUpdated(@Nonnull final NodeId nodeId,
+                                              @Nonnull final Node configNode) {
+        // first disconnect this node
+        topologyDispatcher.unregisterMountPoint(nodeId);
+        topologyDispatcher.disconnectNode(nodeId);
+
+        // now reinit this connection with new settings
+        // TODO add a listener into the device communicator that will notify for connection changes
+        final ListenableFuture<NetconfDeviceCapabilities> connectionFuture = topologyDispatcher.connectNode(nodeId, configNode);
+
+        Futures.addCallback(connectionFuture, new FutureCallback<NetconfDeviceCapabilities>() {
+            @Override
+            public void onSuccess(@Nullable NetconfDeviceCapabilities result) {
+//                roleChangeStrategy.registerRoleCandidate(parentNodeManager);
+                topologyDispatcher.registerConnectionStatusListener(nodeId, ExampleNodeManagerCallback.this);
+            }
+
+            @Override
+            public void onFailure(Throwable t) {
+                LOG.error("Connection to device failed", t);
+            }
+        });
+
+        return Futures.transform(connectionFuture, new Function<NetconfDeviceCapabilities, Node>() {
+            @Nullable
+            @Override
+            public Node apply(NetconfDeviceCapabilities input) {
+                // build state data
+                return new NodeBuilder().addAugmentation(NetconfNode.class,
+                        new NetconfNodeBuilder().setConnectionStatus(NetconfNodeFields.ConnectionStatus.Connected).build()).build();
+            }
+        });
+    }
+
+    @Nonnull @Override public ListenableFuture<Void> nodeDeleted(@Nonnull final NodeId nodeId) {
+        // Disconnect
+        topologyDispatcher.unregisterMountPoint(nodeId);
+        return topologyDispatcher.disconnectNode(nodeId);
+    }
+
+    @Nonnull
+    @Override
+    public ListenableFuture<Node> getCurrentStatusForNode(@Nonnull NodeId nodeId) {
+        return null;
+    }
+
+    @Override
+    public void onRoleChanged(final RoleChangeDTO roleChangeDTO) {
+        if (roleChangeDTO.isOwner() && roleChangeDTO.wasOwner()) {
+            return;
+        }
+        isMaster = roleChangeDTO.isOwner();
+        if (isMaster) {
+            // unregister old mountPoint if ownership changed, register a new one
+//            topologyDispatcher.registerMountPoint(nodeId);
+        } else {
+//            topologyDispatcher.unregisterMountPoint(nodeId);
+        }
+    }
+
+    @Override
+    public void onDeviceConnected(final SchemaContext remoteSchemaContext, final NetconfSessionPreferences netconfSessionPreferences, final DOMRpcService deviceRpc) {
+        // we need to notify the higher level that something happened, get a current status from all other nodes, and aggregate a new result
+//        roleChangeStrategy.registerRoleCandidate(parentNodeManager);
+    }
+
+    @Override
+    public void onDeviceDisconnected() {
+        // we need to notify the higher level that something happened, get a current status from all other nodes, and aggregate a new result
+        // no need to remove mountpoint, we should receive onRoleChanged callback after unregistering from election that unregisters the mountpoint
+        roleChangeStrategy.unregisterRoleCandidate();
+    }
+
+    @Override
+    public void onDeviceFailed(Throwable throwable) {
+        // we need to notify the higher level that something happened, get a current status from all other nodes, and aggregate a new result
+        // no need to remove mountpoint, we should receive onRoleChanged callback after unregistering from election that unregisters the mountpoint
+        roleChangeStrategy.unregisterRoleCandidate();
+    }
+
+    @Override
+    public void onNotification(DOMNotification domNotification) {
+        //NOOP
+    }
+
+    @Override
+    public void close() {
+        //NOOP
+    }
+
+    @Override
+    public void onReceive(Object o, ActorRef actorRef) {
+
+    }
+}
