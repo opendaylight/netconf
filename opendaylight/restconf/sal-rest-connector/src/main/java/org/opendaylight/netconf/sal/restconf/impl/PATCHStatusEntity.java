1241c6ec0aad8b7653ddf16bcbbeadaf44097544
Martin Ciglan <mciglan@cisco.com>
2015-12-03 14:58:17 +0100

    Bug 3866: Support for Restconf HTTP Patch
    - JSON body reader/writer in place
    - XML body reader/writer in place
    - refactored BrokerFacade and RestconfImpl code to apply HTTP Patch within transaction
            in order to be able to rollback in case of error(s)
    
    Change-Id: I974c88fc846dfa6fd9b1672d431c0f246c904877
    Signed-off-by: Martin Ciglan <mciglan@cisco.com>

---
+++ opendaylight/restconf/sal-rest-connector/src/main/java/org/opendaylight/netconf/sal/restconf/impl/PATCHStatusEntity.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.netconf.sal.restconf.impl;
+
+import java.util.List;
+
+public class PATCHStatusEntity {
+
+    private final String editId;
+    private final List<RestconfError> editErrors;
+    private final boolean ok;
+
+    public PATCHStatusEntity(final String editId, final boolean ok, final List<RestconfError> editErrors) {
+        this.editId = editId;
+        this.ok = ok;
+        this.editErrors = editErrors;
+    }
+
+    public String getEditId() {
+        return editId;
+    }
+
+    public boolean isOk() {
+        return ok;
+    }
+
+    public List<RestconfError> getEditErrors() {
+        return editErrors;
+    }
+}
