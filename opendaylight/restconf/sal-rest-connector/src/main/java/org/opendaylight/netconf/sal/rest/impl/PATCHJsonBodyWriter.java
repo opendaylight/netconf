d66f5f2b541f95f23b4b2a9aa4318a34a31c1554
Martin Ciglan <mciglan@cisco.com>
2015-12-03 14:58:17 +0100

    Bug 3866: Support for Restconf HTTP Patch
    - infrastructure, resources
    - json parsing in place
    - each edit generates NormalizedNode result to resulting collection
    - xml reader skeleton
    - test
    
    Change-Id: I974c88fc846dfa6fd9b1672d431c0f246c904877
    Signed-off-by: Martin Ciglan <mciglan@cisco.com>

---
+++ opendaylight/restconf/sal-rest-connector/src/main/java/org/opendaylight/netconf/sal/rest/impl/PATCHJsonBodyWriter.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.netconf.sal.rest.impl;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Type;
+import javax.ws.rs.Produces;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.ext.MessageBodyWriter;
+import javax.ws.rs.ext.Provider;
+import org.opendaylight.netconf.sal.rest.api.Draft02.MediaTypes;
+import org.opendaylight.netconf.sal.rest.api.RestconfService;
+import org.opendaylight.netconf.sal.restconf.impl.PATCHContext;
+
+@Provider
+@Produces({ MediaTypes.PATCH_STATUS + RestconfService.JSON, MediaType.APPLICATION_JSON })
+public class PATCHJsonBodyWriter implements MessageBodyWriter<PATCHContext> {
+    @Override
+    public boolean isWriteable(Class<?> type, Type genericType, Annotation[] annotations, MediaType mediaType) {
+        return type.equals(PATCHContext.class);
+    }
+
+    @Override
+    public long getSize(PATCHContext patchContext, Class<?> type, Type genericType, Annotation[] annotations, MediaType mediaType) {
+        return -1;
+    }
+
+    @Override
+    public void writeTo(PATCHContext patchContext, Class<?> type, Type genericType, Annotation[] annotations, MediaType mediaType, MultivaluedMap<String, Object> httpHeaders, OutputStream entityStream) throws IOException, WebApplicationException {
+        //TODO implement, existing json/xml capabilities can be used, because there is no anyxml in response
+    }
+}
