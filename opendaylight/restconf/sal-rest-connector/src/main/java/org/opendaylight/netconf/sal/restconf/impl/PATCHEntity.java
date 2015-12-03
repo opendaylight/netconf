20dbcaf8f47b0ae539da2f429006a5ec492459e2
Martin Ciglan <mciglan@cisco.com>
2015-12-03 14:58:17 +0100

    DO NOT MERGE YET Bug 3866: Support for Restconf HTTP Patch
    - infrastructure, resources
    - json parsing in place
    - each edit generates NormalizedNode result to resulting collection
    - xml reader in progress & writer skeleton added
    - succesful test for json & xml
    
    Change-Id: I974c88fc846dfa6fd9b1672d431c0f246c904877
    Signed-off-by: Martin Ciglan <mciglan@cisco.com>

---
+++ opendaylight/restconf/sal-rest-connector/src/main/java/org/opendaylight/netconf/sal/restconf/impl/PATCHEntity.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.netconf.sal.restconf.impl;
+
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
+import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNode;
+
+public class PATCHEntity {
+
+    private String operation;
+    private YangInstanceIdentifier targetNode;
+    private NormalizedNode<?,?> node;
+
+    public PATCHEntity(String operation, YangInstanceIdentifier targetNode, NormalizedNode<?, ?> node) {
+        this.operation = operation;
+        this.targetNode = targetNode;
+        this.node = node;
+    }
+
+    public String getOperation() {
+        return operation;
+    }
+
+    public YangInstanceIdentifier getTargetNode() {
+        return targetNode;
+    }
+
+    public NormalizedNode<?, ?> getNode() {
+        return node;
+    }
+
+}
