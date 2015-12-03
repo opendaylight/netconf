89865108dbef563a3c5df92ed4e278a76e2807a4
Martin Ciglan <mciglan@cisco.com>
2015-12-03 14:58:17 +0100

    DO NOT MERGE YET Bug 3866: Support for Restconf HTTP Patch
    - infrastructure, resources
    - json parsing in place
    - each edit generates NormalizedNode result to resulting collection
    - xml reader & writer skeletons added
    - succesful test for json & xml
    
    Change-Id: I974c88fc846dfa6fd9b1672d431c0f246c904877
    Signed-off-by: Martin Ciglan <mciglan@cisco.com>

--- opendaylight/restconf/sal-rest-connector/src/test/java/org/opendaylight/controller/md/sal/rest/common/TestRestconfUtils.java
+++ opendaylight/restconf/sal-rest-connector/src/test/java/org/opendaylight/controller/md/sal/rest/common/TestRestconfUtils.java
@@ -13,6 +13,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
@@ -37,6 +38,10 @@ import org.opendaylight.yangtools.yang.model.api.SchemaNode;
 import org.opendaylight.yangtools.yang.model.parser.api.YangContextParser;
 import org.opendaylight.yangtools.yang.model.parser.api.YangSyntaxErrorException;
 import org.opendaylight.yangtools.yang.parser.impl.YangParserImpl;
+import org.opendaylight.yangtools.yang.parser.spi.meta.ReactorException;
+import org.opendaylight.yangtools.yang.parser.stmt.reactor.CrossSourceStatementReactor;
+import org.opendaylight.yangtools.yang.parser.stmt.rfc6020.YangInferencePipeline;
+import org.opendaylight.yangtools.yang.parser.stmt.rfc6020.YangStatementSourceImpl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.w3c.dom.Document;
@@ -180,6 +185,43 @@ public class TestRestconfUtils {
     private static SchemaContext loadSchemaContext(final String resourceDirectory) throws IOException {
         final Collection<File> testFiles = loadFiles(resourceDirectory);
         return parser.parseFiles(testFiles);
+    }
+
+    public static SchemaContext loadEffectiveSchemaContext(final String resourceDirectory) {
+        Preconditions.checkArgument(resourceDirectory != null, "Path can not be null.");
+        Preconditions.checkArgument((!resourceDirectory.isEmpty()), "Path can not be empty.");
+        try {
+            final CrossSourceStatementReactor.BuildAction reactor = YangInferencePipeline.RFC6020_REACTOR
+                    .newBuild();
+            File[] files;
+
+                files = new File(TestRestconfUtils.class.getResource(resourceDirectory).toURI()).listFiles();
+
+            for (File file : files) {
+                if (file.getName().endsWith(".yang")) {
+                    addSources(reactor, new YangStatementSourceImpl(file.getPath(), true));
+                } else {
+                    LOG.info("Ignoring non-yang file {}", file);
+                }
+            }
+            return reactor.buildEffective();
+
+        //TODO: bring up better error descriptions
+        } catch (ReactorException e) {
+            LOG.warn("Reactor error occured during processing source(s)", e);
+        } catch (URISyntaxException e) {
+            LOG.warn("File cannot be parsed as URI reference", e);
+        }
+
+        return null;
+    }
+
+    private static void addSources(
+            final CrossSourceStatementReactor.BuildAction reactor,
+            final YangStatementSourceImpl... sources) {
+        for (YangStatementSourceImpl source : sources) {
+            reactor.addSource(source);
+        }
     }
 
     private static SchemaContext addSchemaContext(final String resourceDirectory,
