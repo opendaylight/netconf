3a0f3f451a12e8dd7978bd642eb727f60f3265ae
Maros Marsalek <mmarsale@cisco.com>
2016-01-06 15:49:19 +0100

    BUG-5269 Enable device capabilities override
    So far, merge was the dafult strategy. From now on, override
    is also possible.
    
    Useful when it's necessary to completely replace device models
    with user/app models.
    
    Change-Id: I4b1bb68b8257e4a6889f54f7be0e6baee5d7617d
    Signed-off-by: Maros Marsalek <mmarsale@cisco.com>
    (cherry picked from commit 552cd5ad81b9cda47978e34414ccf3a80e84f872)

---
+++ opendaylight/netconf/sal-netconf-connector/src/main/java/org/opendaylight/netconf/sal/connect/netconf/listener/UserPrefenreces.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.netconf.sal.connect.netconf.listener;
+
+import javax.annotation.Nonnull;
+
+/**
+ * DTO with user capabilities to override or merge with device specific capabilities
+ */
+public class UserPrefenreces {
+
+    private final NetconfSessionPreferences sessionPreferences;
+    private final boolean override;
+
+    public UserPrefenreces(@Nonnull final NetconfSessionPreferences sessionPreferences, boolean override) {
+        this.sessionPreferences = sessionPreferences;
+        this.override = override;
+    }
+
+    public NetconfSessionPreferences getSessionPreferences() {
+        return sessionPreferences;
+    }
+
+    public boolean isOverride() {
+        return override;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuffer sb = new StringBuffer("UserPrefenreces{");
+        sb.append("sessionPreferences=").append(sessionPreferences);
+        sb.append(", override=").append(override);
+        sb.append('}');
+        return sb.toString();
+    }
+}
