7c52bce4d51c98396868f8fb78c9a15b2c3b97e5
Jakub Morvay <jmorvay@cisco.com>
2016-02-04 11:19:09 +0100

    Bug 2806 - Immediate and infinite reconnect attempts during negotiation
    Set connection to success, after successful netconf negotiation.
    
    Change-Id: Id044b618c3f996d4e93df2a2537ea7e905717477
    Signed-off-by: Jakub Morvay <jmorvay@cisco.com>

--- opendaylight/netconf/netconf-netty-util/src/main/java/org/opendaylight/netconf/nettyutil/handler/ssh/client/AsyncSshHandler.java
+++ opendaylight/netconf/netconf-netty-util/src/main/java/org/opendaylight/netconf/nettyutil/handler/ssh/client/AsyncSshHandler.java
@@ -13,6 +13,8 @@ import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelOutboundHandlerAdapter;
 import io.netty.channel.ChannelPromise;
+import io.netty.util.concurrent.Future;
+import io.netty.util.concurrent.GenericFutureListener;
 import java.io.IOException;
 import java.net.SocketAddress;
 import java.util.HashMap;
@@ -33,13 +35,10 @@ import org.slf4j.LoggerFactory;
  */
 public class AsyncSshHandler extends ChannelOutboundHandlerAdapter {
 
-    private static final Logger LOG = LoggerFactory.getLogger(AsyncSshHandler.class);
     public static final String SUBSYSTEM = "netconf";
-
     public static final SshClient DEFAULT_CLIENT = SshClient.setUpDefaultClient();
-
     public static final int SSH_DEFAULT_NIO_WORKERS = 8;
-
+    private static final Logger LOG = LoggerFactory.getLogger(AsyncSshHandler.class);
     // Disable default timeouts from mina sshd
     private static final long DEFAULT_TIMEOUT = -1L;
 
@@ -57,6 +56,7 @@ public class AsyncSshHandler extends ChannelOutboundHandlerAdapter {
 
     private final AuthenticationHandler authenticationHandler;
     private final SshClient sshClient;
+    private Future negotiationFuture;
 
     private AsyncSshHandlerReader sshReadAsyncListener;
     private AsyncSshHandlerWriter sshWriteAsyncHandler;
@@ -64,10 +64,12 @@ public class AsyncSshHandler extends ChannelOutboundHandlerAdapter {
     private ClientChannel channel;
     private ClientSession session;
     private ChannelPromise connectPromise;
+    private GenericFutureListener negotiationFutureListener;
 
 
-    public static AsyncSshHandler createForNetconfSubsystem(final AuthenticationHandler authenticationHandler) throws IOException {
-        return new AsyncSshHandler(authenticationHandler, DEFAULT_CLIENT);
+    public AsyncSshHandler(final AuthenticationHandler authenticationHandler, final SshClient sshClient, final Future negotiationFuture) throws IOException {
+        this(authenticationHandler, sshClient);
+        this.negotiationFuture = negotiationFuture;
     }
 
     /**
@@ -81,6 +83,24 @@ public class AsyncSshHandler extends ChannelOutboundHandlerAdapter {
         this.sshClient = Preconditions.checkNotNull(sshClient);
         // Start just in case
         sshClient.start();
+    }
+
+    public static AsyncSshHandler createForNetconfSubsystem(final AuthenticationHandler authenticationHandler) throws IOException {
+        return new AsyncSshHandler(authenticationHandler, DEFAULT_CLIENT);
+    }
+
+    /**
+     *
+     * Create AsyncSshHandler for netconf subsystem. Negotiation future has to be set to success after successful netconf
+     * negotiation.
+     *
+     * @param authenticationHandler
+     * @param negotiationFuture
+     * @return
+     * @throws IOException
+     */
+    public static AsyncSshHandler createForNetconfSubsystem(final AuthenticationHandler authenticationHandler, final Future negotiationFuture) throws IOException {
+        return new AsyncSshHandler(authenticationHandler, DEFAULT_CLIENT, negotiationFuture);
     }
 
     private void startSsh(final ChannelHandlerContext ctx, final SocketAddress address) {
@@ -150,7 +170,9 @@ public class AsyncSshHandler extends ChannelOutboundHandlerAdapter {
     private synchronized void handleSshChanelOpened(final ChannelHandlerContext ctx) {
         LOG.trace("SSH subsystem channel opened successfully on channel: {}", ctx.channel());
 
-        connectPromise.setSuccess();
+        if(negotiationFuture == null) {
+            connectPromise.setSuccess();
+        }
 
         // TODO we should also read from error stream and at least log from that
 
@@ -175,12 +197,13 @@ public class AsyncSshHandler extends ChannelOutboundHandlerAdapter {
 
     private synchronized void handleSshSetupFailure(final ChannelHandlerContext ctx, final Throwable e) {
         LOG.warn("Unable to setup SSH connection on channel: {}", ctx.channel(), e);
-        disconnect(ctx, ctx.newPromise());
 
         // If the promise is not yet done, we have failed with initial connect and set connectPromise to failure
         if(!connectPromise.isDone()) {
             connectPromise.setFailure(e);
         }
+
+        disconnect(ctx, ctx.newPromise());
     }
 
     @Override
@@ -192,6 +215,19 @@ public class AsyncSshHandler extends ChannelOutboundHandlerAdapter {
     public synchronized void connect(final ChannelHandlerContext ctx, final SocketAddress remoteAddress, final SocketAddress localAddress, final ChannelPromise promise) throws Exception {
         LOG.debug("SSH session connecting on channel {}. promise: {} ", ctx.channel(), connectPromise);
         this.connectPromise = promise;
+
+        if(negotiationFuture != null) {
+
+            negotiationFutureListener = new GenericFutureListener<Future<Object>>() {
+                @Override
+                public void operationComplete(Future future) throws Exception {
+                    if (future.isSuccess())
+                        connectPromise.setSuccess();
+                }
+            };
+            //complete connection promise with netconf negotiation future
+            negotiationFuture.addListener(negotiationFutureListener);
+        }
         startSsh(ctx, remoteAddress);
     }
 
@@ -215,6 +251,18 @@ public class AsyncSshHandler extends ChannelOutboundHandlerAdapter {
 
         if(sshReadAsyncListener != null) {
             sshReadAsyncListener.close();
+        }
+
+        //If connection promise is not already set, it means negotiation failed
+        //we must set connection promise to failure
+        if(!connectPromise.isDone()) {
+            connectPromise.setFailure(new IllegalStateException("Negotiation failed"));
+        }
+
+        //Remove listener from negotiation future, we don't want notifications
+        //from negotiation anymore
+        if(negotiationFuture != null) {
+            negotiationFuture.removeListener(negotiationFutureListener);
         }
 
         if(session!= null && !session.isClosed() && !session.isClosing()) {
