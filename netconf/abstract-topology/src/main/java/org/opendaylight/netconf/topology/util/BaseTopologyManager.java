f2ae117e02305e8998eda313efdf7e64adc9b01a
Andrej Mak <andmak@cisco.com>
2016-02-23 15:24:07 +0100

    Remove unneeded todos and fixmes
    AbstractNetconfTopology - default values should not be stored
    ClusteredNetconfTopology - default values should not be stored
    BaseTopologyManager - we want to keep successful connection alive
    KeepaliveSalFacade - if anything fails we log the exception and reconnect
    NetconfDeviceRpc - no need
    NetconfMonitoringServiceTestImpl - test is already implemented in another package
    NetconfTopology - no need
    TransactionProvider - now, every session is nicely isolated
    
    Change-Id: I019c0697172089add16c114b2aa1b8b19e949b28
    Signed-off-by: Andrej Mak <andmak@cisco.com>

--- netconf/abstract-topology/src/main/java/org/opendaylight/netconf/topology/util/BaseTopologyManager.java
+++ netconf/abstract-topology/src/main/java/org/opendaylight/netconf/topology/util/BaseTopologyManager.java
@@ -214,8 +214,6 @@ public final class BaseTopologyManager
                     // If the combined connection attempt failed, set the node to connection failed
                     LOG.debug("Futures aggregation failed");
                     naSalNodeWriter.update(nodeId, delegateTopologyHandler.getFailedState(nodeId, node));
-                    // FIXME disconnect those which succeeded
-                    // just issue a delete on delegateTopologyHandler that gets handled on lower level
                 }
             }, TypedActor.context().dispatcher());
 
@@ -382,8 +380,6 @@ public final class BaseTopologyManager
                     // If the combined connection attempt failed, set the node to connection failed
                     LOG.debug("Futures aggregation failed");
                     naSalNodeWriter.update(nodeId, delegateTopologyHandler.getFailedState(nodeId, null));
-                    // FIXME disconnect those which succeeded
-                    // just issue a delete on delegateTopologyHandler that gets handled on lower level
                 }
             });
             return;
