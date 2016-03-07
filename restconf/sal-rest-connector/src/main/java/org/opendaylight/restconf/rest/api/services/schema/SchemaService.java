8c81b585042f222433108ad38a882c64d10ca87a
Jakub Toth <jatoth@cisco.com>
2016-03-07 11:18:47 +0100

    restconf services api + RestconfConstants
    Change-Id: I0b2ff2099dfaa8e064893525c7ea4a56168bbcdb
    Signed-off-by: Jakub Toth <jatoth@cisco.com>

---
+++ restconf/sal-rest-connector/src/main/java/org/opendaylight/restconf/rest/api/services/schema/SchemaService.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.restconf.rest.api.services.schema;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import org.opendaylight.netconf.md.sal.rest.schema.SchemaExportContext;
+import org.opendaylight.restconf.rest.api.Draft09;
+import org.opendaylight.restconf.utils.RestconfConstants;
+
+@Path("/")
+public interface SchemaService {
+
+    @GET
+    @Produces({ Draft09.MediaTypes.YIN + RestconfConstants.XML, Draft09.MediaTypes.YANG })
+    @Path("data/ietf-yang-library:modules/module/{identifier:.+}/schema")
+    SchemaExportContext getSchema(@PathParam("identifier") String mountAndModuleId);
+}
