656416969a9a6821591e69ac1d86ad5055f99787
Jakub Toth <jatoth@cisco.com>
2016-03-07 11:18:47 +0100

    Bug 5526 - Restconf services api + RestconfConstants
    Change-Id: I0b2ff2099dfaa8e064893525c7ea4a56168bbcdb
    Signed-off-by: Jakub Toth <jatoth@cisco.com>

---
+++ restconf/sal-rest-connector/src/main/java/org/opendaylight/restconf/utils/RestconfConstants.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.restconf.utils;
+
+/**
+ * Util class for Restconf constants.
+ *
+ */
+public class RestconfConstants {
+
+    public static final String XML = "+xml";
+    public static final String JSON = "+json";
+
+    public static final String IDENTIFIER = "identifier";
+
+    private RestconfConstants() {
+        throw new UnsupportedOperationException("Util class");
+    }
+}
