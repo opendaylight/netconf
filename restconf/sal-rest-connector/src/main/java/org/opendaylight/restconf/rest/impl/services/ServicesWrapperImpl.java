a76a3c4f28691f30afa6563bce0338e6e03303ac
Jakub Toth <jatoth@cisco.com>
2016-03-07 14:33:34 +0100

    services implementations
    Change-Id: I5ddccc91feaf3ebfd7f8b40c2f954c4506995622
    Signed-off-by: Jakub Toth <jatoth@cisco.com>

---
+++ restconf/sal-rest-connector/src/main/java/org/opendaylight/restconf/rest/impl/services/ServicesWrapperImpl.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.restconf.rest.impl.services;
+
+import javax.ws.rs.core.UriInfo;
+import org.opendaylight.netconf.sal.restconf.impl.NormalizedNodeContext;
+import org.opendaylight.restconf.rest.api.connector.RestSchemaController;
+import org.opendaylight.restconf.rest.api.services.RestconfModulesService;
+import org.opendaylight.restconf.rest.api.services.RestconfOperationsService;
+import org.opendaylight.restconf.rest.api.services.RestconfStreamsService;
+import org.opendaylight.restconf.rest.api.services.ServicesWrapper;
+
+public class ServicesWrapperImpl implements ServicesWrapper {
+
+    private final RestconfModulesService delegatingRestconfModulesService;
+    private final RestconfOperationsService delegatingRestconfOperationsService;
+    private final RestconfStreamsService delegatingRestconfStreamsService;
+
+    public ServicesWrapperImpl(final RestSchemaController restSchemaController) {
+        this.delegatingRestconfModulesService = new RestconfModulesServiceImpl(restSchemaController);
+        this.delegatingRestconfOperationsService = new RestconfOperationsServiceImpl(restSchemaController);
+        this.delegatingRestconfStreamsService = new RestconfStreamsServiceImpl(restSchemaController);
+    }
+
+    @Override
+    public NormalizedNodeContext getModules(final UriInfo uriInfo) {
+        return this.delegatingRestconfModulesService.getModules(uriInfo);
+    }
+
+    @Override
+    public NormalizedNodeContext getModules(final String identifier, final UriInfo uriInfo) {
+        return this.delegatingRestconfModulesService.getModules(identifier, uriInfo);
+    }
+
+    @Override
+    public NormalizedNodeContext getModule(final String identifier, final UriInfo uriInfo) {
+        return this.delegatingRestconfModulesService.getModules(identifier, uriInfo);
+    }
+
+    @Override
+    public NormalizedNodeContext getOperations(final UriInfo uriInfo) {
+        return this.delegatingRestconfOperationsService.getOperations(uriInfo);
+    }
+
+    @Override
+    public NormalizedNodeContext getOperations(final String identifier, final UriInfo uriInfo) {
+        return this.delegatingRestconfOperationsService.getOperations(identifier, uriInfo);
+    }
+
+    @Override
+    public NormalizedNodeContext getAvailableStreams(final UriInfo uriInfo) {
+        return this.delegatingRestconfStreamsService.getAvailableStreams(uriInfo);
+    }
+
+}
