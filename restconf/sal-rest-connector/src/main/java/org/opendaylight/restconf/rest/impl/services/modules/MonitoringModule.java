96c57fcf4c9ba2e4021ce09b51f3272b3c0c0c66
Jakub Toth <jatoth@cisco.com>
2016-03-07 14:33:34 +0100

    services implementations
    Change-Id: I5ddccc91feaf3ebfd7f8b40c2f954c4506995622
    Signed-off-by: Jakub Toth <jatoth@cisco.com>

---
+++ restconf/sal-rest-connector/src/main/java/org/opendaylight/restconf/rest/impl/services/modules/MonitoringModule.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.restconf.rest.impl.services.modules;
+
+import org.opendaylight.netconf.sal.restconf.impl.RestconfDocumentedException;
+import org.opendaylight.netconf.sal.restconf.impl.RestconfError.ErrorTag;
+import org.opendaylight.netconf.sal.restconf.impl.RestconfError.ErrorType;
+import org.opendaylight.restconf.rest.api.connector.RestSchemaController;
+import org.opendaylight.yangtools.yang.model.api.Module;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MonitoringModule {
+
+    private static final Logger LOG = LoggerFactory.getLogger(RestconfModul.class);
+
+    protected final RestSchemaController restSchemaController;
+
+    public MonitoringModule(final RestSchemaController restSchemaController) {
+        this.restSchemaController = restSchemaController;
+    }
+
+    protected Module getMonitoringModule() {
+        final Module monitoringModule = this.restSchemaController.getMonitoringModule();
+        if (monitoringModule == null) {
+            LOG.debug("ietf-restconf module was not found.");
+            throw new RestconfDocumentedException("ietf-restconf module was not found.", ErrorType.APPLICATION,
+                    ErrorTag.OPERATION_NOT_SUPPORTED);
+        }
+        return monitoringModule;
+    }
+}
