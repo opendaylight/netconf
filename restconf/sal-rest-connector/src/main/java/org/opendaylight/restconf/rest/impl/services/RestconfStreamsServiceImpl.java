220a1f4ed071f13816ec269c01db588d0d9368ea
Jakub Toth <jatoth@cisco.com>
2016-03-07 14:33:34 +0100

    services implementations
    Change-Id: I5ddccc91feaf3ebfd7f8b40c2f954c4506995622
    Signed-off-by: Jakub Toth <jatoth@cisco.com>

---
+++ restconf/sal-rest-connector/src/main/java/org/opendaylight/restconf/rest/impl/services/RestconfStreamsServiceImpl.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.restconf.rest.impl.services;
+
+import javax.ws.rs.core.UriInfo;
+import org.opendaylight.netconf.sal.restconf.impl.NormalizedNodeContext;
+import org.opendaylight.restconf.rest.api.connector.RestSchemaController;
+import org.opendaylight.restconf.rest.api.services.RestconfStreamsService;
+
+public class RestconfStreamsServiceImpl implements RestconfStreamsService {
+
+    private final RestSchemaController restSchemaController;
+
+    public RestconfStreamsServiceImpl(final RestSchemaController restSchemaController) {
+        this.restSchemaController = restSchemaController;
+    }
+
+    @Override
+    public NormalizedNodeContext getAvailableStreams(final UriInfo uriInfo) {
+        return null;
+    }
+
+}
