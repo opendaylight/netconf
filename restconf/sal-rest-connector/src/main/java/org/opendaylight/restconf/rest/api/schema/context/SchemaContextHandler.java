605c8c022ef3b99e0144ff607553d4e43008ea75
Jakub Toth <jatoth@cisco.com>
2016-03-08 16:13:23 +0100

    Bug 5526 - Handling schema context for restconf
    Change-Id: Ia25fc20d73b844960fe5d9a6b845f73690390aac
    Signed-off-by: Jakub Toth <jatoth@cisco.com>

---
+++ restconf/sal-rest-connector/src/main/java/org/opendaylight/restconf/rest/api/schema/context/SchemaContextHandler.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.restconf.rest.api.schema.context;
+
+import org.opendaylight.yangtools.yang.model.api.SchemaContext;
+import org.opendaylight.yangtools.yang.model.api.SchemaContextListener;
+
+/**
+ * Handling schema context:
+ * <ul>
+ * <li>Retention
+ * <li>Update
+ * </ul>
+ */
+public interface SchemaContextHandler extends SchemaContextListener {
+
+    /**
+     * Get the {@link SchemaContext}.
+     *
+     * @return {@link SchemaContext}
+     */
+    SchemaContext getSchemaContext();
+}
