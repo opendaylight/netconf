ed60804bf6e8ae7fd23f349aedbcc498fe6184b5
Jakub Toth <jatoth@cisco.com>
2016-03-11 10:59:06 +0100

    Bug 5526 - Implementation of SchemaContextHandler
    Change-Id: I8575639228e498a16b350a4134b9928089091b4f
    Signed-off-by: Jakub Toth <jatoth@cisco.com>

---
+++ restconf/sal-rest-connector/src/main/java/org/opendaylight/restconf/rest/impl/schema/context/SchemaContextHandlerImpl.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.restconf.rest.impl.schema.context;
+
+import org.opendaylight.restconf.rest.api.schema.context.SchemaContextHandler;
+import org.opendaylight.yangtools.yang.model.api.SchemaContext;
+
+/**
+ * Implementation of {@link SchemaContextHandler}
+ *
+ */
+public class SchemaContextHandlerImpl implements SchemaContextHandler {
+
+    private SchemaContext context;
+
+    @Override
+    public void onGlobalContextUpdated(SchemaContext context) {
+        context = null;
+        this.context = context;
+    }
+
+    @Override
+    public SchemaContext getSchemaContext() {
+        return this.context;
+    }
+}
