1338fce0a65dd3a7b12967345858fdece80fd400
Jakub Toth <jatoth@cisco.com>
2016-03-14 08:01:00 +0100

    rest connector provider
    Change-Id: I8d89b01b39d32b9491c7c4293c20820720a47b5c
    Signed-off-by: Jakub Toth <jatoth@cisco.com>

---
+++ restconf/sal-rest-connector/src/main/java/org/opendaylight/restconf/rest/RestConnectorProvider.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.restconf.rest;
+
+import java.util.Collection;
+import java.util.Collections;
+import org.opendaylight.controller.sal.core.api.Broker.ProviderSession;
+import org.opendaylight.controller.sal.core.api.Provider;
+import org.opendaylight.controller.sal.core.api.model.SchemaService;
+import org.opendaylight.netconf.sal.streams.websockets.WebSocketServer;
+import org.opendaylight.restconf.rest.api.connector.RestConnector;
+import org.opendaylight.restconf.rest.api.connector.RestSchemaController;
+import org.opendaylight.restconf.rest.impl.connector.RestSchemaControllerImpl;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev100924.PortNumber;
+import org.opendaylight.yangtools.concepts.ListenerRegistration;
+import org.opendaylight.yangtools.yang.model.api.SchemaContextListener;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import com.google.common.base.Preconditions;
+
+public class RestConnectorProvider implements Provider, RestConnector, AutoCloseable {
+
+    private static final Logger LOG = LoggerFactory.getLogger(RestConnectorProvider.class);
+
+    private final PortNumber port;
+
+    private Thread webSocketServerThread;
+    private ListenerRegistration<SchemaContextListener> registerSchemaContextListener;
+
+    public RestConnectorProvider(final PortNumber port) {
+        this.port = port;
+    }
+
+    @Override
+    public void onSessionInitiated(final ProviderSession session) {
+        LOG.debug("REST_CONNECTOR_BUNDLE: BUNDLE IS STARTING");
+        final SchemaService schemaService = Preconditions.checkNotNull(session.getService(SchemaService.class));
+        final RestSchemaController restSchemaController = new RestSchemaControllerImpl();
+        restSchemaController.setGlobalSchema(schemaService.getGlobalContext());
+        this.registerSchemaContextListener = schemaService.registerSchemaContextListener(restSchemaController);
+    }
+
+    @Override
+    public Collection<ProviderFunctionality> getProviderFunctionality() {
+        return Collections.emptySet();
+    }
+
+    @Override
+    public void close() throws Exception {
+        // TODO Auto-generated method stub
+    }
+
+    private void streamWebSocketInit() {
+        this.webSocketServerThread = new Thread(WebSocketServer.createInstance(this.port.getValue().intValue()));
+        this.webSocketServerThread.setName("Web socket server on port " + this.port);
+        this.webSocketServerThread.start();
+    }
+}
