5373a6eaed3e27e26c5b51bd89347d30d637cd9d
adetalhouet <adetalhouet@inocybe.com>
2016-03-14 09:58:05 -0400

    Bug 5475 - File descriptor leak on netconf connector reconnects
    Close eventLoop to release resources
    
    Change-Id: If239f87ea4ff707f564f7f10e4f50481033f77fd
    Signed-off-by: adetalhouet <adetalhouet@inocybe.com>

--- netconf/netconf-netty-util/src/test/java/org/opendaylight/netconf/nettyutil/handler/ssh/client/AsyncSshHandlerTest.java
+++ netconf/netconf-netty-util/src/test/java/org/opendaylight/netconf/nettyutil/handler/ssh/client/AsyncSshHandlerTest.java
@@ -33,6 +33,8 @@ import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelPromise;
 import io.netty.channel.DefaultChannelPromise;
+import io.netty.channel.EventLoop;
+import io.netty.util.concurrent.Future;
 import java.io.IOException;
 import java.net.SocketAddress;
 import org.apache.sshd.ClientChannel;
@@ -75,6 +77,8 @@ public class AsyncSshHandlerTest {
     private SocketAddress remoteAddress;
     @Mock
     private SocketAddress localAddress;
+    @Mock
+    private EventLoop eventLoop;
 
     private AsyncSshHandler asyncSshHandler;
 
@@ -91,6 +95,7 @@ public class AsyncSshHandlerTest {
         stubSshClient();
         stubChannel();
         stubCtx();
+        stubEventLoop();
         stubRemoteAddress();
 
         promise = getMockedPromise();
@@ -150,6 +155,11 @@ public class AsyncSshHandlerTest {
 
     private void stubChannel() {
         doReturn("channel").when(channel).toString();
+        doReturn(eventLoop).when(channel).eventLoop();
+    }
+
+    private void stubEventLoop() {
+        doReturn(mock(Future.class)).when(eventLoop).shutdownGracefully();
     }
 
     private void stubSshClient() {
