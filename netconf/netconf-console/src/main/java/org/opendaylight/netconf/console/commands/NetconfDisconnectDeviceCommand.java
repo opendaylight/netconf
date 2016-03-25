bf2e96452f1fe8281ed0a09fffcd17c793b27c12
Rashmi Pujar <rpujar@inocybe.com>
2016-03-25 11:23:10 -0400

    Add Karaf CLI for NETCONF CRUD commands
    Change-Id: I200151c3f8eea34d667f4a5187b69365931e0cb0
    Signed-off-by: Rashmi Pujar <rpujar@inocybe.com>
    Signed-off by: Alexis de Talhouët <adetalhouet@inocybe.com>

---
+++ netconf/netconf-console/src/main/java/org/opendaylight/netconf/console/commands/NetconfDisconnectDeviceCommand.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright (c) 2016 Inocybe Technologies and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.netconf.console.commands;
+
+import org.apache.karaf.shell.commands.Command;
+import org.apache.karaf.shell.commands.Option;
+import org.apache.karaf.shell.console.AbstractAction;
+import org.opendaylight.netconf.console.api.NetconfCommands;
+
+import com.google.common.base.Strings;
+
+@Command(name = "netconf:disconnect-device", scope = "netconf", description = "Disconnect netconf device.")
+public class NetconfDisconnectDeviceCommand extends AbstractAction {
+
+    protected final NetconfCommands service;
+
+    public NetconfDisconnectDeviceCommand(final NetconfCommands service) {
+        this.service = service;
+    }
+
+    @Option(name = "-i",
+            aliases = { "--ipaddress" },
+            description = "IP address of the netconf device",
+            required = false,
+            multiValued = false)
+    private String deviceIp = "";
+
+    @Option(name = "-p",
+            aliases = { "--port" },
+            description = "Port of the netconf device",
+            required = false,
+            multiValued = false)
+    private String devicePort;
+
+    @Option(name = "-id",
+            aliases = { "--identifier" },
+            description = "Node Identifier of the netconf device",
+            required = false,
+            multiValued = false)
+    private String deviceId = "";
+
+    @Override
+    protected Object doExecute() throws Exception {
+        boolean status = false;
+        if (!Strings.isNullOrEmpty(deviceId)) {
+            status = service.disconnectDevice(deviceId);
+        } else {
+            if (!NetconfCommandUtils.isIpValid(deviceIp) && !NetconfCommandUtils.isPortValid(devicePort)) {
+                return "Invalid IP:" + deviceIp + " or Port:" + devicePort + "Please enter a valid entry to proceed.";
+            }
+            status = service.disconnectDevice(deviceIp, devicePort);
+        }
+        final String message = status ? "Netconf connector disconnected succesfully"
+                : "Failed to disconnect netconf connector. Refer to karaf.log for details.";
+        return message;
+    }
+}
