c996fd12a9a5826a56f5dd23cbc48e68c55e2a9f
Jakub Toth <jatoth@cisco.com>
2016-04-22 11:12:40 +0200

    Bug 5526 - Mark old services methods like depricated
    Change-Id: I8f65897d45327d2bf257f51fc541f9fc1210cc08
    Signed-off-by: Jakub Toth <jatoth@cisco.com>

--- restconf/sal-rest-connector/src/main/java/org/opendaylight/netconf/md/sal/rest/schema/SchemaRetrievalService.java
+++ restconf/sal-rest-connector/src/main/java/org/opendaylight/netconf/md/sal/rest/schema/SchemaRetrievalService.java
@@ -12,7 +12,13 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import org.opendaylight.restconf.rest.api.services.schema.RestconfSchemaService;
 
+/**
+ * @deprecated do not use this api. It is replaced by
+ *             {@link RestconfSchemaService}
+ */
+@Deprecated
 @Beta
 public interface SchemaRetrievalService {
 
