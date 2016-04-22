4ccf961b5e075d98156e4e59325f8220df671a73
Ivan Hrasko <ivan.hrasko@pantheon.tech>
2016-04-22 16:04:01 +0200

    Bug 5730 - Delete subset of list items using PATCH?
    - fix: edit is not ignored when end of input is reached
    - fix: only create, merge, replace and insert operations require value to be present
    - fix: if value is not present with create, merge, replace or insert operation then error 400 is returned
    - fix: if value is used with other operations error 400 is returned
    - fix: deleting sublist values instead of full parent
    - fix: order of patch edit fields is not important anymore (target, value)
    - move class StringModuleInstanceIdentifierCodec to own file
    
    Change-Id: I4c3f407858034b49795034da6c93b41f9d891b62
    Signed-off-by: Ivan Hrasko <ivan.hrasko@pantheon.tech>

--- opendaylight/restconf/sal-rest-connector/src/test/java/org/opendaylight/controller/sal/rest/impl/test/providers/TestJsonPATCHBodyReader.java
+++ opendaylight/restconf/sal-rest-connector/src/test/java/org/opendaylight/controller/sal/rest/impl/test/providers/TestJsonPATCHBodyReader.java
@@ -9,6 +9,8 @@
 package org.opendaylight.controller.sal.rest.impl.test.providers;
 
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 import java.io.InputStream;
 import javax.ws.rs.core.MediaType;
@@ -16,6 +18,7 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import org.opendaylight.netconf.sal.rest.impl.JsonToPATCHBodyReader;
 import org.opendaylight.netconf.sal.restconf.impl.PATCHContext;
+import org.opendaylight.netconf.sal.restconf.impl.RestconfDocumentedException;
 import org.opendaylight.yangtools.yang.model.api.SchemaContext;
 
 public class TestJsonPATCHBodyReader extends AbstractBodyReaderTest {
@@ -50,5 +53,42 @@ public class TestJsonPATCHBodyReader extends AbstractBodyReaderTest {
         final PATCHContext returnValue = jsonPATCHBodyReader
                 .readFrom(null, null, null, mediaType, null, inputStream);
         checkPATCHContext(returnValue);
+    }
+
+    @Test
+    public void modulePATCHCreateAndDeleteTest() throws Exception {
+        final String uri = "instance-identifier-patch-module:patch-cont/my-list1/leaf1";
+        mockBodyReader(uri, jsonPATCHBodyReader, false);
+
+        final InputStream inputStream = TestJsonBodyReader.class
+                .getResourceAsStream("/instanceidentifier/json/jsonPATCHdataCreateAndDelete.json");
+
+        final PATCHContext returnValue = jsonPATCHBodyReader
+                .readFrom(null, null, null, mediaType, null, inputStream);
+        checkPATCHContext(returnValue);
+    }
+
+    // Test should fail due to missing value node when attempt to invoke create operation
+    @Test(expected = RestconfDocumentedException.class)
+    public void modulePATCHValueMissingTest() throws Exception {
+        final String uri = "instance-identifier-patch-module:patch-cont/my-list1/leaf1";
+        mockBodyReader(uri, jsonPATCHBodyReader, false);
+
+        final InputStream inputStream = TestJsonBodyReader.class
+                .getResourceAsStream("/instanceidentifier/json/jsonPATCHdataValueMissing.json");
+
+        jsonPATCHBodyReader.readFrom(null, null, null, mediaType, null, inputStream);
+    }
+
+    // Test should fail due to present value node when attempt to invoke delete operation
+    @Test(expected = RestconfDocumentedException.class)
+    public void modulePATCHValueNotSupportedTest() throws Exception {
+        final String uri = "instance-identifier-patch-module:patch-cont/my-list1/leaf1";
+        mockBodyReader(uri, jsonPATCHBodyReader, false);
+
+        final InputStream inputStream = TestJsonBodyReader.class
+                .getResourceAsStream("/instanceidentifier/json/jsonPATCHdataValueNotSupported.json");
+
+        jsonPATCHBodyReader.readFrom(null, null, null, mediaType, null, inputStream);
     }
 }
