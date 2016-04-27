66624e4137965fbd44db5dd01e50ecc78aa72853
Jakub Toth <jatoth@cisco.com>
2016-04-27 15:15:57 +0200

    Bug 5526 - Wrap providers of both implementations of restconf.
    Change-Id: Ic90c49f50d63d6ca99b149e69de236ef3172cb59
    Signed-off-by: Jakub Toth <jatoth@cisco.com>

--- restconf/sal-rest-connector/src/main/java/org/opendaylight/controller/config/yang/md/sal/rest/connector/RestConnectorModule.java
+++ restconf/sal-rest-connector/src/main/java/org/opendaylight/controller/config/yang/md/sal/rest/connector/RestConnectorModule.java
@@ -8,18 +8,18 @@
 
 package org.opendaylight.controller.config.yang.md.sal.rest.connector;
 
-import org.opendaylight.netconf.sal.restconf.impl.RestconfProviderImpl;
+import org.opendaylight.RestconfWrapperProviders;
 
 
 public class RestConnectorModule extends org.opendaylight.controller.config.yang.md.sal.rest.connector.AbstractRestConnectorModule {
 
     private static RestConnectorRuntimeRegistration runtimeRegistration;
 
-    public RestConnectorModule(org.opendaylight.controller.config.api.ModuleIdentifier identifier, org.opendaylight.controller.config.api.DependencyResolver dependencyResolver) {
+    public RestConnectorModule(final org.opendaylight.controller.config.api.ModuleIdentifier identifier, final org.opendaylight.controller.config.api.DependencyResolver dependencyResolver) {
         super(identifier, dependencyResolver);
     }
 
-    public RestConnectorModule(org.opendaylight.controller.config.api.ModuleIdentifier identifier, org.opendaylight.controller.config.api.DependencyResolver dependencyResolver, org.opendaylight.controller.config.yang.md.sal.rest.connector.RestConnectorModule oldModule, java.lang.AutoCloseable oldInstance) {
+    public RestConnectorModule(final org.opendaylight.controller.config.api.ModuleIdentifier identifier, final org.opendaylight.controller.config.api.DependencyResolver dependencyResolver, final org.opendaylight.controller.config.yang.md.sal.rest.connector.RestConnectorModule oldModule, final java.lang.AutoCloseable oldInstance) {
         super(identifier, dependencyResolver, oldModule, oldInstance);
     }
 
@@ -30,21 +30,16 @@ public class RestConnectorModule extends org.opendaylight.controller.config.yang
 
     @Override
     public java.lang.AutoCloseable createInstance() {
-        // Create an instance of our provider
-        RestconfProviderImpl instance = new RestconfProviderImpl();
-        // Set its port
-        instance.setWebsocketPort(getWebsocketPort());
-        // Register it with the Broker
-        getDomBrokerDependency().registerProvider(instance);
+        final RestconfWrapperProviders wrapperProviders = new RestconfWrapperProviders(getWebsocketPort());
+        wrapperProviders.registerProviders(getDomBrokerDependency());
 
         if(runtimeRegistration != null){
             runtimeRegistration.close();
         }
 
-        runtimeRegistration =
-            getRootRuntimeBeanRegistratorWrapper().register(instance);
+        runtimeRegistration = wrapperProviders.runtimeRegistration(getRootRuntimeBeanRegistratorWrapper());
 
-        return instance;
+        return wrapperProviders;
     }
 }
 
