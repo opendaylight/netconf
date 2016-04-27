76447f515d62a56a57d4b93a5267582c7c242bb2
Jakub Toth <jatoth@cisco.com>
2016-04-27 16:44:53 +0200

    Register listener throught bundle service
      * register new service like RestApplication while is creating instance from Application in jersey
      * get this service in provider and set global schema to RestSchemaControllerImpl throught interface RestconfApplicationService
    
    Change-Id: Ifb42aed73f7524a07174c0c5e966ca44bcb228d8
    Signed-off-by: Jakub Toth <jatoth@cisco.com>

--- restconf/sal-rest-connector/src/main/java/org/opendaylight/restconf/rest/RestConnectorProvider.java
+++ restconf/sal-rest-connector/src/main/java/org/opendaylight/restconf/rest/RestConnectorProvider.java
@@ -16,6 +16,9 @@ import org.opendaylight.controller.sal.core.api.model.SchemaService;
 import org.opendaylight.netconf.sal.rest.api.RestConnector;
 import org.opendaylight.yangtools.concepts.ListenerRegistration;
 import org.opendaylight.yangtools.yang.model.api.SchemaContextListener;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.FrameworkUtil;
+import org.osgi.framework.ServiceReference;
 
 /**
  * Provider for restconf draft11.
@@ -28,6 +31,16 @@ public class RestConnectorProvider implements Provider, RestConnector, AutoClose
     @Override
     public void onSessionInitiated(final ProviderSession session) {
         final SchemaService schemaService = Preconditions.checkNotNull(session.getService(SchemaService.class));
+        final RestconfApplication restApp = getObjcetFromBundleContext(RestconfApplication.class,
+                RestconfApplicationService.class.getName());
+
+        this.listenerRegistration = schemaService.registerSchemaContextListener(restApp.getRestConnector());
+    }
+
+    private <T> T getObjcetFromBundleContext(final Class<T> type, final String serviceRefName) {
+        final BundleContext bundleContext = FrameworkUtil.getBundle(getClass()).getBundleContext();
+        final ServiceReference<?> serviceReference = bundleContext.getServiceReference(serviceRefName);
+        return (T) bundleContext.getService(serviceReference);
     }
 
     @Override
