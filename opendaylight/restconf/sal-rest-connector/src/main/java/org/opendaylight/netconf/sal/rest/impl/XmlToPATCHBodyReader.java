40a8d7c49c8f628f5c6eda399c4694d075317ef1
Ivan Hrasko <ivan.hrasko@pantheon.tech>
2016-04-28 16:23:53 +0200

    Bug 5509: HTTP Patch in Restconf doesn't support general absolute or relative target xpath
    Change-Id: I9828ceb7368760642b22b3a336884b949f0edfcc
    Signed-off-by: Ivan Hrasko <ivan.hrasko@pantheon.tech>

--- opendaylight/restconf/sal-rest-connector/src/main/java/org/opendaylight/netconf/sal/rest/impl/XmlToPATCHBodyReader.java
+++ opendaylight/restconf/sal-rest-connector/src/main/java/org/opendaylight/netconf/sal/rest/impl/XmlToPATCHBodyReader.java
@@ -8,13 +8,16 @@
 
 package org.opendaylight.netconf.sal.rest.impl;
 
+import com.google.common.base.Splitter;
 import com.google.common.collect.ImmutableList;
 import java.io.IOException;
 import java.io.InputStream;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Type;
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.WebApplicationException;
@@ -25,30 +28,34 @@ import javax.ws.rs.ext.Provider;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import org.opendaylight.netconf.sal.rest.api.Draft02.MediaTypes;
+import org.opendaylight.netconf.sal.rest.api.RestconfService;
+import org.opendaylight.netconf.sal.restconf.impl.InstanceIdentifierContext;
+import org.opendaylight.netconf.sal.restconf.impl.PATCHContext;
 import org.opendaylight.netconf.sal.restconf.impl.PATCHEntity;
+import org.opendaylight.netconf.sal.restconf.impl.RestconfDocumentedException;
+import org.opendaylight.netconf.sal.restconf.impl.RestconfError.ErrorTag;
+import org.opendaylight.netconf.sal.restconf.impl.RestconfError.ErrorType;
+import org.opendaylight.yangtools.yang.common.QName;
 import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
 import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.NodeIdentifier;
 import org.opendaylight.yangtools.yang.data.api.schema.MapEntryNode;
 import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNode;
+import org.opendaylight.yangtools.yang.data.impl.codec.xml.XmlUtils;
 import org.opendaylight.yangtools.yang.data.impl.schema.ImmutableNodes;
+import org.opendaylight.yangtools.yang.data.impl.schema.transform.dom.parser.DomToNormalizedNodeParserFactory;
 import org.opendaylight.yangtools.yang.model.api.ContainerSchemaNode;
+import org.opendaylight.yangtools.yang.model.api.DataNodeContainer;
+import org.opendaylight.yangtools.yang.model.api.DataSchemaNode;
 import org.opendaylight.yangtools.yang.model.api.ListSchemaNode;
+import org.opendaylight.yangtools.yang.model.api.Module;
+import org.opendaylight.yangtools.yang.model.api.SchemaNode;
+import org.opendaylight.yangtools.yang.model.util.SchemaContextUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
-import org.opendaylight.netconf.sal.rest.api.Draft02.MediaTypes;
-import org.opendaylight.netconf.sal.rest.api.RestconfService;
-import org.opendaylight.netconf.sal.restconf.impl.InstanceIdentifierContext;
-import org.opendaylight.netconf.sal.restconf.impl.PATCHContext;
-import org.opendaylight.netconf.sal.restconf.impl.RestconfDocumentedException;
-import org.opendaylight.netconf.sal.restconf.impl.RestconfError.ErrorTag;
-import org.opendaylight.netconf.sal.restconf.impl.RestconfError.ErrorType;
-import org.opendaylight.yangtools.yang.data.impl.codec.xml.XmlUtils;
-import org.opendaylight.yangtools.yang.data.impl.schema.transform.dom.parser.DomToNormalizedNodeParserFactory;
-import org.opendaylight.yangtools.yang.model.api.DataNodeContainer;
-import org.opendaylight.yangtools.yang.model.api.DataSchemaNode;
 
 @Provider
 @Consumes({MediaTypes.PATCH + RestconfService.XML})
@@ -117,47 +124,106 @@ public class XmlToPATCHBodyReader extends AbstractIdentifierAwareJaxRsProvider i
         final List<PATCHEntity> resultCollection = new ArrayList<>();
         final String patchId = doc.getElementsByTagName("patch-id").item(0).getFirstChild().getNodeValue();
         final NodeList editNodes = doc.getElementsByTagName("edit");
-        final DataSchemaNode schemaNode = (DataSchemaNode) pathContext.getSchemaNode();
         final DomToNormalizedNodeParserFactory parserFactory =
                 DomToNormalizedNodeParserFactory.getInstance(XmlUtils.DEFAULT_XML_CODEC_PROVIDER,
                         pathContext.getSchemaContext());
 
         for (int i = 0; i < editNodes.getLength(); i++) {
-            Element element = (Element) editNodes.item(i);
+            DataSchemaNode schemaNode = (DataSchemaNode) pathContext.getSchemaNode();
+            final Element element = (Element) editNodes.item(i);
             final String operation = element.getElementsByTagName("operation").item(0).getFirstChild().getNodeValue();
             final String editId = element.getElementsByTagName("edit-id").item(0).getFirstChild().getNodeValue();
-            final String target = element.getElementsByTagName("target").item(0).getFirstChild().getNodeValue();
-            DataSchemaNode targetNode = ((DataNodeContainer)(pathContext.getSchemaNode())).getDataChildByName
-                    (target.replace("/", ""));
+            final String target = element.getElementsByTagName("target").item(0).getFirstChild().getNodeValue()
+                    .replaceFirst("/", "");
+            final NodeList valueNodes = element.getElementsByTagName("value").item(0).getChildNodes();
+
+            Element value = null;
+            for (int j = 0; j < valueNodes.getLength(); j++) {
+                if (valueNodes.item(j) instanceof Element) {
+                    value = (Element) valueNodes.item(j);
+                    break;
+                }
+            }
+
+            Module module = pathContext.getSchemaContext().findModuleByNamespace(
+                    URI.create(value.getNamespaceURI())).iterator().next();
+
+            StringModuleInstanceIdentifierCodec codec = new StringModuleInstanceIdentifierCodec(
+                    pathContext.getSchemaContext());
+
+            codec.setDefaultPrefix(module.getName());
+
+            final YangInstanceIdentifier targetII = codec.deserialize(codec.serialize(pathContext
+                    .getInstanceIdentifier()) + prepareNonCondXpath(schemaNode, target, value, module.getQNameModule
+                    ().getFormattedRevision()));
+
+            schemaNode = (DataSchemaNode) SchemaContextUtil.findDataSchemaNode(pathContext.getSchemaContext(),
+                    codec.getDataContextTree().getChild(targetII).getDataSchemaNode().getPath());
+
+            SchemaNode targetNode = SchemaContextUtil.findDataSchemaNode(pathContext.getSchemaContext(),
+                    codec.getDataContextTree().getChild(targetII).getDataSchemaNode().getPath().getParent());
+
             if (targetNode == null) {
                 LOG.debug("Target node {} not found in path {} ", target, pathContext.getSchemaNode());
                 throw new RestconfDocumentedException("Error parsing input", ErrorType.PROTOCOL,
                         ErrorTag.MALFORMED_MESSAGE);
             } else {
-                final YangInstanceIdentifier targetII = pathContext.getInstanceIdentifier().node(targetNode.getQName());
-                final NodeList valueNodes = element.getElementsByTagName("value").item(0).getChildNodes();
-                Element value = null;
-                for (int j = 0; j < valueNodes.getLength(); j++) {
-                    if (valueNodes.item(j) instanceof Element) {
-                        value = (Element) valueNodes.item(j);
-                        break;
-                    }
-                }
                 NormalizedNode<?, ?> parsed = null;
                 if (schemaNode instanceof ContainerSchemaNode) {
                     parsed = parserFactory.getContainerNodeParser().parse(Collections.singletonList(value),
-                            (ContainerSchemaNode) targetNode);
+                            (ContainerSchemaNode) schemaNode);
                 } else if (schemaNode instanceof ListSchemaNode) {
                     NormalizedNode<?, ?> parsedValue = parserFactory.getMapEntryNodeParser().parse(Collections
-                            .singletonList(value), (ListSchemaNode) targetNode);
+                            .singletonList(value), (ListSchemaNode) schemaNode);
                     parsed = ImmutableNodes.mapNodeBuilder().withNodeIdentifier(new NodeIdentifier
                             (targetNode.getQName())).withChild((MapEntryNode) parsedValue).build();
                 }
 
-                resultCollection.add(new PATCHEntity(editId, operation, targetII, parsed));
+                resultCollection.add(new PATCHEntity(editId, operation, targetII.getParent(), parsed));
             }
         }
 
         return new PATCHContext(pathContext, ImmutableList.copyOf(resultCollection), patchId);
+    }
+
+    private String prepareNonCondXpath(DataSchemaNode schemaNode, String target, Element value, String revision) {
+        Iterator<String> args = Splitter.on("/").split(target.substring(target.indexOf(':') + 1))
+                .iterator();
+
+        StringBuffer nonCondXpath = new StringBuffer();
+        SchemaNode childNode = schemaNode;
+
+        while (args.hasNext()) {
+            String s = args.next();
+            nonCondXpath.append("/" + s);
+            childNode = ((DataNodeContainer) childNode).getDataChildByName(QName.create(value.getNamespaceURI(), revision, s));
+
+            if (childNode instanceof ListSchemaNode && args.hasNext()) {
+                createKeyForListFromPath(nonCondXpath, ((ListSchemaNode) childNode).getKeyDefinition().iterator()
+                        .next().getLocalName(), args.next());
+            }
+        }
+
+        if (childNode instanceof ListSchemaNode) {
+            completeTargetForList(nonCondXpath, ((ListSchemaNode) childNode).getKeyDefinition().iterator()
+                    .next().getLocalName(), value);
+        }
+
+        return nonCondXpath.toString();
+    }
+
+    private void completeTargetForList(StringBuffer nonCondXpath, String key, Element value) {
+        createKeyForListFromPath(nonCondXpath, key, value.getElementsByTagName(key).item(0)
+                .getFirstChild().getNodeValue());
+    }
+
+    private void createKeyForListFromPath(StringBuffer nonCondXpath, String key, String keyValue) {
+        nonCondXpath.append(
+                "["
+                + key
+                + "="
+                + "'" + keyValue + "'"
+                + "]"
+        );
     }
 }
