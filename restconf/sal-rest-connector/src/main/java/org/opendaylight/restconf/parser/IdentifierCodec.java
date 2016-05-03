a96af4eaaeff13aba444b8aa36c21d5c8a5ad729
Jakub Toth <jatoth@cisco.com>
2016-05-03 07:30:58 +0200

    Bug 5527 - Re-implement ControllerContext.toInstanceIdentifier() method
      * create YangInstanceIdentifier serializer and deserializer
      * fix mount points
    
    Change-Id: If97ced6cb14a11d9cd037f4b93a8f1c8fc984561
    Signed-off-by: Jakub Toth <jatoth@cisco.com>

---
+++ restconf/sal-rest-connector/src/main/java/org/opendaylight/restconf/parser/IdentifierCodec.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.restconf.parser;
+
+import org.opendaylight.restconf.parser.builder.YangInstanceIdentifierDeserializerBuilder;
+import org.opendaylight.restconf.parser.builder.YangInstanceIdentifierSerializerBuilder;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
+import org.opendaylight.yangtools.yang.data.api.codec.InstanceIdentifierCodec;
+import org.opendaylight.yangtools.yang.model.api.SchemaContext;
+
+public class IdentifierCodec implements InstanceIdentifierCodec<String> {
+
+
+    private final SchemaContext schemaContext;
+    private Object mountPoint;
+
+    public IdentifierCodec(final SchemaContext schemaContext) {
+        this.schemaContext = schemaContext;
+    }
+
+    @Override
+    public String serialize(final YangInstanceIdentifier data) {
+        final YangInstanceIdentifierSerializerBuilder serializer = new YangInstanceIdentifierSerializerBuilder(
+                this.schemaContext, data);
+        return serializer.build();
+    }
+
+    @Override
+    public YangInstanceIdentifier deserialize(final String data) {
+        if (data == null) {
+            return YangInstanceIdentifier.builder().build();
+        }
+        final YangInstanceIdentifierDeserializerBuilder yIISBuilder = new YangInstanceIdentifierDeserializerBuilder(
+                this.schemaContext, data);
+        return YangInstanceIdentifier.create(yIISBuilder.build());
+    }
+}
