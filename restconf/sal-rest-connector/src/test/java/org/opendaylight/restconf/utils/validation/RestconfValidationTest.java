d4fb396d0777aa9b7f40d32bae37a863626a3407
Ivan Hrasko <ivan.hrasko@pantheon.tech>
2016-05-03 15:30:09 +0200

    Bug 5526 - Unit Tests
    Unit tests for:
    -RestconfModuleServiceImpl
    -RestconfOperationsService
    -RestconfSchemaServiceImpl
    -RestconfStreamsServiceImpl
    -RestconfMappingNodeUtil
    -ParserIdentifier
    -RestconfSchemaUtil
    -RestconfValidation
    
    Change-Id: I4a2dbe433aa5ca7b6994a23d3fcf6401948437ee
    Signed-off-by: Jakub Toth <jatoth@cisco.com>
    Signed-off-by: Ivan Hrasko <ivan.hrasko@pantheon.tech>

---
+++ restconf/sal-rest-connector/src/test/java/org/opendaylight/restconf/utils/validation/RestconfValidationTest.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.restconf.utils.validation;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import org.junit.Test;
+import org.opendaylight.netconf.sal.restconf.impl.RestconfDocumentedException;
+
+public class RestconfValidationTest {
+    private static final List<String> revisions = Arrays.asList("2014-01-01", "2015-01-01", "2016-01-01");
+    private static final List<String> names = Arrays.asList("module1", "module2", "module3");
+
+    @Test
+    public void validateAndGetRevisionTest() {
+        Date revision = RestconfValidation.validateAndGetRevision(revisions.iterator());
+        assertNotNull("Correct module revision should be validated", revision);
+
+        Calendar c = Calendar.getInstance();
+        c.setTime(revision);
+
+        assertEquals(2014, c.get(Calendar.YEAR));
+        assertEquals(0, c.get(Calendar.MONTH));
+        assertEquals(1, c.get(Calendar.DAY_OF_MONTH));
+    }
+
+    @Test(expected = RestconfDocumentedException.class)
+    public void validateAndGetRevisionNotSuppliedTest() {
+        RestconfValidation.validateAndGetRevision(new ArrayList<String>().iterator());
+    }
+
+    @Test(expected = RestconfDocumentedException.class)
+    public void validateAndGetRevisionNotParsableTest() {
+        RestconfValidation.validateAndGetRevision(Arrays.asList("not-parsable-as-date").iterator());
+    }
+
+    @Test
+    public void validateAndGetModulNameTest() {
+        String moduleName = RestconfValidation.validateAndGetModulName(names.iterator());
+        assertNotNull(moduleName);
+        assertEquals("Correct module name should be validated", "module1", moduleName);
+    }
+
+    @Test(expected = RestconfDocumentedException.class)
+    public void validateAndGetModulNameNotSuppliedTest() {
+        RestconfValidation.validateAndGetModulName(new ArrayList<String>().iterator());
+    }
+}
