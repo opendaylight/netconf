41661c116159999405706b33f30c353a09ce4575
Jakub Morvay <jmorvay@cisco.com>
2016-05-03 11:16:47 +0200

    Bug 2343 - ODL remote YANG sources repository plugin
    ODL plugin functioning as a remote YANG sources repository.
    
    Plugin takes YANG schema sources from specified directory and provides
    them through web service.
    
    Change-Id: I93370af60e53dc0acafb844422adeea2abccf7a0
    Signed-off-by: Jakub Morvay <jmorvay@cisco.com>

---
+++ netconf/yanglib/src/main/java/org/opendaylight/yanglib/impl/YangLibRestApp.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.yanglib.impl;
+
+import java.util.Collections;
+import java.util.Set;
+import javax.ws.rs.core.Application;
+import org.opendaylight.yanglib.api.YangLibRestAppService;
+import org.opendaylight.yanglib.api.YangLibService;
+import org.osgi.framework.FrameworkUtil;
+
+public class YangLibRestApp extends Application implements YangLibRestAppService {
+
+    private final YangLibServiceImpl yangLibService;
+
+    public YangLibRestApp() {
+        this.yangLibService = new YangLibServiceImpl();
+        FrameworkUtil.getBundle(getClass()).getBundleContext().registerService(YangLibRestAppService.class.getName(),
+                this, null);
+    }
+
+    @Override
+    public Set<Object> getSingletons() {
+        return Collections.<Object>singleton(this.yangLibService);
+    }
+
+    @Override
+    public YangLibServiceImpl getYangLibService() {
+        return null;
+    }
+}
