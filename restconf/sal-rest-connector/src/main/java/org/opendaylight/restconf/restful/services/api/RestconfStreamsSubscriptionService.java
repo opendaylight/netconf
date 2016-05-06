8fe121a4392b39d04e33a2b3a5c2dd8cd2c3ed1a
Jakub Toth <jatoth@cisco.com>
2016-05-06 10:38:32 +0200

    Bug 5528 - api for restful
    Change-Id: I2e8fad15f1ee2f9fbef1dfabdd36258c59c9d86e
    Signed-off-by: Jakub Toth <jatoth@cisco.com>

---
+++ restconf/sal-rest-connector/src/main/java/org/opendaylight/restconf/restful/services/api/RestconfStreamsSubscriptionService.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.restconf.restful.services.api;
+
+import javax.ws.rs.Encoded;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
+
+/**
+ * Subscribing to streams
+ *
+ */
+public interface RestconfStreamsSubscriptionService {
+
+    /**
+     * Subscribing to receive notification from stream support
+     *
+     * @param identifier
+     *            - name of stream
+     * @param uriInfo
+     *            - URI info
+     * @return {@link Response}
+     */
+    @GET
+    @Path("data/ietf-restconf-monitoring:restconf-state/streams/stream/{identifier:.+}")
+    public Response subscribeToStream(@Encoded @PathParam("identifier") String identifier, @Context UriInfo uriInfo);
+}
