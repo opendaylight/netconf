e83e3b912848154479e96b6997e7a00e52832c00
Jakub Toth <jatoth@cisco.com>
2016-05-06 12:28:07 +0200

    Bug 5526 - Testing - SchemaContextHandler
    Change-Id: Iff1330700ecc0209aae2d6d1cf6d745b582ca506
    Signed-off-by: Jakub Toth <jatoth@cisco.com>

---
+++ restconf/sal-rest-connector/src/test/java/org/opendaylight/restconf/rest/impl/schema/context/SchemaContextHandlerTest.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.restconf.rest.impl.schema.context;
+
+import org.junit.Ignore;
+import org.junit.Test;
+import org.opendaylight.yangtools.yang.model.api.SchemaContext;
+
+/**
+ * Tests for handling {@link SchemaContext}
+ *
+ */
+public class SchemaContextHandlerTest {
+
+    /**
+     * Testing init of {@link SchemaContextHandlerImpl}
+     */
+    @Ignore
+    @Test
+    public void schemaContextHandlerImplInitTest() {
+
+    }
+
+    /**
+     * Test getting actual {@link SchemaContext}.
+     */
+    @Ignore
+    @Test
+    public void getSchemaContextTest() {
+
+    }
+
+    /**
+     * Test updating of {@link SchemaContext} .
+     */
+    @Ignore
+    @Test
+    public void onGlobalContextUpdateTest() {
+
+    }
+
+}
