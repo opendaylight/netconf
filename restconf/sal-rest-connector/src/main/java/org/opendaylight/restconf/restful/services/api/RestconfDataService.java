9a73f2988a60fbb06f8551fc576f247f76fda8a5
Jakub Toth <jatoth@cisco.com>
2016-05-06 10:38:32 +0200

    Bug 5528 - api for restful
    Change-Id: I2e8fad15f1ee2f9fbef1dfabdd36258c59c9d86e
    Signed-off-by: Jakub Toth <jatoth@cisco.com>

---
+++ restconf/sal-rest-connector/src/main/java/org/opendaylight/restconf/restful/services/api/RestconfDataService.java
@@ -0,0 +1,92 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.restconf.restful.services.api;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.Encoded;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
+import org.opendaylight.netconf.sal.rest.impl.PATCH;
+import org.opendaylight.netconf.sal.restconf.impl.NormalizedNodeContext;
+import org.opendaylight.netconf.sal.restconf.impl.PATCHContext;
+import org.opendaylight.netconf.sal.restconf.impl.PATCHStatusContext;
+import org.opendaylight.restconf.Draft11;
+import org.opendaylight.restconf.utils.RestconfConstants;
+
+/**
+ * The "{+restconf}/data" subtree represents the datastore resource type, which
+ * is a collection of configuration data and state data nodes
+ *
+ */
+@Path("/")
+public interface RestconfDataService {
+
+    /**
+     * Request the response headers for an XML/JSON representation of the a
+     * specific
+     *
+     * @param identifier
+     *            - path to node identifier
+     * @param uriInfo
+     *            - URI info
+     * @return {@link NormalizedNodeContext}
+     */
+    @GET
+    @Path("/data/{identifier:.+}")
+    @Produces({ Draft11.MediaTypes.DATA + RestconfConstants.JSON, Draft11.MediaTypes.DATA + RestconfConstants.XML,
+            MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.TEXT_XML })
+    public NormalizedNodeContext readData(@Encoded @PathParam("identifier") String identifier,
+            @Context UriInfo uriInfo);
+
+    @PUT
+    @Path("/data/{identifier:.+}")
+    @Consumes({ Draft11.MediaTypes.DATA + RestconfConstants.JSON, Draft11.MediaTypes.DATA + RestconfConstants.XML,
+            MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.TEXT_XML })
+    public Response putData(@Encoded @PathParam("identifier") String identifier, @Context UriInfo uriInfo);
+
+    @POST
+    @Path("/data/{identifier:.+}")
+    @Consumes({ Draft11.MediaTypes.DATA + RestconfConstants.JSON, Draft11.MediaTypes.DATA + RestconfConstants.XML,
+            MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.TEXT_XML })
+    public Response postData(@Encoded @PathParam("identifier") String identifier,
+            @Context UriInfo uriInfo);
+
+    @POST
+    @Path("/data")
+    @Consumes({ Draft11.MediaTypes.DATA + RestconfConstants.JSON, Draft11.MediaTypes.DATA + RestconfConstants.XML,
+            MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.TEXT_XML })
+    public Response postData(NormalizedNodeContext payload, @Context UriInfo uriInfo);
+
+    @DELETE
+    @Path("/data/{identifier:.+}")
+    public Response deleteData(@Encoded @PathParam("identifier") String identifier);
+
+    @PATCH
+    @Path("/data/{identifier:.+}")
+    @Consumes({ Draft11.MediaTypes.PATCH + RestconfConstants.JSON, Draft11.MediaTypes.PATCH + RestconfConstants.XML })
+    @Produces({ Draft11.MediaTypes.PATCH_STATUS + RestconfConstants.JSON,
+            Draft11.MediaTypes.PATCH_STATUS + RestconfConstants.XML })
+    PATCHStatusContext patchData(@Encoded @PathParam("identifier") String identifier, PATCHContext context,
+            @Context UriInfo uriInfo);
+
+    @PATCH
+    @Path("/data/{identifier:.+}")
+    @Consumes({ Draft11.MediaTypes.PATCH + RestconfConstants.JSON, Draft11.MediaTypes.PATCH + RestconfConstants.XML })
+    @Produces({ Draft11.MediaTypes.PATCH_STATUS + RestconfConstants.JSON,
+            Draft11.MediaTypes.PATCH_STATUS + RestconfConstants.XML })
+    PATCHStatusContext patchData(PATCHContext context, @Context UriInfo uriInfo);
+}
