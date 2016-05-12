44532594f4d421927ae0aa6f56e4c701bdcde5c8
Jakub Toth <jatoth@cisco.com>
2016-05-12 10:58:35 +0200

    Bug 5528 - Preparing enviroment for impl of restful services
      * repackeging
    
    Change-Id: I2ece0416ed09c3fc3517957427e18673406013de
    Signed-off-by: Jakub Toth <jatoth@cisco.com>

--- restconf/sal-rest-connector/src/main/java/org/opendaylight/restconf/common/handlers/api/SchemaContextHandler.java
+++ restconf/sal-rest-connector/src/main/java/org/opendaylight/restconf/common/handlers/api/SchemaContextHandler.java
@@ -5,7 +5,7 @@
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
-package org.opendaylight.restconf.rest.api.schema.context;
+package org.opendaylight.restconf.common.handlers.api;
 
 import org.opendaylight.yangtools.yang.model.api.SchemaContext;
 import org.opendaylight.yangtools.yang.model.api.SchemaContextListener;
--- restconf/sal-rest-connector/src/main/java/org/opendaylight/restconf/rest/services/api/RestconfStreamsService.java
+++ restconf/sal-rest-connector/src/main/java/org/opendaylight/restconf/rest/services/api/RestconfStreamsService.java
@@ -5,7 +5,7 @@
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
-package org.opendaylight.restconf.rest.api.services;
+package org.opendaylight.restconf.rest.services.api;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
