c97a5517331e03074fc2729fda3cfaae1adb1f5b
Ivan Hrasko <ivan.hrasko@pantheon.tech>
2016-05-18 11:05:35 +0200

    Bug 5527: Unit testing - serializer
    Unit tests for YangInstanceIdentifierSerializer
    
    Change-Id: Ife737bb38f43e7855a9b8657fa56bd49adf1be18
    Signed-off-by: Ivan Hrasko <ivan.hrasko@pantheon.tech>

---
+++ restconf/sal-rest-connector/src/test/java/org/opendaylight/restconf/parser/builder/YangInstanceIdentifierSerializerTest.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.restconf.parser.builder;
+
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+
+/**
+ * Unit tests for {@link YangInstanceIdentifierSerializer}
+ */
+public class YangInstanceIdentifierSerializerTest {
+
+    @Before
+    public void init() throws Exception {}
+
+    @Test
+    public void serializeTest() {}
+
+    @Ignore
+    @Test
+    public void nullSchemaContextNegativeTest() {}
+
+    @Ignore
+    @Test
+    public void nullDataNegativeTest() {}
+
+
+    @Ignore
+    @Test
+    public void emptyDataTest() {}
+
+}
