88685ce8a8ac51a94cc9671ab72823e2ac6beb13
Ivan Hrasko <ivan.hrasko@pantheon.tech>
2016-05-24 17:19:24 +0200

    Bug 5912 - Restconf draft11 - utils
    Fixing bugs of Draft11 implementation found by unit tests in RestconfSchemaServiceTest:
    - wrong condition in ParserIdentifier#toSchemaExportContextFromIdentifier
    - slash at the end of mount point identifier ParserIdentifier#toInstanceIdentifier
    - problem at the end of the input YangInstanceIdentifierDeserializer#create
    - module names validation
    
    Change-Id: Ib4031fd6d2830a49845191d1792e695a19d04e93
    Signed-off-by: Ivan Hrasko <ivan.hrasko@pantheon.tech>

--- restconf/sal-rest-connector/src/main/java/org/opendaylight/restconf/parser/builder/YangInstanceIdentifierDeserializer.java
+++ restconf/sal-rest-connector/src/main/java/org/opendaylight/restconf/parser/builder/YangInstanceIdentifierDeserializer.java
@@ -59,13 +59,13 @@ public final class YangInstanceIdentifierDeserializer {
             validArg(variables);
             final QName qname = prepareQName(variables);
 
-            if (!allCharsConsumed(variables)
-                    && (currentChar(variables.getOffset(), variables.getData()) == RestconfConstants.SLASH)) {
+            // this is the last identifier (input is consumed) or end of identifier (slash)
+            if (allCharsConsumed(variables)
+                    || currentChar(variables.getOffset(), variables.getData()) == RestconfConstants.SLASH) {
                 prepareIdentifier(qname, path, variables);
                 path.add(variables.getCurrent().getIdentifier());
-            } else if (!allCharsConsumed(variables)
-                    && (currentChar(variables.getOffset(),
-                            variables.getData()) == ParserBuilderConstants.Deserializer.EQUAL)) {
+            } else if (currentChar(variables.getOffset(),
+                    variables.getData()) == ParserBuilderConstants.Deserializer.EQUAL) {
                 current = nextContextNode(qname, path, variables);
                 if (!current.isKeyedEntry()) {
                     prepareNodeWithValue(qname, path, variables);
@@ -77,6 +77,7 @@ public final class YangInstanceIdentifierDeserializer {
                         "Bad char " + currentChar(offset, data) + " on position " + offset + ".");
             }
         }
+
         return ImmutableList.copyOf(path);
     }
 
