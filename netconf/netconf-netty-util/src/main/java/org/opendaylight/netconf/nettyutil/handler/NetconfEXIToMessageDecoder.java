0eb942eaa76c31cf3b74421f9d2048f90e0e4f5d
Robert Varga <rovarga@cisco.com>
2016-05-25 14:56:22 +0200

    Improve SAXTransformerFactory instantiation
    This is just a correctness fix to follow the API contract.
    
    Change-Id: Ia69b5b0d8d6b1ed8165cdeabbb4ff4278b9bfa12
    Signed-off-by: Robert Varga <rovarga@cisco.com>

--- netconf/netconf-netty-util/src/main/java/org/opendaylight/netconf/nettyutil/handler/NetconfEXIToMessageDecoder.java
+++ netconf/netconf-netty-util/src/main/java/org/opendaylight/netconf/nettyutil/handler/NetconfEXIToMessageDecoder.java
@@ -17,6 +17,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
 import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMResult;
 import javax.xml.transform.sax.SAXTransformerFactory;
 import javax.xml.transform.sax.TransformerHandler;
@@ -33,6 +34,15 @@ public final class NetconfEXIToMessageDecoder extends ByteToMessageDecoder {
 
     private static final Logger LOG = LoggerFactory.getLogger(NetconfEXIToMessageDecoder.class);
     private static final SAXTransformerFactory FACTORY = (SAXTransformerFactory) SAXTransformerFactory.newInstance();
+    static {
+        final TransformerFactory f = SAXTransformerFactory.newInstance();
+        if (!f.getFeature(SAXTransformerFactory.FEATURE)) {
+            throw new TransformerConfigurationException(String.format("Factory %s is not a SAXTransformerFactory", f));
+        }
+
+        FACTORY = (SAXTransformerFactory)f;
+    }
+
     /**
      * This class is not marked as shared, so it can be attached to only a single channel,
      * which means that {@link #decode(ChannelHandlerContext, ByteBuf, List)}
