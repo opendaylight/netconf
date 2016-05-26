ea6ec3f1db63e461130aa8e225252ac292e091bb
Jakub Toth <jatoth@cisco.com>
2016-05-26 10:25:57 +0200

    Bug 5528 - Handlers
      *make handlers immutable
        *DOMMountPointServiceHandler
        *SchemaContextHandler
        *TransactionChainHandler
      *common interface for all handlers
      *fix dependencies of these changes
    
    Change-Id: I4f3a78103036d88a5129f084bd088cd51c5b549c
    Signed-off-by: Jakub Toth <jatoth@cisco.com>

---
+++ restconf/sal-rest-connector/src/main/java/org/opendaylight/restconf/handlers/Handler.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.restconf.handlers;
+
+import org.opendaylight.controller.md.sal.dom.api.DOMTransactionChain;
+
+/**
+ * Handler for handling object prepared by provider for Restconf services
+ *
+ * @param <T>
+ *            - specific type go object for handling it
+ */
+interface Handler<T> {
+
+    /**
+     * Get prepared object
+     *
+     * @return {@link DOMTransactionChain}
+     */
+    T get();
+}
