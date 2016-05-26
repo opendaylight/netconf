dcd10e6eca489edb579952ae52491415f95c37a7
Jakub Toth <jatoth@cisco.com>
2016-05-26 10:32:29 +0200

    Bug 5528 - Implementation of RestconfDataService - Read data
      *prepare transaction node
    
    Change-Id: I61f4662b61ac9cf15535492f31bc896a863ce986
    Signed-off-by: Jakub Toth <jatoth@cisco.com>

---
+++ restconf/sal-rest-connector/src/main/java/org/opendaylight/restconf/restful/utils/RestconfDataServiceConstant.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.restconf.restful.utils;
+
+/**
+ * Constants for RestconfDataService
+ *
+ */
+public final class RestconfDataServiceConstant {
+
+    public static final String CONTENT = "content";
+
+    private RestconfDataServiceConstant() {
+        throw new UnsupportedOperationException("Util class.");
+    }
+
+    /**
+     * Constants for read data
+     *
+     */
+    public final class ReadData {
+
+        public static final String CONFIG = "config";
+        public static final String NONCONFIG = "nonconfig";
+        public static final String ALL = "all";
+
+        private ReadData() {
+            throw new UnsupportedOperationException("Util class.");
+        }
+    }
+
+}
