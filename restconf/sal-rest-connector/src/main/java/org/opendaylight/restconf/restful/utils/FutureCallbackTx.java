c9677e4bccd410578f8c24646e53300866fffdd3
Jakub Toth <jatoth@cisco.com>
2016-05-26 15:50:31 +0200

    Bug 5528 - Put data impl
      *make FutureCallbackTx more generaly
      *factory for NormalizedNode(READ) and Response(PUT) from Future
      *fix ReadDataTransactionUtil after remake of FutureCallbackTx
    
    Change-Id: Iec2d35271881c7ffe630cdd832510a3f0df7616a
    Signed-off-by: Jakub Toth <jatoth@cisco.com>

--- restconf/sal-rest-connector/src/main/java/org/opendaylight/restconf/restful/utils/FutureCallbackTx.java
+++ restconf/sal-rest-connector/src/main/java/org/opendaylight/restconf/restful/utils/FutureCallbackTx.java
@@ -7,51 +7,47 @@
  */
 package org.opendaylight.restconf.restful.utils;
 
-import com.google.common.base.Optional;
 import com.google.common.util.concurrent.CheckedFuture;
 import com.google.common.util.concurrent.FutureCallback;
 import com.google.common.util.concurrent.Futures;
 import javax.annotation.Nullable;
 import org.opendaylight.controller.md.sal.common.api.data.AsyncTransaction;
-import org.opendaylight.controller.md.sal.common.api.data.ReadFailedException;
 import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
 import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNode;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-class FutureCallbackTx {
+final class FutureCallbackTx {
 
     private final static Logger LOG = LoggerFactory.getLogger(FutureCallbackTx.class);
 
-    static void addCallback(final CheckedFuture<Optional<NormalizedNode<?, ?>>, ReadFailedException> listenableFuture,
+    static <T, X extends Exception> void addCallback(final CheckedFuture<T, X> listenableFuture,
             final AsyncTransaction<YangInstanceIdentifier, NormalizedNode<?, ?>> transaction, final String txType,
-            final FutureDataFactory dataFactory) {
-        Futures.addCallback(listenableFuture, new FutureCallback<Optional<NormalizedNode<?, ?>>>() {
+            final FutureDataFactory<T> dataFactory) {
+        Futures.addCallback(listenableFuture, new FutureCallback<T>() {
 
             @Override
             public void onFailure(final Throwable t) {
-                brm(t, txType, transaction, null, null);
+                handlingLoggerAndValues(t, txType, transaction, null, null);
             }
 
             @Override
-            public void onSuccess(final Optional<NormalizedNode<?, ?>> result) {
-                brm(null, txType, transaction, result, dataFactory);
+            public void onSuccess(final T result) {
+                handlingLoggerAndValues(null, txType, transaction, result, dataFactory);
             }
 
         });
     }
 
-    protected static void brm(@Nullable final Throwable t, final String txType,
+    protected static <T> void handlingLoggerAndValues(@Nullable final Throwable t, final String txType,
             final AsyncTransaction<YangInstanceIdentifier, NormalizedNode<?, ?>> transaction,
-            final Optional<NormalizedNode<?, ?>> optionalNN, final FutureDataFactory dataFactory) {
+            final T result, final FutureDataFactory<T> dataFactory) {
         if (t != null) {
             LOG.info("Transaction({}) {} FAILED!", txType, transaction.getIdentifier(), t);
             throw new IllegalStateException("  Transaction(" + txType + ") not committed correctly", t);
         } else {
             LOG.trace("Transaction({}) {} SUCCESSFUL!", txType, transaction.getIdentifier());
-            if (optionalNN.isPresent()) {
-                dataFactory.setData(optionalNN.get());
-            }
+            dataFactory.setResult(result);
         }
     }
 }
---
+++ restconf/sal-rest-connector/src/main/java/org/opendaylight/restconf/restful/utils/PutDataTransactionUtil.java
@@ -0,0 +1,217 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.restconf.restful.utils;
+
+import com.google.common.base.Preconditions;
+import com.google.common.collect.Maps;
+import com.google.common.util.concurrent.CheckedFuture;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import javax.ws.rs.core.Response;
+import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
+import org.opendaylight.controller.md.sal.common.api.data.TransactionCommitFailedException;
+import org.opendaylight.controller.md.sal.dom.api.DOMDataWriteTransaction;
+import org.opendaylight.netconf.md.sal.rest.common.RestconfValidationUtils;
+import org.opendaylight.netconf.sal.restconf.impl.InstanceIdentifierContext;
+import org.opendaylight.netconf.sal.restconf.impl.NormalizedNodeContext;
+import org.opendaylight.netconf.sal.restconf.impl.RestconfDocumentedException;
+import org.opendaylight.netconf.sal.restconf.impl.RestconfError.ErrorTag;
+import org.opendaylight.netconf.sal.restconf.impl.RestconfError.ErrorType;
+import org.opendaylight.restconf.common.references.SchemaContextRef;
+import org.opendaylight.restconf.restful.transaction.TransactionVarsWrapper;
+import org.opendaylight.yangtools.yang.common.QName;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.NodeIdentifierWithPredicates;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.PathArgument;
+import org.opendaylight.yangtools.yang.data.api.schema.MapEntryNode;
+import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNode;
+import org.opendaylight.yangtools.yang.data.impl.schema.ImmutableNodes;
+import org.opendaylight.yangtools.yang.model.api.ListSchemaNode;
+import org.opendaylight.yangtools.yang.model.api.Module;
+import org.opendaylight.yangtools.yang.model.api.SchemaContext;
+import org.opendaylight.yangtools.yang.model.api.SchemaNode;
+
+/**
+ * Util class for put data to DS
+ *
+ */
+public final class PutDataTransactionUtil {
+
+    /**
+     * Valid input data with {@link SchemaNode}
+     *
+     * @param schemaNode
+     *            - {@link SchemaNode}
+     * @param payload
+     *            - input data
+     */
+    public static void validInputData(final SchemaNode schemaNode, final NormalizedNodeContext payload) {
+        if ((schemaNode != null) && (payload.getData() == null)) {
+            throw new RestconfDocumentedException("Input is required.", ErrorType.PROTOCOL, ErrorTag.MALFORMED_MESSAGE);
+        } else if ((schemaNode == null) && (payload.getData() != null)) {
+            throw new RestconfDocumentedException("No input expected.", ErrorType.PROTOCOL, ErrorTag.MALFORMED_MESSAGE);
+        }
+    }
+
+    /**
+     * Valid top level node name
+     *
+     * @param path
+     *            - path of node
+     * @param payload
+     *            - data
+     */
+    public static void validTopLevelNodeName(final YangInstanceIdentifier path, final NormalizedNodeContext payload) {
+        final String payloadName = payload.getData().getNodeType().getLocalName();
+
+        if (path.isEmpty()) {
+            if (!payload.getData().getNodeType().equals(RestconfDataServiceConstant.NETCONF_BASE_QNAME)) {
+                throw new RestconfDocumentedException("Instance identifier has to contain at least one path argument",
+                        ErrorType.PROTOCOL, ErrorTag.MALFORMED_MESSAGE);
+            }
+        } else {
+            final String identifierName = path.getLastPathArgument().getNodeType().getLocalName();
+            if (!payloadName.equals(identifierName)) {
+                throw new RestconfDocumentedException(
+                        "Payload name (" + payloadName + ") is different from identifier name (" + identifierName + ")",
+                        ErrorType.PROTOCOL, ErrorTag.MALFORMED_MESSAGE);
+            }
+        }
+    }
+
+    /**
+     * Validates whether keys in {@code payload} are equal to values of keys in
+     * {@code iiWithData} for list schema node
+     *
+     * @throws RestconfDocumentedException
+     *             if key values or key count in payload and URI isn't equal
+     *
+     */
+    public static void validateListKeysEqualityInPayloadAndUri(final NormalizedNodeContext payload) {
+        final InstanceIdentifierContext<?> iiWithData = payload.getInstanceIdentifierContext();
+        final PathArgument lastPathArgument = iiWithData.getInstanceIdentifier().getLastPathArgument();
+        final SchemaNode schemaNode = iiWithData.getSchemaNode();
+        final NormalizedNode<?, ?> data = payload.getData();
+        if (schemaNode instanceof ListSchemaNode) {
+            final List<QName> keyDefinitions = ((ListSchemaNode) schemaNode).getKeyDefinition();
+            if ((lastPathArgument instanceof NodeIdentifierWithPredicates) && (data instanceof MapEntryNode)) {
+                final Map<QName, Object> uriKeyValues = ((NodeIdentifierWithPredicates) lastPathArgument)
+                        .getKeyValues();
+                isEqualUriAndPayloadKeyValues(uriKeyValues, (MapEntryNode) data, keyDefinitions);
+            }
+        }
+    }
+
+    private static void isEqualUriAndPayloadKeyValues(final Map<QName, Object> uriKeyValues, final MapEntryNode payload,
+            final List<QName> keyDefinitions) {
+        final Map<QName, Object> mutableCopyUriKeyValues = Maps.newHashMap(uriKeyValues);
+        for (final QName keyDefinition : keyDefinitions) {
+            final Object uriKeyValue = mutableCopyUriKeyValues.remove(keyDefinition);
+            RestconfValidationUtils.checkDocumentedError(uriKeyValue != null, ErrorType.PROTOCOL, ErrorTag.DATA_MISSING,
+                    "Missing key " + keyDefinition + " in URI.");
+
+            final Object dataKeyValue = payload.getIdentifier().getKeyValues().get(keyDefinition);
+
+            if (!uriKeyValue.equals(dataKeyValue)) {
+                final String errMsg = "The value '" + uriKeyValue + "' for key '" + keyDefinition.getLocalName()
+                        + "' specified in the URI doesn't match the value '" + dataKeyValue
+                        + "' specified in the message body. ";
+                throw new RestconfDocumentedException(errMsg, ErrorType.PROTOCOL, ErrorTag.INVALID_VALUE);
+            }
+        }
+    }
+
+    /**
+     * Check mount point and prepare variables for put data to DS
+     *
+     * @param payload
+     *            - data to put
+     * @param schemaCtxRef
+     *            - reference to {@link SchemaContext}
+     * @param transactionNode
+     * @return {@link CheckedFuture}
+     */
+    public static Response putData(final NormalizedNodeContext payload,
+            final SchemaContextRef schemaCtxRef, final TransactionVarsWrapper transactionNode) {
+        final YangInstanceIdentifier path = payload.getInstanceIdentifierContext().getInstanceIdentifier();
+        final ResponseFactory responseFactory = new ResponseFactory(
+                ReadDataTransactionUtil.readData(RestconfDataServiceConstant.ReadData.CONFIG, transactionNode));
+        final CheckedFuture<Void, TransactionCommitFailedException> submitData = submitData(path, schemaCtxRef.get(),
+                transactionNode.getTransaction(), payload.getData());
+        FutureCallbackTx.addCallback(submitData, transactionNode.getTransaction(),
+                RestconfDataServiceConstant.PutData.PUT_TX_TYPE, responseFactory);
+        return responseFactory.build();
+    }
+
+    /**
+     * Put data to DS
+     *
+     * @param path
+     *            - path of data
+     * @param schemaContext
+     *            - {@link SchemaContext}
+     * @param writeTx
+     *            - write transaction
+     * @param data
+     *            - data
+     * @return {@link CheckedFuture}
+     */
+    private static CheckedFuture<Void, TransactionCommitFailedException> submitData(final YangInstanceIdentifier path,
+            final SchemaContext schemaContext,
+            final DOMDataWriteTransaction writeTx, final NormalizedNode<?, ?> data) {
+        ensureParentsByMerge(path, schemaContext, writeTx);
+        writeTx.put(LogicalDatastoreType.CONFIGURATION, path, data);
+        return writeTx.submit();
+    }
+
+    /**
+     * Merged parents of data
+     *
+     * @param path
+     *            - path of data
+     * @param schemaContext
+     *            - {@link SchemaContext}
+     * @param writeTx
+     *            - write transaction
+     */
+    private static void ensureParentsByMerge(final YangInstanceIdentifier path, final SchemaContext schemaContext,
+            final DOMDataWriteTransaction writeTx) {
+        final List<PathArgument> normalizedPathWithoutChildArgs = new ArrayList<>();
+        boolean hasList = false;
+        YangInstanceIdentifier rootNormalizedPath = null;
+
+        final Iterator<PathArgument> it = path.getPathArguments().iterator();
+        final Module module = schemaContext.findModuleByNamespaceAndRevision(
+                path.getLastPathArgument().getNodeType().getModule().getNamespace(),
+                path.getLastPathArgument().getNodeType().getModule().getRevision());
+
+        while (it.hasNext()) {
+            final PathArgument pathArgument = it.next();
+            if (rootNormalizedPath == null) {
+                rootNormalizedPath = YangInstanceIdentifier.create(pathArgument);
+            }
+            if (it.hasNext()) {
+                normalizedPathWithoutChildArgs.add(pathArgument);
+                if (module.getDataChildByName(pathArgument.getNodeType()) instanceof ListSchemaNode) {
+                    hasList = true;
+                }
+            }
+        }
+        if (normalizedPathWithoutChildArgs.isEmpty()) {
+            return;
+        }
+        if (hasList) {
+            Preconditions.checkArgument(rootNormalizedPath != null, "Empty path received");
+            final NormalizedNode<?, ?> parentStructure = ImmutableNodes.fromInstanceId(schemaContext,
+                    YangInstanceIdentifier.create(normalizedPathWithoutChildArgs));
+            writeTx.merge(LogicalDatastoreType.CONFIGURATION, rootNormalizedPath, parentStructure);
+        }
+    }
+}
