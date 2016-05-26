e1fc22e5f23c3df67763ad25282767b1db4f763c
Jakub Toth <jatoth@cisco.com>
2016-05-26 10:25:57 +0200

    Bug 5528 - Prepare handler for TransactionChain
      *move DOMMountPointServiceHandler to common
    
    Change-Id: I4f3a78103036d88a5129f084bd088cd51c5b549c
    Signed-off-by: Jakub Toth <jatoth@cisco.com>

--- restconf/sal-rest-connector/src/main/java/org/opendaylight/restconf/RestConnectorProvider.java
+++ restconf/sal-rest-connector/src/main/java/org/opendaylight/restconf/RestConnectorProvider.java
@@ -10,18 +10,27 @@ package org.opendaylight.restconf;
 import com.google.common.base.Preconditions;
 import java.util.Collection;
 import java.util.Collections;
+import org.opendaylight.controller.md.sal.common.api.data.AsyncTransaction;
+import org.opendaylight.controller.md.sal.common.api.data.TransactionChain;
+import org.opendaylight.controller.md.sal.common.api.data.TransactionChainListener;
+import org.opendaylight.controller.md.sal.dom.api.DOMDataBroker;
 import org.opendaylight.controller.md.sal.dom.api.DOMMountPointService;
+import org.opendaylight.controller.md.sal.dom.api.DOMTransactionChain;
 import org.opendaylight.controller.sal.core.api.Broker.ProviderSession;
 import org.opendaylight.controller.sal.core.api.Provider;
 import org.opendaylight.controller.sal.core.api.model.SchemaService;
 import org.opendaylight.netconf.sal.rest.api.RestConnector;
+import org.opendaylight.restconf.common.handlers.api.DOMMountPointServiceHandler;
 import org.opendaylight.restconf.common.handlers.api.SchemaContextHandler;
+import org.opendaylight.restconf.common.handlers.api.TransactionChainHandler;
+import org.opendaylight.restconf.common.handlers.impl.DOMMountPointServiceHandlerImpl;
 import org.opendaylight.restconf.common.handlers.impl.SchemaContextHandlerImpl;
-import org.opendaylight.restconf.rest.handlers.api.DOMMountPointServiceHandler;
-import org.opendaylight.restconf.rest.handlers.impl.DOMMountPointServiceHandlerImpl;
+import org.opendaylight.restconf.common.handlers.impl.TransactionChainHandlerImpl;
 import org.opendaylight.restconf.rest.services.impl.Draft11ServicesWrapperImpl;
 import org.opendaylight.yangtools.concepts.ListenerRegistration;
 import org.opendaylight.yangtools.yang.model.api.SchemaContextListener;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Provider for restconf draft11.
@@ -29,7 +38,28 @@ import org.opendaylight.yangtools.yang.model.api.SchemaContextListener;
  */
 public class RestConnectorProvider implements Provider, RestConnector, AutoCloseable {
 
+    private static final Logger LOG = LoggerFactory.getLogger(RestConnectorProvider.class);
+
+    private final TransactionChainListener transactionListener = new TransactionChainListener() {
+        @Override
+        public void onTransactionChainFailed(final TransactionChain<?, ?> chain,
+                final AsyncTransaction<?, ?> transaction, final Throwable cause) {
+            LOG.error("TransactionChain({}) {} FAILED!", chain, transaction.getIdentifier(), cause);
+            chain.close();
+            resetTransactionChainForAdapaters(chain);
+            throw new IllegalStateException("TransactionChain(" + chain + ") not committed correctly", cause);
+        }
+
+        @Override
+        public void onTransactionChainSuccessful(final TransactionChain<?, ?> chain) {
+            LOG.trace("TransactionChain({}) {} SUCCESSFUL", chain);
+        }
+    };
+
     private ListenerRegistration<SchemaContextListener> listenerRegistration;
+    private DOMDataBroker dataBroker;
+    private DOMTransactionChain transactionChain;
+    private TransactionChainHandler transactionChainHandler;
 
     @Override
     public void onSessionInitiated(final ProviderSession session) {
@@ -40,7 +70,27 @@ public class RestConnectorProvider implements Provider, RestConnector, AutoClose
         final Draft11ServicesWrapperImpl wrapperServices = Draft11ServicesWrapperImpl.getInstance();
         this.listenerRegistration = schemaService.registerSchemaContextListener(schemaCtxHandler);
 
+        this.transactionChainHandler = new TransactionChainHandlerImpl();
+        this.dataBroker = session.getService(DOMDataBroker.class);
+        this.transactionChain = this.dataBroker.createTransactionChain(this.transactionListener);
+        this.transactionChainHandler.setTransactionChain(this.transactionChain);
+
         wrapperServices.setHandlers(schemaCtxHandler, domMountPointServiceHandler);
+    }
+
+    /**
+     * Resetting {@link TransactionChain} because of failed.
+     *
+     * @param chain
+     *            - old {@link TransactionChain}
+     */
+    private void resetTransactionChainForAdapaters(final TransactionChain<?, ?> chain) {
+        this.transactionChain = Preconditions.checkNotNull(this.dataBroker)
+                .createTransactionChain(this.transactionListener);
+
+        this.transactionChainHandler.setTransactionChain(this.transactionChain);
+
+        LOG.trace("Resetting TransactionChain({}) to {}", chain, this.transactionChain);
     }
 
     @Override
@@ -52,6 +102,9 @@ public class RestConnectorProvider implements Provider, RestConnector, AutoClose
     public void close() throws Exception {
         if (this.listenerRegistration != null) {
             this.listenerRegistration.close();
+        }
+        if (this.transactionChain != null) {
+            this.transactionChain.close();
         }
     }
 }
