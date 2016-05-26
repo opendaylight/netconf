6878f8ae3727da96cff1350ccc749b350a18ac51
Jakub Toth <jatoth@cisco.com>
2016-05-26 10:25:57 +0200

    Bug 5528 - Prepare handler for TransactionChain
      *move DOMMountPointServiceHandler to common
    
    Change-Id: I4f3a78103036d88a5129f084bd088cd51c5b549c
    Signed-off-by: Jakub Toth <jatoth@cisco.com>

---
+++ restconf/sal-rest-connector/src/main/java/org/opendaylight/restconf/common/handlers/impl/TransactionChainHandlerImpl.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.restconf.common.handlers.impl;
+
+import org.opendaylight.controller.md.sal.dom.api.DOMTransactionChain;
+import org.opendaylight.restconf.common.handlers.api.TransactionChainHandler;
+
+public class TransactionChainHandlerImpl implements TransactionChainHandler {
+
+    private DOMTransactionChain transactionChain;
+
+    @Override
+    public DOMTransactionChain getTransactionChain() {
+        return this.transactionChain;
+    }
+
+    @Override
+    public void setTransactionChain(final DOMTransactionChain transactionChain) {
+        this.transactionChain = transactionChain;
+    }
+
+}
