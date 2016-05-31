54c3a1550fc38ca6de891fe7c60453833ff719ec
Jakub Toth <jatoth@cisco.com>
2016-05-31 10:41:25 +0200

    Bug 5528 - Implementation of RestconfDataService - Delete data
    Change-Id: I47955a4f6563c3ae5ab5d5db38823530bf013cba
    Signed-off-by: Jakub Toth <jatoth@cisco.com>

---
+++ restconf/sal-rest-connector/src/main/java/org/opendaylight/restconf/restful/utils/DeleteDataTransactionUtil.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.restconf.restful.utils;
+
+import com.google.common.base.Optional;
+import com.google.common.util.concurrent.CheckedFuture;
+import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
+import org.opendaylight.controller.md.sal.common.api.data.TransactionCommitFailedException;
+import org.opendaylight.controller.md.sal.dom.api.DOMDataBroker;
+import org.opendaylight.controller.md.sal.dom.api.DOMDataWriteTransaction;
+import org.opendaylight.controller.md.sal.dom.api.DOMMountPoint;
+import org.opendaylight.controller.md.sal.dom.api.DOMTransactionChain;
+import org.opendaylight.netconf.sal.restconf.impl.InstanceIdentifierContext;
+import org.opendaylight.netconf.sal.restconf.impl.RestconfDocumentedException;
+import org.opendaylight.restconf.common.handlers.api.TransactionChainHandler;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
+
+/**
+ * Util class for delete specific data in config DS.
+ *
+ */
+public final class DeleteDataTransactionUtil {
+
+    private DeleteDataTransactionUtil() {
+        throw new UnsupportedOperationException("Util class.");
+    }
+
+    /**
+     * Check mount point and prepare variables for delete data.
+     *
+     * @param instanceIdentifier
+     *            - {@link InstanceIdentifierContext} of data to delete
+     * @param transactionChainHandler
+     *            - chain handler
+     * @return {@link CheckedFuture}
+     */
+    public static CheckedFuture<Void, TransactionCommitFailedException> deleteData(
+            final InstanceIdentifierContext<?> instanceIdentifier,
+            final TransactionChainHandler transactionChainHandler) {
+        final DOMTransactionChain transactionChain = transactionChainHandler.getTransactionChain();
+        final YangInstanceIdentifier path = instanceIdentifier.getInstanceIdentifier();
+        if (instanceIdentifier.getMountPoint() == null) {
+            return submitData(transactionChain.newWriteOnlyTransaction(), path);
+        } else {
+            final DOMMountPoint mountPoint = instanceIdentifier.getMountPoint();
+            final Optional<DOMDataBroker> domDataBrokerService = mountPoint.getService(DOMDataBroker.class);
+
+            if (domDataBrokerService.isPresent()) {
+                return submitData(transactionChain.newWriteOnlyTransaction(), path);
+            }
+            final String errMsg = "DOM data broker service isn't available for mount point " + path;
+            throw new RestconfDocumentedException(errMsg);
+        }
+    }
+
+    /**
+     * Delete data
+     *
+     * @param writeTx
+     *            - write transaction
+     * @param path
+     *            - path of data to delete
+     * @return {@link CheckedFuture}
+     */
+    private static CheckedFuture<Void, TransactionCommitFailedException> submitData(
+            final DOMDataWriteTransaction writeTx, final YangInstanceIdentifier path) {
+        writeTx.delete(LogicalDatastoreType.CONFIGURATION, path);
+        return writeTx.submit();
+    }
+
+}
