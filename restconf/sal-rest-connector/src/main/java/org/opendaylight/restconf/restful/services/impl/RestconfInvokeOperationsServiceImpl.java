ee65de368a9a1c10944ff107af1ea86b98273b9b
Jakub Toth <jatoth@cisco.com>
2016-06-02 10:26:19 +0200

    Bug 5528 - Implementation of RestconfInvokeOperation
    Change-Id: I571166124fbe7588f187091976af6b8d6deb6e22
    Signed-off-by: Jakub Toth <jatoth@cisco.com>

---
+++ restconf/sal-rest-connector/src/main/java/org/opendaylight/restconf/restful/services/impl/RestconfInvokeOperationsServiceImpl.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.restconf.restful.services.impl;
+
+import javax.ws.rs.core.UriInfo;
+import org.opendaylight.netconf.sal.restconf.impl.NormalizedNodeContext;
+import org.opendaylight.restconf.restful.services.api.RestconfInvokeOperationsService;
+
+public class RestconfInvokeOperationsServiceImpl implements RestconfInvokeOperationsService {
+
+    @Override
+    public NormalizedNodeContext invokeRpc(String identifier, NormalizedNodeContext payload, UriInfo uriInfo) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public NormalizedNodeContext invokeAction(String identifier, String action, NormalizedNodeContext payload,
+            UriInfo uriInfo) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
