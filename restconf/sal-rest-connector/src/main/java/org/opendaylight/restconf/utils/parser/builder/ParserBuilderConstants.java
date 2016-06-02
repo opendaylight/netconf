16a70903eced88865d3d8a880ee1dff560898e94
Ivan Hrasko <ivan.hrasko@pantheon.tech>
2016-06-02 16:14:27 +0200

    Bug 5911 - Restconf draft11 - bad parsing
    - fix for leaf-list deserialization
    - fix for YangInstanceIdentifier.EMPTY serialization and deserialization
    (YangInstanceIdentifier.EMPTY is serialized to single slash and single
    slash is deserialized to YangInstanceIdentifier.EMPTY)
    - fix: parser recognizes all reserved characters according to rfc3986
    - fix for list keys serialization
    - fix for percent encoded characters decoding
    - add check if leaf list instance value was supplied, error otherwise
    - fix: module name before identifier is required only if node was not
    defined in parents module
    
    Change-Id: I48a944ed6139da5b7be0835cc99e219e11dc0072
    Signed-off-by: Ivan Hrasko <ivan.hrasko@pantheon.tech>

--- restconf/sal-rest-connector/src/main/java/org/opendaylight/restconf/utils/parser/builder/ParserBuilderConstants.java
+++ restconf/sal-rest-connector/src/main/java/org/opendaylight/restconf/utils/parser/builder/ParserBuilderConstants.java
@@ -8,6 +8,7 @@
 package org.opendaylight.restconf.utils.parser.builder;
 
 import com.google.common.base.CharMatcher;
+import java.util.Arrays;
 import org.opendaylight.restconf.parser.builder.YangInstanceIdentifierDeserializer;
 import org.opendaylight.restconf.parser.builder.YangInstanceIdentifierSerializer;
 
@@ -32,7 +33,9 @@ public final class ParserBuilderConstants {
             throw new UnsupportedOperationException("Util class");
         }
 
-        public static final String DISABLED_CHARS = ",': /";
+        public static final String DISABLED_CHARS = Arrays.toString(new char[] { ':', '/', '?', '#', '[', ']', '@' })
+                .concat(Arrays.toString(new char[] { '!', '$', '&', '\'', '(', ')', '*', '+', ',', ';', '=' }));
+
         public static final CharMatcher PERCENT_ENCODE_CHARS = CharMatcher.anyOf(DISABLED_CHARS).precomputed();
     }
 
@@ -48,22 +51,31 @@ public final class ParserBuilderConstants {
 
         public static final CharMatcher BASE = CharMatcher.inRange('a', 'z').or(CharMatcher.inRange('A', 'Z'))
                 .precomputed();
+
         public static final CharMatcher IDENTIFIER_FIRST_CHAR = BASE.or(CharMatcher.is('_')).precomputed();
+
         public static final CharMatcher IDENTIFIER = IDENTIFIER_FIRST_CHAR.or(CharMatcher.inRange('0', '9'))
                 .or(CharMatcher.anyOf(".-")).precomputed();
+
         public static final CharMatcher IDENTIFIER_HEXA = CharMatcher.inRange('a', 'f')
                 .or(CharMatcher.inRange('A', 'F')).or(CharMatcher.inRange('0', '9')).precomputed();
 
         public static final char COLON = ':';
         public static final char EQUAL = '=';
         public static final char COMMA = ',';
+        public static final char HYPHEN = '-';
         public static final char PERCENT_ENCODING = '%';
-        public static final char QUOTE = '"';
 
-        public static final CharMatcher IDENTIFIER_FIRST_CHAR_PREDICATE = BASE.or(CharMatcher.inRange('0', '9'))
-                .or(CharMatcher.is(QUOTE)).or(CharMatcher.is(PERCENT_ENCODING)).precomputed();
+        public static final CharMatcher IDENTIFIER_PREDICATE = CharMatcher.noneOf(
+                Serializer.DISABLED_CHARS).precomputed();
 
-        public static final CharMatcher IDENTIFIER_PREDICATE = IDENTIFIER_FIRST_CHAR_PREDICATE;
         public static final String EMPTY_STRING = "";
+
+        // position of the first encoded char after percent sign in percent encoded string
+        public static final int FIRST_ENCODED_CHAR = 1;
+        // position of the last encoded char after percent sign in percent encoded string
+        public static final int LAST_ENCODED_CHAR = 3;
+        // percent encoded radix for parsing integers
+        public static final int PERCENT_ENCODED_RADIX = 16;
     }
 }
