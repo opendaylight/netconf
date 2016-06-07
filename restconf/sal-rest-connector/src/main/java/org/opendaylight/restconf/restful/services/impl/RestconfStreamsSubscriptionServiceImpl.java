9a06e7bfbfb0feb9e3a13517023be76442edbc54
Jakub Toth <jatoth@cisco.com>
2016-06-07 11:18:05 +0200

    Bug 5528 - Implementation of RestconfStreamsSubscriptionService
    Change-Id: Ib3d2a755159ee90b2fdafde2aa203f1f5dacbcbd
    Signed-off-by: Jakub Toth <jatoth@cisco.com>

---
+++ restconf/sal-rest-connector/src/main/java/org/opendaylight/restconf/restful/services/impl/RestconfStreamsSubscriptionServiceImpl.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.restconf.restful.services.impl;
+
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
+import org.opendaylight.restconf.restful.services.api.RestconfStreamsSubscriptionService;
+
+public class RestconfStreamsSubscriptionServiceImpl implements RestconfStreamsSubscriptionService {
+
+    @Override
+    public Response subscribeToStream(String identifier, UriInfo uriInfo) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
