701270d7b519640932a8325bc9f3ba835eaaf032
Jakub Toth <jatoth@cisco.com>
2016-06-13 14:49:47 +0200

    Bug 2594 - PUT method returns wrong status for create resource
    Change-Id: I48f8f825da2427357c3f8948cbea52d2f9c235ce
    Signed-off-by: Jakub Toth <jatoth@cisco.com>

--- restconf/sal-rest-connector/src/main/java/org/opendaylight/netconf/sal/restconf/impl/RestconfImpl.java
+++ restconf/sal-rest-connector/src/main/java/org/opendaylight/netconf/sal/restconf/impl/RestconfImpl.java
@@ -652,11 +652,15 @@ public class RestconfImpl implements RestconfService {
          * document the behavior).
          */
         int tries = 2;
+        Status status = Status.NOT_MODIFIED;
         while(true) {
             try {
                 if (mountPoint != null) {
+                    status = this.broker.readConfigurationData(mountPoint, normalizedII) != null ? Status.OK
+                            : Status.CREATED;
                     this.broker.commitConfigurationDataPut(mountPoint, normalizedII, payload.getData()).checkedGet();
                 } else {
+                    status = this.broker.readConfigurationData(normalizedII) != null ? Status.OK : Status.CREATED;
                     this.broker.commitConfigurationDataPut(this.controllerContext.getGlobalSchema(), normalizedII, payload.getData()).checkedGet();
                 }
 
@@ -680,7 +684,7 @@ public class RestconfImpl implements RestconfService {
             }
         }
 
-        return Response.status(Status.OK).build();
+        return Response.status(status).build();
     }
 
     private static void validateTopLevelNodeName(final NormalizedNodeContext node,
