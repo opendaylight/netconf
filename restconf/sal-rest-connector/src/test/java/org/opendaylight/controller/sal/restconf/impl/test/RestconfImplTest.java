233186971658dc828de25b53220988af1abe3a26
Jakub Toth <jatoth@cisco.com>
2016-07-08 15:44:52 +0200

    Bug 3959 - support netconf notification
      *added dependency for converting JSON to XML
      *created new notification listener adapter extended
       by DOMNotificationListener
      *added augment for sal-remote to add new param
       for input of create-notification-stream rpc to
       make a choice between JSON or XML output of
       notification (not mandatory - default is XML)
      *added unit tests
    
    Change-Id: I7f5909208dce71e2280fd1bc6dbe49cb7533523a
    Signed-off-by: Jakub Toth <jatoth@cisco.com>

--- restconf/sal-rest-connector/src/test/java/org/opendaylight/controller/sal/restconf/impl/test/RestconfImplTest.java
+++ restconf/sal-rest-connector/src/test/java/org/opendaylight/controller/sal/restconf/impl/test/RestconfImplTest.java
@@ -13,20 +13,26 @@ import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.anyBoolean;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+
 import com.google.common.base.Optional;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Sets;
 import com.google.common.util.concurrent.Futures;
 import java.io.FileNotFoundException;
+import java.net.URI;
 import java.text.ParseException;
 import java.util.Set;
 import javax.ws.rs.core.MultivaluedHashMap;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriBuilder;
 import javax.ws.rs.core.UriInfo;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.mockito.Mockito;
 import org.opendaylight.controller.md.sal.dom.api.DOMMountPoint;
 import org.opendaylight.controller.md.sal.dom.api.DOMRpcResult;
 import org.opendaylight.controller.md.sal.dom.api.DOMRpcService;
@@ -35,10 +41,16 @@ import org.opendaylight.netconf.sal.restconf.impl.ControllerContext;
 import org.opendaylight.netconf.sal.restconf.impl.InstanceIdentifierContext;
 import org.opendaylight.netconf.sal.restconf.impl.NormalizedNodeContext;
 import org.opendaylight.netconf.sal.restconf.impl.RestconfImpl;
+import org.opendaylight.netconf.sal.streams.listeners.Notificator;
 import org.opendaylight.yangtools.yang.common.QName;
 import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
+import org.opendaylight.yangtools.yang.data.api.schema.ContainerNode;
+import org.opendaylight.yangtools.yang.data.api.schema.DataContainerChild;
+import org.opendaylight.yangtools.yang.data.api.schema.LeafSetEntryNode;
+import org.opendaylight.yangtools.yang.data.api.schema.LeafSetNode;
 import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNode;
 import org.opendaylight.yangtools.yang.model.api.Module;
+import org.opendaylight.yangtools.yang.model.api.RpcDefinition;
 import org.opendaylight.yangtools.yang.model.api.SchemaContext;
 import org.opendaylight.yangtools.yang.model.api.SchemaNode;
 import org.opendaylight.yangtools.yang.model.api.SchemaPath;
@@ -60,9 +72,8 @@ public class RestconfImplTest {
         final Set<Module> allModules = schemaContext.getModules();
         assertNotNull(allModules);
 
-        controllerContext = spy(ControllerContext.getInstance());
+        controllerContext = ControllerContext.getInstance();
         controllerContext.setSchemas(schemaContext);
-
     }
 
     @Before
@@ -104,5 +115,74 @@ public class RestconfImplTest {
         this.restconfImpl.invokeRpc("randomId", ctx, uriInfo);
         this.restconfImpl.invokeRpc("ietf-netconf", ctx, uriInfo);
         verify(rpcService, times(2)).invokeRpc(any(SchemaPath.class), any(NormalizedNode.class));
+    }
+
+    /**
+     * Create notification stream for toaster module
+     */
+    @Test
+    public void createNotificationStreamTest() {
+        final NormalizedNodeContext payload = mock(NormalizedNodeContext.class);
+        final InstanceIdentifierContext iiCtx = mock(InstanceIdentifierContext.class);
+        doReturn(iiCtx).when(payload).getInstanceIdentifierContext();
+
+        final SchemaNode schemaNode = mock(SchemaNode.class,
+                Mockito.withSettings().extraInterfaces(RpcDefinition.class));
+        doReturn(schemaNode).when(iiCtx).getSchemaNode();
+        doReturn(mock(SchemaPath.class)).when(schemaNode).getPath();
+
+        doReturn(QName.create("urn:opendaylight:params:xml:ns:yang:controller:md:sal:remote",
+                "2014-01-14", "create-notification-stream")).when(schemaNode).getQName();
+        doReturn(null).when(iiCtx).getMountPoint();
+
+        final Set<DataContainerChild<?, ?>> children = Sets.newHashSet();
+        final DataContainerChild<?, ?> child = mock(DataContainerChild.class,
+                Mockito.withSettings().extraInterfaces(LeafSetNode.class));
+
+        final LeafSetEntryNode entryNode = mock(LeafSetEntryNode.class);
+        when(entryNode.getValue()).thenReturn("(http://netconfcentral.org/ns/toaster?revision=2009-11-20)toastDone");
+        when(((LeafSetNode) child).getValue()).thenReturn(Sets.newHashSet(entryNode));
+        children.add(child);
+
+        final NormalizedNode<?, ?> normalizedNode = mock(NormalizedNode.class,
+                Mockito.withSettings().extraInterfaces(ContainerNode.class));
+        doReturn(normalizedNode).when(payload).getData();
+        doReturn(children).when(normalizedNode).getValue();
+
+        // register notification
+        final NormalizedNodeContext context = this.restconfImpl
+                .invokeRpc("sal-remote:create-notification-stream", payload, null);
+        assertNotNull(context);
+    }
+
+    /**
+     * Subscribe for notification stream of toaster module
+     */
+    @Test
+    public void subscribeToNotificationStreamTest() throws Exception {
+        final String identifier = "create-notification-stream/toaster:toastDone";
+
+        // register test notification stream
+        final SchemaPath path = SchemaPath.create(
+                true, QName.create("http://netconfcentral.org/ns/toaster", "2009-11-20", "toastDone"));
+        Notificator.createNotificationListener(Lists.newArrayList(path), identifier, "XML");
+
+        final UriInfo uriInfo = mock(UriInfo.class);
+        final UriBuilder uriBuilder = mock(UriBuilder.class);
+        when(uriBuilder.port(8181)).thenReturn(uriBuilder);
+        when(uriBuilder.replacePath(identifier)).thenReturn(uriBuilder);
+        when(uriBuilder.build()).thenReturn(new URI(""));
+        when(uriBuilder.scheme("ws")).thenReturn(uriBuilder);
+        when(uriInfo.getAbsolutePathBuilder()).thenReturn(uriBuilder);
+
+        final BrokerFacade brokerFacade = mock(BrokerFacade.class);
+        this.restconfImpl.setBroker(brokerFacade);
+
+        // subscribe to stream and verify response
+        final Response response = this.restconfImpl.subscribeToStream(identifier, uriInfo);
+        assertEquals(Response.Status.OK.getStatusCode(), response.getStatus());
+
+        // remove test notification stream
+        Notificator.removeAllListeners();
     }
 }
