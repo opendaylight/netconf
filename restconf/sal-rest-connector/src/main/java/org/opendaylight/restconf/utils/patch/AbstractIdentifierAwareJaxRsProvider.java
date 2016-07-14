c2f6d2b5e57677f4128b6b0a9633d470908711cb
Ivan Hrasko <ihrasko@cisco.com>
2016-07-14 15:57:09 +0200

    Yang Patch for Restconf11
    Change-Id: I981dc8cbead3c5a7dc06b7b807c17a40a9cfe7b3
    Signed-off-by: Ivan Hrasko <ihrasko@cisco.com>

---
+++ restconf/sal-rest-connector/src/main/java/org/opendaylight/restconf/utils/patch/AbstractIdentifierAwareJaxRsProvider.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.restconf.utils.patch;
+
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.Request;
+import javax.ws.rs.core.UriInfo;
+import org.opendaylight.netconf.sal.rest.api.RestconfConstants;
+import org.opendaylight.netconf.sal.restconf.impl.ControllerContext;
+import org.opendaylight.netconf.sal.restconf.impl.InstanceIdentifierContext;
+import org.opendaylight.restconf.utils.parser.ParserIdentifier;
+
+public class AbstractIdentifierAwareJaxRsProvider {
+
+    private static final String POST = "POST";
+
+    @Context
+    private UriInfo uriInfo;
+
+    @Context
+    private Request request;
+
+    protected final String getIdentifier() {
+        return uriInfo.getPathParameters(false).getFirst(RestconfConstants.IDENTIFIER);
+    }
+
+    protected InstanceIdentifierContext<?> getInstanceIdentifierContext() {
+        return ParserIdentifier.toInstanceIdentifier(getIdentifier(),
+                ControllerContext.getInstance().getGlobalSchema());
+    }
+
+    protected UriInfo getUriInfo() {
+        return uriInfo;
+    }
+
+    protected boolean isPost() {
+        return POST.equals(request.getMethod());
+    }
+}
