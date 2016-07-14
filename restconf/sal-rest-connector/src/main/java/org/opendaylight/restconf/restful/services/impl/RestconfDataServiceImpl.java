2bbdc2873a4fdbbb3d167cb93b5cede34caf989b
Ivan Hrasko <ihrasko@cisco.com>
2016-07-14 15:57:09 +0200

    Yang HTTP PATCH for Restconf11
    - support for restconf draft11-15 changes
    - support for xml and json media types
    - applied fixes for:
    - Bug 5897 - PATCH merge operation does nothing
    - Bug 5909 - PATCH does not report 409 on
    OptimisticLockFailedException
    - Bug 5730 - Delete subset of list items using PATCH?
    - Bug 5898 - PATCH success "ok" field has wrong JSON value
    - Bug 5915 - PATCH with "target":"/" error
    - Bug 5509 - HTTP Patch in Restconf doesn't support general
    absolute or relative target xpath
    - contains unit tests
    - Draft02 classes marked as deprecated
    
    Change-Id: I981dc8cbead3c5a7dc06b7b807c17a40a9cfe7b3
    Signed-off-by: Ivan Hrasko <ihrasko@cisco.com>

--- restconf/sal-rest-connector/src/main/java/org/opendaylight/restconf/restful/services/impl/RestconfDataServiceImpl.java
+++ restconf/sal-rest-connector/src/main/java/org/opendaylight/restconf/restful/services/impl/RestconfDataServiceImpl.java
@@ -25,6 +25,7 @@ import org.opendaylight.restconf.handlers.TransactionChainHandler;
 import org.opendaylight.restconf.restful.services.api.RestconfDataService;
 import org.opendaylight.restconf.restful.transaction.TransactionVarsWrapper;
 import org.opendaylight.restconf.restful.utils.DeleteDataTransactionUtil;
+import org.opendaylight.restconf.restful.utils.PatchDataTransactionUtil;
 import org.opendaylight.restconf.restful.utils.PostDataTransactionUtil;
 import org.opendaylight.restconf.restful.utils.PutDataTransactionUtil;
 import org.opendaylight.restconf.restful.utils.ReadDataTransactionUtil;
@@ -146,22 +147,36 @@ public class RestconfDataServiceImpl implements RestconfDataService {
 
     @Override
     public PATCHStatusContext patchData(final String identifier, final PATCHContext context, final UriInfo uriInfo) {
-        throw new UnsupportedOperationException("Not yet implemented.");
+        Preconditions.checkNotNull(identifier);
+        return patchData(context, uriInfo);
     }
 
     @Override
     public PATCHStatusContext patchData(final PATCHContext context, final UriInfo uriInfo) {
-        throw new UnsupportedOperationException("Not yet implemented.");
+        Preconditions.checkNotNull(context);
+        final DOMMountPoint mountPoint = context.getInstanceIdentifierContext().getMountPoint();
+
+        final DOMDataReadWriteTransaction transaction;
+        final SchemaContextRef ref;
+        if (mountPoint == null) {
+            transaction = this.transactionChainHandler.get().newReadWriteTransaction();
+            ref = new SchemaContextRef(this.schemaContextHandler.get());
+        } else {
+            transaction = transactionOfMountPoint(mountPoint);
+            ref = new SchemaContextRef(mountPoint.getSchemaContext());
+        }
+
+        final TransactionVarsWrapper transactionNode = new TransactionVarsWrapper(
+                context.getInstanceIdentifierContext(), mountPoint, transaction);
+
+        return PatchDataTransactionUtil.patchData(context, transactionNode, ref);
     }
 
     /**
      * Prepare transaction to read data of mount point, if these data are
      * present.
      * @param mountPoint
-     *
-     * @param transactionNode
-     *            - {@link TransactionVarsWrapper} - wrapper for variables
-     * @return {@link NormalizedNode}
+     * @return {@link DOMDataReadWriteTransaction}
      */
     private static DOMDataReadWriteTransaction transactionOfMountPoint(final DOMMountPoint mountPoint) {
         final Optional<DOMDataBroker> domDataBrokerService = mountPoint.getService(DOMDataBroker.class);
