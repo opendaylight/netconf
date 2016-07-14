91c946fac39da849fc2e1a9769a56de6d925bfef
Ivan Hrasko <ihrasko@cisco.com>
2016-07-14 15:57:09 +0200

    Yang HTTP PATCH for Restconf11
    - support for restconf draft11-15 changes
    - support for xml and json media types
    - applied fixes for:
    - Bug 5897 - PATCH merge operation does nothing
    - Bug 5909 - PATCH does not report 409 on
    OptimisticLockFailedException
    - Bug 5730 - Delete subset of list items using PATCH?
    - Bug 5898 - PATCH success "ok" field has wrong JSON value
    - Bug 5915 - PATCH with "target":"/" error
    - Bug 5509 - HTTP Patch in Restconf doesn't support general
    absolute or relative target xpath
    - contains unit tests
    - Draft02 classes marked as deprecated
    
    Change-Id: I981dc8cbead3c5a7dc06b7b807c17a40a9cfe7b3
    Signed-off-by: Ivan Hrasko <ihrasko@cisco.com>

--- restconf/sal-rest-connector/src/test/java/org/opendaylight/controller/sal/rest/impl/test/providers/Draft11AbstractBodyReaderTest.java
+++ restconf/sal-rest-connector/src/test/java/org/opendaylight/controller/sal/rest/impl/test/providers/Draft11AbstractBodyReaderTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
@@ -21,25 +21,25 @@ import javax.ws.rs.core.Request;
 import javax.ws.rs.core.UriInfo;
 import org.opendaylight.controller.md.sal.rest.common.TestRestconfUtils;
 import org.opendaylight.netconf.sal.rest.api.RestconfConstants;
-import org.opendaylight.netconf.sal.rest.impl.AbstractIdentifierAwareJaxRsProvider;
 import org.opendaylight.netconf.sal.restconf.impl.ControllerContext;
 import org.opendaylight.netconf.sal.restconf.impl.NormalizedNodeContext;
 import org.opendaylight.netconf.sal.restconf.impl.PATCHContext;
+import org.opendaylight.restconf.utils.patch.Draft11AbstractIdentifierAwareJaxRsProvider;
 import org.opendaylight.yangtools.yang.model.api.SchemaContext;
 
-public abstract class AbstractBodyReaderTest {
+public abstract class Draft11AbstractBodyReaderTest {
 
     protected final static ControllerContext controllerContext = ControllerContext.getInstance();
     protected final MediaType mediaType;
     private static Field uriField;
     private static Field requestField;
 
-    public AbstractBodyReaderTest() throws NoSuchFieldException,
+    public Draft11AbstractBodyReaderTest() throws NoSuchFieldException,
             SecurityException {
-        uriField = AbstractIdentifierAwareJaxRsProvider.class
+        uriField = Draft11AbstractIdentifierAwareJaxRsProvider.class
                 .getDeclaredField("uriInfo");
         uriField.setAccessible(true);
-        requestField = AbstractIdentifierAwareJaxRsProvider.class
+        requestField = Draft11AbstractIdentifierAwareJaxRsProvider.class
                 .getDeclaredField("request");
         requestField.setAccessible(true);
         mediaType = getMediaType();
@@ -52,7 +52,7 @@ public abstract class AbstractBodyReaderTest {
         return TestRestconfUtils.loadSchemaContext(yangPath, schemaContext);
     }
 
-    protected static <T extends AbstractIdentifierAwareJaxRsProvider> void mockBodyReader(
+    protected static <T extends Draft11AbstractIdentifierAwareJaxRsProvider> void mockBodyReader(
             final String identifier, final T normalizedNodeProvider,
             final boolean isPost) throws NoSuchFieldException,
             SecurityException, IllegalArgumentException, IllegalAccessException {
