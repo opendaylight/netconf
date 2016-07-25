cd5f80bfe652d37f86de25801344ac183d2ec91e
Ivan Hrasko <ihrasko@cisco.com>
2016-07-25 12:35:53 +0200

    Bug 5730 - Delete subset of list items using PATCH?
    - fix: edit is not ignored when end of input is reached
    - fix: only create, merge, replace and insert operations require value to be present
    - fix: if value is not present with create, merge, replace or insert operation then error 400 is returned
    - fix: if value is used with other operations error 400 is returned
    - fix: deleting sublist values instead of full parent
    - fix: order of patch edit fields is not important anymore (target, value)
    - move class StringModuleInstanceIdentifierCodec to own file
    
    Change-Id: I4c3f407858034b49795034da6c93b41f9d891b62
    Signed-off-by: Ivan Hrasko <ivan.hrasko@pantheon.tech>
    Signed-off-by: Ivan Hrasko <ihrasko@cisco.com>

---
+++ restconf/sal-rest-connector/src/main/java/org/opendaylight/netconf/sal/rest/impl/StringModuleInstanceIdentifierCodec.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.netconf.sal.rest.impl;
+
+import com.google.common.base.Preconditions;
+import java.net.URI;
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import org.opendaylight.yangtools.yang.data.util.AbstractModuleStringInstanceIdentifierCodec;
+import org.opendaylight.yangtools.yang.data.util.DataSchemaContextTree;
+import org.opendaylight.yangtools.yang.model.api.Module;
+import org.opendaylight.yangtools.yang.model.api.SchemaContext;
+
+final class StringModuleInstanceIdentifierCodec extends AbstractModuleStringInstanceIdentifierCodec {
+
+    private final DataSchemaContextTree dataContextTree;
+    private final SchemaContext context;
+
+    StringModuleInstanceIdentifierCodec(SchemaContext context) {
+        this.context = Preconditions.checkNotNull(context);
+        this.dataContextTree = DataSchemaContextTree.from(context);
+    }
+
+    @Override
+    protected Module moduleForPrefix(@Nonnull String prefix) {
+        return context.findModuleByName(prefix, null);
+    }
+
+    @Nonnull
+    @Override
+    protected DataSchemaContextTree getDataContextTree() {
+        return dataContextTree;
+    }
+
+    @Nullable
+    @Override
+    protected String prefixForNamespace(@Nonnull URI namespace) {
+        final Module module = context.findModuleByNamespaceAndRevision(namespace, null);
+        return module == null ? null : module.getName();
+    }
+}
