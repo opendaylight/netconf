2168df823bfb4f414246a383adf614d7f69190ed
Alexis de Talhouët <adetalhouet@inocybe.com>
2016-08-05 08:02:52 -0400

    BUG 6343 - Incorrect handling of configuration failures in SAL netconf connector
    patchset#4: fix unit test
    
    Change-Id: I4fbf06038b4a14c0efecfa6564e69671a3d23bcc
    Signed-off-by: Alexis de Talhouët <adetalhouet@inocybe.com>

--- netconf/sal-netconf-connector/src/test/java/org/opendaylight/netconf/sal/connect/netconf/sal/tx/NetconfDeviceWriteOnlyTxTest.java
+++ netconf/sal-netconf-connector/src/test/java/org/opendaylight/netconf/sal/connect/netconf/sal/tx/NetconfDeviceWriteOnlyTxTest.java
@@ -123,20 +123,46 @@ public class NetconfDeviceWriteOnlyTxTest {
     }
 
     @Test
+    public void testFailedLockRunning() throws Exception {
+        final CheckedFuture<DefaultDOMRpcResult, Exception> rpcErrorFuture =
+                Futures.immediateCheckedFuture(new DefaultDOMRpcResult(RpcResultBuilder.newError(RpcError.ErrorType.APPLICATION, "a", "m")));
+
+         doReturn(Futures.immediateCheckedFuture(new DefaultDOMRpcResult((NormalizedNode<?, ?>) null)))
+        .doReturn(Futures.immediateFailedCheckedFuture(new IllegalStateException("Failed to lock")))
+        .doReturn(rpcErrorFuture)
+        .when(rpc).invokeRpc(any(SchemaPath.class), any(NormalizedNode.class));
+
+        try {
+            new WriteRunningTx(id, new NetconfBaseOps(rpc, mock(SchemaContext.class)), false);
+        } catch (final Exception e) {
+            verify(rpc).invokeRpc(toPath(NetconfMessageTransformUtil.NETCONF_LOCK_QNAME), NetconfBaseOps.getLockContent(NETCONF_RUNNING_QNAME));
+            return;
+        }
+
+        fail("Lock should fail");
+    }
+
+    @Test
     public void testDiscardChangesNotSentWithoutCandidate() {
         doReturn(Futures.immediateCheckedFuture(new DefaultDOMRpcResult((NormalizedNode<?, ?>) null)))
-                .doReturn(Futures.immediateFailedCheckedFuture(new IllegalStateException("Failed tx")))
-                .when(rpc).invokeRpc(any(SchemaPath.class), any(NormalizedNode.class));
+        .doReturn(Futures.immediateFailedCheckedFuture(new IllegalStateException("Failed tx")))
+        .when(rpc).invokeRpc(any(SchemaPath.class), any(NormalizedNode.class));
 
         final WriteRunningTx tx = new WriteRunningTx(id, new NetconfBaseOps(rpc, BaseSchema.BASE_NETCONF_CTX_WITH_NOTIFICATIONS.getSchemaContext()),
                 false);
 
-        tx.delete(LogicalDatastoreType.CONFIGURATION, yangIId);
-        // verify discard changes was sent
-        final InOrder inOrder = inOrder(rpc);
-        inOrder.verify(rpc).invokeRpc(toPath(NetconfMessageTransformUtil.NETCONF_LOCK_QNAME), NetconfBaseOps.getLockContent(NETCONF_RUNNING_QNAME));
-        inOrder.verify(rpc).invokeRpc(eq(toPath(NetconfMessageTransformUtil.NETCONF_EDIT_CONFIG_QNAME)), any(NormalizedNode.class));
-        inOrder.verify(rpc).invokeRpc(toPath(NetconfMessageTransformUtil.NETCONF_UNLOCK_QNAME), NetconfBaseOps.getUnLockContent(NETCONF_RUNNING_QNAME));
+        try {
+            tx.delete(LogicalDatastoreType.CONFIGURATION, yangIId);
+        } catch (final Exception e) {
+            // verify discard changes was sent
+            final InOrder inOrder = inOrder(rpc);
+            inOrder.verify(rpc).invokeRpc(toPath(NetconfMessageTransformUtil.NETCONF_LOCK_QNAME), NetconfBaseOps.getLockContent(NETCONF_RUNNING_QNAME));
+            inOrder.verify(rpc).invokeRpc(eq(toPath(NetconfMessageTransformUtil.NETCONF_EDIT_CONFIG_QNAME)), any(NormalizedNode.class));
+            inOrder.verify(rpc).invokeRpc(toPath(NetconfMessageTransformUtil.NETCONF_UNLOCK_QNAME), NetconfBaseOps.getUnLockContent(NETCONF_RUNNING_QNAME));
+            return;
+        }
+
+        fail("Delete should fail");
     }
 
 }
