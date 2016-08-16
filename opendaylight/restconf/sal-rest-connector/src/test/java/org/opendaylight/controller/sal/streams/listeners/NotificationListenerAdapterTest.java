3aa428aee6611212480d585f221caf02a85c2d78
Jakub Toth <jatoth@cisco.com>
2016-08-16 11:28:03 +0200

    Bug 6324 - Notifications stream output is not same as restconf data
      *added parser for NormalizedNode to JSONObject
    
    Change-Id: I24ceb02d10f0a142fb1b21664b600cad8f69df87
    Signed-off-by: Jakub Toth <jatoth@cisco.com>

---
+++ opendaylight/restconf/sal-rest-connector/src/test/java/org/opendaylight/controller/sal/streams/listeners/NotificationListenerAdapterTest.java
@@ -0,0 +1,152 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.controller.sal.streams.listeners;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.opendaylight.controller.md.sal.dom.api.DOMNotification;
+import org.opendaylight.netconf.sal.restconf.impl.ControllerContext;
+import org.opendaylight.netconf.sal.streams.listeners.NotificationListenerAdapter;
+import org.opendaylight.netconf.sal.streams.listeners.Notificator;
+import org.opendaylight.yangtools.yang.common.QName;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.PathArgument;
+import org.opendaylight.yangtools.yang.data.api.schema.AugmentationNode;
+import org.opendaylight.yangtools.yang.data.api.schema.ContainerNode;
+import org.opendaylight.yangtools.yang.data.api.schema.DataContainerChild;
+import org.opendaylight.yangtools.yang.data.api.schema.LeafNode;
+import org.opendaylight.yangtools.yang.model.api.DataSchemaNode;
+import org.opendaylight.yangtools.yang.model.api.LeafSchemaNode;
+import org.opendaylight.yangtools.yang.model.api.Module;
+import org.opendaylight.yangtools.yang.model.api.NotificationDefinition;
+import org.opendaylight.yangtools.yang.model.api.SchemaContext;
+import org.opendaylight.yangtools.yang.model.api.SchemaPath;
+import org.opendaylight.yangtools.yang.model.api.TypeDefinition;
+import org.opendaylight.yangtools.yang.model.api.type.IntegerTypeDefinition;
+import org.opendaylight.yangtools.yang.model.util.BaseTypes;
+
+public class NotificationListenerAdapterTest {
+
+    @Mock
+    private DOMNotification notification;
+    @Mock
+    private SchemaContext globalSchema;
+    @Mock
+    private ContainerNode node;
+    @Mock
+    private Module module;
+
+    private final QName qname = QName.create("stream-namespace", "2016-08-18", "stream-localname");
+
+    @Before
+    public void init(){
+        MockitoAnnotations.initMocks(this);
+        Mockito.when(this.globalSchema.getQName()).thenReturn(this.qname);
+        Mockito.when(
+                this.globalSchema.findModuleByNamespaceAndRevision(this.qname.getNamespace(), this.qname.getRevision()))
+                .thenReturn(this.module);
+        Mockito.when(this.module.getName()).thenReturn("stream");
+        ControllerContext.getInstance().setGlobalSchema(this.globalSchema);
+    }
+
+    @Test
+    public void onNotifiLeafIntegetTest() {
+        final List<SchemaPath> qnameList = prepareListOfQname("leaf-module", "leaf");
+        final NotificationListenerAdapter notificationListenerAdapter = Notificator
+                .createNotificationListener(qnameList, "leafInteger", "JSON").get(0);
+
+        final DOMNotification leafNotifi = Mockito.mock(DOMNotification.class);
+        Mockito.when(leafNotifi.getType()).thenReturn(qnameList.get(0));
+
+        final ContainerNode contNode = Mockito.mock(ContainerNode.class);
+        Mockito.when(leafNotifi.getBody()).thenReturn(contNode);
+
+        final LeafNode leafNode = Mockito.mock(LeafNode.class);
+        Mockito.when(leafNode.getNodeType()).thenReturn(qnameList.get(0).getLastComponent());
+        Mockito.when(leafNode.getValue()).thenReturn(5);
+
+        final List<DataContainerChild<? extends PathArgument, ?>> childs = new ArrayList<>();
+        childs.add(leafNode);
+        Mockito.when(contNode.getValue()).thenReturn(childs);
+
+        prepareSchemaCtx(qnameList.get(0).getLastComponent());
+        notificationListenerAdapter.onNotification(leafNotifi);
+    }
+
+    private void prepareSchemaCtx(final QName lastComponent) {
+        final SchemaContext schema = Mockito.mock(SchemaContext.class);
+        Mockito.when(schema.getQName()).thenReturn(lastComponent);
+
+        final Module module = Mockito.mock(Module.class);
+        Mockito.when(schema.findModuleByNamespaceAndRevision(lastComponent.getNamespace(), lastComponent.getRevision()))
+                .thenReturn(module);
+        Mockito.when(module.getName()).thenReturn(lastComponent.getLocalName() + "-module");
+
+        final NotificationDefinition notifiDef = Mockito.mock(NotificationDefinition.class);
+        Mockito.when(notifiDef.getQName()).thenReturn(lastComponent);
+
+        final LeafSchemaNode leafSchemaNode = Mockito.mock(LeafSchemaNode.class);
+        Mockito.when(leafSchemaNode.getQName()).thenReturn(lastComponent);
+
+        final TypeDefinition intTypeDef = Mockito.mock(IntegerTypeDefinition.class);
+        Mockito.when(leafSchemaNode.getType()).thenReturn(intTypeDef);
+        Mockito.when(intTypeDef.getQName()).thenReturn(BaseTypes.INT16_QNAME);
+
+        final Collection<DataSchemaNode> childs = new ArrayList<>();
+        childs.add(leafSchemaNode);
+        Mockito.when(notifiDef.getChildNodes()).thenReturn(childs);
+
+
+        final Set<NotificationDefinition> notifis = new HashSet<>();
+        notifis.add(notifiDef);
+        Mockito.when(module.getNotifications()).thenReturn(notifis);
+
+        ControllerContext.getInstance().setGlobalSchema(schema);
+    }
+
+    private List<SchemaPath> prepareListOfQname(final String namespace, final String localName) {
+        final List<SchemaPath> path = new ArrayList<>();
+        path.add(SchemaPath.create(true, QName.create(namespace, "2016-09-26", localName)));
+        return path;
+    }
+
+    @Test
+    public void onNotifiTest() {
+        final List<SchemaPath> paths = new ArrayList<>();
+        final SchemaPath schema = SchemaPath.create(true, this.qname);
+        paths.add(schema);
+        final List<NotificationListenerAdapter> listeners =
+                Notificator.createNotificationListener(paths, "streamName", "JSON");
+        final NotificationListenerAdapter listener = listeners.get(0);
+        Mockito.when(this.notification.getType()).thenReturn(schema);
+        Mockito.when(this.notification.getBody()).thenReturn(this.node);
+        final List<DataContainerChild<? extends PathArgument, ?>> childs = new ArrayList<>();
+        final AugmentationNode augmentLeaf = Mockito.mock(AugmentationNode.class);
+        childs.add(augmentLeaf);
+        final Collection<DataContainerChild<? extends PathArgument, ?>> augmentChilds = new ArrayList<>();
+        final LeafNode leaf = Mockito.mock(LeafNode.class);
+        final QName leafAugmentNodeQName = QName.create("augment-module", "2016-08-18", "leafik");
+        Mockito.when(leaf.getNodeType()).thenReturn(leafAugmentNodeQName);
+        Mockito.when(leaf.getValue()).thenReturn("augmentLeafik");
+        augmentChilds.add(leaf);
+        final Module augmentModule = Mockito.mock(Module.class);
+        Mockito.when(this.globalSchema.findModuleByNamespaceAndRevision(leafAugmentNodeQName.getNamespace(),
+                leafAugmentNodeQName.getRevision())).thenReturn(augmentModule);
+        Mockito.when(augmentModule.getName()).thenReturn("augment-module");
+        Mockito.when(augmentLeaf.getValue()).thenReturn(augmentChilds);
+        Mockito.when(this.node.getValue()).thenReturn(childs);
+        listener.onNotification(this.notification);
+    }
+}
