2c2225edc5c24a5c90967403900c6c922651bdc2
Rudolf Brisuda <rudolf.brisuda@pantheon.tech>
2016-08-17 14:03:04 +0200

    Bug 6023 - Adress for config subsystem netconf endpoint is not configurable
    - added NetconfConfigurationActivator for loading ManagedService
    - updating configuration from karaf netconf.cfg file
    - edited NetconfTCPActivator, NetconfSSHActivator for run with
      cfg values
    
    Change-Id: I5069a9f307a9a9b7c2d79723dc6c1cca0b2cc201
    Signed-off-by: Rudolf Brisuda <rudolf.brisuda@pantheon.tech>

--- netconf/netconf-util/src/main/java/org/opendaylight/netconf/util/osgi/NetconfConfigUtil.java
+++ netconf/netconf-util/src/main/java/org/opendaylight/netconf/util/osgi/NetconfConfigUtil.java
@@ -11,8 +11,12 @@ package org.opendaylight.netconf.util.osgi;
 import com.google.common.base.Optional;
 import io.netty.channel.local.LocalAddress;
 import java.net.InetSocketAddress;
+import java.util.Collection;
 import java.util.concurrent.TimeUnit;
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.InvalidSyntaxException;
+import org.osgi.framework.ServiceReference;
+import org.osgi.service.cm.ManagedService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -108,5 +112,22 @@ public final class NetconfConfigUtil {
             value = null;
         }
         return Optional.fromNullable(value);
+    }
+
+    public static java.util.Optional<NetconfConfiguration> getNetconfConfigurationService(BundleContext bundleContext) {
+        final Collection<ServiceReference<ManagedService>> serviceReferences;
+        try {
+            serviceReferences = bundleContext.getServiceReferences(ManagedService.class, null);
+            for (final ServiceReference<ManagedService> serviceReference : serviceReferences) {
+                ManagedService service = bundleContext.getService(serviceReference);
+                if (service instanceof NetconfConfiguration){
+                    return java.util.Optional.of((NetconfConfiguration) service);
+                }
+            }
+        } catch (InvalidSyntaxException e) {
+            LOG.error("Unable to retrieve references for ManagedService: {}", e);
+        }
+        LOG.error("Unable to retrieve NetconfConfiguration service. Not found. Bundle netconf-util probably failed.");
+        return java.util.Optional.empty();
     }
 }
