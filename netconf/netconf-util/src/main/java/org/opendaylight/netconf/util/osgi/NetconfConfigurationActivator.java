dcb4c9422bc1c685b8e4af62a1b5e722644e79e1
Rudolf Brisuda <rudolf.brisuda@pantheon.tech>
2016-08-17 14:03:04 +0200

    Bug 6023 - Adress for config subsystem netconf endpoint is not configurable
    - added NetconfConfigurationActivator for loading ManagedService
    - updating configuration from karaf netconf.cfg file
    - edited NetconfTCPActivator, NetconfSSHActivator for run with
      cfg values
    
    Change-Id: I5069a9f307a9a9b7c2d79723dc6c1cca0b2cc201
    Signed-off-by: Rudolf Brisuda <rudolf.brisuda@pantheon.tech>

---
+++ netconf/netconf-util/src/main/java/org/opendaylight/netconf/util/osgi/NetconfConfigurationActivator.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.netconf.util.osgi;
+
+import java.util.Hashtable;
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.Constants;
+import org.osgi.framework.ServiceRegistration;
+import org.osgi.service.cm.ManagedService;
+
+public class NetconfConfigurationActivator implements BundleActivator {
+    private static final String CONFIG_PID = "netconf";
+    private ServiceRegistration configService;
+
+    @Override
+    public void start(BundleContext bundleContext) {
+        configService = bundleContext.registerService(ManagedService.class.getName(),
+                NetconfConfiguration.getInstance() , getNetconfConfigProperties());
+    }
+
+    @Override
+    public void stop(BundleContext bundleContext) {
+      	if (configService == null) {
+          configService.unregister();
+          configService = null;
+        }   
+    }
+
+    private Hashtable<String, String> getNetconfConfigProperties(){
+        Hashtable<String, String> properties = new Hashtable<>();
+        properties.put(Constants.SERVICE_PID, CONFIG_PID);
+        return properties;
+    }
+}
