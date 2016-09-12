0fb7bf7e8d840eb3fb5297b446ec52f56e0e09d1
Jakub Toth <jatoth@cisco.com>
2016-09-12 15:30:06 +0200

    Bug 6099 - add instance identifier support like value + test
    Change-Id: I4f6a070b5814ef2a33b68534d887745a099712c8
    Signed-off-by: Jakub Toth <jatoth@cisco.com>

---
+++ restconf/sal-rest-connector/src/test/java/org/opendaylight/controller/sal/rest/impl/InstanceIdentifierTypeLeafTest.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.controller.sal.rest.impl;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.opendaylight.controller.md.sal.rest.common.TestRestconfUtils;
+import org.opendaylight.netconf.sal.restconf.impl.ControllerContext;
+import org.opendaylight.netconf.sal.restconf.impl.InstanceIdentifierContext;
+import org.opendaylight.yangtools.yang.common.QName;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.NodeIdentifierWithPredicates;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.PathArgument;
+import org.opendaylight.yangtools.yang.model.api.SchemaContext;
+
+public class InstanceIdentifierTypeLeafTest {
+
+    @Test
+    public void test() throws Exception {
+        final SchemaContext schemaContext = TestRestconfUtils.loadSchemaContext("/instanceidentifier");
+        ControllerContext.getInstance().setGlobalSchema(schemaContext);
+        final InstanceIdentifierContext<?> instanceIdentifier =
+                ControllerContext.getInstance().toInstanceIdentifier(
+                        "/iid-value-module:cont-iid/iid-list/%2Fiid-value-module%3Acont-iid%2Fiid-value-module%3A"
+                                + "values-iid%5Biid-value-module:value-iid='value'%5D");
+        final YangInstanceIdentifier yiD = instanceIdentifier.getInstanceIdentifier();
+        Assert.assertNotNull(yiD);
+        final PathArgument lastPathArgument = yiD.getLastPathArgument();
+        Assert.assertTrue(lastPathArgument.getNodeType().getNamespace().toString().equals("iid:value:module"));
+        Assert.assertTrue(lastPathArgument.getNodeType().getLocalName().equals("iid-list"));
+
+        final NodeIdentifierWithPredicates list = (NodeIdentifierWithPredicates) lastPathArgument;
+        final YangInstanceIdentifier value = (YangInstanceIdentifier) list.getKeyValues()
+                .get(QName.create(lastPathArgument.getNodeType(), "iid-leaf"));
+        final PathArgument lastPathArgumentOfValue = value.getLastPathArgument();
+        Assert.assertTrue(lastPathArgumentOfValue.getNodeType().getNamespace().toString().equals("iid:value:module"));
+        Assert.assertTrue(lastPathArgumentOfValue.getNodeType().getLocalName().equals("values-iid"));
+
+        final NodeIdentifierWithPredicates valueList = (NodeIdentifierWithPredicates) lastPathArgumentOfValue;
+        final String valueIid = (String) valueList.getKeyValues()
+                .get(QName.create(lastPathArgumentOfValue.getNodeType(), "value-iid"));
+        Assert.assertEquals("value", valueIid);
+    }
+
+}
