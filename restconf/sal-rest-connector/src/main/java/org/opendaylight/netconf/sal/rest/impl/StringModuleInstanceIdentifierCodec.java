7d5e401f1403b7cf91867f15c5891a382c222876
Jakub Toth <jatoth@cisco.com>
2016-09-12 15:30:06 +0200

    Bug 6099 - add instance identifier support like value + test
    Change-Id: I4f6a070b5814ef2a33b68534d887745a099712c8
    Signed-off-by: Jakub Toth <jatoth@cisco.com>

--- restconf/sal-rest-connector/src/main/java/org/opendaylight/netconf/sal/rest/impl/StringModuleInstanceIdentifierCodec.java
+++ restconf/sal-rest-connector/src/main/java/org/opendaylight/netconf/sal/rest/impl/StringModuleInstanceIdentifierCodec.java
@@ -12,6 +12,7 @@ import com.google.common.base.Preconditions;
 import java.net.URI;
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
+import org.opendaylight.restconf.utils.patch.Draft15StringModuleInstanceIdentifierCodec;
 import org.opendaylight.yangtools.yang.data.util.AbstractModuleStringInstanceIdentifierCodec;
 import org.opendaylight.yangtools.yang.data.util.DataSchemaContextTree;
 import org.opendaylight.yangtools.yang.model.api.Module;
@@ -19,29 +20,29 @@ import org.opendaylight.yangtools.yang.model.api.SchemaContext;
 
 /**
  * @deprecated This class will be replaced by
- * {@link org.opendaylight.restconf.utils.patch.Draft11StringModuleInstanceIdentifierCodec}
+ *             {@link Draft15StringModuleInstanceIdentifierCodec}
  */
 @Deprecated
-final class StringModuleInstanceIdentifierCodec extends AbstractModuleStringInstanceIdentifierCodec {
+public final class StringModuleInstanceIdentifierCodec extends AbstractModuleStringInstanceIdentifierCodec {
 
     private final DataSchemaContextTree dataContextTree;
     private final SchemaContext context;
     private final String defaultPrefix;
 
-    StringModuleInstanceIdentifierCodec(SchemaContext context) {
+    public StringModuleInstanceIdentifierCodec(final SchemaContext context) {
         this.context = Preconditions.checkNotNull(context);
         this.dataContextTree = DataSchemaContextTree.from(context);
         this.defaultPrefix = "";
     }
 
-    StringModuleInstanceIdentifierCodec(SchemaContext context, @Nonnull String defaultPrefix) {
+    StringModuleInstanceIdentifierCodec(final SchemaContext context, @Nonnull final String defaultPrefix) {
         this.context = Preconditions.checkNotNull(context);
         this.dataContextTree = DataSchemaContextTree.from(context);
         this.defaultPrefix = defaultPrefix;
     }
 
     @Override
-    protected Module moduleForPrefix(@Nonnull String prefix) {
+    protected Module moduleForPrefix(@Nonnull final String prefix) {
         if (prefix.isEmpty() && !this.defaultPrefix.isEmpty()) {
             return this.context.findModuleByName(this.defaultPrefix, null);
         } else {
@@ -57,7 +58,7 @@ final class StringModuleInstanceIdentifierCodec extends AbstractModuleStringInst
 
     @Nullable
     @Override
-    protected String prefixForNamespace(@Nonnull URI namespace) {
+    protected String prefixForNamespace(@Nonnull final URI namespace) {
         final Module module = this.context.findModuleByNamespaceAndRevision(namespace, null);
         return module == null ? null : module.getName();
     }
