cbd2ae423615df815220b067a824b1d240420622
Andrej Mak <andrej.mak@pantheon.tech>
2016-09-14 08:08:31 +0200

    Bug 6696: Add transaction chain support to netconf connector
    Change-Id: I3d680be67589e7f005ef470f0b3896c3256fca4b
    Signed-off-by: Andrej Mak <andrej.mak@pantheon.tech>

---
+++ netconf/sal-netconf-connector/src/test/java/org/opendaylight/netconf/sal/connect/netconf/sal/tx/TxChainTest.java
@@ -0,0 +1,128 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.netconf.sal.connect.netconf.sal.tx;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.verify;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.opendaylight.controller.md.sal.common.api.data.TransactionChainClosedException;
+import org.opendaylight.controller.md.sal.common.api.data.TransactionChainListener;
+import org.opendaylight.controller.md.sal.common.api.data.TransactionCommitFailedException;
+import org.opendaylight.controller.md.sal.dom.api.DOMDataBroker;
+import org.opendaylight.controller.md.sal.dom.api.DOMDataReadOnlyTransaction;
+import org.opendaylight.yangtools.concepts.ListenerRegistration;
+
+public class TxChainTest {
+
+    @Mock
+    private DOMDataBroker broker;
+    @Mock
+    private TransactionChainListener listener;
+    @Mock
+    private DOMDataReadOnlyTransaction readOnlyTx;
+    @Mock
+    private AbstractWriteTx writeOnlyTx;
+    @Mock
+    private ListenerRegistration<TxListener> registration;
+    private final ArgumentCaptor<TxListener> captor = ArgumentCaptor.forClass(TxListener.class);
+    private TxChain chain;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+        doReturn(readOnlyTx).when(broker).newReadOnlyTransaction();
+        doReturn(writeOnlyTx).when(broker).newWriteOnlyTransaction();
+        doReturn(registration).when(writeOnlyTx).addListener(any());
+        chain = new TxChain(broker, listener);
+    }
+
+    @Test()
+    public void testNewReadOnlyTransactionPrevCompleted() throws Exception {
+        chain.newWriteOnlyTransaction();
+        verify(writeOnlyTx).addListener(captor.capture());
+        captor.getValue().onTransactionSuccessful(writeOnlyTx);
+        verify(registration).close();
+        chain.newReadOnlyTransaction();
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void testNewReadOnlyTransactionPrevNotCompleted() throws Exception {
+        chain.newWriteOnlyTransaction();
+        chain.newReadOnlyTransaction();
+    }
+
+    @Test
+    public void testNewReadWriteTransactionPrevCompleted() throws Exception {
+        doReturn(true).when(writeOnlyTx).isFinished();
+        chain.newReadWriteTransaction();
+        verify(writeOnlyTx).addListener(captor.capture());
+        captor.getValue().onTransactionSuccessful(writeOnlyTx);
+        verify(registration).close();
+        chain.newReadWriteTransaction();
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void testNewReadWriteTransactionPrevNotCompleted() throws Exception {
+        chain.newReadWriteTransaction();
+        chain.newReadWriteTransaction();
+    }
+
+    @Test
+    public void testNewWriteOnlyTransactionPrevCompleted() throws Exception {
+        chain.newWriteOnlyTransaction();
+        verify(writeOnlyTx).addListener(captor.capture());
+        captor.getValue().onTransactionSuccessful(writeOnlyTx);
+        verify(registration).close();
+        chain.newWriteOnlyTransaction();
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void testNewWriteOnlyTransactionPrevNotCompleted() throws Exception {
+        chain.newWriteOnlyTransaction();
+        chain.newWriteOnlyTransaction();
+    }
+
+    @Test(expected = TransactionChainClosedException.class)
+    public void testCloseAfterFinished() throws Exception {
+        chain.close();
+        verify(listener).onTransactionChainSuccessful(chain);
+        chain.newReadOnlyTransaction();
+    }
+
+    @Test
+    public void testChainFail() throws Exception {
+        final AbstractWriteTx writeTx = chain.newWriteOnlyTransaction();
+        final ArgumentCaptor<TxListener> captor = ArgumentCaptor.forClass(TxListener.class);
+        verify(writeOnlyTx).addListener(captor.capture());
+        writeTx.submit();
+        final TransactionCommitFailedException cause = new TransactionCommitFailedException("fail");
+        captor.getValue().onTransactionFailed(writeOnlyTx, cause);
+        verify(registration).close();
+        verify(listener).onTransactionChainFailed(chain, writeOnlyTx, cause);
+    }
+
+    @Test
+    public void testChainSuccess() throws Exception {
+        final AbstractWriteTx writeTx = chain.newWriteOnlyTransaction();
+        chain.close();
+        final ArgumentCaptor<TxListener> captor = ArgumentCaptor.forClass(TxListener.class);
+        verify(writeOnlyTx).addListener(captor.capture());
+        writeTx.submit();
+        captor.getValue().onTransactionSuccessful(writeOnlyTx);
+        verify(registration).close();
+        verify(listener).onTransactionChainSuccessful(chain);
+    }
+
+}
