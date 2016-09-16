4038d853baeada84df0adf6091281638e3969dd0
Alexis de Talhouët <adetalhouet@inocybe.com>
2016-09-16 09:50:38 -0400

    Add blueprint wiring for netconf-console
    Change-Id: Ic4e33ea7717511c26aa9cc9cbde4148f3e5549ac
    Signed-off-by: Alexis de Talhouët <adetalhouet@inocybe.com>

--- netconf/netconf-console/src/main/java/org/opendaylight/netconf/console/impl/NetconfConsoleProvider.java
+++ netconf/netconf-console/src/main/java/org/opendaylight/netconf/console/impl/NetconfConsoleProvider.java
@@ -10,36 +10,18 @@ package org.opendaylight.netconf.console.impl;
 
 import org.opendaylight.controller.md.sal.binding.api.DataBroker;
 import org.opendaylight.controller.md.sal.binding.api.MountPointService;
-import org.opendaylight.controller.sal.binding.api.BindingAwareBroker.ProviderContext;
-import org.opendaylight.controller.sal.binding.api.BindingAwareProvider;
 import org.opendaylight.netconf.console.api.NetconfCommands;
 import org.osgi.framework.BundleContext;
-import org.osgi.framework.FrameworkUtil;
 import org.osgi.framework.ServiceRegistration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class NetconfConsoleProvider implements BindingAwareProvider, AutoCloseable {
+public class NetconfConsoleProvider implements AutoCloseable {
 
     private static final Logger LOG = LoggerFactory.getLogger(NetconfConsoleProvider.class);
     private ServiceRegistration<NetconfCommands> netconfConsoleRegistration;
 
-    @Override
-    public void onSessionInitiated(ProviderContext session) {
-        LOG.info("NetconfProvider Session Initiated");
-
-        // Retrieve DataBroker service to interact with md-sal
-        final DataBroker dataBroker =  session.getSALService(DataBroker.class);
-
-        // Retrieve MountPointService to interact with NETCONF remote devices connected to ODL and register it
-        final MountPointService mountService = session.getSALService(MountPointService.class);
-        final BundleContext context = FrameworkUtil.getBundle(this.getClass()).getBundleContext();
-
-        // Initialization of NETCONF Console Provider service implementation
-        initializeNetconfConsoleProvider(dataBroker, context, mountService);
-    }
-
-    private void initializeNetconfConsoleProvider(DataBroker dataBroker, BundleContext context, MountPointService mountService) {
+    public NetconfConsoleProvider(DataBroker dataBroker, BundleContext context, MountPointService mountService) {
         // Initialize NetconfConsoleProviderImpl class
         final NetconfCommandsImpl consoleProvider = new NetconfCommandsImpl(dataBroker, mountService);
 
