e007b16befb4307e47de5e03aabb461e2051e1b1
Alexis de Talhouët <adetalhouet@inocybe.com>
2016-09-20 15:57:02 -0400

    Add blueprint wiring for netconf-impl
    - create mdsal-netconf-impl bundle containing the blueprint xml
    file to start-up the md-sal netconf server implementation.
    
    Change-Id: I43e57671651d4321f7a955376b6f6850cc430f56
    Signed-off-by: Alexis de Talhouët <adetalhouet@inocybe.com>

--- netconf/netconf-impl/src/main/java/org/opendaylight/controller/config/yang/config/netconf/northbound/impl/NetconfServerMonitoringModuleFactory.java
+++ netconf/netconf-impl/src/main/java/org/opendaylight/controller/config/yang/config/netconf/northbound/impl/NetconfServerMonitoringModuleFactory.java
@@ -16,6 +16,30 @@
 * Do not modify this file unless it is present under src/main directory
 */
 package org.opendaylight.controller.config.yang.config.netconf.northbound.impl;
-public class NetconfServerMonitoringModuleFactory extends org.opendaylight.controller.config.yang.config.netconf.northbound.impl.AbstractNetconfServerMonitoringModuleFactory {
 
+import org.opendaylight.controller.config.api.DependencyResolver;
+import org.osgi.framework.BundleContext;
+
+/**
+ * @deprecated Replaced by blueprint wiring
+ */
+@Deprecated
+public class NetconfServerMonitoringModuleFactory extends AbstractNetconfServerMonitoringModuleFactory {
+
+    @Override
+    public NetconfServerMonitoringModule instantiateModule(String instanceName, DependencyResolver dependencyResolver,
+                                                           NetconfServerMonitoringModule oldModule, AutoCloseable oldInstance, BundleContext bundleContext) {
+        NetconfServerMonitoringModule module = super.instantiateModule(instanceName, dependencyResolver, oldModule,
+                oldInstance, bundleContext);
+        module.setBundleContext(bundleContext);
+        return module;
+    }
+
+    @Override
+    public NetconfServerMonitoringModule instantiateModule(String instanceName, DependencyResolver dependencyResolver,
+                                                           BundleContext bundleContext) {
+        NetconfServerMonitoringModule module = super.instantiateModule(instanceName, dependencyResolver, bundleContext);
+        module.setBundleContext(bundleContext);
+        return module;
+    }
 }
