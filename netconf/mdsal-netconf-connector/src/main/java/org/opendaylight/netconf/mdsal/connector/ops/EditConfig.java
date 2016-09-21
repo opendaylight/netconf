c2978003286ca9a8d4a829ef0cc79dadfe55afec
Andrej Mak <andrej.mak@pantheon.tech>
2016-09-21 12:11:00 +0200

    Fix edit-config behavior
    Check for existence of mixin map node. If it doesn't exist,
    create it. Only then write map entry.
    
    Before this patch if user tried to create map entry,
    but parent mixin map node didn't exist, edit config failed.
    
    Change-Id: I4004a66100eb4e0d0918005c54b06fdebc8ea6dd
    Signed-off-by: Andrej Mak <andrej.mak@pantheon.tech>

--- netconf/mdsal-netconf-connector/src/main/java/org/opendaylight/netconf/mdsal/connector/ops/EditConfig.java
+++ netconf/mdsal-netconf-connector/src/main/java/org/opendaylight/netconf/mdsal/connector/ops/EditConfig.java
@@ -32,7 +32,10 @@ import org.opendaylight.netconf.util.mapping.AbstractSingletonNetconfOperation;
 import org.opendaylight.yangtools.yang.common.QName;
 import org.opendaylight.yangtools.yang.data.api.ModifyAction;
 import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
+import org.opendaylight.yangtools.yang.data.api.schema.MapEntryNode;
+import org.opendaylight.yangtools.yang.data.api.schema.MapNode;
 import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNode;
+import org.opendaylight.yangtools.yang.data.impl.schema.Builders;
 import org.opendaylight.yangtools.yang.data.impl.schema.transform.dom.DomUtils;
 import org.opendaylight.yangtools.yang.data.impl.schema.transform.dom.parser.DomToNormalizedNodeParserFactory;
 import org.opendaylight.yangtools.yang.model.api.ContainerSchemaNode;
@@ -76,7 +79,7 @@ public class EditConfig extends AbstractSingletonNetconfOperation {
 
         final XmlElement configElement = getElement(operationElement, CONFIG_KEY);
 
-        for (XmlElement element : configElement.getChildElements()) {
+        for (final XmlElement element : configElement.getChildElements()) {
             final String ns = element.getNamespace();
             final DataSchemaNode schemaNode = getSchemaNodeFromNamespace(ns, element).get();
 
@@ -115,11 +118,14 @@ public class EditConfig extends AbstractSingletonNetconfOperation {
                     throw new DocumentedException("Data already exists, cannot execute CREATE operation", ErrorType.protocol, ErrorTag.data_exists, ErrorSeverity.error);
                 }
                 rwtx.put(LogicalDatastoreType.CONFIGURATION, YangInstanceIdentifier.create(change.getPath()), change.getChangeRoot());
-            } catch (ReadFailedException e) {
+            } catch (final ReadFailedException e) {
                 LOG.warn("Read from datastore failed when trying to read data for create operation", change, e);
             }
             break;
         case REPLACE:
+            if (change.getChangeRoot() instanceof MapEntryNode) {
+                createMixinNodeIfNonExistent(rwtx, change);
+            }
             rwtx.put(LogicalDatastoreType.CONFIGURATION, YangInstanceIdentifier.create(change.getPath()), change.getChangeRoot());
             break;
         case DELETE:
@@ -129,7 +135,7 @@ public class EditConfig extends AbstractSingletonNetconfOperation {
                     throw new DocumentedException("Data is missing, cannot execute DELETE operation", ErrorType.protocol, ErrorTag.data_missing, ErrorSeverity.error);
                 }
                 rwtx.delete(LogicalDatastoreType.CONFIGURATION, YangInstanceIdentifier.create(change.getPath()));
-            } catch (ReadFailedException e) {
+            } catch (final ReadFailedException e) {
                 LOG.warn("Read from datastore failed when trying to read data for delete operation", change, e);
             }
             break;
@@ -138,6 +144,23 @@ public class EditConfig extends AbstractSingletonNetconfOperation {
             break;
         default:
             LOG.warn("Unknown/not implemented operation, not executing");
+        }
+    }
+
+    private void createMixinNodeIfNonExistent(final DOMDataReadWriteTransaction rwtx, final DataTreeChange change) throws DocumentedException {
+        final List<YangInstanceIdentifier.PathArgument> path = change.getPath();
+        final YangInstanceIdentifier mixinNodeYid = YangInstanceIdentifier.create(path.subList(0, path.size() - 1));
+        try {
+            final Boolean mapNodeMixinExists = rwtx.exists(LogicalDatastoreType.CONFIGURATION, mixinNodeYid).checkedGet();
+            if (!mapNodeMixinExists) {
+                final MapNode mixinNode = Builders.mapBuilder()
+                        .withNodeIdentifier(new YangInstanceIdentifier.NodeIdentifier(mixinNodeYid.getLastPathArgument().getNodeType()))
+                        .build();
+                rwtx.put(LogicalDatastoreType.CONFIGURATION, mixinNodeYid, mixinNode);
+            }
+        } catch (final ReadFailedException e) {
+            throw new DocumentedException("List node existence check failed", e, ErrorType.protocol, ErrorTag.data_missing, ErrorSeverity.error);
+
         }
     }
 
@@ -172,7 +195,7 @@ public class EditConfig extends AbstractSingletonNetconfOperation {
                 throw new NetconfDocumentedException("Unable to find module by namespace: " + namespace,
                         ErrorType.application, ErrorTag.unknown_namespace, ErrorSeverity.error);
             }
-            DataSchemaNode schemaNode =
+            final DataSchemaNode schemaNode =
                     module.getDataChildByName(QName.create(module.getQNameModule(), element.getName()));
             if (schemaNode != null) {
                 dataSchemaNode = Optional.of(schemaNode);
@@ -182,7 +205,7 @@ public class EditConfig extends AbstractSingletonNetconfOperation {
                         ErrorTag.unknown_namespace,
                         ErrorSeverity.error);
             }
-        } catch (URISyntaxException e) {
+        } catch (final URISyntaxException e) {
             LOG.debug("Unable to create URI for namespace : {}", namespace);
         }
 
@@ -215,7 +238,7 @@ public class EditConfig extends AbstractSingletonNetconfOperation {
 
     }
 
-    private XmlElement getElement(final XmlElement operationElement, String elementName) throws DocumentedException {
+    private XmlElement getElement(final XmlElement operationElement, final String elementName) throws DocumentedException {
         final Optional<XmlElement> childNode = operationElement.getOnlyChildElementOptionally(elementName);
         if (!childNode.isPresent()) {
             throw new DocumentedException(elementName + " element is missing",
