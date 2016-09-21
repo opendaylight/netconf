bb09462b6860b600a5d51d0d237d36aae9db8389
Matus.Kubica <Matus.Kubica@pantheon.tech>
2016-09-21 18:11:26 +0200

    Check Testool YANG filenames for revision
    Check Yang files, if its name contains revision date
    Replace original name without revision with name in format xxx-yyy@2000-01-01
    
    Change-Id: Icb123cc0f173295fe98c7913ce376e805ddbb9b1
    Signed-off-by: Matus.Kubica <Matus.Kubica@pantheon.tech>

--- netconf/tools/netconf-testtool/src/main/java/org/opendaylight/netconf/test/tool/Main.java
+++ netconf/tools/netconf-testtool/src/main/java/org/opendaylight/netconf/test/tool/Main.java
@@ -20,7 +20,6 @@ import com.google.common.io.Files;
 import java.io.File;
 import java.io.FileFilter;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
@@ -111,6 +110,8 @@ public final class Main {
                 throw new RuntimeException(e);
             }
         }
+
+
     }
 
     static class ConfigGenerator {
