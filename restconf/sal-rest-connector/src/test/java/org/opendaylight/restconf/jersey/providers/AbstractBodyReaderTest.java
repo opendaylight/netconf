2416133e960ca2baad170cf2e8d28cc58610dba8
Ivan Hrasko <ivan.hrasko@pantheon.tech>
2016-09-23 13:33:13 +0200

    Bug 6272 - support RESTCONF PATCH for mounted NETCONF nodes
    - solution for Restconf draft17
    - make mount point service available when identifier is parsed
    - added unit tests
    
    Change-Id: Ie3c5cdc9cbcb10360ac9d0a082eab52efba75674
    Signed-off-by: Ivan Hrasko <ivan.hrasko@pantheon.tech>

---
+++ restconf/sal-rest-connector/src/test/java/org/opendaylight/restconf/jersey/providers/AbstractBodyReaderTest.java
@@ -0,0 +1,118 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.restconf.jersey.providers;
+
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.lang.reflect.Field;
+import java.util.Collections;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.MultivaluedHashMap;
+import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.core.Request;
+import javax.ws.rs.core.UriInfo;
+import org.opendaylight.controller.md.sal.rest.common.TestRestconfUtils;
+import org.opendaylight.netconf.sal.rest.api.RestconfConstants;
+import org.opendaylight.netconf.sal.restconf.impl.ControllerContext;
+import org.opendaylight.netconf.sal.restconf.impl.NormalizedNodeContext;
+import org.opendaylight.netconf.sal.restconf.impl.PATCHContext;
+import org.opendaylight.restconf.RestConnectorProvider;
+import org.opendaylight.restconf.handlers.DOMMountPointServiceHandler;
+import org.opendaylight.yangtools.yang.model.api.SchemaContext;
+
+abstract class AbstractBodyReaderTest {
+
+    protected final static ControllerContext controllerContext = ControllerContext.getInstance();
+    protected final MediaType mediaType;
+    private static Field uriField;
+    private static Field requestField;
+    protected final static DOMMountPointServiceHandler mountPointServiceHandler = mock(
+            DOMMountPointServiceHandler.class);
+
+    AbstractBodyReaderTest() throws NoSuchFieldException, IllegalAccessException {
+        uriField = AbstractIdentifierAwareJaxRsProvider.class
+                .getDeclaredField("uriInfo");
+        uriField.setAccessible(true);
+
+        requestField = AbstractIdentifierAwareJaxRsProvider.class
+                .getDeclaredField("request");
+        requestField.setAccessible(true);
+
+        mediaType = getMediaType();
+
+        final Field mountPointServiceHandlerField = RestConnectorProvider.class.
+                getDeclaredField("mountPointServiceHandler");
+        mountPointServiceHandlerField.setAccessible(true);
+        mountPointServiceHandlerField.set(RestConnectorProvider.class, mountPointServiceHandler);
+    }
+
+    protected abstract MediaType getMediaType();
+
+    protected static SchemaContext schemaContextLoader(final String yangPath,
+            final SchemaContext schemaContext) {
+        return TestRestconfUtils.loadSchemaContext(yangPath, schemaContext);
+    }
+
+    protected static <T extends AbstractIdentifierAwareJaxRsProvider> void mockBodyReader(
+            final String identifier, final T normalizedNodeProvider,
+            final boolean isPost) throws NoSuchFieldException,
+            SecurityException, IllegalArgumentException, IllegalAccessException {
+        final UriInfo uriInfoMock = mock(UriInfo.class);
+        final MultivaluedMap<String, String> pathParm = new MultivaluedHashMap<>(1);
+
+        if (!identifier.isEmpty()) {
+            pathParm.put(RestconfConstants.IDENTIFIER, Collections.singletonList(identifier));
+        }
+
+        when(uriInfoMock.getPathParameters()).thenReturn(pathParm);
+        when(uriInfoMock.getPathParameters(false)).thenReturn(pathParm);
+        when(uriInfoMock.getPathParameters(true)).thenReturn(pathParm);
+        uriField.set(normalizedNodeProvider, uriInfoMock);
+
+        final Request request = mock(Request.class);
+        if (isPost) {
+            when(request.getMethod()).thenReturn("POST");
+        } else {
+            when(request.getMethod()).thenReturn("PUT");
+        }
+
+        requestField.set(normalizedNodeProvider, request);
+    }
+
+    protected static void checkMountPointNormalizedNodeContext(
+            final NormalizedNodeContext nnContext) {
+        checkNormalizedNodeContext(nnContext);
+        assertNotNull(nnContext.getInstanceIdentifierContext().getMountPoint());
+    }
+
+    protected static void checkNormalizedNodeContext(
+            final NormalizedNodeContext nnContext) {
+        assertNotNull(nnContext.getData());
+        assertNotNull(nnContext.getInstanceIdentifierContext()
+                .getInstanceIdentifier());
+        assertNotNull(nnContext.getInstanceIdentifierContext()
+                .getSchemaContext());
+        assertNotNull(nnContext.getInstanceIdentifierContext().getSchemaNode());
+    }
+
+    protected static void checkPATCHContext(final PATCHContext patchContext) {
+        assertNotNull(patchContext.getData());
+        assertNotNull(patchContext.getInstanceIdentifierContext().getInstanceIdentifier());
+        assertNotNull(patchContext.getInstanceIdentifierContext().getSchemaContext());
+        assertNotNull(patchContext.getInstanceIdentifierContext().getSchemaNode());
+    }
+
+    protected static void checkPATCHContextMountPoint(final PATCHContext patchContext) {
+        checkPATCHContext(patchContext);
+        assertNotNull(patchContext.getInstanceIdentifierContext().getMountPoint());
+        assertNotNull(patchContext.getInstanceIdentifierContext().getMountPoint().getSchemaContext());
+    }
+}
