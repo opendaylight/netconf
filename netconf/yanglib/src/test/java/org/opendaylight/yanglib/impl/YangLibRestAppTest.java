e169047179dff78a22c7ee99d446499878ea21e9
Tomas Cere <tcere@cisco.com>
2016-10-04 13:08:32 +0200

    Use BDD style mocking everywhere if used once.
    Change-Id: Ie8a67f7ef8b07037d37e4813e0afe2908707c1d5
    Signed-off-by: Tomas Cere <tcere@cisco.com>

--- netconf/yanglib/src/test/java/org/opendaylight/yanglib/impl/YangLibRestAppTest.java
+++ netconf/yanglib/src/test/java/org/opendaylight/yanglib/impl/YangLibRestAppTest.java
@@ -9,17 +9,16 @@
 package org.opendaylight.yanglib.impl;
 
 import static junit.framework.TestCase.assertTrue;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
 import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 
 import java.util.Set;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.BDDMockito;
 import org.opendaylight.yanglib.api.YangLibRestAppService;
 import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleContext;
@@ -39,14 +38,14 @@ public class YangLibRestAppTest {
         final BundleContext bundleContext = mock(BundleContext.class);
         final Bundle bundle = mock(Bundle.class);
 
-        BDDMockito.given(FrameworkUtil.getBundle(any())).willReturn(bundle);
-        when(bundle.getBundleContext()).thenReturn(bundleContext);
+        given(FrameworkUtil.getBundle(any())).willReturn(bundle);
+        given(bundle.getBundleContext()).willReturn(bundleContext);
 
         final YangLibRestApp yangLibRestApp = new YangLibRestApp();
         final Set singleton = yangLibRestApp.getSingletons();
 
         assertTrue(singleton.contains(yangLibRestApp.getYangLibService()));
 
-        verify(bundleContext, times(1)).registerService(eq(YangLibRestAppService.class.getName()), eq(yangLibRestApp), eq(null));
+        then(bundleContext).should(times(1)).registerService(eq(YangLibRestAppService.class.getName()), eq(yangLibRestApp), eq(null));
     }
 }
