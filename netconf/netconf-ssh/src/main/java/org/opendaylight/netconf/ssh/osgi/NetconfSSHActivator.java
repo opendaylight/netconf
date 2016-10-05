d2f2bd9e32a13a8fdda0be82e3970c7582f982a9
Alexis de Talhouët <adetalhouet@inocybe.com>
2016-10-05 18:36:15 -0400

    DO NOT MERGE - Move NETCONF to blueprint
    This is for test purposes
    
    Change-Id: I8ecad804d30ec2dae45b6eafe6db4b87684bce42
    Signed-off-by: Alexis de Talhouët <adetalhouet@inocybe.com>

--- netconf/netconf-ssh/src/main/java/org/opendaylight/netconf/ssh/osgi/NetconfSSHActivator.java
+++ netconf/netconf-ssh/src/main/java/org/opendaylight/netconf/ssh/osgi/NetconfSSHActivator.java
@@ -15,25 +15,25 @@ import java.net.InetSocketAddress;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.ThreadFactory;
 import org.apache.sshd.common.util.ThreadUtils;
 import org.apache.sshd.server.keyprovider.PEMGeneratorHostKeyProvider;
 import org.opendaylight.netconf.ssh.SshProxyServer;
 import org.opendaylight.netconf.ssh.SshProxyServerConfigurationBuilder;
 import org.opendaylight.netconf.util.osgi.NetconfConfigUtil;
 import org.opendaylight.netconf.util.osgi.NetconfConfigUtil.InfixProp;
-import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class NetconfSSHActivator implements BundleActivator {
+public class NetconfSSHActivator {
     private static final Logger LOG = LoggerFactory.getLogger(NetconfSSHActivator.class);
 
     private static final java.lang.String ALGORITHM = "RSA";
     private static final int KEY_SIZE = 4096;
     public static final int POOL_SIZE = 8;
     private static final int DEFAULT_IDLE_TIMEOUT = Integer.MAX_VALUE;
+
+    private final BundleContext bundleContext;
 
     private ScheduledExecutorService minaTimerExecutor;
     private NioEventLoopGroup clientGroup;
@@ -42,43 +42,21 @@ public class NetconfSSHActivator implements BundleActivator {
 
     private SshProxyServer server;
 
-    @Override
-    public void start(final BundleContext bundleContext) throws IOException {
-        minaTimerExecutor = Executors.newScheduledThreadPool(POOL_SIZE, new ThreadFactory() {
-            @Override
-            public Thread newThread(final Runnable r) {
-                return new Thread(r, "netconf-ssh-server-mina-timers");
-            }
-        });
+    public NetconfSSHActivator(final BundleContext bundleContext) {
+        this.bundleContext = bundleContext;
+    }
+
+    /**
+     * Invoke by blueprint
+     */
+    public void start() {
+        minaTimerExecutor = Executors.newScheduledThreadPool(POOL_SIZE, r -> new Thread(r, "netconf-ssh-server-mina-timers"));
         clientGroup = new NioEventLoopGroup();
         nioExecutor = ThreadUtils.newFixedThreadPool("netconf-ssh-server-nio-group", POOL_SIZE);
         server = startSSHServer(bundleContext);
     }
 
-    @Override
-    public void stop(final BundleContext context) throws IOException {
-        if (server != null) {
-            server.close();
-        }
-
-        if(authProviderTracker != null) {
-            authProviderTracker.stop();
-        }
-
-        if(nioExecutor!=null) {
-            nioExecutor.shutdownNow();
-        }
-
-        if(clientGroup != null) {
-            clientGroup.shutdownGracefully();
-        }
-
-        if(minaTimerExecutor != null) {
-            minaTimerExecutor.shutdownNow();
-        }
-    }
-
-    private SshProxyServer startSSHServer(final BundleContext bundleContext) throws IOException {
+    private SshProxyServer startSSHServer(final BundleContext bundleContext) {
         final Optional<InetSocketAddress> maybeSshSocketAddress = NetconfConfigUtil.extractNetconfServerAddress(bundleContext, InfixProp.ssh);
         if (!maybeSshSocketAddress.isPresent()) {
             LOG.warn("SSH bridge not configured. Using default value {}", NetconfConfigUtil.DEFAULT_SSH_SERVER_ADRESS);
@@ -100,15 +78,45 @@ public class NetconfSSHActivator implements BundleActivator {
         LOG.trace("Starting netconf SSH bridge with path to ssh private key {}", path);
 
         final SshProxyServer sshProxyServer = new SshProxyServer(minaTimerExecutor, clientGroup, nioExecutor);
-        sshProxyServer.bind(
-                new SshProxyServerConfigurationBuilder()
-                        .setBindingAddress(sshSocketAddress)
-                        .setLocalAddress(localAddress)
-                        .setAuthenticator(authProviderTracker)
-                        .setKeyPairProvider(new PEMGeneratorHostKeyProvider(path, ALGORITHM, KEY_SIZE))
-                        .setIdleTimeout(DEFAULT_IDLE_TIMEOUT)
-                        .createSshProxyServerConfiguration());
+
+        try {
+            sshProxyServer.bind(
+                    new SshProxyServerConfigurationBuilder()
+                            .setBindingAddress(sshSocketAddress)
+                            .setLocalAddress(localAddress)
+                            .setAuthenticator(authProviderTracker)
+                            .setKeyPairProvider(new PEMGeneratorHostKeyProvider(path, ALGORITHM, KEY_SIZE))
+                            .setIdleTimeout(DEFAULT_IDLE_TIMEOUT)
+                            .createSshProxyServerConfiguration());
+        } catch (IOException e) {
+            LOG.error("Failed to bind ssh proxy server", e);
+        }
         return sshProxyServer;
+    }
+
+    /**
+     * Invoke by blueprint
+     */
+    public void stop() {
+        if (server != null) {
+            server.close();
+        }
+
+        if(authProviderTracker != null) {
+            authProviderTracker.stop();
+        }
+
+        if(nioExecutor!=null) {
+            nioExecutor.shutdownNow();
+        }
+
+        if(clientGroup != null) {
+            clientGroup.shutdownGracefully();
+        }
+
+        if(minaTimerExecutor != null) {
+            minaTimerExecutor.shutdownNow();
+        }
     }
 
 }
