f3a63a5aa4a59f3dd47f0e62ea900416e4a37811
Jakub Toth <jatoth@cisco.com>
2016-10-05 14:56:08 +0200

    Bug 6848 - repackage providers for jersey+create xml and json reader for restconf draft17
      *fix tests
      *rename tests part from draft11 to draft17
    
    Change-Id: I1315bb693eccc34c78f07ea0acbf45f6ff972455
    Signed-off-by: Jakub Toth <jatoth@cisco.com>

--- restconf/sal-rest-connector/src/main/java/org/opendaylight/restconf/jersey/providers/JsonToPATCHBodyReader.java
+++ restconf/sal-rest-connector/src/main/java/org/opendaylight/restconf/jersey/providers/JsonToPATCHBodyReader.java
@@ -6,7 +6,7 @@
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
 
-package org.opendaylight.restconf.utils.patch;
+package org.opendaylight.restconf.jersey.providers;
 
 import static org.opendaylight.netconf.sal.restconf.impl.PATCHEditOperation.isPatchOperationWithValue;
 
