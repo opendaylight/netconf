ba4cd4ef2e6718671c768b7b6585c02a559cafda
Ivan Hrasko <ivan.hrasko@pantheon.tech>
2016-10-07 13:55:38 +0200

    Bug 6903 - Implement Query parameters - fields
    Change-Id: I3896dba6faa3fdbcb00e07fdfd7b3fdc02ba57d9
    Signed-off-by: Ivan Hrasko <ivan.hrasko@pantheon.tech>

--- restconf/sal-rest-connector/src/main/java/org/opendaylight/netconf/sal/restconf/impl/WriterParameters.java
+++ restconf/sal-rest-connector/src/main/java/org/opendaylight/netconf/sal/restconf/impl/WriterParameters.java
@@ -9,15 +9,21 @@
 package org.opendaylight.netconf.sal.restconf.impl;
 
 import com.google.common.base.Optional;
+import com.google.common.collect.Lists;
+import java.util.List;
+import java.util.Set;
+import org.opendaylight.yangtools.yang.common.QName;
 
 public class WriterParameters {
     private final String content;
     private final Optional<Integer> depth;
+    private final List<Set<QName>> fields;
     private final boolean prettyPrint;
 
     private WriterParameters(final WriterParametersBuilder builder) {
         this.content = builder.content;
         this.depth = builder.depth;
+        this.fields = builder.fields;
         this.prettyPrint = builder.prettyPrint;
     }
 
@@ -29,6 +35,10 @@ public class WriterParameters {
         return depth;
     }
 
+    public List<Set<QName>> getFields() {
+        return fields;
+    }
+
     public boolean isPrettyPrint() {
         return prettyPrint;
     }
@@ -36,6 +46,7 @@ public class WriterParameters {
     public static class WriterParametersBuilder {
         private String content;
         private Optional<Integer> depth = Optional.absent();
+        private List<Set<QName>> fields = Lists.newLinkedList();
         private boolean prettyPrint;
 
         public WriterParametersBuilder() {}
@@ -47,6 +58,11 @@ public class WriterParameters {
 
         public WriterParametersBuilder setDepth(final int depth) {
             this.depth = Optional.of(depth);
+            return this;
+        }
+
+        public WriterParametersBuilder setFields(final List<Set<QName>> fields) {
+            this.fields = fields;
             return this;
         }
 
---
+++ restconf/sal-rest-connector/src/main/java/org/opendaylight/restconf/utils/parser/ParserFieldsParameter.java
@@ -0,0 +1,212 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.restconf.utils.parser;
+
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+import javax.annotation.Nonnull;
+import org.opendaylight.netconf.sal.restconf.impl.InstanceIdentifierContext;
+import org.opendaylight.netconf.sal.restconf.impl.RestconfDocumentedException;
+import org.opendaylight.netconf.sal.restconf.impl.RestconfError.ErrorTag;
+import org.opendaylight.netconf.sal.restconf.impl.RestconfError.ErrorType;
+import org.opendaylight.restconf.utils.parser.builder.ParserBuilderConstants.Deserializer;
+import org.opendaylight.yangtools.yang.common.QName;
+import org.opendaylight.yangtools.yang.data.util.DataSchemaContextNode;
+import org.opendaylight.yangtools.yang.model.api.DataSchemaNode;
+import org.opendaylight.yangtools.yang.model.api.SchemaContext;
+
+public class ParserFieldsParameter {
+    private static final char COLON = ':';
+    private static final char SEMICOLON = ';';
+    private static final char SLASH = '/';
+    private static final char STARTING_PARENTHESIS = '(';
+    private static final char CLOSING_PARENTHESIS = ')';
+
+    /**
+     * Pasrse fields parameter and return completed list of child nodes organized into levels.
+     * @param identifier identifier context created from request URI
+     * @param input input value of fields parameter
+     * @return {{@link List}}
+     */
+    public static @Nonnull List<Set<QName>> parseFieldsParameter(@Nonnull final InstanceIdentifierContext<?> identifier,
+                                                                 @Nonnull final String input) {
+        final List<Set<QName>> parsed = new LinkedList<>();
+        final SchemaContext context = identifier.getSchemaContext();
+        final String startNamespace = identifier.getSchemaNode().getQName().getNamespace().toString();
+        final DataSchemaContextNode<?> startNode = DataSchemaContextNode.fromDataSchemaNode(
+                (DataSchemaNode) identifier.getSchemaNode());
+
+        if (startNode == null) {
+            throw new RestconfDocumentedException(
+                    "Start node missing in " + input, ErrorType.PROTOCOL, ErrorTag.INVALID_VALUE);
+        }
+
+        parseInput(input, startNamespace, startNode, parsed, context);
+        return parsed;
+    }
+
+    /**
+     * Parse input value of fields parameter and create list of sets. Each set represents one level of child nodes.
+     * @param input input value of fields parameter
+     * @param startNamespace starting namespace
+     * @param startNode starting node
+     * @param parsed list of results
+     * @param context  schema context
+     */
+    private static void parseInput(@Nonnull final String input,
+                                   @Nonnull final String startNamespace,
+                                   @Nonnull final DataSchemaContextNode<?> startNode,
+                                   @Nonnull final List<Set<QName>> parsed,
+                                   @Nonnull final SchemaContext context) {
+        int currentPosition = 0;
+        int startPosition = 0;
+        DataSchemaContextNode<?> currentNode = startNode;
+        String namespace = startNamespace;
+
+        Set<QName> currentLevel = new HashSet<>();
+        parsed.add(currentLevel);
+
+        while (currentPosition < input.length()) {
+            final char currentChar = input.charAt(currentPosition);
+
+            if (Deserializer.IDENTIFIER.matches(currentChar) || currentChar == '/') {
+                if (currentChar == SLASH) {
+                    // add parsed identifier to results for current level
+                    currentNode = addChildToResult(
+                            currentNode, input.substring(startPosition, currentPosition), namespace, currentLevel);
+                    // go one level down
+                    currentLevel = new HashSet<>();
+                    parsed.add(currentLevel);
+
+                    currentPosition++;
+                    startPosition = currentPosition;
+                } else {
+                    currentPosition++;
+                }
+
+                continue;
+            }
+
+            switch (currentChar) {
+                case COLON :
+                    // new namespace found
+                    namespace = context.findModuleByName(
+                            input.substring(startPosition, currentPosition), null).getNamespace().toString();
+                    currentPosition++;
+                    break;
+                case STARTING_PARENTHESIS:
+                    // add current child to parsed results for current level
+                    final DataSchemaContextNode<?> child = addChildToResult(
+                            currentNode, input.substring(startPosition, currentPosition), namespace, currentLevel);
+                    // call with child node as new start node for one level down
+                    int endingBracket = currentPosition + findClosingParenthesis(input.substring(currentPosition + 1));
+                    parseInput(
+                            input.substring(currentPosition + 1, endingBracket),
+                            namespace,
+                            child,
+                            parsed,
+                            context);
+
+                    currentPosition = endingBracket + 1;
+                    break;
+                case SEMICOLON:
+                    // complete identifier found
+                    addChildToResult(
+                            currentNode, input.substring(startPosition, currentPosition), namespace, currentLevel);
+                    currentPosition++;
+                    break;
+                default:
+                    throw new RestconfDocumentedException(
+                            "Unexpected character '" + currentChar + "' found in fields parameter value",
+                            ErrorType.PROTOCOL, ErrorTag.INVALID_VALUE);
+            }
+
+            startPosition = currentPosition;
+        }
+
+        // parse input to end
+        if (startPosition < input.length()) {
+            addChildToResult(currentNode, input.substring(startPosition), namespace, currentLevel);
+        }
+    }
+
+    /**
+     * Add parsed child of current node to result for current level
+     * @param currentNode current node
+     * @param identifier parsed identifier of child node
+     * @param namespace current namespace
+     * @param level current nodes level
+     * @return {@link DataSchemaContextNode}
+     */
+    private static @Nonnull DataSchemaContextNode<?> addChildToResult(
+            @Nonnull final DataSchemaContextNode<?> currentNode,
+            @Nonnull final String identifier,
+            @Nonnull final String namespace,
+            @Nonnull final Set<QName> level) {
+        final QName childQName = QName.create(
+                namespace,
+                identifier,
+                currentNode.getIdentifier().getNodeType().getRevision());
+
+        DataSchemaContextNode<?> child = currentNode.getChild(childQName);
+
+        // if child is mixin continue until not mixin found
+        while (child != null && child.isMixin()) {
+            // also mixin nodes must be added
+            level.add(child.getIdentifier().getNodeType());
+            child = child.getChild(childQName);
+        }
+
+        if (child == null) {
+            throw new RestconfDocumentedException(
+                    "Child " + identifier + " node missing in "
+                            + currentNode.getIdentifier().getNodeType().getLocalName(),
+                    ErrorType.PROTOCOL, ErrorTag.INVALID_VALUE);
+        }
+
+        // add final child node to level nodes
+        level.add(child.getIdentifier().getNodeType());
+        return child;
+    }
+
+    /**
+     * Return position of matching parenthesis increased by one, but at most equals to input size.
+     * @return int
+     */
+    private static int findClosingParenthesis(@Nonnull final String input) {
+        int position = 0;
+        int count = 1;
+
+        while (position < input.length()) {
+            if (input.charAt(position) == STARTING_PARENTHESIS) {
+                count++;
+            }
+
+            if (input.charAt(position) == CLOSING_PARENTHESIS) {
+                count--;
+            }
+
+            if (count == 0) {
+                break;
+            }
+
+            position++;
+        }
+
+        // ending bracket was not found
+        if (position >= input.length()) {
+            throw new RestconfDocumentedException("Missing closing parenthesis in fields parameter",
+                    ErrorType.PROTOCOL, ErrorTag.INVALID_VALUE);
+        }
+
+        return ++position;
+    }
+}
