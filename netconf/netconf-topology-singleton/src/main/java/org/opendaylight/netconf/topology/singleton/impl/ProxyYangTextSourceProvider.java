7124aa82ea269aff862668db177f4aa8880d6f8e
Rudolf Brisuda <rbrisuda@cisco.com>
2016-10-07 11:44:23 +0200

    Bug 6714 - Use singleton service in clustered netconf topology
    Implemented clustered netconf topology with using clustered
    singleton service.
    
    Change-Id: I0e71a9934a70c6bce8eeab0644a5bd05c5548a8c
    Signed-off-by: Rudolf Brisuda <rbrisuda@cisco.com>

---
+++ netconf/netconf-topology-singleton/src/main/java/org/opendaylight/netconf/topology/singleton/impl/ProxyYangTextSourceProvider.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.netconf.topology.singleton.impl;
+
+import akka.actor.ActorContext;
+import akka.actor.ActorRef;
+import akka.dispatch.Futures;
+import akka.dispatch.OnComplete;
+import akka.pattern.Patterns;
+import com.google.common.collect.Sets;
+import java.util.Set;
+import javax.annotation.Nonnull;
+import org.opendaylight.controller.cluster.schema.provider.RemoteYangTextSourceProvider;
+import org.opendaylight.controller.cluster.schema.provider.impl.YangTextSchemaSourceSerializationProxy;
+import org.opendaylight.netconf.topology.singleton.impl.utils.NetconfTopologyUtils;
+import org.opendaylight.netconf.topology.singleton.messages.YangTextSchemaSourceRequest;
+import org.opendaylight.yangtools.yang.model.repo.api.SourceIdentifier;
+import scala.concurrent.Future;
+import scala.concurrent.impl.Promise;
+
+public class ProxyYangTextSourceProvider implements RemoteYangTextSourceProvider {
+
+    private final ActorRef masterRef;
+    private final ActorContext actorContext;
+
+    public ProxyYangTextSourceProvider(final ActorRef masterRef, final ActorContext actorContext) {
+        this.masterRef = masterRef;
+        this.actorContext = actorContext;
+    }
+
+    @Override
+    public Future<Set<SourceIdentifier>> getProvidedSources() {
+        // NOOP
+        return Futures.successful(Sets.newHashSet());
+    }
+
+    @Override
+    public Future<YangTextSchemaSourceSerializationProxy> getYangTextSchemaSource(
+            @Nonnull final SourceIdentifier sourceIdentifier) {
+
+        final Future<Object> scalaFuture = Patterns.ask(masterRef,
+                new YangTextSchemaSourceRequest(sourceIdentifier), NetconfTopologyUtils.TIMEOUT);
+
+        final Promise.DefaultPromise<YangTextSchemaSourceSerializationProxy> promise = new Promise.DefaultPromise<>();
+
+        scalaFuture.onComplete(new OnComplete<Object>() {
+            @Override
+            public void onComplete(final Throwable failure, final Object success) throws Throwable {
+                if (failure != null) {
+                    promise.failure(failure);
+                    return;
+                }
+                if (success instanceof Throwable) {
+                    promise.failure((Throwable) success);
+                    return;
+                }
+                promise.success((YangTextSchemaSourceSerializationProxy) success);
+            }
+        }, actorContext.dispatcher());
+
+        return promise.future();
+
+    }
+}
---
+++ netconf/netconf-topology-singleton/src/main/java/org/opendaylight/netconf/topology/singleton/messages/transactions/EmptyReadResponse.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.netconf.topology.singleton.messages.transactions;
+
+import java.io.Serializable;
+
+/**
+ * Message is sended when read result do not present any value.
+ */
+public class EmptyReadResponse implements Serializable {
+}
