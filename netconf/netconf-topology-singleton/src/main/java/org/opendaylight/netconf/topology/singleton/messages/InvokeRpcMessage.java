a57ffe99e250aff82096b5eaeb76c54fd55cbf43
Rudolf Brisuda <rbrisuda@cisco.com>
2016-10-10 18:13:33 +0200

    Bug 6911 - RPC support in singleton
    - master invoke RPC and send back to slave.
    
    Change-Id: Ica407f800da3d902f722d835bf6d658163c01bb5
    Signed-off-by: Rudolf Brisuda <rbrisuda@cisco.com>

---
+++ netconf/netconf-topology-singleton/src/main/java/org/opendaylight/netconf/topology/singleton/messages/InvokeRpcMessage.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.netconf.topology.singleton.messages;
+
+import java.io.Serializable;
+import java.util.List;
+import org.opendaylight.yangtools.yang.common.QName;
+
+public class InvokeRpcMessage implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    // Schema types parameters
+    private final List<QName> path;
+    private final boolean absolute;
+
+    private final NormalizedNodeMessage normalizedNodeMessage;
+
+    public InvokeRpcMessage(final List<QName> path, final boolean absolute,
+                            final NormalizedNodeMessage normalizedNodeMessage) {
+        this.path = path;
+        this.absolute = absolute;
+        this.normalizedNodeMessage = normalizedNodeMessage;
+    }
+
+    public List<QName> getPath() {
+        return path;
+    }
+
+    public boolean isAbsolute() {
+        return absolute;
+    }
+
+    public NormalizedNodeMessage getNormalizedNodeMessage() {
+        return normalizedNodeMessage;
+    }
+}
