2796ad0fec227927c021795d45f70a39d6450a5e
Luis Gomez <ecelgp@gmail.com>
2016-10-20 01:06:04 -0700

    Refactor OpenFlow sw/link scalability and longevity tests
    - Remove Scalability.robot resource
    - Create WorkflowsOpenFlow.robot and WorkflowsL2switch.robot
    - Modify longevity to use full mesh topology workflow
    - Add flow 10K flow add/delete check to all tests.
    - Add error messages in case of failure.
    - Add topology discovery time and plot for it.
    - Add JVM monitoring.
    - Remove redundant sw scale test.
    
    This goes with https://git.opendaylight.org/gerrit/#/c/47254
    
    Change-Id: I7773ff1b2e7c6b36de46a51db8aa38994c282f30
    Signed-off-by: Luis Gomez <ecelgp@gmail.com>

--- test/csit/variables/Variables.py
+++ test/csit/variables/Variables.py
@@ -136,20 +136,10 @@ GET_INTENTS_URI = '/retconf/config/intent:intents'
 AUTH_TOKEN_API = '/oauth2/token'
 REVOKE_TOKEN_API = '/oauth2/revoke'
 
-# Base Mininet Mac address. DPID of mininet switches will be derived from this.
-BASE_MAC_1 = '00:4b:00:00:00:00'
-# Base IP of mininet hosts
-BASE_IP_1 = '75.75.0.0'
-
 # Vlan Custom Topology Path and File
 CREATE_VLAN_TOPOLOGY_FILE = "vlan_vtn_test.py"
 CREATE_VLAN_TOPOLOGY_FILE_PATH = "MininetTopo/" +\
                                  CREATE_VLAN_TOPOLOGY_FILE
-
-# Mininet Custom Topology Path and File
-CREATE_FULLYMESH_TOPOLOGY_FILE = "create_fullymesh.py"
-CREATE_FULLYMESH_TOPOLOGY_FILE_PATH = "libraries/MininetTopo/" +\
-                                      CREATE_FULLYMESH_TOPOLOGY_FILE
 
 # Mininet Custom Topology Path and File for Path Policy
 CREATE_PATHPOLICY_TOPOLOGY_FILE = "topo-3sw-2host_multipath.py"
