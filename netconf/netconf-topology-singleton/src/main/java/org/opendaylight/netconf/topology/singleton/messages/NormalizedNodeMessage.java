7709849174208144786a10e375fa6ff2df78881c
Alexis de Talhouët <adetalhouet@inocybe.com>
2016-10-21 16:26:02 -0400

    Use SerializationUtils to (de)serialize NormalizedNode and YangInstanceIdentifier
    Controller CDS implementation provide utility method for this,
    we can re-use them, as it appears it's keeping a ThreadLocal
    holding NormalizedNodeDataOutput instance
    
    Change-Id: Ia106f33d04f937ac8dacf89cb6837a157eee8ae1
    Signed-off-by: Alexis de Talhouët <adetalhouet@inocybe.com>
    (cherry picked from commit 4f326302b5dcb41b1da291a0240e2b8808a63893)

--- netconf/netconf-topology-singleton/src/main/java/org/opendaylight/netconf/topology/singleton/messages/NormalizedNodeMessage.java
+++ netconf/netconf-topology-singleton/src/main/java/org/opendaylight/netconf/topology/singleton/messages/NormalizedNodeMessage.java
@@ -12,13 +12,9 @@ import java.io.Externalizable;
 import java.io.IOException;
 import java.io.ObjectInput;
 import java.io.ObjectOutput;
-import org.opendaylight.controller.cluster.datastore.node.utils.stream.NormalizedNodeDataInput;
-import org.opendaylight.controller.cluster.datastore.node.utils.stream.NormalizedNodeDataOutput;
-import org.opendaylight.controller.cluster.datastore.node.utils.stream.NormalizedNodeInputOutput;
+import org.opendaylight.controller.cluster.datastore.utils.SerializationUtils;
 import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
 import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNode;
-import org.opendaylight.yangtools.yang.data.api.schema.stream.NormalizedNodeStreamWriter;
-import org.opendaylight.yangtools.yang.data.api.schema.stream.NormalizedNodeWriter;
 
 /**
  * Message which holds node data, prepared to sending between remote hosts with serialization.
@@ -48,20 +44,16 @@ public class NormalizedNodeMessage implements Externalizable {
 
     @Override
     public void writeExternal(final ObjectOutput out) throws IOException {
-        final NormalizedNodeDataOutput dataOutput = NormalizedNodeInputOutput.newDataOutput(out);
-        final NormalizedNodeWriter normalizedNodeWriter =
-                NormalizedNodeWriter.forStreamWriter((NormalizedNodeStreamWriter) dataOutput);
-
-        dataOutput.writeYangInstanceIdentifier(identifier);
-
-        normalizedNodeWriter.write(node);
+        SerializationUtils.serializePathAndNode(getIdentifier(), node, out);
     }
 
     @Override
     public void readExternal(final ObjectInput in) throws IOException, ClassNotFoundException {
-        final NormalizedNodeDataInput dataInput = NormalizedNodeInputOutput.newDataInput(in);
-
-        identifier = dataInput.readYangInstanceIdentifier();
-        node = dataInput.readNormalizedNode();
+        SerializationUtils.deserializePathAndNode(in, this, APPLIER);
     }
+
+    private static final SerializationUtils.Applier<NormalizedNodeMessage> APPLIER = (instance, path, node) -> {
+        instance.identifier = path;
+        instance.node = node;
+    };
 }
