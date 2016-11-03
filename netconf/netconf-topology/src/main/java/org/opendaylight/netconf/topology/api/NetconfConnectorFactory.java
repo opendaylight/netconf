64c7f76ff51a7678b513953a3ca63c5ae09ff05a
Alexis de Talhouët <adetalhouet@inocybe.com>
2016-11-03 16:25:18 -0400

    Add blueprint wiring for loopback controller-config
    Change-Id: I0bf0664e35f17ba449d71a6555359cc42b61da07
    Signed-off-by: Alexis de Talhouët <adetalhouet@inocybe.com>

---
+++ netconf/netconf-topology/src/main/java/org/opendaylight/netconf/topology/api/NetconfConnectorFactory.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2016 Inocybe Technologies and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.netconf.topology.api;
+
+import org.opendaylight.controller.md.sal.binding.api.DataBroker;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netconf.node.topology.rev150114.NetconfNode;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.Node;
+
+/**
+ * Created by adetalhouet on 2016-11-03.
+ */
+public interface NetconfConnectorFactory {
+
+    /**
+     * Create a new netconf connector with default values.
+     * <p>
+     * This method will create a {@link Node} and a {@link NetconfNode} that will be added as an augmentation to the
+     * {@link Node}. Afterward, that {@link Node} will be written in the MDSAL datastore under the {@link NetconfTopology}.
+     * Listeners of that subtree located within network-topology bundle will setup the session.
+     *
+     * @param dataBroker Instance of the {@link DataBroker}
+     * @param instanceName The name of the node
+     * @param address The address
+     * @param port The port
+     * @param username The username of the netconf session
+     * @param password The password of the netconf session
+     * @param tcpOnly Whether to create a TCP or SSH session
+     * @param reconnectOnSchemaChange Whether to enable ietf-netconf-monitoring and register the NETCONF stream.
+     * @return The created {@link Node}
+     */
+    Node newInstance(final DataBroker dataBroker,
+                     final String instanceName,
+                     final String address,
+                     final Integer port,
+                     final String username,
+                     final String password,
+                     final Boolean tcpOnly,
+                     final Boolean reconnectOnSchemaChange);
+}
