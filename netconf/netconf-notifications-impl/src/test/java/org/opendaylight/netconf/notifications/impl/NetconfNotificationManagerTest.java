808a1378f6c2233526095946ecc2fa6b0f58509c
Alexis de Talhouët <adetalhouet@inocybe.com>
2016-11-18 17:02:06 -0500

    Bug 6645 - 2 digits milliseconds can not be parsed in notification eventTime
    Create a Parser and a Formater based of Java 8 DateTimeFormater API, using
    the ISO_DATE_TIME format, the UTC time-zone offset, and the local time-zone.
    
    As we're moving away from the pattern approach to parse/format those values,
    we have to handle the leap second corner case; it is possible to encounter
    time value being 23:59:60, see <a href="https://en.wikipedia.org/wiki/Leap_second">wikipedia page</a>.
    For that reason, if the parsing fails, we catch the exception and check whether
    the time has a leap second, and if it has, we replace the second-of-minute of
    60 with 59.
    
    Change-Id: I8fcc66f5a18f1781bf14f895c23cd2b890b5b5a7
    Signed-off-by: Alexis de Talhouët <adetalhouet@inocybe.com>

--- netconf/netconf-notifications-impl/src/test/java/org/opendaylight/netconf/notifications/impl/NetconfNotificationManagerTest.java
+++ netconf/netconf-notifications-impl/src/test/java/org/opendaylight/netconf/notifications/impl/NetconfNotificationManagerTest.java
@@ -16,8 +16,10 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
 
 import com.google.common.collect.Lists;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.time.format.DateTimeParseException;
+import java.util.Date;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
@@ -43,28 +45,59 @@ public class NetconfNotificationManagerTest {
         MockitoAnnotations.initMocks(this);
     }
 
-
-    @Test public void testEventTime() throws Exception {
-        final SimpleDateFormat simpleDateFormat = new SimpleDateFormat(
-            NetconfNotification.RFC3339_DATE_FORMAT_BLUEPRINT);
-        final SimpleDateFormat simpleDateFormat2 = new SimpleDateFormat(
-            NetconfNotification.RFC3339_DATE_FORMAT_WITH_MILLIS_BLUEPRINT);
-
+    @Test
+    public void testEventTime() throws Exception {
+        // Testing correct values
         for (String time : Lists.newArrayList(
-            "2001-07-04T12:08:56.235-07:00",
-            "2015-10-23T09:42:27.67175+00:00",
-            "1970-01-01T17:17:22.229568+00:00",
-            "1937-01-01T12:00:27.87+00:20",
-            "1990-12-31T15:59:60-08:00",
-            "1990-12-31T23:59:60Z",
-            "1996-12-19T16:39:57-08:00"
-//          ,"1985-04-12T23:20:50.52Z"
+                "2001-07-04T12:08:56.235-07:00",
+                "2015-10-23T09:42:27.67175+00:00",
+                "1970-01-01T17:17:22.229568+00:00",
+                "1937-01-01T12:00:27.87+00:20",
+                "2015-06-30T23:59:60Z",
+                "1996-12-19T16:39:57-08:00",
+                "2015-10-23T09:42:27Z",
+                "2015-10-23T09:42:27.200001Z",
+                "1985-04-12T23:20:50.52Z",
+                // Values with leap second
+                "2001-07-04T12:08:60.235-07:00",
+                "1990-12-31T15:59:60-08:00",
+                "2015-10-23T09:42:60.67175+00:00",
+                "1970-01-01T17:17:60.229568+00:00",
+                "1937-01-01T12:00:60.87+00:20",
+                "1990-12-31T23:59:60Z",
+                "2015-10-23T09:42:60.200001Z",
+                "1985-04-12T23:20:60.52Z"
         )) {
             try {
-                simpleDateFormat.parse(time);
-            } catch (ParseException e) {
-                simpleDateFormat2.parse(time);
+                NetconfNotification.RFC3339_DATE_PARSER.apply(time);
+            } catch (DateTimeParseException e) {
+                fail("Failed to parse time value = " + time + " " + e);
+                throw e;
             }
+        }
+
+        // Testing that we're consistent from formatting to parsing.
+        Date date0 = Date.from(Instant.ofEpochMilli(0));
+        String dateString0 = NetconfNotification.RFC3339_DATE_FORMATTER.apply(date0);
+        Date date1 = NetconfNotification.RFC3339_DATE_PARSER.apply(dateString0);
+        String dateString1 = NetconfNotification.RFC3339_DATE_FORMATTER.apply(date1);
+        Assert.assertEquals(date0, date1);
+        Assert.assertEquals(dateString0, dateString1);
+
+        // Testing wrong values
+        for (String time : Lists.newArrayList(
+                "0",
+                "205-10-23T09:42:27.67175+00:00",
+                "1970-01-01T17:60:22.229568+00:00",
+                "1937-01-01T32:00:27.87+00:20",
+                "2060-13-31T15:59:90-08:00"
+        )) {
+            try {
+                NetconfNotification.RFC3339_DATE_PARSER.apply(time);
+            } catch (DateTimeParseException e) {
+                continue;
+            }
+            fail("Should have thrown an exception; value= " + time);
         }
     }
 
