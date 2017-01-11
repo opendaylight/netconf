e315ba0e32917b061650982ffc60f07fdea5fa8f
Balaji <bvaradar@brocade.com>
2017-01-11 09:04:27 -0800

    BUG-1422 Introduce Call-Home functionality for the NETCONF Topology.
    Introduce Call-Home reversed SSH support.
    
    Basic support for host key based white-list and authorization by username/password.
    Rebased with master; some code flaws fixed
    Addressed minor code fixes
    
    Change-Id: If3e3c9fdd0af0c97043f8930b7f922586863cabf
    Signed-off-by: Balaji <bvaradar@brocade.com>
    Signed-off-by: Mike Arsenault <mike@mike-arsenault.com>
    Signed-off-by: Allan Clarke <clarkea@brocade.com>

---
+++ netconf/callhome-protocol/src/main/java/org/opendaylight/netconf/callhome/protocol/CallHomeProtocolSessionContext.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) 2016 Brocade Communication Systems and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.netconf.callhome.protocol;
+
+import java.net.InetSocketAddress;
+import java.security.PublicKey;
+
+/**
+ *
+ * Protocol level Session Context for incoming Call Home connections.
+ *
+ */
+public interface CallHomeProtocolSessionContext {
+
+    /**
+     * Returns session identifier provided by  CallHomeAuthorizationProvider
+     *
+     * @return Returns application-provided session identifier
+     */
+    String getSessionName();
+
+    /**
+     * Returns public key provided by remote SSH Server for this session.
+     *
+     * @return public key provided by remote SSH Server
+     */
+    PublicKey getRemoteServerKey();
+
+    /**
+     *
+     * Returns remote socket address associated with this session.
+     *
+     * @return remote socket address associated with this session.
+     */
+    InetSocketAddress getRemoteAddress();
+
+    /**
+     * Returns version string provided by remote server.
+     *
+     * @return Version string provided by remote server.
+     */
+    String getRemoteServerVersion();
+
+}
