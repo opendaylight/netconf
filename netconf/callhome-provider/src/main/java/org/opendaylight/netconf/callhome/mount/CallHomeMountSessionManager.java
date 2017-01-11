30157733bf08dbc7a5f9cc6ded61f19794274caa
Balaji <bvaradar@brocade.com>
2017-01-11 09:04:27 -0800

    BUG-1422 Introduce Call-Home functionality for the NETCONF Topology.
    Introduce Call-Home reversed SSH support.
    
    Basic support for host key based white-list and authorization by username/password.
    Rebased with master; some code flaws fixed
    Addressed minor code fixes
    Addressed most of Tomas' review issues
    Addressed Jakub's review issues
    Addressed more or Tomas' review issues
    Removed mistaken hoisting of odl-netconf-callhome-ssh to be topmost feature
    Reenabled "ignored" unit tests
    
    Change-Id: If3e3c9fdd0af0c97043f8930b7f922586863cabf
    Signed-off-by: Balaji <bvaradar@brocade.com>
    Signed-off-by: Mike Arsenault <mike@mike-arsenault.com>
    Signed-off-by: Allan Clarke <clarkea@brocade.com>
    Signed-off-by: Ryan Goulding <ryandgoulding@gmail.com>

---
+++ netconf/callhome-provider/src/main/java/org/opendaylight/netconf/callhome/mount/CallHomeMountSessionManager.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright (c) 2016 Brocade Communication Systems and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.netconf.callhome.mount;
+
+import com.google.common.collect.Multimap;
+import com.google.common.collect.MultimapBuilder;
+import java.net.InetSocketAddress;
+import java.net.SocketAddress;
+import java.security.PublicKey;
+import java.util.Collection;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+import javax.annotation.Nullable;
+import org.opendaylight.netconf.callhome.mount.CallHomeMountSessionContext.CloseCallback;
+import org.opendaylight.netconf.callhome.protocol.CallHomeChannelActivator;
+import org.opendaylight.netconf.callhome.protocol.CallHomeProtocolSessionContext;
+
+public class CallHomeMountSessionManager implements CallHomeMountSessionContext.CloseCallback {
+
+    private final ConcurrentMap<SocketAddress, CallHomeMountSessionContext> contextByAddress = new ConcurrentHashMap<>();
+    private final Multimap<PublicKey, CallHomeMountSessionContext> contextByPublicKey = MultimapBuilder.hashKeys().hashSetValues().build();
+
+    @Nullable
+    public CallHomeMountSessionContext getByAddress(InetSocketAddress remoteAddr) {
+        return contextByAddress.get(remoteAddr);
+    }
+
+    @Nullable
+    public Collection<CallHomeMountSessionContext> getByPublicKey(PublicKey publicKey) {
+        return contextByPublicKey.get(publicKey);
+    }
+
+    CallHomeMountSessionContext createSession(CallHomeProtocolSessionContext session,
+            CallHomeChannelActivator activator, final CloseCallback onCloseHandler) {
+
+        String name = session.getSessionName();
+        CallHomeMountSessionContext deviceContext = new CallHomeMountSessionContext(name, session, activator, devCtxt -> {
+                CallHomeMountSessionManager.this.onClosed(devCtxt);
+                onCloseHandler.onClosed(devCtxt);
+            });
+
+        contextByAddress.put(deviceContext.getRemoteAddress(), deviceContext);
+        contextByPublicKey.put(deviceContext.getRemoteServerKey(), deviceContext);
+
+        return deviceContext;
+    }
+
+    @Override
+    public synchronized void onClosed(CallHomeMountSessionContext deviceContext) {
+        contextByAddress.remove(deviceContext.getRemoteAddress());
+        contextByPublicKey.remove(deviceContext.getRemoteServerKey(),deviceContext);
+    }
+
+}
---
+++ netconf/callhome-provider/src/main/java/org/opendaylight/netconf/callhome/mount/ContextKey.java
@@ -0,0 +1,87 @@
+/*
+ * Copyright (c) 2016 Brocade Communication Systems and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.netconf.callhome.mount;
+
+import com.google.common.base.Objects;
+import com.google.common.base.Preconditions;
+import java.net.Inet4Address;
+import java.net.Inet6Address;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.SocketAddress;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev130715.IetfInetUtil;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev130715.IpAddress;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev130715.PortNumber;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netconf.node.topology.rev150114.NetconfNode;
+
+class ContextKey {
+
+    private final IpAddress address;
+    private final PortNumber port;
+
+    public ContextKey(IpAddress address, PortNumber port) {
+        this.address = Preconditions.checkNotNull(address);
+        this.port = Preconditions.checkNotNull(port);
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + address.hashCode();
+        result = prime * result + port.hashCode();
+        return result;
+    }
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        ContextKey other = (ContextKey) obj;
+        return Objects.equal(address, other.address) && Objects.equal(port, other.port);
+    }
+
+    public static ContextKey from(NetconfNode node) {
+        return new ContextKey(node.getHost().getIpAddress(), node.getPort());
+    }
+
+    IpAddress getIpAddress() {
+        return address;
+    }
+
+    PortNumber getPort() {
+        return port;
+    }
+
+    public static ContextKey from(SocketAddress remoteAddress) {
+        Preconditions.checkArgument(remoteAddress instanceof InetSocketAddress);
+        InetSocketAddress inetSocketAddr = (InetSocketAddress) remoteAddress;
+        InetAddress ipAddress = inetSocketAddr.getAddress();
+
+        final IpAddress yangIp;
+        if(ipAddress instanceof Inet4Address) {
+            yangIp = new IpAddress(IetfInetUtil.INSTANCE.ipv4AddressFor(ipAddress));
+        } else {
+            Preconditions.checkArgument(ipAddress instanceof Inet6Address);
+            yangIp = new IpAddress(IetfInetUtil.INSTANCE.ipv6AddressFor(ipAddress));
+        }
+        return new ContextKey(yangIp, new PortNumber(inetSocketAddr.getPort()));
+    }
+
+    @Override
+    public String toString() {
+        if(address.getIpv4Address() != null) {
+            return address.getIpv4Address().getValue() + ":" + port.getValue();
+        }
+        return address.getIpv6Address().getValue() + ":" + port.getValue();
+    }
+}
