31f6dd4368f423806928f66ed732e09463fb79a0
Andrej Mak <andrej.mak@pantheon.tech>
2017-02-09 14:35:14 +0100

    Bug 5581: Optimize subtree filtering
    If single list entry with key is specified in filter,
    only that specific entry is read from datastore, thus
    filtering has smaller input to process.
    
    Change-Id: Iae46e3c25067d35f6bcacc40d0997d008da8fb29
    Signed-off-by: Andrej Mak <andrej.mak@pantheon.tech>

--- netconf/mdsal-netconf-connector/src/test/java/org/opendaylight/netconf/mdsal/connector/ops/get/FilterContentValidatorTest.java
+++ netconf/mdsal-netconf-connector/src/test/java/org/opendaylight/netconf/mdsal/connector/ops/get/FilterContentValidatorTest.java
@@ -17,8 +17,14 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -36,14 +42,16 @@ import org.xml.sax.SAXException;
 @RunWith(value = Parameterized.class)
 public class FilterContentValidatorTest {
 
-    private static final int TEST_CASE_COUNT = 8;
+    private static final int TEST_CASE_COUNT = 12;
+    private static final Pattern LIST_ENTRY_PATTERN = Pattern.compile("(.*)\\[\\{(.*)(, .*)+\\}\\]");
     private final XmlElement filterContent;
     private final String expected;
     private FilterContentValidator validator;
 
+
     @Parameterized.Parameters
     public static Collection<Object[]> data() throws IOException, SAXException, URISyntaxException, InitializationError {
-        List<Object[]> result = new ArrayList<>();
+        final List<Object[]> result = new ArrayList<>();
         final Path path = Paths.get(FilterContentValidatorTest.class.getResource("/filter/expected.txt").toURI());
         final List<String> expected = Files.readAllLines(path);
         if (expected.size() != TEST_CASE_COUNT) {
@@ -56,18 +64,18 @@ public class FilterContentValidatorTest {
         return result;
     }
 
-    public FilterContentValidatorTest(Document filterContent, String expected) {
+    public FilterContentValidatorTest(final Document filterContent, final String expected) {
         this.filterContent = XmlElement.fromDomDocument(filterContent);
         this.expected = expected;
     }
 
     @Before
     public void setUp() throws Exception {
-        List<InputStream> sources = new ArrayList<>();
+        final List<InputStream> sources = new ArrayList<>();
         sources.add(getClass().getResourceAsStream("/yang/filter-validator-test-mod-0.yang"));
         sources.add(getClass().getResourceAsStream("/yang/filter-validator-test-augment.yang"));
-        SchemaContext context = YangParserTestUtils.parseYangStreams(sources);
-        CurrentSchemaContext currentContext = mock(CurrentSchemaContext.class);
+        final SchemaContext context = YangParserTestUtils.parseYangStreams(sources);
+        final CurrentSchemaContext currentContext = mock(CurrentSchemaContext.class);
         doReturn(context).when(currentContext).getCurrentContext();
         validator = new FilterContentValidator(currentContext);
     }
@@ -76,16 +84,41 @@ public class FilterContentValidatorTest {
     public void testValidate() throws Exception {
         if (expected.startsWith("success")) {
             final String expId = expected.replace("success=", "");
-            Assert.assertEquals(expId, validator.validate(filterContent).toString());
+//            Assert.assertEquals(expId, validator.validate(filterContent).toString());
+            yidStringEquals(expId, validator.validate(filterContent).toString());
         } else if (expected.startsWith("error")) {
             try {
                 validator.validate(filterContent);
                 Assert.fail(XmlUtil.toString(filterContent) + " is not valid and should throw exception.");
-            } catch (Exception e) {
+            } catch (final Exception e) {
                 final String expectedExceptionClass = expected.replace("error=", "");
                 Assert.assertEquals(expectedExceptionClass, e.getClass().getName());
             }
         }
 
+    }
+
+    private static void yidStringEquals(final String expected, final String actual) {
+        final Iterator<String> expIt = Arrays.asList(expected.split("/")).listIterator();
+        final Iterator<String> acIt = Arrays.asList(actual.split("/")).listIterator();
+        while (expIt.hasNext() && acIt.hasNext()) {
+            final String expElement = expIt.next();
+            final String acElement = acIt.next();
+            final Matcher expMatcher = LIST_ENTRY_PATTERN.matcher(expElement);
+            final Matcher acMatcher = LIST_ENTRY_PATTERN.matcher(acElement);
+            if (expMatcher.matches() && acMatcher.matches()) {
+                Assert.assertEquals(expMatcher.groupCount(), acMatcher.groupCount());
+                Assert.assertEquals(expMatcher.group(1), acMatcher.group(1));
+                final Set<String> expKeys = new HashSet<>();
+                final Set<String> acKeys = new HashSet<>();
+                for (int i = 2; i <= expMatcher.groupCount(); i++) {
+                    expKeys.add(expMatcher.group(i).replace(", ", ""));
+                    acKeys.add(acMatcher.group(i).replace(", ", ""));
+                }
+                Assert.assertEquals(expKeys, acKeys);
+            } else {
+                Assert.assertEquals(expElement, acElement);
+            }
+        }
     }
 }
