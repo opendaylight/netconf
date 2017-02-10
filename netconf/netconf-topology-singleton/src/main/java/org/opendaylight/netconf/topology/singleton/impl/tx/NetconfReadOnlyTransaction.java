68dd1eb5f4f5078b4ab4676910f9881f78bdbfbf
matus.kubica <matus.kubica@pantheon.tech>
2017-02-10 15:02:34 +0100

    Bug 7741 - "Please try again" should not result in http code 500
    Change ErrorType to TRANSPORT
    Change ErrorTag of DocumentedException to RESOURCE_DENIED
    Change ErrorSeverity to ERROR
    Add mapping in BrokerFacade to 503
    Edit RPCError in NetconfReadOnlyTransaction
    
    Change-Id: I039886d80a89e0ef4d376229617b3b4b7520a04c
    Signed-off-by: matus.kubica <matus.kubica@pantheon.tech>

--- netconf/netconf-topology-singleton/src/main/java/org/opendaylight/netconf/topology/singleton/impl/tx/NetconfReadOnlyTransaction.java
+++ netconf/netconf-topology-singleton/src/main/java/org/opendaylight/netconf/topology/singleton/impl/tx/NetconfReadOnlyTransaction.java
@@ -16,12 +16,15 @@ import com.google.common.util.concurrent.CheckedFuture;
 import com.google.common.util.concurrent.Futures;
 import com.google.common.util.concurrent.SettableFuture;
 import javax.annotation.Nullable;
+import org.opendaylight.controller.config.util.xml.DocumentedException;
 import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
 import org.opendaylight.controller.md.sal.common.api.data.ReadFailedException;
 import org.opendaylight.controller.md.sal.dom.api.DOMDataReadOnlyTransaction;
 import org.opendaylight.netconf.sal.connect.util.RemoteDeviceId;
 import org.opendaylight.netconf.topology.singleton.api.NetconfDOMTransaction;
 import org.opendaylight.netconf.topology.singleton.messages.NormalizedNodeMessage;
+import org.opendaylight.yangtools.yang.common.RpcError;
+import org.opendaylight.yangtools.yang.common.RpcResultBuilder;
 import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
 import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNode;
 import org.slf4j.Logger;
@@ -62,7 +65,16 @@ public class NetconfReadOnlyTransaction implements DOMDataReadOnlyTransaction {
             @Nullable
             @Override
             public ReadFailedException apply(Exception cause) {
-                return new ReadFailedException("Read from transaction failed", cause);
+                if (cause.getCause() != null && cause.getCause() instanceof DocumentedException) {
+                    DocumentedException e = (DocumentedException) cause.getCause();
+                    final RpcError error = RpcResultBuilder.newError(
+                            RpcError.ErrorType.TRANSPORT,
+                            e.getErrorTag().getTagValue(),
+                            "Master is down. Please try again.");
+                    return new ReadFailedException("Read from transaction failed", error);
+                } else {
+                    return new ReadFailedException("Read from transaction failed", cause);
+                }
             }
         });
         future.onComplete(new OnComplete<Optional<NormalizedNodeMessage>>() {
