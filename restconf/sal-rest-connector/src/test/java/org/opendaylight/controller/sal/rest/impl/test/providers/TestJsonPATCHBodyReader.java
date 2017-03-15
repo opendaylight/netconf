6779d4984608836e28d5d38aef245c827e299df2
Ivan Hrasko <ivan.hrasko@pantheon.tech>
2017-03-15 08:41:54 +0100

    Bug 7987: Json HTTP PATCH: Problem parsing simple leaf value
    Change-Id: I8094797fc2a26bd809631d995a29e111320e664e
    Signed-off-by: Ivan Hrasko <ivan.hrasko@pantheon.tech>
    (cherry picked from commit aca66f6d6b8065b88d73bec17822ac6d226d9199)

--- restconf/sal-rest-connector/src/test/java/org/opendaylight/controller/sal/rest/impl/test/providers/TestJsonPATCHBodyReader.java
+++ restconf/sal-rest-connector/src/test/java/org/opendaylight/controller/sal/rest/impl/test/providers/TestJsonPATCHBodyReader.java
@@ -158,4 +158,20 @@ public class TestJsonPATCHBodyReader extends AbstractBodyReaderTest {
                 .readFrom(null, null, null, mediaType, null, inputStream);
         checkPATCHContext(returnValue);
     }
+
+    /**
+     * Test reading simple leaf value
+     */
+    @Test
+    public void modulePATCHSimpleLeafValueTest() throws Exception {
+        final String uri = "instance-identifier-patch-module:patch-cont/my-list1/leaf1";
+        mockBodyReader(uri, jsonPATCHBodyReader, false);
+
+        final InputStream inputStream = TestJsonBodyReader.class
+                .getResourceAsStream("/instanceidentifier/json/jsonPATCHSimpleLeafValue.json");
+
+        final PATCHContext returnValue = jsonPATCHBodyReader
+                .readFrom(null, null, null, mediaType, null, inputStream);
+        checkPATCHContext(returnValue);
+    }
 }
