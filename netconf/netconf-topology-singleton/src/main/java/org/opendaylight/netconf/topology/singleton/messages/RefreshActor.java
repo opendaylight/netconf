c831f72ee4a721a5cf9465792645929f4fa93447
miroslav.kovac <miroslav.kovac@pantheon.tech>
2017-04-03 14:39:19 +0200

    Bug 8074 - actor name [test-device] is not unique!
    When creating a new actor with same name as an old one, it might fail
    becuase old actor might not be completely gone at this point.
    
    This creates a slaveActor which will never be closed, but instead
    every time that mountpoint is closed or master is changed, it would
    update NetconfNodeActor data and keep the same actor.
    
    The name of the actor is changed to "odl-{id}" - where {id} is
    number iterated on every new slave actor creation.
    
    Change-Id: I6d10db7b8902048632e336fb5a9701a797b363c6
    Signed-off-by: miroslav.kovac <miroslav.kovac@pantheon.tech>

---
+++ netconf/netconf-topology-singleton/src/main/java/org/opendaylight/netconf/topology/singleton/messages/RefreshActor.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) 2017 Pantheon Technologies s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.netconf.topology.singleton.messages;
+
+import akka.util.Timeout;
+import java.io.Serializable;
+import org.opendaylight.netconf.sal.connect.util.RemoteDeviceId;
+import org.opendaylight.netconf.topology.singleton.impl.utils.NetconfTopologySetup;
+import org.opendaylight.yangtools.yang.model.repo.api.SchemaRepository;
+import org.opendaylight.yangtools.yang.model.repo.spi.SchemaSourceRegistry;
+
+public class RefreshActor implements Serializable {
+
+    private final SchemaRepository schemaRepository;
+    private final RemoteDeviceId id;
+    private final SchemaSourceRegistry schemaRegistry;
+    private final NetconfTopologySetup setup;
+    private final Timeout actorResponseWaitTime;
+
+    public RefreshActor(final NetconfTopologySetup setup, final RemoteDeviceId id,
+                        final SchemaSourceRegistry schemaRegistry, final SchemaRepository schemaRepository,
+                        final Timeout actorResponseWaitTime) {
+        this.setup = setup;
+        this.id = id;
+        this.schemaRegistry = schemaRegistry;
+        this.schemaRepository = schemaRepository;
+        this.actorResponseWaitTime = actorResponseWaitTime;
+    }
+
+    public Timeout getActorResponseWaitTime() {
+        return actorResponseWaitTime;
+    }
+
+    public SchemaRepository getSchemaRepository() {
+        return schemaRepository;
+    }
+
+    public RemoteDeviceId getId() {
+        return id;
+    }
+
+    public SchemaSourceRegistry getSchemaRegistry() {
+        return schemaRegistry;
+    }
+
+    public NetconfTopologySetup getSetup() {
+        return setup;
+    }
+}
