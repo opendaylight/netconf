a2d4a9997d067a8abcd34fed25eaad93d54c2a67
Jakub Toth <jatoth@cisco.com>
2017-04-13 11:50:44 +0200

    Test RestconfWrapperProviders
    Change-Id: Id95c607bba19c56c6924938ba5ef107f8937a76b
    Signed-off-by: Jakub Toth <jatoth@cisco.com>

---
+++ restconf/sal-rest-connector/src/test/java/org/opendaylight/RestconfWrapperProvidersTest.java
@@ -0,0 +1,85 @@
+/*
+ * Copyright (c) 2017 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.lang.reflect.Field;
+import org.junit.Test;
+import org.mockito.Mockito;
+import org.opendaylight.controller.config.yang.md.sal.rest.connector.RestConnectorRuntimeRegistration;
+import org.opendaylight.controller.config.yang.md.sal.rest.connector.RestConnectorRuntimeRegistrator;
+import org.opendaylight.controller.sal.core.api.Broker;
+import org.opendaylight.controller.sal.core.api.Provider;
+import org.opendaylight.netconf.sal.restconf.impl.RestconfProviderImpl;
+import org.opendaylight.restconf.RestConnectorProvider;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev130715.PortNumber;
+
+public class RestconfWrapperProvidersTest {
+
+    private final PortNumber port = new PortNumber(8181);
+
+    @Test
+    public void initRWPTest() {
+        final RestconfWrapperProviders rwp = new RestconfWrapperProviders(this.port);
+        assertNotNull(rwp);
+    }
+
+    @Test
+    public void registerTest() {
+        final RestconfWrapperProviders rwp = new RestconfWrapperProviders(this.port);
+        assertNotNull(rwp);
+
+        final Broker broker = mock(Broker.class);
+        rwp.registerProviders(broker);
+
+        Mockito.verify(broker, times(2)).registerProvider(Mockito.any(Provider.class));
+    }
+
+    @Test
+    public void runtimeRegistrationTest() {
+        final RestconfWrapperProviders rwp = new RestconfWrapperProviders(this.port);
+        assertNotNull(rwp);
+
+        final RestConnectorRuntimeRegistrator runtimeRegistrator = mock(RestConnectorRuntimeRegistrator.class);
+        final RestConnectorRuntimeRegistration value = mock(RestConnectorRuntimeRegistration.class);
+        when(runtimeRegistrator.register(any())).thenReturn(value);
+        final RestConnectorRuntimeRegistration runtimeRegistration = rwp.runtimeRegistration(runtimeRegistrator);
+
+        assertEquals(value, runtimeRegistration);
+    }
+
+    @Test
+    public void closeTest() throws Exception {
+        final RestconfWrapperProviders rwp = new RestconfWrapperProviders(this.port);
+        assertNotNull(rwp);
+
+        final RestconfProviderImpl draft2 = mock(RestconfProviderImpl.class);
+        final RestConnectorProvider draft18 = mock(RestConnectorProvider.class);
+
+        setDecladerField(rwp, "providerDraft02", draft2);
+        setDecladerField(rwp, "providerDraft18", draft18);
+
+        rwp.close();
+
+        verify(draft2, times(1)).close();
+        verify(draft18, times(1)).close();
+    }
+
+    private static void setDecladerField(final Object rwp, final String name, final Object value) throws Exception {
+        final Field declaredField = rwp.getClass().getDeclaredField(name);
+        declaredField.setAccessible(true);
+        declaredField.set(rwp, value);
+    }
+}
