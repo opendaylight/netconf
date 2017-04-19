8de52e832456390738173cd7b0d9f540b7d75c02
matus.kubica <matus.kubica@pantheon.tech>
2017-04-19 17:07:25 +0200

    Bug 8153: Enforce check-style rules for netconf-api
        Organize Imports for Checkstyle compliance.
        Checkstyle compliance: line length.
        Checkstyle compliance: various types of small changes.
        Checkstyle compliant Exception handling.
        Checkstyle final clean up & enforcement.
        Add the fail on violation flag into the pom.xml .
    
    Change-Id: Ibd2c23c38d02a0067d7c980abfd0a54a2cf59306
    Signed-off-by: matus.kubica <matus.kubica@pantheon.tech>

--- netconf/netconf-api/src/main/java/org/opendaylight/netconf/api/NetconfSessionPreferences.java
+++ netconf/netconf-api/src/main/java/org/opendaylight/netconf/api/NetconfSessionPreferences.java
@@ -19,6 +19,8 @@ public class NetconfSessionPreferences {
     }
 
     /**
+     * Getter for {@code NetconfHelloMessage}.
+     *
      * @return the helloMessage
      */
     public NetconfHelloMessage getHelloMessage() {
