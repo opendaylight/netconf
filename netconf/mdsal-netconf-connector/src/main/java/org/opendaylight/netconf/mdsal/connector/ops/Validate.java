c728721dcd704fa707c0f0d50eb031ed775136bb
lbeles <lbeles@frinx.io>
2017-05-12 12:27:33 +0200

    Implementation of Validate Capability
    Change-Id: Iada27e2a309d600d9318d208bcae8c5e5c02dbb9
    Signed-off-by: lbeles <lbeles@frinx.io>

---
+++ netconf/mdsal-netconf-connector/src/main/java/org/opendaylight/netconf/mdsal/connector/ops/Validate.java
@@ -0,0 +1,95 @@
+/*
+ * Copyright (c) 2017 Frinx s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.netconf.mdsal.connector.ops;
+
+import com.google.common.base.Optional;
+import org.opendaylight.controller.config.util.xml.DocumentedException;
+import org.opendaylight.controller.config.util.xml.DocumentedException.ErrorSeverity;
+import org.opendaylight.controller.config.util.xml.DocumentedException.ErrorTag;
+import org.opendaylight.controller.config.util.xml.DocumentedException.ErrorType;
+import org.opendaylight.controller.config.util.xml.XmlElement;
+import org.opendaylight.controller.config.util.xml.XmlUtil;
+import org.opendaylight.netconf.api.xml.XmlNetconfConstants;
+import org.opendaylight.netconf.mdsal.connector.CurrentSchemaContext;
+import org.opendaylight.netconf.mdsal.connector.ops.parser.MdsalNetconfParameter;
+import org.opendaylight.netconf.mdsal.connector.ops.parser.MdsalNetconfParameterType;
+import org.opendaylight.yangtools.yang.data.api.ModifyAction;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+public class Validate extends YangValidationNetconfOperation {
+
+    private static final Logger LOG = LoggerFactory.getLogger(Validate.class);
+
+    private static final String OPERATION_NAME = "validate";
+
+    public Validate(final String netconfSessionIdForReporting, final CurrentSchemaContext schemaContext) {
+        super(netconfSessionIdForReporting, schemaContext);
+    }
+
+    @Override
+    protected Element handleWithNoSubsequentOperations(final Document document, final XmlElement operationElement) throws DocumentedException {
+        final MdsalNetconfParameter inputParameter = extractSourceParameter(operationElement);
+
+        if (MdsalNetconfParameterType.DATASTORE == inputParameter.getType()) {
+            validateSourceDatastore(inputParameter.getDatastore());
+        } else {
+            validateConfigElement(inputParameter.getConfigElement());
+        }
+
+        return XmlUtil.createElement(document, XmlNetconfConstants.OK, Optional.<String>absent());
+    }
+
+
+    /**
+     * Validate XML element <config>. It validates syntax error and particularly also semantic errors
+     *
+     * @param configElement
+     * @throws DocumentedException
+     */
+    private void validateConfigElement(XmlElement configElement) throws DocumentedException {
+        final ModifyAction defaultAction = ModifyAction.CREATE;
+
+        for (final XmlElement element : configElement.getChildElements()) {
+            try {
+                createDataTreeChecker(defaultAction, element);
+            } catch (Exception e) {
+                LOG.error(e.getMessage(),e);
+                throw new DocumentedException(e.getMessage(),
+                        ErrorType.application,
+                        ErrorTag.operation_failed,
+                        ErrorSeverity.error);
+            }
+
+        }
+    }
+
+    /**
+     * Here is not required implementation. Because the data has been already validated in get-config/get operation.
+     * We just check whether validation applying in Candidate datastore
+     * @param datastore
+     * @throws DocumentedException
+     */
+    private void validateSourceDatastore(Datastore datastore) throws DocumentedException {
+        if (datastore == Datastore.running) {
+            throw new DocumentedException("validate on running getDatastore is not supported",
+                    ErrorType.protocol,
+                    ErrorTag.operation_not_supported,
+                    ErrorSeverity.error);
+        }
+    }
+
+
+    @Override
+    protected String getOperationName() {
+        return OPERATION_NAME;
+    }
+
+}
