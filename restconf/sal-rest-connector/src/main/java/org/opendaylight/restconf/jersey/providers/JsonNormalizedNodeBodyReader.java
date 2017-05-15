1e01d059bad202a782bc6ccf4444dbd4f01ee19d
Tomas Cere <tcere@cisco.com>
2017-05-15 12:38:20 +0200

    Don't loose cause when reporting parsing errors.
    Change-Id: I5e5a07c00f29784a6289aa1ed24187ed6f42e833
    Signed-off-by: Tomas Cere <tcere@cisco.com>

--- restconf/sal-rest-connector/src/main/java/org/opendaylight/restconf/jersey/providers/JsonNormalizedNodeBodyReader.java
+++ restconf/sal-rest-connector/src/main/java/org/opendaylight/restconf/jersey/providers/JsonNormalizedNodeBodyReader.java
@@ -129,7 +129,7 @@ public class JsonNormalizedNodeBodyReader extends AbstractNormalizedNodeBodyRead
             LOG.debug("Error parsing json input:", exception);
 
             throw new RestconfDocumentedException("Error parsing json input: Failed to create new parse result data. "
-                    + "Are you creating multiple resources/subresources in POST request?");
+                    + "Are you creating multiple resources/subresources in POST request?", exception);
         }
 
         LOG.debug("Error parsing json input", exception);
