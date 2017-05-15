5a1cdbcc73acbeec59dd2ddabee8038bdd21bc6f
lbeles <lbeles@frinx.io>
2017-05-15 16:11:02 +0200

    Implementation MD-SAL Netconf operation delete-config
    Change-Id: I6842a0e0676768981f4c451e154a8815855a5ced
    Signed-off-by: lbeles <lbeles@frinx.io>

---
+++ netconf/mdsal-netconf-connector/src/main/java/org/opendaylight/netconf/mdsal/connector/ops/DeleteConfig.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright (c) 2017 Frinx s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.netconf.mdsal.connector.ops;
+
+import org.opendaylight.controller.config.util.xml.DocumentedException;
+import org.opendaylight.controller.config.util.xml.DocumentedException.ErrorSeverity;
+import org.opendaylight.controller.config.util.xml.DocumentedException.ErrorTag;
+import org.opendaylight.controller.config.util.xml.DocumentedException.ErrorType;
+import org.opendaylight.controller.config.util.xml.XmlElement;
+import org.opendaylight.netconf.mdsal.connector.CurrentSchemaContext;
+import org.opendaylight.netconf.mdsal.connector.TransactionProvider;
+import org.opendaylight.netconf.mdsal.connector.ops.parser.MdsalNetconfOperation;
+import org.opendaylight.netconf.mdsal.connector.ops.parser.MdsalNetconfParameter;
+import org.opendaylight.netconf.mdsal.connector.ops.parser.MdsalNetconfParameterType;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+public class DeleteConfig extends MdsalNetconfOperation {
+    private static final String OPERATION_NAME = "delete-config";
+
+    private final TransactionProvider transactionProvider;
+
+    public DeleteConfig(final String netconfSessionIdForReporting, final TransactionProvider transactionProvider) {
+        super(netconfSessionIdForReporting);
+        this.transactionProvider = transactionProvider;
+    }
+
+    @Override
+    protected Element handleWithNoSubsequentOperations(final Document document, final XmlElement operationElement) throws DocumentedException {
+        final MdsalNetconfParameter targetParameter = extractTargetParameter(operationElement);
+
+        if (MdsalNetconfParameterType.DATASTORE == targetParameter.getType()) {
+            if (Datastore.candidate == targetParameter.getDatastore()) {
+                return (new DiscardChanges(getNetconfSessionIdForReporting(),transactionProvider)).handleWithNoSubsequentOperations(document,null);
+            } else {
+                throw new DocumentedException("delete-config on running datastore is not supported",
+                        ErrorType.protocol,
+                        ErrorTag.operation_not_supported,
+                        ErrorSeverity.error);
+            }
+        }
+        throw new DocumentedException("unsupported input parameters",
+                ErrorType.protocol,
+                ErrorTag.operation_not_supported,
+                ErrorSeverity.error);
+    }
+
+
+
+    @Override
+    protected String getOperationName() {
+        return OPERATION_NAME;
+    }
+
+}
