a82ca1de18beff0ee80c9742383d818099ec002b
Tom Pantelis <tompantelis@gmail.com>
2017-06-01 15:07:43 -0400

    Add blueprint wiring for restconf connector
    Added the blueprint XML wiring and deprecated the CSS config yang
    and modules. The CSS Module classes are essentially no-ops but
    remain for backwards compatibility until CSS is completely
    removed.
    
    Change-Id: I6f5e7f369958ead9ce00a7fc6c8afe551738f195
    Signed-off-by: matthieu <mcauffiez@inocybe.com>
    Signed-off-by: Tom Pantelis <tompantelis@gmail.com>

--- restconf/sal-rest-connector/src/main/java/org/opendaylight/controller/config/yang/sal/restconf/service/JSONRestconfServiceModule.java
+++ restconf/sal-rest-connector/src/main/java/org/opendaylight/controller/config/yang/sal/restconf/service/JSONRestconfServiceModule.java
@@ -8,10 +8,20 @@
 
 package org.opendaylight.controller.config.yang.sal.restconf.service;
 
-import org.opendaylight.netconf.sal.restconf.impl.JSONRestconfServiceImpl;
+import com.google.common.base.Optional;
+import javax.ws.rs.core.UriInfo;
+import org.opendaylight.controller.config.api.osgi.WaitingServiceTracker;
+import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
+import org.opendaylight.netconf.sal.restconf.api.JSONRestconfService;
+import org.opendaylight.yangtools.yang.common.OperationFailedException;
+import org.osgi.framework.BundleContext;
 
+@Deprecated
 public class JSONRestconfServiceModule
         extends org.opendaylight.controller.config.yang.sal.restconf.service.AbstractJSONRestconfServiceModule {
+
+    private BundleContext bundleContext;
+
     public JSONRestconfServiceModule(org.opendaylight.controller.config.api.ModuleIdentifier identifier,
                                      org.opendaylight.controller.config.api.DependencyResolver dependencyResolver) {
         super(identifier, dependencyResolver);
@@ -26,12 +36,48 @@ public class JSONRestconfServiceModule
     }
 
     @Override
-    public void customValidation() {
-        // add custom validation form module attributes here.
+    public java.lang.AutoCloseable createInstance() {
+        final WaitingServiceTracker<JSONRestconfService> tracker =
+                WaitingServiceTracker.create(JSONRestconfService.class, bundleContext);
+        final JSONRestconfService service = tracker.waitForService(WaitingServiceTracker.FIVE_MINUTES);
+
+        final class AutoCloseableJSONRestconfService implements JSONRestconfService, AutoCloseable {
+            @Override
+            public void close() {
+                tracker.close();
+            }
+
+            @Override
+            public void put(String uriPath, String payload, UriInfo uriInfo) throws OperationFailedException {
+                service.put(uriPath, payload, uriInfo);
+            }
+
+            @Override
+            public void post(String uriPath, String payload, UriInfo uriInfo) throws OperationFailedException {
+                service.post(uriPath, payload, uriInfo);
+            }
+
+            @Override
+            public void delete(String uriPath) throws OperationFailedException {
+                service.delete(uriPath);
+            }
+
+            @Override
+            public Optional<String> get(String uriPath, LogicalDatastoreType datastoreType, UriInfo uriInfo)
+                    throws OperationFailedException {
+                return service.get(uriPath, datastoreType, uriInfo);
+            }
+
+            @Override
+            public Optional<String> invokeRpc(String uriPath, Optional<String> input) throws OperationFailedException {
+                return service.invokeRpc(uriPath, input);
+            }
+        }
+
+        return new AutoCloseableJSONRestconfService();
     }
 
-    @Override
-    public java.lang.AutoCloseable createInstance() {
-        return new JSONRestconfServiceImpl();
+    public void setBundleContext(final BundleContext bundleContext) {
+        this.bundleContext = bundleContext;
     }
 }
