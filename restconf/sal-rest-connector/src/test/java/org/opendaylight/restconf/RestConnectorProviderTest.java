a476cfdbd638d08e1b938397ec7c5f775d7539cb
Tom Pantelis <tompantelis@gmail.com>
2017-06-01 15:07:43 -0400

    Add blueprint wiring for restconf connector
    Added the blueprint XML wiring and deprecated the CSS config yang
    and modules. The CSS Module classes are essentially no-ops but
    remain for backwards compatibility until CSS is completely
    removed.
    
    Change-Id: I6f5e7f369958ead9ce00a7fc6c8afe551738f195
    Signed-off-by: matthieu <mcauffiez@inocybe.com>
    Signed-off-by: Tom Pantelis <tompantelis@gmail.com>

--- restconf/sal-rest-connector/src/test/java/org/opendaylight/restconf/RestConnectorProviderTest.java
+++ restconf/sal-rest-connector/src/test/java/org/opendaylight/restconf/RestConnectorProviderTest.java
@@ -8,10 +8,8 @@
 
 package org.opendaylight.restconf;
 
-import static org.junit.Assert.assertNotNull;
-import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 
 import org.junit.Before;
 import org.junit.Rule;
@@ -22,8 +20,9 @@ import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.opendaylight.controller.md.sal.dom.api.DOMDataBroker;
 import org.opendaylight.controller.md.sal.dom.api.DOMMountPointService;
+import org.opendaylight.controller.md.sal.dom.api.DOMNotificationService;
+import org.opendaylight.controller.md.sal.dom.api.DOMRpcService;
 import org.opendaylight.controller.md.sal.dom.api.DOMTransactionChain;
-import org.opendaylight.controller.sal.core.api.Broker;
 import org.opendaylight.controller.sal.core.api.model.SchemaService;
 import org.opendaylight.restconf.handlers.SchemaContextHandler;
 import org.opendaylight.yangtools.concepts.ListenerRegistration;
@@ -36,9 +35,12 @@ public class RestConnectorProviderTest {
     // service under test
     private RestConnectorProvider connectorProvider;
 
-    @Mock private Broker.ProviderSession mockSession;
     @Mock private SchemaService mockSchemaService;
     @Mock private DOMMountPointService mockMountPointService;
+    @Mock private DOMDataBroker mockDataBroker;
+    @Mock private DOMRpcService mockRpcService;
+    @Mock private DOMNotificationService mockNotificationService;
+    @Mock DOMTransactionChain mockTransactionChain;
     @Mock private ListenerRegistration<SchemaContextListener> mockRegistration;
 
     @Rule
@@ -47,99 +49,26 @@ public class RestConnectorProviderTest {
     @Before
     public void init() {
         MockitoAnnotations.initMocks(this);
-        this.connectorProvider = new RestConnectorProvider();
+
+        doReturn(mockTransactionChain).when(mockDataBroker).createTransactionChain(Mockito.any());
+        doReturn(mockRegistration).when(mockSchemaService).registerSchemaContextListener(
+                Mockito.any(SchemaContextHandler.class));
+
+        this.connectorProvider = new RestConnectorProvider(mockDataBroker, mockSchemaService, mockRpcService,
+                mockNotificationService, mockMountPointService);
     }
 
     /**
-     * Test of successful initialization of {@link RestConnectorProvider}.
+     * Test for successful start when all conditions are satisfied.
      */
     @Test
-    public void restConnectorProviderInitTest() {
-        assertNotNull("Connector provider should be initialized and not null", this.connectorProvider);
-    }
-
-    /**
-     * Test for successful registration with {@link RestConnectorProvider#onSessionInitiated(Broker.ProviderSession)}
-     * when all conditions are satisfied.
-     *
-     * <p>
-     * Condition 1: <code>Broker.ProviderSession</code> contains <code>SchemaService</code>
-     * Condition 2: <code>Broker.ProviderSession</code> contains <code>DOMMountPointService</code>
-     */
-    @Test
-    public void successfulRegistrationTest() {
-        // prepare conditions
-        when(this.mockSession.getService(SchemaService.class)).thenReturn(this.mockSchemaService);
-        when(this.mockSession.getService(DOMMountPointService.class)).thenReturn(this.mockMountPointService);
-        final DOMDataBroker mockDataBroker = Mockito.mock(DOMDataBroker.class);
-        when(this.mockSession.getService(DOMDataBroker.class)).thenReturn(mockDataBroker);
-        final DOMTransactionChain mockTransactionChain = Mockito.mock(DOMTransactionChain.class);
-        when(mockDataBroker.createTransactionChain(Mockito.any())).thenReturn(mockTransactionChain);
-
+    public void successfulStartTest() {
         // test
-        this.connectorProvider.onSessionInitiated(this.mockSession);
+        this.connectorProvider.start();
 
         // verify interactions
-        verify(this.mockSession, times(1)).getService(SchemaService.class);
-        verify(this.mockSession, times(1)).getService(DOMMountPointService.class);
-        verify(this.mockSchemaService, times(1)).registerSchemaContextListener(Mockito.any(SchemaContextHandler.class));
-    }
-
-    /**
-     * Test for successful registration with {@link RestConnectorProvider#onSessionInitiated(Broker.ProviderSession)}
-     * without <code>DOMMountPointService</code>.
-     *
-     * <p>
-     * Condition 1: <code>Broker.ProviderSession</code> contains <code>SchemaService</code>
-     * Condition 2: <code>Broker.ProviderSession</code> does not contain <code>DOMMountPointService</code>
-     */
-    @Test
-    public void successfulRegistrationWithoutMountPointTest() {
-        // prepare conditions
-        when(this.mockSession.getService(SchemaService.class)).thenReturn(this.mockSchemaService);
-        when(this.mockSession.getService(DOMMountPointService.class)).thenReturn(null);
-        final DOMDataBroker mockDataBroker = Mockito.mock(DOMDataBroker.class);
-        when(this.mockSession.getService(DOMDataBroker.class)).thenReturn(mockDataBroker);
-        final DOMTransactionChain mockTransactionChain = Mockito.mock(DOMTransactionChain.class);
-        when(mockDataBroker.createTransactionChain(Mockito.any())).thenReturn(mockTransactionChain);
-        final DOMMountPointService mockDomMountPoint = Mockito.mock(DOMMountPointService.class);
-        when(this.mockSession.getService(DOMMountPointService.class)).thenReturn(mockDomMountPoint);
-
-        // test
-        this.connectorProvider.onSessionInitiated(this.mockSession);
-
-        // verify interactions
-        verify(this.mockSession, times(1)).getService(SchemaService.class);
-        verify(this.mockSession, times(1)).getService(DOMMountPointService.class);
-        verify(this.mockSchemaService, times(1)).registerSchemaContextListener(Mockito.any(SchemaContextHandler.class));
-    }
-
-    /**
-     * Negative test of registration with {@link RestConnectorProvider#onSessionInitiated(Broker.ProviderSession)} with
-     * null input. Test is expected to fail with <code>NullPointerException</code>.
-     */
-    @Test
-    public void nullSessionRegistrationNegativeTest() {
-        this.thrown.expect(NullPointerException.class);
-        this.connectorProvider.onSessionInitiated(null);
-    }
-
-    /**
-     * Negative test of registration with {@link RestConnectorProvider#onSessionInitiated(Broker.ProviderSession)} when
-     * <code>Broker.ProviderSession</code> does not contain <code>SchemaService</code>. Test is expected to fail with
-     * <code>NullPointerException</code>.
-     */
-    @Test
-    public void withoutSchemaServiceRegistrationNegativeTest() {
-        // prepare conditions
-        when(this.mockSession.getService(SchemaService.class)).thenReturn(null);
-
-        // test
-        this.thrown.expect(NullPointerException.class);
-        this.connectorProvider.onSessionInitiated(this.mockSession);
-
-        // verify interaction
-        verify(this.mockSession, times(1)).getService(SchemaService.class);
+        verify(mockDataBroker).createTransactionChain(Mockito.any());
+        verify(mockSchemaService).registerSchemaContextListener(Mockito.any(SchemaContextHandler.class));
     }
 
     /**
@@ -155,24 +84,14 @@ public class RestConnectorProviderTest {
      */
     @Test
     public void closeOpenTest() throws Exception {
-        // prepare conditions
-        when(this.mockSession.getService(SchemaService.class)).thenReturn(this.mockSchemaService);
-        when(this.mockSession.getService(DOMMountPointService.class)).thenReturn(this.mockMountPointService);
-        when(this.mockSchemaService.registerSchemaContextListener(Mockito.any(SchemaContextHandler.class)))
-                .thenReturn(this.mockRegistration);
-        final DOMDataBroker mockDataBroker = Mockito.mock(DOMDataBroker.class);
-        when(this.mockSession.getService(DOMDataBroker.class)).thenReturn(mockDataBroker);
-        final DOMTransactionChain mockTransactionChain = Mockito.mock(DOMTransactionChain.class);
-        when(mockDataBroker.createTransactionChain(Mockito.any())).thenReturn(mockTransactionChain);
+        // start
+        this.connectorProvider.start();
 
-        // register
-        this.connectorProvider.onSessionInitiated(this.mockSession);
-
-        // close registration
+        // close
         this.connectorProvider.close();
 
         // verify interaction
-        verify(this.mockRegistration, times(1)).close();
-        verify(mockTransactionChain, times(1)).close();
+        verify(this.mockRegistration).close();
+        verify(mockTransactionChain).close();
     }
 }
