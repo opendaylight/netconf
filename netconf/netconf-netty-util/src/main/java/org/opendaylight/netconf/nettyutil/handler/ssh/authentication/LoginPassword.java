1fd498da97f0a5174eae6d7679ccc2a6cdb7cc01
Atul Gosain <agosain@brocade.com>
2017-07-20 21:44:33 -0700

    RPC for netconf node addition. Supports encrypt option for password encryption.
    Change-Id: I34beb880cbe2ccc6a3fe8e5f6cd4253f7e1938f4
    Signed-off-by: Atul Gosain <atul.gosain@gmail.com>
    Signed-off-by: Atul Gosain <agosain@brocade.com>

--- netconf/netconf-netty-util/src/main/java/org/opendaylight/netconf/nettyutil/handler/ssh/authentication/LoginPassword.java
+++ netconf/netconf-netty-util/src/main/java/org/opendaylight/netconf/nettyutil/handler/ssh/authentication/LoginPassword.java
@@ -9,20 +9,33 @@
 package org.opendaylight.netconf.nettyutil.handler.ssh.authentication;
 
 import java.io.IOException;
+import java.security.GeneralSecurityException;
+
 import org.apache.sshd.ClientSession;
 import org.apache.sshd.client.future.AuthFuture;
+import org.opendaylight.aaa.encrypt.AAAEncryptionService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Class Providing username/password authentication option to
  * {@link org.opendaylight.netconf.nettyutil.handler.ssh.client.AsyncSshHandler}
  */
 public class LoginPassword extends AuthenticationHandler {
+
     private final String username;
     private final String password;
+    private final AAAEncryptionService encryptionService;
+    private final Logger LOG = LoggerFactory.getLogger(LoginPassword.class);
 
     public LoginPassword(String username, String password) {
+        this(username, password, null);
+    }
+
+    public LoginPassword(final String username, final String password, final AAAEncryptionService encryptionService) {
         this.username = username;
         this.password = password;
+        this.encryptionService = encryptionService;
     }
 
     @Override
@@ -32,7 +45,17 @@ public class LoginPassword extends AuthenticationHandler {
 
     @Override
     public AuthFuture authenticate(final ClientSession session) throws IOException {
-        session.addPasswordIdentity(password);
+        if (encryptionService != null) {
+            String decryptedPassword = password;
+            try {
+                decryptedPassword = encryptionService.decrypt(password);
+            } catch (GeneralSecurityException e) {
+                LOG.warn("Unable to decrypt password.");
+            }
+            session.addPasswordIdentity(decryptedPassword);
+        } else {
+            session.addPasswordIdentity(password);
+        }
         return session.auth();
     }
 }
---
+++ netconf/sal-netconf-connector/src/test/java/org/opendaylight/netconf/sal/connect/netconf/util/NetconfTopologyRPCProviderTest.java
@@ -0,0 +1,102 @@
+/*
+ * Copyright (c) 2017 Brocade Communication Systems and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.netconf.sal.connect.netconf.util;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.opendaylight.aaa.encrypt.AAAEncryptionService;
+import org.opendaylight.controller.md.sal.binding.api.DataBroker;
+import org.opendaylight.netconf.sal.connect.util.NetconfTopologyRPCProvider;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev130715.Host;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev130715.IpAddress;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev130715.Ipv4Address;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev130715.PortNumber;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netconf.node.topology.rev150114.AddNetconfNodeInput;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netconf.node.topology.rev150114.AddNetconfNodeInputBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netconf.node.topology.rev150114.NetconfNode;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netconf.node.topology.rev150114.netconf.node.credentials.credentials.LoginPassword;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netconf.node.topology.rev150114.netconf.node.credentials.credentials.LoginPasswordBuilder;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.NodeId;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.security.GeneralSecurityException;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import static org.mockito.Mockito.when;
+
+public class NetconfTopologyRPCProviderTest {
+    private static final NodeId NODE_ID     = new NodeId("testing-node");
+    private static final String TOPOLOGY_ID = "testing-topology";
+    private static final String TEST_PWD =  "test";
+    private static final String ENC_PWD = "4o9/Hn3Pi4150YrP12N/1g==";
+
+    @Mock
+    private DataBroker dataBroker;
+
+    @Mock
+    private AAAEncryptionService encryptionService;
+
+    NetconfTopologyRPCProvider rpcProvider ;
+
+    @Before
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+        try {
+            when(encryptionService.encrypt(TEST_PWD)).thenReturn(ENC_PWD);
+        } catch (GeneralSecurityException e) {
+            e.printStackTrace();
+        }
+        rpcProvider = new NetconfTopologyRPCProvider(dataBroker, encryptionService, TOPOLOGY_ID);
+    }
+
+    @Test
+    public void testEncryptPassword(){
+
+        NetconfNode node = invokeEncryption(true);
+        assertNotEquals(TEST_PWD, ((LoginPassword)node.getCredentials()).getPassword());
+
+        node = invokeEncryption(false);
+        assertEquals(TEST_PWD, ((LoginPassword)node.getCredentials()).getPassword());
+    }
+
+    private NetconfNode invokeEncryption(boolean encrypt){
+        Method method = null;
+        try {
+            method = NetconfTopologyRPCProvider.class.getDeclaredMethod("encryptPassword", AddNetconfNodeInput.class);
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        }
+        method.setAccessible(true);
+        NetconfNode node = null;
+        try {
+            node = (NetconfNode)method.invoke(rpcProvider, getInput(encrypt));
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
+
+        return node;
+    }
+
+    private AddNetconfNodeInput getInput(boolean encrypt){
+        AddNetconfNodeInputBuilder builder = new AddNetconfNodeInputBuilder();
+        builder.setCredentials(new LoginPasswordBuilder().setPassword(TEST_PWD).setUsername("test").build());
+        builder.setHost(new Host(new IpAddress(new Ipv4Address("10.18.16.188"))));
+        builder.setPort(new PortNumber(830));
+        builder.setTcpOnly(false);
+        builder.setNodeId(NODE_ID.toString());
+        builder.setEncrypt(encrypt);
+        return builder.build();
+    }
+
+}
