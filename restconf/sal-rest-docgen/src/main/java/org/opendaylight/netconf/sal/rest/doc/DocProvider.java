f0931f7ac525218dfb99f9829f11d7dd7a17f82f
Ryan Goulding <ryandgoulding@gmail.com>
2017-08-09 15:46:39 -0400

    Bug 8968: Convert sal-rest-docgen to blueprint
    Removes BundleActivator and corresponding ServiceTracker implementation
    in favor of utilizing blueprint DI.  Advertises a service for
    appropriate downstream consumption.
    
    Change-Id: I4030253db94f1c79423f41c87c6c75493d5b53d4
    Signed-off-by: Ryan Goulding <ryandgoulding@gmail.com>

--- restconf/sal-rest-docgen/src/main/java/org/opendaylight/netconf/sal/rest/doc/DocProvider.java
+++ restconf/sal-rest-docgen/src/main/java/org/opendaylight/netconf/sal/rest/doc/DocProvider.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014 Cisco Systems, Inc. and others.  All rights reserved.
+ * Copyright (c) 2014, 2017 Cisco Systems, Inc. and others.  All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
@@ -7,49 +7,27 @@
  */
 package org.opendaylight.netconf.sal.rest.doc;
 
-import java.util.Collection;
-import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 import org.opendaylight.controller.md.sal.dom.api.DOMMountPointService;
-import org.opendaylight.controller.sal.core.api.Broker;
-import org.opendaylight.controller.sal.core.api.Provider;
 import org.opendaylight.controller.sal.core.api.model.SchemaService;
 import org.opendaylight.controller.sal.core.api.mount.MountProvisionListener;
 import org.opendaylight.netconf.sal.rest.doc.impl.ApiDocGenerator;
 import org.opendaylight.netconf.sal.rest.doc.mountpoints.MountPointSwagger;
 import org.opendaylight.yangtools.concepts.ListenerRegistration;
-import org.osgi.framework.BundleActivator;
-import org.osgi.framework.BundleContext;
-import org.osgi.framework.ServiceReference;
-import org.osgi.util.tracker.ServiceTracker;
-import org.osgi.util.tracker.ServiceTrackerCustomizer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class DocProvider implements BundleActivator, ServiceTrackerCustomizer<Broker, Broker>,
-        Provider, AutoCloseable {
+public class DocProvider {
 
     private static final Logger LOG = LoggerFactory.getLogger(DocProvider.class);
 
-    private ServiceTracker<Broker, Broker> brokerServiceTracker;
-    private BundleContext bundleContext;
-    private Broker.ProviderSession session;
-
     private final List<AutoCloseable> toClose = new LinkedList<>();
 
-    @Override
-    public void close() throws Exception {
-        stop(bundleContext);
-    }
+    public DocProvider(final SchemaService schemaService, final DOMMountPointService mountService) {
 
-    @Override
-    public void onSessionInitiated(final Broker.ProviderSession providerSession) {
-        final SchemaService schemaService = providerSession.getService(SchemaService.class);
         ApiDocGenerator.getInstance().setSchemaService(schemaService);
 
-        final DOMMountPointService mountService = providerSession
-                .getService(DOMMountPointService.class);
         final ListenerRegistration<MountProvisionListener> registration = mountService
                 .registerProvisionListener(MountPointSwagger.getInstance());
         MountPointSwagger.getInstance().setGlobalSchema(schemaService);
@@ -61,54 +39,11 @@ public class DocProvider implements BundleActivator, ServiceTrackerCustomizer<Br
         LOG.debug("Restconf API Explorer started");
     }
 
-    @Override
-    public Collection<ProviderFunctionality> getProviderFunctionality() {
-        return Collections.emptySet();
-    }
-
-    @Override
-    public void start(final BundleContext context) throws Exception {
-        bundleContext = context;
-        brokerServiceTracker = new ServiceTracker<>(context, Broker.class, this);
-        brokerServiceTracker.open();
-    }
-
-    @Override
-    public void stop(final BundleContext context) throws Exception {
-        if (brokerServiceTracker != null) {
-            brokerServiceTracker.close();
-        }
-
-        if (session != null) {
-            session.close();
-        }
-
+    public void close() throws Exception {
         synchronized (toClose) {
             for (final AutoCloseable close : toClose) {
                 close.close();
             }
         }
-    }
-
-    @Override
-    public Broker addingService(final ServiceReference<Broker> reference) {
-        final Broker broker = bundleContext.getService(reference);
-        session = broker.registerProvider(this, bundleContext);
-        return broker;
-    }
-
-    @Override
-    public void modifiedService(final ServiceReference<Broker> reference, final Broker service) {
-        if (session != null) {
-            session.close();
-        }
-
-        final Broker broker = bundleContext.getService(reference);
-        session = broker.registerProvider(this, bundleContext);
-    }
-
-    @Override
-    public void removedService(final ServiceReference<Broker> reference, final Broker service) {
-        bundleContext.ungetService(reference);
     }
 }
