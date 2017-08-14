9f8c132dff8ca4f3f29868c60eac26b05431a7ce
Ryan Goulding <ryandgoulding@gmail.com>
2017-08-14 15:48:46 -0400

    Directly inject CredentialAuth dependency
    In today's blueprint-oriented OSGi DI, it is more efficient to
    express the dependency on AAA's CredentialAuth API rather than to
    spin waiting for a ServiceReference using a ServiceTracker.  This
    change specifies direct injection of AAA's CredentialAuth Service
    through blueprint wiring.
    
    Change-Id: Iec02f8acc4763922505881a772ff128f1f9d9986
    Signed-off-by: Ryan Goulding <ryandgoulding@gmail.com>

--- netconf/aaa-authn-odl-plugin/src/main/java/org/opendaylight/aaa/odl/CredentialServiceAuthProvider.java
+++ netconf/aaa-authn-odl-plugin/src/main/java/org/opendaylight/aaa/odl/CredentialServiceAuthProvider.java
@@ -12,10 +12,6 @@ import org.opendaylight.aaa.api.Claim;
 import org.opendaylight.aaa.api.CredentialAuth;
 import org.opendaylight.aaa.api.PasswordCredentials;
 import org.opendaylight.netconf.auth.AuthProvider;
-import org.osgi.framework.BundleContext;
-import org.osgi.framework.ServiceReference;
-import org.osgi.util.tracker.ServiceTracker;
-import org.osgi.util.tracker.ServiceTrackerCustomizer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -28,38 +24,10 @@ public final class CredentialServiceAuthProvider implements AuthProvider, AutoCl
 
     // FIXME CredentialAuth is generic and it causes warnings during compilation
     // Maybe there should be a PasswordCredentialAuth implements CredentialAuth<PasswordCredentials>
-    private volatile CredentialAuth<PasswordCredentials> nullableCredService;
-    private final ServiceTracker<CredentialAuth, CredentialAuth> listenerTracker;
+    private final CredentialAuth<PasswordCredentials> credService;
 
-    public CredentialServiceAuthProvider(final BundleContext bundleContext) {
-
-        final ServiceTrackerCustomizer<CredentialAuth, CredentialAuth> customizer =
-                new ServiceTrackerCustomizer<CredentialAuth, CredentialAuth>() {
-            @Override
-            public CredentialAuth addingService(final ServiceReference<CredentialAuth> reference) {
-                LOG.trace("Credential service {} added", reference);
-                nullableCredService = bundleContext.getService(reference);
-                return nullableCredService;
-            }
-
-            @Override
-            public void modifiedService(final ServiceReference<CredentialAuth> reference,
-                                        final CredentialAuth service) {
-                LOG.trace("Replacing modified Credential service {}", reference);
-                nullableCredService = service;
-            }
-
-            @Override
-            public void removedService(final ServiceReference<CredentialAuth> reference, final CredentialAuth service) {
-                LOG.trace("Removing Credential service {}. "
-                        + "This AuthProvider will fail to authenticate every time", reference);
-                synchronized (CredentialServiceAuthProvider.this) {
-                    nullableCredService = null;
-                }
-            }
-        };
-        listenerTracker = new ServiceTracker<>(bundleContext, CredentialAuth.class, customizer);
-        listenerTracker.open();
+    public CredentialServiceAuthProvider(final CredentialAuth<PasswordCredentials> credService) {
+        this.credService = credService;
     }
 
     /**
@@ -67,15 +35,11 @@ public final class CredentialServiceAuthProvider implements AuthProvider, AutoCl
      * and delegates the decision to it. If the service is not available, IllegalStateException is thrown.
      */
     @Override
-    public synchronized boolean authenticated(final String username, final String password) {
-        if (nullableCredService == null) {
-            LOG.warn("Cannot authenticate user '{}', Credential service is missing", username);
-            throw new IllegalStateException("Credential service is not available");
-        }
+    public boolean authenticated(final String username, final String password) {
 
         Claim claim;
         try {
-            claim = nullableCredService.authenticate(new PasswordCredentialsWrapper(username, password));
+            claim = credService.authenticate(new PasswordCredentialsWrapper(username, password));
         } catch (AuthenticationException e) {
             LOG.debug("Authentication failed for user '{}' : {}", username, e);
             return false;
@@ -83,15 +47,6 @@ public final class CredentialServiceAuthProvider implements AuthProvider, AutoCl
 
         LOG.debug("Authentication result for user '{}' : {}", username, claim.domain());
         return true;
-    }
-
-    /**
-     * Invoked by blueprint.
-     */
-    @Override
-    public void close() {
-        listenerTracker.close();
-        nullableCredService = null;
     }
 
     private static final class PasswordCredentialsWrapper implements PasswordCredentials {
