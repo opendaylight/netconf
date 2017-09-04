1ba532e3da4a469a02718584863cf5040ae9d66e
Jakub Toth <jakub.toth@pantheon.tech>
2017-09-04 13:24:53 +0200

    Fix failed test - tested string of '..was[n']t..' to '..was[ no]t..'
    Change-Id: I2e49e3d4ce72dfd161807736f9afc9b2a5af3edb
    Signed-off-by: Jakub Toth <jakub.toth@pantheon.tech>

--- restconf/sal-rest-connector/src/test/java/org/opendaylight/controller/sal/restconf/impl/json/to/nn/test/JsonToNnTest.java
+++ restconf/sal-rest-connector/src/test/java/org/opendaylight/controller/sal/restconf/impl/json/to/nn/test/JsonToNnTest.java
@@ -127,7 +127,7 @@ public class JsonToNnTest extends AbstractBodyReaderTest {
         }
         assertNotNull(exception);
         assertEquals(
-                "Error parsing input: Schema node with name cont wasn't found under "
+                "Error parsing input: Schema node with name cont was not found under "
                         + "(urn:ietf:params:xml:ns:netconf:base:1.0)data.",
                 exception.getErrors().get(0).getErrorMessage());
 
@@ -143,7 +143,7 @@ public class JsonToNnTest extends AbstractBodyReaderTest {
         }
         assertNotNull(exception);
         assertEquals(
-                "Error parsing input: Schema node with name lst1 wasn't found under "
+                "Error parsing input: Schema node with name lst1 was not found under "
                         + "(urn:ietf:params:xml:ns:netconf:base:1.0)data.",
                 exception.getErrors().get(0).getErrorMessage());
 
@@ -159,7 +159,7 @@ public class JsonToNnTest extends AbstractBodyReaderTest {
         }
         assertNotNull(exception);
         assertEquals(
-                "Error parsing input: Schema node with name lf wasn't found under "
+                "Error parsing input: Schema node with name lf was not found under "
                         + "(urn:ietf:params:xml:ns:netconf:base:1.0)data.",
                 exception.getErrors().get(0).getErrorMessage());
         assertEquals(3, countExceptions);
