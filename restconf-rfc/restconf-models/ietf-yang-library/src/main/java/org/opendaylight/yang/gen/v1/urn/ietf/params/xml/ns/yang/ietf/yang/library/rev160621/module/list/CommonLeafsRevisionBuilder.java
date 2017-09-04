6150ae62aae0d6b5a9292d9a7767299f893f731b
Jakub Toth <jakub.toth@pantheon.tech>
2017-09-04 10:15:10 +0200

    Split Restconf implementations (draft02 and RFC)
    Modules tree:
    
      * restconf-rfc
        * restconf-models (involve models)
          * ietf-restconf-monitoring + yang model
          * ietf-restconf + yang model
          * ietf-yang-library + yang model
        * restconf-connector
        * restconf-docgen
        * restconf-docgen-maven
        * restconf-artifacts
        * restconf-connector-config
    
    Change-Id: I401a7fc595251dfd2ae08074096a4a16707c6527
    Signed-off-by: Jakub Toth <jakub.toth@pantheon.tech>

---
+++ restconf-rfc/restconf-models/ietf-yang-library/src/main/java/org/opendaylight/yang/gen/v1/urn/ietf/params/xml/ns/yang/ietf/yang/library/rev160621/module/list/CommonLeafsRevisionBuilder.java
@@ -0,0 +1,21 @@
+package org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.yang.library.rev160621.module.list;
+
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.yang.library.rev160621.module.list.CommonLeafs.Revision;
+
+
+/**
+ * The purpose of generated class in src/main/java for Union types is to create new instances of unions from a string representation.
+ * In some cases it is very difficult to automate it since there can be unions such as (uint32 - uint16), or (string - uint32).
+ *
+ * The reason behind putting it under src/main/java is:
+ * This class is generated in form of a stub and needs to be finished by the user. This class is generated only once to prevent
+ * loss of user code.
+ *
+ */
+public class CommonLeafsRevisionBuilder {
+
+    public static Revision getDefaultInstance(final java.lang.String defaultValue) {
+        return new Revision(defaultValue);
+    }
+
+}
