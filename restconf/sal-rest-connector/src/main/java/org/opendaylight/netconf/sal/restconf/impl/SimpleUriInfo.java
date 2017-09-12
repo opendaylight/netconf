38e3b41c05200085e62a8c11949eae53d92d3fb6
Tom Pantelis <tompantelis@gmail.com>
2017-09-12 12:03:00 -0400

    Add local MultivaluedHashMap implementation
    The JSONRestconfService implementation contains a local UriInfo
    implementation, SimpleUriInfo, that uses a MultivaluedHashMap
    implementation from javax.ws.rs.core. This class is provided by
    the dependency used at compile time however, the runtime bundle
    that provides the javax.ws.rs.core interfaces doesn't provide
    MultivaluedHashMap. Therefore I added our own local MultivaluedHashMap
    class based on the external one. I also refactored SimpleUriInfo into
    it's own class as it's used by both JSONRestconfService implementations.
    
    Change-Id: I1af0771327d46c72e55facf14c41038fd583e0ed
    Signed-off-by: Tom Pantelis <tompantelis@gmail.com>

---
+++ restconf/sal-rest-connector/src/main/java/org/opendaylight/netconf/sal/restconf/impl/SimpleUriInfo.java
@@ -0,0 +1,125 @@
+/*
+ * Copyright (c) 2017 Inocybe Technologies and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.netconf.sal.restconf.impl;
+
+import java.net.URI;
+import java.util.Collections;
+import java.util.List;
+import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.core.PathSegment;
+import javax.ws.rs.core.UriBuilder;
+import javax.ws.rs.core.UriInfo;
+
+class SimpleUriInfo implements UriInfo {
+    private final String path;
+    private final MultivaluedMap<String, String> queryParams;
+
+    SimpleUriInfo(String path) {
+        this(path, new MultivaluedHashMap<>());
+    }
+
+    SimpleUriInfo(String path, MultivaluedMap<String, String> queryParams) {
+        this.path = path;
+        this.queryParams = queryParams;
+    }
+
+    @Override
+    public String getPath() {
+        return path;
+    }
+
+    @Override
+    public String getPath(boolean decode) {
+        return path;
+    }
+
+    @Override
+    public List<PathSegment> getPathSegments() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public List<PathSegment> getPathSegments(boolean decode) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public URI getRequestUri() {
+        return URI.create(path);
+    }
+
+    @Override
+    public UriBuilder getRequestUriBuilder() {
+        return UriBuilder.fromUri(getRequestUri());
+    }
+
+    @Override
+    public URI getAbsolutePath() {
+        return getRequestUri();
+    }
+
+    @Override
+    public UriBuilder getAbsolutePathBuilder() {
+        return UriBuilder.fromUri(getAbsolutePath());
+    }
+
+    @Override
+    public URI getBaseUri() {
+        return URI.create("");
+    }
+
+    @Override
+    public UriBuilder getBaseUriBuilder() {
+        return UriBuilder.fromUri(getBaseUri());
+    }
+
+    @Override
+    public MultivaluedMap<String, String> getPathParameters() {
+        return new MultivaluedHashMap<>();
+    }
+
+    @Override
+    public MultivaluedMap<String, String> getPathParameters(boolean decode) {
+        return getPathParameters();
+    }
+
+    @Override
+    public MultivaluedMap<String, String> getQueryParameters() {
+        return queryParams;
+    }
+
+    @Override
+    public MultivaluedMap<String, String> getQueryParameters(boolean decode) {
+        return getQueryParameters();
+    }
+
+    @Override
+    public List<String> getMatchedURIs() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public List<String> getMatchedURIs(boolean decode) {
+        return getMatchedURIs();
+    }
+
+    @Override
+    public List<Object> getMatchedResources() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public URI resolve(URI uri) {
+        return uri;
+    }
+
+    @Override
+    public URI relativize(URI uri) {
+        return uri;
+    }
+}
