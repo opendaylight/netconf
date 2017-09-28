9ae46c73bc2437dc1dab54498b3a64c0600fa9e8
Evan Zeller <evanrzeller@gmail.com>
2017-09-27 18:15:31 -0700

    BUG-9132: don't provide a value for restconf/streams/events
    The yang type of events leaf is 'type empty', meaning there should be no value.
    As of Carbon performing a GET on restconf/streams causes a cast
    exception due to the empty string value here. See EmptyJSONCodec in
    yangtools.
    
    Change-Id: If0501b9c5459f4255096a4f070968c87a749407e
    Signed-off-by: Evan Zeller <evanrzeller@gmail.com>

--- restconf/sal-rest-connector/src/main/java/org/opendaylight/netconf/sal/restconf/impl/RestconfImpl.java
+++ restconf/sal-rest-connector/src/main/java/org/opendaylight/netconf/sal/restconf/impl/RestconfImpl.java
@@ -1503,7 +1503,7 @@ public class RestconfImpl implements RestconfService {
         instanceDataChildrenByName = ControllerContext.findInstanceDataChildrenByName(listStreamSchemaNode, "events");
         final DataSchemaNode eventsSchemaNode = Iterables.getFirst(instanceDataChildrenByName, null);
         Preconditions.checkState(eventsSchemaNode instanceof LeafSchemaNode);
-        streamNodeValues.withChild(Builders.leafBuilder((LeafSchemaNode) eventsSchemaNode).withValue("").build());
+        streamNodeValues.withChild(Builders.leafBuilder((LeafSchemaNode) eventsSchemaNode).build());
 
         return streamNodeValues.build();
     }
