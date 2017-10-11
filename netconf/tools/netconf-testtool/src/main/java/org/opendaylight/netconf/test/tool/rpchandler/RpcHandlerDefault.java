e0e358fdd0d69b76d99e46adc38a813eff7382ce
Juraj Veverka <juraj.veverka@pantheon.tech>
2017-10-11 13:27:58 +0200

    NetConf test tool improved so now it can be used as library not only as CLI tool.
    Change-Id: I14ca03d0b2cec0f6fe8b5994b3e195fa6de8a4d2
    Signed-off-by: Juraj Veverka <juraj.veverka@pantheon.tech>

---
+++ netconf/tools/netconf-testtool/src/main/java/org/opendaylight/netconf/test/tool/rpchandler/RpcHandlerDefault.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.netconf.test.tool.rpchandler;
+
+import java.util.Optional;
+import org.opendaylight.controller.config.util.xml.XmlElement;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.w3c.dom.Document;
+
+public class RpcHandlerDefault implements RpcHandler {
+
+    private static final Logger LOG = LoggerFactory.getLogger(RpcHandlerDefault.class);
+
+    @Override
+    public Optional<Document> getResponse(XmlElement rpcElement) {
+        LOG.info("getResponse: {}", rpcElement.toString());
+        return null;
+    }
+
+}
