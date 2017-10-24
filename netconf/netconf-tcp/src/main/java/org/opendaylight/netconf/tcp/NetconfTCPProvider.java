7ca9b664e025a4272f0ba2520b7fab5b90ac886c
Ryan Goulding <ryandgoulding@gmail.com>
2017-10-24 17:23:10 -0400

    Convert netconf-util, netconf-ssh, netconf-tcp to blueprint
    netconf-util, netconf-ssh, and netconf-tcp still used a BundleActivator
    and accompanying ServiceTracker for service instantiation.  netconf-ssh
    and netconf-tcp depend on netconf-util for NetconfConfiguration, so it
    was decided to do the conversion of all three bundles in one patch.
    
    netconf-util utilizes blueprint-cm to instantiate NetconfConfiguration
    based on the ManagedService identified by the "netconf" PID.
    
    Tests that were aimed at testing bespoke Activator logic were removed,
    since the accompanying Activators were removed.  ServiceTrackers were
    removed in favor of bean references.  Scaffolding used to locate the
    NetconfConfiguration ManagedService was also removed since it is now
    directly referenced using blueprint.
    
    JIRA: NETCONF-510
    Change-Id: I27452ea509bc50333ffc00c6d1d8de3133e8b850
    Signed-off-by: Ryan Goulding <ryandgoulding@gmail.com>
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- netconf/netconf-tcp/src/main/java/org/opendaylight/netconf/tcp/NetconfTCPProvider.java
+++ netconf/netconf-tcp/src/main/java/org/opendaylight/netconf/tcp/NetconfTCPProvider.java
@@ -6,14 +6,11 @@
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
 
-package org.opendaylight.netconf.tcp.osgi;
+package org.opendaylight.netconf.tcp;
 
 import java.net.InetSocketAddress;
 import org.opendaylight.netconf.tcp.netty.ProxyServer;
-import org.opendaylight.netconf.util.osgi.NetconfConfigUtil;
-import org.opendaylight.netconf.util.osgi.NetconfConfiguration;
-import org.osgi.framework.BundleActivator;
-import org.osgi.framework.BundleContext;
+import org.opendaylight.netconf.util.NetconfConfiguration;
 import org.osgi.framework.InvalidSyntaxException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -21,26 +18,33 @@ import org.slf4j.LoggerFactory;
 /**
  * Opens TCP port specified in config.ini, creates bridge between this port and local netconf server.
  */
-public class NetconfTCPActivator implements BundleActivator {
-    private static final Logger LOG = LoggerFactory.getLogger(NetconfTCPActivator.class);
+public class NetconfTCPProvider {
+    private static final Logger LOG = LoggerFactory.getLogger(NetconfTCPProvider.class);
+
+    private final NetconfConfiguration netconfConfiguration;
     private ProxyServer proxyServer;
 
-    @Override
-    public void start(BundleContext context) throws InvalidSyntaxException {
-        final NetconfConfiguration netconfConfiguration = NetconfConfigUtil.getNetconfConfigurationService(context);
+    public NetconfTCPProvider(final NetconfConfiguration netconfConfiguration) {
+        this.netconfConfiguration = netconfConfiguration;
+    }
 
+    // Called via blueprint
+    @SuppressWarnings("unused")
+    public void init() throws InvalidSyntaxException {
         final InetSocketAddress address = netconfConfiguration.getTcpServerAddress();
 
         if (address.getAddress().isAnyLocalAddress()) {
-            LOG.warn("Unprotected netconf TCP address is configured to ANY local address. This is a security risk. "
-                    + "Consider changing tcp-address in netconf.cfg to 127.0.0.1");
+            LOG.warn("Unprotected netconf TCP address is configured to ANY "
+                    + "local address. This is a security risk. Consider "
+                    + "changing tcp-address in netconf.cfg to 127.0.0.1");
         }
         LOG.info("Starting TCP netconf server at {}", address);
         proxyServer = new ProxyServer(address, NetconfConfiguration.NETCONF_LOCAL_ADDRESS);
     }
 
-    @Override
-    public void stop(BundleContext context) {
+    // Called via blueprint
+    @SuppressWarnings("unused")
+    public void destroy() {
         if (proxyServer != null) {
             proxyServer.close();
         }
