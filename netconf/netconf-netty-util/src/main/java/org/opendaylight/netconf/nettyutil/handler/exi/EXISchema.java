8a5b6357bcf9ef5d9996f0605d7b8bf410099b77
Robert Varga <robert.varga@pantheon.tech>
2018-01-17 19:43:52 +0100

    Add the ability to use schema-aware coding
    With the switch to Exificient we can actually use schema-informed
    encoding -- add basic support for that.
    
    Change-Id: If24a418302d151114407ee647ec5e12c3d3b3e76
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ netconf/netconf-netty-util/src/main/java/org/opendaylight/netconf/nettyutil/handler/exi/EXISchema.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright (c) 2018 Pantheon Technologies s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.netconf.nettyutil.handler.exi;
+
+import static java.util.Objects.requireNonNull;
+
+import com.google.common.io.ByteSource;
+import com.google.common.io.Resources;
+import com.siemens.ct.exi.core.exceptions.EXIException;
+import com.siemens.ct.exi.core.grammars.Grammars;
+import com.siemens.ct.exi.grammars.GrammarFactory;
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * Enumeration of schema modes defined by the NETCONF EXI capability.
+ */
+public enum EXISchema {
+    NONE("none", GrammarFactory.newInstance().createSchemaLessGrammars()),
+    BUILTIN("builtin", createBuiltinGrammar()),
+    BASE_1_1("base:1.1", createNetconfGrammar());
+
+    private String option;
+    private Grammars grammar;
+
+    EXISchema(final String option, final Grammars grammar) {
+        this.option = requireNonNull(option);
+        this.grammar = requireNonNull(grammar);
+    }
+
+    final String getOption() {
+        return option;
+    }
+
+    final Grammars getGrammar() {
+        return grammar;
+    }
+
+    static EXISchema forOption(final String id) {
+        for (EXISchema s : EXISchema.values()) {
+            if (id.equals(s.getOption())) {
+                return s;
+            }
+        }
+
+        return null;
+    }
+
+    private static Grammars createNetconfGrammar() {
+        final ByteSource source = Resources.asByteSource(EXISchema.class.getResource("/rfc6241.xsd"));
+        try (InputStream is = source.openStream()) {
+            final Grammars g = GrammarFactory.newInstance().createGrammars(is);
+            g.setSchemaId("base:1.1");
+            return g;
+        } catch (EXIException | IOException e) {
+            throw new IllegalStateException("Failed to create RFC6241 grammar", e);
+        }
+    }
+
+    private static Grammars createBuiltinGrammar() {
+        try {
+            return GrammarFactory.newInstance().createXSDTypesOnlyGrammars();
+        } catch (EXIException e) {
+            throw new IllegalStateException("Failed to create builtin grammar", e);
+        }
+    }
+}
