5185dcce1759f82d17469d591deb44b4089609f1
LiGuosheng <li.guosheng6@zte.com.cn>
2018-02-23 14:34:39 +0800

    Add interface SslHandlerFactory
    This factory is used by the TLS client to create SslHandler that will
    be added into the channel pipeline when the channel is active.
    
    Change-Id: I95c2a0ff105020ac84b02d82207f830d70ef7197
    Signed-off-by: Li guosheng <li.guosheng6@zte.com.cn>

---
+++ netconf/netconf-client/src/main/java/org/opendaylight/netconf/client/SslHandlerFactory.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2018 ZTE Corporation. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.netconf.client;
+
+import io.netty.handler.ssl.SslHandler;
+
+public interface SslHandlerFactory {
+    /**
+     * This factory is used by the TLS client to create SslHandler that will be added
+     * into the channel pipeline when the channel is active.
+     */
+    SslHandler createSslHandler();
+}
