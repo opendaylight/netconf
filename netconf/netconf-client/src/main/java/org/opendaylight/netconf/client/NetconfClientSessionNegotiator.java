5111ad236d453e3096e30051810d13111b047700
jiang.wei <351183359@qq.com>
2018-03-21 15:17:36 +0800

    channelActive may happen later than handleMessage because device will not wait for your hello message.     This is defined in the RFC 6241.The channel is ready because we can get the device hello message normally.     So we can startNegotiation by myself and do what channelActive want to do.
    Change-Id: I46e4a85fc7be1bc221cfbb43386eae95768ac5ac
    Signed-off-by: jiang.wei <351183359@qq.com>

--- netconf/netconf-client/src/main/java/org/opendaylight/netconf/client/NetconfClientSessionNegotiator.java
+++ netconf/netconf-client/src/main/java/org/opendaylight/netconf/client/NetconfClientSessionNegotiator.java
@@ -63,8 +63,18 @@ public class NetconfClientSessionNegotiator extends
         super(sessionPreferences, promise, channel, timer, sessionListener, connectionTimeoutMillis);
     }
 
+    @SuppressWarnings("checkstyle:IllegalCatch")
     @Override
     protected void handleMessage(final NetconfHelloMessage netconfMessage) throws NetconfDocumentedException {
+        if (!startNegotiationAlready()) {
+            LOG.debug("Starting session negotiation on channel {} by myself", channel);
+            try {
+                startNegotiation();
+            } catch (final Exception e) {
+                LOG.warn("Unexpected negotiation failure", e);
+                negotiationFailed(e);
+            }
+        }
         final NetconfClientSession session = getSessionForHelloMessage(netconfMessage);
         replaceHelloMessageInboundHandler(session);
 
