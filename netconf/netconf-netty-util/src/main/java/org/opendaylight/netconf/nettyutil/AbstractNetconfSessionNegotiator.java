ab8fa2162c86c3bd81b6bda70a0c3c9698e37a53
jiang.wei <351183359@qq.com>
2018-03-21 15:17:36 +0800

    channelActive may happen later than handleMessage because device will not wait for your hello message.     This is defined in the RFC 6241.The channel is ready because we can get the device hello message normally.     So we can startNegotiation by myself and do what channelActive want to do.
    Change-Id: I46e4a85fc7be1bc221cfbb43386eae95768ac5ac
    Signed-off-by: jiang.wei <351183359@qq.com>

--- netconf/netconf-netty-util/src/main/java/org/opendaylight/netconf/nettyutil/AbstractNetconfSessionNegotiator.java
+++ netconf/netconf-netty-util/src/main/java/org/opendaylight/netconf/nettyutil/AbstractNetconfSessionNegotiator.java
@@ -79,20 +79,29 @@ public abstract class AbstractNetconfSessionNegotiator<P extends NetconfSessionP
 
     @Override
     protected final void startNegotiation() {
-        final Optional<SslHandler> sslHandler = getSslHandler(channel);
-        if (sslHandler.isPresent()) {
-            Future<Channel> future = sslHandler.get().handshakeFuture();
-            future.addListener(new GenericFutureListener<Future<? super Channel>>() {
-                @Override
-                public void operationComplete(final Future<? super Channel> future) {
-                    Preconditions.checkState(future.isSuccess(), "Ssl handshake was not successful");
-                    LOG.debug("Ssl handshake complete");
-                    start();
-                }
-            });
+        if (isStateIdle()) {
+            LOG.info("start Negotiation already on channel {}", channel);
         } else {
-            start();
+            final Optional<SslHandler> sslHandler = getSslHandler(channel);
+            if (sslHandler.isPresent()) {
+                Future<Channel> future = sslHandler.get().handshakeFuture();
+                future.addListener(new GenericFutureListener<Future<? super Channel>>() {
+                    @Override
+                    public void operationComplete(final Future<? super Channel> future) {
+                        Preconditions.checkState(future.isSuccess(), "Ssl handshake was not successful");
+                        LOG.debug("Ssl handshake complete");
+                        start();
+                    }
+                });
+            } else {
+                start();
+            }
         }
+    }
+
+    protected final boolean isStateIdle() {
+        //It indicates whether start Negotiation Already
+        return this.state != State.IDLE;
     }
 
     private static Optional<SslHandler> getSslHandler(final Channel channel) {
