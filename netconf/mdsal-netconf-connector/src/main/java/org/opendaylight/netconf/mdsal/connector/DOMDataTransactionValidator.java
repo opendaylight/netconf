f2b917a0da0c87f89796526ae1f424c63ccc7ef1
Marek Gradzki <mgradzki@cisco.com>
2018-03-28 21:52:41 +0200

    NETCONF-536: support for <candidate> validation
    Provides support <validate> RPC, as defined in:
    https://tools.ietf.org/html/rfc4741#section-8.6.
    
    Only <candidate> datastore is supported.
    
    Support for test-option and capability advertisement
    are not included in this patch.
    
    The actual implementation of validation process is delegated to
    DOMDataTransactionValidator - an DOMDataBrokerExtension.
    
    Change-Id: I4bbaba18d2e630787d127a51c26918d05fd7e648
    Signed-off-by: Marek Gradzki <mgradzki@cisco.com>

---
+++ netconf/mdsal-netconf-connector/src/main/java/org/opendaylight/netconf/mdsal/connector/DOMDataTransactionValidator.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright (c) 2018 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.netconf.mdsal.connector;
+
+import com.google.common.annotations.Beta;
+import com.google.common.util.concurrent.CheckedFuture;
+import org.opendaylight.controller.md.sal.dom.api.DOMDataBroker;
+import org.opendaylight.controller.md.sal.dom.api.DOMDataBrokerExtension;
+import org.opendaylight.controller.md.sal.dom.api.DOMDataWriteTransaction;
+import org.opendaylight.controller.md.sal.dom.api.DOMServiceExtension;
+import org.opendaylight.yangtools.yang.common.OperationFailedException;
+import org.opendaylight.yangtools.yang.common.RpcError;
+import org.opendaylight.yangtools.yang.common.RpcResultBuilder;
+
+/**
+ * A {@link DOMServiceExtension} which allows users to provide Validate capability for {@link DOMDataBroker}.
+ *
+ * <p> See <a href="https://tools.ietf.org/html/rfc4741#section-8.6">RFC4741 section 8.6</a> for details.
+ */
+@Beta
+public interface DOMDataTransactionValidator extends DOMDataBrokerExtension {
+    /**
+     * Validates state of the data tree associated with the provided {@link DOMDataWriteTransaction}.
+     *
+     * <p>The operation should not have any side-effects on the transaction state.
+     *
+     * <p>It can be executed many times, providing the same results if the state of the transaction has not been
+     * changed.
+     *
+     * @param transaction
+     *     transaction to be validated
+     * @return
+     *     a CheckedFuture containing the result of the validate operation. The future blocks until the validation
+     *     operation is complete. A successful validate returns nothing. On failure, the Future will fail
+     *     with a {@link ValidationFailedException} or an exception derived from ValidationFailedException.
+     */
+    CheckedFuture<Void, ValidationFailedException> validate(DOMDataWriteTransaction transaction);
+
+    /**
+     * Failed validation of asynchronous transaction. This exception is raised and returned when transaction validation
+     * failed.
+     */
+    class ValidationFailedException extends OperationFailedException {
+        private static final long serialVersionUID = 1L;
+
+        public ValidationFailedException(final String message, final Throwable cause) {
+            super(message, cause, RpcResultBuilder.newError(RpcError.ErrorType.APPLICATION, "invalid-value", message,
+                null, null, cause));
+        }
+
+        public ValidationFailedException(final String message) {
+            this(message, null);
+        }
+    }
+}
+
---
+++ netconf/mdsal-netconf-connector/src/main/java/org/opendaylight/netconf/mdsal/connector/ops/AbstractConfigOperation.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2018 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.netconf.mdsal.connector.ops;
+
+import com.google.common.base.Strings;
+import org.opendaylight.controller.config.util.xml.DocumentedException;
+import org.opendaylight.controller.config.util.xml.XmlElement;
+import org.opendaylight.netconf.util.mapping.AbstractSingletonNetconfOperation;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+
+abstract class AbstractConfigOperation extends AbstractSingletonNetconfOperation {
+
+    protected AbstractConfigOperation(final String netconfSessionIdForReporting) {
+        super(netconfSessionIdForReporting);
+    }
+
+    protected static NodeList getElementsByTagName(final XmlElement parent, final String key) throws
+        DocumentedException {
+        final Element domParent = parent.getDomElement();
+        final NodeList elementsByTagName;
+
+        if (Strings.isNullOrEmpty(domParent.getPrefix())) {
+            elementsByTagName = domParent.getElementsByTagName(key);
+        } else {
+            elementsByTagName = domParent.getElementsByTagNameNS(parent.getNamespace(), key);
+        }
+
+        return elementsByTagName;
+    }
+}
