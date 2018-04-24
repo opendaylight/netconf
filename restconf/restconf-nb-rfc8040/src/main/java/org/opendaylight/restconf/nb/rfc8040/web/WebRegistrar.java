376aa290821b8df3ad4d192724ec2fabdeb646bd
Tom Pantelis <tompantelis@gmail.com>
2018-04-24 14:48:30 -0400

    Convert rcf8040 from web.xml to programmtic web API
    To simplify, the noauth feature was removed. Since this is
    a new API, it's been decided not to carry it forward. The
    noauth feature for the bierman02 implementation was put in
    and kept for legacy reasons.
    
    Change-Id: Ic432b175fae62caee8cc641bb77c19f38a5ab6f6
    Signed-off-by: Tom Pantelis <tompantelis@gmail.com>

---
+++ restconf/restconf-nb-rfc8040/src/main/java/org/opendaylight/restconf/nb/rfc8040/web/WebRegistrar.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright (c) 2018 Inocybe Technologies and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.restconf.nb.rfc8040.web;
+
+import javax.servlet.ServletException;
+import javax.ws.rs.core.Application;
+import org.opendaylight.aaa.filterchain.configuration.CustomFilterAdapterConfiguration;
+import org.opendaylight.aaa.filterchain.filters.CustomFilterAdapter;
+import org.opendaylight.aaa.web.FilterDetails;
+import org.opendaylight.aaa.web.ServletDetails;
+import org.opendaylight.aaa.web.WebContext;
+import org.opendaylight.aaa.web.WebContextBuilder;
+import org.opendaylight.aaa.web.WebContextRegistration;
+import org.opendaylight.aaa.web.WebContextSecurer;
+import org.opendaylight.aaa.web.WebServer;
+
+/**
+ * Registers the web application.
+ *
+ * @author Thomas Pantelis
+ */
+public class WebRegistrar {
+    private final WebContextRegistration registration;
+
+    public WebRegistrar(WebServer webServer,  WebContextSecurer webContextSecurer,
+            Application webApp, CustomFilterAdapterConfiguration customFilterAdapterConfig) throws ServletException {
+        WebContextBuilder webContextBuilder = WebContext.builder().contextPath("restconf").supportsSessions(true)
+                .addServlet(ServletDetails.builder().servlet(
+                        new com.sun.jersey.spi.container.servlet.ServletContainer(webApp))
+                    .addUrlPattern("/*").build())
+
+                // Allows user to add javax.servlet.Filter(s) in front of REST services
+                .addFilter(FilterDetails.builder().filter(new CustomFilterAdapter(customFilterAdapterConfig))
+                    .addUrlPattern("/*").build())
+
+                .addFilter(FilterDetails.builder().filter(new org.eclipse.jetty.servlets.GzipFilter())
+                    .putInitParam("mimeTypes",
+                        "application/xml,application/yang.data+xml,xml,application/json,application/yang.data+json")
+                    .addUrlPattern("/*").build());
+
+        webContextSecurer.requireAuthentication(webContextBuilder, "/*");
+
+        registration = webServer.registerWebContext(webContextBuilder.build());
+    }
+
+    public void close() {
+        if (registration != null) {
+            registration.close();
+        }
+    }
+}
