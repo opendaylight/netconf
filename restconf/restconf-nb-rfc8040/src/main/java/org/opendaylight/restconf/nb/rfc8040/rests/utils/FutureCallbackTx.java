c5565d8b4a9659da5e5c9e40a5dcfc8e48935dff
miroslav.kovac <miroslav.kovac@pantheon.tech>
2018-05-14 10:34:11 +0200

    NETCONF-542: PUT request return 500 if operational data are used
    Change-Id: I5e45b0276e39c3a6af647b6632b67b3f96751464
    Signed-off-by: miroslav.kovac <miroslav.kovac@pantheon.tech>

--- restconf/restconf-nb-rfc8040/src/main/java/org/opendaylight/restconf/nb/rfc8040/rests/utils/FutureCallbackTx.java
+++ restconf/restconf-nb-rfc8040/src/main/java/org/opendaylight/restconf/nb/rfc8040/rests/utils/FutureCallbackTx.java
@@ -7,12 +7,16 @@
  */
 package org.opendaylight.restconf.nb.rfc8040.rests.utils;
 
+import com.google.common.base.Throwables;
 import com.google.common.util.concurrent.CheckedFuture;
 import java.util.ArrayList;
 import java.util.List;
+import org.opendaylight.controller.md.sal.common.api.data.TransactionCommitFailedException;
 import org.opendaylight.controller.md.sal.dom.api.DOMRpcException;
 import org.opendaylight.controller.md.sal.dom.spi.DefaultDOMRpcResult;
+import org.opendaylight.netconf.api.NetconfDocumentedException;
 import org.opendaylight.restconf.common.errors.RestconfDocumentedException;
+import org.opendaylight.restconf.common.errors.RestconfError;
 import org.opendaylight.yangtools.yang.common.RpcError;
 import org.opendaylight.yangtools.yang.common.RpcResultBuilder;
 import org.slf4j.Logger;
@@ -58,9 +62,26 @@ final class FutureCallbackTx {
                 rpcErrorList.add(
                         RpcResultBuilder.newError(RpcError.ErrorType.RPC, "operation-failed", e.getMessage()));
                 dataFactory.setResult((T) new DefaultDOMRpcResult(rpcErrorList));
+            } else if (e instanceof TransactionCommitFailedException) {
+                /* If device send some error message we want this message to get to client
+                   and not just to throw it away or override it with new generic message.
+                   We search for NetconfDocumentedException that was send from netconfSB
+                   and we create RestconfDocumentedException accordingly.
+                */
+                final List<Throwable> causalChain = Throwables.getCausalChain(e);
+                for (Throwable error : causalChain) {
+                    if (error instanceof NetconfDocumentedException) {
+                        throw new RestconfDocumentedException(error.getMessage(),
+                                RestconfError.ErrorType.valueOfCaseInsensitive(
+                                        ((NetconfDocumentedException) error).getErrorType().getTypeValue()),
+                                RestconfError.ErrorTag.valueOfCaseInsensitive(
+                                        ((NetconfDocumentedException) error).getErrorTag().getTagValue()), e);
+                    }
+                }
+
+                throw new RestconfDocumentedException("Transaction(" + txType + ") not committed correctly", e);
             } else {
-                throw new RestconfDocumentedException(
-                        "Transaction(" + txType + ") not committed correctly", e);
+                throw new RestconfDocumentedException("Transaction failed", e);
             }
         }
     }
