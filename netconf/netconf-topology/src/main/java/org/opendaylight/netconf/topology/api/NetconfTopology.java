e3b143f15982018b8237a9448a30feb14e465e13
Robert Varga <robert.varga@pantheon.tech>
2018-07-02 20:24:11 +0200

    Migrate to use ietf-network
    This patch migrates netconf to use ietf-network instead of draft
    ietf-topology -- thus using a standard model.
    
    JIRA: NETCONF-550
    Change-Id: I3b1db04a84bd871e180964630b6dcbfb6df4c64a
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- netconf/netconf-topology/src/main/java/org/opendaylight/netconf/topology/api/NetconfTopology.java
+++ netconf/netconf-topology/src/main/java/org/opendaylight/netconf/topology/api/NetconfTopology.java
@@ -10,8 +10,8 @@ package org.opendaylight.netconf.topology.api;
 
 import com.google.common.util.concurrent.ListenableFuture;
 import org.opendaylight.netconf.sal.connect.netconf.listener.NetconfDeviceCapabilities;
-import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.NodeId;
-import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.Node;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.network.rev180226.NodeId;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.network.rev180226.networks.network.Node;
 
 public interface NetconfTopology {
 
