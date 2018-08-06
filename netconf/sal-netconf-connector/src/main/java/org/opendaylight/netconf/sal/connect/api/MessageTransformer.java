142ab2fd6b02bf0d842cf65fb96dbcbd3ed44552
Jakub Tóth <jakub.toth@pantheon.tech>
2018-08-06 16:11:29 +0200

    Prepare netconf to support YANG 1.1 actions
    This allows implementation of a factory for creating of DOMActionService
    of a connected device. This service is going to be part of services provided
    by the device's mountpoint.
    
    Change-Id: I1c79b4bc8f1b1c63f7ffb2306f07ea9872f15882
    Signed-off-by: Jakub Tóth <jakub.toth@pantheon.tech>
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- netconf/sal-netconf-connector/src/main/java/org/opendaylight/netconf/sal/connect/api/MessageTransformer.java
+++ netconf/sal-netconf-connector/src/main/java/org/opendaylight/netconf/sal/connect/api/MessageTransformer.java
@@ -9,6 +9,8 @@ package org.opendaylight.netconf.sal.connect.api;
 
 import org.opendaylight.controller.md.sal.dom.api.DOMNotification;
 import org.opendaylight.controller.md.sal.dom.api.DOMRpcResult;
+import org.opendaylight.mdsal.dom.api.DOMActionResult;
+import org.opendaylight.mdsal.dom.api.DOMDataTreeIdentifier;
 import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNode;
 import org.opendaylight.yangtools.yang.model.api.SchemaPath;
 
@@ -20,4 +22,27 @@ public interface MessageTransformer<M> {
 
     DOMRpcResult toRpcResult(M message, SchemaPath rpc);
 
+    /**
+     * Parse action into message for request.
+     *
+     * @param action - action schema path
+     * @param domDataTreeIdentifier - identifier of action
+     * @param payload - input of action
+     * @return message
+     */
+    default M toActionRequest(SchemaPath action, DOMDataTreeIdentifier domDataTreeIdentifier, NormalizedNode<?,
+            ?> payload) {
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * Parse result of invoking action into DOM result.
+     *
+     * @param action - action schema path
+     * @param message - message to parsing
+     * @return {@link DOMActionResult}
+     */
+    default DOMActionResult toActionResult(SchemaPath action, M message) {
+        throw new UnsupportedOperationException();
+    }
 }
