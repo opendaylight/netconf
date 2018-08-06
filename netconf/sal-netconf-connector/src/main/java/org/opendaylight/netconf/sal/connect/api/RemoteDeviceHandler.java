fc71e17ca46e029ad6535613498842ff2854868d
Jakub Tóth <jakub.toth@pantheon.tech>
2018-08-06 16:11:29 +0200

    Prepare netconf to support YANG 1.1 actions
    This allows implementation of a factory for creating of DOMActionService
    of a connected device. This service is going to be part of services provided
    by the device's mountpoint.
    
    Change-Id: I1c79b4bc8f1b1c63f7ffb2306f07ea9872f15882
    Signed-off-by: Jakub Tóth <jakub.toth@pantheon.tech>
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- netconf/sal-netconf-connector/src/main/java/org/opendaylight/netconf/sal/connect/api/RemoteDeviceHandler.java
+++ netconf/sal-netconf-connector/src/main/java/org/opendaylight/netconf/sal/connect/api/RemoteDeviceHandler.java
@@ -7,14 +7,37 @@
  */
 package org.opendaylight.netconf.sal.connect.api;
 
+import org.opendaylight.controller.md.sal.dom.api.DOMActionService;
 import org.opendaylight.controller.md.sal.dom.api.DOMNotification;
 import org.opendaylight.controller.md.sal.dom.api.DOMRpcService;
 import org.opendaylight.yangtools.yang.model.api.SchemaContext;
 
 public interface RemoteDeviceHandler<PREF> extends AutoCloseable {
 
-    void onDeviceConnected(SchemaContext remoteSchemaContext,
-                           PREF netconfSessionPreferences, DOMRpcService deviceRpc);
+    /**
+     * When device connected, init new mount point with specific schema context and DOM services.
+     *
+     * @param remoteSchemaContext - schema context of connected device
+     * @param netconfSessionPreferences - session of device
+     * @param deviceRpc - {@link DOMRpcService} of device
+     */
+    default void onDeviceConnected(SchemaContext remoteSchemaContext, PREF netconfSessionPreferences,
+            DOMRpcService deviceRpc) {
+        // DO NOTHING
+    }
+
+    /**
+     * When device connected, init new mount point with specific schema context and DOM services.
+     *
+     * @param remoteSchemaContext - schema context of connected device
+     * @param netconfSessionPreferences - session of device
+     * @param deviceRpc - {@link DOMRpcService} of device
+     * @param deviceAction - {@link DOMActionService} of device
+     */
+    default void onDeviceConnected(SchemaContext remoteSchemaContext, PREF netconfSessionPreferences,
+            DOMRpcService deviceRpc, DOMActionService deviceAction) {
+        // DO NOTHING
+    }
 
     void onDeviceDisconnected();
 
