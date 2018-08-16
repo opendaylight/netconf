f7f26f5cdaabda847dd8785659ab5a7dd5e2e656
Jakub Morvay <jmorvay@frinx.io>
2018-08-16 14:02:02 +0200

    Get rid of Netty's DelimiterBasedFrameDecoder
    Netty's DelimiterBasedFrameDecoder is not really effective when dealing
    with huge NETCONF messages and reading them in just really small parts
    at a time.
    
    DelimiterBasedFrameDecoder always searches whole input ByteBuf for
    delimiter when new input bytes are available. It performs the search
    also on bytes already checked in previous tries. Obviously, this can be
    really ineffective when reading large messages by very small parts at
    a time.
    
    Replace Netty's DelimiterBasedFrameDecoder by our own frame detection
    implementation. The implementation remembers already checked part of
    the input ByteBuf and searches the delimiter just in a new bytes
    available.
    
    JIRA: NETCONF-889
    Change-Id: If5e78f4373d767f4cf465024313eeac873c1621d
    Signed-off-by: Jakub Morvay <jmorvay@frinx.io>
    Signed-off-by: Maros Marsalek <mmarsalek@frinx.io>
    Signed-off-by: Sangwook Ha <sangwook.ha@verizon.com>

--- netconf/netconf-netty-util/src/main/java/org/opendaylight/netconf/nettyutil/handler/NetconfEOMAggregator.java
+++ netconf/netconf-netty-util/src/main/java/org/opendaylight/netconf/nettyutil/handler/NetconfEOMAggregator.java
@@ -5,18 +5,81 @@
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
-
 package org.opendaylight.netconf.nettyutil.handler;
+
+import static com.google.common.base.Verify.verify;
 
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
-import io.netty.handler.codec.DelimiterBasedFrameDecoder;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.ByteToMessageDecoder;
+import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-public class NetconfEOMAggregator extends DelimiterBasedFrameDecoder {
+public final class NetconfEOMAggregator extends ByteToMessageDecoder {
 
-    public static final ByteBuf DELIMITER = Unpooled.wrappedBuffer(MessageParts.END_OF_MESSAGE);
+    private static final Logger LOG = LoggerFactory.getLogger(NetconfEOMAggregator.class);
+    private static final ByteBuf DELIMITER = Unpooled.wrappedBuffer(MessageParts.END_OF_MESSAGE);
+    private static final int DELIMITER_LENGTH = DELIMITER.capacity();
 
-    public NetconfEOMAggregator() {
-        super(Integer.MAX_VALUE, DELIMITER);
+    private int nextByteToCheck = -1;
+    private int bytesConsumed = 0;
+
+    @Override
+    protected void decode(final ChannelHandlerContext ctx, final ByteBuf in, final List<Object> out) {
+        if (bytesConsumed != nextByteToCheck - in.readerIndex()) {
+            LOG.debug("Buffer reset: in {}, ctx {}, nextByteToCheck {}, bytesConsumed {}",
+                    in, ctx, nextByteToCheck, bytesConsumed);
+            // readerIndex is reset or the method is called first time. Adjust nextByteToCheck.
+            nextByteToCheck = in.readerIndex() + bytesConsumed;
+        }
+        verify(nextByteToCheck >= in.readerIndex(), "nextByteToCheck (%s) is smaller than readerIndex (%s).",
+                nextByteToCheck, in.readerIndex());
+
+        final int frameLength = indexOfDelimiter(in);
+
+        if (frameLength > 0) {
+            LOG.debug("Frame detected: length {}, in {}", frameLength, in);
+            final ByteBuf frame = in.readSlice(frameLength);
+            in.skipBytes(DELIMITER_LENGTH);
+            out.add(frame.retain());
+            bytesConsumed = 0;
+        } else {
+            bytesConsumed = nextByteToCheck - in.readerIndex();
+        }
+    }
+
+    private int indexOfDelimiter(final ByteBuf in) {
+        for (int i = nextByteToCheck; i < in.writerIndex(); i++) {
+            int inputBufIndex = i;
+            int delimiterIndex;
+            for (delimiterIndex = 0; delimiterIndex < DELIMITER_LENGTH; delimiterIndex++) {
+                if (in.getByte(inputBufIndex) != DELIMITER.getByte(delimiterIndex)) {
+                    break;
+                } else {
+                    inputBufIndex++;
+                    if (inputBufIndex == in.writerIndex() && delimiterIndex != DELIMITER_LENGTH - 1) {
+                        // Delimiter pattern matched up to (delimiterIndex + 1) bytes,
+                        // but reached the end of the available data, so next search should
+                        // start from the current index.
+                        nextByteToCheck = i;
+                        return -1;
+                    }
+                }
+            }
+
+            if (delimiterIndex == DELIMITER_LENGTH) {
+                // Found the delimiter in the input buffer!
+                // Next search should start after skipping the delimiter.
+                nextByteToCheck = i + DELIMITER_LENGTH;
+                return i - in.readerIndex();
+            }
+        }
+
+        // Delimiter was not found and there is no partial match at the end. So next search
+        // can start from writerIndex.
+        nextByteToCheck = in.writerIndex();
+        return -1;
     }
 }
