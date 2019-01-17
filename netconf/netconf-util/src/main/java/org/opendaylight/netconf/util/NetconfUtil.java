0bbc712293c31ae2e0b1d940bd6fd776483ee4b1
Robert Varga <robert.varga@pantheon.tech>
2019-01-17 13:05:32 +0100

    Rework NetconfUtil.writeNormalizedNode()
    This refactors existing methods to support writing raw data into
    either a Document or an Element, without exposing JAXP details, like
    XMLStreamException and DOMResult to the callers.
    
    Also create a specialized createAnyxmlNode, which creates an
    element bound to specified document and fills it with specified
    normalized node and encapsulates it into an AnyXmlNode.
    
    JIRA: NETCONF-603
    Change-Id: Icff5e045c4e901bb53990177d448a1ea43952c62
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- netconf/netconf-util/src/main/java/org/opendaylight/netconf/util/NetconfUtil.java
+++ netconf/netconf-util/src/main/java/org/opendaylight/netconf/util/NetconfUtil.java
@@ -7,15 +7,23 @@
  */
 package org.opendaylight.netconf.util;
 
-import com.google.common.base.Preconditions;
+import static com.google.common.base.Preconditions.checkArgument;
+import static com.google.common.base.Preconditions.checkState;
+import static java.util.Objects.requireNonNull;
+
+import com.google.common.annotations.Beta;
+import com.google.common.base.Strings;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.util.Iterator;
+import java.util.Map.Entry;
+import javax.xml.XMLConstants;
 import javax.xml.stream.XMLOutputFactory;
 import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.XMLStreamWriter;
 import javax.xml.transform.dom.DOMResult;
 import javax.xml.transform.dom.DOMSource;
+import org.eclipse.jdt.annotation.NonNull;
 import org.eclipse.jdt.annotation.Nullable;
 import org.opendaylight.netconf.api.DocumentedException;
 import org.opendaylight.netconf.api.xml.XmlElement;
@@ -25,13 +33,16 @@ import org.opendaylight.yangtools.rfc7952.data.api.NormalizedMetadata;
 import org.opendaylight.yangtools.rfc7952.data.util.NormalizedMetadataWriter;
 import org.opendaylight.yangtools.yang.common.QName;
 import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.NodeIdentifier;
 import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.PathArgument;
+import org.opendaylight.yangtools.yang.data.api.schema.AnyXmlNode;
 import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNode;
 import org.opendaylight.yangtools.yang.data.api.schema.stream.NormalizedNodeStreamWriter;
 import org.opendaylight.yangtools.yang.data.api.schema.stream.NormalizedNodeWriter;
 import org.opendaylight.yangtools.yang.data.codec.xml.XMLStreamNormalizedNodeStreamWriter;
 import org.opendaylight.yangtools.yang.data.codec.xml.XmlCodecFactory;
 import org.opendaylight.yangtools.yang.data.codec.xml.XmlParserStream;
+import org.opendaylight.yangtools.yang.data.impl.schema.Builders;
 import org.opendaylight.yangtools.yang.data.impl.schema.ImmutableNormalizedNodeStreamWriter;
 import org.opendaylight.yangtools.yang.data.impl.schema.NormalizedNodeResult;
 import org.opendaylight.yangtools.yang.model.api.ContainerSchemaNode;
@@ -40,6 +51,8 @@ import org.opendaylight.yangtools.yang.model.api.SchemaPath;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
 import org.xml.sax.SAXException;
 
 public final class NetconfUtil {
@@ -60,7 +73,7 @@ public final class NetconfUtil {
 
     public static Document checkIsMessageOk(final Document response) throws DocumentedException {
         XmlElement element = XmlElement.fromDomDocument(response);
-        Preconditions.checkState(element.getName().equals(XmlNetconfConstants.RPC_REPLY_KEY));
+        checkState(element.getName().equals(XmlNetconfConstants.RPC_REPLY_KEY));
         element = element.getOnlyChildElement();
         if (element.getName().equals(XmlNetconfConstants.OK)) {
             return response;
@@ -70,27 +83,125 @@ public final class NetconfUtil {
                 + XmlUtil.toString(response));
     }
 
-    @SuppressWarnings("checkstyle:IllegalCatch")
-    public static void writeNormalizedNode(final NormalizedNode<?, ?> normalized, final DOMResult result,
-                                           final SchemaPath schemaPath, final SchemaContext context)
-            throws IOException, XMLStreamException {
-        final XMLStreamWriter writer = XML_FACTORY.createXMLStreamWriter(result);
+    /**
+     * Create an {@link AnyXmlNode} containing the content of a {@link NormalizedNode} structure.
+     *
+     * @param document W3C DOM document to use for creating nodes
+     * @param nodeId NodeIdentifier of resulting AnyXmlNode
+     * @param data Data to write
+     * @param context SchemaContext to use for encoding
+     * @param schemaPath Schema root path
+     * @throws IOException when the writing process fails
+     * @throws NullPointerException if any of the arguments is null
+     */
+    @Beta
+    public static @NonNull AnyXmlNode createAnyxmlNode(final Document document, final NodeIdentifier nodeId,
+            final NormalizedNode<?, ?> data, final SchemaContext context, final SchemaPath schemaPath)
+                    throws IOException {
+        return createElement(document, nodeId, data, context, schemaPath, null);
+    }
+
+    /**
+     * Create an {@link AnyXmlNode} containing the content of a {@link NormalizedNode} structure.
+     *
+     * @param document W3C DOM document to use for creating nodes
+     * @param nodeId NodeIdentifier of resulting AnyXmlNode
+     * @param data Data to write
+     * @param context SchemaContext to use for encoding
+     * @param schemaPath Schema root path
+     * @throws IOException when the writing process fails
+     * @throws NullPointerException if any of the arguments is null
+     */
+    @Beta
+    public static @NonNull AnyXmlNode createAnyxmlNode(final Document document, final NodeIdentifier nodeId,
+            final NormalizedNode<?, ?> data, final SchemaContext context, final SchemaPath schemaPath,
+            final AnyXmlNamespaceContext namespaceContext) throws IOException {
+        return createElement(document, nodeId, data, context, schemaPath, requireNonNull(namespaceContext));
+    }
+
+    /**
+     * Write the content of a {@link NormalizedNode} structure into a W3C DOM {@link Node}. This process creates new
+     * nodes underneath the target node -- which can be either a {@link Document} or a Node itself.
+     *
+     * @param output Target node
+     * @param data Data to write
+     * @param context SchemaContext to use for encoding
+     * @param schemaPath Schema root path
+     * @throws IOException when the writing process fails
+     * @throws NullPointerException if any of the arguments is null
+     */
+    @Beta
+    public static void writeNormalizedNode(final Node output, final NormalizedNode<?, ?> data,
+            final SchemaContext context, final SchemaPath schemaPath) throws IOException {
+        final DOMResult result = new DOMResult(requireNonNull(output));
+        try {
+            XMLStreamWriter writer = XML_FACTORY.createXMLStreamWriter(result);
+            try {
+                writeNormalizedNode(writer, data, context, schemaPath);
+            } finally {
+                writer.close();
+            }
+        } catch (XMLStreamException e) {
+            throw new IOException("Failed to write node", e);
+        }
+    }
+
+    private static void writeNormalizedNode(final XMLStreamWriter writer, final NormalizedNode<?, ?> normalized,
+            final SchemaContext context, final SchemaPath schemaPath) throws IOException {
         try (
-             NormalizedNodeStreamWriter normalizedNodeStreamWriter =
-                     XMLStreamNormalizedNodeStreamWriter.create(writer, context, schemaPath);
-             NormalizedNodeWriter normalizedNodeWriter =
-                     NormalizedNodeWriter.forStreamWriter(normalizedNodeStreamWriter)
-        ) {
+                NormalizedNodeStreamWriter normalizedNodeStreamWriter = XMLStreamNormalizedNodeStreamWriter.create(
+                    writer, context, schemaPath);
+                NormalizedNodeWriter normalizedNodeWriter = NormalizedNodeWriter.forStreamWriter(
+                    normalizedNodeStreamWriter)) {
             normalizedNodeWriter.write(normalized);
             normalizedNodeWriter.flush();
-        } finally {
+        }
+    }
+
+    private static @NonNull AnyXmlNode createElement(final Document document, final NodeIdentifier nodeId,
+            final NormalizedNode<?, ?> data, final SchemaContext context, final SchemaPath schemaPath,
+            final @Nullable AnyXmlNamespaceContext namespaceContext) throws IOException {
+        final QName qname = nodeId.getNodeType();
+        final String elementNs = qname.getNamespace().toString();
+        final Element element = document.createElementNS(elementNs, qname.getLocalName());
+        element.setAttributeNS(XMLConstants.XMLNS_ATTRIBUTE_NS_URI, XMLConstants.XMLNS_ATTRIBUTE, elementNs);
+
+        final DOMResult result = new DOMResult(element);
+        try {
+            final XMLStreamWriter writer = XML_FACTORY.createXMLStreamWriter(result);
             try {
-                if (writer != null) {
-                    writer.close();
+                writer.setDefaultNamespace(elementNs);
+                if (namespaceContext != null) {
+                    applyNamespaceContext(element, writer, namespaceContext);
                 }
-            } catch (final Exception e) {
-                LOG.warn("Unable to close resource properly", e);
+
+                writeNormalizedNode(writer, data, context, schemaPath);
+            } finally {
+                writer.close();
             }
+        } catch (XMLStreamException e) {
+            throw new IOException("Failed to write node", e);
+        }
+
+        return Builders.anyXmlBuilder().withNodeIdentifier(nodeId).withValue(new DOMSource(element)).build();
+    }
+
+    private static void applyNamespaceContext(final Element element, final XMLStreamWriter writer,
+            final AnyXmlNamespaceContext namespaceContext) throws XMLStreamException {
+        for (Entry<String, String> entry : namespaceContext.uriPrefixEntries()) {
+            final String namespaceURI = entry.getKey();
+            final String prefix = entry.getValue();
+            checkArgument(!Strings.isNullOrEmpty(prefix));
+            element.setAttributeNS(XMLConstants.XMLNS_ATTRIBUTE_NS_URI, XMLConstants.XMLNS_ATTRIBUTE + ':' + prefix,
+                namespaceURI);
+            writer.setPrefix(prefix, namespaceURI);
+        }
+
+        try {
+            writer.setNamespaceContext(namespaceContext);
+        } catch (UnsupportedOperationException e) {
+            // Java DOM writer is helpful this way
+            LOG.debug("Initial namespace context not set", e);
         }
     }
 
