2c83539d73b7ca54cc5f3f5f721fad4111aeaabf
Jakub Morvay <jmorvay@frinx.io>
2019-01-18 14:19:43 +0100

    Remove netconf-notification
    Change-Id: I3f2968623295beff9bcfbce6aa48c82532ef1502
    Signed-off-by: Jakub Morvay <jmorvay@frinx.io>

---
+++ netconf/mdsal-netconf-notification/src/main/java/org/opendaylight/netconf/mdsal/notification/impl/CapabilityChangeNotificationProducer.java
@@ -0,0 +1,110 @@
+/*
+ * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.netconf.mdsal.notification.impl;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.Sets;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Set;
+import javax.annotation.Nonnull;
+import org.opendaylight.mdsal.binding.api.DataBroker;
+import org.opendaylight.mdsal.binding.api.DataObjectModification;
+import org.opendaylight.mdsal.binding.api.DataTreeModification;
+import org.opendaylight.netconf.notifications.BaseNotificationPublisherRegistration;
+import org.opendaylight.netconf.notifications.NetconfNotificationCollector;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev130715.Uri;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.netconf.monitoring.rev101004.NetconfState;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.netconf.monitoring.rev101004.netconf.state.Capabilities;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.netconf.notifications.rev120206.NetconfCapabilityChangeBuilder;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.netconf.notifications.rev120206.changed.by.parms.ChangedByBuilder;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.netconf.notifications.rev120206.changed.by.parms.changed.by.server.or.user.ServerBuilder;
+import org.opendaylight.yangtools.concepts.ListenerRegistration;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Listens on capabilities changes in data store and publishes them to base
+ * netconf notification stream listener.
+ */
+public final class CapabilityChangeNotificationProducer extends OperationalDatastoreListener<Capabilities>
+    implements AutoCloseable {
+
+    private static final InstanceIdentifier<Capabilities> CAPABILITIES_INSTANCE_IDENTIFIER =
+            InstanceIdentifier.create(NetconfState.class).child(Capabilities.class);
+    private static final Logger LOG = LoggerFactory.getLogger(CapabilityChangeNotificationProducer.class);
+
+    private final BaseNotificationPublisherRegistration baseNotificationPublisherRegistration;
+    private final ListenerRegistration capabilityChangeListenerRegistration;
+
+    public CapabilityChangeNotificationProducer(final NetconfNotificationCollector netconfNotificationCollector,
+                                                final DataBroker dataBroker) {
+        super(CAPABILITIES_INSTANCE_IDENTIFIER);
+        this.baseNotificationPublisherRegistration = netconfNotificationCollector.registerBaseNotificationPublisher();
+        this.capabilityChangeListenerRegistration = registerOnChanges(dataBroker);
+    }
+
+    @Override
+    public void onDataTreeChanged(@Nonnull final Collection<DataTreeModification<Capabilities>> changes) {
+        for (DataTreeModification<Capabilities> change : changes) {
+            final DataObjectModification<Capabilities> rootNode = change.getRootNode();
+            final DataObjectModification.ModificationType modificationType = rootNode.getModificationType();
+            switch (modificationType) {
+                case WRITE: {
+                    final Capabilities dataAfter = rootNode.getDataAfter();
+                    final Capabilities dataBefore = rootNode.getDataBefore();
+                    final Set<Uri> before = dataBefore != null ? ImmutableSet.copyOf(dataBefore.getCapability()) :
+                            Collections.emptySet();
+                    final Set<Uri> after = dataAfter != null ? ImmutableSet.copyOf(dataAfter.getCapability()) :
+                            Collections.emptySet();
+                    final Set<Uri> added = Sets.difference(after, before);
+                    final Set<Uri> removed = Sets.difference(before, after);
+                    publishNotification(added, removed);
+                    break;
+                }
+                case DELETE: {
+                    final Capabilities dataBeforeDelete = rootNode.getDataBefore();
+                    if (dataBeforeDelete != null) {
+                        final Set<Uri> removed = ImmutableSet.copyOf(dataBeforeDelete.getCapability());
+                        publishNotification(Collections.emptySet(), removed);
+                    }
+                    break;
+                }
+                default:
+                    LOG.debug("Received intentionally unhandled type: {}.", modificationType);
+            }
+        }
+
+    }
+
+    private void publishNotification(final Set<Uri> added, final Set<Uri> removed) {
+        final NetconfCapabilityChangeBuilder netconfCapabilityChangeBuilder = new NetconfCapabilityChangeBuilder();
+        netconfCapabilityChangeBuilder.setChangedBy(new ChangedByBuilder().setServerOrUser(new ServerBuilder()
+                .setServer(true).build()).build());
+        netconfCapabilityChangeBuilder.setAddedCapability(ImmutableList.copyOf(added));
+        netconfCapabilityChangeBuilder.setDeletedCapability(ImmutableList.copyOf(removed));
+        // TODO modified should be computed ... but why ?
+        netconfCapabilityChangeBuilder.setModifiedCapability(Collections.emptyList());
+        baseNotificationPublisherRegistration.onCapabilityChanged(netconfCapabilityChangeBuilder.build());
+    }
+
+    /**
+     * Invoked by blueprint.
+     */
+    @Override
+    public void close() {
+        if (baseNotificationPublisherRegistration != null) {
+            baseNotificationPublisherRegistration.close();
+        }
+        if (capabilityChangeListenerRegistration != null) {
+            capabilityChangeListenerRegistration.close();
+        }
+    }
+}
---
+++ netconf/mdsal-netconf-notification/src/test/java/org/opendaylight/netconf/mdsal/notification/impl/CapabilityChangeNotificationProducerTest.java
@@ -0,0 +1,136 @@
+/*
+ * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.netconf.mdsal.notification.impl;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.opendaylight.mdsal.binding.api.DataBroker;
+import org.opendaylight.mdsal.binding.api.DataObjectModification;
+import org.opendaylight.mdsal.binding.api.DataTreeChangeListener;
+import org.opendaylight.mdsal.binding.api.DataTreeIdentifier;
+import org.opendaylight.mdsal.binding.api.DataTreeModification;
+import org.opendaylight.netconf.notifications.BaseNotificationPublisherRegistration;
+import org.opendaylight.netconf.notifications.NetconfNotificationCollector;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev130715.Uri;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.netconf.monitoring.rev101004.NetconfState;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.netconf.monitoring.rev101004.netconf.state.Capabilities;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.netconf.monitoring.rev101004.netconf.state.CapabilitiesBuilder;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.netconf.notifications.rev120206.NetconfCapabilityChange;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.netconf.notifications.rev120206.NetconfCapabilityChangeBuilder;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.netconf.notifications.rev120206.changed.by.parms.ChangedByBuilder;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.netconf.notifications.rev120206.changed.by.parms.changed.by.server.or.user.ServerBuilder;
+import org.opendaylight.yangtools.concepts.ListenerRegistration;
+import org.opendaylight.yangtools.yang.binding.DataObject;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+
+public class CapabilityChangeNotificationProducerTest {
+
+    private CapabilityChangeNotificationProducer capabilityChangeNotificationProducer;
+
+    @Mock
+    private BaseNotificationPublisherRegistration baseNotificationPublisherRegistration;
+    @Mock
+    private ListenerRegistration listenerRegistration;
+
+    @Mock
+    private NetconfNotificationCollector netconfNotificationCollector;
+    @Mock
+    private DataBroker dataBroker;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+
+        doReturn(listenerRegistration).when(dataBroker).registerDataTreeChangeListener(any(DataTreeIdentifier.class),
+                any(DataTreeChangeListener.class));
+
+        doNothing().when(baseNotificationPublisherRegistration).onCapabilityChanged(any(NetconfCapabilityChange.class));
+
+        doReturn(baseNotificationPublisherRegistration).when(netconfNotificationCollector)
+                .registerBaseNotificationPublisher();
+
+        capabilityChangeNotificationProducer = new CapabilityChangeNotificationProducer(netconfNotificationCollector,
+                dataBroker);
+    }
+
+    @Test
+    public void testOnDataChangedCreate() {
+        final InstanceIdentifier<Capabilities> capabilitiesIdentifier =
+                InstanceIdentifier.create(NetconfState.class).child(Capabilities.class);
+        final List<Uri> newCapabilitiesList =
+                Lists.newArrayList(new Uri("newCapability"), new Uri("createdCapability"));
+        Capabilities newCapabilities = new CapabilitiesBuilder().setCapability(newCapabilitiesList).build();
+        Map<InstanceIdentifier<?>, DataObject> createdData = Maps.newHashMap();
+        createdData.put(capabilitiesIdentifier, newCapabilities);
+        verifyDataTreeChange(DataObjectModification.ModificationType.WRITE, null, newCapabilities,
+                changedCapabilitesFrom(newCapabilitiesList, Collections.emptyList()));
+    }
+
+    @Test
+    public void testOnDataChangedUpdate() {
+        final List<Uri> originalCapabilitiesList =
+                Lists.newArrayList(new Uri("originalCapability"), new Uri("anotherOriginalCapability"));
+        final List<Uri> updatedCapabilitiesList =
+                Lists.newArrayList(new Uri("originalCapability"), new Uri("newCapability"));
+        Capabilities originalCapabilities = new CapabilitiesBuilder().setCapability(originalCapabilitiesList).build();
+        Capabilities updatedCapabilities = new CapabilitiesBuilder().setCapability(updatedCapabilitiesList).build();
+        verifyDataTreeChange(DataObjectModification.ModificationType.WRITE, originalCapabilities,
+                updatedCapabilities, changedCapabilitesFrom(
+                Lists.newArrayList(new Uri("newCapability")), Lists.newArrayList(new Uri("anotherOriginalCapability")
+                        )));
+    }
+
+    @Test
+    public void testOnDataChangedDelete() {
+        final List<Uri> originalCapabilitiesList = Lists.newArrayList(new Uri("originalCapability"),
+                new Uri("anotherOriginalCapability"));
+        final Capabilities originalCapabilities =
+                new CapabilitiesBuilder().setCapability(originalCapabilitiesList).build();
+        verifyDataTreeChange(DataObjectModification.ModificationType.DELETE, originalCapabilities, null,
+                changedCapabilitesFrom(Collections.emptyList(), originalCapabilitiesList));
+    }
+
+    @SuppressWarnings("unchecked")
+    private void verifyDataTreeChange(final DataObjectModification.ModificationType modificationType,
+                                      final Capabilities originalCapabilities, final Capabilities updatedCapabilities,
+                                      final NetconfCapabilityChange expectedChange) {
+        final DataTreeModification<Capabilities> treeChange2 = mock(DataTreeModification.class);
+        final DataObjectModification<Capabilities> objectChange2 = mock(DataObjectModification.class);
+        doReturn(modificationType).when(objectChange2).getModificationType();
+        doReturn(objectChange2).when(treeChange2).getRootNode();
+        doReturn(originalCapabilities).when(objectChange2).getDataBefore();
+        doReturn(updatedCapabilities).when(objectChange2).getDataAfter();
+        capabilityChangeNotificationProducer.onDataTreeChanged(Collections.singleton(treeChange2));
+        verify(baseNotificationPublisherRegistration).onCapabilityChanged(expectedChange);
+    }
+
+    private static NetconfCapabilityChange changedCapabilitesFrom(final List<Uri> added, final List<Uri> deleted) {
+        NetconfCapabilityChangeBuilder netconfCapabilityChangeBuilder = new NetconfCapabilityChangeBuilder();
+        netconfCapabilityChangeBuilder.setChangedBy(new ChangedByBuilder().setServerOrUser(
+                new ServerBuilder().setServer(true).build()).build());
+
+        netconfCapabilityChangeBuilder.setModifiedCapability(Collections.emptyList());
+        netconfCapabilityChangeBuilder.setAddedCapability(added);
+        netconfCapabilityChangeBuilder.setDeletedCapability(deleted);
+
+        return netconfCapabilityChangeBuilder.build();
+    }
+}
