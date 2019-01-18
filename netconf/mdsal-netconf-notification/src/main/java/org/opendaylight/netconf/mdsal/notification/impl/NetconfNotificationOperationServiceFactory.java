285a83a0ad1dcf597bfd715f21c73692e5ce58cc
Jakub Morvay <jmorvay@frinx.io>
2019-01-18 14:19:43 +0100

    Remove netconf-notification
    Change-Id: I3f2968623295beff9bcfbce6aa48c82532ef1502
    Signed-off-by: Jakub Morvay <jmorvay@frinx.io>

---
+++ netconf/mdsal-netconf-notification/src/main/java/org/opendaylight/netconf/mdsal/notification/impl/NetconfNotificationOperationServiceFactory.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.netconf.mdsal.notification.impl;
+
+import java.util.Collections;
+import java.util.Set;
+import org.opendaylight.controller.sal.common.util.NoopAutoCloseable;
+import org.opendaylight.netconf.api.capability.Capability;
+import org.opendaylight.netconf.api.monitoring.CapabilityListener;
+import org.opendaylight.netconf.mapping.api.NetconfOperationService;
+import org.opendaylight.netconf.mapping.api.NetconfOperationServiceFactory;
+import org.opendaylight.netconf.mapping.api.NetconfOperationServiceFactoryListener;
+import org.opendaylight.netconf.notifications.NetconfNotificationRegistry;
+
+public class NetconfNotificationOperationServiceFactory implements NetconfOperationServiceFactory, AutoCloseable {
+
+    private final NetconfNotificationRegistry netconfNotificationRegistry;
+    private final NetconfOperationServiceFactoryListener netconfOperationServiceFactoryListener;
+
+    public NetconfNotificationOperationServiceFactory(
+            final NetconfNotificationRegistry netconfNotificationRegistry,
+            final NetconfOperationServiceFactoryListener netconfOperationServiceFactoryListener) {
+        this.netconfNotificationRegistry = netconfNotificationRegistry;
+        this.netconfOperationServiceFactoryListener = netconfOperationServiceFactoryListener;
+
+        this.netconfOperationServiceFactoryListener.onAddNetconfOperationServiceFactory(this);
+    }
+
+    @Override
+    public Set<Capability> getCapabilities() {
+        // TODO
+        // No capabilities exposed to prevent clashes with schemas from
+        // config-netconf-connector (it exposes all the schemas)
+        // If the schemas exposed by config-netconf-connector are filtered,
+        // this class would expose monitoring related models
+        return Collections.emptySet();
+    }
+
+    @Override
+    public NetconfOperationService createService(String netconfSessionIdForReporting) {
+        return new NetconfNotificationOperationService(netconfSessionIdForReporting, netconfNotificationRegistry);
+    }
+
+    @Override
+    public AutoCloseable registerCapabilityListener(final CapabilityListener listener) {
+        return NoopAutoCloseable.INSTANCE;
+    }
+
+    @Override
+    public void close() {
+        this.netconfOperationServiceFactoryListener.onRemoveNetconfOperationServiceFactory(this);
+    }
+}
