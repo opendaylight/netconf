f365ea4493ba84d12d59103d9779344ca1c1b119
Tom Pantelis <tompantelis@gmail.com>
2019-01-28 11:45:00 -0500

    Move BP xml files to standard OSGI-INF/blueprint
    Change-Id: I879cabdb7591fccfbe22d84cd64d4cdae66a9cc8
    Signed-off-by: Tom Pantelis <tompantelis@gmail.com>

---
+++ netconf/netconf-impl/src/main/java/org/opendaylight/netconf/impl/ServerChannelInitializer.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2019 Red Hat, Inc. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.netconf.impl;
+
+import io.netty.channel.Channel;
+import io.netty.util.concurrent.Promise;
+import org.opendaylight.netconf.impl.util.DeserializerExceptionHandler;
+import org.opendaylight.netconf.nettyutil.AbstractChannelInitializer;
+
+public class ServerChannelInitializer extends AbstractChannelInitializer<NetconfServerSession> {
+
+    public static final String DESERIALIZER_EX_HANDLER_KEY = "deserializerExHandler";
+
+    private final NetconfServerSessionNegotiatorFactory negotiatorFactory;
+
+
+    public ServerChannelInitializer(NetconfServerSessionNegotiatorFactory negotiatorFactory) {
+        this.negotiatorFactory = negotiatorFactory;
+
+    }
+
+    @Override
+    protected void initializeMessageDecoder(Channel ch) {
+        super.initializeMessageDecoder(ch);
+        ch.pipeline().addLast(DESERIALIZER_EX_HANDLER_KEY, new DeserializerExceptionHandler());
+    }
+
+    @Override
+    protected void initializeSessionNegotiator(Channel ch, Promise<NetconfServerSession> promise) {
+        ch.pipeline().addAfter(DESERIALIZER_EX_HANDLER_KEY, AbstractChannelInitializer.NETCONF_SESSION_NEGOTIATOR,
+                negotiatorFactory.getSessionNegotiator(null, ch, promise));
+    }
+}
