32348315c9edd28c0c945ade1d1767bb71baa671
Jakub Morvay <jmorvay@frinx.io>
2019-01-30 20:19:54 +0100

    Initialize EXISchema grammars lazily
    Creating schema-backed netconf grammar is costly operation. Do not
    create it until we actually need it.
    
    This way we do not introduce any performance regression for netconf
    sessions that do not want to use schema-informed grammars for EXI
    encoding.
    
    JIRA: NETCONF-584
    Change-Id: I83f5e18f423cd21c1dc993980f42e030f36a12ed
    Signed-off-by: Jakub Morvay <jmorvay@frinx.io>
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- netconf/netconf-netty-util/src/main/java/org/opendaylight/netconf/nettyutil/handler/exi/EXISchema.java
+++ netconf/netconf-netty-util/src/main/java/org/opendaylight/netconf/nettyutil/handler/exi/EXISchema.java
@@ -9,10 +9,12 @@ package org.opendaylight.netconf.nettyutil.handler.exi;
 
 import static java.util.Objects.requireNonNull;
 
+import com.google.common.base.Suppliers;
 import com.google.common.io.ByteSource;
 import com.google.common.io.Resources;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.function.Supplier;
 import org.opendaylight.netconf.shaded.exificient.core.exceptions.EXIException;
 import org.opendaylight.netconf.shaded.exificient.core.grammars.Grammars;
 import org.opendaylight.netconf.shaded.exificient.grammars.GrammarFactory;
@@ -21,16 +23,42 @@ import org.opendaylight.netconf.shaded.exificient.grammars.GrammarFactory;
  * Enumeration of schema modes defined by the NETCONF EXI capability.
  */
 public enum EXISchema {
-    NONE("none", GrammarFactory.newInstance().createSchemaLessGrammars()),
-    BUILTIN("builtin", createBuiltinGrammar()),
-    BASE_1_1("base:1.1", createNetconfGrammar());
+    NONE("none") {
+        @Override
+        Grammars createGrammar() {
+            return GrammarFactory.newInstance().createSchemaLessGrammars();
+        }
+    },
+    BUILTIN("builtin") {
+        @Override
+        Grammars createGrammar() {
+            try {
+                return GrammarFactory.newInstance().createXSDTypesOnlyGrammars();
+            } catch (EXIException e) {
+                throw new IllegalStateException("Failed to create builtin grammar", e);
+            }
+        }
+    },
+    BASE_1_1("base:1.1") {
+        @Override
+        Grammars createGrammar() {
+            final ByteSource source = Resources.asByteSource(EXISchema.class.getResource("/rfc6241.xsd"));
+            try (InputStream is = source.openStream()) {
+                final Grammars g = GrammarFactory.newInstance().createGrammars(is);
+                g.setSchemaId(getOption());
+                return g;
+            } catch (EXIException | IOException e) {
+                throw new IllegalStateException("Failed to create RFC6241 grammar", e);
+            }
+        }
+    };
 
     private String option;
-    private Grammars grammar;
+    private Supplier<Grammars> grammarsSupplier;
 
-    EXISchema(final String option, final Grammars grammar) {
+    EXISchema(final String option) {
         this.option = requireNonNull(option);
-        this.grammar = requireNonNull(grammar);
+        this.grammarsSupplier = Suppliers.memoize(this::createGrammar);
     }
 
     final String getOption() {
@@ -38,8 +66,10 @@ public enum EXISchema {
     }
 
     final Grammars getGrammar() {
-        return grammar;
+        return grammarsSupplier.get();
     }
+
+    abstract Grammars createGrammar();
 
     static EXISchema forOption(final String id) {
         for (EXISchema s : EXISchema.values()) {
@@ -49,24 +79,5 @@ public enum EXISchema {
         }
 
         return null;
-    }
-
-    private static Grammars createNetconfGrammar() {
-        final ByteSource source = Resources.asByteSource(EXISchema.class.getResource("/rfc6241.xsd"));
-        try (InputStream is = source.openStream()) {
-            final Grammars g = GrammarFactory.newInstance().createGrammars(is);
-            g.setSchemaId("base:1.1");
-            return g;
-        } catch (EXIException | IOException e) {
-            throw new IllegalStateException("Failed to create RFC6241 grammar", e);
-        }
-    }
-
-    private static Grammars createBuiltinGrammar() {
-        try {
-            return GrammarFactory.newInstance().createXSDTypesOnlyGrammars();
-        } catch (EXIException e) {
-            throw new IllegalStateException("Failed to create builtin grammar", e);
-        }
     }
 }
