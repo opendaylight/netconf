285fd5522d95ac64e53fe44798e54e39e9dc5f97
Jakub Morvay <jmorvay@frinx.io>
2019-02-02 11:01:00 +0100

    Merge bierman02-auth and bierman02-base features
    We do not need odl-restconf-nb-bierman02-base feature now. It is
    bierman-02 restconf implementations and used to be pulled by both
    bierman02-auth and bierman02-noauth features. These two features just
    initialized the web application (with or without authentication).
    
    Since bierman02-noauth is no longer with us, merge remaining two
    features into odl-restconf-nb-bierman02 feature.
    
    Change-Id: I121e3c109ca1b5f60aac66e76af011cef0774f7d
    Signed-off-by: Jakub Morvay <jmorvay@frinx.io>

---
+++ restconf/restconf-nb-bierman02/src/main/java/org/opendaylight/netconf/sal/restconf/web/WebInitializer.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright (c) 2018 Inocybe Technologies and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.netconf.sal.restconf.web;
+
+import javax.servlet.ServletException;
+import javax.ws.rs.core.Application;
+import org.opendaylight.aaa.filterchain.configuration.CustomFilterAdapterConfiguration;
+import org.opendaylight.aaa.filterchain.filters.CustomFilterAdapter;
+import org.opendaylight.aaa.web.FilterDetails;
+import org.opendaylight.aaa.web.ServletDetails;
+import org.opendaylight.aaa.web.WebContext;
+import org.opendaylight.aaa.web.WebContextBuilder;
+import org.opendaylight.aaa.web.WebContextRegistration;
+import org.opendaylight.aaa.web.WebContextSecurer;
+import org.opendaylight.aaa.web.WebServer;
+import org.opendaylight.aaa.web.servlet.ServletSupport;
+
+/**
+ * Initializes the bierman-02 endpoint.
+ *
+ * @author Thomas Pantelis
+ */
+public class WebInitializer {
+    private final WebContextRegistration registration;
+
+    public WebInitializer(final WebServer webServer, final WebContextSecurer webContextSecurer,
+            final ServletSupport servletSupport, final Application webApp,
+            final CustomFilterAdapterConfiguration customFilterAdapterConfig) throws ServletException {
+
+        final WebContextBuilder webContextBuilder = WebContext.builder().contextPath("restconf").supportsSessions(true)
+                .addServlet(ServletDetails.builder().servlet(servletSupport.createHttpServletBuilder(webApp).build())
+                        .addUrlPattern("/*").build())
+
+                // Allows user to add javax.servlet.Filter(s) in front of REST services
+                .addFilter(FilterDetails.builder().filter(new CustomFilterAdapter(customFilterAdapterConfig))
+                        .addUrlPattern("/*").build())
+
+                .addFilter(FilterDetails.builder().filter(new org.eclipse.jetty.servlets.GzipFilter())
+                        .putInitParam("mimeTypes",
+                                "application/xml,application/yang.data+xml,xml,application/json,application/yang.data+json")
+                        .addUrlPattern("/*").build());
+
+        webContextSecurer.requireAuthentication(webContextBuilder, "/*");
+
+        registration = webServer.registerWebContext(webContextBuilder.build());
+    }
+
+    public void close() {
+        if (registration != null) {
+            registration.close();
+        }
+    }
+}
