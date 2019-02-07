e7d4d7f7b8d34cf17a5feec534ce4d302332aba4
Michael Vorburger <vorburger@redhat.com>
2019-02-07 18:50:37 +0100

    introduce Rfc8040RestConfWiring for standalone (simple) environments
    very similar to the previous Bierman02RestConfWiring
    
    including a non-regression test
    
    Change-Id: If7b7fdabe85c39ddfb4af351daed4f8771b2bcbe
    Signed-off-by: Michael Vorburger <vorburger@redhat.com>

---
+++ restconf/restconf-nb-rfc8040/src/test/java/org/opendaylight/restconf/nb/rfc8040/test/Rfc8040RestConfWiringTest.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright (c) 2019 Red Hat, Inc. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.restconf.nb.rfc8040.test;
+
+import com.google.inject.AbstractModule;
+import com.google.inject.Provides;
+import java.io.IOException;
+import javax.inject.Inject;
+import javax.inject.Singleton;
+import org.junit.Rule;
+import org.junit.Test;
+import org.opendaylight.aaa.filterchain.configuration.CustomFilterAdapterConfiguration;
+import org.opendaylight.aaa.web.WebServer;
+import org.opendaylight.aaa.web.testutils.TestWebClient;
+import org.opendaylight.aaa.web.testutils.WebTestModule;
+import org.opendaylight.infrautils.inject.guice.testutils.AnnotationsModule;
+import org.opendaylight.infrautils.inject.guice.testutils.GuiceRule;
+import org.opendaylight.infrautils.testutils.LogRule;
+import org.opendaylight.restconf.nb.rfc8040.RestconfApplication;
+import org.opendaylight.restconf.nb.rfc8040.Rfc8040RestConfWiring;
+import org.opendaylight.restconf.nb.rfc8040.handlers.SchemaContextHandler;
+import org.opendaylight.restconf.nb.rfc8040.rests.services.impl.JSONRestconfServiceRfc8040Impl;
+import org.opendaylight.restconf.nb.rfc8040.services.wrapper.ServicesWrapper;
+import org.opendaylight.restconf.nb.rfc8040.test.incubate.InMemoryMdsalModule;
+import org.opendaylight.restconf.nb.rfc8040.web.WebInitializer;
+import org.opendaylight.yangtools.yang.model.api.SchemaContextProvider;
+
+/**
+ * Tests if the {@link Rfc8040RestConfWiring} works.
+ *
+ * @author Michael Vorburger.ch
+ */
+public class Rfc8040RestConfWiringTest {
+
+    public static class TestModule extends AbstractModule {
+        @Override
+        protected void configure() {
+            bind(Rfc8040RestConfWiring.class).asEagerSingleton();
+            bind(RestconfApplication.class).asEagerSingleton();
+            bind(JSONRestconfServiceRfc8040Impl.class).asEagerSingleton();
+            bind(WebInitializer.class).asEagerSingleton();
+            bind(CustomFilterAdapterConfiguration.class).toInstance(listener -> { });
+        }
+
+        @Provides
+        @Singleton ServicesWrapper getServicesWrapper(Rfc8040RestConfWiring wiring) {
+            return wiring.getServicesWrapper();
+        }
+    }
+
+    public @Rule LogRule logRule = new LogRule();
+
+    public @Rule GuiceRule guice = new GuiceRule(TestModule.class,
+            InMemoryMdsalModule.class, WebTestModule.class, AnnotationsModule.class);
+
+    @Inject WebServer webServer;
+    @Inject TestWebClient webClient;
+
+    @Inject SchemaContextProvider schemaContextProvider;
+    @Inject SchemaContextHandler schemaContextHandler;
+
+    @Test
+    public void testWiring() throws IOException {
+        schemaContextHandler.onGlobalContextUpdated(schemaContextProvider.getSchemaContext());
+        // TODO what URL to test here?  /restconf/modules/ is, apparently, Bierman only..
+        // /yang-library-version doesn't work either, so how do we test??
+        // Toaster doesn't work either... :-(
+        // assertThat(webClient.request(GET,
+        //     "/rests/data/toaster:toaster?content=nonconfig").getStatus()).isEqualTo(200);
+    }
+}
