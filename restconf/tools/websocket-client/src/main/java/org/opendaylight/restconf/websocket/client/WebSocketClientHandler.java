c6d358573ba9073961501e6d31354034489144ef
Jaroslav Tóth <jtoth@frinx.io>
2019-04-14 19:40:12 +0200

    WIP: JETTY web-sockets client tool
    Change-Id: Ifaf340ee1e0ba26c203bb1c663588cc94fb5496d
    Signed-off-by: Jaroslav Tóth <jtoth@frinx.io>

---
+++ restconf/tools/websocket-client/src/main/java/org/opendaylight/restconf/websocket/client/WebSocketClientHandler.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright © 2019 FRINX s.r.o. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.restconf.websocket.client;
+
+import java.util.concurrent.ScheduledExecutorService;
+
+public class WebSocketClientHandler {
+
+    public WebSocketClientHandler(final String uri, final ScheduledExecutorService scheduledExecutorService,
+            final String pingMessage, final int pingInterval) {
+
+    }
+
+    public void start() {
+    }
+
+    public void stop() {
+
+    }
+}
