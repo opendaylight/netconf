fe6c202e637feaa0760f9fa87adfa3dc9f94ebee
Jakub Toth <jtoth@luminanetworks.com>
2019-05-12 13:15:34 -0700

    Convert anyxml nodes lazily
    The problem is talking about the direct creating of ContainerNode
    from the Anyxml in the NetconfMessageTransformer. Later, while
    the YT is parsing data, it expects the anyxml on that place
    according to the schema context. In this way, we need to use
    the AnyXml builder while the NetconfMessage is processing.
    
    Transformation to a container-based layout is performed afterwards
    as needed.
    
    JIRA: NETCONF-616
    Change-Id: Iab00b9ef712dbebf0661546550b10bf04507e97a
    Signed-off-by: Jakub Toth <jtoth@luminanetworks.com>
    Signed-off-by: Miroslav Macko <mmacko@luminanetworks.com>
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- netconf/sal-netconf-connector/src/main/java/org/opendaylight/netconf/sal/connect/netconf/schema/mapping/NetconfMessageTransformer.java
+++ netconf/sal-netconf-connector/src/main/java/org/opendaylight/netconf/sal/connect/netconf/schema/mapping/NetconfMessageTransformer.java
@@ -46,6 +46,7 @@ import org.opendaylight.yangtools.yang.common.Revision;
 import org.opendaylight.yangtools.yang.common.RpcError;
 import org.opendaylight.yangtools.yang.common.YangConstants;
 import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
+import org.opendaylight.yangtools.yang.data.api.schema.AnyXmlNode;
 import org.opendaylight.yangtools.yang.data.api.schema.ContainerNode;
 import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNode;
 import org.opendaylight.yangtools.yang.data.api.schema.stream.NormalizedNodeStreamWriter;
@@ -56,6 +57,7 @@ import org.opendaylight.yangtools.yang.data.impl.schema.NormalizedNodeResult;
 import org.opendaylight.yangtools.yang.data.util.DataSchemaContextTree;
 import org.opendaylight.yangtools.yang.model.api.ActionDefinition;
 import org.opendaylight.yangtools.yang.model.api.ActionNodeContainer;
+import org.opendaylight.yangtools.yang.model.api.AnyXmlSchemaNode;
 import org.opendaylight.yangtools.yang.model.api.ContainerSchemaNode;
 import org.opendaylight.yangtools.yang.model.api.DataNodeContainer;
 import org.opendaylight.yangtools.yang.model.api.DataSchemaNode;
@@ -101,7 +103,7 @@ public class NetconfMessageTransformer implements MessageTransformer<NetconfMess
 
     @VisibleForTesting
     Set<ActionDefinition> getActions() {
-        Builder<ActionDefinition> builder = ImmutableSet.builder();
+        final Builder<ActionDefinition> builder = ImmutableSet.builder();
         for (DataSchemaNode dataSchemaNode : schemaContext.getChildNodes()) {
             if (dataSchemaNode instanceof ActionNodeContainer) {
                 findAction(dataSchemaNode, builder);
@@ -202,7 +204,7 @@ public class NetconfMessageTransformer implements MessageTransformer<NetconfMess
             // If the schema context for netconf device does not contain model for base netconf operations,
             // use default pre build context with just the base model
             // This way operations like lock/unlock are supported even if the source for base model was not provided
-            SchemaContext ctx = needToUseBaseCtx ? baseSchema.getSchemaContext() : schemaContext;
+            final SchemaContext ctx = needToUseBaseCtx ? baseSchema.getSchemaContext() : schemaContext;
             NetconfMessageTransformUtil.writeNormalizedRpc((ContainerNode) payload, result, rpcInput, ctx);
         } catch (final XMLStreamException | IOException | IllegalStateException e) {
             throw new IllegalStateException("Unable to serialize " + rpcInput, e);
@@ -270,23 +272,20 @@ public class NetconfMessageTransformer implements MessageTransformer<NetconfMess
             final Element xmlData = NetconfMessageTransformUtil.getDataSubtree(message.getDocument());
             final ContainerSchemaNode schemaForDataRead =
                     NetconfMessageTransformUtil.createSchemaForDataRead(schemaContext);
-            final ContainerNode dataNode;
-
             try {
                 final NormalizedNodeResult resultHolder = new NormalizedNodeResult();
                 final NormalizedNodeStreamWriter writer = ImmutableNormalizedNodeStreamWriter.from(resultHolder);
                 final XmlParserStream xmlParser = XmlParserStream.create(writer, schemaContext, schemaForDataRead,
                         strictParsing);
                 xmlParser.traverse(new DOMSource(xmlData));
-                dataNode = (ContainerNode) resultHolder.getResult();
             } catch (XMLStreamException | URISyntaxException | IOException | SAXException e) {
                 throw new IllegalArgumentException(String.format("Failed to parse data response %s", xmlData), e);
             }
-
-            normalizedNode = Builders.containerBuilder()
-                    .withNodeIdentifier(new YangInstanceIdentifier
-                            .NodeIdentifier(NetconfMessageTransformUtil.NETCONF_RPC_REPLY_QNAME))
-                    .withChild(dataNode).build();
+            final AnyXmlSchemaNode dataRead = NetconfMessageTransformUtil.createSchemaForAnyXmlDataRead();
+            final AnyXmlNode anyXmlNode = Builders.anyXmlBuilder(dataRead).withValue(new DOMSource(xmlData)).build();
+            normalizedNode = Builders.containerBuilder().withNodeIdentifier(
+                    new YangInstanceIdentifier.NodeIdentifier(NetconfMessageTransformUtil.NETCONF_RPC_REPLY_QNAME))
+                    .withChild(anyXmlNode).build();
         } else {
 
             Map<QName, RpcDefinition> currentMappedRpcs = mappedRpcs;
@@ -318,7 +317,7 @@ public class NetconfMessageTransformer implements MessageTransformer<NetconfMess
             }
         }
         Preconditions.checkNotNull(actionDefinition, "Action does not exist: %s", action);
-        ContainerNode normalizedNode = (ContainerNode) parseResult(message, actionDefinition);
+        final ContainerNode normalizedNode = (ContainerNode) parseResult(message, actionDefinition);
 
         if (normalizedNode == null) {
             return new SimpleDOMActionResult(Collections.<RpcError>emptyList());
