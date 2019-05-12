9476f72d10f85cf02565324f9cb58f96b514e433
Jakub Toth <jtoth@luminanetworks.com>
2019-05-12 13:15:34 -0700

    Convert anyxml nodes lazily
    The problem is talking about the direct creating of ContainerNode
    from the Anyxml in the NetconfMessageTransformer. Later, while
    the YT is parsing data, it expects the anyxml on that place
    according to the schema context. In this way, we need to use
    the AnyXml builder while the NetconfMessage is processing.
    
    Transformation to a container-based layout is performed afterwards
    as needed.
    
    JIRA: NETCONF-616
    Change-Id: Iab00b9ef712dbebf0661546550b10bf04507e97a
    Signed-off-by: Jakub Toth <jtoth@luminanetworks.com>
    Signed-off-by: Miroslav Macko <mmacko@luminanetworks.com>
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ netconf/sal-netconf-connector/src/main/java/org/opendaylight/netconf/sal/connect/netconf/util/NodeAnyXmlProxy.java
@@ -0,0 +1,84 @@
+/*
+ * Copyright (c) 2019 Lumina Networks, Inc. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.netconf.sal.connect.netconf.util;
+
+import java.util.Collection;
+import java.util.Optional;
+import org.eclipse.jdt.annotation.NonNull;
+import org.opendaylight.yangtools.yang.common.QName;
+import org.opendaylight.yangtools.yang.model.api.AnyXmlSchemaNode;
+import org.opendaylight.yangtools.yang.model.api.MustDefinition;
+import org.opendaylight.yangtools.yang.model.api.RevisionAwareXPath;
+import org.opendaylight.yangtools.yang.model.api.SchemaPath;
+import org.opendaylight.yangtools.yang.model.api.Status;
+
+public class NodeAnyXmlProxy implements AnyXmlSchemaNode {
+
+    private final QName netconfDataQname;
+
+    public NodeAnyXmlProxy(final QName netconfDataQname) {
+        this.netconfDataQname = netconfDataQname;
+    }
+
+    @Override
+    public boolean isConfiguration() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public @NonNull QName getQName() {
+        return netconfDataQname;
+    }
+
+    @Override
+    public @NonNull SchemaPath getPath() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public @NonNull Status getStatus() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Optional<String> getDescription() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Optional<String> getReference() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean isAugmenting() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean isAddedByUses() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Optional<RevisionAwareXPath> getWhenCondition() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean isMandatory() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Collection<MustDefinition> getMustConstraints() {
+        throw new UnsupportedOperationException();
+    }
+
+}
