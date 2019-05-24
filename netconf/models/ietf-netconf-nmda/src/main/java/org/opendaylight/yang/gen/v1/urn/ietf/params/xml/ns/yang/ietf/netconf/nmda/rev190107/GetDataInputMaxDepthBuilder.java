efff2208ab828b971eb6d497b0850304608c36e1
Robert Varga <robert.varga@pantheon.tech>
2019-05-24 16:30:12 +0200

    Add ietf-netconf-nmda data model
    This adds NMDA and with-defaults models, so that we can build up
    the implementation.
    
    Change-Id: I33104b40863983df23b0f1984922daf294c2fa4f
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ netconf/models/ietf-netconf-nmda/src/main/java/org/opendaylight/yang/gen/v1/urn/ietf/params/xml/ns/yang/ietf/netconf/nmda/rev190107/GetDataInputMaxDepthBuilder.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2019 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */package org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.netconf.nmda.rev190107;
+
+import java.util.Optional;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.netconf.nmda.rev190107.GetDataInput.MaxDepth;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.netconf.nmda.rev190107.GetDataInput.MaxDepth.Enumeration;
+
+/**
+ * MaxDepth utilities.
+ */
+public final class GetDataInputMaxDepthBuilder {
+    private GetDataInputMaxDepthBuilder() {
+        // Exists only to defeat instantiation.
+    }
+
+    public static MaxDepth getDefaultInstance(final String defaultValue) {
+        final Optional<Enumeration> optEnum = Enumeration.forName(defaultValue);
+        if (optEnum.isPresent()) {
+            return new MaxDepth(optEnum.get());
+        }
+        // FIXME: consider being stricter about number formats here
+        return new MaxDepth(Integer.valueOf(defaultValue));
+    }
+}
