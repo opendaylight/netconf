4310b2d190a226386eace4b3652b08a8a760dcc6
Jaroslav Tóth <jtoth@frinx.io>
2019-07-16 13:14:31 +0200

    Added full YIID to ietf-restconf-monitoring DS
    - Currently local-name of the QName is used as key to the list
      of created streams - that is a bug because local-name doesn't
      have to be unique in scope of one module or whole schema context.
    - This fix puts the whole serialized YIID to DS as stream name.
    
    JIRA: NETCONF-726
    Change-Id: I28012a478b958b933af993c2ede5f91c2eb8e211
    Signed-off-by: Jaroslav Tóth <jtoth@frinx.io>

--- restconf/restconf-nb-rfc8040/src/main/java/org/opendaylight/restconf/nb/rfc8040/rests/services/impl/SubscribeToStreamUtil.java
+++ restconf/restconf-nb-rfc8040/src/main/java/org/opendaylight/restconf/nb/rfc8040/rests/services/impl/SubscribeToStreamUtil.java
@@ -54,7 +54,7 @@ import org.slf4j.LoggerFactory;
  */
 abstract class SubscribeToStreamUtil {
     /**
-     * Implementation of {@link UrlResolver} for Server-sent events.
+     * Implementation of UrlResolver for Server-sent events.
      */
     private static final class ServerSentEvents extends SubscribeToStreamUtil {
         static final ServerSentEvents INSTANCE = new ServerSentEvents();
@@ -68,7 +68,7 @@ abstract class SubscribeToStreamUtil {
     }
 
     /**
-     * Implementation of {@link UrlResolver} for Web sockets.
+     * Implementation of UrlResolver for Web sockets.
      */
     private static final class WebSockets extends SubscribeToStreamUtil {
         static final WebSockets INSTANCE = new WebSockets();
@@ -123,7 +123,6 @@ abstract class SubscribeToStreamUtil {
      * @param uriInfo                 URI information.
      * @param notificationQueryParams Query parameters of notification.
      * @param handlersHolder          Holder of handlers for notifications.
-     * @param urlResolver             Resolver for proper implementation. Possibilities is WS or SSE.
      * @return Stream location for listening.
      */
     final @NonNull URI subscribeToYangStream(final String identifier, final UriInfo uriInfo,
@@ -135,7 +134,7 @@ abstract class SubscribeToStreamUtil {
         final Optional<NotificationListenerAdapter> notificationListenerAdapter =
                 ListenersBroker.getInstance().getNotificationListenerFor(streamName);
 
-        if (!notificationListenerAdapter.isPresent()) {
+        if (notificationListenerAdapter.isEmpty()) {
             throw new RestconfDocumentedException(String.format(
                     "Stream with name %s was not found.", streamName),
                     ErrorType.PROTOCOL,
@@ -218,13 +217,13 @@ abstract class SubscribeToStreamUtil {
         final DOMDataTreeReadWriteTransaction writeTransaction = transactionChain.newReadWriteTransaction();
         final EffectiveModelContext schemaContext = handlersHolder.getSchemaHandler().get();
         final boolean exist = checkExist(schemaContext, writeTransaction);
+        final String serializedPath = IdentifierCodec.serialize(listener.get().getPath(), schemaContext);
 
         final NormalizedNode<?, ?> mapToStreams = RestconfMappingNodeUtil
                 .mapDataChangeNotificationStreamByIetfRestconfMonitoring(listener.get().getPath(),
                         notificationQueryParams.getStart(), listener.get().getOutputType(), uri,
-                        getMonitoringModule(schemaContext), exist, schemaContext);
-        writeDataToDS(schemaContext, listener.get().getPath().getLastPathArgument().getNodeType().getLocalName(),
-                writeTransaction, exist, mapToStreams);
+                        getMonitoringModule(schemaContext), exist, schemaContext, serializedPath);
+        writeDataToDS(schemaContext, serializedPath, writeTransaction, exist, mapToStreams);
         submitData(writeTransaction);
         transactionChain.close();
         return uri;
@@ -247,7 +246,7 @@ abstract class SubscribeToStreamUtil {
                 IdentifierCodec.deserialize(pathId, schemaContext), mapToStreams);
     }
 
-    private static void submitData(final DOMDataTreeReadWriteTransaction readWriteTransaction) {
+    static void submitData(final DOMDataTreeReadWriteTransaction readWriteTransaction) {
         try {
             readWriteTransaction.commit().get();
         } catch (final InterruptedException | ExecutionException e) {
