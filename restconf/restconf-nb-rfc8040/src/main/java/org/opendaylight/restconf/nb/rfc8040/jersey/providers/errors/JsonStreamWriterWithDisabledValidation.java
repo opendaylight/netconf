e9bcdb6c9383d9427c5d37c8ea9ce5286bdf7b0c
Jaroslav Tóth <jtoth@frinx.io>
2019-08-08 17:01:13 +0200

    Added ExceptionMapper to RFC-8040 impl
    It is similar to draft-02 implementation but with several diffs
    that had to be made because of compliancy against specification,
    different revisions of QNames, and need for refactoring:
    - Response must have yang-data+json or yang-data+xml types, the
      parsing of request content type and acceptable media types was
      adjusted accordingly.
    - Schema context was used as feed to NormalizedNode builders -
      this approach is useless.
    - Anonymous classes used for special serializaton with disabled
      validation of error-info leaf were moved to distinct classes
      (they share some lines of code, so the abstract parent class
      is added).
    - Added error-path leaf to serialized JSON/XML data (it was in
      todo tag for unknown reasons).
    - Next small fixes and refactoring of code by extraction of
      methods.
    - Unit test was written from scratch, because of changes done
      to parsing of status code and media type.
    
    Change-Id: I9c17229e8df1b382a37ca588a506128040c6d42f
    Signed-off-by: Jaroslav Tóth <jtoth@frinx.io>
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ restconf/restconf-nb-rfc8040/src/main/java/org/opendaylight/restconf/nb/rfc8040/jersey/providers/errors/JsonStreamWriterWithDisabledValidation.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright © 2019 FRINX s.r.o. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.restconf.nb.rfc8040.jersey.providers.errors;
+
+import com.google.gson.stream.JsonWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.net.URI;
+import org.opendaylight.restconf.nb.rfc8040.handlers.SchemaContextHandler;
+import org.opendaylight.yangtools.yang.common.QName;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.NodeIdentifier;
+import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNode;
+import org.opendaylight.yangtools.yang.data.api.schema.stream.NormalizedNodeStreamWriter;
+import org.opendaylight.yangtools.yang.data.codec.gson.JSONCodecFactorySupplier;
+import org.opendaylight.yangtools.yang.data.codec.gson.JSONNormalizedNodeStreamWriter;
+import org.opendaylight.yangtools.yang.data.codec.gson.JsonWriterFactory;
+import org.opendaylight.yangtools.yang.model.api.SchemaPath;
+
+/**
+ * JSON stream-writer with disabled leaf-type validation for specified QName.
+ */
+final class JsonStreamWriterWithDisabledValidation extends StreamWriterWithDisabledValidation {
+
+    private final JsonWriter jsonWriter;
+    private final NormalizedNodeStreamWriter jsonNodeStreamWriter;
+
+    /**
+     * Creation of the custom JSON stream-writer.
+     *
+     * @param excludedQName        QName of the element that is excluded from type-check.
+     * @param outputWriter         Output stream that is used for creation of JSON writers.
+     * @param schemaPath           Schema-path of the {@link NormalizedNode} to be written.
+     * @param initialNs            Initial namespace derived from schema node of the data that are serialized.
+     * @param schemaContextHandler Handler that holds actual schema context.
+     */
+    JsonStreamWriterWithDisabledValidation(final QName excludedQName, final OutputStreamWriter outputWriter,
+            final SchemaPath schemaPath, final URI initialNs, final SchemaContextHandler schemaContextHandler) {
+        super(excludedQName);
+        this.jsonWriter = JsonWriterFactory.createJsonWriter(outputWriter);
+        this.jsonNodeStreamWriter = JSONNormalizedNodeStreamWriter.createExclusiveWriter(
+                JSONCodecFactorySupplier.DRAFT_LHOTKA_NETMOD_YANG_JSON_02.getShared(schemaContextHandler.get()),
+                schemaPath, initialNs, jsonWriter);
+    }
+
+    @Override
+    protected NormalizedNodeStreamWriter delegate() {
+        return jsonNodeStreamWriter;
+    }
+
+    @Override
+    void startLeafNodeWithDisabledValidation(final NodeIdentifier nodeIdentifier) throws IOException {
+        jsonWriter.name(nodeIdentifier.getNodeType().getLocalName());
+    }
+
+    @Override
+    void scalarValueWithDisabledValidation(final Object value) throws IOException {
+        jsonWriter.value(value.toString());
+    }
+
+    @Override
+    void endNodeWithDisabledValidation() {
+        // nope
+    }
+
+    @Override
+    public void close() throws IOException {
+        jsonWriter.close();
+    }
+}
