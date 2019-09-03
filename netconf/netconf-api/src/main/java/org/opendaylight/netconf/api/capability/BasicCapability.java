90996ae34f3c8254be383816834ff78a1a27dcc6
Robert Varga <robert.varga@pantheon.tech>
2019-09-03 09:22:51 +0200

    Use java.util.Optional
    Modernizer is point out use of Guava's Optional, migrate to Java
    equivalents.
    
    Change-Id: I732ea429505c730ef7ab38d45849662265267e0b
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- netconf/netconf-api/src/main/java/org/opendaylight/netconf/api/capability/BasicCapability.java
+++ netconf/netconf-api/src/main/java/org/opendaylight/netconf/api/capability/BasicCapability.java
@@ -5,18 +5,16 @@
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
-
 package org.opendaylight.netconf.api.capability;
 
-import com.google.common.base.Optional;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.Optional;
 
 /**
  * Capability representing a basic, one-line, string based capability.
  */
 public class BasicCapability implements Capability {
-
     private final String capability;
 
     public BasicCapability(final String capability) {
@@ -30,22 +28,22 @@ public class BasicCapability implements Capability {
 
     @Override
     public Optional<String> getModuleNamespace() {
-        return Optional.absent();
+        return Optional.empty();
     }
 
     @Override
     public Optional<String> getModuleName() {
-        return Optional.absent();
+        return Optional.empty();
     }
 
     @Override
     public Optional<String> getRevision() {
-        return Optional.absent();
+        return Optional.empty();
     }
 
     @Override
     public Optional<String> getCapabilitySchema() {
-        return Optional.absent();
+        return Optional.empty();
     }
 
     @Override
