4dfa995681eb385ca8d859a62da8481bea8b2a43
Jaroslav Tóth <jtoth@frinx.io>
2020-01-03 13:12:28 +0100

    More robust processing of NETCONF chunk aggregator exceptions
    - Goal: making chunk aggregator more robust against errors
      with known message-id that occur during parsing - this way,
      NETCONF transactions fail immediatelly rather then hanging
      on some timeout in client code. Malformed messages occur a lot
      on IOS XR 6.x devices - it seems to be caused  by 'transport
      throttling error' which is not properly encoded into NETCONF
      chunks:
    
      ... NetconfChunkAggregator ... Got byte 60 while waiting for 10
      <rpc-error><error-type>transport</error-type>
      <error-tag>resource-denied</error-tag><error-message>transport
      throttling error</error-message></rpc-error>
      #462
        <record>
         <record>1395</record>
         <timestamp>1578037903</timestamp>
      ...
    
    - Propagation of exceptions from NETCONF chunk aggregator
      to higher layers: NetconfSession that tries to extract message-id
      from malformed chunk and then to NetconfDeviceCommunicator
      that processes malformed message with known ID by dropping
      of corresponding request with the same message-id. It works
      in the same fassion like processing of regular messages -
      request futures are processed in order until request with
      the message-id is found - if the request message-id doesn't
      equal to malformed message ID, it is set to failure too.
    - Added 'MALFORMED_DATA' state to NetconfChunkAggregator that
      ignores data bytes after exception has been thrown in some part
      of the processing. Thrown exception also temporaly disables
      propagation of exceptions and parsed chunks to higher layers
      till the non-malformed message is processed.
    - Refactored NetconfChunkAggregator - improved logging (error
      messages also contain already aggregated bytes) and exception
      throwing.
    - Added unit tests.
    
    JIRA: NETCONF-724
    Change-Id: I826d531fc5ec1c0bfccff0f12941a25f072f939c
    Signed-off-by: Jaroslav Tóth <jtoth@frinx.io>

--- netconf/netconf-netty-util/src/test/java/org/opendaylight/netconf/nettyutil/AbstractNetconfSessionTest.java
+++ netconf/netconf-netty-util/src/test/java/org/opendaylight/netconf/nettyutil/AbstractNetconfSessionTest.java
@@ -14,6 +14,7 @@ import static org.mockito.Mockito.doAnswer;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
@@ -22,10 +23,12 @@ import static org.mockito.Mockito.verifyNoMoreInteractions;
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelPipeline;
 import io.netty.channel.ChannelPromise;
 import io.netty.channel.EventLoop;
 import io.netty.handler.codec.ByteToMessageDecoder;
+import io.netty.handler.codec.DecoderException;
 import io.netty.handler.codec.MessageToByteEncoder;
 import java.util.Collections;
 import java.util.Optional;
@@ -34,6 +37,8 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
+import org.opendaylight.netconf.api.NetconfChunkException;
+import org.opendaylight.netconf.api.NetconfDocumentedException;
 import org.opendaylight.netconf.api.NetconfMessage;
 import org.opendaylight.netconf.api.NetconfSessionListener;
 import org.opendaylight.netconf.api.NetconfTerminationReason;
@@ -54,6 +59,8 @@ public class AbstractNetconfSessionTest {
     private EventLoop eventLoop;
     @Mock
     private ChannelPromise writeFuture;
+    @Mock
+    private ChannelHandlerContext channelContext;
 
     private NetconfHelloMessage clientHello;
 
@@ -64,6 +71,7 @@ public class AbstractNetconfSessionTest {
         doNothing().when(listener).onSessionDown(any(TestingNetconfSession.class), any(Exception.class));
         doNothing().when(listener).onSessionTerminated(any(TestingNetconfSession.class),
                 any(NetconfTerminationReason.class));
+        doNothing().when(listener).processMalformedRpc(any(String.class), any(NetconfDocumentedException.class));
 
         doReturn(writeFuture).when(channel).newPromise();
         doReturn(writeFuture).when(channel).writeAndFlush(any(NetconfMessage.class), any(ChannelPromise.class));
@@ -77,19 +85,20 @@ public class AbstractNetconfSessionTest {
             invocation.<Runnable>getArgument(0).run();
             return null;
         }).when(eventLoop).execute(any(Runnable.class));
+        doReturn(channelContext).when(channelContext).fireExceptionCaught(any(Throwable.class));
 
         clientHello = NetconfHelloMessage.createClientHello(Collections.emptySet(), Optional.empty());
     }
 
     @Test
-    public void testHandleMessage() throws Exception {
+    public void testHandleMessage() {
         final TestingNetconfSession testingNetconfSession = new TestingNetconfSession(listener, channel, 1L);
         testingNetconfSession.handleMessage(clientHello);
         verify(listener).onMessage(testingNetconfSession, clientHello);
     }
 
     @Test
-    public void testSessionUp() throws Exception {
+    public void testSessionUp() {
         final TestingNetconfSession testingNetconfSession = new TestingNetconfSession(listener, channel, 1L);
         testingNetconfSession.sessionUp();
         verify(listener).onSessionUp(testingNetconfSession);
@@ -97,7 +106,7 @@ public class AbstractNetconfSessionTest {
     }
 
     @Test
-    public void testClose() throws Exception {
+    public void testClose() {
         final TestingNetconfSession testingNetconfSession = new TestingNetconfSession(listener, channel, 1L);
         testingNetconfSession.sessionUp();
         testingNetconfSession.close();
@@ -106,7 +115,7 @@ public class AbstractNetconfSessionTest {
     }
 
     @Test
-    public void testReplaceHandlers() throws Exception {
+    public void testReplaceHandlers() {
         final TestingNetconfSession testingNetconfSession = new TestingNetconfSession(listener, channel, 1L);
         final ChannelHandler mock = mock(ChannelHandler.class);
 
@@ -125,7 +134,7 @@ public class AbstractNetconfSessionTest {
     }
 
     @Test
-    public void testStartExi() throws Exception {
+    public void testStartExi() {
         TestingNetconfSession testingNetconfSession = new TestingNetconfSession(listener, channel, 1L);
         testingNetconfSession = spy(testingNetconfSession);
 
@@ -134,7 +143,7 @@ public class AbstractNetconfSessionTest {
     }
 
     @Test
-    public void testEndOfInput() throws Exception {
+    public void testEndOfInput() {
         final TestingNetconfSession testingNetconfSession = new TestingNetconfSession(listener, channel, 1L);
         testingNetconfSession.endOfInput();
         verifyNoMoreInteractions(listener);
@@ -144,11 +153,56 @@ public class AbstractNetconfSessionTest {
     }
 
     @Test
-    public void testSendMessage() throws Exception {
+    public void testSendMessage() {
         final TestingNetconfSession testingNetconfSession = new TestingNetconfSession(listener, channel, 1L);
         final NetconfHelloMessage hello = NetconfHelloMessage.createClientHello(Collections.emptySet(),
             Optional.empty());
         testingNetconfSession.sendMessage(hello);
         verify(channel).writeAndFlush(hello, writeFuture);
+    }
+
+    @Test
+    public void testChunkExceptionWithMessageIdCaught() {
+        final String malformedNetconfRpc = "<?xml version=\"1.0\"?>\n"
+                + "<rpc message-id=\"10\" a=\"64\""
+                + " xmlns=\"urn:ietf:params:xml:ns:netconf:base:1.0\">\n"
+                + "<rpc-error>transport throttling error</rpc-error>\n"
+                + "    <get/>\n"
+                + "</rpc>";
+        final NetconfChunkException chunkException = NetconfChunkException.create(
+                malformedNetconfRpc.getBytes(), "Malformed chunk header encountered (byte 0)");
+        final DecoderException decoderException = new DecoderException(chunkException);
+
+        final TestingNetconfSession testingNetconfSession = new TestingNetconfSession(listener, channel, 1L);
+        testingNetconfSession.exceptionCaught(channelContext, decoderException);
+
+        verify(listener).processMalformedRpc("10", chunkException);
+        verify(channelContext, never()).fireExceptionCaught(any(Throwable.class));
+    }
+
+    @Test
+    public void testChunkExceptionWithoutMessageIdCaught() {
+        final String malformedNetconfRpc = "a=\"64\" xmlns=\"urn:ietf:params:xml:ns:netconf:base:1.0\">\n"
+                + "<rpc-error>transport throttling error</rpc-error>\n"
+                + "    <get/>\n"
+                + "</rpc>";
+        final NetconfChunkException chunkException = NetconfChunkException.create(
+                malformedNetconfRpc.getBytes(), "Malformed chunk header encountered (byte 0)");
+        final DecoderException decoderException = new DecoderException(chunkException);
+        final TestingNetconfSession testingNetconfSession = new TestingNetconfSession(listener, channel, 1L);
+        testingNetconfSession.exceptionCaught(channelContext, decoderException);
+
+        verify(listener, never()).processMalformedRpc(any(String.class), any(NetconfDocumentedException.class));
+        verify(channelContext, never()).fireExceptionCaught(any(Throwable.class));
+    }
+
+    @Test
+    public void testUnknownExceptionCaptured() {
+        final Exception exception = new Exception("unknown exception");
+        final TestingNetconfSession testingNetconfSession = new TestingNetconfSession(listener, channel, 1L);
+        testingNetconfSession.exceptionCaught(channelContext, exception);
+
+        verify(listener, never()).processMalformedRpc(any(String.class), any(NetconfDocumentedException.class));
+        verify(channelContext).fireExceptionCaught(exception);
     }
 }
