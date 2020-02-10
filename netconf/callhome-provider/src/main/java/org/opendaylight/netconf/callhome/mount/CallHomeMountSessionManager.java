98ea9b25926663baf73a35ad75784661d8784938
Manoj Chokka <manoj.chokka@verizon.com>
2020-02-10 23:45:34 +0530

    Reject multiple sessions with the same host key
    SSH host key is supposed to be unique among devices. If there is an
    attempt to establish a session with the same key, terminate the new
    session.
    
    JIRA: NETCONF-653
    Change-Id: I2d78b9aa34ee4b16d0c6d2ed2ac172942c575851
    Signed-off-by: Manoj Chokka <manoj.chokka@verizon.com>
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- netconf/callhome-provider/src/main/java/org/opendaylight/netconf/callhome/mount/CallHomeMountSessionManager.java
+++ netconf/callhome-provider/src/main/java/org/opendaylight/netconf/callhome/mount/CallHomeMountSessionManager.java
@@ -7,54 +7,58 @@
  */
 package org.opendaylight.netconf.callhome.mount;
 
-import com.google.common.collect.Multimap;
-import com.google.common.collect.MultimapBuilder;
 import java.net.InetSocketAddress;
 import java.net.SocketAddress;
 import java.security.PublicKey;
-import java.util.Collection;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
-import org.eclipse.jdt.annotation.NonNull;
 import org.eclipse.jdt.annotation.Nullable;
 import org.opendaylight.netconf.callhome.mount.CallHomeMountSessionContext.CloseCallback;
 import org.opendaylight.netconf.callhome.protocol.CallHomeChannelActivator;
 import org.opendaylight.netconf.callhome.protocol.CallHomeProtocolSessionContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class CallHomeMountSessionManager implements CallHomeMountSessionContext.CloseCallback {
+    private static final Logger LOG = LoggerFactory.getLogger(CallHomeMountSessionManager.class);
 
     private final ConcurrentMap<SocketAddress, CallHomeMountSessionContext> contextByAddress =
         new ConcurrentHashMap<>();
-    private final Multimap<PublicKey, CallHomeMountSessionContext> contextByPublicKey = MultimapBuilder.hashKeys()
-        .hashSetValues().build();
+    private final ConcurrentMap<PublicKey, CallHomeMountSessionContext> contextByPublicKey = new ConcurrentHashMap<>();
 
-    public @Nullable CallHomeMountSessionContext getByAddress(InetSocketAddress remoteAddr) {
+    public @Nullable CallHomeMountSessionContext getByAddress(final InetSocketAddress remoteAddr) {
         return contextByAddress.get(remoteAddr);
     }
 
-
-    public @NonNull Collection<CallHomeMountSessionContext> getByPublicKey(PublicKey publicKey) {
+    public @Nullable CallHomeMountSessionContext getByPublicKey(final PublicKey publicKey) {
         return contextByPublicKey.get(publicKey);
     }
 
-    CallHomeMountSessionContext createSession(CallHomeProtocolSessionContext session,
-                                              CallHomeChannelActivator activator, final CloseCallback onCloseHandler) {
+    CallHomeMountSessionContext createSession(final CallHomeProtocolSessionContext session,
+            final CallHomeChannelActivator activator, final CloseCallback onCloseHandler) {
+        final CallHomeMountSessionContext deviceContext = new CallHomeMountSessionContext(session.getSessionName(),
+            session, activator, devCtxt -> {
+                CallHomeMountSessionManager.this.onClosed(devCtxt);
+                onCloseHandler.onClosed(devCtxt);
+            });
 
-        String name = session.getSessionName();
-        CallHomeMountSessionContext deviceContext = new CallHomeMountSessionContext(name,
-                session, activator, devCtxt -> {
-            CallHomeMountSessionManager.this.onClosed(devCtxt);
-            onCloseHandler.onClosed(devCtxt);
-        });
+        final PublicKey remoteKey = deviceContext.getRemoteServerKey();
+        final CallHomeMountSessionContext existing = contextByPublicKey.putIfAbsent(remoteKey, deviceContext);
+        if (existing != null) {
+            // Check if the sshkey of the incoming netconf server is present. If present return null, else store the
+            // session. The sshkey is the uniqueness of the callhome sessions not the uniqueid/devicename.
+            LOG.error("SSH Host Key {} is associated with existing session {}, closing session {}", remoteKey, existing,
+                session);
+            session.terminate();
+            return null;
+        }
 
         contextByAddress.put(deviceContext.getRemoteAddress(), deviceContext);
-        contextByPublicKey.put(deviceContext.getRemoteServerKey(), deviceContext);
-
         return deviceContext;
     }
 
     @Override
-    public synchronized void onClosed(CallHomeMountSessionContext deviceContext) {
+    public synchronized void onClosed(final CallHomeMountSessionContext deviceContext) {
         contextByAddress.remove(deviceContext.getRemoteAddress());
         contextByPublicKey.remove(deviceContext.getRemoteServerKey(), deviceContext);
     }
