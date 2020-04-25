efd72b4eb0c76fddc6deb157fd5a67ed7455b3e2
Robert Varga <robert.varga@pantheon.tech>
2020-04-26 00:11:08 +0200

    Eliminate Blueprint from sal-netconf-connector
    This is a very simple component, convert it to OSGi DS, hiding
    implementation details from exports.
    
    Change-Id: Ibc961b141803672e9791208795ed967d651ca0ee
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- netconf/sal-netconf-connector/src/main/java/org/opendaylight/netconf/sal/connect/impl/DefaultDeviceActionFactory.java
+++ netconf/sal-netconf-connector/src/main/java/org/opendaylight/netconf/sal/connect/impl/DefaultDeviceActionFactory.java
@@ -5,8 +5,7 @@
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
-
-package org.opendaylight.netconf.sal.connect.netconf;
+package org.opendaylight.netconf.sal.connect.impl;
 
 import static java.util.Objects.requireNonNull;
 
@@ -15,6 +14,7 @@ import com.google.common.collect.ImmutableClassToInstanceMap;
 import com.google.common.util.concurrent.Futures;
 import com.google.common.util.concurrent.ListenableFuture;
 import com.google.common.util.concurrent.MoreExecutors;
+import javax.inject.Singleton;
 import org.opendaylight.mdsal.dom.api.DOMActionResult;
 import org.opendaylight.mdsal.dom.api.DOMActionService;
 import org.opendaylight.mdsal.dom.api.DOMActionServiceExtension;
@@ -27,6 +27,7 @@ import org.opendaylight.yangtools.yang.common.RpcResult;
 import org.opendaylight.yangtools.yang.data.api.schema.ContainerNode;
 import org.opendaylight.yangtools.yang.model.api.SchemaContext;
 import org.opendaylight.yangtools.yang.model.api.SchemaPath;
+import org.osgi.service.component.annotations.Component;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -36,9 +37,10 @@ import org.slf4j.LoggerFactory;
  * transforms replied NETCONF message  to action result, and using {@link RemoteDeviceCommunicator} that is responsible
  * for sending of built RPCs to NETCONF client.
  */
-public class DeviceActionFactoryImpl implements DeviceActionFactory {
-
-    private static final Logger LOG = LoggerFactory.getLogger(DeviceActionFactoryImpl.class);
+@Component(immediate = true)
+@Singleton
+public final class DefaultDeviceActionFactory implements DeviceActionFactory {
+    private static final Logger LOG = LoggerFactory.getLogger(DefaultDeviceActionFactory.class);
 
     @Override
     public DOMActionService createDeviceAction(final MessageTransformer<NetconfMessage> messageTransformer,
