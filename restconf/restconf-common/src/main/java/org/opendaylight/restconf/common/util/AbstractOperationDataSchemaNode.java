bf0432058b563ab5339cefdd72b89b26db9685b7
Robert Varga <robert.varga@pantheon.tech>
2020-04-26 13:47:02 +0200

    Factor out common operations schema/data construction
    These bits of code are just copy&pasted between the two
    implementations, move them to common code and refactor them to
    make them simpler.
    
    Change-Id: Ica72ee1623c6dc2734d2fd9456be96e8a514fccf
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ restconf/restconf-common/src/main/java/org/opendaylight/restconf/common/util/AbstractOperationDataSchemaNode.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2020 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.restconf.common.util;
+
+import java.util.Optional;
+import org.opendaylight.yangtools.yang.model.api.DataSchemaNode;
+import org.opendaylight.yangtools.yang.model.api.RevisionAwareXPath;
+import org.opendaylight.yangtools.yang.model.api.Status;
+
+abstract class AbstractOperationDataSchemaNode implements DataSchemaNode {
+    @Override
+    public final Status getStatus() {
+        return Status.CURRENT;
+    }
+
+    @Override
+    public final boolean isConfiguration() {
+        return false;
+    }
+
+    @Override
+    public final boolean isAugmenting() {
+        return false;
+    }
+
+    @Override
+    public final boolean isAddedByUses() {
+        return false;
+    }
+
+    @Override
+    public final Optional<String> getDescription() {
+        return Optional.empty();
+    }
+
+    @Override
+    public final Optional<String> getReference() {
+        return Optional.empty();
+    }
+
+    @Override
+    public final Optional<RevisionAwareXPath> getWhenCondition() {
+        return Optional.empty();
+    }
+}
---
+++ restconf/restconf-common/src/main/java/org/opendaylight/restconf/common/util/OperationsContainerSchemaNode.java
@@ -0,0 +1,102 @@
+/*
+ * Copyright (c) 2020 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.restconf.common.util;
+
+import static java.util.Objects.requireNonNull;
+
+import com.google.common.collect.Maps;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+import org.eclipse.jdt.annotation.NonNull;
+import org.opendaylight.yangtools.yang.common.QName;
+import org.opendaylight.yangtools.yang.model.api.ActionDefinition;
+import org.opendaylight.yangtools.yang.model.api.AugmentationSchemaNode;
+import org.opendaylight.yangtools.yang.model.api.ContainerSchemaNode;
+import org.opendaylight.yangtools.yang.model.api.DataSchemaNode;
+import org.opendaylight.yangtools.yang.model.api.GroupingDefinition;
+import org.opendaylight.yangtools.yang.model.api.MustDefinition;
+import org.opendaylight.yangtools.yang.model.api.NotificationDefinition;
+import org.opendaylight.yangtools.yang.model.api.SchemaPath;
+import org.opendaylight.yangtools.yang.model.api.TypeDefinition;
+import org.opendaylight.yangtools.yang.model.api.UsesNode;
+
+final class OperationsContainerSchemaNode extends AbstractOperationDataSchemaNode implements ContainerSchemaNode {
+    // There is no need to intern this nor add a revision, as we are providing the corresponding context anyway
+    private static final @NonNull QName QNAME = QName.create(OperationsRestconfModule.NAMESPACE, "operations");
+    static final @NonNull SchemaPath PATH = SchemaPath.create(true, QNAME);
+
+    private final Map<QName, OperationsLeafSchemaNode> children;
+
+    OperationsContainerSchemaNode(final Collection<OperationsLeafSchemaNode> children) {
+        this.children = Maps.uniqueIndex(children, OperationsLeafSchemaNode::getQName);
+    }
+
+    @Override
+    public QName getQName() {
+        return QNAME;
+    }
+
+    @Override
+    public SchemaPath getPath() {
+        return PATH;
+    }
+
+    @Override
+    @SuppressWarnings({ "unchecked", "rawtypes" })
+    public Collection<DataSchemaNode> getChildNodes() {
+        return (Collection) children.values();
+    }
+
+    @Override
+    public Optional<DataSchemaNode> findDataChildByName(final QName name) {
+        return Optional.ofNullable(children.get(requireNonNull(name)));
+    }
+
+    @Override
+    public Set<TypeDefinition<?>> getTypeDefinitions() {
+        return Collections.emptySet();
+    }
+
+    @Override
+    public Set<GroupingDefinition> getGroupings() {
+        return Collections.emptySet();
+    }
+
+    @Override
+    public Set<UsesNode> getUses() {
+        return Collections.emptySet();
+    }
+
+    @Override
+    public Set<AugmentationSchemaNode> getAvailableAugmentations() {
+        return Collections.emptySet();
+    }
+
+    @Override
+    public Set<NotificationDefinition> getNotifications() {
+        return Collections.emptySet();
+    }
+
+    @Override
+    public Set<ActionDefinition> getActions() {
+        return Collections.emptySet();
+    }
+
+    @Override
+    public Collection<MustDefinition> getMustConstraints() {
+        return Collections.emptySet();
+    }
+
+    @Override
+    public boolean isPresenceContainer() {
+        return false;
+    }
+}
