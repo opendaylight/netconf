3f89f1c15cc0e42c587f215d929e6d92b14d6567
Robert Varga <robert.varga@pantheon.tech>
2020-04-27 20:45:25 +0200

    Upgrade mina-sshd to 2.4.0
    https://github.com/apache/mina-sshd/blob/master/docs/changes/2.4.0.md
    
    Change-Id: I26a5cdc8eb699c16c86352156b55ffa331762c5e
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- netconf/mdsal-netconf-ssh/src/main/java/org/opendaylight/netconf/ssh/RemoteNetconfCommand.java
+++ netconf/mdsal-netconf-ssh/src/main/java/org/opendaylight/netconf/ssh/RemoteNetconfCommand.java
@@ -21,7 +21,6 @@ import java.io.OutputStream;
 import java.net.InetSocketAddress;
 import java.net.SocketAddress;
 import org.opendaylight.netconf.api.messages.NetconfHelloMessageAdditionalHeader;
-import org.opendaylight.netconf.shaded.sshd.common.NamedFactory;
 import org.opendaylight.netconf.shaded.sshd.common.io.IoInputStream;
 import org.opendaylight.netconf.shaded.sshd.common.io.IoOutputStream;
 import org.opendaylight.netconf.shaded.sshd.server.Environment;
@@ -30,6 +29,7 @@ import org.opendaylight.netconf.shaded.sshd.server.channel.ChannelSession;
 import org.opendaylight.netconf.shaded.sshd.server.command.AsyncCommand;
 import org.opendaylight.netconf.shaded.sshd.server.command.Command;
 import org.opendaylight.netconf.shaded.sshd.server.session.ServerSession;
+import org.opendaylight.netconf.shaded.sshd.server.subsystem.SubsystemFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -163,7 +163,7 @@ public class RemoteNetconfCommand implements AsyncCommand {
         return netconfHelloMessageAdditionalHeader.getAddress();
     }
 
-    public static class NetconfCommandFactory implements NamedFactory<Command> {
+    public static class NetconfCommandFactory implements SubsystemFactory {
 
         public static final String NETCONF = "netconf";
 
@@ -182,7 +182,7 @@ public class RemoteNetconfCommand implements AsyncCommand {
         }
 
         @Override
-        public RemoteNetconfCommand create() {
+        public Command createSubsystem(final ChannelSession channel) {
             return new RemoteNetconfCommand(clientBootstrap, localAddress);
         }
     }
