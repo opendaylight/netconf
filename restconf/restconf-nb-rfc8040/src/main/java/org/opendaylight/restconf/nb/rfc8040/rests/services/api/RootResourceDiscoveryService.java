a9d6e7875620dcb5f8ad0f49485c61b0f64b8ce0
wsx25289 <10200860@zte.com.cn>
2020-04-28 03:58:36 -0400

    Implement RESTCONF Extensibility (discovery)
    Added Root Resource Discovery mechanism according to RFC8040.
    
    JIRA: NETCONF-499
    Change-Id: I414ebda7e34db7cd468fb0e488bcac457d7cf7ff
    Signed-off-by: Wsx25289 <10200860@zte.com.cn>
    Signed-off-by: Iaroslav <iaroslav.kholiavko@pantheon.tech>

---
+++ restconf/restconf-nb-rfc8040/src/main/java/org/opendaylight/restconf/nb/rfc8040/rests/services/api/RootResourceDiscoveryService.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2014 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.restconf.nb.rfc8040.rests.services.api;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
+import org.opendaylight.restconf.nb.rfc8040.Rfc8040;
+import org.opendaylight.restconf.nb.rfc8040.utils.RestconfConstants;
+
+
+/**
+ * Controller for determining the root of the RESTCONF API.
+ * See: https://tools.ietf.org/html/rfc8040#section-3.1
+ */
+public interface RootResourceDiscoveryService {
+    @GET
+    @Path("/host-meta")
+    @Produces({Rfc8040.MediaTypes.XRD + RestconfConstants.XML})
+    Response readXrdData();
+
+    @GET
+    @Path("/host-meta.json")
+    @Produces({Rfc8040.MediaTypes.XRD + RestconfConstants.JSON})
+    Response readJsonData();
+}
