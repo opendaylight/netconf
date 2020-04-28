8b70b2159b38e3d2ecd17e1ea7a62ef601d5692b
wsx25289 <10200860@zte.com.cn>
2020-04-28 03:58:36 -0400

    Implement RESTCONF Extensibility (discovery)
    Added Root Resource Discovery mechanism according to RFC8040.
    
    JIRA: NETCONF-499
    Change-Id: I414ebda7e34db7cd468fb0e488bcac457d7cf7ff
    Signed-off-by: Wsx25289 <10200860@zte.com.cn>
    Signed-off-by: Iaroslav <iaroslav.kholiavko@pantheon.tech>

---
+++ restconf/restconf-nb-rfc8040/src/main/java/org/opendaylight/restconf/nb/rfc8040/services/wrapper/RootResourceDiscoveryServiceImpl.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2014 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.restconf.nb.rfc8040.services.wrapper;
+
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+import org.opendaylight.restconf.nb.rfc8040.rests.services.api.RootResourceDiscoveryService;
+import org.opendaylight.restconf.nb.rfc8040.utils.RestconfConstants;
+
+@Path("/")
+public class RootResourceDiscoveryServiceImpl implements RootResourceDiscoveryService {
+
+    private final Response response;
+
+    private RootResourceDiscoveryServiceImpl() {
+        response = Response.status(200)
+                .entity("<XRD xmlns='http://docs.oasis-open.org/ns/xri/xrd-1.0'>\n"
+                        + "     <Link rel='restconf' href='/" + RestconfConstants.BASE_URI_PATTERN + "'/>\n"
+                        + "</XRD>").build();
+    }
+
+    private static final class InstanceHolder {
+        private static final RootResourceDiscoveryServiceImpl INSTANCE = new RootResourceDiscoveryServiceImpl();
+
+        private InstanceHolder() {
+        }
+    }
+
+    public static RootResourceDiscoveryServiceImpl getInstance() {
+        return RootResourceDiscoveryServiceImpl.InstanceHolder.INSTANCE;
+    }
+
+    @Override
+    public Response readXrdData() {
+        return response;
+    }
+}
