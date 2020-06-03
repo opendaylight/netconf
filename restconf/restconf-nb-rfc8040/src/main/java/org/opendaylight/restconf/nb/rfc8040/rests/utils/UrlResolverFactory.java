497715104b94596266972a53b058dd797ea79c37
Lukas Baca <lbaca@luminanetworks.com>
2020-06-03 12:19:13 +0200

    RESTCONF RFC8040 compliance: SSE support
    Add implementation of Server-sent events according to RFC8040(https://tools.ietf.org/html/rfc8040#section-6) and make it default behavior.
    Previous behavior(websocket) is possible turn on by configuration @see restconf-bp.xml
    Add example of config file which is located etc/org.opendaylight.restconf.nb.rfc8040.cfg
    Add JavaDoc
    
    WIP:
    -for proper work is need merg patch AAA-199(https://git.opendaylight.org/gerrit/c/aaa/+/89860)
    -will need update CSIT tests
    
    JIRA: NETCONF-679
    
    Change-Id: I3be8f25e10512b9972dbd42d3e48c03719e0704f
    Signed-off-by: Lukas Baca <lbaca@luminanetworks.com>

---
+++ restconf/restconf-nb-rfc8040/src/main/java/org/opendaylight/restconf/nb/rfc8040/rests/utils/UrlResolverFactory.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2020 Lumina Networks, Inc. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.restconf.nb.rfc8040.rests.utils;
+
+import org.opendaylight.restconf.nb.rfc8040.streams.Configuration;
+
+/**
+ * Factory which create proper {@link UrlResolver} from {@link Configuration}.
+ *
+ */
+public class UrlResolverFactory {
+
+    private final boolean useSSE;
+
+    public UrlResolverFactory(Configuration conf) {
+        this.useSSE = conf.isUseSSE();
+    }
+
+    public UrlResolver build() {
+        if (useSSE) {
+            return new UrlResolverSSE();
+        } else {
+            return new UrlResolverWS();
+        }
+    }
+
+    public static UrlResolver getFactory(Configuration conf) {
+        return new UrlResolverFactory(conf).build();
+    }
+}
