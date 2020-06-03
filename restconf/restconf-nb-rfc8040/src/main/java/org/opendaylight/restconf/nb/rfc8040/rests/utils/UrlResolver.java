6e5ed067077196f497de3483a55ac87212be4c4b
Lukas Baca <lbaca@luminanetworks.com>
2020-06-03 12:19:13 +0200

    RESTCONF RFC8040 compliance: SSE support
    Add implementation of Server-sent events according to RFC8040(https://tools.ietf.org/html/rfc8040#section-6) and make it default behavior.
    Previous behavior(websocket) is possible turn on by configuration @see restconf-bp.xml
    Add example of config file which is located etc/org.opendaylight.restconf.nb.rfc8040.cfg
    Add JavaDoc
    
    WIP:
    -for proper work is need merg patch AAA-199(https://git.opendaylight.org/gerrit/c/aaa/+/89860)
    -will need update CSIT tests
    
    JIRA: NETCONF-679
    
    Change-Id: I3be8f25e10512b9972dbd42d3e48c03719e0704f
    Signed-off-by: Lukas Baca <lbaca@luminanetworks.com>

---
+++ restconf/restconf-nb-rfc8040/src/main/java/org/opendaylight/restconf/nb/rfc8040/rests/utils/UrlResolver.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2020 Lumina Networks, Inc. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.restconf.nb.rfc8040.rests.utils;
+
+import java.net.URI;
+import javax.ws.rs.core.UriInfo;
+/**
+ * Interface for prepare stream URL.
+ *
+ */
+public interface UrlResolver {
+
+    /**
+     * Prepare URL from base name and stream name. 
+     * @param uriInfo base URL information
+     * @param streamName name of stream for create 
+     * @return final URL
+     */
+    public abstract URI prepareUriByStreamName(UriInfo uriInfo, String streamName);
+}
---
+++ restconf/restconf-nb-rfc8040/src/main/java/org/opendaylight/restconf/nb/rfc8040/rests/utils/UrlResolverWS.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2020 Lumina Networks, Inc. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.restconf.nb.rfc8040.rests.utils;
+
+import java.net.URI;
+import javax.ws.rs.core.UriBuilder;
+import javax.ws.rs.core.UriInfo;
+import org.opendaylight.restconf.nb.rfc8040.utils.RestconfConstants;
+
+/**
+ * Implementation of {@link UrlResolver} for Web sockets.
+ *
+ */
+public class UrlResolverWS implements UrlResolver {
+
+    @Override
+    public URI prepareUriByStreamName(final UriInfo uriInfo, final String streamName) {
+        final String scheme = uriInfo.getAbsolutePath().getScheme();
+        final UriBuilder uriBuilder = uriInfo.getBaseUriBuilder();
+        switch (scheme) {
+            case "https":
+                // Secured HTTP goes to Secured WebSockets
+                uriBuilder.scheme("wss");
+                break;
+            case "http":
+            default:
+                // Unsecured HTTP and others go to unsecured WebSockets
+                uriBuilder.scheme("ws");
+        }
+        return uriBuilder.replacePath(RestconfConstants.BASE_URI_PATTERN + '/' + streamName).build();
+    }
+
+}
