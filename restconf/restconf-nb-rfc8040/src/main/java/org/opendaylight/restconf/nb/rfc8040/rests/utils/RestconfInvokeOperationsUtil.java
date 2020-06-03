49f98b8bda823bf8b7e60690f02e48d249e42041
ajay_dp001 <ajay.deep.singh@est.tech>
2020-06-03 09:57:28 +0530

    Fix nested YANG 1.1 Action invocation
    Invocation of action fails if the action is defined as an augmentation.
    As it turns out the logic can be very much simplified by just cutting
    the last item from YangInstanceIdentifier, as that points to the parent
    (and hence context) of the action.
    
    JIRA: NETCONF-696
    Change-Id: I7b03b2b1ebdd7bffbfab5291552c430700ff2850
    Signed-off-by: ajay_dp001 <ajay.deep.singh@est.tech>

--- restconf/restconf-nb-rfc8040/src/main/java/org/opendaylight/restconf/nb/rfc8040/rests/utils/RestconfInvokeOperationsUtil.java
+++ restconf/restconf-nb-rfc8040/src/main/java/org/opendaylight/restconf/nb/rfc8040/rests/utils/RestconfInvokeOperationsUtil.java
@@ -8,8 +8,6 @@
 package org.opendaylight.restconf.nb.rfc8040.rests.utils;
 
 import com.google.common.util.concurrent.ListenableFuture;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Optional;
 import java.util.concurrent.CancellationException;
 import javax.ws.rs.core.Response.Status;
@@ -28,7 +26,6 @@ import org.opendaylight.restconf.nb.rfc8040.handlers.ActionServiceHandler;
 import org.opendaylight.restconf.nb.rfc8040.handlers.RpcServiceHandler;
 import org.opendaylight.yangtools.yang.common.YangConstants;
 import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
-import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.PathArgument;
 import org.opendaylight.yangtools.yang.data.api.schema.ContainerNode;
 import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNode;
 import org.opendaylight.yangtools.yang.data.impl.schema.ImmutableNodes;
@@ -141,9 +138,7 @@ public final class RestconfInvokeOperationsUtil {
         if (!mountPointService.isPresent()) {
             throw new RestconfDocumentedException("DomAction service is missing.");
         }
-
-        return prepareActionResult(mountPointService.get().invokeAction(schemaPath,
-            prepareDataTreeId(yangIId, schemaPath), data));
+        return prepareActionResult(mountPointService.get().invokeAction(schemaPath, prepareDataTreeId(yangIId), data));
     }
 
     /**
@@ -158,9 +153,9 @@ public final class RestconfInvokeOperationsUtil {
      * @return {@link DOMActionResult}
      */
     public static DOMActionResult invokeAction(final ContainerNode data, final SchemaPath schemaPath,
-            final ActionServiceHandler actionServiceHandler, final YangInstanceIdentifier yangIId) {
-        return prepareActionResult(actionServiceHandler.get().invokeAction(schemaPath,
-            prepareDataTreeId(yangIId, schemaPath), data));
+        final ActionServiceHandler actionServiceHandler, final YangInstanceIdentifier yangIId) {
+        return prepareActionResult(actionServiceHandler.get().invokeAction(schemaPath, prepareDataTreeId(yangIId),
+            data));
     }
 
     /**
@@ -205,22 +200,9 @@ public final class RestconfInvokeOperationsUtil {
      *
      * @param yangIId
      *             {@link YangInstanceIdentifier}
-     * @param schemaPath
-     *              {@link SchemaPath}
      * @return {@link DOMDataTreeIdentifier} domDataTreeIdentifier
      */
-    private static DOMDataTreeIdentifier prepareDataTreeId(final YangInstanceIdentifier yangIId,
-            final SchemaPath schemaPath) {
-        final List<PathArgument> pathArg = new ArrayList<>();
-        for (PathArgument path : yangIId.getPathArguments()) {
-            if (path.getNodeType().getLocalName().equals(schemaPath.getLastComponent().getLocalName())) {
-                break;
-            }
-            pathArg.add(path);
-        }
-        YangInstanceIdentifier yangInstanceIdentifier = YangInstanceIdentifier.builder().append(pathArg).build();
-        DOMDataTreeIdentifier domDataTreeIdentifier = new DOMDataTreeIdentifier(LogicalDatastoreType.OPERATIONAL,
-            yangInstanceIdentifier);
-        return domDataTreeIdentifier;
+    private static DOMDataTreeIdentifier prepareDataTreeId(final YangInstanceIdentifier yangIId) {
+        return new DOMDataTreeIdentifier(LogicalDatastoreType.OPERATIONAL, yangIId.getParent());
     }
 }
