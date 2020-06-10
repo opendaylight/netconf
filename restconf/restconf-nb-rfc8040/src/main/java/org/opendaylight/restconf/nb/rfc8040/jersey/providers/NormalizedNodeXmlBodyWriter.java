b40a4e5914204cbc0e146a9d45be08cb7314fcc2
Vladyslav Marchenko <vladyslav.marchenko@pantheon.tech>
2020-06-10 12:19:19 +0300

    Create NetconfDataTreeService with base and additional operations for netconf
    Service provided by netconf mountpoints. It provides an api for all netconf operations, instead of mapping mdsal api to netconf requests. Restconf now uses the new NetconfDataTreeService when available. If no - fall back to DOMDataBroker.
    
    JIRA: NETCONF-312
    Signed-off-by: Vladyslav Marchenko <vladyslav.marchenko@pantheon.tech>
    Change-Id: I354449883b54c8a3a4e7ffdc12d7d532c8120b6a

--- restconf/restconf-nb-rfc8040/src/main/java/org/opendaylight/restconf/nb/rfc8040/jersey/providers/NormalizedNodeXmlBodyWriter.java
+++ restconf/restconf-nb-rfc8040/src/main/java/org/opendaylight/restconf/nb/rfc8040/jersey/providers/NormalizedNodeXmlBodyWriter.java
@@ -14,6 +14,7 @@ import java.lang.annotation.Annotation;
 import java.lang.reflect.Type;
 import java.nio.charset.StandardCharsets;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 import javanet.staxutils.IndentingXMLStreamWriter;
 import javax.ws.rs.Produces;
@@ -83,6 +84,11 @@ public class NormalizedNodeXmlBodyWriter implements MessageBodyWriter<Normalized
         final InstanceIdentifierContext<?> pathContext = context.getInstanceIdentifierContext();
         if (context.getData() == null) {
             return;
+        }
+        if (httpHeaders != null) {
+            for (final Map.Entry<String, Object> entry : context.getNewHeaders().entrySet()) {
+                httpHeaders.add(entry.getKey(), entry.getValue());
+            }
         }
 
         XMLStreamWriter xmlWriter;
