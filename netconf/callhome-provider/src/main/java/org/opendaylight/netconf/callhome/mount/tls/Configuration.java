769efb497316e04f737470b85fca77cb4e9dc932
Oleksii Mozghovyi <oleksii.mozghovyi@bluefield.tech>
2020-06-15 23:07:04 +0300

    Add Call-Home implementation for the TLS secure-transport
    JIRA: NETCONF-5
    Change-Id: I666444b7c187d2afbc8869a32331cba4a1393add
    Signed-off-by: Oleksii Mozghovyi <oleksii.mozghovyi@pantheon.tech>

---
+++ netconf/callhome-provider/src/main/java/org/opendaylight/netconf/callhome/mount/tls/Configuration.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright (c) 2020 Pantheon Technologies, s.r.o. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.netconf.callhome.mount.tls;
+
+public class Configuration {
+
+    private String host;
+    private Integer port;
+    private Integer timeout;
+    private Integer maxConnections;
+
+    public Configuration() {
+        // empty constructor
+    }
+
+    public String getHost() {
+        return host;
+    }
+
+    public void setHost(final String host) {
+        this.host = host;
+    }
+
+    public Integer getPort() {
+        return port;
+    }
+
+    public void setPort(final Integer port) {
+        this.port = port;
+    }
+
+    public Integer getTimeout() {
+        return timeout;
+    }
+
+    public void setTimeout(final Integer timeout) {
+        this.timeout = timeout;
+    }
+
+    public Integer getMaxConnections() {
+        return maxConnections;
+    }
+
+    public void setMaxConnections(final Integer maxConnections) {
+        this.maxConnections = maxConnections;
+    }
+}
