8f15ca2303d8f00f6f6b3440419b0bbdcf4248ca
Tomas Cere <tomas.cere@pantheon.tech>
2020-06-25 11:00:47 +0200

    Make ListenerAdapter serialize JSON directly
    Change websocket notification serialization to serialize
    to json directly instead of first serializing to xml and
    subsequently converting to json.
    While we are here fixup the websocket notification format
    so it actually conforms to the sal-remote.yang model and
    encode namespaces according to the json-ietf standard
    instead of bundling an xmlns node in the json ouptut.
    
    JIRA: NETCONF-471
    Change-Id: I1b90c99980c0692b217523640bd6f67050f21e14
    Signed-off-by: Tomas Cere <tomas.cere@pantheon.tech>

---
+++ restconf/restconf-common/src/main/java/org/opendaylight/restconf/common/formatters/NotificationFormatterFactory.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2020 Pantheon.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.restconf.common.formatters;
+
+import javax.xml.xpath.XPathExpressionException;
+import org.opendaylight.mdsal.dom.api.DOMNotification;
+
+public interface NotificationFormatterFactory extends EventFormatterFactory<DOMNotification> {
+    @Override
+    NotificationFormatter getFormatter();
+
+    @Override
+    NotificationFormatter getFormatter(String xpathFilter) throws XPathExpressionException;
+}
