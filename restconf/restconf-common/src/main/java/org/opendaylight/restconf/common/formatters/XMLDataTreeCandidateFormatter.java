57c476fce6ac7f7ad2cfdaa10d25b1736cf917f5
Tomas Cere <tomas.cere@pantheon.tech>
2020-06-25 11:00:47 +0200

    Make ListenerAdapter serialize JSON directly
    Change websocket notification serialization to serialize
    to json directly instead of first serializing to xml and
    subsequently converting to json.
    While we are here fixup the websocket notification format
    so it actually conforms to the sal-remote.yang model and
    encode namespaces according to the json-ietf standard
    instead of bundling an xmlns node in the json ouptut.
    
    JIRA: NETCONF-471
    Change-Id: I1b90c99980c0692b217523640bd6f67050f21e14
    Signed-off-by: Tomas Cere <tomas.cere@pantheon.tech>

---
+++ restconf/restconf-common/src/main/java/org/opendaylight/restconf/common/formatters/XMLDataTreeCandidateFormatter.java
@@ -0,0 +1,89 @@
+/*
+ * Copyright (c) 2020 Pantheon.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.restconf.common.formatters;
+
+import static org.opendaylight.restconf.common.formatters.NotificationFormatter.XML_OUTPUT_FACTORY;
+import static org.opendaylight.restconf.common.formatters.XMLNotificationFormatter.NOTIFICATION_ELEMENT;
+import static org.opendaylight.restconf.common.formatters.XMLNotificationFormatter.NOTIFICATION_NAMESPACE;
+
+import java.io.IOException;
+import java.io.StringWriter;
+import java.time.Instant;
+import java.util.Collection;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamWriter;
+import javax.xml.xpath.XPathExpressionException;
+import org.opendaylight.restconf.common.serializer.XmlDataTreeCandidateSerializer;
+import org.opendaylight.yangtools.yang.data.api.schema.tree.DataTreeCandidate;
+import org.opendaylight.yangtools.yang.model.api.EffectiveModelContext;
+
+public final class XMLDataTreeCandidateFormatter extends DataTreeCandidateFormatter {
+    private static final XMLDataTreeCandidateFormatter INSTANCE = new XMLDataTreeCandidateFormatter();
+
+    public static final DataTreeCandidateFormatterFactory FACTORY =
+        new DataTreeCandidateFormatterFactory() {
+            @Override
+            public XMLDataTreeCandidateFormatter getFormatter(final String xpathFilter)
+                    throws XPathExpressionException {
+                return new XMLDataTreeCandidateFormatter(xpathFilter);
+            }
+
+            @Override
+            public XMLDataTreeCandidateFormatter getFormatter() {
+                return INSTANCE;
+            }
+        };
+
+    private XMLDataTreeCandidateFormatter() {
+    }
+
+    private XMLDataTreeCandidateFormatter(final String xpathFilter) throws XPathExpressionException {
+        super(xpathFilter);
+    }
+
+    @Override
+    String createText(EffectiveModelContext schemaContext, Collection<DataTreeCandidate> input, Instant now,
+                      boolean leafNodesOnly, boolean skipData)
+            throws Exception {
+        StringWriter writer = new StringWriter();
+
+        final XMLStreamWriter xmlStreamWriter;
+        try {
+            xmlStreamWriter = XML_OUTPUT_FACTORY.createXMLStreamWriter(writer);
+            xmlStreamWriter.setDefaultNamespace(NOTIFICATION_NAMESPACE);
+
+            xmlStreamWriter.writeStartElement(NOTIFICATION_NAMESPACE, NOTIFICATION_ELEMENT);
+            xmlStreamWriter.writeDefaultNamespace(NOTIFICATION_NAMESPACE);
+
+            xmlStreamWriter.writeStartElement("eventTime");
+            xmlStreamWriter.writeCharacters(toRFC3339(now));
+            xmlStreamWriter.writeEndElement();
+
+            xmlStreamWriter.writeStartElement("data-changed-notification");
+
+            final XmlDataTreeCandidateSerializer serializer =
+                    new XmlDataTreeCandidateSerializer(schemaContext, xmlStreamWriter);
+
+            for (final DataTreeCandidate candidate : input) {
+                serializer.serialize(candidate, leafNodesOnly, skipData);
+            }
+
+            // data-changed-notification
+            xmlStreamWriter.writeEndElement();
+
+            // notification
+            xmlStreamWriter.writeEndElement();
+        } catch (XMLStreamException e) {
+            throw new IOException("Failed to write notification content", e);
+        }
+
+
+        return writer.toString();
+    }
+}
---
+++ restconf/restconf-common/src/main/java/org/opendaylight/restconf/common/formatters/XMLNotificationFormatter.java
@@ -0,0 +1,81 @@
+/*
+ * Copyright (c) 2020 Pantheon.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.restconf.common.formatters;
+
+import java.io.IOException;
+import java.io.StringWriter;
+import java.time.Instant;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamWriter;
+import javax.xml.xpath.XPathExpressionException;
+import org.opendaylight.mdsal.dom.api.DOMNotification;
+import org.opendaylight.yangtools.yang.data.api.schema.stream.NormalizedNodeStreamWriter;
+import org.opendaylight.yangtools.yang.data.api.schema.stream.NormalizedNodeWriter;
+import org.opendaylight.yangtools.yang.data.codec.xml.XMLStreamNormalizedNodeStreamWriter;
+import org.opendaylight.yangtools.yang.model.api.EffectiveModelContext;
+
+public class XMLNotificationFormatter extends NotificationFormatter {
+    private static final XMLNotificationFormatter INSTANCE = new XMLNotificationFormatter();
+
+    static final String NOTIFICATION_NAMESPACE = "urn:ietf:params:xml:ns:netconf:notification:1.0";
+    static final String NOTIFICATION_ELEMENT = "notification";
+
+    public static final NotificationFormatterFactory FACTORY = new NotificationFormatterFactory() {
+        @Override
+        public XMLNotificationFormatter getFormatter(final String xpathFilter) throws XPathExpressionException {
+            return new XMLNotificationFormatter(xpathFilter);
+        }
+
+        @Override
+        public XMLNotificationFormatter getFormatter() {
+            return INSTANCE;
+        }
+    };
+
+    public XMLNotificationFormatter() {
+    }
+
+    public XMLNotificationFormatter(String xpathFilter) throws XPathExpressionException {
+        super(xpathFilter);
+    }
+
+    @Override
+    String createText(EffectiveModelContext schemaContext, DOMNotification input, Instant now,
+                      boolean leafNodesOnly, boolean skipData)
+            throws IOException {
+        final StringWriter writer = new StringWriter();
+        try {
+            final XMLStreamWriter xmlStreamWriter = XML_OUTPUT_FACTORY.createXMLStreamWriter(writer);
+            xmlStreamWriter.setDefaultNamespace(NOTIFICATION_NAMESPACE);
+
+            xmlStreamWriter.writeStartElement(NOTIFICATION_NAMESPACE, NOTIFICATION_ELEMENT);
+            xmlStreamWriter.writeDefaultNamespace(NOTIFICATION_NAMESPACE);
+
+            xmlStreamWriter.writeStartElement("eventTime");
+            xmlStreamWriter.writeCharacters(toRFC3339(now));
+            xmlStreamWriter.writeEndElement();
+
+            final NormalizedNodeStreamWriter nnStreamWriter =
+                    XMLStreamNormalizedNodeStreamWriter.create(xmlStreamWriter, schemaContext, input.getType());
+
+            final NormalizedNodeWriter nnWriter = NormalizedNodeWriter.forStreamWriter(nnStreamWriter);
+            nnWriter.write(input.getBody());
+            nnWriter.flush();
+
+            xmlStreamWriter.writeEndElement();
+            xmlStreamWriter.writeEndDocument();
+            xmlStreamWriter.flush();
+
+            nnWriter.close();
+
+            return writer.toString();
+        } catch (XMLStreamException e) {
+            throw new IOException("Failed to write notification content", e);
+        }
+    }
+}
