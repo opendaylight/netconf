a322dcb7114eb0e893ca69795883d8023a195024
Tomas Cere <tomas.cere@pantheon.tech>
2020-06-25 11:00:47 +0200

    Make ListenerAdapter serialize JSON directly
    Change websocket notification serialization to serialize
    to json directly instead of first serializing to xml and
    subsequently converting to json.
    While we are here fixup the websocket notification format
    so it actually conforms to the sal-remote.yang model and
    encode namespaces according to the json-ietf standard
    instead of bundling an xmlns node in the json ouptut.
    
    JIRA: NETCONF-471
    Change-Id: I1b90c99980c0692b217523640bd6f67050f21e14
    Signed-off-by: Tomas Cere <tomas.cere@pantheon.tech>

---
+++ restconf/restconf-common/src/main/java/org/opendaylight/restconf/common/serializer/XmlDataTreeCandidateSerializer.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright (c) 2020 Pantheon.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.restconf.common.serializer;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamWriter;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.PathArgument;
+import org.opendaylight.yangtools.yang.data.api.schema.stream.NormalizedNodeStreamWriter;
+import org.opendaylight.yangtools.yang.data.api.schema.stream.NormalizedNodeWriter;
+import org.opendaylight.yangtools.yang.data.api.schema.tree.DataTreeCandidateNode;
+import org.opendaylight.yangtools.yang.data.codec.xml.XMLStreamNormalizedNodeStreamWriter;
+import org.opendaylight.yangtools.yang.model.api.EffectiveModelContext;
+import org.opendaylight.yangtools.yang.model.api.SchemaPath;
+
+public class XmlDataTreeCandidateSerializer extends AbstractWebsocketSerializer<Exception> {
+
+    private final EffectiveModelContext context;
+    private final XMLStreamWriter xmlWriter;
+
+    public XmlDataTreeCandidateSerializer(EffectiveModelContext context, XMLStreamWriter xmlWriter) {
+        this.context = context;
+        this.xmlWriter = xmlWriter;
+    }
+
+    @Override
+    void serializeData(Collection<PathArgument> nodePath, DataTreeCandidateNode candidate, boolean skipData)
+            throws Exception {
+        final SchemaPath path = SchemaPath.create(nodePath.stream()
+                .filter(p -> !(p instanceof YangInstanceIdentifier.NodeIdentifierWithPredicates))
+                .map(PathArgument::getNodeType).collect(Collectors.toList()), true);
+        final NormalizedNodeStreamWriter nodeStreamWriter =
+                XMLStreamNormalizedNodeStreamWriter.create(xmlWriter, context, path.getParent());
+
+        xmlWriter.writeStartElement("data-changed-event");
+        serializePath(nodePath);
+
+        if (!skipData && candidate.getDataAfter().isPresent()) {
+            xmlWriter.writeStartElement("data");
+            NormalizedNodeWriter nnWriter = NormalizedNodeWriter.forStreamWriter(nodeStreamWriter);
+            nnWriter.write(candidate.getDataAfter().get());
+            nnWriter.flush();
+
+            xmlWriter.writeEndElement();
+        }
+        serializeOperation(candidate);
+
+        xmlWriter.writeEndElement();
+    }
+
+    @Override
+    public void serializePath(Collection<PathArgument> pathArguments) throws XMLStreamException {
+        xmlWriter.writeStartElement("path");
+        xmlWriter.writeCharacters(convertPath(pathArguments));
+        xmlWriter.writeEndElement();
+    }
+
+    @Override
+    public void serializeOperation(DataTreeCandidateNode candidate) throws XMLStreamException {
+        xmlWriter.writeStartElement("operation");
+        xmlWriter.writeCharacters(modificationTypeToOperation(candidate, candidate.getModificationType()));
+        xmlWriter.writeEndElement();
+    }
+}
