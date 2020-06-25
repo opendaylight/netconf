802e872c91267833e9ba0df474d03d7e58f56a8b
Tomas Cere <tomas.cere@pantheon.tech>
2020-06-25 11:00:47 +0200

    Make ListenerAdapter serialize JSON directly
    Change websocket notification serialization to serialize
    to json directly instead of first serializing to xml and
    subsequently converting to json.
    While we are here fixup the websocket notification format
    so it actually conforms to the sal-remote.yang model and
    encode namespaces according to the json-ietf standard
    instead of bundling an xmlns node in the json ouptut.
    
    JIRA: NETCONF-471
    Change-Id: I1b90c99980c0692b217523640bd6f67050f21e14
    Signed-off-by: Tomas Cere <tomas.cere@pantheon.tech>

--- restconf/restconf-nb-rfc8040/src/test/java/org/opendaylight/restconf/nb/rfc8040/streams/listeners/JsonNotificationListenerTest.java
+++ restconf/restconf-nb-rfc8040/src/test/java/org/opendaylight/restconf/nb/rfc8040/streams/listeners/JsonNotificationListenerTest.java
@@ -7,7 +7,6 @@
  */
 package org.opendaylight.restconf.nb.rfc8040.streams.listeners;
 
-import static java.util.Objects.requireNonNull;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
@@ -15,6 +14,7 @@ import static org.mockito.Mockito.when;
 
 import com.google.common.collect.Lists;
 import java.net.URI;
+import java.time.Instant;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Optional;
@@ -42,9 +42,13 @@ import org.opendaylight.yangtools.yang.data.api.schema.MapEntryNode;
 import org.opendaylight.yangtools.yang.data.api.schema.MapNode;
 import org.opendaylight.yangtools.yang.model.api.EffectiveModelContext;
 import org.opendaylight.yangtools.yang.model.api.stmt.SchemaNodeIdentifier.Absolute;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @RunWith(MockitoJUnitRunner.StrictStubs.class)
-public class NotificationListenerTest {
+public class JsonNotificationListenerTest {
+    private static final Logger LOG = LoggerFactory.getLogger(JsonNotificationListenerTest.class);
+
     private static final QNameModule MODULE = QNameModule.create(URI.create("notifi:mod"), Revision.of("2016-11-23"));
 
     private static EffectiveModelContext SCHEMA_CONTEXT;
@@ -72,6 +76,8 @@ public class NotificationListenerTest {
         when(notificationData.getBody()).thenReturn(notifiBody);
 
         final String result = prepareJson(notificationData, schemaPathNotifi);
+
+        LOG.info("json result: {}", result);
 
         assertTrue(result.contains("ietf-restconf:notification"));
         assertTrue(result.contains("event-time"));
@@ -216,9 +222,10 @@ public class NotificationListenerTest {
         return child;
     }
 
-    private static String prepareJson(final DOMNotification notificationData, final Absolute schemaPathNotifi) {
+    private static String prepareJson(final DOMNotification notificationData, final Absolute schemaPathNotifi)
+            throws Exception {
         final NotificationListenerAdapter notifiAdapter = ListenersBroker.getInstance().registerNotificationListener(
-                schemaPathNotifi, "stream-name", NotificationOutputType.JSON);
-        return requireNonNull(notifiAdapter.prepareJson(SCHEMA_CONTEXT, notificationData));
+                schemaPathNotifi, "json-stream", NotificationOutputType.JSON);
+        return notifiAdapter.formatter.eventData(SCHEMA_CONTEXT, notificationData, Instant.now(), false, false).get();
     }
 }
