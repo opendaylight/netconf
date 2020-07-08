af3e13f689713b4ab0a43e873741ded5f5805d36
Robert Varga <robert.varga@pantheon.tech>
2020-07-08 15:16:57 +0200

    Eliminate RestconfConstants.SLASH
    This is a rather useless constant, which is just muddling things
    up. Inline it into users, clearing some very surprising ways of use.
    
    Change-Id: I7d98f670238c32fab32681d6084a9fe28bc8f0b3
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- restconf/restconf-nb-rfc8040/src/main/java/org/opendaylight/restconf/nb/rfc8040/utils/parser/YangInstanceIdentifierSerializer.java
+++ restconf/restconf-nb-rfc8040/src/main/java/org/opendaylight/restconf/nb/rfc8040/utils/parser/YangInstanceIdentifierSerializer.java
@@ -15,7 +15,6 @@ import java.util.Map.Entry;
 import org.opendaylight.restconf.common.errors.RestconfDocumentedException;
 import org.opendaylight.restconf.common.errors.RestconfError.ErrorTag;
 import org.opendaylight.restconf.common.errors.RestconfError.ErrorType;
-import org.opendaylight.restconf.nb.rfc8040.utils.RestconfConstants;
 import org.opendaylight.yangtools.concepts.Serializer;
 import org.opendaylight.yangtools.yang.common.QName;
 import org.opendaylight.yangtools.yang.common.QNameModule;
@@ -76,12 +75,12 @@ public final class YangInstanceIdentifierSerializer {
             if (!arg.getNodeType().getModule().equals(parentModule)) {
                 // append slash if it is not the first path argument
                 if (path.length() > 0) {
-                    path.append(RestconfConstants.SLASH);
+                    path.append('/');
                 }
 
                 path.append(prefixForNamespace(arg.getNodeType(), schemaContext)).append(':');
             } else {
-                path.append(RestconfConstants.SLASH);
+                path.append('/');
             }
 
             if (arg instanceof NodeIdentifierWithPredicates) {
