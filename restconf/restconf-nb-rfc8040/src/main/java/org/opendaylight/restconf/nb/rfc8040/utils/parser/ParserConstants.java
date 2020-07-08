8a7686520779b883d4bc0302704276772e521343
Robert Varga <robert.varga@pantheon.tech>
2020-07-08 13:34:06 +0200

    Remove org.opendaylight.restconf.nb.rfc8040.utils.parser.builder
    This package holds only a few constants, which are better
    redistributed and kept where they are actually used.
    
    Change-Id: I0fc614072e17df463615b1ea3544a3fae0fd659d
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ restconf/restconf-nb-rfc8040/src/main/java/org/opendaylight/restconf/nb/rfc8040/utils/parser/ParserConstants.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2020 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.restconf.nb.rfc8040.utils.parser;
+
+import com.google.common.base.CharMatcher;
+
+/**
+ * Various constants related to RFC3986 (URI) and RFC7950 (YANG) parsing in the context of RFC8040.
+ */
+final class ParserConstants {
+    // Reserved characters as per https://tools.ietf.org/html/rfc3986#section-2.2
+    static final String RFC3986_RESERVED_CHARACTERS = ":/?#[]@" + "!$&'()*+,;="
+            // FIXME: this space should not be here, but that was a day-0 bug and we have asserts on this
+            + " ";
+
+    // First character of RFC7950 "identifier" rule
+    static final CharMatcher YANG_IDENTIFIER_START =
+            CharMatcher.inRange('A', 'Z').or(CharMatcher.inRange('a', 'z').or(CharMatcher.is('_'))).precomputed();
+    // Subsequent characters of RFC7950 "identifier" rule
+    static final CharMatcher YANG_IDENTIFIER_PART =
+            YANG_IDENTIFIER_START.or(CharMatcher.inRange('0', '9')).or(CharMatcher.anyOf("-.")).precomputed();
+
+    private ParserConstants() {
+        // Hidden on purpose
+    }
+}
