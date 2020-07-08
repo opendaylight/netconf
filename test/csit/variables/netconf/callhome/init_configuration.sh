cb0c2d821ae95fab5bcedbac8724f98a7735e284
Oleksii Mozghovyi <oleksii.mozghovyi@pantheon.tech>
2020-07-08 23:18:14 +0300

    Add test cases for call-home (devices connected with TLS transport)
    Verification for the TLS transport based on the following flow:
    - Generates certificate for the device
    - Installs certificates into the netopeer2 device
    - Installs certificates into the OpenDaylight controller
    - Checks if the device is recognized by the OpenDaylight CallHome feature and
    mounted successfully
    
    JIRA: NETCONF-5
    Change-Id: Ibe08c61ab6956d83748ae95be776a1be8c1a1d78
    Signed-off-by: Oleksii Mozghovyi <oleksii.mozghovyi@pantheon.tech>
    Signed-off-by: Vladyslav Marchenko <vladyslav.marchenko@pantheon.tech>

--- test/csit/variables/netconf/callhome/init_configuration.sh
+++ test/csit/variables/netconf/callhome/init_configuration.sh
@@ -11,11 +11,17 @@ CONFIG_PATH='/root/configuration-files'
 # Configuration from the following modules will be imported to the sysrepo datastore.
 # 1. Each module's configuration file should be placed under the $CONFIG_PATH folder with .xml extension, e.g. ietf-truststore.xml.
 # 2. Script will replace all variable placeholders with corresponding environment variables, e.g. $CALL_HOME_SERVER_IP.
-# Please, note that following environment variables will be set according to the provided keys(under the $CONFIG_PATH folder):
+# Please, note that following environment variables will be set according to the provided keys and certificates(under the $CONFIG_PATH/certs folder):
 #  - $NP_PRIVKEY
 #  - $NP_PUBKEY
+#  - $NP_CA_CERT
+#  - $NP_CLIENT_CERT
+#  - $NP_SERVER_PRIVATE_KEY
+#  - $NP_SERVER_PUBLIC_KEY
+#  - $NP_SERVER_CERTIFICATE
+#  - $NP_CLIENT_CERT_FINGERPRINT
 # 3. Modules must be provided in the correct order.
-MODULES_LIST=("ietf-keystore" "ietf-netconf-server")
+MODULES_LIST=("ietf-truststore" "ietf-keystore" "ietf-netconf-server")
 
 import_module()
 {
@@ -50,6 +56,15 @@ SAVEIFS=$IFS
 IFS=
 export NP_PRIVKEY=`cat /etc/ssh/ssh_host_rsa_key | sed -u '1d; $d'`
 export NP_PUBKEY=`openssl rsa -in /etc/ssh/ssh_host_rsa_key -pubout | sed -u '1d; $d'`
+
+if [ -d "$CONFIG_PATH/certs" ]; then
+    export NP_CA_CERT=`sed -u '1d; $d' $CONFIG_PATH/certs/ca.pem`
+    export NP_CLIENT_CERT=`sed -u '1d; $d' $CONFIG_PATH/certs/client.crt`
+    export NP_SERVER_PRIVATE_KEY=`sed -u '1d; $d' $CONFIG_PATH/certs/server.key`
+    export NP_SERVER_PUBLIC_KEY=`sed -u '1d; $d' $CONFIG_PATH/certs/server.pub`
+    export NP_SERVER_CERTIFICATE=`sed -u '1d; $d' $CONFIG_PATH/certs/server.crt`
+    export NP_CLIENT_CERT_FINGERPRINT=`openssl x509 -noout -fingerprint -in $CONFIG_PATH/certs/ca.pem -sha1 | cut -d'=' -f2-`
+fi
 IFS=$SAVEIFS
 
 # Import all provided configuration files for netopeer
