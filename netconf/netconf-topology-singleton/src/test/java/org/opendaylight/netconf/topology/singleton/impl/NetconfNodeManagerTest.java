bc8f0f5ec11559d07892632612630697d8235009
Vladyslav Marchenko <vladyslav.marchenko@pantheon.tech>
2020-07-09 17:27:22 +0300

    Add support of new a NetconfDataTreeService in clustered netconf
    In NETCONF-312 was introduced a new service (NetconfDataTreeService) for exposing operations in their NETCONF-native form.
    We need a proxy implementation of this new service for the slave which would forward the requests to master.
    
    JIRA: NETCONF-706
    Signed-off-by: Vladyslav Marchenko <vladyslav.marchenko@pantheon.tech>
    Change-Id: I58cdc6791fb4476e64b474e95c12292a3a52e771

--- netconf/netconf-topology-singleton/src/test/java/org/opendaylight/netconf/topology/singleton/impl/NetconfNodeManagerTest.java
+++ netconf/netconf-topology-singleton/src/test/java/org/opendaylight/netconf/topology/singleton/impl/NetconfNodeManagerTest.java
@@ -59,6 +59,7 @@ import org.opendaylight.mdsal.dom.api.DOMMountPoint;
 import org.opendaylight.mdsal.dom.api.DOMMountPointService;
 import org.opendaylight.mdsal.dom.api.DOMNotificationService;
 import org.opendaylight.mdsal.dom.api.DOMRpcService;
+import org.opendaylight.netconf.dom.api.NetconfDataTreeService;
 import org.opendaylight.netconf.sal.connect.api.NetconfDeviceSchemasResolver;
 import org.opendaylight.netconf.sal.connect.netconf.NetconfDevice;
 import org.opendaylight.netconf.sal.connect.util.RemoteDeviceId;
@@ -115,6 +116,9 @@ public class NetconfNodeManagerTest extends AbstractBaseSchemasTest {
 
     @Mock
     private DataBroker mockDataBroker;
+
+    @Mock
+    private NetconfDataTreeService netconfService;
 
     @Mock
     private DOMDataBroker mockDeviceDataBroker;
@@ -386,8 +390,8 @@ public class NetconfNodeManagerTest extends AbstractBaseSchemasTest {
 
     private void initializeMaster() {
         TestKit kit = new TestKit(masterSystem);
-
-        testMasterActorRef.tell(new CreateInitialMasterActorData(mockDeviceDataBroker, SOURCE_IDENTIFIERS,
+        testMasterActorRef.tell(new CreateInitialMasterActorData(mockDeviceDataBroker, netconfService,
+            SOURCE_IDENTIFIERS,
                 mockRpcService, mockActionService), kit.getRef());
 
         kit.expectMsgClass(MasterActorDataInitialized.class);
