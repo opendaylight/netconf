1ebc08e7307fdbf18a54a832ec27cac259b75411
Vladyslav Marchenko <vladyslav.marchenko@pantheon.tech>
2020-07-09 17:27:22 +0300

    Add support of new a NetconfDataTreeService in clustered netconf
    In NETCONF-312 was introduced a new service (NetconfDataTreeService) for exposing operations in their NETCONF-native form.
    We need a proxy implementation of this new service for the slave which would forward the requests to master.
    
    JIRA: NETCONF-706
    Signed-off-by: Vladyslav Marchenko <vladyslav.marchenko@pantheon.tech>
    Change-Id: I58cdc6791fb4476e64b474e95c12292a3a52e771

---
+++ netconf/netconf-topology-singleton/src/main/java/org/opendaylight/netconf/topology/singleton/messages/netconf/GetConfigRequest.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2020 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.netconf.topology.singleton.messages.netconf;
+
+import org.opendaylight.netconf.topology.singleton.messages.transactions.ReadActorMessage;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
+
+public class GetConfigRequest implements ReadActorMessage {
+    private static final long serialVersionUID = 1L;
+
+    private final YangInstanceIdentifier path;
+
+    public GetConfigRequest(final YangInstanceIdentifier path) {
+        this.path = path;
+    }
+
+    public YangInstanceIdentifier getPath() {
+        return path;
+    }
+}
