bdd71505b92999ea46d6aa683314100439cd8008
Robert Varga <robert.varga@pantheon.tech>
2020-07-10 12:46:02 +0200

    Fix OperationsCreator javadoc
    checkstyle is a bit more touchy, fix it up.
    
    Change-Id: I39ad689d10ba508f2284cbc6cdf9c915ab5796c9
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- netconf/tools/netconf-testtool/src/main/java/org/opendaylight/netconf/test/tool/operations/OperationsCreator.java
+++ netconf/tools/netconf-testtool/src/main/java/org/opendaylight/netconf/test/tool/operations/OperationsCreator.java
@@ -5,7 +5,6 @@
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
-
 package org.opendaylight.netconf.test.tool.operations;
 
 import java.util.Set;
@@ -14,24 +13,17 @@ import org.opendaylight.netconf.impl.SessionIdProvider;
 import org.opendaylight.netconf.mapping.api.NetconfOperationService;
 
 /**
- * Creator which enables testtool users to inject own.
- * implementation of {@link NetconfOperationService}
+ * Creator which enables testtool users to inject own. implementation of {@link NetconfOperationService}
  */
 public interface OperationsCreator {
-
     /**
      * Creates instance of {@link NetconfOperationService} based on caller context.
-     * @param capabilities
-     *   Model capabilities.
-     * @param idProvider
-     *   Provider's session context.
-     * @param netconfSessionIdForReporting
-     *   Netconf SessionId for reporting
-     * @return
-     *   Instance of {@link NetconfOperationService}.
+     *
+     * @param capabilities Model capabilities.
+     * @param idProvider Provider's session context.
+     * @param netconfSessionIdForReporting Netconf SessionId for reporting
+     * @return Instance of {@link NetconfOperationService}.
      */
-    NetconfOperationService getNetconfOperationService(Set<Capability> capabilities,
-        SessionIdProvider idProvider,
+    NetconfOperationService getNetconfOperationService(Set<Capability> capabilities, SessionIdProvider idProvider,
         String netconfSessionIdForReporting);
-
 }
