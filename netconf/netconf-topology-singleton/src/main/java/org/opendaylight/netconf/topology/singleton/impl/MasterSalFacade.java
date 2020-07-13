3d36bd1dcba32d759b5b864d6ddeaa5ea31c4163
Tibor Král <tibor.kral@pantheon.tech>
2020-07-13 10:46:42 +0200

    Add session-id to the operational datastore
    Expose the session-id provided by the device in the HELLO-MESSAGE.
    Can be used to retrive session-specific info of the device.
    
    JIRA: NETCONF-710
    Change-Id: I1995e825cf32462232e164727a469a7b47d3acd0
    Signed-off-by: Tibor Král <tibor.kral@pantheon.tech>

--- netconf/netconf-topology-singleton/src/main/java/org/opendaylight/netconf/topology/singleton/impl/MasterSalFacade.java
+++ netconf/netconf-topology-singleton/src/main/java/org/opendaylight/netconf/topology/singleton/impl/MasterSalFacade.java
@@ -111,7 +111,7 @@ class MasterSalFacade implements AutoCloseable, RemoteDeviceHandler<NetconfSessi
     @Override
     public void onDeviceDisconnected() {
         LOG.info("Device {} disconnected - unregistering master mount point", id);
-        salProvider.getTopologyDatastoreAdapter().updateDeviceData(false, new NetconfDeviceCapabilities());
+        salProvider.getTopologyDatastoreAdapter().updateDeviceDataToConnecting(new NetconfDeviceCapabilities());
         unregisterMasterMountPoint();
     }
 
@@ -179,7 +179,7 @@ class MasterSalFacade implements AutoCloseable, RemoteDeviceHandler<NetconfSessi
         final String masterAddress = Cluster.get(actorSystem).selfAddress().toString();
         LOG.debug("{}: updateDeviceData with master address {}", id, masterAddress);
         salProvider.getTopologyDatastoreAdapter().updateClusteredDeviceData(true, masterAddress,
-                netconfSessionPreferences.getNetconfDeviceCapabilities());
+                netconfSessionPreferences.getNetconfDeviceCapabilities(), netconfSessionPreferences.getSessionId());
     }
 
     private void unregisterMasterMountPoint() {
