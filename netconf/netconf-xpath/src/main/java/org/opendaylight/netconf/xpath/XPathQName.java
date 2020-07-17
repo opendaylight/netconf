bc25c84020f58b4d756c55e9dfbcb73620608628
Jakub Toth <jakubt4@gmail.com>
2020-07-18 01:07:48 +0200

    Introduce XPath Context based on the RFC 6241
    Based on the RFC 6241, the Netconf plugin can use the XPath for
    get and get-config operations. Commit introduce the context for
    XPath which contains the method for creating XPath from
    YangInstanceIdentifier and additional parameters with mapped
    namespaces by special prefix in List.
    
    JIRA: NETCONF-698
    
    Change-Id: I5793160a7acda53cec5cd25bc94828d957c33e22
    Signed-off-by: Jakub Toth <jakubt4@gmail.com>

---
+++ netconf/netconf-xpath/src/main/java/org/opendaylight/netconf/xpath/XPathQName.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright (c) 2020 Lumina Networks, Inc. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.netconf.xpath;
+
+import static java.util.Objects.requireNonNull;
+
+import org.eclipse.jdt.annotation.NonNull;
+
+/**
+ * The QName consists of local name of element and XML namespace, but for XPath,
+ * it was added namespace prefix to it.
+ */
+public class XPathQName {
+
+    private final String namespace;
+    private final String namespacePrefix;
+    private final String name;
+
+    public XPathQName(@NonNull final String namespace, @NonNull final String namespacePrefix,
+            @NonNull final String name) {
+        this.namespace = requireNonNull(namespace);
+        this.namespacePrefix = requireNonNull(namespacePrefix);
+        this.name = requireNonNull(name);
+    }
+
+    /**
+     * Allows to get a local name of a element.
+     *
+     * @return local name
+     */
+    public @NonNull String getName() {
+        return name;
+    }
+
+    /**
+     * Allows to get a namespace of a element.
+     *
+     * @return namespace
+     */
+    public @NonNull String getNamespace() {
+        return namespace;
+    }
+
+    /**
+     * Allows to get a prefix for namespace of a element.
+     *
+     * @return namespace prefix
+     */
+    public @NonNull String getNamespacePrefix() {
+        return namespacePrefix;
+    }
+}
