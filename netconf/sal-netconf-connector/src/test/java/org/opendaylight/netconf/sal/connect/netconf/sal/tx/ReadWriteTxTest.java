eeb939f566afcb848e8e6143cef126f599f81f63
Sarguna Dharani <sarguna.dharani@verizon.com>
2020-08-03 15:18:46 +0530

    Check for datastore lock and reject config if locked
    JIRA: NETCONF-705
    Signed-off-by: Sarguna Dharani <sarguna.dharani@verizon.com>
    Change-Id: Ia9ae697d33dfdf3b05d28db945f650bb3aa65e05

--- netconf/sal-netconf-connector/src/test/java/org/opendaylight/netconf/sal/connect/netconf/sal/tx/ReadWriteTxTest.java
+++ netconf/sal-netconf-connector/src/test/java/org/opendaylight/netconf/sal/connect/netconf/sal/tx/ReadWriteTxTest.java
@@ -13,23 +13,31 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import com.google.common.util.concurrent.FluentFuture;
+import java.util.Optional;
+import java.util.concurrent.ExecutionException;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.opendaylight.mdsal.common.api.LogicalDatastoreType;
 import org.opendaylight.mdsal.dom.api.DOMDataTreeReadTransaction;
 import org.opendaylight.mdsal.dom.api.DOMDataTreeWriteTransaction;
+import org.opendaylight.netconf.api.xml.XmlNetconfConstants;
 import org.opendaylight.yangtools.util.concurrent.FluentFutures;
+import org.opendaylight.yangtools.yang.common.RpcError;
 import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
 import org.opendaylight.yangtools.yang.data.api.schema.ContainerNode;
 import org.opendaylight.yangtools.yang.data.api.schema.LeafNode;
+import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNode;
 
 public class ReadWriteTxTest {
     @Mock
     private DOMDataTreeReadTransaction delegateReadTx;
     @Mock
     private DOMDataTreeWriteTransaction delegateWriteTx;
+    @Mock
+    private RpcError rpcError;
     private ReadWriteTx tx;
 
     @Before
@@ -66,6 +74,16 @@ public class ReadWriteTxTest {
         verify(delegateReadTx).read(LogicalDatastoreType.CONFIGURATION, TxTestUtils.getContainerId());
     }
 
+    @Test(expected = ExecutionException.class)
+    public void readNegativeTest() throws Exception {
+        ReadWriteTx readWriteTx = Mockito.spy(tx);
+        Mockito.doReturn(rpcError).when(readWriteTx).getResultsFutures();
+        when(rpcError.getTag()).thenReturn(XmlNetconfConstants.LOCK_DENIED);
+        final FluentFuture<Optional<NormalizedNode<?, ?>>> read =
+            readWriteTx.read(LogicalDatastoreType.CONFIGURATION, TxTestUtils.getContainerId());
+        read.get();
+    }
+
     @Test
     public void exists() throws Exception {
         final YangInstanceIdentifier id = TxTestUtils.getContainerId();
@@ -73,6 +91,16 @@ public class ReadWriteTxTest {
             FluentFutures.immediateTrueFluentFuture());
         final FluentFuture<Boolean> exists = tx.exists(LogicalDatastoreType.CONFIGURATION, id);
         assertTrue(exists.get());
+    }
+
+    @Test(expected = ExecutionException.class)
+    public void existsNegativeTest() throws Exception {
+        final YangInstanceIdentifier id = TxTestUtils.getContainerId();
+        ReadWriteTx readWriteTx = Mockito.spy(tx);
+        Mockito.doReturn(rpcError).when(readWriteTx).getResultsFutures();
+        when(rpcError.getTag()).thenReturn(XmlNetconfConstants.LOCK_DENIED);
+        final FluentFuture<Boolean> exists = readWriteTx.exists(LogicalDatastoreType.CONFIGURATION, id);
+        exists.get();
     }
 
     @Test
