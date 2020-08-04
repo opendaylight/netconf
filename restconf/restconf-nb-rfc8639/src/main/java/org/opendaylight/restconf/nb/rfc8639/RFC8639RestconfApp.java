f0a1abb278a3abfd6851be8916a7fb2ae395118a
Ivan Hrasko <ivan.hrasko@pantheon.tech>
2020-08-04 16:57:56 +0200

    Subscribe to NETCONF streams
    Signed-off-by: Ivan Hrasko <ivan.hrasko@pantheon.tech>
    Change-Id: I42e54ae48aa8150740da015a99bc9102451ddf40

--- restconf/restconf-nb-rfc8639/src/main/java/org/opendaylight/restconf/nb/rfc8639/RFC8639RestconfApp.java
+++ restconf/restconf-nb-rfc8639/src/main/java/org/opendaylight/restconf/nb/rfc8639/RFC8639RestconfApp.java
@@ -23,7 +23,9 @@ import org.opendaylight.mdsal.dom.api.DOMNotificationService;
 import org.opendaylight.mdsal.dom.api.DOMRpcIdentifier;
 import org.opendaylight.mdsal.dom.api.DOMRpcImplementation;
 import org.opendaylight.mdsal.dom.api.DOMRpcProviderService;
+import org.opendaylight.mdsal.dom.api.DOMRpcService;
 import org.opendaylight.mdsal.dom.api.DOMSchemaService;
+import org.opendaylight.restconf.nb.rfc8040.handlers.RpcServiceHandler;
 import org.opendaylight.restconf.nb.rfc8040.handlers.SchemaContextHandler;
 import org.opendaylight.restconf.nb.rfc8040.handlers.TransactionChainHandler;
 import org.opendaylight.restconf.nb.rfc8639.layer.services.impl.SubscribedNotificationsImpl;
@@ -67,17 +69,19 @@ public class RFC8639RestconfApp extends Application {
     private final DOMSchemaService domSchemaService;
     private final DOMMountPointService domMountPointService;
     private final DOMRpcProviderService domRpcProviderService;
+    private final RpcServiceHandler domRpcServiceHandler;
 
     public RFC8639RestconfApp(final TransactionChainHandler transactionChainHandler,
             final SchemaContextHandler schemaContextHandler, final DOMNotificationService domNotificationService,
             final DOMSchemaService domSchemaService, final DOMMountPointService domMountPointService,
-            final DOMRpcProviderService domRpcProviderService) {
+            final DOMRpcProviderService domRpcProviderService, final RpcServiceHandler domRpcServiceHandler) {
         this.transactionChainHandler = requireNonNull(transactionChainHandler);
         this.schemaContextHandler = requireNonNull(schemaContextHandler);
         this.domNotificationService = requireNonNull(domNotificationService);
         this.domSchemaService = requireNonNull(domSchemaService);
         this.domMountPointService = requireNonNull(domMountPointService);
         this.domRpcProviderService = requireNonNull(domRpcProviderService);
+        this.domRpcServiceHandler = domRpcServiceHandler;
     }
 
     public void init() {
@@ -86,7 +90,7 @@ public class RFC8639RestconfApp extends Application {
 
         final DOMRpcImplementation establishSubscriptionRpc = new EstablishSubscriptionRpc(NOTIFICATIONS_HOLDER,
                 REPLAY_BUFFER, domNotificationService, domSchemaService, domMountPointService, transactionChainHandler,
-                executorService);
+                executorService, domRpcServiceHandler.get());
         domRpcProviderService.registerRpcImplementation(establishSubscriptionRpc, RPC_ESTABLISH);
 
         final DOMRpcImplementation modifySubscriptionRpc = new ModifySubscriptionRpc(NOTIFICATIONS_HOLDER,
