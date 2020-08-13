48efb5349b6e037c4b73ed8908778e7c3a47fa34
Ivan Hrasko <ivan.hrasko@pantheon.tech>
2020-08-13 16:53:05 +0200

    Register to listen to all notifications
    Signed-off-by: Ivan Hrasko <ivan.hrasko@pantheon.tech>
    Change-Id: I68308abd552f46e2b2d4d2c734c4616a3bde78c6

--- restconf/restconf-nb-rfc8639/src/main/java/org/opendaylight/restconf/nb/rfc8639/layer/services/impl/SubscribedNotificationsImpl.java
+++ restconf/restconf-nb-rfc8639/src/main/java/org/opendaylight/restconf/nb/rfc8639/layer/services/impl/SubscribedNotificationsImpl.java
@@ -9,32 +9,21 @@ package org.opendaylight.restconf.nb.rfc8639.layer.services.impl;
 
 import static java.util.Objects.requireNonNull;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.UriInfo;
 import org.glassfish.jersey.media.sse.EventOutput;
 import org.opendaylight.restconf.common.errors.RestconfDocumentedException;
 import org.opendaylight.restconf.common.errors.RestconfError;
-import org.opendaylight.restconf.nb.rfc8040.handlers.SchemaContextHandler;
 import org.opendaylight.restconf.nb.rfc8639.layer.services.api.SubscribedNotifications;
 import org.opendaylight.restconf.nb.rfc8639.layer.services.subscriptions.RegisteredNotificationWrapper;
 import org.opendaylight.restconf.nb.rfc8639.layer.services.subscriptions.SubscriptionsHolder;
-import org.opendaylight.restconf.nb.rfc8639.util.services.SubscribedNotificationsUtil;
 import org.opendaylight.yangtools.yang.common.Uint32;
-import org.opendaylight.yangtools.yang.model.api.NotificationDefinition;
 
 @Path("/")
 public class SubscribedNotificationsImpl implements SubscribedNotifications {
-    private static final Pattern PREFIXED_NOTIFICATION_STREAM_NAME_PATTERN = Pattern
-            .compile("[a-zA-Z_0-9\\-]+:[a-zA-Z_0-9\\-]+");
-
-    private final SchemaContextHandler schemaContextHandler;
     private final SubscriptionsHolder holder;
 
-    public SubscribedNotificationsImpl(final SchemaContextHandler schemaContextHandler,
-            final SubscriptionsHolder holder) {
-        this.schemaContextHandler = requireNonNull(schemaContextHandler);
+    public SubscribedNotificationsImpl(final SubscriptionsHolder holder) {
         this.holder = requireNonNull(holder);
     }
 
@@ -53,26 +42,6 @@ public class SubscribedNotificationsImpl implements SubscribedNotifications {
         if (notificationSubscription == null) {
             throw new RestconfDocumentedException("Notification stream subscription with id "
                     + subscriptionId + " does not exist.",
-                    RestconfError.ErrorType.PROTOCOL, RestconfError.ErrorTag.INVALID_VALUE);
-        }
-
-        final NotificationDefinition streamSubscriptionNotificationDef = notificationSubscription
-                .getSubscriptionNotificationListener().getNotificationDefinition();
-        final String modulePrefixAndName = SubscribedNotificationsUtil.qNameToModulePrefixAndName(
-                streamSubscriptionNotificationDef.getQName(), schemaContextHandler.get());
-
-        final Matcher matcher = PREFIXED_NOTIFICATION_STREAM_NAME_PATTERN.matcher(streamName);
-        if (!matcher.matches()) {
-            throw new RestconfDocumentedException("Name of the notification stream in the request URI should be "
-                    + "prefixed with the name of the module to which it belongs."
-                    + "The correct form is module:notification." + "In case of this subscription it should be: "
-                    + modulePrefixAndName,
-                    RestconfError.ErrorType.PROTOCOL, RestconfError.ErrorTag.INVALID_VALUE);
-        }
-
-        if (!modulePrefixAndName.equals(streamName)) {
-            throw new RestconfDocumentedException("Notification stream subscription with id "
-                    + subscriptionId + " does not belong to stream: '" + streamName + "'.",
                     RestconfError.ErrorType.PROTOCOL, RestconfError.ErrorTag.INVALID_VALUE);
         }
 
