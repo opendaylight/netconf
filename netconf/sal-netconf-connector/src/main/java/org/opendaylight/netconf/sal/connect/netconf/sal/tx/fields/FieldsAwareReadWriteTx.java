d0f8de92aff6add55c24bff54bca9e4deb98d7bb
Jaroslav Tóth <jtoth@frinx.io>
2020-10-15 08:17:25 +0200

    Added fields-aware NETCONF DOM API
    - Added option to specify fields that are read from NETCONF device.
      Currently it is possible to only read one whole subtree
      using get/get-config NETCONF RPC. However, NETCONF RFC allows
      more complex subtree filtering including multiple subtrees
      in single RPC.
    - It is implemented as:
      1) DOMDataBroker extension - option to create customized
         read-only, read-write transactions, and transaction chains.
      2) NetconfDataTreeService methods - using get(..) and
         getConfig(..) calls.
    
    Signed-off-by: Jaroslav Tóth <jtoth@frinx.io>
    Change-Id: I584bb9740f3ba6756ce283067198f9b344c75f9c

---
+++ netconf/sal-netconf-connector/src/main/java/org/opendaylight/netconf/sal/connect/netconf/sal/tx/fields/FieldsAwareReadWriteTx.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright © 2020 FRINX s.r.o. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.netconf.sal.connect.netconf.sal.tx.fields;
+
+import com.google.common.util.concurrent.FluentFuture;
+import java.util.List;
+import java.util.Optional;
+import org.opendaylight.mdsal.common.api.LogicalDatastoreType;
+import org.opendaylight.mdsal.dom.api.DOMDataTreeWriteTransaction;
+import org.opendaylight.netconf.api.dom.NetconfDOMFieldsReadTransaction;
+import org.opendaylight.netconf.api.dom.NetconfDOMFieldsReadWriteTransaction;
+import org.opendaylight.netconf.sal.connect.netconf.sal.tx.ReadWriteTx;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
+import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNode;
+
+public final class FieldsAwareReadWriteTx extends ReadWriteTx<NetconfDOMFieldsReadTransaction>
+        implements NetconfDOMFieldsReadWriteTransaction {
+
+    public FieldsAwareReadWriteTx(final NetconfDOMFieldsReadTransaction readTransaction,
+                                  final DOMDataTreeWriteTransaction writeTransaction) {
+        super(readTransaction, writeTransaction);
+    }
+
+    @Override
+    public FluentFuture<Optional<NormalizedNode<?, ?>>> read(final LogicalDatastoreType store,
+            final YangInstanceIdentifier path, final List<YangInstanceIdentifier> fields) {
+        return delegateReadTx.read(store, path, fields);
+    }
+}
