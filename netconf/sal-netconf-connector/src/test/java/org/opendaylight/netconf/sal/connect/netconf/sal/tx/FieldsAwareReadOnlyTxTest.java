a7f0764bed41fbdf8fe82eabcdf4f562dcce7391
Jaroslav Tóth <jtoth@frinx.io>
2020-10-19 09:28:13 +0200

    Implement subtree filtering using fields
    - Fields are used for selection of specific subtrees under
      parent path - this way it is possible to limit requested
      data to specific entities without the necessity to send
      multiple get/get-config RPCs.
    - Look at added unit tests to understand the functionality.
    
    JIRA: NETCONF-735
    Change-Id: I17cd364e11f8c8e61e5537fcff71a7ecedefdba3
    Signed-off-by: Jaroslav Tóth <jtoth@frinx.io>
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ netconf/sal-netconf-connector/src/test/java/org/opendaylight/netconf/sal/connect/netconf/sal/tx/FieldsAwareReadOnlyTxTest.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright © 2020 FRINX s.r.o. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.netconf.sal.connect.netconf.sal.tx;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.opendaylight.netconf.sal.connect.netconf.util.NetconfMessageTransformUtil.NETCONF_GET_CONFIG_QNAME;
+import static org.opendaylight.netconf.sal.connect.netconf.util.NetconfMessageTransformUtil.NETCONF_GET_QNAME;
+
+import java.net.InetSocketAddress;
+import java.util.List;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.opendaylight.mdsal.common.api.LogicalDatastoreType;
+import org.opendaylight.mdsal.dom.api.DOMRpcService;
+import org.opendaylight.mdsal.dom.spi.DefaultDOMRpcResult;
+import org.opendaylight.netconf.sal.connect.netconf.util.NetconfBaseOps;
+import org.opendaylight.netconf.sal.connect.util.RemoteDeviceId;
+import org.opendaylight.yangtools.rfc8528.data.api.MountPointContext;
+import org.opendaylight.yangtools.util.concurrent.FluentFutures;
+import org.opendaylight.yangtools.yang.common.QName;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
+import org.opendaylight.yangtools.yang.data.api.schema.ContainerNode;
+import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNode;
+
+@RunWith(MockitoJUnitRunner.StrictStubs.class)
+public class FieldsAwareReadOnlyTxTest {
+    @Mock
+    private DOMRpcService rpc;
+    @Mock
+    private NormalizedNode<?, ?> mockedNode;
+
+    @Test
+    public void testReadWithFields() {
+        doReturn(FluentFutures.immediateFluentFuture(new DefaultDOMRpcResult(mockedNode))).when(rpc)
+            .invokeRpc(any(QName.class), any(ContainerNode.class));
+
+        final NetconfBaseOps netconfOps = new NetconfBaseOps(rpc, mock(MountPointContext.class));
+        try (FieldsAwareReadOnlyTx readOnlyTx = new FieldsAwareReadOnlyTx(netconfOps,
+                new RemoteDeviceId("a", new InetSocketAddress("localhost", 196)))) {
+
+            readOnlyTx.read(LogicalDatastoreType.CONFIGURATION, YangInstanceIdentifier.empty(),
+                List.of(YangInstanceIdentifier.empty()));
+            verify(rpc).invokeRpc(eq(NETCONF_GET_CONFIG_QNAME), any(ContainerNode.class));
+
+            readOnlyTx.read(LogicalDatastoreType.OPERATIONAL, YangInstanceIdentifier.empty());
+            verify(rpc).invokeRpc(eq(NETCONF_GET_QNAME), any(ContainerNode.class));
+        }
+    }
+}
