27ef6d101b21610296e3cf4359a7a8f1335e55e9
Tibor Král <tibor.kral@pantheon.tech>
2020-10-26 04:03:02 +0100

    Add workaround for SSH connection issue related to SSHD-1028
    Provide custom version of Nio2Session which overrides the
    bugged method.
    
    JIRA: NETCONF-736
    Change-Id: I0ef091680a9bdbe6bcab889335bb5ff48e91b703
    Signed-off-by: Tibor Král <tibor.kral@pantheon.tech>

---
+++ netconf/netconf-netty-util/src/main/java/org/opendaylight/netconf/nettyutil/handler/ssh/sshd1028/NetconfNio2ServiceFactory.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) 2020 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.netconf.nettyutil.handler.ssh.sshd1028;
+
+import java.lang.reflect.Field;
+import java.nio.channels.AsynchronousChannelGroup;
+import org.opendaylight.netconf.shaded.sshd.common.FactoryManager;
+import org.opendaylight.netconf.shaded.sshd.common.io.IoConnector;
+import org.opendaylight.netconf.shaded.sshd.common.io.IoHandler;
+import org.opendaylight.netconf.shaded.sshd.common.io.nio2.Nio2ServiceFactory;
+import org.opendaylight.netconf.shaded.sshd.common.util.threads.CloseableExecutorService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Custom Nio2ServiceFactory which creates instances of NetconfNio2Connector instead of Nio2Connector.
+ * Should be removed when SSHD-1028 is fixed.
+ */
+public class NetconfNio2ServiceFactory extends Nio2ServiceFactory {
+    private static final Logger LOG = LoggerFactory.getLogger(NetconfNio2ServiceFactory.class);
+    private static final Field FIELD_GROUP;
+    static {
+        Field fieldGroup = null;
+        try {
+            fieldGroup = NetconfNio2ServiceFactory.class.getSuperclass().getDeclaredField("group");
+            fieldGroup.setAccessible(true);
+        } catch (NoSuchFieldException e) {
+            LOG.error("Cannot access the ChannelGroup from the " + "Nio2ServiceFactory");
+        }
+        FIELD_GROUP = fieldGroup;
+    }
+
+    public NetconfNio2ServiceFactory(final FactoryManager factoryManager, final CloseableExecutorService service) {
+        super(factoryManager, service);
+    }
+
+    @Override
+    public IoConnector createConnector(final IoHandler handler) {
+        if (FIELD_GROUP != null) {
+            try {
+                final AsynchronousChannelGroup group = (AsynchronousChannelGroup)FIELD_GROUP.get(this);
+                return autowireCreatedService(new NetconfNio2Connector(getFactoryManager(), handler, group));
+            } catch (IllegalAccessException e) {
+                LOG.error("NetconfNio2Connector cannot be instanciated. Creating default Nio2Connector instead.");
+            }
+        }
+
+        return super.createConnector(handler);
+    }
+}
