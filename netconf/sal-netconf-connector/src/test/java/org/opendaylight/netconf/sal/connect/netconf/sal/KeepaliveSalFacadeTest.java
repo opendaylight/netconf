28e7c205f1cc683c7f2fc8a69dd5fbfeb1e5513f
Robert Varga <robert.varga@pantheon.tech>
2020-11-17 21:01:31 +0100

    Refactor KeepaliveSalFacade time tracking
    This is very simple state tracking, but unfortunately it is utterly
    convoluted. Essentially want to be sending keepalives as long as there
    is not something outstanding already.
    
    JIRA: NETCONF-717
    Change-Id: I4ee045b4d3395572a59167948ea8843bcac584ce
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- netconf/sal-netconf-connector/src/test/java/org/opendaylight/netconf/sal/connect/netconf/sal/KeepaliveSalFacadeTest.java
+++ netconf/sal-netconf-connector/src/test/java/org/opendaylight/netconf/sal/connect/netconf/sal/KeepaliveSalFacadeTest.java
@@ -62,7 +62,7 @@ public class KeepaliveSalFacadeTest {
     private DOMRpcService proxyRpc;
 
     @Mock
-    private ScheduledFuture currentKeepalive;
+    private ScheduledFuture<?> currentKeepalive;
 
     private KeepaliveSalFacade keepaliveSalFacade;
 
@@ -71,17 +71,15 @@ public class KeepaliveSalFacadeTest {
         executorServiceSpy = Executors.newScheduledThreadPool(1);
 
         doNothing().when(listener).disconnect();
-        doNothing().when(underlyingSalFacade).onDeviceConnected(isNull(),
-            isNull(), any(DOMRpcService.class), isNull());
+        doNothing().when(underlyingSalFacade).onDeviceConnected(isNull(), isNull(), any(DOMRpcService.class), isNull());
 
         ScheduledExecutorService executorService = Executors.newScheduledThreadPool(1);
         executorServiceSpy = Mockito.spy(executorService);
 
-        doAnswer(
-            invocationOnMock -> {
-                invocationOnMock.callRealMethod();
-                return currentKeepalive;
-            }).when(executorServiceSpy).schedule(Mockito.<Runnable>any(), Mockito.anyLong(), any());
+        doAnswer(invocationOnMock -> {
+            invocationOnMock.callRealMethod();
+            return currentKeepalive;
+        }).when(executorServiceSpy).schedule(Mockito.<Runnable>any(), Mockito.anyLong(), any());
 
         keepaliveSalFacade =
                 new KeepaliveSalFacade(REMOTE_DEVICE_ID, underlyingSalFacade, executorServiceSpy, 1L, 1L);
@@ -144,11 +142,8 @@ public class KeepaliveSalFacadeTest {
 
     @Test
     public void testNonKeepaliveRpcFailure() throws Exception {
-        doAnswer(
-            invocationOnMock -> {
-                proxyRpc = (DOMRpcService) invocationOnMock.getArguments()[2];
-                return null;
-            }).when(underlyingSalFacade).onDeviceConnected(isNull(), isNull(), any(DOMRpcService.class), isNull());
+        doAnswer(invocation -> proxyRpc = invocation.getArgument(2))
+                .when(underlyingSalFacade).onDeviceConnected(isNull(), isNull(), any(DOMRpcService.class), isNull());
 
         doReturn(FluentFutures.immediateFailedFluentFuture(new IllegalStateException("illegal-state")))
                 .when(deviceRpc).invokeRpc(any(QName.class), any(ContainerNode.class));
