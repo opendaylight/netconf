305905a220ee7bde3c13a29010c25510842d343e
Iaroslav <iaroslav.kholiavko@pantheon.tech>
2021-02-10 12:31:08 +0200

    EndToEnd test tool
    Added EndToEnd test tool for adding routes to test device using REST.
    Part of feature: features-netconf-testing
    
    JIRA: NETCONF-759
    Change-Id: Iba141c91a315b3d13d1890b8315dd17bb899cda9
    Signed-off-by: Iaroslav <iaroslav.kholiavko@pantheon.tech>

---
+++ netconf/tools/netconf-test-perf/src/main/java/org/opendaylight/test/endtoend/EndtoendTestService.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2020 Pantheon Technologies, s.r.o. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.test.endtoend;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+import org.opendaylight.mdsal.binding.api.MountPointService;
+import org.opendaylight.mdsal.binding.api.RpcProviderService;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.ncmount.rev150105.NcmountService;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Singleton
+@Component(immediate = true)
+public class EndtoendTestService {
+    private static final Logger LOGGER = LoggerFactory.getLogger(EndtoendTestService.class);
+    private final RpcProviderService rpcProviderService;
+    private final MountPointService mountPointService;
+
+    @Inject
+    @Activate
+    public EndtoendTestService(final @Reference RpcProviderService rpcProviderService,
+                               final @Reference MountPointService mountPointService) {
+        this.rpcProviderService = rpcProviderService;
+        this.mountPointService = mountPointService;
+        rpcProviderService.registerRpcImplementation(NcmountService.class,
+                new NcmountServiceImpl(mountPointService));
+    }
+}
