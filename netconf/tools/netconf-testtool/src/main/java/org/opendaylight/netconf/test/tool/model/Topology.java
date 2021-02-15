e9f16bc66932cdb7a541bf645d847c2455dd368d
Illia <illia.ihushev@pantheon.tech>
2021-02-15 20:03:51 +0200

    Add GSON models for device registration
    Intention is to use the PATCH method for node registration(no matter if
    that's a single node registration or multiple nodes).
    
    Change-Id: Ia624cfe2f953f6f5dc7a4ebd0d7cfbb0c0185f1a
    Signed-off-by: Illia <illia.ihushev@pantheon.tech>

---
+++ netconf/tools/netconf-testtool/src/main/java/org/opendaylight/netconf/test/tool/model/Topology.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2020 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.netconf.test.tool.model;
+
+import com.google.gson.annotations.SerializedName;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Topology {
+    private String topologyId;
+
+    @SerializedName("node")
+    private List<Node> nodeList = new ArrayList<>();
+
+    public Topology() {
+
+    }
+
+    public Topology(final String topologyId) {
+        this.topologyId = topologyId;
+    }
+
+    public void addNode(final Node node) {
+        this.nodeList.add(node);
+    }
+
+    public String getTopologyId() {
+        return topologyId;
+    }
+
+    public void setTopologyId(String topologyId) {
+        this.topologyId = topologyId;
+    }
+
+    public List<Node> getNodeList() {
+        return nodeList;
+    }
+
+    public void setNodeList(List<Node> nodeList) {
+        this.nodeList = nodeList;
+    }
+}
