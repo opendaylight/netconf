435163b4ebcd54b337497d35e83124f14c39a987
Illia <illia.ihushev@pantheon.tech>
2021-02-22 13:39:30 +0200

    Refactor getThreadsPayloads method
    1. Migrate RESTCONF path from draft to RFC8040.
    2. Split ports into batches using GSON.
    3. Create request bodies not using node config template, but Node,
    Topology models.
    4. Request bodies consist solely of nodes. Wrap nodes with topology
    object.
    5. Replace ArrayList with List in method signature.
    6. Register devices with PATCH instead of POST.
    
    JIRA: NETCONF-758
    Change-Id: Ifd7c74e9b07bb56b84e6a1ffea0bf2809b85246c
    Signed-off-by: Illia <illia.ihushev@pantheon.tech>
    Signed-off-by: Ivan Hrasko <ivan.hrasko@pantheon.tech>

--- netconf/tools/netconf-testtool/src/main/java/org/opendaylight/netconf/test/tool/Main.java
+++ netconf/tools/netconf-testtool/src/main/java/org/opendaylight/netconf/test/tool/Main.java
@@ -5,7 +5,6 @@
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
-
 package org.opendaylight.netconf.test.tool;
 
 import ch.qos.logback.classic.Level;
@@ -28,7 +27,7 @@ public final class Main {
     private static final Logger LOG = LoggerFactory.getLogger(Main.class);
 
     private Main() {
-
+        // hidden on purpose
     }
 
     @SuppressWarnings("checkstyle:IllegalCatch")
@@ -51,17 +50,17 @@ public final class Main {
             }
             //if ODL controller ip is not set NETCONF devices will be started, but not registered at the controller
             if (params.controllerIp != null) {
-                final ArrayList<ArrayList<Execution.DestToPayload>> allThreadsPayloads = params
-                    .getThreadsPayloads(openDevices);
-                final ArrayList<Execution> executions = new ArrayList<>();
-                for (ArrayList<Execution.DestToPayload> payloads : allThreadsPayloads) {
+                final List<List<Execution.DestToPayload>> allThreadsPayloads = params.getThreadsPayloads(openDevices);
+                final List<Execution> executions = new ArrayList<>();
+                for (final List<Execution.DestToPayload> payloads : allThreadsPayloads) {
                     executions.add(new Execution(params, payloads));
                 }
                 final ExecutorService executorService = Executors.newFixedThreadPool(params.threadAmount);
                 final Stopwatch time = Stopwatch.createStarted();
-                List<Future<Void>> futures = executorService.invokeAll(executions, params.timeOut, TimeUnit.SECONDS);
+                final List<Future<Void>> futures = executorService.invokeAll(executions,
+                        params.timeOut, TimeUnit.SECONDS);
                 int threadNum = 0;
-                for (Future<Void> future : futures) {
+                for (final Future<Void> future : futures) {
                     threadNum++;
                     if (future.isCancelled()) {
                         LOG.info("{}. thread timed out.",threadNum);
@@ -76,7 +75,7 @@ public final class Main {
                 time.stop();
                 LOG.info("Time spent with configuration of devices: {}.",time);
             }
-        } catch (RuntimeException | InterruptedException e) {
+        } catch (final RuntimeException | InterruptedException e) {
             LOG.error("Unhandled exception", e);
             netconfDeviceSimulator.close();
             System.exit(1);
