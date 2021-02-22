3ed32d026fc52b9a18ef0972a5a3994b08161051
Illia <illia.ihushev@pantheon.tech>
2021-02-22 13:39:30 +0200

    Fix functionality of the test tool
    The NETCONF test tool functionality for automatic bulk
    registration of devices was broken for some time.
    
    In addition, we would like to switch to new RESTCONF
    implementation (RFC 8040).
    
    - Update installed feature to odl-netconf-nb-rfc8040.
    - Migrate RESTCONF path from draft to RFC8040.
    - Create request bodies using NormalizedNodes
    instead of using config templates.
    - Register devices with PATCH instead of POST
    to allow merging nodes into topology and prevent
    error for consequent batch requests.
    - Encapsulate TesttoolParameters fields.
    - Encapsulate perf/Parameters fields.
    - Refactor where appropriate.
    
    JIRA: NETCONF-758
    Change-Id: Ifd7c74e9b07bb56b84e6a1ffea0bf2809b85246c
    Signed-off-by: Illia <illia.ihushev@pantheon.tech>
    Signed-off-by: Ivan Hrasko <ivan.hrasko@pantheon.tech>

--- netconf/tools/netconf-testtool/src/main/java/org/opendaylight/netconf/test/tool/client/http/perf/RequestMessageUtils.java
+++ netconf/tools/netconf-testtool/src/main/java/org/opendaylight/netconf/test/tool/client/http/perf/RequestMessageUtils.java
@@ -12,18 +12,13 @@ import com.ning.http.client.Realm;
 import com.ning.http.client.Request;
 import org.opendaylight.netconf.test.tool.TestToolUtils;
 
-public final class RequestMessageUtils {
-
+final class RequestMessageUtils {
     private static final String PEER_KEY = "{PEERID}";
     private static final String INT_LEAF_KEY = "{INTLEAF}";
-
     private static final String PHYS_ADDR_PLACEHOLDER = "{PHYS_ADDR}";
-
     private static final String HOST_KEY = "{HOST}";
     private static final String PORT_KEY = "{PORT}";
     private static final String DEVICE_PORT_KEY = "{DEVICE_PORT}";
-
-
     private static final String DEST = "http://{HOST}:{PORT}";
 
     private static long macStart = 0xAABBCCDD0000L;
@@ -32,9 +27,8 @@ public final class RequestMessageUtils {
         // Hidden on purpose
     }
 
-    public static String prepareMessage(final int idi, final int idj, final String editContentString,
-                                        final int devicePort) {
-        StringBuilder messageBuilder = new StringBuilder(editContentString);
+    static String prepareMessage(final int idi, final int idj, final String editContentString, final int devicePort) {
+        final StringBuilder messageBuilder = new StringBuilder(editContentString);
         if (editContentString.contains(PEER_KEY)) {
             messageBuilder.replace(
                     messageBuilder.indexOf(PEER_KEY),
@@ -63,25 +57,25 @@ public final class RequestMessageUtils {
         return messageBuilder.toString();
     }
 
-    public static RestPerfClient.RequestData formPayload(Parameters parameters, String editContentString,
-                                                         int threadId, int requests) {
-        final int devicePort = parameters.sameDevice
-                ? parameters.devicePortRangeStart : parameters.devicePortRangeStart + threadId;
+    static RestPerfClient.RequestData formPayload(final Parameters parameters, final String editContentString,
+            final int threadId, final int requests) {
+        final int devicePort = parameters.isSameDevice()
+                ? parameters.getDevicePortRangeStart() : parameters.getDevicePortRangeStart() + threadId;
         final StringBuilder destBuilder = new StringBuilder(DEST);
         destBuilder.replace(
                 destBuilder.indexOf(HOST_KEY),
                 destBuilder.indexOf(HOST_KEY) + HOST_KEY.length(),
-                parameters.ip)
+                parameters.getIp())
                 .replace(
                         destBuilder.indexOf(PORT_KEY),
                         destBuilder.indexOf(PORT_KEY) + PORT_KEY.length(),
-                        parameters.port + "");
-        final StringBuilder suffixBuilder = new StringBuilder(parameters.destination);
+                        Integer.toString(parameters.getPort()));
+        final StringBuilder suffixBuilder = new StringBuilder(parameters.getDestination());
         if (suffixBuilder.indexOf(DEVICE_PORT_KEY) != -1) {
             suffixBuilder.replace(
                     suffixBuilder.indexOf(DEVICE_PORT_KEY),
                     suffixBuilder.indexOf(DEVICE_PORT_KEY) + DEVICE_PORT_KEY.length(),
-                    devicePort + "");
+                    Integer.toString(devicePort));
         }
         destBuilder.append(suffixBuilder);
 
@@ -90,23 +84,22 @@ public final class RequestMessageUtils {
                 threadId, devicePort, requests);
     }
 
-    public static Request formRequest(AsyncHttpClient asyncHttpClient, String url, Parameters params, String msg) {
-        AsyncHttpClient.BoundRequestBuilder requestBuilder = asyncHttpClient.preparePost(url)
+    static Request formRequest(final AsyncHttpClient asyncHttpClient, final String url,
+            final Parameters params, final String msg) {
+        final AsyncHttpClient.BoundRequestBuilder requestBuilder = asyncHttpClient.preparePost(url)
                 .addHeader("content-type", "application/json")
                 .addHeader("Accept", "application/xml")
                 .setBody(msg)
                 .setRequestTimeout(Integer.MAX_VALUE);
 
-        if (params.auth != null) {
+        if (params.getAuth() != null) {
             requestBuilder.setRealm(new Realm.RealmBuilder()
                     .setScheme(Realm.AuthScheme.BASIC)
-                    .setPrincipal(params.auth.get(0))
-                    .setPassword(params.auth.get(1))
+                    .setPrincipal(params.getAuth().get(0))
+                    .setPassword(params.getAuth().get(1))
                     .setUsePreemptiveAuth(true)
                     .build());
         }
         return requestBuilder.build();
     }
-
-
 }
