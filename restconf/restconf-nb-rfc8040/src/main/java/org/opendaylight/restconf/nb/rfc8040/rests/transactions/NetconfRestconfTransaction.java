dfc8fc1fd9cd4005339ea35f2660bb5fa2e46510
Oleksii Mozghovyi <oleksii.mozghovyi@pantheon.tech>
2021-02-24 22:11:04 +0200

    Fix lock/unlock/commit issues for the NetconfRestconfTransactions
    - Fix the problem when one transaction could suddenly remove
    configuration-lock created by another one.
    - Fix the problem when transaction without successful lock operation
    could execute edit-config operations
    - Fix the problem when failure to acquire a lock on a candidate could
    lead to unexpected unlocking of the running configuration
    - Fix the problem when commit didn't take into account results of
    previous operations
    - Execute operation futures in a chain to guarantee the correct order of
    operations inside the transaction
    
    // FIXME: fix netconf-topology-singleton handling
    
    JIRA: NETCONF-705
    Change-Id: I588c6e2788983aef20b92726463930038abde3c9
    Signed-off-by: Oleksii Mozghovyi <oleksii.mozghovyi@pantheon.tech>
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- restconf/restconf-nb-rfc8040/src/main/java/org/opendaylight/restconf/nb/rfc8040/rests/transactions/NetconfRestconfTransaction.java
+++ restconf/restconf-nb-rfc8040/src/main/java/org/opendaylight/restconf/nb/rfc8040/rests/transactions/NetconfRestconfTransaction.java
@@ -9,16 +9,32 @@ package org.opendaylight.restconf.nb.rfc8040.rests.transactions;
 
 import static java.util.Objects.requireNonNull;
 
+import com.google.common.collect.ImmutableList;
 import com.google.common.util.concurrent.FluentFuture;
+import com.google.common.util.concurrent.FutureCallback;
+import com.google.common.util.concurrent.Futures;
 import com.google.common.util.concurrent.ListenableFuture;
+import com.google.common.util.concurrent.MoreExecutors;
+import com.google.common.util.concurrent.SettableFuture;
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
+import java.util.StringJoiner;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
+import org.checkerframework.checker.nullness.qual.Nullable;
 import org.eclipse.jdt.annotation.NonNull;
 import org.opendaylight.mdsal.common.api.CommitInfo;
 import org.opendaylight.mdsal.common.api.LogicalDatastoreType;
 import org.opendaylight.mdsal.dom.api.DOMRpcResult;
+import org.opendaylight.mdsal.dom.spi.DefaultDOMRpcResult;
+import org.opendaylight.netconf.api.DocumentedException;
+import org.opendaylight.netconf.api.NetconfDocumentedException;
 import org.opendaylight.netconf.dom.api.NetconfDataTreeService;
+import org.opendaylight.yangtools.yang.common.RpcError;
 import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
 import org.opendaylight.yangtools.yang.data.api.schema.LeafSetNode;
 import org.opendaylight.yangtools.yang.data.api.schema.MapNode;
@@ -26,38 +42,47 @@ import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNode;
 import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNodeContainer;
 import org.opendaylight.yangtools.yang.data.impl.schema.ImmutableNodes;
 import org.opendaylight.yangtools.yang.model.api.SchemaContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 final class NetconfRestconfTransaction extends RestconfTransaction {
-    private final NetconfDataTreeService netconfService;
 
-    private List<ListenableFuture<? extends DOMRpcResult>> resultsFutures;
+    private static final Logger LOG = LoggerFactory.getLogger(NetconfRestconfTransaction.class);
+
+    private final NetconfDataTreeService netconfService;
+    private final ListenableFuture<? extends DOMRpcResult> lockResult;
+    private volatile ListenableFuture<? extends DOMRpcResult> lastOperationFuture;
+    private final List<ListenableFuture<? extends DOMRpcResult>> resultsFutures =
+        Collections.synchronizedList(new ArrayList<>());
 
     NetconfRestconfTransaction(final NetconfDataTreeService netconfService) {
         this.netconfService = requireNonNull(netconfService);
-        this.resultsFutures = new ArrayList<>(netconfService.lock());
+        this.lockResult = netconfService.lock();
+        resultsFutures.add(lockResult);
+        this.lastOperationFuture = null;
     }
 
     @Override
     public void cancel() {
-        resultsFutures = null;
-        netconfService.discardChanges();
-        netconfService.unlock();
+        resultsFutures.clear();
+        executeAndLogResponse(netconfService::discardChanges);
+        executeAndLogResponse(netconfService::unlock);
     }
 
     @Override
     public void delete(final LogicalDatastoreType store, final YangInstanceIdentifier path) {
-        resultsFutures.add(netconfService.delete(store, path));
+        scheduleOperation(() -> netconfService.delete(store, path));
     }
 
     @Override
     public void remove(final LogicalDatastoreType store, final YangInstanceIdentifier path) {
-        resultsFutures.add(netconfService.remove(store, path));
+        scheduleOperation(() -> netconfService.remove(store, path));
     }
 
     @Override
     public void merge(final LogicalDatastoreType store, final YangInstanceIdentifier path,
             final NormalizedNode<?, ?> data) {
-        resultsFutures.add(netconfService.merge(store, path, data, Optional.empty()));
+        scheduleOperation(() -> netconfService.merge(store, path, data, Optional.empty()));
     }
 
     @Override
@@ -70,10 +95,10 @@ final class NetconfRestconfTransaction extends RestconfTransaction {
 
             for (final NormalizedNode<?, ?> child : ((NormalizedNodeContainer<?, ?, ?>) data).getValue()) {
                 final YangInstanceIdentifier childPath = path.node(child.getIdentifier());
-                resultsFutures.add(netconfService.create(store, childPath, child, Optional.empty()));
+                scheduleOperation(() -> netconfService.create(store, childPath, child, Optional.empty()));
             }
         } else {
-            resultsFutures.add(netconfService.create(store, path, data, Optional.empty()));
+            scheduleOperation(() -> netconfService.create(store, path, data, Optional.empty()));
         }
     }
 
@@ -87,15 +112,182 @@ final class NetconfRestconfTransaction extends RestconfTransaction {
 
             for (final NormalizedNode<?, ?> child : ((NormalizedNodeContainer<?, ?, ?>) data).getValue()) {
                 final YangInstanceIdentifier childPath = path.node(child.getIdentifier());
-                resultsFutures.add(netconfService.replace(store, childPath, child, Optional.empty()));
+                scheduleOperation(() -> netconfService.replace(store, childPath, child, Optional.empty()));
             }
         } else {
-            resultsFutures.add(netconfService.replace(store, path, data, Optional.empty()));
+            scheduleOperation(() -> netconfService.replace(store, path, data, Optional.empty()));
         }
     }
 
     @Override
     public FluentFuture<? extends @NonNull CommitInfo> commit() {
-        return FluentFuture.from(netconfService.commit(resultsFutures));
+        final SettableFuture<CommitInfo> commitResult = SettableFuture.create();
+
+        // First complete all resultsFutures and merge them ...
+        final ListenableFuture<DOMRpcResult> resultErrors = mergeFutures(resultsFutures);
+
+        // ... then evaluate if there are any problems
+        Futures.addCallback(resultErrors, new FutureCallback<>() {
+            @Override
+            public void onSuccess(final DOMRpcResult result) {
+                final Collection<? extends RpcError> errors = result.getErrors();
+                if (!allWarnings(errors)) {
+                    Futures.whenAllComplete(discardAndUnlock()).run(
+                        () -> commitResult.setException(mapRpcErrorsToNetconfDocException(errors)),
+                        MoreExecutors.directExecutor());
+                    return;
+                }
+
+                // ... no problems so far, initiate commit
+                Futures.addCallback(netconfService.commit(), new FutureCallback<DOMRpcResult>() {
+                        @Override
+                        public void onSuccess(@Nullable DOMRpcResult rpcResult) {
+                            final Collection<? extends RpcError> errors = result.getErrors();
+                            if (errors.isEmpty()) {
+                                commitResult.set(CommitInfo.empty());
+                                return;
+                            }
+                            if (allWarnings(errors)) {
+                                LOG.info("Commit successful with warnings {}", errors);
+                                commitResult.set(CommitInfo.empty());
+                                return;
+                            }
+                            Futures.whenAllComplete(discardAndUnlock()).run(
+                                () -> commitResult.setException(mapRpcErrorsToNetconfDocException(errors)),
+                                MoreExecutors.directExecutor());
+                            commitResult.set(CommitInfo.empty());
+                        }
+
+                        @Override
+                        public void onFailure(Throwable throwable) {
+                            commitResult.setException(throwable);
+                        }
+                    }, MoreExecutors.directExecutor());
+            }
+
+            @Override
+            public void onFailure(final Throwable throwable) {
+                Futures.whenAllComplete(discardAndUnlock()).run(() -> commitResult.setException(throwable),
+                    MoreExecutors.directExecutor());
+            }
+        }, MoreExecutors.directExecutor());
+
+        return FluentFuture.from(commitResult);
+    }
+
+    @SuppressFBWarnings(value = "UPM_UNCALLED_PRIVATE_METHOD",
+        justification = "https://github.com/spotbugs/spotbugs/issues/811")
+    private List<ListenableFuture<?>> discardAndUnlock() {
+        return List.of(netconfService.discardChanges(), netconfService.unlock());
+    }
+
+    private void scheduleOperation(final Supplier<ListenableFuture<? extends DOMRpcResult>> operation) {
+        final ListenableFuture<? extends DOMRpcResult> operationFuture;
+        // if that's a first operation in transaction, then start chain from the result of lock operation,
+        // otherwise use last operation in transaction
+        if (lastOperationFuture == null) {
+            operationFuture = Futures.transformAsync(lockResult,
+                result -> {
+                    if (result != null && (result.getErrors().isEmpty() || allWarnings(result.getErrors()))) {
+                        return operation.get();
+                    } else {
+                        return Futures.immediateFailedFuture(new NetconfDocumentedException("Lock operation failed",
+                            DocumentedException.ErrorType.APPLICATION, DocumentedException.ErrorTag.LOCK_DENIED,
+                            DocumentedException.ErrorSeverity.ERROR));
+                    }
+                },
+                MoreExecutors.directExecutor());
+        } else {
+            operationFuture = Futures.transformAsync(lastOperationFuture, future -> operation.get(),
+                MoreExecutors.directExecutor());
+        }
+
+        // save to results to the list, will be needed for commit operation
+        resultsFutures.add(operationFuture);
+
+        // update the latest operation in the chain
+        lastOperationFuture = operationFuture;
+    }
+
+    // Transform list of futures related to RPC operation into a single Future
+    private static ListenableFuture<DOMRpcResult> mergeFutures(
+        final List<ListenableFuture<? extends DOMRpcResult>> futures) {
+        return Futures.whenAllComplete(futures).call(() -> {
+            if (futures.size() == 1) {
+                // Fast path
+                return Futures.getDone(futures.get(0));
+            }
+
+            final var builder = ImmutableList.<RpcError>builder();
+            for (ListenableFuture<? extends DOMRpcResult> future : futures) {
+                builder.addAll(Futures.getDone(future).getErrors());
+            }
+            return new DefaultDOMRpcResult(null, builder.build());
+        }, MoreExecutors.directExecutor());
+    }
+
+    private static NetconfDocumentedException mapRpcErrorsToNetconfDocException(
+        final Collection<? extends RpcError> errors) {
+        DocumentedException.ErrorType errType = DocumentedException.ErrorType.APPLICATION;
+        DocumentedException.ErrorSeverity errSeverity = DocumentedException.ErrorSeverity.ERROR;
+        StringJoiner msgBuilder = new StringJoiner(" ");
+        String errorTag = "operation-failed";
+        for (final RpcError error : errors) {
+            final RpcError.ErrorType errorType = error.getErrorType();
+            switch (errorType) {
+                case RPC:
+                    errType = DocumentedException.ErrorType.RPC;
+                    break;
+                case PROTOCOL:
+                    errType = DocumentedException.ErrorType.PROTOCOL;
+                    break;
+                case TRANSPORT:
+                    errType = DocumentedException.ErrorType.TRANSPORT;
+                    break;
+                case APPLICATION:
+                default:
+                    errType = DocumentedException.ErrorType.APPLICATION;
+                    break;
+            }
+            final RpcError.ErrorSeverity severity = error.getSeverity();
+            switch (severity) {
+                case WARNING:
+                    errSeverity = DocumentedException.ErrorSeverity.WARNING;
+                    break;
+                case ERROR:
+                default:
+                    errSeverity = DocumentedException.ErrorSeverity.ERROR;
+                    break;
+            }
+            msgBuilder.add(error.getMessage());
+            msgBuilder.add(error.getInfo());
+            errorTag = error.getTag();
+        }
+        return new NetconfDocumentedException("RPC during tx failed. " + msgBuilder.toString(), errType,
+            DocumentedException.ErrorTag.from(errorTag), errSeverity);
+    }
+
+    private static void executeAndLogResponse(final Supplier<ListenableFuture<? extends DOMRpcResult>> operation) {
+        final ListenableFuture<? extends DOMRpcResult> operationResult = operation.get();
+        Futures.addCallback(operationResult, new FutureCallback<DOMRpcResult>() {
+            @Override
+            public void onSuccess(@Nullable final DOMRpcResult rpcResult) {
+                if (rpcResult != null && !rpcResult.getErrors().isEmpty()) {
+                    LOG.error("Errors occurred during processing of the RPC operation: {}",
+                        rpcResult.getErrors().stream().map(Object::toString).collect(Collectors.joining(",")));
+                }
+            }
+
+            @Override
+            public void onFailure(final Throwable throwable) {
+                LOG.error("Error processing operation", throwable);
+            }
+        }, MoreExecutors.directExecutor());
+    }
+
+    @SuppressFBWarnings(value = "UPM_UNCALLED_PRIVATE_METHOD",
+        justification = "https://github.com/spotbugs/spotbugs/issues/811")
+    private static boolean allWarnings(final Collection<? extends @NonNull RpcError> errors) {
+        return errors.stream().allMatch(error -> error.getSeverity() == RpcError.ErrorSeverity.WARNING);
     }
 }
