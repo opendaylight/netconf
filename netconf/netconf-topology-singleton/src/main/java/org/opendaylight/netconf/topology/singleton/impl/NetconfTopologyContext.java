9727b038dee5bbac875e252cecbfbbf0af2a117c
Ivan Hrasko <ivan.hrasko@pantheon.tech>
2021-04-12 20:48:54 +0200

    Introduce isolated leader delay
    - to prevent situation when isolated
    leader writes data to operational DS
    (it means it rewrites the current real leader data)
    and then lost its leadership we introduce 15s
    delay when write to operation DS can be stopped
    
    - the problem is that the real leader is not notified
    when isolated leader was leader for short amount of time
    and correct leadership data are not written again
    making the cluster mountpoints to not work
    
    JIRA: NETCONF-454
    Change-Id: I6beae66cc9e825e89d5ce6bc934670ec6de06a7e
    Signed-off-by: Ivan Hrasko <ivan.hrasko@pantheon.tech>

--- netconf/netconf-topology-singleton/src/main/java/org/opendaylight/netconf/topology/singleton/impl/NetconfTopologyContext.java
+++ netconf/netconf-topology-singleton/src/main/java/org/opendaylight/netconf/topology/singleton/impl/NetconfTopologyContext.java
@@ -10,6 +10,10 @@ package org.opendaylight.netconf.topology.singleton.impl;
 import akka.util.Timeout;
 import com.google.common.util.concurrent.ListenableFuture;
 import io.netty.util.concurrent.EventExecutor;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
 import org.opendaylight.aaa.encrypt.AAAEncryptionService;
 import org.opendaylight.controller.config.threadpool.ScheduledThreadPool;
 import org.opendaylight.controller.config.threadpool.ThreadPool;
@@ -32,11 +36,23 @@ import org.slf4j.LoggerFactory;
 class NetconfTopologyContext implements ClusterSingletonService, AutoCloseable {
     private static final Logger LOG = LoggerFactory.getLogger(NetconfTopologyContext.class);
 
+    // sometimes node after isolation is for short time again leader (we have two leaders!) and then
+    // it is changed to follower without any notification about it to current real leader
+    // we decided to not modify operational datastore in first 15 seconds to prevent unwanted modifications
+    private static final int LEADER_DELAY_SEC = 15;
+
     private final ServiceGroupIdentifier serviceGroupIdent;
-    private final NetconfTopologySingletonImpl topologySingleton;
     private final RemoteDeviceId remoteDeviceId;
+    private final NetconfTopologySingletonImpl topologySingleton;
+    private final ScheduledThreadPoolExecutor executor = new ScheduledThreadPoolExecutor(1);
+    private final AtomicBoolean leader = new AtomicBoolean(false);
+
+    private ScheduledFuture<?> scheduledFuture;
 
     private volatile boolean closed;
+
+    // we consider every node that is once set to leader to be potentially isolated leader in the future
+    private volatile boolean isolatedLeader;
 
     NetconfTopologyContext(final String topologyId, final NetconfClientDispatcher clientDispatcher,
             final EventExecutor eventExecutor, final ScheduledThreadPool keepaliveExecutor,
@@ -54,26 +70,45 @@ class NetconfTopologyContext implements ClusterSingletonService, AutoCloseable {
                 dataBroker, mountPointService,
                 encryptionService, deviceActionFactory,
                 baseSchemas, remoteDeviceId, setup, actorResponseWaitTime);
+        executor.setRemoveOnCancelPolicy(true);
     }
 
     @Override
-    public void instantiateServiceInstance() {
+    public synchronized void instantiateServiceInstance() {
         LOG.info("Leader was selected: {}", remoteDeviceId.getHost().getIpAddress());
         if (closed) {
             LOG.warn("Instance is already closed.");
             return;
         }
-        topologySingleton.becomeTopologyLeader();
+        if (!leader.compareAndSet(false, true)) {
+            LOG.warn("Instance is already leader.");
+            return;
+        }
+
+        scheduledFuture = executor.schedule(topologySingleton::becomeTopologyLeader,
+                isolatedLeader ? LEADER_DELAY_SEC : 0, TimeUnit.SECONDS);
+
+        isolatedLeader = true;
     }
 
     @Override
-    public ListenableFuture<?> closeServiceInstance() {
+    public synchronized ListenableFuture<?> closeServiceInstance() {
         // this method is also called when topology data are deleted
         LOG.info("Follower was selected: {}", remoteDeviceId.getHost().getIpAddress());
         if (closed) {
             LOG.warn("Instance is already closed.");
             return FluentFutures.immediateNullFluentFuture();
         }
+        if (!leader.compareAndSet(true, false)) {
+            LOG.warn("Instance is already follower.");
+            return FluentFutures.immediateNullFluentFuture();
+        }
+
+        // do not change operational DS if this leader fails to hold its leadership
+        if (scheduledFuture != null) {
+            scheduledFuture.cancel(false);
+        }
+
         topologySingleton.becomeTopologyFollower();
         return FluentFutures.immediateNullFluentFuture();
     }
@@ -89,6 +124,7 @@ class NetconfTopologyContext implements ClusterSingletonService, AutoCloseable {
             LOG.warn("Instance is already closed.");
             return;
         }
+        executor.shutdown();
         topologySingleton.close();
         closed = true;
     }
