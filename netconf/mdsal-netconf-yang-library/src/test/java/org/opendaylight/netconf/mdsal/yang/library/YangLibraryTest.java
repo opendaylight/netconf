41c0fbdf189c8ac5215b4289dcc69ad13e2db11d
Robert Varga <robert.varga@pantheon.tech>
2021-05-12 12:40:38 +0200

    Rework mdsal-netconf-yang-library
    The primary intent here is elimination of blueprint, but while we
    are at it, we also remove duplicated code and unify the two exporters
    into a single class.
    
    JIRA: NETCONF-770
    Change-Id: I70bba28b1776001d6c71e367b10d87d59399c13a
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- netconf/mdsal-netconf-yang-library/src/test/java/org/opendaylight/netconf/mdsal/yang/library/YangLibraryTest.java
+++ netconf/mdsal-netconf-yang-library/src/test/java/org/opendaylight/netconf/mdsal/yang/library/YangLibraryTest.java
@@ -6,28 +6,13 @@
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
 
-package org.opendaylight.netconf.mdsal.yang.library.rfc8525;
-
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.doNothing;
-import static org.mockito.Mockito.doReturn;
-import static org.mockito.Mockito.never;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
-import static org.opendaylight.mdsal.common.api.CommitInfo.emptyFluentFuture;
+package org.opendaylight.netconf.mdsal.yang.library;
 
 import com.google.common.collect.ImmutableMap;
 import java.util.Collections;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
-import org.opendaylight.mdsal.binding.api.DataBroker;
-import org.opendaylight.mdsal.binding.api.WriteTransaction;
-import org.opendaylight.mdsal.common.api.LogicalDatastoreType;
-import org.opendaylight.mdsal.dom.api.DOMSchemaService;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.datastores.rev180214.Operational;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev130715.Uri;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.yang.library.rev190104.RevisionIdentifier;
@@ -45,68 +30,14 @@ import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.yang.librar
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.yang.library.rev190104.yang.library.parameters.Schema;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.yang.library.rev190104.yang.library.parameters.SchemaBuilder;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.yang.types.rev130715.YangIdentifier;
-import org.opendaylight.yangtools.concepts.ListenerRegistration;
 import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
-import org.opendaylight.yangtools.yang.model.api.EffectiveModelContext;
-import org.opendaylight.yangtools.yang.model.api.EffectiveModelContextListener;
-import org.opendaylight.yangtools.yang.test.util.YangParserTestUtils;
+import org.opendaylight.yangtools.yang.binding.util.BindingMap;
 
 @RunWith(MockitoJUnitRunner.StrictStubs.class)
-public class SchemaServiceToMdsalWriterTest {
-    private static final InstanceIdentifier<YangLibrary> YANG_LIBRARY_INSTANCE_IDENTIFIER =
-            InstanceIdentifier.create(YangLibrary.class);
-
-    @Mock
-    private DOMSchemaService schemaService;
-    @Mock
-    private DataBroker dataBroker;
-    @Mock
-    private WriteTransaction writeTransaction;
-
-    private SchemaServiceToMdsalWriter schemaServiceToMdsalWriter;
-
-    @Before
-    public void setUp() {
-        when(dataBroker.newWriteOnlyTransaction()).thenReturn(writeTransaction);
-        doNothing().when(writeTransaction).put(eq(LogicalDatastoreType.OPERATIONAL), any(), any());
-        doReturn(emptyFluentFuture()).when(writeTransaction).commit();
-        when(schemaService.registerSchemaContextListener(any())).thenReturn(
-                new ListenerRegistration<>() {
-                    @Override
-                    public void close() {
-
-                    }
-
-                    @Override
-                    public EffectiveModelContextListener getInstance() {
-                        return null;
-                    }
-                });
-        schemaServiceToMdsalWriter = new SchemaServiceToMdsalWriter(schemaService, dataBroker);
-    }
-
+public class YangLibraryTest extends AbstractYangLibraryWriterTest {
     @Test
-    public void testOnGlobalContextUpdatedWithYangLibraryModule() {
-        schemaServiceToMdsalWriter.start();
-
-        schemaServiceToMdsalWriter.onModelContextUpdated(getSchema(true));
-        verify(writeTransaction).put(eq(LogicalDatastoreType.OPERATIONAL),
-                eq(YANG_LIBRARY_INSTANCE_IDENTIFIER), eq(createTestModuleSet()));
-    }
-
-    @Test
-    public void testOnGlobalContextUpdatedWithoutYangLibraryModule() {
-        schemaServiceToMdsalWriter.start();
-
-        schemaServiceToMdsalWriter.onModelContextUpdated(getSchema(false));
-        verify(writeTransaction, never()).put(any(), any(), any());
-    }
-
-    private static EffectiveModelContext getSchema(boolean withYangLibrary) {
-        String[] modules = withYangLibrary
-                ? new String[]{"/test-module.yang", "/test-submodule.yang", "/ietf-yang-library.yang"}
-                : new String[]{"/test-module.yang", "/test-submodule.yang"};
-        return YangParserTestUtils.parseYangResources(SchemaServiceToMdsalWriterTest.class, modules);
+    public void testUpdate() {
+        assertOperationalUpdate(InstanceIdentifier.create(YangLibrary.class), createTestModuleSet());
     }
 
     private static YangLibrary createTestModuleSet() {
@@ -141,10 +72,10 @@ public class SchemaServiceToMdsalWriterTest {
                 .build();
 
         return new YangLibraryBuilder()
-                .setModuleSet(ImmutableMap.of(modulesSet.key(), modulesSet))
-                .setSchema(ImmutableMap.of(schema.key(), schema))
-                .setDatastore(ImmutableMap.of(datastore.key(), datastore))
-                .setContentId("0")
-                .build();
+            .setModuleSet(BindingMap.of(modulesSet))
+            .setSchema(BindingMap.of(schema))
+            .setDatastore(BindingMap.of(datastore))
+            .setContentId("0")
+            .build();
     }
 }
