fc12f5073efab9eab40fbc3f373d7ab8ab266dcb
Robert Varga <robert.varga@pantheon.tech>
2021-06-25 23:26:01 +0200

    WIP: Update RESTCONF error mapping
    yang.common.YangError is an attachment used to propagate RFC6020-defined
    failure modes enforced by YANG Tools as exceptions. One such example is
    YangInvalidValueException, which is reporting YANG constraint
    violations.
    
    Recognize these when mapping to RestconfDocumentedException, so that
    their error-app-tag and error-message are properly propagated.
    
    // FIXME: add a few unit tests
    
    JIRA: NETCONF-786
    Change-Id: Id6ea1877e03f42fc193942a4e2b83ecb6daeb631
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- restconf/restconf-nb-bierman02/src/main/java/org/opendaylight/netconf/sal/rest/impl/JsonNormalizedNodeBodyReader.java
+++ restconf/restconf-nb-bierman02/src/main/java/org/opendaylight/netconf/sal/rest/impl/JsonNormalizedNodeBodyReader.java
@@ -7,6 +7,7 @@
  */
 package org.opendaylight.netconf.sal.rest.impl;
 
+import com.google.common.base.Throwables;
 import com.google.common.collect.Iterables;
 import com.google.gson.stream.JsonReader;
 import java.io.IOException;
@@ -164,19 +165,15 @@ public class JsonNormalizedNodeBodyReader
     }
 
     private static void propagateExceptionAs(final Exception exception) throws RestconfDocumentedException {
-        if (exception instanceof RestconfDocumentedException) {
-            throw (RestconfDocumentedException)exception;
-        }
+        Throwables.throwIfInstanceOf(exception, RestconfDocumentedException.class);
+        LOG.debug("Error parsing json input", exception);
 
         if (exception instanceof ResultAlreadySetException) {
-            LOG.debug("Error parsing json input:", exception);
-
             throw new RestconfDocumentedException("Error parsing json input: Failed to create new parse result data. "
                     + "Are you creating multiple resources/subresources in POST request?", exception);
         }
 
-        LOG.debug("Error parsing json input", exception);
-
+        RestconfDocumentedException.throwIfYangError(exception);
         throw new RestconfDocumentedException("Error parsing input: " + exception.getMessage(), ErrorType.PROTOCOL,
                 ErrorTag.MALFORMED_MESSAGE, exception);
     }
