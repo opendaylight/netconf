8ccc23cd635ba39306fab956f2ba599b7922281e
Robert Varga <robert.varga@pantheon.tech>
2021-07-19 12:00:58 +0200

    Use Java 11 HttpClient
    Reduce dependencies by using JRE-provided HTTP client.
    
    JIRA: NETCONF-751
    Change-Id: I49a51306a042ed534bafd7174633e300f00ce305
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- netconf/tools/netconf-testtool/src/main/java/org/opendaylight/netconf/test/tool/client/http/perf/AsyncExecutionStrategy.java
+++ netconf/tools/netconf-testtool/src/main/java/org/opendaylight/netconf/test/tool/client/http/perf/AsyncExecutionStrategy.java
@@ -5,16 +5,13 @@
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
-
 package org.opendaylight.netconf.test.tool.client.http.perf;
 
 import static org.opendaylight.netconf.test.tool.client.http.perf.RequestMessageUtils.formRequest;
 
-import com.ning.http.client.AsyncCompletionHandler;
-import com.ning.http.client.AsyncHttpClient;
-import com.ning.http.client.HttpResponseStatus;
-import com.ning.http.client.Request;
-import com.ning.http.client.Response;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse.BodyHandlers;
 import java.util.concurrent.Semaphore;
 import org.opendaylight.netconf.test.tool.client.stress.ExecutionStrategy;
 import org.slf4j.Logger;
@@ -24,15 +21,15 @@ public class AsyncExecutionStrategy implements ExecutionStrategy {
 
     private static final Logger LOG = LoggerFactory.getLogger(AsyncExecutionStrategy.class);
 
+    private final HttpClient httpClient;
     private final Parameters params;
-    private final AsyncHttpClient asyncHttpClient;
     private final Semaphore semaphore;
     RestPerfClient.RequestData payloads;
 
-    AsyncExecutionStrategy(final Parameters params, final AsyncHttpClient asyncHttpClient,
-                           final RestPerfClient.RequestData payloads) {
+    AsyncExecutionStrategy(final Parameters params, final HttpClient httpClient,
+            final RestPerfClient.RequestData payloads) {
         this.params = params;
-        this.asyncHttpClient = asyncHttpClient;
+        this.httpClient = httpClient;
         this.payloads = payloads;
         this.semaphore = new Semaphore(RestPerfClient.throttle);
     }
@@ -42,30 +39,25 @@ public class AsyncExecutionStrategy implements ExecutionStrategy {
         LOG.info("Begin sending async requests");
 
         for (int i = 0; i < payloads.getRequests(); i++) {
-            String message = RequestMessageUtils.prepareMessage(payloads.getThreadId(), i,
+            final String message = RequestMessageUtils.prepareMessage(payloads.getThreadId(), i,
                     payloads.getContentString(), payloads.getPort());
-            Request request = formRequest(asyncHttpClient, payloads.getDestination(), params, message);
+            final String url = payloads.getDestination();
+            final HttpRequest request = formRequest(url, message);
             try {
                 semaphore.acquire();
             } catch (InterruptedException e) {
                 LOG.warn("Semaphore acquire interrupted");
             }
-            asyncHttpClient.executeRequest(request, new AsyncCompletionHandler<Response>() {
-                @Override
-                public STATE onStatusReceived(HttpResponseStatus status) throws Exception {
-                    super.onStatusReceived(status);
-                    if (status.getStatusCode() != 200 && status.getStatusCode() != 204) {
-                        LOG.warn("Request failed, status code: {}", status.getStatusCode() + status.getStatusText());
-                        LOG.warn("request: {}", request.toString());
-                    }
-                    return STATE.CONTINUE;
+            httpClient.sendAsync(request, BodyHandlers.ofString()).whenComplete((response, error) -> {
+                switch (response.statusCode()) {
+                    case 200:
+                    case 204:
+                        break;
+                    default:
+                        LOG.warn("Request failed, status code: {}", response.statusCode());
+                        LOG.warn("request: {}", request);
                 }
-
-                @Override
-                public Response onCompleted(Response response) {
-                    semaphore.release();
-                    return response;
-                }
+                semaphore.release();
             });
         }
         LOG.info("Requests sent, waiting for responses");
