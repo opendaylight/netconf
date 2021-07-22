67d183d4ff4029e05ec7b93b8a42d224242d4e02
Konstantin.Nosach <Kostiantyn.Nosach@pantheon.tech>
2021-07-22 13:35:32 +0300

    Eliminate use of AsyncSshHandlerReader in call home
    Using AsyncSshHandlerReader did not require ChannelHandlerContext for reading.
    After eliminating AsyncSshHandlerReader, we need to create a ChannelSubsystem
    with the ChannelHandlerContext, which we can only obtain after
    creating and calling the open listener on the ChannelSubsystem.
    
    Current solution is to utilize pipelines from MinaSshNettyChannel.
    The logic from MinaSshNettyChannel has been moved to CallHomeSessionContext,
    where a ChannelOutboundHandlerAdapter is created and added as the first
    in the pipeline. This call will create the ChannelHandlerContext
    in the pipeline, and it can be used later inside the ChannelSubsystem.
    
    JIRA: NETCONF-699
    Change-Id: I58c702949bc31061b89b12d166f0fa6b8bc383c7
    Signed-off-by: Kostiantyn Nosach <kostiantyn.nosach@pantheon.tech>
    Signed-off-by: Ivan Hrasko <ivan.hrasko@pantheon.tech>
    Signed-off-by: Peter Suna <peter.suna@pantheon.tech>

--- netconf/callhome-protocol/src/test/java/org/opendaylight/netconf/callhome/protocol/NetconfCallHomeServerTest.java
+++ netconf/callhome-protocol/src/test/java/org/opendaylight/netconf/callhome/protocol/NetconfCallHomeServerTest.java
@@ -30,9 +30,10 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
-import org.opendaylight.netconf.shaded.sshd.client.SshClient;
+import org.opendaylight.netconf.nettyutil.handler.ssh.client.NetconfClientBuilder;
+import org.opendaylight.netconf.nettyutil.handler.ssh.client.NetconfSshClient;
+import org.opendaylight.netconf.nettyutil.handler.ssh.client.NettyAwareClientSession;
 import org.opendaylight.netconf.shaded.sshd.client.future.AuthFuture;
-import org.opendaylight.netconf.shaded.sshd.client.session.ClientSession;
 import org.opendaylight.netconf.shaded.sshd.client.session.ClientSessionImpl;
 import org.opendaylight.netconf.shaded.sshd.common.future.SshFutureListener;
 import org.opendaylight.netconf.shaded.sshd.common.io.IoAcceptor;
@@ -46,6 +47,7 @@ public class NetconfCallHomeServerTest {
     private static EventLoopGroup EVENT_LOOP_GROUP;
     private static InetSocketAddress MOCK_ADDRESS;
 
+    private NetconfSshClient mockSshClient;
     @Mock
     private CallHomeAuthorizationProvider mockCallHomeAuthProv;
     @Mock
@@ -53,7 +55,7 @@ public class NetconfCallHomeServerTest {
     @Mock
     private CallHomeSessionContext.Factory mockFactory;
     @Mock
-    private ClientSession mockSession;
+    private NettyAwareClientSession mockSession;
     @Mock
     private StatusRecorder mockStatusRecorder;
 
@@ -74,10 +76,11 @@ public class NetconfCallHomeServerTest {
 
     @Before
     public void setup() {
+        mockSshClient = spy(new NetconfClientBuilder().build());
         mockCallHomeAuthProv = mock(CallHomeAuthorizationProvider.class);
         mockAuth = mock(CallHomeAuthorization.class);
         mockFactory = mock(CallHomeSessionContext.Factory.class);
-        mockSession = mock(ClientSession.class);
+        mockSession = mock(NettyAwareClientSession.class);
         mockStatusRecorder = mock(StatusRecorder.class);
 
         Map<String, String> props = new HashMap<>();
