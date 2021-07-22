887b9f76965a8cc7784cad753e4877a63ba40c72
Konstantin.Nosach <Kostiantyn.Nosach@pantheon.tech>
2021-07-22 13:35:32 +0300

    Eliminate use of AsyncSshHandlerReader in call home
    Call home has a twisted way of doing things and therefore is still using
    AsyncSshHandlerReader. Rafactor it to completely eliminate
    AsyncSshHandlerReader.
    
    JIRA: NETCONF-699
    Signed-off-by: Kostiantyn Nosach <kostiantyn.nosach@pantheon.tech>
    Change-Id: I58c702949bc31061b89b12d166f0fa6b8bc383c7

--- netconf/netconf-netty-util/src/main/java/org/opendaylight/netconf/nettyutil/handler/ssh/client/NettyPipelineAwareChannelSubsystem.java
+++ netconf/netconf-netty-util/src/main/java/org/opendaylight/netconf/nettyutil/handler/ssh/client/NettyPipelineAwareChannelSubsystem.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ * Copyright (c) 2021 PANTHEON.tech, s.r.o. and others.  All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
@@ -7,34 +7,34 @@
  */
 package org.opendaylight.netconf.nettyutil.handler.ssh.client;
 
-import com.google.common.annotations.Beta;
+
 import io.netty.buffer.Unpooled;
 import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelPipeline;
 import java.io.IOException;
 import org.opendaylight.netconf.shaded.sshd.client.channel.ChannelSubsystem;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+
 /**
  * A {@link ChannelSubsystem} for subsystem which routes incoming data to a particular {@link ChannelHandlerContext}.
  */
-@Beta
-public class NettyAwareChannelSubsystem extends ChannelSubsystem {
-    private static final Logger LOG = LoggerFactory.getLogger(NettyAwareChannelSubsystem.class);
+public class NettyPipelineAwareChannelSubsystem extends ChannelSubsystem {
+    private static final Logger LOG = LoggerFactory.getLogger(NettyPipelineAwareChannelSubsystem.class);
 
-    private final ChannelHandlerContext ctx;
+    private final ChannelPipeline pipeline;
 
-    public NettyAwareChannelSubsystem(final String subsystem, final ChannelHandlerContext ctx) {
+    public NettyPipelineAwareChannelSubsystem(String subsystem, ChannelPipeline pipeline) {
         super(subsystem);
-        this.ctx = ctx;
+        this.pipeline = pipeline;
     }
 
     @Override
     protected void doWriteData(final byte[] data, final int off, final long len) throws IOException {
         // If we're already closing, ignore incoming data
         if (!isClosing()) {
-            // TODO: consider using context's allocator for heap buffer here
-            ctx.fireChannelRead(Unpooled.copiedBuffer(data, off, (int) len));
+            pipeline.firstContext().fireChannelRead(Unpooled.copiedBuffer(data, off, (int) len));
             adjustWindow(len);
         }
     }
