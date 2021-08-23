02579aac7d13ee7de64f5aadf8c87cef35f19f00
Robert Varga <robert.varga@pantheon.tech>
2021-08-24 01:35:44 +0200

    WIP: Movee SendErrorExceptionUtil
    // FIXME: fixup tests
    
    Change-Id: I4e93c0d3fcbe87a6689f55d592a085d7aa6b8be3
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- netconf/netconf-impl/src/test/java/org/opendaylight/netconf/impl/SendErrorExceptionUtilTest.java
+++ netconf/netconf-impl/src/test/java/org/opendaylight/netconf/impl/SendErrorExceptionUtilTest.java
@@ -5,8 +5,7 @@
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
-
-package org.opendaylight.netconf.util.messages;
+package org.opendaylight.netconf.impl;
 
 import static org.junit.Assert.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
