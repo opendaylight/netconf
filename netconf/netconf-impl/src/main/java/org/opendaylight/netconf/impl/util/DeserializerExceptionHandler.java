77d67c10c3a9aec2d63adb7fcccd1d24a5272633
Robert Varga <robert.varga@pantheon.tech>
2021-08-24 01:28:01 +0200

    Improve DeserializerExceptionHandler
    We are getting a deprecation warning, as there exceptionCaught() really
    makes sense only on inbound side. This happens to be what we actually
    are doing with DeserializerExceptionHandler anyway, so just pick up
    ChannelInboundHandlerAdapter and use that as the superclass.
    
    Change-Id: Ic661e4e85f4442a395c820463abf00e71c52543e
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- netconf/netconf-impl/src/main/java/org/opendaylight/netconf/impl/util/DeserializerExceptionHandler.java
+++ netconf/netconf-impl/src/main/java/org/opendaylight/netconf/impl/util/DeserializerExceptionHandler.java
@@ -7,9 +7,8 @@
  */
 package org.opendaylight.netconf.impl.util;
 
-import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelHandlerContext;
-import java.util.HashMap;
+import io.netty.channel.ChannelInboundHandlerAdapter;
 import java.util.Map;
 import org.opendaylight.netconf.api.DocumentedException;
 import org.opendaylight.netconf.util.messages.SendErrorExceptionUtil;
@@ -18,18 +17,8 @@ import org.opendaylight.yangtools.yang.common.ErrorType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public final class DeserializerExceptionHandler implements ChannelHandler {
+public final class DeserializerExceptionHandler extends ChannelInboundHandlerAdapter {
     private static final Logger LOG = LoggerFactory.getLogger(DeserializerExceptionHandler.class);
-
-    @Override
-    public void handlerAdded(final ChannelHandlerContext ctx) {
-        // NOOP
-    }
-
-    @Override
-    public void handlerRemoved(final ChannelHandlerContext ctx) {
-        // NOOP
-    }
 
     @Override
     public void exceptionCaught(final ChannelHandlerContext ctx, final Throwable cause) {
@@ -38,11 +27,9 @@ public final class DeserializerExceptionHandler implements ChannelHandler {
     }
 
     private static void handleDeserializerException(final ChannelHandlerContext ctx, final Throwable cause) {
-        final Map<String, String> info = new HashMap<>();
-        info.put("cause", cause.getMessage());
-        final DocumentedException ex = new DocumentedException(cause.getMessage(),
-                ErrorType.RPC, DocumentedException.MALFORMED_MESSAGE, ErrorSeverity.ERROR, info);
-
-        SendErrorExceptionUtil.sendErrorMessage(ctx.channel(), ex);
+        final String message = cause.getMessage();
+        SendErrorExceptionUtil.sendErrorMessage(ctx.channel(), new DocumentedException(message,
+            ErrorType.RPC, DocumentedException.MALFORMED_MESSAGE, ErrorSeverity.ERROR,
+            message != null ? Map.of("cause", message) : Map.of()));
     }
 }
