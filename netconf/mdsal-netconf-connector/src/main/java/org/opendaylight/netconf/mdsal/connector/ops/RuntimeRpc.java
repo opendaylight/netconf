cbd03f0284954c55d81f647b297ffaa326090c37
Robert Varga <robert.varga@pantheon.tech>
2021-08-23 10:53:08 +0200

    WIP: Bump MRI upstreams
    Adopt:
    - odlparent-9.0.5
    - yangtools-7.0.7
    - mdsal-8.0.4
    - controller-4.0.2
    - infrautils-2.0.5
    - aaa-0.14.2
    
    Change-Id: I525bcbd1c137cfe00c98b0d31c23de92035a98de
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- netconf/mdsal-netconf-connector/src/main/java/org/opendaylight/netconf/mdsal/connector/ops/RuntimeRpc.java
+++ netconf/mdsal-netconf-connector/src/main/java/org/opendaylight/netconf/mdsal/connector/ops/RuntimeRpc.java
@@ -259,8 +259,8 @@ public class RuntimeRpc extends AbstractSingletonNetconfOperation {
         try {
             xmlParser.traverse(new DOMSource(element.getDomElement()));
         } catch (final XMLStreamException | URISyntaxException | IOException | SAXException ex) {
-            throw new NetconfDocumentedException("Error parsing input: " + ex.getMessage(), ex, ErrorType.PROTOCOL,
-                    DocumentedException.MALFORMED_MESSAGE, ErrorSeverity.ERROR);
+            throw new NetconfDocumentedException("Error parsing input: " + ex.getMessage(), ex,
+                ErrorType.PROTOCOL, ErrorTag.MALFORMED_MESSAGE, ErrorSeverity.ERROR);
         }
 
         return (ContainerNode) resultHolder.getResult();
