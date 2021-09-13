6b0431098fc893fcf3137615cc17119066f0fdce
Robert Varga <robert.varga@pantheon.tech>
2021-09-13 23:55:39 +0200

    Remove RestconfInvokeOperationsUtil
    This is a stateless utility class, whose only use to make cooperating
    code harder to understand. Remove it by moving its methods to
    Restconf{Data,InvokeOperations}ServiceImpl, who are its only users.
    
    JIRA: NETCONF-773
    Change-Id: Ie2cd32af2b26f09d4196df0c52be737809d10850
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- restconf/restconf-nb-rfc8040/src/main/java/org/opendaylight/restconf/nb/rfc8040/rests/services/impl/RestconfInvokeOperationsServiceImpl.java
+++ restconf/restconf-nb-rfc8040/src/main/java/org/opendaylight/restconf/nb/rfc8040/rests/services/impl/RestconfInvokeOperationsServiceImpl.java
@@ -9,30 +9,46 @@ package org.opendaylight.restconf.nb.rfc8040.rests.services.impl;
 
 import static java.util.Objects.requireNonNull;
 
+import com.google.common.annotations.VisibleForTesting;
+import com.google.common.base.Throwables;
+import com.google.common.collect.ImmutableList;
+import com.google.common.util.concurrent.Futures;
+import com.google.common.util.concurrent.ListenableFuture;
+import com.google.common.util.concurrent.MoreExecutors;
 import java.util.Optional;
+import java.util.concurrent.CancellationException;
+import java.util.concurrent.ExecutionException;
 import javax.ws.rs.Path;
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
+import org.eclipse.jdt.annotation.NonNull;
 import org.opendaylight.mdsal.dom.api.DOMMountPoint;
+import org.opendaylight.mdsal.dom.api.DOMRpcException;
 import org.opendaylight.mdsal.dom.api.DOMRpcResult;
 import org.opendaylight.mdsal.dom.api.DOMRpcService;
 import org.opendaylight.mdsal.dom.api.DOMSchemaService;
+import org.opendaylight.mdsal.dom.spi.DefaultDOMRpcResult;
 import org.opendaylight.restconf.common.context.InstanceIdentifierContext;
 import org.opendaylight.restconf.common.context.NormalizedNodeContext;
 import org.opendaylight.restconf.common.errors.RestconfDocumentedException;
 import org.opendaylight.restconf.nb.rfc8040.handlers.SchemaContextHandler;
 import org.opendaylight.restconf.nb.rfc8040.rests.services.api.RestconfInvokeOperationsService;
-import org.opendaylight.restconf.nb.rfc8040.rests.utils.RestconfInvokeOperationsUtil;
 import org.opendaylight.restconf.nb.rfc8040.rests.utils.RestconfStreamsConstants;
 import org.opendaylight.yangtools.yang.common.ErrorTag;
 import org.opendaylight.yangtools.yang.common.ErrorType;
 import org.opendaylight.yangtools.yang.common.QName;
+import org.opendaylight.yangtools.yang.common.RpcError;
+import org.opendaylight.yangtools.yang.common.RpcResultBuilder;
 import org.opendaylight.yangtools.yang.common.XMLNamespace;
+import org.opendaylight.yangtools.yang.common.YangConstants;
 import org.opendaylight.yangtools.yang.data.api.schema.ContainerNode;
 import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNode;
+import org.opendaylight.yangtools.yang.data.impl.schema.ImmutableNodes;
 import org.opendaylight.yangtools.yang.model.api.EffectiveModelContext;
 import org.opendaylight.yangtools.yang.model.api.RpcDefinition;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Implementation of {@link RestconfInvokeOperationsService}.
@@ -40,6 +56,9 @@ import org.opendaylight.yangtools.yang.model.api.RpcDefinition;
  */
 @Path("/")
 public class RestconfInvokeOperationsServiceImpl implements RestconfInvokeOperationsService {
+    private static final Logger LOG = LoggerFactory.getLogger(RestconfInvokeOperationsServiceImpl.class);
+
+    // FIXME: at some point we do not want to have this here
     private static final XMLNamespace SAL_REMOTE_NAMESPACE =
         XMLNamespace.of("urn:opendaylight:params:xml:ns:yang:controller:md:sal:remote");
 
@@ -71,15 +90,15 @@ public class RestconfInvokeOperationsServiceImpl implements RestconfInvokeOperat
                             ErrorTag.OPERATION_NOT_SUPPORTED);
                 }
             } else {
-                response = RestconfInvokeOperationsUtil.invokeRpc(payload.getData(), schemaPath, this.rpcService);
+                response = invokeRpc(payload.getData(), schemaPath, this.rpcService);
             }
             schemaContextRef = this.schemaContextHandler.get();
         } else {
-            response = RestconfInvokeOperationsUtil.invokeRpc(payload.getData(), schemaPath, mountPoint);
+            response = invokeRpc(payload.getData(), schemaPath, mountPoint);
             schemaContextRef = modelContext(mountPoint);
         }
 
-        final DOMRpcResult result = RestconfInvokeOperationsUtil.checkResponse(response);
+        final DOMRpcResult result = checkResponse(response);
 
         RpcDefinition resultNodeSchema = null;
         NormalizedNode resultData = null;
@@ -93,6 +112,85 @@ public class RestconfInvokeOperationsServiceImpl implements RestconfInvokeOperat
         } else {
             return new NormalizedNodeContext(new InstanceIdentifierContext<>(null, resultNodeSchema, mountPoint,
                     schemaContextRef), resultData);
+        }
+    }
+
+    /**
+     * Invoking rpc via mount point.
+     *
+     * @param mountPoint mount point
+     * @param data input data
+     * @param rpc RPC type
+     * @return {@link DOMRpcResult}
+     */
+    // FIXME: NETCONF-718: we should be returning a future here
+    @VisibleForTesting
+    static DOMRpcResult invokeRpc(final NormalizedNode data, final QName rpc, final DOMMountPoint mountPoint) {
+        return invokeRpc(data, rpc, mountPoint.getService(DOMRpcService.class).orElseThrow(() -> {
+            final String errmsg = "RPC service is missing.";
+            LOG.debug(errmsg);
+            return new RestconfDocumentedException(errmsg);
+        }));
+    }
+
+    /**
+     * Invoke rpc.
+     *
+     * @param data input data
+     * @param rpc RPC type
+     * @param rpcService rpc service to invoke rpc
+     * @return {@link DOMRpcResult}
+     */
+    // FIXME: NETCONF-718: we should be returning a future here
+    @VisibleForTesting
+    static DOMRpcResult invokeRpc(final NormalizedNode data, final QName rpc, final DOMRpcService rpcService) {
+        return checkedGet(Futures.catching(
+            rpcService.invokeRpc(rpc, nonnullInput(rpc, data)), DOMRpcException.class,
+            cause -> new DefaultDOMRpcResult(ImmutableList.of(RpcResultBuilder.newError(
+                RpcError.ErrorType.RPC, "operation-failed", cause.getMessage()))),
+            MoreExecutors.directExecutor()));
+    }
+
+    private static @NonNull NormalizedNode nonnullInput(final QName type, final NormalizedNode input) {
+        return input != null ? input
+                : ImmutableNodes.containerNode(YangConstants.operationInputQName(type.getModule()));
+    }
+
+    /**
+     * Check the validity of the result.
+     *
+     * @param response response of rpc
+     * @return {@link DOMRpcResult} result
+     */
+    @VisibleForTesting
+    static DOMRpcResult checkResponse(final DOMRpcResult response) {
+        if (response == null) {
+            return null;
+        }
+        try {
+            if (response.getErrors().isEmpty()) {
+                return response;
+            }
+            LOG.debug("RpcError message {}", response.getErrors());
+            throw new RestconfDocumentedException("RPCerror message ", null, response.getErrors());
+        } catch (final CancellationException e) {
+            final String errMsg = "The operation was cancelled while executing.";
+            LOG.debug("Cancel RpcExecution: {}", errMsg, e);
+            throw new RestconfDocumentedException(errMsg, ErrorType.RPC, ErrorTag.PARTIAL_OPERATION, e);
+        }
+    }
+
+    @Deprecated
+    static <T> T checkedGet(final ListenableFuture<T> future) {
+        try {
+            return future.get();
+        } catch (InterruptedException e) {
+            throw new RestconfDocumentedException("Interrupted while waiting for result of invocation", e);
+        } catch (ExecutionException e) {
+            LOG.trace("Invocation failed", e);
+            final Throwable cause = e.getCause();
+            Throwables.throwIfInstanceOf(cause, RestconfDocumentedException.class);
+            throw new RestconfDocumentedException("Invocation failed", cause);
         }
     }
 
