d3aef9e18b6ad5448cc801f241f7076e9c9db3b3
Robert Varga <robert.varga@pantheon.tech>
2021-09-16 04:09:00 +0200

    Add rfc8040.ApiPath
    Our YangInstanceIdentifierSerializer is a rather complex piece of
    machinery, which is a maintenance burden and cannot be modified easily.
    
    In order to deal with it, we will split parsing into two steps:
    
    1) structural tokenization, which splits the input string into a
       representation of 'api-path' ABNF producation.
    
    2) semantic binding to an EffectiveModelContext, which reconciles the
       structure with the context, producing YangInstanceIdentifier in the
       process.
    
    This patch deals with item 1) by introducing an ApiPath class, which
    can be constructed from a string.
    
    JIRA: NETCONF-631
    Change-Id: I44c8ad1308b27ee459f95f8617f54c8537ea9c65
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ restconf/restconf-nb-rfc8040/src/main/java/org/opendaylight/restconf/nb/rfc8040/ApiPath.java
@@ -0,0 +1,170 @@
+/*
+ * Copyright (c) 2021 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.restconf.nb.rfc8040;
+
+import static com.google.common.base.Verify.verifyNotNull;
+import static java.util.Objects.requireNonNull;
+
+import com.google.common.annotations.Beta;
+import com.google.common.base.MoreObjects;
+import com.google.common.base.MoreObjects.ToStringHelper;
+import com.google.common.collect.ImmutableList;
+import java.text.ParseException;
+import javax.ws.rs.PathParam;
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.eclipse.jdt.annotation.Nullable;
+import org.opendaylight.restconf.common.errors.RestconfDocumentedException;
+import org.opendaylight.yangtools.concepts.Immutable;
+import org.opendaylight.yangtools.yang.common.ErrorTag;
+import org.opendaylight.yangtools.yang.common.ErrorType;
+import org.opendaylight.yangtools.yang.common.UnqualifiedQName;
+
+/**
+ * Intermediate representation of a parsed {@code api-path} string as defined in
+ * <a href="https://datatracker.ietf.org/doc/html/rfc8040#section-3.5.3.1">RFC section 3.5.3.1</a>. It models the
+ * path as a series of {@link Step}s.
+ */
+@Beta
+@NonNullByDefault
+public final class ApiPath implements Immutable {
+    /**
+     * A single step in an {@link ApiPath}.
+     */
+    public abstract static class Step implements Immutable {
+        private final @Nullable String module;
+        private final UnqualifiedQName identifier;
+
+        Step(final @Nullable String module, final String identifier) {
+            this.identifier = verifyNotNull(UnqualifiedQName.tryCreate(identifier), "Unexpected invalid identifier %s",
+                identifier);
+            this.module = module;
+        }
+
+        public UnqualifiedQName identifier() {
+            return identifier;
+        }
+
+        public @Nullable String module() {
+            return module;
+        }
+
+        @Override
+        public final String toString() {
+            return addToStringAttributes(MoreObjects.toStringHelper(this).omitNullValues()).toString();
+        }
+
+        ToStringHelper addToStringAttributes(final ToStringHelper helper) {
+            return helper.add("module", module).add("identifier", identifier);
+        }
+    }
+
+    /**
+     * An {@code api-identifier} step in a {@link ApiPath}.
+     */
+    static final class ApiIdentifier extends Step {
+        ApiIdentifier(final @Nullable String module, final String identifier) {
+            super(module, identifier);
+        }
+    }
+
+    /**
+     * A {@code list-instance} step in a {@link ApiPath}.
+     */
+    static final class ListInstance extends Step {
+        private final ImmutableList<String> keyValues;
+
+        ListInstance(final @Nullable String module, final String identifier, final ImmutableList<String> keyValues) {
+            super(module, identifier);
+            this.keyValues = requireNonNull(keyValues);
+        }
+
+        public ImmutableList<String> keyValues() {
+            return keyValues;
+        }
+
+        @Override
+        ToStringHelper addToStringAttributes(final ToStringHelper helper) {
+            return helper.add("keyValues", keyValues);
+        }
+    }
+
+    private static final ApiPath EMPTY = new ApiPath(ImmutableList.of());
+
+    private final ImmutableList<Step> steps;
+
+    private ApiPath(final ImmutableList<Step> steps) {
+        this.steps = requireNonNull(steps);
+    }
+
+    /**
+     * Parse an {@link ApiPath} from a raw Request URI fragment. The string is expected to contain percent-encoded
+     * bytes. Any sequence of such bytes is interpreted as a {@code UTF-8}-encoded string. Invalid sequences are
+     * rejected.
+     *
+     * @param str Request URI part
+     * @return An {@link ApiPath}
+     * @throws NullPointerException if {@code str} is {@code null}
+     * @throws ParseException if the string cannot be parsed
+     */
+    public static ApiPath parse(final String str) throws ParseException {
+        return str.isEmpty() ? EMPTY : new ApiPath(ApiPathParser.parseSteps(str));
+    }
+
+    /**
+     * Parse an {@link ApiPath} from a raw Request URI. The string is expected to contain percent-encoded bytes. Any
+     * sequence of such bytes is interpreted as a {@code UTF-8}-encoded string. Invalid sequences are rejected.
+     *
+     * @param str Request URI part
+     * @return An {@link ApiPath}
+     * @throws RestconfDocumentedException if the string cannot be parsed
+     * @see PathParam
+     */
+    public static ApiPath valueOf(final @Nullable String str) {
+        if (str == null) {
+            return EMPTY;
+        }
+
+        try {
+            return parse(str);
+        } catch (ParseException e) {
+            throw new RestconfDocumentedException("Invalid path '" + str + "'", ErrorType.APPLICATION,
+                ErrorTag.MALFORMED_MESSAGE, e);
+        }
+    }
+
+    public ImmutableList<Step> steps() {
+        return steps;
+    }
+
+    public int indexOf(final String module, final String identifier) {
+        final var m = requireNonNull(module);
+        final var id = requireNonNull(identifier);
+        for (int i = 0, size = steps.size(); i < size; ++i) {
+            final var step = steps.get(i);
+            if (m.equals(step.module) && id.equals(step.identifier)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public ApiPath subPath(final int fromIndex) {
+        return subPath(fromIndex, steps.size());
+    }
+
+    public ApiPath subPath(final int fromIndex, final int toIndex) {
+        final var subList = steps.subList(fromIndex, toIndex);
+        if (subList == steps) {
+            return this;
+        } else if (subList.isEmpty()) {
+            return EMPTY;
+        } else {
+            return new ApiPath(subList);
+        }
+    }
+}
