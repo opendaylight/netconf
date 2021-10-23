804f62f0f7e1302dac87d255f963b6f1b25d95fb
Robert Varga <robert.varga@pantheon.tech>
2021-10-24 00:17:18 +0200

    WIP: Add ReadDataService
    // FIXME: more wiring needs to be done
    
    Change-Id: Ib0448d20cb1ef1e50d6da971035f2884f3146df4
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ restconf/restconf-nb-rfc8040/src/main/java/org/opendaylight/restconf/nb/rfc8040/api/ReadDataService.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2021 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.restconf.nb.rfc8040.api;
+
+import com.google.common.annotations.Beta;
+import java.util.concurrent.CompletionStage;
+import org.opendaylight.restconf.nb.rfc8040.ApiPath;
+import org.opendaylight.restconf.nb.rfc8040.ReadDataParams;
+
+/**
+ * RESTCONF GET methods on data and datastore resources.
+ */
+@Beta
+public interface ReadDataService {
+
+
+    CompletionStage<ReadDataResponse> readData(ApiPath path, ReadDataParams parameters);
+}
