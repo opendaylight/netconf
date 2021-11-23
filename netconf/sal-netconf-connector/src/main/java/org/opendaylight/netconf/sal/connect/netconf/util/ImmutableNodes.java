af7108c102c0771a6e8a1e4b18b0663080861f5b
Ivan Hrasko <ivan.hrasko@pantheon.tech>
2021-11-23 15:44:32 +0100

    Implement delete-config operation
    Change-Id: I9171397cd9aafd458cff3601b6e75046f695b5de
    Signed-off-by: Ivan Hrasko <ivan.hrasko@pantheon.tech>

---
+++ netconf/sal-netconf-connector/src/main/java/org/opendaylight/netconf/sal/connect/netconf/util/ImmutableNodes.java
@@ -0,0 +1,253 @@
+/*
+ * Copyright (c) 2014 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.netconf.sal.connect.netconf.util;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+import java.util.Iterator;
+import java.util.Optional;
+import org.eclipse.jdt.annotation.NonNull;
+import org.opendaylight.yangtools.yang.common.QName;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.NodeIdentifier;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.NodeIdentifierWithPredicates;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.PathArgument;
+import org.opendaylight.yangtools.yang.data.api.schema.ChoiceNode;
+import org.opendaylight.yangtools.yang.data.api.schema.ContainerNode;
+import org.opendaylight.yangtools.yang.data.api.schema.LeafNode;
+import org.opendaylight.yangtools.yang.data.api.schema.MapEntryNode;
+import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNode;
+import org.opendaylight.yangtools.yang.data.api.schema.SystemMapNode;
+import org.opendaylight.yangtools.yang.data.api.schema.UnkeyedListNode;
+import org.opendaylight.yangtools.yang.data.api.schema.UserMapNode;
+import org.opendaylight.yangtools.yang.data.api.schema.builder.CollectionNodeBuilder;
+import org.opendaylight.yangtools.yang.data.api.schema.builder.DataContainerNodeBuilder;
+import org.opendaylight.yangtools.yang.data.impl.schema.builder.impl.ImmutableChoiceNodeBuilder;
+import org.opendaylight.yangtools.yang.data.impl.schema.builder.impl.ImmutableContainerNodeBuilder;
+import org.opendaylight.yangtools.yang.data.impl.schema.builder.impl.ImmutableLeafNodeBuilder;
+import org.opendaylight.yangtools.yang.data.impl.schema.builder.impl.ImmutableMapEntryNodeBuilder;
+import org.opendaylight.yangtools.yang.data.impl.schema.builder.impl.ImmutableMapNodeBuilder;
+import org.opendaylight.yangtools.yang.data.impl.schema.builder.impl.ImmutableUnkeyedListNodeBuilder;
+import org.opendaylight.yangtools.yang.data.impl.schema.builder.impl.ImmutableUserMapNodeBuilder;
+import org.opendaylight.yangtools.yang.model.api.DataSchemaNode;
+import org.opendaylight.yangtools.yang.model.api.SchemaContext;
+
+public final class ImmutableNodes {
+    // FIXME: YANGTOOLS-1074: we do not want this name
+    private static final NodeIdentifier SCHEMACONTEXT_NAME = NodeIdentifier.create(SchemaContext.NAME);
+
+    private ImmutableNodes() {
+        // Hidden on purpose
+    }
+
+    public static @NonNull CollectionNodeBuilder<MapEntryNode, SystemMapNode> mapNodeBuilder() {
+        return ImmutableMapNodeBuilder.create();
+    }
+
+    public static @NonNull CollectionNodeBuilder<MapEntryNode, SystemMapNode> mapNodeBuilder(final QName name) {
+        return mapNodeBuilder(NodeIdentifier.create(name));
+    }
+
+    public static @NonNull CollectionNodeBuilder<MapEntryNode, SystemMapNode> mapNodeBuilder(
+            final NodeIdentifier name) {
+        return ImmutableMapNodeBuilder.create().withNodeIdentifier(name);
+    }
+
+    /**
+     * Create an immutable map node.
+     *
+     * @param name QName which will be used as node identifier
+     * @return An unordered Map node
+     */
+    public static @NonNull SystemMapNode mapNode(final QName name) {
+        return mapNode(NodeIdentifier.create(name));
+    }
+
+    /**
+     * Create an immutable map node.
+     *
+     * @param name QName which will be used as node identifier
+     * @return An unordered Map node
+     */
+    public static @NonNull SystemMapNode mapNode(final NodeIdentifier name) {
+        return mapNodeBuilder(name).build();
+    }
+
+    /**
+     * Create immutable ordered map node.
+     *
+     * @param name QName which will be used as node identifier
+     * @return An ordered Map node
+     */
+    public static @NonNull UserMapNode orderedMapNode(final QName name) {
+        return orderedMapNode(NodeIdentifier.create(name));
+    }
+
+    /**
+     * Create immutable ordered map node.
+     *
+     * @param name Node identifier
+     * @return An ordered Map node
+     */
+    public static @NonNull UserMapNode orderedMapNode(final NodeIdentifier name) {
+        return ImmutableUserMapNodeBuilder.create().withNodeIdentifier(name).build();
+    }
+
+    /**
+     * Construct immutable leaf node.
+     *
+     * @param name Identifier of leaf node
+     * @param value Value of leaf node
+     * @param <T> Type of leaf node value
+     * @return Leaf node with supplied identifier and value
+     */
+    public static <T> @NonNull LeafNode<T> leafNode(final NodeIdentifier name, final T value) {
+        return ImmutableLeafNodeBuilder.createNode(name, value);
+    }
+
+    /**
+     * Construct immutable leaf node.
+     *
+     * @param name QName which will be used as node identifier
+     * @param value Value of leaf node.
+     * @param <T> Type of leaf node value
+     * @return Leaf node with supplied identifier and value
+     */
+    public static <T> @NonNull LeafNode<T> leafNode(final QName name, final T value) {
+        return leafNode(NodeIdentifier.create(name), value);
+    }
+
+    public static @NonNull DataContainerNodeBuilder<NodeIdentifierWithPredicates, MapEntryNode> mapEntryBuilder(
+            final QName nodeName, final QName keyName, final Object keyValue) {
+        return ImmutableMapEntryNodeBuilder.create()
+                .withNodeIdentifier(NodeIdentifierWithPredicates.of(nodeName, keyName, keyValue))
+                .withChild(leafNode(keyName, keyValue));
+    }
+
+    public static @NonNull DataContainerNodeBuilder<NodeIdentifierWithPredicates, MapEntryNode> mapEntryBuilder() {
+        return ImmutableMapEntryNodeBuilder.create();
+    }
+
+    public static @NonNull MapEntryNode mapEntry(final QName nodeName, final QName keyName, final Object keyValue) {
+        return mapEntryBuilder(nodeName, keyName, keyValue).build();
+    }
+
+    /**
+     * Create an immutable container node.
+     *
+     * @param name QName which will be used as node identifier
+     * @return A container node
+     */
+    public static @NonNull ContainerNode containerNode(final QName name) {
+        return containerNode(NodeIdentifier.create(name));
+    }
+
+    /**
+     * Create an immutable container node.
+     *
+     * @param name Node identifier
+     * @return A container node
+     */
+    public static @NonNull ContainerNode containerNode(final NodeIdentifier name) {
+        return ImmutableContainerNodeBuilder.create().withNodeIdentifier(name).build();
+    }
+
+    /**
+     * Create an immutable choice node.
+     *
+     * @param name QName which will be used as node identifier
+     * @return A choice node
+     */
+    public static @NonNull ChoiceNode choiceNode(final QName name) {
+        return choiceNode(NodeIdentifier.create(name));
+    }
+
+    /**
+     * Create an immutable choice node.
+     *
+     * @param name Node identifier
+     * @return A choice node
+     */
+    public static @NonNull ChoiceNode choiceNode(final NodeIdentifier name) {
+        return ImmutableChoiceNodeBuilder.create().withNodeIdentifier(name).build();
+    }
+
+    /**
+     * Create an immutable list node.
+     *
+     * @param name QName which will be used as node identifier
+     * @return An unkeyed list node
+     */
+    public static @NonNull UnkeyedListNode listNode(final QName name) {
+        return listNode(NodeIdentifier.create(name));
+    }
+
+    /**
+     * Create an immutable list node.
+     *
+     * @param name Node identifier
+     * @return An unkeyed list node
+     */
+    public static @NonNull UnkeyedListNode listNode(final NodeIdentifier name) {
+        return ImmutableUnkeyedListNodeBuilder.create().withNodeIdentifier(name).build();
+    }
+
+    /**
+     * Convert YangInstanceIdentifier into a normalized node structure.
+     *
+     * @param ctx schema context to used during serialization
+     * @param id instance identifier to convert to node structure starting from root
+     * @return serialized normalized node for provided instance Id
+     */
+    public static @NonNull NormalizedNode fromInstanceId(final SchemaContext ctx, final YangInstanceIdentifier id) {
+        return fromInstanceId(ctx, id, Optional.empty());
+    }
+
+    /**
+     * Convert YangInstanceIdentifier into a normalized node structure.
+     *
+     * @param ctx schema context to used during serialization
+     * @param id instance identifier to convert to node structure starting from root
+     * @param deepestElement pre-built deepest child that will be inserted at the last path argument of provided
+     *                       instance identifier
+     * @return serialized normalized node for provided instance Id with overridden last child.
+     */
+    public static @NonNull NormalizedNode fromInstanceId(final SchemaContext ctx, final YangInstanceIdentifier id,
+            final NormalizedNode deepestElement) {
+        return fromInstanceId(ctx, id, Optional.of(deepestElement));
+    }
+
+    /**
+     * Convert YangInstanceIdentifier into a normalized node structure.
+     *
+     * @param ctx schema context to used during serialization
+     * @param id instance identifier to convert to node structure starting from root
+     * @param deepestElement pre-built deepest child that will be inserted at the last path argument of provided
+     *                       instance identifier
+     * @return serialized normalized node for provided instance Id with (optionally) overridden last child
+     *         and (optionally) marked with specific operation attribute.
+     */
+    public static @NonNull NormalizedNode fromInstanceId(final SchemaContext ctx, final YangInstanceIdentifier id,
+            final Optional<NormalizedNode> deepestElement) {
+        final PathArgument topLevelElement;
+        final InstanceIdToNodes<?> instanceIdToNodes;
+        final Iterator<PathArgument> it = id.getPathArguments().iterator();
+        if (it.hasNext()) {
+            topLevelElement = it.next();
+            final DataSchemaNode dataChildByName = ctx.dataChildByName(topLevelElement.getNodeType());
+            checkNotNull(dataChildByName,
+                    "Cannot find %s node in schema context. Instance identifier has to start from root", topLevelElement);
+            instanceIdToNodes = InstanceIdToNodes.fromSchemaAndQNameChecked(ctx, topLevelElement.getNodeType());
+        } else {
+            topLevelElement = SCHEMACONTEXT_NAME;
+            instanceIdToNodes = InstanceIdToNodes.fromDataSchemaNode(ctx);
+        }
+
+        return instanceIdToNodes.create(topLevelElement, it, deepestElement);
+    }
+}
---
+++ netconf/sal-netconf-connector/src/main/java/org/opendaylight/netconf/sal/connect/netconf/util/InstanceIdToNodes.java
@@ -0,0 +1,238 @@
+/*
+ * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.netconf.sal.connect.netconf.util;
+
+import static com.google.common.base.Preconditions.checkArgument;
+import static com.google.common.base.Preconditions.checkState;
+
+import com.google.common.collect.Iterables;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Optional;
+import javax.xml.transform.dom.DOMSource;
+import org.eclipse.jdt.annotation.NonNull;
+import org.eclipse.jdt.annotation.Nullable;
+import org.opendaylight.yangtools.concepts.AbstractSimpleIdentifiable;
+import org.opendaylight.yangtools.yang.common.QName;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.NodeIdentifier;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.PathArgument;
+import org.opendaylight.yangtools.yang.data.api.schema.AnydataNode;
+import org.opendaylight.yangtools.yang.data.api.schema.DOMSourceAnyxmlNode;
+import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNode;
+import org.opendaylight.yangtools.yang.data.api.schema.UnkeyedListEntryNode;
+import org.opendaylight.yangtools.yang.data.api.schema.UnkeyedListNode;
+import org.opendaylight.yangtools.yang.data.api.schema.builder.CollectionNodeBuilder;
+import org.opendaylight.yangtools.yang.data.api.schema.builder.NormalizedNodeBuilder;
+import org.opendaylight.yangtools.yang.data.impl.schema.Builders;
+import org.opendaylight.yangtools.yang.model.api.AnydataSchemaNode;
+import org.opendaylight.yangtools.yang.model.api.AnyxmlSchemaNode;
+import org.opendaylight.yangtools.yang.model.api.AugmentationSchemaNode;
+import org.opendaylight.yangtools.yang.model.api.AugmentationTarget;
+import org.opendaylight.yangtools.yang.model.api.CaseSchemaNode;
+import org.opendaylight.yangtools.yang.model.api.ChoiceSchemaNode;
+import org.opendaylight.yangtools.yang.model.api.ContainerLike;
+import org.opendaylight.yangtools.yang.model.api.DataNodeContainer;
+import org.opendaylight.yangtools.yang.model.api.DataSchemaNode;
+import org.opendaylight.yangtools.yang.model.api.LeafListSchemaNode;
+import org.opendaylight.yangtools.yang.model.api.LeafSchemaNode;
+import org.opendaylight.yangtools.yang.model.api.ListSchemaNode;
+
+/**
+ * Base strategy for converting an instance identifier into a normalized node structure.
+ * Use provided static methods for generic YangInstanceIdentifier -> NormalizedNode translation in ImmutableNodes.
+ */
+abstract class InstanceIdToNodes<T extends PathArgument> extends AbstractSimpleIdentifiable<T> {
+    InstanceIdToNodes(final T identifier) {
+        super(identifier);
+    }
+
+    /**
+     * Build a strategy for the next path argument.
+     *
+     * @param child child identifier
+     * @return transformation strategy for a specific child
+     */
+    abstract @Nullable InstanceIdToNodes<?> getChild(PathArgument child);
+
+    /**
+     * Convert instance identifier into a NormalizedNode structure.
+     *
+     * @param instanceId Instance identifier to transform into NormalizedNodes
+     * @param deepestChild Optional normalized node to be inserted as the last child
+     * @param operation Optional modify operation to be set on the last child
+     * @return NormalizedNode structure corresponding to submitted instance ID
+     */
+    abstract @NonNull NormalizedNode create(PathArgument first, Iterator<PathArgument> others,
+            Optional<NormalizedNode> deepestChild);
+
+    abstract boolean isMixin();
+
+    private static final class UnkeyedListMixinNormalization extends InstanceIdToCompositeNodes<NodeIdentifier> {
+        private final UnkeyedListItemNormalization innerNode;
+
+        UnkeyedListMixinNormalization(final ListSchemaNode list) {
+            super(NodeIdentifier.create(list.getQName()));
+            this.innerNode = new UnkeyedListItemNormalization(list);
+        }
+
+        @Override
+        CollectionNodeBuilder<UnkeyedListEntryNode, UnkeyedListNode> createBuilder(final PathArgument compositeNode) {
+            return Builders.unkeyedListBuilder().withNodeIdentifier(getIdentifier());
+        }
+
+        @Override
+        InstanceIdToNodes<?> getChild(final PathArgument child) {
+            return child.getNodeType().equals(getIdentifier().getNodeType()) ? innerNode : null;
+        }
+
+        @Override
+        boolean isMixin() {
+            return true;
+        }
+    }
+
+    private abstract static class AbstractOpaqueNormalization extends InstanceIdToNodes<NodeIdentifier> {
+        AbstractOpaqueNormalization(final DataSchemaNode schema) {
+            super(NodeIdentifier.create(schema.getQName()));
+        }
+
+        @Override
+        final InstanceIdToNodes<?> getChild(final PathArgument child) {
+            return null;
+        }
+
+        @Override
+        final boolean isMixin() {
+            return false;
+        }
+    }
+
+    private static final class AnydataNormalization extends AbstractOpaqueNormalization {
+        AnydataNormalization(final AnydataSchemaNode schema) {
+            super(schema);
+        }
+
+        @Override
+        NormalizedNode create(final PathArgument first, final Iterator<PathArgument> others,
+                final Optional<NormalizedNode> deepestChild) {
+            checkState(deepestChild.isPresent(), "Cannot instantiate anydata node without a value");
+            final NormalizedNode child = deepestChild.get();
+            checkState(child instanceof AnydataNode, "Invalid child %s", child);
+            return createAnydata((AnydataNode<?>) child);
+        }
+
+        private <T> AnydataNode<T> createAnydata(final AnydataNode<T> child) {
+            return Builders.anydataBuilder(child.bodyObjectModel()).withValue(child.body())
+                    .withNodeIdentifier(getIdentifier()).build();
+        }
+    }
+
+    private static final class AnyXmlNormalization extends AbstractOpaqueNormalization {
+        AnyXmlNormalization(final AnyxmlSchemaNode schema) {
+            super(schema);
+        }
+
+        @Override
+        NormalizedNode create(final PathArgument first, final Iterator<PathArgument> others,
+                final Optional<NormalizedNode> deepestChild) {
+            final NormalizedNodeBuilder<NodeIdentifier, DOMSource, DOMSourceAnyxmlNode> builder =
+                    Builders.anyXmlBuilder()
+                            .withNodeIdentifier(getIdentifier());
+            if (deepestChild.isPresent()) {
+                final NormalizedNode child = deepestChild.get();
+                checkState(child instanceof DOMSourceAnyxmlNode, "Invalid child %s", child);
+                builder.withValue(((DOMSourceAnyxmlNode) child).body());
+            }
+
+            return builder.build();
+        }
+    }
+
+    private static Optional<DataSchemaNode> findChildSchemaNode(final DataNodeContainer parent, final QName child) {
+        final Optional<DataSchemaNode> potential = parent.findDataChildByName(child);
+        return potential.isPresent() ? potential : Optional.ofNullable(
+                findChoice(Iterables.filter(parent.getChildNodes(), ChoiceSchemaNode.class), child));
+    }
+
+    static @Nullable InstanceIdToNodes<?> fromSchemaAndQNameChecked(final DataNodeContainer schema, final QName child) {
+        final Optional<DataSchemaNode> potential = findChildSchemaNode(schema, child);
+        checkArgument(potential.isPresent(),
+                "Supplied QName %s is not valid according to schema %s, potential children nodes: %s", child, schema,
+                schema.getChildNodes());
+
+        final DataSchemaNode result = potential.get();
+        // We try to look up if this node was added by augmentation
+        if (schema instanceof DataSchemaNode && result.isAugmenting()) {
+            return fromAugmentation(schema, (AugmentationTarget) schema, result);
+        }
+        return fromDataSchemaNode(result);
+    }
+
+    private static @Nullable ChoiceSchemaNode findChoice(final Iterable<ChoiceSchemaNode> choices, final QName child) {
+        for (final ChoiceSchemaNode choice : choices) {
+            for (final CaseSchemaNode caze : choice.getCases()) {
+                if (findChildSchemaNode(caze, child).isPresent()) {
+                    return choice;
+                }
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Returns a SchemaPathUtil for provided child node
+     * <p/>
+     * If supplied child is added by Augmentation this operation returns
+     * a SchemaPathUtil for augmentation,
+     * otherwise returns a SchemaPathUtil for child as
+     * call for {@link #fromDataSchemaNode(org.opendaylight.yangtools.yang.model.api.DataSchemaNode)}.
+     */
+    private static @Nullable InstanceIdToNodes<?> fromAugmentation(final DataNodeContainer parent,
+            final AugmentationTarget parentAug, final DataSchemaNode child) {
+        for (final AugmentationSchemaNode aug : parentAug.getAvailableAugmentations()) {
+            final Optional<DataSchemaNode> potential = aug.findDataChildByName(child.getQName());
+            if (potential.isPresent()) {
+                return new InstanceIdToCompositeNodes.AugmentationNormalization(aug, parent);
+            }
+        }
+        return fromDataSchemaNode(child);
+    }
+
+    static @Nullable InstanceIdToNodes<?> fromDataSchemaNode(final DataSchemaNode potential) {
+        if (potential instanceof ContainerLike) {
+            return new InstanceIdToCompositeNodes.ContainerTransformation((ContainerLike) potential);
+        } else if (potential instanceof ListSchemaNode) {
+            return fromListSchemaNode((ListSchemaNode) potential);
+        } else if (potential instanceof LeafSchemaNode) {
+            return new InstanceIdToSimpleNodes.LeafNormalization((LeafSchemaNode) potential);
+        } else if (potential instanceof ChoiceSchemaNode) {
+            return new InstanceIdToCompositeNodes.ChoiceNodeNormalization((ChoiceSchemaNode) potential);
+        } else if (potential instanceof LeafListSchemaNode) {
+            return fromLeafListSchemaNode((LeafListSchemaNode) potential);
+        } else if (potential instanceof AnydataSchemaNode) {
+            return new AnydataNormalization((AnydataSchemaNode) potential);
+        } else if (potential instanceof AnyxmlSchemaNode) {
+            return new AnyXmlNormalization((AnyxmlSchemaNode) potential);
+        }
+        return null;
+    }
+
+    private static InstanceIdToNodes<?> fromListSchemaNode(final ListSchemaNode potential) {
+        final List<QName> keyDefinition = potential.getKeyDefinition();
+        if (keyDefinition == null || keyDefinition.isEmpty()) {
+            return new UnkeyedListMixinNormalization(potential);
+        }
+        return potential.isUserOrdered() ? new InstanceIdToCompositeNodes.OrderedMapMixinNormalization(potential)
+                : new InstanceIdToCompositeNodes.UnorderedMapMixinNormalization(potential);
+    }
+
+    private static InstanceIdToNodes<?> fromLeafListSchemaNode(final LeafListSchemaNode potential) {
+        return potential.isUserOrdered() ? new InstanceIdToCompositeNodes.OrderedLeafListMixinNormalization(potential)
+                : new InstanceIdToCompositeNodes.UnorderedLeafListMixinNormalization(potential);
+    }
+}
