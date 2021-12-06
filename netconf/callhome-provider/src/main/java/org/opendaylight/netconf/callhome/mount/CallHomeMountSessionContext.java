e34cf406359e97bd3676e898b64f9bff684b5665
Ivan Hrasko <ivan.hrasko@pantheon.tech>
2021-12-06 11:58:02 +0100

    Refactor NETCONF node defaults
    In fact, the difference between devices connected by call-home and created
    manually in NETCONF topology is that call-home devices do not have any data
    in configuration data-store.
    
    Configuration data is used during the connection setup.
    
    We can create full default configuration node for call-home connected device
    to unify the processing of both kind of devices.
    
    In addition, MDSAL now supports default values in data-store so we can remove
    those values from AbstractNetconfTopology.
    
    JIRA: NETCONF-832
    Change-Id: Ib58ca344a7d74612cb7d049805a3e3c9279986d4
    Signed-off-by: Ivan Hrasko <ivan.hrasko@pantheon.tech>
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- netconf/callhome-provider/src/main/java/org/opendaylight/netconf/callhome/mount/CallHomeMountSessionContext.java
+++ netconf/callhome-provider/src/main/java/org/opendaylight/netconf/callhome/mount/CallHomeMountSessionContext.java
@@ -12,6 +12,7 @@ import static java.util.Objects.requireNonNull;
 import com.google.common.base.MoreObjects;
 import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import io.netty.util.concurrent.Future;
+import java.math.BigDecimal;
 import org.opendaylight.netconf.api.NetconfMessage;
 import org.opendaylight.netconf.api.NetconfTerminationReason;
 import org.opendaylight.netconf.callhome.protocol.CallHomeChannelActivator;
@@ -20,10 +21,14 @@ import org.opendaylight.netconf.client.NetconfClientSession;
 import org.opendaylight.netconf.client.NetconfClientSessionListener;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev130715.Host;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.netconf.node.topology.rev150114.NetconfNodeBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netconf.node.topology.rev150114.netconf.node.connection.parameters.Protocol;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netconf.node.topology.rev150114.netconf.node.connection.parameters.ProtocolBuilder;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.netconf.node.topology.rev150114.netconf.node.credentials.credentials.LoginPasswordBuilder;
 import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.NodeId;
 import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.Node;
 import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.NodeBuilder;
+import org.opendaylight.yangtools.yang.common.Uint16;
+import org.opendaylight.yangtools.yang.common.Uint32;
 
 // Non-final to allow mocking
 class CallHomeMountSessionContext {
@@ -61,18 +66,41 @@ class CallHomeMountSessionContext {
         return key;
     }
 
+    /**
+     * Create device default configuration.
+     *
+     * <p>
+     * This configuration is a replacement of configuration device data
+     * which is normally stored in configuration datastore but is absent for call-home devices.
+     *
+     * @return {@link Node} containing the default device configuration
+     */
+    // FIXME make these defaults tune-able in odl-netconf-callhome-server
     Node getConfigNode() {
         return new NodeBuilder()
                 .setNodeId(getId())
                 .addAugmentation(new NetconfNodeBuilder()
-                    .setHost(new Host(key.getIpAddress()))
-                    .setPort(key.getPort())
-                    .setTcpOnly(Boolean.FALSE)
-                    .setCredentials(new LoginPasswordBuilder()
-                        .setUsername("ommited")
-                        .setPassword("ommited")
-                        .build())
-                    .setSchemaless(Boolean.FALSE)
+                        .setHost(new Host(key.getIpAddress()))
+                        .setPort(key.getPort())
+                        .setTcpOnly(false)
+                        .setProtocol(new ProtocolBuilder()
+                                .setName(Protocol.Name.valueOf(protocol.getTransportType().name()))
+                                .build())
+                        .setSchemaless(false)
+                        .setReconnectOnChangedSchema(false)
+                        .setConnectionTimeoutMillis(Uint32.valueOf(20000))
+                        .setDefaultRequestTimeoutMillis(Uint32.valueOf(60000))
+                        .setMaxConnectionAttempts(Uint32.ZERO)
+                        .setBetweenAttemptsTimeoutMillis(Uint16.valueOf(2000))
+                        .setSleepFactor(new BigDecimal("1.5"))
+                        .setKeepaliveDelay(Uint32.valueOf(120))
+                        .setConcurrentRpcLimit(Uint16.ZERO)
+                        .setActorResponseWaitTime(Uint16.valueOf(5))
+                        // the real call-home device credentials are applied in CallHomeAuthProviderImpl
+                        .setCredentials(new LoginPasswordBuilder()
+                                .setUsername("omitted")
+                                .setPassword("omitted")
+                                .build())
                     .build())
                 .build();
     }
