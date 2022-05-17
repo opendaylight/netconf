31fb4c7923a3acb70e395fb8775264b3361b7407
Robert Varga <robert.varga@pantheon.tech>
2022-05-17 11:53:23 +0200

    Centralize NetconfNode/InetSocketAddress conversion
    The process of creating a RemoteDeviceId and/or the target
    InetSocketAddress is duplicated in a number of places, each slightly
    different.
    
    Centralize conversion in netconf.topology.spi and use it from there,
    which leads us to topology-singleton being able to use non-IP addresses.
    
    JIRA: NETCONF-875
    Change-Id: I5c4a610eb4f4b3cd6586198d5e9a4b84b476d351
    Signed-off-by: Rohini.Ambika <rohini.ambika@infosys.com>
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>
    (cherry picked from commit 90dcdd5d040eb22b74e87baccffd70a30d93bfc0)

---
+++ netconf/netconf-topology/src/test/java/org/opendaylight/netconf/topology/spi/NetconfNodeUtilsTest.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2022 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.netconf.topology.spi;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.opendaylight.netconf.sal.connect.util.RemoteDeviceId;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev130715.Host;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev130715.IpAddress;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev130715.Ipv4Address;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev130715.PortNumber;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netconf.node.topology.rev150114.NetconfNodeBuilder;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.NodeId;
+import org.opendaylight.yangtools.yang.common.Uint16;
+
+public class NetconfNodeUtilsTest {
+    @Test
+    public void testCreateRemoteDeviceId() {
+        final Host host = new Host(new IpAddress(new Ipv4Address("127.0.0.1")));
+        final RemoteDeviceId id = NetconfNodeUtils.toRemoteDeviceId(new NodeId("testing-node"), new NetconfNodeBuilder()
+            .setHost(host)
+            .setPort(new PortNumber(Uint16.valueOf(9999)))
+            .build());
+
+        assertEquals("testing-node", id.getName());
+        assertEquals(host, id.getHost());
+        assertEquals(9999, id.getAddress().getPort());
+    }
+}
