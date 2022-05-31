bebb527a997ac93766c4639c9ee04c2ac7b2995b
Robert Varga <robert.varga@pantheon.tech>
2022-05-31 22:30:11 +0200

    Use a simple future
    Channel is an invariant, hence we do not need to dereference
    ChannelFuture, allowing us to reduce verbosity quite a bit.
    
    Change-Id: Ifa9b9b89e1841479f3e66141e02b2619492f3900
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- netconf/netconf-netty-util/src/main/java/org/opendaylight/netconf/nettyutil/AbstractNetconfSessionNegotiator.java
+++ netconf/netconf-netty-util/src/main/java/org/opendaylight/netconf/nettyutil/AbstractNetconfSessionNegotiator.java
@@ -11,14 +11,12 @@ import static com.google.common.base.Preconditions.checkState;
 import static java.util.Objects.requireNonNull;
 
 import io.netty.channel.Channel;
-import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.handler.ssl.SslHandler;
 import io.netty.util.Timeout;
 import io.netty.util.Timer;
-import io.netty.util.concurrent.GenericFutureListener;
 import io.netty.util.concurrent.Promise;
 import java.util.Optional;
 import java.util.concurrent.TimeUnit;
@@ -134,11 +132,11 @@ public abstract class AbstractNetconfSessionNegotiator<P extends NetconfSessionP
                     connectionTimeoutMillis);
                 changeState(State.FAILED);
 
-                channel.close().addListener((GenericFutureListener<ChannelFuture>) future -> {
+                channel.close().addListener(future -> {
                     if (future.isSuccess()) {
-                        LOG.debug("Channel {} closed: success", future.channel());
+                        LOG.debug("Channel {} closed: success", channel);
                     } else {
-                        LOG.warn("Channel {} closed: fail", future.channel());
+                        LOG.warn("Channel {} closed: fail", channel);
                     }
                 });
             }
