c232bd24c4c3fab95548e78562d80c9c2c7f079b
Robert Varga <robert.varga@pantheon.tech>
2022-06-02 20:40:16 +0200

    NetconfSessionPreferences should be abstract
    The class should be abstract with some specializations. Also specify
    a toString() method based on ToStringHelper.
    
    Change-Id: Ib1053c7475c384661f7427c48bfa4fea3444f25a
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- netconf/netconf-api/src/main/java/org/opendaylight/netconf/api/NetconfSessionPreferences.java
+++ netconf/netconf-api/src/main/java/org/opendaylight/netconf/api/NetconfSessionPreferences.java
@@ -5,17 +5,20 @@
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
-
 package org.opendaylight.netconf.api;
 
+import static java.util.Objects.requireNonNull;
+
+import com.google.common.base.MoreObjects;
+import com.google.common.base.MoreObjects.ToStringHelper;
+import org.eclipse.jdt.annotation.NonNull;
 import org.opendaylight.netconf.api.messages.NetconfHelloMessage;
 
-public class NetconfSessionPreferences {
+public abstract class NetconfSessionPreferences {
+    private final @NonNull NetconfHelloMessage helloMessage;
 
-    private final NetconfHelloMessage helloMessage;
-
-    public NetconfSessionPreferences(final NetconfHelloMessage helloMessage) {
-        this.helloMessage = helloMessage;
+    protected NetconfSessionPreferences(final NetconfHelloMessage helloMessage) {
+        this.helloMessage = requireNonNull(helloMessage);
     }
 
     /**
@@ -23,8 +26,16 @@ public class NetconfSessionPreferences {
      *
      * @return the helloMessage
      */
-    public NetconfHelloMessage getHelloMessage() {
-        return this.helloMessage;
+    public final @NonNull NetconfHelloMessage getHelloMessage() {
+        return helloMessage;
     }
 
+    @Override
+    public final String toString() {
+        return addToStringAttributes(MoreObjects.toStringHelper(this).omitNullValues()).toString();
+    }
+
+    protected ToStringHelper addToStringAttributes(final ToStringHelper helper) {
+        return helper.add("hello", helloMessage);
+    }
 }
