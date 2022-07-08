4f4788af20f067bde706713849fb3fdf832172b3
Ivan Martiniak <ivan.martiniak@pantheon.tech>
2022-07-08 14:24:51 +0200

    Remove unused parameter in CallHomeSessionContext.java
    Parameter in the constructor 'final SocketAddress remoteAddress'
    was marked as unused.
    Method and tests related to this constructor were adjusted.
    
    JIRA: NETCONF-681
    Change-Id: Ifd8a76c6edcf801e432b7b925873806014b43b1a
    Signed-off-by: Ivan Martiniak <ivan.martiniak@pantheon.tech>

--- netconf/callhome-protocol/src/main/java/org/opendaylight/netconf/callhome/protocol/CallHomeSessionContext.java
+++ netconf/callhome-protocol/src/main/java/org/opendaylight/netconf/callhome/protocol/CallHomeSessionContext.java
@@ -16,7 +16,6 @@ import io.netty.util.concurrent.GlobalEventExecutor;
 import io.netty.util.concurrent.Promise;
 import java.io.IOException;
 import java.net.InetSocketAddress;
-import java.net.SocketAddress;
 import java.security.PublicKey;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
@@ -51,7 +50,7 @@ class CallHomeSessionContext implements CallHomeProtocolSessionContext {
 
     @SuppressFBWarnings(value = "MC_OVERRIDABLE_METHOD_CALL_IN_CONSTRUCTOR", justification = "Passing 'this' around")
     CallHomeSessionContext(final ClientSession sshSession, final CallHomeAuthorization authorization,
-                           final SocketAddress remoteAddress, final Factory factory) {
+                           final Factory factory) {
         this.authorization = requireNonNull(authorization, "authorization");
         checkArgument(this.authorization.isServerAllowed(), "Server was not allowed.");
         this.factory = requireNonNull(factory);
@@ -176,10 +175,8 @@ class CallHomeSessionContext implements CallHomeProtocolSessionContext {
             return subsystemListener;
         }
 
-        void createIfNotExists(final ClientSession sshSession,
-                final CallHomeAuthorization authorization, final SocketAddress remoteAddress) {
-            CallHomeSessionContext session = new CallHomeSessionContext(sshSession, authorization,
-                    remoteAddress, this);
+        void createIfNotExists(final ClientSession sshSession, final CallHomeAuthorization authorization) {
+            CallHomeSessionContext session = new CallHomeSessionContext(sshSession, authorization, this);
             sessions.putIfAbsent(session.getSessionId(), session);
         }
 
