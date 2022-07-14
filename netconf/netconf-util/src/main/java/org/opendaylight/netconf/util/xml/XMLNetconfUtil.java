b75ba1717154ecabbcc2b41bd134cb871e391d16
Sangwook Ha <sangwook.ha@verizon.com>
2022-07-14 01:03:54 -0700

    Filter non-standard nodes from NETCONF monitoring schemas
    Some NETCONF servers use augmented netconf-monitoring schema and
    controller fails to parse XML with strict parsing requirement.
    
    Add a filtering function based on the namespace of the XML node,
    and filter out any node not in the netconf-monitoring namespace to
    prevent parsing failure.
    
    JIRA: NETCONF-881
    Change-Id: I93b7da0baf00de59613d970fd4ec89e47eb26e58
    Signed-off-by: Sangwook Ha <sangwook.ha@verizon.com>
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- netconf/netconf-util/src/main/java/org/opendaylight/netconf/util/xml/XMLNetconfUtil.java
+++ netconf/netconf-util/src/main/java/org/opendaylight/netconf/util/xml/XMLNetconfUtil.java
@@ -7,12 +7,20 @@
  */
 package org.opendaylight.netconf.util.xml;
 
+import java.util.Set;
 import javax.xml.namespace.NamespaceContext;
+import javax.xml.transform.dom.DOMSource;
 import javax.xml.xpath.XPath;
 import javax.xml.xpath.XPathExpression;
 import javax.xml.xpath.XPathExpressionException;
 import javax.xml.xpath.XPathFactory;
 import org.opendaylight.netconf.api.xml.XmlNetconfConstants;
+import org.opendaylight.netconf.api.xml.XmlUtil;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.traversal.DocumentTraversal;
+import org.w3c.dom.traversal.NodeFilter;
+import org.w3c.dom.traversal.TreeWalker;
 
 public final class XMLNetconfUtil {
     private static final XPathFactory FACTORY = XPathFactory.newInstance();
@@ -33,4 +41,33 @@ public final class XMLNetconfUtil {
         }
     }
 
+    public static DOMSource filterDomNamespaces(final DOMSource domSource, final Set<String> namespacesAllowed) {
+        final var sourceDoc = XmlUtil.newDocument();
+        sourceDoc.appendChild(sourceDoc.importNode(domSource.getNode(), true));
+
+        final var treeWalker = ((DocumentTraversal) sourceDoc).createTreeWalker(sourceDoc.getDocumentElement(),
+            NodeFilter.SHOW_ALL, node -> {
+                final var namespace = node.getNamespaceURI();
+                return namespace == null || namespacesAllowed.contains(node.getNamespaceURI())
+                    ? NodeFilter.FILTER_ACCEPT : NodeFilter.FILTER_REJECT;
+            }, false);
+
+        final var filteredDoc = XmlUtil.newDocument();
+        filteredDoc.appendChild(filteredDoc.importNode(treeWalker.getRoot(), false));
+        filterChildren(treeWalker, filteredDoc, filteredDoc.getDocumentElement());
+
+        return new DOMSource(filteredDoc.getDocumentElement());
+    }
+
+    private static void filterChildren(final TreeWalker treeWalker, final Document filteredDoc,
+            final Node filteredNode) {
+        if (treeWalker.firstChild() != null) {
+            for (var node = treeWalker.getCurrentNode(); node != null; node = treeWalker.nextSibling()) {
+                final var newFilteredNode = filteredDoc.importNode(node, false);
+                filteredNode.appendChild(newFilteredNode);
+                filterChildren(treeWalker, filteredDoc, newFilteredNode);
+                treeWalker.setCurrentNode(node);
+            }
+        }
+    }
 }
