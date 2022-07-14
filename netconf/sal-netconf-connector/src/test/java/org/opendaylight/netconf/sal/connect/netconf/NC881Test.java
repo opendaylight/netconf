54780e2965572914fd93c86c32324303744aa70b
Sangwook Ha <sangwook.ha@verizon.com>
2022-07-14 01:03:54 -0700

    Filter non-standard nodes from NETCONF monitoring schemas
    Some NETCONF servers use augmented netconf-monitoring schema and
    controller fails to parse XML with strict parsing requirement.
    
    Add a filtering function based on the namespace of the XML node,
    and filter out any node not in the netconf-monitoring namespace to
    prevent parsing failure.
    
    JIRA: NETCONF-881
    Change-Id: I93b7da0baf00de59613d970fd4ec89e47eb26e58
    Signed-off-by: Sangwook Ha <sangwook.ha@verizon.com>
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ netconf/sal-netconf-connector/src/test/java/org/opendaylight/netconf/sal/connect/netconf/NC881Test.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2022 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.netconf.sal.connect.netconf;
+
+import static org.junit.Assert.assertTrue;
+
+import java.io.IOException;
+import javax.xml.transform.dom.DOMSource;
+import org.custommonkey.xmlunit.XMLUnit;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.opendaylight.netconf.api.xml.XmlUtil;
+import org.xml.sax.SAXException;
+
+public class NC881Test {
+    @BeforeClass
+    public static void classSetUp() {
+        XMLUnit.setIgnoreWhitespace(true);
+    }
+
+    @Test
+    public void testFilterDomNamespaces() throws IOException, SAXException {
+        final var source = XmlUtil.readXmlToDocument(
+                getClass().getResourceAsStream("/nc881/netconf-state.xml"));
+        final var expected = XmlUtil.readXmlToDocument(
+                getClass().getResourceAsStream("/nc881/netconf-state-filtered.xml"));
+
+        final var filteredDom = NetconfStateSchemas.ietfMonitoringCopy(new DOMSource(source.getDocumentElement()));
+        final var filtered = XmlUtil.newDocument();
+        filtered.appendChild(filtered.importNode(filteredDom.getNode(), true));
+
+        final var diff = XMLUnit.compareXML(filtered, expected);
+        assertTrue(diff.toString(), diff.similar());
+    }
+}
