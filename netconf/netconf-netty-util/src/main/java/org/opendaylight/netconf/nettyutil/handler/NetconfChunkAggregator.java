1a2fd885d98974336cc136dc582d9dc1c910a7b9
Robert Varga <robert.varga@pantheon.tech>
2022-07-29 18:32:03 +0200

    Require maximum chunk size to be specified
    The default incoming chunk size is controlled by session negotiator,
    make sure to co-locate the tuneable value there and remove deprecated
    constructors for both NetconfChunkAggregator and
    AbstractNetconfSessionNegotiator.
    
    Change-Id: Iab489e35e18905c33a747a52c1698ece4e51d3ef
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- netconf/netconf-netty-util/src/main/java/org/opendaylight/netconf/nettyutil/handler/NetconfChunkAggregator.java
+++ netconf/netconf-netty-util/src/main/java/org/opendaylight/netconf/nettyutil/handler/NetconfChunkAggregator.java
@@ -16,7 +16,6 @@ import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.ByteToMessageDecoder;
 import java.util.List;
 import org.checkerframework.checker.index.qual.NonNegative;
-import org.opendaylight.netconf.nettyutil.AbstractNetconfSessionNegotiator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -26,10 +25,6 @@ public class NetconfChunkAggregator extends ByteToMessageDecoder {
     private static final String GOT_PARAM_WHILE_WAITING_FOR_PARAM_PARAM = "Got byte {} while waiting for {}-{}";
     private static final String GOT_PARAM_WHILE_WAITING_FOR_PARAM_PARAM_PARAM =
         "Got byte {} while waiting for {}-{}-{}";
-
-    @Deprecated(since = "4.0.1", forRemoval = true)
-    public static final @NonNegative int DEFAULT_MAXIMUM_CHUNK_SIZE =
-        AbstractNetconfSessionNegotiator.DEFAULT_MAXIMUM_INCOMING_CHUNK_SIZE;
 
     private enum State {
         HEADER_ONE, // \n
@@ -47,16 +42,6 @@ public class NetconfChunkAggregator extends ByteToMessageDecoder {
     private State state = State.HEADER_ONE;
     private long chunkSize;
     private CompositeByteBuf chunk;
-
-    /**
-     * Construct an instance with maximum chunk size set to {@link #DEFAULT_MAXIMUM_CHUNK_SIZE}.
-     *
-     * @deprecated Prefer {@link #NetconfChunkAggregator(int)} for fine-grained control.
-     */
-    @Deprecated(since = "4.0.1", forRemoval = true)
-    public NetconfChunkAggregator() {
-        this(DEFAULT_MAXIMUM_CHUNK_SIZE);
-    }
 
     /**
      * Construct an instance with specified maximum chunk size.
