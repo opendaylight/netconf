5ab866d3e64ba09de0c78b718f322297dbb11b55
Robert Varga <robert.varga@pantheon.tech>
2022-08-10 20:23:21 +0200

    WIP: Add transport-{api,tcp}
    NETCONF (and potentially RESTCONF) has rather well-defined concepts
    how it works on the transport layer. Furthermore there are quite mature
    YANG models covering the configuration of both NETCONF server and
    NETCONF client.
    
    This patch introduces the transport-level APIs to establish a transport
    stack and its interactions with protocol negotiation and then the
    messages layer. A baseline implementation based on plain TCP is also
    provided.
    
    // FIXME: add unit tests
    // FIXME: add javadocs
    
    JIRA: NETCONF-590
    Change-Id: I29c207a2036e1c54ea1f6d7fe48f7eb42a3a4a7d
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ transport/transport-api/src/main/java/org/opendaylight/netconf/transport/api/AbstractTransportStack.java
@@ -0,0 +1,165 @@
+/*
+ * Copyright (c) 2022 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.netconf.transport.api;
+
+import static java.util.Objects.requireNonNull;
+
+import com.google.common.base.MoreObjects;
+import com.google.common.base.MoreObjects.ToStringHelper;
+import com.google.common.util.concurrent.Futures;
+import com.google.common.util.concurrent.ListenableFuture;
+import com.google.common.util.concurrent.MoreExecutors;
+import com.google.common.util.concurrent.SettableFuture;
+import io.netty.util.concurrent.Future;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+import org.checkerframework.checker.lock.qual.GuardedBy;
+import org.eclipse.jdt.annotation.NonNull;
+import org.opendaylight.yangtools.yang.common.Empty;
+
+/**
+ * Convenience base class for {@link TransportStack} implementations. It mediates atomic idempotence of
+ * {@link #shutdown()}.
+ */
+public abstract class AbstractTransportStack<T extends TransportChannel> implements TransportStack {
+    private final @NonNull TransportChannelListener listener;
+
+    /**
+     * Polymorphic state. It can be in one of four states:
+     * <ol>
+     *   <li>{@code null} when there is no attached transport channel,</li>
+     *   <li>a {@link TransportChannel} instance when there is exactly one attached transport channel,</li>
+     *   <li>a {@link Set} holding multiple attached transport channels,</li>
+     *   <li>a {@link ListenableFuture} completing when shutdown is complete
+     * </ol>
+     */
+    @GuardedBy("this")
+    private Object state;
+
+    protected AbstractTransportStack(final TransportChannelListener listener) {
+        this.listener = requireNonNull(listener);
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public final ListenableFuture<Empty> shutdown() {
+        final SettableFuture<Empty> future;
+        final Set<TransportChannel> channels;
+
+        synchronized (this) {
+            var local = state;
+            if (local instanceof ListenableFuture) {
+                // Already shutting down, no-op
+                return (ListenableFuture<Empty>) local;
+            }
+
+            if (local == null) {
+                channels = Set.of();
+            } else if (local instanceof Set) {
+                channels = (Set<TransportChannel>) local;
+            } else if (local instanceof TransportChannel tc) {
+                channels = Set.of(tc);
+            } else {
+                throw new IllegalStateException("Unexpected state " + local);
+            }
+            state = future = SettableFuture.create();
+        }
+
+        final var futures = new ArrayList<ListenableFuture<?>>(channels.size() + 1);
+        futures.add(startShutdown());
+        for (var channel : channels) {
+            futures.add(toListenableFuture(channel.channel().close()));
+        }
+        Futures.whenAllComplete(futures).run(() -> future.set(Empty.value()), MoreExecutors.directExecutor());
+        return future;
+    }
+
+    protected abstract @NonNull ListenableFuture<Empty> startShutdown();
+
+    protected final void addTransportChannel(final @NonNull T channel) {
+        // Careful stepping here so we do not invoke callbacks while holding a lock...
+        final var ch = channel.channel();
+        if (add(channel)) {
+            // The channel is tracked in state, make sure we remove it when it goes away. Invoke the user listener only
+            // after that, so our listener fires first (and the user does not have a chance to close() before that).
+            ch.closeFuture().addListener(ignored -> remove(channel));
+            listener.onTransportChannelEstablished(channel);
+        } else {
+            // We are already shutting down, just close the channel
+            ch.close();
+        }
+    }
+
+    protected final void notifyTransportChannelFailed(final @NonNull Throwable cause) {
+        listener.onTransportChannelFailed(cause);
+    }
+
+    @Override
+    public final String toString() {
+        return addToStringAttributes(MoreObjects.toStringHelper(this).omitNullValues()).toString();
+    }
+
+    protected synchronized ToStringHelper addToStringAttributes(final ToStringHelper helper) {
+        return helper.add("listener", listener).add("state", state);
+    }
+
+    protected static final @NonNull ListenableFuture<Empty> toListenableFuture(final Future<?> nettyFuture) {
+        final var ret = SettableFuture.<Empty>create();
+        nettyFuture.addListener(future -> {
+            if (future.isSuccess()) {
+                ret.set(Empty.value());
+            } else {
+                ret.setException(future.cause());
+            }
+        });
+        return ret;
+    }
+
+    @SuppressWarnings("unchecked")
+    private synchronized boolean add(final @NonNull TransportChannel channel) {
+        final var local = state;
+        if (local instanceof ListenableFuture) {
+            // Already shutting down
+            return false;
+        }
+        if (local == null) {
+            // First session, simple
+            state = channel;
+        } else if (local instanceof Set) {
+            ((Set<TransportChannel>) local).add(channel);
+        } else if (local instanceof TransportChannel tc) {
+            final var set = new HashSet<TransportChannel>(4);
+            set.add(tc);
+            set.add(channel);
+            state = set;
+        } else {
+            throw new IllegalStateException("Unhandled state " + local);
+        }
+        return true;
+    }
+
+    private synchronized void remove(final @NonNull TransportChannel channel) {
+        final var local = state;
+        if (local == null || local instanceof ListenableFuture) {
+            // No recorded channel or we are already shutting down
+            return;
+        }
+
+        if (local.equals(channel)) {
+            // Single channel, go back to null
+            state = null;
+        } else if (local instanceof Set<?> set) {
+            // Multiple channels, let's just remove it. Note this does not collapse the set if there's just one
+            // remaining session -- the chances are we will go back to more than one soon
+            set.remove(channel);
+        } else {
+            throw new IllegalStateException("Unhandled state " + local);
+        }
+    }
+}
