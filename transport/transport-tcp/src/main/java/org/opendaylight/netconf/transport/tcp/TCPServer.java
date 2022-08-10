7348101e8f2db43647f76b7c07375f8adc192498
Robert Varga <robert.varga@pantheon.tech>
2022-08-10 20:23:21 +0200

    WIP: Add transport-{api,tcp}
    NETCONF (and potentially RESTCONF) has rather well-defined concepts
    how it works on the transport layer. Furthermore there are quite mature
    YANG models covering the configuration of both NETCONF server and
    NETCONF client.
    
    This patch introduces the transport-level APIs to establish a transport
    stack and its interactions with protocol negotiation and then the
    messages layer. A baseline implementation based on plain TCP is also
    provided.
    
    // FIXME: add unit tests
    // FIXME: add javadocs
    // FIXME: shutdown lifecycle is probably not right (see TLS)
    
    JIRA: NETCONF-590
    Change-Id: I29c207a2036e1c54ea1f6d7fe48f7eb42a3a4a7d
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ transport/transport-tcp/src/main/java/org/opendaylight/netconf/transport/tcp/TCPServer.java
@@ -0,0 +1,86 @@
+/*
+ * Copyright (c) 2022 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.netconf.transport.tcp;
+
+import static com.google.common.base.Verify.verifyNotNull;
+import static java.util.Objects.requireNonNull;
+
+import com.google.common.util.concurrent.ListenableFuture;
+import com.google.common.util.concurrent.SettableFuture;
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.ChannelHandler.Sharable;
+import io.netty.channel.ChannelInitializer;
+import org.eclipse.jdt.annotation.NonNull;
+import org.opendaylight.netconf.transport.api.TransportChannelListener;
+import org.opendaylight.netconf.transport.api.TransportStack;
+import org.opendaylight.netconf.transport.api.UnsupportedConfigurationException;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.tcp.server.rev220524.TcpServerGrouping;
+import org.opendaylight.yangtools.yang.common.Empty;
+
+/**
+ * A {@link TransportStack} acting as a TCP server.
+ */
+public final class TCPServer extends TCPTransportStack {
+    @Sharable
+    private static final class ListenChannelInitializer extends ChannelInitializer<Channel> {
+        private final TCPServer stack;
+
+        ListenChannelInitializer(final TCPServer stack) {
+            this.stack = requireNonNull(stack);
+        }
+
+        @Override
+        protected void initChannel(final Channel ch) {
+            verifyNotNull(stack, "Stack not initialized while handling channel %s", ch)
+                .onTransportChannelEstablished(new TCPTransportChannel(ch));
+        }
+    }
+
+    private volatile Channel listenChannel;
+
+    private TCPServer(final TransportChannelListener listener) {
+        super(listener);
+    }
+
+    public static @NonNull ListenableFuture<TCPServer> listen(final TransportChannelListener listener,
+            final ServerBootstrap bootstrap, final TcpServerGrouping listenParams)
+                throws UnsupportedConfigurationException {
+        NettyTransportSupport.configureKeepalives(bootstrap, listenParams.getKeepalives());
+
+        final var ret = SettableFuture.<TCPServer>create();
+        final var stack = new TCPServer(listener);
+        final var initializer = new ListenChannelInitializer(stack);
+        bootstrap
+            .childHandler(initializer)
+            .bind(socketAddressOf(listenParams.requireLocalAddress(), listenParams.requireLocalPort()))
+            .addListener((ChannelFutureListener) future -> {
+                if (future.isSuccess()) {
+                    stack.setListenChannel(future.channel());
+                    ret.set(stack);
+                } else {
+                    ret.setException(future.cause());
+                }
+            });
+        return ret;
+    }
+
+    @Override
+    protected ListenableFuture<Empty> startShutdown() {
+        return toListenableFuture(listenChannel().close());
+    }
+
+    private @NonNull Channel listenChannel() {
+        return verifyNotNull(listenChannel, "Channel not initialized");
+    }
+
+    private void setListenChannel(final Channel listenChannel) {
+        this.listenChannel = requireNonNull(listenChannel);
+    }
+}
