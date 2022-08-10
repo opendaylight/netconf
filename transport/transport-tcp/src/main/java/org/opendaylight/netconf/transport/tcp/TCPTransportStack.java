df5f3f5fb068ab16e918a2634137e134e10e5198
Robert Varga <robert.varga@pantheon.tech>
2022-08-10 20:23:21 +0200

    WIP: Add transport-{api,tcp}
    NETCONF (and potentially RESTCONF) has rather well-defined concepts
    how it works on the transport layer. Furthermore there are quite mature
    YANG models covering the configuration of both NETCONF server and
    NETCONF client.
    
    This patch introduces the transport-level APIs to establish a transport
    stack and its interactions with protocol negotiation and then the
    messages layer. A baseline implementation based on plain TCP is also
    provided.
    
    // FIXME: add unit tests
    // FIXME: add javadocs
    // FIXME: shutdown lifecycle is probably not right (see TLS)
    
    JIRA: NETCONF-590
    Change-Id: I29c207a2036e1c54ea1f6d7fe48f7eb42a3a4a7d
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ transport/transport-tcp/src/main/java/org/opendaylight/netconf/transport/tcp/TCPTransportStack.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2022 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.netconf.transport.tcp;
+
+import java.net.InetSocketAddress;
+import org.opendaylight.netconf.transport.api.AbstractTransportStack;
+import org.opendaylight.netconf.transport.api.TransportChannelListener;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev130715.Host;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev130715.IetfInetUtil;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev130715.IpAddress;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev130715.PortNumber;
+
+/**
+ * Base class for TCP TransportStacks.
+ */
+public abstract sealed class TCPTransportStack extends AbstractTransportStack permits TCPClient, TCPServer {
+    TCPTransportStack(final TransportChannelListener listener) {
+        super(listener);
+    }
+
+    static final InetSocketAddress socketAddressOf(final Host host, final PortNumber port) {
+        final var addr = host.getIpAddress();
+        return addr != null ? socketAddressOf(addr, port)
+            : InetSocketAddress.createUnresolved(host.getDomainName().getValue(), port.getValue().toJava());
+    }
+
+    static final InetSocketAddress socketAddressOf(final IpAddress addr, final PortNumber port) {
+        final int portNum = port == null ? 0 : port.getValue().toJava();
+        if (addr == null) {
+            return port == null ? null : new InetSocketAddress(portNum);
+        }
+        return new InetSocketAddress(IetfInetUtil.INSTANCE.inetAddressFor(addr), portNum);
+    }
+}
