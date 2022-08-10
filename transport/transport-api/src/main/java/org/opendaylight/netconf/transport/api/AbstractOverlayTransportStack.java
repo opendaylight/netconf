b69cfaa35695c2875fa83ead972ec50a09de462c
Robert Varga <robert.varga@pantheon.tech>
2022-08-10 20:23:21 +0200

    WIP: Add transport-{api,tcp}
    NETCONF (and potentially RESTCONF) has rather well-defined concepts
    how it works on the transport layer. Furthermore there are quite mature
    YANG models covering the configuration of both NETCONF server and
    NETCONF client.
    
    This patch introduces the transport-level APIs to establish a transport
    stack and its interactions with protocol negotiation and then the
    messages layer. A baseline implementation based on plain TCP is also
    provided.
    
    // FIXME: add unit tests
    // FIXME: add javadocs
    
    JIRA: NETCONF-590
    Change-Id: I29c207a2036e1c54ea1f6d7fe48f7eb42a3a4a7d
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ transport/transport-api/src/main/java/org/opendaylight/netconf/transport/api/AbstractOverlayTransportStack.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright (c) 2022 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.netconf.transport.api;
+
+import static java.util.Objects.requireNonNull;
+
+import com.google.common.util.concurrent.Futures;
+import com.google.common.util.concurrent.ListenableFuture;
+import com.google.common.util.concurrent.MoreExecutors;
+import org.eclipse.jdt.annotation.NonNull;
+import org.opendaylight.yangtools.yang.common.Empty;
+
+/**
+ * Abstract base class for {@link TransportStack}s overlaid on a different stack.
+ */
+public abstract class AbstractOverlayTransportStack<C extends TransportChannel> extends AbstractTransportStack<C> {
+    private final @NonNull TransportChannelListener asListener = new TransportChannelListener() {
+        @Override
+        public void onTransportChannelFailed(final Throwable cause) {
+            notifyTransportChannelFailed(cause);
+        }
+
+        @Override
+        public void onTransportChannelEstablished(final TransportChannel channel) {
+            onUnderlayChannelEstablished(channel);
+        }
+    };
+
+    private volatile TransportStack underlay;
+
+    protected AbstractOverlayTransportStack(final TransportChannelListener listener) {
+        super(listener);
+    }
+
+    @Override
+    protected final ListenableFuture<Empty> startShutdown() {
+        return underlay.shutdown();
+    }
+
+    protected final @NonNull TransportChannelListener asListener() {
+        return asListener;
+    }
+
+    protected abstract void onUnderlayChannelEstablished(@NonNull TransportChannel underlayChannel);
+
+    final void setUnderlay(final TransportStack underlay) {
+        this.underlay = requireNonNull(underlay);
+    }
+
+    protected static final <T extends AbstractOverlayTransportStack<?>> @NonNull ListenableFuture<T> transformUnderlay(
+            final T stack, final ListenableFuture<? extends TransportStack> tcpFuture) {
+        return Futures.transform(tcpFuture, tcpStack -> {
+            stack.setUnderlay(tcpStack);
+            return stack;
+        }, MoreExecutors.directExecutor());
+    }
+}
