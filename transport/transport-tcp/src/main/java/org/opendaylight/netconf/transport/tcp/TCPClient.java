9563db0a77232319c98f6f8d83fb3540691ca25c
Robert Varga <robert.varga@pantheon.tech>
2022-08-10 20:23:21 +0200

    WIP: Add transport-{api,tcp}
    NETCONF (and potentially RESTCONF) has rather well-defined concepts
    how it works on the transport layer. Furthermore there are quite mature
    YANG models covering the configuration of both NETCONF server and
    NETCONF client.
    
    This patch introduces the transport-level APIs to establish a transport
    stack and its interactions with protocol negotiation and then the
    messages layer. A baseline implementation based on plain TCP is also
    provided.
    
    // FIXME: add unit tests
    // FIXME: add javadocs
    // FIXME: shutdown lifecycle is probably not right (see TLS)
    
    JIRA: NETCONF-590
    Change-Id: I29c207a2036e1c54ea1f6d7fe48f7eb42a3a4a7d
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ transport/transport-tcp/src/main/java/org/opendaylight/netconf/transport/tcp/TCPClient.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) 2022 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.netconf.transport.tcp;
+
+import com.google.common.util.concurrent.Futures;
+import com.google.common.util.concurrent.ListenableFuture;
+import com.google.common.util.concurrent.SettableFuture;
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelFutureListener;
+import org.eclipse.jdt.annotation.NonNull;
+import org.opendaylight.netconf.transport.api.TransportChannelListener;
+import org.opendaylight.netconf.transport.api.TransportStack;
+import org.opendaylight.netconf.transport.api.UnsupportedConfigurationException;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.tcp.client.rev220524.TcpClientGrouping;
+import org.opendaylight.yangtools.yang.common.Empty;
+
+/**
+ * A {@link TransportStack} acting as a TCP client.
+ */
+public final class TCPClient extends TCPTransportStack {
+    private static final @NonNull ListenableFuture<Empty> SHUTDOWN_FUTURE = Futures.immediateFuture(Empty.value());
+
+    private TCPClient(final TransportChannelListener listener) {
+        super(listener);
+    }
+
+    public static @NonNull ListenableFuture<TCPClient> connect(final TransportChannelListener listener,
+            final Bootstrap bootstrap, final TcpClientGrouping connectParams)
+                throws UnsupportedConfigurationException {
+        NettyTransportSupport.configureKeepalives(bootstrap, connectParams.getKeepalives());
+
+        final var ret = SettableFuture.<TCPClient>create();
+        bootstrap
+            .connect(
+                socketAddressOf(connectParams.requireRemoteAddress(), connectParams.requireRemotePort()),
+                socketAddressOf(connectParams.getLocalAddress(), connectParams.getLocalPort()))
+            .addListener((ChannelFutureListener) future -> {
+                if (future.isSuccess()) {
+                    // Order of operations is important here: the stack should be visible before the underlying channel
+                    final var stack = new TCPClient(listener);
+                    ret.set(stack);
+                    stack.addTransportChannel(new TCPTransportChannel(future.channel()));
+                } else {
+                    ret.setException(future.cause());
+                }
+            });
+        return ret;
+    }
+
+    @Override
+    protected ListenableFuture<Empty> startShutdown() {
+        return SHUTDOWN_FUTURE;
+    }
+}
