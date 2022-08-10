1ed0aa926558b005dd08ef671d9ca4b2a37bec69
Robert Varga <robert.varga@pantheon.tech>
2022-08-10 20:23:21 +0200

    WIP: Add transport-{api,tcp}
    NETCONF (and potentially RESTCONF) has rather well-defined concepts
    how it works on the transport layer. Furthermore there are quite mature
    YANG models covering the configuration of both NETCONF server and
    NETCONF client.
    
    This patch introduces the transport-level APIs to establish a transport
    stack and its interactions with protocol negotiation and then the
    messages layer. A baseline implementation based on plain TCP is also
    provided.
    
    // FIXME: add unit tests
    // FIXME: add javadocs
    // FIXME: shutdown lifecycle is probably not right (see TLS)
    
    JIRA: NETCONF-590
    Change-Id: I29c207a2036e1c54ea1f6d7fe48f7eb42a3a4a7d
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ transport/transport-tcp/src/main/java/org/opendaylight/netconf/transport/tcp/IetfTcpCommonFeatureProvider.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2022 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.netconf.transport.tcp;
+
+import java.util.Set;
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.kohsuke.MetaInfServices;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.tcp.common.rev220524.IetfTcpCommonData;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.tcp.common.rev220524.KeepalivesSupported;
+import org.opendaylight.yangtools.yang.binding.YangFeature;
+import org.opendaylight.yangtools.yang.binding.YangFeatureProvider;
+
+/**
+ * Baseline features supported from {@code ietf-tcp-common.yang}.
+ */
+@MetaInfServices
+@NonNullByDefault
+public final class IetfTcpCommonFeatureProvider implements YangFeatureProvider<IetfTcpCommonData> {
+    @Override
+    public Class<IetfTcpCommonData> boundModule() {
+        return IetfTcpCommonData.class;
+    }
+
+    @Override
+    public Set<? extends YangFeature<?, IetfTcpCommonData>> supportedFeatures() {
+        return Set.of(KeepalivesSupported.VALUE);
+    }
+}
