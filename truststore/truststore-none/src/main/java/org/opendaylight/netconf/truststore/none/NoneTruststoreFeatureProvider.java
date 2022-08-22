3d4c1b70ea49711d9cc565c6782d155213a9f60f
Robert Varga <robert.varga@pantheon.tech>
2022-08-22 15:08:15 +0200

    Add {keystore,truststore}-api and none implementations
    The configuration bits for NETCONF/RESTCONF clients and server are part
    of a large scale effort in NETCONF WG. In particular the concept of
    crypto types and a keystore/truststore are shared between SSH and TLS
    transports.
    
    This patch adds the YANG models and an a 'none' implementation, which
    the central keystore/truststore.
    
    JIRA: NETCONF-590
    Change-Id: I0827756df83aa9cfc83e98ee50b1e44f6310e82e
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ truststore/truststore-none/src/main/java/org/opendaylight/netconf/truststore/none/NoneTruststoreFeatureProvider.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2022 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.netconf.truststore.none;
+
+import java.util.Set;
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.kohsuke.MetaInfServices;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.truststore.rev220524.IetfTruststoreData;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.truststore.rev220524.LocalDefinitionsSupported;
+import org.opendaylight.yangtools.yang.binding.YangFeature;
+import org.opendaylight.yangtools.yang.binding.YangFeatureProvider;
+
+/**
+ * Simple implementation, which advertizes we do not have a central truststore.
+ */
+@MetaInfServices
+@NonNullByDefault
+public final class NoneTruststoreFeatureProvider implements YangFeatureProvider<IetfTruststoreData> {
+    @Override
+    public Class<IetfTruststoreData> boundModule() {
+        return IetfTruststoreData.class;
+    }
+
+    @Override
+    public Set<? extends YangFeature<?, IetfTruststoreData>> supportedFeatures() {
+        return Set.of(LocalDefinitionsSupported.VALUE);
+    }
+}
