7e3786d5e6a2c7e6fdd6d7c0ad72be6f42eff97d
Nikhil Soni <nikk.sonitech@gmail.com>
2022-08-25 17:06:59 +0530

    Netconf Device Notification
    Requirement - HTTP client should be able to get Device notification
    after subscription.
    
    Implementation-
    
    1. Added a new Device Notification Yang for Rest request which takes
     input of Device mount point path
    2. Added a DeviceNotificationListenerAdaptor which is get registered
    in Device Notification service (on Mount point)
    3. Created a dynamic Rest Notification SSE HTTP rest url (Response of 1st rest
    call )
    4. Updated Test-tool for Notification
    
    JIRA: NETCONF-745
    Signed-off-by: Nikhil Soni <nikk.sonitech@gmail.com>
    Change-Id: I9cac35d0a7b0f79382d223ae4ec0ac4a3ce2bae9
    Signed-off-by: Nikhil Soni <nikk.sonitech@gmail.com>

--- restconf/restconf-nb/src/main/java/org/opendaylight/restconf/nb/rfc8040/RestconfApplication.java
+++ restconf/restconf-nb/src/main/java/org/opendaylight/restconf/nb/rfc8040/RestconfApplication.java
@@ -30,15 +30,18 @@ import org.opendaylight.restconf.nb.rfc8040.streams.Configuration;
 @Singleton
 public class RestconfApplication extends AbstractRestconfApplication {
     private RestconfApplication(final SchemaContextHandler schemaContextHandler,
-            final DOMMountPointService mountPointService, final RestconfStreamsSubscriptionService streamSubscription,
-            final DOMDataBroker dataBroker, final DOMRpcService rpcService, final DOMActionService actionService,
-            final DOMNotificationService notificationService, final DOMSchemaService domSchemaService,
-            final Configuration configuration) {
+                                final DOMMountPointService mountPointService,
+                                final RestconfStreamsSubscriptionService streamSubscription,
+                                final DOMDataBroker dataBroker, final DOMRpcService rpcService,
+                                final DOMActionService actionService,
+                                final DOMNotificationService notificationService,
+                                final DOMSchemaService domSchemaService,
+                                final Configuration configuration) {
         super(schemaContextHandler, mountPointService, List.of(
             streamSubscription,
             new RestconfDataServiceImpl(schemaContextHandler, dataBroker, mountPointService, streamSubscription,
                 actionService, configuration),
-            new RestconfInvokeOperationsServiceImpl(rpcService, schemaContextHandler),
+            new RestconfInvokeOperationsServiceImpl(rpcService, schemaContextHandler, mountPointService, configuration),
             new RestconfOperationsServiceImpl(schemaContextHandler, mountPointService),
             new RestconfSchemaServiceImpl(schemaContextHandler, mountPointService,
                 domSchemaService.getExtensions().getInstance(DOMYangTextSourceProvider.class)),
@@ -50,11 +53,11 @@ public class RestconfApplication extends AbstractRestconfApplication {
     public RestconfApplication(final SchemaContextHandler schemaContextHandler,
             final DOMMountPointService mountPointService, final DOMDataBroker dataBroker,
             final DOMRpcService rpcService, final DOMActionService actionService,
-            final DOMNotificationService notificationService,
-            final DOMSchemaService domSchemaService, final Configuration configuration) {
+            final DOMNotificationService notificationService, final DOMSchemaService domSchemaService,
+            final Configuration configuration) {
         this(schemaContextHandler, mountPointService,
             new RestconfStreamsSubscriptionServiceImpl(dataBroker, notificationService, schemaContextHandler,
-                configuration),
-            dataBroker, rpcService, actionService, notificationService, domSchemaService, configuration);
+                configuration),dataBroker, rpcService, actionService, notificationService, domSchemaService,
+                configuration);
     }
 }
