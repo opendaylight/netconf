227e88d72c1357dc8454bcd2bc3fdf4d2c23db30
Nikhil Soni <nikk.sonitech@gmail.com>
2022-08-25 17:06:59 +0530

    Netconf Device Notification
    Requirement - HTTP client should be able to get Device notification
    after subscription.
    
    Implementation:
    1. Added a new Device Notification Yang for Rest request which takes
     input of Device mount point path
    2. Added a DeviceNotificationListenerAdaptor which is get registered
    in Device Notification service (on Mount point)
    3. Created a dynamic Rest Notification SSE HTTP rest url (Response of 1st rest
    call )
    4. Updated Test-tool for Notification
    
    JIRA: NETCONF-745
    Change-Id: I9cac35d0a7b0f79382d223ae4ec0ac4a3ce2bae9
    Signed-off-by: Nikhil Soni <nikk.sonitech@gmail.com>
    Signed-off-by: nikhil.soni.lumina <nikk.sonitech@gmail.com>

--- restconf/restconf-nb/src/main/java/org/opendaylight/restconf/nb/rfc8040/streams/listeners/NotificationListenerAdapter.java
+++ restconf/restconf-nb/src/main/java/org/opendaylight/restconf/nb/rfc8040/streams/listeners/NotificationListenerAdapter.java
@@ -7,26 +7,19 @@
  */
 package org.opendaylight.restconf.nb.rfc8040.streams.listeners;
 
-import java.time.Instant;
-import java.util.Optional;
-import org.opendaylight.mdsal.dom.api.DOMNotification;
-import org.opendaylight.mdsal.dom.api.DOMNotificationListener;
+import static java.util.Objects.requireNonNull;
+
+import com.google.common.base.MoreObjects.ToStringHelper;
 import org.opendaylight.mdsal.dom.api.DOMNotificationService;
 import org.opendaylight.yang.gen.v1.urn.sal.restconf.event.subscription.rev140708.NotificationOutputTypeGrouping.NotificationOutputType;
-import org.opendaylight.yangtools.yang.data.codec.gson.JSONCodecFactorySupplier;
+import org.opendaylight.yangtools.yang.model.api.EffectiveModelContext;
 import org.opendaylight.yangtools.yang.model.api.stmt.SchemaNodeIdentifier.Absolute;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /**
  * {@link NotificationListenerAdapter} is responsible to track events on notifications.
  */
-public final class NotificationListenerAdapter extends AbstractCommonSubscriber<Absolute, DOMNotification>
-        implements DOMNotificationListener {
-
-    private static final Logger LOG = LoggerFactory.getLogger(NotificationListenerAdapter.class);
-    private static final NotificationFormatterFactory JSON_FORMATTER_FACTORY = JSONNotificationFormatter.createFactory(
-            JSONCodecFactorySupplier.RFC7951);
+public final class NotificationListenerAdapter extends AbstractNotificationListenerAdaptor {
+    private final Absolute path;
 
     /**
      * Set path of listener and stream name.
@@ -36,35 +29,13 @@ public final class NotificationListenerAdapter extends AbstractCommonSubscriber<
      * @param outputType Type of output on notification (JSON or XML).
      */
     NotificationListenerAdapter(final Absolute path, final String streamName, final NotificationOutputType outputType) {
-        super(path.lastNodeIdentifier(), streamName, path, outputType, getFormatterFactory(outputType));
-    }
-
-    private static NotificationFormatterFactory getFormatterFactory(final NotificationOutputType outputType) {
-        return switch (outputType) {
-            case JSON -> JSON_FORMATTER_FACTORY;
-            case XML -> XMLNotificationFormatter.FACTORY;
-        };
+        super(path.lastNodeIdentifier(), streamName, outputType);
+        this.path = requireNonNull(path);
     }
 
     @Override
-    @SuppressWarnings("checkstyle:IllegalCatch")
-    public void onNotification(final DOMNotification notification) {
-        final Instant now = Instant.now();
-        if (!checkStartStop(now)) {
-            return;
-        }
-
-        final Optional<String> maybeOutput;
-        try {
-            maybeOutput = formatter().eventData(databindProvider.currentContext().modelContext(), notification, now,
-                getLeafNodesOnly(), isSkipNotificationData(), getChangedLeafNodesOnly());
-        } catch (Exception e) {
-            LOG.error("Failed to process notification {}", notification, e);
-            return;
-        }
-        if (maybeOutput.isPresent()) {
-            post(maybeOutput.get());
-        }
+    EffectiveModelContext effectiveModel() {
+        return databindProvider.currentContext().modelContext();
     }
 
     /**
@@ -73,12 +44,17 @@ public final class NotificationListenerAdapter extends AbstractCommonSubscriber<
      * @return The configured schema path that points to observing YANG notification schema node.
      */
     public Absolute getSchemaPath() {
-        return path();
+        return path;
     }
 
     public synchronized void listen(final DOMNotificationService notificationService) {
         if (!isListening()) {
             setRegistration(notificationService.registerNotificationListener(this, getSchemaPath()));
         }
+    }
+
+    @Override
+    ToStringHelper addToStringAttributes(final ToStringHelper helper) {
+        return super.addToStringAttributes(helper.add("path", path));
     }
 }
