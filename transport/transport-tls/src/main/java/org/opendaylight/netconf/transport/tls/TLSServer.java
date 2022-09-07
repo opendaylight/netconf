517bffc5007b37057e30db5d86ee85511cafea1b
Robert Varga <robert.varga@pantheon.tech>
2022-09-07 11:48:51 +0200

    WIP: Add transport-tls
    Add NETCONF transport implementation based on TCP+TLS.
    
    // FIXME: finish key/certificate loading
    // FIXME: add javadocs
    // FIXME: add unit tests
    
    JIRA: NETCONF-590
    Change-Id: I5e0d6ddce30971dd5f2b0918600b9410aa439c08
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ transport/transport-tls/src/main/java/org/opendaylight/netconf/transport/tls/TLSServer.java
@@ -0,0 +1,101 @@
+/*
+ * Copyright (c) 2022 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.netconf.transport.tls;
+
+import com.google.common.util.concurrent.ListenableFuture;
+import io.netty.bootstrap.Bootstrap;
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.handler.ssl.ClientAuth;
+import io.netty.handler.ssl.SslContext;
+import io.netty.handler.ssl.SslContextBuilder;
+import org.eclipse.jdt.annotation.NonNull;
+import org.opendaylight.netconf.transport.api.TransportChannelListener;
+import org.opendaylight.netconf.transport.api.TransportStack;
+import org.opendaylight.netconf.transport.api.UnsupportedConfigurationException;
+import org.opendaylight.netconf.transport.tcp.TCPClient;
+import org.opendaylight.netconf.transport.tcp.TCPServer;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.tcp.client.rev221212.TcpClientGrouping;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.tcp.server.rev221212.TcpServerGrouping;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.tls.server.rev221212.TlsServerGrouping;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.tls.server.rev221212.tls.server.grouping.server.identity.auth.type.Certificate;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.tls.server.rev221212.tls.server.grouping.server.identity.auth.type.RawPrivateKey;
+
+/**
+ * A {@link TransportStack} acting as a TLS server.
+ */
+public final class TLSServer extends TLSTransportStack {
+    private TLSServer(final TransportChannelListener listener, final SslContext sslContext) {
+        super(listener, sslContext);
+    }
+
+    public static @NonNull ListenableFuture<TLSServer> connect(final TransportChannelListener listener,
+            final Bootstrap bootstrap, final TcpClientGrouping connectParams, final TlsServerGrouping serverParams)
+                throws UnsupportedConfigurationException {
+        final var server = newServer(listener, serverParams);
+        return transformUnderlay(server, TCPClient.connect(server.asListener(), bootstrap, connectParams));
+    }
+
+    public static @NonNull ListenableFuture<TLSServer> listen(final TransportChannelListener listener,
+            final ServerBootstrap bootstrap, final TcpServerGrouping listenParams, final TlsServerGrouping serverParams)
+                throws UnsupportedConfigurationException {
+        final var server = newServer(listener, serverParams);
+        return transformUnderlay(server, TCPServer.listen(server.asListener(), bootstrap, listenParams));
+    }
+
+    private static TLSServer newServer(final TransportChannelListener listener, final TlsServerGrouping serverParams)
+            throws UnsupportedConfigurationException {
+        final var serverIdentity = serverParams.getServerIdentity();
+        if (serverIdentity == null) {
+            throw new UnsupportedConfigurationException("Missing server identity");
+        }
+
+        final SslContextBuilder builder;
+        final var authType = serverIdentity.getAuthType();
+        if (authType instanceof Certificate cert) {
+            builder = newSslContextBuilder(cert);
+        } else if (authType instanceof RawPrivateKey rawKey) {
+            builder = newSslContextBuilder(rawKey);
+        } else if (authType != null) {
+            throw new UnsupportedConfigurationException("Unsupported server authentication type " + authType);
+        } else {
+            throw new UnsupportedConfigurationException("Missing server authentication type");
+        }
+
+        final var clientAuth = serverParams.getClientAuthentication();
+        if (clientAuth != null) {
+            final var trustManager = newTrustManager(clientAuth.getCaCerts(), clientAuth.getEeCerts(),
+                clientAuth.getRawPublicKeys());
+            if (trustManager == null) {
+                throw new UnsupportedOperationException("No client authentication methods in " + clientAuth);
+            }
+            builder.clientAuth(ClientAuth.REQUIRE).trustManager(trustManager);
+        } else {
+            builder.clientAuth(ClientAuth.NONE);
+        }
+
+        return new TLSServer(listener, buildSslContext(builder, serverParams.getHelloParams()));
+    }
+
+    private static SslContextBuilder newSslContextBuilder(final Certificate cert)
+            throws UnsupportedConfigurationException {
+        final var certificate = cert.getCertificate();
+        if (certificate == null) {
+            throw new UnsupportedConfigurationException("Missing certificate in " + cert);
+        }
+        return SslContextBuilder.forServer(newKeyManager(certificate));
+    }
+
+    private static SslContextBuilder newSslContextBuilder(final RawPrivateKey rawKey)
+            throws UnsupportedConfigurationException {
+        final var rawPrivateKey = rawKey.getRawPrivateKey();
+        if (rawPrivateKey == null) {
+            throw new UnsupportedConfigurationException("Missing key in " + rawKey);
+        }
+        return SslContextBuilder.forServer(newKeyManager(rawPrivateKey));
+    }
+}
