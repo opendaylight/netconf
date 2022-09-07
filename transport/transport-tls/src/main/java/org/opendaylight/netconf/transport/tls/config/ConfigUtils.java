c5a510416c6693c9bdf35b8a1f327ccdcb039a8f
Robert Varga <robert.varga@pantheon.tech>
2022-09-07 11:48:51 +0200

    Add transport-tls
    Add NETCONF transport implementation based on TCP+TLS.
    
    JIRA: NETCONF-590
    Change-Id: I5e0d6ddce30971dd5f2b0918600b9410aa439c08
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>
    Signed-off-by: Ruslan Kashapov <ruslan.kashapov@pantheon.tech>

---
+++ transport/transport-tls/src/main/java/org/opendaylight/netconf/transport/tls/config/ConfigUtils.java
@@ -0,0 +1,228 @@
+/*
+ * Copyright (c) 2023 PANTHEON.tech s.r.o. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.netconf.transport.tls.config;
+
+import static java.util.Optional.ofNullable;
+import static org.opendaylight.netconf.transport.tls.config.KeyStoreUtils.buildX509Certificate;
+import static org.opendaylight.netconf.transport.tls.config.KeyUtils.buildPrivateKey;
+import static org.opendaylight.netconf.transport.tls.config.KeyUtils.buildPublicKeyFromSshEncoding;
+import static org.opendaylight.netconf.transport.tls.config.KeyUtils.buildX509PublicKey;
+import static org.opendaylight.netconf.transport.tls.config.KeyUtils.validateKeyPair;
+import static org.opendaylight.netconf.transport.tls.config.KeyUtils.validatePublicKey;
+
+import com.google.common.collect.ImmutableList;
+import java.io.IOException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.cert.Certificate;
+import java.security.cert.CertificateException;
+import java.util.List;
+import org.eclipse.jdt.annotation.NonNull;
+import org.eclipse.jdt.annotation.Nullable;
+import org.opendaylight.netconf.transport.api.UnsupportedConfigurationException;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.crypto.types.rev221212.AsymmetricKeyPairGrouping;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.crypto.types.rev221212.EcPrivateKeyFormat;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.crypto.types.rev221212.PrivateKeyFormat;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.crypto.types.rev221212.PublicKeyFormat;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.crypto.types.rev221212.RsaPrivateKeyFormat;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.crypto.types.rev221212.SshPublicKeyFormat;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.crypto.types.rev221212.SubjectPublicKeyInfoFormat;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.crypto.types.rev221212.asymmetric.key.pair.grouping.PrivateKeyType;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.crypto.types.rev221212.asymmetric.key.pair.grouping._private.key.type.CleartextPrivateKey;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.keystore.rev221212.LocalOrKeystoreAsymmetricKeyGrouping;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.keystore.rev221212.LocalOrKeystoreEndEntityCertWithKeyGrouping;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.truststore.rev221212.LocalOrTruststoreCertsGrouping;
+
+public class ConfigUtils {
+
+    static final char[] EMPTY_SECRET = new char[0];
+    static final String DEFAULT_PRIVATE_KEY_ALIAS = "private";
+    static final String DEFAULT_CERTIFICATE_ALIAS = "certificate";
+
+    private ConfigUtils() {
+        // utility class
+    }
+
+    /**
+     * Builds X.509 certificates based on configuration data provided then sets them to given key store.
+     *
+     * @param keyStore key store
+     * @param caCerts CA certificates configuration
+     * @param eeCerts EE certificates configuration
+     * @throws UnsupportedConfigurationException if error occurs
+     */
+    public static void setX509Certificates(final @NonNull KeyStore keyStore,
+            final @Nullable LocalOrTruststoreCertsGrouping caCerts,
+            final @Nullable LocalOrTruststoreCertsGrouping eeCerts) throws UnsupportedConfigurationException {
+        var certInfos = ImmutableList.<X509CertificateInfo>builder()
+                .addAll(collectCertificateInfo(caCerts, "ca-"))
+                .addAll(collectCertificateInfo(eeCerts, "ee-")).build();
+        setX509Certificates(keyStore, certInfos);
+    }
+
+    private static List<X509CertificateInfo> collectCertificateInfo(
+            @Nullable final LocalOrTruststoreCertsGrouping certs,
+            @NonNull final String aliasPrefix) throws UnsupportedConfigurationException {
+        if (certs == null) {
+            return List.of();
+        }
+        final var local = ofType(org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.truststore
+                        .rev221212.local.or.truststore.certs.grouping.local.or.truststore.Local.class,
+                certs.getLocalOrTruststore());
+        final var localDef = ofNullable(local.getLocalDefinition())
+                .orElseThrow(() -> new UnsupportedConfigurationException("Missing local definition in " + local));
+        final var listBuilder = ImmutableList.<X509CertificateInfo>builder();
+        for (var cert : localDef.nonnullCertificate().values()) {
+            final var certAlias = aliasPrefix + cert.requireName();
+            final var certBytes = cert.requireCertData().getValue();
+            listBuilder.add(new X509CertificateInfo(certAlias, certBytes));
+        }
+        return listBuilder.build();
+    }
+
+    private static void setX509Certificates(@NonNull final KeyStore keyStore,
+            @NonNull final List<X509CertificateInfo> certInfos) throws UnsupportedConfigurationException {
+        for (var certInfo : certInfos) {
+            try {
+                keyStore.setCertificateEntry(certInfo.name(), buildX509Certificate(certInfo.bytes()));
+            } catch (CertificateException | KeyStoreException | IOException e) {
+                throw new UnsupportedConfigurationException("Failed to load certificate" + certInfo, e);
+            }
+        }
+    }
+
+    /**
+     * Builds asymmetric key pair from configuration data provided, validates it then puts into given key store.
+     *
+     * @param keyStore keystore
+     * @param input configuration
+     * @throws UnsupportedConfigurationException
+     */
+    public static void setAsymmetricKey(final @NonNull KeyStore keyStore,
+            final @NonNull LocalOrKeystoreAsymmetricKeyGrouping input)
+            throws UnsupportedConfigurationException {
+        /*
+            ietf-crypto-types:grouping asymmetric-key-pair-grouping
+            "A private key and its associated public key.  Implementations
+            SHOULD ensure that the two keys are a matching pair."
+         */
+        final var local = ofType(org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.keystore.rev221212
+                        .local.or.keystore.asymmetric.key.grouping.local.or.keystore.Local.class,
+                input.getLocalOrKeystore());
+        final var localDef = ofNullable(local.getLocalDefinition())
+                .orElseThrow(() -> new UnsupportedConfigurationException("Missing local definition in " + local));
+        setPrivateKeyFromKeyPair(keyStore, collectKeyPairInfo(localDef));
+    }
+
+    /**
+     * Builds asymmetric key pair and associated certificate from configuration data provided, validates
+     * then puts into given key store.
+     *
+     * @param keyStore key store
+     * @param input configuration
+     * @throws UnsupportedConfigurationException
+     */
+    public static void setEndEntityCertificateWithKey(final @NonNull KeyStore keyStore,
+            final @NonNull LocalOrKeystoreEndEntityCertWithKeyGrouping input) throws UnsupportedConfigurationException {
+        /*
+          ietf-crypto-types:asymmetric-key-pair-with-cert-grouping
+          "A private/public key pair and an associated certificate.
+          Implementations SHOULD assert that certificates contain the matching public key.";
+         */
+        final var local = ofType(org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.keystore.rev221212
+                        .local.or.keystore.end.entity.cert.with.key.grouping.local.or.keystore.Local.class,
+                input.getLocalOrKeystore());
+        final var localDef = ofNullable(local.getLocalDefinition())
+                .orElseThrow(() -> new UnsupportedConfigurationException("Missing local definition in " + local));
+
+        final var keyPairInfo = collectKeyPairInfo(localDef);
+        final var privateKey = buildPrivateKey(keyPairInfo.privateKeyFormat().name(), keyPairInfo.privateKeyBytes());
+        final var publicKey = KeyPairInfo.PublicKeyFormat.SSH.equals(keyPairInfo.publicKeyFormat())
+                ? buildPublicKeyFromSshEncoding(keyPairInfo.publicKeyBytes())
+                : buildX509PublicKey(keyPairInfo.privateKeyFormat().name(), keyPairInfo.publicKeyBytes());
+        final Certificate certificate;
+        try {
+            certificate = buildX509Certificate(localDef.requireCertData().getValue());
+        } catch (IOException | CertificateException e) {
+            throw new UnsupportedConfigurationException("Failed to load certificate" + localDef, e);
+        }
+        validateKeyPair(publicKey, privateKey);
+        validatePublicKey(publicKey, certificate);
+        try {
+            keyStore.setCertificateEntry(DEFAULT_CERTIFICATE_ALIAS, certificate);
+            keyStore.setKeyEntry(DEFAULT_PRIVATE_KEY_ALIAS, privateKey, EMPTY_SECRET, new Certificate[]{certificate});
+        } catch (KeyStoreException e) {
+            throw new UnsupportedConfigurationException("Failed to load certificate and/or private key", e);
+        }
+    }
+
+    private static void setPrivateKeyFromKeyPair(@NonNull final KeyStore keyStore, final KeyPairInfo keyPairInfo)
+            throws UnsupportedConfigurationException {
+        final var privateKey = buildPrivateKey(keyPairInfo.privateKeyFormat().name(), keyPairInfo.privateKeyBytes());
+        final var publicKey = KeyPairInfo.PublicKeyFormat.SSH.equals(keyPairInfo.publicKeyFormat())
+                ? buildPublicKeyFromSshEncoding(keyPairInfo.publicKeyBytes())
+                : buildX509PublicKey(keyPairInfo.privateKeyFormat().name(), keyPairInfo.publicKeyBytes());
+        validateKeyPair(publicKey, privateKey);
+        try {
+            // FIXME
+            // below line throws an exception bc keyStore does not support private key without certificate chain
+            // (belongs to implementation of raw public key feature support)
+            keyStore.setKeyEntry(DEFAULT_PRIVATE_KEY_ALIAS, privateKey, EMPTY_SECRET, null);
+        } catch (KeyStoreException e) {
+            throw new UnsupportedConfigurationException("Failed to load private key", e);
+        }
+    }
+
+    private static KeyPairInfo collectKeyPairInfo(final AsymmetricKeyPairGrouping keyPair)
+            throws UnsupportedConfigurationException {
+        final var privateKeyFormat = getPrivateKeyFormat(keyPair.getPrivateKeyFormat());
+        final byte[] privateKeyBytes = getPrivateKeyBytes(keyPair.getPrivateKeyType());
+        final var publicKeyType = getPublicKeyType(keyPair.getPublicKeyFormat());
+        return new KeyPairInfo(privateKeyFormat, privateKeyBytes, publicKeyType, keyPair.getPublicKey());
+    }
+
+    private static KeyPairInfo.PrivateKeyFormat getPrivateKeyFormat(final @Nullable PrivateKeyFormat format)
+            throws UnsupportedConfigurationException {
+        if (EcPrivateKeyFormat.VALUE.equals(format)) {
+            return KeyPairInfo.PrivateKeyFormat.EC;
+        } else if (RsaPrivateKeyFormat.VALUE.equals(format)) {
+            return KeyPairInfo.PrivateKeyFormat.RSA;
+        } else {
+            throw new UnsupportedConfigurationException("Unsupported private key format " + format);
+        }
+    }
+
+    private static byte[] getPrivateKeyBytes(final @Nullable PrivateKeyType type)
+            throws UnsupportedConfigurationException {
+        if (type instanceof CleartextPrivateKey clearText) {
+            return clearText.requireCleartextPrivateKey();
+        } else {
+            throw new UnsupportedConfigurationException("Unsupported private key type " + type);
+        }
+    }
+
+    private static KeyPairInfo.PublicKeyFormat getPublicKeyType(final @Nullable PublicKeyFormat format)
+            throws UnsupportedConfigurationException {
+        if (SubjectPublicKeyInfoFormat.VALUE.equals(format)) {
+            return KeyPairInfo.PublicKeyFormat.SUBJECT_INFO;
+        } else if (SshPublicKeyFormat.VALUE.equals(format)) {
+            return KeyPairInfo.PublicKeyFormat.SSH;
+        } else {
+            throw new UnsupportedConfigurationException("Unsupported public key format " + format);
+        }
+    }
+
+    private static <T> T ofType(final Class<T> expectedType, final Object obj)
+            throws UnsupportedConfigurationException {
+        if (!expectedType.isInstance(obj)) {
+            throw new UnsupportedConfigurationException("Expected type: " + expectedType
+                    + " actual: " + obj.getClass());
+        }
+        return expectedType.cast(obj);
+    }
+}
---
+++ transport/transport-tls/src/test/java/org/opendaylight/netconf/transport/tls/TlsClientServerTest.java
@@ -0,0 +1,212 @@
+/*
+ * Copyright (c) 2023 PANTHEON.tech s.r.o. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.netconf.transport.tls;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertInstanceOf;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.timeout;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.opendaylight.netconf.transport.tls.TestUtils.X509_EC_INPUTS;
+import static org.opendaylight.netconf.transport.tls.TestUtils.X509_RSA_INPUTS;
+import static org.opendaylight.netconf.transport.tls.TestUtils.buildEndEntityCertWithKeyGrouping;
+import static org.opendaylight.netconf.transport.tls.TestUtils.buildLocalOrTruststore;
+
+import io.netty.channel.Channel;
+import io.netty.channel.EventLoopGroup;
+import io.netty.handler.ssl.SslHandler;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Stream;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.opendaylight.netconf.transport.api.TransportChannel;
+import org.opendaylight.netconf.transport.api.TransportChannelListener;
+import org.opendaylight.netconf.transport.tcp.NettyTransportSupport;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.crypto.types.rev221212.EcPrivateKeyFormat;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.crypto.types.rev221212.RsaPrivateKeyFormat;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.crypto.types.rev221212.SubjectPublicKeyInfoFormat;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev130715.Host;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev130715.IetfInetUtil;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev130715.PortNumber;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.tcp.client.rev221212.TcpClientGrouping;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.tcp.server.rev221212.TcpServerGrouping;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.tls.client.rev221212.TlsClientGrouping;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.tls.client.rev221212.tls.client.grouping.ClientIdentityBuilder;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.tls.client.rev221212.tls.client.grouping.ServerAuthenticationBuilder;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.tls.server.rev221212.TlsServerGrouping;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.tls.server.rev221212.tls.server.grouping.ClientAuthenticationBuilder;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.tls.server.rev221212.tls.server.grouping.ServerIdentityBuilder;
+import org.opendaylight.yangtools.yang.common.Uint16;
+
+@ExtendWith(MockitoExtension.class)
+class TlsClientServerTest {
+
+    @Mock
+    private TcpClientGrouping tcpClientConfig;
+    @Mock
+    private TlsClientGrouping tlsClientConfig;
+    @Mock
+    private TransportChannelListener clientListener;
+    @Mock
+    private TcpServerGrouping tcpServerConfig;
+    @Mock
+    private TlsServerGrouping tlsServerConfig;
+    @Mock
+    private TransportChannelListener serverListener;
+
+    @Captor
+    ArgumentCaptor<TransportChannel> clientTransportChannelCaptor;
+    @Captor
+    ArgumentCaptor<TransportChannel> serverTransportChannelCaptor;
+
+    private static EventLoopGroup group;
+    private ServerSocket socket;
+
+    @BeforeAll
+    static void beforeAll() {
+        group = NettyTransportSupport.newEventLoopGroup("IntegrationTest");
+    }
+
+    @AfterAll
+    static void afterAll() {
+        group.shutdownGracefully();
+        group = null;
+    }
+
+    @BeforeEach
+    void beforeEach() throws IOException {
+
+        // create temp socket to get available port for test
+        socket = new ServerSocket(0);
+        final var localAddress = IetfInetUtil.INSTANCE.ipAddressFor(InetAddress.getLoopbackAddress());
+        final var localPort = new PortNumber(Uint16.valueOf(socket.getLocalPort()));
+        socket.close();
+
+        when(tcpServerConfig.getLocalAddress()).thenReturn(localAddress);
+        when(tcpServerConfig.requireLocalAddress()).thenCallRealMethod();
+        when(tcpServerConfig.getLocalPort()).thenReturn(localPort);
+        when(tcpServerConfig.requireLocalPort()).thenCallRealMethod();
+
+        when(tcpClientConfig.getRemoteAddress()).thenReturn(new Host(localAddress));
+        when(tcpClientConfig.requireRemoteAddress()).thenCallRealMethod();
+        when(tcpClientConfig.getRemotePort()).thenReturn(localPort);
+        when(tcpClientConfig.requireRemotePort()).thenCallRealMethod();
+    }
+
+    private static Stream<Arguments> itWithCertificateConfigArgs() {
+        return Stream.of(
+                Arguments.of("RSA", X509_RSA_INPUTS),
+                Arguments.of("EC", X509_EC_INPUTS)
+        );
+    }
+
+    @ParameterizedTest(name = "TLS using X.509 certificates: {0}")
+    @MethodSource("itWithCertificateConfigArgs")
+    void itWithCertificateConfig(final String algorithm, final TestUtils.X509CertInputs in) throws Exception {
+
+        // common config parts
+        var localOrKeystore = buildEndEntityCertWithKeyGrouping(
+                SubjectPublicKeyInfoFormat.VALUE, in.publicKey(),
+                "RSA".equals(algorithm) ? RsaPrivateKeyFormat.VALUE : EcPrivateKeyFormat.VALUE,
+                in.privateKey(), in.certBytes()).getLocalOrKeystore();
+        var localOrTrustStore = buildLocalOrTruststore(Map.of("cert", in.certBytes()));
+
+        // client config
+        final var clientIdentity = new ClientIdentityBuilder()
+                .setAuthType(new org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.tls.client.rev221212
+                        .tls.client.grouping.client.identity.auth.type.CertificateBuilder()
+                        .setCertificate(
+                                new org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.tls.client.rev221212
+                                        .tls.client.grouping.client.identity.auth.type.certificate.CertificateBuilder()
+                                        .setLocalOrKeystore(localOrKeystore)
+                                        .build()).build()).build();
+        final var serverAuth = new ServerAuthenticationBuilder().setCaCerts(
+                new org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.tls.client.rev221212
+                        .tls.client.grouping.server.authentication.CaCertsBuilder()
+                        .setLocalOrTruststore(localOrTrustStore).build()).build();
+        when(tlsClientConfig.getClientIdentity()).thenReturn(clientIdentity);
+        when(tlsClientConfig.getServerAuthentication()).thenReturn(serverAuth);
+
+        // server config
+        final var serverIdentity = new ServerIdentityBuilder()
+                .setAuthType(new org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.tls.server.rev221212
+                        .tls.server.grouping.server.identity.auth.type.CertificateBuilder()
+                        .setCertificate(
+                                new org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.tls.server.rev221212
+                                        .tls.server.grouping.server.identity.auth.type.certificate.CertificateBuilder()
+                                        .setLocalOrKeystore(localOrKeystore)
+                                        .build()).build()).build();
+        final var clientAuth = new ClientAuthenticationBuilder().setCaCerts(
+                new org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.tls.server.rev221212
+                        .tls.server.grouping.client.authentication.CaCertsBuilder()
+                        .setLocalOrTruststore(localOrTrustStore).build()).build();
+        when(tlsServerConfig.getServerIdentity()).thenReturn(serverIdentity);
+        when(tlsServerConfig.getClientAuthentication()).thenReturn(clientAuth);
+
+        integrationTest();
+    }
+
+    private void integrationTest() throws Exception {
+        // start server
+        final var server = TLSServer.listen(serverListener, NettyTransportSupport.newServerBootstrap().group(group),
+                tcpServerConfig, tlsServerConfig).get(2, TimeUnit.SECONDS);
+        try {
+            // connect with client
+            final var client = TLSClient.connect(clientListener, NettyTransportSupport.newBootstrap().group(group),
+                    tcpClientConfig, tlsClientConfig).get(2, TimeUnit.SECONDS);
+            try {
+                verify(serverListener, timeout(500))
+                        .onTransportChannelEstablished(serverTransportChannelCaptor.capture());
+                verify(clientListener, timeout(500))
+                        .onTransportChannelEstablished(clientTransportChannelCaptor.capture());
+                // validate channels are in expected state
+                var serverChannel = assertChannel(serverTransportChannelCaptor.getAllValues());
+                var clientChannel = assertChannel(clientTransportChannelCaptor.getAllValues());
+                // validate channels are connecting same sockets
+                assertEquals(serverChannel.remoteAddress(), clientChannel.localAddress());
+                assertEquals(serverChannel.localAddress(), clientChannel.remoteAddress());
+
+            } finally {
+                client.shutdown().get(2, TimeUnit.SECONDS);
+            }
+        } finally {
+            server.shutdown().get(2, TimeUnit.SECONDS);
+        }
+    }
+
+    private static Channel assertChannel(List<TransportChannel> transportChannels){
+        assertNotNull(transportChannels);
+        assertEquals(1, transportChannels.size());
+        final var channel = assertInstanceOf(TLSTransportChannel.class, transportChannels.get(0)).channel();
+        assertNotNull(channel);
+        assertTrue(channel.isOpen()); // connection is open
+        assertNotNull(channel.pipeline().get(SslHandler.class)); //  has an SSL handler within a pipeline
+        return channel;
+    }
+}
