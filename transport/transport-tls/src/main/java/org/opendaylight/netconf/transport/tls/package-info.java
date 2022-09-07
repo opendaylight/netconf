44a017eb5c7d8546d2dbd2f53648b16424dc67d5
Robert Varga <robert.varga@pantheon.tech>
2022-09-07 11:48:51 +0200

    WIP: Add transport-tls
    Add NETCONF transport implementation based on TCP+TLS.
    
    // FIXME: finish key/certificate loading
    // FIXME: add javadocs
    // FIXME: add unit tests
    
    JIRA: NETCONF-590
    Change-Id: I5e0d6ddce30971dd5f2b0918600b9410aa439c08
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ transport/transport-tls/src/main/java/org/opendaylight/netconf/transport/tls/package-info.java
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2022 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+/**
+ * NETCONF Secure Transport over TLS, as detailed in <a href="https://www.rfc-editor.org/rfc/rfc7589">RFC7589</a>.
+ * Configuration follows <a href="https://datatracker.ietf.org/doc/html/draft-ietf-netconf-tls-client-server-29">
+ * draft-ietf-netconf-tls-client-server</a>.
+ */
+package org.opendaylight.netconf.transport.tls;
