032f19b00354c06eba43d0878436c82a253ef7d8
Robert Varga <robert.varga@pantheon.tech>
2022-09-07 11:46:06 +0200

    WIP: Add transport-ssh
    Add NETCONF transport implementation based on SSHD.
    
    // FIXME: finish this up
    
    JIRA: NETCONF-590
    Change-Id: I21245b2f8b932da0a57fa386ee62b05dfcf8cc18
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ transport/transport-ssh/src/main/java/org/opendaylight/netconf/transport/ssh/SSHClientSession.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2022 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.netconf.transport.ssh;
+
+import org.opendaylight.netconf.shaded.sshd.client.ClientFactoryManager;
+import org.opendaylight.netconf.shaded.sshd.client.session.ClientSessionImpl;
+
+final class SSHClientSession extends ClientSessionImpl {
+    SSHClientSession(final ClientFactoryManager client, final TCPIoSession ioSession) throws Exception {
+        super(client, ioSession);
+    }
+}
