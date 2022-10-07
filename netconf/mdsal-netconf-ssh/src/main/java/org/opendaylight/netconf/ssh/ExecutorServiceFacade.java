8bf562246efb1a6840e32a80dd9c8fe8484479c0
Ivan Hrasko <ivan.hrasko@pantheon.tech>
2022-10-07 20:32:15 +0200

    Fix mdsal-netconf-ssh shutdown
    Initialize AsynchronousChannelGroup thread pool with
    ForwardingExecutorService with NO-OP shutdown method
    in order to prevent throwing UnsupportedOperationException
    from OSGiGlobalEventExecutor.
    
    JIRA: NETCONF-900
    Change-Id: Ibedb95413bf5476f5d22ae899e71496951fb6ed7
    Signed-off-by: Ivan Hrasko <ivan.hrasko@pantheon.tech>
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ netconf/mdsal-netconf-ssh/src/main/java/org/opendaylight/netconf/ssh/ExecutorServiceFacade.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2022 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.netconf.ssh;
+
+import static java.util.Objects.requireNonNull;
+
+import com.google.common.util.concurrent.ForwardingExecutorService;
+import java.util.concurrent.ExecutorService;
+
+/**
+ * Facade for guarding against {@link #shutdown()} invocations. This is necessary as SSHD wants to shutdown the executor
+ * when the server shuts down.
+ */
+final class ExecutorServiceFacade extends ForwardingExecutorService {
+    private final ExecutorService delegate;
+
+    ExecutorServiceFacade(final ExecutorService delegate) {
+        this.delegate = requireNonNull(delegate);
+    }
+
+    @Override
+    protected ExecutorService delegate() {
+        return delegate;
+    }
+
+    @Override
+    public void shutdown() {
+        // NO-OP
+    }
+}
