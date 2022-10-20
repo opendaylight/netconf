6b209efa026af1a22d94ded83f49c52e43a03d9d
OleksandrZharov <Oleksandr.Zharov@pantheon.tech>
2022-10-20 14:22:29 +0200

    Fix RFC8040 compliance: API Resource
    Added alternative url path for GET target data resource from data root
    request.
    
    JIRA: NETCONF-740
    Change-Id: I7bdf6155c3a53d547fd3aba1faca5cf099923fc7
    Signed-off-by: OleksandrZharov <Oleksandr.Zharov@pantheon.tech>
    Signed-off-by: Ivan Hrasko <ivan.hrasko@pantheon.tech>

--- restconf/restconf-nb/src/main/java/org/opendaylight/restconf/nb/rfc8040/rests/services/impl/RestconfImpl.java
+++ restconf/restconf-nb/src/main/java/org/opendaylight/restconf/nb/rfc8040/rests/services/impl/RestconfImpl.java
@@ -10,13 +10,20 @@ package org.opendaylight.restconf.nb.rfc8040.rests.services.impl;
 import static java.util.Objects.requireNonNull;
 
 import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
 import org.opendaylight.restconf.common.context.InstanceIdentifierContext;
 import org.opendaylight.restconf.nb.rfc8040.Rfc8040.IetfYangLibrary;
 import org.opendaylight.restconf.nb.rfc8040.databind.DatabindProvider;
 import org.opendaylight.restconf.nb.rfc8040.legacy.NormalizedNodePayload;
 import org.opendaylight.restconf.nb.rfc8040.rests.services.api.RestconfService;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.restconf.rev170126.Restconf;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.restconf.rev170126.restconf.restconf.Data;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.restconf.rev170126.restconf.restconf.Operations;
 import org.opendaylight.yangtools.yang.common.QName;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
+import org.opendaylight.yangtools.yang.data.api.schema.LeafNode;
+import org.opendaylight.yangtools.yang.data.impl.schema.Builders;
 import org.opendaylight.yangtools.yang.data.impl.schema.ImmutableNodes;
 import org.opendaylight.yangtools.yang.model.api.EffectiveModelContext;
 import org.opendaylight.yangtools.yang.model.util.SchemaInferenceStack;
@@ -43,5 +50,29 @@ public class RestconfImpl implements RestconfService {
 
         return NormalizedNodePayload.of(InstanceIdentifierContext.ofStack(stack),
             ImmutableNodes.leafNode(YANG_LIBRARY_VERSION, IetfYangLibrary.REVISION.toString()));
+    }
+
+    @Override
+    public Response readDataRoot(final UriInfo uriInfo) {
+        final var context = databindProvider.currentContext().modelContext();
+        final var stack = SchemaInferenceStack.of(context);
+        stack.enterGrouping(Restconf.QNAME);
+        stack.enterDataTree(Restconf.QNAME);
+        final var identifier = InstanceIdentifierContext.ofStack(stack);
+
+        // TODO combine data, operations and yang library version into {@code data} below
+        final var libVersion = getLibraryVersion().getData();
+
+        final var data = Builders.containerBuilder()
+                .withNodeIdentifier(YangInstanceIdentifier.NodeIdentifier.create(Restconf.QNAME))
+                .withChild(Builders.containerBuilder()
+                        .withNodeIdentifier(YangInstanceIdentifier.NodeIdentifier.create(Data.QNAME))
+                        .build())
+                .withChild(Builders.containerBuilder()
+                        .withNodeIdentifier(YangInstanceIdentifier.NodeIdentifier.create(Operations.QNAME))
+                        .build())
+                .withChild(((LeafNode) libVersion))
+                .build();
+        return Response.status(Response.Status.OK).entity(NormalizedNodePayload.of(identifier, data)).build();
     }
 }
