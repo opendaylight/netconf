b54e1add43ddd8732b60cfab8f561a9c14117acb
Robert Varga <robert.varga@pantheon.tech>
2022-11-11 01:10:36 +0100

    Split out CapabilitiesWriter
    Maintaining ietf-restconf-monitoring capabilities is a separate task
    not done by any other component (unlike ietf-yang-library). Separate it
    out.
    
    JIRA: NETCONF-668
    Change-Id: Icc0e7bbf4bc0c9ecdac385b7ce311ea49868b9c3
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>
    Signed-off-by: Ivan Hrasko <ivan.hrasko@pantheon.tech>

--- restconf/restconf-nb/src/test/java/org/opendaylight/restconf/nb/rfc8040/legacy/SchemaContextHandlerTest.java
+++ restconf/restconf-nb/src/test/java/org/opendaylight/restconf/nb/rfc8040/legacy/SchemaContextHandlerTest.java
@@ -5,20 +5,15 @@
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
-package org.opendaylight.restconf.nb.rfc8040.handlers;
+package org.opendaylight.restconf.nb.rfc8040.legacy;
 
-import static org.hamcrest.CoreMatchers.equalTo;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.containsInAnyOrder;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doReturn;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
 import java.io.FileNotFoundException;
-import java.util.stream.Collectors;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
@@ -26,16 +21,10 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
-import org.opendaylight.mdsal.common.api.CommitInfo;
 import org.opendaylight.mdsal.dom.api.DOMDataBroker;
-import org.opendaylight.mdsal.dom.api.DOMDataTreeWriteTransaction;
 import org.opendaylight.mdsal.dom.api.DOMSchemaService;
 import org.opendaylight.restconf.nb.rfc8040.TestRestconfUtils;
-import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.restconf.monitoring.rev170126.restconf.state.Capabilities;
 import org.opendaylight.yangtools.concepts.ListenerRegistration;
-import org.opendaylight.yangtools.yang.data.api.schema.ContainerNode;
-import org.opendaylight.yangtools.yang.data.api.schema.LeafSetEntryNode;
-import org.opendaylight.yangtools.yang.data.api.schema.LeafSetNode;
 import org.opendaylight.yangtools.yang.model.api.EffectiveModelContext;
 import org.opendaylight.yangtools.yang.model.api.SchemaContext;
 import org.opendaylight.yangtools.yang.test.util.YangParserTestUtils;
@@ -45,14 +34,12 @@ import org.opendaylight.yangtools.yang.test.util.YangParserTestUtils;
  */
 @RunWith(MockitoJUnitRunner.StrictStubs.class)
 public class SchemaContextHandlerTest {
-
-    private static final String PATH_FOR_ACTUAL_SCHEMA_CONTEXT = "/modules";
-    private static final String PATH_FOR_NEW_SCHEMA_CONTEXT = "/modules/modules-behind-mount-point";
-
     private static EffectiveModelContext SCHEMA_CONTEXT;
 
     private SchemaContextHandler schemaContextHandler;
 
+    @Mock
+    private DOMDataBroker mockDOMDataBroker;
     @Mock
     private DOMSchemaService mockDOMSchemaService;
     @Mock
@@ -60,8 +47,7 @@ public class SchemaContextHandlerTest {
 
     @BeforeClass
     public static void beforeClass() throws FileNotFoundException {
-        SCHEMA_CONTEXT = YangParserTestUtils.parseYangFiles(
-            TestRestconfUtils.loadFiles(PATH_FOR_ACTUAL_SCHEMA_CONTEXT));
+        SCHEMA_CONTEXT = YangParserTestUtils.parseYangFiles(TestRestconfUtils.loadFiles("/modules"));
     }
 
     @AfterClass
@@ -71,14 +57,9 @@ public class SchemaContextHandlerTest {
 
     @Before
     public void setup() throws Exception {
-        final DOMDataBroker dataBroker = mock(DOMDataBroker.class);
-        final DOMDataTreeWriteTransaction wTx = mock(DOMDataTreeWriteTransaction.class);
-        doReturn(wTx).when(dataBroker).newWriteOnlyTransaction();
-        doReturn(CommitInfo.emptyFluentFuture()).when(wTx).commit();
-
         doReturn(mockListenerReg).when(mockDOMSchemaService).registerSchemaContextListener(any());
 
-        schemaContextHandler = new SchemaContextHandler(dataBroker, mockDOMSchemaService);
+        schemaContextHandler = new SchemaContextHandler(mockDOMDataBroker, mockDOMSchemaService);
         verify(mockDOMSchemaService).registerSchemaContextListener(schemaContextHandler);
 
         schemaContextHandler.onModelContextUpdated(SCHEMA_CONTEXT);
@@ -117,7 +98,7 @@ public class SchemaContextHandlerTest {
     public void onGlobalContextUpdateTest() throws Exception {
         // create new SchemaContext and update SchemaContextHandler
         final EffectiveModelContext newSchemaContext =
-                YangParserTestUtils.parseYangFiles(TestRestconfUtils.loadFiles(PATH_FOR_NEW_SCHEMA_CONTEXT));
+                YangParserTestUtils.parseYangFiles(TestRestconfUtils.loadFiles("/modules/modules-behind-mount-point"));
         schemaContextHandler.onModelContextUpdated(newSchemaContext);
 
         assertNotEquals("SchemaContextHandler should not has reference to old SchemaContext",
@@ -125,36 +106,4 @@ public class SchemaContextHandlerTest {
         assertEquals("SchemaContextHandler should has reference to new SchemaContext",
                 newSchemaContext, schemaContextHandler.get());
     }
-
-    @Test
-    public void restconfStateCapabilitesTest() {
-        final ContainerNode normNode = SchemaContextHandler.mapCapabilites();
-
-        @SuppressWarnings("unchecked")
-        final LeafSetNode<String> capability = (LeafSetNode<String>) normNode.body().stream()
-            // Find 'capabilities' container
-            .filter(child -> Capabilities.QNAME.equals(child.getIdentifier().getNodeType()))
-            .findFirst()
-            .map(ContainerNode.class::cast)
-            .orElseThrow()
-            // Find 'capability' leaf-list
-            .body().stream()
-            .filter(child -> SchemaContextHandler.CAPABILITY_QNAME.equals(child.getIdentifier().getNodeType()))
-            .findFirst()
-            .orElseThrow();
-
-        assertThat(
-            capability.body().stream().map(entry -> ((LeafSetEntryNode<?>) entry).body()).collect(Collectors.toList()),
-            containsInAnyOrder(
-                equalTo("urn:ietf:params:restconf:capability:depth:1.0"),
-                equalTo("urn:ietf:params:restconf:capability:fields:1.0"),
-                equalTo("urn:ietf:params:restconf:capability:filter:1.0"),
-                equalTo("urn:ietf:params:restconf:capability:replay:1.0"),
-                equalTo("urn:ietf:params:restconf:capability:with-defaults:1.0"),
-                equalTo("urn:opendaylight:params:restconf:capability:pretty-print:1.0"),
-                equalTo("urn:opendaylight:params:restconf:capability:leaf-nodes-only:1.0"),
-                equalTo("urn:opendaylight:params:restconf:capability:changed-leaf-nodes-only:1.0"),
-                equalTo("urn:opendaylight:params:restconf:capability:skip-notification-data:1.0")));
-    }
-
 }
---
+++ restconf/restconf-nb/src/test/java/org/opendaylight/restconf/nb/rfc8040/monitoring/CapabilitiesWriterTest.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2022 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.restconf.nb.rfc8040.monitoring;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsInAnyOrder;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.opendaylight.yangtools.yang.data.api.schema.LeafSetEntryNode;
+
+public class CapabilitiesWriterTest {
+    @Test
+    public void restconfStateCapabilitiesTest() {
+        final var capability = CapabilitiesWriter.mapCapabilites();
+        assertEquals(CapabilitiesWriter.CAPABILITY, capability.name());
+
+        assertThat(capability.body().stream().map(LeafSetEntryNode::body).toList(),
+            containsInAnyOrder(
+                equalTo("urn:ietf:params:restconf:capability:depth:1.0"),
+                equalTo("urn:ietf:params:restconf:capability:fields:1.0"),
+                equalTo("urn:ietf:params:restconf:capability:filter:1.0"),
+                equalTo("urn:ietf:params:restconf:capability:replay:1.0"),
+                equalTo("urn:ietf:params:restconf:capability:with-defaults:1.0"),
+                equalTo("urn:opendaylight:params:restconf:capability:pretty-print:1.0"),
+                equalTo("urn:opendaylight:params:restconf:capability:leaf-nodes-only:1.0"),
+                equalTo("urn:opendaylight:params:restconf:capability:skip-notification-data:1.0")));
+    }
+}
