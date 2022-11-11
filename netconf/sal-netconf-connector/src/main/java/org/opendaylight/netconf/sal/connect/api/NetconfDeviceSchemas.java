1b66165534d5e3fddb8c085e0652d0b853571124
Robert Varga <robert.varga@pantheon.tech>
2022-11-12 00:46:39 +0100

    Drop documentation FIXMEs
    For an API, sal.connect.api is sorely under-documented. Add FIXMEs to
    rememediate this.
    
    Change-Id: Icbd4ff44c276e471af2471f6c297e2a07e3befcd
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- netconf/sal-netconf-connector/src/main/java/org/opendaylight/netconf/sal/connect/api/NetconfDeviceSchemas.java
+++ netconf/sal-netconf-connector/src/main/java/org/opendaylight/netconf/sal/connect/api/NetconfDeviceSchemas.java
@@ -5,13 +5,12 @@
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
-
 package org.opendaylight.netconf.sal.connect.api;
 
 import java.util.Set;
 import org.opendaylight.yangtools.yang.common.QName;
 
 public interface NetconfDeviceSchemas {
-
+    // FIXME: document this method
     Set<QName> getAvailableYangSchemasQNames();
 }
