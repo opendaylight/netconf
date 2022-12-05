9aae112b05e186bb7d502f54d048fd5f89bfb471
Robert Varga <robert.varga@pantheon.tech>
2022-12-06 00:36:25 +0100

    Move transaction chain handling
    NetconfDeviceTopologyAdapter is the only thing using DataBroker, push
    the transaction chain lifecycle management into it.
    
    JIRA: NETCONF-918
    Change-Id: I137bd759204be283e09e98ae88dfc4d8484ce8be
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- netconf/sal-netconf-connector/src/test/java/org/opendaylight/netconf/sal/connect/netconf/sal/NetconfDeviceSalProviderTest.java
+++ netconf/sal-netconf-connector/src/test/java/org/opendaylight/netconf/sal/connect/netconf/sal/NetconfDeviceSalProviderTest.java
@@ -7,33 +7,27 @@
  */
 package org.opendaylight.netconf.sal.connect.netconf.sal;
 
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.doReturn;
-import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
-import static org.opendaylight.mdsal.common.api.CommitInfo.emptyFluentFuture;
 
 import java.net.InetSocketAddress;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.opendaylight.mdsal.binding.api.DataBroker;
 import org.opendaylight.mdsal.binding.api.TransactionChain;
 import org.opendaylight.mdsal.binding.api.TransactionChainListener;
 import org.opendaylight.mdsal.binding.api.WriteTransaction;
-import org.opendaylight.mdsal.common.api.LogicalDatastoreType;
+import org.opendaylight.mdsal.common.api.CommitInfo;
 import org.opendaylight.mdsal.dom.api.DOMMountPointService;
 import org.opendaylight.netconf.sal.connect.util.RemoteDeviceId;
 
 @RunWith(MockitoJUnitRunner.StrictStubs.class)
 public class NetconfDeviceSalProviderTest {
-
     @Mock
     private WriteTransaction tx;
     @Mock
@@ -44,45 +38,36 @@ public class NetconfDeviceSalProviderTest {
     private DOMMountPointService mountPointService;
     @Mock
     private WriteTransaction writeTx;
+    @Captor
+    private ArgumentCaptor<TransactionChainListener> listeners;
 
     private NetconfDeviceSalProvider provider;
 
     @Before
-    public void setUp() throws Exception {
-        doReturn(chain).when(dataBroker).createTransactionChain(any(TransactionChainListener.class));
+    public void setUp() {
+        doReturn(chain).when(dataBroker).createMergingTransactionChain(listeners.capture());
         doReturn(writeTx).when(chain).newWriteOnlyTransaction();
-        doNothing().when(writeTx).merge(eq(LogicalDatastoreType.OPERATIONAL), any(), any());
         doReturn("Some object").when(writeTx).getIdentifier();
-        doReturn(emptyFluentFuture()).when(writeTx).commit();
+        doReturn(CommitInfo.emptyFluentFuture()).when(writeTx).commit();
         provider = new NetconfDeviceSalProvider(new RemoteDeviceId("device1",
                 InetSocketAddress.createUnresolved("localhost", 17830)), mountPointService, dataBroker);
-        when(chain.newWriteOnlyTransaction()).thenReturn(tx);
-        doReturn(emptyFluentFuture()).when(tx).commit();
-        when(tx.getIdentifier()).thenReturn(tx);
+        doReturn(tx).when(chain).newWriteOnlyTransaction();
+        doReturn(CommitInfo.emptyFluentFuture()).when(tx).commit();
+        doReturn(tx).when(tx).getIdentifier();
     }
 
     @Test
-    public void replaceChainIfFailed() throws Exception {
-        final ArgumentCaptor<TransactionChainListener> captor = ArgumentCaptor.forClass(TransactionChainListener.class);
-        verify(dataBroker).createTransactionChain(captor.capture());
-        try {
-            captor.getValue().onTransactionChainFailed(chain, tx, new Exception("chain failed"));
-        } catch (final IllegalStateException e) {
-            //expected
-        }
-        verify(dataBroker, times(2)).createTransactionChain(any());
-    }
-
-    @Test
-    public void close() throws Exception {
+    public void close() {
+        listeners.getValue().onTransactionChainSuccessful(chain);
         provider.close();
         verify(chain).close();
     }
 
     @Test
-    public void closeWithoutNPE() throws Exception {
+    public void closeWithoutNPE()  {
+        close();
+
+        // No further interations
         provider.close();
-        provider.close();
-        verify(chain, times(2)).close();
     }
 }
