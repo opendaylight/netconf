1360b22cdadad84ba92d0a12deb5f85cc7e8163e
Robert Varga <robert.varga@pantheon.tech>
2022-12-07 00:19:17 +0100

    WIP: Rehost reconnection handling
    AbstractNetconfTopology has all the tools it needs to react to device's
    failure to establish context. Move the reconnect logic there, cleaning
    up the bonkers routing done through NetconfDevice.
    
    // FIXME: reimplement the logic
    
    JIRA: NETCONF-925
    Change-Id: Iaf52f268d7e12f60fd2300d345fbc8c9d30a4c21
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- netconf/sal-netconf-connector/src/main/java/org/opendaylight/netconf/sal/connect/api/RemoteDeviceHandler.java
+++ netconf/sal-netconf-connector/src/main/java/org/opendaylight/netconf/sal/connect/api/RemoteDeviceHandler.java
@@ -11,7 +11,6 @@ import org.opendaylight.mdsal.dom.api.DOMActionService;
 import org.opendaylight.mdsal.dom.api.DOMNotification;
 import org.opendaylight.mdsal.dom.api.DOMRpcService;
 import org.opendaylight.netconf.sal.connect.netconf.listener.NetconfSessionPreferences;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.netconf.node.topology.rev150114.NetconfNode;
 import org.opendaylight.yangtools.rfc8528.data.api.MountPointContext;
 
 public interface RemoteDeviceHandler extends AutoCloseable {
@@ -38,10 +37,6 @@ public interface RemoteDeviceHandler extends AutoCloseable {
     default void onDeviceConnected(final MountPointContext mountContext,
             final NetconfSessionPreferences sessionPreferences,
             final DOMRpcService deviceRpc, final DOMActionService deviceAction) {
-        // DO NOTHING
-    }
-
-    default void onDeviceReconnected(final NetconfSessionPreferences sessionPreferences, final NetconfNode node) {
         // DO NOTHING
     }
 
