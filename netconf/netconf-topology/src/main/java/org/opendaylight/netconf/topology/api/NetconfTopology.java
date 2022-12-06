55b857f08d4fbf7a827fddb47a35b9318bf93a72
Robert Varga <robert.varga@pantheon.tech>
2022-12-06 23:32:55 +0100

    Clean up NetconfTopology methods
    No caller cares about NetconfDeviceCapabilities and we should not be
    using Void. Switch both futures to result in Empty, which is always
    non-null.
    
    This also removes one call site which assumes NetconfSessionPreferences
    contain NetconfDeviceCapabilities -- which they will not.
    
    JIRA: NETCONF-923
    Change-Id: I934d8b1cd5915117500b1c0d6028838b727a2353
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- netconf/netconf-topology/src/main/java/org/opendaylight/netconf/topology/api/NetconfTopology.java
+++ netconf/netconf-topology/src/main/java/org/opendaylight/netconf/topology/api/NetconfTopology.java
@@ -5,18 +5,16 @@
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
-
 package org.opendaylight.netconf.topology.api;
 
 import com.google.common.util.concurrent.ListenableFuture;
-import org.opendaylight.netconf.sal.connect.netconf.listener.NetconfDeviceCapabilities;
 import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.NodeId;
 import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.Node;
+import org.opendaylight.yangtools.yang.common.Empty;
 
 public interface NetconfTopology {
 
-    ListenableFuture<NetconfDeviceCapabilities> connectNode(NodeId nodeId, Node configNode);
+    ListenableFuture<Empty> connectNode(NodeId nodeId, Node configNode);
 
-    ListenableFuture<Void> disconnectNode(NodeId nodeId);
-
+    ListenableFuture<Empty> disconnectNode(NodeId nodeId);
 }
