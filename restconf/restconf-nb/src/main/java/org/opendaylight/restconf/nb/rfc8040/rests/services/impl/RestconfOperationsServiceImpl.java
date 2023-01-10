e897f5f8c2bfd124c87d8709b878ba7320d0e0f2
Yaroslav Lastivka <yaroslav.lastivka@pantheon.tech>
2023-01-10 11:39:21 +0200

    Fix retrieving operations resource
    Fix retrieving operations resource from controller and mount points
    by adapting getOperationsJSON/XML to accept identifiers and enhancing
    its functionality with ability to match exact operation.
    
    This way we enhance solution proposed by NETCONF-822 to cover
    all use cases and avoid error 500.
    
    JIRA: NETCONF-928
    JIRA: NETCONF-929
    Change-Id: Ic8f0ca5b07e060d36747a110f9049d3d9a2b96c8
    Signed-off-by: Yaroslav Lastivka <yaroslav.lastivka@pantheon.tech>
    Signed-off-by: Ivan Hrasko <ivan.hrasko@pantheon.tech>
    Signed-off-by: Peter Suna <peter.suna@pantheon.tech>

--- restconf/restconf-nb/src/main/java/org/opendaylight/restconf/nb/rfc8040/rests/services/impl/RestconfOperationsServiceImpl.java
+++ restconf/restconf-nb/src/main/java/org/opendaylight/restconf/nb/rfc8040/rests/services/impl/RestconfOperationsServiceImpl.java
@@ -9,44 +9,19 @@ package org.opendaylight.restconf.nb.rfc8040.rests.services.impl;
 
 import static java.util.Objects.requireNonNull;
 
-import com.google.common.collect.ImmutableSet;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Map;
-import java.util.Map.Entry;
 import java.util.Optional;
 import javax.ws.rs.Path;
-import javax.ws.rs.core.UriInfo;
-import org.eclipse.jdt.annotation.NonNull;
-import org.eclipse.jdt.annotation.Nullable;
-import org.opendaylight.mdsal.dom.api.DOMMountPoint;
 import org.opendaylight.mdsal.dom.api.DOMMountPointService;
-import org.opendaylight.mdsal.dom.api.DOMSchemaService;
-import org.opendaylight.restconf.common.context.InstanceIdentifierContext;
 import org.opendaylight.restconf.nb.rfc8040.databind.DatabindProvider;
-import org.opendaylight.restconf.nb.rfc8040.legacy.NormalizedNodePayload;
 import org.opendaylight.restconf.nb.rfc8040.rests.services.api.RestconfOperationsService;
 import org.opendaylight.restconf.nb.rfc8040.utils.parser.ParserIdentifier;
-import org.opendaylight.yangtools.yang.common.Empty;
-import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.NodeIdentifier;
-import org.opendaylight.yangtools.yang.data.api.schema.ContainerNode;
-import org.opendaylight.yangtools.yang.data.api.schema.builder.DataContainerNodeBuilder;
-import org.opendaylight.yangtools.yang.data.impl.schema.Builders;
-import org.opendaylight.yangtools.yang.data.impl.schema.ImmutableNodes;
 import org.opendaylight.yangtools.yang.model.api.EffectiveModelContext;
-import org.opendaylight.yangtools.yang.model.api.Module;
-import org.opendaylight.yangtools.yang.model.api.RpcDefinition;
-import org.opendaylight.yangtools.yang.model.util.SchemaInferenceStack;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /**
  * Implementation of {@link RestconfOperationsService}.
  */
 @Path("/")
 public class RestconfOperationsServiceImpl implements RestconfOperationsService {
-    private static final Logger LOG = LoggerFactory.getLogger(RestconfOperationsServiceImpl.class);
-
     private final DatabindProvider databindProvider;
     private final DOMMountPointService mountPointService;
 
@@ -68,59 +43,21 @@ public class RestconfOperationsServiceImpl implements RestconfOperationsService 
     }
 
     @Override
+    public String getOperationJSON(final String identifier) {
+        final var identifierContext = ParserIdentifier.toInstanceIdentifier(identifier,
+                databindProvider.currentContext().modelContext(), Optional.of(mountPointService));
+        return OperationsContent.JSON.bodyFor(identifierContext);
+    }
+
+    @Override
     public String getOperationsXML() {
         return OperationsContent.XML.bodyFor(databindProvider.currentContext().modelContext());
     }
 
     @Override
-    public NormalizedNodePayload getOperations(final String identifier, final UriInfo uriInfo) {
-        final InstanceIdentifierContext mountPointIdentifier = ParserIdentifier.toInstanceIdentifier(identifier,
-            databindProvider.currentContext().modelContext(), Optional.of(mountPointService));
-        final DOMMountPoint mountPoint = mountPointIdentifier.getMountPoint();
-        final var entry = contextForModelContext(modelContext(mountPoint), mountPoint);
-        return NormalizedNodePayload.of(entry.getKey(), entry.getValue());
+    public String getOperationXML(final String identifier) {
+        final var identifierContext = ParserIdentifier.toInstanceIdentifier(identifier,
+                databindProvider.currentContext().modelContext(), Optional.of(mountPointService));
+        return OperationsContent.XML.bodyFor(identifierContext);
     }
-
-    private static EffectiveModelContext modelContext(final DOMMountPoint mountPoint) {
-        return mountPoint.getService(DOMSchemaService.class)
-            .flatMap(svc -> Optional.ofNullable(svc.getGlobalContext()))
-            .orElse(null);
-    }
-
-    // FIXME: remove this method and everything it uses
-    @Deprecated(forRemoval = true, since = "4.0.0")
-    private static @NonNull Entry<InstanceIdentifierContext, ContainerNode> contextForModelContext(
-            final @NonNull EffectiveModelContext context, final @Nullable DOMMountPoint mountPoint) {
-        // Determine which modules we need and construct leaf schemas to correspond to all RPC definitions
-        final Collection<Module> modules = new ArrayList<>();
-        final ArrayList<OperationsLeafSchemaNode> rpcLeafSchemas = new ArrayList<>();
-        for (final Module m : context.getModules()) {
-            final Collection<? extends RpcDefinition> rpcs = m.getRpcs();
-            if (!rpcs.isEmpty()) {
-                modules.add(new OperationsImportedModule(m));
-                rpcLeafSchemas.ensureCapacity(rpcLeafSchemas.size() + rpcs.size());
-                for (RpcDefinition rpc : rpcs) {
-                    rpcLeafSchemas.add(new OperationsLeafSchemaNode(rpc));
-                }
-            }
-        }
-
-        // Now generate a module for RESTCONF so that operations contain what they need
-        final OperationsContainerSchemaNode operatationsSchema = new OperationsContainerSchemaNode(rpcLeafSchemas);
-        modules.add(new OperationsRestconfModule(operatationsSchema));
-
-        // Now build the operations container and combine it with the context
-        final DataContainerNodeBuilder<NodeIdentifier, ContainerNode> operationsBuilder = Builders.containerBuilder()
-                .withNodeIdentifier(new NodeIdentifier(OperationsContainerSchemaNode.QNAME));
-        for (final OperationsLeafSchemaNode leaf : rpcLeafSchemas) {
-            operationsBuilder.withChild(ImmutableNodes.leafNode(leaf.getQName(), Empty.value()));
-        }
-
-        final var opContext = new OperationsEffectiveModuleContext(ImmutableSet.copyOf(modules));
-        final var stack = SchemaInferenceStack.of(opContext);
-        stack.enterSchemaTree(operatationsSchema.getQName());
-
-        return Map.entry(InstanceIdentifierContext.ofStack(stack, mountPoint), operationsBuilder.build());
-    }
-
 }
