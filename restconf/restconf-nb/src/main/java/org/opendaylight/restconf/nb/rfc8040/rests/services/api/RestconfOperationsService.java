cd925be4f90bff22660f519799ba128c521fede6
Yaroslav Lastivka <yaroslav.lastivka@pantheon.tech>
2023-01-10 11:39:21 +0200

    Fix retrieving operation resource
    The problem that it was not possible to retrieve operation resource,
    for example when invoked:
    `GET: rests/operations/aaa-cert-rpc:getODLCertificate`
    The response is error 400 with message.
    According to RFC 8040, p. 22an empty leaf should be returned.
    
    Were created two getOperationsJSON() and getOperationsXML()
    overloaded methods - which contain
    and does not contain identifier as parameter.
    
    getOperations() method was deleted.
    
    JIRA: NETCONF-929
    JIRA: NETCONF-928
    Change-Id: Ic8f0ca5b07e060d36747a110f9049d3d9a2b96c8
    Signed-off-by: Yaroslav Lastivka <yaroslav.lastivka@pantheon.tech>
    Signed-off-by: Ivan Hrasko <ivan.hrasko@pantheon.tech>
    Signed-off-by: Peter Suna <peter.suna@pantheon.tech>

--- restconf/restconf-nb/src/main/java/org/opendaylight/restconf/nb/rfc8040/rests/services/api/RestconfOperationsService.java
+++ restconf/restconf-nb/src/main/java/org/opendaylight/restconf/nb/rfc8040/rests/services/api/RestconfOperationsService.java
@@ -11,11 +11,8 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.UriInfo;
 import org.opendaylight.restconf.nb.rfc8040.MediaTypes;
-import org.opendaylight.restconf.nb.rfc8040.legacy.NormalizedNodePayload;
 
 /**
  * Container that provides access to the data-model specific operations supported by the server.
@@ -32,6 +29,17 @@ public interface RestconfOperationsService {
     String getOperationsJSON();
 
     /**
+     * Retrieve list of operations and action supported by the server or device.
+     *
+     * @param identifier path parameter
+     * @return A string that contains JSON with available RPCs and actions.
+     */
+    @GET
+    @Path("/operations/{identifier:.+}")
+    @Produces({ MediaTypes.APPLICATION_YANG_DATA_JSON, MediaType.APPLICATION_JSON })
+    String getOperationJSON(@PathParam("identifier") String identifier);
+
+    /**
      * List RPC and action operations in RFC8040 XML format.
      *
      * @return A string containing a JSON document conforming to both RFC8040 section 11.3.1 and page 84.
@@ -42,20 +50,13 @@ public interface RestconfOperationsService {
     String getOperationsXML();
 
     /**
-     * Valid for mount points. List of operations supported by the server.
+     * Retrieve list of operations and action supported by the server or device.
      *
      * @param identifier path parameter
-     * @param uriInfo URI information
-     * @return {@link NormalizedNodePayload}
+     * @return A string that contains XML with available RPCs and actions.
      */
     @GET
     @Path("/operations/{identifier:.+}")
-    @Produces({
-        MediaTypes.APPLICATION_YANG_DATA_JSON,
-        MediaTypes.APPLICATION_YANG_DATA_XML,
-        MediaType.APPLICATION_JSON,
-        MediaType.APPLICATION_XML,
-        MediaType.TEXT_XML
-    })
-    NormalizedNodePayload getOperations(@PathParam("identifier") String identifier, @Context UriInfo uriInfo);
+    @Produces({ MediaTypes.APPLICATION_YANG_DATA_XML, MediaType.APPLICATION_XML, MediaType.TEXT_XML })
+    String getOperationXML(@PathParam("identifier") String identifier);
 }
--- restconf/restconf-nb/src/test/java/org/opendaylight/restconf/nb/rfc8040/rests/services/impl/Netconf822Test.java
+++ restconf/restconf-nb/src/test/java/org/opendaylight/restconf/nb/rfc8040/rests/services/impl/Netconf822Test.java
@@ -11,22 +11,30 @@ import static org.junit.Assert.assertEquals;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.opendaylight.restconf.common.context.InstanceIdentifierContext;
+import org.opendaylight.yangtools.yang.common.QName;
+import org.opendaylight.yangtools.yang.common.Revision;
 import org.opendaylight.yangtools.yang.model.api.EffectiveModelContext;
+import org.opendaylight.yangtools.yang.model.util.SchemaInferenceStack;
 import org.opendaylight.yangtools.yang.test.util.YangParserTestUtils;
 
 public class Netconf822Test {
     private static EffectiveModelContext CONTEXT;
+    private static SchemaInferenceStack STACK;
 
     @BeforeClass
     public static void beforeClass() {
         CONTEXT = YangParserTestUtils.parseYangResourceDirectory("/nc822");
+        STACK = SchemaInferenceStack.of(CONTEXT);
+        STACK.enterSchemaTree(QName.create("foo", "new1", Revision.of("2021-09-30")));
     }
 
     @Test
     public void testOperationsContentJSON() {
         assertEquals("{\n"
             + "  \"ietf-restconf:operations\" : {\n"
-            + "    \"foo:new\": [null]\n"
+            + "    \"foo:new\": [null],\n"
+            + "    \"foo:new1\": [null]\n"
             + "  }\n"
             + "}", OperationsContent.JSON.bodyFor(CONTEXT));
     }
@@ -37,6 +45,27 @@ public class Netconf822Test {
             + "<operations xmlns=\"urn:ietf:params:xml:ns:yang:ietf-restconf\"\n"
             + "            xmlns:ns0=\"foo\" >\n"
             + "  <ns0:new/>\n"
+            + "  <ns0:new1/>\n"
             + "</operations>", OperationsContent.XML.bodyFor(CONTEXT));
+    }
+
+    @Test
+    public void testOperationsContentByIdentifierJSON() {
+        InstanceIdentifierContext instanceIdentifierContext = InstanceIdentifierContext.ofStack(STACK);
+        assertEquals("{\n"
+                + "  \"ietf-restconf:operations\" : {\n"
+                + "    \"foo:new1\": [null]\n"
+                + "  }\n"
+                + "}", OperationsContent.JSON.bodyFor(instanceIdentifierContext));
+    }
+
+    @Test
+    public void testOperationsContentByIdentifierXML() {
+        InstanceIdentifierContext instanceIdentifierContext = InstanceIdentifierContext.ofStack(STACK);
+        assertEquals("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
+                + "<operations xmlns=\"urn:ietf:params:xml:ns:yang:ietf-restconf\"\n"
+                + "            xmlns:ns0=\"foo\" >\n"
+                + "  <ns0:new1/>\n"
+                + "</operations>", OperationsContent.XML.bodyFor(instanceIdentifierContext));
     }
 }
