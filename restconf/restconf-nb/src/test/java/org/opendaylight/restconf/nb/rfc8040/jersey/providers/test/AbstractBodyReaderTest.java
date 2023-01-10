16d0926e4bad77ac41ee1a1e29c5599926c0d380
Peter Suna <peter.suna@pantheon.tech>
2023-01-10 10:55:13 +0100

    Fix XML parser condition in XmlPatchBodyReader
    Remove redundant if condition which reject to parse
    LeafListSchemaNode or LeafSchemaNode value from XML
    
    JIRA: NETCONF-937
    Change-Id: Ib001582b8476f4508bab7d746296cb7ea1b1619e
    Signed-off-by: Peter Suna <peter.suna@pantheon.tech>

--- restconf/restconf-nb/src/test/java/org/opendaylight/restconf/nb/rfc8040/jersey/providers/test/AbstractBodyReaderTest.java
+++ restconf/restconf-nb/src/test/java/org/opendaylight/restconf/nb/rfc8040/jersey/providers/test/AbstractBodyReaderTest.java
@@ -38,10 +38,14 @@ import org.opendaylight.restconf.nb.rfc8040.jersey.providers.spi.AbstractIdentif
 import org.opendaylight.restconf.nb.rfc8040.legacy.NormalizedNodePayload;
 import org.opendaylight.yangtools.yang.common.ErrorTag;
 import org.opendaylight.yangtools.yang.common.ErrorType;
+import org.opendaylight.yangtools.yang.common.QName;
 import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
 import org.opendaylight.yangtools.yang.model.api.EffectiveModelContext;
 
 public abstract class AbstractBodyReaderTest {
+    public static final QName LEAF_NAME_QNAME = QName.create("instance:identifier:patch:module", "2015-11-21","name")
+            .intern();
+
     protected final MediaType mediaType;
     protected final DatabindProvider databindProvider;
     protected final DOMMountPointService mountPointService;
