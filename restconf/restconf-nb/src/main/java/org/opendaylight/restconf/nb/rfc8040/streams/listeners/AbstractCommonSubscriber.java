ada45920795638d81e0f9bd9bcd9e133a2f8167b
Nikhil Soni <nikk.sonitech@gmail.com>
2023-01-23 19:22:39 +0530

    Resubscribe Device Notification when Device gets reconnect
    JIRA: NETCONF-907
    Change-Id: I3f370cc2b93bc959d29a6109b162ec98291c300e
    Signed-off-by: Nikhil Soni <nikk.sonitech@gmail.com>

--- restconf/restconf-nb/src/main/java/org/opendaylight/restconf/nb/rfc8040/streams/listeners/AbstractCommonSubscriber.java
+++ restconf/restconf-nb/src/main/java/org/opendaylight/restconf/nb/rfc8040/streams/listeners/AbstractCommonSubscriber.java
@@ -53,10 +53,9 @@ abstract class AbstractCommonSubscriber<T> extends AbstractNotificationsData imp
 
     private final EventFormatterFactory<T> formatterFactory;
     private final NotificationOutputType outputType;
-    private final String streamName;
+    protected final String streamName;
 
-    @GuardedBy("this")
-    private final Set<StreamSessionHandler> subscribers = new HashSet<>();
+    private static final Set<StreamSessionHandler> SUBSCRIBERS = new HashSet<>();
     @GuardedBy("this")
     private Registration registration;
 
@@ -90,12 +89,12 @@ abstract class AbstractCommonSubscriber<T> extends AbstractNotificationsData imp
 
     @Override
     public final synchronized boolean hasSubscribers() {
-        return !subscribers.isEmpty();
+        return !SUBSCRIBERS.isEmpty();
     }
 
     @Override
     public final synchronized Set<StreamSessionHandler> getSubscribers() {
-        return new HashSet<>(subscribers);
+        return new HashSet<>(SUBSCRIBERS);
     }
 
     @Override
@@ -104,8 +103,16 @@ abstract class AbstractCommonSubscriber<T> extends AbstractNotificationsData imp
             registration.close();
             registration = null;
         }
-        deleteDataInDS(streamName).get();
-        subscribers.clear();
+
+        deleteDataInDS().get();
+        SUBSCRIBERS.clear();
+    }
+
+    public final void resetRegistration() {
+        if (registration != null) {
+            registration.close();
+            registration = null;
+        }
     }
 
     @Override
@@ -113,12 +120,12 @@ abstract class AbstractCommonSubscriber<T> extends AbstractNotificationsData imp
         final boolean isConnected = subscriber.isConnected();
         checkState(isConnected);
         LOG.debug("Subscriber {} is added.", subscriber);
-        subscribers.add(subscriber);
+        SUBSCRIBERS.add(subscriber);
     }
 
     @Override
     public synchronized void removeSubscriber(final StreamSessionHandler subscriber) {
-        subscribers.remove(subscriber);
+        SUBSCRIBERS.remove(subscriber);
         LOG.debug("Subscriber {} is removed", subscriber);
         if (!hasSubscribers()) {
             ListenersBroker.getInstance().removeAndCloseListener(this);
@@ -220,7 +227,7 @@ abstract class AbstractCommonSubscriber<T> extends AbstractNotificationsData imp
      * @param data Data of incoming notifications.
      */
     synchronized void post(final String data) {
-        final Iterator<StreamSessionHandler> iterator = subscribers.iterator();
+        final Iterator<StreamSessionHandler> iterator = SUBSCRIBERS.iterator();
         while (iterator.hasNext()) {
             final StreamSessionHandler subscriber = iterator.next();
             final boolean isConnected = subscriber.isConnected();
