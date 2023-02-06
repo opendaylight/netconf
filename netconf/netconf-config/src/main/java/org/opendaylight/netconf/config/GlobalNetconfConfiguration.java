993856406715db7496e1591ae486b5ecb78b663b
Robert Varga <robert.varga@pantheon.tech>
2023-02-06 19:37:10 +0100

    Convert netconf-config to OSGi DS
    Convert blueprint to explicit OSGi DS wiring, retaining the API and
    behaviour of the wiring. This requires use of ComponentFactories to
    deal with the fact the configuration is in a single configurationPid,
    but really ends up being configuration of three inter-dependend classes.
    
    JIRA: NETCONF-950
    Change-Id: I19d506a336483bf0ea06a230ef5108a35d8845cc
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ netconf/netconf-config/src/main/java/org/opendaylight/netconf/config/GlobalNetconfConfiguration.java
@@ -0,0 +1,107 @@
+/*
+ * Copyright (c) 2023 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.netconf.config;
+
+import static com.google.common.base.Verify.verifyNotNull;
+import static java.util.Objects.requireNonNull;
+
+import java.util.ArrayList;
+import java.util.Map;
+import org.osgi.framework.FrameworkUtil;
+import org.osgi.service.component.ComponentFactory;
+import org.osgi.service.component.ComponentInstance;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.service.component.annotations.Modified;
+import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.metatype.annotations.Designate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Component instantiating global NETCONF resources.
+ */
+@Component(service = { }, configurationPid = "org.opendaylight.netconf.config")
+@Designate(ocd = Configuration.class)
+public final class GlobalNetconfConfiguration {
+    private static final Logger LOG = LoggerFactory.getLogger(GlobalNetconfConfiguration.class);
+
+    private final ComponentFactory<GlobalNetconfProcessingExecutor> processingFactory;
+    private final ComponentFactory<GlobalNetconfSshScheduledExecutor> sshScheduledFactory;
+
+    private GlobalNetconfThreadFactory threadFactory;
+    private ComponentInstance<GlobalNetconfProcessingExecutor> processingExecutor;
+    private Map<String, ?> processingProps;
+    private ComponentInstance<GlobalNetconfSshScheduledExecutor> sshScheduledExecutor;
+    private Map<String, ?> sshScheduledProps;
+
+    @Activate
+    public GlobalNetconfConfiguration(
+            @Reference(target = "(component.factory=" + GlobalNetconfProcessingExecutor.FACTORY_NAME + ")")
+            final ComponentFactory<GlobalNetconfProcessingExecutor> processingFactory,
+            @Reference(target = "(component.factory=" + GlobalNetconfSshScheduledExecutor.FACTORY_NAME + ")")
+            final ComponentFactory<GlobalNetconfSshScheduledExecutor> sshScheduledFactory,
+            final Configuration configuration) {
+        this.processingFactory = requireNonNull(processingFactory);
+        this.sshScheduledFactory = requireNonNull(sshScheduledFactory);
+
+        threadFactory = new GlobalNetconfThreadFactory(configuration.namePrefix());
+        processingProps = GlobalNetconfProcessingExecutor.props(threadFactory, configuration);
+        processingExecutor = processingFactory.newInstance(FrameworkUtil.asDictionary(processingProps));
+        sshScheduledProps = GlobalNetconfSshScheduledExecutor.props(threadFactory, configuration);
+        sshScheduledExecutor = sshScheduledFactory.newInstance(FrameworkUtil.asDictionary(sshScheduledProps));
+        LOG.info("Global NETCONF configuration pools started");
+    }
+
+    @Modified
+    void modified(final Configuration configuration) {
+        final var newNamePrefix = configuration.namePrefix();
+        if (!threadFactory.getNamePrefix().equals(newNamePrefix)) {
+            threadFactory = new GlobalNetconfThreadFactory(newNamePrefix);
+            processingProps = null;
+            sshScheduledProps = null;
+            LOG.debug("Forcing restart of all executors");
+        }
+
+        // We want to instantiate new services before we dispose old ones, so
+        final var toDispose = new ArrayList<ComponentInstance<?>>();
+
+        final var newProcessingProps = GlobalNetconfProcessingExecutor.props(threadFactory, configuration);
+        if (!newProcessingProps.equals(processingProps)) {
+            processingProps = newProcessingProps;
+            toDispose.add(processingExecutor);
+            processingExecutor = processingFactory.newInstance(FrameworkUtil.asDictionary(processingProps));
+            LOG.debug("Processing executor restarted with {}", processingProps);
+        }
+
+        final var newSshScheduledProps = GlobalNetconfSshScheduledExecutor.props(threadFactory, configuration);
+        if (!newSshScheduledProps.equals(sshScheduledProps)) {
+            sshScheduledProps = newSshScheduledProps;
+            toDispose.add(sshScheduledExecutor);
+            sshScheduledExecutor = sshScheduledFactory.newInstance(FrameworkUtil.asDictionary(sshScheduledProps));
+            LOG.debug("Scheduled executor restarted with {}", sshScheduledProps);
+        }
+
+        toDispose.forEach(ComponentInstance::dispose);
+    }
+
+    @Deactivate
+    void deactivate() {
+        processingExecutor.dispose();
+        processingExecutor = null;
+        sshScheduledExecutor.dispose();
+        sshScheduledExecutor = null;
+        threadFactory = null;
+        LOG.info("Global NETCONF configuration pools stopped");
+    }
+
+    static <T> T extractProp(final Map<String, ?> properties, final String key, final Class<T> valueType) {
+        return valueType.cast(verifyNotNull(properties.get(requireNonNull(key))));
+    }
+}
