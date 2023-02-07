c8ef39052ea5e77cbce6d27657a2a17e401277a3
Robert Varga <robert.varga@pantheon.tech>
2023-02-07 19:47:51 +0100

    Split out NetconfMapperAggregator
    Create a component to host the corresponding Blueprint element.
    This starts the path to making the contract of
    AggregatedNetconfOperationServiceFactory more explicit.
    
    JIRA: NETCONF-951
    Change-Id: Ic4f0164ac3423f39ca0d52a6200dcbbfa8f35f7f
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ netconf/mdsal-netconf-impl/src/main/java/org/opendaylight/netconf/impl/mdsal/NetconfMapperAggregator.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2023 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.netconf.impl.mdsal;
+
+import org.opendaylight.netconf.impl.osgi.AggregatedNetconfOperationServiceFactory;
+import org.opendaylight.netconf.mapping.api.NetconfOperationServiceFactory;
+import org.opendaylight.netconf.mapping.api.NetconfOperationServiceFactoryListener;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+
+@Component(service = { NetconfOperationServiceFactory.class, NetconfOperationServiceFactoryListener.class },
+           property = "type=mapper-aggregator-registry", immediate = true)
+public final class NetconfMapperAggregator extends AggregatedNetconfOperationServiceFactory {
+    @Activate
+    public NetconfMapperAggregator() {
+        super();
+    }
+
+    @Override
+    @Deactivate
+    public void close() {
+        super.close();
+    }
+}
