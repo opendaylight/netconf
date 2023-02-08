63edfa3b52d61306cd093c41ae560004ee8ba1e7
Robert Varga <robert.varga@pantheon.tech>
2023-02-08 20:04:52 +0100

    Refactor restconf-nb blueprint
    Blueprint here is really a large code fragment. Move it to a new class,
    RestconfNorthbound, so that we purely inject dependencies/configuration.
    
    Change-Id: Ic7214393a599c136aa3876c111145182cc68db0b
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ restconf/restconf-nb/src/main/java/org/opendaylight/restconf/nb/rfc8040/RestconfNorthbound.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright (c) 2023 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.restconf.nb.rfc8040;
+
+import com.google.common.annotations.Beta;
+import javax.servlet.ServletException;
+import org.opendaylight.aaa.filterchain.configuration.CustomFilterAdapterConfiguration;
+import org.opendaylight.aaa.web.WebContextSecurer;
+import org.opendaylight.aaa.web.WebServer;
+import org.opendaylight.aaa.web.servlet.ServletSupport;
+import org.opendaylight.controller.config.threadpool.util.NamingThreadPoolFactory;
+import org.opendaylight.controller.config.threadpool.util.ScheduledThreadPoolWrapper;
+import org.opendaylight.mdsal.dom.api.DOMActionService;
+import org.opendaylight.mdsal.dom.api.DOMDataBroker;
+import org.opendaylight.mdsal.dom.api.DOMMountPointService;
+import org.opendaylight.mdsal.dom.api.DOMNotificationService;
+import org.opendaylight.mdsal.dom.api.DOMRpcService;
+import org.opendaylight.mdsal.dom.api.DOMSchemaService;
+import org.opendaylight.restconf.nb.rfc8040.databind.DatabindProvider;
+import org.opendaylight.restconf.nb.rfc8040.rests.services.impl.RestconfDataStreamServiceImpl;
+import org.opendaylight.restconf.nb.rfc8040.streams.Configuration;
+import org.opendaylight.restconf.nb.rfc8040.streams.WebSocketInitializer;
+import org.opendaylight.restconf.nb.rfc8040.web.WebInitializer;
+
+/**
+ * Main entrypoint into RFC8040 northbound. Take care of wiring up all applications activating them through JAX-RS.
+ */
+@Beta
+public final class RestconfNorthbound implements AutoCloseable {
+    private final WebInitializer webInitializer;
+
+    public RestconfNorthbound(final WebServer webServer, final WebContextSecurer webContextSecurer,
+            final ServletSupport servletSupport, final CustomFilterAdapterConfiguration filterAdapterConfiguration,
+            final DOMActionService actionService, final DOMDataBroker dataBroker,
+            final DOMMountPointService mountPointService, final DOMNotificationService notificationService,
+            final DOMRpcService rpcService, final DOMSchemaService schemaService,
+            final DatabindProvider databindProvider,
+            final String pingNamePrefix, final int pingMaxThreadCount, final int maximumFragmentLength,
+            final int heartbeatInterval, final int idleTimeout, final boolean useSSE) throws ServletException {
+        final var configuration = new Configuration(maximumFragmentLength, idleTimeout, heartbeatInterval, useSSE);
+        final var scheduledThreadPool = new ScheduledThreadPoolWrapper(pingMaxThreadCount,
+            new NamingThreadPoolFactory(pingNamePrefix));
+
+        webInitializer = new WebInitializer(webServer, webContextSecurer, servletSupport,
+            new RestconfApplication(databindProvider, mountPointService, dataBroker, rpcService, actionService,
+                notificationService, schemaService, configuration),
+            new DataStreamApplication(databindProvider, mountPointService,
+                new RestconfDataStreamServiceImpl(scheduledThreadPool, configuration)),
+            filterAdapterConfiguration,
+            new WebSocketInitializer(scheduledThreadPool, configuration));
+    }
+
+    @Override
+    public void close() {
+        webInitializer.close();
+    }
+}
