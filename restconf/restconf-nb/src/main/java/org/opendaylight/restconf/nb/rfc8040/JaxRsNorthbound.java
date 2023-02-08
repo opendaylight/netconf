18c75b46acfb67db74cf791cc8cffcc2d60cfc44
Robert Varga <robert.varga@pantheon.tech>
2023-02-08 20:36:20 +0100

    Remove WebInitializer
    WebInitializer is a useless indirection, remove it and inline its logic
    into JaxRsNorthbound.
    
    JIRA: NETCONF-958
    Change-Id: I8a51c128f4e17a36fb719f9c3209e0d7dde933a2
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- restconf/restconf-nb/src/main/java/org/opendaylight/restconf/nb/rfc8040/JaxRsNorthbound.java
+++ restconf/restconf-nb/src/main/java/org/opendaylight/restconf/nb/rfc8040/JaxRsNorthbound.java
@@ -7,9 +7,18 @@
  */
 package org.opendaylight.restconf.nb.rfc8040;
 
+import static org.opendaylight.restconf.nb.rfc8040.rests.utils.RestconfStreamsConstants.DATA_SUBSCRIPTION;
+import static org.opendaylight.restconf.nb.rfc8040.rests.utils.RestconfStreamsConstants.NOTIFICATION_STREAM;
+import static org.opendaylight.restconf.nb.rfc8040.utils.RestconfConstants.BASE_URI_PATTERN;
+import static org.opendaylight.restconf.nb.rfc8040.utils.RestconfConstants.NOTIF;
+
 import com.google.common.annotations.Beta;
 import javax.servlet.ServletException;
 import org.opendaylight.aaa.filterchain.configuration.CustomFilterAdapterConfiguration;
+import org.opendaylight.aaa.filterchain.filters.CustomFilterAdapter;
+import org.opendaylight.aaa.web.FilterDetails;
+import org.opendaylight.aaa.web.ServletDetails;
+import org.opendaylight.aaa.web.WebContext;
 import org.opendaylight.aaa.web.WebContextSecurer;
 import org.opendaylight.aaa.web.WebServer;
 import org.opendaylight.aaa.web.servlet.ServletSupport;
@@ -25,14 +34,15 @@ import org.opendaylight.restconf.nb.rfc8040.databind.DatabindProvider;
 import org.opendaylight.restconf.nb.rfc8040.rests.services.impl.RestconfDataStreamServiceImpl;
 import org.opendaylight.restconf.nb.rfc8040.streams.Configuration;
 import org.opendaylight.restconf.nb.rfc8040.streams.WebSocketInitializer;
-import org.opendaylight.restconf.nb.rfc8040.web.WebInitializer;
+import org.opendaylight.yangtools.concepts.Registration;
 
 /**
  * Main entrypoint into RFC8040 northbound. Take care of wiring up all applications activating them through JAX-RS.
  */
 @Beta
 public final class JaxRsNorthbound implements AutoCloseable {
-    private final WebInitializer webInitializer;
+    private final Registration discoveryReg;
+    private final Registration restconfReg;
 
     public JaxRsNorthbound(final WebServer webServer, final WebContextSecurer webContextSecurer,
             final ServletSupport servletSupport, final CustomFilterAdapterConfiguration filterAdapterConfiguration,
@@ -46,17 +56,60 @@ public final class JaxRsNorthbound implements AutoCloseable {
         final var scheduledThreadPool = new ScheduledThreadPoolWrapper(pingMaxThreadCount,
             new NamingThreadPoolFactory(pingNamePrefix));
 
-        webInitializer = new WebInitializer(webServer, webContextSecurer, servletSupport,
-            new RestconfApplication(databindProvider, mountPointService, dataBroker, rpcService, actionService,
-                notificationService, schemaService, configuration),
-            new DataStreamApplication(databindProvider, mountPointService,
-                new RestconfDataStreamServiceImpl(scheduledThreadPool, configuration)),
-            filterAdapterConfiguration,
-            new WebSocketInitializer(scheduledThreadPool, configuration));
+        final var restconfBuilder = WebContext.builder()
+            .name("RFC8040 RESTCONF")
+            .contextPath("/" + BASE_URI_PATTERN)
+            .supportsSessions(false)
+            .addServlet(ServletDetails.builder()
+                .addUrlPattern("/*")
+                .servlet(servletSupport.createHttpServletBuilder(
+                    new RestconfApplication(databindProvider, mountPointService, dataBroker, rpcService, actionService,
+                        notificationService,schemaService, configuration)).build())
+                .asyncSupported(true)
+                .build())
+            .addServlet(ServletDetails.builder()
+                .addUrlPattern("/" + NOTIF + "/*")
+                .servlet(servletSupport.createHttpServletBuilder(
+                    new DataStreamApplication(databindProvider, mountPointService,
+                        new RestconfDataStreamServiceImpl(scheduledThreadPool, configuration))).build())
+                .name("notificationServlet")
+                .asyncSupported(true)
+                .build())
+            .addServlet(ServletDetails.builder()
+                .addUrlPattern("/" + DATA_SUBSCRIPTION + "/*")
+                .addUrlPattern("/" + NOTIFICATION_STREAM + "/*")
+                .servlet(new WebSocketInitializer(scheduledThreadPool, configuration))
+                .build())
+
+            // Allows user to add javax.servlet.Filter(s) in front of REST services
+            .addFilter(FilterDetails.builder()
+                .addUrlPattern("/*")
+                .filter(new CustomFilterAdapter(filterAdapterConfiguration))
+                .asyncSupported(true)
+                .build());
+
+        webContextSecurer.requireAuthentication(restconfBuilder, true, "/*");
+
+        restconfReg = webServer.registerWebContext(restconfBuilder.build());
+
+        final var discoveryBuilder = WebContext.builder()
+            .name("RFC6415 Web Host Metadata")
+            .contextPath("/.well-known")
+            .supportsSessions(false)
+            .addServlet(ServletDetails.builder()
+                .addUrlPattern("/*")
+                .servlet(servletSupport.createHttpServletBuilder(new RootFoundApplication(BASE_URI_PATTERN)).build())
+                .name("Rootfound")
+                .build());
+
+        webContextSecurer.requireAuthentication(discoveryBuilder, true, "/*");
+
+        discoveryReg = webServer.registerWebContext(discoveryBuilder.build());
     }
 
     @Override
     public void close() {
-        webInitializer.close();
+        discoveryReg.close();
+        restconfReg.close();
     }
 }
