e7996455d2d0e4a2369c5b40142cad0b0e176d23
Ivan Hrasko <ivan.hrasko@pantheon.tech>
2023-03-08 10:22:03 +0100

    Fix YangLib media types
    Fix YangLib media types to return "application/yang" with default
    fallback to "text/plain".
    
    JIRA: NETCONF-972
    Change-Id: Ibd7a6939342cfbe4b525442d8606e4c27d3e8b65
    Signed-off-by: Ivan Hrasko <ivan.hrasko@pantheon.tech>

--- netconf/yanglib/src/main/java/org/opendaylight/yanglib/api/YangLibService.java
+++ netconf/yanglib/src/main/java/org/opendaylight/yanglib/api/YangLibService.java
@@ -5,18 +5,20 @@
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
-
 package org.opendaylight.yanglib.api;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import org.opendaylight.yangtools.yang.common.YangConstants;
 
 /**
  * Service provides YANG schema sources for modules from yang library.
  */
 @Path("/")
+@Produces({ MediaType.TEXT_PLAIN, YangConstants.RFC6020_YANG_MEDIA_TYPE })
 public interface YangLibService {
 
     /**
@@ -26,7 +28,6 @@ public interface YangLibService {
      * @return Module's source
      */
     @GET
-    @Produces("text/plain")
     @Path("/schemas/{modelName}/{revision:([0-9\\-]*)}")
     String getSchema(@PathParam("modelName") String name, @PathParam("revision") String revision);
 }
