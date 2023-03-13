dbfe312d3f62b6711c0b011a44be4481df291224
Peter Suna <peter.suna@pantheon.tech>
2023-03-13 17:57:58 +0100

    WIP: Use JsonGenerator in DefinitionGenerator
    Change-Id: I87a4771b9fe39d68f591ce4293cca373ba18f03d
    Signed-off-by: Peter Suna <peter.suna@pantheon.tech>

--- restconf/sal-rest-docgen/src/main/java/org/opendaylight/netconf/sal/rest/doc/impl/DefinitionObject.java
+++ restconf/sal-rest-docgen/src/main/java/org/opendaylight/netconf/sal/rest/doc/impl/DefinitionObject.java
@@ -8,9 +8,11 @@
 
 package org.opendaylight.netconf.sal.rest.doc.impl;
 
+import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.JsonNodeFactory;
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import java.io.IOException;
 import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.Map.Entry;
@@ -18,16 +20,39 @@ import java.util.Objects;
 import java.util.Optional;
 
 public class DefinitionObject {
+//   "toaster2_toaster_toasterSlot_config_slotInfo": {   <-- Root node
+//        "properties": {                                <-- Nodes with parent
+//            "numberOfToastPrepared": {
+//                "description": "",
+//                "format": "int64",
+//                "default": 0,
+//                "type": "integer",
+//                "xml": {
+//                    "name": "numberOfToastPrepared",
+//                    "namespace": "http://netconfcentral.org/ns/toaster/augmented"
+//                }
+//            }
+//        },
+//        "type": "object",
+//        "title": "toaster2_toaster_toasterSlot_config_slotInfo",
+//        "description": "",
+//        "xml": {
+//            "name": "slotInfo",
+//            "namespace": "http://netconfcentral.org/ns/toaster/augmented"
+//        }
+//    },
     private final HashMap<String, DataType<?>> data;
     private final DefinitionObject parent;
 
     public DefinitionObject() {
         this.parent = null;
+        // The root node contains a large number of nodes that are stored inside the data.
         this.data = new HashMap<>(1000, 0.8f);
     }
 
     public DefinitionObject(final DefinitionObject parent) {
         this.parent = parent.getParent().orElse(parent);
+        // The nodes that are not in the root contain a small number of nodes, usually six or fewer.
         this.data = new HashMap<>(8, 0.75f);
     }
 
@@ -192,6 +217,83 @@ public class DefinitionObject {
             }
         }
         return result;
+    }
+
+    public void writeDataToJsonGenerator(final JsonGenerator gen) throws IOException {
+        for (final Entry<String, DataType<?>> entry : data.entrySet()) {
+            final DataType<?> dataType = entry.getValue();
+            switch (dataType.getClazz()) {
+                case STRING -> {
+                    gen.writeStringField(entry.getKey(), (String) dataType.getValue());
+                }
+                case BOOLEAN -> {
+                    gen.writeBooleanField(entry.getKey(), (Boolean) dataType.getValue());
+                }
+                case INTEGER -> {
+                    gen.writeNumberField(entry.getKey(), (Integer) dataType.getValue());
+                }
+                case BIG_DECIMAL -> {
+                    gen.writeNumberField(entry.getKey(), (BigDecimal) dataType.getValue());
+                }
+                case LONG -> {
+                    gen.writeNumberField(entry.getKey(), (Long) dataType.getValue());
+                }
+                case DOUBLE -> {
+                    gen.writeNumberField(entry.getKey(), (Double) dataType.getValue());
+                }
+                case FLOAT -> {
+                    gen.writeNumberField(entry.getKey(), (Float) dataType.getValue());
+                }
+                case SHORT -> {
+                    gen.writeNumberField(entry.getKey(), (Short) dataType.getValue());
+                }
+                case DEFINITION_OBJECT -> {
+                    gen.writeObjectFieldStart(entry.getKey());
+                    ((DefinitionObject) dataType.getValue()).writeDataToJsonGenerator(gen);
+                    gen.writeEndObject();
+                }
+                case STRING_ARRAY -> {
+                    gen.writeArrayFieldStart(entry.getKey());
+                    final String[] value = (String[]) dataType.getValue();
+                    gen.writeArray(value, 0, value.length);
+                    gen.writeEndArray();
+                }
+                case INTEGER_ARRAY -> {
+                    gen.writeArrayFieldStart(entry.getKey());
+                    final Integer[] value = (Integer[]) dataType.getValue();
+                    for (final Integer val : value) {
+                        gen.writeNumber(val);
+                    }
+                    gen.writeEndArray();
+                }
+                case BOOLEAN_ARRAY -> {
+                    gen.writeArrayFieldStart(entry.getKey());
+                    final Boolean[] value = (Boolean[]) dataType.getValue();
+                    for (final Boolean val : value) {
+                        gen.writeBoolean(val);
+                    }
+                    gen.writeEndArray();
+                }
+                case BIG_DECIMAL_ARRAY -> {
+                    gen.writeArrayFieldStart(entry.getKey());
+                    final BigDecimal[] value = (BigDecimal[]) dataType.getValue();
+                    for (final BigDecimal val : value) {
+                        gen.writeNumber(val);
+                    }
+                    gen.writeEndArray();
+                }
+                case LONG_ARRAY -> {
+                    gen.writeArrayFieldStart(entry.getKey());
+                    final Long[] value = (Long[]) dataType.getValue();
+                    for (final Long val : value) {
+                        gen.writeNumber(val);
+                    }
+                    gen.writeEndArray();
+                }
+                default -> {
+                }
+            }
+        }
     }
 
     public static final class DataType<T> {
