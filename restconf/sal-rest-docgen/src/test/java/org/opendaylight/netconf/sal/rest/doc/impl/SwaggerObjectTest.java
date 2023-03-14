d3de873bf71a5cc457f28be5031a2c5bed8e3bbe
Peter Suna <peter.suna@pantheon.tech>
2023-03-14 16:52:04 +0100

    WIP: Use JsonGenerator in DefinitionGenerator class
    Refactor the DefinitionGenerator class to use JsonGenerator
    wherever possible.
    
    In the Components class, the schema is currently stored
    as an ObjectNode class. To align with the OpenApi specifications,
    replace this with a Map<String, ObjectNode>.
    This change will ensure consistency with the standards and improve
    the code's readability.
    
    JIRA: NETCONF-938
    Change-Id: I6badf92d62226faa16383ee6224aa1cdf89fd940
    Signed-off-by: Peter Suna <peter.suna@pantheon.tech>

--- restconf/sal-rest-docgen/src/test/java/org/opendaylight/netconf/sal/rest/doc/impl/SwaggerObjectTest.java
+++ restconf/sal-rest-docgen/src/test/java/org/opendaylight/netconf/sal/rest/doc/impl/SwaggerObjectTest.java
@@ -9,33 +9,33 @@ package org.opendaylight.netconf.sal.rest.doc.impl;
 
 import static org.junit.Assert.assertNotNull;
 
-import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.IOException;
 import org.junit.Test;
 import org.opendaylight.yangtools.yang.common.Revision;
 
 public final class SwaggerObjectTest extends AbstractApiDocTest {
+
+    private static final ObjectMapper MAPPER = new ObjectMapper();
+
     @Test
     public void testConvertToJsonSchema() throws IOException {
         final var module = CONTEXT.findModule("opflex", Revision.of("2014-05-28")).orElseThrow();
-        final DefinitionGenerator generator = new DefinitionGenerator();
-        final ObjectNode jsonObject = generator.convertToJsonSchema(module, CONTEXT, new DefinitionNames(), true);
-        assertNotNull(jsonObject);
+        final var result = DefinitionGenerator.getSchema(module, CONTEXT, new DefinitionNames(), true, MAPPER);
+        assertNotNull(result);
     }
 
     @Test
     public void testActionTypes() throws IOException {
         final var module = CONTEXT.findModule("action-types").orElseThrow();
-        final DefinitionGenerator generator = new DefinitionGenerator();
-        final ObjectNode jsonObject = generator.convertToJsonSchema(module, CONTEXT, new DefinitionNames(), true);
-        assertNotNull(jsonObject);
+        final var result = DefinitionGenerator.getSchema(module, CONTEXT, new DefinitionNames(), true, MAPPER);
+        assertNotNull(result);
     }
 
     @Test
     public void testStringTypes() throws IOException {
         final var module = CONTEXT.findModule("string-types").orElseThrow();
-        final DefinitionGenerator generator = new DefinitionGenerator();
-        final ObjectNode jsonObject = generator.convertToJsonSchema(module, CONTEXT, new DefinitionNames(), true);
-        assertNotNull(jsonObject);
+        final var result = DefinitionGenerator.getSchema(module, CONTEXT, new DefinitionNames(), true, MAPPER);
+        assertNotNull(result);
     }
 }
