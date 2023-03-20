220f9a1da40050129e2bda8d898f803c88fd5874
Peter Suna <peter.suna@pantheon.tech>
2023-03-20 08:38:30 +0100

    Add missing required elements to JSON ApiDoc
    This commit adds missing mandatory nodes to the required
    field of the JSON OpenApi data. According to RFC-7950, a node
    is mandatory if it is:
    A leaf, choice, anydata, or anyxml node with a "mandatory"
             statement with the value "true".
    https://www.rfc-editor.org/rfc/rfc7950#page-14
    
    The only exception for this patch is the 'choice' node.
    If the 'choice' node is mandatory, we cannot require the
    'choice' or 'case' name in the body.
    Additionally, nodes inside the 'case' should only be required
    if they are mandatory.
    
    JIRA: NETCONF-976
    Change-Id: I3d1220737a582b27163a9ff8f33ceee1edb94c87
    Signed-off-by: Peter Suna <peter.suna@pantheon.tech>

--- restconf/restconf-openapi/src/test/java/org/opendaylight/restconf/openapi/impl/OpenApiGeneratorRFC8040Test.java
+++ restconf/restconf-openapi/src/test/java/org/opendaylight/restconf/openapi/impl/OpenApiGeneratorRFC8040Test.java
@@ -26,6 +26,7 @@ import org.opendaylight.yangtools.yang.common.Revision;
 
 public final class OpenApiGeneratorRFC8040Test extends AbstractOpenApiTest {
     private static final String NAME = "toaster2";
+    private static final String MANDATORY_TEST = "mandatory-test";
     private static final String REVISION_DATE = "2009-11-20";
     private static final String NAME_2 = "toaster";
     private static final String REVISION_DATE_2 = "2009-11-20";
@@ -173,6 +174,31 @@ public final class OpenApiGeneratorRFC8040Test extends AbstractOpenApiTest {
         assertFalse(secondContainer.getProperties().has("leaf-second-case"));
     }
 
+    @Test
+    public void testMandatory() {
+        final var module = CONTEXT.findModule(MANDATORY_TEST).orElseThrow();
+        final var doc = generator.getOpenApiSpec(module, "http", "localhost:8181", "/", "", CONTEXT);
+        assertNotNull(doc);
+        final var definitions = doc.getComponents().getSchemas();
+        //TODO: missing mandatory-container, mandatory-list
+        final var reqRootContainer = "[\"mandatory-root-leaf\",\"mandatory-first-choice\"]";
+        verifyRequiredField(definitions.get("mandatory-test_config_root-container"), reqRootContainer);
+        verifyRequiredField(definitions.get("mandatory-test_root-container"), reqRootContainer);
+
+        //TODO: missing leaf-list-with-min-elemnets
+        final var reqMandatoryContainer = "[\"mandatory-leaf\"]";
+        verifyRequiredField(definitions.get("mandatory-test_root-container_config_mandatory-container"),
+                reqMandatoryContainer);
+        verifyRequiredField(definitions.get("mandatory-test_root-container_mandatory-container"),
+                reqMandatoryContainer);
+
+        final var reqMandatoryList = "[\"mandatory-list-field\"]";
+        verifyRequiredField(definitions.get("mandatory-test_root-container_config_mandatory-list"), reqMandatoryList);
+        verifyRequiredField(definitions.get("mandatory-test_root-container_mandatory-list"), reqMandatoryList);
+
+        //TODO: missing required field inside "mandatory-test_module" with ["root-container","root-mandatory-list"]
+    }
+
     /**
      * Test that checks for correct amount of parameters in requests.
      */
@@ -216,5 +242,13 @@ public final class OpenApiGeneratorRFC8040Test extends AbstractOpenApiTest {
             assertFalse(patch.isMissingNode());
             assertEquals(expectedSize, patch.get("parameters").size());
         }
+    }
+
+    private static void verifyRequiredField(final Schema rootContainer, final String expected) {
+        assertNotNull(rootContainer);
+        final var requiredNode = rootContainer.getRequired();
+        assertNotNull(requiredNode);
+        assertTrue(requiredNode.isArray());
+        assertEquals(expected, requiredNode.toString());
     }
 }
