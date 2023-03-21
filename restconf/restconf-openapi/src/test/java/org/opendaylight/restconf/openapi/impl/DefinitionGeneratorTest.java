aca80000839cbaf76f5021b08c5e29e30ad08056
Yaroslav Lastivka <yaroslav.lastivka@pantheon.tech>
2023-03-21 14:39:19 +0200

    Remove "Generex" dependency
    Removed "Generex" and replaced with direct usage of "Automaton"
    to generate random strings as "Genegex" seems to be no longer
    actively developed project.
    
    In fact the previous logic to generate example strings was broken.
    We have fixed it by using RegularExpressionString instead of
    JavaPatternString.
    
    Now we have the same examples generated every run which helps us
    to write better tests with assertions.
    
    JIRA: NETCONF-980
    Change-Id: I52e6d9d1a0f0cf43eb17b99309d9101c32d75a42
    Signed-off-by: Yaroslav Lastivka <yaroslav.lastivka@pantheon.tech>
    Signed-off-by: Ivan Hrasko <ivan.hrasko@pantheon.tech>

--- restconf/restconf-openapi/src/test/java/org/opendaylight/restconf/openapi/impl/DefinitionGeneratorTest.java
+++ restconf/restconf-openapi/src/test/java/org/opendaylight/restconf/openapi/impl/DefinitionGeneratorTest.java
@@ -7,6 +7,7 @@
  */
 package org.opendaylight.restconf.openapi.impl;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 import java.io.IOException;
@@ -37,5 +38,18 @@ public final class DefinitionGeneratorTest extends AbstractOpenApiTest {
         final DefinitionGenerator generator = new DefinitionGenerator();
         final var schemas = generator.convertToSchemas(module, CONTEXT, new DefinitionNames(), true);
         assertNotNull(schemas);
+    }
+
+    @Test
+    public void testStringFromRegex() throws IOException {
+        final var module = CONTEXT.findModule("strings-from-regex").orElseThrow();
+        final var generator = new DefinitionGenerator();
+        final var jsonObject = generator.convertToSchemas(module, CONTEXT, new DefinitionNames(), true);
+        assertNotNull(jsonObject);
+
+        var properties = jsonObject.get("strings-from-regex_test").getProperties();
+        assertEquals("00:00:00:00:00:00", properties.get("mac-address").get("default").asText());
+        assertEquals("0000-00-00T00:00:00Z", properties.get("login-date-time").get("default").asText());
+        assertEquals("0.0.0.0", properties.get("ipv4-address").get("default").asText());
     }
 }
