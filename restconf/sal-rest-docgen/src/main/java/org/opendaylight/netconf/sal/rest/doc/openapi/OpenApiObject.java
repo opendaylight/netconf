a0b9daa2b9aaf25f7a1532bb5e5a085ce7b0c83d
OleksandrZharov <Oleksandr.Zharov@pantheon.tech>
2023-03-23 13:45:50 +0100

    Eliminate CommonApiObject
    Eliminated CommonApiObject.
    Enhanced OpenApiObject with some fields from SwaggerObject to
    replace last one with OpenApiObject.
    
    JIRA: NETCONF-984
    Change-Id: Ia641f0183b599a2a1b98f45a6ce8a1bdf57ed174
    Signed-off-by: OleksandrZharov <Oleksandr.Zharov@pantheon.tech>
    Signed-off-by: Yaroslav Lastivka <yaroslav.lastivka@pantheon.tech>

--- restconf/sal-rest-docgen/src/main/java/org/opendaylight/netconf/sal/rest/doc/openapi/OpenApiObject.java
+++ restconf/sal-rest-docgen/src/main/java/org/opendaylight/netconf/sal/rest/doc/openapi/OpenApiObject.java
@@ -11,13 +11,22 @@ import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import java.util.List;
 
-public class OpenApiObject implements CommonApiObject {
+public class OpenApiObject {
     private String openapi;
     private Info info;
     private List<Server> servers;
     private ObjectNode paths;
     private Components components;
     private ArrayNode security;
+    private ObjectNode definitions;
+
+    public ObjectNode getDefinitions() {
+        return definitions;
+    }
+
+    public void setDefinitions(ObjectNode definitions) {
+        this.definitions = definitions;
+    }
 
     public String getOpenapi() {
         return openapi;
