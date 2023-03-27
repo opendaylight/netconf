10901e66783da9e214d73326925eb50e209dd005
Peter Suna <peter.suna@pantheon.tech>
2023-03-27 17:58:48 +0200

    Create only config data for OpenApi
    There is no need to create duplicate JSON nodes for operational
    data just to provide an example for the Get response.
    Instead, we can use an example from the configuration memory,
    which is sufficient for demonstration purposes.
    Therefore, creating operational example data should be removed.
    
    Change-Id: I894e6c2860ce622fb5258ea3518c19c853adac39
    Signed-off-by: Peter Suna <peter.suna@pantheon.tech>

--- restconf/sal-rest-docgen/src/main/java/org/opendaylight/netconf/sal/rest/doc/impl/BaseYangSwaggerGenerator.java
+++ restconf/sal-rest-docgen/src/main/java/org/opendaylight/netconf/sal/rest/doc/impl/BaseYangSwaggerGenerator.java
@@ -7,6 +7,7 @@
  */
 package org.opendaylight.netconf.sal.rest.doc.impl;
 
+import static org.opendaylight.netconf.sal.rest.doc.model.builder.OperationBuilder.CONFIG;
 import static org.opendaylight.netconf.sal.rest.doc.model.builder.OperationBuilder.TOP;
 import static org.opendaylight.netconf.sal.rest.doc.model.builder.OperationBuilder.buildDelete;
 import static org.opendaylight.netconf.sal.rest.doc.model.builder.OperationBuilder.buildGet;
@@ -422,7 +423,7 @@ public abstract class BaseYangSwaggerGenerator {
 
         final String nodeName = node.getQName().getLocalName();
 
-        final String defName = parentName + "_" + nodeName + TOP + discriminator;
+        final String defName = parentName + CONFIG + "_" + nodeName + TOP + discriminator;
         final ObjectNode get = buildGet(node, moduleName, deviceName, pathParams, defName, isConfig, oaversion);
         operations.put("get", get);
 
