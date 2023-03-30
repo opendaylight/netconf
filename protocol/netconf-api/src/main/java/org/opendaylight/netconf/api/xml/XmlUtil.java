be9d70bf02907ebf12b5203d1255cdcdc96f825f
zw1127 <zw1016@gmail.com>
2023-03-30 17:19:55 +0800

    Remove empty XML lines
    Java 9+ has changed the way indentation works, which means that unlike
    Java 8, we are emitting empty lines, quite similar to what has been
    reported in https://bugs.openjdk.org/browse/JDK-8262285.
    
    Use an XSL style sheet to ensure we filter out empty Text nodes during
    indenting transform opereations.
    
    JIRA: NETCONF-988
    Change-Id: Ia9e614d2c77766126923b9e1af82bb177dfb0c05
    Signed-off-by: zw1127 <zw1016@gmail.com>
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- protocol/netconf-api/src/main/java/org/opendaylight/netconf/api/xml/XmlUtil.java
+++ protocol/netconf-api/src/main/java/org/opendaylight/netconf/api/xml/XmlUtil.java
@@ -10,6 +10,7 @@ package org.opendaylight.netconf.api.xml;
 import static javax.xml.XMLConstants.XMLNS_ATTRIBUTE;
 import static javax.xml.XMLConstants.XMLNS_ATTRIBUTE_NS_URI;
 
+import com.google.common.io.Resources;
 import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileInputStream;
@@ -25,10 +26,11 @@ import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.OutputKeys;
 import javax.xml.transform.Source;
+import javax.xml.transform.Templates;
 import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
 import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.TransformerFactoryConfigurationError;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
 import javax.xml.transform.stream.StreamSource;
@@ -42,10 +44,23 @@ import org.w3c.dom.Node;
 import org.xml.sax.SAXException;
 
 public final class XmlUtil {
+    /**
+     * A pre-compiled XSL template to deal with Java XML transform creating empty lines when indenting is enabled, as
+     * detailed in <a href="https://bugs.openjdk.org/browse/JDK-8262285">JDK-8262285</a>.
+     */
+    public static final Templates PRETTY_PRINT_TEMPLATE;
 
-    private static final DocumentBuilderFactory BUILDER_FACTORY;
-    private static final TransformerFactory TRANSFORMER_FACTORY = TransformerFactory.newInstance();
+    static {
+        try {
+            PRETTY_PRINT_TEMPLATE = TransformerFactory.newInstance()
+                .newTemplates(new StreamSource(Resources.getResource(XmlUtil.class, "/pretty-print.xsl").openStream()));
+        } catch (IOException | TransformerConfigurationException e) {
+            throw new ExceptionInInitializerError(e);
+        }
+    }
+
     private static final SchemaFactory SCHEMA_FACTORY = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+    private static final DocumentBuilderFactory BUILDER_FACTORY;
 
     static {
         DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
@@ -181,11 +196,11 @@ public final class XmlUtil {
         final StringWriter writer = new StringWriter();
 
         try {
-            Transformer transformer = TRANSFORMER_FACTORY.newTransformer();
+            Transformer transformer = PRETTY_PRINT_TEMPLATE.newTransformer();
             transformer.setOutputProperty(OutputKeys.INDENT, "yes");
             transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, addXmlDeclaration ? "no" : "yes");
             transformer.transform(new DOMSource(xml), new StreamResult(writer));
-        } catch (TransformerFactoryConfigurationError | TransformerException e) {
+        } catch (TransformerException e) {
             throw new IllegalStateException("Unable to serialize xml element " + xml, e);
         }
 
