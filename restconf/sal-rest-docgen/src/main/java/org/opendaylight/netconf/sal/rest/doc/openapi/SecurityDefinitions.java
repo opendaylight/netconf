3b6957e76e381893ebb8493c351b993f7f0c224d
OleksandrZharov <Oleksandr.Zharov@pantheon.tech>
2023-04-25 10:33:29 +0200

    Move swagger classes into openapi package
    Changed package name for swagger files in prepare
    for next patches where swagger will be removed.
    
    JIRA: NETCONF-984
    Change-Id: Ie992d707875103952244fadaeac95ef4c21e9da7
    Signed-off-by: OleksandrZharov <Oleksandr.Zharov@pantheon.tech>

--- restconf/sal-rest-docgen/src/main/java/org/opendaylight/netconf/sal/rest/doc/openapi/SecurityDefinitions.java
+++ restconf/sal-rest-docgen/src/main/java/org/opendaylight/netconf/sal/rest/doc/openapi/SecurityDefinitions.java
@@ -5,14 +5,14 @@
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
-package org.opendaylight.netconf.sal.rest.doc.swagger;
+package org.opendaylight.netconf.sal.rest.doc.openapi;
 
 import com.fasterxml.jackson.databind.node.ObjectNode;
 
-public class SecuritySchemes {
+public class SecurityDefinitions {
     private ObjectNode basicAuth;
 
-    public SecuritySchemes(ObjectNode basicAuth) {
+    public SecurityDefinitions(ObjectNode basicAuth) {
         this.basicAuth = basicAuth;
     }
 
