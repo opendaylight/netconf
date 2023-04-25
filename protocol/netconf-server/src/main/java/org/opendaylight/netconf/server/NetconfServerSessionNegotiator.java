4265e31fe50a7b0e0f0e45cd72f6913b5f33aced
Robert Varga <robert.varga@pantheon.tech>
2023-04-25 11:37:06 +0200

    Rename netconf-impl to netconf-server
    This artifact contains only server-side things. Make sure to rename it
    to reflect that.
    
    JIRA: NETCONF-945
    Change-Id: I13322553c4bdd8979fe564ce1337dfb9e43c7e7a
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ protocol/netconf-server/src/main/java/org/opendaylight/netconf/server/NetconfServerSessionNegotiator.java
@@ -0,0 +1,81 @@
+/*
+ * Copyright (c) 2013 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.netconf.server;
+
+import io.netty.channel.Channel;
+import io.netty.channel.local.LocalAddress;
+import io.netty.util.Timer;
+import io.netty.util.concurrent.Promise;
+import java.net.InetSocketAddress;
+import java.net.SocketAddress;
+import java.util.AbstractMap.SimpleImmutableEntry;
+import java.util.Map;
+import org.checkerframework.checker.index.qual.NonNegative;
+import org.opendaylight.netconf.api.NetconfDocumentedException;
+import org.opendaylight.netconf.api.messages.NetconfHelloMessage;
+import org.opendaylight.netconf.api.messages.NetconfHelloMessageAdditionalHeader;
+import org.opendaylight.netconf.nettyutil.AbstractNetconfSessionNegotiator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public final class NetconfServerSessionNegotiator
+        extends AbstractNetconfSessionNegotiator<NetconfServerSession, NetconfServerSessionListener> {
+    private static final Logger LOG = LoggerFactory.getLogger(NetconfServerSessionNegotiator.class);
+    private static final String UNKNOWN = "unknown";
+
+    private final long sessionId;
+
+    NetconfServerSessionNegotiator(final NetconfHelloMessage hello, final long sessionId,
+            final Promise<NetconfServerSession> promise, final Channel channel, final Timer timer,
+            final NetconfServerSessionListener sessionListener, final long connectionTimeoutMillis,
+            final @NonNegative int maximumIncomingChunkSize) {
+        super(hello, promise, channel, timer, sessionListener, connectionTimeoutMillis,
+            maximumIncomingChunkSize);
+        this.sessionId = sessionId;
+    }
+
+    @Override
+    protected void handleMessage(final NetconfHelloMessage netconfMessage) throws NetconfDocumentedException {
+        NetconfServerSession session = getSessionForHelloMessage(netconfMessage);
+        replaceHelloMessageInboundHandler(session);
+        // Negotiation successful after all non hello messages were processed
+        negotiationSuccessful(session);
+    }
+
+    @Override
+    protected NetconfServerSession getSession(final NetconfServerSessionListener sessionListener, final Channel channel,
+            final NetconfHelloMessage message) {
+        final var additionalHeader = message.getAdditionalHeader();
+        final var parsedHeader = additionalHeader.orElseGet(() -> {
+            final var hostName = getHostName(channel.localAddress());
+            return new NetconfHelloMessageAdditionalHeader(UNKNOWN, hostName.getValue(), hostName.getKey(), "tcp",
+                "client");
+        });
+
+        LOG.debug("Additional header from hello parsed as {} from {}", parsedHeader, additionalHeader);
+        return new NetconfServerSession(sessionListener, channel, sessionId, parsedHeader);
+    }
+
+    /**
+     * Get a name of the host.
+     *
+     * @param socketAddress type of socket address LocalAddress, or
+     *                      InetSocketAddress, for others returns unknown
+     * @return Two values - port and host of socket address
+     */
+    protected static Map.Entry<String, String> getHostName(final SocketAddress socketAddress) {
+        if (socketAddress instanceof InetSocketAddress inetSocketAddress) {
+            return new SimpleImmutableEntry<>(Integer.toString(inetSocketAddress.getPort()),
+                    inetSocketAddress.getHostString());
+        } else if (socketAddress instanceof LocalAddress localAddress) {
+            return new SimpleImmutableEntry<>(UNKNOWN, localAddress.id());
+        } else {
+            return new SimpleImmutableEntry<>(UNKNOWN, UNKNOWN);
+        }
+    }
+}
---
+++ protocol/netconf-server/src/main/java/org/opendaylight/netconf/server/util/DeserializerExceptionHandler.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2013 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.netconf.server.util;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import java.util.Map;
+import org.opendaylight.netconf.api.DocumentedException;
+import org.opendaylight.netconf.server.SendErrorExceptionUtil;
+import org.opendaylight.yangtools.yang.common.ErrorSeverity;
+import org.opendaylight.yangtools.yang.common.ErrorTag;
+import org.opendaylight.yangtools.yang.common.ErrorType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public final class DeserializerExceptionHandler extends ChannelInboundHandlerAdapter {
+    private static final Logger LOG = LoggerFactory.getLogger(DeserializerExceptionHandler.class);
+
+    @Override
+    public void exceptionCaught(final ChannelHandlerContext ctx, final Throwable cause) {
+        LOG.warn("An exception occurred during message handling", cause);
+        handleDeserializerException(ctx, cause);
+    }
+
+    private static void handleDeserializerException(final ChannelHandlerContext ctx, final Throwable cause) {
+        final String message = cause.getMessage();
+        SendErrorExceptionUtil.sendErrorMessage(ctx.channel(), new DocumentedException(message,
+            ErrorType.RPC, ErrorTag.MALFORMED_MESSAGE, ErrorSeverity.ERROR,
+            message != null ? Map.of("cause", message) : Map.of()));
+    }
+}
