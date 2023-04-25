8c1a4caabb4d1978e28d2c7bfd43943e22576334
Robert Varga <robert.varga@pantheon.tech>
2023-04-25 11:37:06 +0200

    Rename netconf-impl to netconf-server
    This artifact contains only server-side things. Make sure to rename it
    to reflect that.
    
    JIRA: NETCONF-945
    Change-Id: I13322553c4bdd8979fe564ce1337dfb9e43c7e7a
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ protocol/netconf-server/src/test/java/org/opendaylight/netconf/server/NetconfDispatcherImplTest.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright (c) 2013 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.netconf.server;
+
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.util.HashedWheelTimer;
+import java.net.InetSocketAddress;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.opendaylight.netconf.server.osgi.AggregatedNetconfOperationServiceFactory;
+
+public class NetconfDispatcherImplTest {
+    private EventLoopGroup nettyGroup;
+    private NetconfServerDispatcherImpl dispatch;
+    private HashedWheelTimer hashedWheelTimer;
+
+    @Before
+    public void setUp() throws Exception {
+        nettyGroup = new NioEventLoopGroup();
+
+        AggregatedNetconfOperationServiceFactory factoriesListener = new AggregatedNetconfOperationServiceFactory();
+
+        SessionIdProvider idProvider = new DefaultSessionIdProvider();
+        hashedWheelTimer = new HashedWheelTimer();
+
+        NetconfServerSessionNegotiatorFactory serverNegotiatorFactory =
+                new NetconfServerSessionNegotiatorFactoryBuilder()
+                        .setAggregatedOpService(factoriesListener)
+                        .setTimer(hashedWheelTimer)
+                        .setIdProvider(idProvider)
+                        .setMonitoringService(ConcurrentClientsTest.createMockedMonitoringService())
+                        .setConnectionTimeoutMillis(5000)
+                        .build();
+
+        ServerChannelInitializer serverChannelInitializer =
+                new ServerChannelInitializer(serverNegotiatorFactory);
+
+        dispatch = new NetconfServerDispatcherImpl(
+                serverChannelInitializer, nettyGroup, nettyGroup);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        hashedWheelTimer.stop();
+        nettyGroup.shutdownGracefully();
+    }
+
+    @Test
+    public void test() throws Exception {
+        InetSocketAddress addr = new InetSocketAddress("127.0.0.1", 8333);
+        ChannelFuture server = dispatch.createServer(addr);
+        server.get();
+    }
+}
---
+++ protocol/netconf-server/src/test/java/org/opendaylight/netconf/server/SendErrorExceptionUtilTest.java
@@ -0,0 +1,88 @@
+/*
+ * Copyright (c) 2014 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.netconf.server;
+
+import static java.util.Objects.requireNonNull;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelFuture;
+import io.netty.util.concurrent.GenericFutureListener;
+import java.io.IOException;
+import java.io.InputStream;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.opendaylight.netconf.api.DocumentedException;
+import org.opendaylight.netconf.api.NetconfMessage;
+import org.opendaylight.netconf.api.NetconfSession;
+import org.opendaylight.netconf.api.xml.XmlUtil;
+import org.w3c.dom.Element;
+import org.xml.sax.SAXException;
+
+@RunWith(MockitoJUnitRunner.StrictStubs.class)
+public class SendErrorExceptionUtilTest {
+    private final DocumentedException exception = new DocumentedException("err");
+
+    @Mock
+    NetconfSession netconfSession;
+    @Mock
+    ChannelFuture channelFuture;
+    @Mock
+    Channel channel;
+
+    @Before
+    public void setUp() throws Exception {
+        doReturn(channelFuture).when(netconfSession).sendMessage(any(NetconfMessage.class));
+        doReturn(channelFuture).when(channelFuture).addListener(any(GenericFutureListener.class));
+        doReturn(channelFuture).when(channel).writeAndFlush(any(NetconfMessage.class));
+    }
+
+    @Test
+    public void testSendErrorMessage1() throws Exception {
+        SendErrorExceptionUtil.sendErrorMessage(netconfSession, exception);
+        verify(channelFuture).addListener(any(GenericFutureListener.class));
+        verify(netconfSession).sendMessage(any(NetconfMessage.class));
+    }
+
+    @Test
+    public void testSendErrorMessage2() throws Exception {
+        SendErrorExceptionUtil.sendErrorMessage(channel, exception);
+        verify(channelFuture).addListener(any(GenericFutureListener.class));
+    }
+
+    @Test
+    public void testSendErrorMessage3() throws Exception {
+        SendErrorExceptionUtil.sendErrorMessage(netconfSession, exception, readMessage("rpc.xml"));
+        verify(channelFuture).addListener(any(GenericFutureListener.class));
+    }
+
+    @Test
+    public void testSendErrorMessage4() throws Exception {
+        SendErrorExceptionUtil.sendErrorMessage(netconfSession, exception, readMessage("rpc_ns.xml"));
+        final ArgumentCaptor<NetconfMessage> messageCaptor = ArgumentCaptor.forClass(NetconfMessage.class);
+        verify(netconfSession, times(1)).sendMessage(messageCaptor.capture());
+        final Element rpcReply = messageCaptor.getValue().getDocument().getDocumentElement();
+        assertEquals("Invalid value of message-id attribute in the reply message", "a",
+            rpcReply.getAttribute("message-id"));
+    }
+
+    private static NetconfMessage readMessage(final String name) throws IOException, SAXException {
+        try (InputStream resource =
+                requireNonNull(SendErrorExceptionUtilTest.class.getResourceAsStream("/messages/" + name))) {
+            return new NetconfMessage(XmlUtil.readXmlToDocument(resource));
+        }
+    }
+}
