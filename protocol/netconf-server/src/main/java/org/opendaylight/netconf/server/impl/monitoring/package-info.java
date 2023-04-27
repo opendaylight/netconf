0c0f56cdc85012ed6ef862b098be99a02f85be61
Robert Varga <robert.varga@pantheon.tech>
2023-04-27 19:20:11 +0200

    Rehost netconf-monitoring operations
    The individual operations are not dependent on anything MD-SAL, rehost
    them into the baseline netconf-server. The only remaining thing in
    mdsal-netconf-monitoring is MonitoringToMdsalWriter, whose final
    destination is still to be determined.
    
    This is an intermediate step, as we are still looking for the set of
    services provided at the plugins/ level, so a further movement is
    likely.
    
    JIRA: NETCONF-945
    Change-Id: Ib77bea74ff242b22a21cbb9f0f0b8cb7990dee5a
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ protocol/netconf-server/src/main/java/org/opendaylight/netconf/server/impl/monitoring/package-info.java
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2023 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+/**
+ * Default implementation of <a href="https://www.rfc-editor.org/rfc/rfc6022">RFC6020 NETCONF Monitoring</a> operations.
+ */
+package org.opendaylight.netconf.server.impl.monitoring;
