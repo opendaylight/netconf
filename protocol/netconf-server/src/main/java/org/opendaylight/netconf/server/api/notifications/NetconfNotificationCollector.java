74182efbfc257334a48fee24d5e647073d5030e5
Robert Varga <robert.varga@pantheon.tech>
2023-04-27 13:05:22 +0200

    Integrate netconf-notifications-api into netconf-server
    netconf-notifications-api is decidedly server-side, integrate it into
    netconf-server.
    
    JIRA: NETCONF-945
    Change-Id: I8c901429e054d4f3f29943118ec608293e24e5b3
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- protocol/netconf-server/src/main/java/org/opendaylight/netconf/server/api/notifications/NetconfNotificationCollector.java
+++ protocol/netconf-server/src/main/java/org/opendaylight/netconf/server/api/notifications/NetconfNotificationCollector.java
@@ -5,7 +5,7 @@
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
-package org.opendaylight.netconf.notifications;
+package org.opendaylight.netconf.server.api.notifications;
 
 import org.eclipse.jdt.annotation.NonNull;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.netconf.notification._1._0.rev080714.StreamNameType;
