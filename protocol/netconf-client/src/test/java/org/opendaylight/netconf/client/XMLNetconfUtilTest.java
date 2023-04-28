f8ed8e011f559412318cc29313ab16f11323d22a
Robert Varga <robert.varga@pantheon.tech>
2023-04-28 16:48:17 +0200

    Move netconf.util.xml to netconf-client
    This package is only used internally by netconf-client, move it to its
    sole user and clean up HardcodedNamespaceResolver to use Map.of().
    
    Change-Id: I219c849362fb4b02297fd8fa243a0ee171f00f14
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- protocol/netconf-client/src/test/java/org/opendaylight/netconf/client/XMLNetconfUtilTest.java
+++ protocol/netconf-client/src/test/java/org/opendaylight/netconf/client/XMLNetconfUtilTest.java
@@ -5,7 +5,7 @@
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
-package org.opendaylight.netconf.util.xml;
+package org.opendaylight.netconf.client;
 
 import static org.hamcrest.CoreMatchers.startsWith;
 import static org.hamcrest.MatcherAssert.assertThat;
