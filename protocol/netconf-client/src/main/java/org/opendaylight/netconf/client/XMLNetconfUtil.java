199e6eb04d307460419abe0a0e7e898002a89ddc
Robert Varga <robert.varga@pantheon.tech>
2023-04-28 16:48:17 +0200

    Move netconf.util.xml to netconf-client
    This package is only used internally by netconf-client, move it to its
    sole user and clean up HardcodedNamespaceResolver to use Map.of().
    
    Change-Id: I219c849362fb4b02297fd8fa243a0ee171f00f14
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- protocol/netconf-client/src/main/java/org/opendaylight/netconf/client/XMLNetconfUtil.java
+++ protocol/netconf-client/src/main/java/org/opendaylight/netconf/client/XMLNetconfUtil.java
@@ -5,7 +5,7 @@
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
-package org.opendaylight.netconf.util.xml;
+package org.opendaylight.netconf.client;
 
 import javax.xml.namespace.NamespaceContext;
 import javax.xml.xpath.XPath;
@@ -14,7 +14,7 @@ import javax.xml.xpath.XPathExpressionException;
 import javax.xml.xpath.XPathFactory;
 import org.opendaylight.netconf.api.xml.XmlNetconfConstants;
 
-public final class XMLNetconfUtil {
+final class XMLNetconfUtil {
     private static final XPathFactory FACTORY = XPathFactory.newInstance();
     private static final NamespaceContext NS_CONTEXT = new HardcodedNamespaceResolver("netconf",
         XmlNetconfConstants.URN_IETF_PARAMS_XML_NS_NETCONF_BASE_1_0);
@@ -23,7 +23,7 @@ public final class XMLNetconfUtil {
         // Hidden on purpose
     }
 
-    public static XPathExpression compileXPath(final String xpath) {
+    static XPathExpression compileXPath(final String xpath) {
         final XPath newXPath = FACTORY.newXPath();
         newXPath.setNamespaceContext(NS_CONTEXT);
         try {
@@ -32,5 +32,4 @@ public final class XMLNetconfUtil {
             throw new IllegalStateException("Error while compiling xpath expression " + xpath, e);
         }
     }
-
 }
