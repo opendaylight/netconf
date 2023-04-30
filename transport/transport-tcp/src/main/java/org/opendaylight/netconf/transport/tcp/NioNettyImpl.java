54e0874b8906de7273c6569d4efd85dce6a6403c
Robert Varga <robert.varga@pantheon.tech>
2023-05-01 00:26:01 +0200

    Workaround Karaf not exposing jdk.net
    Karaf is not exposing jdk.net, hence we cannot rely on ExtendedOptions
    being available.
    
    JIRA: NETCONF-590
    Change-Id: Ibcbe23a1ae789955b880814c1466e57ea0fb0bb9
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- transport/transport-tcp/src/main/java/org/opendaylight/netconf/transport/tcp/NioNettyImpl.java
+++ transport/transport-tcp/src/main/java/org/opendaylight/netconf/transport/tcp/NioNettyImpl.java
@@ -12,6 +12,7 @@ import io.netty.bootstrap.ServerBootstrap;
 import io.netty.channel.ChannelOption;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannelConfig;
 import io.netty.channel.socket.nio.NioChannelOption;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
@@ -20,21 +21,89 @@ import java.util.concurrent.ThreadFactory;
 import jdk.net.ExtendedSocketOptions;
 import org.eclipse.jdt.annotation.NonNullByDefault;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.tcp.common.rev221212.tcp.common.grouping.Keepalives;
+import org.slf4j.LoggerFactory;
 
 @NonNullByDefault
 final class NioNettyImpl extends AbstractNettyImpl {
-    private static final ChannelOption<Integer> TCP_KEEPIDLE = NioChannelOption.of(ExtendedSocketOptions.TCP_KEEPIDLE);
-    private static final ChannelOption<Integer> TCP_KEEPCNT = NioChannelOption.of(ExtendedSocketOptions.TCP_KEEPCOUNT);
-    private static final ChannelOption<Integer> TCP_KEEPINTVL =
-        NioChannelOption.of(ExtendedSocketOptions.TCP_KEEPINTERVAL);
+    // FIXME: all of this is a workaround for https://issues.apache.org/jira/browse/KARAF-7690
+    private abstract static class AbstractSupport {
+
+        abstract boolean configureKeepalives(SocketChannelConfig config);
+
+        abstract void configureKeepalives(ServerBootstrap bootstrap, Keepalives keepalives);
+
+        abstract void configureKeepalives(Bootstrap bootstrap, Keepalives keepalives);
+    }
+
+    private static final class Supported extends AbstractSupport {
+        private static final ChannelOption<Integer> TCP_KEEPIDLE =
+            NioChannelOption.of(ExtendedSocketOptions.TCP_KEEPIDLE);
+        private static final ChannelOption<Integer> TCP_KEEPCNT =
+            NioChannelOption.of(ExtendedSocketOptions.TCP_KEEPCOUNT);
+        private static final ChannelOption<Integer> TCP_KEEPINTVL =
+            NioChannelOption.of(ExtendedSocketOptions.TCP_KEEPINTERVAL);
+
+        @Override
+        boolean configureKeepalives(final SocketChannelConfig config) {
+            return config.setOptions(Map.of(
+                ChannelOption.SO_KEEPALIVE, Boolean.TRUE, TCP_KEEPIDLE, 7200, TCP_KEEPCNT, 3, TCP_KEEPINTVL, 5));
+        }
+
+        @Override
+        void configureKeepalives(final ServerBootstrap bootstrap, final Keepalives keepalives) {
+            bootstrap
+                .childOption(TCP_KEEPIDLE, keepalives.requireIdleTime().toJava())
+                .childOption(TCP_KEEPCNT, keepalives.requireMaxProbes().toJava())
+                .childOption(TCP_KEEPINTVL, keepalives.requireProbeInterval().toJava());
+        }
+
+        @Override
+        void configureKeepalives(final Bootstrap bootstrap, final Keepalives keepalives) {
+            bootstrap
+                .option(TCP_KEEPIDLE, keepalives.requireIdleTime().toJava())
+                .option(TCP_KEEPCNT, keepalives.requireMaxProbes().toJava())
+                .option(TCP_KEEPINTVL, keepalives.requireProbeInterval().toJava());
+        }
+    }
+
+    private static final class Unsupported extends AbstractSupport {
+        @Override
+        boolean configureKeepalives(final SocketChannelConfig config) {
+            return false;
+        }
+
+        @Override
+        void configureKeepalives(final ServerBootstrap bootstrap, final Keepalives keepalives) {
+            // Noop
+        }
+
+        @Override
+        void configureKeepalives(final Bootstrap bootstrap, final Keepalives keepalives) {
+            // Noop
+        }
+    }
+
+    private static final AbstractSupport SUPPORT;
+
+    static {
+        AbstractSupport support;
+
+        try {
+            support = new Supported();
+        } catch (NoClassDefFoundError e) {
+            LoggerFactory.getLogger(NioNettyImpl.class).warn("Disabling keepalive support", e);
+            support = new Unsupported();
+        }
+
+        SUPPORT = support;
+    }
 
     private final boolean supportsKeepalives;
 
     NioNettyImpl() {
         final var ch = new NioSocketChannel();
         try {
-            supportsKeepalives = ch.config().setOptions(Map.of(
-                ChannelOption.SO_KEEPALIVE, Boolean.TRUE, TCP_KEEPIDLE, 7200, TCP_KEEPCNT, 3, TCP_KEEPINTVL, 5));
+            supportsKeepalives = SUPPORT.configureKeepalives(ch.config());
         } finally {
             ch.close();
         }
@@ -62,20 +131,12 @@ final class NioNettyImpl extends AbstractNettyImpl {
 
     @Override
     void configureKeepalives(final Bootstrap bootstrap, final Keepalives keepalives) {
-        bootstrap
-            .option(ChannelOption.SO_KEEPALIVE, Boolean.TRUE)
-            .option(TCP_KEEPIDLE, keepalives.requireIdleTime().toJava())
-            .option(TCP_KEEPCNT, keepalives.requireMaxProbes().toJava())
-            .option(TCP_KEEPINTVL, keepalives.requireProbeInterval().toJava());
+        SUPPORT.configureKeepalives(bootstrap.option(ChannelOption.SO_KEEPALIVE, Boolean.TRUE), keepalives);
     }
 
     @Override
     void configureKeepalives(final ServerBootstrap bootstrap, final Keepalives keepalives) {
-        bootstrap
-            .childOption(ChannelOption.SO_KEEPALIVE, Boolean.TRUE)
-            .childOption(TCP_KEEPIDLE, keepalives.requireIdleTime().toJava())
-            .childOption(TCP_KEEPCNT, keepalives.requireMaxProbes().toJava())
-            .childOption(TCP_KEEPINTVL, keepalives.requireProbeInterval().toJava());
+        SUPPORT.configureKeepalives(bootstrap.childOption(ChannelOption.SO_KEEPALIVE, Boolean.TRUE), keepalives);
     }
 
     @Override
