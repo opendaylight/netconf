e3cab8f52c2efde5431933be4b6e2cc482b603e9
Robert Varga <robert.varga@pantheon.tech>
2023-04-30 21:29:31 +0200

    Split out DefaultPasswordCredentials
    We would really like a default implementation coming from the API, but
    this is really the best we can do for now.
    
    Change-Id: I0cf30a2e86da0efc6c480f0404cf59dd0c2c9cc0
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ netconf/aaa-authn-odl-plugin/src/main/java/org/opendaylight/netconf/authprovider/DefaultPasswordCredentials.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2023 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.netconf.authprovider;
+
+import org.opendaylight.aaa.api.PasswordCredentials;
+
+final class DefaultPasswordCredentials implements PasswordCredentials {
+    private final String username;
+    private final String password;
+
+    DefaultPasswordCredentials(final String username, final String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    @Override
+    public String username() {
+        return username;
+    }
+
+    @Override
+    public String password() {
+        return password;
+    }
+
+    @Override
+    public String domain() {
+        // If this is left null, default "sdn" domain is assumed
+        return null;
+    }
+}
