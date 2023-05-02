6ea0e9f1f244cfd718fd75dd9fe83ee147225966
Robert Varga <robert.varga@pantheon.tech>
2023-05-03 01:11:55 +0200

    Refactor NetconfRemoteSchemaYangSourceProvider
    This provider is based on ietf-netconf-monitoring. Split out the
    YangTextSchemaSource implementation to CachedYangTextSchemaSource
    and the the provider into netconf.client.mdsal, making it very clear
    as to what is going on.
    
    JIRA: NETCONF-1006
    Change-Id: Ieff749ce67c94fa125ce780dd8d1fa4dfd79622d
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- plugins/netconf-client-mdsal/src/main/java/org/opendaylight/netconf/sal/connect/netconf/schema/YangLibrarySchemaYangSourceProvider.java
+++ plugins/netconf-client-mdsal/src/main/java/org/opendaylight/netconf/sal/connect/netconf/schema/YangLibrarySchemaYangSourceProvider.java
@@ -14,11 +14,10 @@ import com.google.common.collect.ImmutableMap;
 import com.google.common.util.concurrent.Futures;
 import com.google.common.util.concurrent.ListenableFuture;
 import java.io.IOException;
-import java.io.InputStream;
 import java.net.URL;
 import java.util.Map;
+import org.opendaylight.netconf.client.mdsal.CachedYangTextSchemaSource;
 import org.opendaylight.netconf.client.mdsal.api.RemoteDeviceId;
-import org.opendaylight.netconf.sal.connect.netconf.schema.NetconfRemoteSchemaYangSourceProvider.NetconfYangTextSchemaSource;
 import org.opendaylight.yangtools.yang.model.repo.api.SchemaSourceException;
 import org.opendaylight.yangtools.yang.model.repo.api.SourceIdentifier;
 import org.opendaylight.yangtools.yang.model.repo.api.YangTextSchemaSource;
@@ -32,29 +31,32 @@ import org.slf4j.LoggerFactory;
 public final class YangLibrarySchemaYangSourceProvider implements SchemaSourceProvider<YangTextSchemaSource> {
     private static final Logger LOG = LoggerFactory.getLogger(YangLibrarySchemaYangSourceProvider.class);
 
-    private final Map<SourceIdentifier, URL> availableSources;
+    private final ImmutableMap<SourceIdentifier, URL> availableSources;
     private final RemoteDeviceId id;
 
     public YangLibrarySchemaYangSourceProvider(final RemoteDeviceId id,
             final Map<SourceIdentifier, URL> availableSources) {
-        this.id = id;
+        this.id = requireNonNull(id);
         this.availableSources = ImmutableMap.copyOf(availableSources);
     }
 
     @Override
     public ListenableFuture<? extends YangTextSchemaSource> getSource(final SourceIdentifier sourceIdentifier) {
-        final URL url = availableSources.get(requireNonNull(sourceIdentifier));
+        final var url = availableSources.get(requireNonNull(sourceIdentifier));
         checkArgument(url != null);
-        try (InputStream in = url.openStream()) {
-            final byte[] schemaContent = in.readAllBytes();
-            final NetconfYangTextSchemaSource yangSource = new NetconfYangTextSchemaSource(id, sourceIdentifier,
-                url.toString(), schemaContent);
-            LOG.debug("Source {} downloaded from a yang library's url {}", sourceIdentifier, url);
-            return Futures.immediateFuture(yangSource);
+
+        final byte[] schemaContent;
+        try (var in = url.openStream()) {
+            schemaContent = in.readAllBytes();
         } catch (IOException e) {
             LOG.warn("Unable to download source {} from a yang library's url {}", sourceIdentifier, url, e);
             return Futures.immediateFailedFuture(new SchemaSourceException(
                 "Unable to download remote schema for " + sourceIdentifier + " from " + url, e));
         }
+
+        final var yangSource = new CachedYangTextSchemaSource(id, sourceIdentifier,
+            url.toString(), schemaContent);
+        LOG.debug("Source {} downloaded from a yang library's url {}", sourceIdentifier, url);
+        return Futures.immediateFuture(yangSource);
     }
 }
