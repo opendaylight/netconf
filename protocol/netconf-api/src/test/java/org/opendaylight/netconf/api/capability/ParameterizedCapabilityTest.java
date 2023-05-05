559e3532a0604d5cdacfb3932ea993c56e333b02
Robert Varga <robert.varga@pantheon.tech>
2023-05-05 02:21:03 +0200

    Design a Capability object model
    Implemented YangModuleCapability and EXiCapability
    which extend ParameterizedCapability.
    These classes appear to represent YANG module capabilities
    and have attributes like content, revision, module name,
    features, deviations, etc.
    Added enumeration of SimpleCapability which implements
    the Capability interface, listing various NETCONF
    capabilities based on different RFCs and drafts.
    
    JIRA: NETCONF-1015
    Change-Id: I6c935dc4818e1968d656ce00aa08fb82f7650fc5
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>
    Signed-off-by: Yaroslav Lastivka <yaroslav.lastivka@pantheon.tech>

---
+++ protocol/netconf-api/src/test/java/org/opendaylight/netconf/api/capability/ParameterizedCapabilityTest.java
@@ -0,0 +1,85 @@
+/*
+ * Copyright (c) 2023 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.netconf.api.capability;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Stream;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+class ParameterizedCapabilityTest {
+
+    @ParameterizedTest
+    @MethodSource("provideYangModuleCapabilityParams")
+    void testYangModuleCapabilityUrn(final String namespace, final String module, final String revision,
+            final List<String> features, final List<String> deviations, final String expectedUrn) {
+        final var capability = new YangModuleCapability(namespace, module, revision, features, deviations);
+        assertEquals(expectedUrn, capability.urn());
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideExiCapabilityParams")
+    void testExiCapabilityUrn(final Integer compression, final ExiSchemas schema, final String expectedUrn) {
+        final var capability = new ExiCapability(compression, schema);
+        assertEquals(expectedUrn, capability.urn());
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideExiCapabilityParams")
+    void testExiCapabilityEquals(final Integer compression, final ExiSchemas schema) {
+        assertEquals(new ExiCapability(compression, schema), new ExiCapability(compression, schema));
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideYangModuleCapabilityParams")
+    void testYangModuleCapabilityEquals(final String namespace, final String module, final String revision,
+            final List<String> features, final List<String> deviations) {
+        assertEquals(new YangModuleCapability(namespace, module, revision, features, deviations),
+            new YangModuleCapability(namespace, module, revision, features, deviations));
+    }
+
+    static Stream<Arguments> provideExiCapabilityParams() {
+        return Stream.of(
+            Arguments.of(null, null, "urn:ietf:params:netconf:capability:exi:1.0"),
+            Arguments.of(1000, ExiSchemas.BUILTIN,
+                "urn:ietf:params:netconf:capability:exi:1.0?compression=1000&schemas=builtin"),
+            Arguments.of(1000, null, "urn:ietf:params:netconf:capability:exi:1.0?compression=1000"),
+            Arguments.of(null, ExiSchemas.BASE_1_1,
+                "urn:ietf:params:netconf:capability:exi:1.0?schemas=base:1.1"),
+            Arguments.of(null, ExiSchemas.DYNAMIC,
+                "urn:ietf:params:netconf:capability:exi:1.0?schemas=dynamic")
+        );
+    }
+
+    static Stream<Arguments> provideYangModuleCapabilityParams() {
+        return Stream.of(
+            Arguments.of("http://example.com", null, null, null, null, "http://example.com"),
+            Arguments.of("http://example.com", "module", "2023-08-21", List.of("feature1", "feature2"),
+                List.of("deviation1", "deviation2"), "http://example.com?module=module&revision=2023-08-21"
+                    + "&features=feature1,feature2&deviations=deviation1,deviation2"),
+            Arguments.of("http://example.com", null, "2023-08-21", null, null,
+                "http://example.com?revision=2023-08-21"),
+            Arguments.of("http://example.com", null, "2023-08-21", List.of("feature"), List.of("deviation"),
+                "http://example.com?revision=2023-08-21&features=feature&deviations=deviation"),
+            Arguments.of("http://example.com", "module", null, List.of("feature"), List.of("deviation"),
+                "http://example.com?module=module&features=feature&deviations=deviation"),
+            Arguments.of("http://example.com", "module", "2023-08-21", null, List.of("deviation"),
+                "http://example.com?module=module&revision=2023-08-21&deviations=deviation"),
+            Arguments.of("http://example.com", "module", "2023-08-21", List.of("feature"), null,
+                "http://example.com?module=module&revision=2023-08-21&features=feature"),
+            Arguments.of("http://example.com", "module", "2023-08-21", Collections.emptyList(), List.of("deviation"),
+                "http://example.com?module=module&revision=2023-08-21&deviations=deviation"),
+            Arguments.of("http://example.com", "module", "2023-08-21", List.of("feature"), Collections.emptyList(),
+                "http://example.com?module=module&revision=2023-08-21&features=feature")
+        );
+    }
+}
