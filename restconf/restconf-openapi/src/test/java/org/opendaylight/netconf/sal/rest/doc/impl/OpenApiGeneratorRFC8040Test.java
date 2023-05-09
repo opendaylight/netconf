dc4b90ed2bf5e980871f6666fdc35ddfbf286a26
OleksandrZharov <Oleksandr.Zharov@pantheon.tech>
2023-05-09 12:42:24 +0200

    Refactor org.opendaylight.netconf.sal.rest.doc.api
    Renamed package org.opendaylight.netconf.sal.rest.doc.api to
    org.opendaylight.restconf.openapi.api.
    
    JIRA: NETCONF-1011
    Change-Id: I681caea35a57e02f445713adf27084fb0c142305
    Signed-off-by: OleksandrZharov <Oleksandr.Zharov@pantheon.tech>
    Signed-off-by: Ivan Hrasko <ivan.hrasko@pantheon.tech>

--- restconf/restconf-openapi/src/test/java/org/opendaylight/netconf/sal/rest/doc/impl/OpenApiGeneratorRFC8040Test.java
+++ restconf/restconf-openapi/src/test/java/org/opendaylight/netconf/sal/rest/doc/impl/OpenApiGeneratorRFC8040Test.java
@@ -17,10 +17,10 @@ import com.fasterxml.jackson.databind.node.ObjectNode;
 import java.util.List;
 import java.util.Set;
 import org.junit.Test;
-import org.opendaylight.netconf.sal.rest.doc.AbstractOpenApiTest;
 import org.opendaylight.netconf.sal.rest.doc.DocGenTestHelper;
 import org.opendaylight.netconf.sal.rest.doc.openapi.OpenApiObject;
 import org.opendaylight.netconf.sal.rest.doc.openapi.Path;
+import org.opendaylight.restconf.openapi.api.AbstractOpenApiTest;
 import org.opendaylight.yangtools.yang.common.Revision;
 
 public final class OpenApiGeneratorRFC8040Test extends AbstractOpenApiTest {
--- restconf/restconf-openapi/src/test/java/org/opendaylight/netconf/sal/rest/doc/mountpoints/MountPointOpenApiTest.java
+++ restconf/restconf-openapi/src/test/java/org/opendaylight/netconf/sal/rest/doc/mountpoints/MountPointOpenApiTest.java
@@ -25,11 +25,11 @@ import org.junit.Test;
 import org.opendaylight.mdsal.dom.api.DOMMountPoint;
 import org.opendaylight.mdsal.dom.api.DOMMountPointService;
 import org.opendaylight.mdsal.dom.api.DOMSchemaService;
-import org.opendaylight.netconf.sal.rest.doc.AbstractOpenApiTest;
 import org.opendaylight.netconf.sal.rest.doc.DocGenTestHelper;
 import org.opendaylight.netconf.sal.rest.doc.impl.MountPointOpenApiGeneratorRFC8040;
 import org.opendaylight.netconf.sal.rest.doc.openapi.OpenApiObject;
 import org.opendaylight.netconf.sal.rest.doc.openapi.Path;
+import org.opendaylight.restconf.openapi.api.AbstractOpenApiTest;
 import org.opendaylight.yangtools.yang.common.QName;
 import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
 
