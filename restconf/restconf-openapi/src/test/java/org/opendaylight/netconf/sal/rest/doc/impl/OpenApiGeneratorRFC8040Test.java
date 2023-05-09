a501361177d780660ac69d699a92b18471655f04
OleksandrZharov <Oleksandr.Zharov@pantheon.tech>
2023-05-09 12:42:24 +0200

    Refactor package org.opendaylight.netconf.sal.rest.doc.api
    Renamed package org.opendaylight.netconf.sal.rest.doc.api to
    org.opendaylight.restconf.openapi.api
    
    JIRA: NETCONF-1011
    Change-Id: I681caea35a57e02f445713adf27084fb0c142305
    Signed-off-by: OleksandrZharov <Oleksandr.Zharov@pantheon.tech>

--- restconf/restconf-openapi/src/test/java/org/opendaylight/netconf/sal/rest/doc/impl/OpenApiGeneratorRFC8040Test.java
+++ restconf/restconf-openapi/src/test/java/org/opendaylight/netconf/sal/rest/doc/impl/OpenApiGeneratorRFC8040Test.java
@@ -17,9 +17,9 @@ import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.google.common.collect.ImmutableList;
 import java.util.List;
 import org.junit.Test;
-import org.opendaylight.netconf.sal.rest.doc.api.AbstractOpenApiTest;
 import org.opendaylight.netconf.sal.rest.doc.helper.DocGenTestHelper;
 import org.opendaylight.netconf.sal.rest.doc.openapi.OpenApiObject;
+import org.opendaylight.restconf.openapi.api.AbstractOpenApiTest;
 import org.opendaylight.yangtools.yang.common.Revision;
 
 public final class OpenApiGeneratorRFC8040Test extends AbstractOpenApiTest {
