62a7ce10e0d9c233f52f15b1e8fa87aec0f568c5
tobias.pobocik <tobias.pobocik@pantheon.tech>
2023-05-10 13:24:34 +0200

    Set default for instance-identifier type
    We should distinguish between "default" and "example" OpenAPI
    property.
    
    Set "default" with "example" since it was misused.
    
    JIRA: NETCONF-999
    Change-Id: I36450788b4ade0d386d48cf99c17055acdf41e70
    Signed-off-by: tobias.pobocik <tobias.pobocik@pantheon.tech>

--- restconf/restconf-openapi/src/main/java/org/opendaylight/restconf/openapi/impl/DefinitionGenerator.java
+++ restconf/restconf-openapi/src/main/java/org/opendaylight/restconf/openapi/impl/DefinitionGenerator.java
@@ -851,8 +851,10 @@ public class DefinitionGenerator {
             final var container = module.orElseThrow().getChildNodes().stream()
                     .filter(n -> n instanceof ContainerSchemaNode)
                     .findFirst();
-            container.ifPresent(c -> setExampleValue(property, String.format("/%s:%s", module.orElseThrow().getPrefix(),
-                    c.getQName().getLocalName())));
+            container.ifPresent(c -> setDefaultValue(property, String.valueOf(
+                    ((InstanceIdentifierTypeDefinition) c).getDefaultValue())));
+            setExampleValue(property, String.format("/%s:%s", module.orElseThrow().getPrefix(),
+                    module.orElseThrow().getName()));
         }
 
         return STRING_TYPE;
