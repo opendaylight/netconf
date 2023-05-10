194bf4edd4f81b3feaaf085c396a51f8a42d3202
tobias.pobocik <tobias.pobocik@pantheon.tech>
2023-05-10 13:06:14 +0200

    Set default for union type
    Set default value for union type when such value is present.
    
    When at place added examples values for each of union's types.
    
    JIRA: NETCONF-999
    Change-Id: I2ef7d002f8cb44a9c7af858209caf20374a92411
    Signed-off-by: tobias.pobocik <tobias.pobocik@pantheon.tech>
    Signed-off-by: Ivan Hrasko <ivan.hrasko@pantheon.tech>

--- restconf/restconf-openapi/src/main/java/org/opendaylight/restconf/openapi/impl/DefinitionGenerator.java
+++ restconf/restconf-openapi/src/main/java/org/opendaylight/restconf/openapi/impl/DefinitionGenerator.java
@@ -621,7 +621,7 @@ public class DefinitionGenerator {
         } else if (leafTypeDef instanceof StringTypeDefinition stringType) {
             jsonType = processStringType(stringType, property, node.getQName().getLocalName());
         } else if (leafTypeDef instanceof UnionTypeDefinition unionType) {
-            jsonType = processUnionType(unionType);
+            jsonType = processUnionType(unionType, property, node.getQName().getLocalName());
         } else if (leafTypeDef instanceof EmptyTypeDefinition) {
             jsonType = OBJECT_TYPE;
         } else if (leafTypeDef instanceof LeafrefTypeDefinition leafrefType) {
@@ -843,7 +843,8 @@ public class DefinitionGenerator {
         return STRING_TYPE;
     }
 
-    private static String processUnionType(final UnionTypeDefinition unionType) {
+    private static String processUnionType(final UnionTypeDefinition unionType, final ObjectNode property,
+            final String nodeName) {
         boolean isStringTakePlace = false;
         boolean isNumberTakePlace = false;
         boolean isBooleanTakePlace = false;
@@ -865,14 +866,22 @@ public class DefinitionGenerator {
             }
         }
         if (isStringTakePlace) {
+            unionType.getDefaultValue().ifPresent(v -> setDefaultValue(property, (String) v));
+            setExampleValue(property, "Some " + nodeName);
             return STRING_TYPE;
         }
         if (isBooleanTakePlace) {
             if (isNumberTakePlace) {
-                return STRING_TYPE;
+                unionType.getDefaultValue().ifPresent(v -> setDefaultValue(property, v.toString()));
+                setExampleValue(property, 0);
+                return NUMBER_TYPE;
             }
+            unionType.getDefaultValue().ifPresent(v -> setDefaultValue(property, (Boolean) v));
+            setExampleValue(property, true);
             return BOOLEAN_TYPE;
         }
+        unionType.getDefaultValue().ifPresent(v -> setDefaultValue(property, v.toString()));
+        setExampleValue(property, 0);
         return NUMBER_TYPE;
     }
 
