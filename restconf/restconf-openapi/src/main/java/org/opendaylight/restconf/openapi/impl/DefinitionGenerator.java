64f33b0d8887515c87ff10dae1a4ec50ac24e459
tobias.pobocik <tobias.pobocik@pantheon.tech>
2023-05-10 13:22:11 +0200

    Set default for string type
    Set default value for string type leaf when such value is present.
    
    JIRA: NETCONF-999
    Change-Id: Ia0bbecd0913b65b68a3fa30e33485b515964ada6
    Signed-off-by: tobias.pobocik <tobias.pobocik@pantheon.tech>
    Signed-off-by: Ivan Hrasko <ivan.hrasko@pantheon.tech>

--- restconf/restconf-openapi/src/main/java/org/opendaylight/restconf/openapi/impl/DefinitionGenerator.java
+++ restconf/restconf-openapi/src/main/java/org/opendaylight/restconf/openapi/impl/DefinitionGenerator.java
@@ -784,6 +784,8 @@ public class DefinitionGenerator {
         } else {
             setExampleValue(property, "Some " + nodeName);
         }
+
+        stringType.getDefaultValue().ifPresent(v -> setDefaultValue(property, (String) v));
         return STRING_TYPE;
     }
 
