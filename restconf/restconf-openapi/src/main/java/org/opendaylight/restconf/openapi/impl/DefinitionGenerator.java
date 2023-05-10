5a070c0f150f2c351dcbd6e449528d547496c861
tobias.pobocik <tobias.pobocik@pantheon.tech>
2023-05-10 13:23:35 +0200

    Set default for number type
    We should distinguish between "default" and "example" OpenAPI
    property.
    
    Set "default" with "example" since it was misused.
    
    JIRA: NETCONF-999
    Change-Id: I2fbb6ab7643c6028219f38da3e07d2e67a8f3049
    Signed-off-by: tobias.pobocik <tobias.pobocik@pantheon.tech>

--- restconf/restconf-openapi/src/main/java/org/opendaylight/restconf/openapi/impl/DefinitionGenerator.java
+++ restconf/restconf-openapi/src/main/java/org/opendaylight/restconf/openapi/impl/DefinitionGenerator.java
@@ -24,6 +24,7 @@ import com.google.common.collect.RangeSet;
 import dk.brics.automaton.RegExp;
 import java.io.IOException;
 import java.math.BigDecimal;
+import java.math.BigInteger;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
@@ -800,6 +801,8 @@ public class DefinitionGenerator {
         }
 
         if (leafTypeDef instanceof DecimalTypeDefinition) {
+            leafTypeDef.getDefaultValue().ifPresent(number ->
+                    setDefaultValue(property, Decimal64.valueOf((String) number).decimalValue()));
             maybeLower.ifPresent(number -> setExampleValue(property, ((Decimal64) number).decimalValue()));
             return NUMBER_TYPE;
         }
@@ -810,14 +813,20 @@ public class DefinitionGenerator {
                 || leafTypeDef instanceof Int32TypeDefinition) {
 
             property.put(FORMAT_KEY, INT32_FORMAT);
+            leafTypeDef.getDefaultValue().ifPresent(number -> setDefaultValue(property,
+                Integer.valueOf((String) number)));
             maybeLower.ifPresent(number -> setExampleValue(property, Integer.valueOf(number.toString())));
         } else if (leafTypeDef instanceof Uint32TypeDefinition
                 || leafTypeDef instanceof Int64TypeDefinition) {
 
             property.put(FORMAT_KEY, INT64_FORMAT);
+            leafTypeDef.getDefaultValue().ifPresent(number -> setDefaultValue(property,
+                Long.valueOf((String) number)));
             maybeLower.ifPresent(number -> setExampleValue(property, Long.valueOf(number.toString())));
         } else {
             //uint64
+            leafTypeDef.getDefaultValue().ifPresent(number -> setDefaultValue(property,
+                new BigInteger((String) number)));
             setExampleValue(property, 0);
         }
         return INTEGER_TYPE;
@@ -945,7 +954,15 @@ public class DefinitionGenerator {
         property.put(DEFAULT_KEY, value);
     }
 
+    private static void setDefaultValue(final ObjectNode property, final Integer value) {
+        property.put(DEFAULT_KEY, value);
+    }
+
     private static void setDefaultValue(final ObjectNode property, final Long value) {
+        property.put(DEFAULT_KEY, value);
+    }
+
+    private static void setDefaultValue(final ObjectNode property, final BigInteger value) {
         property.put(DEFAULT_KEY, value);
     }
 
