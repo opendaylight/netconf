0a2e90ceaafb47902944afe74a148c6df7c883bb
Yaroslav Lastivka <yaroslav.lastivka@pantheon.tech>
2023-05-10 14:01:09 +0300

    Fix Invalid swagger URLs in BBF yang devices
    Some BBF yang devices using actions have invalid swagger URLs generated
    
    Implemented logic for adding “pathParameters” to path parameters for actions.
    Added logic for generating correct swagger URLs.
    
    JIRA: NETCONF-860
    Change-Id: If01af731770029fb8f7529312417a3ea46fd5841
    Signed-off-by: Yaroslav Lastivka <yaroslav.lastivka@pantheon.tech>

--- restconf/restconf-openapi/src/main/java/org/opendaylight/restconf/openapi/impl/BaseYangOpenApiGenerator.java
+++ restconf/restconf-openapi/src/main/java/org/opendaylight/restconf/openapi/impl/BaseYangOpenApiGenerator.java
@@ -262,11 +262,12 @@ public abstract class BaseYangOpenApiGenerator {
             }
         }
 
+        final ArrayNode pathParams = JsonNodeFactory.instance.arrayNode();
         for (final RpcDefinition rpcDefinition : module.getRpcs()) {
             final String resolvedPath = getResourcePath("operations", context) + "/" + moduleName + ":"
                     + rpcDefinition.getQName().getLocalName();
             addOperations(rpcDefinition, moduleName, deviceName, paths, moduleName, definitionNames,
-                resolvedPath);
+                resolvedPath, pathParams);
         }
 
         LOG.debug("Number of Paths found [{}]", paths.size());
@@ -326,9 +327,13 @@ public abstract class BaseYangOpenApiGenerator {
 
         if (node instanceof ActionNodeContainer) {
             ((ActionNodeContainer) node).getActions().forEach(actionDef -> {
-                final String resolvedPath = "rests/operations" + resourcePath.substring(11)
-                        + "/" + resolvePathArgumentsName(actionDef.getQName(), node.getQName(), schemaContext);
-                addOperations(actionDef, moduleName, deviceName, paths, parentName, definitionNames, resolvedPath);
+                final String resolvedPathArgumentsName = resolvePathArgumentsName(actionDef.getQName(), node.getQName(),
+                        schemaContext);
+                final String resolvedPath = resourcePath.matches(".*\\/" + resolvedPathArgumentsName + "$")
+                        ? "/rests/operations" + resourcePath.substring(11)
+                        : "/rests/operations" + resourcePath.substring(11) + "/" + resolvedPathArgumentsName;
+                addOperations(actionDef, moduleName, deviceName, paths, parentName, definitionNames, resolvedPath,
+                        parentPathParams);
             });
         }
 
@@ -467,9 +472,10 @@ public abstract class BaseYangOpenApiGenerator {
 
     private static void addOperations(final OperationDefinition operDef, final String moduleName,
             final Optional<String> deviceName, final Map<String, Path> paths, final String parentName,
-            final DefinitionNames definitionNames, final String resourcePath) {
+            final DefinitionNames definitionNames, final String resourcePath, final ArrayNode parentPathParams) {
         final var pathBuilder = new Path.Builder();
-        pathBuilder.post(buildPostOperation(operDef, moduleName, deviceName, parentName, definitionNames));
+        pathBuilder.post(buildPostOperation(operDef, moduleName, deviceName, parentName, definitionNames,
+                parentPathParams));
         paths.put(resourcePath, pathBuilder.build());
     }
 
--- restconf/restconf-openapi/src/test/java/org/opendaylight/restconf/openapi/impl/OpenApiGeneratorRFC8040Test.java
+++ restconf/restconf-openapi/src/test/java/org/opendaylight/restconf/openapi/impl/OpenApiGeneratorRFC8040Test.java
@@ -265,6 +265,16 @@ public final class OpenApiGeneratorRFC8040Test {
     }
 
     @Test
+    public void testActionPathsParams() {
+        final var module = context.findModule("action-types").orElseThrow();
+        final var doc = generator.getOpenApiSpec(module, "http", "localhost:8181", "/", "", context);
+
+        var path = "/rests/operations/action-types:list={name}/list-action";
+        assertTrue(doc.paths().containsKey(path));
+        assertEquals(List.of("name"), getPathParameters(doc.paths(), path));
+    }
+
+    @Test
     public void testSimpleOpenApiObjects() {
         final var module = context.findModule("my-yang", Revision.of("2022-10-06")).orElseThrow();
         final var doc = generator.getOpenApiSpec(module, "http", "localhost:8181", "/", "", context);
