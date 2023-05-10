32b5e48f0dfd54c9cab2c8d151f7b2a3bf469aea
tobias.pobocik <tobias.pobocik@pantheon.tech>
2023-05-10 12:56:14 +0200

    Set default for binary type
    Set default value for binary type leaf when such value is present.
    
    Due to constrained nature of this type its difficult to set
    example value.
    
    JIRA: NETCONF-999
    Change-Id: I7f5b3cc30ee1c6adab2d159efea0995f3a3b379f
    Signed-off-by: tobias.pobocik <tobias.pobocik@pantheon.tech>
    Signed-off-by: Ivan Hrasko <ivan.hrasko@pantheon.tech>

--- restconf/restconf-openapi/src/main/java/org/opendaylight/restconf/openapi/impl/DefinitionGenerator.java
+++ restconf/restconf-openapi/src/main/java/org/opendaylight/restconf/openapi/impl/DefinitionGenerator.java
@@ -603,8 +603,8 @@ public class DefinitionGenerator {
             final ObjectNode property, final SchemaInferenceStack stack, final Map<String, Schema> definitions,
             final DefinitionNames definitionNames) {
         final String jsonType;
-        if (leafTypeDef instanceof BinaryTypeDefinition) {
-            jsonType = processBinaryType(property);
+        if (leafTypeDef instanceof BinaryTypeDefinition binaryType) {
+            jsonType = processBinaryType(binaryType, property);
 
         } else if (leafTypeDef instanceof BitsTypeDefinition) {
             jsonType = processBitsType((BitsTypeDefinition) leafTypeDef, property);
@@ -666,7 +666,8 @@ public class DefinitionGenerator {
         return jsonType;
     }
 
-    private static String processBinaryType(final ObjectNode property) {
+    private static String processBinaryType(final BinaryTypeDefinition definition, final ObjectNode property) {
+        definition.getDefaultValue().ifPresent(v -> setDefaultValue(property, ((String) v)));
         property.put(FORMAT_KEY, "byte");
         return STRING_TYPE;
     }
