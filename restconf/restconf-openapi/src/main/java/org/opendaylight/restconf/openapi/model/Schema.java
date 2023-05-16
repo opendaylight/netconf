ecd5354afd61495393c3a5a6246eb03080e5caae
OleksandrZharov <Oleksandr.Zharov@pantheon.tech>
2023-05-16 12:30:38 +0200

    Use Java Schema object instead of JsonNode
    Created Schema.java object and replaced ObjectNode schemas inside
    Components class with Map<String, Schema>. This allowed us remove
    JsonUtil#addFields method which is addressed by another issue.
    
    JIRA: NETCONF-1024
    Change-Id: I13477fae13298b782df4af6e100fe9f807b331bd
    Signed-off-by: OleksandrZharov <Oleksandr.Zharov@pantheon.tech>

---
+++ restconf/restconf-openapi/src/main/java/org/opendaylight/restconf/openapi/model/Schema.java
@@ -0,0 +1,116 @@
+/*
+ * Copyright (c) 2023 PANTHEON.tech s.r.o and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.restconf.openapi.model;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class Schema {
+    private ArrayNode schemaEnum;
+    private ArrayNode required;
+    private ObjectNode discriminator;
+    private ObjectNode examples;
+    private ObjectNode externalDocs;
+    private ObjectNode properties;
+    private ObjectNode xml;
+    private String description;
+    private String ref;
+    private String title;
+    private String type;
+
+    public ObjectNode getDiscriminator() {
+        return discriminator;
+    }
+
+    public void setDiscriminator(ObjectNode discriminator) {
+        this.discriminator = discriminator;
+    }
+
+    public ObjectNode getXml() {
+        return xml;
+    }
+
+    public void setXml(ObjectNode xml) {
+        this.xml = xml;
+    }
+
+    public ObjectNode getExternalDocs() {
+        return externalDocs;
+    }
+
+    public void setExternalDocs(ObjectNode externalDocs) {
+        this.externalDocs = externalDocs;
+    }
+
+    public ObjectNode getExamples() {
+        return examples;
+    }
+
+    public void setExamples(ObjectNode examples) {
+        this.examples = examples;
+    }
+
+    public ObjectNode getProperties() {
+        return properties;
+    }
+
+    public void setProperties(ObjectNode properties) {
+        this.properties = properties;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public ArrayNode getRequired() {
+        return required;
+    }
+
+    public void setRequired(ArrayNode required) {
+        this.required = required;
+    }
+
+    public String getRef() {
+        return ref;
+    }
+
+    public void setRef(String ref) {
+        this.ref = ref;
+    }
+
+    public ArrayNode getEnum() {
+        return schemaEnum;
+    }
+
+    public void setEnum(ArrayNode enumKey) {
+        this.schemaEnum = enumKey;
+    }
+}
