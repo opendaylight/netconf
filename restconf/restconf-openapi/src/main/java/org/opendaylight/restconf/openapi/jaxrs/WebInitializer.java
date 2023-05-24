c73946e0d7c6b60367c0a3968f9ecb0348afc183
Ivan Hrasko <ivan.hrasko@pantheon.tech>
2023-05-24 15:20:32 +0200

    WIP: Remove OpenApiApplication
    Remove not-nneded stuff.
    
    Signed-off-by: Ivan Hrasko <ivan.hrasko@pantheon.tech>
    Change-Id: Idfdc00e66e15d812e1921c8e8404c50e8185fa7f

--- restconf/restconf-openapi/src/main/java/org/opendaylight/restconf/openapi/jaxrs/WebInitializer.java
+++ restconf/restconf-openapi/src/main/java/org/opendaylight/restconf/openapi/jaxrs/WebInitializer.java
@@ -7,10 +7,12 @@
  */
 package org.opendaylight.restconf.openapi.jaxrs;
 
+import java.util.Set;
 import javax.annotation.PreDestroy;
 import javax.inject.Inject;
 import javax.inject.Singleton;
 import javax.servlet.ServletException;
+import javax.ws.rs.core.Application;
 import org.opendaylight.aaa.web.ResourceDetails;
 import org.opendaylight.aaa.web.ServletDetails;
 import org.opendaylight.aaa.web.WebContext;
@@ -44,8 +46,13 @@ public final class WebInitializer implements AutoCloseable {
             .contextPath("/openapi")
             .supportsSessions(true)
             .addServlet(ServletDetails.builder()
-                .servlet(servletSupport.createHttpServletBuilder(new OpenApiApplication(openApiService)).build())
-                .addUrlPattern("/api/v3/*")
+                .servlet(servletSupport.createHttpServletBuilder(new Application() {
+                    @Override
+                    public Set<Object> getSingletons() {
+                        return Set.of(openApiService);
+                    }
+                }).build())
+            .addUrlPattern("/api/v3/*")
                 .build())
             .addResource(ResourceDetails.builder().name("/explorer").build());
 
