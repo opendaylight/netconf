d66f4a1d1d7b23fdad0f8e696ee26aa169988c9b
Peter Suna <peter.suna@pantheon.tech>
2023-05-24 12:35:49 +0200

    Reduce XML nodes from OpenApi
    OpenAPI uses XML nodes to determine the namespace,
    but it is not necessary for every element. It is only required
    in the first node and the augmented element.
    
    JIRA: NETCONF-1036
    Change-Id: Iec1586c2197f91fc7ef6d88980b704b33114ffb0
    Signed-off-by: Peter Suna <peter.suna@pantheon.tech>
    Signed-off-by: Ivan Hrasko <ivan.hrasko@pantheon.tech>

--- restconf/restconf-openapi/src/test/java/org/opendaylight/restconf/openapi/impl/OpenApiGeneratorRFC8040Test.java
+++ restconf/restconf-openapi/src/test/java/org/opendaylight/restconf/openapi/impl/OpenApiGeneratorRFC8040Test.java
@@ -376,4 +376,45 @@ public final class OpenApiGeneratorRFC8040Test {
         assertNotNull(postXmlRef);
         assertEquals(expectedXmlRef, postXmlRef.textValue());
     }
+
+    /**
+     * Test if "xml" nodes are added with correct namespace.
+     */
+    @Test
+    public void testXmlNodes() {
+        final var context1036 = YangParserTestUtils.parseYangResourceDirectory("/NETCONF-1036");
+        final var module = context1036.findModule("module").orElseThrow();
+        final var mockSchemaService = mock(DOMSchemaService.class);
+        when(mockSchemaService.getGlobalContext()).thenReturn(context1036);
+        final var generatorRFC8040 = new OpenApiGeneratorRFC8040(mockSchemaService);
+        final var doc = generatorRFC8040.getOpenApiSpec(module, "http", "localhost:8181", "/", "", context1036);
+        assertNotNull(doc);
+
+        final var schemas = doc.components().schemas();
+        final var simpleList1 = schemas.get("module_root_simple-root_list-1");
+        assertEquals("urn:ietf:params:xml:ns:yang:test:augmentation", simpleList1.xml().get("namespace").asText());
+        assertNull(simpleList1.properties().get("leaf-x").get("xml"));
+
+        final var simpleAbc = schemas.get("module_root_simple-root_abc");
+        assertEquals("urn:ietf:params:xml:ns:yang:test:augmentation", simpleAbc.xml().get("namespace").asText());
+        assertNull(simpleAbc.properties().get("leaf-abc").get("xml"));
+
+        final var simple = schemas.get("module_root_simple-root");
+        assertEquals("urn:ietf:params:xml:ns:yang:test:augmentation", simple.properties().get("leaf-y")
+                .get("xml").get("namespace").asText());
+        assertNull(simple.properties().get("leaf-a").get("xml"));
+
+        final var topList1 = schemas.get("module_root_top-list_list-1");
+        assertEquals("urn:ietf:params:xml:ns:yang:test:augmentation", topList1.xml().get("namespace").asText());
+        assertNull(topList1.properties().get("leaf-x").get("xml"));
+
+        final var topAbc = schemas.get("module_root_top-list_abc");
+        assertEquals("urn:ietf:params:xml:ns:yang:test:augmentation", topAbc.xml().get("namespace").asText());
+        assertNull(topAbc.properties().get("leaf-abc").get("xml"));
+
+        final var top = schemas.get("module_root_top-list");
+        assertEquals("urn:ietf:params:xml:ns:yang:test:augmentation", top.properties().get("leaf-y")
+                .get("xml").get("namespace").asText());
+        assertNull(top.properties().get("key-1").get("xml"));
+    }
 }
