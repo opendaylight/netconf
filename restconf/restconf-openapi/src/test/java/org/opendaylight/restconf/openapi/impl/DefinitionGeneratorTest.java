762957dbdb31c2741180b2488df4af82feb78921
tobias.pobocik <tobias.pobocik@pantheon.tech>
2023-05-25 17:17:13 +0200

    Improve DefinitionGenerator testing
    This commit addresses the lack of test coverage for various sections
    within the DefinitionGenerator class. By adding more tests, we
    significantly enhance the overall coverage of this class.
    This improvement ensures a more robust and reliable testing environment.
    
    The existing test also had to be modified to match the latest patch
    where the default keyword was misused, and was replaced by example.
    
    JIRA: NETCONF-999
    Change-Id: Iac703199c62f56b40dc02ff992e001a7b56554bf
    Signed-off-by: tobias.pobocik <tobias.pobocik@pantheon.tech>

--- restconf/restconf-openapi/src/test/java/org/opendaylight/restconf/openapi/impl/DefinitionGeneratorTest.java
+++ restconf/restconf-openapi/src/test/java/org/opendaylight/restconf/openapi/impl/DefinitionGeneratorTest.java
@@ -34,7 +34,7 @@ public final class DefinitionGeneratorTest {
     @Test
     public void testConvertToSchemas() throws IOException {
         final var module = context.findModule("opflex", Revision.of("2014-05-28")).orElseThrow();
-        final DefinitionGenerator generator = new DefinitionGenerator();
+        final var generator = new DefinitionGenerator();
         final var schemas = generator.convertToSchemas(module, context, new DefinitionNames(), true);
         assertNotNull(schemas);
     }
@@ -42,7 +42,7 @@ public final class DefinitionGeneratorTest {
     @Test
     public void testActionTypes() throws IOException {
         final var module = context.findModule("action-types").orElseThrow();
-        final DefinitionGenerator generator = new DefinitionGenerator();
+        final var generator = new DefinitionGenerator();
         final var schemas = generator.convertToSchemas(module, context, new DefinitionNames(), true);
         assertNotNull(schemas);
     }
@@ -50,9 +50,88 @@ public final class DefinitionGeneratorTest {
     @Test
     public void testStringTypes() throws IOException {
         final var module = context.findModule("string-types").orElseThrow();
-        final DefinitionGenerator generator = new DefinitionGenerator();
+        final var generator = new DefinitionGenerator();
         final var schemas = generator.convertToSchemas(module, context, new DefinitionNames(), true);
         assertNotNull(schemas);
+    }
+
+    @Test
+    public void testEnumType() throws IOException {
+        final var module = context.findModule("definition-test").orElseThrow();
+        final var generator = new DefinitionGenerator();
+        final var jsonObject = generator.convertToSchemas(module, context, new DefinitionNames(), true);
+        assertNotNull(jsonObject);
+
+        final var properties = jsonObject.get("definition-test_config_enum-container").properties();
+        assertEquals("up", properties.get("status").get("default").asText());
+    }
+
+    @Test
+    public void testUnionTypes() throws IOException {
+        final var module = context.findModule("definition-test").orElseThrow();
+        final var generator = new DefinitionGenerator();
+        final var jsonObject = generator.convertToSchemas(module, context, new DefinitionNames(), true);
+        assertNotNull(jsonObject);
+
+        final var properties = jsonObject.get("definition-test_config_union-container").properties();
+        assertEquals("5", properties.get("testUnion1").get("default").asText());
+        assertEquals("false", properties.get("testUnion2").get("default").asText());
+    }
+
+    @Test
+    public void testBinaryType() throws IOException {
+        final var module = context.findModule("definition-test").orElseThrow();
+        final var generator = new DefinitionGenerator();
+        final var jsonObject = generator.convertToSchemas(module, context, new DefinitionNames(), true);
+        assertNotNull(jsonObject);
+
+        final var properties = jsonObject.get("definition-test_config_binary-container").properties();
+        assertEquals("SGVsbG8gdGVzdCE=", properties.get("binary-data").get("default").asText());
+    }
+
+    @Test
+    public void testBooleanType() throws IOException {
+        final var module = context.findModule("definition-test").orElseThrow();
+        final var generator = new DefinitionGenerator();
+        final var jsonObject = generator.convertToSchemas(module, context, new DefinitionNames(), true);
+        assertNotNull(jsonObject);
+
+        final var properties = jsonObject.get("definition-test_config_union-container").properties();
+        assertEquals("true", properties.get("testBoolean").get("default").asText());
+        assertEquals("true", properties.get("testBoolean").get("example").asText());
+    }
+
+    @Test
+    public void testNumberType() throws IOException {
+        final var module = context.findModule("definition-test").orElseThrow();
+        final var generator = new DefinitionGenerator();
+        final var jsonObject = generator.convertToSchemas(module, context, new DefinitionNames(), true);
+        assertNotNull(jsonObject);
+
+        final var properties = jsonObject.get("definition-test_config_number-container").properties();
+        assertEquals("42", properties.get("testInteger").get("default").asText());
+        assertEquals("42", properties.get("testInt64").get("default").asText());
+        assertEquals("42", properties.get("testUint64").get("default").asText());
+        assertEquals("100", properties.get("testUnsignedInteger").get("default").asText());
+        assertEquals("3.14", properties.get("testDecimal").get("default").asText());
+        assertEquals("3.14159265359", properties.get("testDouble").get("default").asText());
+    }
+
+    @Test
+    public void testInstanceIdentifierType() throws IOException {
+        final var module = context.findModule("definition-test").orElseThrow();
+        final var generator = new DefinitionGenerator();
+        final var jsonObject = generator.convertToSchemas(module, context, new DefinitionNames(), true);
+        assertNotNull(jsonObject);
+
+        final var properties = jsonObject.get("definition-test_config_network-container").properties();
+        final var networkRef = properties.get("network-ref");
+
+        assertNotNull(networkRef);
+        assertEquals("string", networkRef.get("type").asText());
+
+        assertEquals("/network/nodes[node-id='node1']", networkRef.get("default").asText());
+        assertEquals("/sample:definition-test", networkRef.get("example").asText());
     }
 
     @Test
@@ -66,5 +145,7 @@ public final class DefinitionGeneratorTest {
         assertEquals("00:00:00:00:00:00", properties.get("mac-address").get("example").asText());
         assertEquals("0000-00-00T00:00:00Z", properties.get("login-date-time").get("example").asText());
         assertEquals("0.0.0.0", properties.get("ipv4-address").get("example").asText());
+
+        assertEquals("ab:cd:ef:12:34:56", properties.get("mac-address").get("default").asText());
     }
 }
