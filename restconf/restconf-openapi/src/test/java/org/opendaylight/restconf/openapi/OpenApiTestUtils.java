b068cd54c13ee5472a3860b9d1835540c4d14091
OleksandrZharov <Oleksandr.Zharov@pantheon.tech>
2023-05-30 11:15:11 +0200

    Rework unit test infrastructure
    Changed AbstractOpenApiTest class in util class named
    OpenApiTestUtils.
    Copied contents of BeforeClass to each affected test.
    This allow us configure resources for each test more freely.
    
    JIRA: NETCONF-1040
    Change-Id: I80392d46495b443a56ac568755716511c7e97298
    Signed-off-by: OleksandrZharov <Oleksandr.Zharov@pantheon.tech>

---
+++ restconf/restconf-openapi/src/test/java/org/opendaylight/restconf/openapi/OpenApiTestUtils.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2022 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.restconf.openapi;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import org.opendaylight.restconf.openapi.model.Path;
+
+public final class OpenApiTestUtils {
+
+    /**
+     * Get path parameters name for {@code path}.
+     *
+     * @return {@link List<String>} of parameters
+     */
+    public static List<String> getPathParameters(final Map<String, Path> paths, final String path) {
+        final var params = new ArrayList<String>();
+        paths.get(path).getPost().get("parameters").elements()
+            .forEachRemaining(p -> params.add(p.get("name").asText()));
+        return params;
+    }
+}
