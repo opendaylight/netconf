bec73b64ccb6b99cae14650b720d00172ded6a13
Šimon Ukuš <simon.ukus@pantheon.tech>
2023-05-30 09:09:25 +0200

    Replace Optional range with separate methods
    The logic was divided into two methods: one for range-based
    doc creation and the other for non-range doc creation,
    eliminating the use of Optional<Range<Integer>>.
    
    JIRA: NETCONF-998
    Change-Id: I521813b87e08d28b6d1071e006b2b2f85f438437
    Signed-off-by: Šimon Ukuš <simon.ukus@pantheon.tech>
    Signed-off-by: Ivan Hrasko <ivan.hrasko@pantheon.tech>

--- restconf/restconf-openapi/src/main/java/org/opendaylight/restconf/openapi/impl/OpenApiServiceImpl.java
+++ restconf/restconf-openapi/src/main/java/org/opendaylight/restconf/openapi/impl/OpenApiServiceImpl.java
@@ -11,7 +11,6 @@ import static java.util.Objects.requireNonNull;
 
 import com.google.common.annotations.VisibleForTesting;
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 import javax.inject.Inject;
 import javax.inject.Singleton;
@@ -120,10 +119,8 @@ public final class OpenApiServiceImpl implements OpenApiService {
     @Override
     public synchronized Response getMountDoc(final String instanceNum, final UriInfo uriInfo) {
         final String stringPageNum = uriInfo.getQueryParameters().getFirst(PAGE_NUM);
-        final Optional<Integer> pageNum = stringPageNum != null ? Optional.of(Integer.valueOf(stringPageNum))
-                : Optional.empty();
         final OpenApiObject api = mountPointOpenApiRFC8040.getMountPointApi(uriInfo,
-                Long.parseLong(instanceNum), pageNum);
+                Long.parseLong(instanceNum), stringPageNum);
         return Response.ok(api).build();
     }
 }
