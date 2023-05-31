ad5e507bd9f747cf98cab7f2db965b8327e5a1bd
Šimon Ukuš <simon.ukus@pantheon.tech>
2023-05-31 10:48:03 +0200

    Throw NotFoundException on MissingSchemaSource
    Throwing NotFoundException, instead of a more general
    WebApplicationException, will return 404 status code to the client,
    rather than 500 with the WebAppException.
    
    JIRA: NETCONF-964
    Change-Id: Ic72b068a477a98f618bf303c43d7deca94254f3e
    Signed-off-by: Šimon Ukuš <simon.ukus@pantheon.tech>

--- netconf/yanglib/src/main/java/org/opendaylight/yanglib/impl/YangLibProvider.java
+++ netconf/yanglib/src/main/java/org/opendaylight/yanglib/impl/YangLibProvider.java
@@ -22,6 +22,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
 import java.util.concurrent.ExecutionException;
+import javax.ws.rs.NotFoundException;
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.Response;
 import org.opendaylight.mdsal.binding.api.DataBroker;
@@ -197,6 +198,9 @@ public class YangLibProvider implements AutoCloseable, SchemaSourceListener, Yan
             final var yangTextSchemaSource = yangTextSchemaFuture.get();
             return yangTextSchemaSource.asCharSource(StandardCharsets.UTF_8).read();
         } catch (InterruptedException | ExecutionException e) {
+            if (e.getCause() instanceof MissingSchemaSourceException mss) {
+                throw new NotFoundException("Schema source " + sourceId + " not found", mss);
+            }
             throw new WebApplicationException("Unable to get schema " + sourceId, e);
         } catch (IOException e) {
             throw new WebApplicationException("Unable to read schema " + sourceId, e);
