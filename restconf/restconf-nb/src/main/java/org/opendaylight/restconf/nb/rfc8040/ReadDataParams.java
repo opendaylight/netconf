c8635c671f61294b3cf7f4216e4a80371537c120
OleksandrZharov <Oleksandr.Zharov@pantheon.tech>
2023-05-31 16:43:48 +0200

    Convert ReadDataParams to Java record
    Refactored ReadDataParams into Java record.
    
    JIRA: NETCONF-1044
    Change-Id: Ib9f667d53f48cbe2b7602c1bc68f063237c9fad3
    Signed-off-by: OleksandrZharov <Oleksandr.Zharov@pantheon.tech>

--- restconf/restconf-nb/src/main/java/org/opendaylight/restconf/nb/rfc8040/ReadDataParams.java
+++ restconf/restconf-nb/src/main/java/org/opendaylight/restconf/nb/rfc8040/ReadDataParams.java
@@ -12,73 +12,27 @@ import static java.util.Objects.requireNonNull;
 import com.google.common.annotations.Beta;
 import com.google.common.base.MoreObjects;
 import org.eclipse.jdt.annotation.NonNull;
-import org.eclipse.jdt.annotation.Nullable;
 import org.opendaylight.restconf.api.query.ContentParam;
 import org.opendaylight.restconf.api.query.DepthParam;
 import org.opendaylight.restconf.api.query.FieldsParam;
 import org.opendaylight.restconf.api.query.PrettyPrintParam;
 import org.opendaylight.restconf.api.query.WithDefaultsParam;
-import org.opendaylight.yangtools.concepts.Immutable;
 
 /**
  * Parser and holder of query parameters from uriInfo for data and datastore read operations.
  */
 @Beta
-// FIXME: this should be a record once we have JDK17+
-public final class ReadDataParams implements Immutable {
+public record ReadDataParams(ContentParam content, DepthParam depth, FieldsParam fields, WithDefaultsParam withDefaults,
+                             boolean tagged, PrettyPrintParam prettyPrint) {
     private static final @NonNull ReadDataParams EMPTY =
         new ReadDataParams(ContentParam.ALL, null, null, null, false, null);
 
-    private final @NonNull ContentParam content;
-    private final WithDefaultsParam withDefaults;
-    private final PrettyPrintParam prettyPrint;
-    private final FieldsParam fields;
-    private final DepthParam depth;
-    private final boolean tagged;
-
-    private ReadDataParams(final ContentParam content,  final DepthParam depth, final FieldsParam fields,
-            final WithDefaultsParam withDefaults, final boolean tagged, final PrettyPrintParam prettyPrint) {
-        this.content = requireNonNull(content);
-        this.depth = depth;
-        this.fields = fields;
-        this.withDefaults = withDefaults;
-        this.tagged = tagged;
-        this.prettyPrint = prettyPrint;
+    public ReadDataParams {
+        requireNonNull(content);
     }
 
     public static @NonNull ReadDataParams empty() {
         return EMPTY;
-    }
-
-    public static @NonNull ReadDataParams of(final ContentParam content,  final DepthParam depth,
-            final FieldsParam fields, final WithDefaultsParam withDefaults, final boolean tagged,
-            final PrettyPrintParam prettyPrint) {
-        return new ReadDataParams(content, depth, fields, withDefaults, tagged, prettyPrint);
-    }
-
-    public @NonNull ContentParam content() {
-        return content;
-    }
-
-    public @Nullable DepthParam depth() {
-        return depth;
-    }
-
-    public @Nullable FieldsParam fields() {
-        return fields;
-    }
-
-    public @Nullable WithDefaultsParam withDefaults() {
-        return withDefaults;
-    }
-
-    public @Nullable PrettyPrintParam prettyPrint() {
-        return prettyPrint;
-    }
-
-    // FIXME: for migration only
-    public boolean tagged() {
-        return tagged;
     }
 
     @Override
