a603128f934a3ad9b66c08ff2bbb1ce7867149b3
OleksandrZharov <Oleksandr.Zharov@pantheon.tech>
2023-06-06 13:02:50 +0200

    Introduce Operation object
    Created model for Operations.
    All affected logic was adapted to use this model instead of JsonNode.
    
    JIRA: NETCONF-1050
    Change-Id: If4c06fd66c4c4487978541aaf372dbc6a20158d9
    Signed-off-by: OleksandrZharov <Oleksandr.Zharov@pantheon.tech>

---
+++ restconf/restconf-openapi/src/main/java/org/opendaylight/restconf/openapi/model/Operation.java
@@ -0,0 +1,104 @@
+/*
+ * Copyright (c) 2023 PANTHEON.tech s.r.o and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.restconf.openapi.model;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public record Operation(boolean deprecated, ArrayNode tags, ArrayNode parameters, ArrayNode security, ArrayNode servers,
+        ObjectNode callbacks, ObjectNode externalDocs, ObjectNode requestBody, ObjectNode responses,
+        String description, String operationId, String summary) {
+
+    private Operation(Builder builder) {
+        this(builder.deprecated, builder.tags, builder.parameters, builder.security, builder.servers, builder.callbacks,
+                builder.externalDocs, builder.requestBody, builder.responses, builder.description, builder.operationId,
+                builder.summary);
+    }
+
+    @SuppressWarnings("checkstyle:hiddenField")
+    public static class Builder {
+        private boolean deprecated;
+        private ArrayNode tags;
+        private ArrayNode parameters;
+        private ArrayNode security;
+        private ArrayNode servers;
+        private ObjectNode callbacks;
+        private ObjectNode externalDocs;
+        private ObjectNode requestBody;
+        private ObjectNode responses;
+        private String description;
+        private String operationId;
+        private String summary;
+
+        public Builder deprecated(boolean deprecated) {
+            this.deprecated = deprecated;
+            return this;
+        }
+
+        public Builder tags(ArrayNode tags) {
+            this.tags = tags;
+            return this;
+        }
+
+        public Builder parameters(ArrayNode parameters) {
+            this.parameters = parameters;
+            return this;
+        }
+
+        public Builder security(ArrayNode security) {
+            this.security = security;
+            return this;
+        }
+
+        public Builder servers(ArrayNode servers) {
+            this.servers = servers;
+            return this;
+        }
+
+        public Builder callbacks(ObjectNode callbacks) {
+            this.callbacks = callbacks;
+            return this;
+        }
+
+        public Builder externalDocs(ObjectNode externalDocs) {
+            this.externalDocs = externalDocs;
+            return this;
+        }
+
+        public Builder requestBody(ObjectNode requestBody) {
+            this.requestBody = requestBody;
+            return this;
+        }
+
+        public Builder responses(ObjectNode responses) {
+            this.responses = responses;
+            return this;
+        }
+
+        public Builder description(String description) {
+            this.description = description;
+            return this;
+        }
+
+        public Builder operationId(String operationId) {
+            this.operationId = operationId;
+            return this;
+        }
+
+        public Builder summary(String summary) {
+            this.summary = summary;
+            return this;
+        }
+
+        public Operation build() {
+            return new Operation(this);
+        }
+    }
+}
