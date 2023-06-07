ea7e3673a158c53f67ebee27d5cdbb53c200d8da
Šimon Ukuš <simon.ukus@pantheon.tech>
2023-06-07 12:42:40 +0200

    Refactor test to verify the dynamicity of basePath
    The previous change related to NETCONF-1021 modified
    the nature of basePath. It is no longer hard coded as rests,
    but can be specified by the user.
    
    This change adjusts test to be parameterized, to verify the
    correctness of the previous change by using the default basePath
    along with a custom one.
    
    JIRA: NETCONF-1021
    Change-Id: I6c5bf49f2c2e0b34a37636886c2848ac904ca0a3
    Signed-off-by: Šimon Ukuš <simon.ukus@pantheon.tech>

--- restconf/restconf-openapi/src/test/java/org/opendaylight/restconf/openapi/impl/OpenApiServiceImplTest.java
+++ restconf/restconf-openapi/src/test/java/org/opendaylight/restconf/openapi/impl/OpenApiServiceImplTest.java
@@ -42,7 +42,7 @@ public final class OpenApiServiceImplTest {
     @BeforeClass
     public static void beforeClass() {
         schemaService = mock(DOMSchemaService.class);
-        context = YangParserTestUtils.parseYangResourceDirectory("/yang");
+        context = YangParserTestUtils.parseYangResourceDirectory("/yang/base");
         when(schemaService.getGlobalContext()).thenReturn(context);
     }
 
