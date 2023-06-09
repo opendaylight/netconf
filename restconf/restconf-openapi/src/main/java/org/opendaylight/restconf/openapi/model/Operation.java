3caba5fa84487630369601f8868633ea926e7c51
OleksandrZharov <Oleksandr.Zharov@pantheon.tech>
2023-06-09 16:35:42 +0200

    Add Builder for Operations
    Added nested Builder for Operations. It help us to limit usage
    of null and increase readability.
    
    JIRA: NETCONF-1050
    Change-Id: Iaf931433872b82f0a36ca32847cc9da4baec2a3d
    Signed-off-by: OleksandrZharov <Oleksandr.Zharov@pantheon.tech>

--- restconf/restconf-openapi/src/main/java/org/opendaylight/restconf/openapi/model/Operation.java
+++ restconf/restconf-openapi/src/main/java/org/opendaylight/restconf/openapi/model/Operation.java
@@ -15,4 +15,90 @@ import com.fasterxml.jackson.databind.node.ObjectNode;
 public record Operation(boolean deprecated, ArrayNode tags, ArrayNode parameters, ArrayNode security, ArrayNode servers,
         ObjectNode callbacks, ObjectNode externalDocs, ObjectNode requestBody, ObjectNode responses,
         String description, String operationId, String summary) {
+
+    private Operation(final Builder builder) {
+        this(builder.deprecated, builder.tags, builder.parameters, builder.security, builder.servers, builder.callbacks,
+            builder.externalDocs, builder.requestBody, builder.responses, builder.description, builder.operationId,
+            builder.summary);
+    }
+
+    @SuppressWarnings("checkstyle:hiddenField")
+    public static class Builder {
+        private boolean deprecated;
+        private ArrayNode tags;
+        private ArrayNode parameters;
+        private ArrayNode security;
+        private ArrayNode servers;
+        private ObjectNode callbacks;
+        private ObjectNode externalDocs;
+        private ObjectNode requestBody;
+        private ObjectNode responses;
+        private String description;
+        private String operationId;
+        private String summary;
+
+        public Builder deprecated(final boolean deprecated) {
+            this.deprecated = deprecated;
+            return this;
+        }
+
+        public Builder tags(final ArrayNode tags) {
+            this.tags = tags;
+            return this;
+        }
+
+        public Builder parameters(final ArrayNode parameters) {
+            this.parameters = parameters;
+            return this;
+        }
+
+        public Builder security(final ArrayNode security) {
+            this.security = security;
+            return this;
+        }
+
+        public Builder servers(final ArrayNode servers) {
+            this.servers = servers;
+            return this;
+        }
+
+        public Builder callbacks(final ObjectNode callbacks) {
+            this.callbacks = callbacks;
+            return this;
+        }
+
+        public Builder externalDocs(final ObjectNode externalDocs) {
+            this.externalDocs = externalDocs;
+            return this;
+        }
+
+        public Builder requestBody(final ObjectNode requestBody) {
+            this.requestBody = requestBody;
+            return this;
+        }
+
+        public Builder responses(final ObjectNode responses) {
+            this.responses = responses;
+            return this;
+        }
+
+        public Builder description(final String description) {
+            this.description = description;
+            return this;
+        }
+
+        public Builder operationId(final String operationId) {
+            this.operationId = operationId;
+            return this;
+        }
+
+        public Builder summary(final String summary) {
+            this.summary = summary;
+            return this;
+        }
+
+        public Operation build() {
+            return new Operation(this);
+        }
+    }
 }
