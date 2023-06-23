77bf7a7161cb0df2f6ad6e4a86f9daf1c884d923
Yaroslav Lastivka <yaroslav.lastivka@pantheon.tech>
2023-06-23 14:00:49 +0300

    Declare discriminator in #getOpenApiSpec
    Logic of numbering for path parameters is broken.
    
    Discriminators was declaired as a Stack in #getOpenApiSpec.
    It allows to pass new discriminators to next recursions,
    and decrease number of loops in logic of creating uniqe
    parameter's names.
    
    JIRA: NETCONF-1060
    Change-Id: I37f70d5f66b6157e3bd95c323615f51b8dfbeeca
    Signed-off-by: Yaroslav Lastivka <yaroslav.lastivka@pantheon.tech>

--- restconf/restconf-openapi/src/test/java/org/opendaylight/restconf/openapi/impl/KeysMappingTest.java
+++ restconf/restconf-openapi/src/test/java/org/opendaylight/restconf/openapi/impl/KeysMappingTest.java
@@ -82,9 +82,9 @@ public class KeysMappingTest {
     @Test
     public void testKeysMapping() {
         var pathToList1 = "/rests/data/keys-mapping:multiple-key-list={name},{name2}/multiple-key-list2={name1},"
-            + "{name3}/multiple-key-list3={name31},{name4}/multiple-key-list4={name5}";
+            + "{name3}/multiple-key-list3={name32},{name34}/multiple-key-list4={name5}";
         assertTrue(doc.paths().containsKey(pathToList1));
-        assertEquals(List.of("name","name2", "name1", "name3", "name31", "name4", "name5"),
+        assertEquals(List.of("name","name2", "name1", "name3", "name32", "name34", "name5"),
             getPathParameters(doc.paths(), pathToList1));
     }
 }
