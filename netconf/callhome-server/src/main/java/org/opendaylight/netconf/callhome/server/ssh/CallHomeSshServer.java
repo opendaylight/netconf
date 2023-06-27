a396e6aa34b7afa1110b04e8779f183d8b8186f9
Samuel Schneider <samuel.schneider@pantheon.tech>
2023-06-27 09:52:21 +0200

    Make timeout and keep-alive configurable for Call-Home
    Make request timeout and keep-alive configurable for Call-Home
    services. And propagate connection timeout configuration
    correctly.
    
    Additionally, join configurations for both TLS and SSH services in
    to one so all configurations for callhome-provider bundle ar in one place.
    
    JIRA: NETCONF-1213
    Change-Id: I453d092bf3c5a77404e951cd4de5cce849727f2b
    Signed-off-by: Samuel Schneider <samuel.schneider@pantheon.tech>

--- netconf/callhome-server/src/main/java/org/opendaylight/netconf/callhome/server/ssh/CallHomeSshServer.java
+++ netconf/callhome-server/src/main/java/org/opendaylight/netconf/callhome/server/ssh/CallHomeSshServer.java
@@ -55,7 +55,7 @@ public final class CallHomeSshServer implements AutoCloseable {
     private final SSHClient client;
 
     @VisibleForTesting
-    CallHomeSshServer(final TcpServerGrouping tcpServerParams,
+    CallHomeSshServer(final TcpServerGrouping tcpServerParams, final long timeoutMillis,
             final SSHTransportStackFactory transportStackFactory,
             final NetconfClientSessionNegotiatorFactory negotiatorFactory,
             final CallHomeSshSessionContextManager contextManager,
@@ -81,7 +81,7 @@ public final class CallHomeSshServer implements AutoCloseable {
         };
         try {
             client = transportStackFactory.listenClient(TransportConstants.SSH_SUBSYSTEM, transportChannelListener,
-                tcpServerParams, sshClientParams, configurator).get(DEFAULT_TIMEOUT_MILLIS, TimeUnit.MILLISECONDS);
+                tcpServerParams, sshClientParams, configurator).get(timeoutMillis, TimeUnit.MILLISECONDS);
         } catch (UnsupportedConfigurationException | InterruptedException | ExecutionException | TimeoutException e) {
             throw new IllegalStateException("Could not start SSH Call-Home server", e);
         }
@@ -150,6 +150,7 @@ public final class CallHomeSshServer implements AutoCloseable {
     public static final class Builder {
         private InetAddress address;
         private int port = DEFAULT_PORT;
+        private long timeoutMillis = DEFAULT_TIMEOUT_MILLIS;
         private SSHTransportStackFactory transportStackFactory;
         private NetconfClientSessionNegotiatorFactory negotiationFactory;
         private CallHomeSshAuthProvider authProvider;
@@ -162,9 +163,9 @@ public final class CallHomeSshServer implements AutoCloseable {
 
         public @NonNull CallHomeSshServer build() {
             return new CallHomeSshServer(
-                toServerParams(address, port),
+                toServerParams(address, port), timeoutMillis,
                 transportStackFactory == null ? defaultTransportStackFactory() : transportStackFactory,
-                negotiationFactory == null ? defaultNegotiationFactory() : negotiationFactory,
+                negotiationFactory == null ? defaultNegotiationFactory(timeoutMillis) : negotiationFactory,
                 contextManager == null ? new CallHomeSshSessionContextManager() : contextManager,
                 authProvider, statusRecorder);
         }
@@ -203,6 +204,11 @@ public final class CallHomeSshServer implements AutoCloseable {
             this.negotiationFactory = newNegotiationFactory;
             return this;
         }
+
+        public Builder withTimeout(final int newTimeoutMillis) {
+            this.timeoutMillis = newTimeoutMillis;
+            return this;
+        }
     }
 
     private static TcpServerGrouping toServerParams(final InetAddress address, final int port) {
@@ -216,8 +222,8 @@ public final class CallHomeSshServer implements AutoCloseable {
         return new SSHTransportStackFactory("ssh-call-home-server", 0);
     }
 
-    private static NetconfClientSessionNegotiatorFactory defaultNegotiationFactory() {
+    private static NetconfClientSessionNegotiatorFactory defaultNegotiationFactory(final long timeoutMillis) {
         return new NetconfClientSessionNegotiatorFactory(new HashedWheelTimer(),
-            Optional.empty(), DEFAULT_TIMEOUT_MILLIS, DEFAULT_CLIENT_CAPABILITIES);
+            Optional.empty(), timeoutMillis, DEFAULT_CLIENT_CAPABILITIES);
     }
 }
