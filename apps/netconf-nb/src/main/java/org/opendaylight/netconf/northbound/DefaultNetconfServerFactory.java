067b3ff079d1d129a27ae4119a735057dfecf18d
Ruslan Kashapov <ruslan.kashapov@pantheon.tech>
2023-07-03 17:51:49 +0300

    Northbound Netconf servers moved to new transport implementation
    Deprecated NetconfServerDispatcher is replaced with usage of new
    NetconfServerFactory. Netty thread group provider was added as fix
    for incompatible netty-threadgroup-config (nio only, fails with epoll)
    from controller project.
    
    JIRA: NETCONF-1106
    Change-Id: I076020565b844ac7ddc4dfecc4b23154b825e4bf
    Signed-off-by: Ruslan Kashapov <ruslan.kashapov@pantheon.tech>

---
+++ apps/netconf-nb/src/main/java/org/opendaylight/netconf/northbound/DefaultNetconfServerFactory.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright (c) 2023 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.netconf.northbound;
+
+import static java.util.Objects.requireNonNull;
+
+import com.google.common.util.concurrent.ListenableFuture;
+import io.netty.channel.EventLoopGroup;
+import java.util.Map;
+import org.opendaylight.netconf.server.NetconfServerFactoryImpl;
+import org.opendaylight.netconf.server.ServerChannelInitializer;
+import org.opendaylight.netconf.server.api.NetconfServerFactory;
+import org.opendaylight.netconf.transport.api.UnsupportedConfigurationException;
+import org.opendaylight.netconf.transport.ssh.SSHServer;
+import org.opendaylight.netconf.transport.ssh.ServerFactoryManagerConfigurator;
+import org.opendaylight.netconf.transport.tcp.TCPServer;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.ssh.server.rev230417.SshServerGrouping;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.tcp.server.rev230417.TcpServerGrouping;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+
+@Component(factory = DefaultNetconfServerFactory.FACTORY_NAME, service = NetconfServerFactory.class)
+public final class DefaultNetconfServerFactory implements NetconfServerFactory {
+    static final String FACTORY_NAME = "org.opendaylight.netconf.northbound.DefaultNetconfServerFactory";
+
+    private static final String BOSS_PROP = ".bossGroup";
+    private static final String WORKER_PROP = ".workerGroup";
+    private static final String INITIALIZER_PROP = ".initializer";
+
+    private final NetconfServerFactory delegate;
+
+    @Activate
+    public DefaultNetconfServerFactory(final Map<String, ?> properties) {
+        this.delegate = new NetconfServerFactoryImpl(
+            OSGiNetconfServer.extractProp(properties, INITIALIZER_PROP, ServerChannelInitializer.class),
+            OSGiNetconfServer.extractProp(properties, BOSS_PROP, EventLoopGroup.class),
+            OSGiNetconfServer.extractProp(properties, WORKER_PROP, EventLoopGroup.class));
+    }
+
+    static Map<String, ?> props(final EventLoopGroup bossGroup, final EventLoopGroup workerGroup,
+            final ServerChannelInitializer initializer) {
+        return Map.of(
+            "type", "netconf-server-factory",
+            BOSS_PROP, requireNonNull(bossGroup),
+            WORKER_PROP, requireNonNull(workerGroup),
+            INITIALIZER_PROP, requireNonNull(initializer));
+    }
+
+    @Override
+    public ListenableFuture<TCPServer> createTcpServer(final TcpServerGrouping params)
+            throws UnsupportedConfigurationException {
+        return delegate.createTcpServer(params);
+    }
+
+    @Override
+    public ListenableFuture<SSHServer> createSshServer(final TcpServerGrouping tcpParams,
+            final SshServerGrouping sshParams) throws UnsupportedConfigurationException {
+        return delegate.createSshServer(tcpParams, sshParams);
+    }
+
+    @Override
+    public ListenableFuture<SSHServer> createSshServer(final TcpServerGrouping tcpParams,
+            final SshServerGrouping sshParams, final ServerFactoryManagerConfigurator configurator)
+            throws UnsupportedConfigurationException {
+        return delegate.createSshServer(tcpParams, sshParams, configurator);
+    }
+}
--- apps/netconf-nb/src/main/java/org/opendaylight/netconf/northbound/NetconfNorthboundSshServer.java
+++ apps/netconf-nb/src/main/java/org/opendaylight/netconf/northbound/NetconfNorthboundSshServer.java
@@ -7,19 +7,21 @@
  */
 package org.opendaylight.netconf.northbound;
 
-import io.netty.channel.ChannelFuture;
-import io.netty.channel.EventLoopGroup;
-import io.netty.channel.local.LocalAddress;
-import io.netty.util.concurrent.EventExecutor;
 import java.io.IOException;
-import java.net.InetSocketAddress;
-import java.util.concurrent.Executors;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
 import org.opendaylight.netconf.auth.AuthProvider;
-import org.opendaylight.netconf.northbound.ssh.SshProxyServer;
-import org.opendaylight.netconf.northbound.ssh.SshProxyServerConfigurationBuilder;
-import org.opendaylight.netconf.server.api.NetconfServerDispatcher;
+import org.opendaylight.netconf.server.api.NetconfServerFactory;
+import org.opendaylight.netconf.shaded.sshd.server.auth.password.UserAuthPasswordFactory;
 import org.opendaylight.netconf.shaded.sshd.server.keyprovider.SimpleGeneratorHostKeyProvider;
+import org.opendaylight.netconf.transport.api.UnsupportedConfigurationException;
+import org.opendaylight.netconf.transport.ssh.SSHServer;
+import org.opendaylight.netconf.transport.ssh.ServerFactoryManagerConfigurator;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev130715.IetfInetUtil;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev130715.PortNumber;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.netconf.server.rev230417.netconf.server.listen.stack.grouping.transport.ssh.ssh.TcpServerParameters;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.netconf.server.rev230417.netconf.server.listen.stack.grouping.transport.ssh.ssh.TcpServerParametersBuilder;
+import org.opendaylight.yangtools.yang.common.Uint16;
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Deactivate;
@@ -46,64 +48,45 @@ public final class NetconfNorthboundSshServer implements AutoCloseable {
 
     private static final Logger LOG = LoggerFactory.getLogger(NetconfNorthboundSshServer.class);
 
-    private final ChannelFuture localServer;
-    private final SshProxyServer sshProxyServer;
+    private final SSHServer sshServer;
 
     @Activate
     public NetconfNorthboundSshServer(
-            @Reference final NetconfServerDispatcher netconfServerDispatcher,
-            @Reference(target = "(type=global-worker-group)") final EventLoopGroup workerGroup,
-            @Reference(target = "(type=global-event-executor)") final EventExecutor eventExecutor,
+            @Reference final NetconfServerFactory netconfServerFactory,
             @Reference(target = "(type=netconf-auth-provider)") final AuthProvider authProvider,
             final Configuration configuration) {
-        this(netconfServerDispatcher, workerGroup, eventExecutor, authProvider, configuration.bindingAddress(),
-            configuration.portNumber());
+        this(netconfServerFactory, authProvider, configuration.bindingAddress(), configuration.portNumber());
     }
 
-    public NetconfNorthboundSshServer(final NetconfServerDispatcher netconfServerDispatcher,
-            final EventLoopGroup workerGroup, final EventExecutor eventExecutor, final AuthProvider authProvider,
-            final String bindingAddress, final int portNumber) {
-        final LocalAddress localAddress = new LocalAddress(String.valueOf(portNumber));
-        final var sshProxyServerConfiguration = new SshProxyServerConfigurationBuilder()
-            .setBindingAddress(getInetAddress(bindingAddress, portNumber))
-            .setLocalAddress(localAddress)
-            .setAuthenticator(authProvider)
-            .setIdleTimeout(Integer.MAX_VALUE)
-            .setKeyPairProvider(new SimpleGeneratorHostKeyProvider())
-            .createSshProxyServerConfiguration();
+    public NetconfNorthboundSshServer(final NetconfServerFactory netconfServerFactory,
+            final AuthProvider authProvider, final String bindingAddress, final int portNumber) {
 
-        localServer = netconfServerDispatcher.createLocalServer(localAddress);
-        sshProxyServer = new SshProxyServer(Executors.newScheduledThreadPool(1), workerGroup, eventExecutor);
+        final TcpServerParameters tcpConfig = new TcpServerParametersBuilder()
+            .setLocalAddress(IetfInetUtil.ipAddressFor(bindingAddress))
+            .setLocalPort(new PortNumber(Uint16.valueOf(portNumber))).build();
 
-        localServer.addListener(future -> {
-            if (future.isDone() && !future.isCancelled()) {
-                try {
-                    sshProxyServer.bind(sshProxyServerConfiguration);
-                } catch (final IOException e) {
-                    throw new IllegalStateException("Unable to start SSH netconf server", e);
-                }
-                LOG.info("Netconf SSH endpoint started successfully at {}", bindingAddress);
-            } else {
-                LOG.warn("Unable to start SSH netconf server at {}", bindingAddress, future.cause());
-                throw new IllegalStateException("Unable to start SSH netconf server", future.cause());
-            }
-        });
+        final ServerFactoryManagerConfigurator configurator = factoryMgr -> {
+            factoryMgr.setUserAuthFactories(List.of(new UserAuthPasswordFactory()));
+            factoryMgr.setPasswordAuthenticator(
+                (username, password, session) -> authProvider.authenticated(username, password));
+            factoryMgr.setKeyPairProvider(new SimpleGeneratorHostKeyProvider());
+        };
+
+        try {
+            sshServer = netconfServerFactory.createSshServer(tcpConfig, null, configurator).get();
+        } catch (UnsupportedConfigurationException | ExecutionException | InterruptedException e) {
+            LOG.warn("Could not start SSH netconf server at {}", bindingAddress, e);
+            throw new IllegalStateException("Unable to start SSH netconf server", e);
+        }
     }
 
     @Deactivate
     @Override
     public void close() throws IOException {
-        sshProxyServer.close();
-
-        if (localServer.isDone()) {
-            localServer.channel().close();
-        } else {
-            localServer.cancel(true);
+        try {
+            sshServer.shutdown().get();
+        } catch (ExecutionException | InterruptedException e) {
+            LOG.warn("Could not stop SSH netconf server", e);
         }
-    }
-
-    private static InetSocketAddress getInetAddress(final String bindingAddress, final int portNumber) {
-        final var ipAddress = IetfInetUtil.ipAddressFor(bindingAddress);
-        return new InetSocketAddress(IetfInetUtil.inetAddressFor(ipAddress), portNumber);
     }
 }
