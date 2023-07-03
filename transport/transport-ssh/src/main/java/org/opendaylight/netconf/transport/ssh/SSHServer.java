b5c55895f55cc6b5c370b76e8af30eb2374843a5
Ruslan Kashapov <ruslan.kashapov@pantheon.tech>
2023-07-03 13:51:41 +0300

    External service integration support for ssh transport.
    Existing authentication service expected to be used
    for ssh authentication when server is running in karaf.
    
    JIRA: NETCONF-590
    Change-Id: I1c6df46ee165a78f7bdd87bd7597ae8f35f75715
    Signed-off-by: Ruslan Kashapov <ruslan.kashapov@pantheon.tech>

--- transport/transport-ssh/src/main/java/org/opendaylight/netconf/transport/ssh/SSHServer.java
+++ transport/transport-ssh/src/main/java/org/opendaylight/netconf/transport/ssh/SSHServer.java
@@ -7,6 +7,7 @@
  */
 package org.opendaylight.netconf.transport.ssh;
 
+import static com.google.common.base.Preconditions.checkArgument;
 import static java.util.Objects.requireNonNull;
 
 import com.google.common.collect.ImmutableList;
@@ -67,30 +68,58 @@ public final class SSHServer extends SSHTransportStack {
     public static @NonNull ListenableFuture<SSHServer> connect(final TransportChannelListener listener,
             final Bootstrap bootstrap, final TcpClientGrouping connectParams, final SshServerGrouping serverParams)
             throws UnsupportedConfigurationException {
-        final var server = new SSHServer(listener, newFactoryManager(serverParams));
+        final var server = new SSHServer(listener, newFactoryManager(requireNonNull(serverParams), null));
         return transformUnderlay(server, TCPClient.connect(server.asListener(), bootstrap, connectParams));
     }
 
     public static @NonNull ListenableFuture<SSHServer> listen(final TransportChannelListener listener,
             final ServerBootstrap bootstrap, final TcpServerGrouping connectParams,
-            final SshServerGrouping serverParams)
+            final SshServerGrouping serverParams) throws UnsupportedConfigurationException {
+        requireNonNull(serverParams);
+        return listen(listener, bootstrap, connectParams, serverParams, null);
+    }
+
+    /**
+     * Builds and starts SSH Server.
+     *
+     * @param listener server channel listener, required
+     * @param bootstrap server bootstrap instance, required
+     * @param connectParams tcp transport configuration, required
+     * @param serverParams ssh overlay configuration, optional if configurator is defined, required otherwise
+     * @param configurator server factory manager configurator, optional if serverParams is defined, required otherwise
+     * @return server instance as listenable future
+     * @throws UnsupportedConfigurationException if any of configurations is invalid or incomplete
+     * @throws NullPointerException if any of required parameters is null
+     * @throws IllegalArgumentException if both configurator and serverParams are null
+     */
+    public static @NonNull ListenableFuture<SSHServer> listen(final TransportChannelListener listener,
+            final ServerBootstrap bootstrap, final TcpServerGrouping connectParams,
+            final SshServerGrouping serverParams, final ServerFactoryManagerConfigurator configurator)
             throws UnsupportedConfigurationException {
-        final var server = new SSHServer(listener, newFactoryManager(serverParams));
+        checkArgument(serverParams != null || configurator != null,
+            "Neither server parameters nor factory configurator is defined");
+        final var factoryMgr = newFactoryManager(serverParams, configurator);
+        final var server = new SSHServer(listener, factoryMgr);
         return transformUnderlay(server, TCPServer.listen(server.asListener(), bootstrap, connectParams));
     }
 
     private static ServerFactoryManager newFactoryManager(
-            final SshServerGrouping serverParams)
+            final @Nullable SshServerGrouping serverParams,
+            final @Nullable ServerFactoryManagerConfigurator configurator)
             throws UnsupportedConfigurationException {
-        var factoryMgr = SshServer.setUpDefaultServer();
 
-        ConfigUtils.setTransportParams(factoryMgr, serverParams.getTransportParams());
-        ConfigUtils.setKeepAlives(factoryMgr, serverParams.getKeepalives());
-        setServerIdentity(factoryMgr, serverParams.getServerIdentity());
-        setClientAuthentication(factoryMgr, serverParams.getClientAuthentication());
-
+        final var factoryMgr = SshServer.setUpDefaultServer();
+        if (serverParams != null) {
+            ConfigUtils.setTransportParams(factoryMgr, serverParams.getTransportParams());
+            ConfigUtils.setKeepAlives(factoryMgr, serverParams.getKeepalives());
+            setServerIdentity(factoryMgr, serverParams.getServerIdentity());
+            setClientAuthentication(factoryMgr, serverParams.getClientAuthentication());
+        }
         factoryMgr.setServiceFactories(SshServer.DEFAULT_SERVICE_FACTORIES);
         factoryMgr.setScheduledExecutorService(ThreadUtils.newSingleThreadScheduledExecutor(""));
+        if (configurator != null) {
+            configurator.configureServerFactoryManager(factoryMgr);
+        }
         return factoryMgr;
     }
 
