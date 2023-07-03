b6174c45503c66b98e43318d812166ec59551918
Ruslan Kashapov <ruslan.kashapov@pantheon.tech>
2023-07-03 17:51:49 +0300

    Northbound Netconf servers moved to new transport implementation
    Deprecated NetconfServerDispatcher is replaced with usage of new
    NetconfServerFactory. Netty thread group provider was added as fix
    for incompatible netty-threadgroup-config (nio only, fails with epoll)
    from controller project.
    
    JIRA: NETCONF-590
    Change-Id: I076020565b844ac7ddc4dfecc4b23154b825e4bf
    Signed-off-by: Ruslan Kashapov <ruslan.kashapov@pantheon.tech>

---
+++ apps/netconf-nb/src/main/java/org/opendaylight/netconf/northbound/OSGiNetconfThreadGroups.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright (c) 2023 PANTHEON.tech s.r.o. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.netconf.northbound;
+
+import io.netty.channel.EventLoopGroup;
+import org.opendaylight.netconf.transport.tcp.NettyTransportSupport;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.service.metatype.annotations.AttributeDefinition;
+import org.osgi.service.metatype.annotations.Designate;
+import org.osgi.service.metatype.annotations.ObjectClassDefinition;
+
+/**
+ * Component provides Netty thread group instances for NETCONF northbound servers.
+ *
+ * <p> The artifact substitutes the thread groups provided via
+ * org.opendaylight.controller:netty-threadgroup-config module (delivered via
+ * org.opendaylight.controller:odl-controller-exp-netty-config) because of
+ * incompatibility with new netconf transport implementation. Same configuration
+ * (configurationPid) is used as before.
+ */
+@Component(service = OSGiNetconfThreadGroups.class, configurationPid = "org.opendaylight.netty.threadgroup")
+@Designate(ocd = OSGiNetconfThreadGroups.Configuration.class)
+public final class OSGiNetconfThreadGroups {
+
+    @ObjectClassDefinition
+    public @interface Configuration {
+        @AttributeDefinition(name = "global-boss-group-thread-count")
+        int bossThreadCount() default 0;
+
+        @AttributeDefinition(name = "global-worker-group-thread-count")
+        int workerThreadCount() default 0;
+    }
+
+    private final EventLoopGroup bossGroup;
+    private final EventLoopGroup workerGroup;
+
+    @Activate
+    public OSGiNetconfThreadGroups(final Configuration configuration) {
+        bossGroup = buildEventLoopGroup("netconf-boss-group", configuration.bossThreadCount());
+        workerGroup = buildEventLoopGroup("netconf-worker-group", configuration.workerThreadCount());
+    }
+
+    @Deactivate
+    public void deactivate() {
+        bossGroup.shutdownGracefully();
+        workerGroup.shutdownGracefully();
+    }
+
+    public EventLoopGroup getBossGroup() {
+        return bossGroup;
+    }
+
+    public EventLoopGroup getWorkerGroup() {
+        return workerGroup;
+    }
+
+    private static EventLoopGroup buildEventLoopGroup(final String name, final int threads) {
+        return NettyTransportSupport.newEventLoopGroup(name, Math.max(threads, 0));
+    }
+}
