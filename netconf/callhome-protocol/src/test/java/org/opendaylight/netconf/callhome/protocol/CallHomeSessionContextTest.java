d350f56310c5e0241859ec98146272adbbb17842
Peter Suna <peter.suna@pantheon.tech>
2023-07-04 13:15:15 +0200

    Fix tests formating in callhome-protocol
    - Use final var for variables.
    - Move variables closer to their actual usage.
    - Use Exception in tests
    - Move class atributes to local varibale if it is possible.
    
    Change-Id: Icdae07a71d0f24e38f12bcebb7ea74438e0b11a4
    Signed-off-by: Peter Suna <peter.suna@pantheon.tech>

--- netconf/callhome-protocol/src/test/java/org/opendaylight/netconf/callhome/protocol/CallHomeSessionContextTest.java
+++ netconf/callhome-protocol/src/test/java/org/opendaylight/netconf/callhome/protocol/CallHomeSessionContextTest.java
@@ -24,13 +24,13 @@ import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelPipeline;
 import io.netty.channel.DefaultChannelPipeline;
 import io.netty.channel.EventLoopGroup;
-import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.security.PublicKey;
 import org.junit.Before;
 import org.junit.Test;
 import org.opendaylight.netconf.client.NetconfClientSessionListener;
 import org.opendaylight.netconf.client.NetconfClientSessionNegotiatorFactory;
+import org.opendaylight.netconf.callhome.protocol.CallHomeSessionContext.Factory;
 import org.opendaylight.netconf.nettyutil.handler.ssh.client.NetconfClientSessionImpl;
 import org.opendaylight.netconf.nettyutil.handler.ssh.client.NettyPipelineAwareChannelSubsystem;
 import org.opendaylight.netconf.shaded.sshd.client.channel.ClientChannel;
@@ -49,50 +49,42 @@ public class CallHomeSessionContextTest {
     private NetconfClientSessionImpl mockSession;
     private CallHomeAuthorization mockAuth;
     private ClientChannel mockChannel;
-    private InetSocketAddress address;
-
-    private ReverseSshChannelInitializer mockChannelInitializer;
-    private CallHomeNetconfSubsystemListener subListener;
     private EventLoopGroup mockNettyGroup;
-    private CallHomeSessionContext.Factory realFactory;
+    private Factory realFactory;
     private CallHomeSessionContext instance;
-    private NetconfClientSessionNegotiatorFactory mockNegotiatior;
 
     @Before
     public void setup() {
         mockSession = mock(NetconfClientSessionImpl.class);
         mockAuth = mock(CallHomeAuthorization.class);
         mockChannel = mock(ClientChannel.class);
-        address = mock(InetSocketAddress.class);
 
-        mockNegotiatior = mock(NetconfClientSessionNegotiatorFactory.class);
-        subListener = mock(CallHomeNetconfSubsystemListener.class);
+        final var mockNegotiatior = mock(NetconfClientSessionNegotiatorFactory.class);
+        final var subListener = mock(CallHomeNetconfSubsystemListener.class);
         mockNettyGroup = mock(EventLoopGroup.class);
+        realFactory = new Factory(mockNettyGroup, mockNegotiatior, subListener);
 
-        realFactory = new CallHomeSessionContext.Factory(mockNettyGroup, mockNegotiatior, subListener);
-
-        KeyExchange kexMock = mock(KeyExchange.class);
+        final var kexMock = mock(KeyExchange.class);
         doReturn(kexMock).when(mockSession).getKex();
-
-        PublicKey keyMock = mock(PublicKey.class);
+        final var keyMock = mock(PublicKey.class);
         doReturn(keyMock).when(mockSession).getServerKey();
-        IoReadFuture mockFuture = mock(IoReadFuture.class);
-        IoInputStream mockIn = mock(IoInputStream.class);
-        doReturn(mockFuture).when(mockIn).read(any(Buffer.class));
-        IoOutputStream mockOut = mock(IoOutputStream.class);
 
+        final var mockFuture = mock(IoReadFuture.class);
+        final var mockIn = mock(IoInputStream.class);
+        doReturn(mockFuture).when(mockIn).read(any(Buffer.class));
+        final var mockOut = mock(IoOutputStream.class);
         doReturn(mockIn).when(mockChannel).getAsyncOut();
         doReturn(mockOut).when(mockChannel).getAsyncIn();
 
-        doReturn(true).when(mockAuth).isServerAllowed();
-
-        IoSession ioSession = mock(IoSession.class);
+        final var ioSession = mock(IoSession.class);
         doReturn(ioSession).when(mockSession).getIoSession();
+        final var address = mock(InetSocketAddress.class);
         doReturn(address).when(ioSession).getRemoteAddress();
         doReturn(null).when(mockSession).setAttribute(any(AttributeKey.class), any());
         doReturn(null).when(mockSession).getAttribute(any(AttributeKey.class));
         doReturn("testSession").when(mockSession).toString();
 
+        doReturn(true).when(mockAuth).isServerAllowed();
         doNothing().when(mockAuth).applyTo(mockSession);
         doReturn("test").when(mockAuth).getSessionName();
     }
@@ -113,7 +105,7 @@ public class CallHomeSessionContextTest {
     }
 
     @Test
-    public void anAuthorizeActionShouldApplyToTheBoundSession() throws IOException {
+    public void anAuthorizeActionShouldApplyToTheBoundSession() throws Exception {
         instance = realFactory.createIfNotExists(mockSession, mockAuth);
         // when
         doReturn(null).when(mockSession).auth();
@@ -123,10 +115,10 @@ public class CallHomeSessionContextTest {
     }
 
     @Test
-    public void creatingAChannelSuccessfullyShouldResultInAnAttachedListener() throws IOException {
+    public void creatingAChannelSuccessfullyShouldResultInAnAttachedListener() throws Exception {
         // given
-        OpenFuture mockFuture = mock(OpenFuture.class);
-        NettyPipelineAwareChannelSubsystem mockChannelSubsystem = mock(NettyPipelineAwareChannelSubsystem.class);
+        final var mockFuture = mock(OpenFuture.class);
+        final var mockChannelSubsystem = mock(NettyPipelineAwareChannelSubsystem.class);
         doReturn(mockFuture).when(mockChannelSubsystem).open();
         doReturn(mockChannelSubsystem).when(mockSession).createSubsystemChannel(anyString(),
                 any(DefaultChannelPipeline.class));
@@ -143,30 +135,30 @@ public class CallHomeSessionContextTest {
     @Test
     public void openingTheChannelSuccessfullyNotifyTheChannelListener() {
         // given
-        MinaSshNettyChannel mockMinaChannel = mock(MinaSshNettyChannel.class);
-        CallHomeSessionContext.Factory mockFactory = mock(CallHomeSessionContext.Factory.class);
-
-        CallHomeNetconfSubsystemListener mockListener = mock(CallHomeNetconfSubsystemListener.class);
+        final var mockListener = mock(CallHomeNetconfSubsystemListener.class);
         doNothing().when(mockListener).onNetconfSubsystemOpened(any(CallHomeProtocolSessionContext.class),
                 any(CallHomeChannelActivator.class));
 
-        ChannelFuture mockChanFuture = mock(ChannelFuture.class);
+        final var mockChanFuture = mock(ChannelFuture.class);
         doReturn(mockChanFuture).when(mockNettyGroup).register(any(Channel.class));
 
+        final var mockFactory = mock(Factory.class);
         doReturn(mockNettyGroup).when(mockFactory).getNettyGroup();
+        final var mockChannelInitializer = mock(ReverseSshChannelInitializer.class);
         doReturn(mockChannelInitializer).when(mockFactory)
                 .getChannelInitializer(any(NetconfClientSessionListener.class));
         doReturn(mockListener).when(mockFactory).getChannelOpenListener();
 
-        ChannelPipeline mockPipeline = mock(ChannelPipeline.class);
+        final var mockPipeline = mock(ChannelPipeline.class);
+        final var mockMinaChannel = mock(MinaSshNettyChannel.class);
         doReturn(mockPipeline).when(mockMinaChannel).pipeline();
 
-        OpenFuture mockFuture = mock(OpenFuture.class);
+        final var mockFuture = mock(OpenFuture.class);
         doReturn(true).when(mockFuture).isOpened();
 
         instance = spy(new CallHomeSessionContext(mockSession, mockAuth, mockFactory));
         doReturn(mockMinaChannel).when(instance).newMinaSshNettyChannel();
-        SshFutureListener<OpenFuture> listener = instance.newSshFutureListener(mockChannel, mockMinaChannel);
+        final var listener = instance.newSshFutureListener(mockChannel, mockMinaChannel);
 
         // when
         listener.operationComplete(mockFuture);
@@ -179,14 +171,13 @@ public class CallHomeSessionContextTest {
     public void failureToOpenTheChannelShouldCauseTheSessionToClose() {
         // given
         instance = realFactory.createIfNotExists(mockSession, mockAuth);
-        OpenFuture mockFuture = mock(OpenFuture.class);
+        final var mockFuture = mock(OpenFuture.class);
         doReturn(false).when(mockFuture).isOpened();
         doReturn(new RuntimeException("test")).when(mockFuture).getException();
-
         doReturn(null).when(mockSession).close(anyBoolean());
 
         // when
-        SshFutureListener<OpenFuture> listener = instance.newSshFutureListener(mockChannel, null);
+        final var listener = instance.newSshFutureListener(mockChannel, null);
         listener.operationComplete(mockFuture);
         // then
         // You'll see an error message logged to the console - it is expected.
