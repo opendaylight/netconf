cc7853e0f09223e4084dfc91649047b935bce323
Šimon Ukuš <simon.ukus@pantheon.tech>
2023-07-07 12:28:56 +0200

    Convert securitySchema's basicAuth field to record
    This change replaces the JsonNode for a custom Java record that
    contains only fields relevant to the Basic HTTP authentication scheme.
    
    JIRA: NETCONF-1075
    Change-Id: I2a53b9002871e19808eef95739a07f5acd958815
    Signed-off-by: Šimon Ukuš <simon.ukus@pantheon.tech>

--- restconf/restconf-openapi/src/main/java/org/opendaylight/restconf/openapi/model/SecuritySchemes.java
+++ restconf/restconf-openapi/src/main/java/org/opendaylight/restconf/openapi/model/SecuritySchemes.java
@@ -7,10 +7,78 @@
  */
 package org.opendaylight.restconf.openapi.model;
 
+import static com.google.common.base.Verify.verifyNotNull;
+
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
-import com.fasterxml.jackson.databind.node.ObjectNode;
+import org.eclipse.jdt.annotation.NonNull;
+import org.eclipse.jdt.annotation.Nullable;
 
 @JsonInclude(Include.NON_NULL)
-public record SecuritySchemes(ObjectNode basicAuth) {
+public record SecuritySchemes(SecurityObject basicAuth) {
+
+    /**
+     * This record serves as a Security Scheme Object as defined in
+     * <a href=https://swagger.io/specification/#security-scheme-object>Swagger Specification</a> for HTTP type.
+     *
+     * <p>
+     * Only fields, supported by <b>HTTP</b> Security Scheme Object type, are implemented
+     *
+     * @param type type of security scheme object
+     * @param description description for security scheme
+     * @param scheme name of the HTTP Authorization scheme to be used in the Authorization header
+     * @param bearerFormat hint to the client to identify how the bearer token is formatted
+     */
+    @JsonInclude(Include.NON_NULL)
+    public record SecurityObject(
+        @NonNull Type type,
+        @Nullable String description,
+        @NonNull String scheme,
+        @Nullable String bearerFormat) {
+
+        private SecurityObject(final Builder builder) {
+            this(builder.type, builder.description, builder.scheme, builder.bearerFormat);
+        }
+
+        @SuppressWarnings("checkstyle:hiddenField")
+        public static class Builder {
+            private Type type;
+            private String description;
+            private String scheme;
+            private String bearerFormat;
+
+            public Builder type(final Type type) {
+                this.type = type;
+                return this;
+            }
+
+            public Builder description(final String description) {
+                this.description = description;
+                return this;
+            }
+
+            public Builder scheme(final String scheme) {
+                this.scheme = scheme;
+                return this;
+            }
+
+            public Builder bearerFormat(final String bearerFormat) {
+                this.bearerFormat = bearerFormat;
+                return this;
+            }
+
+            public SecurityObject build() {
+                verifyNotNull(type, "The Security Scheme Object's type must not be null");
+                verifyNotNull(scheme, "The Security Scheme Object's scheme must not be null");
+                return new SecurityObject(this);
+            }
+        }
+    }
+
+    public enum Type {
+        http
+        // https://swagger.io/specification/#security-scheme-object specifies valid type values:
+        // "apiKey", "http" "mutualTLS", "oauth2", "openIdConnect",
+        // we are only using the "http" (which might change in the future => extend the Type enum)
+    }
 }
