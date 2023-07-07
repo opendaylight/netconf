0807fda678e19886a43a9b3ceb9ce82540b61d9c
Šimon Ukuš <simon.ukus@pantheon.tech>
2023-07-07 12:28:56 +0200

    Allow multiple security entries in securitySchema
    The swagger docs specifies that the securitySchema is a map of entries
    with the key being a custom name of the security method and the value
    being an instance of a Security Scheme Object.
    
    This change replaces the use of JsonNode etc., which added extra
    overhead, for classes from java API and custom records.
    It reworks the securitySchemes field in Components to be a map
    and introduces an interface SecuritySchemeObject to be implemented
    by records that represent the specific security object type.
    
    It also introduces an Http record - an implementation of said interface.
    Others might come in the future.
    
    JIRA: NETCONF-1075
    Change-Id: I2a53b9002871e19808eef95739a07f5acd958815
    Signed-off-by: Šimon Ukuš <simon.ukus@pantheon.tech>

---
+++ restconf/restconf-openapi/src/main/java/org/opendaylight/restconf/openapi/model/security/Http.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright (c) 2023 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.restconf.openapi.model.security;
+
+import static com.google.common.base.Verify.verifyNotNull;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import org.eclipse.jdt.annotation.NonNull;
+import org.eclipse.jdt.annotation.Nullable;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public record Http(
+    @NonNull String scheme,
+    @Nullable String description,
+    @Nullable String bearerFormat) implements SecuritySchemeObject {
+    private static final Type TYPE = Type.http;
+
+    private Http(final Builder builder) {
+        this(builder.scheme, builder.description, builder.bearerFormat);
+    }
+
+    @Override
+    public Type getType() {
+        return TYPE;
+    }
+
+    @SuppressWarnings("checkstyle:hiddenField")
+    public static class Builder {
+        private String description;
+        private String scheme;
+        private String bearerFormat;
+
+        public Builder description(final String description) {
+            this.description = description;
+            return this;
+        }
+
+        public Builder scheme(final String scheme) {
+            this.scheme = scheme;
+            return this;
+        }
+
+        public Builder bearerFormat(final String bearerFormat) {
+            this.bearerFormat = bearerFormat;
+            return this;
+        }
+
+        public Http build() {
+            verifyNotNull(scheme, "The Security Scheme Object's scheme must not be null");
+            return new Http(this);
+        }
+    }
+}
