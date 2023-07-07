5bd0901fd6d3e9a899216115556f5e49cc4882c7
Šimon Ukuš <simon.ukus@pantheon.tech>
2023-07-07 12:28:56 +0200

    Allow multiple security entries in securitySchema
    The swagger docs specifies that the securitySchema is a map of entries
    with the key being a custom name of the security method and the value
    being an instance of a Security Scheme Object.
    
    This change replaces the use of JsonNode etc., which added extra
    overhead, for classes from java API and custom records.
    It reworks the securitySchemes field in Components to be a map
    and introduces an interface SecuritySchemeObject to be implemented
    by records that represent the specific security object type.
    
    It also introduces an Http record - an implementation of said interface.
    Others might come in the future.
    
    JIRA: NETCONF-1075
    Change-Id: I2a53b9002871e19808eef95739a07f5acd958815
    Signed-off-by: Šimon Ukuš <simon.ukus@pantheon.tech>
    Signed-off-by: Ivan Hrasko <ivan.hrasko@pantheon.tech>

---
+++ restconf/restconf-openapi/src/main/java/org/opendaylight/restconf/openapi/model/security/SecuritySchemeObject.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2023 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.restconf.openapi.model.security;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public interface SecuritySchemeObject {
+    @JsonProperty("type")
+    Type type();
+
+    /**
+     * Security types enumeration.
+     *
+     * <p>
+     * <a href="https://swagger.io/specification/#security-scheme-object">OpenApi spec</a> specifies valid type values:
+     * "apiKey", "http" "mutualTLS", "oauth2", "openIdConnect".
+     *
+     * <p>
+     * We are only using the "http" which might change in the future, so we will extend the Type enum.
+     */
+    enum Type {
+        http
+    }
+}
