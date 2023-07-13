aa2c1770f2e2b52cbe647ea9f83fb47b9e890255
Peter Suna <peter.suna@pantheon.tech>
2023-07-13 17:55:39 +0200

    Remove "config" prefix from OpenApi
    Remove "config" prefix from OpenApi schemas node.
    It is no longer required as configurational data and operational
    data are not being created multiple times.
    
    JIRA: NETCONF-982
    Change-Id: I56be54dcb9a36cc41c4955ae40aa9f94b701bd83
    Signed-off-by: Peter Suna <peter.suna@pantheon.tech>

--- restconf/restconf-openapi/src/test/java/org/opendaylight/restconf/openapi/impl/OpenApiGeneratorRFC8040Test.java
+++ restconf/restconf-openapi/src/test/java/org/opendaylight/restconf/openapi/impl/OpenApiGeneratorRFC8040Test.java
@@ -39,9 +39,9 @@ public final class OpenApiGeneratorRFC8040Test {
     private static final String TOASTER_2 = "toaster2";
     private static final String REVISION_DATE = "2009-11-20";
     private static final String MANDATORY_TEST = "mandatory-test";
-    private static final String CONFIG_ROOT_CONTAINER = "mandatory-test_config_root-container";
-    private static final String CONFIG_MANDATORY_CONTAINER = "mandatory-test_root-container_config_mandatory-container";
-    private static final String CONFIG_MANDATORY_LIST = "mandatory-test_root-container_config_mandatory-list";
+    private static final String CONFIG_ROOT_CONTAINER = "mandatory-test_root-container";
+    private static final String CONFIG_MANDATORY_CONTAINER = "mandatory-test_root-container_mandatory-container";
+    private static final String CONFIG_MANDATORY_LIST = "mandatory-test_root-container_mandatory-list";
     private static final String MANDATORY_TEST_MODULE = "mandatory-test_module";
 
     private static EffectiveModelContext context;
@@ -114,47 +114,47 @@ public final class OpenApiGeneratorRFC8040Test {
         final Map<String, Schema> schemas = doc.components().schemas();
         assertNotNull(schemas);
 
-        final Schema configLstTop = schemas.get("toaster2_config_lst_TOP");
+        final Schema configLstTop = schemas.get("toaster2_lst_TOP");
         assertNotNull(configLstTop);
-        DocGenTestHelper.containsReferences(configLstTop, "lst", "#/components/schemas/toaster2_config_lst");
+        DocGenTestHelper.containsReferences(configLstTop, "lst", "#/components/schemas/toaster2_lst");
 
-        final Schema configLst = schemas.get("toaster2_config_lst");
+        final Schema configLst = schemas.get("toaster2_lst");
         assertNotNull(configLst);
-        DocGenTestHelper.containsReferences(configLst, "lst1", "#/components/schemas/toaster2_lst_config_lst1");
-        DocGenTestHelper.containsReferences(configLst, "cont1", "#/components/schemas/toaster2_lst_config_cont1");
+        DocGenTestHelper.containsReferences(configLst, "lst1", "#/components/schemas/toaster2_lst_lst1");
+        DocGenTestHelper.containsReferences(configLst, "cont1", "#/components/schemas/toaster2_lst_cont1");
 
-        final Schema configLst1Top = schemas.get("toaster2_lst_config_lst1_TOP");
+        final Schema configLst1Top = schemas.get("toaster2_lst_lst1_TOP");
         assertNotNull(configLst1Top);
-        DocGenTestHelper.containsReferences(configLst1Top, "lst1", "#/components/schemas/toaster2_lst_config_lst1");
+        DocGenTestHelper.containsReferences(configLst1Top, "lst1", "#/components/schemas/toaster2_lst_lst1");
 
-        final Schema configLst1 = schemas.get("toaster2_lst_config_lst1");
+        final Schema configLst1 = schemas.get("toaster2_lst_lst1");
         assertNotNull(configLst1);
 
-        final Schema configCont1Top = schemas.get("toaster2_lst_config_cont1_TOP");
+        final Schema configCont1Top = schemas.get("toaster2_lst_cont1_TOP");
         assertNotNull(configCont1Top);
-        DocGenTestHelper.containsReferences(configCont1Top, "cont1", "#/components/schemas/toaster2_lst_config_cont1");
+        DocGenTestHelper.containsReferences(configCont1Top, "cont1", "#/components/schemas/toaster2_lst_cont1");
 
-        final Schema configCont1 = schemas.get("toaster2_lst_config_cont1");
+        final Schema configCont1 = schemas.get("toaster2_lst_cont1");
         assertNotNull(configCont1);
         DocGenTestHelper.containsReferences(configCont1, "cont11",
-                "#/components/schemas/toaster2_lst_cont1_config_cont11");
+                "#/components/schemas/toaster2_lst_cont1_cont11");
         DocGenTestHelper.containsReferences(configCont1, "lst11",
-                "#/components/schemas/toaster2_lst_cont1_config_lst11");
+                "#/components/schemas/toaster2_lst_cont1_lst11");
 
-        final Schema configCont11Top = schemas.get("toaster2_lst_cont1_config_cont11_TOP");
+        final Schema configCont11Top = schemas.get("toaster2_lst_cont1_cont11_TOP");
         assertNotNull(configCont11Top);
         DocGenTestHelper.containsReferences(configCont11Top,
-                "cont11", "#/components/schemas/toaster2_lst_cont1_config_cont11");
+                "cont11", "#/components/schemas/toaster2_lst_cont1_cont11");
 
-        final Schema configCont11 = schemas.get("toaster2_lst_cont1_config_cont11");
+        final Schema configCont11 = schemas.get("toaster2_lst_cont1_cont11");
         assertNotNull(configCont11);
 
-        final Schema configLst11Top = schemas.get("toaster2_lst_cont1_config_lst11_TOP");
+        final Schema configLst11Top = schemas.get("toaster2_lst_cont1_lst11_TOP");
         assertNotNull(configLst11Top);
         DocGenTestHelper.containsReferences(configLst11Top, "lst11",
-                "#/components/schemas/toaster2_lst_cont1_config_lst11");
+                "#/components/schemas/toaster2_lst_cont1_lst11");
 
-        final Schema configLst11 = schemas.get("toaster2_lst_cont1_config_lst11");
+        final Schema configLst11 = schemas.get("toaster2_lst_cont1_lst11");
         assertNotNull(configLst11);
     }
 
@@ -185,12 +185,12 @@ public final class OpenApiGeneratorRFC8040Test {
         assertNotNull(doc);
 
         final var schemas = doc.components().schemas();
-        final Schema firstContainer = schemas.get("choice-test_config_first-container");
+        final Schema firstContainer = schemas.get("choice-test_first-container");
         assertEquals("default-value",
                 firstContainer.properties().get("leaf-default").get("default").asText());
         assertFalse(firstContainer.properties().has("leaf-non-default"));
 
-        final Schema secondContainer = schemas.get("choice-test_config_second-container");
+        final Schema secondContainer = schemas.get("choice-test_second-container");
         assertTrue(secondContainer.properties().has("leaf-first-case"));
         assertFalse(secondContainer.properties().has("leaf-second-case"));
     }
@@ -324,18 +324,18 @@ public final class OpenApiGeneratorRFC8040Test {
 
         assertEquals(Set.of("/rests/data", "/rests/data/my-yang:data"), doc.paths().keySet());
         final var JsonNodeMyYangData = doc.paths().get("/rests/data/my-yang:data");
-        verifyRequestRef(JsonNodeMyYangData.post(), "#/components/schemas/my-yang_config_data",
-                "#/components/schemas/my-yang_config_data");
-        verifyRequestRef(JsonNodeMyYangData.put(), "#/components/schemas/my-yang_config_data_TOP",
-                "#/components/schemas/my-yang_config_data");
-        verifyRequestRef(JsonNodeMyYangData.get(), "#/components/schemas/my-yang_config_data_TOP",
-                "#/components/schemas/my-yang_config_data");
+        verifyRequestRef(JsonNodeMyYangData.post(), "#/components/schemas/my-yang_data",
+                "#/components/schemas/my-yang_data");
+        verifyRequestRef(JsonNodeMyYangData.put(), "#/components/schemas/my-yang_data_TOP",
+                "#/components/schemas/my-yang_data");
+        verifyRequestRef(JsonNodeMyYangData.get(), "#/components/schemas/my-yang_data_TOP",
+                "#/components/schemas/my-yang_data");
 
         // Test `components/schemas` objects
         final var definitions = doc.components().schemas();
         assertEquals(3, definitions.size());
-        assertTrue(definitions.containsKey("my-yang_config_data"));
-        assertTrue(definitions.containsKey("my-yang_config_data_TOP"));
+        assertTrue(definitions.containsKey("my-yang_data"));
+        assertTrue(definitions.containsKey("my-yang_data_TOP"));
         assertTrue(definitions.containsKey("my-yang_module"));
     }
 
@@ -345,48 +345,48 @@ public final class OpenApiGeneratorRFC8040Test {
         final var doc = generator.getOpenApiSpec(module, "http", "localhost:8181", "/", "", context);
 
         final var jsonNodeToaster = doc.paths().get("/rests/data/toaster2:toaster");
-        verifyRequestRef(jsonNodeToaster.post(), "#/components/schemas/toaster2_config_toaster",
-                "#/components/schemas/toaster2_config_toaster");
-        verifyRequestRef(jsonNodeToaster.put(), "#/components/schemas/toaster2_config_toaster_TOP",
-                "#/components/schemas/toaster2_config_toaster");
-        verifyRequestRef(jsonNodeToaster.get(), "#/components/schemas/toaster2_config_toaster_TOP",
-                "#/components/schemas/toaster2_config_toaster");
+        verifyRequestRef(jsonNodeToaster.post(), "#/components/schemas/toaster2_toaster",
+                "#/components/schemas/toaster2_toaster");
+        verifyRequestRef(jsonNodeToaster.put(), "#/components/schemas/toaster2_toaster_TOP",
+                "#/components/schemas/toaster2_toaster");
+        verifyRequestRef(jsonNodeToaster.get(), "#/components/schemas/toaster2_toaster_TOP",
+                "#/components/schemas/toaster2_toaster");
 
         final var jsonNodeToasterSlot = doc.paths().get("/rests/data/toaster2:toaster/toasterSlot={slotId}");
-        verifyRequestRef(jsonNodeToasterSlot.post(), "#/components/schemas/toaster2_toaster_config_toasterSlot",
-                "#/components/schemas/toaster2_toaster_config_toasterSlot");
-        verifyRequestRef(jsonNodeToasterSlot.put(), "#/components/schemas/toaster2_toaster_config_toasterSlot_TOP",
-                "#/components/schemas/toaster2_toaster_config_toasterSlot");
-        verifyRequestRef(jsonNodeToasterSlot.get(), "#/components/schemas/toaster2_toaster_config_toasterSlot_TOP",
-                "#/components/schemas/toaster2_toaster_config_toasterSlot");
+        verifyRequestRef(jsonNodeToasterSlot.post(), "#/components/schemas/toaster2_toaster_toasterSlot",
+                "#/components/schemas/toaster2_toaster_toasterSlot");
+        verifyRequestRef(jsonNodeToasterSlot.put(), "#/components/schemas/toaster2_toaster_toasterSlot_TOP",
+                "#/components/schemas/toaster2_toaster_toasterSlot");
+        verifyRequestRef(jsonNodeToasterSlot.get(), "#/components/schemas/toaster2_toaster_toasterSlot_TOP",
+                "#/components/schemas/toaster2_toaster_toasterSlot");
 
         final var jsonNodeSlotInfo = doc.paths().get(
                 "/rests/data/toaster2:toaster/toasterSlot={slotId}/toaster-augmented:slotInfo");
         verifyRequestRef(jsonNodeSlotInfo.post(),
-                "#/components/schemas/toaster2_toaster_toasterSlot_config_slotInfo",
-                "#/components/schemas/toaster2_toaster_toasterSlot_config_slotInfo");
+                "#/components/schemas/toaster2_toaster_toasterSlot_slotInfo",
+                "#/components/schemas/toaster2_toaster_toasterSlot_slotInfo");
         verifyRequestRef(jsonNodeSlotInfo.put(),
-                "#/components/schemas/toaster2_toaster_toasterSlot_config_slotInfo_TOP",
-                "#/components/schemas/toaster2_toaster_toasterSlot_config_slotInfo");
+                "#/components/schemas/toaster2_toaster_toasterSlot_slotInfo_TOP",
+                "#/components/schemas/toaster2_toaster_toasterSlot_slotInfo");
         verifyRequestRef(jsonNodeSlotInfo.get(),
-                "#/components/schemas/toaster2_toaster_toasterSlot_config_slotInfo_TOP",
-                "#/components/schemas/toaster2_toaster_toasterSlot_config_slotInfo");
+                "#/components/schemas/toaster2_toaster_toasterSlot_slotInfo_TOP",
+                "#/components/schemas/toaster2_toaster_toasterSlot_slotInfo");
 
         final var jsonNodeLst = doc.paths().get("/rests/data/toaster2:lst");
-        verifyRequestRef(jsonNodeLst.post(), "#/components/schemas/toaster2_config_lst",
-                "#/components/schemas/toaster2_config_lst");
-        verifyRequestRef(jsonNodeLst.put(), "#/components/schemas/toaster2_config_lst_TOP",
-                "#/components/schemas/toaster2_config_lst");
-        verifyRequestRef(jsonNodeLst.get(), "#/components/schemas/toaster2_config_lst_TOP",
-                "#/components/schemas/toaster2_config_lst");
+        verifyRequestRef(jsonNodeLst.post(), "#/components/schemas/toaster2_lst",
+                "#/components/schemas/toaster2_lst");
+        verifyRequestRef(jsonNodeLst.put(), "#/components/schemas/toaster2_lst_TOP",
+                "#/components/schemas/toaster2_lst");
+        verifyRequestRef(jsonNodeLst.get(), "#/components/schemas/toaster2_lst_TOP",
+                "#/components/schemas/toaster2_lst");
 
         final var jsonNodeLst1 = doc.paths().get("/rests/data/toaster2:lst/lst1={key1},{key2}");
-        verifyRequestRef(jsonNodeLst1.post(), "#/components/schemas/toaster2_lst_config_lst1",
-                "#/components/schemas/toaster2_lst_config_lst1");
-        verifyRequestRef(jsonNodeLst1.put(), "#/components/schemas/toaster2_lst_config_lst1_TOP",
-                "#/components/schemas/toaster2_lst_config_lst1");
-        verifyRequestRef(jsonNodeLst1.get(), "#/components/schemas/toaster2_lst_config_lst1_TOP",
-                "#/components/schemas/toaster2_lst_config_lst1");
+        verifyRequestRef(jsonNodeLst1.post(), "#/components/schemas/toaster2_lst_lst1",
+                "#/components/schemas/toaster2_lst_lst1");
+        verifyRequestRef(jsonNodeLst1.put(), "#/components/schemas/toaster2_lst_lst1_TOP",
+                "#/components/schemas/toaster2_lst_lst1");
+        verifyRequestRef(jsonNodeLst1.get(), "#/components/schemas/toaster2_lst_lst1_TOP",
+                "#/components/schemas/toaster2_lst_lst1");
 
         final var jsonNodeMakeToast = doc.paths().get("/rests/operations/toaster2:make-toast");
         assertNull(jsonNodeMakeToast.get());
