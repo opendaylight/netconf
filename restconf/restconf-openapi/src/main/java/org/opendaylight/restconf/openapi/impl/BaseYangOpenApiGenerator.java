1d5ea289871ce33bf63ab88c4efec2f422b74484
Matej Sramcik <matej.sramcik@pantheon.tech>
2023-07-19 12:35:33 +0200

    Centralize parameter creation - query parameter
    Redundant variables are removed from build methods.
    
    Void method addQueryParameters is changed to method
    createQueryParameter in BaseYangOpenApiGenerator classwhich return
    Parameter
    
    JIRA: NETCONF-1086
    Change-Id: Iffed4ec8e6bcc6104314f9280d6945f247db7067
    Signed-off-by: Matej Sramcik <matej.sramcik@pantheon.tech>

--- restconf/restconf-openapi/src/main/java/org/opendaylight/restconf/openapi/impl/BaseYangOpenApiGenerator.java
+++ restconf/restconf-openapi/src/main/java/org/opendaylight/restconf/openapi/impl/BaseYangOpenApiGenerator.java
@@ -78,9 +78,7 @@ public abstract class BaseYangOpenApiGenerator {
     public static final String BASE_PATH = "/";
     public static final String MODULE_NAME_SUFFIX = "_module";
 
-
-
-
+    public static final String STRING = "string";
 
     private static final ObjectNode OPEN_API_BASIC_AUTH = JsonNodeFactory.instance.objectNode()
             .put("type", "http")
@@ -347,7 +345,7 @@ public abstract class BaseYangOpenApiGenerator {
         param.forEach((paramName,description) -> {
             final Parameter.Builder pathParamBuilder = new Parameter.Builder()
                 .name(paramName)
-                .schema(new Schema.Builder().type("string").build())
+                .schema(new Schema.Builder().type(STRING).build())
                 .in("path")
                 .required(true)
                 .description(description);
@@ -368,6 +366,20 @@ public abstract class BaseYangOpenApiGenerator {
             return postBuilder.build();
         }
         return null;
+    }
+
+    public static Parameter createQueryParameter(final boolean isConfig) {
+        final ArrayNode cases = JsonNodeFactory.instance.arrayNode();
+        cases.add("nonconfig");
+        if (isConfig) {
+            cases.add("config");
+        }
+        final Parameter.Builder contentParamBuilder = new Parameter.Builder()
+            .in("query")
+            .name("content")
+            .schema(new Schema.Builder().type(STRING).schemaEnum(cases).build())
+            .required(!isConfig);
+        return contentParamBuilder.build();
     }
 
     private static Path operations(final DataSchemaNode node, final String moduleName,
