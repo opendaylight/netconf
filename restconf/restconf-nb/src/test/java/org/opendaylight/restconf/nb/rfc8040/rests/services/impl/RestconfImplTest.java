8a7b457b17e030e9ce8440a49042d83680cf5488
Matej Sramcik <matej.sramcik@pantheon.tech>
2023-07-24 09:46:44 +0200

    Split out Rfc8040#IetfYangLibrary class
    Class IetfYangLibrary is removed and constants are moved to SchemaContextHandler class.
    
    JIRA: NETCONF-1092
    Change-Id: Id089620022afc629ade29451dcc9ccc140bb54fa
    Signed-off-by: Matej Sramcik <matej.sramcik@pantheon.tech>
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- restconf/restconf-nb/src/test/java/org/opendaylight/restconf/nb/rfc8040/rests/services/impl/RestconfImplTest.java
+++ restconf/restconf-nb/src/test/java/org/opendaylight/restconf/nb/rfc8040/rests/services/impl/RestconfImplTest.java
@@ -10,19 +10,15 @@ package org.opendaylight.restconf.nb.rfc8040.rests.services.impl;
 import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
-import org.opendaylight.restconf.nb.rfc8040.Rfc8040.IetfYangLibrary;
 import org.opendaylight.restconf.nb.rfc8040.databind.DatabindContext;
-import org.opendaylight.restconf.nb.rfc8040.legacy.NormalizedNodePayload;
-import org.opendaylight.yangtools.yang.data.api.schema.LeafNode;
 import org.opendaylight.yangtools.yang.test.util.YangParserTestUtils;
 
 public class RestconfImplTest {
     @Test
     public void restImplTest() {
         final var context = YangParserTestUtils.parseYangResourceDirectory("/restconf/impl");
-        final RestconfImpl restconfImpl = new RestconfImpl(() -> DatabindContext.ofModel(context));
-        final NormalizedNodePayload libraryVersion = restconfImpl.getLibraryVersion();
-        final LeafNode<?> value = (LeafNode<?>) libraryVersion.getData();
-        assertEquals(IetfYangLibrary.REVISION.toString(), value.body());
+        final var restconfImpl = new RestconfImpl(() -> DatabindContext.ofModel(context));
+        final var libraryVersion = restconfImpl.getLibraryVersion();
+        assertEquals("2019-01-04", libraryVersion.getData().body());
     }
 }
