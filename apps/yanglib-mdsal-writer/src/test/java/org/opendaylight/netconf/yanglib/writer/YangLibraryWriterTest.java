13596c750d7bd3a0de0a389239820dfdad54d716
Ruslan Kashapov <ruslan.kashapov@pantheon.tech>
2023-07-25 18:14:19 +0300

    Standalone yang library data writer
    Due to yang library data is being written from multiple places it may cause
    data inconsistency and/or corruption. New standalone dedicated module is
    designed to replace existing writers with single one.
    
    JIRA: NETCONF-688
    Change-Id: I34021ce5b26ee35b9662b8b156423fff5dbd9c80
    Signed-off-by: Ruslan Kashapov <ruslan.kashapov@pantheon.tech>

---
+++ apps/yanglib-mdsal-writer/src/test/java/org/opendaylight/netconf/yanglib/writer/YangLibraryWriterTest.java
@@ -0,0 +1,240 @@
+/*
+ * Copyright (c) 2023 PANTHEON.tech s.r.o. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.netconf.yanglib.writer;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoInteractions;
+import static org.opendaylight.mdsal.common.api.CommitInfo.emptyFluentFuture;
+import static org.opendaylight.mdsal.common.api.LogicalDatastoreType.OPERATIONAL;
+import static org.opendaylight.netconf.yanglib.writer.YangLibraryContentBuilderUtil.DEFAULT_MODULE_SET_NAME;
+import static org.opendaylight.netconf.yanglib.writer.YangLibraryContentBuilderUtil.DEFAULT_SCHEMA_NAME;
+import static org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.yang.library.rev190104.module.list.Module.ConformanceType.Implement;
+import static org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.yang.library.rev190104.module.list.Module.ConformanceType.Import;
+import static org.opendaylight.yangtools.yang.test.util.YangParserTestUtils.parseYangResources;
+
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Stream;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.opendaylight.mdsal.binding.api.DataBroker;
+import org.opendaylight.mdsal.binding.api.WriteTransaction;
+import org.opendaylight.mdsal.dom.api.DOMSchemaService;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.datastores.rev180214.Operational;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev130715.Uri;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.yang.library.rev190104.LegacyRevisionUtils;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.yang.library.rev190104.ModulesState;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.yang.library.rev190104.ModulesStateBuilder;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.yang.library.rev190104.RevisionIdentifier;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.yang.library.rev190104.RevisionUtils;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.yang.library.rev190104.YangLibrary;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.yang.library.rev190104.YangLibraryBuilder;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.yang.library.rev190104.module.list.CommonLeafs;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.yang.library.rev190104.module.list.module.DeviationBuilder;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.yang.library.rev190104.module.set.parameters.ModuleBuilder;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.yang.library.rev190104.module.set.parameters.module.SubmoduleBuilder;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.yang.library.rev190104.yang.library.parameters.DatastoreBuilder;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.yang.library.rev190104.yang.library.parameters.ModuleSetBuilder;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.yang.library.rev190104.yang.library.parameters.SchemaBuilder;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.yang.types.rev130715.YangIdentifier;
+import org.opendaylight.yangtools.concepts.ListenerRegistration;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+import org.opendaylight.yangtools.yang.binding.util.BindingMap;
+import org.opendaylight.yangtools.yang.model.api.EffectiveModelContextListener;
+
+@ExtendWith(MockitoExtension.class)
+public class YangLibraryWriterTest {
+
+    private static final YangLibrarySchemaSourceUrlProvider URL_PROVIDER = (moduleSetName, moduleName, revision) ->
+            Optional.of(new Uri("/url/to/" + moduleName + (revision == null ? "" : "/" + revision)));
+
+    private static final boolean WITH_LEGACY = true;
+    private static final boolean NO_LEGACY = false;
+    private static final boolean WITH_URLS = true;
+    private static final boolean NO_URLS = false;
+
+    private static final InstanceIdentifier<YangLibrary> YANG_LIBRARY_II =
+        InstanceIdentifier.create(YangLibrary.class);
+    private static final InstanceIdentifier<ModulesState> MODULES_STATE_II =
+        InstanceIdentifier.create(ModulesState.class);
+
+    @Mock
+    private DOMSchemaService schemaService;
+    @Mock
+    private DataBroker dataBroker;
+    @Mock
+    private WriteTransaction writeTransaction;
+    @Mock
+    private ListenerRegistration<EffectiveModelContextListener> registration;
+    @Mock
+    private YangLibraryWriter.Configuration config;
+    @Captor
+    ArgumentCaptor<YangLibrary> yangLibraryCaptor;
+    @Captor
+    ArgumentCaptor<ModulesState> modulesStateCaptor;
+
+    YangLibraryWriter writer;
+
+    @BeforeEach
+    public void beforeEach() {
+        doReturn(registration).when(schemaService).registerSchemaContextListener(any());
+    }
+
+    private YangLibraryWriter.Configuration setupConfig(final boolean writeLegacy) {
+        doReturn(writeLegacy).when(config).write$_$legacy();
+        return config;
+    }
+
+    @Test
+    @DisplayName("No update bc context has no ietf-yang-library")
+    void noUpdate() {
+        writer = new YangLibraryWriter(schemaService, dataBroker, setupConfig(NO_LEGACY));
+        writer.onModelContextUpdated(parseYangResources(YangLibraryWriterTest.class,
+            "/test-module.yang", "/test-submodule.yang"));
+        verifyNoInteractions(dataBroker);
+    }
+
+    @ParameterizedTest(name = "Write data -- with URLs: {0}, include legacy: {1}")
+    @MethodSource("writeContentArgs")
+    void writeContent(final boolean withUrls, final boolean writeLegacy, final YangLibrary expectedData,
+        final ModulesState expectedLegacyData) {
+
+        doReturn(writeTransaction).when(dataBroker).newWriteOnlyTransaction();
+        doNothing().when(writeTransaction).put(eq(OPERATIONAL), any(), any());
+        doReturn(emptyFluentFuture()).when(writeTransaction).commit();
+
+        writer = new YangLibraryWriter(schemaService, dataBroker, setupConfig(writeLegacy));
+        if (withUrls) {
+            writer.schemaSourceUrlProvider = URL_PROVIDER;
+        }
+        writer.onModelContextUpdated(parseYangResources(YangLibraryWriterTest.class,
+            "/test-module.yang", "/test-submodule.yang", "/test-more.yang", "/ietf-yang-library.yang"));
+
+        verify(writeTransaction).put(eq(OPERATIONAL), eq(YANG_LIBRARY_II), yangLibraryCaptor.capture());
+        assertEquals(expectedData, yangLibraryCaptor.getValue());
+        if (writeLegacy) {
+            verify(writeTransaction).put(eq(OPERATIONAL), eq(MODULES_STATE_II), modulesStateCaptor.capture());
+            assertEquals(expectedLegacyData, modulesStateCaptor.getValue());
+        } else {
+            verify(writeTransaction, never()).put(eq(OPERATIONAL), eq(MODULES_STATE_II), any());
+        }
+        verify(writeTransaction).commit();
+    }
+
+    private static Stream<Arguments> writeContentArgs() {
+        return Stream.of(
+            Arguments.of(NO_URLS, NO_LEGACY, buildYangLibrary(NO_URLS), null),
+            Arguments.of(NO_URLS, WITH_LEGACY, buildYangLibrary(NO_URLS), buildModulesState(NO_URLS)),
+            Arguments.of(WITH_URLS, NO_LEGACY, buildYangLibrary(WITH_URLS), null),
+            Arguments.of(WITH_URLS, WITH_LEGACY, buildYangLibrary(WITH_URLS), buildModulesState(WITH_URLS)));
+    }
+
+    private static YangLibrary buildYangLibrary(final boolean withUrls) {
+        return new YangLibraryBuilder()
+            .setModuleSet(BindingMap.of(
+                new ModuleSetBuilder()
+                    .setName(DEFAULT_MODULE_SET_NAME)
+                    .setModule(BindingMap.of(
+                        new ModuleBuilder().setName(new YangIdentifier("test-module_2013-07-22"))
+                            .setNamespace(new Uri("test:namespace"))
+                            .setRevision(new RevisionIdentifier("2013-07-22"))
+                            .setLocation(withUrls ? Set.of(new Uri("/url/to/test-module/2013-07-22")) : null)
+                            .setSubmodule(BindingMap.of(
+                                new SubmoduleBuilder()
+                                    .setName(new YangIdentifier("test-submodule"))
+                                    .setRevision(RevisionUtils.emptyRevision().getRevisionIdentifier())
+                                    .setLocation(withUrls ? Set.of(new Uri("/url/to/test-submodule")) : null)
+                                    .build()))
+                            .build(),
+                        new ModuleBuilder().setName(new YangIdentifier("test-more_2023-07-25"))
+                            .setNamespace(new Uri("test:more"))
+                            .setRevision(new RevisionIdentifier("2023-07-25"))
+                            .setLocation(withUrls ? Set.of(new Uri("/url/to/test-more/2023-07-25")) : null)
+                            .setFeature(Set.of(
+                                new YangIdentifier("first-feature"), new YangIdentifier("second-feature")))
+                            .setDeviation(Set.of(new YangIdentifier("test-module_2013-07-22")))
+                            .build(),
+                        new ModuleBuilder().setName(new YangIdentifier("ietf-yang-library_2019-01-04"))
+                            .setNamespace(new Uri("urn:ietf:params:xml:ns:yang:ietf-yang-library"))
+                            .setRevision(new RevisionIdentifier("2019-01-04"))
+                            .setLocation(withUrls ? Set.of(new Uri("/url/to/ietf-yang-library/2019-01-04")) : null)
+                            .build()))
+                    .build()))
+            .setSchema(BindingMap.of(new SchemaBuilder()
+                .setName(DEFAULT_SCHEMA_NAME)
+                .setModuleSet(Set.of(DEFAULT_MODULE_SET_NAME))
+                .build()))
+            .setDatastore(BindingMap.of(
+                new DatastoreBuilder().setName(Operational.VALUE)
+                    .setSchema(DEFAULT_SCHEMA_NAME)
+                    .build()))
+            .setContentId("1")
+            .build();
+    }
+
+    private static ModulesState buildModulesState(final boolean withUrls) {
+        return new ModulesStateBuilder()
+            .setModule(BindingMap.of(
+                new org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.yang.library.rev190104
+                    .module.list.ModuleBuilder()
+                    .setName(new YangIdentifier("test-module_2013-07-22"))
+                    .setNamespace(new Uri("test:namespace"))
+                    .setRevision(new CommonLeafs.Revision(new RevisionIdentifier("2013-07-22")))
+                    .setSchema(withUrls ? new Uri("/url/to/test-module/2013-07-22") : null)
+                    .setSubmodule(BindingMap.of(
+                        new org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.yang.library.rev190104
+                            .module.list.module.SubmoduleBuilder()
+                            .setName(new YangIdentifier("test-submodule"))
+                            .setRevision(LegacyRevisionUtils.emptyRevision())
+                            .setSchema(withUrls ? new Uri("/url/to/test-submodule") : null)
+                            .build()))
+                    .setConformanceType(Import)
+                    .build(),
+                new org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.yang.library.rev190104
+                    .module.list.ModuleBuilder()
+                    .setName(new YangIdentifier("test-more_2023-07-25"))
+                    .setNamespace(new Uri("test:more"))
+                    .setRevision(new CommonLeafs.Revision(new RevisionIdentifier("2023-07-25")))
+                    .setSchema(withUrls ? new Uri("/url/to/test-more/2023-07-25") : null)
+                    .setFeature(Set.of(
+                        new YangIdentifier("first-feature"), new YangIdentifier("second-feature")))
+                    .setDeviation(BindingMap.of(
+                        new DeviationBuilder()
+                            .setName(new YangIdentifier("test-module_2013-07-22"))
+                            .setRevision(new CommonLeafs.Revision(new RevisionIdentifier("2013-07-22")))
+                            .build()
+                    ))
+                    .setConformanceType(Implement)
+                    .build(),
+                new org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.yang.library.rev190104
+                    .module.list.ModuleBuilder()
+                    .setName(new YangIdentifier("ietf-yang-library_2019-01-04"))
+                    .setNamespace(new Uri("urn:ietf:params:xml:ns:yang:ietf-yang-library"))
+                    .setRevision(new CommonLeafs.Revision(new RevisionIdentifier("2019-01-04")))
+                    .setSchema(withUrls ? new Uri("/url/to/ietf-yang-library/2019-01-04") : null)
+                    .setConformanceType(Import)
+                    .build()))
+            .setModuleSetId("1")
+            .build();
+    }
+}
