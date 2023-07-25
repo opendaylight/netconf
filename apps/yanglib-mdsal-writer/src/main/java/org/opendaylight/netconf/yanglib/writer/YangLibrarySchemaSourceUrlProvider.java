491ba2bf4a2d228ad5d6175d99fb76b92305362f
Ruslan Kashapov <ruslan.kashapov@pantheon.tech>
2023-07-25 18:14:19 +0300

    Standalone yang library data writer
    Due to yang library data is being written from multiple places
    it may cause data inconsistency and/or corruption. New standalone
    dedicated module is designed to replace existing writers with
    single one.
    
    JIRA: NETCONF-668
    Change-Id: I34021ce5b26ee35b9662b8b156423fff5dbd9c80
    Signed-off-by: Ruslan Kashapov <ruslan.kashapov@pantheon.tech>

---
+++ apps/yanglib-mdsal-writer/src/main/java/org/opendaylight/netconf/yanglib/writer/YangLibrarySchemaSourceUrlProvider.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2023 PANTHEON.tech s.r.o. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.netconf.yanglib.writer;
+
+import java.util.Optional;
+import org.eclipse.jdt.annotation.NonNull;
+import org.eclipse.jdt.annotation.Nullable;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev130715.Uri;
+import org.opendaylight.yangtools.yang.common.Revision;
+
+/**
+ * The service providing URLs to yang schema sources.
+ */
+// TODO: current interface is a part of integration with YangLibrarySupport and expected
+//  to be removed once the similar interface is implemented there.
+//  Addresses https://jira.opendaylight.org/browse/MDSAL-833
+public interface YangLibrarySchemaSourceUrlProvider {
+
+    /**
+     * Provides yang schema source URL where it can be downloaded from.
+     *
+     * @param moduleSetName the module set name the requested resource belongs to
+     * @param moduleName referenced module or submodule name
+     * @param revision optional revision
+     *
+     * @return optional of URL to requested resource
+     */
+    Optional<Uri> getSchemaSourceUrl(@NonNull String moduleSetName, @NonNull String moduleName,
+        @Nullable Revision revision);
+}
+
