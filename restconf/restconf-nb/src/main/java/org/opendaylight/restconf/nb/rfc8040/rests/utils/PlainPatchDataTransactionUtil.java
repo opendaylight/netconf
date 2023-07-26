a9b1272556dd1655c0c9b9cdb9ad256b66349cbf
Robert Varga <robert.varga@pantheon.tech>
2023-07-26 23:09:14 +0200

    Disconnect PlainPatchDataTransactionUtil from NormalizedNodePayload
    We only require path and data here, there is no need to encapsulate this
    in a legacy wrapper.
    
    Change-Id: I1dcadfe0b7c985d5a357cbf112cb720cf883a65e
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- restconf/restconf-nb/src/main/java/org/opendaylight/restconf/nb/rfc8040/rests/utils/PlainPatchDataTransactionUtil.java
+++ restconf/restconf-nb/src/main/java/org/opendaylight/restconf/nb/rfc8040/rests/utils/PlainPatchDataTransactionUtil.java
@@ -8,13 +8,10 @@
  */
 package org.opendaylight.restconf.nb.rfc8040.rests.utils;
 
-import com.google.common.util.concurrent.ListenableFuture;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
-import org.opendaylight.mdsal.common.api.CommitInfo;
 import org.opendaylight.mdsal.dom.api.DOMTransactionChain;
 import org.opendaylight.restconf.common.errors.RestconfDocumentedException;
-import org.opendaylight.restconf.nb.rfc8040.legacy.NormalizedNodePayload;
 import org.opendaylight.restconf.nb.rfc8040.rests.transactions.RestconfStrategy;
 import org.opendaylight.restconf.nb.rfc8040.rests.transactions.RestconfTransaction;
 import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
@@ -27,28 +24,25 @@ import org.slf4j.LoggerFactory;
  * Util class for plain patch data to DS.
  */
 public final class PlainPatchDataTransactionUtil {
-
     private static final Logger LOG = LoggerFactory.getLogger(PlainPatchDataTransactionUtil.class);
 
     private PlainPatchDataTransactionUtil() {
+        // Hidden on purpose
     }
 
     /**
      * Prepare variables for put data to DS. Close {@link DOMTransactionChain} if any inside of object
      * {@link RestconfStrategy} provided as a parameter if any.
      *
-     * @param payload       data to put
+     * @param path          data path
+     * @param data          data to put
      * @param schemaContext reference to {@link EffectiveModelContext}
      * @param strategy      object that perform the actual DS operations
      * @return {@link Response}
      */
-    public static Response patchData(final NormalizedNodePayload payload,
-                                     final RestconfStrategy strategy,
-                                     final EffectiveModelContext schemaContext) {
-
+    public static Response patchData(final YangInstanceIdentifier path, final NormalizedNode data,
+            final RestconfStrategy strategy, final EffectiveModelContext schemaContext) {
         final RestconfTransaction transaction = strategy.prepareWriteExecution();
-        YangInstanceIdentifier path = payload.getInstanceIdentifierContext().getInstanceIdentifier();
-        NormalizedNode data = payload.getData();
 
         try {
             LOG.trace("Merge CONFIGURATION within Restconf Patch: {} with payload {}", path, data);
@@ -59,7 +53,7 @@ public final class PlainPatchDataTransactionUtil {
             throw new IllegalArgumentException(e);
         }
 
-        final ListenableFuture<? extends CommitInfo> future = transaction.commit();
+        final var future = transaction.commit();
         final ResponseFactory response = new ResponseFactory(Status.OK);
 
         // closes transactionChain if any, may throw
