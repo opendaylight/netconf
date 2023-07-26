a1e1fb82e1fd90b83d954bdb579528ac2b39098f
Yaroslav Lastivka <yaroslav.lastivka@pantheon.tech>
2023-07-26 18:18:03 +0300

    OpenApi: Remove incorrect list POST requests
    Our logic creates POST requests for lists with keys, containing
    multiple resources in payload. This does not align with the expected
    structure for creating child resources in the YANG model.
    
    Added condition that prevents the creation of POST requests
    that has list as a last element.
    
    JIRA: NETCONF-1101
    Change-Id: I46820f222c9c5ef8078ac8675d2adc12b06f1253
    Signed-off-by: Yaroslav Lastivka <yaroslav.lastivka@pantheon.tech>
    Signed-off-by: Ivan Hrasko <ivan.hrasko@pantheon.tech>

--- restconf/restconf-openapi/src/test/java/org/opendaylight/restconf/openapi/mountpoints/MountPointOpenApiTest.java
+++ restconf/restconf-openapi/src/test/java/org/opendaylight/restconf/openapi/mountpoints/MountPointOpenApiTest.java
@@ -13,6 +13,7 @@ import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 import static org.opendaylight.restconf.openapi.OpenApiTestUtils.getPathParameters;
+import static org.opendaylight.restconf.openapi.OpenApiTestUtils.getPathPostParameters;
 import static org.opendaylight.restconf.openapi.impl.BaseYangOpenApiGenerator.BASIC_AUTH_NAME;
 
 import java.util.ArrayList;
@@ -160,7 +161,7 @@ public final class MountPointOpenApiTest {
         }
 
         assertEquals("Unexpected GET paths size", 37, getOperations.size());
-        assertEquals("Unexpected POST paths size", 43, postOperations.size());
+        assertEquals("Unexpected POST paths size", 27, postOperations.size());
         assertEquals("Unexpected PUT paths size", 35, putOperations.size());
         assertEquals("Unexpected PATCH paths size", 35, patchOperations.size());
         assertEquals("Unexpected DELETE paths size", 35, deleteOperations.size());
@@ -204,14 +205,16 @@ public final class MountPointOpenApiTest {
             assertNotNull(delete);
             assertEquals(expectedSize, delete.parameters().size());
 
-            final var post = path.post();
-            assertNotNull(post);
-            assertEquals(expectedSize, post.parameters().size());
-
             final var patch = path.patch();
             assertNotNull(patch);
             assertEquals(expectedSize, patch.parameters().size());
         }
+
+        // POST request exists only for containers
+        final var post = paths.get("/rests/data/nodes/node=123/yang-ext:mount/recursive:container-root").post();
+        assertNotNull(post);
+        final int expectedSize = configPaths.get("/rests/data/nodes/node=123/yang-ext:mount/recursive:container-root");
+        assertEquals(expectedSize, post.parameters().size());
     }
 
     /**
@@ -263,12 +266,12 @@ public final class MountPointOpenApiTest {
         final var pathWithParameters =
             "/rests/operations/nodes/node=123/yang-ext:mount/action-types:list={name}/list-action";
         assertTrue(mountPointApi.paths().containsKey(pathWithParameters));
-        assertEquals(List.of("name"), getPathParameters(mountPointApi.paths(), pathWithParameters));
+        assertEquals(List.of("name"), getPathPostParameters(mountPointApi.paths(), pathWithParameters));
 
         final var pathWithoutParameters =
             "/rests/operations/nodes/node=123/yang-ext:mount/action-types:multi-container/inner-container/action";
         assertTrue(mountPointApi.paths().containsKey(pathWithoutParameters));
-        assertEquals(List.of(), getPathParameters(mountPointApi.paths(), pathWithoutParameters));
+        assertEquals(List.of(), getPathPostParameters(mountPointApi.paths(), pathWithoutParameters));
     }
 
     /**
