46ac209d7b5d615879d972a18d59ee70252ed28d
Yaroslav Lastivka <yaroslav.lastivka@pantheon.tech>
2023-07-26 18:18:03 +0300

    OpenApi: Remove incorrect list POST requests
    Our logic creates POST requests with multiple resources in payload
    which do not align with the expected structure for creating
    child resources in the YANG model.
    
    Added condition that prevents the creation of POST requests
    for the path which contain lists as parameter.
    
    JIRA: NETCONF-1101
    Change-Id: I46820f222c9c5ef8078ac8675d2adc12b06f1253
    Signed-off-by: Yaroslav Lastivka <yaroslav.lastivka@pantheon.tech>

---
+++ restconf/restconf-openapi/src/test/java/org/opendaylight/restconf/openapi/impl/ListPostRequestsTest.java
@@ -0,0 +1,49 @@
+package org.opendaylight.restconf.openapi.impl;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.opendaylight.mdsal.dom.api.DOMSchemaService;
+import org.opendaylight.restconf.openapi.model.OpenApiObject;
+import org.opendaylight.yangtools.yang.test.util.YangParserTestUtils;
+
+public class ListPostRequestsTest {
+    private static OpenApiObject doc;
+
+    @BeforeClass
+    public static void startUp() {
+        final var context = YangParserTestUtils.parseYang("""
+            module list-post {
+              namespace "list-post";
+              prefix lp;
+                container container {
+                  list list {
+                    key "name address";
+                    leaf name {
+                      type string;
+                    }
+                    leaf address {
+                      type string;
+                    }
+                  }
+                }
+              }""");
+        final var schemaService = mock(DOMSchemaService.class);
+        when(schemaService.getGlobalContext()).thenReturn(context);
+        final var generator = new OpenApiGeneratorRFC8040(schemaService);
+        final var module = context.findModule("list-post").orElseThrow();
+        doc = generator.getOpenApiSpec(module, "http", "localhost:8181", "/", "", context);
+        assertNotNull(doc);
+    }
+
+    @Test
+    public void testListPostRequest() {
+        final var pathToListWithTwoKeys = "/rests/data/list-post:container/list={name},{address}";
+        assertNull(doc.paths().get(pathToListWithTwoKeys).post());
+        assertNotNull(doc.paths().get(pathToListWithTwoKeys).put());
+    }
+}
