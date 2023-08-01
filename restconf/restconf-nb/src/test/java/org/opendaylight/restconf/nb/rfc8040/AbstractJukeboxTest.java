d4dd8e0c7ddc070afb1b687006714a484e1d1817
Robert Varga <robert.varga@pantheon.tech>
2023-08-01 18:13:20 +0200

    Clean up PatchDataTransactionUtilTest
    We are about to split this test up, clean up constants and other
    interactions. This flushes out a weirdness in addressing which will
    need to be addressed.
    
    JIRA: NETCONF-1107
    Change-Id: Ie76e493a4074036ea8f71de24896c514daa36c4e
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- restconf/restconf-nb/src/test/java/org/opendaylight/restconf/nb/rfc8040/AbstractJukeboxTest.java
+++ restconf/restconf-nb/src/test/java/org/opendaylight/restconf/nb/rfc8040/AbstractJukeboxTest.java
@@ -44,6 +44,8 @@ public abstract class AbstractJukeboxTest {
     protected static final QName DESCRIPTION_QNAME = QName.create(JUKEBOX_QNAME, "description");
 
     protected static final YangInstanceIdentifier JUKEBOX_IID = YangInstanceIdentifier.of(JUKEBOX_QNAME);
+
+    // instance identifier for accessing leaf node "gap"
     protected static final YangInstanceIdentifier GAP_IID =
         YangInstanceIdentifier.of(JUKEBOX_QNAME, PLAYER_QNAME, GAP_QNAME);
 
--- restconf/restconf-nb/src/test/java/org/opendaylight/restconf/nb/rfc8040/rests/utils/PatchDataTransactionUtilTest.java
+++ restconf/restconf-nb/src/test/java/org/opendaylight/restconf/nb/rfc8040/rests/utils/PatchDataTransactionUtilTest.java
@@ -21,8 +21,6 @@ import static org.opendaylight.yangtools.util.concurrent.FluentFutures.immediate
 import static org.opendaylight.yangtools.util.concurrent.FluentFutures.immediateTrueFluentFuture;
 
 import com.google.common.util.concurrent.Futures;
-import com.google.common.util.concurrent.SettableFuture;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import org.junit.Before;
@@ -35,15 +33,12 @@ import org.opendaylight.mdsal.common.api.LogicalDatastoreType;
 import org.opendaylight.mdsal.common.api.TransactionCommitFailedException;
 import org.opendaylight.mdsal.dom.api.DOMDataBroker;
 import org.opendaylight.mdsal.dom.api.DOMDataTreeReadWriteTransaction;
-import org.opendaylight.mdsal.dom.api.DOMRpcResult;
 import org.opendaylight.mdsal.dom.spi.DefaultDOMRpcResult;
 import org.opendaylight.netconf.api.NetconfDocumentedException;
 import org.opendaylight.netconf.dom.api.NetconfDataTreeService;
 import org.opendaylight.restconf.common.context.InstanceIdentifierContext;
 import org.opendaylight.restconf.common.patch.PatchContext;
 import org.opendaylight.restconf.common.patch.PatchEntity;
-import org.opendaylight.restconf.common.patch.PatchStatusContext;
-import org.opendaylight.restconf.common.patch.PatchStatusEntity;
 import org.opendaylight.restconf.nb.rfc8040.AbstractJukeboxTest;
 import org.opendaylight.restconf.nb.rfc8040.rests.transactions.MdsalRestconfStrategy;
 import org.opendaylight.restconf.nb.rfc8040.rests.transactions.NetconfRestconfStrategy;
@@ -54,13 +49,22 @@ import org.opendaylight.yangtools.yang.common.ErrorType;
 import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
 import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.NodeIdentifier;
 import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.NodeIdentifierWithPredicates;
-import org.opendaylight.yangtools.yang.data.api.schema.ContainerNode;
-import org.opendaylight.yangtools.yang.data.api.schema.MapNode;
 import org.opendaylight.yangtools.yang.data.impl.schema.Builders;
 import org.opendaylight.yangtools.yang.data.impl.schema.ImmutableNodes;
 
 @RunWith(MockitoJUnitRunner.StrictStubs.class)
 public class PatchDataTransactionUtilTest extends AbstractJukeboxTest {
+    // instance identifier for accessing container node "player"
+    private static final YangInstanceIdentifier PLAYER_IID = YangInstanceIdentifier.of(JUKEBOX_QNAME, PLAYER_QNAME);
+    private static final YangInstanceIdentifier ARTIST_IID = YangInstanceIdentifier.builder()
+        .node(JUKEBOX_QNAME)
+        .node(LIBRARY_QNAME)
+        .node(ARTIST_QNAME)
+        .nodeWithKey(ARTIST_QNAME, NAME_QNAME, "name of artist")
+        .build();
+    // FIXME: this looks weird
+    private static final YangInstanceIdentifier CREATE_AND_DELETE_TARGET = GAP_IID.node(PLAYER_QNAME).node(GAP_QNAME);
+
     @Mock
     private DOMDataTreeReadWriteTransaction rwTransaction;
     @Mock
@@ -68,60 +72,8 @@ public class PatchDataTransactionUtilTest extends AbstractJukeboxTest {
     @Mock
     private NetconfDataTreeService netconfService;
 
-    private YangInstanceIdentifier instanceIdContainer;
-    private YangInstanceIdentifier instanceIdCreateAndDelete;
-    private YangInstanceIdentifier instanceIdMerge;
-    private ContainerNode buildBaseContainerForTests;
-    private YangInstanceIdentifier targetNodeForCreateAndDelete;
-    private YangInstanceIdentifier targetNodeMerge;
-    private MapNode buildArtistList;
-
     @Before
-    public void setUp() {
-        // instance identifier for accessing container node "player"
-        instanceIdContainer = YangInstanceIdentifier.of(JUKEBOX_QNAME, PLAYER_QNAME);
-
-        // instance identifier for accessing leaf node "gap"
-        instanceIdCreateAndDelete = instanceIdContainer.node(GAP_QNAME);
-
-        // values that are used for creating leaf for testPatchDataCreateAndDelete test
-        buildBaseContainerForTests = Builders.containerBuilder()
-            .withNodeIdentifier(new NodeIdentifier(JUKEBOX_QNAME))
-            .withChild(Builders.containerBuilder()
-                .withNodeIdentifier(new NodeIdentifier(PLAYER_QNAME))
-                .withChild(GAP_LEAF)
-                .build())
-            .build();
-
-        targetNodeForCreateAndDelete = instanceIdCreateAndDelete.node(PLAYER_QNAME).node(GAP_QNAME);
-
-        // instance identifier for accessing leaf node "name" in list "artist"
-        instanceIdMerge = YangInstanceIdentifier.builder()
-                .node(JUKEBOX_QNAME)
-                .node(LIBRARY_QNAME)
-                .node(ARTIST_QNAME)
-                .nodeWithKey(ARTIST_QNAME, NAME_QNAME, "name of artist")
-                .node(NAME_QNAME)
-                .build();
-
-        // values that are used for creating leaf for testPatchDataReplaceMergeAndRemove test
-        buildArtistList = Builders.mapBuilder()
-            .withNodeIdentifier(new NodeIdentifier(ARTIST_QNAME))
-            .withChild(Builders.mapEntryBuilder()
-                .withNodeIdentifier(NodeIdentifierWithPredicates.of(ARTIST_QNAME, NAME_QNAME, "name of artist"))
-                .withChild(ImmutableNodes.leafNode(NAME_QNAME, "name of artist"))
-                .withChild(ImmutableNodes.leafNode(DESCRIPTION_QNAME, "description of artist"))
-                .build())
-            .build();
-
-        targetNodeMerge = YangInstanceIdentifier.builder()
-                .node(JUKEBOX_QNAME)
-                .node(LIBRARY_QNAME)
-                .node(ARTIST_QNAME)
-                .nodeWithKey(ARTIST_QNAME, NAME_QNAME, "name of artist")
-                .build();
-
-        /* Mocks */
+    public void before() {
         doReturn(rwTransaction).when(mockDataBroker).newReadWriteTransaction();
         doReturn(CommitInfo.emptyFluentFuture()).when(rwTransaction).commit();
         doReturn(Futures.immediateFuture(new DefaultDOMRpcResult())).when(netconfService).commit();
@@ -136,22 +88,24 @@ public class PatchDataTransactionUtilTest extends AbstractJukeboxTest {
 
     @Test
     public void testPatchDataReplaceMergeAndRemove() {
-        final PatchEntity entityReplace =
-                new PatchEntity("edit1", REPLACE, targetNodeMerge, buildArtistList);
-        final PatchEntity entityMerge = new PatchEntity("edit2", MERGE, targetNodeMerge, buildArtistList);
-        final PatchEntity entityRemove = new PatchEntity("edit3", REMOVE, targetNodeMerge);
-        final List<PatchEntity> entities = new ArrayList<>();
+        final var buildArtistList = Builders.mapBuilder()
+            .withNodeIdentifier(new NodeIdentifier(ARTIST_QNAME))
+            .withChild(Builders.mapEntryBuilder()
+                .withNodeIdentifier(NodeIdentifierWithPredicates.of(ARTIST_QNAME, NAME_QNAME, "name of artist"))
+                .withChild(ImmutableNodes.leafNode(NAME_QNAME, "name of artist"))
+                .withChild(ImmutableNodes.leafNode(DESCRIPTION_QNAME, "description of artist"))
+                .build())
+            .build();
 
-        entities.add(entityReplace);
-        entities.add(entityMerge);
-        entities.add(entityRemove);
-
-        final InstanceIdentifierContext iidContext =
-            InstanceIdentifierContext.ofLocalPath(JUKEBOX_SCHEMA, instanceIdMerge);
-        final PatchContext patchContext = new PatchContext(iidContext, entities, "patchRMRm");
+        final var patchContext = new PatchContext(
+            InstanceIdentifierContext.ofLocalPath(JUKEBOX_SCHEMA, ARTIST_IID.node(NAME_QNAME)),
+            List.of(new PatchEntity("edit1", REPLACE, ARTIST_IID, buildArtistList),
+                new PatchEntity("edit2", MERGE, ARTIST_IID, buildArtistList),
+                new PatchEntity("edit3", REMOVE, ARTIST_IID)),
+            "patchRMRm");
 
         doReturn(Futures.immediateFuture(new DefaultDOMRpcResult())).when(netconfService)
-            .remove(LogicalDatastoreType.CONFIGURATION, targetNodeMerge);
+            .remove(LogicalDatastoreType.CONFIGURATION, ARTIST_IID);
 
         patch(patchContext, new MdsalRestconfStrategy(mockDataBroker), false);
         patch(patchContext, new NetconfRestconfStrategy(netconfService), false);
@@ -160,27 +114,18 @@ public class PatchDataTransactionUtilTest extends AbstractJukeboxTest {
     @Test
     public void testPatchDataCreateAndDelete() {
         doReturn(immediateFalseFluentFuture()).when(rwTransaction).exists(LogicalDatastoreType.CONFIGURATION,
-            instanceIdContainer);
+            PLAYER_IID);
         doReturn(immediateTrueFluentFuture()).when(rwTransaction).exists(LogicalDatastoreType.CONFIGURATION,
-            targetNodeForCreateAndDelete);
+            CREATE_AND_DELETE_TARGET);
         doReturn(Futures.immediateFuture(new DefaultDOMRpcResult())).when(netconfService)
-            .create(LogicalDatastoreType.CONFIGURATION, instanceIdContainer, buildBaseContainerForTests,
-                Optional.empty());
+            .create(LogicalDatastoreType.CONFIGURATION, PLAYER_IID, EMPTY_JUKEBOX, Optional.empty());
         doReturn(Futures.immediateFuture(new DefaultDOMRpcResult())).when(netconfService)
-            .delete(LogicalDatastoreType.CONFIGURATION, targetNodeForCreateAndDelete);
+            .delete(LogicalDatastoreType.CONFIGURATION, CREATE_AND_DELETE_TARGET);
 
-        final PatchEntity entityCreate =
-                new PatchEntity("edit1", CREATE, instanceIdContainer, buildBaseContainerForTests);
-        final PatchEntity entityDelete =
-                new PatchEntity("edit2", DELETE, targetNodeForCreateAndDelete);
-        final List<PatchEntity> entities = new ArrayList<>();
-
-        entities.add(entityCreate);
-        entities.add(entityDelete);
-
-        final InstanceIdentifierContext iidContext =
-            InstanceIdentifierContext.ofLocalPath(JUKEBOX_SCHEMA, instanceIdCreateAndDelete);
-        final PatchContext patchContext = new PatchContext(iidContext, entities, "patchCD");
+        final var patchContext = new PatchContext(InstanceIdentifierContext.ofLocalPath(JUKEBOX_SCHEMA, GAP_IID),
+            List.of(new PatchEntity("edit1", CREATE, PLAYER_IID, EMPTY_JUKEBOX),
+                new PatchEntity("edit2", DELETE, CREATE_AND_DELETE_TARGET)),
+            "patchCD");
         patch(patchContext, new MdsalRestconfStrategy(mockDataBroker), true);
         patch(patchContext, new NetconfRestconfStrategy(netconfService), true);
     }
@@ -188,47 +133,31 @@ public class PatchDataTransactionUtilTest extends AbstractJukeboxTest {
     @Test
     public void deleteNonexistentDataTest() {
         doReturn(immediateFalseFluentFuture()).when(rwTransaction).exists(LogicalDatastoreType.CONFIGURATION,
-            targetNodeForCreateAndDelete);
-        final NetconfDocumentedException exception = new NetconfDocumentedException("id",
-            ErrorType.RPC, ErrorTag.DATA_MISSING, ErrorSeverity.ERROR);
-        final SettableFuture<? extends DOMRpcResult> ret = SettableFuture.create();
-        ret.setException(new TransactionCommitFailedException(
-            String.format("Commit of transaction %s failed", this), exception));
-
-        doReturn(ret).when(netconfService).commit();
+            CREATE_AND_DELETE_TARGET);
+        doReturn(Futures.immediateFailedFuture(
+            new TransactionCommitFailedException("Commit of transaction " + this + " failed",
+                new NetconfDocumentedException("id", ErrorType.RPC, ErrorTag.DATA_MISSING, ErrorSeverity.ERROR))))
+            .when(netconfService).commit();
         doReturn(Futures.immediateFuture(new DefaultDOMRpcResult())).when(netconfService)
-            .delete(LogicalDatastoreType.CONFIGURATION, targetNodeForCreateAndDelete);
+            .delete(LogicalDatastoreType.CONFIGURATION, CREATE_AND_DELETE_TARGET);
 
-        final PatchEntity entityDelete = new PatchEntity("edit", DELETE, targetNodeForCreateAndDelete);
-        final List<PatchEntity> entities = new ArrayList<>();
-
-        entities.add(entityDelete);
-
-        final PatchContext patchContext = new PatchContext(
-            InstanceIdentifierContext.ofLocalPath(JUKEBOX_SCHEMA, instanceIdCreateAndDelete), entities, "patchD");
+        final var patchContext = new PatchContext(InstanceIdentifierContext.ofLocalPath(JUKEBOX_SCHEMA, GAP_IID),
+            List.of(new PatchEntity("edit", DELETE, CREATE_AND_DELETE_TARGET)), "patchD");
         deleteMdsal(patchContext, new MdsalRestconfStrategy(mockDataBroker));
         deleteNetconf(patchContext, new NetconfRestconfStrategy(netconfService));
     }
 
     @Test
     public void testPatchMergePutContainer() {
-        final PatchEntity entityMerge =
-                new PatchEntity("edit1", MERGE, instanceIdContainer, buildBaseContainerForTests);
-        final List<PatchEntity> entities = new ArrayList<>();
-
-        entities.add(entityMerge);
-
-        final InstanceIdentifierContext iidContext =
-            InstanceIdentifierContext.ofLocalPath(JUKEBOX_SCHEMA, instanceIdCreateAndDelete);
-        final PatchContext patchContext = new PatchContext(iidContext, entities, "patchM");
+        final var patchContext = new PatchContext(InstanceIdentifierContext.ofLocalPath(JUKEBOX_SCHEMA, GAP_IID),
+            List.of(new PatchEntity("edit1", MERGE, PLAYER_IID, EMPTY_JUKEBOX)), "patchM");
         patch(patchContext, new MdsalRestconfStrategy(mockDataBroker), false);
         patch(patchContext, new NetconfRestconfStrategy(netconfService), false);
     }
 
     private static void patch(final PatchContext patchContext, final RestconfStrategy strategy, final boolean failed) {
-        final PatchStatusContext patchStatusContext =
-                PatchDataTransactionUtil.patchData(patchContext, strategy, JUKEBOX_SCHEMA);
-        for (final PatchStatusEntity entity : patchStatusContext.getEditCollection()) {
+        final var patchStatusContext = PatchDataTransactionUtil.patchData(patchContext, strategy, JUKEBOX_SCHEMA);
+        for (var entity : patchStatusContext.getEditCollection()) {
             if (failed) {
                 assertTrue("Edit " + entity.getEditId() + " failed", entity.isOk());
             } else {
@@ -239,24 +168,25 @@ public class PatchDataTransactionUtilTest extends AbstractJukeboxTest {
     }
 
     private static void deleteMdsal(final PatchContext patchContext, final RestconfStrategy strategy) {
-        final PatchStatusContext patchStatusContext =
-                PatchDataTransactionUtil.patchData(patchContext, strategy, JUKEBOX_SCHEMA);
-
+        final var patchStatusContext = PatchDataTransactionUtil.patchData(patchContext, strategy, JUKEBOX_SCHEMA);
         assertFalse(patchStatusContext.isOk());
-        assertEquals(ErrorType.PROTOCOL,
-                patchStatusContext.getEditCollection().get(0).getEditErrors().get(0).getErrorType());
-        assertEquals(ErrorTag.DATA_MISSING,
-                patchStatusContext.getEditCollection().get(0).getEditErrors().get(0).getErrorTag());
+
+        final var editCollection = patchStatusContext.getEditCollection();
+        assertEquals(1, editCollection.size());
+        final var editErrors = patchStatusContext.getEditCollection().get(0).getEditErrors();
+        assertEquals(1, editErrors.size());
+        final var editError = editErrors.get(0);
+        assertEquals(ErrorType.PROTOCOL, editError.getErrorType());
+        assertEquals(ErrorTag.DATA_MISSING, editError.getErrorTag());
     }
 
     private static void deleteNetconf(final PatchContext patchContext, final RestconfStrategy strategy) {
-        final PatchStatusContext patchStatusContext = PatchDataTransactionUtil.patchData(patchContext, strategy,
-            JUKEBOX_SCHEMA);
-
+        final var patchStatusContext = PatchDataTransactionUtil.patchData(patchContext, strategy, JUKEBOX_SCHEMA);
         assertFalse(patchStatusContext.isOk());
-        assertEquals(ErrorType.PROTOCOL,
-            patchStatusContext.getGlobalErrors().get(0).getErrorType());
-        assertEquals(ErrorTag.DATA_MISSING,
-            patchStatusContext.getGlobalErrors().get(0).getErrorTag());
+        final var globalErrors = patchStatusContext.getGlobalErrors();
+        assertEquals(1, globalErrors.size());
+        final var globalError = globalErrors.get(0);
+        assertEquals(ErrorType.PROTOCOL, globalError.getErrorType());
+        assertEquals(ErrorTag.DATA_MISSING, globalError.getErrorTag());
     }
 }
