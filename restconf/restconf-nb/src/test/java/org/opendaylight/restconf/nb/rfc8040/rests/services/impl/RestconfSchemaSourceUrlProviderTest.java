75f09baa07d70e6dca5705e23946dd90accf2665
Yaroslav Lastivka <yaroslav.lastivka@pantheon.tech>
2023-08-02 13:39:07 +0300

    Restconf schema source URL provider for yanglib-mdsal-writer
    Due to restconf-nb contains the service to download yang
    resources it needs the component to provide valid URLs
    when yang-library data is constructed.
    
    JIRA: NETCONF-668
    Change-Id: I948bc12dd4852ff25cc7a47007c4de8eaa7bc495
    Signed-off-by: Ruslan Kashapov <ruslan.kashapov@pantheon.tech>

---
+++ restconf/restconf-nb/src/test/java/org/opendaylight/restconf/nb/rfc8040/rests/services/impl/RestconfSchemaSourceUrlProviderTest.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2023 PANTHEON.tech s.r.o. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.restconf.nb.rfc8040.rests.services.impl;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.opendaylight.restconf.nb.rfc8040.rests.services.impl.RestconfSchemaSourceUrlProvider.SUPPORTED_MODULE_SET_NAME;
+
+import java.util.stream.Stream;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev130715.Uri;
+import org.opendaylight.yangtools.yang.common.Revision;
+
+class RestconfSchemaSourceUrlProviderTest {
+
+    @Test
+    void unsupportedModuleSet() {
+        final var urlProvider = new RestconfSchemaSourceUrlProvider();
+        final var result = urlProvider.getSchemaSourceUrl("some-module-set", "module", null);
+        assertTrue(result.isEmpty());
+    }
+
+    @ParameterizedTest(name = "URL: {2}")
+    @MethodSource("getSchemaSourceUrlArgs")
+    void getSchemaSourceUrl(final String moduleName, final Revision revision, final Uri expected) {
+        final var urlProvider = new RestconfSchemaSourceUrlProvider();
+        final var result = urlProvider.getSchemaSourceUrl(SUPPORTED_MODULE_SET_NAME, moduleName, revision);
+        assertTrue(result.isPresent());
+        assertEquals(expected, result.orElseThrow());
+    }
+
+    private static Stream<Arguments> getSchemaSourceUrlArgs() {
+        return Stream.of(
+            // module-name, revision, expected result
+            Arguments.of("odl-module", Revision.of("2023-02-23"), new Uri("/rests/modules/odl-module/2023-02-23")),
+            Arguments.of("module-no-revision", null, new Uri("/rests/modules/module-no-revision"))
+        );
+    }
+}
