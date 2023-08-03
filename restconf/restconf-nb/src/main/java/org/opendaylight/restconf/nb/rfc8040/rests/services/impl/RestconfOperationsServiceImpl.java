7819c8fd21d053d9c7e93af8823e6719644d6eb5
Robert Varga <robert.varga@pantheon.tech>
2023-08-03 15:36:55 +0200

    Remove RestconfOperationsService
    This interface is not really useful, inline it into its sole
    implementation. Also rename the test to match its name and refactor it a
    bit to isolate state.
    
    JIRA: NETCONF-1117
    Change-Id: I81d92321bbc06b5aef32981a078565a02b4f2c33
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- restconf/restconf-nb/src/main/java/org/opendaylight/restconf/nb/rfc8040/rests/services/impl/RestconfOperationsServiceImpl.java
+++ restconf/restconf-nb/src/main/java/org/opendaylight/restconf/nb/rfc8040/rests/services/impl/RestconfOperationsServiceImpl.java
@@ -9,18 +9,22 @@ package org.opendaylight.restconf.nb.rfc8040.rests.services.impl;
 
 import static java.util.Objects.requireNonNull;
 
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 import org.opendaylight.mdsal.dom.api.DOMMountPointService;
+import org.opendaylight.restconf.nb.rfc8040.MediaTypes;
 import org.opendaylight.restconf.nb.rfc8040.databind.DatabindProvider;
-import org.opendaylight.restconf.nb.rfc8040.rests.services.api.RestconfOperationsService;
 import org.opendaylight.restconf.nb.rfc8040.utils.parser.ParserIdentifier;
 import org.opendaylight.yangtools.yang.model.api.EffectiveModelContext;
 
 /**
- * Implementation of {@link RestconfOperationsService}.
+ * Container that provides access to the data-model specific operations supported by the server.
  */
 @Path("/")
-public class RestconfOperationsServiceImpl implements RestconfOperationsService {
+public final class RestconfOperationsServiceImpl {
     private final DatabindProvider databindProvider;
     private final DOMMountPointService mountPointService;
 
@@ -36,24 +40,54 @@ public class RestconfOperationsServiceImpl implements RestconfOperationsService 
         this.mountPointService = requireNonNull(mountPointService);
     }
 
-    @Override
+    /**
+     * List RPC and action operations in RFC7951 format.
+     *
+     * @return A string containing a JSON document conforming to both RFC8040 and RFC7951.
+     */
+    @GET
+    @Path("/operations")
+    @Produces({ MediaTypes.APPLICATION_YANG_DATA_JSON, MediaType.APPLICATION_JSON })
     public String getOperationsJSON() {
         return OperationsContent.JSON.bodyFor(databindProvider.currentContext().modelContext());
     }
 
-    @Override
-    public String getOperationJSON(final String identifier) {
+    /**
+     * Retrieve list of operations and actions supported by the server or device in JSON format.
+     *
+     * @param identifier path parameter to identify device and/or operation
+     * @return A string containing a JSON document conforming to both RFC8040 and RFC7951.
+     */
+    @GET
+    @Path("/operations/{identifier:.+}")
+    @Produces({ MediaTypes.APPLICATION_YANG_DATA_JSON, MediaType.APPLICATION_JSON })
+    public String getOperationJSON(@PathParam("identifier") final String identifier) {
         return OperationsContent.JSON.bodyFor(ParserIdentifier.toInstanceIdentifier(identifier,
             databindProvider.currentContext().modelContext(), mountPointService));
     }
 
-    @Override
+    /**
+     * List RPC and action operations in RFC8040 XML format.
+     *
+     * @return A string containing an XML document conforming to both RFC8040 section 11.3.1 and page 84.
+     */
+    @GET
+    @Path("/operations")
+    @Produces({ MediaTypes.APPLICATION_YANG_DATA_XML, MediaType.APPLICATION_XML, MediaType.TEXT_XML })
     public String getOperationsXML() {
         return OperationsContent.XML.bodyFor(databindProvider.currentContext().modelContext());
     }
 
-    @Override
-    public String getOperationXML(final String identifier) {
+    /**
+     * Retrieve list of operations and actions supported by the server or device in XML format.
+     *
+     * @param identifier path parameter to identify device and/or operation
+     * @return A string containing an XML document conforming to both RFC8040 section 11.3.1 and page 84.
+     */
+    @GET
+    @Path("/operations/{identifier:.+}")
+    @Produces({ MediaTypes.APPLICATION_YANG_DATA_XML, MediaType.APPLICATION_XML, MediaType.TEXT_XML })
+    public String getOperationXML(@PathParam("identifier") final String identifier) {
         return OperationsContent.XML.bodyFor(ParserIdentifier.toInstanceIdentifier(identifier,
             databindProvider.currentContext().modelContext(), mountPointService));
     }
