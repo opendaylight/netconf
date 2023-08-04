850896130a9d3b13c9597e192a2f87686e429356
Yaroslav Lastivka <yaroslav.lastivka@pantheon.tech>
2023-08-04 10:40:33 +0300

    Use first child in POST payloads
    Enhanced the logic to generate POST payloads by choosing one
    child for both container and list types.
    Introduced unit tests to validate the correctness of the
    request body generated by this new logic.
    
    JIRA: NETCONF-1054
    Change-Id: I8568f570fe77a9ef0b7f20122feef6a85758ccef
    Signed-off-by: Yaroslav Lastivka <yaroslav.lastivka@pantheon.tech>

--- restconf/restconf-openapi/src/main/java/org/opendaylight/restconf/openapi/model/builder/OperationBuilder.java
+++ restconf/restconf-openapi/src/main/java/org/opendaylight/restconf/openapi/model/builder/OperationBuilder.java
@@ -16,6 +16,8 @@ import com.fasterxml.jackson.databind.node.JsonNodeFactory;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.StreamSupport;
 import javax.ws.rs.HttpMethod;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -24,6 +26,8 @@ import org.opendaylight.restconf.openapi.impl.DefinitionNames;
 import org.opendaylight.restconf.openapi.model.Operation;
 import org.opendaylight.restconf.openapi.model.Parameter;
 import org.opendaylight.restconf.openapi.model.Schema;
+import org.opendaylight.yangtools.yang.model.api.ContainerSchemaNode;
+import org.opendaylight.yangtools.yang.model.api.DataNodeContainer;
 import org.opendaylight.yangtools.yang.model.api.DataSchemaNode;
 import org.opendaylight.yangtools.yang.model.api.InputSchemaNode;
 import org.opendaylight.yangtools.yang.model.api.ListSchemaNode;
@@ -52,15 +56,23 @@ public final class OperationBuilder {
         // Hidden on purpose
     }
 
-    public static Operation buildPost(final String parentName, final String nodeName, final String discriminator,
-            final String moduleName, final @NonNull String deviceName, final String description,
-            final List<Parameter> pathParams) {
+    public static Operation buildPost(final DataSchemaNode node, final String parentName, final String nodeName,
+            final String discriminator, final String moduleName, final @NonNull String deviceName,
+            final String description, final List<Parameter> pathParams) {
         final var summary = SUMMARY_TEMPLATE.formatted(HttpMethod.POST, deviceName, moduleName, nodeName);
         final List<String> tags = List.of(deviceName + " " + moduleName);
         final List<Parameter> parameters = new ArrayList<>(pathParams);
-        final String cleanDefName = parentName + "_" + nodeName;
-        final String defName = cleanDefName + discriminator;
-        final ObjectNode requestBody = createPostDataRequestBodyParameter(defName, nodeName);
+        final ObjectNode requestBody;
+        final DataSchemaNode childNode = node != null ? getListOrContainerChildNode(node).orElse(null) : null;
+        if ((node instanceof ListSchemaNode || node instanceof ContainerSchemaNode)
+            && childNode != null && childNode.effectiveConfig().orElse(true)) {
+            final String childNodeName = childNode.getQName().getLocalName();
+            final  String childDefName = parentName + "_" + nodeName + "_" + childNodeName + discriminator;
+            requestBody = createRequestBodyParameter(childDefName, childNodeName, childNode, summary, childNodeName);
+        } else {
+            final String defName = parentName + "_" + nodeName + discriminator;
+            requestBody = createPostDataRequestBodyParameter(defName, nodeName);
+        }
         final ObjectNode responses = JsonNodeFactory.instance.objectNode();
         responses.set(String.valueOf(Response.Status.CREATED.getStatusCode()),
                 buildResponse(Response.Status.CREATED.getReasonPhrase()));
@@ -319,5 +331,14 @@ public final class OperationBuilder {
         response.set(CONTENT_KEY, content);
         response.put(DESCRIPTION_KEY, description);
         return response;
+    }
+
+    private static Optional<? extends DataSchemaNode> getListOrContainerChildNode(DataSchemaNode node) {
+        var childNodes = ((DataNodeContainer) node).getChildNodes()
+            .spliterator();
+        var firstNode = StreamSupport.stream(childNodes, false)
+            .filter(n -> n instanceof ListSchemaNode || n instanceof ContainerSchemaNode)
+            .findFirst();
+        return firstNode;
     }
 }
