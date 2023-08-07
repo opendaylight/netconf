0c37fc22f356742f2d224cfb37d7a69c04d2802c
Robert Varga <robert.varga@pantheon.tech>
2023-08-07 14:01:13 +0200

    Convert CredentialServiceAuthProviderTest to JUnit5
    This is a simplistic test, convert it.
    
    Change-Id: Iaac999d069ae16bf2f118d7c66c68d0166f7f682
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- plugins/netconf-auth-aaa/src/test/java/org/opendaylight/netconf/authprovider/CredentialServiceAuthProviderTest.java
+++ plugins/netconf-auth-aaa/src/test/java/org/opendaylight/netconf/authprovider/CredentialServiceAuthProviderTest.java
@@ -7,42 +7,49 @@
  */
 package org.opendaylight.netconf.authprovider;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.doThrow;
-import static org.mockito.Mockito.mock;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
-import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.opendaylight.aaa.api.AuthenticationException;
 import org.opendaylight.aaa.api.Claim;
 import org.opendaylight.aaa.api.PasswordCredentialAuth;
 import org.opendaylight.aaa.api.PasswordCredentials;
 import org.opendaylight.netconf.auth.aaa.CredentialServiceAuthProvider;
 
-@RunWith(MockitoJUnitRunner.StrictStubs.class)
-public class CredentialServiceAuthProviderTest {
+@ExtendWith(MockitoExtension.class)
+class CredentialServiceAuthProviderTest {
     @Mock
     private PasswordCredentialAuth credAuth;
+    @Mock
+    private Claim claim;
 
-    @Test
-    public void testAuthenticatedTrue() throws Exception {
-        Claim claim = mock(Claim.class);
-        doReturn("domain").when(claim).domain();
-        doReturn(claim).when(credAuth).authenticate(any(PasswordCredentials.class));
+    private CredentialServiceAuthProvider authProvider;
 
-        CredentialServiceAuthProvider credentialServiceAuthProvider = new CredentialServiceAuthProvider(credAuth);
-        assertTrue(credentialServiceAuthProvider.authenticated("user", "pwd"));
+    @BeforeEach
+    void beforeEach() {
+        authProvider = new CredentialServiceAuthProvider(credAuth);
     }
 
     @Test
-    public void testAuthenticatedFalse() throws Exception {
+    void testAuthenticatedTrue() {
+        doReturn("domain").when(claim).domain();
+        doReturn(claim).when(credAuth).authenticate(any(PasswordCredentials.class));
+
+        assertTrue(authProvider.authenticated("user", "pwd"));
+    }
+
+    @Test
+    void testAuthenticatedFalse() {
         doThrow(AuthenticationException.class).when(credAuth).authenticate(any(PasswordCredentials.class));
-        CredentialServiceAuthProvider credentialServiceAuthProvider = new CredentialServiceAuthProvider(credAuth);
-        assertFalse(credentialServiceAuthProvider.authenticated("user", "pwd"));
+
+        assertFalse(authProvider.authenticated("user", "pwd"));
     }
 }
