5575ff8607336d60b4a2d2a636efd8af07a22560
matus.matok <matus.matok@pantheon.tech>
2023-08-08 14:09:00 +0200

    Introduce RpcMessage and RpcReplyMessage
    Introduced RpcMessage and RpcReplyMessage both extending NetconfMessage, with
    basic wrapping functionality. Added tests to verify correct wrapping.
    
    JIRA: NETCONF-1014
    Change-Id: I72d5d03dde6553201d2fca4db1faf5d0a844d872
    Signed-off-by: matus.matok <matus.matok@pantheon.tech>
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ protocol/netconf-api/src/main/java/org/opendaylight/netconf/api/messages/RpcMessage.java
@@ -0,0 +1,87 @@
+/*
+ * Copyright (c) 2023 PANTHEON.tech s.r.o. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.netconf.api.messages;
+
+import static java.util.Objects.requireNonNull;
+
+import com.google.common.collect.ImmutableMap;
+import org.eclipse.jdt.annotation.NonNull;
+import org.opendaylight.netconf.api.DocumentedException;
+import org.opendaylight.netconf.api.NamespaceURN;
+import org.opendaylight.netconf.api.xml.XmlNetconfConstants;
+import org.opendaylight.yangtools.yang.common.ErrorSeverity;
+import org.opendaylight.yangtools.yang.common.ErrorTag;
+import org.opendaylight.yangtools.yang.common.ErrorType;
+import org.w3c.dom.Document;
+
+/**
+ * A NETCONF RPC request message, as defined in
+ * <a href="https://www.rfc-editor.org/rfc/rfc6241#section-4.1">RFC6241, section 4.1</a>. Its document element is
+ * guaranteed to be a <@code <rpc/>} in the {@value NamespaceURN#BASE} namespace.
+ */
+public final class RpcMessage extends NetconfMessage {
+    private RpcMessage(final Document document) {
+        super(document);
+    }
+
+    /**
+     * Return an {@link RpcMessage} backed by specified {@link Document}.
+     *
+     * @param document Backing document
+     * @return An {@link RpcMessage}
+     * @throws DocumentedException if the document's structure does not form a valid {@code <rpc/>} message
+     * @throws NullPointerException if {@code document} is {@code null}
+     */
+    public static @NonNull RpcMessage of(final Document document) throws DocumentedException {
+        final var root = document.getDocumentElement();
+        final var rootName = root.getLocalName();
+        if (!XmlNetconfConstants.RPC_KEY.equals(rootName)) {
+            throw new DocumentedException("Unexpected element name " + rootName, ErrorType.PROTOCOL,
+                ErrorTag.UNKNOWN_ELEMENT, ErrorSeverity.ERROR, ImmutableMap.of("bad-element", rootName));
+        }
+        final var rootNs = root.getNamespaceURI();
+        if (!NamespaceURN.BASE.equals(rootNs)) {
+            throw new DocumentedException("Unexpected element namespace " + rootNs, ErrorType.PROTOCOL,
+                ErrorTag.UNKNOWN_NAMESPACE, ErrorSeverity.ERROR, ImmutableMap.of(
+                    "bad-element", rootName,
+                    "bad-namespace", rootNs));
+        }
+
+        final var messageIdAttr = root.getAttributeNode(XmlNetconfConstants.MESSAGE_ID);
+        if (messageIdAttr == null) {
+            throw new DocumentedException("Missing message-id attribute", ErrorType.RPC, ErrorTag.MISSING_ATTRIBUTE,
+                ErrorSeverity.ERROR, ImmutableMap.of(
+                    "bad-attribute", XmlNetconfConstants.MESSAGE_ID,
+                    "bad-element", XmlNetconfConstants.RPC_KEY));
+        }
+        if (!messageIdAttr.getSpecified()) {
+            throw new IllegalArgumentException("Document element's message-id attribute is not specified");
+        }
+        return new RpcMessage(document);
+    }
+
+    /**
+     * Return an {@link RpcMessage} wrapping an RPC operation supplied as {@link Document}. The supplied document is
+     * modified to have its document element replaced with a {@code <rpc/>} element which contains it.
+     *
+     * @param document Backing operation document
+     * @return An {@link RpcMessage}
+     * @throws NullPointerException if any argument is {@code null}
+     */
+    public static @NonNull RpcMessage ofOperation(final String messageId, final Document document) {
+        final var rpcElem = document.createElementNS(NamespaceURN.BASE, XmlNetconfConstants.RPC_KEY);
+        rpcElem.appendChild(document.getDocumentElement());
+        rpcElem.setAttribute(XmlNetconfConstants.MESSAGE_ID, requireNonNull(messageId));
+        document.appendChild(rpcElem);
+        return new RpcMessage(document);
+    }
+
+    public @NonNull String messageId() {
+        return getDocument().getDocumentElement().getAttribute(XmlNetconfConstants.MESSAGE_ID);
+    }
+}
---
+++ protocol/netconf-api/src/test/java/org/opendaylight/netconf/api/messages/RpcReplyMessageTest.java
@@ -0,0 +1,88 @@
+/*
+ * Copyright (c) 2023 PANTHEON.tech s.r.o. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.netconf.api.messages;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import com.google.common.collect.ImmutableMap;
+import org.junit.jupiter.api.Test;
+import org.opendaylight.netconf.api.DocumentedException;
+import org.opendaylight.netconf.api.xml.XmlNetconfConstants;
+import org.opendaylight.yangtools.util.xml.UntrustedXML;
+import org.opendaylight.yangtools.yang.common.ErrorSeverity;
+import org.opendaylight.yangtools.yang.common.ErrorTag;
+import org.opendaylight.yangtools.yang.common.ErrorType;
+import org.w3c.dom.Document;
+
+class RpcReplyMessageTest {
+    private final Document document = UntrustedXML.newDocumentBuilder().newDocument();
+
+    @Test
+    void testOfDocument() throws Exception {
+        final var rootElement = document.createElementNS("urn:ietf:params:xml:ns:netconf:base:1.0", "rpc-reply");
+        rootElement.setAttribute("message-id", "foo");
+        document.appendChild(rootElement);
+
+        final var msg = RpcReplyMessage.of(document);
+        assertEquals("foo", msg.messageId());
+        assertEquals("""
+            <rpc-reply xmlns="urn:ietf:params:xml:ns:netconf:base:1.0" message-id="foo"/>
+            """, msg.toString());
+    }
+
+    @Test
+    void testOfException() throws Exception {
+        final var msg = RpcReplyMessage.of(new DocumentedException("Missing message-id attribute",
+            ErrorType.RPC, ErrorTag.MISSING_ATTRIBUTE, ErrorSeverity.ERROR, ImmutableMap.of(
+                "bad-attribute", XmlNetconfConstants.MESSAGE_ID,
+                "bad-element", XmlNetconfConstants.RPC_KEY)));
+        assertEquals("""
+            <rpc-reply xmlns="urn:ietf:params:xml:ns:netconf:base:1.0">
+                <rpc-error>
+                    <error-type>rpc</error-type>
+                    <error-tag>missing-attribute</error-tag>
+                    <error-severity>error</error-severity>
+                    <error-message>Missing message-id attribute</error-message>
+                    <error-info>
+                        <bad-attribute>message-id</bad-attribute>
+                        <bad-element>rpc</bad-element>
+                    </error-info>
+                </rpc-error>
+            </rpc-reply>
+            """, msg.toString());
+    }
+
+    @Test
+    void testOfBadElementName() {
+        final var rootElement = document.createElementNS("urn:ietf:params:xml:ns:netconf:base:1.0", "bad");
+        rootElement.setAttribute("message-id", "foo");
+        document.appendChild(rootElement);
+
+        final var ex = assertThrows(DocumentedException.class, () -> RpcReplyMessage.of(document));
+        assertEquals("Unexpected element name bad", ex.getMessage());
+        assertEquals(ErrorSeverity.ERROR, ex.getErrorSeverity());
+        assertEquals(ErrorType.PROTOCOL, ex.getErrorType());
+        assertEquals(ErrorTag.UNKNOWN_ELEMENT, ex.getErrorTag());
+        assertEquals(ImmutableMap.of("bad-element", "bad"), ex.getErrorInfo());
+    }
+
+    @Test
+    void testOfBadElementNs() {
+        final var rootElement = document.createElementNS("bad", "rpc-reply");
+        rootElement.setAttribute("message-id", "foo");
+        document.appendChild(rootElement);
+
+        final var ex = assertThrows(DocumentedException.class, () -> RpcReplyMessage.of(document));
+        assertEquals("Unexpected element namespace bad", ex.getMessage());
+        assertEquals(ErrorSeverity.ERROR, ex.getErrorSeverity());
+        assertEquals(ErrorType.PROTOCOL, ex.getErrorType());
+        assertEquals(ErrorTag.UNKNOWN_NAMESPACE, ex.getErrorTag());
+        assertEquals(ImmutableMap.of("bad-element", "rpc-reply", "bad-namespace", "bad"), ex.getErrorInfo());
+    }
+}
