67c90a6b136a3b6bc27f8c477cd4e8cf10697bb6
matus.matok <matus.matok@pantheon.tech>
2023-08-08 14:09:00 +0200

    Introduce RpcMessage and RpcReplyMessage
    Introduced RpcMessage and RpcReplyMessage both extending NetconfMessage, with
    basic wrapping functionality.
    Added tests to verify correct wrapping.
    
    JIRA: NETCONF-1014
    Change-Id: I72d5d03dde6553201d2fca4db1faf5d0a844d872
    Signed-off-by: matus.matok <matus.matok@pantheon.tech>

---
+++ protocol/netconf-api/src/main/java/org/opendaylight/netconf/api/messages/RpcReplyMessage.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) 2023 PANTHEON.tech s.r.o. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.netconf.api.messages;
+
+import static java.util.Objects.requireNonNull;
+
+import org.opendaylight.netconf.api.NamespaceURN;
+import org.opendaylight.netconf.api.xml.XmlNetconfConstants;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+public final class RpcReplyMessage extends NetconfMessage {
+
+    private RpcReplyMessage(final Document document) {
+        super(document);
+    }
+
+    /**
+     * Create new RpcReplyMessage with provided message ID.
+     */
+    public static RpcReplyMessage wrapDocumentAsRpcReply(final Document document, final String messageId) {
+        return new RpcReplyMessage(wrapRpcReply(document, messageId), messageId);
+    }
+
+    private static Document wrapRpcReply(final Document rpcContent, final String messageId) {
+        requireNonNull(rpcContent);
+
+        final Element baseRpc = rpcContent.getDocumentElement();
+        final Element entireRpc = rpcContent.createElementNS(NamespaceURN.BASE, XmlNetconfConstants.RPC_REPLY_KEY);
+        entireRpc.appendChild(baseRpc);
+        entireRpc.setAttributeNS(NamespaceURN.BASE, XmlNetconfConstants.MESSAGE_ID, messageId);
+
+        rpcContent.appendChild(entireRpc);
+        return rpcContent;
+    }
+}
