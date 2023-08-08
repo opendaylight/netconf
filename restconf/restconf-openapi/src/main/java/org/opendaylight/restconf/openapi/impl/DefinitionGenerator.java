0b734f87b4702d46308f3a367c2e476dc98b9687
Oleksandr Zharov <oleksandr.zharov@pantheon.tech>
2023-08-08 14:18:59 +0200

    Eliminate TOP schemas
    Removed TOP schemas from OpenApi object.
    
    JIRA: NETCONF-1057
    Change-Id: I88d8d5f5f52f9215d1124a26e54330252580c8f8
    Signed-off-by: Oleksandr Zharov <oleksandr.zharov@pantheon.tech>

--- restconf/restconf-openapi/src/main/java/org/opendaylight/restconf/openapi/impl/DefinitionGenerator.java
+++ restconf/restconf-openapi/src/main/java/org/opendaylight/restconf/openapi/impl/DefinitionGenerator.java
@@ -10,9 +10,7 @@ package org.opendaylight.restconf.openapi.impl;
 import static org.opendaylight.restconf.openapi.impl.BaseYangOpenApiGenerator.MODULE_NAME_SUFFIX;
 import static org.opendaylight.restconf.openapi.model.builder.OperationBuilder.COMPONENTS_PREFIX;
 import static org.opendaylight.restconf.openapi.model.builder.OperationBuilder.NAME_KEY;
-import static org.opendaylight.restconf.openapi.model.builder.OperationBuilder.TOP;
 import static org.opendaylight.restconf.openapi.model.builder.OperationBuilder.XML_KEY;
-import static org.opendaylight.restconf.openapi.util.RestDocgenUtil.resolveFullNameFromNode;
 
 import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.JsonNodeFactory;
@@ -311,19 +309,17 @@ public final class DefinitionGenerator {
             processChildren(childSchemaBuilder, container.getChildNodes(), parentName, definitions, definitionNames,
                 stack, module);
             final String discriminator =
-                definitionNames.pickDiscriminator(container, List.of(filename, filename + TOP));
+                definitionNames.pickDiscriminator(container, List.of(filename));
             definitions.put(filename + discriminator, childSchemaBuilder.build());
-            processTopData(filename, discriminator, definitions, container, stack.getEffectiveModelContext());
         }
         stack.exit();
     }
 
-    private static ObjectNode processTopData(final String filename, final String discriminator,
-            final Map<String, Schema> definitions, final SchemaNode schemaNode, final EffectiveModelContext context) {
+    private static ObjectNode processRef(final String filename, final String discriminator,
+            final SchemaNode schemaNode) {
         final ObjectNode dataNodeProperties = JsonNodeFactory.instance.objectNode();
         final String name = filename + discriminator;
         final String ref = COMPONENTS_PREFIX + name;
-        final String topName = filename + TOP;
 
         if (schemaNode instanceof ListSchemaNode) {
             dataNodeProperties.put(TYPE_KEY, ARRAY_TYPE);
@@ -338,16 +334,6 @@ public final class DefinitionGenerator {
               */
             dataNodeProperties.put(REF_KEY, ref);
         }
-
-        final ObjectNode properties = JsonNodeFactory.instance.objectNode();
-        properties.set(resolveFullNameFromNode(schemaNode.getQName(), context), dataNodeProperties);
-        final var schema = new Schema.Builder()
-            .type(OBJECT_TYPE)
-            .properties(properties)
-            .title(topName)
-            .build();
-
-        definitions.put(topName + discriminator, schema);
 
         return dataNodeProperties;
     }
@@ -399,9 +385,7 @@ public final class DefinitionGenerator {
         final String discriminator;
         if (!definitionNames.isListedNode(schemaNode)) {
             final String parentNameConfigLocalName = parentName + "_" + localName;
-            final String nameAsParent = parentName + "_" + localName;
-            final List<String> names = List.of(parentNameConfigLocalName, parentNameConfigLocalName + TOP,
-                nameAsParent, nameAsParent + TOP);
+            final List<String> names = List.of(parentNameConfigLocalName);
             discriminator = definitionNames.pickDiscriminator(schemaNode, names);
         } else {
             discriminator = definitionNames.getDiscriminator(schemaNode);
@@ -411,7 +395,7 @@ public final class DefinitionGenerator {
         childSchemaBuilder.xml(buildXmlParameter(schemaNode));
         definitions.put(defName, childSchemaBuilder.build());
 
-        return processTopData(nodeName, discriminator, definitions, schemaNode, stack.getEffectiveModelContext());
+        return processRef(nodeName, discriminator, schemaNode);
     }
 
     /**
