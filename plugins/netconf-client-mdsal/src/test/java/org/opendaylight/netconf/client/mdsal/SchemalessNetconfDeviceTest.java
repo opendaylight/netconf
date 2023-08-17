0c78ec44153ff2ddc733253ea1742a33f0bc59f5
matus.matok <matus.matok@pantheon.tech>
2023-08-17 19:28:19 +0200

    Fix NetconfMessage payloads in tests
    Usage of NetconfMessage.of(Document) method requires documents to represent a valid NetconfMessage,
    either notification, nello, rpc or rpc-reply. Some of the documents were malformed, which would cause test
    failures. We fixed those inputs, enabling the following patch to have all its test passing.
    
    JIRA: NETCONF-1014
    Change-Id: I327fda47080c4c35b722b0ee3a79a221bc4d187b
    Signed-off-by: matus.matok <matus.matok@pantheon.tech>

--- plugins/netconf-client-mdsal/src/test/java/org/opendaylight/netconf/client/mdsal/SchemalessNetconfDeviceTest.java
+++ plugins/netconf-client-mdsal/src/test/java/org/opendaylight/netconf/client/mdsal/SchemalessNetconfDeviceTest.java
@@ -20,7 +20,8 @@ import org.junit.Test;
 import org.mockito.Mockito;
 import org.opendaylight.mdsal.dom.api.DOMNotification;
 import org.opendaylight.netconf.api.CapabilityURN;
-import org.opendaylight.netconf.api.messages.NetconfMessage;
+import org.opendaylight.netconf.api.messages.RpcMessage;
+import org.opendaylight.netconf.api.xml.XmlUtil;
 import org.opendaylight.netconf.client.mdsal.api.NetconfSessionPreferences;
 import org.opendaylight.netconf.client.mdsal.api.RemoteDeviceHandler;
 import org.opendaylight.netconf.client.mdsal.api.RemoteDeviceId;
@@ -48,7 +49,8 @@ public class SchemalessNetconfDeviceTest extends AbstractBaseSchemasTest {
         final NetconfSessionPreferences sessionCaps = getSessionCaps(true,
                 Lists.newArrayList(TEST_NAMESPACE + "?module=" + TEST_MODULE + "&amp;revision=" + TEST_REVISION));
 
-        final NetconfMessage netconfMessage = mock(NetconfMessage.class);
+        final RpcMessage netconfMessage = RpcMessage.wrapDocumentAsRpc(
+            XmlUtil.readXmlToElement("<test/>").getOwnerDocument(),"id");
 
         device.onRemoteSessionUp(sessionCaps, listener);
         verify(facade).onDeviceConnected(
