1097a09f5342e062fc3b192d7b4e017a52d30585
lubos-cicut <lubos.cicut@pantheon.tech>
2023-08-21 12:55:40 +0200

    Convert SchemaExportContext to Java record
    SchemaExportContext Class converted into Java Record. Usage
    of methods simplified.
    
    JIRA: NETCONF-1136
    Change-Id: Ia2f4ee168ca5e94bb883bbb4075886a91646fad9
    Signed-off-by: lubos-cicut <lubos.cicut@pantheon.tech>

--- restconf/restconf-nb/src/main/java/org/opendaylight/restconf/nb/rfc8040/jersey/providers/YinSchemaExportBodyWriter.java
+++ restconf/restconf-nb/src/main/java/org/opendaylight/restconf/nb/rfc8040/jersey/providers/YinSchemaExportBodyWriter.java
@@ -28,7 +28,7 @@ public class YinSchemaExportBodyWriter extends AbstractSchemaExportBodyWriter {
             final Annotation[] annotations, final MediaType mediaType,
             final MultivaluedMap<String, Object> httpHeaders, final OutputStream entityStream) throws IOException {
         try {
-            YinExportUtils.writeModuleAsYinText(context.getModule().asEffectiveStatement(), entityStream);
+            YinExportUtils.writeModuleAsYinText(context.module().asEffectiveStatement(), entityStream);
         } catch (final XMLStreamException e) {
             throw new IOException("Failed to export module", e);
         }
