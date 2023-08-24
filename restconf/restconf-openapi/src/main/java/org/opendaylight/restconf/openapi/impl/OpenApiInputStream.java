79950a2ae0ee6ad8cebdd074b9d14bd25811b8a0
Ivan Hrasko <ivan.hrasko@pantheon.tech>
2023-08-24 17:01:05 +0200

    WIP: Draft reading YANG context into OpenApiInputStream
    We would like to create JSON document according to
    OpenApi specification from EffectiveModelContext.
    
    We are going to use OpenApiInputStream to create overall
    document structure by deploying nested input streams.
    
    For example we are creating nested input stream for 'paths'
    where we are iterating over EffectiveModelContext multiple times
    (for each module) to read each module's paths by dedicated input
    stream. This way we will never have in memory more paths than paths
    for one module.
    
    The same applies for schemas.
    
    Some parts of the document can be done without iterating over
    EffectiveModelContext, like 'info', 'servers', etc. These are just
    read from their respective entities where we even can use hard-coded
    or constant values.
    
    JIRA: NETCONF-938
    Change-Id: I75db8424e5559786f402a6ed79d35b2ff8ec647e
    Signed-off-by: Ivan Hrasko <ivan.hrasko@pantheon.tech>

---
+++ restconf/restconf-openapi/src/main/java/org/opendaylight/restconf/openapi/impl/OpenApiInputStream.java
@@ -0,0 +1,86 @@
+/*
+ * Copyright (c) 2023 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.restconf.openapi.impl;
+
+import com.fasterxml.jackson.core.JsonFactoryBuilder;
+import com.fasterxml.jackson.core.JsonGenerator;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.List;
+import java.util.Map;
+import org.opendaylight.restconf.openapi.jaxrs.OpenApiBodyWriter;
+import org.opendaylight.restconf.openapi.model.Info;
+import org.opendaylight.restconf.openapi.model.InfoEntity;
+import org.opendaylight.restconf.openapi.model.OpenApiVersionEntity;
+import org.opendaylight.restconf.openapi.model.Server;
+import org.opendaylight.restconf.openapi.model.ServerEntity;
+import org.opendaylight.restconf.openapi.model.ServersEntity;
+import org.opendaylight.yangtools.yang.model.api.EffectiveModelContext;
+
+public final class OpenApiInputStream extends InputStream {
+    private final ByteArrayOutputStream stream = new ByteArrayOutputStream();
+    private final JsonGenerator generator = new JsonFactoryBuilder().build().createGenerator(stream);
+    private final Deque<InputStream> stack = new ArrayDeque<>();
+
+    private Reader reader;
+
+    private boolean eof;
+
+    public OpenApiInputStream(final EffectiveModelContext context, final String openApiVersion, final Info info,
+            final List<Server> servers, final List<Map<String, List<String>>> security) throws IOException {
+        final OpenApiBodyWriter writer = new OpenApiBodyWriter(generator, stream);
+        stack.add(new OpenApiVersionStream(new OpenApiVersionEntity(), writer));
+        stack.add(new InfoStream(new InfoEntity(info.version(), info.title(), info.description()), writer));
+        stack.add(new ServersStream(new ServersEntity(
+            List.of(new ServerEntity(servers.iterator().next().url()))), writer));
+        stack.add(new PathsSteam(context, writer, generator, stream));
+        stack.add(new SchemasStream(context, writer, generator, stream));
+        stack.add(new SecurityStream(writer));
+    }
+
+    @Override
+    public int read() throws IOException {
+        if (eof) {
+            return -1;
+        }
+        if (reader == null) {
+            generator.writeStartObject();
+            generator.flush();
+            reader = new InputStreamReader(new ByteArrayInputStream(stream.toByteArray()), StandardCharsets.UTF_8);
+            stream.reset();
+        }
+
+        var read = reader.read();
+        while (read == -1) {
+            if (stack.isEmpty()) {
+                generator.writeEndObject();
+                generator.flush();
+                reader = new InputStreamReader(new ByteArrayInputStream(stream.toByteArray()), StandardCharsets.UTF_8);
+                stream.reset();
+                eof = true;
+                return reader.read();
+            }
+            reader = new InputStreamReader(stack.pop(), StandardCharsets.UTF_8);
+            read = reader.read();
+        }
+
+        return read;
+    }
+
+    @Override
+    public int read(final byte[] array, final int off, final int len) throws IOException {
+        return super.read(array, off, len);
+    }
+}
