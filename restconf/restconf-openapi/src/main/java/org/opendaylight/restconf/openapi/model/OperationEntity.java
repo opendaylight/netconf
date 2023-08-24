70f50a56a2834cad13b10eb15e5b582e67340099
Ivan Hrasko <ivan.hrasko@pantheon.tech>
2023-08-24 17:01:05 +0200

    WIP: Draft reading YANG context into OpenApiInputStream
    We would like to create JSON document according to
    OpenApi specification from EffectiveModelContext.
    
    We are going to use OpenApiInputStream to create overall
    document structure by deploying nested input streams.
    
    For example we are creating nested input stream for 'paths'
    where we are iterating over EffectiveModelContext multiple times
    (for each module) to read each module's paths by dedicated input
    stream. This way we will never have in memory more paths than paths
    for one module.
    
    The same applies for schemas.
    
    Some parts of the document can be done without iterating over
    EffectiveModelContext, like 'info', 'servers', etc. These are just
    read from their respective entities where we even can use hard-coded
    or constant values.
    
    JIRA: NETCONF-938
    Change-Id: I75db8424e5559786f402a6ed79d35b2ff8ec647e
    Signed-off-by: Ivan Hrasko <ivan.hrasko@pantheon.tech>

---
+++ restconf/restconf-openapi/src/main/java/org/opendaylight/restconf/openapi/model/OperationEntity.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright (c) 2023 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.restconf.openapi.model;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import java.io.IOException;
+import org.eclipse.jdt.annotation.NonNull;
+import org.eclipse.jdt.annotation.Nullable;
+import org.opendaylight.yangtools.yang.model.api.OperationDefinition;
+
+/**
+ * Archetype for an Operation.
+ */
+public abstract sealed class OperationEntity extends OpenApiEntity permits PostEntity {
+    private final OperationDefinition schema;
+    private final String deviceName;
+    private final String moduleName;
+
+    protected OperationDefinition schema() {
+        return schema;
+    }
+
+    protected String deviceName() {
+        return deviceName;
+    }
+
+    protected String moduleName() {
+        return moduleName;
+    }
+
+    public OperationEntity(final OperationDefinition schema, final String deviceName, final String moduleName) {
+        this.schema = schema;
+        this.deviceName = deviceName;
+        this.moduleName = moduleName;
+    }
+
+    @Override
+    public void generate(@NonNull JsonGenerator generator) throws IOException {
+        generator.writeObjectFieldStart(operation());
+        final var deprecated = deprecated();
+        if (deprecated != null) {
+            generator.writeBooleanField("deprecated", deprecated);
+        }
+        final var description = description();
+        if (description != null) {
+            generator.writeStringField("description", description);
+        }
+        final var summary = summary();
+        if (summary != null) {
+            generator.writeStringField("summary", summary);
+        }
+        generator.writeEndObject();
+    }
+
+    protected abstract String operation();
+
+    @Nullable Boolean deprecated() {
+        return Boolean.FALSE;
+    }
+
+    @Nullable String description() {
+        return null;
+    }
+
+    @Nullable abstract String summary();
+}
