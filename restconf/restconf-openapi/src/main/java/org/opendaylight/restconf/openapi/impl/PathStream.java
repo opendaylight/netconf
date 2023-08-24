37560c681292abc51bb0e9b0b4c24bb95a7b56d5
Ivan Hrasko <ivan.hrasko@pantheon.tech>
2023-08-24 17:01:05 +0200

    WIP: Draft reading YANG context into OpenApiInputStream
    We would like to create JSON document according to
    OpenApi specification from EffectiveModelContext.
    
    We are going to use OpenApiInputStream to create overall
    document structure by deploying nested input streams.
    
    For example we are creating nested input stream for 'paths'
    where we are iterating over EffectiveModelContext multiple times
    (for each module) to read each module's paths by dedicated input
    stream. This way we will never have in memory more paths than paths
    for one module.
    
    The same applies for schemas.
    
    Some parts of the document can be done without iterating over
    EffectiveModelContext, like 'info', 'servers', etc. These are just
    read from their respective entities where we even can use hard-coded
    or constant values.
    
    JIRA: NETCONF-938
    Change-Id: I75db8424e5559786f402a6ed79d35b2ff8ec647e
    Signed-off-by: Ivan Hrasko <ivan.hrasko@pantheon.tech>

---
+++ restconf/restconf-openapi/src/main/java/org/opendaylight/restconf/openapi/impl/PathStream.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright (c) 2023 PANTHEON.tech s.r.o and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.restconf.openapi.impl;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.nio.charset.StandardCharsets;
+import java.util.Deque;
+import org.opendaylight.restconf.openapi.jaxrs.OpenApiBodyWriter;
+import org.opendaylight.restconf.openapi.model.OpenApiEntity;
+import org.opendaylight.restconf.openapi.model.PathEntity;
+
+public final class PathStream extends InputStream {
+    private final Deque<PathEntity> stack;
+    private final OpenApiBodyWriter writer;
+
+    private Reader reader;
+
+    public PathStream(final Deque<PathEntity> paths, final OpenApiBodyWriter writer) {
+        this.stack = paths;
+        this.writer = writer;
+    }
+
+    @Override
+    public int read() throws IOException {
+        if (reader == null) {
+            if (stack.isEmpty()) {
+                return -1;
+            }
+            reader = new InputStreamReader(new ByteArrayInputStream(writeNextEntity(stack.pop())),
+                StandardCharsets.UTF_8);
+        }
+
+        var read = reader.read();
+        while (read == -1) {
+            if (stack.isEmpty()) {
+                return -1;
+            }
+            reader = new InputStreamReader(new ByteArrayInputStream(writeNextEntity(stack.pop())),
+                StandardCharsets.UTF_8);
+            read = reader.read();
+        }
+
+        return read;
+    }
+
+    @Override
+    public int read(final byte[] array, final int off, final int len) throws IOException {
+        return super.read(array, off, len);
+    }
+
+    private byte[] writeNextEntity(final OpenApiEntity entity) throws IOException {
+        writer.writeTo(entity, null, null, null, null, null, null);
+        return writer.readFrom();
+    }
+}
---
+++ restconf/restconf-openapi/src/main/java/org/opendaylight/restconf/openapi/model/ServersEntity.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2023 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.restconf.openapi.model;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import java.io.IOException;
+import java.util.List;
+import org.eclipse.jdt.annotation.NonNull;
+
+public final class ServersEntity extends OpenApiEntity {
+    private final List<ServerEntity> servers;
+
+    public ServersEntity(final List<ServerEntity> servers) {
+        this.servers = servers;
+    }
+
+    @Override
+    public void generate(@NonNull final JsonGenerator generator) throws IOException {
+        generator.writeArrayFieldStart("servers");
+        for (final var server : servers) {
+            server.generate(generator);
+        }
+        generator.writeEndArray();
+    }
+}
