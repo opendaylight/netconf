6542d2188112aafbd33a0aebb19b6da49d1c6985
Šimon Ukuš <simon.ukus@pantheon.tech>
2023-08-25 13:44:00 +0200

    Rework servers record by eliminating JsonNodes
    The servers field in the Path record, although not used, is incorrectly
    defined as an ObjectNode. The OpenApi Specification declares that such
    field is an array of Server Objects.
    
    This change modifies the field declaration accordingly  and extends
    the already present Server record by additional fields.
    Introducing ServerVariable, conforming to the specification.
    
    JIRA: NETCONF-938
    Change-Id: I7b8141c38db18c49585a05e427ef64922db4bf6b
    Signed-off-by: Šimon Ukuš <simon.ukus@pantheon.tech>

--- restconf/restconf-openapi/src/main/java/org/opendaylight/restconf/openapi/model/Server.java
+++ restconf/restconf-openapi/src/main/java/org/opendaylight/restconf/openapi/model/Server.java
@@ -7,9 +7,36 @@
  */
 package org.opendaylight.restconf.openapi.model;
 
+import static java.util.Objects.requireNonNull;
+
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import java.util.Map;
+import org.eclipse.jdt.annotation.NonNull;
+import org.eclipse.jdt.annotation.Nullable;
 
+/**
+ * Represents a Server. The target host is indicated by the {@link Server#url} field.
+ * This URL supports server variables, which are specified in the {@link Server#variables} field
+ * and must be enclosed in {brackets} within the URL.
+ *
+ * <p>
+ * In the example URL: <b>http://localhost:8181/openapi/api/v3/mounts/{id}</b>,
+ * 'id' represents the variable name (key), and the corresponding ServerVariable (value)
+ * contains information about possible values.
+ *
+ * @param url <b>Required</b> {@link String} representing the URL to the target host.
+ * @param description {@link String} describing the host designated by the URL.
+ * @param variables {@link Map} of variables used for substitution, where the key is the variable name
+ *                             and value is a {@link ServerVariable} object containing relevant information.
+ */
 @JsonInclude(Include.NON_NULL)
-public record Server(String url) {
+public record Server(
+        @NonNull String url,
+        @Nullable String description,
+        @Nullable Map<String, ServerVariable> variables) {
+
+    public Server {
+        requireNonNull(url);
+    }
 }
