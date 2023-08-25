1f1c8fef3cffe97ad20811631493187eab25827e
Robert Varga <robert.varga@pantheon.tech>
2023-08-25 12:41:53 +0200

    Eliminate TestRestconfUtils
    This class is largely unused. Move the single used method to
    AbstractBodyReaderTest, adjusting its users.
    
    Change-Id: I26aa4f974c9bc7e3f431a470dc9a05571eebd330
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- restconf/restconf-nb/src/test/java/org/opendaylight/restconf/nb/rfc8040/jersey/providers/XmlNormalizedNodeBodyWriterTest.java
+++ restconf/restconf-nb/src/test/java/org/opendaylight/restconf/nb/rfc8040/jersey/providers/XmlNormalizedNodeBodyWriterTest.java
@@ -18,13 +18,13 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.opendaylight.restconf.common.context.InstanceIdentifierContext;
-import org.opendaylight.restconf.nb.rfc8040.TestRestconfUtils;
 import org.opendaylight.restconf.nb.rfc8040.legacy.NormalizedNodePayload;
 import org.opendaylight.yangtools.yang.common.QName;
 import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.NodeIdentifier;
 import org.opendaylight.yangtools.yang.data.impl.schema.Builders;
 import org.opendaylight.yangtools.yang.model.api.EffectiveModelContext;
 import org.opendaylight.yangtools.yang.model.api.SchemaContext;
+import org.opendaylight.yangtools.yang.test.util.YangParserTestUtils;
 
 @RunWith(MockitoJUnitRunner.StrictStubs.class)
 public class XmlNormalizedNodeBodyWriterTest {
@@ -46,8 +46,7 @@ public class XmlNormalizedNodeBodyWriterTest {
 
     @Test
     public void testRootContainerWrite() throws IOException {
-        final EffectiveModelContext schemaContext =
-                TestRestconfUtils.loadSchemaContext("/instanceidentifier/yang", null);
+        final var schemaContext = YangParserTestUtils.parseYangResourceDirectory("/instanceidentifier/yang");
 
         final NormalizedNodePayload nodePayload = NormalizedNodePayload.of(
             InstanceIdentifierContext.ofLocalRoot(schemaContext),
