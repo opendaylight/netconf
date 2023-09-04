f8f36fcb289a603d6e46d01152b827164297210e
Yaroslav Lastivka <yaroslav.lastivka@pantheon.tech>
2023-09-04 13:04:51 +0300

    Fix incorrect operational state of device configuration
    Initialization of clientConfig in NetconfNodeHandler can throw
    a RuntimeException, leaving no handler to clean up
    the operational datastore. This results in garbage
    data upon node deletion.
    
    Made clientConfig nullable and wrapped its initialization in
    a try/catch block to handle exceptions. Moved input validation
    for host/port from AbstractNetconfTopology#setupConnection()
    to DefaultNetconfClientConfigurationBuilderFactory
    and included relevant unit tests.
    
    JIRA: NETCONF-1114
    Change-Id: Ieb74ee3c1a4e0cbab7f0e01c942d1e8c1d6e20e4
    Signed-off-by: Yaroslav Lastivka <yaroslav.lastivka@pantheon.tech>

--- apps/netconf-topology/src/main/java/org/opendaylight/netconf/topology/spi/DefaultNetconfClientConfigurationBuilderFactory.java
+++ apps/netconf-topology/src/main/java/org/opendaylight/netconf/topology/spi/DefaultNetconfClientConfigurationBuilderFactory.java
@@ -59,6 +59,12 @@ public final class DefaultNetconfClientConfigurationBuilderFactory implements Ne
             final NetconfNode node) {
         final var builder  = NetconfClientConfigurationBuilder.create();
 
+        if (node.getHost() == null) {
+            throw new IllegalArgumentException("Host must be specified for " + nodeId.getValue());
+        }
+        if (node.getPort() == null) {
+            throw new IllegalArgumentException("Port must be specified for " + nodeId.getValue());
+        }
         final var protocol = node.getProtocol();
         if (node.requireTcpOnly()) {
             builder.withProtocol(NetconfClientProtocol.TCP)
