51f5ea3e9ce044a22ea3712550df762bf1463111
Robert Varga <robert.varga@pantheon.tech>
2023-09-06 19:20:45 +0200

    Clarify NotificationQueryParams checks
    Using checkArgument() involves logical negation of the condition with
    respect the message. This can get quite daunting when trying to
    understand.
    
    Switch to using plain if() statements, which make the logic much
    clearer.
    
    Change-Id: I659921075fe3c9e4e234a86bceba24a771a1cf89
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- restconf/restconf-nb/src/main/java/org/opendaylight/restconf/nb/rfc8040/NotificationQueryParams.java
+++ restconf/restconf-nb/src/main/java/org/opendaylight/restconf/nb/rfc8040/NotificationQueryParams.java
@@ -8,8 +8,6 @@
  */
 package org.opendaylight.restconf.nb.rfc8040;
 
-import static com.google.common.base.Preconditions.checkArgument;
-
 import com.google.common.base.MoreObjects;
 import org.eclipse.jdt.annotation.NonNull;
 import org.eclipse.jdt.annotation.Nullable;
@@ -48,10 +46,13 @@ public final class NotificationQueryParams implements Immutable {
             final FilterParam filter, final LeafNodesOnlyParam leafNodesOnly,
             final SkipNotificationDataParam skipNotificationData,
             final ChangedLeafNodesOnlyParam changedLeafNodesOnly) {
-        checkArgument(stopTime == null || startTime != null,
-            "Stop-time parameter has to be used with start-time parameter.");
-        checkArgument(changedLeafNodesOnly == null || leafNodesOnly == null,
-            "ChangedLeafNodesOnly parameter cannot be used with leafNodesOnlyParameter.");
+        if (stopTime != null && startTime == null) {
+            throw new IllegalArgumentException("Stop-time parameter has to be used with start-time parameter.");
+        }
+        if (changedLeafNodesOnly != null && leafNodesOnly != null) {
+            throw new IllegalArgumentException(
+                "ChangedLeafNodesOnly parameter cannot be used with leafNodesOnlyParameter.");
+        }
         return new NotificationQueryParams(startTime, stopTime, filter, leafNodesOnly, skipNotificationData,
                 changedLeafNodesOnly);
     }
