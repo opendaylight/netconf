41e3baec04e6227c47cfa2fc6f83cccd493990db
Robert Varga <robert.varga@pantheon.tech>
2023-09-06 23:24:02 +0200

    Clean up JSON*Formatter.createText()
    Use proper try-with-resources blocks and take advantage of method
    returns.
    
    Also improve element ordering, with eventTime being emitted before the
    body.
    
    Finally we tie the constants used to model and highlight the problems
    that are in the current implementation.
    
    Change-Id: Ie8f8659833d91f87903d9936d8bf2c9fad5b96ff
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>
    (cherry picked from commit 5328fb813b8bc004994b1d592092bbb176e22ecf)
    (cherry picked from commit 101dc509a591f287bb038c02162c5a3ed37cdb40)

--- restconf/restconf-nb/src/main/java/org/opendaylight/restconf/nb/rfc8040/streams/listeners/AbstractWebsocketSerializer.java
+++ restconf/restconf-nb/src/main/java/org/opendaylight/restconf/nb/rfc8040/streams/listeners/AbstractWebsocketSerializer.java
@@ -136,6 +136,7 @@ abstract class AbstractWebsocketSerializer<T extends Exception> {
     //
     //        Who is interpreting this output? Can we eliminate the replacement with the acknowledgement that
     //        a localName cannot hold a ':' so the string is split using String.lastIndexOf(':')?
+    @Deprecated
     static final String convertPath(final Collection<PathArgument> path) {
         final var sb = new StringBuilder();
 
@@ -160,7 +161,8 @@ abstract class AbstractWebsocketSerializer<T extends Exception> {
         return sb.toString();
     }
 
-    private static void appendQName(final StringBuilder sb, final QName qname) {
+    @Deprecated
+    static void appendQName(final StringBuilder sb, final QName qname) {
         // FIXME: err: what?! what is this replacement?!
         sb.append(qname.getNamespace().toString().replace(':', '-')).append(':').append(qname.getLocalName());
     }
--- restconf/restconf-nb/src/main/java/org/opendaylight/restconf/nb/rfc8040/streams/listeners/JSONNotificationFormatter.java
+++ restconf/restconf-nb/src/main/java/org/opendaylight/restconf/nb/rfc8040/streams/listeners/JSONNotificationFormatter.java
@@ -12,15 +12,23 @@ import static java.util.Objects.requireNonNull;
 import com.google.gson.stream.JsonWriter;
 import java.io.IOException;
 import java.io.StringWriter;
-import java.io.Writer;
 import java.time.Instant;
 import javax.xml.xpath.XPathExpressionException;
+import org.eclipse.jdt.annotation.NonNull;
 import org.opendaylight.mdsal.dom.api.DOMNotification;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.restconf.rev170126.$YangModuleInfoImpl;
 import org.opendaylight.yangtools.yang.data.codec.gson.JSONCodecFactorySupplier;
 import org.opendaylight.yangtools.yang.data.codec.gson.JSONNormalizedNodeStreamWriter;
 import org.opendaylight.yangtools.yang.model.api.EffectiveModelContext;
 
 final class JSONNotificationFormatter extends NotificationFormatter {
+    private static final @NonNull String NOTIFICATION_NAME;
+
+    static {
+        final var ietfRestconfName = $YangModuleInfoImpl.getInstance().getName();
+        NOTIFICATION_NAME = ietfRestconfName.getLocalName() + ":notification";
+    }
+
     private final JSONCodecFactorySupplier codecSupplier;
 
     private JSONNotificationFormatter(final TextParameters textParams, final JSONCodecFactorySupplier codecSupplier) {
@@ -53,14 +61,16 @@ final class JSONNotificationFormatter extends NotificationFormatter {
     @Override
     String createText(final TextParameters params, final EffectiveModelContext schemaContext,
             final DOMNotification input, final Instant now) throws IOException {
-        final Writer writer = new StringWriter();
-        final JsonWriter jsonWriter = new JsonWriter(writer).beginObject();
-        jsonWriter.name("ietf-restconf:notification").beginObject();
-        writeNotificationBody(JSONNormalizedNodeStreamWriter.createNestedWriter(
-                codecSupplier.getShared(schemaContext), input.getType(), null, jsonWriter), input.getBody());
-        jsonWriter.endObject();
-        jsonWriter.name("event-time").value(toRFC3339(now)).endObject();
-        jsonWriter.close();
-        return writer.toString();
+        try (var writer = new StringWriter()) {
+            try (var jsonWriter = new JsonWriter(writer)) {
+                jsonWriter.beginObject()
+                    .name(NOTIFICATION_NAME).beginObject()
+                        .name("event-time").value(toRFC3339(now));
+                writeNotificationBody(JSONNormalizedNodeStreamWriter.createNestedWriter(
+                    codecSupplier.getShared(schemaContext), input.getType(), null, jsonWriter), input.getBody());
+                jsonWriter.endObject().endObject();
+            }
+            return writer.toString();
+        }
     }
 }
