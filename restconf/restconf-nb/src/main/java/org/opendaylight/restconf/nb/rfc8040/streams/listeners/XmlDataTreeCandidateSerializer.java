0af6765f2e56634cc082b3ee5f6dd3ea552c4815
Robert Varga <robert.varga@pantheon.tech>
2023-09-07 01:35:20 +0200

    Use NormalizedNodeStreamWriter to emit JSON data
    We are already instantiating NormalizedNode-based writer, which means we
    get help in terms for formatting things.
    
    JIRA: NETCONF-1152
    Change-Id: Ie076ba571887c70a94cdd1baf7ea5e1ec6d8e91f
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- restconf/restconf-nb/src/main/java/org/opendaylight/restconf/nb/rfc8040/streams/listeners/XmlDataTreeCandidateSerializer.java
+++ restconf/restconf-nb/src/main/java/org/opendaylight/restconf/nb/rfc8040/streams/listeners/XmlDataTreeCandidateSerializer.java
@@ -11,13 +11,12 @@ import static java.util.Objects.requireNonNull;
 import static org.opendaylight.restconf.nb.rfc8040.streams.listeners.NotificationFormatter.DATA_CHANGE_EVENT_ELEMENT;
 
 import java.util.Collection;
-import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.XMLStreamWriter;
 import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.PathArgument;
-import org.opendaylight.yangtools.yang.data.api.schema.stream.NormalizedNodeStreamWriter;
 import org.opendaylight.yangtools.yang.data.api.schema.stream.NormalizedNodeWriter;
 import org.opendaylight.yangtools.yang.data.codec.xml.XMLStreamNormalizedNodeStreamWriter;
 import org.opendaylight.yangtools.yang.data.tree.api.DataTreeCandidateNode;
+import org.opendaylight.yangtools.yang.data.tree.api.ModificationType;
 import org.opendaylight.yangtools.yang.model.api.EffectiveModelContext;
 import org.opendaylight.yangtools.yang.model.util.SchemaInferenceStack.Inference;
 
@@ -30,39 +29,33 @@ final class XmlDataTreeCandidateSerializer extends AbstractWebsocketSerializer<E
     }
 
     @Override
-    void serializeData(final Inference parent, final Collection<PathArgument> nodePath,
+    void serializeData(final Inference parent, final Collection<PathArgument> dataPath,
             final DataTreeCandidateNode candidate, final boolean skipData) throws Exception {
-        NormalizedNodeStreamWriter nodeStreamWriter = XMLStreamNormalizedNodeStreamWriter.create(xmlWriter, parent);
-        xmlWriter.writeStartElement(DATA_CHANGE_EVENT_ELEMENT);
-        serializePath(nodePath);
+        final var modificationType = candidate.modificationType();
+        if (modificationType != ModificationType.UNMODIFIED) {
+            xmlWriter.writeStartElement(DATA_CHANGE_EVENT_ELEMENT);
+            xmlWriter.writeStartElement("path");
+            // FIXME: use proper XML codec for YangInstanceIdentifier
+            xmlWriter.writeCharacters(convertPath(dataPath));
+            xmlWriter.writeEndElement();
 
-        if (!skipData) {
-            final var dataAfter = getDataAfter(candidate);
-            if (dataAfter != null) {
-                xmlWriter.writeStartElement("data");
-                NormalizedNodeWriter nnWriter = NormalizedNodeWriter.forStreamWriter(nodeStreamWriter);
-                nnWriter.write(dataAfter);
-                nnWriter.flush();
-                xmlWriter.writeEndElement();
+            xmlWriter.writeStartElement("operation");
+            xmlWriter.writeCharacters(modificationTypeToOperation(candidate));
+            xmlWriter.writeEndElement();
+
+            if (!skipData) {
+                final var dataAfter = getDataAfter(candidate);
+                if (dataAfter != null) {
+                    xmlWriter.writeStartElement("data");
+                    final var nnWriter = NormalizedNodeWriter.forStreamWriter(
+                        XMLStreamNormalizedNodeStreamWriter.create(xmlWriter, parent));
+                    nnWriter.write(dataAfter);
+                    nnWriter.flush();
+                    xmlWriter.writeEndElement();
+                }
             }
+
+            xmlWriter.writeEndElement();
         }
-        serializeOperation(candidate);
-
-        xmlWriter.writeEndElement();
-    }
-
-    @Override
-    public void serializePath(final Collection<PathArgument> pathArguments) throws XMLStreamException {
-        xmlWriter.writeStartElement("path");
-        // FIXME: use proper XML codec for YangInstanceIdentifier
-        xmlWriter.writeCharacters(convertPath(pathArguments));
-        xmlWriter.writeEndElement();
-    }
-
-    @Override
-    public void serializeOperation(final DataTreeCandidateNode candidate) throws XMLStreamException {
-        xmlWriter.writeStartElement("operation");
-        xmlWriter.writeCharacters(modificationTypeToOperation(candidate, candidate.modificationType()));
-        xmlWriter.writeEndElement();
     }
 }
