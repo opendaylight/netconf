b0d83a38c6a583a4c3beabcad0dbbbb6e392318c
Robert Varga <robert.varga@pantheon.tech>
2023-09-12 03:07:29 +0200

    Rehost PatchDataTransactionUtil logging
    We have simple wrappers that just log stuff and forward execution to
    RestconfTransaction. Rehost these to final RestconfTransaction methods,
    so we have greater traceability coverage.
    
    JIRA: NETCONF-1107
    Change-Id: I6548860a1d90daf06e5eb9077f803a4aae23fa91
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- restconf/restconf-nb/src/main/java/org/opendaylight/restconf/nb/rfc8040/rests/transactions/RestconfTransaction.java
+++ restconf/restconf-nb/src/main/java/org/opendaylight/restconf/nb/rfc8040/rests/transactions/RestconfTransaction.java
@@ -7,17 +7,22 @@
  */
 package org.opendaylight.restconf.nb.rfc8040.rests.transactions;
 
+import static java.util.Objects.requireNonNull;
+
 import com.google.common.annotations.Beta;
 import com.google.common.util.concurrent.ListenableFuture;
 import java.util.Optional;
 import org.eclipse.jdt.annotation.NonNull;
 import org.eclipse.jdt.annotation.Nullable;
 import org.opendaylight.mdsal.common.api.CommitInfo;
+import org.opendaylight.odlparent.logging.markers.Markers;
 import org.opendaylight.restconf.nb.rfc8040.rests.utils.TransactionUtil;
 import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
 import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNode;
 import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNodeContainer;
 import org.opendaylight.yangtools.yang.model.api.EffectiveModelContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * A handle to a set of operations being executed atomically on top of some backing store.
@@ -26,6 +31,8 @@ import org.opendaylight.yangtools.yang.model.api.EffectiveModelContext;
 //        operations. This should be handled through proper allocation indirection.
 @Beta
 public abstract class RestconfTransaction {
+    private static final Logger LOG = LoggerFactory.getLogger(RestconfTransaction.class);
+
     RestconfTransaction() {
         // Hidden on purpose
     }
@@ -48,14 +55,24 @@ public abstract class RestconfTransaction {
      *
      * @param path the data object path
      */
-    public abstract void delete(YangInstanceIdentifier path);
+    public final void delete(final YangInstanceIdentifier path) {
+        LOG.trace("Delete {}", path);
+        deleteImpl(requireNonNull(path));
+    }
+
+    abstract void deleteImpl(@NonNull YangInstanceIdentifier path);
 
     /**
      * Remove data from the datastore.
      *
-     * @param path  the data object path
+     * @param path the data object path
      */
-    public abstract void remove(YangInstanceIdentifier path);
+    public final void remove(final YangInstanceIdentifier path) {
+        LOG.trace("Remove {}", path);
+        removeImpl(requireNonNull(path));
+    }
+
+    abstract void removeImpl(@NonNull YangInstanceIdentifier path);
 
     /**
      * Merges a piece of data with the existing data at a specified path.
@@ -63,25 +80,54 @@ public abstract class RestconfTransaction {
      * @param path the data object path
      * @param data the data object to be merged to the specified path
      */
-    public abstract void merge(YangInstanceIdentifier path, NormalizedNode data);
+    public final void merge(final YangInstanceIdentifier path, final NormalizedNode data) {
+        if (LOG.isTraceEnabled()) {
+            LOG.trace("Merge {}", path);
+            LOG.trace(Markers.confidential(), "Merge with {}", data.prettyTree());
+        }
+        mergeImpl(requireNonNull(path), data);
+    }
+
+    abstract void mergeImpl(@NonNull YangInstanceIdentifier path, @NonNull NormalizedNode data);
 
     /**
      * Stores a piece of data at the specified path.
      *
-     * @param path          the data object path
-     * @param data          the data object to be merged to the specified path
-     * @param schemaContext static view of compiled yang files
+     * @param path    the data object path
+     * @param data    the data object to be merged to the specified path
+     * @param context static view of compiled yang files
      */
-    public abstract void create(YangInstanceIdentifier path, NormalizedNode data, EffectiveModelContext schemaContext);
+    public final void create(final YangInstanceIdentifier path, final NormalizedNode data,
+            final EffectiveModelContext context) {
+        if (LOG.isTraceEnabled()) {
+            LOG.trace("Create {}", path);
+            LOG.trace(Markers.confidential(), "Create as {}", data.prettyTree());
+        }
+        createImpl(requireNonNull(path), data, requireNonNull(context));
+    }
+
+    abstract void createImpl(@NonNull YangInstanceIdentifier path, @NonNull NormalizedNode data,
+        @NonNull EffectiveModelContext context);
 
     /**
      * Replace a piece of data at the specified path.
      *
-     * @param path          the data object path
-     * @param data          the data object to be merged to the specified path
-     * @param schemaContext static view of compiled yang files
+     * @param path    the data object path
+     * @param data    the data object to be merged to the specified path
+     * @param context static view of compiled yang files
      */
-    public abstract void replace(YangInstanceIdentifier path, NormalizedNode data, EffectiveModelContext schemaContext);
+    public final void replace(final YangInstanceIdentifier path, final NormalizedNode data,
+            final EffectiveModelContext context) {
+        if (LOG.isTraceEnabled()) {
+            LOG.trace("Replace {}", path);
+            LOG.trace(Markers.confidential(), "Replace with {}", data.prettyTree());
+        }
+        replaceImpl(requireNonNull(path), data, requireNonNull(context));
+    }
+
+    abstract void replaceImpl(@NonNull YangInstanceIdentifier path, @NonNull NormalizedNode data,
+        @NonNull EffectiveModelContext context);
+
 
     final @Nullable NormalizedNodeContainer<?> readList(final YangInstanceIdentifier path) {
         return (NormalizedNodeContainer<?>) TransactionUtil.syncAccess(read(path), path).orElse(null);
