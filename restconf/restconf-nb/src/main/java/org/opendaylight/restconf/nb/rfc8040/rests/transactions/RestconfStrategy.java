1f0f517bd1fb8e14d37dbdcf5a838f91c9d7845e
Robert Varga <robert.varga@pantheon.tech>
2023-09-12 03:27:34 +0200

    Eliminate PatchDataTransactionUtil
    This class is hosting a single method, which is strongly related to
    RestconfStrategy.
    
    JIRA: NETCONF-1107
    Change-Id: I15b32b055a9be8787d32be5ede12e1d40a539a94
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- restconf/restconf-nb/src/main/java/org/opendaylight/restconf/nb/rfc8040/rests/transactions/RestconfStrategy.java
+++ restconf/restconf-nb/src/main/java/org/opendaylight/restconf/nb/rfc8040/rests/transactions/RestconfStrategy.java
@@ -14,6 +14,7 @@ import com.google.common.util.concurrent.FutureCallback;
 import com.google.common.util.concurrent.Futures;
 import com.google.common.util.concurrent.ListenableFuture;
 import com.google.common.util.concurrent.MoreExecutors;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import org.eclipse.jdt.annotation.NonNull;
@@ -25,8 +26,12 @@ import org.opendaylight.mdsal.dom.api.DOMMountPoint;
 import org.opendaylight.netconf.dom.api.NetconfDataTreeService;
 import org.opendaylight.restconf.api.query.PointParam;
 import org.opendaylight.restconf.common.errors.RestconfDocumentedException;
+import org.opendaylight.restconf.common.errors.RestconfError;
 import org.opendaylight.restconf.common.errors.RestconfFuture;
 import org.opendaylight.restconf.common.errors.SettableRestconfFuture;
+import org.opendaylight.restconf.common.patch.PatchContext;
+import org.opendaylight.restconf.common.patch.PatchStatusContext;
+import org.opendaylight.restconf.common.patch.PatchStatusEntity;
 import org.opendaylight.restconf.nb.rfc8040.Insert;
 import org.opendaylight.restconf.nb.rfc8040.rests.utils.TransactionUtil;
 import org.opendaylight.restconf.nb.rfc8040.utils.parser.YangInstanceIdentifierDeserializer;
@@ -367,6 +372,98 @@ public abstract class RestconfStrategy {
                 yield tx.commit();
             }
         };
+    }
+
+    /**
+     * Process edit operations of one {@link PatchContext}.
+     *
+     * @param patch    Patch context to be processed
+     * @param context  Global schema context
+     * @return {@link PatchStatusContext}
+     */
+    public final @NonNull PatchStatusContext patchData(final PatchContext patch, final EffectiveModelContext context) {
+        final var editCollection = new ArrayList<PatchStatusEntity>();
+        final var tx = prepareWriteExecution();
+
+        boolean noError = true;
+        for (var patchEntity : patch.getData()) {
+            if (noError) {
+                final var targetNode = patchEntity.getTargetNode();
+                final var editId = patchEntity.getEditId();
+
+                switch (patchEntity.getOperation()) {
+                    case Create:
+                        try {
+                            tx.create(targetNode, patchEntity.getNode(), context);
+                            editCollection.add(new PatchStatusEntity(editId, true, null));
+                        } catch (RestconfDocumentedException e) {
+                            editCollection.add(new PatchStatusEntity(editId, false, e.getErrors()));
+                            noError = false;
+                        }
+                        break;
+                    case Delete:
+                        try {
+                            tx.delete(targetNode);
+                            editCollection.add(new PatchStatusEntity(editId, true, null));
+                        } catch (RestconfDocumentedException e) {
+                            editCollection.add(new PatchStatusEntity(editId, false, e.getErrors()));
+                            noError = false;
+                        }
+                        break;
+                    case Merge:
+                        try {
+                            TransactionUtil.ensureParentsByMerge(targetNode, context, tx);
+                            tx.merge(targetNode, patchEntity.getNode());
+                            editCollection.add(new PatchStatusEntity(editId, true, null));
+                        } catch (RestconfDocumentedException e) {
+                            editCollection.add(new PatchStatusEntity(editId, false, e.getErrors()));
+                            noError = false;
+                        }
+                        break;
+                    case Replace:
+                        try {
+                            tx.replace(targetNode, patchEntity.getNode(), context);
+                            editCollection.add(new PatchStatusEntity(editId, true, null));
+                        } catch (RestconfDocumentedException e) {
+                            editCollection.add(new PatchStatusEntity(editId, false, e.getErrors()));
+                            noError = false;
+                        }
+                        break;
+                    case Remove:
+                        try {
+                            tx.remove(targetNode);
+                            editCollection.add(new PatchStatusEntity(editId, true, null));
+                        } catch (RestconfDocumentedException e) {
+                            editCollection.add(new PatchStatusEntity(editId, false, e.getErrors()));
+                            noError = false;
+                        }
+                        break;
+                    default:
+                        editCollection.add(new PatchStatusEntity(editId, false, List.of(
+                            new RestconfError(ErrorType.PROTOCOL, ErrorTag.OPERATION_NOT_SUPPORTED,
+                                "Not supported Yang Patch operation"))));
+                        noError = false;
+                        break;
+                }
+            } else {
+                break;
+            }
+        }
+
+        // if no errors then submit transaction, otherwise cancel
+        if (noError) {
+            try {
+                TransactionUtil.syncCommit(tx.commit(), "PATCH", null);
+            } catch (RestconfDocumentedException e) {
+                // if errors occurred during transaction commit then patch failed and global errors are reported
+                return new PatchStatusContext(patch.getPatchId(), List.copyOf(editCollection), false, e.getErrors());
+            }
+
+            return new PatchStatusContext(patch.getPatchId(), List.copyOf(editCollection), true, null);
+        } else {
+            tx.cancel();
+            return new PatchStatusContext(patch.getPatchId(), List.copyOf(editCollection), false, null);
+        }
     }
 
     private static void insertWithPointPost(final RestconfTransaction tx, final YangInstanceIdentifier path,
