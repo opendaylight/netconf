1928e917c2fec865f10d3ec3e096df7d5b79e9a1
Robert Varga <robert.varga@pantheon.tech>
2023-09-16 19:06:22 +0200

    WIP: Introduce RequestMode
    We are retiring InstanceIdentifierContext in favor of a better
    construct.
    
    This construct is RequestMode, which has three specializations:
    - ResourceMode, for when the request pertains a particular resource,
      a datastore or in general the global scope
    - CreateResourceMode, for when a POST URL indicates we should embark
      on resource creation
    - InvokeOperationMode, for when a POST URL indicates we should embark
      on RPC or Action invocation.
    
    The latter are indirect, going through a POSTMode intermediate class,
    which allows us to model unified return type for binding PUT methods.
    
    The flow now makes it clear that DatabindProvider should be integrated
    into MdsalRestconfServer. For the two cases which do weird things on
    local scope, we expose a legacy localDatabind() method.
    
    This further clarifies that ParserIdentifier.toInstanceIdentifier()
    cannot be invoked with null identifier, so we get rid of all the
    complexity -- eventually guaranteeing that an InstanceIdentifierContext
    always has an InstanceIdentifier.
    
    // FIXME: finish conversion
    
    JIRA: NETCONF-1157
    Change-Id: I635648d79ad23529d633d43e9c8b1ac1c3497776
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ restconf/restconf-nb/src/main/java/org/opendaylight/restconf/nb/rfc8040/databind/ResourceMode.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2023 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.restconf.nb.rfc8040.databind;
+
+import static java.util.Objects.requireNonNull;
+
+import org.eclipse.jdt.annotation.NonNull;
+import org.eclipse.jdt.annotation.Nullable;
+import org.opendaylight.mdsal.dom.api.DOMMountPoint;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
+import org.opendaylight.yangtools.yang.model.util.SchemaInferenceStack.Inference;
+
+/**
+ * A shared resource mode for {@code GET}, {@code PUT} and {@code PATCH} operations, as they do their own thing.
+ */
+public record ResourceMode(
+        @NonNull DatabindContext localDatabind,
+        @NonNull Inference inference,
+        @NonNull YangInstanceIdentifier path,
+        @Nullable DOMMountPoint mountPoint) implements RequestMode {
+    public ResourceMode {
+        requireNonNull(localDatabind);
+        requireNonNull(inference);
+        requireNonNull(path);
+    }
+
+    @Deprecated
+    public @NonNull DatabindContext localDatabind() {
+        return localDatabind;
+    }
+}
