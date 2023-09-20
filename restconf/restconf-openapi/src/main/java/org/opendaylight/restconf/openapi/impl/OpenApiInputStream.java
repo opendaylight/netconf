813370fbfa893028b3929039cb1bf5bc4db7528a
Ivan Hrasko <ivan.hrasko@pantheon.tech>
2023-09-20 15:41:20 +0200

    WIP: Draft iteration over model context
    When OpenApiInputStream get out of original stack to write
    it asks for new one stack for next model.
    
    JIRA: NETCONF-938
    Change-Id: Ic2c7b30312804a40958e7bc87728d7f4e37e1047
    Signed-off-by: Ivan Hrasko <ivan.hrasko@pantheon.tech>

--- restconf/restconf-openapi/src/main/java/org/opendaylight/restconf/openapi/impl/OpenApiInputStream.java
+++ restconf/restconf-openapi/src/main/java/org/opendaylight/restconf/openapi/impl/OpenApiInputStream.java
@@ -14,23 +14,32 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
 import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import org.opendaylight.restconf.openapi.jaxrs.OpenApiBodyWriter;
 import org.opendaylight.restconf.openapi.model.Info;
 import org.opendaylight.restconf.openapi.model.InfoEntity;
 import org.opendaylight.restconf.openapi.model.OpenApiEntity;
+import org.opendaylight.restconf.openapi.model.OpenApiVersionEntity;
 import org.opendaylight.restconf.openapi.model.Server;
 import org.opendaylight.restconf.openapi.model.ServerEntity;
 import org.opendaylight.yangtools.yang.model.api.EffectiveModelContext;
 
 public class OpenApiInputStream extends InputStream {
-    private final ArrayDeque<OpenApiEntity> stack = new ArrayDeque<>();
+
+    private final Deque<OpenApiEntity> stack = new ArrayDeque<>();
+    private final EffectiveModelContext context;
+    private final Iterator<Deque<OpenApiEntity>> iterator;
 
     private Reader reader;
 
     public OpenApiInputStream(final EffectiveModelContext context, final String openApiVersion, final Info info,
             final List<Server> servers, final List<Map<String, List<String>>> security) {
+        this.context = context;
+        this.iterator = new OpenApiModelSnippet(context).iterator();
+        stack.add(new OpenApiVersionEntity());
         stack.add(new InfoEntity(info.version(), info.title(), info.description()));
         stack.add(new ServerEntity(servers.iterator().next().url()));
     }
@@ -44,6 +53,10 @@ public class OpenApiInputStream extends InputStream {
         var read = reader.read();
         while (read == -1) {
             if (stack.isEmpty()) {
+                if (iterator.hasNext()) {
+                    stack.addAll(iterator.next());
+                    continue;
+                }
                 return -1;
             }
             reader = new InputStreamReader(new ByteArrayInputStream(writeNextEntity(stack.pop())));
@@ -55,8 +68,9 @@ public class OpenApiInputStream extends InputStream {
 
     private static byte[] writeNextEntity(final OpenApiEntity entity) throws IOException {
         var openApiBodyWriter = new OpenApiBodyWriter();
-        var outStream = new ByteArrayOutputStream();
-        openApiBodyWriter.writeTo(entity, null, null, null, null, null, outStream);
-        return outStream.toByteArray();
+        try (var outStream = new ByteArrayOutputStream()) {
+            openApiBodyWriter.writeTo(entity, null, null, null, null, null, outStream);
+            return outStream.toByteArray();
+        }
     }
 }
