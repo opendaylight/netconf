13b7da10b7250ce0bc6a3abbc82c89b2d1b06fc9
Robert Varga <robert.varga@pantheon.tech>
2023-09-20 17:42:26 +0200

    Introduce TransportSsh{Client,Server}
    We are using both SshClient and SshServer, configuring them extensively,
    but we never start them.
    
    Introduce proper specializations of SshClient and SshServer, which do
    not allow themselves to be start or stopped. Strictly instantiate them
    through provided builders, which also serve as hosts for configuration
    adaptation.
    
    This reduces clutter in SSH{Client,Server}, making them single-page
    classes.
    
    JIRA: NETCONF-590
    Change-Id: If68714c21fe06de84cdff743c59c08f0f96f884c
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- transport/transport-ssh/src/main/java/org/opendaylight/netconf/transport/ssh/SSHClient.java
+++ transport/transport-ssh/src/main/java/org/opendaylight/netconf/transport/ssh/SSHClient.java
@@ -7,38 +7,22 @@
  */
 package org.opendaylight.netconf.transport.ssh;
 
-import com.google.common.collect.ImmutableList;
 import com.google.common.util.concurrent.ListenableFuture;
 import io.netty.bootstrap.Bootstrap;
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.channel.group.DefaultChannelGroup;
 import io.netty.util.concurrent.GlobalEventExecutor;
-import java.security.cert.Certificate;
 import org.eclipse.jdt.annotation.NonNull;
-import org.eclipse.jdt.annotation.Nullable;
 import org.opendaylight.netconf.shaded.sshd.client.ClientFactoryManager;
-import org.opendaylight.netconf.shaded.sshd.client.SshClient;
-import org.opendaylight.netconf.shaded.sshd.client.auth.UserAuthFactory;
-import org.opendaylight.netconf.shaded.sshd.client.auth.hostbased.HostKeyIdentityProvider;
-import org.opendaylight.netconf.shaded.sshd.client.auth.hostbased.UserAuthHostBasedFactory;
-import org.opendaylight.netconf.shaded.sshd.client.auth.password.PasswordIdentityProvider;
-import org.opendaylight.netconf.shaded.sshd.client.auth.password.UserAuthPasswordFactory;
-import org.opendaylight.netconf.shaded.sshd.client.auth.pubkey.UserAuthPublicKeyFactory;
-import org.opendaylight.netconf.shaded.sshd.client.keyverifier.AcceptAllServerKeyVerifier;
 import org.opendaylight.netconf.shaded.sshd.client.session.ClientSessionImpl;
 import org.opendaylight.netconf.shaded.sshd.client.session.SessionFactory;
 import org.opendaylight.netconf.shaded.sshd.common.io.IoHandler;
-import org.opendaylight.netconf.shaded.sshd.common.keyprovider.KeyIdentityProvider;
-import org.opendaylight.netconf.shaded.sshd.common.util.threads.ThreadUtils;
 import org.opendaylight.netconf.transport.api.TransportChannelListener;
 import org.opendaylight.netconf.transport.api.TransportStack;
 import org.opendaylight.netconf.transport.api.UnsupportedConfigurationException;
 import org.opendaylight.netconf.transport.tcp.TCPClient;
 import org.opendaylight.netconf.transport.tcp.TCPServer;
-import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.crypto.types.rev230417.password.grouping.password.type.CleartextPassword;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.ssh.client.rev230417.SshClientGrouping;
-import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.ssh.client.rev230417.ssh.client.grouping.ClientIdentity;
-import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.ssh.client.rev230417.ssh.client.grouping.ServerAuthentication;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.tcp.client.rev230417.TcpClientGrouping;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.tcp.server.rev230417.TcpServerGrouping;
 
@@ -94,76 +78,11 @@ public final class SSHClient extends SSHTransportStack {
 
     private static ClientFactoryManager newFactoryManager(final SshClientGrouping parameters)
             throws UnsupportedConfigurationException {
-        final var factoryMgr = SshClient.setUpDefaultClient();
-
-        ConfigUtils.setTransportParams(factoryMgr, parameters.getTransportParams());
-        ConfigUtils.setKeepAlives(factoryMgr, parameters.getKeepalives());
-
-        setClientIdentity(factoryMgr, parameters.getClientIdentity());
-        setServerAuthentication(factoryMgr, parameters.getServerAuthentication());
-
-        factoryMgr.setServiceFactories(SshClient.DEFAULT_SERVICE_FACTORIES);
-        factoryMgr.setScheduledExecutorService(ThreadUtils.newSingleThreadScheduledExecutor("sshd-client-pool"));
-        return factoryMgr;
-    }
-
-    private static void setClientIdentity(@NonNull final ClientFactoryManager factoryMgr,
-            final @Nullable ClientIdentity clientIdentity) throws UnsupportedConfigurationException {
-        if (clientIdentity == null || clientIdentity.getNone() != null) {
-            return;
-        }
-        final var authFactoriesListBuilder = ImmutableList.<UserAuthFactory>builder();
-        final var password = clientIdentity.getPassword();
-        if (password != null) {
-            if (password.getPasswordType() instanceof CleartextPassword clearTextPassword) {
-                factoryMgr.setPasswordIdentityProvider(
-                        PasswordIdentityProvider.wrapPasswords(clearTextPassword.requireCleartextPassword()));
-                authFactoriesListBuilder.add(new UserAuthPasswordFactory());
-            }
-            // TODO support encrypted password -- requires augmentation of default schema
-        }
-        final var hostBased = clientIdentity.getHostbased();
-        if (hostBased != null) {
-            var keyPair = ConfigUtils.extractKeyPair(hostBased.getInlineOrKeystore());
-            var factory = new UserAuthHostBasedFactory();
-            factory.setClientHostKeys(HostKeyIdentityProvider.wrap(keyPair));
-            factory.setClientUsername(clientIdentity.getUsername());
-            factory.setClientHostname(null); // not provided via config
-            factory.setSignatureFactories(factoryMgr.getSignatureFactories());
-            authFactoriesListBuilder.add(factory);
-        }
-        final var publicKey = clientIdentity.getPublicKey();
-        if (publicKey != null) {
-            final var keyPairs = ConfigUtils.extractKeyPair(publicKey.getInlineOrKeystore());
-            factoryMgr.setKeyIdentityProvider(KeyIdentityProvider.wrapKeyPairs(keyPairs));
-            final var factory = new UserAuthPublicKeyFactory();
-            factory.setSignatureFactories(factoryMgr.getSignatureFactories());
-            authFactoriesListBuilder.add(factory);
-        }
-        // FIXME implement authentication using X509 certificate
-        final var userAuthFactories = authFactoriesListBuilder.build();
-        if (userAuthFactories.isEmpty()) {
-            throw new UnsupportedConfigurationException("Client Identity has no authentication mechanism defined");
-        }
-        factoryMgr.setUserAuthFactories(userAuthFactories);
-    }
-
-    private static void setServerAuthentication(final @NonNull ClientFactoryManager factoryMgr,
-            final @Nullable ServerAuthentication serverAuthentication) throws UnsupportedConfigurationException {
-        if (serverAuthentication == null) {
-            factoryMgr.setServerKeyVerifier(AcceptAllServerKeyVerifier.INSTANCE);
-            return;
-        }
-        final var certificatesList = ImmutableList.<Certificate>builder()
-                .addAll(ConfigUtils.extractCertificates(serverAuthentication.getCaCerts()))
-                .addAll(ConfigUtils.extractCertificates(serverAuthentication.getEeCerts()))
-                .build();
-        final var publicKeys = ConfigUtils.extractPublicKeys(serverAuthentication.getSshHostKeys());
-        if (!certificatesList.isEmpty() || !publicKeys.isEmpty()) {
-            factoryMgr.setServerKeyVerifier(new ServerPublicKeyVerifier(certificatesList, publicKeys));
-        } else {
-            throw new UnsupportedConfigurationException("Server authentication should contain either ssh-host-keys "
-                    + "or ca-certs or ee-certs");
-        }
+        return new TransportSshClient.Builder()
+            .transportParams(parameters.getTransportParams())
+            .keepAlives(parameters.getKeepalives())
+            .clientIdentity(parameters.getClientIdentity())
+            .serverAuthentication(parameters.getServerAuthentication())
+            .buildChecked();
     }
 }
