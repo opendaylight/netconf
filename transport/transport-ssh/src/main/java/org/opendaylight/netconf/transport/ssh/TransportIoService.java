63e42dcb53563872a35a8107c7679516ae5b22d2
Robert Varga <robert.varga@pantheon.tech>
2023-09-21 17:40:54 +0200

    Clean up SSHTransportStack
    Now that both client and server are initializing their SessionFactory,
    we can simply pass down the arguments required for SshIoService down,
    so that the stack can handle the dispatch itself.
    
    While we are at it, rename SshIo{Service,Session} to
    TransportIo{Session,Service}.
    
    JIRA: NETCONF-590
    Change-Id: I3e1b9f638e0fc441b9d68648cd63344caeb72378
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- transport/transport-ssh/src/main/java/org/opendaylight/netconf/transport/ssh/TransportIoService.java
+++ transport/transport-ssh/src/main/java/org/opendaylight/netconf/transport/ssh/TransportIoService.java
@@ -7,13 +7,14 @@
  */
 package org.opendaylight.netconf.transport.ssh;
 
+import java.net.SocketAddress;
 import org.opendaylight.netconf.shaded.sshd.common.FactoryManager;
 import org.opendaylight.netconf.shaded.sshd.common.io.IoHandler;
 import org.opendaylight.netconf.shaded.sshd.netty.NettyIoService;
 import org.opendaylight.netconf.shaded.sshd.netty.NettyIoServiceFactory;
 
-final class SshIoService extends NettyIoService {
-    SshIoService(final FactoryManager factoryManager, final IoHandler handler) {
+final class TransportIoService extends NettyIoService {
+    TransportIoService(final FactoryManager factoryManager, final IoHandler handler) {
         super((NettyIoServiceFactory) factoryManager.getIoServiceFactory(), handler);
 
         // Required to keep things working, but does not need to be functional at all.
@@ -24,5 +25,9 @@ final class SshIoService extends NettyIoService {
         // We do not need to do that, as we track the underlying transport instead, hence any channels tracked here will
         // disappear when we shut down the underlay.
         channelGroup = CompatChannelGroup.INSTANCE;
+    }
+
+    TransportIoSession createSession(final SocketAddress acceptanceAddress) {
+        return new TransportIoSession(this, handler, acceptanceAddress);
     }
 }
