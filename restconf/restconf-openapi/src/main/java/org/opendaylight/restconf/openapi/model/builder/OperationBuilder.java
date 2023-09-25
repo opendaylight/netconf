790681ea8ecd5d841aecd2e56a2f7a454f81a12e
Samuel Schneider <samuel.schneider@pantheon.tech>
2023-09-25 16:02:36 +0200

    Fix incorrect schema reference in root POST link
    We are creating a root POST link, when requesting swagger document
    for single module. We were passing empty string as module name
    when building the POST operation, which resulted in our schema ref
    beginning with underscore (like this: "_toaster_module").
    The schema is created, but without the leading underscore.
    
    This patch fixes it by passing null value and adding logic
    that performs the null checking to construct correct schema reference.
    
    JIRA: NETCONF-1133
    Change-Id: Ia32c140f4f54a3d60b9d4f206988aae2518c1bd1
    Signed-off-by: Šimon Ukuš <simon.ukus@pantheon.tech>
    Signed-off-by: Samuel Schneider <samuel.schneider@pantheon.tech>
    (cherry picked from commit 49e965432e29092aa8aa3ffb64a5875632e3c255)
    Signed-off-by: Ivan Hrasko <ivan.hrasko@pantheon.tech>

--- restconf/restconf-openapi/src/main/java/org/opendaylight/restconf/openapi/model/builder/OperationBuilder.java
+++ restconf/restconf-openapi/src/main/java/org/opendaylight/restconf/openapi/model/builder/OperationBuilder.java
@@ -15,6 +15,7 @@ import static org.opendaylight.restconf.openapi.impl.DefinitionGenerator.OUTPUT_
 import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.JsonNodeFactory;
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import javax.ws.rs.HttpMethod;
@@ -68,20 +69,26 @@ public final class OperationBuilder {
         final var summary = buildSummaryValue(HttpMethod.POST, moduleName, deviceName, nodeName);
         final ArrayNode tags = buildTagsValue(deviceName, moduleName);
         final ArrayNode parameters = JsonNodeFactory.instance.arrayNode().addAll(pathParams);
-        final ObjectNode ref = JsonNodeFactory.instance.objectNode();
-        final String cleanDefName = parentName + CONFIG + "_" + nodeName;
-        final String defName = cleanDefName + discriminator;
-        final String xmlDefName = cleanDefName + discriminator;
-        ref.put(REF_KEY, COMPONENTS_PREFIX + defName);
-
         final ObjectNode requestBody;
         final DataSchemaNode childNode = getListOrContainerChildNode(Optional.ofNullable(node));
+
+        final List<String> nameElements = new ArrayList<>();
         if (childNode != null && childNode.isConfiguration()) {
             final String childNodeName = childNode.getQName().getLocalName();
-            final String childDefName = parentName + "_" + nodeName + CONFIG + "_" + childNodeName + discriminator;
+            if (parentName != null) {
+                nameElements.add(parentName);
+            }
+            nameElements.add(nodeName + CONFIG);
+            nameElements.add(childNodeName + discriminator);
+            final String childDefName = String.join("_", nameElements);
             requestBody = createPostRequestBodyParameter(childNode, childDefName, childNodeName);
         } else {
-            requestBody = createRequestBodyParameter(defName, xmlDefName, nodeName + CONFIG, summary);
+            if (parentName != null) {
+                nameElements.add(parentName + CONFIG);
+            }
+            nameElements.add(nodeName + discriminator);
+            final String defName = String.join("_", nameElements);
+            requestBody = createRequestBodyParameter(defName, defName, nodeName + CONFIG, summary);
         }
 
         final ObjectNode responses = JsonNodeFactory.instance.objectNode();
