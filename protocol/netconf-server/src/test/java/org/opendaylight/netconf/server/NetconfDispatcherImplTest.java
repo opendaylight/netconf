089d0d18fb374473bbaefebc0c4a0d783a32dfdf
Ruslan Kashapov <ruslan.kashapov@pantheon.tech>
2023-10-02 13:41:53 +0300

    Refactor ConcurrentClientsTest using new transport
    JIRA: NETCONF-1106
    Change-Id: Id1e7a9148ee2ad8177eb25443a7b184acbd92064
    Signed-off-by: Ruslan Kashapov <ruslan.kashapov@pantheon.tech>

--- protocol/netconf-server/src/test/java/org/opendaylight/netconf/server/NetconfDispatcherImplTest.java
+++ protocol/netconf-server/src/test/java/org/opendaylight/netconf/server/NetconfDispatcherImplTest.java
@@ -7,17 +7,28 @@
  */
 package org.opendaylight.netconf.server;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
+
 import io.netty.channel.ChannelFuture;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.util.HashedWheelTimer;
 import java.net.InetSocketAddress;
+import java.util.Set;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.opendaylight.netconf.server.api.SessionIdProvider;
+import org.opendaylight.netconf.server.api.monitoring.NetconfMonitoringService;
+import org.opendaylight.netconf.server.api.monitoring.SessionEvent;
+import org.opendaylight.netconf.server.api.monitoring.SessionListener;
 import org.opendaylight.netconf.server.impl.DefaultSessionIdProvider;
 import org.opendaylight.netconf.server.osgi.AggregatedNetconfOperationServiceFactory;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.netconf.monitoring.rev101004.netconf.state.CapabilitiesBuilder;
+import org.opendaylight.yangtools.concepts.Registration;
 
 public class NetconfDispatcherImplTest {
     private EventLoopGroup nettyGroup;
@@ -38,7 +49,7 @@ public class NetconfDispatcherImplTest {
                         .setAggregatedOpService(factoriesListener)
                         .setTimer(hashedWheelTimer)
                         .setIdProvider(idProvider)
-                        .setMonitoringService(ConcurrentClientsTest.createMockedMonitoringService())
+                        .setMonitoringService(createMockedMonitoringService())
                         .setConnectionTimeoutMillis(5000)
                         .build();
 
@@ -60,5 +71,18 @@ public class NetconfDispatcherImplTest {
         InetSocketAddress addr = new InetSocketAddress("127.0.0.1", 8333);
         ChannelFuture server = dispatch.createServer(addr);
         server.get();
+    }
+
+    private static NetconfMonitoringService createMockedMonitoringService() {
+        final NetconfMonitoringService monitoring = mock(NetconfMonitoringService.class);
+        final SessionListener sessionListener = mock(SessionListener.class);
+        doNothing().when(sessionListener).onSessionUp(any(NetconfServerSession.class));
+        doNothing().when(sessionListener).onSessionDown(any(NetconfServerSession.class));
+        doNothing().when(sessionListener).onSessionEvent(any(SessionEvent.class));
+        doReturn((Registration) () -> { }).when(monitoring)
+            .registerCapabilitiesListener(any(NetconfMonitoringService.CapabilitiesListener.class));
+        doReturn(sessionListener).when(monitoring).getSessionListener();
+        doReturn(new CapabilitiesBuilder().setCapability(Set.of()).build()).when(monitoring).getCapabilities();
+        return monitoring;
     }
 }
