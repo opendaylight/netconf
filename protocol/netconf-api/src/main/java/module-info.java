77251b0ffc91edbae16d1cd656e3676d4bdfafbc
Robert Varga <robert.varga@pantheon.tech>
2023-10-03 18:17:23 +0200

    WIP: Introduce XmlUtil.requireSingleElement
    Change-Id: If1efd60aa268d659b1e5f43686caf49e77e40c05
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- protocol/netconf-api/src/main/java/module-info.java
+++ protocol/netconf-api/src/main/java/module-info.java
@@ -15,6 +15,7 @@ module org.opendaylight.netconf.api {
     requires transitive org.opendaylight.yangtools.yang.common;
     requires transitive org.opendaylight.yang.gen.ietf.netconf.rfc6241;
     requires com.google.common;
+    requires org.opendaylight.odlparent.logging.markers;
     requires org.opendaylight.yangtools.util;
     requires org.opendaylight.yangtools.yang.binding;
     requires org.slf4j;
