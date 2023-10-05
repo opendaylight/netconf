a09d6a55340dfc8947a3150248856dc440704e84
Oleksandr Zharov <oleksandr.zharov@pantheon.tech>
2023-10-05 14:19:57 +0200

    Throws RDE exception in case model was not found
    Added proper response in case model was not found using
    RestconfDocumentedException's DATA_MISSING error tag.
    
    Added unit tests to cover this cases.
    
    JIRA: NETCONF-1159
    Change-Id: Ife5b3836c8c2d974963d80df4d497ec019a83cc1
    Signed-off-by: Oleksandr Zharov <oleksandr.zharov@pantheon.tech>
    Signed-off-by: Ivan Hrasko <ivan.hrasko@pantheon.tech>
    (cherry picked from commit e17f784409f8d6a42a9529ef45423dfcd06c39f7)

--- restconf/restconf-nb/src/test/java/org/opendaylight/restconf/nb/rfc8040/rests/services/impl/RestconfSchemaServiceTest.java
+++ restconf/restconf-nb/src/test/java/org/opendaylight/restconf/nb/rfc8040/rests/services/impl/RestconfSchemaServiceTest.java
@@ -13,8 +13,11 @@ import static org.junit.Assert.assertThrows;
 import static org.mockito.Mockito.when;
 
 import com.google.common.collect.ImmutableClassToInstanceMap;
+<<<<<<< HEAD   (52ee62 Throw exception when module cannot be found)
 import java.io.FileNotFoundException;
 import java.util.NoSuchElementException;
+=======
+>>>>>>> CHANGE (e17f78 Throws RDE exception in case model was not found)
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
@@ -133,7 +136,10 @@ public class RestconfSchemaServiceTest {
         when(mockSchemaService.getGlobalContext()).thenReturn(SCHEMA_CONTEXT);
 
         // make test & verify
-        assertThrows(NoSuchElementException.class, () -> schemaService.getSchema(NOT_EXISTING_MODULE));
+        final var ex = assertThrows(RestconfDocumentedException.class, () ->
+            schemaService.getSchema(NOT_EXISTING_MODULE));
+        assertEquals("Not expected error tag", ErrorTag.DATA_MISSING, ex.getErrors().get(0).getErrorTag());
+        assertEquals("Not expected error type", ErrorType.APPLICATION, ex.getErrors().get(0).getErrorType());
     }
 
     /**
@@ -169,7 +175,10 @@ public class RestconfSchemaServiceTest {
         when(mockSchemaService.getGlobalContext()).thenReturn(SCHEMA_CONTEXT_WITH_MOUNT_POINTS);
 
         // make test & verify
-        assertThrows(NoSuchElementException.class, () -> schemaService.getSchema(MOUNT_POINT + NOT_EXISTING_MODULE));
+        final var ex = assertThrows(RestconfDocumentedException.class, () ->
+            schemaService.getSchema(MOUNT_POINT + NOT_EXISTING_MODULE));
+        assertEquals("Not expected error tag", ErrorTag.DATA_MISSING, ex.getErrors().get(0).getErrorTag());
+        assertEquals("Not expected error type", ErrorType.APPLICATION, ex.getErrors().get(0).getErrorType());
     }
 
     /**
