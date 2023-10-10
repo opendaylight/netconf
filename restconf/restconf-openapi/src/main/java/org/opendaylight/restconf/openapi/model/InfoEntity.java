56e325b6d047cca4405ed823fe02d91a99a4ed49
Ivan Hrasko <ivan.hrasko@pantheon.tech>
2023-10-10 15:53:55 +0200

    Adapt API to OpenApiObject removal
    Adapted REST API to use new logic.
    
    Adapted tests for OpenApiInputStream.
    
    JIRA: NETCONF-938
    Change-Id: Idc6a35bb29f77c37e7ac70e873fd6b720fc5584d
    Signed-off-by: Ivan Hrasko <ivan.hrasko@pantheon.tech>
    Signed-off-by: lubos-cicut <lubos.cicut@pantheon.tech>
    Signed-off-by: Yaroslav Lastivka <yaroslav.lastivka@pantheon.tech>

--- restconf/restconf-openapi/src/main/java/org/opendaylight/restconf/openapi/model/InfoEntity.java
+++ restconf/restconf-openapi/src/main/java/org/opendaylight/restconf/openapi/model/InfoEntity.java
@@ -12,28 +12,26 @@ import java.io.IOException;
 import org.eclipse.jdt.annotation.NonNull;
 
 public final class InfoEntity extends OpenApiEntity {
-    private final String version;
     private final String title;
-    private final String description;
+    private static final String DESCRIPTION = """
+        We are providing full API for configurational data which can be edited (by POST, PUT, PATCH and DELETE).
+        For operational data we only provide GET API.\n
+        For majority of request you can see only config data in examples. That is because we can show only one example
+        per request. The exception when you can see operational data in example is when data are representing
+        operational (config false) container with no config data in it.""";
 
-    public InfoEntity(final String version, final String title, final String description) {
-        this.version = version;
+    public InfoEntity(final String title) {
         this.title = title;
-        this.description = description;
     }
 
     @Override
     public void generate(@NonNull JsonGenerator generator) throws IOException {
         generator.writeObjectFieldStart("info");
-        if (version != null) {
-            generator.writeStringField("version", version);
-        }
+        generator.writeStringField("version", "1.0.0");
         if (title != null) {
             generator.writeStringField("title", title);
         }
-        if (description != null) {
-            generator.writeStringField("description", description);
-        }
+        generator.writeStringField("description", DESCRIPTION);
         generator.writeEndObject();
     }
 }
