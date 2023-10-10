62aa67ab3ad58dc5845de24343bfafd821e5eb22
Ivan Hrasko <ivan.hrasko@pantheon.tech>
2023-10-10 15:41:24 +0200

    Adapt tests for OpenApi
    As intermediate step in adaptation from OpenApiObject record
    remove all tests.
    
    We will have to definitely rewrite at least those introduced by:
    NETCONF-1131.
    
    JIRA: NETCONF-938
    Change-Id: Ie64b28037652a7608f2129b19dd822400216e88e
    Signed-off-by: Ivan Hrasko <ivan.hrasko@pantheon.tech>
    Signed-off-by: lubos-cicut <lubos.cicut@pantheon.tech>

--- restconf/restconf-openapi/src/main/java/org/opendaylight/restconf/openapi/impl/BaseYangOpenApiGenerator.java
+++ restconf/restconf-openapi/src/main/java/org/opendaylight/restconf/openapi/impl/BaseYangOpenApiGenerator.java
@@ -85,7 +85,7 @@ public abstract class BaseYangOpenApiGenerator {
     public static final String DESCRIPTION = """
         We are providing full API for configurational data which can be edited (by POST, PUT, PATCH and DELETE).
         For operational data we only provide GET API.\n
-        For majority of request you can see only config data in examples. That’s because we can show only one example
+        For majority of request you can see only config data in examples. That is because we can show only one example
         per request. The exception when you can see operational data in example is when data are representing
         operational (config false) container with no config data in it.""";
 
