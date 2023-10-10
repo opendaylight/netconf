e445265a01c453439b4fd2f3729b5ecea4117e37
Ivan Hrasko <ivan.hrasko@pantheon.tech>
2023-10-10 15:53:55 +0200

    Adapt API to OpenApiObject removal
    For now we have just adapted REST API to use new logic
    for demonstration purposes.
    
    JIRA: NETCONF-938
    Change-Id: Idc6a35bb29f77c37e7ac70e873fd6b720fc5584d
    Signed-off-by: Ivan Hrasko <ivan.hrasko@pantheon.tech>
    Signed-off-by: lubos-cicut <lubos.cicut@pantheon.tech>
    Signed-off-by: Yaroslav Lastivka <yaroslav.lastivka@pantheon.tech>

--- restconf/restconf-openapi/src/main/java/org/opendaylight/restconf/openapi/model/security/SecuritySchemeObject.java
+++ restconf/restconf-openapi/src/main/java/org/opendaylight/restconf/openapi/model/security/SecuritySchemeObject.java
@@ -7,12 +7,7 @@
  */
 package org.opendaylight.restconf.openapi.model.security;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.annotation.JsonProperty;
-
-@JsonInclude(JsonInclude.Include.NON_NULL)
 public interface SecuritySchemeObject {
-    @JsonProperty("type")
     Type type();
 
     /**
