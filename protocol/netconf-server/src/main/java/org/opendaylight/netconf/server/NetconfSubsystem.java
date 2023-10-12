20a1e11b764aa45934f12d73f6d4f73774f7bfe4
Robert Varga <robert.varga@pantheon.tech>
2023-10-12 17:32:32 +0200

    Split out NetconfSubsystemOutboundChannelHandler
    Do not use an anonymous class, but rather split the logic into its own
    dedicated class.
    
    JIRA: NETCONF-1106
    Change-Id: Ic8786dc950225a51d93d61e2c6a9bb64e144a7af
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- protocol/netconf-server/src/main/java/org/opendaylight/netconf/server/NetconfSubsystem.java
+++ protocol/netconf-server/src/main/java/org/opendaylight/netconf/server/NetconfSubsystem.java
@@ -9,38 +9,27 @@ package org.opendaylight.netconf.server;
 
 import static java.util.Objects.requireNonNull;
 
-import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
-import io.netty.channel.ChannelOutboundHandlerAdapter;
-import io.netty.channel.ChannelPromise;
 import io.netty.channel.embedded.EmbeddedChannel;
 import io.netty.util.concurrent.DefaultPromise;
 import io.netty.util.concurrent.GlobalEventExecutor;
-import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.nio.charset.StandardCharsets;
 import org.opendaylight.netconf.api.messages.NetconfHelloMessageAdditionalHeader;
 import org.opendaylight.netconf.shaded.sshd.common.io.IoInputStream;
 import org.opendaylight.netconf.shaded.sshd.common.io.IoOutputStream;
-import org.opendaylight.netconf.shaded.sshd.common.util.buffer.ByteArrayBuffer;
 import org.opendaylight.netconf.shaded.sshd.server.channel.ChannelDataReceiver;
 import org.opendaylight.netconf.shaded.sshd.server.channel.ChannelSession;
 import org.opendaylight.netconf.shaded.sshd.server.channel.ChannelSessionAware;
 import org.opendaylight.netconf.shaded.sshd.server.command.AbstractCommandSupport;
 import org.opendaylight.netconf.shaded.sshd.server.command.AsyncCommand;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 final class NetconfSubsystem extends AbstractCommandSupport
         implements AsyncCommand, ChannelSessionAware, ChannelDataReceiver {
-    private static final Logger LOG = LoggerFactory.getLogger(NetconfSubsystem.class);
-
     private final EmbeddedChannel innerChannel = new EmbeddedChannel();
     private final ServerChannelInitializer channelInitializer;
-
-    private IoOutputStream ioOutputStream;
 
     NetconfSubsystem(final String name, final ServerChannelInitializer channelInitializer) {
         super(name, null);
@@ -64,38 +53,12 @@ final class NetconfSubsystem extends AbstractCommandSupport
 
     @Override
     public void setIoOutputStream(final IoOutputStream out) {
-        ioOutputStream = out;
-
         /*
          * While NETCONF protocol handlers are designed to operate over Netty channel, the inner channel is used to
          * serve NETCONF over SSH.
          */
         // outbound packet handler, adding fist means it will be invoked last because of flow direction
-        innerChannel.pipeline().addFirst(
-            new ChannelOutboundHandlerAdapter() {
-                @Override
-                public void write(final ChannelHandlerContext ctx, final Object msg, final ChannelPromise promise) {
-                    if (msg instanceof ByteBuf byteBuf) {
-                        // redirect channel outgoing packets to output stream linked to transport
-                        final byte[] bytes = new byte[byteBuf.readableBytes()];
-                        byteBuf.readBytes(bytes);
-                        try {
-                            ioOutputStream.writeBuffer(new ByteArrayBuffer(bytes))
-                                .addListener(future -> {
-                                    if (future.isWritten()) {
-                                        byteBuf.release(); // report outbound message being handled
-                                        promise.setSuccess();
-                                    } else if (future.getException() != null) {
-                                        LOG.error("Error writing buffer", future.getException());
-                                        promise.setFailure(future.getException());
-                                    }
-                                });
-                        } catch (IOException e) {
-                            LOG.error("Error writing buffer", e);
-                        }
-                    }
-                }
-            });
+        innerChannel.pipeline().addFirst(new NetconfSubsystemOutboundChannelHandler(out));
 
         // inner channel termination handler
         innerChannel.pipeline().addLast(
