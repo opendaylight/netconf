25177ca43aafc38b1ce62d60c9f48a72eb427c52
Robert Varga <robert.varga@pantheon.tech>
2023-10-16 19:12:53 +0200

    Clean up CryptHashPasswordAuthenticator
    Use simple record for CryptHashValidator, inlining its isValid() method
    into its sole caller. Also extract validation logic into the
    the authenticator construction loop.
    
    Change-Id: I6a26b36786b7d5f1f9c6b12e0e01e3c3304fe1ce
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- transport/transport-ssh/src/main/java/org/opendaylight/netconf/transport/ssh/CryptHashPasswordAuthenticator.java
+++ transport/transport-ssh/src/main/java/org/opendaylight/netconf/transport/ssh/CryptHashPasswordAuthenticator.java
@@ -7,67 +7,58 @@
  */
 package org.opendaylight.netconf.transport.ssh;
 
-import static com.google.common.base.Preconditions.checkArgument;
 import static java.util.Objects.requireNonNull;
 
 import com.google.common.collect.ImmutableMap;
 import java.util.Map;
 import java.util.regex.Pattern;
 import org.apache.commons.codec.digest.Crypt;
-import org.eclipse.jdt.annotation.NonNull;
-import org.opendaylight.netconf.shaded.sshd.server.auth.AsyncAuthException;
 import org.opendaylight.netconf.shaded.sshd.server.auth.password.PasswordAuthenticator;
-import org.opendaylight.netconf.shaded.sshd.server.auth.password.PasswordChangeRequiredException;
 import org.opendaylight.netconf.shaded.sshd.server.session.ServerSession;
 
 final class CryptHashPasswordAuthenticator implements PasswordAuthenticator {
+    private record CryptHash(String salt, String hash) {
+        CryptHash {
+            requireNonNull(salt);
+            requireNonNull(hash);
+        }
+    }
 
     private static final Pattern CRYPT_HASH_PATTERN = Pattern.compile(
-            "\\$0\\$.*" // clear text
-                    + "|\\$1\\$[a-zA-Z0-9./]{1,8}\\$[a-zA-Z0-9./]{22}" // MD5
-                    + "|\\$5\\$(rounds=\\d+\\$)?[a-zA-Z0-9./]{1,16}\\$[a-zA-Z0-9./]{43}" // SHA-256
-                    + "|\\$6\\$(rounds=\\d+\\$)?[a-zA-Z0-9./]{1,16}\\$[a-zA-Z0-9./]{86}"); // SHA-512
-
+        // clear text, or
+        "\\$0\\$.*"
+        // MD5, or
+        + "|\\$1\\$[a-zA-Z0-9./]{1,8}\\$[a-zA-Z0-9./]{22}"
+        // SHA-256, or
+        + "|\\$5\\$(rounds=\\d+\\$)?[a-zA-Z0-9./]{1,16}\\$[a-zA-Z0-9./]{43}"
+        // SHA-512
+        + "|\\$6\\$(rounds=\\d+\\$)?[a-zA-Z0-9./]{1,16}\\$[a-zA-Z0-9./]{86}");
     private static final String DEFAULT_SALT = "$5$rounds=3500$default";
 
-    final Map<String, CryptHashValidator> userValidatorMap;
+    private final ImmutableMap<String, CryptHash> knownHashes;
 
-    CryptHashPasswordAuthenticator(final @NonNull Map<String, String> userHashes) {
-        requireNonNull(userHashes);
-        checkArgument(!userHashes.isEmpty(), "Hashes map should not be empty");
-        final var mapBuilder = ImmutableMap.<String, CryptHashValidator>builder();
-        for (var entry : userHashes.entrySet()) {
-            mapBuilder.put(entry.getKey(), new CryptHashValidator(entry.getValue()));
+    CryptHashPasswordAuthenticator(final Map<String, String> userHashes) {
+        if (userHashes.isEmpty()) {
+            throw new IllegalArgumentException("Hashes map should not be empty");
         }
-        userValidatorMap = mapBuilder.build();
+
+        final var mapBuilder = ImmutableMap.<String, CryptHash>builder();
+        for (var entry : userHashes.entrySet()) {
+            final var hash = entry.getValue();
+            if (!CRYPT_HASH_PATTERN.matcher(hash).matches()) {
+                throw new IllegalArgumentException("Invalid crypt hash string \"" + hash + '"');
+            }
+
+            mapBuilder.put(entry.getKey(), hash.startsWith("$0$")
+                ? new CryptHash(DEFAULT_SALT, Crypt.crypt(hash.substring(3), DEFAULT_SALT))
+                    : new CryptHash(hash.substring(0, hash.lastIndexOf('$')), hash));
+        }
+        knownHashes = mapBuilder.build();
     }
 
     @Override
-    public boolean authenticate(final String username, final String password, final ServerSession serverSession)
-            throws PasswordChangeRequiredException, AsyncAuthException {
-        final var validator = userValidatorMap.get(username);
-        return validator == null ? false : validator.isValid(password);
-    }
-
-    private static class CryptHashValidator {
-        final String salt;
-        final String cryptHash;
-
-        CryptHashValidator(final String cryptHash) {
-            requireNonNull(cryptHash);
-            checkArgument(CRYPT_HASH_PATTERN.matcher(cryptHash).matches(), "Not a valid crypt hash string");
-            if (cryptHash.startsWith("$0$")) {
-                // clear text password, build a hash using default salt
-                this.salt = DEFAULT_SALT;
-                this.cryptHash = Crypt.crypt(cryptHash.substring(3), DEFAULT_SALT);
-            } else {
-                this.salt = cryptHash.substring(0, cryptHash.lastIndexOf('$'));
-                this.cryptHash = cryptHash;
-            }
-        }
-
-        boolean isValid(final String password) {
-            return cryptHash.equals(Crypt.crypt(password, salt));
-        }
+    public boolean authenticate(final String username, final String password, final ServerSession serverSession) {
+        final var found = knownHashes.get(username);
+        return found != null && found.hash.equals(Crypt.crypt(password, found.salt));
     }
 }
