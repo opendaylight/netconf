b0dc6578dc2a9fb106fb42c1537da98234905eaf
Robert Varga <robert.varga@pantheon.tech>
2023-10-18 23:26:31 +0200

    Clean up NetconfTopologyImpl(Test)
    Improve dispatch of changes and eliminate useless mucking with
    yang-binding internals.
    
    We also migrate to JUnit and simplify mocking, eliminating a number
    of warnings in the process of doing so.
    
    Change-Id: I0357cf2f728e456918b036ff384da9cd226b6291
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>
    (cherry picked from commit a9b99dbc7b24b57cf30b7fa246bb8121d8910e4b)

---
+++ apps/netconf-topology-impl/src/main/java/org/opendaylight/netconf/topology/impl/NetconfTopologyImpl.java
@@ -0,0 +1,153 @@
+<<<<<<< HEAD   (083787 Bump upstreams)
+=======
+/*
+ * Copyright (c) 2015 Cisco Systems, Inc. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.netconf.topology.impl;
+
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+import io.netty.util.concurrent.EventExecutor;
+import java.util.Collection;
+import javax.annotation.PreDestroy;
+import javax.inject.Inject;
+import javax.inject.Singleton;
+import org.opendaylight.aaa.encrypt.AAAEncryptionService;
+import org.opendaylight.controller.config.threadpool.ScheduledThreadPool;
+import org.opendaylight.controller.config.threadpool.ThreadPool;
+import org.opendaylight.mdsal.binding.api.DataBroker;
+import org.opendaylight.mdsal.binding.api.DataTreeChangeListener;
+import org.opendaylight.mdsal.binding.api.DataTreeIdentifier;
+import org.opendaylight.mdsal.binding.api.DataTreeModification;
+import org.opendaylight.mdsal.binding.api.RpcProviderService;
+import org.opendaylight.mdsal.common.api.LogicalDatastoreType;
+import org.opendaylight.mdsal.dom.api.DOMMountPointService;
+import org.opendaylight.netconf.client.NetconfClientDispatcher;
+import org.opendaylight.netconf.client.mdsal.api.BaseNetconfSchemas;
+import org.opendaylight.netconf.client.mdsal.api.DeviceActionFactory;
+import org.opendaylight.netconf.client.mdsal.api.SchemaResourceManager;
+import org.opendaylight.netconf.topology.spi.AbstractNetconfTopology;
+import org.opendaylight.netconf.topology.spi.NetconfClientConfigurationBuilderFactory;
+import org.opendaylight.netconf.topology.spi.NetconfNodeUtils;
+import org.opendaylight.netconf.topology.spi.NetconfTopologyRPCProvider;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.NetworkTopology;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.TopologyId;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.Topology;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.TopologyKey;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.Node;
+import org.opendaylight.yangtools.concepts.Registration;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.service.component.annotations.Reference;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+// Non-final for testing
+@Singleton
+@Component(service = { })
+public class NetconfTopologyImpl extends AbstractNetconfTopology
+        implements DataTreeChangeListener<Node>, AutoCloseable {
+    private static final Logger LOG = LoggerFactory.getLogger(NetconfTopologyImpl.class);
+
+    private Registration dtclReg;
+    private NetconfTopologyRPCProvider rpcProvider;
+
+    @Inject
+    @Activate
+    public NetconfTopologyImpl(
+            @Reference(target = "(type=netconf-client-dispatcher)") final NetconfClientDispatcher clientDispatcher,
+            @Reference(target = "(type=global-event-executor)") final EventExecutor eventExecutor,
+            @Reference(target = "(type=global-netconf-ssh-scheduled-executor)")
+            final ScheduledThreadPool keepaliveExecutor,
+            @Reference(target = "(type=global-netconf-processing-executor)") final ThreadPool processingExecutor,
+            @Reference final SchemaResourceManager schemaRepositoryProvider, @Reference final DataBroker dataBroker,
+            @Reference final DOMMountPointService mountPointService,
+            @Reference final AAAEncryptionService encryptionService,
+            @Reference final NetconfClientConfigurationBuilderFactory builderFactory,
+            @Reference final RpcProviderService rpcProviderService, @Reference final BaseNetconfSchemas baseSchemas,
+            @Reference final DeviceActionFactory deviceActionFactory) {
+        this(NetconfNodeUtils.DEFAULT_TOPOLOGY_NAME, clientDispatcher, eventExecutor, keepaliveExecutor,
+            processingExecutor, schemaRepositoryProvider, dataBroker, mountPointService, encryptionService,
+            builderFactory, rpcProviderService, baseSchemas, deviceActionFactory);
+    }
+
+    public NetconfTopologyImpl(final String topologyId, final NetconfClientDispatcher clientDispatcher,
+            final EventExecutor eventExecutor, final ScheduledThreadPool keepaliveExecutor,
+            final ThreadPool processingExecutor, final SchemaResourceManager schemaRepositoryProvider,
+            final DataBroker dataBroker, final DOMMountPointService mountPointService,
+            final AAAEncryptionService encryptionService, final NetconfClientConfigurationBuilderFactory builderFactory,
+            final RpcProviderService rpcProviderService, final BaseNetconfSchemas baseSchemas) {
+        this(topologyId, clientDispatcher, eventExecutor, keepaliveExecutor, processingExecutor,
+            schemaRepositoryProvider, dataBroker, mountPointService, encryptionService, builderFactory,
+            rpcProviderService, baseSchemas, null);
+    }
+
+    @SuppressFBWarnings(value = "MC_OVERRIDABLE_METHOD_CALL_IN_CONSTRUCTOR",
+        justification = "DTCL registration of 'this'")
+    public NetconfTopologyImpl(final String topologyId, final NetconfClientDispatcher clientDispatcher,
+            final EventExecutor eventExecutor, final ScheduledThreadPool keepaliveExecutor,
+            final ThreadPool processingExecutor, final SchemaResourceManager schemaRepositoryProvider,
+            final DataBroker dataBroker, final DOMMountPointService mountPointService,
+            final AAAEncryptionService encryptionService, final NetconfClientConfigurationBuilderFactory builderFactory,
+            final RpcProviderService rpcProviderService, final BaseNetconfSchemas baseSchemas,
+            final DeviceActionFactory deviceActionFactory) {
+        super(topologyId, clientDispatcher, eventExecutor, keepaliveExecutor, processingExecutor,
+            schemaRepositoryProvider, dataBroker, mountPointService, builderFactory, deviceActionFactory, baseSchemas);
+
+        LOG.debug("Registering datastore listener");
+        dtclReg = dataBroker.registerDataTreeChangeListener(DataTreeIdentifier.create(
+            LogicalDatastoreType.CONFIGURATION, InstanceIdentifier.builder(NetworkTopology.class)
+                .child(Topology.class, new TopologyKey(new TopologyId(topologyId)))
+                .child(Node.class)
+                .build()), this);
+        rpcProvider = new NetconfTopologyRPCProvider(rpcProviderService, dataBroker, encryptionService, topologyId);
+    }
+
+    @PreDestroy
+    @Deactivate
+    @Override
+    public void close() {
+        if (rpcProvider != null) {
+            rpcProvider.close();
+            rpcProvider = null;
+        }
+
+        // close all existing connectors, delete whole topology in datastore?
+        deleteAllNodes();
+
+        if (dtclReg != null) {
+            dtclReg.close();
+            dtclReg = null;
+        }
+    }
+
+    @Override
+    public void onDataTreeChanged(final Collection<DataTreeModification<Node>> changes) {
+        for (var change : changes) {
+            final var rootNode = change.getRootNode();
+            final var modType = rootNode.getModificationType();
+            switch (modType) {
+                case SUBTREE_MODIFIED -> ensureNode("updated", rootNode.getDataAfter());
+                case WRITE -> ensureNode("created", rootNode.getDataAfter());
+                case DELETE -> {
+                    final var nodeId = InstanceIdentifier.keyOf(change.getRootPath().getRootIdentifier()).getNodeId();
+                    LOG.debug("Config for node {} deleted", nodeId);
+                    deleteNode(nodeId);
+                }
+                default -> LOG.debug("Unsupported modification type: {}.", modType);
+            }
+        }
+    }
+
+    private void ensureNode(final String operation, final Node node) {
+        final var nodeId = node.getNodeId();
+        LOG.debug("Config for node {} {}", nodeId, operation);
+        ensureNode(node);
+    }
+}
+>>>>>>> CHANGE (a9b99d Clean up NetconfTopologyImpl(Test))
---
+++ apps/netconf-topology-impl/src/test/java/org/opendaylight/netconf/topology/impl/NetconfTopologyImplTest.java
@@ -0,0 +1,178 @@
+<<<<<<< HEAD   (083787 Bump upstreams)
+=======
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.netconf.topology.impl;
+
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+import com.google.common.util.concurrent.MoreExecutors;
+import io.netty.util.concurrent.EventExecutor;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.opendaylight.aaa.encrypt.AAAEncryptionService;
+import org.opendaylight.controller.config.threadpool.ScheduledThreadPool;
+import org.opendaylight.controller.config.threadpool.ThreadPool;
+import org.opendaylight.mdsal.binding.api.DataBroker;
+import org.opendaylight.mdsal.binding.api.DataObjectModification;
+import org.opendaylight.mdsal.binding.api.DataTreeIdentifier;
+import org.opendaylight.mdsal.binding.api.DataTreeModification;
+import org.opendaylight.mdsal.binding.api.RpcProviderService;
+import org.opendaylight.mdsal.binding.api.WriteTransaction;
+import org.opendaylight.mdsal.common.api.CommitInfo;
+import org.opendaylight.mdsal.common.api.LogicalDatastoreType;
+import org.opendaylight.mdsal.dom.api.DOMMountPointService;
+import org.opendaylight.netconf.client.NetconfClientDispatcher;
+import org.opendaylight.netconf.client.mdsal.api.BaseNetconfSchemas;
+import org.opendaylight.netconf.client.mdsal.api.SchemaResourceManager;
+import org.opendaylight.netconf.client.mdsal.impl.DefaultBaseNetconfSchemas;
+import org.opendaylight.netconf.topology.spi.NetconfClientConfigurationBuilderFactory;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev130715.Host;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev130715.IpAddress;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev130715.Ipv4Address;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev130715.PortNumber;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netconf.device.rev230430.credentials.credentials.LoginPasswordBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netconf.node.topology.rev221225.NetconfNodeBuilder;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.NetworkTopology;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.NodeId;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.TopologyId;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.Topology;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.TopologyBuilder;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.TopologyKey;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.Node;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.NodeBuilder;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.NodeKey;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+import org.opendaylight.yangtools.yang.binding.KeyedInstanceIdentifier;
+import org.opendaylight.yangtools.yang.common.Uint16;
+import org.opendaylight.yangtools.yang.common.Uint32;
+import org.opendaylight.yangtools.yang.parser.impl.DefaultYangParserFactory;
+
+@ExtendWith(MockitoExtension.class)
+class NetconfTopologyImplTest {
+    private static final TopologyKey TOPOLOGY_KEY = new TopologyKey(new TopologyId("testing-topology"));
+    private static final KeyedInstanceIdentifier<Topology, TopologyKey> TOPOLOGY_PATH =
+        InstanceIdentifier.builder(NetworkTopology.class).child(Topology.class, TOPOLOGY_KEY).build();
+
+    @Mock
+    private NetconfClientDispatcher mockedClientDispatcher;
+    @Mock
+    private EventExecutor mockedEventExecutor;
+    @Mock
+    private ScheduledThreadPool mockedKeepaliveExecutor;
+    @Mock
+    private ThreadPool mockedProcessingExecutor;
+    @Mock
+    private SchemaResourceManager mockedResourceManager;
+    @Mock
+    private DataBroker dataBroker;
+    @Mock
+    private DOMMountPointService mountPointService;
+    @Mock
+    private AAAEncryptionService encryptionService;
+    @Mock
+    private RpcProviderService rpcProviderService;
+    @Mock
+    private NetconfClientConfigurationBuilderFactory builderFactory;
+    @Mock
+    private WriteTransaction wtx;
+    @Mock
+    private DataObjectModification<Node> objMod;
+    @Mock
+    private DataTreeModification<Node> treeMod;
+
+    private TestingNetconfTopologyImpl topology;
+    private TestingNetconfTopologyImpl spyTopology;
+
+    @Test
+    void testOnDataTreeChange() throws Exception {
+        doReturn(MoreExecutors.newDirectExecutorService()).when(mockedProcessingExecutor).getExecutor();
+        doReturn(wtx).when(dataBroker).newWriteOnlyTransaction();
+        doReturn(CommitInfo.emptyFluentFuture()).when(wtx).commit();
+
+        topology = new TestingNetconfTopologyImpl(TOPOLOGY_KEY.getTopologyId().getValue(), mockedClientDispatcher,
+            mockedEventExecutor, mockedKeepaliveExecutor, mockedProcessingExecutor, mockedResourceManager, dataBroker,
+            mountPointService, encryptionService, builderFactory, rpcProviderService,
+            new DefaultBaseNetconfSchemas(new DefaultYangParserFactory()));
+        //verify initialization of topology
+        verify(wtx).merge(LogicalDatastoreType.OPERATIONAL, TOPOLOGY_PATH,
+            new TopologyBuilder().withKey(TOPOLOGY_KEY).build());
+
+        spyTopology = spy(topology);
+
+        final var key = new NodeKey(new NodeId("testing-node"));
+        final var node = new NodeBuilder()
+            .withKey(key)
+            .addAugmentation(new NetconfNodeBuilder()
+                .setHost(new Host(new IpAddress(new Ipv4Address("127.0.0.1"))))
+                .setPort(new PortNumber(Uint16.valueOf(9999)))
+                .setReconnectOnChangedSchema(true)
+                .setDefaultRequestTimeoutMillis(Uint32.valueOf(1000))
+                .setBetweenAttemptsTimeoutMillis(Uint16.valueOf(100))
+                .setKeepaliveDelay(Uint32.valueOf(1000))
+                .setTcpOnly(true)
+                .setCredentials(new LoginPasswordBuilder()
+                    .setUsername("testuser")
+                    .setPassword("testpassword")
+                    .build())
+                .build())
+            .build();
+
+        doReturn(DataObjectModification.ModificationType.WRITE).when(objMod).getModificationType();
+        doReturn(node).when(objMod).getDataAfter();
+
+        doReturn(DataTreeIdentifier.create(LogicalDatastoreType.CONFIGURATION, TOPOLOGY_PATH.child(Node.class, key)))
+            .when(treeMod).getRootPath();
+        final var changes = List.of(treeMod);
+
+        doReturn(objMod).when(treeMod).getRootNode();
+        spyTopology.onDataTreeChanged(changes);
+        verify(spyTopology).ensureNode(node);
+
+        doReturn(DataObjectModification.ModificationType.DELETE).when(objMod).getModificationType();
+        spyTopology.onDataTreeChanged(changes);
+        verify(spyTopology).deleteNode(key.getNodeId());
+
+        doReturn(DataObjectModification.ModificationType.SUBTREE_MODIFIED).when(objMod).getModificationType();
+        spyTopology.onDataTreeChanged(changes);
+
+        // one in previous creating and deleting node and one in updating
+        verify(spyTopology, times(2)).ensureNode(node);
+    }
+
+    private static class TestingNetconfTopologyImpl extends NetconfTopologyImpl {
+        TestingNetconfTopologyImpl(final String topologyId, final NetconfClientDispatcher clientDispatcher,
+                final EventExecutor eventExecutor, final ScheduledThreadPool keepaliveExecutor,
+                final ThreadPool processingExecutor, final SchemaResourceManager schemaRepositoryProvider,
+                final DataBroker dataBroker, final DOMMountPointService mountPointService,
+                final AAAEncryptionService encryptionService,
+                final NetconfClientConfigurationBuilderFactory builderFactory,
+                final RpcProviderService rpcProviderService, final BaseNetconfSchemas baseSchemas) {
+            super(topologyId, clientDispatcher, eventExecutor, keepaliveExecutor, processingExecutor,
+                schemaRepositoryProvider, dataBroker, mountPointService, encryptionService, builderFactory,
+                rpcProviderService, baseSchemas);
+        }
+
+        @Override
+        public void ensureNode(final Node configNode) {
+            // No-op
+        }
+
+        @Override
+        public void deleteNode(final NodeId nodeId) {
+            // No-op
+        }
+    }
+}
+>>>>>>> CHANGE (a9b99d Clean up NetconfTopologyImpl(Test))
