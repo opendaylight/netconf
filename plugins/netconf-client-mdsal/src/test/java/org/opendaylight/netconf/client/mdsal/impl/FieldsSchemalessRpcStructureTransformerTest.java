3ee7ed0ba7951fbbbeed37e873a2f3ef810529be
Robert Varga <robert.varga@pantheon.tech>
2023-10-18 11:51:52 +0200

    Migrate FieldsSchemalessRpcStructureTransformerTest
    Migrate tu xmlunit-code and JUnit5.
    
    Change-Id: I2f7ebbf7644e5d8b27a2e3d19588f792031bf535
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- plugins/netconf-client-mdsal/src/test/java/org/opendaylight/netconf/client/mdsal/impl/FieldsSchemalessRpcStructureTransformerTest.java
+++ plugins/netconf-client-mdsal/src/test/java/org/opendaylight/netconf/client/mdsal/impl/FieldsSchemalessRpcStructureTransformerTest.java
@@ -7,17 +7,11 @@
  */
 package org.opendaylight.netconf.client.mdsal.impl;
 
-import static org.junit.Assert.assertTrue;
-import static org.opendaylight.netconf.client.mdsal.impl.NetconfMessageTransformUtil.toId;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
-import java.io.IOException;
-import java.net.URISyntaxException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import com.google.common.collect.ImmutableMap;
 import java.util.List;
-import java.util.Map;
-import org.custommonkey.xmlunit.Diff;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.opendaylight.netconf.api.xml.XmlUtil;
 import org.opendaylight.yangtools.yang.common.QName;
 import org.opendaylight.yangtools.yang.common.QNameModule;
@@ -28,12 +22,12 @@ import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.NodeIdent
 import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.NodeIdentifierWithPredicates;
 import org.opendaylight.yangtools.yang.data.api.schema.DOMSourceAnyxmlNode;
 import org.w3c.dom.Element;
-import org.xml.sax.SAXException;
+import org.xmlunit.builder.DiffBuilder;
 
-public class FieldsSchemalessRpcStructureTransformerTest {
-
-    /**
-     * Sample YANG structure.<br>
+class FieldsSchemalessRpcStructureTransformerTest {
+    /*
+     * Sample YANG structure:
+     *
      * container c-1 {
      *     leaf leaf-1 {
      *         type string;
@@ -67,61 +61,59 @@ public class FieldsSchemalessRpcStructureTransformerTest {
     private static final QNameModule TEST_MODULE = QNameModule.create(
             XMLNamespace.of("test-namespace"), Revision.of("2020-10-25"));
 
-    private static final NodeIdentifier C1_NID = toId(QName.create(TEST_MODULE, "c-1"));
-    private static final NodeIdentifier C2_NID = toId(QName.create(TEST_MODULE, "c-2"));
-    private static final NodeIdentifier CX_NID = toId(QName.create(TEST_MODULE, "c-x"));
+    private static final NodeIdentifier C1_NID = new NodeIdentifier(QName.create(TEST_MODULE, "c-1"));
+    private static final NodeIdentifier C2_NID = new NodeIdentifier(QName.create(TEST_MODULE, "c-2"));
+    private static final NodeIdentifier CX_NID = new NodeIdentifier(QName.create(TEST_MODULE, "c-x"));
 
-    private static final NodeIdentifier LEAF1_NID = toId(QName.create(TEST_MODULE, "leaf-1"));
-    private static final NodeIdentifier LEAF2_NID = toId(QName.create(TEST_MODULE, "leaf-2"));
-    private static final NodeIdentifier LEAF3_NID = toId(QName.create(TEST_MODULE, "leaf-3"));
-    private static final NodeIdentifier LX_NID = toId(QName.create(TEST_MODULE, "l-x"));
+    private static final NodeIdentifier LEAF1_NID = new NodeIdentifier(QName.create(TEST_MODULE, "leaf-1"));
+    private static final NodeIdentifier LEAF2_NID = new NodeIdentifier(QName.create(TEST_MODULE, "leaf-2"));
+    private static final NodeIdentifier LEAF3_NID = new NodeIdentifier(QName.create(TEST_MODULE, "leaf-3"));
+    private static final NodeIdentifier LX_NID = new NodeIdentifier(QName.create(TEST_MODULE, "l-x"));
 
     private static final QName KEY1_QNAME = QName.create(TEST_MODULE, "key-1");
     private static final QName KEY2_QNAME = QName.create(TEST_MODULE, "key-2");
-    private static final NodeIdentifier KEY1_NID = toId(KEY1_QNAME);
+    private static final NodeIdentifier KEY1_NID = new NodeIdentifier(KEY1_QNAME);
 
     private static final QName LIST1_QNAME = QName.create(TEST_MODULE, "list-1");
-    private static final NodeIdentifier LIST1_NID = toId(LIST1_QNAME);
+    private static final NodeIdentifier LIST1_NID = new NodeIdentifier(LIST1_QNAME);
 
     private final SchemalessRpcStructureTransformer transformer = new SchemalessRpcStructureTransformer();
 
     @Test
-    public void toFilterStructureWithSingleRootTest() throws SAXException, IOException, URISyntaxException {
-        final YangInstanceIdentifier rootPath = YangInstanceIdentifier.of(C1_NID);
-        final YangInstanceIdentifier leaf1Field = YangInstanceIdentifier.of(LEAF1_NID);
-        final YangInstanceIdentifier leaf3Field = YangInstanceIdentifier.of(NodeIdentifierWithPredicates.of(
-                LIST1_QNAME, Map.of(KEY1_QNAME, "key1", KEY2_QNAME, "key2")), C2_NID, LEAF3_NID);
-        final YangInstanceIdentifier key1Field = YangInstanceIdentifier.of(NodeIdentifierWithPredicates.of(
-                LIST1_QNAME, Map.of(KEY1_QNAME, "key1", KEY2_QNAME, "key2")), KEY1_NID);
-        final FieldsFilter filter = FieldsFilter.of(rootPath, List.of(leaf1Field, leaf3Field, key1Field));
+    void toFilterStructureWithSingleRootTest() throws Exception {
+        final var rootPath = YangInstanceIdentifier.of(C1_NID);
+        final var leaf1Field = YangInstanceIdentifier.of(LEAF1_NID);
+        final var leaf3Field = YangInstanceIdentifier.of(NodeIdentifierWithPredicates.of(
+                LIST1_QNAME, ImmutableMap.of(KEY1_QNAME, "key1", KEY2_QNAME, "key2")), C2_NID, LEAF3_NID);
+        final var key1Field = YangInstanceIdentifier.of(NodeIdentifierWithPredicates.of(
+                LIST1_QNAME, ImmutableMap.of(KEY1_QNAME, "key1", KEY2_QNAME, "key2")), KEY1_NID);
+        final var filter = FieldsFilter.of(rootPath, List.of(leaf1Field, leaf3Field, key1Field));
 
-        final DOMSourceAnyxmlNode filterStructure = (DOMSourceAnyxmlNode) transformer.toFilterStructure(
-                List.of(filter));
-        final Diff diff = getDiff("one-root-filter.xml", filterStructure);
-        assertTrue(diff.similar());
+        final var filterStructure = (DOMSourceAnyxmlNode) transformer.toFilterStructure(List.of(filter));
+        assertSimilar("one-root-filter.xml", filterStructure);
     }
 
     @Test
-    public void toFilterStructureWithTwoRootContainersTest() throws SAXException, IOException, URISyntaxException {
-        final YangInstanceIdentifier c1RootPath = YangInstanceIdentifier.of(C1_NID);
-        final YangInstanceIdentifier cxRootPath = YangInstanceIdentifier.of(CX_NID);
-        final YangInstanceIdentifier c2Field = YangInstanceIdentifier.of(LIST1_NID, C2_NID);
-        final YangInstanceIdentifier leaf2Field = YangInstanceIdentifier.of(LIST1_NID, C2_NID, LEAF2_NID);
-        final YangInstanceIdentifier lxField = YangInstanceIdentifier.of(LX_NID);
+    void toFilterStructureWithTwoRootContainersTest() throws Exception {
+        final var c1RootPath = YangInstanceIdentifier.of(C1_NID);
+        final var cxRootPath = YangInstanceIdentifier.of(CX_NID);
+        final var c2Field = YangInstanceIdentifier.of(LIST1_NID, C2_NID);
+        final var leaf2Field = YangInstanceIdentifier.of(LIST1_NID, C2_NID, LEAF2_NID);
+        final var lxField = YangInstanceIdentifier.of(LX_NID);
 
-        final FieldsFilter filter1 = FieldsFilter.of(c1RootPath, List.of(c2Field, leaf2Field));
-        final FieldsFilter filter2 = FieldsFilter.of(cxRootPath, List.of(lxField));
-        final DOMSourceAnyxmlNode filterStructure = (DOMSourceAnyxmlNode) transformer.toFilterStructure(
-                List.of(filter1, filter2));
-        final Diff diff = getDiff("two-roots-filter.xml", filterStructure);
-        assertTrue(diff.similar());
+        final var filter1 = FieldsFilter.of(c1RootPath, List.of(c2Field, leaf2Field));
+        final var filter2 = FieldsFilter.of(cxRootPath, List.of(lxField));
+        final var filterStructure = (DOMSourceAnyxmlNode) transformer.toFilterStructure(List.of(filter1, filter2));
+        assertSimilar("two-roots-filter.xml", filterStructure);
     }
 
-    private static Diff getDiff(final String filterFileName, final DOMSourceAnyxmlNode filterStructure)
-            throws IOException, SAXException, URISyntaxException {
-        final String body = XmlUtil.toString((Element) filterStructure.body().getNode());
-        final String expectedBody = new String(Files.readAllBytes(Paths.get(FieldsSchemalessRpcStructureTransformerTest
-                .class.getResource("/schemaless/filter/" + filterFileName).toURI())));
-        return new Diff(expectedBody, body);
+    private static void assertSimilar(final String filterFileName, final DOMSourceAnyxmlNode filterStructure)
+            throws Exception {
+        final var diff = DiffBuilder.compare(FieldsSchemalessRpcStructureTransformerTest.class.getResourceAsStream(
+            "/schemaless/filter/" + filterFileName))
+            .withTest(XmlUtil.toString((Element) filterStructure.body().getNode()))
+            .checkForIdentical()
+            .build();
+        assertFalse(diff.hasDifferences(), diff.toString());
     }
 }
