0528b0d1d8f184f044a685149185462b85f8154f
Samuel Schneider <samuel.schneider@pantheon.tech>
2023-10-24 15:57:04 +0200

    OpenApi: Remove incorrect list POST requests
    Our logic creates POST requests for lists with keys, containing
    multiple resources in payload. This does not align with the expected
    structure for creating child resources in the YANG model.
    
    Added condition that prevents the creation of POST requests
    that has list as a last element.
    
    JIRA: NETCONF-1101
    Change-Id: I46820f222c9c5ef8078ac8675d2adc12b06f1253
    Signed-off-by: Yaroslav Lastivka <yaroslav.lastivka@pantheon.tech>
    Signed-off-by: Ivan Hrasko <ivan.hrasko@pantheon.tech>
    Signed-off-by: Samuel Schneider <samuel.schneider@pantheon.tech>
    (cherry picked from commit 1d64392feb896e9c74f4d9085eea52bbfdac1211)

---
+++ restconf/sal-rest-docgen/src/test/java/org/opendaylight/netconf/sal/rest/doc/impl/ListPostRequestsTest.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright (c) 2023 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.netconf.sal.rest.doc.impl;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.opendaylight.mdsal.dom.api.DOMSchemaService;
+import org.opendaylight.netconf.sal.rest.doc.swagger.OpenApiObject;
+import org.opendaylight.yangtools.yang.test.util.YangParserTestUtils;
+
+public class ListPostRequestsTest {
+    private static OpenApiObject doc;
+
+    @BeforeClass
+    public static void startUp() throws Exception {
+        final var context = YangParserTestUtils.parseYangResource("/yang/list-post.yang");
+        final var schemaService = mock(DOMSchemaService.class);
+        when(schemaService.getGlobalContext()).thenReturn(context);
+        final var generator = new ApiDocGeneratorRFC8040(schemaService);
+        final var uriInfo = DocGenTestHelper.createMockUriInfo("http://localhost/path");
+        doc = (OpenApiObject) generator.getApiDeclaration("list-post", null, uriInfo, ApiDocServiceImpl.OAversion.V3_0);
+        assertNotNull(doc);
+    }
+
+    /**
+     * Test to verify that we do NOT generate OpenApi example POST request for path ending with list element.
+     *
+     * <p>
+     * Assert that for paths ending with container we have examples for all types of requests.
+     * Assert that for paths ending with list we do NOT have POST example.
+     */
+    @Test
+    public void testListPostRequest() {
+        // for container, we have both post (with child as payload) and put (with itself as payload)
+        final var pathToContainer = "/rests/data/list-post:container";
+        assertNotNull(doc.getPaths().get(pathToContainer).get("get"));
+        assertNotNull(doc.getPaths().get(pathToContainer).get("post"));
+        assertNotNull(doc.getPaths().get(pathToContainer).get("put"));
+        assertNotNull(doc.getPaths().get(pathToContainer).get("delete"));
+        // for list, we cannot make a post request
+        final var pathToList = "/rests/data/list-post:container/list={name},{address}";
+        assertNotNull(doc.getPaths().get(pathToList).get("get"));
+        assertNull(doc.getPaths().get(pathToList).get("post"));
+        assertNotNull(doc.getPaths().get(pathToList).get("put"));
+        assertNotNull(doc.getPaths().get(pathToList).get("delete"));
+    }
+}
