e7a1c5847b84f733db5651bfe7baa72d51d5604a
Yaroslav Lastivka <yaroslav.lastivka@pantheon.tech>
2023-10-30 20:02:29 +0200

    Re-implement logic for security and securityShemas
    Added SecurityEntity, SecuritySchemasEntity for generating
    Security fields in JSON.
    
    JIRA: NETCONF-938
    Change-Id: Ia45e5b590bee65325b7b131d5e4f11b0836c5634
    Signed-off-by: Yaroslav Lastivka <yaroslav.lastivka@pantheon.tech>

--- restconf/restconf-openapi/src/main/java/org/opendaylight/restconf/openapi/impl/SecurityStream.java
+++ restconf/restconf-openapi/src/main/java/org/opendaylight/restconf/openapi/impl/SecurityStream.java
@@ -7,21 +7,42 @@
  */
 package org.opendaylight.restconf.openapi.impl;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.nio.charset.StandardCharsets;
 import org.opendaylight.restconf.openapi.jaxrs.OpenApiBodyWriter;
+import org.opendaylight.restconf.openapi.model.OpenApiEntity;
+import org.opendaylight.restconf.openapi.model.SecurityEntity;
 
 public final class SecurityStream extends InputStream {
-    public SecurityStream(final OpenApiBodyWriter writer) {
+    private final OpenApiBodyWriter writer;
+    private final SecurityEntity entity;
+
+    private Reader reader;
+
+    public SecurityStream(final OpenApiBodyWriter writer, final SecurityEntity entity) {
+        this.writer = writer;
+        this.entity = entity;
     }
 
     @Override
     public int read() throws IOException {
-        return -1;
+        if (reader == null) {
+            reader = new InputStreamReader(new ByteArrayInputStream(writeNextEntity(entity)), StandardCharsets.UTF_8);
+        }
+        return reader.read();
     }
 
     @Override
     public int read(final byte[] array, final int off, final int len) throws IOException {
         return super.read(array, off, len);
+    }
+
+    private byte[] writeNextEntity(final OpenApiEntity next) throws IOException {
+        writer.writeTo(next, null, null, null, null, null, null);
+        return writer.readFrom();
     }
 }
