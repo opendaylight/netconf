b5c6e94c8758e8e135b7fd4577b17b4423f54bf3
Yaroslav Lastivka <yaroslav.lastivka@pantheon.tech>
2023-10-30 20:02:29 +0200

    Re-implement logic for security and securityShemas
    Added SecurityEntity, SecuritySchemasEntity for generating
    Security fields in JSON.
    
    JIRA: NETCONF-938
    Change-Id: Ia45e5b590bee65325b7b131d5e4f11b0836c5634
    Signed-off-by: Yaroslav Lastivka <yaroslav.lastivka@pantheon.tech>

--- restconf/restconf-openapi/src/main/java/org/opendaylight/restconf/openapi/impl/SchemasStream.java
+++ restconf/restconf-openapi/src/main/java/org/opendaylight/restconf/openapi/impl/SchemasStream.java
@@ -18,8 +18,10 @@ import java.nio.charset.StandardCharsets;
 import java.util.ArrayDeque;
 import java.util.Deque;
 import java.util.Iterator;
+import java.util.Map;
 import org.opendaylight.restconf.openapi.jaxrs.OpenApiBodyWriter;
 import org.opendaylight.restconf.openapi.model.SchemaEntity;
+import org.opendaylight.restconf.openapi.model.security.Http;
 import org.opendaylight.yangtools.yang.model.api.ActionNodeContainer;
 import org.opendaylight.yangtools.yang.model.api.ChoiceSchemaNode;
 import org.opendaylight.yangtools.yang.model.api.ContainerSchemaNode;
@@ -31,6 +33,9 @@ import org.opendaylight.yangtools.yang.model.api.Module;
 import org.opendaylight.yangtools.yang.model.util.SchemaInferenceStack;
 
 public final class SchemasStream extends InputStream {
+    private static final String BASIC_AUTH_NAME = "basicAuth";
+    private static final Http OPEN_API_BASIC_AUTH = new Http("basic", null, null);
+
     private static final String OBJECT_TYPE = "object";
     private static final String INPUT_SUFFIX = "_input";
     private static final String OUTPUT_SUFFIX = "_output";
@@ -81,12 +86,13 @@ public final class SchemasStream extends InputStream {
                 continue;
             }
             if (!schemesWritten) {
-                reader = new InputStreamReader(new SecuritySchemesStream(writer), StandardCharsets.UTF_8);
+                generator.writeEndObject();
+                reader = new InputStreamReader(new SecuritySchemesStream(writer, Map.of(BASIC_AUTH_NAME,
+                    OPEN_API_BASIC_AUTH)), StandardCharsets.UTF_8);
                 read = reader.read();
                 schemesWritten = true;
                 continue;
             }
-            generator.writeEndObject();
             generator.writeEndObject();
             generator.flush();
             reader = new InputStreamReader(new ByteArrayInputStream(stream.toByteArray()), StandardCharsets.UTF_8);
