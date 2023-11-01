138c813361f750072fddb08531281dda952eb968
Ruslan Kashapov <ruslan.kashapov@pantheon.tech>
2023-11-01 10:57:22 +0200

    Call-Home server implementation using transport-api
    New implementation for TLS and SSH Call-Home
    servers using transport-api targeted to replace
    legacy implementation -> callhome-protocol module
    
    JIRA: NETCONF-1183
    Change-Id: I326d0ff2ac6ff2ba8e228e5281759c7c9b8e0d74
    Signed-off-by: Ruslan Kashapov <ruslan.kashapov@pantheon.tech>

---
+++ netconf/callhome-server/src/main/java/org/opendaylight/netconf/callhome/server/ssh/CallHomeSshServer.java
@@ -0,0 +1,220 @@
+/*
+ * Copyright (c) 2016 Brocade Communication Systems and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.netconf.callhome.server.ssh;
+
+import static java.util.Objects.requireNonNull;
+import static org.opendaylight.netconf.client.NetconfClientSessionNegotiatorFactory.DEFAULT_CLIENT_CAPABILITIES;
+
+import com.google.common.annotations.VisibleForTesting;
+import io.netty.util.HashedWheelTimer;
+import java.net.InetAddress;
+import java.net.SocketAddress;
+import java.security.PublicKey;
+import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+import org.eclipse.jdt.annotation.NonNull;
+import org.opendaylight.netconf.api.TransportConstants;
+import org.opendaylight.netconf.callhome.server.CallHomeStatusRecorder;
+import org.opendaylight.netconf.callhome.server.CallHomeTransportChannelListener;
+import org.opendaylight.netconf.client.NetconfClientSessionNegotiatorFactory;
+import org.opendaylight.netconf.shaded.sshd.client.auth.password.UserAuthPasswordFactory;
+import org.opendaylight.netconf.shaded.sshd.client.auth.pubkey.UserAuthPublicKeyFactory;
+import org.opendaylight.netconf.shaded.sshd.client.session.ClientSession;
+import org.opendaylight.netconf.shaded.sshd.common.session.Session;
+import org.opendaylight.netconf.shaded.sshd.common.session.SessionListener;
+import org.opendaylight.netconf.transport.api.UnsupportedConfigurationException;
+import org.opendaylight.netconf.transport.ssh.ClientFactoryManagerConfigurator;
+import org.opendaylight.netconf.transport.ssh.SSHClient;
+import org.opendaylight.netconf.transport.ssh.SSHTransportStackFactory;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev130715.IetfInetUtil;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev130715.PortNumber;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.netconf.client.rev230417.netconf.client.initiate.stack.grouping.transport.ssh.ssh.SshClientParametersBuilder;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.netconf.client.rev230417.netconf.client.listen.stack.grouping.transport.ssh.ssh.TcpServerParametersBuilder;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.ssh.client.rev230417.ssh.client.grouping.ClientIdentityBuilder;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.tcp.server.rev230417.TcpServerGrouping;
+import org.opendaylight.yangtools.yang.common.Uint16;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public final class CallHomeSshServer implements AutoCloseable {
+    private static final Logger LOG = LoggerFactory.getLogger(CallHomeSshServer.class);
+    private static final long DEFAULT_TIMEOUT_MILLIS = 10000L;
+    private static final int DEFAULT_PORT = 4334;
+
+    private final CallHomeSshAuthProvider authProvider;
+    private final CallHomeStatusRecorder statusRecorder;
+    private final CallHomeSshSessionContextManager contextManager;
+    private final SSHClient client;
+
+    @VisibleForTesting
+    CallHomeSshServer(final TcpServerGrouping tcpServerParams,
+            final SSHTransportStackFactory transportStackFactory,
+            final NetconfClientSessionNegotiatorFactory negotiatorFactory,
+            final CallHomeSshSessionContextManager contextManager,
+            final CallHomeSshAuthProvider authProvider,
+            final CallHomeStatusRecorder statusRecorder) {
+        this.authProvider = requireNonNull(authProvider);
+        this.statusRecorder = requireNonNull(statusRecorder);
+        this.contextManager = requireNonNull(contextManager);
+
+        // netconf layer
+        final var transportChannelListener =
+            new CallHomeTransportChannelListener(negotiatorFactory, contextManager, statusRecorder);
+
+        // SSH transport layer configuration
+        // NB actual username will be assigned dynamically but predefined one is required for transport initialization
+        final var sshClientParams = new SshClientParametersBuilder().setClientIdentity(
+            new ClientIdentityBuilder().setUsername("ignored").build()).build();
+        final ClientFactoryManagerConfigurator configurator = factoryMgr -> {
+            factoryMgr.setServerKeyVerifier(this::verifyServerKey);
+            factoryMgr.addSessionListener(createSessionListener());
+            // supported auth factories
+            factoryMgr.setUserAuthFactories(List.of(new UserAuthPasswordFactory(), new UserAuthPublicKeyFactory()));
+        };
+        try {
+            client = transportStackFactory.listenClient(TransportConstants.SSH_SUBSYSTEM, transportChannelListener,
+                tcpServerParams, sshClientParams, configurator).get(DEFAULT_TIMEOUT_MILLIS, TimeUnit.MILLISECONDS);
+        } catch (UnsupportedConfigurationException | InterruptedException | ExecutionException | TimeoutException e) {
+            throw new IllegalStateException("Could not start SSH Call-Home server", e);
+        }
+    }
+
+    private SessionListener createSessionListener() {
+        return new SessionListener() {
+            @Override
+            public void sessionClosed(final Session session) {
+                if (session instanceof ClientSession clientSession) {
+                    final var context = contextManager.findBySshSession(clientSession);
+                    if (context != null) {
+                        contextManager.remove(context.id());
+                        if (!clientSession.isAuthenticated()) {
+                            // threat unauthenticated session closure as authentication failure
+                            // in case there was context object created for the session
+                            statusRecorder.reportFailedAuth(context.id());
+                        }
+                    }
+                }
+            }
+        };
+    }
+
+    private boolean verifyServerKey(final ClientSession clientSession, final SocketAddress remoteAddress,
+            final PublicKey serverKey) {
+        final CallHomeSshAuthSettings authSettings = authProvider.provideAuth(remoteAddress, serverKey);
+        if (authSettings == null) {
+            // no auth for server key
+            statusRecorder.reportUnknown(serverKey);
+            LOG.info("No auth settings found. Connection from {} rejected.", remoteAddress);
+            return false;
+        }
+        final var context = contextManager.createContext(authSettings.id(), clientSession);
+        if (context == null) {
+            // if there is an issue creating context then the cause expected to be
+            // logged within overridden createContext() method
+            return false;
+        }
+        if (!contextManager.register(context)) {
+            // Session context with same id already exists
+            LOG.info("Session context with same id {} already exists. Connection from {} rejected.",
+                authSettings.id(), remoteAddress);
+            return false;
+        }
+
+        // Session context is ok, apply auth settings to current session
+        authSettings.applyTo(clientSession);
+        LOG.debug("Session context is created for SSH session: {}", context);
+        return true;
+    }
+
+    @Override
+    public void close() throws Exception {
+        contextManager.close();
+        client.shutdown().get();
+    }
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    public static final class Builder {
+        private InetAddress address;
+        private int port = DEFAULT_PORT;
+        private SSHTransportStackFactory transportStackFactory;
+        private NetconfClientSessionNegotiatorFactory negotiationFactory;
+        private CallHomeSshAuthProvider authProvider;
+        private CallHomeSshSessionContextManager contextManager;
+        private CallHomeStatusRecorder statusRecorder;
+
+        private Builder() {
+            // on purpose
+        }
+
+        public @NonNull CallHomeSshServer build() {
+            return new CallHomeSshServer(
+                toServerParams(address, port),
+                transportStackFactory == null ? defaultTransportStackFactory() : transportStackFactory,
+                negotiationFactory == null ? defaultNegotiationFactory() : negotiationFactory,
+                contextManager == null ? new CallHomeSshSessionContextManager() : contextManager,
+                authProvider, statusRecorder);
+        }
+
+        public Builder withAuthProvider(final CallHomeSshAuthProvider newAuthProvider) {
+            this.authProvider = newAuthProvider;
+            return this;
+        }
+
+        public Builder withSessionContextManager(final CallHomeSshSessionContextManager newContextManager) {
+            this.contextManager = newContextManager;
+            return this;
+        }
+
+        public Builder withStatusRecorder(final CallHomeStatusRecorder newStatusRecorder) {
+            this.statusRecorder = newStatusRecorder;
+            return this;
+        }
+
+        public Builder withAddress(InetAddress newAddress) {
+            this.address = newAddress;
+            return this;
+        }
+
+        public Builder withPort(final int newPort) {
+            this.port = newPort;
+            return this;
+        }
+
+        public Builder withTransportStackFactory(final SSHTransportStackFactory newTransportStackFactory) {
+            this.transportStackFactory = newTransportStackFactory;
+            return this;
+        }
+
+        public Builder withNegotiationFactory(final NetconfClientSessionNegotiatorFactory newNegotiationFactory) {
+            this.negotiationFactory = newNegotiationFactory;
+            return this;
+        }
+    }
+
+    private static TcpServerGrouping toServerParams(InetAddress address, int port) {
+        final var ipAddress = IetfInetUtil.ipAddressFor(
+            address == null ? InetAddress.getLoopbackAddress() : address);
+        final var portNumber = new PortNumber(Uint16.valueOf(port < 0 ? DEFAULT_PORT : port));
+        return new TcpServerParametersBuilder().setLocalAddress(ipAddress).setLocalPort(portNumber).build();
+    }
+
+    private static SSHTransportStackFactory defaultTransportStackFactory() {
+        return new SSHTransportStackFactory("ssh-call-home-server", 0);
+    }
+
+    private static NetconfClientSessionNegotiatorFactory defaultNegotiationFactory() {
+        return new NetconfClientSessionNegotiatorFactory(new HashedWheelTimer(),
+            Optional.empty(), DEFAULT_TIMEOUT_MILLIS, DEFAULT_CLIENT_CAPABILITIES);
+    }
+}
---
+++ netconf/callhome-server/src/main/java/org/opendaylight/netconf/callhome/server/tls/CallHomeTlsSessionContextManager.java
@@ -0,0 +1,93 @@
+/*
+ * Copyright (c) 2023 PANTHEON.tech s.r.o. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.netconf.callhome.server.tls;
+
+import static java.util.Objects.requireNonNull;
+
+import com.google.common.util.concurrent.SettableFuture;
+import io.netty.channel.Channel;
+import io.netty.handler.ssl.SslHandler;
+import java.security.PublicKey;
+import javax.net.ssl.SSLPeerUnverifiedException;
+import org.opendaylight.netconf.callhome.server.AbstractCallHomeSessionContextManager;
+import org.opendaylight.netconf.callhome.server.CallHomeStatusRecorder;
+import org.opendaylight.netconf.client.SimpleNetconfClientSessionListener;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CallHomeTlsSessionContextManager extends AbstractCallHomeSessionContextManager<CallHomeTlsSessionContext> {
+    private static final Logger LOG = LoggerFactory.getLogger(CallHomeTlsSessionContextManager.class);
+
+    private final CallHomeTlsAuthProvider authProvider;
+    private final CallHomeStatusRecorder statusRecorder;
+
+    public CallHomeTlsSessionContextManager(final CallHomeTlsAuthProvider authProvider,
+        final CallHomeStatusRecorder statusRecorder) {
+        super();
+        this.authProvider = requireNonNull(authProvider);
+        this.statusRecorder = requireNonNull(statusRecorder);
+    }
+
+    @Override
+    public CallHomeTlsSessionContext findByChannel(final Channel channel) {
+        requireNonNull(channel);
+        return channel.isOpen() ? createValidContext(channel) : null;
+    }
+
+    private CallHomeTlsSessionContext createValidContext(final Channel channel) {
+        // extract peer public key from SSL session
+        final PublicKey publicKey;
+        try {
+            final var cert = channel.pipeline().get(SslHandler.class).engine().getSession()
+                .getPeerCertificates()[0];
+            publicKey = cert.getPublicKey();
+        } catch (SSLPeerUnverifiedException e) {
+            LOG.error("Exception retrieving certificate", e);
+            return null;
+        }
+        // identify connection
+        final String id = authProvider.idFor(publicKey);
+        if (id == null) {
+            statusRecorder.reportUnknown(publicKey);
+            return null;
+        }
+        // ensure
+        final var context = createContext(id, channel);
+        if (context == null) {
+            // if there is an issue creating context then the cause expected to be
+            // logged within overridden createContext() method
+            return null;
+        }
+        if (!register(context)) {
+            LOG.error("The session context with same id {} already exists. Context omitted.", id);
+            return null;
+        }
+        // close callback
+        channel.closeFuture().addListener(ignored -> remove(id));
+
+        LOG.debug("Session context is created for TLS session: {}", context);
+        return context;
+    }
+
+    /**
+     * Builds {@link CallHomeTlsSessionContext} based on {@link Channel} object.
+     *
+     * <p> The method expected to be overridden in order to inject
+     * {@link org.opendaylight.netconf.client.NetconfClientSessionListener NetconfClientSessionListener} and/or
+     * {@link SettableFuture} of a {@link org.opendaylight.netconf.client.NetconfClientSession NetconfClientSession}
+     *  to be established on current connection.
+     *
+     * @param id unique client (device) identifier
+     * @param channel netty channel instance
+     * @return created object or {@code null} if it cannot be created for some reason
+     */
+    public CallHomeTlsSessionContext createContext(final String id, final Channel channel) {
+        return new CallHomeTlsSessionContext(id, channel, new SimpleNetconfClientSessionListener(),
+            SettableFuture.create());
+    }
+}
