2e4205f81b533d4461ae086bcf3da5cf96186683
Ruslan Kashapov <ruslan.kashapov@pantheon.tech>
2023-11-01 10:57:22 +0200

    Call-Home server implementation using transport-api
    New implementation for TLS and SSH Call-Home
    servers using transport-api targeted to replace
    legacy implementation -> callhome-protocol module
    
    JIRA: NETCONF-1183
    Change-Id: I326d0ff2ac6ff2ba8e228e5281759c7c9b8e0d74
    Signed-off-by: Ruslan Kashapov <ruslan.kashapov@pantheon.tech>

---
+++ netconf/callhome-server/src/main/java/org/opendaylight/netconf/callhome/server/CallHomeTransportChannelListener.java
@@ -0,0 +1,78 @@
+/*
+ * Copyright (c) 2023 PANTHEON.tech s.r.o. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.netconf.callhome.server;
+
+import io.netty.handler.ssl.SslHandler;
+import io.netty.util.concurrent.Promise;
+import org.eclipse.jdt.annotation.NonNull;
+import org.opendaylight.netconf.client.ClientChannelInitializer;
+import org.opendaylight.netconf.client.NetconfClientSession;
+import org.opendaylight.netconf.client.NetconfClientSessionNegotiatorFactory;
+import org.opendaylight.netconf.transport.api.TransportChannel;
+import org.opendaylight.netconf.transport.api.TransportChannelListener;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CallHomeTransportChannelListener implements TransportChannelListener {
+    private static final Logger LOG = LoggerFactory.getLogger(CallHomeTransportChannelListener.class);
+
+    final NetconfClientSessionNegotiatorFactory negotiationFactory;
+    final CallHomeSessionContextManager<?> contextManager;
+    final CallHomeStatusRecorder statusRecorder;
+
+    public CallHomeTransportChannelListener(final NetconfClientSessionNegotiatorFactory negotiationFactory,
+            final CallHomeSessionContextManager<?> contextManager, final CallHomeStatusRecorder statusRecorder) {
+        this.negotiationFactory = negotiationFactory;
+        this.contextManager = contextManager;
+        this.statusRecorder = statusRecorder;
+    }
+
+    @Override
+    public void onTransportChannelEstablished(@NonNull TransportChannel transportChannel) {
+        final var channel = transportChannel.channel();
+
+        // identify or create session context associated with current connection
+        final var context = contextManager.findByChannel(channel);
+        if (context == null) {
+            LOG.error("No valid context found for incoming connection from {}. Connection rejected.",
+                channel.remoteAddress());
+            channel.close();
+            return;
+        }
+
+        LOG.info("Starting netconf negotiation for context: {}", context);
+
+        // init NETCONF negotiation
+        final Promise<NetconfClientSession> promise = channel.eventLoop().newPromise();
+        promise.addListener(ignored -> {
+            final var cause = promise.cause();
+            if (cause != null) {
+                contextManager.remove(context.id());
+                context.settableFuture().setException(cause);
+                statusRecorder.reportNetconfFailure(context.id());
+                LOG.error("Netconf session failed for context: {}", context, cause);
+            } else {
+                statusRecorder.reportSuccess(context.id());
+                context.settableFuture().set(promise.getNow());
+                LOG.info("Netconf session established for context: {}", context);
+            }
+        });
+        new ClientChannelInitializer(negotiationFactory, context::netconfSessionListener)
+            .initialize(channel, promise);
+
+        // this is required to trigger NETCONF negotiation on TLS
+        if (channel.pipeline().get(SslHandler.class) != null) {
+            channel.pipeline().fireChannelActive();
+        }
+    }
+
+    @Override
+    public void onTransportChannelFailed(@NonNull Throwable cause) {
+        statusRecorder.onTransportChannelFailure(cause);
+    }
+}
