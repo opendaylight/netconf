a29c5e92a9ad787bcd322cac6569b52e0bbc5d13
Samuel Schneider <samuel.schneider@pantheon.tech>
2023-11-06 20:51:49 +0100

    Re-implement paths for RPCs
    Re-implement logic for generating paths for RPCs
    with new input streams approach.
    
    JIRA: NETCONF-938
    Change-Id: I4957d303148fcdffc0fb09b393209d72e9f7b63b
    Signed-off-by: Samuel Schneider <samuel.schneider@pantheon.tech>

--- restconf/restconf-openapi/src/main/java/org/opendaylight/restconf/openapi/model/PostEntity.java
+++ restconf/restconf-openapi/src/main/java/org/opendaylight/restconf/openapi/model/PostEntity.java
@@ -7,13 +7,28 @@
  */
 package org.opendaylight.restconf.openapi.model;
 
+import static javax.ws.rs.core.Response.Status.NO_CONTENT;
+import static javax.ws.rs.core.Response.Status.OK;
+import static org.opendaylight.restconf.openapi.impl.DefinitionGenerator.OUTPUT_SUFFIX;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import java.io.IOException;
 import javax.ws.rs.HttpMethod;
+import javax.ws.rs.core.MediaType;
+import org.eclipse.jdt.annotation.NonNull;
 import org.eclipse.jdt.annotation.Nullable;
 import org.opendaylight.yangtools.yang.model.api.OperationDefinition;
+import org.opendaylight.yangtools.yang.model.api.RpcDefinition;
 
 public final class PostEntity extends OperationEntity {
     private static final String SUMMARY_TEMPLATE = "%s - %s - %s - %s";
     private static final String INPUT_SUFFIX = "_input";
+    private static final String INPUT_KEY = "input";
+    private static final String OBJECT = "object";
+    private static final String SCHEMA = "schema";
+    private static final String TYPE = "type";
+    private static final String DESCRIPTION = "description";
+    private static final String COMPONENTS_PREFIX = "#/components/schemas/";
 
     public PostEntity(final OperationDefinition schema, final String deviceName, final String moduleName) {
         super(schema, deviceName, moduleName);
@@ -26,5 +41,96 @@ public final class PostEntity extends OperationEntity {
     @Nullable String summary() {
         final var operationName = schema().getQName().getLocalName() + INPUT_SUFFIX;
         return SUMMARY_TEMPLATE.formatted(HttpMethod.POST, deviceName(), moduleName(), operationName);
+    }
+
+    @Override
+    void generateResponses(final @NonNull JsonGenerator generator) throws IOException {
+        if (schema() instanceof RpcDefinition rpc) {
+            generator.writeObjectFieldStart("responses");
+            final var output = rpc.getOutput();
+            final var operationName = rpc.getQName().getLocalName();
+            if (!output.getChildNodes().isEmpty()) {
+                // TODO: add proper discriminator from DefinitionNames when schemas re-implementation is done
+                final var ref = COMPONENTS_PREFIX + moduleName() + "_" + operationName + OUTPUT_SUFFIX;
+                generator.writeObjectFieldStart(String.valueOf(OK.getStatusCode()));
+                generator.writeStringField(DESCRIPTION, String.format("RPC %s success", operationName));
+
+                generator.writeObjectFieldStart("content");
+                generateMediaTypeSchemaRef(generator, MediaType.APPLICATION_JSON, ref);
+                generateMediaTypeSchemaRef(generator, MediaType.APPLICATION_XML, ref);
+                generator.writeEndObject();
+
+                generator.writeEndObject();
+
+            } else {
+                generator.writeObjectFieldStart(String.valueOf(NO_CONTENT.getStatusCode()));
+                generator.writeStringField(DESCRIPTION, String.format("RPC %s success", operationName));
+                generator.writeEndObject();
+
+            }
+            generator.writeEndObject();
+        }
+    }
+
+    @Override
+    void generateRequestBody(final @NonNull JsonGenerator generator) throws IOException {
+        if (schema() instanceof RpcDefinition rpc) {
+            generator.writeObjectFieldStart("requestBody");
+            final var input = rpc.getInput();
+            final var operationName = rpc.getQName().getLocalName();
+            generator.writeStringField(DESCRIPTION, operationName + INPUT_SUFFIX);
+            generator.writeObjectFieldStart("content");
+            if (!input.getChildNodes().isEmpty()) {
+                // TODO: add proper discriminator from DefinitionNames when schemas re-implementation is done
+                final var ref = COMPONENTS_PREFIX + moduleName() + "_" + operationName + INPUT_SUFFIX;
+                generator.writeObjectFieldStart(MediaType.APPLICATION_JSON);
+                generator.writeObjectFieldStart(SCHEMA);
+                generator.writeObjectFieldStart("properties");
+                generator.writeObjectFieldStart(INPUT_KEY);
+                generator.writeStringField("$ref", ref);
+                generator.writeStringField(TYPE, OBJECT);
+                generator.writeEndObject();
+                generator.writeEndObject();
+                generator.writeEndObject();
+                generator.writeEndObject();
+                generateMediaTypeSchemaRef(generator, MediaType.APPLICATION_XML, ref);
+            } else {
+                generator.writeObjectFieldStart(MediaType.APPLICATION_JSON);
+                generator.writeObjectFieldStart(SCHEMA);
+
+                generator.writeObjectFieldStart("properties");
+                generator.writeObjectFieldStart(INPUT_KEY);
+                generator.writeStringField(TYPE, OBJECT);
+                generator.writeEndObject();
+                generator.writeEndObject();
+
+                generator.writeStringField(TYPE, OBJECT);
+                generator.writeEndObject();
+                generator.writeEndObject();
+
+                generator.writeObjectFieldStart(MediaType.APPLICATION_XML);
+                generator.writeObjectFieldStart(SCHEMA);
+
+                generator.writeObjectFieldStart("xml");
+                generator.writeStringField("name", INPUT_KEY);
+                generator.writeStringField("namespace", input.getQName().getNamespace().toString());
+                generator.writeEndObject();
+
+                generator.writeStringField(TYPE, OBJECT);
+                generator.writeEndObject();
+                generator.writeEndObject();
+            }
+            generator.writeEndObject();
+            generator.writeEndObject();
+        }
+    }
+
+    private static void generateMediaTypeSchemaRef(final JsonGenerator generator, final String mediaType,
+            final String ref) throws IOException {
+        generator.writeObjectFieldStart(mediaType);
+        generator.writeObjectFieldStart(SCHEMA);
+        generator.writeStringField("$ref", ref);
+        generator.writeEndObject();
+        generator.writeEndObject();
     }
 }
