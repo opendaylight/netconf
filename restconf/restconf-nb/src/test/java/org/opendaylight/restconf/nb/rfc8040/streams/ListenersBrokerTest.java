8a0e893f279bb95a8a7bc55ec2ce8948719ad014
Robert Varga <robert.varga@pantheon.tech>
2023-11-07 20:17:08 +0100

    Populate restconf-state/streams
    ListenersBroker needs to manage restconf-state/streams, so users
    can locate the endpoint servicing each stream.
    
    Pick up the required pieces from RestconfStateStreams and surrounding
    commented-out code to generate proper entries.
    
    Also clean up tests, so that we have nice and crispy test suite, without
    duplications and asserting proper changes.
    
    JIRA: NETCONF-1102
    Change-Id: Ifddb9fba119e6580ac9cb1c6377b63285d8d6cb0
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- restconf/restconf-nb/src/test/java/org/opendaylight/restconf/nb/rfc8040/streams/ListenersBrokerTest.java
+++ restconf/restconf-nb/src/test/java/org/opendaylight/restconf/nb/rfc8040/streams/ListenersBrokerTest.java
@@ -13,25 +13,40 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertInstanceOf;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.doReturn;
 
+import java.net.URI;
 import java.util.UUID;
+import javax.ws.rs.core.UriBuilder;
+import javax.ws.rs.core.UriInfo;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.opendaylight.mdsal.common.api.CommitInfo;
+import org.opendaylight.mdsal.common.api.LogicalDatastoreType;
 import org.opendaylight.mdsal.dom.api.DOMDataBroker;
+import org.opendaylight.mdsal.dom.api.DOMDataTreeWriteTransaction;
 import org.opendaylight.restconf.common.errors.RestconfDocumentedException;
 import org.opendaylight.restconf.nb.rfc8040.databind.DatabindContext;
 import org.opendaylight.restconf.nb.rfc8040.databind.DatabindProvider;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.restconf.monitoring.rev170126.restconf.state.streams.stream.Access;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.controller.md.sal.remote.rev140114.CreateDataChangeEventSubscriptionOutput;
 import org.opendaylight.yangtools.yang.common.ErrorTag;
 import org.opendaylight.yangtools.yang.common.ErrorType;
 import org.opendaylight.yangtools.yang.common.QName;
 import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
 import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.NodeIdentifier;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.NodeIdentifierWithPredicates;
 import org.opendaylight.yangtools.yang.data.api.schema.ContainerNode;
 import org.opendaylight.yangtools.yang.data.api.schema.LeafNode;
+import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNode;
 import org.opendaylight.yangtools.yang.data.impl.schema.Builders;
 import org.opendaylight.yangtools.yang.data.impl.schema.ImmutableNodes;
 import org.opendaylight.yangtools.yang.model.api.ContainerLike;
@@ -46,6 +61,18 @@ class ListenersBrokerTest {
 
     @Mock
     private DOMDataBroker dataBroker;
+    @Mock
+    private DOMDataTreeWriteTransaction tx;
+    @Mock
+    private UriInfo uriInfo;
+    @Mock
+    private UriBuilder uriBuilder;
+    @Captor
+    private ArgumentCaptor<String> uriCaptor;
+    @Captor
+    private ArgumentCaptor<YangInstanceIdentifier> pathCaptor;
+    @Captor
+    private ArgumentCaptor<NormalizedNode> dataCaptor;
 
     private ListenersBroker listenersBroker;
     private DatabindProvider databindProvider;
@@ -58,8 +85,16 @@ class ListenersBrokerTest {
 
     @Test
     void createStreamTest() {
+        doReturn(tx).when(dataBroker).newWriteOnlyTransaction();
+        doNothing().when(tx).put(eq(LogicalDatastoreType.OPERATIONAL), pathCaptor.capture(), dataCaptor.capture());
+        doReturn(CommitInfo.emptyFluentFuture()).when(tx).commit();
+
+        doReturn(uriBuilder).when(uriInfo).getBaseUriBuilder();
+        doReturn(uriBuilder).when(uriBuilder).replacePath(uriCaptor.capture());
+        doAnswer(inv -> new URI(uriCaptor.getValue())).when(uriBuilder).build();
+
         final var output = assertInstanceOf(ContainerNode.class, listenersBroker.createDataChangeNotifiStream(
-            databindProvider, prepareDomPayload("create-data-change-event-subscription", "toaster", "path"),
+            databindProvider, uriInfo, prepareDomPayload("create-data-change-event-subscription", "toaster", "path"),
             SCHEMA_CTX).getOrThrow().orElse(null));
 
         assertEquals(new NodeIdentifier(CreateDataChangeEventSubscriptionOutput.QNAME), output.name());
@@ -72,13 +107,41 @@ class ListenersBrokerTest {
         assertEquals(45, name.length());
         assertThat(name, startsWith("urn:uuid:"));
         assertNotNull(UUID.fromString(name.substring(9)));
+
+        final var rcStream = QName.create("urn:ietf:params:xml:ns:yang:ietf-restconf-monitoring", "2017-01-26",
+            "stream");
+        final var rcName = QName.create(rcStream, "name");
+        final var streamId = NodeIdentifierWithPredicates.of(rcStream, rcName, name);
+        final var rcEncoding = QName.create(rcStream, "encoding");
+
+        assertEquals(YangInstanceIdentifier.of(
+            new NodeIdentifier(QName.create(rcStream, "restconf-state")),
+            new NodeIdentifier(QName.create(rcStream, "streams")),
+            new NodeIdentifier(rcStream),
+            streamId), pathCaptor.getValue());
+        assertEquals(Builders.mapEntryBuilder()
+            .withNodeIdentifier(streamId)
+            .withChild(ImmutableNodes.leafNode(rcName, name))
+            .withChild(ImmutableNodes.leafNode(QName.create(rcStream, "description"),
+                "Events occuring in CONFIGURATION datastore under /toaster:toaster"))
+            .withChild(Builders.mapBuilder()
+                .withNodeIdentifier(new NodeIdentifier(Access.QNAME))
+                .withChild(Builders.mapEntryBuilder()
+                    .withNodeIdentifier(NodeIdentifierWithPredicates.of(Access.QNAME, rcEncoding, ""))
+                    .withChild(ImmutableNodes.leafNode(rcEncoding, ""))
+                    .withChild(ImmutableNodes.leafNode(QName.create(rcStream, "location"),
+                        "rests/streams/" + name))
+                    .build())
+                .build())
+            .build(), dataCaptor.getValue());
     }
 
     @Test
     void createStreamWrongValueTest() {
         final var payload = prepareDomPayload("create-data-change-event-subscription", "String value", "path");
         final var errors = assertThrows(RestconfDocumentedException.class,
-            () -> listenersBroker.createDataChangeNotifiStream(databindProvider, payload, SCHEMA_CTX)).getErrors();
+            () -> listenersBroker.createDataChangeNotifiStream(databindProvider, uriInfo, payload, SCHEMA_CTX))
+            .getErrors();
         assertEquals(1, errors.size());
         final var error = errors.get(0);
         assertEquals(ErrorType.APPLICATION, error.getErrorType());
@@ -90,7 +153,8 @@ class ListenersBrokerTest {
     void createStreamWrongInputRpcTest() {
         final var payload = prepareDomPayload("create-data-change-event-subscription2", "toaster", "path2");
         final var errors = assertThrows(RestconfDocumentedException.class,
-            () -> listenersBroker.createDataChangeNotifiStream(databindProvider, payload, SCHEMA_CTX)).getErrors();
+            () -> listenersBroker.createDataChangeNotifiStream(databindProvider, uriInfo, payload, SCHEMA_CTX))
+            .getErrors();
         assertEquals(1, errors.size());
         final var error = errors.get(0);
         assertEquals(ErrorType.APPLICATION, error.getErrorType());
