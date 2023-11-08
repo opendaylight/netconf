73f53916ce9aabbdf8b9d9c62bb2f939ec9735a0
Robert Varga <robert.varga@pantheon.tech>
2023-11-08 01:52:48 +0100

    Refactor EventFormatter interface
    We have some amount of method duplication and tangling. Move methods
    around and clean them up.
    
    JIRA: NETCONF-1102
    Change-Id: Ica1af471d1c035142a77bfba14d94ebde624438a
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- restconf/restconf-nb/src/main/java/org/opendaylight/restconf/nb/rfc8040/streams/EventFormatter.java
+++ restconf/restconf-nb/src/main/java/org/opendaylight/restconf/nb/rfc8040/streams/EventFormatter.java
@@ -10,6 +10,7 @@ package org.opendaylight.restconf.nb.rfc8040.streams;
 import static java.util.Objects.requireNonNull;
 
 import java.io.IOException;
+import java.io.Writer;
 import java.time.Instant;
 import java.time.OffsetDateTime;
 import java.time.ZoneId;
@@ -18,6 +19,8 @@ import javax.xml.XMLConstants;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.stream.XMLOutputFactory;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamWriter;
 import javax.xml.xpath.XPath;
 import javax.xml.xpath.XPathConstants;
 import javax.xml.xpath.XPathExpression;
@@ -25,9 +28,14 @@ import javax.xml.xpath.XPathExpressionException;
 import javax.xml.xpath.XPathFactory;
 import org.eclipse.jdt.annotation.NonNull;
 import org.eclipse.jdt.annotation.Nullable;
+import org.opendaylight.netconf.api.NamespaceURN;
 import org.opendaylight.yangtools.concepts.Immutable;
+import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNode;
+import org.opendaylight.yangtools.yang.data.api.schema.stream.NormalizedNodeStreamWriter;
+import org.opendaylight.yangtools.yang.data.api.schema.stream.NormalizedNodeWriter;
 import org.opendaylight.yangtools.yang.model.api.EffectiveModelContext;
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 
 abstract class EventFormatter<T> implements Immutable {
     private static final XPathFactory XPF = XPathFactory.newInstance();
@@ -134,7 +142,35 @@ abstract class EventFormatter<T> implements Immutable {
      * @param now time stamp
      * @return Data specified by RFC3339.
      */
-    static String toRFC3339(final Instant now) {
+    static final String toRFC3339(final Instant now) {
         return DateTimeFormatter.ISO_OFFSET_DATE_TIME.format(OffsetDateTime.ofInstant(now, ZoneId.systemDefault()));
+    }
+
+    static final @NonNull Element createNotificationElement(final Document doc, final Instant now) {
+        final var notificationElement = doc.createElementNS(NamespaceURN.NOTIFICATION, "notification");
+        final var eventTimeElement = doc.createElement("eventTime");
+        eventTimeElement.setTextContent(toRFC3339(now));
+        notificationElement.appendChild(eventTimeElement);
+        return notificationElement;
+    }
+
+    static final @NonNull XMLStreamWriter createStreamWriterWithNotification(final Writer writer, final Instant now)
+            throws XMLStreamException {
+        final var xmlStreamWriter = XML_OUTPUT_FACTORY.createXMLStreamWriter(writer);
+        xmlStreamWriter.setDefaultNamespace(NamespaceURN.NOTIFICATION);
+
+        xmlStreamWriter.writeStartElement(NamespaceURN.NOTIFICATION, "notification");
+        xmlStreamWriter.writeDefaultNamespace(NamespaceURN.NOTIFICATION);
+
+        xmlStreamWriter.writeStartElement("eventTime");
+        xmlStreamWriter.writeCharacters(toRFC3339(now));
+        xmlStreamWriter.writeEndElement();
+        return xmlStreamWriter;
+    }
+
+    static final void writeBody(final NormalizedNodeStreamWriter writer, final NormalizedNode body) throws IOException {
+        try (var nodeWriter = NormalizedNodeWriter.forStreamWriter(writer)) {
+            nodeWriter.write(body);
+        }
     }
 }
