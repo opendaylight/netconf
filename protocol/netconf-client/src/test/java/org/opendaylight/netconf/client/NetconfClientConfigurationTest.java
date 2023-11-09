4050078dfb49c71c429d090c72b69ed270fdb433
Ruslan Kashapov <ruslan.kashapov@pantheon.tech>
2023-11-09 13:08:22 +0200

    Netconf client configuration cleanup
    Legacy attributes and related obsolete artifacts removed.
    
    JIRA: NETCONF-1108
    Change-Id: Ibf317c2bffb83793603eea343463a74fe37084d0
    Signed-off-by: Ruslan Kashapov <ruslan.kashapov@pantheon.tech>
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- protocol/netconf-client/src/test/java/org/opendaylight/netconf/client/NetconfClientConfigurationTest.java
+++ protocol/netconf-client/src/test/java/org/opendaylight/netconf/client/NetconfClientConfigurationTest.java
@@ -7,55 +7,125 @@
  */
 package org.opendaylight.netconf.client;
 
-import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.mock;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.opendaylight.netconf.client.conf.NetconfClientConfiguration.NetconfClientProtocol.SSH;
+import static org.opendaylight.netconf.client.conf.NetconfClientConfiguration.NetconfClientProtocol.TCP;
+import static org.opendaylight.netconf.client.conf.NetconfClientConfiguration.NetconfClientProtocol.TLS;
 
-import java.net.InetSocketAddress;
-import java.util.Optional;
-import org.junit.Test;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.opendaylight.netconf.api.messages.NetconfHelloMessageAdditionalHeader;
 import org.opendaylight.netconf.client.conf.NetconfClientConfiguration;
+import org.opendaylight.netconf.client.conf.NetconfClientConfiguration.NetconfClientProtocol;
 import org.opendaylight.netconf.client.conf.NetconfClientConfigurationBuilder;
-import org.opendaylight.netconf.nettyutil.handler.ssh.authentication.AuthenticationHandler;
+import org.opendaylight.netconf.transport.ssh.ClientFactoryManagerConfigurator;
+import org.opendaylight.netconf.transport.tls.SslHandlerFactory;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev130715.Uri;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.ssh.client.rev230417.SshClientGrouping;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.tcp.client.rev230417.TcpClientGrouping;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.tls.client.rev230417.TlsClientGrouping;
 
-public class NetconfClientConfigurationTest {
+@ExtendWith(MockitoExtension.class)
+class NetconfClientConfigurationTest {
+    private static final String NAME = "name";
+    private static final long TIMEOUT_MILLIS = 123456L;
+    private static final int MAX_CHUNK_SIZE = 6789;
+    private static final List<Uri> CAPABILITIES = List.of(new Uri("test::uri"));
+    private static final Class<? extends Throwable> NPE = NullPointerException.class;
+    private static final Class<? extends Throwable> IAE = IllegalArgumentException.class;
+
+    @Mock
+    private TcpClientGrouping tcpParams;
+    @Mock
+    private TlsClientGrouping tlsParams;
+    @Mock
+    private SslHandlerFactory sslHandlerFactory;
+    @Mock
+    private SshClientGrouping sshParams;
+    @Mock
+    private ClientFactoryManagerConfigurator sshConfigurator;
+    @Mock
+    private NetconfHelloMessageAdditionalHeader additionalHeader;
+    @Mock
+    private NetconfClientSessionListener sessionListener;
+
     @Test
-    public void testNetconfClientConfiguration() throws Exception {
-        Long timeout = 200L;
-        NetconfHelloMessageAdditionalHeader header =
-                new NetconfHelloMessageAdditionalHeader("a", "host", "port", "trans", "id");
-        NetconfClientSessionListener listener = new SimpleNetconfClientSessionListener();
-        InetSocketAddress address = InetSocketAddress.createUnresolved("host", 830);
-        AuthenticationHandler handler = mock(AuthenticationHandler.class);
-        NetconfClientConfiguration cfg = NetconfClientConfigurationBuilder.create()
-                .withProtocol(NetconfClientConfiguration.NetconfClientProtocol.SSH)
-                .withAddress(address)
-                .withConnectionTimeoutMillis(timeout)
-                .withAdditionalHeader(header)
-                .withSessionListener(listener)
-                .withAuthHandler(handler).build();
+    void tcpClientConfiguration() {
+        final var cfg = baseBuilder(TCP).build();
+        assertBaseConf(cfg, TCP);
+    }
 
-        assertEquals(timeout, cfg.getConnectionTimeoutMillis());
-        assertEquals(Optional.of(header), cfg.getAdditionalHeader());
-        assertEquals(listener, cfg.getSessionListener());
-        assertEquals(handler, cfg.getAuthHandler());
-        assertEquals(NetconfClientConfiguration.NetconfClientProtocol.SSH, cfg.getProtocol());
-        assertEquals(address, cfg.getAddress());
+    @Test
+    void tlsClientConfig() {
+        final var cfg1 = baseBuilder(TLS).withTlsParameters(tlsParams).build();
+        assertBaseConf(cfg1, TLS);
+        assertSame(tlsParams, cfg1.getTlsParameters());
 
-        SslHandlerFactory sslHandlerFactory = mock(SslHandlerFactory.class);
-        NetconfClientConfiguration cfg2 = NetconfClientConfigurationBuilder.create()
-                .withProtocol(NetconfClientConfiguration.NetconfClientProtocol.TLS)
-                .withAddress(address)
-                .withConnectionTimeoutMillis(timeout)
-                .withAdditionalHeader(header)
-                .withSessionListener(listener)
-                .withSslHandlerFactory(sslHandlerFactory).build();
+        final var cfg2 = baseBuilder(TLS).withSslHandlerFactory(sslHandlerFactory).build();
+        assertBaseConf(cfg2, TLS);
+        assertSame(sslHandlerFactory, cfg2.getSslHandlerFactory());
+    }
 
-        assertEquals(timeout, cfg2.getConnectionTimeoutMillis());
-        assertEquals(Optional.of(header), cfg2.getAdditionalHeader());
-        assertEquals(listener, cfg2.getSessionListener());
-        assertEquals(sslHandlerFactory, cfg2.getSslHandlerFactory());
-        assertEquals(NetconfClientConfiguration.NetconfClientProtocol.TLS, cfg2.getProtocol());
-        assertEquals(address, cfg2.getAddress());
+    @Test
+    void sshClientConfig() {
+        final var cfg = baseBuilder(SSH).withSshParameters(sshParams)
+            .withSshConfigurator(sshConfigurator).build();
+        assertBaseConf(cfg, SSH);
+        assertSame(sshParams, cfg.getSshParameters());
+        assertSame(sshConfigurator, cfg.getSshConfigurator());
+    }
+
+    @ParameterizedTest(name = "Invalid configuration: {0}")
+    @MethodSource
+    void invalidConfig(final String testDesc, final NetconfClientProtocol protocol,
+            final BuilderModification modification, final Class<? extends Throwable> expected) {
+        assertThrows(expected, () -> modification.modify(baseBuilder(protocol)).build());
+    }
+
+    private static List<Arguments> invalidConfig() {
+        return List.of(
+            Arguments.of("No Protocol", TCP, (BuilderModification) builder -> builder.withProtocol(null), NPE),
+            Arguments.of("No Netconf Session Listener",
+                TCP, (BuilderModification) builder -> builder.withSessionListener(null), NPE),
+            Arguments.of("No TCP params", TCP, (BuilderModification) builder -> builder.withTcpParameters(null), NPE),
+            Arguments.of("No TLS params", TLS, (BuilderModification) builder -> builder, IAE),
+            Arguments.of("No SSH params", SSH, (BuilderModification) builder -> builder, NPE)
+        );
+    }
+
+    private NetconfClientConfigurationBuilder baseBuilder(final NetconfClientProtocol protocol) {
+        return NetconfClientConfigurationBuilder.create()
+            .withProtocol(protocol)
+            .withTcpParameters(tcpParams)
+            .withName(NAME)
+            .withAdditionalHeader(additionalHeader)
+            .withSessionListener(sessionListener)
+            .withOdlHelloCapabilities(CAPABILITIES)
+            .withConnectionTimeoutMillis(TIMEOUT_MILLIS)
+            .withMaximumIncomingChunkSize(MAX_CHUNK_SIZE);
+    }
+
+    private void assertBaseConf(final NetconfClientConfiguration cfg, final NetconfClientProtocol protocol) {
+        assertEquals(protocol, cfg.getProtocol());
+        assertSame(tcpParams, cfg.getTcpParameters());
+        assertEquals(NAME, cfg.getName());
+        assertSame(additionalHeader, cfg.getAdditionalHeader().orElseThrow());
+        assertSame(sessionListener, cfg.getSessionListener());
+        assertEquals(CAPABILITIES, cfg.getOdlHelloCapabilities());
+        assertEquals(TIMEOUT_MILLIS, cfg.getConnectionTimeoutMillis());
+        assertEquals(MAX_CHUNK_SIZE, cfg.getMaximumIncomingChunkSize());
+    }
+
+    @FunctionalInterface
+    private interface BuilderModification {
+        NetconfClientConfigurationBuilder modify(NetconfClientConfigurationBuilder builder);
     }
 }
