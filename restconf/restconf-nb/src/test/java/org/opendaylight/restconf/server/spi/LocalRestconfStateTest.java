e50568b067083f863aefc064212a415070b46966
Robert Varga <robert.varga@pantheon.tech>
2023-11-10 07:46:36 +0100

    WIP: Add LocalRestconfStreamRegistry
    // FIXME: wire to dataservice and eliminate
    CapabilitiesWriter/MdsalRestconfStreamRegistry.
    
    JIRA: NETCONF-773
    Change-Id: Id54ef110374f9efed7d41587f5cd7f87f0777d41
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- restconf/restconf-nb/src/test/java/org/opendaylight/restconf/server/spi/LocalRestconfStateTest.java
+++ restconf/restconf-nb/src/test/java/org/opendaylight/restconf/server/spi/LocalRestconfStateTest.java
@@ -5,19 +5,21 @@
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
-package org.opendaylight.restconf.server.mdsal;
+package org.opendaylight.restconf.server.spi;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.Set;
 import org.junit.jupiter.api.Test;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.restconf.monitoring.rev170126.RestconfState;
+import org.opendaylight.yangtools.yang.common.QName;
 import org.opendaylight.yangtools.yang.data.api.schema.LeafSetEntryNode;
 
-class CapabilitiesWriterTest {
+class LocalRestconfStateTest {
     @Test
     void restconfStateCapabilitiesTest() {
-        final var capability = CapabilitiesWriter.mapCapabilities();
-        assertEquals(CapabilitiesWriter.CAPABILITY, capability.name());
+        final var capability = LocalRestconfState.CAPABILITY;
+        assertEquals(QName.create(RestconfState.QNAME, "capability"), capability.name().getNodeType());
 
         final var entries = capability.body().stream().map(LeafSetEntryNode::body).toList();
         final var unique = Set.copyOf(entries);
