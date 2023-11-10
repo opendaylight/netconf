1fc1271a07ebef2ab918551eacb3dd23205ff508
Matej Sramcik <matej.sramcik@pantheon.tech>
2023-11-10 12:06:16 +0100

    Re-implement components/schemas for RPCs
    Re-implement logic for generating components/schemas for RPCs
    with new input streams approach.
    
    Change-Id: I9b42659155d16e3efd316608e436c97a2becd8bc
    Signed-off-by: Matej Sramcik <matej.sramcik@pantheon.tech>

---
+++ restconf/restconf-openapi/src/main/java/org/opendaylight/restconf/openapi/model/TypeDef.java
@@ -0,0 +1,124 @@
+/*
+ * Copyright (c) 2023 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.restconf.openapi.model;
+
+import java.util.List;
+
+public class TypeDef {
+    private Object defaultValue;
+    private Object example;
+    private String format;
+    private Integer minItems;
+    private Boolean uniqueItems;
+    private List<String> enums;
+    private Integer minLength;
+    private Integer maxLength ;
+
+    public Object getDefaultValue() {
+        return defaultValue;
+    }
+
+    public void setDefaultValue(Object defaultValue) {
+        this.defaultValue = defaultValue;
+    }
+
+    public boolean hasDefaultValue() {
+        return defaultValue != null;
+    }
+
+
+    public Object getExample() {
+        return example;
+    }
+
+    public void setExample(Object example) {
+        this.example = example;
+    }
+
+    public boolean hasExample() {
+        return example != null;
+    }
+
+
+    public String getFormat() {
+        return format;
+    }
+
+    public void setFormat(String format) {
+        this.format = format;
+    }
+
+    public boolean hasFormat() {
+        return format != null;
+    }
+
+
+    public Integer getMinItems() {
+        return minItems;
+    }
+
+    public void setMinItems(Integer minItems) {
+        this.minItems = minItems;
+    }
+
+    public boolean hasMinItems() {
+        return minItems != null;
+    }
+
+
+    public Boolean getUniqueItems() {
+        return uniqueItems;
+    }
+
+    public void setUniqueItems(Boolean uniqueItems) {
+        this.uniqueItems = uniqueItems;
+    }
+
+    public boolean hasUniqueItems() {
+        return uniqueItems != null;
+    }
+
+
+    public List<String> getEnums() {
+        return enums;
+    }
+
+    public void setEnums(List<String> enums) {
+        this.enums = enums;
+    }
+
+    public boolean hasEnums() {
+        return enums != null;
+    }
+
+
+    public Integer getMinLength() {
+        return minLength;
+    }
+
+    public void setMinLength(Integer minLength) {
+        this.minLength = minLength;
+    }
+
+    public boolean hasMinLength() {
+        return minLength != null;
+    }
+
+
+    public Integer getMaxLength() {
+        return maxLength;
+    }
+
+    public void setMaxLength(Integer maxLength) {
+        this.maxLength = maxLength;
+    }
+
+    public boolean hasMaxLength() {
+        return maxLength != null;
+    }
+}
