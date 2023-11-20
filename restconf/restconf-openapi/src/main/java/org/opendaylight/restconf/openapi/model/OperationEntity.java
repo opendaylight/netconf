dbf6d1f64a857bac9c4b9b682e6f5c6bd62b6836
lubos-cicut <lubos.cicut@pantheon.tech>
2023-11-20 18:17:27 +0100

    Re-implement paths for ChildNodes
    Re-implement logic for generating paths for child nodes
    with new input streams approach.
    
    JIRA: NETCONF-938
    Change-Id: I38bda3346e8ec49751a2ac511ad8bd790a6d27cd
    Signed-off-by: lubos-cicut <lubos.cicut@pantheon.tech>

--- restconf/restconf-openapi/src/main/java/org/opendaylight/restconf/openapi/model/OperationEntity.java
+++ restconf/restconf-openapi/src/main/java/org/opendaylight/restconf/openapi/model/OperationEntity.java
@@ -7,21 +7,47 @@
  */
 package org.opendaylight.restconf.openapi.model;
 
+import static javax.ws.rs.core.Response.Status.OK;
+
 import com.fasterxml.jackson.core.JsonGenerator;
 import java.io.IOException;
+import java.util.List;
+import javax.ws.rs.HttpMethod;
 import org.eclipse.jdt.annotation.NonNull;
 import org.eclipse.jdt.annotation.Nullable;
-import org.opendaylight.yangtools.yang.model.api.OperationDefinition;
+import org.opendaylight.yangtools.yang.model.api.SchemaNode;
 
 /**
  * Archetype for an Operation.
  */
-public abstract sealed class OperationEntity extends OpenApiEntity permits PostEntity {
-    private final OperationDefinition schema;
+public abstract sealed class OperationEntity extends OpenApiEntity permits DeleteEntity, GetEntity, PatchEntity,
+        PostEntity, PutEntity {
+    protected static final String SCHEMA = "schema";
+    protected static final String SUMMARY_TEMPLATE = "%s - %s - %s - %s";
+    protected static final String RESPONSES = "responses";
+    protected static final String DESCRIPTION = "description";
+    protected static final String OBJECT = "object";
+    protected static final String CONTENT = "content";
+    protected static final String COMPONENTS_PREFIX = "#/components/schemas/";
+    protected static final String PROPERTIES = "properties";
+    protected static final String TYPE = "type";
+    protected static final String ARRAY = "array";
+    protected static final String ITEMS = "items";
+    protected static final String REF = "$ref";
+    protected static final String PARAMETERS = "parameters";
+    protected static final String SUMMARY = "summary";
+    protected static final String NAME = "name";
+    protected static final String IN = "in";
+    protected static final String REQUIRED = "required";
+    protected static final String REQUEST_BODY = "requestBody";
+
+    private final SchemaNode schema;
     private final String deviceName;
     private final String moduleName;
+    private final String refPath;
+    private final List<ParameterEntity> parameters;
 
-    protected OperationDefinition schema() {
+    protected SchemaNode schema() {
         return schema;
     }
 
@@ -33,32 +59,47 @@ public abstract sealed class OperationEntity extends OpenApiEntity permits PostE
         return moduleName;
     }
 
-    public OperationEntity(final OperationDefinition schema, final String deviceName, final String moduleName) {
+    protected List<ParameterEntity> parameters() {
+        return parameters;
+    }
+
+    protected String refPath() {
+        return refPath;
+    }
+
+    public OperationEntity(final SchemaNode schema, final String deviceName, final String moduleName,
+            final List<ParameterEntity> parameters, final String refPath) {
         this.schema = schema;
         this.deviceName = deviceName;
         this.moduleName = moduleName;
+        this.parameters = parameters;
+        this.refPath = refPath;
     }
 
     @Override
     public void generate(@NonNull JsonGenerator generator) throws IOException {
-        generator.writeObjectFieldStart(operation());
-        final var deprecated = deprecated();
-        if (deprecated != null) {
-            generator.writeBooleanField("deprecated", deprecated);
+        if (schema() == null) {
+            generateGetRoot(generator, moduleName());
+        } else {
+            generator.writeObjectFieldStart(operation());
+            generateBasics(generator);
+            generateRequestBody(generator);
+            generateResponses(generator);
+            generateTags(generator);
+            generateParams(generator);
+            generator.writeEndObject();
         }
+    }
+
+    public void generateBasics(@NonNull JsonGenerator generator) throws IOException {
         final var description = description();
         if (description != null) {
-            generator.writeStringField("description", description);
+            generator.writeStringField(DESCRIPTION, description);
         }
         final var summary = summary();
         if (summary != null) {
-            generator.writeStringField("summary", summary);
+            generator.writeStringField(SUMMARY, summary);
         }
-        generateRequestBody(generator);
-        generateResponses(generator);
-        generateTags(generator);
-        generateParams(generator);
-        generator.writeEndObject();
     }
 
     protected abstract String operation();
@@ -69,6 +110,13 @@ public abstract sealed class OperationEntity extends OpenApiEntity permits PostE
 
     @Nullable String description() {
         return schema.getDescription().orElse("");
+    }
+
+    @Nullable String nodeName() {
+        if (schema() != null) {
+            return schema().getQName().getLocalName();
+        }
+        return null;
     }
 
     @Nullable abstract String summary();
@@ -88,8 +136,54 @@ public abstract sealed class OperationEntity extends OpenApiEntity permits PostE
     }
 
     void generateParams(final @NonNull JsonGenerator generator) throws IOException {
-        generator.writeArrayFieldStart("parameters");
-        // need empty array here
-        generator.writeEndArray();
+        generator.writeArrayFieldStart(PARAMETERS);
+        if (!parameters().isEmpty()) {
+            for (final var parameter : parameters()) {
+                generator.writeStartObject();
+                generator.writeStringField(NAME, parameter.name());
+                generator.writeStringField(IN, parameter.in());
+                generator.writeBooleanField(REQUIRED, parameter.required());
+                generator.writeObjectFieldStart(SCHEMA);
+                generator.writeStringField(TYPE, parameter.schema().type());
+                generator.writeEndObject(); //end of schema
+                if (parameter.description() != null) {
+                    generator.writeStringField(DESCRIPTION, parameter.description());
+                }
+                generator.writeEndObject(); //end of parameter
+            }
+        }
+        generator.writeEndArray(); //end of params
+    }
+
+
+    protected static void generateMediaTypeSchemaRef(final JsonGenerator generator, final String mediaType,
+            final String ref) throws IOException {
+        generator.writeObjectFieldStart(mediaType);
+        generator.writeObjectFieldStart(SCHEMA);
+        generator.writeStringField(REF, ref);
+        generator.writeEndObject();
+        generator.writeEndObject();
+    }
+
+    void generateGetRoot(final JsonGenerator generator, final String resourceType)
+            throws IOException {
+        generator.writeObjectFieldStart("get");
+        if (resourceType.equals("data")) {
+            generator.writeStringField(DESCRIPTION, "Queries the config (startup) datastore on the mounted hosted.");
+        } else if (resourceType.equals("operations")) {
+            generator.writeStringField(DESCRIPTION,
+                "Queries the available operations (RPC calls) on the mounted hosted.");
+        }
+        generator.writeObjectFieldStart(RESPONSES);
+        generator.writeObjectFieldStart(String.valueOf(OK.getStatusCode()));
+        generator.writeStringField(DESCRIPTION, "OK");
+        generator.writeEndObject(); //end of 200
+        generator.writeEndObject(); // end of responses
+        final var summary = HttpMethod.GET + " - " + deviceName() + " - datastore - " + resourceType;
+        generator.writeStringField(SUMMARY, summary);
+        generator.writeArrayFieldStart("tags");
+        generator.writeString(deviceName + " GET root");
+        generator.writeEndArray(); //end of tags
+        generator.writeEndObject(); //end of get
     }
 }
