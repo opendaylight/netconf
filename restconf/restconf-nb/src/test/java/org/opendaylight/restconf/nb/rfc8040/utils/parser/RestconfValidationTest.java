f69ff51f3464b7d73859764e5fc5c591afe56ff3
Robert Varga <robert.varga@pantheon.tech>
2023-11-21 18:18:40 +0100

    Migrate RestconfValidationTest to JUnit5
    Migrate tests cases and improve assertions.
    
    Change-Id: I8266abb1cbbcbb72a4a7635a0c21d26ac9a48c12
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- restconf/restconf-nb/src/test/java/org/opendaylight/restconf/nb/rfc8040/utils/parser/RestconfValidationTest.java
+++ restconf/restconf-nb/src/test/java/org/opendaylight/restconf/nb/rfc8040/utils/parser/RestconfValidationTest.java
@@ -7,18 +7,14 @@
  */
 package org.opendaylight.restconf.nb.rfc8040.utils.parser;
 
-import static org.hamcrest.CoreMatchers.containsString;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import com.google.common.collect.Iterators;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
-import org.junit.Test;
-import org.junit.function.ThrowingRunnable;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
 import org.opendaylight.restconf.common.errors.RestconfDocumentedException;
 import org.opendaylight.restconf.common.errors.RestconfError;
 import org.opendaylight.yangtools.yang.common.ErrorTag;
@@ -28,18 +24,16 @@ import org.opendaylight.yangtools.yang.common.Revision;
 /**
  * Unit test for {@link ParserIdentifier}'s validate methods.
  */
-public class RestconfValidationTest {
-    private static final List<String> REVISIONS = Arrays.asList("2014-01-01", "2015-01-01", "2016-01-01");
-    private static final List<String> NAMES = Arrays.asList("_module-1", "_module-2", "_module-3");
+class RestconfValidationTest {
+    private static final List<String> REVISIONS = List.of("2014-01-01", "2015-01-01", "2016-01-01");
+    private static final List<String> NAMES = List.of("_module-1", "_module-2", "_module-3");
 
     /**
      * Test of successful validation of module revision.
      */
     @Test
-    public void validateAndGetRevisionTest() {
-        final Revision revision = ParserIdentifier.validateAndGetRevision(REVISIONS.iterator());
-        assertNotNull("Correct module revision should be validated", revision);
-        assertEquals(Revision.of("2014-01-01"), revision);
+    void validateAndGetRevisionTest() {
+        assertEquals(Revision.of("2014-01-01"), ParserIdentifier.validateAndGetRevision(REVISIONS.iterator()));
     }
 
     /**
@@ -47,7 +41,7 @@ public class RestconfValidationTest {
      * <code>RestconfDocumentedException</code> and checking for correct error type, error tag and error status code.
      */
     @Test
-    public void validateAndGetRevisionNotSuppliedTest() {
+    void validateAndGetRevisionNotSuppliedTest() {
         final var error = assertInvalidValue(
             () -> ParserIdentifier.validateAndGetRevision(Collections.emptyIterator()));
         assertEquals("Revision date must be supplied.", error.getErrorMessage());
@@ -58,20 +52,23 @@ public class RestconfValidationTest {
      * catching <code>RestconfDocumentedException</code>.
      */
     @Test
-    public void validateAndGetRevisionNotParsableTest() {
-        final RestconfDocumentedException ex = assertThrows(RestconfDocumentedException.class,
+    void validateAndGetRevisionNotParsableTest() {
+        final var ex = assertThrows(RestconfDocumentedException.class,
             () -> ParserIdentifier.validateAndGetRevision(Iterators.singletonIterator("not-parsable-as-date")));
-        assertThat(ex.getMessage(), containsString("Supplied revision is not in expected date format YYYY-mm-dd"));
+        final var errors = ex.getErrors();
+        assertEquals(1, errors.size());
+        final var error = errors.get(0);
+        assertEquals("Supplied revision is not in expected date format YYYY-mm-dd", error.getErrorMessage());
+        assertEquals(ErrorType.APPLICATION, error.getErrorType());
+        assertEquals(ErrorTag.OPERATION_FAILED, error.getErrorTag());
     }
 
     /**
      * Test of successful validation of module name.
      */
     @Test
-    public void validateAndGetModulNameTest() {
-        final String moduleName = ParserIdentifier.validateAndGetModulName(NAMES.iterator());
-        assertNotNull("Correct module name should be validated", moduleName);
-        assertEquals("_module-1", moduleName);
+    void validateAndGetModulNameTest() {
+        assertEquals("_module-1", ParserIdentifier.validateAndGetModulName(NAMES.iterator()));
     }
 
     /**
@@ -79,11 +76,10 @@ public class RestconfValidationTest {
      * <code>RestconfDocumentedException</code> and checking for correct error type, error tag and error status code.
      */
     @Test
-    public void validateAndGetModulNameNotSuppliedTest() {
+    void validateAndGetModulNameNotSuppliedTest() {
         final var error = assertInvalidValue(
             () -> ParserIdentifier.validateAndGetModulName(Collections.emptyIterator()));
         assertEquals("Module name must be supplied.", error.getErrorMessage());
-
     }
 
     /**
@@ -92,7 +88,7 @@ public class RestconfValidationTest {
      * error tag and error status code.
      */
     @Test
-    public void validateAndGetModuleNameNotParsableFirstTest() {
+    void validateAndGetModuleNameNotParsableFirstTest() {
         final var error = assertInvalidValue(
             () -> ParserIdentifier.validateAndGetModulName(Iterators.singletonIterator(
                 "01-not-parsable-as-name-on-firts-char")));
@@ -116,7 +112,7 @@ public class RestconfValidationTest {
      * <code>RestconfDocumentedException</code> and checking for correct error type, error tag and error status code.
      */
     @Test
-    public void validateAndGetModuleNameNotParsableXmlTest() {
+    void validateAndGetModuleNameNotParsableXmlTest() {
         final var error = assertInvalidValue(
             () -> ParserIdentifier.validateAndGetModulName(Iterators.singletonIterator("xMl-module-name")));
         assertEquals("Identifier must NOT start with XML ignore case.", error.getErrorMessage());
@@ -127,13 +123,13 @@ public class RestconfValidationTest {
      * <code>RestconfDocumentedException</code> and checking for correct error type, error tag and error status code.
      */
     @Test
-    public void validateAndGetModuleNameEmptyTest() {
+    void validateAndGetModuleNameEmptyTest() {
         final var error = assertInvalidValue(
             () -> ParserIdentifier.validateAndGetModulName(Iterators.singletonIterator("")));
         assertEquals("Identifier must start with character from set 'a-zA-Z_", error.getErrorMessage());
     }
 
-    private static RestconfError assertInvalidValue(final ThrowingRunnable runnable) {
+    private static RestconfError assertInvalidValue(final Executable runnable) {
         final var ex = assertThrows(RestconfDocumentedException.class, runnable);
         final var errors = ex.getErrors();
         assertEquals(1, errors.size());
