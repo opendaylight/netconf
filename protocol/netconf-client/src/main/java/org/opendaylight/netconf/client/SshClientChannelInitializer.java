15bc288a64f57c9f3684112412864f47c1326fb6
Peter Suna <peter.suna@pantheon.tech>
2023-11-24 18:19:44 +0100

    Remove blocking call from AsyncSshHandler
    Remove the ConnectFuture verify() call from the
    AsyncSshHandler connect() method.
    
    Implement a method to time out the connection feature
    using the Timer class, which creates an asynchronous
    thread and does not block other connections.
    
    Set the maximum connection timeout for SshClient
    to only allow a connection timeout from
    the AsyncSshHandler timer.
    
    JIRA: NETCONF-1167
    Change-Id: Ib9f4211020a165e093bd3a5080e221e70af49ca8
    Signed-off-by: Peter Suna <peter.suna@pantheon.tech>

--- protocol/netconf-client/src/main/java/org/opendaylight/netconf/client/SshClientChannelInitializer.java
+++ protocol/netconf-client/src/main/java/org/opendaylight/netconf/client/SshClientChannelInitializer.java
@@ -8,6 +8,7 @@
 package org.opendaylight.netconf.client;
 
 import io.netty.channel.Channel;
+import io.netty.util.Timer;
 import io.netty.util.concurrent.Promise;
 import org.eclipse.jdt.annotation.Nullable;
 import org.opendaylight.netconf.nettyutil.handler.ssh.authentication.AuthenticationHandler;
@@ -16,29 +17,32 @@ import org.opendaylight.netconf.nettyutil.handler.ssh.client.NetconfSshClient;
 
 final class SshClientChannelInitializer extends AbstractClientChannelInitializer {
     private final AuthenticationHandler authenticationHandler;
+    private final Timer timer;
     private final @Nullable NetconfSshClient sshClient;
     private final @Nullable String name;
 
     SshClientChannelInitializer(final AuthenticationHandler authHandler,
             final NetconfClientSessionNegotiatorFactory negotiatorFactory,
-            final NetconfClientSessionListener sessionListener, final @Nullable NetconfSshClient sshClient,
-            final @Nullable String name) {
+            final NetconfClientSessionListener sessionListener, final Timer timer,
+            final @Nullable NetconfSshClient sshClient, final @Nullable String name) {
         super(negotiatorFactory, sessionListener);
         authenticationHandler = authHandler;
+        this.timer = timer;
         this.sshClient = sshClient;
         this.name = name;
     }
 
     SshClientChannelInitializer(final AuthenticationHandler authHandler,
             final NetconfClientSessionNegotiatorFactory negotiatorFactory,
-            final NetconfClientSessionListener sessionListener) {
-        this(authHandler, negotiatorFactory, sessionListener, null, null);
+            final NetconfClientSessionListener sessionListener, final Timer timer) {
+        this(authHandler, negotiatorFactory, sessionListener, timer, null, null);
     }
 
     @Override
     public void initialize(final Channel ch, final Promise<NetconfClientSession> promise) {
         // ssh handler has to be the first handler in pipeline
-        var asyncHandler = AsyncSshHandler.createForNetconfSubsystem(authenticationHandler, promise, sshClient, name);
+        var asyncHandler = AsyncSshHandler.createForNetconfSubsystem(authenticationHandler, promise, timer, sshClient,
+            name);
         ch.pipeline().addFirst(asyncHandler);
         super.initialize(ch, promise);
     }
