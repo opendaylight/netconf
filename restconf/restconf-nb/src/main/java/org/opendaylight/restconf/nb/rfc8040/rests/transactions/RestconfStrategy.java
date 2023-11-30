c237fdb6e508c8fca32d8550324039a664acb90e
Oleksandr Zharov <oleksandr.zharov@pantheon.tech>
2023-11-30 12:07:00 +0100

    Fix PUT/POST requests for lists
    Added condition into PUT/POST requests with insert=after option
    that covers insert afret last element of the list.
    
    JIRA: NETCONF-1180
    Change-Id: Ib55818741c82c907648b259c2bd362b9b4348e52
    Signed-off-by: Oleksandr Zharov <oleksandr.zharov@pantheon.tech>

--- restconf/restconf-nb/src/main/java/org/opendaylight/restconf/nb/rfc8040/rests/transactions/RestconfStrategy.java
+++ restconf/restconf-nb/src/main/java/org/opendaylight/restconf/nb/rfc8040/rests/transactions/RestconfStrategy.java
@@ -347,6 +347,13 @@ public abstract class RestconfStrategy {
             tx.replace(childPath, nodeChild);
             lastInsertedPosition++;
         }
+
+        // In case we are inserting after last element
+//        if (!before) {
+//            if (lastInsertedPosition == lastItemPosition) {
+//                tx.replace(path, data);
+//            }
+//        }
     }
 
     private static ListenableFuture<? extends CommitInfo> replaceAndCommit(final RestconfTransaction tx,
@@ -582,6 +589,13 @@ public abstract class RestconfStrategy {
             tx.replace(grandParentPath.node(nodeChild.name()), nodeChild);
             lastInsertedPosition++;
         }
+
+        // In case we are inserting after last element
+//        if (!before) {
+//            if (lastInsertedPosition == lastItemPosition) {
+//                tx.replace(path, data);
+//            }
+//        }
     }
 
     private static ListenableFuture<? extends CommitInfo> createAndCommit(final RestconfTransaction tx,
