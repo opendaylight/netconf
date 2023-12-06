63a192fc4923daa7de8aa010627dfb0dada5be29
Robert Varga <robert.varga@pantheon.tech>
2023-12-06 08:58:28 +0100

    Allow dataPOST() to control Etag/Last-Modified
    In Create Resource Mode, each RestconfStrategy should be able to
    communicate ETag and Last-Modified headers.
    
    Retrofit DataPostResult.CreateResource to implement
    ConfigurationMetadata and propagate its content when generating the HTTP
    response.
    
    JIRA: NETCONF-1207
    Change-Id: I6b4440f018c17a41887fecc19b220542f360e2c6
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- restconf/restconf-nb/src/main/java/org/opendaylight/restconf/server/api/DataPostResult.java
+++ restconf/restconf-nb/src/main/java/org/opendaylight/restconf/server/api/DataPostResult.java
@@ -9,7 +9,8 @@ package org.opendaylight.restconf.server.api;
 
 import static java.util.Objects.requireNonNull;
 
-import org.eclipse.jdt.annotation.NonNullByDefault;
+import java.time.Instant;
+import org.eclipse.jdt.annotation.NonNull;
 import org.eclipse.jdt.annotation.Nullable;
 import org.opendaylight.restconf.nb.rfc8040.legacy.NormalizedNodePayload;
 
@@ -17,18 +18,26 @@ import org.opendaylight.restconf.nb.rfc8040.legacy.NormalizedNodePayload;
  * Result of a {@code POST} request as defined in
  * <a href="https://www.rfc-editor.org/rfc/rfc8040#section-4.4">RFC8040 section 4.4</a>.
  */
-@NonNullByDefault
 public sealed interface DataPostResult {
     /**
      * Result of a {@code POST} request in as defined in
      * <a href="https://www.rfc-editor.org/rfc/rfc8040#section-4.4.1">RFC8040 Create Resource Mode</a>.
      *
      * @param createdPath API path of the newly-created resource
+     * @param entityTag response {@code ETag} header, or {@code null} if not applicable
+     * @param lastModified response {@code Last-Modified} header, or {@code null} if not applicable
      */
-    // FIXME: use ApiPath instead of String
-    record CreateResource(String createdPath) implements DataPostResult {
+    record CreateResource(
+            // FIXME: use ApiPath instead of String
+            @NonNull String createdPath,
+            @Nullable EntityTag entityTag,
+            @Nullable Instant lastModified) implements DataPostResult, ConfigurationMetadata {
         public CreateResource {
             requireNonNull(createdPath);
+        }
+
+        public CreateResource(final @NonNull String createdPath) {
+            this(createdPath, null, null);
         }
     }
 
