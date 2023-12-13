f28e9cb0bd1853693613cc4a0cc7b49a306de1a8
Ivan Hrasko <ivan.hrasko@pantheon.tech>
2023-12-13 12:32:14 +0100

    Remove jackson-annotations from OpenApi
    Remove the last unnecessary usage and completely remove
    jackson-annotations dependency from restconf OpenApi.
    
    Change-Id: I08ccc9240bc45b753df2a5e6c0ba828b5e3a6f9e
    Signed-off-by: Ivan Hrasko <ivan.hrasko@pantheon.tech>

--- restconf/restconf-openapi/src/main/java/org/opendaylight/restconf/openapi/model/MountPointInstance.java
+++ restconf/restconf-openapi/src/main/java/org/opendaylight/restconf/openapi/model/MountPointInstance.java
@@ -7,9 +7,5 @@
  */
 package org.opendaylight.restconf.openapi.model;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.annotation.JsonInclude.Include;
-
-@JsonInclude(Include.NON_NULL)
 public record MountPointInstance(String instance, Long id) {
 }
