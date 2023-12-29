7ced5d08234f85970aaaa3359aacf8303daa2816
Robert Varga <robert.varga@pantheon.tech>
2023-12-29 11:36:29 +0100

    WIP: Migrate Json body parsers
    // FIXME: JsonResourceBody needs API update
    // FIXME: JsonOperationInputBody needs API update
    
    // FIXME: issue
    
    Change-Id: If42c1504b2c1401552c89d49af1eb9e60fc8fcac
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- restconf/restconf-nb/src/main/java/org/opendaylight/restconf/nb/rfc8040/databind/JsonChildBody.java
+++ restconf/restconf-nb/src/main/java/org/opendaylight/restconf/nb/rfc8040/databind/JsonChildBody.java
@@ -7,32 +7,14 @@
  */
 package org.opendaylight.restconf.nb.rfc8040.databind;
 
-import com.google.common.base.Throwables;
 import com.google.common.collect.ImmutableList;
-import com.google.gson.stream.JsonReader;
 import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.nio.charset.StandardCharsets;
-import org.eclipse.jdt.annotation.NonNull;
-import org.opendaylight.restconf.common.errors.RestconfDocumentedException;
 import org.opendaylight.restconf.server.api.DataPostPath;
-import org.opendaylight.yangtools.yang.common.ErrorTag;
-import org.opendaylight.yangtools.yang.common.ErrorType;
-import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.NodeIdentifier;
 import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.PathArgument;
-import org.opendaylight.yangtools.yang.data.api.schema.ChoiceNode;
-import org.opendaylight.yangtools.yang.data.api.schema.MapEntryNode;
-import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNode;
-import org.opendaylight.yangtools.yang.data.codec.gson.JsonParserStream;
-import org.opendaylight.yangtools.yang.data.impl.schema.ImmutableNormalizedNodeStreamWriter;
-import org.opendaylight.yangtools.yang.data.impl.schema.NormalizationResultHolder;
-import org.opendaylight.yangtools.yang.data.impl.schema.ResultAlreadySetException;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.opendaylight.yangtools.yang.data.api.schema.stream.InputStreamNormalizer.PrefixAndResult;
+import org.opendaylight.yangtools.yang.data.api.schema.stream.NormalizationException;
 
 public final class JsonChildBody extends ChildBody {
-    private static final Logger LOG = LoggerFactory.getLogger(JsonChildBody.class);
-
     public JsonChildBody(final InputStream inputStream) {
         super(inputStream);
     }
@@ -40,47 +22,17 @@ public final class JsonChildBody extends ChildBody {
     @Override
     @SuppressWarnings("checkstyle:illegalCatch")
     PrefixAndBody toPayload(final DataPostPath path, final InputStream inputStream) {
-        NormalizedNode result;
+        final PrefixAndResult prefixAndResult;
         try {
-            result = toNormalizedNode(path, inputStream);
-        } catch (Exception e) {
-            Throwables.throwIfInstanceOf(e, RestconfDocumentedException.class);
-            LOG.debug("Error parsing json input", e);
-
-            if (e instanceof ResultAlreadySetException) {
-                throw new RestconfDocumentedException(
-                    "Error parsing json input: Failed to create new parse result data. "
-                        + "Are you creating multiple resources/subresources in POST request?", e);
-            }
-
-            throwIfYangError(e);
-            throw new RestconfDocumentedException("Error parsing input: " + e.getMessage(), ErrorType.PROTOCOL,
-                ErrorTag.MALFORMED_MESSAGE, e);
+            prefixAndResult = path.databind().jsonCodecs().parseChildData(path.inference(), inputStream);
+        } catch (NormalizationException e) {
+            throw mapException(e);
         }
 
-        final var iiToDataList = ImmutableList.<PathArgument>builder();
-        while (result instanceof ChoiceNode choice) {
-            final var childNode = choice.body().iterator().next();
-            iiToDataList.add(result.name());
-            result = childNode;
-        }
-
-        final var resultName = result.name();
-        if (result instanceof MapEntryNode) {
-            iiToDataList.add(new NodeIdentifier(resultName.getNodeType()));
-        }
-        iiToDataList.add(resultName);
-
-        return new PrefixAndBody(iiToDataList.build(), result);
-    }
-
-    private static @NonNull NormalizedNode toNormalizedNode(final DataPostPath path, final InputStream inputStream) {
-        final var resultHolder = new NormalizationResultHolder();
-        final var writer = ImmutableNormalizedNodeStreamWriter.from(resultHolder);
-        final var jsonParser = JsonParserStream.create(writer, path.databind().jsonCodecs(), path.inference());
-        final var reader = new JsonReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8));
-        jsonParser.parse(reader);
-
-        return resultHolder.getResult().data();
+        final var result = prefixAndResult.result().data();
+        return new PrefixAndBody(ImmutableList.<PathArgument>builder()
+            .addAll(prefixAndResult.prefix())
+            .add(result.name())
+            .build(), result);
     }
 }
