f286ba2ce622bd834490fa746dca33e737754dec
Matej Sramcik <matej.sramcik@pantheon.tech>
2024-01-04 14:05:37 +0100

    Disconnect device deleted from allowed devices
    After removing connected device from the allowed devices,
    the device continued to stay connected with the node
    in the netconf topology.
    This change ensures that a device is properly disconnected
    after being removed from the allowed devices.
    
    JIRA: NETCONF-989
    Change-Id: I35cba26c4d004a5f90b9fa4f65651c9f58f386bd
    Signed-off-by: Matej Sramcik <matej.sramcik@pantheon.tech>
    Signed-off-by: Yaroslav Lastivka <yaroslav.lastivka@pantheon.tech>

--- apps/callhome-provider/src/test/java/org/opendaylight/netconf/topology/callhome/CallHomeMountServiceTest.java
+++ apps/callhome-provider/src/test/java/org/opendaylight/netconf/topology/callhome/CallHomeMountServiceTest.java
@@ -16,31 +16,47 @@ import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.doAnswer;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.opendaylight.mdsal.binding.api.DataObjectModification.ModificationType.DELETE;
+import static org.opendaylight.mdsal.binding.api.DataObjectModification.ModificationType.WRITE;
 
 import com.google.common.util.concurrent.ListenableFuture;
 import io.netty.channel.Channel;
 import java.lang.annotation.Annotation;
 import java.net.InetSocketAddress;
 import java.net.SocketAddress;
+import java.util.List;
+import org.eclipse.jdt.annotation.NonNull;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.opendaylight.mdsal.binding.api.DataObjectModification;
+import org.opendaylight.mdsal.binding.api.DataTreeIdentifier;
+import org.opendaylight.mdsal.binding.api.DataTreeModification;
+import org.opendaylight.mdsal.common.api.LogicalDatastoreType;
 import org.opendaylight.netconf.client.NetconfClientSession;
 import org.opendaylight.netconf.client.NetconfClientSessionListener;
 import org.opendaylight.netconf.shaded.sshd.client.session.ClientSession;
 import org.opendaylight.netconf.transport.api.UnsupportedConfigurationException;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.netconf.node.topology.rev240611.NetconfNode;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.netconf.callhome.server.rev240129.NetconfCallhomeServer;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.netconf.callhome.server.rev240129.netconf.callhome.server.AllowedDevices;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.netconf.callhome.server.rev240129.netconf.callhome.server.allowed.devices.Device;
 import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.NodeId;
 import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.Node;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
 
 @ExtendWith(MockitoExtension.class)
 public class CallHomeMountServiceTest {
 
     private static final SocketAddress SOCKET_ADDRESS = new InetSocketAddress("127.0.0.1", 12345);
+    private static final InstanceIdentifier<Device> IDENTIFIER = InstanceIdentifier.builder(
+        NetconfCallhomeServer.class).child(AllowedDevices.class).child(Device.class).build();
     private static final String ID1 = "id1";
     private static final NodeId NODE_ID1 = new NodeId(ID1);
     private static final String ID2 = "id2";
@@ -65,32 +81,11 @@ public class CallHomeMountServiceTest {
     @BeforeEach
     void beforeEach() {
         service = new CallHomeMountService(topology, defaultConfig());
-        /*
-         * Reproduce behavior of org.opendaylight.netconf.topology.spi.AbstractNetconfTopology#ensureNode(Node)
-         * for ID1 only.
-         */
-        doAnswer(invocation -> {
-            node1 = (Node) invocation.getArguments()[0];
-            if (ID1.equals(node1.requireNodeId().getValue())) {
-                final var configBuilderFactory = service.createClientConfigurationBuilderFactory();
-                final var config = configBuilderFactory
-                    .createClientConfigurationBuilder(node1.requireNodeId(), node1.augmentation(NetconfNode.class))
-                    .withSessionListener(sessionListener).build();
-                try {
-                    netconfSessionFuture = service.createClientFactory().createClient(config);
-                } catch (UnsupportedConfigurationException e) {
-                    netconfSessionFuture = null;
-                }
-            } else {
-                netconfSessionFuture = null;
-            }
-            return null;
-        }).when(topology).enableNode(any(Node.class));
-        doNothing().when(topology).disableNode(any(NodeId.class));
     }
 
     @Test
-    void sshSessionContextManager() throws Exception {
+    void sshSessionContextManager() {
+        reproduceEnsureNodeId2();
         doReturn(SOCKET_ADDRESS).when(sshSession).getRemoteAddress();
         final var sshSessionContextManager = service.createSshSessionContextManager();
 
@@ -116,6 +111,7 @@ public class CallHomeMountServiceTest {
 
     @Test
     void tlsSessionContextManager() {
+        reproduceEnsureNodeId2();
         doReturn(SOCKET_ADDRESS).when(nettyChannel).remoteAddress();
         final var tlsSessionContextManager = service.createTlsSessionContextManager(tlsAuthProvider, statusRecorder);
 
@@ -136,6 +132,82 @@ public class CallHomeMountServiceTest {
         // remove context
         tlsSessionContextManager.remove(ID1);
         verify(topology, times(1)).disableNode(eq(NODE_ID1));
+    }
+
+    @Test
+    void testDeletingDeviceFromTopologyAfterModification() {
+        final var mockDeviceBeforeAddition = createMockDevice(ID1);
+        final var mockObjectModification = mock(DataObjectModification.class);
+
+        // modification of overriding device inside 'allowed-devices' container
+        when(mockObjectModification.modificationType()).thenReturn(WRITE);
+        when(mockObjectModification.dataBefore()).thenReturn(mockDeviceBeforeAddition);
+        service.onAllowedDevicesChanged(List.of(new CustomTreeModification(DataTreeIdentifier.of(
+            LogicalDatastoreType.CONFIGURATION, IDENTIFIER), mockObjectModification)));
+        verify(topology, times(1)).disableNode(NODE_ID1);
+
+        // modification of deleting device from 'allowed-devices' container
+        when(mockObjectModification.modificationType()).thenReturn(DELETE);
+        service.onAllowedDevicesChanged(List.of(new CustomTreeModification(DataTreeIdentifier.of(
+            LogicalDatastoreType.CONFIGURATION, IDENTIFIER), mockObjectModification)));
+        verify(topology, times(2)).disableNode(NODE_ID1);
+    }
+
+    @Test
+    void testDeletingMultipleDevices() {
+        final var mockDevice1 = createMockDevice(ID1);
+        final var mockDevice2 = createMockDevice(ID2);
+
+        // Mock DataObjectModifications for each device
+        final var mockModification1 = mock(DataObjectModification.class);
+        final var mockModification2 = mock(DataObjectModification.class);
+
+        when(mockModification1.modificationType()).thenReturn(DELETE);
+        when(mockModification1.dataBefore()).thenReturn(mockDevice1);
+
+        when(mockModification2.modificationType()).thenReturn(DELETE);
+        when(mockModification2.dataBefore()).thenReturn(mockDevice2);
+
+        service.onAllowedDevicesChanged(List.of(
+            new CustomTreeModification(DataTreeIdentifier.of(LogicalDatastoreType.CONFIGURATION, IDENTIFIER),
+                mockModification1),
+            new CustomTreeModification(DataTreeIdentifier.of(LogicalDatastoreType.CONFIGURATION, IDENTIFIER),
+                mockModification2)
+        ));
+
+        verify(topology, times(1)).disableNode(new NodeId(ID1));
+        verify(topology, times(1)).disableNode(new NodeId(ID2));
+    }
+
+    private void reproduceEnsureNodeId2() {
+        /*
+         * Reproduce behavior of org.opendaylight.netconf.topology.spi.AbstractNetconfTopology#ensureNode(Node)
+         * for ID1 only.
+         */
+        doAnswer(invocation -> {
+            node1 = (Node) invocation.getArguments()[0];
+            if (ID1.equals(node1.requireNodeId().getValue())) {
+                final var configBuilderFactory = service.createClientConfigurationBuilderFactory();
+                final var config = configBuilderFactory
+                    .createClientConfigurationBuilder(node1.requireNodeId(), node1.augmentation(NetconfNode.class))
+                    .withSessionListener(sessionListener).build();
+                try {
+                    netconfSessionFuture = service.createClientFactory().createClient(config);
+                } catch (UnsupportedConfigurationException e) {
+                    netconfSessionFuture = null;
+                }
+            } else {
+                netconfSessionFuture = null;
+            }
+            return null;
+        }).when(topology).enableNode(any(Node.class));
+        doNothing().when(topology).disableNode(any(NodeId.class));
+    }
+
+    private static Device createMockDevice(final String deviceId) {
+        final var device = mock(Device.class);
+        when(device.getUniqueId()).thenReturn(deviceId);
+        return device;
     }
 
     private static CallHomeMountService.Configuration defaultConfig() {
@@ -230,5 +302,26 @@ public class CallHomeMountServiceTest {
                 return null;
             }
         };
+    }
+
+    private static class CustomTreeModification implements DataTreeModification<Device> {
+        private final DataTreeIdentifier<Device> rootPath;
+        private final DataObjectModification<Device> rootNode;
+
+        CustomTreeModification(final DataTreeIdentifier<Device> rootPath,
+            final DataObjectModification<Device> rootNode) {
+            this.rootPath = rootPath;
+            this.rootNode = rootNode;
+        }
+
+        @Override
+        public @NonNull DataTreeIdentifier<Device> getRootPath() {
+            return rootPath;
+        }
+
+        @Override
+        public DataObjectModification<Device> getRootNode() {
+            return rootNode;
+        }
     }
 }
