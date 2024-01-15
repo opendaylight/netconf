4bf2dab4b83bd1ecb5277dd8694f26bbe90f93f4
Ivan Hrasko <ivan.hrasko@pantheon.tech>
2024-01-15 15:17:56 +0100

    Remove redundant escape character
    The newline escape character in InfoEntity description is
    redundant. Remove it.
    
    Change-Id: I146bc26c800214321e3681f205ea4554b934c2b4
    Signed-off-by: Ivan Hrasko <ivan.hrasko@pantheon.tech>

--- restconf/restconf-openapi/src/main/java/org/opendaylight/restconf/openapi/model/InfoEntity.java
+++ restconf/restconf-openapi/src/main/java/org/opendaylight/restconf/openapi/model/InfoEntity.java
@@ -15,7 +15,7 @@ public final class InfoEntity extends OpenApiEntity {
     private final String title;
     private static final String DESCRIPTION = """
         We are providing full API for configurational data which can be edited (by POST, PUT, PATCH and DELETE).
-        For operational data we only provide GET API.\n
+        For operational data we only provide GET API.
         For majority of request you can see only config data in examples. That's because we can show only one example
         per request. The exception when you can see operational data in example is when data are representing
         operational (config false) container with no config data in it.""";
