a8952762ab2df8d8245be190905de1a67874b288
Robert Varga <robert.varga@pantheon.tech>
2024-01-16 14:51:32 +0100

    Implement registerNotificationListeners()
    Bulk listener registration, as introduced a long time ago, should be
    supported for all clients. Make sure we have an implementation.
    
    While we are here, also fix thread safety hazards around
    registration/unregistration -- with minimal critical sections.
    
    Optimize retained memory by specializing empty/one/multiple registrations,
    which in turn allows for some code reuse.
    
    JIRA: NETCONF-1224
    Change-Id: I8e40a6c149dbc8bea63d840cbd4cd0a807860175
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>
    (cherry picked from commit e4e519e6c1c6db47d9c9cd96108f9206d1ca8062)
    (cherry picked from commit 2a36ff8b05f9a0eb0fb50365b207b860f3f754d7)

---
+++ plugins/sal-netconf-connector/src/test/java/org/opendaylight/netconf/sal/connect/netconf/sal/NC1224Test.java
@@ -0,0 +1,78 @@
+/*
+ * Copyright (c) 2024 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.netconf.sal.connect.netconf.sal;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.Map;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.opendaylight.mdsal.dom.api.DOMNotificationListener;
+import org.opendaylight.yangtools.yang.common.QName;
+import org.opendaylight.yangtools.yang.model.api.stmt.SchemaNodeIdentifier.Absolute;
+
+@ExtendWith(MockitoExtension.class)
+class NC1224Test {
+    private static final Absolute ONE = Absolute.of(QName.create("test", "one"));
+    private static final Absolute TWO = Absolute.of(QName.create("test", "two"));
+
+    private final NetconfDeviceNotificationService svc = new NetconfDeviceNotificationService();
+
+    @Mock
+    private DOMNotificationListener listener;
+
+    @AfterEach
+    void afterEach() {
+        assertEquals(0, svc.size());
+    }
+
+    @Test
+    void registerEmpty() {
+        try (var reg = svc.registerNotificationListener(listener)) {
+            assertEquals(0, svc.size());
+        }
+    }
+
+    @Test
+    void registerEmptyMap() {
+        try (var reg = svc.registerNotificationListeners(Map.of())) {
+            assertEquals(0, svc.size());
+        }
+    }
+
+    @Test
+    void registerOne() {
+        try (var reg = svc.registerNotificationListener(listener, ONE)) {
+            assertEquals(1, svc.size());
+        }
+    }
+
+    @Test
+    void registerOneMap() {
+        try (var reg = svc.registerNotificationListeners(Map.of(ONE, listener))) {
+            assertEquals(1, svc.size());
+        }
+    }
+
+    @Test
+    void registerOneTwo() {
+        try (var reg = svc.registerNotificationListener(listener, ONE, TWO)) {
+            assertEquals(2, svc.size());
+        }
+    }
+
+    @Test
+    void registerOneTwoMap() {
+        try (var reg = svc.registerNotificationListeners(Map.of(ONE, listener, TWO, listener))) {
+            assertEquals(2, svc.size());
+        }
+    }
+}
