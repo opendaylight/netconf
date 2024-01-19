5f66138a6875cec01d7bcc7e730fa83e81641006
Ivan Hrasko <ivan.hrasko@pantheon.tech>
2024-01-19 16:39:23 +0100

    Support random backoff between connection attempts
    First connection attempt gets initialized to minimum sleep, each
    subsequent is exponentially backed off by sleepFactor (default 1.5)
    until reach max sleep and randomized by +/- jitter (default 0.1).
    
    JIRA: NETCONF-1193
    Change-Id: Ia252e61e6b574bad09969e2c629c14ebf7320644
    Signed-off-by: lubos-cicut <lubos.cicut@pantheon.tech>
    (cherry picked from commit 803bfe32f22bb50a256d0086fee1de252e48de2e)
    Signed-off-by: Ivan Hrasko <ivan.hrasko@pantheon.tech>

--- apps/netconf-topology/src/main/java/org/opendaylight/netconf/topology/spi/NetconfNodeHandler.java
+++ apps/netconf-topology/src/main/java/org/opendaylight/netconf/topology/spi/NetconfNodeHandler.java
@@ -69,6 +69,7 @@ public final class NetconfNodeHandler extends AbstractRegistration implements Re
     private final long maxAttempts;
     private final int minSleep;
     private final double sleepFactor;
+    private final double jitter;
 
     @GuardedBy("this")
     private long attempts;
@@ -94,6 +95,7 @@ public final class NetconfNodeHandler extends AbstractRegistration implements Re
         sleepFactor = node.requireSleepFactor().doubleValue();
         final long potentialMaxSleep = node.requireMaxTimeoutBetweenAttemptsMillis().toJava();
         maxSleep = potentialMaxSleep >= minSleep ? potentialMaxSleep : minSleep;
+        jitter = node.getBackoffJitter().doubleValue();
 
         // Setup reconnection on empty context, if so configured
         // FIXME: NETCONF-925: implement this
@@ -243,17 +245,11 @@ public final class NetconfNodeHandler extends AbstractRegistration implements Re
             LOG.info("Failed to connect {} after {} attempts, not attempting", deviceId, attempts);
             return new ConnectGivenUpException("Given up connecting " + deviceId + " after " + attempts + " attempts");
         }
-
         // First connection attempt gets initialized to minimum sleep, each subsequent is exponentially backed off
-        // by sleepFactor.
+        // by sleepFactor (default 1.5) until reach max sleep and randomized by +/- jitter (default 0.1).
         if (attempts != 0) {
-            final long nextSleep = (long) (lastSleep * sleepFactor);
-            if (nextSleep <= maxSleep) {
-                // check for overflow
-                delayMillis = nextSleep >= 0 ? nextSleep : maxSleep;
-            } else {
-                delayMillis = maxSleep;
-            }
+            final var currentBackoff = Math.min(lastSleep * sleepFactor, maxSleep);
+            delayMillis = (long) (currentBackoff * (Math.random() * (jitter * 2) + (1 - jitter)));
         } else {
             delayMillis = minSleep;
         }
