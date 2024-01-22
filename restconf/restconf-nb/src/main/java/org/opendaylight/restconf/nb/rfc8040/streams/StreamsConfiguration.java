50ac56e4dd045ad345d9c92dd8b5d5199ff7ea07
Robert Varga <robert.varga@pantheon.tech>
2024-01-22 22:13:39 +0100

    Make RESTCONF base path configurable
    Move BASE_PATH constant into OSGi configuration to make it
    configurable and propagate it to other components through
    RestconfStreamServletFactory.
    
    JIRA: NETCONF-1218
    Change-Id: Ie1aed49ed37ff3e0cc862db77c7b88c7470c082a
    Signed-off-by: Oleksandr Zharov <oleksandr.zharov@pantheon.tech>
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>
    (cherry picked from commit cebda3ec669df9ee4177a8865803151cc7537aae)

--- restconf/restconf-nb/src/main/java/org/opendaylight/restconf/nb/rfc8040/streams/StreamsConfiguration.java
+++ restconf/restconf-nb/src/main/java/org/opendaylight/restconf/nb/rfc8040/streams/StreamsConfiguration.java
@@ -8,6 +8,7 @@
 package org.opendaylight.restconf.nb.rfc8040.streams;
 
 import static com.google.common.base.Preconditions.checkArgument;
+import static java.util.Objects.requireNonNull;
 
 /**
  * RESTCONF configuration holder and verifier.
@@ -17,8 +18,14 @@ import static com.google.common.base.Preconditions.checkArgument;
  * @param idleTimeout           Maximum idle time of web-socket session before the session is closed (milliseconds).
  * @param heartbeatInterval     Interval in milliseconds between sending of ping control frames.
  * @param useSSE                when is {@code true} use SSE else use WS
+ * @param basePath              The value of RFC8040 URI template, pointing to the root resource. Must not end with '/'.
  */
-public record StreamsConfiguration(int maximumFragmentLength, int idleTimeout, int heartbeatInterval, boolean useSSE) {
+public record StreamsConfiguration(
+        int maximumFragmentLength,
+        int idleTimeout,
+        int heartbeatInterval,
+        boolean useSSE,
+        String basePath) {
     // FIXME: can this be 64KiB exactly? if so, maximumFragmentLength should become a Uint16 and validation should be
     //        pushed out to users
     public static final int MAXIMUM_FRAGMENT_LENGTH_LIMIT = 65534;
@@ -31,5 +38,7 @@ public record StreamsConfiguration(int maximumFragmentLength, int idleTimeout, i
             "Heartbeat ping interval must be disabled (0) or specified by positive value.");
         // we need at least one heartbeat before we time out the socket
         checkArgument(idleTimeout > heartbeatInterval, "Idle timeout must be greater than heartbeat ping interval.");
+        requireNonNull(basePath);
+        checkArgument(basePath.endsWith("/"), "Base path value ends with /.");
     }
 }
