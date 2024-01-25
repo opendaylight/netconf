ec43252dc314aa374fb0dac61936a2c037849527
lubos-cicut <lubos.cicut@pantheon.tech>
2024-01-25 08:42:52 +0100

    Fix nullability checks in ServerEntity class
    During rewrite we have lost requireNonNull checks.
    
    JIRA: NETCONF-938
    Change-Id: I36e524b42a43698a80972e4980ab7450dfb95e66
    Signed-off-by: lubos-cicut <lubos.cicut@pantheon.tech>

--- restconf/restconf-openapi/src/main/java/org/opendaylight/restconf/openapi/model/ServerEntity.java
+++ restconf/restconf-openapi/src/main/java/org/opendaylight/restconf/openapi/model/ServerEntity.java
@@ -7,23 +7,23 @@
  */
 package org.opendaylight.restconf.openapi.model;
 
+import static java.util.Objects.requireNonNull;
+
 import com.fasterxml.jackson.core.JsonGenerator;
 import java.io.IOException;
 import org.eclipse.jdt.annotation.NonNull;
 
 public final class ServerEntity extends OpenApiEntity {
-    private final String url;
+    private final @NonNull String url;
 
-    public ServerEntity(final String url) {
-        this.url = url;
+    public ServerEntity(final @NonNull String url) {
+        this.url = requireNonNull(url);
     }
 
     @Override
     public void generate(@NonNull final JsonGenerator generator) throws IOException {
-        if (url != null) {
-            generator.writeStartObject();
-            generator.writeStringField("url", url);
-            generator.writeEndObject();
-        }
+        generator.writeStartObject();
+        generator.writeStringField("url", url);
+        generator.writeEndObject();
     }
 }
