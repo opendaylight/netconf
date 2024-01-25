ddb904cb2848785a7327b280ea6548d993e4cef5
lubos-cicut <lubos.cicut@pantheon.tech>
2024-01-25 08:39:09 +0100

    Fix nullability checks in InfoEntity class
    During rewrite we have lost requireNonNull checks.
    
    JIRA: NETCONF-938
    Change-Id: I31b556927df909878e3bbe0b0bc3cc6865660011
    Signed-off-by: lubos-cicut <lubos.cicut@pantheon.tech>

--- restconf/restconf-openapi/src/main/java/org/opendaylight/restconf/openapi/model/InfoEntity.java
+++ restconf/restconf-openapi/src/main/java/org/opendaylight/restconf/openapi/model/InfoEntity.java
@@ -7,12 +7,14 @@
  */
 package org.opendaylight.restconf.openapi.model;
 
+import static java.util.Objects.requireNonNull;
+
 import com.fasterxml.jackson.core.JsonGenerator;
 import java.io.IOException;
 import org.eclipse.jdt.annotation.NonNull;
 
 public final class InfoEntity extends OpenApiEntity {
-    private final String title;
+    private final @NonNull String title;
     private static final String DESCRIPTION = """
         We are providing full API for configurational data which can be edited (by POST, PUT, PATCH and DELETE).
         For operational data we only provide GET API.
@@ -21,17 +23,15 @@ public final class InfoEntity extends OpenApiEntity {
         per request. The exception when you can see operational data in example is when data are representing
         operational (config false) container with no config data in it.""";
 
-    public InfoEntity(final String title) {
-        this.title = title;
+    public InfoEntity(final @NonNull String title) {
+        this.title = requireNonNull(title);
     }
 
     @Override
     public void generate(@NonNull JsonGenerator generator) throws IOException {
         generator.writeObjectFieldStart("info");
         generator.writeStringField("version", "1.0.0");
-        if (title != null) {
-            generator.writeStringField("title", title);
-        }
+        generator.writeStringField("title", title);
         generator.writeStringField("description", DESCRIPTION);
         generator.writeEndObject();
     }
