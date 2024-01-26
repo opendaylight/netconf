921607c62799d94780c518f72c4142a88ed0188a
Robert Varga <robert.varga@pantheon.tech>
2024-01-26 04:53:28 +0100

    Remove use of threadpool-config-impl
    We are using just one utility, inline the few lines of code that are
    needed to make the threadpool work.
    
    JIRA: CONTROLLER-2092
    Change-Id: I93d08f348f3331a8b28e40c7f18f60831cd18d8d
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- apps/netconf-topology/src/main/java/org/opendaylight/netconf/topology/spi/NetconfTopologySchemaAssembler.java
+++ apps/netconf-topology/src/main/java/org/opendaylight/netconf/topology/spi/NetconfTopologySchemaAssembler.java
@@ -7,12 +7,17 @@
  */
 package org.opendaylight.netconf.topology.spi;
 
+import com.google.common.util.concurrent.ForwardingBlockingQueue;
 import com.google.common.util.concurrent.ThreadFactoryBuilder;
+import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.Executor;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.RejectedExecutionException;
+import java.util.concurrent.RejectedExecutionHandler;
 import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 import org.eclipse.jdt.annotation.NonNullByDefault;
-import org.opendaylight.controller.config.threadpool.util.FlexibleThreadPoolWrapper;
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Deactivate;
@@ -34,16 +39,43 @@ public final class NetconfTopologySchemaAssembler implements AutoCloseable {
         long assembler$_$keep$_$alive$_$millis() default 60_000;
     }
 
+    private static final class SynchronousBlockingQueue extends ForwardingBlockingQueue<Runnable> {
+        private final LinkedBlockingQueue<Runnable> delegate = new LinkedBlockingQueue<>();
+
+        @Override
+        protected BlockingQueue<Runnable> delegate() {
+            return delegate;
+        }
+
+        @Override
+        @SuppressWarnings("checkstyle:parameterName")
+        public boolean offer(final Runnable o) {
+            // ThreadPoolExecutor will spawn a new thread after core size is reached only if an offer is rejected. We
+            // always do that and recover via the execution handler.
+            return false;
+        }
+    }
+
     private static final ThreadFactory THREAD_FACTORY = new ThreadFactoryBuilder()
         .setNameFormat("topology-schema-assembler-%d")
         .setDaemon(true)
         .build();
+    private static final RejectedExecutionHandler BLOCKING_REJECTED_EXECUTION_HANDLER = (runnable, executor) -> {
+        // if maximum number of threads are reached, the threadpool would reject the execution. We override that
+        // behaviour and block until the queue accepts the task or we get interrupted
+        try {
+            executor.getQueue().put(runnable);
+        } catch (InterruptedException e) {
+            throw new RejectedExecutionException("Interrupted while waiting on the queue", e);
+        }
+    };
 
-    private final FlexibleThreadPoolWrapper threadPool;
+    private final ThreadPoolExecutor executor;
 
     public NetconfTopologySchemaAssembler(final int minThreads, final int maxThreads, final long keepAliveTime,
             final TimeUnit unit) {
-        threadPool = new FlexibleThreadPoolWrapper(minThreads, maxThreads, keepAliveTime, unit, THREAD_FACTORY);
+        executor = new ThreadPoolExecutor(minThreads, maxThreads, keepAliveTime, unit, new SynchronousBlockingQueue(),
+            THREAD_FACTORY, BLOCKING_REJECTED_EXECUTION_HANDLER);
     }
 
     @Activate
@@ -55,10 +87,10 @@ public final class NetconfTopologySchemaAssembler implements AutoCloseable {
     @Override
     @Deactivate
     public void close() {
-        threadPool.close();
+        executor.shutdown();
     }
 
     Executor executor() {
-        return threadPool.getExecutor();
+        return executor;
     }
 }
