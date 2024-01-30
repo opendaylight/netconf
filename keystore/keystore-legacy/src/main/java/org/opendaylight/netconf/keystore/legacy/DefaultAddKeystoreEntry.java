1ba415ffba1cae7ede58fa842a1d2e2fc470a5fb
Robert Varga <robert.varga@pantheon.tech>
2024-01-30 05:36:15 +0100

    Refactor NetconfSalKeystoreRpcs
    Move RPC implementations into keystore-legacy and wrap them in a
    ClusterSingletonService.
    
    JIRA: NETCONF-1237
    Change-Id: I1fdc81500c90cb46235838c9e77e741d44160807
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ keystore/keystore-legacy/src/main/java/org/opendaylight/netconf/keystore/legacy/DefaultAddKeystoreEntry.java
@@ -0,0 +1,84 @@
+/*
+ * Copyright (c) 2024 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.netconf.keystore.legacy;
+
+import static java.util.Objects.requireNonNull;
+
+import com.google.common.util.concurrent.ListenableFuture;
+import com.google.common.util.concurrent.MoreExecutors;
+import java.nio.charset.StandardCharsets;
+import java.security.GeneralSecurityException;
+import java.util.ArrayList;
+import java.util.Base64;
+import org.opendaylight.aaa.encrypt.AAAEncryptionService;
+import org.opendaylight.mdsal.binding.api.DataBroker;
+import org.opendaylight.mdsal.common.api.LogicalDatastoreType;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netconf.keystore.rev171017.AddKeystoreEntry;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netconf.keystore.rev171017.AddKeystoreEntryInput;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netconf.keystore.rev171017.AddKeystoreEntryOutput;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netconf.keystore.rev171017.AddKeystoreEntryOutputBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netconf.keystore.rev171017.Keystore;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netconf.keystore.rev171017.keystore.entry.KeyCredential;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netconf.keystore.rev171017.keystore.entry.KeyCredentialBuilder;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+import org.opendaylight.yangtools.yang.common.ErrorType;
+import org.opendaylight.yangtools.yang.common.RpcResult;
+import org.opendaylight.yangtools.yang.common.RpcResultBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+final class DefaultAddKeystoreEntry extends AbstractRpc implements AddKeystoreEntry {
+    private static final Logger LOG = LoggerFactory.getLogger(DefaultAddKeystoreEntry.class);
+
+    private final AAAEncryptionService encryptionService;
+
+    DefaultAddKeystoreEntry(final DataBroker dataBroker, final AAAEncryptionService encryptionService) {
+        super(dataBroker);
+        this.encryptionService = requireNonNull(encryptionService);
+    }
+
+    @Override
+    public ListenableFuture<RpcResult<AddKeystoreEntryOutput>> invoke(final AddKeystoreEntryInput input) {
+        final var plain = input.getKeyCredential();
+        if (plain == null || plain.isEmpty()) {
+            return RpcResultBuilder.success(new AddKeystoreEntryOutputBuilder().build()).buildFuture();
+        }
+
+        LOG.debug("Adding keypairs: {}", plain);
+        final var encrypted = new ArrayList<KeyCredential>(plain.size());
+        for (var credential : plain.values()) {
+            final var keyId = credential.getKeyId();
+            try {
+                encrypted.add(new KeyCredentialBuilder(credential)
+                    .setPrivateKey(encryptString(credential.getPrivateKey()))
+                    .setPassphrase(encryptString(credential.getPassphrase()))
+                    .build());
+            } catch (GeneralSecurityException e) {
+                LOG.debug("Cannot decrypt key credential {}}", credential, e);
+                return RpcResultBuilder.<AddKeystoreEntryOutput>failed()
+                    .withError(ErrorType.APPLICATION, "Failed to decrypt key " + keyId, e)
+                    .buildFuture();
+            }
+        }
+
+        final var tx = newTransaction();
+        for (var keypair : encrypted) {
+            tx.put(LogicalDatastoreType.CONFIGURATION,
+                InstanceIdentifier.create(Keystore.class).child(KeyCredential.class, keypair.key()), keypair);
+        }
+
+        return tx.commit().transform(commitInfo -> {
+            LOG.debug("Updated keypairs: {}", plain.keySet());
+            return RpcResultBuilder.success(new AddKeystoreEntryOutputBuilder().build()).build();
+        }, MoreExecutors.directExecutor());
+    }
+
+    private String encryptString(final String plain) throws GeneralSecurityException {
+        return Base64.getEncoder().encodeToString(encryptionService.encrypt(plain.getBytes(StandardCharsets.UTF_8)));
+    }
+}
---
+++ keystore/keystore-legacy/src/main/java/org/opendaylight/netconf/keystore/legacy/DefaultRemovePrivateKey.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright (c) 2024 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.netconf.keystore.legacy;
+
+import com.google.common.util.concurrent.ListenableFuture;
+import com.google.common.util.concurrent.MoreExecutors;
+import java.util.stream.Collectors;
+import org.opendaylight.mdsal.binding.api.DataBroker;
+import org.opendaylight.mdsal.common.api.LogicalDatastoreType;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netconf.keystore.rev171017.Keystore;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netconf.keystore.rev171017.RemovePrivateKey;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netconf.keystore.rev171017.RemovePrivateKeyInput;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netconf.keystore.rev171017.RemovePrivateKeyOutput;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netconf.keystore.rev171017.RemovePrivateKeyOutputBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netconf.keystore.rev171017._private.keys.PrivateKey;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netconf.keystore.rev171017._private.keys.PrivateKeyKey;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+import org.opendaylight.yangtools.yang.common.RpcResult;
+import org.opendaylight.yangtools.yang.common.RpcResultBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+final class DefaultRemovePrivateKey extends AbstractRpc implements RemovePrivateKey {
+    private static final Logger LOG = LoggerFactory.getLogger(DefaultRemovePrivateKey.class);
+
+    DefaultRemovePrivateKey(final DataBroker dataBroker) {
+        super(dataBroker);
+    }
+
+    @Override
+    public ListenableFuture<RpcResult<RemovePrivateKeyOutput>> invoke(final RemovePrivateKeyInput input) {
+        final var names = input.getName();
+        if (names == null || names.isEmpty()) {
+            return RpcResultBuilder.success(new RemovePrivateKeyOutputBuilder().build()).buildFuture();
+        }
+
+        final var keys = names.stream().map(PrivateKeyKey::new).collect(Collectors.toSet());
+        LOG.debug("Removing private keys: {}", keys);
+
+
+        final var tx = newTransaction();
+        for (var key : keys) {
+            tx.delete(LogicalDatastoreType.CONFIGURATION,
+                InstanceIdentifier.create(Keystore.class).child(PrivateKey.class, key));
+        }
+
+        return tx.commit().transform(commitInfo -> {
+            LOG.debug("Removed trusted keys: {}", keys);
+            return RpcResultBuilder.success(new RemovePrivateKeyOutputBuilder().build()).build();
+        }, MoreExecutors.directExecutor());
+    }
+}
