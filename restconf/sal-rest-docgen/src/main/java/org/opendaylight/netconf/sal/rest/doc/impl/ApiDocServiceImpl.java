72e7305fb1ba9b7b7a7600561f6772b5529d9601
lubos-cicut <lubos.cicut@pantheon.tech>
2024-02-07 23:56:28 +0100

    Fix OpenAPI is ignoring restconf settings
    Restconf OpenAPI is ignoring restconf settings in
    etc/org.opendaylight.restconf.nb.rfc8040.cfg namely restconf
    setting to specify the base path which has been introduced
    in NETCONF-1218.
    
    JIRA: NETCONF-1230
    Change-Id: I6c658b9feb89aba11a68290073b115c27d98bd13
    Signed-off-by: lubos-cicut <lubos.cicut@pantheon.tech>
    Signed-off-by: Ivan Hrasko <ivan.hrasko@pantheon.tech>
    (cherry picked from commit 56cd5cb20451d56cfb04ab88fd9a0a2bd716b36b)

--- restconf/sal-rest-docgen/src/main/java/org/opendaylight/netconf/sal/rest/doc/impl/ApiDocServiceImpl.java
+++ restconf/sal-rest-docgen/src/main/java/org/opendaylight/netconf/sal/rest/doc/impl/ApiDocServiceImpl.java
@@ -24,6 +24,7 @@ import org.opendaylight.netconf.sal.rest.doc.mountpoints.MountPointSwagger;
 import org.opendaylight.netconf.sal.rest.doc.swagger.CommonApiObject;
 import org.opendaylight.netconf.sal.rest.doc.swagger.MountPointInstance;
 import org.opendaylight.netconf.sal.rest.doc.swagger.SwaggerObject;
+import org.opendaylight.restconf.nb.rfc8040.JaxRsNorthbound;
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
@@ -52,16 +53,9 @@ public final class ApiDocServiceImpl implements ApiDocService {
     @Inject
     @Activate
     public ApiDocServiceImpl(final @Reference DOMSchemaService schemaService,
-                             final @Reference DOMMountPointService mountPointService) {
-        this(new MountPointSwaggerGeneratorRFC8040(schemaService, mountPointService),
-            new ApiDocGeneratorRFC8040(schemaService));
-    }
-
-    public ApiDocServiceImpl(final DOMSchemaService schemaService,
-                             final DOMMountPointService mountPointService,
-                             final String basePath) {
-        this(new MountPointSwaggerGeneratorRFC8040(schemaService, mountPointService, basePath),
-            new ApiDocGeneratorRFC8040(schemaService, basePath));
+            final @Reference DOMMountPointService mountPointService, @Reference JaxRsNorthbound.Configuration context) {
+        this(new MountPointSwaggerGeneratorRFC8040(schemaService, mountPointService, context.restconf()),
+            new ApiDocGeneratorRFC8040(schemaService, context.restconf()));
     }
 
     @VisibleForTesting
