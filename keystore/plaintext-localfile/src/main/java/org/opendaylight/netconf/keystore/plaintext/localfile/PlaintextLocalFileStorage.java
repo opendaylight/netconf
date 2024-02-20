ad71b216c4753b18eb586ea25300e08261a8c066
Ruslan Kashapov <ruslan.kashapov@pantheon.tech>
2024-02-20 11:47:23 +0200

    Keystore Plaintext Storage API and Local File implementation
    Data is being persisted into local file with AES-GCM-SIV encryption
    using 16 or 32 byte key and dynamically generated nonce.
    New feature artifact includes plaintext storage api, local file
    storage implementation and karaf shell commands (cli) to access
    and modify the data.
    
    JIRA: NETCONF-1257
    Change-Id: I841d255c4bcda5ecee7560b28330e568591f8833
    Signed-off-by: Ruslan Kashapov <ruslan.kashapov@pantheon.tech>

---
+++ keystore/plaintext-localfile/src/main/java/org/opendaylight/netconf/keystore/plaintext/localfile/PlaintextLocalFileStorage.java
@@ -0,0 +1,260 @@
+/*
+ * Copyright (c) 2024 PANTHEON.tech s.r.o. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.netconf.keystore.plaintext.localfile;
+
+import static java.nio.file.StandardCopyOption.ATOMIC_MOVE;
+import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
+import static java.util.Objects.requireNonNull;
+
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Base64;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+import java.util.Properties;
+import java.util.Random;
+import java.util.Set;
+import java.util.concurrent.atomic.AtomicReference;
+import org.eclipse.jdt.annotation.NonNull;
+import org.eclipse.jdt.annotation.Nullable;
+import org.opendaylight.netconf.keystore.plaintext.api.MutablePlaintextStorage;
+import org.opendaylight.netconf.keystore.plaintext.api.PlaintextStorage;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.metatype.annotations.AttributeDefinition;
+import org.osgi.service.metatype.annotations.Designate;
+import org.osgi.service.metatype.annotations.ObjectClassDefinition;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Local file implementation of{@link PlaintextStorage} and {@link MutablePlaintextStorage}.
+ */
+@Component(service = {PlaintextStorage.class, MutablePlaintextStorage.class}, immediate = true,
+        configurationPid = "org.opendaylight.netconf.keystore.plaintext.localfile")
+@Designate(ocd = PlaintextLocalFileStorage.Configuration.class)
+@SuppressFBWarnings("PZLA_PREFER_ZERO_LENGTH_ARRAYS")
+public class PlaintextLocalFileStorage implements MutablePlaintextStorage {
+
+    @ObjectClassDefinition
+    public @interface Configuration {
+        @AttributeDefinition
+        String key$_$file() default "etc/plaintext.keystore.key";
+
+        @AttributeDefinition
+        String data$_$file() default "etc/plaintext.keystore.data";
+
+        @AttributeDefinition
+        String import$_$from$_$file() default "etc/plaintext.keystore.data.unencrypted";
+    }
+
+    private static final Logger LOG = LoggerFactory.getLogger(PlaintextLocalFileStorage.class);
+
+    private final File file;
+    private final byte[] secret;
+    private final AtomicReference<Set<StorageEntry>> entriesRef = new AtomicReference<>();
+
+    @Activate
+    public PlaintextLocalFileStorage(final Configuration configuration) throws IOException {
+        this(
+            secretFrom(new File(configuration.key$_$file())),
+            new File(configuration.data$_$file()),
+            new File(configuration.import$_$from$_$file())
+        );
+        LOG.info("Service activated");
+    }
+
+    public PlaintextLocalFileStorage(final byte[] secret, final File file) throws IOException {
+        this(secret, file, null);
+    }
+
+    public PlaintextLocalFileStorage(final byte[] secret, final File file, final File importFile)
+            throws IOException {
+        this.file = requireNonNull(file);
+        this.secret = validateSecret(secret);
+        entriesRef.set(Set.copyOf(readData(secret, file, importFile)));
+    }
+
+    @Override
+    public final Iterator<Map.Entry<byte[], byte[]>> iterator() {
+        final var array = entries().toArray(StorageEntry[]::new);
+        return new Iterator<>() {
+            private final int maxIndex = array.length - 1;
+            private int index = -1;
+
+            @Override
+            public boolean hasNext() {
+                return index < maxIndex;
+            }
+
+            @Override
+            public Map.Entry<byte[], byte[]> next() {
+                if (index >= maxIndex) {
+                    throw new NoSuchElementException();
+                }
+                return array[++index];
+            }
+        };
+    }
+
+    @Override
+    public byte @Nullable [] lookup(byte @NonNull [] key) {
+        return findByKey(key).map(StorageEntry::getValue).orElse(null);
+    }
+
+    @Override
+    public byte @Nullable [] removeKey(byte @NonNull [] key) throws IOException {
+        final var toRemove = findByKey(key).orElse(null);
+        if (toRemove == null) {
+            return null;
+        }
+        final var entries = new HashSet<>(entries());
+        entries.remove(toRemove);
+        updateWith(Set.copyOf(entries));
+        return toRemove.value();
+    }
+
+    @Override
+    public boolean removeEntry(byte @NonNull [] key, byte @NonNull [] value) throws IOException {
+        final var entries = new HashSet<>(entries());
+        final var removed = entries.remove(new StorageEntry(key, value));
+        if (removed) {
+            updateWith(Set.copyOf(entries));
+        }
+        return removed;
+    }
+
+    @Override
+    public byte @Nullable [] insertEntry(byte @NonNull [] key, byte @NonNull [] value) throws IOException {
+        final var existing = findByKey(key).orElse(null);
+        if (existing != null) {
+            return existing.getValue();
+        }
+        final var entries = new HashSet<>(entries());
+        entries.add(new StorageEntry(key, value));
+        updateWith(Set.copyOf(entries));
+        return null;
+    }
+
+    @Override
+    public byte @Nullable [] putEntry(byte @NonNull [] key, byte @NonNull [] value) throws IOException {
+        final var previous = findByKey(key).orElse(null);
+        final var entries = new HashSet<>(entries());
+        if (previous != null) {
+            if (Arrays.equals(previous.value(), value)) {
+                // same value, no reason to update
+                return value;
+            }
+            entries.remove(previous);
+        }
+        entries.add(new StorageEntry(key, value));
+        updateWith(Set.copyOf(entries));
+        return previous == null ? null : previous.getValue();
+    }
+
+    private Set<StorageEntry> entries() {
+        final var entries = entriesRef.get();
+        return entries == null ? Set.of() : entries;
+    }
+
+    private Optional<StorageEntry> findByKey(final byte @NonNull [] key) {
+        return entries().stream().filter(entry -> Arrays.equals(key, entry.key())).findFirst();
+    }
+
+    private void updateWith(final Set<StorageEntry> mutable) throws IOException {
+        final var immutable = Set.copyOf(mutable);
+        writeToFile(immutable);
+        entriesRef.set(immutable);
+    }
+
+    private void writeToFile(final Collection<StorageEntry> data) throws IOException {
+        // randomize order before persistence
+        final var list = new ArrayList<>(data);
+        Collections.shuffle(list, new Random());
+        // write data to temp file
+        final var tmpFile = File.createTempFile("localfile-", ".temp");
+        try (var fos = new FileOutputStream(tmpFile)) {
+            CipherUtils.toBase64Stream(list, secret, fos);
+        }
+        // replace file with newly written
+        Files.move(tmpFile.toPath(), file.toPath(), ATOMIC_MOVE, REPLACE_EXISTING);
+    }
+
+    private static byte[] secretFrom(final File file) throws IOException {
+        if (file.exists()) {
+            try (var in = Base64.getDecoder().wrap(new FileInputStream(file))) {
+                return in.readAllBytes();
+            }
+        }
+        LOG.warn("Key file {} does not exist. Using generated key.", file);
+        final var secret = CipherUtils.generateSecret();
+        try (var out = Base64.getEncoder().wrap(new FileOutputStream(file))) {
+            out.write(secret);
+        }
+        return secret;
+    }
+
+    private static byte[] validateSecret(final byte[] secret) {
+        requireNonNull(secret);
+        if (secret.length != 16 && secret.length != 32) {
+            throw new IllegalArgumentException("Invalid secret length " + secret.length
+                + ". Expected 16 or 32.");
+        }
+        return secret;
+    }
+
+    private static Collection<StorageEntry> readData(final byte[] secret, final File file, final File importFile)
+            throws IOException {
+        if (file.exists()) {
+            try (var fis = new FileInputStream(file)) {
+                return CipherUtils.fromBase64Stream(secret, fis);
+            }
+        }
+        LOG.warn("Data file {} does not exist.", file);
+        final var props = new Properties();
+        if (importFile.exists()) {
+            LOG.info("Importing initial data from unencrypted properties file {}", importFile);
+            try (var fis = new FileInputStream(importFile)) {
+                props.load(fis);
+            }
+        } else {
+            LOG.info("Importing initial data from default properties");
+            try (var in = Thread.currentThread().getContextClassLoader().getResourceAsStream("default.properties")) {
+                props.load(in);
+            }
+        }
+        final var data = new LinkedList<StorageEntry>();
+        for (var name : props.stringPropertyNames()) {
+            final var value = props.getProperty(name, "");
+            if (!value.isEmpty()) {
+                data.add(
+                    new StorageEntry(name.getBytes(StandardCharsets.UTF_8), value.getBytes(StandardCharsets.UTF_8)));
+            }
+        }
+        try (var fos = new FileOutputStream(file)) {
+            CipherUtils.toBase64Stream(data, secret, fos);
+        }
+        if (importFile.delete()) {
+            LOG.info("Data moved to encrypted storage, import data file {} removed as insecure.", importFile);
+        }
+        return data;
+    }
+}
---
+++ keystore/plaintext-localfile/src/test/java/org/opendaylight/netconf/keystore/plaintext/localfile/PlaintextLocalFileStorageTest.java
@@ -0,0 +1,210 @@
+/*
+ * Copyright (c) 2024 PANTHEON.tech s.r.o. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.netconf.keystore.plaintext.localfile;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.lenient;
+import static org.opendaylight.netconf.keystore.plaintext.localfile.TestUtils.TEST_ENTRIES;
+import static org.opendaylight.netconf.keystore.plaintext.localfile.TestUtils.assertFileContains;
+import static org.opendaylight.netconf.keystore.plaintext.localfile.TestUtils.assertStorageContains;
+import static org.opendaylight.netconf.keystore.plaintext.localfile.TestUtils.prepareDataFile;
+import static org.opendaylight.netconf.keystore.plaintext.localfile.TestUtils.preparePropertiesFile;
+import static org.opendaylight.netconf.keystore.plaintext.localfile.TestUtils.prepareSecretFile;
+import static org.opendaylight.netconf.keystore.plaintext.localfile.TestUtils.secretFromFile;
+import static org.opendaylight.netconf.keystore.plaintext.localfile.TestUtils.storageEntry;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Collection;
+import java.util.List;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.io.TempDir;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.opendaylight.netconf.keystore.plaintext.api.MutablePlaintextStorage;
+
+@ExtendWith(MockitoExtension.class)
+public class PlaintextLocalFileStorageTest {
+
+    private static final StorageEntry ENTRY_1 = storageEntry("key-1", "value-1");
+    private static final StorageEntry ENTRY_1_MODIFIED = storageEntry("key-1", "value-1-modified");
+    private static final StorageEntry ENTRY_2 = storageEntry("key-2", "value-2");
+    private static final StorageEntry ENTRY_2_MODIFIED = storageEntry("key-2", "value-3-modified");
+    private static final StorageEntry ENTRY_3 = storageEntry("key-3", "value-3");
+    private static final StorageEntry ENTRY_3_MODIFIED = storageEntry("key-3", "value-3-modified");
+    private static final StorageEntry ENTRY_4 = storageEntry("key-4", "value-4");
+    private static final Collection<StorageEntry> INITIAL_DATA = List.of(ENTRY_1, ENTRY_2, ENTRY_3);
+    private static final Collection<StorageEntry> DEFAULT_DATA = List.of(storageEntry("admin", "admin"));
+
+    @TempDir
+    private static File tempDir;
+
+    @Mock
+    private PlaintextLocalFileStorage.Configuration config;
+
+    private static File storageFile;
+    private static File keyFile;
+    private static File importFile;
+
+    private byte[] secret;
+
+    @BeforeAll
+    static void beforeAll() {
+        // init file paths after temp dir is initialized
+        storageFile = new File(tempDir, "data-file");
+        keyFile = new File(tempDir, "key-file");
+        importFile = new File(tempDir, "import-file");
+    }
+
+    @BeforeEach
+    void beforeEach() {
+        secret = CipherUtils.generateSecret();
+        // clear dir content before test
+        for (var file : tempDir.listFiles()) {
+            if (file.isFile()) {
+                file.delete();
+            }
+        }
+    }
+
+    private void mockConfig() {
+        doReturn(keyFile.getAbsolutePath()).when(config).key$_$file();
+        doReturn(storageFile.getAbsolutePath()).when(config).data$_$file();
+        lenient().doReturn(importFile.getAbsolutePath()).when(config).import$_$from$_$file();
+    }
+
+    @Test
+    void initFromFile() throws IOException {
+        prepareDataFile(storageFile, secret, TEST_ENTRIES);
+        assertStorageContains(new PlaintextLocalFileStorage(secret, storageFile), TEST_ENTRIES);
+    }
+
+    @Test
+    void initFromConfig() throws IOException {
+        // key and encrypted data loaded from existing files
+        prepareSecretFile(keyFile, secret);
+        prepareDataFile(storageFile, secret, TEST_ENTRIES);
+        mockConfig();
+        assertStorageContains(new PlaintextLocalFileStorage(config), TEST_ENTRIES);
+    }
+
+    @Test
+    void initWithImportedData() throws IOException {
+        // key data loaded from existing file,
+        prepareSecretFile(keyFile, secret);
+        // data to be imported from unencrypted properties file
+        preparePropertiesFile(importFile, TEST_ENTRIES);
+        // no encrypted storage
+        assertFalse(storageFile.exists());
+        mockConfig();
+
+        assertStorageContains(new PlaintextLocalFileStorage(config), TEST_ENTRIES);
+        // data should be persisted into secure storage, original data file should be removed
+        assertTrue(storageFile.exists());
+        assertFileContains(storageFile, secret, TEST_ENTRIES);
+        assertFalse(importFile.exists());
+    }
+
+    @Test
+    void initWithDefaults() throws IOException {
+        // no files existing
+        assertFalse(keyFile.exists());
+        assertFalse(storageFile.exists());
+        mockConfig();
+
+        final var storage = new PlaintextLocalFileStorage(config);
+        // 32 byte secret generated and persisted
+        final var newSecret = secretFromFile(keyFile);
+        assertNotNull(newSecret);
+        assertEquals(32, newSecret.length);
+        // default data populated and persisted
+        assertStorageContains(storage, DEFAULT_DATA);
+        assertFileContains(storageFile, newSecret, DEFAULT_DATA);
+    }
+
+    @Test
+    void removeKey() throws IOException {
+        final var storage = initStorage();
+        assertStorageContains(storage, INITIAL_DATA);
+        // delete non existent
+        assertNull(storage.removeKey(ENTRY_4.key()));
+        // delete existent
+        assertArrayEquals(ENTRY_1.value(), storage.removeKey(ENTRY_1.key()));
+        assertArrayEquals(ENTRY_3.value(), storage.removeKey(ENTRY_3.key()));
+        // validate data
+        final var expected = List.of(ENTRY_2);
+        assertStorageContains(storage, expected);
+        assertFileContains(storageFile, secret, expected);
+    }
+
+    @Test
+    void removeEntry() throws IOException {
+        final var storage = initStorage();
+        assertStorageContains(storage, INITIAL_DATA);
+        // delete non existent
+        assertFalse(storage.removeEntry(ENTRY_4.key(), ENTRY_4.value()));
+        // value mismatches
+        assertFalse(storage.removeEntry(ENTRY_1.key(), ENTRY_1_MODIFIED.value()));
+        // delete existent
+        assertTrue(storage.removeEntry(ENTRY_1.key(), ENTRY_1.value()));
+        // validate data
+        final var expected = List.of(ENTRY_2, ENTRY_3);
+        assertStorageContains(storage, expected);
+        assertFileContains(storageFile, secret, expected);
+    }
+
+    @Test
+    void insertEntry() throws IOException {
+        final var storage = initStorage();
+        assertStorageContains(storage, INITIAL_DATA);
+        // key exists
+        assertArrayEquals(ENTRY_1.value(), storage.insertEntry(ENTRY_1.key(), ENTRY_1_MODIFIED.value()));
+        assertArrayEquals(ENTRY_2.value(), storage.insertEntry(ENTRY_2.key(), ENTRY_2_MODIFIED.value()));
+        assertArrayEquals(ENTRY_3.value(), storage.insertEntry(ENTRY_3.key(), ENTRY_3_MODIFIED.value()));
+        // ensure no data modified
+        assertStorageContains(storage, INITIAL_DATA);
+        // new entry
+        assertNull(storage.insertEntry(ENTRY_4.key(), ENTRY_4.value()));
+        // validate data
+        final var expected = List.of(ENTRY_1, ENTRY_2, ENTRY_3, ENTRY_4);
+        assertStorageContains(storage, expected);
+        assertFileContains(storageFile, secret, expected);
+    }
+
+    @Test
+    void putEntry() throws IOException {
+        final var storage = initStorage();
+        assertStorageContains(storage, INITIAL_DATA);
+        // same entry
+        assertArrayEquals(ENTRY_1.value(), storage.putEntry(ENTRY_1.key(), ENTRY_1.value()));
+        // update entries
+        assertArrayEquals(ENTRY_1.value(), storage.putEntry(ENTRY_1.key(), ENTRY_1_MODIFIED.value()));
+        assertArrayEquals(ENTRY_2.value(), storage.putEntry(ENTRY_2.key(), ENTRY_2_MODIFIED.value()));
+        assertArrayEquals(ENTRY_3.value(), storage.putEntry(ENTRY_3.key(), ENTRY_3_MODIFIED.value()));
+        // insert new
+        assertNull(storage.putEntry(ENTRY_4.key(), ENTRY_4.value()));
+        // validate data
+        final var expected = List.of(ENTRY_1_MODIFIED, ENTRY_2_MODIFIED, ENTRY_3_MODIFIED, ENTRY_4);
+        assertStorageContains(storage, expected);
+        assertFileContains(storageFile, secret, expected);
+    }
+
+    private MutablePlaintextStorage initStorage() throws IOException {
+        prepareDataFile(storageFile, secret, INITIAL_DATA);
+        return new PlaintextLocalFileStorage(secret, storageFile);
+    }
+}
