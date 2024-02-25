14c367f62a364e782a260cb46410dc1b9733c14f
Robert Varga <robert.varga@pantheon.tech>
2024-02-25 11:31:27 +0100

    Improve ClientTransportChannelListener
    Since we are completing the same future, there is no need to allocate
    a new FutureListener and capture the promise.
    
    Make ClientTransportChannelListener a FutureListener itself, and pick
    the result from the future we get handed.
    
    Change-Id: I14af7d49af8b02ea530c7c994968d2adf48083a0
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- protocol/netconf-client/src/main/java/org/opendaylight/netconf/client/ClientTransportChannelListener.java
+++ protocol/netconf-client/src/main/java/org/opendaylight/netconf/client/ClientTransportChannelListener.java
@@ -11,11 +11,17 @@ import static java.util.Objects.requireNonNull;
 
 import com.google.common.util.concurrent.ListenableFuture;
 import com.google.common.util.concurrent.SettableFuture;
+import io.netty.util.concurrent.Future;
+import io.netty.util.concurrent.FutureListener;
 import org.eclipse.jdt.annotation.NonNull;
 import org.opendaylight.netconf.transport.api.TransportChannel;
 import org.opendaylight.netconf.transport.api.TransportChannelListener;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-final class ClientTransportChannelListener implements TransportChannelListener {
+final class ClientTransportChannelListener implements TransportChannelListener, FutureListener<NetconfClientSession> {
+    private static final Logger LOG = LoggerFactory.getLogger(ClientTransportChannelListener.class);
+
     private final @NonNull SettableFuture<NetconfClientSession> sessionFuture = SettableFuture.create();
     private final ClientChannelInitializer initializer;
 
@@ -29,21 +35,25 @@ final class ClientTransportChannelListener implements TransportChannelListener {
 
     @Override
     public void onTransportChannelEstablished(final TransportChannel channel) {
+        LOG.debug("Initializing established transport {}", channel);
         final var nettyChannel = channel.channel();
         final var promise = nettyChannel.eventLoop().<NetconfClientSession>newPromise();
         initializer.initialize(nettyChannel, promise);
-        promise.addListener(ignored -> {
-            final var cause = promise.cause();
-            if (cause != null) {
-                sessionFuture.setException(cause);
-            } else {
-                sessionFuture.set(promise.getNow());
-            }
-        });
+        promise.addListener(this);
     }
 
     @Override
     public void onTransportChannelFailed(final Throwable cause) {
         sessionFuture.setException(cause);
+    }
+
+    @Override
+    public void operationComplete(final Future<NetconfClientSession> future) throws Exception {
+        final var cause = future.cause();
+        if (cause != null) {
+            sessionFuture.setException(cause);
+        } else {
+            sessionFuture.set(future.getNow());
+        }
     }
 }
