13f355be7ddde333ece9e94344216648dc8f85bd
Robert Varga <robert.varga@pantheon.tech>
2024-02-25 08:43:27 +0100

    Clean up NetconfNodeActor fields
    Do not retain NetconfTopologySetup, as we are using only actor system,
    which is a constant for the actor anyway.
    
    Retain SchemaResourcesDTO without extracting things out of it, as we
    will be evolving it a bit -- and update them when
    RefreshSetupMasterActorData comes in.
    
    JIRA: NETCONF-840
    Change-Id: I2b07aa84ae1b51153cc4144bc739ef8cae0480f6
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- apps/netconf-topology-singleton/src/main/java/org/opendaylight/netconf/topology/singleton/messages/RefreshSlaveActor.java
+++ apps/netconf-topology-singleton/src/main/java/org/opendaylight/netconf/topology/singleton/messages/RefreshSlaveActor.java
@@ -7,25 +7,21 @@
  */
 package org.opendaylight.netconf.topology.singleton.messages;
 
+import static java.util.Objects.requireNonNull;
+
 import akka.util.Timeout;
 import org.opendaylight.netconf.client.mdsal.api.RemoteDeviceId;
 import org.opendaylight.netconf.topology.singleton.impl.utils.NetconfTopologySetup;
-import org.opendaylight.yangtools.yang.model.repo.api.SchemaRepository;
-import org.opendaylight.yangtools.yang.model.repo.spi.SchemaSourceRegistry;
 
 public class RefreshSlaveActor {
-    private final SchemaRepository schemaRepository;
     private final RemoteDeviceId id;
-    private final SchemaSourceRegistry schemaRegistry;
     private final NetconfTopologySetup setup;
     private final Timeout actorResponseWaitTime;
 
     public RefreshSlaveActor(final NetconfTopologySetup setup, final RemoteDeviceId id,
-                             final Timeout actorResponseWaitTime) {
-        this.setup = setup;
-        this.id = id;
-        schemaRegistry = setup.getSchemaResourcesDTO().getSchemaRegistry();
-        schemaRepository = setup.getSchemaResourcesDTO().getSchemaRepository();
+            final Timeout actorResponseWaitTime) {
+        this.setup = requireNonNull(setup);
+        this.id = requireNonNull(id);
         this.actorResponseWaitTime = actorResponseWaitTime;
     }
 
@@ -33,16 +29,8 @@ public class RefreshSlaveActor {
         return actorResponseWaitTime;
     }
 
-    public SchemaRepository getSchemaRepository() {
-        return schemaRepository;
-    }
-
     public RemoteDeviceId getId() {
         return id;
-    }
-
-    public SchemaSourceRegistry getSchemaRegistry() {
-        return schemaRegistry;
     }
 
     public NetconfTopologySetup getSetup() {
