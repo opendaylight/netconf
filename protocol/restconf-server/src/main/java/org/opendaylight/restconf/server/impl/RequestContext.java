657269b3910b41ae6dfd9d0c90c3ed212b496321
Ruslan Kashapov <ruslan.kashapov@pantheon.tech>
2024-03-11 10:57:14 +0100

    WIP Implement Netty based Restconf server
    Includes
    - AAA Shiro integration for Basic Auth
    - Request dispatcher mapping /data GET and PUT
    
    JIRA: NETCONF-873
    Change-Id: I18b73302973631914f402997f0bf588b3bea7676
    Signed-off-by: Ivan Hrasko <ivan.hrasko@pantheon.tech>
    Signed-off-by: Ruslan Kashapov <ruslan.kashapov@pantheon.tech>

---
+++ protocol/restconf-server/src/main/java/org/opendaylight/restconf/server/impl/RequestContext.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2024 PANTHEON.tech s.r.o. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.restconf.server.impl;
+
+import com.google.common.util.concurrent.FutureCallback;
+import io.netty.handler.codec.http.FullHttpRequest;
+import io.netty.handler.codec.http.FullHttpResponse;
+import io.netty.handler.codec.http.HttpMethod;
+import io.netty.util.AsciiString;
+import java.io.InputStream;
+import java.util.List;
+import java.util.Map;
+import org.eclipse.jdt.annotation.NonNull;
+import org.eclipse.jdt.annotation.Nullable;
+import org.opendaylight.restconf.api.query.PrettyPrintParam;
+
+interface RequestContext {
+
+    @NonNull HttpMethod method();
+
+    @Nullable AsciiString contentType();
+
+    @NonNull String contextPath();
+
+    @NonNull Map<String, List<String>> queryParameters();
+
+    @NonNull InputStream requestBody();
+
+    @NonNull FullHttpRequest request();
+
+    @NonNull FutureCallback<FullHttpResponse> callback();
+
+    boolean hasContextPath();
+
+    @NonNull PrettyPrintParam defaultPrettyPrint();
+
+    @NonNull AsciiString defaultContentType();
+}
