999e1c6d86e4117d58e76bd547b093465ac79081
Ruslan Kashapov <ruslan.kashapov@pantheon.tech>
2024-03-11 10:57:14 +0100

    Draft implementation of Netty based Restconf server
    Includes
    - Alternative Restconf northbound OSGi service
    - AAA Shiro integration for Basic Auth
    - URI path parser for API resources and root discoveries
    - Request dispatcher processing /data GET and POST +UT
    
    JIRA: NETCONF-873
    Change-Id: I18b73302973631914f402997f0bf588b3bea7676
    Signed-off-by: Ivan Hrasko <ivan.hrasko@pantheon.tech>
    Signed-off-by: Ruslan Kashapov <ruslan.kashapov@pantheon.tech>

---
+++ protocol/restconf-server/src/main/java/org/opendaylight/restconf/server/impl/RequestUtils.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright (c) 2024 PANTHEON.tech s.r.o. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.restconf.server.impl;
+
+import com.google.common.util.concurrent.FutureCallback;
+import io.netty.handler.codec.http.FullHttpResponse;
+import java.io.InputStream;
+import java.text.ParseException;
+import java.util.function.Function;
+import org.opendaylight.restconf.api.ApiPath;
+import org.opendaylight.restconf.api.ConsumableBody;
+import org.opendaylight.restconf.server.api.AbstractServerRequest;
+import org.opendaylight.restconf.server.api.ServerException;
+import org.opendaylight.restconf.server.api.ServerRequest;
+
+final class RequestUtils {
+
+    private RequestUtils() {
+        // hidden on purpose
+    }
+
+    static ApiPath extractApiPath(final RequestParameters params) {
+        try {
+            return ApiPath.parse(params.pathParameters().childIdentifier());
+        } catch (ParseException e) {
+            // FIXME use proper exception to build expected RESTCONF response
+            throw new IllegalArgumentException(e.getMessage(), e);
+        }
+    }
+
+    static <T> ServerRequest<T> serverRequest(final RequestParameters params,
+            final FutureCallback<FullHttpResponse> callback, final Function<T, FullHttpResponse> transformer) {
+        return new AbstractServerRequest<T>(params.queryParameters(), params.defaultPrettyPrint()) {
+            @Override
+            protected void onSuccess(final T result) {
+                callback.onSuccess(transformer.apply(result));
+            }
+
+            @Override
+            protected void onFailure(final ServerException failure) {
+                ResponseUtils.handleException(params, callback, failure);
+            }
+        };
+    }
+
+    static <T extends ConsumableBody> T requestBody(final RequestParameters params,
+            final Function<InputStream, T> jsonBodyBuilder, final Function<InputStream, T> xmlBodyBuilder) {
+        return ContentTypes.JSON_TYPES.contains(params.contentType())
+            ? jsonBodyBuilder.apply(params.requestBody()) : xmlBodyBuilder.apply(params.requestBody());
+    }
+}
