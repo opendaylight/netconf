4af430fda6cc47b3baaf6175894bb4f8933f8e29
Ruslan Kashapov <ruslan.kashapov@pantheon.tech>
2024-03-11 10:57:14 +0100

    Draft implementation of Netty based Restconf server
    Includes
    - Init via Restconf northbound OSGi service
    - AAA Shiro integration for Basic Auth and Principal delivery
    - URI path parser for API resources and root discoveries
    - Request dispatcher processing /data GET and POST +UT
    
    JIRA: NETCONF-873
    Change-Id: I18b73302973631914f402997f0bf588b3bea7676
    Signed-off-by: Ivan Hrasko <ivan.hrasko@pantheon.tech>
    Signed-off-by: Ruslan Kashapov <ruslan.kashapov@pantheon.tech>

---
+++ protocol/restconf-server/src/main/java/org/opendaylight/restconf/server/RequestUtils.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2024 PANTHEON.tech s.r.o. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.restconf.server;
+
+import java.io.InputStream;
+import java.text.ParseException;
+import java.util.function.Function;
+import org.opendaylight.restconf.api.ApiPath;
+import org.opendaylight.restconf.api.ConsumableBody;
+
+final class RequestUtils {
+
+    private RequestUtils() {
+        // hidden on purpose
+    }
+
+    static ApiPath extractApiPath(final RequestParameters params) {
+        try {
+            return ApiPath.parse(params.pathParameters().childIdentifier());
+        } catch (ParseException e) {
+            // FIXME use proper exception to build expected RESTCONF response
+            throw new IllegalArgumentException(e.getMessage(), e);
+        }
+    }
+
+    static <T extends ConsumableBody> T requestBody(final RequestParameters params,
+            final Function<InputStream, T> jsonBodyBuilder, final Function<InputStream, T> xmlBodyBuilder) {
+        return NettyMediaTypes.JSON_TYPES.contains(params.contentType())
+            ? jsonBodyBuilder.apply(params.requestBody()) : xmlBodyBuilder.apply(params.requestBody());
+    }
+}
