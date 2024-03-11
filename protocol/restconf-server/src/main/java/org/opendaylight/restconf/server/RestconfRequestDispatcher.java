256fb71c9c8a464b24efcc32e186627f6e6392ef
Ruslan Kashapov <ruslan.kashapov@pantheon.tech>
2024-03-11 10:57:14 +0100

    Draft implementation of Netty based Restconf server
    Includes
    - Alternative Restconf northbound OSGi service
    - AAA Shiro integration for Basic Auth
    - URI path parser for API resources and root discoveries
    - Request dispatcher processing /data GET and POST +UT
    
    JIRA: NETCONF-873
    Change-Id: I18b73302973631914f402997f0bf588b3bea7676
    Signed-off-by: Ivan Hrasko <ivan.hrasko@pantheon.tech>
    Signed-off-by: Ruslan Kashapov <ruslan.kashapov@pantheon.tech>

---
+++ protocol/restconf-server/src/main/java/org/opendaylight/restconf/server/RestconfRequestDispatcher.java
@@ -0,0 +1,82 @@
+/*
+ * Copyright (c) 2024 PANTHEON.tech s.r.o. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.restconf.server;
+
+import static java.util.Objects.requireNonNull;
+import static org.opendaylight.restconf.server.ResponseUtils.handleException;
+
+import com.google.common.util.concurrent.FutureCallback;
+import io.netty.handler.codec.http.FullHttpRequest;
+import io.netty.handler.codec.http.FullHttpResponse;
+import io.netty.handler.codec.http.HttpResponseStatus;
+import io.netty.util.AsciiString;
+import org.opendaylight.netconf.transport.http.RequestDispatcher;
+import org.opendaylight.restconf.api.query.PrettyPrintParam;
+import org.opendaylight.restconf.server.api.RestconfServer;
+import org.opendaylight.restconf.server.spi.ErrorTagMapping;
+import org.opendaylight.yangtools.yang.common.ErrorTag;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public final class RestconfRequestDispatcher implements RequestDispatcher {
+    private static final Logger LOG = LoggerFactory.getLogger(RestconfRequestDispatcher.class);
+
+    private final String basePath;
+    private final RestconfServer restconfService;
+    private final ErrorTagMapping errorTagMapping;
+    private final AsciiString defaultAcceptType;
+    private final PrettyPrintParam defaultPrettyPrint;
+
+    public RestconfRequestDispatcher(final RestconfServer restconfService, final String topLevelResource,
+        final ErrorTagMapping errorTagMapping, final AsciiString defaultAcceptType,
+        final PrettyPrintParam defaultPrettyPrint) {
+        this.restconfService = requireNonNull(restconfService);
+        basePath = "/" + requireNonNull(topLevelResource);
+        this.errorTagMapping = requireNonNull(errorTagMapping);
+        this.defaultAcceptType = requireNonNull(defaultAcceptType);
+        this.defaultPrettyPrint = requireNonNull(defaultPrettyPrint);
+
+        LOG.info("{} initialized with service {}", getClass().getSimpleName(), restconfService.getClass());
+        LOG.info("Base path: {}, default accept: {}, default pretty print: {}",
+            basePath, defaultAcceptType, defaultPrettyPrint.value());
+    }
+
+    @Override
+    @SuppressWarnings("IllegalCatch")
+    public void dispatch(final FullHttpRequest request, final FutureCallback<FullHttpResponse> callback) {
+        LOG.debug("Dispatching {} {}", request.method(), request.uri());
+
+        try {
+            final var params = new RequestParameters(basePath, request,
+                    errorTagMapping, defaultAcceptType, defaultPrettyPrint);
+            final var apiResource = params.pathParameters().apiResource();
+            if (PathParameters.DATA.equals(apiResource)) {
+                DataRequestProcessor.processDataRequest(params, restconfService, callback);
+            } else if (PathParameters.OPERATIONS.equals(apiResource)) {
+                // TODO implement
+                callback.onSuccess(ResponseUtils.simpleResponse(params, HttpResponseStatus.NOT_IMPLEMENTED));
+            } else if (PathParameters.YANG_LIBRARY_VERSION.equals(apiResource)) {
+                // TODO implement
+                callback.onSuccess(ResponseUtils.simpleResponse(params, HttpResponseStatus.NOT_IMPLEMENTED));
+            } else if (PathParameters.MODULES.equals(apiResource)) {
+                // TODO implement
+                callback.onSuccess(ResponseUtils.simpleResponse(params, HttpResponseStatus.NOT_IMPLEMENTED));
+            } else if (PathParameters.HOST_META.equals(apiResource)) {
+                // TODO implement
+                callback.onSuccess(ResponseUtils.simpleResponse(params, HttpResponseStatus.NOT_IMPLEMENTED));
+            } else if (PathParameters.HOST_META_JSON.equals(apiResource)) {
+                // TODO implement
+                callback.onSuccess(ResponseUtils.simpleResponse(params, HttpResponseStatus.NOT_IMPLEMENTED));
+            } else {
+                callback.onSuccess(ResponseUtils.simpleErrorResponse(params, ErrorTag.DATA_MISSING));
+            }
+        } catch (RuntimeException e) {
+            handleException(request, errorTagMapping, callback, e);
+        }
+    }
+}
