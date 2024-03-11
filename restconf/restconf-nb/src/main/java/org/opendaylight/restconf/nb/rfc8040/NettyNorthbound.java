444e376fcf9ed9fcc9410811edff4cc7d00d8984
Ivan Hrasko <ivan.hrasko@pantheon.tech>
2024-03-11 16:06:44 +0100

    WIP: Implement NettyNorthbound
    Implement NettyNorthbound which bootstraps Restconf Netty.
    
    JIRA: NETCONF-873
    Change-Id: Iad9b7489b5282566c0c6d9f325deb21733dfb87f
    Signed-off-by: Ivan Hrasko <ivan.hrasko@pantheon.tech>

---
+++ restconf/restconf-nb/src/main/java/org/opendaylight/restconf/nb/rfc8040/NettyNorthbound.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2024 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.restconf.nb.rfc8040;
+
+import com.google.common.annotations.Beta;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+
+@Beta
+@Component(service = { })
+public final class NettyNorthbound implements AutoCloseable {
+
+    @Activate
+    public NettyNorthbound() {
+    }
+
+    @Deactivate
+    @Override
+    public void close() {
+    }
+}
