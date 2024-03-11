05b1e5f5bd0bc69788e7a62dc3bc472247bf9045
Ruslan Kashapov <ruslan.kashapov@pantheon.tech>
2024-03-11 10:57:14 +0100

    Netty based Restconf server
    - Init via restconf-nb OSGi service
    - AAA Shiro integration for Basic Auth and Principal delivery
    - SSE overlay adapter integrating RestconfStream.Registry
    - Request dispatcher processing for restconf resource URIs
      plus /modules and resource discovery URIs
    - error responses using restconf model
    
    JIRA: NETCONF-873
    Change-Id: I18b73302973631914f402997f0bf588b3bea7676
    Signed-off-by: Ivan Hrasko <ivan.hrasko@pantheon.tech>
    Signed-off-by: Matej Sramcik <matej.sramcik@pantheon.tech>
    Signed-off-by: lubos-cicut <lubos.cicut@pantheon.tech>
    Signed-off-by: Oleksandr Zharov <oleksandr.zharov@pantheon.tech>
    Signed-off-by: Ruslan Kashapov <ruslan.kashapov@pantheon.tech>

---
+++ protocol/restconf-server/src/main/java/org/opendaylight/restconf/server/ModulesRequestProcessor.java
@@ -0,0 +1,130 @@
+/*
+ * Copyright (c) 2024 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.restconf.server;
+
+import static org.opendaylight.restconf.server.ResponseUtils.responseBuilder;
+import static org.opendaylight.restconf.server.ResponseUtils.simpleErrorResponse;
+import static org.opendaylight.restconf.server.ResponseUtils.simpleResponse;
+import static org.opendaylight.restconf.server.ResponseUtils.unmappedRequestErrorResponse;
+
+import com.google.common.annotations.VisibleForTesting;
+import com.google.common.util.concurrent.FutureCallback;
+import io.netty.handler.codec.http.FullHttpResponse;
+import io.netty.handler.codec.http.HttpHeaderNames;
+import io.netty.handler.codec.http.HttpMethod;
+import io.netty.handler.codec.http.HttpResponseStatus;
+import io.netty.util.AsciiString;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import org.opendaylight.restconf.api.ApiPath;
+import org.opendaylight.restconf.api.FormattableBody;
+import org.opendaylight.restconf.server.api.ModulesGetResult;
+import org.opendaylight.restconf.server.api.RestconfServer;
+import org.opendaylight.restconf.server.api.ServerRequest;
+import org.opendaylight.yangtools.yang.common.ErrorTag;
+
+/**
+ * Static processor implementation serving {@code /yang-library-version} and {@code /modules} path requests.
+ */
+final class ModulesRequestProcessor {
+    @VisibleForTesting
+    static final String REVISION = "revision";
+    @VisibleForTesting
+    static final String MISSING_FILENAME_ERROR = "Module name is missing";
+    @VisibleForTesting
+    static final String SOURCE_READ_FAILURE_ERROR = "Failure reading module source: ";
+
+    private ModulesRequestProcessor() {
+        // hidden on purpose
+    }
+
+    static void processYangLibraryVersion(final RequestParameters params, final RestconfServer service,
+            final FutureCallback<FullHttpResponse> callback) {
+        if (HttpMethod.GET.equals(params.method())) {
+            getYangLibraryVersion(params, service, callback);
+        } else {
+            callback.onSuccess(unmappedRequestErrorResponse(params));
+        }
+    }
+
+    static void processModules(final RequestParameters params, final RestconfServer service,
+            final FutureCallback<FullHttpResponse> callback) {
+        if (HttpMethod.GET.equals(params.method())) {
+            getModule(params, service, callback);
+        } else {
+            callback.onSuccess(unmappedRequestErrorResponse(params));
+        }
+    }
+
+    private static void getYangLibraryVersion(final RequestParameters params, final RestconfServer service,
+        final FutureCallback<FullHttpResponse> callback) {
+        final var request = new NettyServerRequest<FormattableBody>(params, callback,
+            result -> responseBuilder(params, HttpResponseStatus.OK).setBody(result).build());
+        service.yangLibraryVersionGET(request);
+    }
+
+    private static void getModule(final RequestParameters params, final RestconfServer service,
+            final FutureCallback<FullHttpResponse> callback) {
+        final var file = extractModuleFile(params.pathParameters().childIdentifier());
+        final var revision = params.queryParameters().lookup(REVISION);
+        if (file.name().isEmpty()) {
+            callback.onSuccess(simpleErrorResponse(params, ErrorTag.MISSING_ELEMENT, MISSING_FILENAME_ERROR));
+        }
+        final var acceptYang = params.requestHeaders()
+            .contains(HttpHeaderNames.ACCEPT, NettyMediaTypes.APPLICATION_YANG, true);
+        final var acceptYin = params.requestHeaders()
+            .contains(HttpHeaderNames.ACCEPT, NettyMediaTypes.APPLICATION_YIN_XML, true);
+        if (acceptYin && !acceptYang) {
+            // YIN if explicitly requested
+            final var request = getModuleRequest(params, callback,NettyMediaTypes.APPLICATION_YIN_XML);
+            if (file.mountPath.isEmpty()) {
+                service.modulesYinGET(request, file.name(), revision);
+            } else {
+                service.modulesYinGET(request, file.mountPath(), file.name(), revision);
+            }
+        } else {
+            // YANG by default, incl accept any
+            final var request = getModuleRequest(params, callback,NettyMediaTypes.APPLICATION_YANG);
+            if (file.mountPath.isEmpty()) {
+                service.modulesYangGET(request, file.name(), revision);
+            } else {
+                service.modulesYangGET(request, file.mountPath(), file.name(), revision);
+            }
+        }
+    }
+
+    private static ServerRequest<ModulesGetResult> getModuleRequest(final RequestParameters params,
+            final FutureCallback<FullHttpResponse> callback, final AsciiString mediaType) {
+        return new NettyServerRequest<>(params, callback,
+            result -> {
+                final byte[] bytes;
+                try {
+                    bytes = result.source().asByteSource(StandardCharsets.UTF_8).read();
+                } catch (IOException e) {
+                    throw new ServerErrorException(ErrorTag.OPERATION_FAILED,
+                       SOURCE_READ_FAILURE_ERROR + e.getMessage(), e);
+                }
+                return simpleResponse(params, HttpResponseStatus.OK, mediaType, bytes);
+            });
+    }
+
+    private static ModuleFile extractModuleFile(final String path) {
+        // optional mountPath followed by file name separated by slash
+        final var lastIndex = path.length() - 1;
+        final var splitIndex = path.lastIndexOf('/');
+        if (splitIndex < 0) {
+            return new ModuleFile(ApiPath.empty(), path);
+        }
+        final var apiPath = RequestUtils.extractApiPath(path.substring(0, splitIndex));
+        final var name = splitIndex == lastIndex ? "" : path.substring(splitIndex + 1);
+        return new ModuleFile(apiPath, name);
+    }
+
+    private record ModuleFile(ApiPath mountPath, String name) {
+    }
+}
---
+++ protocol/restconf-server/src/main/java/org/opendaylight/restconf/server/RequestParameters.java
@@ -0,0 +1,215 @@
+/*
+ * Copyright (c) 2024 PANTHEON.tech s.r.o. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.restconf.server;
+
+import io.netty.buffer.ByteBufInputStream;
+import io.netty.handler.codec.http.FullHttpRequest;
+import io.netty.handler.codec.http.HttpHeaders;
+import io.netty.handler.codec.http.HttpMethod;
+import io.netty.handler.codec.http.HttpUtil;
+import io.netty.handler.codec.http.HttpVersion;
+import io.netty.handler.codec.http.QueryStringDecoder;
+import io.netty.util.AsciiString;
+import java.io.InputStream;
+import java.nio.charset.Charset;
+import java.security.Principal;
+import org.eclipse.jdt.annotation.NonNull;
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.eclipse.jdt.annotation.Nullable;
+import org.opendaylight.restconf.api.QueryParameters;
+import org.opendaylight.restconf.api.query.PrettyPrintParam;
+import org.opendaylight.restconf.server.spi.ErrorTagMapping;
+
+/**
+ * Request parameters. Provides parameters parsed from request object.
+ */
+@NonNullByDefault
+final class RequestParameters {
+    private final String basePath;
+    private final PathParameters pathParameters;
+    private final AsciiString contentType;
+    private final MediaTypeGroup mediaTypeGroup;
+    private final AsciiString defaultAcceptType;
+    private final QueryParameters queryParameters;
+    private final FullHttpRequest request;
+    private final ErrorTagMapping errorTagMapping;
+    private final Principal principal;
+    private final PrettyPrintParam defaultPrettyPrint;
+
+    RequestParameters(final String basePath, final FullHttpRequest request, final @Nullable Principal principal,
+            final ErrorTagMapping errorTagMapping, final AsciiString defaultAcceptType,
+            final PrettyPrintParam defaultPrettyPrint) {
+        this.basePath = basePath;
+        this.request = request;
+        this.principal = principal;
+        this.errorTagMapping = errorTagMapping;
+        this.defaultAcceptType = defaultAcceptType;
+        this.defaultPrettyPrint = defaultPrettyPrint;
+
+        contentType = extractMimeType(request, defaultAcceptType);
+        mediaTypeGroup = MediaTypeGroup.of(contentType);
+
+        final var charset = HttpUtil.getCharset(request, Charset.defaultCharset());
+        final var decoder = new QueryStringDecoder(request.uri(), charset);
+        pathParameters = PathParameters.from(decoder.path(), basePath);
+        queryParameters = QueryParameters.ofMultiValue(decoder.parameters());
+    }
+
+    /**
+     * Returns the HTTP request method.
+     *
+     * @return HTTP request method
+     */
+    public HttpMethod method() {
+        return request.method();
+    }
+
+    /**
+     * Returns content-type header value if defined.
+     *
+     * @return content-type value as {@link AsciiString} if not defined, null otherwise
+     */
+    public AsciiString contentType() {
+        return contentType;
+    }
+
+    /**
+     * Returns media type group the content-type of current request belongs to.
+     *
+     * @return media type group
+     */
+    public MediaTypeGroup mediaTypeGroup() {
+        return mediaTypeGroup;
+    }
+
+    /**
+     * Returns base path of URI configured.
+     *
+     * @return base path value
+     */
+    public String basePath() {
+        return basePath;
+    }
+
+    /**
+     * Path parameters extracted from request URI.
+     *
+     * @return path parameters
+     * @see PathParameters
+     */
+    public @NonNull PathParameters pathParameters() {
+        return pathParameters;
+    }
+
+    /**
+     * Returns query parameters.
+     *
+     * @return query parameters
+     */
+    public @NonNull QueryParameters queryParameters() {
+        return queryParameters;
+    }
+
+    /**
+     * Returns request headers.
+     *
+     * @return request headers
+     */
+    public @NonNull HttpHeaders requestHeaders() {
+        return request.headers();
+    }
+
+    /**
+     * Returns HTTP protocol version.
+     *
+     * @return HTTP protocol version
+     */
+    public @NonNull HttpVersion protocolVersion() {
+        return request.protocolVersion();
+    }
+
+    /**
+     * Returns request body (content).
+     *
+     * @return request body as {@link InputStream}
+     */
+    public @NonNull InputStream requestBody() {
+        return new ByteBufInputStream(request.content());
+    }
+
+    /**
+     * Returns configured default content-type value for response encoding.
+     * To be used if 'accept' header is absent.
+     *
+     * @return default value configured
+     */
+    public AsciiString defaultAcceptType() {
+        return defaultAcceptType;
+    }
+
+    /**
+     * Returns configured default value for pretty print parameter.
+     *
+     * @return default value configured
+     */
+    public PrettyPrintParam defaultPrettyPrint() {
+        return defaultPrettyPrint;
+    }
+
+    /**
+     * Returns pretty print parameter value to be used for current request .
+     *
+     * @return parameter value
+     */
+    public PrettyPrintParam prettyPrint() {
+        final var requestParam = queryParameters.lookup(PrettyPrintParam.uriName, PrettyPrintParam::forUriValue);
+        return requestParam != null ? requestParam : defaultPrettyPrint;
+    }
+
+    /**
+     * Returns error tag mapping configured.
+     *
+     * @return error tag mapping instance
+     */
+    public ErrorTagMapping errorTagMapping() {
+        return errorTagMapping;
+    }
+
+    /**
+     * Returns user principal associated with current request.
+     *
+     * @return principal object if defined, null otherwise
+     */
+    public @Nullable Principal principal() {
+        return principal;
+    }
+
+    private static AsciiString extractMimeType(final FullHttpRequest request, final AsciiString defaultType) {
+        final var mimeType = HttpUtil.getMimeType(request);
+        if (mimeType != null) {
+            return AsciiString.of(mimeType);
+        }
+        // when request body is empty content-type value plays no role, and eligible to be absent,
+        // in this case apply default type to prevent unsupported media type error when checked subsequently
+        return request.content().readableBytes() == 0 ? defaultType : AsciiString.EMPTY_STRING;
+    }
+
+    @NonNullByDefault
+    enum MediaTypeGroup {
+        RESTCONF, YANG_PATCH, OTHER;
+        static MediaTypeGroup of(AsciiString mediaType) {
+            if (NettyMediaTypes.RESTCONF_TYPES.contains(mediaType)) {
+                return RESTCONF;
+            }
+            if (NettyMediaTypes.YANG_PATCH_TYPES.contains(mediaType)) {
+                return YANG_PATCH;
+            }
+            return OTHER;
+        }
+    }
+}
