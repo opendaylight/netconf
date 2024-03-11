3857a319b707332d2d7002d2a50c794a2bd70239
Ruslan Kashapov <ruslan.kashapov@pantheon.tech>
2024-03-11 10:57:14 +0100

    Netty based Restconf server
    - Init via restconf-nb OSGi service, TLS supported
    - AAA Shiro integration for Basic Auth and Principal delivery
    - SSE overlay adapter integrating RestconfStream.Registry
    - Request dispatcher processing for restconf resource URIs
      plus /modules and resource discovery URIs
    - error responses using restconf model
    
    JIRA: NETCONF-873
    Change-Id: I18b73302973631914f402997f0bf588b3bea7676
    Signed-off-by: Ivan Hrasko <ivan.hrasko@pantheon.tech>
    Signed-off-by: Matej Sramcik <matej.sramcik@pantheon.tech>
    Signed-off-by: lubos-cicut <lubos.cicut@pantheon.tech>
    Signed-off-by: Oleksandr Zharov <oleksandr.zharov@pantheon.tech>
    Signed-off-by: Ruslan Kashapov <ruslan.kashapov@pantheon.tech>

---
+++ protocol/restconf-server/src/main/java/org/opendaylight/restconf/server/RequestParameters.java
@@ -0,0 +1,197 @@
+/*
+ * Copyright (c) 2024 PANTHEON.tech s.r.o. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.restconf.server;
+
+import io.netty.buffer.ByteBufInputStream;
+import io.netty.handler.codec.http.FullHttpRequest;
+import io.netty.handler.codec.http.HttpHeaderNames;
+import io.netty.handler.codec.http.HttpHeaders;
+import io.netty.handler.codec.http.HttpVersion;
+import io.netty.handler.codec.http.QueryStringDecoder;
+import io.netty.util.AsciiString;
+import java.io.InputStream;
+import java.net.URI;
+import java.nio.charset.StandardCharsets;
+import java.security.Principal;
+import org.eclipse.jdt.annotation.NonNull;
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.eclipse.jdt.annotation.Nullable;
+import org.opendaylight.restconf.api.QueryParameters;
+import org.opendaylight.restconf.api.query.PrettyPrintParam;
+import org.opendaylight.restconf.server.spi.ErrorTagMapping;
+
+/**
+ * Request parameters. Provides parameters parsed from request object.
+ */
+@NonNullByDefault
+final class RequestParameters {
+    private final URI baseUri;
+    private final PathParameters pathParameters;
+    private final AsciiString contentType;
+    private final AsciiString defaultAcceptType;
+    private final QueryParameters queryParameters;
+    private final FullHttpRequest request;
+    private final ErrorTagMapping errorTagMapping;
+    private final Principal principal;
+    private final PrettyPrintParam defaultPrettyPrint;
+
+    RequestParameters(final URI baseUri, final FullHttpRequest request, final @Nullable Principal principal,
+        final ErrorTagMapping errorTagMapping, final AsciiString defaultAcceptType,
+        final PrettyPrintParam defaultPrettyPrint) {
+        this.baseUri = baseUri;
+        this.request = request;
+        this.principal = principal;
+        this.errorTagMapping = errorTagMapping;
+        this.defaultAcceptType = defaultAcceptType;
+        this.defaultPrettyPrint = defaultPrettyPrint;
+
+        contentType = extractContentType(request, defaultAcceptType);
+        final var decoder = new QueryStringDecoder(request.uri(), StandardCharsets.UTF_8);
+        pathParameters = PathParameters.from(decoder.path(), baseUri.getPath());
+        queryParameters = QueryParameters.ofMultiValue(decoder.parameters());
+    }
+
+    /**
+     * Returns enum representation of HTTP request method.
+     *
+     * @return request method
+     */
+    public Method method() {
+        return org.opendaylight.restconf.server.Method.of(request.method());
+    }
+
+    /**
+     * Returns content-type header value if defined.
+     *
+     * @return content-type value as {@link AsciiString} if not defined, null otherwise
+     */
+    public AsciiString contentType() {
+        return contentType;
+    }
+
+    /**
+     * Returns base URI configured.
+     *
+     * @return base URI value
+     */
+    public URI baseUri() {
+        return baseUri;
+    }
+
+    /**
+     * Returns base path of URI configured.
+     *
+     * @return base path value
+     */
+    public String basePath() {
+        return baseUri.getPath();
+    }
+
+    /**
+     * Path parameters extracted from request URI.
+     *
+     * @return path parameters
+     * @see PathParameters
+     */
+    public @NonNull PathParameters pathParameters() {
+        return pathParameters;
+    }
+
+    /**
+     * Returns query parameters.
+     *
+     * @return query parameters
+     */
+    public @NonNull QueryParameters queryParameters() {
+        return queryParameters;
+    }
+
+    /**
+     * Returns request headers.
+     *
+     * @return request headers
+     */
+    public @NonNull HttpHeaders requestHeaders() {
+        return request.headers();
+    }
+
+    /**
+     * Returns HTTP protocol version.
+     *
+     * @return HTTP protocol version
+     */
+    public @NonNull HttpVersion protocolVersion() {
+        return request.protocolVersion();
+    }
+
+    /**
+     * Returns request body (content).
+     *
+     * @return request body as {@link InputStream}
+     */
+    public @NonNull InputStream requestBody() {
+        return new ByteBufInputStream(request.content());
+    }
+
+    /**
+     * Returns configured default content-type value for response encoding.
+     * To be used if 'accept' header is absent.
+     *
+     * @return default value configured
+     */
+    public AsciiString defaultAcceptType() {
+        return defaultAcceptType;
+    }
+
+    /**
+     * Returns configured default value for pretty print parameter.
+     *
+     * @return default value configured
+     */
+    public PrettyPrintParam defaultPrettyPrint() {
+        return defaultPrettyPrint;
+    }
+
+    /**
+     * Returns pretty print parameter value to be used for current request .
+     *
+     * @return parameter value
+     */
+    public PrettyPrintParam prettyPrint() {
+        final var requestParam = queryParameters.lookup(PrettyPrintParam.uriName, PrettyPrintParam::forUriValue);
+        return requestParam != null ? requestParam : defaultPrettyPrint;
+    }
+
+    /**
+     * Returns error tag mapping configured.
+     *
+     * @return error tag mapping instance
+     */
+    public ErrorTagMapping errorTagMapping() {
+        return errorTagMapping;
+    }
+
+    /**
+     * Returns user principal associated with current request.
+     *
+     * @return principal object if defined, null otherwise
+     */
+    public @Nullable Principal principal() {
+        return principal;
+    }
+
+    private static AsciiString extractContentType(final FullHttpRequest request, final AsciiString defaultType) {
+        final var contentType = request.headers().get(HttpHeaderNames.CONTENT_TYPE);
+        if (contentType != null) {
+            return AsciiString.of(contentType);
+        }
+        // when request body is empty content-type value plays no role, and eligible to be absent,
+        // in this case apply default type to prevent unsupported media type error when checked subsequently
+        return request.content().readableBytes() == 0 ? defaultType : AsciiString.EMPTY_STRING;
+    }
+}
---
+++ protocol/restconf-server/src/test/java/org/opendaylight/restconf/server/ModulesRequestProcessorTest.java
@@ -0,0 +1,162 @@
+/*
+ * Copyright (c) 2024 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.restconf.server;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.ArgumentMatchers.isNull;
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.doThrow;
+import static org.opendaylight.restconf.server.ModulesRequestProcessor.ALLOW_METHODS;
+import static org.opendaylight.restconf.server.ModulesRequestProcessor.MISSING_FILENAME_ERROR;
+import static org.opendaylight.restconf.server.ModulesRequestProcessor.REVISION;
+import static org.opendaylight.restconf.server.ModulesRequestProcessor.SOURCE_READ_FAILURE_ERROR;
+import static org.opendaylight.restconf.server.PathParameters.MODULES;
+import static org.opendaylight.restconf.server.PathParameters.YANG_LIBRARY_VERSION;
+import static org.opendaylight.restconf.server.TestUtils.answerCompleteWith;
+import static org.opendaylight.restconf.server.TestUtils.assertErrorResponse;
+import static org.opendaylight.restconf.server.TestUtils.assertOptionsResponse;
+import static org.opendaylight.restconf.server.TestUtils.assertResponse;
+import static org.opendaylight.restconf.server.TestUtils.buildRequest;
+import static org.opendaylight.restconf.server.TestUtils.charSource;
+import static org.opendaylight.restconf.server.TestUtils.formattableBody;
+
+import com.google.common.io.ByteSource;
+import com.google.common.io.CharSource;
+import io.netty.handler.codec.http.DefaultFullHttpRequest;
+import io.netty.handler.codec.http.HttpMethod;
+import io.netty.handler.codec.http.HttpResponseStatus;
+import io.netty.handler.codec.http.HttpVersion;
+import java.io.IOException;
+import java.util.stream.Stream;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.mockito.Mock;
+import org.opendaylight.restconf.server.TestUtils.TestEncoding;
+import org.opendaylight.restconf.server.api.ModulesGetResult;
+import org.opendaylight.yangtools.yang.common.ErrorTag;
+
+class ModulesRequestProcessorTest extends AbstractRequestProcessorTest {
+    private static final String YANG_LIBRARY_VERSION_URI = BASE_PATH + YANG_LIBRARY_VERSION;
+    private static final String MODULES_PATH = BASE_PATH + MODULES + "/";
+    private static final String MODULE_FILENAME = "module-filename";
+    private static final String MODULE_URI = MODULES_PATH + MODULE_FILENAME;
+    private static final String MODULE_URI_WITH_MOUNT = MODULES_PATH + MOUNT_PATH + "/" + MODULE_FILENAME;
+    private static final String REVISION_VALUE = "revision-value";
+    private static final String REVISION_PARAM = "?" + REVISION + "=" + REVISION_VALUE;
+    private static final String YANG_CONTENT = "yang-content";
+    private static final String YIN_CONTENT = "yin-content";
+
+    @Mock
+    private CharSource source;
+    @Mock
+    private ByteSource byteSource;
+
+    @ParameterizedTest
+    @ValueSource(strings = {YANG_LIBRARY_VERSION_URI, MODULES_PATH, MODULE_URI, MODULE_URI_WITH_MOUNT})
+    void options(final String uri) {
+        final var request = new DefaultFullHttpRequest(HttpVersion.HTTP_1_1, HttpMethod.OPTIONS, uri);
+        assertOptionsResponse(dispatch(request), ALLOW_METHODS);
+
+    }
+
+    @ParameterizedTest
+    @MethodSource("encodings")
+    void getYangLibraryVersion(final TestEncoding encoding, final String content) {
+        final var result = formattableBody(encoding, content);
+        doAnswer(answerCompleteWith(result)).when(service).yangLibraryVersionGET(any());
+
+        final var request = buildRequest(HttpMethod.GET, YANG_LIBRARY_VERSION_URI, encoding, null);
+        final var response = dispatch(request);
+        assertResponse(response, HttpResponseStatus.OK, encoding.responseType, content);
+    }
+
+    @ParameterizedTest
+    @MethodSource("modules")
+    void getModule(final TestEncoding encoding, final String content, final boolean hasRevision) {
+        final var result = new ModulesGetResult(charSource(content));
+        final var revision = hasRevision ? REVISION_VALUE : null;
+        if (encoding.isYin()) {
+            doAnswer(answerCompleteWith(result)).when(service).modulesYinGET(any(), eq(MODULE_FILENAME), eq(revision));
+        } else {
+            doAnswer(answerCompleteWith(result)).when(service).modulesYangGET(any(), eq(MODULE_FILENAME), eq(revision));
+        }
+
+        final var uri = MODULE_URI + (hasRevision ? REVISION_PARAM : "");
+        final var request = buildRequest(HttpMethod.GET, uri, encoding, null);
+        final var response = dispatch(request);
+        assertResponse(response, HttpResponseStatus.OK, encoding.responseType, content);
+    }
+
+    @ParameterizedTest
+    @MethodSource("modules")
+    void getModuleWithMountPath(final TestEncoding encoding, final String content, final boolean hasRevision) {
+        final var result = new ModulesGetResult(charSource(content));
+        final var revision = hasRevision ? REVISION_VALUE : null;
+        if (encoding.isYin()) {
+            doAnswer(answerCompleteWith(result)).when(service)
+                .modulesYinGET(any(), eq(MOUNT_API_PATH), eq(MODULE_FILENAME), eq(revision));
+        } else {
+            doAnswer(answerCompleteWith(result)).when(service)
+                .modulesYangGET(any(), eq(MOUNT_API_PATH), eq(MODULE_FILENAME), eq(revision));
+        }
+        final var uri = MODULE_URI_WITH_MOUNT + (hasRevision ? REVISION_PARAM : "");
+        final var request = buildRequest(HttpMethod.GET, uri, encoding, null);
+        final var response = dispatch(request);
+        assertResponse(response, HttpResponseStatus.OK, encoding.responseType, content);
+    }
+
+    private static Stream<Arguments> modules() {
+        return Stream.of(
+            // encoding, content, hasRevision
+            Arguments.of(TestEncoding.YANG, YANG_CONTENT, true),
+            Arguments.of(TestEncoding.YANG, YANG_CONTENT, false),
+            Arguments.of(TestEncoding.YIN, YIN_CONTENT, true),
+            Arguments.of(TestEncoding.YIN, YIN_CONTENT, false)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("moduleErrorEncodings")
+    void noFilenameError(final TestEncoding encoding, final TestEncoding errorEncoding) {
+        final var request = buildRequest(HttpMethod.GET, MODULES_PATH, encoding, null);
+        final var response = dispatch(request);
+        assertErrorResponse(response, errorEncoding, ErrorTag.MISSING_ELEMENT, MISSING_FILENAME_ERROR);
+    }
+
+    @ParameterizedTest
+    @MethodSource("moduleErrorEncodings")
+    void sourceReadFailure(final TestEncoding encoding, final TestEncoding errorEncoding) throws IOException {
+        final var errorMessage = "source-read-failure";
+        doReturn(byteSource).when(source).asByteSource(any());
+        doThrow(new IOException(errorMessage)).when(byteSource).read();
+
+        final var result = new ModulesGetResult(source);
+        if (encoding.isYin()) {
+            doAnswer(answerCompleteWith(result)).when(service).modulesYinGET(any(), eq(MODULE_FILENAME), isNull());
+        } else {
+            doAnswer(answerCompleteWith(result)).when(service).modulesYangGET(any(), eq(MODULE_FILENAME), isNull());
+        }
+
+        final var request = buildRequest(HttpMethod.GET, MODULE_URI, encoding, null);
+        final var response = dispatch(request);
+        assertErrorResponse(response, errorEncoding, ErrorTag.OPERATION_FAILED,
+            SOURCE_READ_FAILURE_ERROR + errorMessage);
+    }
+
+    private static Stream<Arguments> moduleErrorEncodings() {
+        return Stream.of(
+            // request accept encoding, error response encoding
+            Arguments.of(TestEncoding.YANG, DEFAULT_ENCODING),
+            Arguments.of(TestEncoding.YIN, TestEncoding.XML)
+        );
+    }
+}
