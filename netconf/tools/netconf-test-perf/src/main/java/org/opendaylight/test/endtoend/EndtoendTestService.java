3ce4479b9d7c22cd7e7864301bc225befe763477
Robert Varga <robert.varga@pantheon.tech>
2024-03-17 09:48:26 +0100

    Eliminate NcmountRpcs
    There is no need for a dedicated class -- just integrate it into
    EndtoendTestService, removing the use of ClassToInstanceMap in the
    process of doing so.
    
    Change-Id: I64b15c6ff55c5e389de990096f56c1cde61ec26c
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- netconf/tools/netconf-test-perf/src/main/java/org/opendaylight/test/endtoend/EndtoendTestService.java
+++ netconf/tools/netconf-test-perf/src/main/java/org/opendaylight/test/endtoend/EndtoendTestService.java
@@ -7,12 +7,58 @@
  */
 package org.opendaylight.test.endtoend;
 
+import static java.util.Objects.requireNonNull;
+
+import com.google.common.util.concurrent.ListenableFuture;
+import com.google.common.util.concurrent.MoreExecutors;
+import java.util.Map;
+import java.util.Optional;
 import javax.annotation.PreDestroy;
 import javax.inject.Inject;
 import javax.inject.Singleton;
+import org.opendaylight.mdsal.binding.api.DataBroker;
+import org.opendaylight.mdsal.binding.api.MountPoint;
 import org.opendaylight.mdsal.binding.api.MountPointService;
 import org.opendaylight.mdsal.binding.api.RpcProviderService;
+import org.opendaylight.mdsal.binding.api.WriteTransaction;
+import org.opendaylight.mdsal.common.api.LogicalDatastoreType;
+import org.opendaylight.netconf.topology.spi.NetconfNodeUtils;
+import org.opendaylight.yang.gen.v1.http.cisco.com.ns.yang.cisco.ios.xr.ip._static.cfg.rev130722.RouterStatic;
+import org.opendaylight.yang.gen.v1.http.cisco.com.ns.yang.cisco.ios.xr.ip._static.cfg.rev130722.address.family.AddressFamilyBuilder;
+import org.opendaylight.yang.gen.v1.http.cisco.com.ns.yang.cisco.ios.xr.ip._static.cfg.rev130722.address.family.address.family.Vrfipv4Builder;
+import org.opendaylight.yang.gen.v1.http.cisco.com.ns.yang.cisco.ios.xr.ip._static.cfg.rev130722.router._static.Vrfs;
+import org.opendaylight.yang.gen.v1.http.cisco.com.ns.yang.cisco.ios.xr.ip._static.cfg.rev130722.router._static.vrfs.Vrf;
+import org.opendaylight.yang.gen.v1.http.cisco.com.ns.yang.cisco.ios.xr.ip._static.cfg.rev130722.router._static.vrfs.VrfBuilder;
+import org.opendaylight.yang.gen.v1.http.cisco.com.ns.yang.cisco.ios.xr.ip._static.cfg.rev130722.router._static.vrfs.VrfKey;
+import org.opendaylight.yang.gen.v1.http.cisco.com.ns.yang.cisco.ios.xr.ip._static.cfg.rev130722.vrf.prefix.table.VrfPrefixesBuilder;
+import org.opendaylight.yang.gen.v1.http.cisco.com.ns.yang.cisco.ios.xr.ip._static.cfg.rev130722.vrf.prefix.table.vrf.prefixes.VrfPrefix;
+import org.opendaylight.yang.gen.v1.http.cisco.com.ns.yang.cisco.ios.xr.ip._static.cfg.rev130722.vrf.prefix.table.vrf.prefixes.VrfPrefixBuilder;
+import org.opendaylight.yang.gen.v1.http.cisco.com.ns.yang.cisco.ios.xr.ip._static.cfg.rev130722.vrf.prefix.table.vrf.prefixes.VrfPrefixKey;
+import org.opendaylight.yang.gen.v1.http.cisco.com.ns.yang.cisco.ios.xr.ip._static.cfg.rev130722.vrf.route.VrfRouteBuilder;
+import org.opendaylight.yang.gen.v1.http.cisco.com.ns.yang.cisco.ios.xr.ip._static.cfg.rev130722.vrf.route.vrf.route.VrfNextHopsBuilder;
+import org.opendaylight.yang.gen.v1.http.cisco.com.ns.yang.cisco.ios.xr.ip._static.cfg.rev130722.vrf.route.vrf.route.vrf.next.hops.NextHopAddressBuilder;
+import org.opendaylight.yang.gen.v1.http.cisco.com.ns.yang.cisco.ios.xr.ip._static.cfg.rev130722.vrf.unicast.VrfUnicastBuilder;
+import org.opendaylight.yang.gen.v1.http.cisco.com.ns.yang.cisco.xr.types.rev150119.CiscoIosXrString;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev130715.IpAddress;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.ncmount.rev150105.ListNodes;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.ncmount.rev150105.ListNodesInput;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.ncmount.rev150105.ListNodesOutput;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.ncmount.rev150105.ShowNode;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.ncmount.rev150105.ShowNodeInput;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.ncmount.rev150105.ShowNodeOutput;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.ncmount.rev150105.WriteRoutes;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.ncmount.rev150105.WriteRoutesInput;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.ncmount.rev150105.WriteRoutesOutput;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.ncmount.rev150105.WriteRoutesOutputBuilder;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.NodeId;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.Node;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.NodeKey;
 import org.opendaylight.yangtools.concepts.Registration;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+import org.opendaylight.yangtools.yang.binding.util.BindingMap;
+import org.opendaylight.yangtools.yang.common.ErrorType;
+import org.opendaylight.yangtools.yang.common.RpcResult;
+import org.opendaylight.yangtools.yang.common.RpcResultBuilder;
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Deactivate;
@@ -20,15 +66,19 @@ import org.osgi.service.component.annotations.Reference;
 
 @Singleton
 @Component(immediate = true)
-public class EndtoendTestService implements AutoCloseable {
+public final class EndtoendTestService implements AutoCloseable {
+    private final MountPointService mountPointService;
     private final Registration registration;
 
     @Inject
     @Activate
     public EndtoendTestService(final @Reference RpcProviderService rpcProviderService,
-                               final @Reference MountPointService mountPointService) {
-        final var ncmountRpcs = new NcmountRpcs(mountPointService);
-        registration = rpcProviderService.registerRpcImplementations(ncmountRpcs.getRpcClassToInstanceMap());
+            final MountPointService mountPointService) {
+        this.mountPointService = requireNonNull(mountPointService);
+        registration = rpcProviderService.registerRpcImplementations(
+            (WriteRoutes) this::writeRoutes,
+            (ShowNode) this::showNode,
+            (ListNodes) this::listNodes);
     }
 
     @Override
@@ -36,5 +86,70 @@ public class EndtoendTestService implements AutoCloseable {
     @PreDestroy
     public void close() {
         registration.close();
+    }
+
+    private ListenableFuture<RpcResult<WriteRoutesOutput>> writeRoutes(final WriteRoutesInput input) {
+        final Optional<MountPoint> optMountPoint = mountPointService.getMountPoint(
+            NetconfNodeUtils.DEFAULT_TOPOLOGY_IID.child(Node.class, new NodeKey(new NodeId(input.getMountName()))));
+        if (optMountPoint.isEmpty()) {
+            return RpcResultBuilder.<WriteRoutesOutput>failed()
+                .withError(ErrorType.TRANSPORT, "Mount point not present")
+                .buildFuture();
+        }
+
+        final Optional<DataBroker> optBroker = optMountPoint.orElseThrow().getService(DataBroker.class);
+        if (optBroker.isEmpty()) {
+            return RpcResultBuilder.<WriteRoutesOutput>failed()
+                .withError(ErrorType.TRANSPORT, "Mount point does not provide DataBroker service")
+                .buildFuture();
+        }
+
+        final Map<VrfPrefixKey, VrfPrefix> routes = input.nonnullRoute().entrySet().stream()
+            .map(entry -> new VrfPrefixBuilder()
+                .withKey(new VrfPrefixKey(new IpAddress(entry.getValue().getIpv4Prefix()),
+                    entry.getValue().getIpv4PrefixLength().toUint32()))
+                .setVrfRoute(new VrfRouteBuilder()
+                    .setVrfNextHops(new VrfNextHopsBuilder()
+                        .setNextHopAddress(BindingMap.of(new NextHopAddressBuilder()
+                            .setNextHopAddress(new IpAddress(entry.getValue().getIpv4NextHop()))
+                            .build()))
+                        .build())
+                    .build())
+                .build())
+            .collect(BindingMap.toOrderedMap());
+
+        final Vrf vrf = new VrfBuilder()
+            .withKey(new VrfKey(new CiscoIosXrString(input.getVrfId())))
+            .setAddressFamily(new AddressFamilyBuilder()
+                .setVrfipv4(new Vrfipv4Builder()
+                    .setVrfUnicast(new VrfUnicastBuilder()
+                        .setVrfPrefixes(new VrfPrefixesBuilder()
+                            // FIXME: inline routes once it is unambiguous
+                            .setVrfPrefix(routes)
+                            .build())
+                        .build())
+                    .build())
+                .build())
+            .build();
+
+        final WriteTransaction writeTransaction = optBroker.orElseThrow().newWriteOnlyTransaction();
+        writeTransaction.merge(LogicalDatastoreType.CONFIGURATION,
+            InstanceIdentifier.create(RouterStatic.class).child(Vrfs.class).child(Vrf.class, vrf.key()), vrf);
+
+        return writeTransaction.commit().transform(
+            info -> RpcResultBuilder.success(new WriteRoutesOutputBuilder().build()).build(),
+            MoreExecutors.directExecutor());
+    }
+
+    private ListenableFuture<RpcResult<ShowNodeOutput>> showNode(final ShowNodeInput input) {
+        return RpcResultBuilder.<ShowNodeOutput>failed()
+            .withError(ErrorType.APPLICATION, "Not implemented")
+            .buildFuture();
+    }
+
+    private ListenableFuture<RpcResult<ListNodesOutput>> listNodes(final ListNodesInput input) {
+        return RpcResultBuilder.<ListNodesOutput>failed()
+            .withError(ErrorType.APPLICATION, "Not implemented")
+            .buildFuture();
     }
 }
