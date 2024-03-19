eb810f18294cb2e19a885f2d6c5a129f509347ae
Ivan Hrasko <ivan.hrasko@pantheon.tech>
2024-03-19 13:46:53 +0100

    WIP: Draft Json to message encoder/decoder
    JIRA: NETCONF-873
    Change-Id: Ibf8c2991cc981206ab3466cf18d0206b8617c769
    Signed-off-by: Ivan Hrasko <ivan.hrasko@pantheon.tech>

---
+++ restconf/restconf-nb/src/main/java/org/opendaylight/restconf/nb/netty/MessageToJsonDecoder.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2024 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.restconf.nb.netty;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.ByteToMessageDecoder;
+import java.util.List;
+
+public class MessageToJsonDecoder extends ByteToMessageDecoder {
+    @Override
+    protected void decode(ChannelHandlerContext ctx, ByteBuf in, List<Object> out) throws Exception {
+
+    }
+}
