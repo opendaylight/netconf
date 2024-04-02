7034c2ad76b5e30afae22ac4ad3a9b6464a1611f
Ivan Hrasko <ivan.hrasko@pantheon.tech>
2024-04-02 14:58:38 +0200

    OpenAPI: Implement pagination
    Enable user to limit number of modules for which OpenAPI documentation
    is generated.
    
    Then new API parameters offset and limit can be used as:
    ?offset=0&limit=0 for all modules
    ?offset=0&limit=10 for first 10 modules
    ?offset=10&limit=10 for 10-20 modules
    ?offset=1000 offset is out of range return empty paths and components
    ?limit=1000 limit out of range return all models
    ?offset=-1 or ?limit=-1 negative number always return empty paths and
    components
    
    JIRA: NETCONF-1225
    Change-Id: Ib091a622f90c554946fc0f7504397de3814f681e
    Signed-off-by: Ivan Hrasko <ivan.hrasko@pantheon.tech>
    Signed-off-by: lubos-cicut <lubos.cicut@pantheon.tech>

--- restconf/restconf-openapi/src/main/java/org/opendaylight/restconf/openapi/api/OpenApiService.java
+++ restconf/restconf-openapi/src/main/java/org/opendaylight/restconf/openapi/api/OpenApiService.java
@@ -7,6 +7,7 @@
  */
 package org.opendaylight.restconf.openapi.api;
 
+import com.google.common.collect.ImmutableCollection;
 import java.io.IOException;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -17,6 +18,7 @@ import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
+import org.opendaylight.yangtools.yang.model.api.EffectiveModelContext;
 
 /**
  * This service generates swagger (See <a
@@ -27,10 +29,40 @@ import javax.ws.rs.core.UriInfo;
 @Path("/")
 public interface OpenApiService {
 
+    /**
+     * Generate OpenAPI specification document.
+     *
+     * <p>
+     * Generate OpenAPI specification document for modules of controller schema context starting with a module with
+     * index specified by {@code offset}. The resulting documentation contains as many modules as defined
+     * by {@code limit}.
+     *
+     * <p>
+     * If user wishes to get document for all modules in controller's model context then value 0 should be used
+     * for both {@code offset} and {@code limit}.
+     *
+     * <p>
+     * We relly on {@link EffectiveModelContext} usage of {@link ImmutableCollection} which preserves iteration order,
+     * so we are able to read first 40 modules with {@code ?offset=0&limit=20} and consequent request with parameters
+     * {@code ?offset=20&limit=20}.
+     *
+     * <p>
+     * If user uses value out of range for {@code offset} which is greater than number of modules in mount point schema
+     * or negative value, the response will contain empty OpenAPI document. Same if user uses negative value for
+     * {@code limit}.
+     *
+     * @param uriInfo Requests {@link UriInfo}.
+     * @param offset First model to read. 0 means read from the first model.
+     * @param limit The number of models to read. 0 means read all models.
+     * @return Response containing the OpenAPI document for number of modules specified by {@code offset}
+     *     and {@code limit}.
+     * @throws IOException When I/O error occurs.
+     */
     @GET
     @Path("/single")
     @Produces(MediaType.APPLICATION_JSON)
-    Response getAllModulesDoc(@Context UriInfo uriInfo) throws IOException;
+    Response getAllModulesDoc(@Context UriInfo uriInfo, @QueryParam("offset") Integer offset,
+            @QueryParam("limit") Integer limit) throws IOException;
 
     /**
      * Generates Swagger compliant document listing APIs for module.
@@ -70,10 +102,38 @@ public interface OpenApiService {
                                  @Context UriInfo uriInfo) throws IOException;
 
     /**
-     * Generates Swagger compliant document listing APIs for all modules of mount point.
+     * Generate OpenAPI specification document listing APIs for all modules of mount point..
+     *
+     * <p>
+     * Generates OpenAPI specification document listing APIs for all modules of mount point if value 0 is used for both
+     * {@code offset} and {@code limit}.
+     *
+     * <p>
+     * Generate OpenAPI specification document for modules of mount point schema context starting with a module with
+     * index specified by {@code offset}. The resulting documentation contains as many modules as defined
+     * by {@code limit}.
+     *
+     * <p>
+     * We relly on {@link EffectiveModelContext} usage of {@link ImmutableCollection} which preserves iteration order,
+     * so we are able to read first 40 modules with {@code ?offset=0&limit=20} and consequent request with parameters
+     * {@code ?offset=20&limit=20}.
+     *
+     * <p>
+     * If user uses value out of range for {@code offset} which is greater than number of modules in mount point schema
+     * or negative value, the response will contain empty OpenAPI document. Same if user uses negative value for
+     * {@code limit}.
+     *
+     * @param instanceNum Instance number of the mount point.
+     * @param uriInfo Requests {@link UriInfo}.
+     * @param offset First model to read. 0 means read from the first model.
+     * @param limit The number of models to read. 0 means read all models.
+     * @return Response containing the OpenAPI document for number of modules specified by {@code offset}
+     *     and {@code limit}.
+     * @throws IOException When I/O error occurs.
      */
     @GET
     @Path("/mounts/{instance}")
     @Produces(MediaType.APPLICATION_JSON)
-    Response getMountDoc(@PathParam("instance") String instanceNum, @Context UriInfo uriInfo) throws IOException;
+    Response getMountDoc(@PathParam("instance") String instanceNum, @Context UriInfo uriInfo,
+            @QueryParam("offset") Integer offset, @QueryParam("limit") Integer limit) throws IOException;
 }
