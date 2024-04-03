ff615dab16f46823f99b129740e34156eb2aac36
Ivan Hrasko <ivan.hrasko@pantheon.tech>
2024-04-03 15:31:08 +0200

    Implement depth parameter
    Implement depth parameter to enable to retrieve OpenAPI
    document in smaller chunks for every model.
    
    JIRA: NETCONF-1298
    Change-Id: Ia615a776b6daa1d1d1b0de6d0863613d9d29ee79
    Signed-off-by: Ivan Hrasko <ivan.hrasko@pantheon.tech>

--- restconf/restconf-openapi/src/main/java/org/opendaylight/restconf/openapi/model/PropertyEntity.java
+++ restconf/restconf-openapi/src/main/java/org/opendaylight/restconf/openapi/model/PropertyEntity.java
@@ -99,17 +99,22 @@ public class PropertyEntity {
     private final @NonNull String parentName;
     private final @NonNull DefinitionNames definitionNames;
     private final @NonNull Integer width;
+    private final @NonNull Integer depth;
+    private final int nodeDepth;
 
     public PropertyEntity(final @NonNull DataSchemaNode node, final @NonNull JsonGenerator generator,
             final @NonNull SchemaInferenceStack stack, final @NonNull List<String> required,
             final @NonNull String parentName, final boolean isParentConfig,
-            final @NonNull DefinitionNames definitionNames, final @NonNull Integer width) throws IOException {
+            final @NonNull DefinitionNames definitionNames, final @NonNull Integer width,
+            final @NonNull Integer depth, final int nodeDepth) throws IOException {
         this.node = requireNonNull(node);
         this.generator = requireNonNull(generator);
         this.required = requireNonNull(required);
         this.parentName = requireNonNull(parentName);
         this.definitionNames = requireNonNull(definitionNames);
         this.width = requireNonNull(width);
+        this.depth = requireNonNull(depth);
+        this.nodeDepth = requireNonNull(nodeDepth);
         generate(stack, isParentConfig);
     }
 
@@ -117,17 +122,20 @@ public class PropertyEntity {
         if (node instanceof ChoiceSchemaNode choice) {
             stack.enterSchemaTree(node.getQName());
             final var isConfig = isParentConfig && node.isConfiguration();
-            processChoiceNodeRecursively(isConfig, stack, choice);
+            processChoiceNodeRecursively(isConfig, stack, choice, nodeDepth);
             stack.exit();
         } else {
             generator.writeObjectFieldStart(node.getQName().getLocalName());
-            processChildNode(node, stack, isParentConfig);
+            processChildNode(node, stack, isParentConfig, nodeDepth);
             generator.writeEndObject();
         }
     }
 
     private void processChoiceNodeRecursively(final boolean isConfig, final SchemaInferenceStack stack,
-            final ChoiceSchemaNode choice) throws IOException {
+            final ChoiceSchemaNode choice, final int nodeDepth) throws IOException {
+        if (depth > 0 && nodeDepth + 1 > depth) {
+            return;
+        }
         if (!choice.getCases().isEmpty()) {
             final var caseSchemaNode = choice.getDefaultCase().orElse(
                 choice.getCases().stream().findFirst().orElseThrow());
@@ -139,11 +147,11 @@ public class PropertyEntity {
                 if (childNode instanceof ChoiceSchemaNode childChoice) {
                     final var isChildConfig = isConfig && childNode.isConfiguration();
                     stack.enterSchemaTree(childNode.getQName());
-                    processChoiceNodeRecursively(isChildConfig, stack, childChoice);
+                    processChoiceNodeRecursively(isChildConfig, stack, childChoice, nodeDepth + 1);
                     stack.exit();
                 } else if (!isConfig || childNode.isConfiguration()) {
                     generator.writeObjectFieldStart(childNode.getQName().getLocalName());
-                    processChildNode(childNode, stack, isConfig);
+                    processChildNode(childNode, stack, isConfig, nodeDepth + 1);
                     generator.writeEndObject();
                 }
             }
@@ -152,13 +160,13 @@ public class PropertyEntity {
     }
 
     private void processChildNode(final DataSchemaNode schemaNode, final SchemaInferenceStack stack,
-            final boolean isParentConfig) throws IOException {
+            final boolean isParentConfig, final int nodeDepth) throws IOException {
         final var parentNamespace = stack.toSchemaNodeIdentifier().lastNodeIdentifier().getNamespace();
         stack.enterSchemaTree(schemaNode.getQName());
         final var name = schemaNode.getQName().getLocalName();
         final var shouldBeAddedAsChild = !isParentConfig || schemaNode.isConfiguration();
         if (schemaNode instanceof ListSchemaNode || schemaNode instanceof ContainerSchemaNode) {
-            processDataNodeContainer((DataNodeContainer) schemaNode, stack);
+            processDataNodeContainer((DataNodeContainer) schemaNode, stack, nodeDepth + 1);
             if (shouldBeAddedAsChild && isSchemaNodeMandatory(schemaNode)) {
                 required.add(name);
             }
@@ -179,8 +187,8 @@ public class PropertyEntity {
         stack.exit();
     }
 
-    private void processDataNodeContainer(final DataNodeContainer dataNode, final SchemaInferenceStack stack)
-            throws IOException {
+    private void processDataNodeContainer(final DataNodeContainer dataNode, final SchemaInferenceStack stack,
+            final int nodeDepth) throws IOException {
         final var schemaNode = (SchemaNode) dataNode;
         final var localName = schemaNode.getQName().getLocalName();
         final var nodeName = parentName + "_" + localName;
@@ -194,13 +202,16 @@ public class PropertyEntity {
             discriminator = definitionNames.getDiscriminator(schemaNode);
         }
 
-        processRef(nodeName, schemaNode, discriminator, stack);
+        processRef(nodeName, schemaNode, discriminator, stack, nodeDepth);
     }
 
     private void processRef(final String name, final SchemaNode schemaNode, String discriminator,
-            final SchemaInferenceStack stack) throws IOException {
+            final SchemaInferenceStack stack, final int nodeDepth) throws IOException {
         final var ref = COMPONENTS_PREFIX + name + discriminator;
         if (schemaNode instanceof ListSchemaNode listNode) {
+            if (depth > 0 && nodeDepth + 1 > depth) {
+                return;
+            }
             generator.writeStringField(TYPE, ARRAY_TYPE);
             generator.writeObjectFieldStart(ITEMS);
             generator.writeStringField("$ref", ref);
