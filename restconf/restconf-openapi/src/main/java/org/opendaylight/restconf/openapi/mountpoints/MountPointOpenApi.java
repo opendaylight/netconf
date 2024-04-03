8e1172cd646b8fb4e07e029f5b92b793ed54e930
Ivan Hrasko <ivan.hrasko@pantheon.tech>
2024-04-03 15:31:08 +0200

    Implement depth parameter
    Implement depth parameter to enable to retrieve OpenAPI
    document in smaller chunks for every model.
    
    JIRA: NETCONF-1298
    Change-Id: Ia615a776b6daa1d1d1b0de6d0863613d9d29ee79
    Signed-off-by: Ivan Hrasko <ivan.hrasko@pantheon.tech>
    Signed-off-by: Oleksandr Zharov <oleksandr.zharov@pantheon.tech>

--- restconf/restconf-openapi/src/main/java/org/opendaylight/restconf/openapi/mountpoints/MountPointOpenApi.java
+++ restconf/restconf-openapi/src/main/java/org/opendaylight/restconf/openapi/mountpoints/MountPointOpenApi.java
@@ -130,7 +130,7 @@ public class MountPointOpenApi implements DOMMountPointListener, AutoCloseable {
     }
 
     public OpenApiInputStream getMountPointApi(final UriInfo uriInfo, final Long id, final String module,
-            final String revision, final Integer width) throws IOException  {
+            final String revision, final Integer width, final Integer depth) throws IOException  {
         final YangInstanceIdentifier iid = longIdToInstanceId.get(id);
         final EffectiveModelContext modelContext = getModelContext(iid);
         final String urlPrefix = getYangMountUrl(iid);
@@ -141,14 +141,14 @@ public class MountPointOpenApi implements DOMMountPointListener, AutoCloseable {
         }
 
         if (DATASTORES_LABEL.equals(module) && DATASTORES_REVISION.equals(revision)) {
-            return generateDataStoreOpenApi(modelContext, uriInfo, urlPrefix, deviceName, width);
+            return generateDataStoreOpenApi(modelContext, uriInfo, urlPrefix, deviceName, width, depth);
         }
         return openApiGenerator.getApiDeclaration(module, revision, uriInfo, modelContext, urlPrefix, deviceName,
-            width);
+            width, depth);
     }
 
     public OpenApiInputStream getMountPointApi(final UriInfo uriInfo, final Long id, final @Nullable String strPageNum,
-            final Integer width) throws IOException {
+            final Integer width, final Integer depth) throws IOException {
         final var iid = longIdToInstanceId.get(id);
         final var context = getModelContext(iid);
         final var urlPrefix = getYangMountUrl(iid);
@@ -178,7 +178,7 @@ public class MountPointOpenApi implements DOMMountPointListener, AutoCloseable {
         final var url = schema + "://" + host + "/";
         final var basePath = openApiGenerator.getBasePath();
         return new OpenApiInputStream(context, title, url, SECURITY, deviceName, urlPrefix, false, includeDataStore,
-            modules, basePath, width);
+            modules, basePath, width, depth);
     }
 
     private static String extractDeviceName(final YangInstanceIdentifier iid) {
@@ -187,15 +187,15 @@ public class MountPointOpenApi implements DOMMountPointListener, AutoCloseable {
     }
 
     private OpenApiInputStream generateDataStoreOpenApi(final EffectiveModelContext modelContext,
-            final UriInfo uriInfo, final String urlPrefix, final String deviceName, final Integer width)
-            throws IOException {
+            final UriInfo uriInfo, final String urlPrefix, final String deviceName, final Integer width,
+            final Integer depth) throws IOException {
         final var schema = openApiGenerator.createSchemaFromUriInfo(uriInfo);
         final var host = openApiGenerator.createHostFromUriInfo(uriInfo);
         final var url = schema + "://" + host + "/";
         final var basePath = openApiGenerator.getBasePath();
         final var modules = BaseYangOpenApiGenerator.getModulesWithoutDuplications(modelContext);
         return new OpenApiInputStream(modelContext, urlPrefix, url, SECURITY, deviceName, urlPrefix, true, false,
-            modules, basePath, width);
+            modules, basePath, width, depth);
     }
 
     @Override
