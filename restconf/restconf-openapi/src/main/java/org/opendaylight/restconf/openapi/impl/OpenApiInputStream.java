c92a250a27d5bf54e6ec4cdfad5722dc9b9733b9
Oleksandr Zharov <oleksandr.zharov@pantheon.tech>
2024-04-23 19:03:59 +0200

    Implement width parameter
    Added width parameter that limit number of children that will be present
    in every module/node of the result schema.
    
    JIRA: NETCONF-1298
    Change-Id: I86e9e2b5cf837a21f92be39ceb2579bb5d990757
    Signed-off-by: Oleksandr Zharov <oleksandr.zharov@pantheon.tech>

--- restconf/restconf-openapi/src/main/java/org/opendaylight/restconf/openapi/impl/OpenApiInputStream.java
+++ restconf/restconf-openapi/src/main/java/org/opendaylight/restconf/openapi/impl/OpenApiInputStream.java
@@ -47,14 +47,15 @@ public final class OpenApiInputStream extends InputStream {
     public OpenApiInputStream(final EffectiveModelContext modelContext, final String title, final String url,
             final List<Map<String, List<String>>> security, final String deviceName, final String urlPrefix,
             final boolean isForSingleModule, final boolean includeDataStore, final Collection<? extends Module> modules,
-            final String basePath) throws IOException {
+            final String basePath, final Integer width) throws IOException {
         final OpenApiBodyWriter writer = new OpenApiBodyWriter(generator, stream);
         stack.add(new OpenApiVersionStream(new OpenApiVersionEntity(), writer));
         stack.add(new InfoStream(new InfoEntity(title), writer));
         stack.add(new ServersStream(new ServersEntity(List.of(new ServerEntity(url))), writer));
         stack.add(new PathsStream(modelContext, writer, deviceName, urlPrefix, isForSingleModule, includeDataStore,
-            modules.iterator(), basePath, stream, generator));
-        stack.add(new ComponentsStream(modelContext, writer, generator, stream, modules.iterator(), isForSingleModule));
+            modules.iterator(), basePath, stream, generator, width));
+        stack.add(new ComponentsStream(modelContext, writer, generator, stream, modules.iterator(), isForSingleModule,
+            width));
         stack.add(new SecurityStream(writer, new SecurityEntity(security)));
     }
 
--- restconf/restconf-openapi/src/main/java/org/opendaylight/restconf/openapi/model/NodeSchemaEntity.java
+++ restconf/restconf-openapi/src/main/java/org/opendaylight/restconf/openapi/model/NodeSchemaEntity.java
@@ -8,6 +8,7 @@
 package org.opendaylight.restconf.openapi.model;
 
 import static org.opendaylight.restconf.openapi.model.PropertyEntity.isSchemaNodeMandatory;
+import static org.opendaylight.restconf.openapi.util.RestDocgenUtil.widthList;
 
 import com.fasterxml.jackson.core.JsonGenerator;
 import java.io.IOException;
@@ -27,22 +28,24 @@ public final class NodeSchemaEntity extends SchemaEntity {
     public NodeSchemaEntity(final @NonNull SchemaNode value, final @NonNull String title,
             final @Nullable String discriminator, final @NonNull String type,
             final @NonNull SchemaInferenceStack context, final @NonNull String parentName, final boolean isParentConfig,
-            final @NonNull DefinitionNames definitionNames) {
-        super(value, title, discriminator, type, context, parentName, isParentConfig, definitionNames);
+            final @NonNull DefinitionNames definitionNames, final @NonNull Integer width) {
+        super(value, title, discriminator, type, context, parentName, isParentConfig, definitionNames, width);
     }
 
     @Override
     void generateProperties(final @NonNull JsonGenerator generator, final @NonNull List<String> required)
             throws IOException {
         final var childNodes = new HashMap<String, DataSchemaNode>();
-        for (final var childNode : ((DataNodeContainer) value()).getChildNodes()) {
+        final var dataSchemaNodes = widthList((DataNodeContainer) value(), width);
+        for (final var childNode : dataSchemaNodes) {
             childNodes.put(childNode.getQName().getLocalName(), childNode);
         }
         final boolean isValueConfig = ((DataSchemaNode) value()).isConfiguration();
+
         for (final var childNode : childNodes.values()) {
             if (shouldBeAddedAsProperty(childNode, isValueConfig)) {
                 new PropertyEntity(childNode, generator, stack(), required, parentName() + "_"
-                    + value().getQName().getLocalName(), isValueConfig, definitionNames());
+                    + value().getQName().getLocalName(), isValueConfig, definitionNames(), width);
             }
         }
     }
