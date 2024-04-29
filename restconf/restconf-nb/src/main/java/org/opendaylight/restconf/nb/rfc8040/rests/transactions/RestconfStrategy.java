a8201a1cd5af7d2f8d60e832fdae7199d953dccd
Yaroslav Lastivka <yaroslav.lastivka@pantheon.tech>
2024-04-29 22:43:29 +0300

    Eliminate ensureParentsByMerge logic
    Calling ensureParentsByMerge has specefied to use only in
    MdsalRestconfStrategy by overriding
    RestocnfStrategy#executeMergeTransaction.
    
    JIRA: NETCONF-1276
    Change-Id: I14747d5b3e037ff2c98b4f05e613be8d0b5fdeda
    Signed-off-by: Yaroslav Lastivka <yaroslav.lastivka@pantheon.tech>

--- restconf/restconf-nb/src/main/java/org/opendaylight/restconf/nb/rfc8040/rests/transactions/RestconfStrategy.java
+++ restconf/restconf-nb/src/main/java/org/opendaylight/restconf/nb/rfc8040/rests/transactions/RestconfStrategy.java
@@ -310,10 +310,7 @@ public abstract class RestconfStrategy implements DatabindAware {
 
     private void merge(final @NonNull SettableRestconfFuture<DataPatchResult> future,
             final @NonNull YangInstanceIdentifier path, final @NonNull NormalizedNode data) {
-        final var tx = prepareWriteExecution();
-        // FIXME: this method should be further specialized to eliminate this call -- it is only needed for MD-SAL
-        tx.ensureParentsByMerge(path);
-        tx.merge(path, data);
+        final var tx = executeMergeTransaction(path, data);
         Futures.addCallback(tx.commit(), new FutureCallback<CommitInfo>() {
             @Override
             public void onSuccess(final CommitInfo result) {
@@ -326,6 +323,13 @@ public abstract class RestconfStrategy implements DatabindAware {
                 future.setFailure(TransactionUtil.decodeException(cause, "MERGE", path, modelContext()));
             }
         }, MoreExecutors.directExecutor());
+    }
+
+    protected RestconfTransaction executeMergeTransaction(final @NonNull YangInstanceIdentifier path,
+        final @NonNull NormalizedNode data) {
+        final var tx = prepareWriteExecution();
+        tx.merge(path, data);
+        return tx;
     }
 
     public @NonNull RestconfFuture<DataPutResult> dataPUT(final ServerRequest request, final ApiPath apiPath,
