3e89dc4972a00bcc644de61134c161a54684423e
Samuel Schneider <samuel.schneider@pantheon.tech>
2024-04-30 17:58:51 +0200

    Add validation for certificates
    Add validation for certificates added by keystore RPCs
    
    JIRA: NETCONF-1304
    Change-Id: Ib5fe1926e8d36f2f071efae2f7661a5e18dbdb76
    Signed-off-by: Samuel Schneider <samuel.schneider@pantheon.tech>

--- keystore/keystore-legacy/src/main/java/org/opendaylight/netconf/keystore/legacy/impl/DefaultAddPrivateKey.java
+++ keystore/keystore-legacy/src/main/java/org/opendaylight/netconf/keystore/legacy/impl/DefaultAddPrivateKey.java
@@ -10,6 +10,7 @@ package org.opendaylight.netconf.keystore.legacy.impl;
 import com.google.common.util.concurrent.ListenableFuture;
 import com.google.common.util.concurrent.MoreExecutors;
 import java.nio.charset.StandardCharsets;
+import java.security.GeneralSecurityException;
 import org.opendaylight.mdsal.binding.api.DataBroker;
 import org.opendaylight.mdsal.common.api.LogicalDatastoreType;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.netconf.keystore.rev231109.AddPrivateKey;
@@ -27,6 +28,7 @@ import org.slf4j.LoggerFactory;
 
 final class DefaultAddPrivateKey extends AbstractRpc implements AddPrivateKey {
     private static final Logger LOG = LoggerFactory.getLogger(DefaultAddPrivateKey.class);
+    private final SecurityHelper securityHelper = new SecurityHelper();
 
     DefaultAddPrivateKey(final DataBroker dataBroker) {
         super(dataBroker);
@@ -37,6 +39,21 @@ final class DefaultAddPrivateKey extends AbstractRpc implements AddPrivateKey {
         final var keys = input.getPrivateKey();
         if (keys == null || keys.isEmpty()) {
             return RpcResultBuilder.success(new AddPrivateKeyOutputBuilder().build()).buildFuture();
+        }
+
+        // Validate certificates
+        for (var key : keys.values()) {
+            if (key.getCertificateChain() == null || key.getCertificateChain().isEmpty()) {
+                continue;
+            }
+            key.getCertificateChain().forEach(cert -> {
+                try {
+                    securityHelper.generateCertificate(cert.getBytes(StandardCharsets.UTF_8));
+                } catch (GeneralSecurityException e) {
+                    LOG.debug("Failed to generate certificate chain for private key {}", key.getName(), e);
+                    throw new IllegalArgumentException("Failed to generate certificate for: " + key.getName(), e);
+                }
+            });
         }
 
         LOG.debug("Adding private keys: {}", keys);
