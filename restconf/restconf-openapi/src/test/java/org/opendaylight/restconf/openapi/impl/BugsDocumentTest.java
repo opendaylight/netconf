77f1993c6237f8448c4c192a0ad36badad2040d8
lubos-cicut <lubos.cicut@pantheon.tech>
2024-05-13 10:41:38 +0200

    Openapi: Missing schema for node processed by another parent test
    If the same node was a child of more than one parent, only the first
    one was processed.
    
    Parent patch fixes this problem. Now when node is processed it will be
    saved with the current parent. This patch tests if generated document
    is as expected.
    
    Created new test to simplify testing of new reported bugs.
    
    JIRA: NETCONF-1309
    Change-Id: I7cc72f5d12286154056da6f5b551a11429518332
    Signed-off-by: lubos-cicut <lubos.cicut@pantheon.tech>
    (cherry picked from commit 09cc85f261c72e7a4e51f7e2b7d63d4ee0b9c9e1)

---
+++ restconf/restconf-openapi/src/test/java/org/opendaylight/restconf/openapi/impl/BugsDocumentTest.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2024 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.restconf.openapi.impl;
+
+import java.util.stream.Stream;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.skyscreamer.jsonassert.JSONAssert;
+
+public class BugsDocumentTest extends AbstractDocumentTest {
+    /**
+     * Model netconf-1309 is used for test issue NETCONF-1309.
+     */
+    private static final String NETCONF_1309 = "netconf-1309";
+    private static final String NETCONF_1309_REVISION = "2024-05-13";
+
+    @BeforeAll
+    public static void beforeAll() {
+        initializeClass("/bugs/");
+    }
+
+    /**
+     * Tests the swagger document that is result of the call to the '/moduleName' endpoint.
+     */
+    @ParameterizedTest
+    @MethodSource("getOperationalParameters")
+    public void getDocByModuleTest(final String moduleName, final String revision, final String jsonPath)
+            throws Exception {
+        final var expectedJson = getExpectedDoc("bugs-document/" + jsonPath);
+        final var moduleDoc = getDocByModule(moduleName, revision);
+        JSONAssert.assertEquals(expectedJson, moduleDoc, IGNORE_ORDER);
+    }
+
+    static Stream<Arguments> getOperationalParameters() {
+        // moduleName, revision, jsonPath
+        return Stream.of(
+            Arguments.of(NETCONF_1309, NETCONF_1309_REVISION, "controller-netconf-1309.json")
+        );
+    }
+}
