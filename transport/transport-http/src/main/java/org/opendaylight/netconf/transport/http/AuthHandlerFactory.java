d6e0f17aa15d81623ebcaa1ef8622b03b3f1a7bd
Ruslan Kashapov <ruslan.kashapov@pantheon.tech>
2024-05-17 16:29:12 +0300

    Support auth* service integration for HttpTransport
    Due to channel handler is stateful object using same auth*
    handler for multiple connection causes error. Fixed by
    replacing handler instance with handler factory usage.
    
    Server API extended to accept custom authn/authz handler
    factory. This allows external services integration.
    
    JIRA: NETCONF-1316
    Change-Id: I99cd267c0955b93d10e7f75de31897533edb5b9b
    Signed-off-by: Ruslan Kashapov <ruslan.kashapov@pantheon.tech>
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ transport/transport-http/src/main/java/org/opendaylight/netconf/transport/http/AuthHandlerFactory.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2024 PANTHEON.tech s.r.o. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.netconf.transport.http;
+
+import org.eclipse.jdt.annotation.NonNull;
+
+/**
+ * Factory building {@link AuthHandler} instances to serve requests authentication and authorization for new connections
+ * established.
+ */
+@FunctionalInterface
+public interface AuthHandlerFactory {
+    /**
+     * Builds {@link AuthHandler} instance.
+     *
+     * @return handler instance
+     */
+    @NonNull AuthHandler<?> create();
+}
---
+++ transport/transport-http/src/main/java/org/opendaylight/netconf/transport/http/BasicAuthHandlerFactory.java
@@ -0,0 +1,87 @@
+/*
+ * Copyright (c) 2024 PANTHEON.tech s.r.o. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.netconf.transport.http;
+
+import static java.util.Objects.requireNonNull;
+
+import com.google.common.collect.ImmutableMap;
+import java.util.regex.Pattern;
+import org.apache.commons.codec.digest.Crypt;
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.eclipse.jdt.annotation.Nullable;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.http.server.rev240208.HttpServerGrouping;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.http.server.rev240208.http.server.grouping.client.authentication.users.user.auth.type.Basic;
+
+/**
+ * {@link BasicAuthHandlerFactory} implementation for Basic Authorization.
+ *
+ * <p>
+ * Uses predefined (configured) collection of usernames with password hashes to authenticate/authorize users.
+ */
+final class BasicAuthHandlerFactory implements AuthHandlerFactory {
+    @NonNullByDefault
+    private record CryptHash(String salt, String hash) {
+        CryptHash {
+            requireNonNull(salt);
+            requireNonNull(hash);
+        }
+    }
+
+    private static final Pattern CRYPT_HASH_PATTERN = Pattern.compile("""
+        \\$0\\$.*\
+        |\\$1\\$[a-zA-Z0-9./]{1,8}\\$[a-zA-Z0-9./]{22}\
+        |\\$5\\$(rounds=\\d+\\$)?[a-zA-Z0-9./]{1,16}\\$[a-zA-Z0-9./]{43}\
+        |\\$6\\$(rounds=\\d+\\$)?[a-zA-Z0-9./]{1,16}\\$[a-zA-Z0-9./]{86}""");
+    private static final String DEFAULT_SALT = "$5$rounds=3500$default";
+
+    private final ImmutableMap<String, CryptHash> knownHashes;
+
+    private BasicAuthHandlerFactory(final ImmutableMap<String, CryptHash> knownHashes) {
+        this.knownHashes = requireNonNull(knownHashes);
+    }
+
+    static @Nullable BasicAuthHandlerFactory ofNullable(final HttpServerGrouping httpParams) {
+        if (httpParams == null) {
+            return null;
+        }
+        final var clientAuth = httpParams.getClientAuthentication();
+        if (clientAuth == null) {
+            return null;
+        }
+
+        // Basic authorization handler
+        final var builder = ImmutableMap.<String, CryptHash>builder();
+        clientAuth.nonnullUsers().nonnullUser().forEach((ignored, user) -> {
+            if (user.getAuthType() instanceof Basic basicAuth) {
+                final var basic = basicAuth.nonnullBasic();
+                final var hashedPassword = basic.nonnullPassword().requireHashedPassword().getValue();
+                if (!CRYPT_HASH_PATTERN.matcher(hashedPassword).matches()) {
+                    throw new IllegalArgumentException("Invalid crypt hash string \"" + hashedPassword + '"');
+                }
+                final var cryptHash = hashedPassword.startsWith("$0$")
+                    ? new CryptHash(DEFAULT_SALT, Crypt.crypt(hashedPassword.substring(3), DEFAULT_SALT))
+                    : new CryptHash(hashedPassword.substring(0, hashedPassword.lastIndexOf('$')), hashedPassword);
+                builder.put(basic.requireUsername(), cryptHash);
+            }
+        });
+        final var knownHashes = builder.build();
+        return knownHashes.isEmpty() ? null : new BasicAuthHandlerFactory(knownHashes);
+    }
+
+    @Override
+    public AuthHandler<?> create() {
+        // using String as authentication object: returning username if authenticated, null otherwise
+        return new AbstractBasicAuthHandler<String>() {
+            @Override
+            protected String authenticate(final String username, final String password) {
+                final var found = knownHashes.get(username);
+                return found != null && found.hash.equals(Crypt.crypt(password, found.salt)) ? username : null;
+            }
+        };
+    }
+}
