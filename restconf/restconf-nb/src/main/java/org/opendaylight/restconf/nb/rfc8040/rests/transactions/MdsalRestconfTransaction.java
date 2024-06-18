c8d9a4862e0b79c22c582b2f563faa0fdc9257f0
Robert Varga <robert.varga@pantheon.tech>
2024-06-18 10:38:17 +0200

    Eliminate TransactionUtil
    The methods here are primarily used in RestconfStrategy implementations.
    Rehost them there.
    
    JIRA: NETCONF-1188
    Change-Id: Ib0e2425a7664601981c6f0c2cfd352856a0bd358
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- restconf/restconf-nb/src/main/java/org/opendaylight/restconf/nb/rfc8040/rests/transactions/MdsalRestconfTransaction.java
+++ restconf/restconf-nb/src/main/java/org/opendaylight/restconf/nb/rfc8040/rests/transactions/MdsalRestconfTransaction.java
@@ -57,7 +57,7 @@ final class MdsalRestconfTransaction extends RestconfTransaction {
 
     @Override
     void deleteImpl(final YangInstanceIdentifier path) throws ServerException {
-        if (TransactionUtil.syncAccess(verifyNotNull(rwTx).exists(CONFIGURATION, path), path)) {
+        if (RestconfStrategy.syncAccess(verifyNotNull(rwTx).exists(CONFIGURATION, path), path)) {
             rwTx.delete(CONFIGURATION, path);
         } else {
             LOG.trace("Operation via Restconf was not executed because data at {} does not exist", path);
@@ -182,6 +182,6 @@ final class MdsalRestconfTransaction extends RestconfTransaction {
 
     @Override
     NormalizedNodeContainer<?> readList(final YangInstanceIdentifier path) throws ServerException {
-        return (NormalizedNodeContainer<?>) TransactionUtil.syncAccess(read(path), path).orElse(null);
+        return (NormalizedNodeContainer<?>) RestconfStrategy.syncAccess(read(path), path).orElse(null);
     }
 }
