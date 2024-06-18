19bd205a9d82ff8a81f812a09bcb77d9a3aca31e
Robert Varga <robert.varga@pantheon.tech>
2024-06-18 12:47:22 +0200

    Remove restconf-common
    Move the two remaining classes to restconf.server.api and remove
    restconf-common artifact.
    
    JIRA: NETCONF-773
    Change-Id: I14990e3b3e59ce77d9f1186338f3c09b8fa1e671
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- restconf/restconf-nb/src/main/java/org/opendaylight/restconf/server/api/PatchContext.java
+++ restconf/restconf-nb/src/main/java/org/opendaylight/restconf/server/api/PatchContext.java
@@ -5,14 +5,16 @@
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
-package org.opendaylight.restconf.common.patch;
+package org.opendaylight.restconf.server.api;
 
 import static java.util.Objects.requireNonNull;
 
+import com.google.common.annotations.Beta;
 import com.google.common.collect.ImmutableList;
 import java.util.List;
 import org.eclipse.jdt.annotation.NonNullByDefault;
 
+@Beta
 @NonNullByDefault
 public record PatchContext(String patchId, ImmutableList<PatchEntity> entities) {
     public PatchContext {
