0ab7670054f48441bc840e29ec0991d65d3e5748
lubos-cicut <lubos.cicut@pantheon.tech>
2024-06-28 12:10:59 +0200

    Make UncancellableFuture always non cancellable
    Clean up UncancellableFuture.java and make uncancellation
    to always true.
    
    JIRA: NETCONF-1336
    Change-Id: I7142aa07bb0518f8163b6c56496872b9bd674f58
    Signed-off-by: lubos-cicut <lubos.cicut@pantheon.tech>

--- plugins/netconf-client-mdsal/src/main/java/org/opendaylight/netconf/client/mdsal/UncancellableFuture.java
+++ plugins/netconf-client-mdsal/src/main/java/org/opendaylight/netconf/client/mdsal/UncancellableFuture.java
@@ -7,45 +7,28 @@
  */
 package org.opendaylight.netconf.client.mdsal;
 
-import static com.google.common.base.Preconditions.checkState;
-
 import com.google.common.util.concurrent.AbstractFuture;
 
+/**
+ * A future that cannot be cancelled. Used to communicate with the device and wait for a response.
+ */
 final class UncancellableFuture<V> extends AbstractFuture<V> {
-    private volatile boolean uncancellable = false;
 
-    // FIXME rework this class be have uncancellable always true
-    UncancellableFuture(final boolean uncancellable) {
-        this.uncancellable = uncancellable;
-    }
-
-    public boolean setUncancellable() {
-        if (isCancelled()) {
-            return false;
-        }
-
-        uncancellable = true;
-        return true;
-    }
-
-    public boolean isUncancellable() {
-        return uncancellable;
+    UncancellableFuture() {
     }
 
     @Override
     public boolean cancel(final boolean mayInterruptIfRunning) {
-        return !uncancellable && super.cancel(mayInterruptIfRunning);
+        return false;
     }
 
     @Override
     public boolean set(final V value) {
-        checkState(uncancellable);
         return super.set(value);
     }
 
     @Override
     protected boolean setException(final Throwable throwable) {
-        checkState(uncancellable);
         return super.setException(throwable);
     }
 }
