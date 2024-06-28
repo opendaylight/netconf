78d585b4c56dd1030175f0168e2d2d75582e1aac
Ivan Hrasko <ivan.hrasko@pantheon.tech>
2024-06-28 13:09:16 +0200

    Use PathsEntity again
    We got lost the solution of NETCONF-938 in NETCONF-1241.
    This patch brings it back.
    
    JIRA: NETCONF-1337
    Change-Id: I1b4cb06e5e3e689ac8413baa98db5151da124fc6
    Signed-off-by: Ivan Hrasko <ivan.hrasko@pantheon.tech>
    Signed-off-by: lubos-cicut <lubos.cicut@pantheon.tech>

--- restconf/restconf-openapi/src/main/java/org/opendaylight/restconf/openapi/model/OpenApiEntity.java
+++ restconf/restconf-openapi/src/main/java/org/opendaylight/restconf/openapi/model/OpenApiEntity.java
@@ -15,7 +15,8 @@ import org.eclipse.jdt.annotation.NonNull;
  * A response entity for complex generated type.
  */
 public abstract sealed class OpenApiEntity permits ComponentsEntity, InfoEntity, OpenApiVersionEntity, OperationEntity,
-        PathEntity, SchemaEntity, SchemasEntity, SecurityEntity, SecuritySchemesEntity, ServerEntity, ServersEntity {
+        PathEntity, PathsEntity, SchemaEntity, SchemasEntity, SecurityEntity, SecuritySchemesEntity, ServerEntity,
+        ServersEntity {
     /**
      * Generate JSON events into specified generator.
      *
