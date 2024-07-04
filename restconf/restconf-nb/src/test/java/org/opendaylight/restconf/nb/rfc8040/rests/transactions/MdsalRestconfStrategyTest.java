b9dd9c02b746c9c6a5a2e1ed249e53890e53a18b
Yaroslav Lastivka <yaroslav.lastivka@pantheon.tech>
2024-07-04 14:15:21 +0300

    Localize ensureParentsByMerge inside MdsalRestconfTransaction
    Added ensureParentsByMerge into MdsalRestconfTransaction#mergeImpl.
    Eliminate usage of ensureParentsByMerge outside of MdsalRestconfTransaction.
    Eliminate usage of emptySubtree logic.
    
    JIRA: NETCONF-1330
    Change-Id: I3bb0dbb683a24278410534bfc51f9adb83e34da6
    Signed-off-by: Yaroslav Lastivka <yaroslav.lastivka@pantheon.tech>

--- restconf/restconf-nb/src/test/java/org/opendaylight/restconf/nb/rfc8040/rests/transactions/MdsalRestconfStrategyTest.java
+++ restconf/restconf-nb/src/test/java/org/opendaylight/restconf/nb/rfc8040/rests/transactions/MdsalRestconfStrategyTest.java
@@ -172,7 +172,7 @@ final class MdsalRestconfStrategyTest extends AbstractRestconfStrategyTest {
         doReturn(tx).when(strategy).prepareWriteExecution();
 
         strategy.putData(dataPutRequest, GAP_IID, GAP_LEAF);
-        verify(tx).merge(eq(GAP_IID.getAncestor(1)), any());
+        verify(readWrite).merge(eq(LogicalDatastoreType.CONFIGURATION), eq(GAP_IID.getAncestor(1)), any());
         assertNotNull(dataPutRequest.getResult());
     }
 
