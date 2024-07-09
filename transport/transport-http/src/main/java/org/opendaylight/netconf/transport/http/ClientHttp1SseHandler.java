dee60f31e10c815a61fa6c7f72997d95c7113982
Ruslan Kashapov <ruslan.kashapov@pantheon.tech>
2024-07-09 10:14:57 +0300

    Server-Sent Event (SSE) support for HTTP transport
    SSE stream handlers are implemented as overlay over
    existing HTTP layer to be deployed as extension using
    transport channel listeners.
    
    Stream is implemented as chunked response, handlers are
    intercepting inbound response chunks as event messages.
    
    Listener and service interfaces are provided for custom
    components integration for both client and server sides
    
    JIRA: NETCONF-1339
    Change-Id: I9dbb68a7695f9d6f87ce522bbe2c1dbf24c51832
    Signed-off-by: Ruslan Kashapov <ruslan.kashapov@pantheon.tech>

---
+++ transport/transport-http/src/main/java/org/opendaylight/netconf/transport/http/ClientHttp1SseHandler.java
@@ -0,0 +1,145 @@
+/*
+ * Copyright (c) 2024 PANTHEON.tech s.r.o. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.netconf.transport.http;
+
+import static java.util.Objects.requireNonNull;
+import static org.opendaylight.netconf.transport.http.SseUtils.SseConsumerState.AWAITING_ERROR_MESSAGE;
+import static org.opendaylight.netconf.transport.http.SseUtils.SseConsumerState.AWAITING_EVENTS;
+import static org.opendaylight.netconf.transport.http.SseUtils.SseConsumerState.AWAITING_RESPONSE;
+import static org.opendaylight.netconf.transport.http.SseUtils.processChunks;
+
+import com.google.common.util.concurrent.FutureCallback;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.ByteBufUtil;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.handler.codec.http.FullHttpResponse;
+import io.netty.handler.codec.http.HttpContent;
+import io.netty.handler.codec.http.HttpHeaderNames;
+import io.netty.handler.codec.http.HttpHeaderValues;
+import io.netty.handler.codec.http.HttpResponse;
+import io.netty.handler.codec.http.HttpResponseStatus;
+import io.netty.handler.codec.http.LastHttpContent;
+import java.nio.charset.StandardCharsets;
+import org.opendaylight.netconf.transport.http.SseUtils.SseConsumerState;
+import org.opendaylight.yangtools.concepts.Registration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Client side Server-Sent Event (SSE) handler for HTTP 1.1.
+ *
+ * <p>
+ * Deployed by {@link ClientHttp1SseService} after SSE stream request sent to process the server response.
+ * The expected {@link HttpResponse} object (header part) with status {@code OK} and
+ * {@code content-type=text/event-stream} header indicates the stream request being accepted by the server
+ * (stream beginning), then subsequent {@link HttpContent} objects are treated as event messages.
+ * Other responses are treated as server request decline, message content as error message.
+ *
+ * <p>
+ * On request decline or stream end the handler self-removed from a channel pipeline to unblock underlying
+ * HTTP 1 request/response traffic. While being active all the inbound traffic is consumed.
+ */
+final class ClientHttp1SseHandler extends ChannelInboundHandlerAdapter {
+    private static final Logger LOG = LoggerFactory.getLogger(ClientHttp1SseHandler.class);
+
+    private final String uri;
+    private final FutureCallback<Registration> requestCallback;
+    private final EventStreamListener listener;
+    private final Runnable onClose;
+
+    private SseConsumerState state = AWAITING_RESPONSE;
+
+    ClientHttp1SseHandler(final String uri, final EventStreamListener listener,
+            final FutureCallback<Registration> requestCallback, final Runnable onClose) {
+        this.uri = requireNonNull(uri);
+        this.listener = requireNonNull(listener);
+        this.requestCallback = requestCallback;
+        this.onClose = onClose;
+    }
+
+    String uri() {
+        return uri;
+    }
+
+    @Override
+    public void channelRead(final ChannelHandlerContext ctx, final Object msg) throws Exception {
+        final var currentState = state;
+        if (AWAITING_RESPONSE == currentState && msg instanceof HttpResponse response) {
+            // response header to stream request
+            if (HttpResponseStatus.OK.equals(response.status()) && response.headers()
+                .contains(HttpHeaderNames.CONTENT_TYPE, HttpHeaderValues.TEXT_EVENT_STREAM, true)) {
+                // stream request accepted
+                state = AWAITING_EVENTS;
+                listener.onStreamStart();
+                // notify SSE request succeeded with Registration (Closable)
+                requestCallback.onSuccess(() -> {
+                    if (!ctx.isRemoved() && ctx.channel().isActive()) {
+                        // terminate connection if current handler is attached to active connection
+                        // so the server is notified the stream no longer consumed
+                        ctx.channel().close();
+                    }
+                });
+            } else {
+                // stream request declined
+                if (msg instanceof FullHttpResponse fullResponse) {
+                    // error message is in current object
+                    onRequestDeclineError(ctx, fullResponse.content(), true);
+                } else {
+                    // error message to be delivered as separate object
+                    state = AWAITING_ERROR_MESSAGE;
+                }
+            }
+        } else if (AWAITING_ERROR_MESSAGE == currentState && msg instanceof HttpContent content) {
+            // decline response body
+            onRequestDeclineError(ctx, content.content(), content instanceof LastHttpContent);
+
+        } else if (AWAITING_EVENTS == currentState && msg instanceof HttpContent chunk) {
+            // event chunk
+            processChunks(chunk.content(), listener);
+
+            // if last one
+            if (msg instanceof LastHttpContent) {
+                // end stream
+                LOG.debug("SSE stream ended for URI={}", uri);
+                listener.onStreamEnd();
+                // nothing to expect -- remove self
+                ctx.pipeline().remove(this);
+            }
+        } else {
+            LOG.warn("Unexpected SSE message {} in state {}", msg, currentState);
+        }
+    }
+
+    private void onRequestDeclineError(final ChannelHandlerContext ctx, final ByteBuf error,
+        final boolean endOfStream) {
+        final var errorMessage = new String(ByteBufUtil.getBytes(error), StandardCharsets.UTF_8);
+        requestCallback.onFailure(new IllegalStateException(errorMessage));
+        if (endOfStream) {
+            ctx.pipeline().remove(this);
+        }
+    }
+
+    @Override
+    public void channelInactive(final ChannelHandlerContext ctx) throws Exception {
+        if (AWAITING_EVENTS == state) {
+            // stream already started, close it
+            listener.onStreamEnd();
+        } else {
+            // stream isn't started yet, notify request failed
+            requestCallback.onFailure(new IllegalStateException("Connection closed."));
+        }
+    }
+
+    @Override
+    public void handlerRemoved(final ChannelHandlerContext ctx) throws Exception {
+        if (onClose != null) {
+            onClose.run();
+        }
+    }
+}
--- transport/transport-http/src/test/java/org/opendaylight/netconf/transport/http/TestUtils.java
+++ transport/transport-http/src/test/java/org/opendaylight/netconf/transport/http/TestUtils.java
@@ -9,18 +9,57 @@ package org.opendaylight.netconf.transport.http;
 
 import static org.opendaylight.netconf.transport.http.AbstractBasicAuthHandler.BASIC_AUTH_PREFIX;
 
+import com.google.common.util.concurrent.FutureCallback;
+import com.google.common.util.concurrent.ListenableFuture;
+import com.google.common.util.concurrent.SettableFuture;
+import io.netty.buffer.ByteBufUtil;
+import io.netty.buffer.Unpooled;
+import io.netty.handler.codec.http.DefaultFullHttpResponse;
 import io.netty.handler.codec.http.DefaultHttpRequest;
+import io.netty.handler.codec.http.FullHttpRequest;
+import io.netty.handler.codec.http.FullHttpResponse;
 import io.netty.handler.codec.http.HttpHeaderNames;
 import io.netty.handler.codec.http.HttpMethod;
 import io.netty.handler.codec.http.HttpRequest;
 import io.netty.handler.codec.http.HttpVersion;
+import java.io.IOException;
+import java.math.BigInteger;
+import java.net.ServerSocket;
 import java.nio.charset.StandardCharsets;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.PrivateKey;
+import java.security.SecureRandom;
+import java.security.cert.X509Certificate;
+import java.security.spec.ECGenParameterSpec;
+import java.security.spec.RSAKeyGenParameterSpec;
+import java.time.Duration;
+import java.time.Instant;
 import java.util.Base64;
+import java.util.Date;
+import org.bouncycastle.asn1.x500.X500Name;
+import org.bouncycastle.cert.jcajce.JcaX509CertificateConverter;
+import org.bouncycastle.cert.jcajce.JcaX509v3CertificateBuilder;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.bouncycastle.operator.jcajce.JcaContentSignerBuilder;
 
 final class TestUtils {
+    private static final SecureRandom SECURE_RANDOM = new SecureRandom();
 
     private TestUtils() {
         // hidden on purpose
+    }
+
+    static int freePort() {
+        // find free port
+        try {
+            final var socket = new ServerSocket(0);
+            final var localPort = socket.getLocalPort();
+            socket.close();
+            return localPort;
+        } catch (IOException e) {
+            throw new AssertionError(e);
+        }
     }
 
     static String basicAuthHeader(final String username, final String password) {
@@ -38,5 +77,64 @@ final class TestUtils {
             request.headers().add(HttpHeaderNames.AUTHORIZATION, authHeader);
         }
         return request;
+    }
+
+    static ListenableFuture<FullHttpResponse> invoke(final HTTPClient client, final FullHttpRequest request) {
+        final var future = SettableFuture.<FullHttpResponse>create();
+        client.invoke(request, new FutureCallback<>() {
+            @Override
+            public void onSuccess(final FullHttpResponse response) {
+                // To simplify the test code we use future on top of callback.
+                // Due to content of response object is released (cleared) on exit from this method
+                // we need to ensure the content is copied channel independent byte buffer.
+                // Using response.copy() is not suitable because it uses same byte buf allocator
+                // as the original message, this may result ResourceLeakDetector exception
+                // if copied content buffer is not released (read) in a moment when
+                // byte buf allocator's garbage collector is called.
+                final var copy = new DefaultFullHttpResponse(response.protocolVersion(), response.status());
+                copy.headers().set(response.headers());
+                copy.replace(Unpooled.wrappedBuffer(ByteBufUtil.getBytes(response.content())));
+                future.set(copy);
+            }
+
+            @Override
+            public void onFailure(final Throwable cause) {
+                future.setException(cause);
+            }
+        });
+        return future;
+    }
+
+    static X509CertData generateX509CertData(final String algorithm) throws Exception {
+        final var keyPairGenerator = KeyPairGenerator.getInstance(algorithm);
+        if (isRSA(algorithm)) {
+            keyPairGenerator.initialize(new RSAKeyGenParameterSpec(2048, RSAKeyGenParameterSpec.F4), SECURE_RANDOM);
+        } else {
+            keyPairGenerator.initialize(new ECGenParameterSpec("secp256r1"), SECURE_RANDOM);
+        }
+        final var keyPair = keyPairGenerator.generateKeyPair();
+        final var certificate = generateCertificate(keyPair, isRSA(algorithm) ? "SHA256withRSA" : "SHA256withECDSA");
+        return new X509CertData(certificate, keyPair.getPrivate());
+    }
+
+    static X509Certificate generateCertificate(final KeyPair keyPair, final String hashAlgorithm) throws Exception {
+        final var now = Instant.now();
+        final var contentSigner = new JcaContentSignerBuilder(hashAlgorithm).build(keyPair.getPrivate());
+
+        final var x500Name = new X500Name("CN=TestCertificate");
+        final var certificateBuilder = new JcaX509v3CertificateBuilder(x500Name,
+            BigInteger.valueOf(now.toEpochMilli()),
+            Date.from(now), Date.from(now.plus(Duration.ofDays(365))),
+            x500Name,
+            keyPair.getPublic());
+        return new JcaX509CertificateConverter()
+            .setProvider(new BouncyCastleProvider()).getCertificate(certificateBuilder.build(contentSigner));
+    }
+
+    static boolean isRSA(final String algorithm) {
+        return "RSA".equals(algorithm);
+    }
+
+    record X509CertData(X509Certificate certificate, PrivateKey privateKey) {
     }
 }
