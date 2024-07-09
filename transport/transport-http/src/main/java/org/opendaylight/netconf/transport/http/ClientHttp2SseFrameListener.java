bb7cd05ff73d0bf2ae3f6724958cf1ab0e77087e
Ruslan Kashapov <ruslan.kashapov@pantheon.tech>
2024-07-09 10:14:57 +0300

    Server-Sent Event (SSE) support for HTTP transport
    SSE stream handlers are implemented as overlay over
    existing HTTP layer to be deployed as extension using
    transport channel listeners.
    
    Stream is implemented as chunked response, handlers are
    intercepting inbound response chunks as event messages.
    
    Listener and service interfaces are provided for custom
    components integration for both client and server sides
    
    JIRA: NETCONF-1339
    Change-Id: I9dbb68a7695f9d6f87ce522bbe2c1dbf24c51832
    Signed-off-by: Ruslan Kashapov <ruslan.kashapov@pantheon.tech>

---
+++ transport/transport-http/src/main/java/org/opendaylight/netconf/transport/http/ClientHttp2SseFrameListener.java
@@ -0,0 +1,137 @@
+/*
+ * Copyright (c) 2024 PANTHEON.tech s.r.o. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.netconf.transport.http;
+
+import static java.util.Objects.requireNonNull;
+import static org.opendaylight.netconf.transport.http.SseUtils.processChunks;
+
+import com.google.common.util.concurrent.FutureCallback;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.ByteBufUtil;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.http.HttpHeaderNames;
+import io.netty.handler.codec.http.HttpResponseStatus;
+import io.netty.handler.codec.http2.Http2Exception;
+import io.netty.handler.codec.http2.Http2FrameAdapter;
+import io.netty.handler.codec.http2.Http2Headers;
+import io.netty.handler.codec.http2.HttpConversionUtil;
+import java.nio.charset.StandardCharsets;
+import java.util.concurrent.atomic.AtomicBoolean;
+import org.opendaylight.yangtools.concepts.Registration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Client side Server-Sent Event (SSE) HTTP/2 frame listener implementation.
+ *
+ * <p>
+ * Acts as alternative inbound stream processor invoked by {@link Http2ToHttpAdapter}. Intercepts header and data
+ * frames for associated stream-id.
+ *
+ * <p>
+ * The expected header frame with response status {@code OK} and {@code content-type=text/event-stream} header
+ * indicates the stream request being accepted by the server (stream beginning), then subsequent data frames are
+ * treated as event messages. Other headers are treated as server request decline, following data frame is treated
+ * as error message.
+ *
+ */
+final class ClientHttp2SseFrameListener extends Http2FrameAdapter {
+    private static final Logger LOG = LoggerFactory.getLogger(ClientHttp2SseFrameListener.class);
+
+    private final String uri;
+    private final FutureCallback<Registration> requestCallback;
+    private final EventStreamListener listener;
+    private final Runnable onClose;
+    private final AtomicBoolean streaming = new AtomicBoolean(false);
+    private final AtomicBoolean awaitingErrorMessage = new AtomicBoolean(false);
+
+    ClientHttp2SseFrameListener(final String uri, final EventStreamListener listener,
+            final FutureCallback<Registration> requestCallback, final Runnable onClose) {
+        this.uri = uri;
+        this.listener = requireNonNull(listener);
+        this.requestCallback = requestCallback;
+        this.onClose = onClose;
+    }
+
+    EventStreamListener listener() {
+        return listener;
+    }
+
+    @Override
+    public void onHeadersRead(final ChannelHandlerContext ctx, final int streamId, final Http2Headers headers,
+        final int padding, final boolean endStream) throws Http2Exception {
+        onHeadersRead(headers, endStream);
+    }
+
+    @Override
+    public void onHeadersRead(final ChannelHandlerContext ctx, final int streamId, final Http2Headers headers,
+        final int streamDependency, final short weight, final boolean exclusive, final int padding,
+        final boolean endStream) throws Http2Exception {
+        onHeadersRead(headers, endStream);
+    }
+
+    private void onHeadersRead(final Http2Headers headers, final boolean endStream) throws Http2Exception {
+        if (streaming.get()) {
+            LOG.warn("Unexpected headers while data stream is expected. {}", headers);
+            onClose();
+            return;
+        }
+        final var status = HttpConversionUtil.parseStatus(headers.status());
+        if (HttpResponseStatus.OK.equals(status)
+            && headers.contains(HttpHeaderNames.CONTENT_TYPE, SseUtils.TEXT_EVENT_STREAM, true)) {
+            // stream request accepted
+            streaming.set(true);
+            listener.onStreamStart();
+            // notify SSE request succeeded with Registration (Closable)
+            // TODO: notify server to close stream (require server side frame listener)
+            requestCallback.onSuccess(ClientHttp2SseFrameListener.this::onClose);
+        } else {
+            // stream request declined
+            if (endStream) {
+                // no error message, use status
+                requestCallback.onFailure(new IllegalStateException("Error: " + status));
+                onClose();
+            } else {
+                // error message to be delivered with data frame
+                awaitingErrorMessage.set(true);
+            }
+        }
+    }
+
+    @Override
+    public int onDataRead(final ChannelHandlerContext ctx, final int streamId, final ByteBuf data, final int padding,
+        final boolean endOfStream) throws Http2Exception {
+        final var readableBytes = data.readableBytes();
+        if (awaitingErrorMessage.get()) {
+            // decline response body
+            final var errorMessage = new String(ByteBufUtil.getBytes(data.slice()), StandardCharsets.UTF_8);
+            requestCallback.onFailure(new IllegalStateException(errorMessage));
+            onClose();
+
+        } else if (streaming.get()) {
+            // data chunk
+            processChunks(data, listener);
+            if (endOfStream) {
+                // end stream
+                LOG.debug("SSE stream ended for URI={}", uri);
+                listener.onStreamEnd();
+                onClose();
+            }
+        } else {
+            LOG.warn("Unexpected data frame ({} bytes) when headers are expected.", readableBytes);
+            onClose();
+        }
+        return padding + readableBytes;
+    }
+
+    private void onClose() {
+        if (onClose != null) {
+            onClose.run();
+        }
+    }
+}
