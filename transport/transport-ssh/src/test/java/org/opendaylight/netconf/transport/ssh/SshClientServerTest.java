07ee01a23ea1bb60f43e3d0504d4f4eb5bfbf3db
Matej Sramcik <matej.sramcik@pantheon.tech>
2024-07-10 14:15:28 +0200

    Increase timeouts for transport-ssh tests
    Change-Id: I49bc0860c70d42104b891a0a05b8badf85639042
    Signed-off-by: Matej Sramcik <matej.sramcik@pantheon.tech>

--- transport/transport-ssh/src/test/java/org/opendaylight/netconf/transport/ssh/SshClientServerTest.java
+++ transport/transport-ssh/src/test/java/org/opendaylight/netconf/transport/ssh/SshClientServerTest.java
@@ -237,9 +237,9 @@ public class SshClientServerTest {
             // connect with client
             final var client = clientBuilder.build().get(2, TimeUnit.SECONDS);
             try {
-                verify(serverListener, timeout(10_000))
+                verify(serverListener, timeout(60_000))
                         .onTransportChannelEstablished(serverTransportChannelCaptor.capture());
-                verify(clientListener, timeout(10_000))
+                verify(clientListener, timeout(15_000))
                         .onTransportChannelEstablished(clientTransportChannelCaptor.capture());
                 // validate channels are in expected state
                 var serverChannel = assertChannel(serverTransportChannelCaptor.getAllValues());
@@ -286,9 +286,9 @@ public class SshClientServerTest {
             final var server = FACTORY.connectServer(SUBSYSTEM, serverListener, tcpClientConfig, null,
                     serverConfigurator(username)).get(2, TimeUnit.SECONDS);
             try {
-                verify(serverListener, timeout(10_000))
+                verify(serverListener, timeout(15_000))
                     .onTransportChannelEstablished(serverTransportChannelCaptor.capture());
-                verify(clientListener, timeout(10_000))
+                verify(clientListener, timeout(15_000))
                     .onTransportChannelEstablished(clientTransportChannelCaptor.capture());
                 // validate channels are in expected state
                 var serverChannel = assertChannel(serverTransportChannelCaptor.getAllValues());
