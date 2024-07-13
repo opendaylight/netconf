5c60754e3ce9f979ff684eff9df55dc32e8d77d4
Robert Varga <robert.varga@pantheon.tech>
2024-07-13 14:20:33 +0200

    Fix TCPClient.connect() initialization
    The firing of ChannelFutureListeners and handler callbacks is slightly
    diifferent in that listeners may be fired in a separate pass, but
    handler callbacks are guaranteed to fire during event reception.
    
    We really want to fire when the channel becomes active, similarly to the
    no-op we have via ConnectChannelInitializer -- thus immediately updating
    the pipeline.
    
    This ensures that NettyIoSession.channelActive() has been invoked before
    Netty can deliver any events to it.
    
    While we are touching SshClientServerTest, make it package-protected and
    hide its fields, as JUnit5 can handle that.
    
    JIRA: NETCONF-1348
    Change-Id: I68a718d1870a572dc6a1a8bafd5c7b31e6ee4ece
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>
    (cherry picked from commit 53d9cb1203c7b02daae8b9b0422b850c69daae33)
    (cherry picked from commit 2ecd1454f0b45825caabd00e12791409b8827e4a)

--- transport/transport-ssh/src/test/java/org/opendaylight/netconf/transport/ssh/SshClientServerTest.java
+++ transport/transport-ssh/src/test/java/org/opendaylight/netconf/transport/ssh/SshClientServerTest.java
@@ -69,8 +69,7 @@ import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.tcp.server.
 import org.opendaylight.yangtools.yang.common.Uint16;
 
 @ExtendWith(MockitoExtension.class)
-public class SshClientServerTest {
-
+class SshClientServerTest {
     private static final String RSA = "RSA";
     private static final String EC = "EC";
     private static final String USER = "user";
@@ -92,9 +91,9 @@ public class SshClientServerTest {
     private TransportChannelListener serverListener;
 
     @Captor
-    ArgumentCaptor<TransportChannel> clientTransportChannelCaptor;
+    private ArgumentCaptor<TransportChannel> clientTransportChannelCaptor;
     @Captor
-    ArgumentCaptor<TransportChannel> serverTransportChannelCaptor;
+    private ArgumentCaptor<TransportChannel> serverTransportChannelCaptor;
 
     private static EventLoopGroup group;
     private ServerSocket socket;
