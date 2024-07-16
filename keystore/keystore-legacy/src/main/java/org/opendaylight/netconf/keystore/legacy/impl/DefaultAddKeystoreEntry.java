adfb164aa1bc50215888d3be0a5ed172813b6482
lubos-cicut <lubos.cicut@pantheon.tech>
2024-07-16 13:37:47 +0200

    Unify certificates and keys payload
    Use PEM format in payloads for rpcs input. Now for keystore rpcs
    requires the use of BEGIN and END prefixes. Input is validated in
    rpc, so we can no longer store an invalid key or certificate in the
    keystore.
    
    JIRA: NETCONF-1304
    Change-Id: I3e2608c895046136da816d7c1c714f00558f0c98
    Signed-off-by: lubos-cicut <lubos.cicut@pantheon.tech>
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- keystore/keystore-legacy/src/main/java/org/opendaylight/netconf/keystore/legacy/impl/DefaultAddKeystoreEntry.java
+++ keystore/keystore-legacy/src/main/java/org/opendaylight/netconf/keystore/legacy/impl/DefaultAddKeystoreEntry.java
@@ -7,14 +7,12 @@
  */
 package org.opendaylight.netconf.keystore.legacy.impl;
 
-import static java.util.Objects.requireNonNull;
-
 import com.google.common.util.concurrent.ListenableFuture;
 import com.google.common.util.concurrent.MoreExecutors;
-import java.nio.charset.StandardCharsets;
+import java.io.IOException;
 import java.security.GeneralSecurityException;
+import java.security.KeyPair;
 import java.util.ArrayList;
-import java.util.Base64;
 import org.opendaylight.aaa.encrypt.AAAEncryptionService;
 import org.opendaylight.mdsal.binding.api.DataBroker;
 import org.opendaylight.mdsal.common.api.LogicalDatastoreType;
@@ -26,20 +24,16 @@ import org.opendaylight.yang.gen.v1.urn.opendaylight.netconf.keystore.rev240708.
 import org.opendaylight.yang.gen.v1.urn.opendaylight.netconf.keystore.rev240708.keystore.entry.KeyCredential;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.netconf.keystore.rev240708.keystore.entry.KeyCredentialBuilder;
 import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
-import org.opendaylight.yangtools.yang.common.ErrorType;
 import org.opendaylight.yangtools.yang.common.RpcResult;
 import org.opendaylight.yangtools.yang.common.RpcResultBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-final class DefaultAddKeystoreEntry extends AbstractRpc implements AddKeystoreEntry {
+final class DefaultAddKeystoreEntry extends AbstractEncryptingRpc implements AddKeystoreEntry {
     private static final Logger LOG = LoggerFactory.getLogger(DefaultAddKeystoreEntry.class);
 
-    private final AAAEncryptionService encryptionService;
-
     DefaultAddKeystoreEntry(final DataBroker dataBroker, final AAAEncryptionService encryptionService) {
-        super(dataBroker);
-        this.encryptionService = requireNonNull(encryptionService);
+        super(dataBroker, encryptionService);
     }
 
     @Override
@@ -53,33 +47,40 @@ final class DefaultAddKeystoreEntry extends AbstractRpc implements AddKeystoreEn
         final var encrypted = new ArrayList<KeyCredential>(plain.size());
         for (var credential : plain.values()) {
             final var keyId = credential.getKeyId();
+
+            final KeyPair keyPair;
             try {
-                encrypted.add(new KeyCredentialBuilder()
-                    .setKeyId(credential.getKeyId())
-                    .setPrivateKey(encryptToBytes(credential.getPrivateKey()))
-                    .setPassphrase(encryptToBytes(credential.getPassphrase()))
-                    .build());
-            } catch (GeneralSecurityException e) {
-                LOG.debug("Cannot decrypt key credential {}}", credential, e);
-                return RpcResultBuilder.<AddKeystoreEntryOutput>failed()
-                    .withError(ErrorType.APPLICATION, "Failed to decrypt key " + keyId, e)
-                    .buildFuture();
+                keyPair = new SecurityHelper().decodePrivateKey(credential.getPrivateKey(), credential.getPassphrase());
+            } catch (IOException e) {
+                LOG.debug("Cannot decode private key {}}", keyId, e);
+                return returnFailed("Failed to decode private key " + keyId, e);
             }
+
+            final var priv = keyPair.getPrivate();
+            final byte[] encodedPriv;
+            final byte[] encodedPub;
+            try {
+                encodedPriv = encryptEncoded(priv.getEncoded());
+                encodedPub = encryptEncoded(keyPair.getPublic().getEncoded());
+            } catch (GeneralSecurityException e) {
+                LOG.debug("Cannot encrypt key credential {}}", credential, e);
+                return returnFailed("Failed to encrypt key credential " + keyId, e);
+            }
+
+            encrypted.add(new KeyCredentialBuilder()
+                .setKeyId(credential.getKeyId())
+                .setAlgorithm(priv.getAlgorithm())
+                .setPrivateKey(encodedPriv)
+                .setPublicKey(encodedPub)
+                .build());
         }
 
         final var tx = newTransaction();
-        for (var keypair : encrypted) {
-            tx.put(LogicalDatastoreType.CONFIGURATION,
-                InstanceIdentifier.create(Keystore.class).child(KeyCredential.class, keypair.key()), keypair);
-        }
-
+        encrypted.forEach(keypair -> tx.put(LogicalDatastoreType.CONFIGURATION,
+            InstanceIdentifier.create(Keystore.class).child(KeyCredential.class, keypair.key()), keypair));
         return tx.commit().transform(commitInfo -> {
             LOG.debug("Updated keypairs: {}", plain.keySet());
             return RpcResultBuilder.success(new AddKeystoreEntryOutputBuilder().build()).build();
         }, MoreExecutors.directExecutor());
-    }
-
-    private byte[] encryptToBytes(final String plain) throws GeneralSecurityException {
-        return Base64.getEncoder().encode(encryptionService.encrypt(plain.getBytes(StandardCharsets.UTF_8)));
     }
 }
