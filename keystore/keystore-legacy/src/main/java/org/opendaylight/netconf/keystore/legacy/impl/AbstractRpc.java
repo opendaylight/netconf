010b8c7da0b3b95c96863e2b2cbd32e2da82f2cf
lubos-cicut <lubos.cicut@pantheon.tech>
2024-07-16 13:37:47 +0200

    Unify certificates and keys payload
    Use PEM format in payloads for rpcs.
    
    JIRA: NETCONF-1304
    Change-Id: I3e2608c895046136da816d7c1c714f00558f0c98
    Signed-off-by: lubos-cicut <lubos.cicut@pantheon.tech>

--- keystore/keystore-legacy/src/main/java/org/opendaylight/netconf/keystore/legacy/impl/AbstractRpc.java
+++ keystore/keystore-legacy/src/main/java/org/opendaylight/netconf/keystore/legacy/impl/AbstractRpc.java
@@ -9,18 +9,43 @@ package org.opendaylight.netconf.keystore.legacy.impl;
 
 import static java.util.Objects.requireNonNull;
 
+import com.google.common.util.concurrent.ListenableFuture;
+import java.security.GeneralSecurityException;
 import org.eclipse.jdt.annotation.NonNull;
+import org.opendaylight.aaa.encrypt.AAAEncryptionService;
 import org.opendaylight.mdsal.binding.api.DataBroker;
 import org.opendaylight.mdsal.binding.api.WriteTransaction;
+import org.opendaylight.yangtools.yang.common.ErrorType;
+import org.opendaylight.yangtools.yang.common.RpcResult;
+import org.opendaylight.yangtools.yang.common.RpcResultBuilder;
 
 abstract class AbstractRpc {
     private final DataBroker dataBroker;
+    private final AAAEncryptionService encryptionService;
+
 
     AbstractRpc(final DataBroker dataBroker) {
         this.dataBroker = requireNonNull(dataBroker);
+        this.encryptionService = null;
+    }
+
+    AbstractRpc(final DataBroker dataBroker, final AAAEncryptionService encryptionService) {
+        this.dataBroker = requireNonNull(dataBroker);
+        this.encryptionService = requireNonNull(encryptionService);
     }
 
     final @NonNull WriteTransaction newTransaction() {
         return dataBroker.newWriteOnlyTransaction();
+    }
+
+    final byte @NonNull [] encryptEncoded(final byte[] bytes) throws GeneralSecurityException {
+        requireNonNull(encryptionService);
+        return encryptionService.encrypt(bytes);
+    }
+
+    final <T> ListenableFuture<RpcResult<T>> returnFailed(final String message, final Exception exception) {
+        return RpcResultBuilder.<T>failed()
+            .withError(ErrorType.APPLICATION, message, exception)
+            .buildFuture();
     }
 }
