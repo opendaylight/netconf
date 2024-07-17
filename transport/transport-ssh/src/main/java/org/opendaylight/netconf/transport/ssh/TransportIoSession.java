b4502d36a523a6a99ba53ae3faa11d7bb3b13abf
Ruslan Kashapov <ruslan.kashapov@pantheon.tech>
2024-07-17 11:08:31 +0300

    Fix SSH connection initialization failure
    SSH connection is served by netty adapter which builds
    mina Session on handler placement, and attaches to IoSession.
    In case IoSession has no Session object attached yet but
    inbound message is passed already then read operation fails.
    
    The fix ensures initialization is completed before read
    operation is invoked.
    
    Signed-off-by: Ruslan Kashapov <ruslan.kashapov@pantheon.tech>
    Change-Id: Iadf1efc6829f0a7d7e050c5f4c2f581495060404
    Signed-off-by: Ruslan Kashapov <ruslan.kashapov@pantheon.tech>

--- transport/transport-ssh/src/main/java/org/opendaylight/netconf/transport/ssh/TransportIoSession.java
+++ transport/transport-ssh/src/main/java/org/opendaylight/netconf/transport/ssh/TransportIoSession.java
@@ -10,10 +10,18 @@ package org.opendaylight.netconf.transport.ssh;
 import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelHandlerContext;
 import java.net.SocketAddress;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 import org.opendaylight.netconf.shaded.sshd.common.io.IoHandler;
+import org.opendaylight.netconf.shaded.sshd.common.util.Readable;
 import org.opendaylight.netconf.shaded.sshd.netty.NettyIoSession;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 final class TransportIoSession extends NettyIoSession {
+    private static final Logger LOG = LoggerFactory.getLogger(TransportIoSession.class);
+    private final CountDownLatch initialized = new CountDownLatch(1);
+
     TransportIoSession(final TransportIoService service, final IoHandler handler,
             final SocketAddress acceptanceAddress) {
         super(service, handler, acceptanceAddress);
@@ -24,9 +32,26 @@ final class TransportIoSession extends NettyIoSession {
     }
 
     @Override
+    protected void channelActive(final ChannelHandlerContext ctx) throws Exception {
+        super.channelActive(ctx);
+        initialized.countDown();
+    }
+
+    @Override
+    protected void channelRead(final ChannelHandlerContext ctx, final Readable msg) throws Exception {
+        // channelActive() builds the SSH Session instance and attaches it to current IoSession instance;
+        // current method expects the SSH Session being already attached and can be accessed for data input,
+        // so we need assurance the channelActive() method execution is completed before executing current one
+        if (!initialized.await(2, TimeUnit.SECONDS)) {
+            LOG.warn("Read operation is invoked while initialization cannot be completed for more than 2 sec");
+        }
+        super.channelRead(ctx, msg);
+    }
+
+    @Override
     protected void channelInactive(final ChannelHandlerContext ctx) throws Exception {
-        // adapter is not propagating fireChannelInactive() down the the pipeline, but instead loops here. Once we have
-        // cleaned up, propagate fireChannelInactive() outselves.
+        // adapter is not propagating fireChannelInactive() down the pipeline, but instead loops here.
+        // Once we have cleaned up, propagate fireChannelInactive() by ourselves.
         super.channelInactive(ctx);
         ctx.fireChannelInactive();
     }
