84e144121639288d6e257c48c1b509db79342e30
Robert Varga <robert.varga@pantheon.tech>
2024-07-19 01:59:13 +0200

    Refactor ServerRpcOperations implementations
    We support both local and DOMRpcService-mediated RPC implementations,
    but have the relationship rather hard-coded.
    
    InterceptingServerRpcOperations provides the ability to look up a
    RpcImplementation and delegate to it. If the lookup fails, the request
    will be forwarded to a downstream ServerRpcOperations.
    
    A default downstream is provided as NotSupportedServerRpcOperations,
    which always completes the request with an ErrorTag.NOT_SUPPORTED
    error.
    
    We also split out DOMRpcService-based implementation into
    MdsalServerRpcOperations and have RestconfStrategy instantiate a
    combination of these two.
    
    The same is true for a DOMActionService-based implementation, named
    MdsalServerActionOperations.
    
    JIRA: NETCONF-1331
    Change-Id: Icdf1690a816a67b972eb331e56db038cd0474e1e
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ protocol/restconf-server-spi/src/main/java/org/opendaylight/restconf/server/spi/NotSupportedServerActionOperations.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2024 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.restconf.server.spi;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.opendaylight.restconf.server.api.DatabindPath.Action;
+import org.opendaylight.restconf.server.api.InvokeResult;
+import org.opendaylight.restconf.server.api.ServerException;
+import org.opendaylight.restconf.server.api.ServerRequest;
+import org.opendaylight.yangtools.yang.common.ErrorTag;
+import org.opendaylight.yangtools.yang.common.ErrorType;
+import org.opendaylight.yangtools.yang.data.api.schema.ContainerNode;
+
+/**
+ * A {@link ServerRpcOperations} implementation fails all {@link #invokeAction(ServerRequest, Action, ContainerNode)}
+ * requests with {@link ErrorTag#OPERATION_NOT_SUPPORTED}.
+ */
+@NonNullByDefault
+public final class NotSupportedServerActionOperations implements ServerActionOperations {
+    public static final NotSupportedServerActionOperations INSTANCE = new NotSupportedServerActionOperations();
+
+    private NotSupportedServerActionOperations() {
+        // Hidden on purpose
+    }
+
+    @Override
+    public void invokeAction(final ServerRequest<? super InvokeResult> request, final Action path,
+            final ContainerNode input) {
+        request.completeWith(new ServerException(ErrorType.PROTOCOL, ErrorTag.OPERATION_NOT_SUPPORTED,
+            "Action not supported"));
+    }
+}
