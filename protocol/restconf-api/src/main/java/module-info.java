f72f617d76a53c21daf9a0c1a46ff39043fdfb35
Robert Varga <robert.varga@pantheon.tech>
2024-07-20 14:45:09 +0200

    Add restconf.api module documentation
    Add a simple block of Javadoc.
    
    JIRA: NETCONF-773
    Change-Id: I3379d619aca3359a0df902558043f6bd438a289e
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- protocol/restconf-api/src/main/java/module-info.java
+++ protocol/restconf-api/src/main/java/module-info.java
@@ -5,6 +5,9 @@
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
+/**
+ * Common RESTCONF constructs.
+ */
 module org.opendaylight.restconf.api {
     exports org.opendaylight.restconf.api;
     exports org.opendaylight.restconf.api.query;
