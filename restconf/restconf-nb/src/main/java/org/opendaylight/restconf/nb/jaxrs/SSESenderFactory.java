0432d2abdf23419ba9b52424e9b25d71e5cb5309
Robert Varga <robert.varga@pantheon.tech>
2024-07-23 00:36:41 +0200

    Convert SSESenderFactory into a Component
    SSESenderFactory encapulates a configured component, let's express that
    in OSGi SR.
    
    JIRA: NETCONF-773
    Change-Id: Ie687159240b1bb87277c9bf18f2bee7d22f5f634
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- restconf/restconf-nb/src/main/java/org/opendaylight/restconf/nb/jaxrs/SSESenderFactory.java
+++ restconf/restconf-nb/src/main/java/org/opendaylight/restconf/nb/jaxrs/SSESenderFactory.java
@@ -10,30 +10,49 @@ package org.opendaylight.restconf.nb.jaxrs;
 import static java.util.Objects.requireNonNull;
 
 import java.io.UnsupportedEncodingException;
+import java.util.Map;
 import javax.ws.rs.BadRequestException;
 import javax.ws.rs.NotFoundException;
 import javax.ws.rs.sse.Sse;
 import javax.ws.rs.sse.SseEventSink;
 import javax.xml.xpath.XPathExpressionException;
-import org.eclipse.jdt.annotation.NonNullByDefault;
 import org.opendaylight.restconf.nb.rfc8040.streams.StreamsConfiguration;
 import org.opendaylight.restconf.server.api.EventStreamGetParams;
 import org.opendaylight.restconf.server.spi.RestconfStream;
 import org.opendaylight.restconf.server.spi.RestconfStream.EncodingName;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
 
 /**
  * A factory for creating {@link SSESender}s.
  */
-@NonNullByDefault
-public final class SSESenderFactory {
-    private final PingExecutor pingExecutor;
-    private final int maximumFragmentLength;
-    private final int heartbeatInterval;
+@Component(factory = SSESenderFactory.FACTORY_NAME, service = SSESenderFactory.class)
+public final class SSESenderFactory implements AutoCloseable {
+    public static final String FACTORY_NAME = "org.opendaylight.restconf.nb.jaxrs.SSESenderFactory";
 
-    public SSESenderFactory(final PingExecutor pingExecutor, final StreamsConfiguration configuration) {
-        this.pingExecutor = requireNonNull(pingExecutor);
-        heartbeatInterval = configuration.heartbeatInterval();
-        maximumFragmentLength = configuration.maximumFragmentLength();
+    private static final String PROP_NAME_PREFIX = ".namePrefix";
+    private static final String PROP_CORE_POOL_SIZE = ".corePoolSize";
+    private static final String PROP_STREAMS_CONFIGURATION = ".streamsConfiguration";
+
+    private final StreamsConfiguration configuration;
+    private final DefaultPingExecutor pingExecutor;
+
+    public SSESenderFactory(final StreamsConfiguration configuration, final String namePrefix, final int corePoolSize) {
+        this.configuration = requireNonNull(configuration);
+        pingExecutor = new DefaultPingExecutor(namePrefix, corePoolSize);
+    }
+
+    @Activate
+    public SSESenderFactory(final Map<String, ?> props) {
+        this((StreamsConfiguration) props.get(PROP_STREAMS_CONFIGURATION), (String) props.get(PROP_NAME_PREFIX),
+            (int) requireNonNull(props.get(PROP_CORE_POOL_SIZE)));
+    }
+
+    @Override
+    @Deactivate
+    public void close() {
+        pingExecutor.close();
     }
 
     void newSSESender(final SseEventSink sink, final Sse sse, final RestconfStream<?> stream,
@@ -41,7 +60,7 @@ public final class SSESenderFactory {
         // FIXME: invert control here: we should call 'listener.addSession()', which in turn should call
         //        handler.init()/handler.close()
         final var handler = new SSESender(pingExecutor, sink, sse, stream, encodingName, getParams,
-            maximumFragmentLength, heartbeatInterval);
+            configuration.maximumFragmentLength(), configuration.heartbeatInterval());
 
         try {
             handler.init();
@@ -50,5 +69,13 @@ public final class SSESenderFactory {
         } catch (IllegalArgumentException | XPathExpressionException e) {
             throw new BadRequestException(e.getMessage(), e);
         }
+    }
+
+    public static Map<String, ?> props(final StreamsConfiguration streamsConfiguration,
+            final String namePrefix, final int corePoolSize) {
+        return Map.of(
+            PROP_STREAMS_CONFIGURATION, streamsConfiguration,
+            PROP_NAME_PREFIX, namePrefix,
+            PROP_CORE_POOL_SIZE, corePoolSize);
     }
 }
