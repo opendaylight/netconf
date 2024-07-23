da1d32fdcf6a51c5849fe463a02984d5ca5037e3
Robert Varga <robert.varga@pantheon.tech>
2024-07-24 01:09:00 +0200

    Split out restconf-server-jaxrs
    We now have a neatly self-sufficient JAX-RS endpoint. Split it out into
    its own artifact.
    
    JIRA: NETCONF-773
    Change-Id: I350460030251e396ceb9dc0829c77f1de1d48de5
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ restconf/restconf-nb/src/test/java/org/opendaylight/restconf/server/jaxrs/RestconfSchemaServiceMountTest.java
@@ -0,0 +1,209 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.restconf.server.jaxrs;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.opendaylight.restconf.server.jaxrs.AbstractRestconfTest.assertEntity;
+import static org.opendaylight.restconf.server.jaxrs.AbstractRestconfTest.assertError;
+
+import com.google.common.io.CharStreams;
+import java.io.Reader;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.opendaylight.mdsal.dom.api.DOMActionService;
+import org.opendaylight.mdsal.dom.api.DOMDataBroker;
+import org.opendaylight.mdsal.dom.api.DOMRpcService;
+import org.opendaylight.mdsal.dom.api.DOMSchemaService;
+import org.opendaylight.mdsal.dom.broker.DOMMountPointServiceImpl;
+import org.opendaylight.mdsal.dom.spi.FixedDOMSchemaService;
+import org.opendaylight.restconf.api.ApiPath;
+import org.opendaylight.restconf.api.ErrorMessage;
+import org.opendaylight.restconf.api.query.PrettyPrintParam;
+import org.opendaylight.restconf.server.mdsal.MdsalDatabindProvider;
+import org.opendaylight.restconf.server.mdsal.MdsalRestconfServer;
+import org.opendaylight.restconf.server.spi.ErrorTagMapping;
+import org.opendaylight.restconf.server.spi.ErrorTags;
+import org.opendaylight.yangtools.yang.common.ErrorTag;
+import org.opendaylight.yangtools.yang.common.ErrorType;
+import org.opendaylight.yangtools.yang.common.QName;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
+import org.opendaylight.yangtools.yang.model.api.EffectiveModelContext;
+import org.opendaylight.yangtools.yang.model.api.source.YangTextSource;
+import org.opendaylight.yangtools.yang.test.util.YangParserTestUtils;
+
+/**
+ * Unit tests for {@code RestconfSchemaService}.
+ */
+@ExtendWith(MockitoExtension.class)
+class RestconfSchemaServiceMountTest {
+    private static final ApiPath MOUNT_POINT = AbstractRestconfTest.apiPath("mount-point-1:cont/yang-ext:mount");
+    private static final ApiPath NULL_MOUNT_POINT = AbstractRestconfTest.apiPath("mount-point-2:cont/yang-ext:mount");
+    private static final ApiPath NOT_EXISTING_MOUNT_POINT =
+        AbstractRestconfTest.apiPath("mount-point-3:cont/yang-ext:mount");
+
+    // schema context with modules behind mount point
+    private static final EffectiveModelContext SCHEMA_CONTEXT_BEHIND_MOUNT_POINT =
+        YangParserTestUtils.parseYangResourceDirectory("/modules/modules-behind-mount-point");
+    // schema context with mount points
+    private static final EffectiveModelContext SCHEMA_CONTEXT_WITH_MOUNT_POINTS =
+        YangParserTestUtils.parseYangResourceDirectory("/modules/mount-points");
+
+    // handlers
+    @Mock
+    private DOMDataBroker dataBroker;
+    @Mock
+    private DOMActionService actionService;
+    @Mock
+    private DOMRpcService rpcService;
+    @Mock
+    private YangTextSource yangSource;
+    @Mock
+    private Reader yangReader;
+
+    // service under test
+    private JaxRsRestconf restconf;
+
+    @BeforeEach
+    void setup() {
+        final var mountPointService = new DOMMountPointServiceImpl();
+        // create and register mount points
+        mountPointService
+                .createMountPoint(YangInstanceIdentifier.of(QName.create("mount:point:1", "2016-01-01", "cont")))
+                .addService(DOMSchemaService.class, new FixedDOMSchemaService(SCHEMA_CONTEXT_BEHIND_MOUNT_POINT))
+                .addService(DOMDataBroker.class, dataBroker)
+                .register();
+        mountPointService
+                .createMountPoint(YangInstanceIdentifier.of(QName.create("mount:point:2", "2016-01-01", "cont")))
+                .register();
+
+        restconf = new JaxRsRestconf(
+            new MdsalRestconfServer(new MdsalDatabindProvider(
+                new FixedDOMSchemaService(SCHEMA_CONTEXT_WITH_MOUNT_POINTS)), dataBroker, rpcService, actionService,
+                mountPointService),
+            ErrorTagMapping.RFC8040, PrettyPrintParam.FALSE);
+    }
+
+    /**
+     * Get schema with identifier of existing module behind mount point and check if correct module was found.
+     */
+    @Test
+    void getSchemaMountPointTest() throws Exception {
+        final var reader = assertEntity(Reader.class, 200,
+            ar -> restconf.modulesYangGET(MOUNT_POINT, "module1-behind-mount-point", "2014-02-03", ar));
+        assertEquals("""
+            module module1-behind-mount-point {
+              namespace module:1:behind:mount:point;
+              prefix mod1bemopo;
+              revision 2014-02-03;
+              rpc rpc-behind-module1;
+            }
+            """, CharStreams.toString(reader));
+    }
+
+    /**
+     * Get schema with identifier of not-existing module behind mount point. Trying to create
+     * <code>SchemaExportContext</code> with not-existing module behind mount point should result in error.
+     */
+    @Test
+    void getSchemaForNotExistingModuleMountPointTest() {
+        final var error = assertError(ar -> restconf.modulesYangGET(MOUNT_POINT, "not-existing", "2016-01-01", ar));
+        assertEquals(new ErrorMessage("Source not-existing@2016-01-01 not found"), error.message());
+        assertEquals(ErrorType.APPLICATION, error.type());
+        assertEquals(ErrorTag.DATA_MISSING, error.tag());
+    }
+
+    /**
+     * Try to get schema with <code>null</code> <code>SchemaContext</code> behind mount point when using
+     * <code>NULL_MOUNT_POINT</code>. Test is expected to fail with <code>NullPointerException</code>.
+     */
+    @Test
+    void getSchemaNullSchemaContextBehindMountPointTest() {
+        // make test - call service on mount point with null schema context
+        // NULL_MOUNT_POINT contains null schema context
+        final var error = assertError(
+            ar -> restconf.modulesYangGET(NULL_MOUNT_POINT, "module1-behind-mount-point", "2014-02-03", ar));
+        assertEquals(new ErrorMessage("Mount point does not expose DOMSchemaService"), error.message());
+        assertEquals(ErrorType.PROTOCOL, error.type());
+        assertEquals(ErrorTags.RESOURCE_DENIED_TRANSPORT, error.tag());
+        final var errorPath = error.path();
+        assertNotNull(errorPath);
+        assertEquals(YangInstanceIdentifier.of(QName.create("mount:point:2", "2016-01-01", "cont")), errorPath.path());
+    }
+
+    /**
+     * Try to get schema with empty (not valid) identifier behind mount point catching
+     * <code>RestconfDocumentedException</code>. Error type, error tag and error status code are compared to expected
+     * values.
+     */
+    @Test
+    void getSchemaWithEmptyIdentifierMountPointTest() {
+        final var error = assertError(ar -> restconf.modulesYangGET(MOUNT_POINT, "", null, ar));
+        assertEquals(new ErrorMessage("Identifier must start with character from set 'a-zA-Z_"), error.message());
+        assertEquals(ErrorType.PROTOCOL, error.type());
+        assertEquals(ErrorTag.INVALID_VALUE, error.tag());
+    }
+
+    /**
+     * Try to get schema behind mount point with not-parsable identifier catching
+     * <code>RestconfDocumentedException</code>. Error type, error tag and error status code are compared to expected
+     * values.
+     */
+    @Test
+    void getSchemaWithNotParsableIdentifierMountPointTest() {
+        final var error = assertError(ar -> restconf.modulesYangGET(MOUNT_POINT, "01_module", "2016-01-01", ar));
+        assertEquals(new ErrorMessage("Identifier must start with character from set 'a-zA-Z_"), error.message());
+        assertEquals(ErrorType.PROTOCOL, error.type());
+        assertEquals(ErrorTag.INVALID_VALUE, error.tag());
+    }
+
+    /**
+     * Try to get schema with wrong (not valid) identifier behind mount point catching
+     * <code>RestconfDocumentedException</code>. Error type, error tag and error status code are compared to expected
+     * values.
+     *
+     * <p>
+     * Not valid identifier contains only revision without module name.
+     */
+    @Test
+    void getSchemaWrongIdentifierMountPointTest() {
+        final var error = assertError(ar -> restconf.modulesYangGET(MOUNT_POINT, "2014-01-01", null, ar));
+        assertEquals(new ErrorMessage("Identifier must start with character from set 'a-zA-Z_"), error.message());
+        assertEquals(ErrorType.PROTOCOL, error.type());
+        assertEquals(ErrorTag.INVALID_VALUE, error.tag());
+    }
+
+    /**
+     * Try to get schema behind mount point with identifier when does not contain revision catching
+     * <code>RestconfDocumentedException</code>. Error type, error tag and error status code are compared to expected
+     * values.
+     */
+    @Test
+    void getSchemaWithoutRevisionMountPointTest() {
+        final var error = assertError(ar -> restconf.modulesYangGET(MOUNT_POINT, "module", null, ar));
+        assertEquals(new ErrorMessage("Source module not found"), error.message());
+        assertEquals(ErrorType.APPLICATION, error.type());
+        assertEquals(ErrorTag.DATA_MISSING, error.tag());
+    }
+
+    /**
+     * Negative test when mount point module is not found in current <code>SchemaContext</code> for mount points.
+     * <code>IllegalArgumentException</code> exception is expected.
+     */
+    @Test
+    void getSchemaContextWithNotExistingMountPointTest() {
+        final var error = assertError(
+            ar -> restconf.modulesYangGET(NOT_EXISTING_MOUNT_POINT, "module1-behind-mount-point", "2014-02-03", ar));
+        assertEquals(new ErrorMessage("Failed to lookup for module with name 'mount-point-3'."), error.message());
+        assertEquals(ErrorType.PROTOCOL, error.type());
+        assertEquals(ErrorTag.UNKNOWN_ELEMENT, error.tag());
+    }
+}
---
+++ restconf/restconf-nb/src/test/java/org/opendaylight/restconf/server/jaxrs/SSESessionHandlerTest.java
@@ -0,0 +1,221 @@
+/*
+ * Copyright (c) 2020 Lumina Networks, Inc. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.restconf.server.jaxrs;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+
+import java.util.concurrent.TimeUnit;
+import javax.ws.rs.sse.Sse;
+import javax.ws.rs.sse.SseEventSink;
+import org.glassfish.jersey.media.sse.OutboundEvent;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.opendaylight.restconf.server.api.EventStreamGetParams;
+import org.opendaylight.restconf.server.spi.RestconfStream;
+import org.opendaylight.restconf.server.spi.RestconfStream.EncodingName;
+import org.opendaylight.yangtools.concepts.Registration;
+
+@ExtendWith(MockitoExtension.class)
+class SSESessionHandlerTest {
+    @Mock
+    private PingExecutor pingExecutor;
+    @Mock
+    private RestconfStream<?> stream;
+    @Mock
+    private Registration pingRegistration;
+    @Mock
+    private SseEventSink eventSink;
+    @Mock
+    private Sse sse;
+    @Mock
+    private Registration reg;
+
+    private SSESender setup(final int maxFragmentSize, final long heartbeatInterval) throws Exception {
+        final var sseSessionHandler = new SSESender(pingExecutor, eventSink, sse, stream,
+            EncodingName.RFC8040_XML, new EventStreamGetParams(null, null, null, null, null, null, null),
+            maxFragmentSize, heartbeatInterval);
+        doReturn(reg).when(stream).addSubscriber(eq(sseSessionHandler), any(), any());
+        return sseSessionHandler;
+    }
+
+    private void setupEvent() {
+        doAnswer(inv -> new OutboundEvent.Builder().data(String.class, inv.getArgument(0, String.class)).build())
+          .when(sse).newEvent(any());
+    }
+
+    private void setupPing(final long maxFragmentSize, final long heartbeatInterval) {
+        doReturn(pingRegistration).when(pingExecutor)
+            .startPingProcess(any(Runnable.class), eq(heartbeatInterval), eq(TimeUnit.MILLISECONDS));
+    }
+
+    @Test
+    void onSSEConnectedWithEnabledPing() throws Exception {
+        final var heartbeatInterval = 1000L;
+        final var sseSessionHandler = setup(1000, heartbeatInterval);
+
+        sseSessionHandler.init();
+        verify(pingExecutor).startPingProcess(any(Runnable.class), eq(heartbeatInterval), eq(TimeUnit.MILLISECONDS));
+    }
+
+    @Test
+    void onSSEConnectedWithDisabledPing() throws Exception {
+        final int heartbeatInterval = 0;
+        final var sseSessionHandler = setup(1000, heartbeatInterval);
+
+        sseSessionHandler.init();
+        verifyNoMoreInteractions(pingExecutor);
+    }
+
+    @Test
+    void onSSEClosedWithOpenSession() throws Exception {
+        final var sseSessionHandler = setup(200, 10000);
+
+        sseSessionHandler.init();
+
+        sseSessionHandler.close();
+        verify(reg).close();
+    }
+
+    @Test
+    void onSSECloseWithEnabledPingAndLivingSession() throws Exception {
+        final var sseSessionHandler = setup(150, 8000);
+        setupPing(150, 8000);
+        sseSessionHandler.init();
+
+        doNothing().when(pingRegistration).close();
+        sseSessionHandler.close();
+        verify(reg).close();
+    }
+
+    @Test
+    void onSSECloseWithEnabledPingAndDeadSession() throws Exception {
+        final var sseSessionHandler = setup(150, 8000);
+        setupPing(150, 8000);
+        sseSessionHandler.init();
+
+        doNothing().when(pingRegistration).close();
+        sseSessionHandler.close();
+        verify(reg).close();
+    }
+
+    @Test
+    void onSSECloseWithDisabledPingAndDeadSession() throws Exception {
+        final var sseSessionHandler = setup(150, 8000);
+        sseSessionHandler.init();
+
+        sseSessionHandler.close();
+        verify(reg).close();
+        verifyNoMoreInteractions(pingRegistration);
+    }
+
+    @Test
+    void sendDataMessageWithDisabledFragmentation() throws Exception {
+        final var sseSessionHandler = setup(0, 0);
+        doReturn(false).when(eventSink).isClosed();
+        setupEvent();
+        sseSessionHandler.init();
+        final String testMessage = generateRandomStringOfLength(100);
+        sseSessionHandler.sendDataMessage(testMessage);
+
+        final var cap = ArgumentCaptor.forClass(OutboundEvent.class);
+        verify(eventSink, times(1)).send(cap.capture());
+        final var event = cap.getAllValues().get(0);
+        assertNotNull(event);
+        assertEquals(event.getData(), testMessage);
+    }
+
+    @Test
+    void sendDataMessageWithDisabledFragAndDeadSession() throws Exception {
+        final var sseSessionHandler = setup(0, 0);
+        doReturn(true).when(eventSink).isClosed();
+        sseSessionHandler.init();
+
+        final String testMessage = generateRandomStringOfLength(11);
+        sseSessionHandler.sendDataMessage(testMessage);
+        verify(eventSink, times(0)).send(any());
+    }
+
+    @Test
+    void sendDataMessageWithEnabledFragAndSmallMessage() throws Exception {
+        final var sseSessionHandler = setup(100, 0);
+        doReturn(false).when(eventSink).isClosed();
+        setupEvent();
+        sseSessionHandler.init();
+
+        // in both cases, fragmentation should not be applied
+        final String testMessage1 = generateRandomStringOfLength(100);
+        final String testMessage2 = generateRandomStringOfLength(50);
+        sseSessionHandler.sendDataMessage(testMessage1);
+        sseSessionHandler.sendDataMessage(testMessage2);
+
+        final var cap = ArgumentCaptor.forClass(OutboundEvent.class);
+        // without fragmentation there will be 2 write calls
+        verify(eventSink, times(2)).send(cap.capture());
+        OutboundEvent event1 = cap.getAllValues().get(0);
+        OutboundEvent event2 = cap.getAllValues().get(1);
+        assertNotNull(event1);
+        assertNotNull(event2);
+        assertEquals(event1.getData(), testMessage1);
+        assertEquals(event2.getData(), testMessage2);
+        String[] lines1 = ((String) event1.getData()).split("\r\n|\r|\n");
+        assertEquals(lines1.length, 1);
+        String[] lines2 = ((String) event2.getData()).split("\r\n|\r|\n");
+        assertEquals(lines2.length, 1);
+    }
+
+    @Test
+    void sendDataMessageWithZeroLength() throws Exception {
+        final var sseSessionHandler = setup(100, 0);
+        sseSessionHandler.init();
+
+        sseSessionHandler.sendDataMessage("");
+        verifyNoMoreInteractions(eventSink);
+    }
+
+    @Test
+    void sendDataMessageWithEnabledFragAndLargeMessage1() throws Exception {
+        final var sseSessionHandler = setup(100, 0);
+        doReturn(false).when(eventSink).isClosed();
+        setupEvent();
+        sseSessionHandler.init();
+
+        // there should be 10 fragments of length 100 characters
+        final String testMessage = generateRandomStringOfLength(1000);
+        sseSessionHandler.sendDataMessage(testMessage);
+        final var cap = ArgumentCaptor.forClass(OutboundEvent.class);
+        // SSE automatically send fragmented packet ended with new line character due to eventOutput
+        // have only 1 write call
+        verify(eventSink, times(1)).send(cap.capture());
+        OutboundEvent event = cap.getAllValues().get(0);
+        assertNotNull(event);
+        String[] lines = ((String) event.getData()).split("\r\n|\r|\n");
+        assertEquals(lines.length, 10);
+    }
+
+    private static String generateRandomStringOfLength(final int length) {
+        final var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvxyz";
+        final var sb = new StringBuilder(length);
+        for (int i = 0; i < length; i++) {
+            int index = (int) (alphabet.length() * Math.random());
+            sb.append(alphabet.charAt(index));
+        }
+        return sb.toString();
+    }
+}
