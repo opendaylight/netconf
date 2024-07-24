c66c0085ded93f521d3138bb0228841217a35d57
Robert Varga <robert.varga@pantheon.tech>
2024-07-24 04:54:06 +0200

    WIP: Remove ServerExceptionMapper
    Acknowledge the existence of YangErrorsBody in that it is a fair game to
    complete a ServerRequest with such a body.
    
    In contexts where we have a ErrorTagMapping, we can produce a nice
    FormattableBody with a HttpStatusCode, which ties right into what
    ServerExceptionMapper does with AsyncResponse.
    
    // FIXME: MappingServerRequestTest
    
    JIRA: NETCONF-1188
    Change-Id: I3e8509358a16e32fb6ef8f5ba16b5d6f1bd4a8f6
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- apps/restconf-nb/src/test/java/org/opendaylight/restconf/server/jaxrs/MappingServerRequestTest.java
+++ apps/restconf-nb/src/test/java/org/opendaylight/restconf/server/jaxrs/MappingServerRequestTest.java
@@ -10,13 +10,12 @@ package org.opendaylight.restconf.server.jaxrs;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assumptions.assumeTrue;
 import static org.junit.jupiter.params.provider.Arguments.arguments;
-import static org.mockito.Mockito.doReturn;
-import static org.mockito.Mockito.mock;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.verify;
 
 import java.util.List;
 import java.util.stream.Stream;
-import javax.ws.rs.core.HttpHeaders;
-import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 import org.json.JSONException;
@@ -25,13 +24,20 @@ import org.json.XML;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.mockito.ArgumentCaptor;
 import org.opendaylight.restconf.api.ErrorMessage;
+import org.opendaylight.restconf.api.FormattableBody;
+import org.opendaylight.restconf.api.HttpStatusCode;
+import org.opendaylight.restconf.api.QueryParameters;
+import org.opendaylight.restconf.api.query.PrettyPrintParam;
 import org.opendaylight.restconf.server.api.DatabindContext;
 import org.opendaylight.restconf.server.api.ServerError;
 import org.opendaylight.restconf.server.api.ServerErrorInfo;
 import org.opendaylight.restconf.server.api.ServerErrorPath;
 import org.opendaylight.restconf.server.api.ServerException;
+import org.opendaylight.restconf.server.spi.AbstractJukeboxTest;
 import org.opendaylight.restconf.server.spi.ErrorTagMapping;
+import org.opendaylight.restconf.server.spi.MappingServerRequest;
 import org.opendaylight.yangtools.yang.common.ErrorTag;
 import org.opendaylight.yangtools.yang.common.ErrorType;
 import org.opendaylight.yangtools.yang.common.QName;
@@ -40,15 +46,25 @@ import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
 import org.opendaylight.yangtools.yang.test.util.YangParserTestUtils;
 import org.skyscreamer.jsonassert.JSONAssert;
 
-class ServerExceptionMapperTest {
+class MappingServerRequestTest extends AbstractJukeboxTest {
+    private abstract static class Req extends MappingServerRequest<Object> {
+        Req() {
+            super(QueryParameters.of(), PrettyPrintParam.TRUE, ErrorTagMapping.RFC8040);
+        }
+
+        @Override
+        public abstract void onSuccess(Object result);
+
+        @Override
+        public abstract void onFailure(HttpStatusCode status, FormattableBody body);
+    }
+
     private static final QNameModule MONITORING_MODULE_INFO =
         QNameModule.ofRevision("instance:identifier:patch:module", "2015-11-21");
     private static final DatabindContext DATABIND = DatabindContext.ofModel(
-        YangParserTestUtils.parseYangResources(ServerExceptionMapperTest.class,
+        YangParserTestUtils.parseYangResources(MappingServerRequestTest.class,
             "/restconf/impl/ietf-restconf@2017-01-26.yang",
             "/instance-identifier/instance-identifier-patch-module.yang"));
-
-    private final ServerExceptionMapper exceptionMapper = new ServerExceptionMapper(ErrorTagMapping.RFC8040);
 
     /**
      * Testing entries 0 - 6: testing of media types and empty responses.
@@ -59,7 +75,7 @@ class ServerExceptionMapperTest {
      */
     static Stream<Arguments> data() {
         final var sampleComplexError = new ServerException(List.of(
-            new ServerError(ErrorType.APPLICATION, ErrorTag.BAD_ATTRIBUTE, new ErrorMessage("message 1"), "app tag #1",
+           new ServerError(ErrorType.APPLICATION, ErrorTag.BAD_ATTRIBUTE, new ErrorMessage("message 1"), "app tag #1",
                 null, null),
             new ServerError(ErrorType.APPLICATION, ErrorTag.OPERATION_FAILED, new ErrorMessage("message 2"),
                 "app tag #2", null, new ServerErrorInfo("my info")),
@@ -78,7 +94,6 @@ class ServerExceptionMapperTest {
                 "Mapping of the exception with one error entry but null status code. This status code should"
                     + " be derived from single error entry; JSON output",
                 new ServerException("Sample error message"),
-                mockHttpHeaders(MediaType.APPLICATION_JSON_TYPE, List.of(JaxRsMediaTypes.APPLICATION_YANG_PATCH_JSON)),
                 Response.status(Status.INTERNAL_SERVER_ERROR)
                     .type(JaxRsMediaTypes.APPLICATION_YANG_DATA_JSON)
                     .entity("""
@@ -102,7 +117,6 @@ class ServerExceptionMapperTest {
                     new ServerError(ErrorType.APPLICATION, ErrorTag.BAD_ATTRIBUTE, "message 1"),
                     new ServerError(ErrorType.APPLICATION, ErrorTag.OPERATION_FAILED, "message 2")),
                     new IllegalStateException("cause"), "general message"),
-                mockHttpHeaders(MediaType.APPLICATION_JSON_TYPE, List.of(JaxRsMediaTypes.APPLICATION_YANG_PATCH_XML)),
                 Response.status(Status.BAD_REQUEST)
                     .type(JaxRsMediaTypes.APPLICATION_YANG_DATA_XML)
                     .entity("""
@@ -121,9 +135,8 @@ class ServerExceptionMapperTest {
                     .build()
             ),
             arguments("Mapping of the exception with three entries and optional entries set: error app tag"
-                    + " (the first error), error info (the second error), and error path (the last error); JSON output",
-                sampleComplexError, mockHttpHeaders(MediaType.APPLICATION_JSON_TYPE, List.of(
-                    MediaType.APPLICATION_JSON_TYPE)),
+                  + " (the first error), error info (the second error), and error path (the last error); JSON output",
+                sampleComplexError,
                 Response.status(Status.BAD_REQUEST)
                     .type(JaxRsMediaTypes.APPLICATION_YANG_DATA_JSON)
                     .entity("""
@@ -157,9 +170,8 @@ class ServerExceptionMapperTest {
                             }""")
                     .build()),
             arguments("Mapping of the exception with three entries and optional entries set: error app tag"
-                    + " (the first error), error info (the second error), and error path (the last error); XML output",
-                sampleComplexError, mockHttpHeaders(JaxRsMediaTypes.APPLICATION_YANG_PATCH_JSON,
-                    List.of(JaxRsMediaTypes.APPLICATION_YANG_DATA_XML)),
+                   + " (the first error), error info (the second error), and error path (the last error); XML output",
+                sampleComplexError,
                 Response.status(Status.BAD_REQUEST)
                     .type(JaxRsMediaTypes.APPLICATION_YANG_DATA_XML)
                     .entity("""
@@ -194,8 +206,7 @@ class ServerExceptionMapperTest {
     @ParameterizedTest
     @MethodSource("data")
     void testMappingOfExceptionToResponse(final String testDescription, final ServerException thrownException,
-            final HttpHeaders httpHeaders, final Response expectedResponse) throws JSONException {
-        exceptionMapper.setHttpHeaders(httpHeaders);
+            final Response expectedResponse) throws JSONException {
         final Response response = exceptionMapper.toResponse(thrownException);
         compareResponseWithExpectation(expectedResponse, response);
     }
@@ -203,19 +214,11 @@ class ServerExceptionMapperTest {
     @ParameterizedTest
     @MethodSource("data")
     void testFormattingJson(final String testDescription, final ServerException thrownException,
-        final HttpHeaders httpHeaders, final Response expectedResponse) throws JSONException {
+            final Response expectedResponse) throws JSONException {
         assumeTrue(expectedResponse.getMediaType().equals(JaxRsMediaTypes.APPLICATION_YANG_DATA_JSON));
-
-        exceptionMapper.setHttpHeaders(httpHeaders);
-        final Response response = exceptionMapper.toResponse(thrownException);
-        assertEquals(expectedResponse.getEntity().toString(), response.getEntity().toString());
-    }
-
-    private static HttpHeaders mockHttpHeaders(final MediaType contentType, final List<MediaType> acceptedTypes) {
-        final HttpHeaders httpHeaders = mock(HttpHeaders.class);
-        doReturn(contentType).when(httpHeaders).getMediaType();
-        doReturn(acceptedTypes).when(httpHeaders).getAcceptableMediaTypes();
-        return httpHeaders;
+        assertFormat(expectedResponse.getEntity().toString(),
+            assertMapped(thrownException, new HttpStatusCode(expectedResponse.getStatus(), null))::formatToJSON,
+            true);
     }
 
     private static void compareResponseWithExpectation(final Response expectedResponse, final Response actualResponse)
@@ -232,5 +235,14 @@ class ServerExceptionMapperTest {
             final JSONObject actualResponseInJson = XML.toJSONObject(actualResponse.getEntity().toString());
             JSONAssert.assertEquals(expectedResponseInJson, actualResponseInJson, true);
         }
+    }
+
+    private static FormattableBody assertMapped(final ServerException ex, final HttpStatusCode expectedCode) {
+        final var req = spy(Req.class);
+//        doNothing().when(req).onFailure(any(), any());
+        req.completeWith(ex);
+        final var captor = ArgumentCaptor.forClass(FormattableBody.class);
+        verify(req).onFailure(eq(expectedCode), captor.capture());
+        return captor.getValue();
     }
 }
