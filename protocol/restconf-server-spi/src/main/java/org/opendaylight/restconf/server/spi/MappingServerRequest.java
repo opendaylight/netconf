674bb93155c3223da84a6b15e974554335b8de55
Robert Varga <robert.varga@pantheon.tech>
2024-07-24 04:54:06 +0200

    WIP: Remove ServerExceptionMapper
    Acknowledge the existence of YangErrorsBody in that it is a fair game to
    complete a ServerRequest with such a body.
    
    In contexts where we have a ErrorTagMapping, we can produce a nice
    FormattableBody with a HttpStatusCode, which ties right into what
    ServerExceptionMapper does with AsyncResponse.
    
    // FIXME: MappingServerRequestTest
    
    JIRA: NETCONF-1188
    Change-Id: I3e8509358a16e32fb6ef8f5ba16b5d6f1bd4a8f6
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ protocol/restconf-server-spi/src/main/java/org/opendaylight/restconf/server/spi/MappingServerRequest.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) 2024 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.restconf.server.spi;
+
+import static java.util.Objects.requireNonNull;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.opendaylight.restconf.api.FormattableBody;
+import org.opendaylight.restconf.api.HttpStatusCode;
+import org.opendaylight.restconf.api.QueryParameters;
+import org.opendaylight.restconf.api.query.PrettyPrintParam;
+import org.opendaylight.restconf.server.api.AbstractServerRequest;
+import org.opendaylight.restconf.server.api.YangErrorsBody;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * An {@link AbstractServerRequest} which maps reported errors using {@link ErrorTagMapping}.
+ */
+@NonNullByDefault
+public abstract class MappingServerRequest<T> extends AbstractServerRequest<T> {
+    private static final Logger LOG = LoggerFactory.getLogger(MappingServerRequest.class);
+
+    private final ErrorTagMapping errorTagMapping;
+
+    protected MappingServerRequest(final QueryParameters queryParameters, final PrettyPrintParam defaultPrettyPrint,
+            final ErrorTagMapping errorTagMapping) {
+        super(queryParameters, defaultPrettyPrint);
+        this.errorTagMapping = requireNonNull(errorTagMapping);
+    }
+
+    @Override
+    protected final void onFailure(final YangErrorsBody body) {
+        final var errors = body.errors();
+        final var statusCodes = errors.stream()
+            .map(restconfError -> errorTagMapping.statusOf(restconfError.tag()))
+            .distinct()
+            .toList();
+        if (statusCodes.size() > 1) {
+            LOG.warn("""
+                An unexpected error occurred during translation to response: Different status codes have been found in
+                appended error entries: {}. The first error entry status code is chosen for response.""",
+                statusCodes, new Throwable());
+        }
+        onFailure(statusCodes.getFirst(), body);
+    }
+
+    protected abstract void onFailure(HttpStatusCode status, FormattableBody body);
+}
