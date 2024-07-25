3f59bd13af90c916d4b9e7a0fac1c2611bcabaa9
Robert Varga <robert.varga@pantheon.tech>
2024-07-25 14:01:45 +0200

    Make restconf-server-mdsal a module
    Make sure this artifact is a good citizen by declaring its module-info.
    
    JIRA: NETCONF-773
    Change-Id: I77f1c73e76e068fb02f2366dea6c132a191a86a1
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ plugins/restconf-server-mdsal/src/main/java/module-info.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2024 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+module org.opendaylight.restconf.server.mdsal {
+    exports org.opendaylight.restconf.server.mdsal;
+
+    requires transitive org.opendaylight.restconf.mdsal.spi;
+    requires transitive org.opendaylight.restconf.server.api;
+    requires transitive org.opendaylight.restconf.server.spi;
+    requires org.opendaylight.netconf.dom.api;
+    requires org.opendaylight.yangtools.yang.data.spi;
+    requires org.slf4j;
+
+    // Annotation-only dependencies
+    requires static transitive java.annotation;
+    requires static transitive javax.inject;
+    requires static transitive org.eclipse.jdt.annotation;
+    requires static com.github.spotbugs.annotations;
+    requires static org.osgi.annotation.bundle;
+    requires static org.osgi.service.component.annotations;
+}
