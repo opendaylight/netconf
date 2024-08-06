a0327bd5f34e7530e859890c06dd6d91fdd8005e
Ivan Hrasko <ivan.hrasko@pantheon.tech>
2024-08-06 13:45:04 +0200

    WIP: Add end-to-end tests for Netty restconf
    Add end-to-end tests for Netty based restconf implementation.
    
    JIRA: NETCONF-873
    Change-Id: Ibb9dc960cc03b1a7215cb8ff39e2fd2bd136db0e
    Signed-off-by: Ivan Hrasko <ivan.hrasko@pantheon.tech>
    Signed-off-by: Ruslan Kashapov <ruslan.kashapov@pantheon.tech>

---
+++ apps/restconf-nb/src/test/java/org/opendaylight/restconf/server/netty/DataE2ETest.java
@@ -0,0 +1,220 @@
+/*
+ * Copyright (c) 2024 PANTHEON.tech s.r.o. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.restconf.server.netty;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.opendaylight.restconf.api.MediaTypes.APPLICATION_YANG_DATA_JSON;
+
+import io.netty.handler.codec.http.HttpMethod;
+import org.junit.jupiter.api.Test;
+
+class DataE2ETest extends AbstractE2ETest {
+
+    @Test
+    void userAuthenticationTest() {
+        // TODO
+    }
+
+    @Test
+    void crudOperationsTest() throws Exception {
+        var result = invokeRequest(buildRequest(HttpMethod.POST,
+            "/rests/data/network-topology:network-topology/topology=topology-netconf",
+            APPLICATION_YANG_DATA_JSON,
+            """
+                {
+                    "node": [
+                        {
+                            "node-id": "netopeer2",
+                            "netconf-node-topology:login-password-unencrypted": {
+                                "netconf-node-topology:password": "admin",
+                                "netconf-node-topology:username": "admin"
+                            },
+                            "netconf-node-topology:host": "172.17.0.2",
+                            "netconf-node-topology:port": 830,
+                            "netconf-node-topology:tcp-only": false,
+                            "protocol": {
+                                "name": "SSH"
+                            }
+                        }
+                    ]
+                }
+                """));
+        assertEquals(201, result.status().code());
+
+        result = invokeRequest(buildRequest(HttpMethod.GET,
+            "/rests/data/network-topology:network-topology/topology=topology-netconf/node=netopeer2",
+            APPLICATION_YANG_DATA_JSON,
+            null));
+        assertEquals(200, result.status().code());
+
+        result = invokeRequest(buildRequest(HttpMethod.PUT,
+            "/rests/data/network-topology:network-topology/topology=topology-netconf/node=netopeer2",
+            APPLICATION_YANG_DATA_JSON,
+            """
+                {
+                    "node": [
+                        {
+                            "node-id": "netopeer2",
+                            "netconf-node-topology:login-password-unencrypted": {
+                                "netconf-node-topology:password": "admin",
+                                "netconf-node-topology:username": "admin"
+                            },
+                            "netconf-node-topology:host": "172.17.0.2",
+                            "netconf-node-topology:port": 6513,
+                            "netconf-node-topology:tcp-only": false,
+                            "protocol": {
+                                "name": "TLS"
+                            }
+                        }
+                    ]
+                }
+                """));
+        assertEquals(201, result.status().code());
+
+        result = invokeRequest(buildRequest(HttpMethod.DELETE,
+            "/rests/data/network-topology:network-topology/topology=topology-netconf/node=netopeer2",
+            APPLICATION_YANG_DATA_JSON,
+            null));
+        assertEquals(204, result.status().code());
+    }
+
+    @Test
+    void createErrorHandlingTest() throws Exception {
+        var result = invokeRequest(buildRequest(HttpMethod.POST,
+            "/rests/data/network-topology:network-topology/topology=topology-netconf",
+            APPLICATION_YANG_DATA_JSON,
+            """
+                {
+                    "node": [
+                        {
+                            "node-id": "netopeer2",
+                            "netconf-node-topology:login-password-unencrypted": {
+                                "netconf-node-topology:password": "admin",
+                                "netconf-node-topology:username": "admin"
+                            },
+                            "netconf-node-topology:host": "172.17.0.2",
+                            "netconf-node-topology:port": 830,
+                            "netconf-node-topology:tcp-only": false,
+                            "protocol": {
+                                "name": "SSH"
+                            }
+                        }
+                    ]
+                }
+                """));
+        assertEquals(201, result.status().code());
+
+        result = invokeRequest(buildRequest(HttpMethod.POST,
+            "/rests/data/network-topology:network-topology/topology=topology-netconf",
+            APPLICATION_YANG_DATA_JSON,
+            """
+                {
+                    "node": [
+                        {
+                            "node-id": "netopeer2",
+                            "netconf-node-topology:login-password-unencrypted": {
+                                "netconf-node-topology:password": "admin",
+                                "netconf-node-topology:username": "admin"
+                            },
+                            "netconf-node-topology:host": "172.17.0.2",
+                            "netconf-node-topology:port": 830,
+                            "netconf-node-topology:tcp-only": false,
+                            "protocol": {
+                                "name": "SSH"
+                            }
+                        }
+                    ]
+                }
+                """));
+        assertEquals(409, result.status().code());
+    }
+
+    @Test
+    void readErrorHandlingTest() throws Exception {
+        final var result = invokeRequest(buildRequest(HttpMethod.GET,
+            "/rests/data/network-topology:network-topology/topology=topology-netconf/node=netopeer2",
+            APPLICATION_YANG_DATA_JSON,
+            null));
+        assertEquals(409, result.status().code());
+    }
+
+    @Test
+    void updateErrorHandlingTest() throws Exception {
+        var result = invokeRequest(buildRequest(HttpMethod.PUT,
+            "/rests/data/network-topology:network-topology/topology=topology-netconf/node=netopeer2",
+            APPLICATION_YANG_DATA_JSON,
+            """
+                {
+                    "node": [
+                        {
+                            "node-id": "netopeer2",
+                            "netconf-node-topology:login-password-unencrypted": {
+                                "netconf-node-topology:password": "admin",
+                                "netconf-node-topology:username": "admin"
+                            },
+                            "netconf-node-topology:host": "172.17.0.2",
+                            "netconf-node-topology:port": 6513,
+                            "netconf-node-topology:tcp-only": false,
+                            "protocol": {
+                                "name": "TLS"
+                            }
+                        }
+                    ]
+                }
+                """));
+        assertEquals(201, result.status().code());
+
+        result = invokeRequest(buildRequest(HttpMethod.PUT,
+            "/rests/data/network-topology:network-topology/topology=topology-netconf/node=netopeer2",
+            APPLICATION_YANG_DATA_JSON,
+            """
+                {
+                    "node": [
+                        {
+                            "node-id": "netopeer2",
+                            "netconf-node-topology:login-password-unencrypted": {
+                                "netconf-node-topology:password": "admin",
+                                "netconf-node-topology:username": "admin"
+                            },
+                            "netconf-node-topology:host": "172.17.0.2",
+                            "netconf-node-topology:port": 6513,
+                            "netconf-node-topology:tcp-only": false,
+                            "protocol": {
+                                "name": "TLS"
+                            }
+                        }
+                    ]
+                }
+                """));
+        assertEquals(200, result.status().code());
+    }
+
+    @Test
+    void deleteErrorHandlingTest() throws Exception {
+        final var result = invokeRequest(buildRequest(HttpMethod.DELETE,
+            "/rests/data/network-topology:network-topology/topology=topology-netconf/node=netopeer2",
+            APPLICATION_YANG_DATA_JSON,
+            null));
+        assertEquals(409, result.status().code());
+    }
+
+    @Test
+    void invokeActionTest() throws Exception {
+
+    }
+
+    @Test
+    void dataOptionsTest() throws Exception {
+
+    }
+
+    @Test
+    void dataHeadTest() throws Exception {
+
+    }
+}
