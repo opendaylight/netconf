63df6882d1ab721f8bd8e017a78f93378506ab75
Matej Sramcik <matej.sramcik@pantheon.tech>
2024-08-07 12:16:07 +0200

    Improve OpenAPI parameters validation
    Check nullability of width, depth, offset and limit at beginning
    and pass them further like int instead of Integer.
    
    JIRA: NETCONF-1346
    Change-Id: I22388eff543d25b404736d45c2ca554a61e0b497
    Signed-off-by: Matej Sramcik <matej.sramcik@pantheon.tech>

--- restconf/restconf-openapi/src/main/java/org/opendaylight/restconf/openapi/impl/SchemasStream.java
+++ restconf/restconf-openapi/src/main/java/org/opendaylight/restconf/openapi/impl/SchemasStream.java
@@ -7,7 +7,6 @@
  */
 package org.opendaylight.restconf.openapi.impl;
 
-import static java.util.Objects.requireNonNullElse;
 import static org.opendaylight.restconf.openapi.util.RestDocgenUtil.widthList;
 
 import com.fasterxml.jackson.core.JsonGenerator;
@@ -54,8 +53,8 @@ public final class SchemasStream extends InputStream {
     private final boolean isForSingleModule;
     private final ByteArrayOutputStream stream;
     private final JsonGenerator generator;
-    private final Integer width;
-    private final Integer depth;
+    private final int width;
+    private final int depth;
 
     private Reader reader;
     private ReadableByteChannel channel;
@@ -63,16 +62,16 @@ public final class SchemasStream extends InputStream {
 
     public SchemasStream(final EffectiveModelContext modelContext, final OpenApiBodyWriter writer,
             final Iterator<? extends Module> iterator, final boolean isForSingleModule,
-            final ByteArrayOutputStream stream, final JsonGenerator generator, final Integer width,
-            final Integer depth) {
+            final ByteArrayOutputStream stream, final JsonGenerator generator, final int width,
+            final int depth) {
         this.iterator = iterator;
         this.modelContext = modelContext;
         this.writer = writer;
         this.isForSingleModule = isForSingleModule;
         this.stream = stream;
         this.generator = generator;
-        this.width = requireNonNullElse(width, 0);
-        this.depth = requireNonNullElse(depth, 0);
+        this.width = width;
+        this.depth = depth;
     }
 
     @Override
