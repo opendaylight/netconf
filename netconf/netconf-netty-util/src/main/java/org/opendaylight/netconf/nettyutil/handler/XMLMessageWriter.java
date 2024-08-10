75beb721dfed68a67e806952a52cb5769fcbd3ce
Robert Varga <robert.varga@pantheon.tech>
2024-08-10 15:40:08 +0200

    WIP: Split MessageEncoder up
    MessageEncoder has currently two functions:
    - it receives a NetconfMessage from the pipeline
    - it encodes it into a ByteBuf
    - it passes the ByteBuf down the pipeline
    
    Refactor MessageEncoder.encodeTo() to a separate class, MessageWriter,
    which handles the details of how to write a NetconfMessage into an
    OutputStream.
    
    This makes MessageEncoder a final class, which has the notion of which
    MessageWriter it is currently using -- and that can be switched from the
    pipeline context.
    
    This completely eliminates the need for modifying the pipeline when
    switch XML/EXI and provides for nice integration surface for further
    consolidation.
    
    This entire exercise means we have only four objects dealing with
    default XML encoding -- be it pretty-printing or otherwise.
    
    Change-Id: I8bb4e75465da26e655226c5c83853adc9c47b670
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ netconf/netconf-netty-util/src/main/java/org/opendaylight/netconf/nettyutil/handler/XMLMessageWriter.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) 2014 Cisco Systems, Inc. and others.  All rights reserved.
+ * Copyright (c) 2024 PANTHEON.tech, s.r.o.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.netconf.nettyutil.handler;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.nio.charset.StandardCharsets;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.opendaylight.netconf.api.messages.NetconfMessage;
+import org.opendaylight.netconf.codec.MessageWriter;
+
+@NonNullByDefault
+public final class XMLMessageWriter extends MessageWriter {
+    private static final XMLMessageWriter DEFAULT = new XMLMessageWriter(false);
+    private static final XMLMessageWriter PRETTY = new XMLMessageWriter(true);
+
+    private XMLMessageWriter(final boolean pretty) {
+        super(pretty);
+    }
+
+    public static XMLMessageWriter of() {
+        return DEFAULT;
+    }
+
+    public static XMLMessageWriter of(final boolean pretty) {
+        return pretty ? PRETTY : DEFAULT;
+    }
+
+    public static XMLMessageWriter pretty() {
+        return PRETTY;
+    }
+
+    @Override
+    protected void writeMessage(final NetconfMessage message, final Transformer transformer, final OutputStream out)
+            throws IOException, TransformerException {
+        // Wrap OutputStreamWriter with BufferedWriter as suggested in javadoc for OutputStreamWriter
+
+        // Using custom BufferedWriter that does not provide newLine method as performance improvement
+        // see javadoc for BufferedWriter
+        final var result = new StreamResult(new BufferedWriter(new OutputStreamWriter(out, StandardCharsets.UTF_8)));
+        final var source = new DOMSource(message.getDocument());
+        transformer.transform(source, result);
+    }
+}
--- protocol/netconf-codec/src/test/java/org/opendaylight/netconf/codec/ThreadLocalTransformersTest.java
+++ protocol/netconf-codec/src/test/java/org/opendaylight/netconf/codec/ThreadLocalTransformersTest.java
@@ -5,44 +5,45 @@
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
-
-package org.opendaylight.netconf.nettyutil.handler;
+package org.opendaylight.netconf.codec;
 
 import static org.junit.jupiter.api.Assertions.assertNotSame;
 import static org.junit.jupiter.api.Assertions.assertSame;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
-import javax.xml.transform.Transformer;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class ThreadLocalTransformersTest {
-
     private ExecutorService executorService;
 
     @BeforeEach
-    void setUp() {
+    void beforeEach() {
         executorService = Executors.newSingleThreadExecutor();
+    }
+
+    @AfterEach
+    void afterEach() {
+        executorService.shutdown();
     }
 
     @Test
     void testGetDefaultTransformer() throws Exception {
-        final Transformer t1 = ThreadLocalTransformers.getDefaultTransformer();
-        final Transformer t2 = ThreadLocalTransformers.getDefaultTransformer();
+        final var t1 = ThreadLocalTransformers.getDefaultTransformer();
+        final var t2 = ThreadLocalTransformers.getDefaultTransformer();
         assertSame(t1, t2);
-        final Future<Transformer> future = executorService.submit(ThreadLocalTransformers::getDefaultTransformer);
+        final var future = executorService.submit(ThreadLocalTransformers::getDefaultTransformer);
         assertNotSame(t1, future.get());
     }
 
     @Test
     void testGetPrettyTransformer() throws Exception {
-        final Transformer t1 = ThreadLocalTransformers.getPrettyTransformer();
-        final Transformer t2 = ThreadLocalTransformers.getPrettyTransformer();
+        final var t1 = ThreadLocalTransformers.getPrettyTransformer();
+        final var t2 = ThreadLocalTransformers.getPrettyTransformer();
         assertSame(t1, t2);
-        final Future<Transformer> future = executorService.submit(ThreadLocalTransformers::getPrettyTransformer);
+        final var future = executorService.submit(ThreadLocalTransformers::getPrettyTransformer);
         assertNotSame(t1, future.get());
     }
 
