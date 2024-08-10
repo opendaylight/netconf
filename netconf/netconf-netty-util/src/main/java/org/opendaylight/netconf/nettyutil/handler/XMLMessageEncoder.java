ec563186850c1b4a482bf51c94d2a8038dd895dd
Robert Varga <robert.varga@pantheon.tech>
2024-08-10 11:21:27 +0200

    Eliminate cliendId from XMLMessageEncoder
    This is old baggage coming from the initial implementation. Let's not
    modify the message being sent, as the clientId field is never
    initialized to anything but null.
    
    Change-Id: If7006647abd33a4bc8257082261bf8e1ac6cafb5
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- netconf/netconf-netty-util/src/main/java/org/opendaylight/netconf/nettyutil/handler/XMLMessageEncoder.java
+++ netconf/netconf-netty-util/src/main/java/org/opendaylight/netconf/nettyutil/handler/XMLMessageEncoder.java
@@ -11,37 +11,15 @@ import com.google.common.annotations.VisibleForTesting;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.nio.charset.StandardCharsets;
-import java.util.Optional;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
-import org.eclipse.jdt.annotation.Nullable;
 import org.opendaylight.netconf.api.messages.NetconfMessage;
 import org.opendaylight.netconf.codec.MessageEncoder;
 
 public class XMLMessageEncoder extends MessageEncoder {
-    private final @Nullable String clientId;
-
-    public XMLMessageEncoder() {
-        this((String) null);
-    }
-
-    public XMLMessageEncoder(final @Nullable String clientId) {
-        this.clientId = clientId;
-    }
-
-    @Deprecated(since = "8.0.0", forRemoval = true)
-    public XMLMessageEncoder(final Optional<String> clientId) {
-        this(clientId.orElse(null));
-    }
-
     @Override
     @VisibleForTesting
     public void encodeTo(final NetconfMessage msg, final OutputStream out) throws Exception {
-        if (clientId != null) {
-            final var doc = msg.getDocument();
-            doc.appendChild(doc.createComment("clientId:" + clientId));
-        }
-
         // Wrap OutputStreamWriter with BufferedWriter as suggested in javadoc for OutputStreamWriter
 
         // Using custom BufferedWriter that does not provide newLine method as performance improvement
