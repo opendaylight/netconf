e241ecc0f21e9b8ffc71c2c4ed05032bff23faf3
Robert Varga <robert.varga@pantheon.tech>
2024-08-11 04:17:38 +0200

    Deprecate NetconfHelloMessageAdditionalHeader
    This class should not exist, but getting rid of it requires us to do
    quite some work, which in turn has impacts in netconf-{codec,netty-util}
    and perhaps somewhere else as well.
    
    Deprecate this class for removal to drive its replacements. Also provide
    a lot of FIXMEs explaining the various changes to
    netconf-{api,codec,client,server} that need to happen to solidify the
    pipeline layout, the fool-proof the end user APIs and provide reliable
    operation.
    
    Change-Id: I8ec055dc6f032df3171cd0dddc78f33498956fde
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- protocol/netconf-codec/src/main/java/org/opendaylight/netconf/codec/FrameDecoder.java
+++ protocol/netconf-codec/src/main/java/org/opendaylight/netconf/codec/FrameDecoder.java
@@ -13,6 +13,19 @@ import org.eclipse.jdt.annotation.NonNull;
 /**
  * An channel handler assembling inbound messages from specified framing.
  */
+// TODO: Reconsider use of ByteToMessageDecoder here. While the Cumulator use is nice, I think we can do better if we
+//       do our buffer management ourselves:
+//       - we already use a CompositeByteBuf in ChunkedFrameDecoder with a buch of state tracking
+//       - we have searchIndex in order to optimize resumed searches, so taking ownership of the buffer should not be
+//         a big problem
+//
+//       ChannelInboundHandlerAdapter should be a good first step, allowing us to focus on the implementation rather
+//       than the relationship to MessageDecoder.
+//
+//       Ultimately this should become a base class disconnected from ChannelHandler, instances of which are given out
+//       by FramingSupport to MessageEncoder, which then handles interactions between those objects and the Netty
+//       channel, becoming also ChannelInboundHandler. Note that when that happens, channel initialization (now in
+//       AbstractChannelInitializer) must be updated to place MessageEncoder *before* MessageDecoder.
 public abstract sealed class FrameDecoder extends ByteToMessageDecoder permits ChunkedFrameDecoder, EOMFrameDecoder {
     /**
      * The name of the handler providing frame decoding.
