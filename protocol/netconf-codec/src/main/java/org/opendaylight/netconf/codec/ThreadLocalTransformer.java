3b107e542c4f1949b4757417d27ac158dd1b9916
Robert Varga <robert.varga@pantheon.tech>
2024-08-11 10:59:55 +0200

    Refactor ThreadLocalTransformers
    Rather than being a plain holder class with duplicated code for each
    constant, rename the class ThreadLocalTransformer so that it extends
    ThreadLocal<Transformer>, being abstract and requiring instances to
    provide configure() method.
    
    The instances themselves have a nice place in MessageWriter, as that
    is where they are used.
    
    Since we have a dedicated class and constants, these replace the boolean
    -- instead we store the reference to the ThreadLocalTransformer instance
    which produces corresponding transformers.
    
    The equivalent of getters is provided by ThreadLocalTransformer.get(),
    i.e. getPrettyTransformer() becomes PRETTY_TRANSFORMER.get() without any
    intermediates.
    
    Since we are in the area, let's not access the thread local immediately
    -- expose threadLocalTransformer(), which just does ThreadLocal.get(), so
    that subclasses defer access to when they actually need it.
    
    Change-Id: I3e641e2f8fd3406052e04e15af9a6a74247d9777
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ protocol/netconf-codec/src/main/java/org/opendaylight/netconf/codec/ThreadLocalTransformer.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2014 Cisco Systems, Inc. and others.  All rights reserved.
+ * Copyright (c) 2024 PANTHEON.tech, s.r.o.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.netconf.codec;
+
+import static com.google.common.base.Verify.verifyNotNull;
+
+import com.google.common.base.VerifyException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.TransformerFactoryConfigurationError;
+import org.eclipse.jdt.annotation.NonNullByDefault;
+
+/**
+ * Utility class for cached thread-local transformers. This class exists mostly for use by handlers.
+ */
+@NonNullByDefault
+abstract class ThreadLocalTransformer extends ThreadLocal<Transformer> {
+    private static final TransformerFactory FACTORY = TransformerFactory.newInstance();
+
+    @Override
+    public final void set(final Transformer value) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    protected final Transformer initialValue() {
+        final Transformer transformer;
+        try {
+            transformer = FACTORY.newTransformer();
+        } catch (TransformerConfigurationException | TransformerFactoryConfigurationError e) {
+            throw new VerifyException(e);
+        }
+        return verifyNotNull(configure(transformer));
+    }
+
+    abstract Transformer configure(Transformer transformer);
+}
