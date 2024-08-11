0586b7202e183593ca83a2d2930fbc21ecf4e514
Robert Varga <robert.varga@pantheon.tech>
2024-08-11 09:45:27 +0200

    Split out JavaCommonCounters
    We are maintaining a native view of CommonCounters, which are defined to
    be wrapping unsigned 32bit counters.
    
    Our intermal use does not correctly truncate these, as the original code
    got confused by 'Long' representing 'uint32' in Binding long time ago.
    
    There is no need to store these counters in long form -- we can convert
    that on demand via a newly-introduced JavaCommonCounters -- which extend
    CommonCounters, i.e. the Binding interface generated for the grouping.
    
    Add two implementations, mutable and immutable, and expose the ability
    to increment as MutableJavaCommonCounters.
    
    This allows us to to expose the read-only view from NetconfServerSession
    without giving users access to directly modify them -- making the class
    more convenient.
    
    One side effect of us implementing CommonCounters is that we can use
    SessionBuilder(CommonCounters) constructor, which performs an automatic
    initialization to current state.
    
    Change-Id: I9287fb1a8d552f236f649c84cfefdf9433c09a5b
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ protocol/netconf-server/src/main/java/org/opendaylight/netconf/server/spi/AbstractJavaCommonCounters.java
@@ -0,0 +1,92 @@
+/*
+ * Copyright (c) 2024 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.netconf.server.spi;
+
+import com.google.common.base.MoreObjects;
+import org.opendaylight.netconf.server.api.monitoring.JavaCommonCounters;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.yang.types.rev130715.ZeroBasedCounter32;
+import org.opendaylight.yangtools.yang.common.Uint32;
+
+/**
+ * Abstract base class for {@link JavaCommonCounters} implementations.
+ */
+abstract sealed class AbstractJavaCommonCounters implements JavaCommonCounters
+        permits ImmutableCommonCounters, MutableCommonCounters {
+    @Override
+    public final long inRpcs() {
+        return Integer.toUnsignedLong(rawInRpcs());
+    }
+
+    @Override
+    public final long inBadRpcs() {
+        return Integer.toUnsignedLong(rawInBadRpcs());
+    }
+
+    @Override
+    public final long outRpcErrors() {
+        return Integer.toUnsignedLong(rawOutRpcErrors());
+    }
+
+    @Override
+    public final long outNotifications() {
+        return Integer.toUnsignedLong(rawOutNotifications());
+    }
+
+    @Override
+    public final ZeroBasedCounter32 getInRpcs() {
+        return counterOf(rawInRpcs());
+    }
+
+    @Override
+    public final ZeroBasedCounter32 getInBadRpcs() {
+        return counterOf(rawInBadRpcs());
+    }
+
+    @Override
+    public final ZeroBasedCounter32 getOutRpcErrors() {
+        return counterOf(rawOutRpcErrors());
+    }
+
+    @Override
+    public final ZeroBasedCounter32 getOutNotifications() {
+        return counterOf(rawOutNotifications());
+    }
+
+    private static ZeroBasedCounter32 counterOf(final int bits) {
+        return new ZeroBasedCounter32(Uint32.fromIntBits(bits));
+    }
+
+    abstract int rawInRpcs();
+
+    abstract int rawInBadRpcs();
+
+    abstract int rawOutRpcErrors();
+
+    abstract int rawOutNotifications();
+
+    static final int raw(final long unsigned) {
+        if (unsigned >> Integer.SIZE != 0) {
+            throw new IllegalArgumentException("Bad counter value " + unsigned);
+        }
+        return (int) unsigned;
+    }
+
+    static final int raw(final ZeroBasedCounter32 counter) {
+        return counter.getValue().intValue();
+    }
+
+    @Override
+    public final String toString() {
+        return MoreObjects.toStringHelper(this)
+            .add("in-rpcs", inRpcs())
+            .add("in-bad-rpcs", inBadRpcs())
+            .add("out-rpc-errors", outRpcErrors())
+            .add("out-notifications", outNotifications())
+            .toString();
+    }
+}
