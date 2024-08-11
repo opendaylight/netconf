cc35f85e6d2113e475e812a20332f95bf5ae6311
Robert Varga <robert.varga@pantheon.tech>
2024-08-11 09:45:27 +0200

    Split out JavaCommonCounters
    We are maintaining a native view of CommonCounters, which are defined to
    be wrapping unsigned 32bit counters.
    
    Our intermal use does not correctly truncate these, as the original code
    got confused by 'Long' representing 'uint32' in Binding long time ago.
    
    There is no need to store these counters in long form -- we can convert
    that on demand via a newly-introduced JavaCommonCounters -- which extend
    CommonCounters, i.e. the Binding interface generated for the grouping.
    
    Add two implementations, mutable and immutable, and expose the ability
    to increment as MutableJavaCommonCounters.
    
    This allows us to to expose the read-only view from NetconfServerSession
    without giving users access to directly modify them -- making the class
    more convenient.
    
    One side effect of us implementing CommonCounters is that we can use
    SessionBuilder(CommonCounters) constructor, which performs an automatic
    initialization to current state.
    
    Change-Id: I9287fb1a8d552f236f649c84cfefdf9433c09a5b
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ protocol/netconf-server/src/main/java/org/opendaylight/netconf/server/spi/MutableCommonCounters.java
@@ -0,0 +1,107 @@
+/*
+ * Copyright (c) 2024 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.netconf.server.spi;
+
+import static com.google.common.primitives.UnsignedInts.checkedCast;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.opendaylight.netconf.server.api.monitoring.JavaCommonCounters;
+import org.opendaylight.netconf.server.api.monitoring.MutableJavaCommonCounters;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.netconf.monitoring.rev101004.CommonCounters;
+
+/**
+ * Default implementation of {@link MutableJavaCommonCounters}.
+ */
+@NonNullByDefault
+public final class MutableCommonCounters extends AbstractJavaCommonCounters implements MutableJavaCommonCounters {
+    private int inRpcs;
+    private int inBadRpcs;
+    private int outRpcErrors;
+    private int outNotifications;
+
+    private MutableCommonCounters() {
+        // for default initialization
+    }
+
+    private MutableCommonCounters(final int inRpcs, final int inBadRpcs, final int outRpcErrors,
+            final int outNotifications) {
+        this.inRpcs = inRpcs;
+        this.inBadRpcs = inBadRpcs;
+        this.outRpcErrors = outRpcErrors;
+        this.outNotifications = outNotifications;
+    }
+
+    MutableCommonCounters(final AbstractJavaCommonCounters from) {
+        inRpcs = from.rawInRpcs();
+        inBadRpcs = from.rawInBadRpcs();
+        outRpcErrors = from.rawOutRpcErrors();
+        outNotifications = from.rawOutNotifications();
+    }
+
+    /**
+     * Return a new instance with all counters are set to zero.
+     */
+    public static MutableCommonCounters of() {
+        return new MutableCommonCounters();
+    }
+
+    /**
+     * Return a new instance with all counters initialized from another {@link CommonCounters}.
+     *
+     * @param values initial counter values
+     */
+    public static MutableCommonCounters copyOf(final CommonCounters values) {
+        return switch (values) {
+            case AbstractJavaCommonCounters from -> new MutableCommonCounters(from);
+            case JavaCommonCounters from -> new MutableCommonCounters(checkedCast(from.inRpcs()),
+                checkedCast(from.inBadRpcs()), checkedCast(from.outRpcErrors()), checkedCast(from.outNotifications()));
+            default -> new MutableCommonCounters(unwrap(values.requireInRpcs()), unwrap(values.requireInBadRpcs()),
+                unwrap(values.requireOutRpcErrors()), unwrap(values.requireOutNotifications()));
+        };
+    }
+
+    @Override
+    public void incInRpcs() {
+        inRpcs++;
+    }
+
+    @Override
+    public void incInBadRpcs() {
+        inBadRpcs++;
+    }
+
+    @Override
+    public void incOutRpcErrors() {
+        outRpcErrors++;
+    }
+
+    @Override
+    public void incOutNotifications() {
+        outNotifications++;
+    }
+
+    @Override
+    int rawInRpcs() {
+        return inRpcs;
+    }
+
+    @Override
+    int rawInBadRpcs() {
+        return inBadRpcs;
+    }
+
+    @Override
+    int rawOutRpcErrors() {
+        return outRpcErrors;
+    }
+
+    @Override
+    int rawOutNotifications() {
+        return outNotifications;
+    }
+}
