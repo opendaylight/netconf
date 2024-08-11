7bf6678afd5ead5f3d9429f790a0108439c66091
Robert Varga <robert.varga@pantheon.tech>
2024-08-11 09:45:27 +0200

    Split out JavaCommonCounters
    We are maintaining a native view of CommonCounters, which are defined to
    be wrapping unsigned 32bit counters.
    
    Our intermal use does not correctly truncate these, as the original code
    got confused by 'Long' representing 'uint32' in Binding long time ago.
    
    There is no need to store these counters in long form -- we can convert
    that on demand via a newly-introduced JavaCommonCounters -- which extend
    CommonCounters, i.e. the Binding interface generated for the grouping.
    
    Add two implementations, mutable and immutable, and expose the ability
    to increment as MutableJavaCommonCounters.
    
    This allows us to to expose the read-only view from NetconfServerSession
    without giving users access to directly modify them -- making the class
    more convenient.
    
    One side effect of us implementing CommonCounters is that we can use
    SessionBuilder(CommonCounters) constructor, which performs an automatic
    initialization to current state.
    
    Change-Id: I9287fb1a8d552f236f649c84cfefdf9433c09a5b
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ protocol/netconf-server/src/main/java/org/opendaylight/netconf/server/spi/ImmutableCommonCounters.java
@@ -0,0 +1,84 @@
+/*
+ * Copyright (c) 2024 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.netconf.server.spi;
+
+import static java.util.Objects.requireNonNull;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.opendaylight.netconf.server.api.monitoring.JavaCommonCounters;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.netconf.monitoring.rev101004.CommonCounters;
+import org.opendaylight.yangtools.concepts.Immutable;
+
+/**
+ * {@link Immutable} implementation of {@link JavaCommonCounters}.
+ */
+@NonNullByDefault
+public final class ImmutableCommonCounters extends AbstractJavaCommonCounters implements Immutable {
+    private final int inRpcs;
+    private final int inBadRpcs;
+    private final int outRpcErrors;
+    private final int outNotifications;
+
+    private ImmutableCommonCounters(final int inRpcs, final int inBadRpcs, final int outRpcErrors,
+            final int outNotifications) {
+        this.inRpcs = inRpcs;
+        this.inBadRpcs = inBadRpcs;
+        this.outRpcErrors = outRpcErrors;
+        this.outNotifications = outNotifications;
+    }
+
+    private ImmutableCommonCounters(final CommonCounters from) {
+        this(raw(from.requireInRpcs()), raw(from.requireInBadRpcs()), raw(from.requireOutRpcErrors()),
+            raw(from.requireOutNotifications()));
+    }
+
+    public static ImmutableCommonCounters copyOf(final CommonCounters values) {
+        return switch (values) {
+            case ImmutableCommonCounters immutable -> immutable;
+            case MutableCommonCounters from -> copyOf(from);
+            case JavaCommonCounters from -> new ImmutableCommonCounters(raw(from.inRpcs()), raw(from.inBadRpcs()),
+                raw(from.outRpcErrors()), raw(from.outNotifications()));
+            default -> new ImmutableCommonCounters(values);
+        };
+    }
+
+    public static ImmutableCommonCounters copyOf(final MutableCommonCounters values) {
+        return new ImmutableCommonCounters(values.rawInRpcs(), values.rawInBadRpcs(), values.rawOutRpcErrors(),
+            values.rawOutNotifications());
+    }
+
+    // Do not call
+    @Deprecated(forRemoval = true, since = "8.0.1")
+    public static ImmutableCommonCounters copyOf(final ImmutableCommonCounters counters) {
+        return requireNonNull(counters);
+    }
+
+    public MutableCommonCounters toMutable() {
+        return new MutableCommonCounters(this);
+    }
+
+    @Override
+    int rawInRpcs() {
+        return inRpcs;
+    }
+
+    @Override
+    int rawInBadRpcs() {
+        return inBadRpcs;
+    }
+
+    @Override
+    int rawOutRpcErrors() {
+        return outRpcErrors;
+    }
+
+    @Override
+    int rawOutNotifications() {
+        return outNotifications;
+    }
+}
