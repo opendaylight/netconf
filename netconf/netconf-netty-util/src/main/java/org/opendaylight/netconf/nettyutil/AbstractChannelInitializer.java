a3118cebde19fd2b481d60af08cfe1748425d094
Robert Varga <robert.varga@pantheon.tech>
2024-08-11 04:17:38 +0200

    Deprecate NetconfHelloMessageAdditionalHeader
    This class should not exist, but getting rid of it requires us to do
    quite some work, which in turn has impacts in netconf-{codec,netty-util}
    and perhaps somewhere else as well.
    
    Deprecate this class for removal to drive its replacements. Also provide
    a lot of FIXMEs explaining the various changes to
    netconf-{api,codec,client,server} that need to happen to solidify the
    pipeline layout, the fool-proof the end user APIs and provide reliable
    operation.
    
    Change-Id: I8ec055dc6f032df3171cd0dddc78f33498956fde
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- netconf/netconf-netty-util/src/main/java/org/opendaylight/netconf/nettyutil/AbstractChannelInitializer.java
+++ netconf/netconf-netty-util/src/main/java/org/opendaylight/netconf/nettyutil/AbstractChannelInitializer.java
@@ -17,9 +17,18 @@ import org.opendaylight.netconf.codec.MessageDecoder;
 import org.opendaylight.netconf.codec.MessageEncoder;
 import org.opendaylight.netconf.nettyutil.handler.HelloXMLMessageDecoder;
 
+// FIXME: This is not related Netty's'ChannelInitializer' and is pretty much unused.
+//        Perhaps a NetconfConnectionFactory? It would:
+//        - take a TransportChannel
+//        - initialize the pipeline with MessageEncoder
+//        - produce a NetconfConnection, which has:
+//          -  TransportChannel transport()
+//          -  MessageEncoder messageEncoder()
 public abstract class AbstractChannelInitializer<S extends NetconfSession> {
     public static final String NETCONF_SESSION_NEGOTIATOR = "negotiator";
 
+    // FIXME: this should be taking only a TransportChannel and should be factory method producing NetconfConnections,
+    //        without the 'attach a negotiator with some promise' part
     public void initialize(final Channel ch, final Promise<S> promise) {
         ch.pipeline().addLast(FrameDecoder.HANDLER_NAME, new EOMFrameDecoder());
         initializeMessageDecoder(ch);
@@ -40,5 +49,7 @@ public abstract class AbstractChannelInitializer<S extends NetconfSession> {
      * Insert session negotiator into the pipeline. It must be inserted after message decoder
      * identified by {@link MessageDecoder#HANDLER_NAME}, (or any other custom decoder processor)
      */
+    // FIXME: This should be somewhere else. It should take an already-initialized NetconfConnection and perform
+    //        the listener magic of eventually giving out the negotiated NetconfSession.
     protected abstract void initializeSessionNegotiator(Channel ch, Promise<S> promise);
 }
