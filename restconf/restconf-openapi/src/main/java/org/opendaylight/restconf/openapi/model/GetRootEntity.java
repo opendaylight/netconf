2f86c06572947b834977f100ed82bfeeda64e84a
Oleksandr Zharov <oleksandr.zharov@pantheon.tech>
2024-08-21 21:43:09 +0200

    Add response body example for ROOT data GET
    Added refs to root data GET requests to fill the response
    for this path. Controller and devices have different
    models wired up to them depending on what you can
    actually see after executing GET request to them.
    
    All UT test resources have been adapted for this change.
    
    JIRA: NETCONF-1342
    Change-Id: I38ec5df3183809e062fed1af09ba43cf59e1a034
    Signed-off-by: Oleksandr Zharov <oleksandr.zharov@pantheon.tech>
    (cherry picked from commit 5da08ce83dd4ff26d095ab0ebe985df4257ec127)

--- restconf/restconf-openapi/src/main/java/org/opendaylight/restconf/openapi/model/GetRootEntity.java
+++ restconf/restconf-openapi/src/main/java/org/opendaylight/restconf/openapi/model/GetRootEntity.java
@@ -18,6 +18,11 @@ import javax.ws.rs.core.MediaType;
 import org.eclipse.jdt.annotation.NonNull;
 
 public final class GetRootEntity extends GetEntity {
+    private static final String DATA = "data";
+    private static final String CONTROLLER = "Controller";
+    private static final String NETCONF_STATE = "#/components/schemas/ietf-netconf-monitoring_netconf-state";
+    private static final String MODULES_STATE = "#/components/schemas/ietf-yang-library_modules-state";
+
     private final String type;
 
     public GetRootEntity(final @NonNull String deviceName, final @NonNull String type) {
@@ -28,9 +33,20 @@ public final class GetRootEntity extends GetEntity {
     @Override
     public void generate(final @NonNull JsonGenerator generator) throws IOException {
         generator.writeObjectFieldStart("get");
-        generator.writeStringField(DESCRIPTION, type.equals("data")
-            ? "Queries the config (startup) datastore on the mounted hosted."
-            : "Queries the available operations (RPC calls) on the mounted hosted.");
+        if (type.equals(DATA)) {
+            if (deviceName().equals(CONTROLLER)) {
+                generator.writeStringField(DESCRIPTION,
+                    "Result of this GET contains YANG module monitoring information.");
+            } else {
+                generator.writeStringField(DESCRIPTION, """
+                    Result of this GET contains data from the monitoring data model, i.e., several lists that have
+                    information about capabilities supported by the server, configuration datastores, data model schemas
+                    supported by the server, some session-specific data and statistical data.""");
+            }
+        } else {
+            generator.writeStringField(DESCRIPTION,
+                "Queries the available operations (RPC calls) on the mounted hosted.");
+        }
         generator.writeObjectFieldStart(RESPONSES);
         generator.writeObjectFieldStart(String.valueOf(OK.getStatusCode()));
         generator.writeStringField(DESCRIPTION, "OK");
@@ -38,13 +54,30 @@ public final class GetRootEntity extends GetEntity {
         generator.writeObjectFieldStart(MediaType.APPLICATION_JSON);
         generator.writeObjectFieldStart(SCHEMA);
         generator.writeObjectFieldStart(PROPERTIES);
+        if (type.equals(DATA)) {
+            if (deviceName().equals(CONTROLLER)) {
+                generator.writeObjectFieldStart("ietf-yang-library:modules-state");
+                generator.writeStringField(TYPE, OBJECT);
+                generator.writeStringField(REF, MODULES_STATE);
+            } else {
+                generator.writeObjectFieldStart("ietf-netconf-monitoring:netconf-state");
+                generator.writeStringField(TYPE, OBJECT);
+                generator.writeStringField(REF, NETCONF_STATE);
+            }
+            generator.writeEndObject(); // end of state
+        }
         generator.writeEndObject(); // end of properties
         generator.writeEndObject(); // end of json schema
         generator.writeEndObject(); //end of json
         generator.writeObjectFieldStart(MediaType.APPLICATION_XML);
         generator.writeObjectFieldStart(SCHEMA);
-        generator.writeObjectFieldStart(PROPERTIES);
-        generator.writeEndObject(); // end of properties
+        if (type.equals(DATA)) {
+            if (deviceName().equals(CONTROLLER)) {
+                generator.writeStringField(REF, MODULES_STATE);
+            } else {
+                generator.writeStringField(REF, NETCONF_STATE);
+            }
+        }
         generator.writeEndObject(); // end of xml schema
         generator.writeEndObject(); // end of xml
         generator.writeEndObject(); //end of content
