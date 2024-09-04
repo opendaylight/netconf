c5f94d862c3969e05fb0eaa5e3de9e355a3a7450
Ivan Hrasko <ivan.hrasko@pantheon.tech>
2024-09-04 14:30:25 +0300

    End-to-end tests for Netty RESTCONF server
    Integration tests provided for Netty based RESTCONF server
    implementation. Instance of Netty endpoint with basic
    controller models is created then HTTP requests are performed
    using Netty based client from transport-http.
    
    Tests covering requests to /data (incl actions and yang-patch),
    /operations, /yang-library-version, /modules URIs plus
    /.well-known/host-meta{.json} URIs.
    
    Additional scenarios:
    - topology node mount using device simulator
    - data change notifications event stream
    - device notification event stream
    
    JIRA: NETCONF-1367
    Change-Id: Ibb9dc960cc03b1a7215cb8ff39e2fd2bd136db0e
    Signed-off-by: Ivan Hrasko <ivan.hrasko@pantheon.tech>
    Signed-off-by: Oleksandr Zharov <oleksandr.zharov@pantheon.tech>
    Signed-off-by: Matej Sramcik <matej.sramcik@pantheon.tech>
    Signed-off-by: Ruslan Kashapov <ruslan.kashapov@pantheon.tech>

---
+++ apps/restconf-it/src/test/java/org/opendaylight/restconf/server/netty/DataE2ETest.java
@@ -0,0 +1,404 @@
+/*
+ * Copyright (c) 2024 PANTHEON.tech s.r.o. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.restconf.server.netty;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import io.netty.handler.codec.http.HttpHeaderNames;
+import io.netty.handler.codec.http.HttpMethod;
+import io.netty.handler.codec.http.HttpResponseStatus;
+import java.util.Set;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.opendaylight.restconf.api.MediaTypes;
+import org.opendaylight.yangtools.yang.common.ErrorTag;
+import org.opendaylight.yangtools.yang.common.ErrorType;
+
+class DataE2ETest extends AbstractE2ETest {
+    private static final String DATA_URI = "/rests/data";
+    private static final String PARENT_URI = DATA_URI + "/example-jukebox:jukebox/library";
+    private static final String ACTIONS_URI = DATA_URI + "/example-action:root";
+    private static final String ITEM_URI = PARENT_URI + "/artist=artist";
+    private static final String INITIAL_NODE_JSON = """
+        {
+            "example-jukebox:artist": [
+                {
+                    "name": "artist",
+                    "album": [{
+                        "name": "album1",
+                        "genre": "example-jukebox:rock",
+                        "year": 2020
+                    }]
+                }
+            ]
+        }""";
+    private static final String ACTION_INPUT_JSON = """
+        {
+            "input": {
+                "data": "Some data"
+            }
+        }""";
+
+    @BeforeEach
+    @Override
+    void beforeEach() throws Exception {
+        super.beforeEach();
+        resetDataNode();
+    }
+
+    @Test
+    void dataCRUDJson() throws Exception {
+        // create
+        var response = invokeRequest(HttpMethod.POST, PARENT_URI, APPLICATION_JSON, INITIAL_NODE_JSON);
+        assertEquals(HttpResponseStatus.CREATED, response.status());
+
+        // read (validate created)
+        assertContentJson(ITEM_URI, INITIAL_NODE_JSON);
+
+        // update (merge)
+        response = invokeRequest(HttpMethod.PATCH, ITEM_URI, APPLICATION_JSON,
+            """
+                {
+                    "example-jukebox:artist": [
+                        {
+                            "name": "artist",
+                            "album": [{
+                                "name": "album1",
+                                "genre": "example-jukebox:jazz"
+                            }]
+                        }
+                    ]
+                }""");
+        assertEquals(HttpResponseStatus.OK, response.status());
+
+        // validate updated
+        assertContentJson(ITEM_URI, """
+                {
+                    "example-jukebox:artist": [
+                        {
+                            "name": "artist",
+                            "album": [{
+                                "name": "album1",
+                                "genre": "example-jukebox:jazz",
+                                "year": 2020
+                            }]
+                        }
+                    ]
+                }""");
+
+        // replace
+        final var replaceNode = """
+                {
+                    "example-jukebox:artist": [
+                        {
+                            "name": "artist",
+                            "album": [{
+                                "name": "album2",
+                                "genre": "example-jukebox:pop",
+                                "year": 2024
+                            }]
+                        }
+                    ]
+                }""";
+        response = invokeRequest(HttpMethod.PUT, ITEM_URI, APPLICATION_JSON, replaceNode);
+        assertEquals(HttpResponseStatus.NO_CONTENT, response.status());
+
+        // validate replaced
+        assertContentJson(ITEM_URI, replaceNode);
+
+        // delete
+        response = invokeRequest(HttpMethod.DELETE, ITEM_URI);
+        assertEquals(HttpResponseStatus.NO_CONTENT, response.status());
+
+        // validate deleted
+        response = invokeRequest(HttpMethod.GET, ITEM_URI);
+        assertErrorResponseJson(response, ErrorType.PROTOCOL, ErrorTag.DATA_MISSING);
+    }
+
+    @Test
+    void dataExistsErrorJson() throws Exception {
+        // insert data first time
+        var response = invokeRequest(HttpMethod.POST, PARENT_URI, APPLICATION_JSON, INITIAL_NODE_JSON);
+        assertEquals(HttpResponseStatus.CREATED, response.status());
+        // subsequent insert of same node
+        response = invokeRequest(HttpMethod.POST, PARENT_URI, APPLICATION_JSON, INITIAL_NODE_JSON);
+        assertErrorResponseJson(response, ErrorType.PROTOCOL, ErrorTag.DATA_EXISTS);
+    }
+
+    @Test
+    void dataMissingErrorJson() throws Exception {
+        // GET case
+        var response = invokeRequest(HttpMethod.GET, ITEM_URI);
+        assertErrorResponseJson(response, ErrorType.PROTOCOL, ErrorTag.DATA_MISSING);
+        // DELETE case
+        response = invokeRequest(HttpMethod.DELETE, ITEM_URI);
+        assertErrorResponseJson(response, ErrorType.PROTOCOL, ErrorTag.DATA_MISSING);
+    }
+
+    @Test
+    void invokeAction() throws Exception {
+        // invoke action
+        final var response = invokeRequest(HttpMethod.POST, ACTIONS_URI + "/example-action", APPLICATION_JSON,
+            ACTION_INPUT_JSON);
+        assertContentJson(response, """
+            {
+                "example-action:output": {
+                    "response":"Action was invoked"
+                }
+            }""");
+    }
+
+    @Test
+    void invokeActionWithBadInputs() throws Exception {
+        // invoke action
+        final var response = invokeRequest(HttpMethod.POST, ACTIONS_URI + "/example-action", APPLICATION_JSON,
+            """
+            {
+                "input": {
+                    "wrong-data": "Some wrong data"
+                }
+            }""");
+        assertErrorResponseJson(response, ErrorType.PROTOCOL, ErrorTag.OPERATION_FAILED);
+    }
+
+    @Test
+    void invokeNotImplementedAction() throws Exception {
+        // invoke not implemented action
+        final var response = invokeRequest(HttpMethod.POST, ACTIONS_URI + "/not-implemented", APPLICATION_JSON,
+            ACTION_INPUT_JSON);
+        assertErrorResponseJson(response, ErrorType.RPC, ErrorTag.OPERATION_FAILED);
+    }
+
+    @Test
+    void yangPatch() throws Exception {
+        // CRUD
+        var response = invokeRequest(HttpMethod.PATCH, ITEM_URI, MediaTypes.APPLICATION_YANG_PATCH_JSON, """
+            {
+                "ietf-yang-patch:yang-patch" : {
+                    "patch-id" : "patch1",
+                    "edit" : [
+                        {
+                            "edit-id": "edit1",
+                            "operation": "create",
+                            "target": "/album=album1",
+                            "value": {
+                                "album": {
+                                    "name": "album1",
+                                    "genre": "example-jukebox:rock",
+                                    "year": 2020
+                                }
+                            }
+                        },
+                        {
+                            "edit-id": "edit2",
+                            "operation": "create",
+                            "target": "/album=album2",
+                            "value": {
+                                "album": {
+                                    "name": "album2",
+                                    "genre": "example-jukebox:jazz",
+                                    "year": 2020
+                                }
+                            }
+                        },
+                        {
+                            "edit-id": "edit3",
+                            "operation": "replace",
+                            "target": "/album=album1",
+                            "value": {
+                                "album": {
+                                    "name": "album1",
+                                    "genre": "example-jukebox:pop",
+                                    "year": 2024
+                                }
+                            }
+                        },
+                        {
+                            "edit-id": "edit4",
+                            "operation": "delete",
+                            "target": "/album=album2"
+                        }
+                    ]
+                }
+            }""");
+        assertEquals(HttpResponseStatus.OK, response.status());
+
+        // read (validate result)
+        assertContentJson(ITEM_URI, """
+            {
+                "example-jukebox:artist": [
+                    {
+                        "name": "artist",
+                        "album": [
+                            {
+                                "name": "album1",
+                                "genre": "example-jukebox:pop",
+                                "year": 2024
+                            }
+                        ]
+                    }
+                ]
+            }""");
+    }
+
+    @Test
+    void yangPatchDataMissingError() throws Exception {
+        // One correct edit, one - not
+        var response = invokeRequest(HttpMethod.PATCH, ITEM_URI, MediaTypes.APPLICATION_YANG_PATCH_JSON, """
+            {
+                "ietf-yang-patch:yang-patch" : {
+                    "patch-id" : "patch1",
+                    "edit" : [
+                        {
+                            "edit-id": "edit1",
+                            "operation": "create",
+                            "target": "/album=album1",
+                            "value": {
+                                "album": {
+                                    "name": "album1",
+                                    "genre": "example-jukebox:rock",
+                                    "year": 2020
+                                }
+                            }
+                        },
+                        {
+                            "edit-id": "edit2",
+                            "operation": "delete",
+                            "target": "/album=album2"
+                        }
+                    ]
+                }
+            }""");
+        assertContentJson(response, """
+            {
+                "ietf-yang-patch:yang-patch-status": {
+                    "patch-id":"patch1",
+                    "edit-status": {
+                        "edit": [
+                        {
+                            "edit-id":"edit1",
+                            "ok":[null]
+                        },
+                        {
+                            "edit-id":"edit2",
+                            "errors": {
+                                "error": [
+                                {
+                                    "error-type":"protocol",
+                                    "error-tag":"data-missing",
+                                    "error-path":
+                                    "/example-jukebox:jukebox/library/artist[name='artist']/album[name='album2']",
+                                    "error-message":"Data does not exist"
+                                }]
+                            }
+                        }]
+                    }
+                }
+            }""");
+    }
+
+    @Test
+    void yangPatchDataExistsError() throws Exception {
+        // One correct edit, one - not
+        var response = invokeRequest(HttpMethod.PATCH, ITEM_URI, MediaTypes.APPLICATION_YANG_PATCH_JSON, """
+            {
+                "ietf-yang-patch:yang-patch" : {
+                    "patch-id" : "patch1",
+                    "edit" : [
+                        {
+                            "edit-id": "edit1",
+                            "operation": "create",
+                            "target": "/album=album1",
+                            "value": {
+                                "album": {
+                                    "name": "album1",
+                                    "genre": "example-jukebox:rock",
+                                    "year": 2020
+                                }
+                            }
+                        },
+                        {
+                            "edit-id": "edit2",
+                            "operation": "create",
+                            "target": "/album=album1",
+                            "value": {
+                                "album": {
+                                    "name": "album1",
+                                    "genre": "example-jukebox:jazz",
+                                    "year": 2020
+                                }
+                            }
+                        }
+                    ]
+                }
+            }""");
+        assertContentJson(response, """
+            {
+                "ietf-yang-patch:yang-patch-status": {
+                    "patch-id":"patch1",
+                    "edit-status": {
+                        "edit": [
+                        {
+                            "edit-id":"edit1",
+                            "ok":[null]
+                        },
+                        {
+                            "edit-id":"edit2",
+                            "errors": {
+                                "error": [
+                                {
+                                    "error-type":"protocol",
+                                    "error-tag":"data-exists",
+                                    "error-path":
+                                    "/example-jukebox:jukebox/library/artist[name='artist']/album[name='album1']",
+                                    "error-message":"Data already exists"
+                                }]
+                            }
+                        }]
+                    }
+                }
+            }""");
+    }
+
+    @Test
+    void dataOptions() throws Exception {
+        final var patchAcceptTypes =  Set.of("text/xml", APPLICATION_JSON, APPLICATION_XML,
+            MediaTypes.APPLICATION_YANG_DATA_JSON, MediaTypes.APPLICATION_YANG_DATA_XML,
+            MediaTypes.APPLICATION_YANG_PATCH_JSON, MediaTypes.APPLICATION_YANG_PATCH_XML);
+        // root
+        var response = invokeRequest(HttpMethod.OPTIONS, DATA_URI);
+        assertOptionsResponse(response, Set.of("GET", "POST", "PUT", "PATCH", "OPTIONS", "HEAD"));
+        assertHeaderValue(response, HttpHeaderNames.ACCEPT_PATCH, patchAcceptTypes);
+
+        // non-root also deletable
+        response = invokeRequest(HttpMethod.OPTIONS, PARENT_URI);
+        assertOptionsResponse(response, Set.of("GET", "POST", "PUT", "PATCH", "OPTIONS", "HEAD", "DELETE"));
+        assertHeaderValue(response, HttpHeaderNames.ACCEPT_PATCH, patchAcceptTypes);
+    }
+
+    @Test
+    void dataHead() throws Exception {
+        assertHead(DATA_URI);
+    }
+
+    private void resetDataNode() throws Exception {
+        // ensure data node exists before we insert a child node
+        var response = invokeRequest(HttpMethod.PUT,
+            "/rests/data/example-jukebox:jukebox",
+            APPLICATION_JSON,
+            """
+                {
+                    "example-jukebox:jukebox": {
+                        "library": {}
+                    }
+                }""");
+        final var status = response.status().code();
+        assertTrue(status == HttpResponseStatus.OK.code() || status ==  HttpResponseStatus.CREATED.code());
+    }
+}
---
+++ apps/restconf-it/src/test/java/org/opendaylight/restconf/server/netty/OperationsE2ETest.java
@@ -0,0 +1,210 @@
+/*
+ * Copyright (c) 2024 PANTHEON.tech s.r.o. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.restconf.server.netty;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import io.netty.handler.codec.http.HttpMethod;
+import io.netty.handler.codec.http.HttpResponseStatus;
+import java.util.Set;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.opendaylight.mdsal.eos.dom.simple.SimpleDOMEntityOwnershipService;
+import org.opendaylight.mdsal.singleton.impl.EOSClusterSingletonServiceProvider;
+import org.opendaylight.netconf.keystore.legacy.impl.DefaultNetconfKeystoreService;
+import org.opendaylight.restconf.server.netty.TestUtils.TestEncryptionService;
+import org.opendaylight.yangtools.yang.common.ErrorTag;
+import org.opendaylight.yangtools.yang.common.ErrorType;
+
+class OperationsE2ETest extends AbstractE2ETest {
+    private static final String OPERATIONS_URI = "/rests/operations";
+    private static final String SUBSCRIBE_DEVICE_NOTIFICATIONS_URI =
+        OPERATIONS_URI + "/odl-device-notification:subscribe-device-notification";
+    private static final String WRONG_TYPE = "application/svg+xml";
+    private static final String ADD_KEYSTORE_ENTRY = "/rests/operations/netconf-keystore:add-keystore-entry";
+
+    private DefaultNetconfKeystoreService netconfKeystoreService;
+
+    @BeforeEach
+    @Override
+    void beforeEach() throws Exception {
+        super.beforeEach();
+        final var cssService = new EOSClusterSingletonServiceProvider(new SimpleDOMEntityOwnershipService());
+        netconfKeystoreService = new DefaultNetconfKeystoreService(getDataBroker(), rpcProviderService,
+            cssService, new TestEncryptionService());
+    }
+
+    @AfterEach
+    @Override
+    void afterEach() {
+        netconfKeystoreService.close();
+        netconfKeystoreService = null;
+        super.afterEach();
+    }
+
+    @Test
+    void readOperationsJson() throws Exception {
+        // check those we'll use in tests
+        assertContentJson(OPERATIONS_URI, """
+            {
+                "ietf-restconf:operations" : {
+                    "netconf-keystore:add-keystore-entry" : [null],
+                    "netconf-keystore:remove-keystore-entry" : [null],
+                    "netconf-keystore:add-private-key" : [null],
+                    "netconf-keystore:remove-private-key" : [null],
+                    "netconf-keystore:add-trusted-certificate" : [null],
+                    "netconf-keystore:remove-trusted-certificate" : [null],
+                }
+            }""");
+    }
+
+    @Test
+    void readSingleOperationJson() throws Exception {
+        assertContentJson(SUBSCRIBE_DEVICE_NOTIFICATIONS_URI, """
+            {
+                "odl-device-notification:subscribe-device-notification": [null],
+            }""");
+    }
+
+    @Test
+    @SuppressWarnings("checkstyle:LineLength")
+    void createKeystoreEntry() throws Exception {
+        final var response = invokeRequest(HttpMethod.POST, ADD_KEYSTORE_ENTRY,
+            APPLICATION_XML, """
+            <input xmlns="urn:opendaylight:netconf:keystore">
+                <key-credential>
+                    <key-id>key-id</key-id>
+                    <private-key>-----BEGIN RSA PRIVATE KEY-----
+            MIIEpgIBAAKCAQEAvM2whCdDEmxTfio1oUCmxlZBvkyircTGraSjCeZWA5gdXcFg
+            R3Po49/obQLXONwVE8ULP+wm9vFVae5AdXRjndG/xxi/Z/HAZqkXMXvjRIYiBVgD
+            cu3c0jRdbimwS84zKYIygXGeHODvNKQcMNFNENQz2f/QIVPRioFB4sHxynvP4re0
+            +pYUNsl6gpZFtMZ804oqWAuk90Db0x1jgn87FLh3FXAOLHqmWDDjoj6jLo4FMLNo
+            ydstJQ14Nm3Gsg8LWoV9qGT/hYpfYnzuQizb/ZsPs3vEcV3g67RRCc82X5jKeXJo
+            /C5r3czrM68X3TKBtvIMThmF17p79/nuLyt9kQIDAQABAoIBAQCOWLoj+QINqtSM
+            Q8CpcfgLg08P7fGc98YfdwhhV2M0VISXgktXs+E7pT40qjagLPZLMH2Z1S9PcYbH
+            VhUNORI+E7z2nAb7lH5OKGBPM6uWp1aRFtmK1iFt7oMeopnDnZRfUEVJ6OKfvUs8
+            Mhr7B2KGNKdfTgqahfpu5aNKFpV45fGaPFhIeKJeTFExVQNmcHwxzQPqeocOHuEF
+            XaOzLidXM9KT9gGpsf0Fo0rv6K9oZGO3C5GRnf2jkWL0PpoJLWjR7fNGWZp1nsGa
+            oG8m9wjb6KYkQwrrvcVmzjPcmwvfcG0FBUMLwMNKiJ5/nispbR0TpL7nt01fG2P1
+            rwyunbVtAoGBAO8RjNHJt7FFOi82xKcJNe6SwD6p8TRJNW9j1MxhOS/yaq9DDyVR
+            pwFHZo7pK/TU4HBc6DYyFN9W5+4Tn/cWmuu9YfimhNquoFO531qF3ltJ6ZY+wRs3
+            cYzQouC7uww68PdLGGE2vxJLXuvVrmsbevh3+VKtLdqXO8v618KCc+3/AoGBAMos
+            zgJ0Rjxm+VObvYjJz+fzaLCNNORJO4C47F9woy4inSUOO2P30QEBRC+UPjM5AJiz
+            Up8X1NCOW43o3zfAmBhtDLIwhrW8fWc/OQeqDt7GM9Q2a8yyXPACqDqsBDxTeu3M
+            hEwACzXuHRscQyIRV8HHCWph3xwTwLGG9ZIEcLRvAoGBAIbsNbh0ispuUo8w7r2C
+            skBp7Duxd6LVqmWqRv/t4vOPcexmAVdDhOhw3o3LRPaRafWgSaHElAkUKCMySjaO
+            OHLRWEiX2iT9Jxj5rveM09hbl4wm8J8mpFwfp70D1mXpofM/G4xJ9H4jsXeSCjUC
+            tl0igMDLYjSa47GUaU6qhzkLAoGBAMID9T7NrolQmHvvvReD9Ay3vgOPvu5EiOGi
+            lNOSGEax2PQykDQDIYNBX9n4/SfS0Au6KtOZ3xS1SI8Kpwutu0fVfpWRk/TbicyH
+            E4eTXunScvJ3t0Oc9yssoZyMbxQlWJbT6TG16Qw8EZpuqM4Mrpa7FwIMIjujiQvU
+            Y91YfX/pAoGBANtGPuVorE8U7PmAfTvEUKwvpDUz2QWZCOYOazBMrXNVsWLSdaaI
+            6ZQosKgQ9ThVYwItqE1dBp5vjPQKwjBGW2EbCHl2XUJXh9cH87P3kyYAc9sqeQGf
+            +dSOkNEWsmFl1VwaCUt6L+jqhfBGQQK1bAHmjdRfsaj1gZyE9OoHfvk2
+            -----END RSA PRIVATE KEY-----</private-key>
+                    <passphrase></passphrase>
+                </key-credential>
+            </input>""");
+        assertEquals(HttpResponseStatus.NO_CONTENT, response.status());
+
+        // validate content
+        assertContentXml("/rests/data/netconf-keystore:keystore/key-credential=key-id", """
+            <key-credential xmlns="urn:opendaylight:netconf:keystore">
+                <key-id>key-id</key-id>
+                <algorithm>RSA</algorithm>
+                <public-key>MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAvM2whCdDEmxTfio1oUCmxlZBvkyircTGraSjCeZWA5gdXcFgR3Po49/obQLXONwVE8ULP+wm9vFVae5AdXRjndG/xxi/Z/HAZqkXMXvjRIYiBVgDcu3c0jRdbimwS84zKYIygXGeHODvNKQcMNFNENQz2f/QIVPRioFB4sHxynvP4re0+pYUNsl6gpZFtMZ804oqWAuk90Db0x1jgn87FLh3FXAOLHqmWDDjoj6jLo4FMLNoydstJQ14Nm3Gsg8LWoV9qGT/hYpfYnzuQizb/ZsPs3vEcV3g67RRCc82X5jKeXJo/C5r3czrM68X3TKBtvIMThmF17p79/nuLyt9kQIDAQAB</public-key>
+                <private-key>MIIEwAIBADANBgkqhkiG9w0BAQEFAASCBKowggSmAgEAAoIBAQC8zbCEJ0MSbFN+KjWhQKbGVkG+TKKtxMatpKMJ5lYDmB1dwWBHc+jj3+htAtc43BUTxQs/7Cb28VVp7kB1dGOd0b/HGL9n8cBmqRcxe+NEhiIFWANy7dzSNF1uKbBLzjMpgjKBcZ4c4O80pBww0U0Q1DPZ/9AhU9GKgUHiwfHKe8/it7T6lhQ2yXqClkW0xnzTiipYC6T3QNvTHWOCfzsUuHcVcA4seqZYMOOiPqMujgUws2jJ2y0lDXg2bcayDwtahX2oZP+Fil9ifO5CLNv9mw+ze8RxXeDrtFEJzzZfmMp5cmj8LmvdzOszrxfdMoG28gxOGYXXunv3+e4vK32RAgMBAAECggEBAI5YuiP5Ag2q1IxDwKlx+AuDTw/t8Zz3xh93CGFXYzRUhJeCS1ez4TulPjSqNqAs9kswfZnVL09xhsdWFQ05Ej4TvPacBvuUfk4oYE8zq5anVpEW2YrWIW3ugx6imcOdlF9QRUno4p+9SzwyGvsHYoY0p19OCpqF+m7lo0oWlXjl8Zo8WEh4ol5MUTFVA2ZwfDHNA+p6hw4e4QVdo7MuJ1cz0pP2Aamx/QWjSu/or2hkY7cLkZGd/aORYvQ+mgktaNHt80ZZmnWewZqgbyb3CNvopiRDCuu9xWbOM9ybC99wbQUFQwvAw0qInn+eKyltHROkvue3TV8bY/WvDK6dtW0CgYEA7xGM0cm3sUU6LzbEpwk17pLAPqnxNEk1b2PUzGE5L/Jqr0MPJVGnAUdmjukr9NTgcFzoNjIU31bn7hOf9xaa671h+KaE2q6gU7nfWoXeW0nplj7BGzdxjNCi4Lu7DDrw90sYYTa/Ekte69Wuaxt6+Hf5Uq0t2pc7y/rXwoJz7f8CgYEAyizOAnRGPGb5U5u9iMnP5/NosI005Ek7gLjsX3CjLiKdJQ47Y/fRAQFEL5Q+MzkAmLNSnxfU0I5bjejfN8CYGG0MsjCGtbx9Zz85B6oO3sYz1DZrzLJc8AKoOqwEPFN67cyETAALNe4dGxxDIhFXwccJamHfHBPAsYb1kgRwtG8CgYEAhuw1uHSKym5SjzDuvYKyQGnsO7F3otWqZapG/+3i849x7GYBV0OE6HDejctE9pFp9aBJocSUCRQoIzJKNo44ctFYSJfaJP0nGPmu94zT2FuXjCbwnyakXB+nvQPWZemh8z8bjEn0fiOxd5IKNQK2XSKAwMtiNJrjsZRpTqqHOQsCgYEAwgP1Ps2uiVCYe++9F4P0DLe+A4++7kSI4aKU05IYRrHY9DKQNAMhg0Ff2fj9J9LQC7oq05nfFLVIjwqnC627R9V+lZGT9NuJzIcTh5Ne6dJy8ne3Q5z3KyyhnIxvFCVYltPpMbXpDDwRmm6ozgyulrsXAgwiO6OJC9Rj3Vh9f+kCgYEA20Y+5WisTxTs+YB9O8RQrC+kNTPZBZkI5g5rMEytc1WxYtJ1pojplCiwqBD1OFVjAi2oTV0Gnm+M9ArCMEZbYRsIeXZdQleH1wfzs/eTJgBz2yp5AZ/51I6Q0RayYWXVXBoJS3ov6OqF8EZBArVsAeaN1F+xqPWBnIT06gd++TY=</private-key>
+            </key-credential>
+            """);
+    }
+
+    @Test
+    void invalidOperationErrorXML() throws Exception {
+        final var response = invokeRequest(HttpMethod.POST,
+            "/rests/operations/netconf-keystore:invalid-operation",
+            APPLICATION_XML, """
+                <input xmlns="urn:opendaylight:netconf:keystore" />""");
+        assertErrorResponseXml(response, ErrorType.PROTOCOL, ErrorTag.DATA_MISSING);
+    }
+
+    @Test
+    void missingDataErrorXml() throws Exception {
+        final var response = invokeRequest(HttpMethod.POST,
+            ADD_KEYSTORE_ENTRY,
+            APPLICATION_XML,
+            """
+                <input xmlns="urn:opendaylight:netconf:keystore">
+                    <key-credential>
+                        <key-id>key-id</key-id>
+                        <passphrase></passphrase>
+                    </key-credential>
+                </input>""");
+        assertErrorResponseXml(response, ErrorType.PROTOCOL, ErrorTag.OPERATION_FAILED);
+    }
+
+    @Test
+    void invalidDataErrorXml() throws Exception {
+        final var response = invokeRequest(HttpMethod.POST,
+            ADD_KEYSTORE_ENTRY,
+            APPLICATION_XML,
+            """
+                <input xmlns="urn:opendaylight:netconf:keystore">
+                    <key-credential>
+                        <key-id>key-id</key-id>
+                        <private-key>bad-content</private-key>
+                        <passphrase></passphrase>
+                    </key-credential>
+                </input>""");
+        assertErrorResponseXml(response, ErrorType.PROTOCOL, ErrorTag.OPERATION_FAILED);
+    }
+
+    @Test
+    void invalidMediaTypeTest() throws Exception {
+        final var response = invokeRequest(HttpMethod.POST, ADD_KEYSTORE_ENTRY, WRONG_TYPE,"""
+                <input xmlns="urn:opendaylight:netconf:keystore" />""");
+        assertEquals(HttpResponseStatus.UNSUPPORTED_MEDIA_TYPE, response.status());
+    }
+
+    @Test
+    void invokeRpcNotRegisteredTest() throws Exception {
+        final var response = invokeRequest(HttpMethod.POST,
+            "rests/operations/netconf-node-topology:create-device",
+            APPLICATION_JSON,
+            """
+                {
+                    "input": {
+                        "login-password": {
+                            "password": "Some password",
+                            "username": "Some username"
+                        },
+                        "host": "0.0.0.0",
+                        "port": 0,
+                        "tcp-only": true,
+                        "protocol": {
+                            "name": "SSH"
+                        },
+                        "schemaless": true,
+                        "reconnect-on-changed-schema": true,
+                        "node-id": "Some node-id"
+                    }
+                }""");
+        assertErrorResponseJson(response, ErrorType.PROTOCOL, ErrorTag.DATA_MISSING);
+    }
+
+    @Test
+    void operationsOptions() throws Exception {
+        // FIXME to be fixed via https://lf-opendaylight.atlassian.net/browse/NETCONF-1210
+        // assertOptions(OPERATIONS_URI, Set.of("GET", "HEAD", "OPTIONS"));
+        assertOptions(SUBSCRIBE_DEVICE_NOTIFICATIONS_URI, Set.of("GET", "HEAD", "OPTIONS", "POST"));
+    }
+
+    @Test
+    void operationsHead() throws Exception {
+        assertHead(OPERATIONS_URI);
+    }
+}
