02bd10316babcbb445811736803a4726eb2cee76
Ivan Hrasko <ivan.hrasko@pantheon.tech>
2024-09-04 14:30:25 +0300

    End-to-end tests for Netty RESTCONF server
    Integration tests provided for Netty based RESTCONF server
    implementation. Instance of Netty endpoint with basic
    controller models is created then HTTP requests are performed
    using Netty based client from transport-http.
    
    Tests covering requests to /data (incl actions and yang-patch),
    /operations, /yang-library-version, /modules URIs plus
    /.well-known/host-meta{.json} URIs.
    
    Additional scenarios:
    - topology node mount using device simulator
    - data change notifications event stream
    - device notification event stream
    
    JIRA: NETCONF-1367
    Change-Id: Ibb9dc960cc03b1a7215cb8ff39e2fd2bd136db0e
    Signed-off-by: Ivan Hrasko <ivan.hrasko@pantheon.tech>
    Signed-off-by: Oleksandr Zharov <oleksandr.zharov@pantheon.tech>
    Signed-off-by: Matej Sramcik <matej.sramcik@pantheon.tech>
    Signed-off-by: Ruslan Kashapov <ruslan.kashapov@pantheon.tech>
    Signed-off-by: Yaroslav Lastivka <yaroslav.lastivka@pantheon.tech>

---
+++ apps/restconf-it/src/test/java/org/opendaylight/restconf/server/netty/TestUtils.java
@@ -0,0 +1,167 @@
+/*
+ * Copyright (c) 2024 PANTHEON.tech s.r.o. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.restconf.server.netty;
+
+import com.google.common.util.concurrent.FutureCallback;
+import io.netty.buffer.ByteBufUtil;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.Channel;
+import io.netty.handler.codec.http.DefaultFullHttpResponse;
+import io.netty.handler.codec.http.FullHttpResponse;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Consumer;
+import org.eclipse.jdt.annotation.NonNull;
+import org.opendaylight.aaa.encrypt.AAAEncryptionService;
+import org.opendaylight.netconf.transport.api.TransportChannel;
+import org.opendaylight.netconf.transport.api.TransportChannelListener;
+import org.opendaylight.netconf.transport.http.EventStreamListener;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.http.client.rev240208.HttpClientStackGrouping;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.http.client.rev240208.http.client.stack.grouping.Transport;
+import org.opendaylight.yangtools.binding.DataContainer;
+
+final class TestUtils {
+
+    private TestUtils() {
+        // hidden on purpose
+    }
+
+    static int freePort() {
+        // find free port
+        try {
+            final var socket = new ServerSocket(0);
+            final var localPort = socket.getLocalPort();
+            socket.close();
+            return localPort;
+        } catch (IOException e) {
+            throw new AssertionError(e);
+        }
+    }
+
+    static final class TestTransportListener implements TransportChannelListener<TransportChannel> {
+        private final Consumer<Channel> initializer;
+        private volatile boolean initialized;
+
+        TestTransportListener() {
+            this.initializer = ignored -> { };
+        }
+
+        TestTransportListener(final Consumer<Channel> initializer) {
+            this.initializer = initializer;
+        }
+
+        boolean initialized() {
+            return initialized;
+        }
+
+        @Override
+        public void onTransportChannelEstablished(final TransportChannel channel) {
+            initializer.accept(channel.channel());
+            initialized = true;
+        }
+
+        @Override
+        public void onTransportChannelFailed(final @NonNull Throwable cause) {
+            throw new IllegalStateException("HTTP connection failure", cause);
+        }
+    }
+
+    static final class TestRequestCallback implements FutureCallback<FullHttpResponse> {
+        private volatile boolean completed;
+        private volatile FullHttpResponse response;
+
+        @Override
+        public void onSuccess(final FullHttpResponse result) {
+            // detach response object from channel, so message content is not lost after client is disconnected
+            final var content = Unpooled.wrappedBuffer(ByteBufUtil.getBytes(result.content()));
+            final var copy = new DefaultFullHttpResponse(result.protocolVersion(), result.status(), content);
+            copy.headers().set(result.headers());
+            this.response = copy;
+            this.completed = true;
+        }
+
+        @Override
+        public void onFailure(final @NonNull Throwable throwable) {
+            this.completed = true;
+            throw new IllegalStateException("Failed request ", throwable);
+        }
+
+        FullHttpResponse response() {
+            return response;
+        }
+
+        boolean completed() {
+            return completed;
+        }
+    }
+
+    static final class TestEncryptionService implements AAAEncryptionService {
+        // no encryption
+        @Override
+        public byte[] encrypt(final byte[] data) {
+            return data;
+        }
+
+        // no encryption
+        @Override
+        public byte[] decrypt(final byte[] encryptedData) {
+            return encryptedData;
+        }
+    }
+
+    static final class TestEventListener implements EventStreamListener {
+        private volatile boolean started = false;
+        private volatile boolean ended = false;
+        private final BlockingQueue<String> queue = new ArrayBlockingQueue<>(5);
+
+        @Override
+        public void onStreamStart() {
+            started = true;
+        }
+
+        boolean started() {
+            return started;
+        }
+
+        @Override
+        public void onEventField(@NonNull final String fieldName, @NonNull final String fieldValue) {
+            if ("data".equals(fieldName)) {
+                queue.add(fieldValue);
+            }
+        }
+
+        String readNext() throws InterruptedException {
+            return queue.poll(5, TimeUnit.SECONDS);
+        }
+
+        @Override
+        public void onStreamEnd() {
+            ended = true;
+        }
+
+        boolean ended() {
+            return ended;
+        }
+    }
+
+    record TestClientStackGrouping(Transport clientTransport) implements HttpClientStackGrouping {
+
+        @Override
+        public @NonNull Class<? extends DataContainer> implementedInterface() {
+            return HttpClientStackGrouping.class;
+        }
+
+        @Override
+        public Transport getTransport() {
+            return clientTransport;
+        }
+    }
+}
