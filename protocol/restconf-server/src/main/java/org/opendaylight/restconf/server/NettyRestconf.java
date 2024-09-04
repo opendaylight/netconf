c83580c5d100e720b887c8c1cb1a29fdd0b545a2
Robert Varga <robert.varga@pantheon.tech>
2024-09-05 00:51:11 +0200

    Factor out NettyRestconf
    The wiring between NettyEndpoint and HTTPServer is quite wrong, as it
    conflates Secure Transport layer concerns (TransportChannel) with
    Operations layer concerns (RequestDispatcher).
    
    This patch introduces NettyRestconf, which is a
    TransportChannelListener, providing proper place for transport/messages
    integration.
    
    Change-Id: Ia1a45dfa957f737923aca5e0da3b99004fe727c3
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ protocol/restconf-server/src/main/java/org/opendaylight/restconf/server/NettyRestconf.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2024 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.restconf.server;
+
+import static java.util.Objects.requireNonNull;
+
+import org.opendaylight.netconf.transport.api.TransportChannel;
+import org.opendaylight.netconf.transport.api.TransportChannelListener;
+import org.opendaylight.netconf.transport.http.SseUtils;
+import org.opendaylight.restconf.server.api.RestconfServer;
+import org.opendaylight.restconf.server.spi.RestconfStream;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Glue between a {@link RestconfServer} and a transport stack instance.
+ */
+final class NettyRestconf implements TransportChannelListener {
+    private static final Logger LOG = LoggerFactory.getLogger(NettyRestconf.class);
+
+    private final RestconfStream.Registry streamRegistry;
+    private final NettyEndpointConfiguration configuration;
+
+    NettyRestconf(final RestconfStream.Registry streamRegistry, final NettyEndpointConfiguration configuration) {
+        this.streamRegistry = requireNonNull(streamRegistry);
+        this.configuration = requireNonNull(configuration);
+    }
+
+    @Override
+    public void onTransportChannelEstablished(final TransportChannel channel) {
+        SseUtils.enableServerSse(channel.channel(),
+            new RestconfStreamService(streamRegistry, configuration.baseUri(), configuration.errorTagMapping(),
+                configuration.defaultAcceptType(), configuration.prettyPrint()),
+            configuration.sseMaximumFragmentLength().toJava(), configuration.sseHeartbeatIntervalMillis().toJava());
+    }
+
+    @Override
+    public void onTransportChannelFailed(final Throwable cause) {
+        LOG.warn("Connection failed", cause);
+    }
+}
