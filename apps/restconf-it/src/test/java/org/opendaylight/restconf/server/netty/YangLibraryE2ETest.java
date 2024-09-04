8b78c9ca3fb4b271049afdcf4546fb06f07aaeb1
Ivan Hrasko <ivan.hrasko@pantheon.tech>
2024-09-04 14:30:25 +0300

    End-to-end tests for Netty RESTCONF server
    Integration tests provided for Netty based RESTCONF server
    implementation. Instance of Netty endpoint with basic
    controller models is created then HTTP requests are performed
    using Netty based client from transport-http.
    
    Tests covering requests to /data (incl actions and yang-patch),
    /operations, /yang-library-version, /modules URIs plus
    /.well-known/host-meta{.json} URIs.
    
    Additional scenarios:
    - topology node mount using device simulator
    - data change notifications event stream
    - device notification event stream
    
    JIRA: NETCONF-1367
    Change-Id: Ibb9dc960cc03b1a7215cb8ff39e2fd2bd136db0e
    Signed-off-by: Ivan Hrasko <ivan.hrasko@pantheon.tech>
    Signed-off-by: Oleksandr Zharov <oleksandr.zharov@pantheon.tech>
    Signed-off-by: Matej Sramcik <matej.sramcik@pantheon.tech>
    Signed-off-by: Ruslan Kashapov <ruslan.kashapov@pantheon.tech>
    Signed-off-by: Yaroslav Lastivka <yaroslav.lastivka@pantheon.tech>

---
+++ apps/restconf-it/src/test/java/org/opendaylight/restconf/server/netty/YangLibraryE2ETest.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) 2024 PANTHEON.tech s.r.o. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.restconf.server.netty;
+
+import java.util.Set;
+import org.junit.jupiter.api.Test;
+
+class YangLibraryE2ETest extends AbstractE2ETest {
+    private static final String URI = "/rests/yang-library-version";
+
+    @Test
+    void readJsonTest() throws Exception {
+        assertContentJson(URI, """
+            {
+              "ietf-restconf:yang-library-version": "2019-01-04"
+            }""");
+    }
+
+    @Test
+    void readXmlTest() throws Exception {
+        assertContentXml(URI, """
+            <ietf-restconf:yang-library-version xmlns:ietf-restconf="urn:ietf:params:xml:ns:yang:ietf-restconf">
+                2019-01-04
+            </ietf-restconf:yang-library-version>""");
+    }
+
+    @Test
+    void headTest() throws Exception {
+        assertHead(URI);
+    }
+
+    @Test
+    void optionsTest() throws Exception {
+        assertOptions(URI, Set.of("GET", "OPTIONS", "HEAD"));
+    }
+}
