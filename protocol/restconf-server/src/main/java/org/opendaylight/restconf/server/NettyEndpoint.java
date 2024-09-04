272cd3ccffe5b19cf3d6e0d5ff9bb05a2c2e0f32
Robert Varga <robert.varga@pantheon.tech>
2024-09-05 00:51:11 +0200

    Factor out RestconfTransportChannelListener
    Pipeline initialization for transport channels sits in an anonymous
    class, make it non-obvious as to what its actual role should be.
    
    Split it out into RestconfTransportChannelListener, making it
    independent of NettyEndpoint.
    
    Change-Id: Ia1a45dfa957f737923aca5e0da3b99004fe727c3
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- protocol/restconf-server/src/main/java/org/opendaylight/restconf/server/NettyEndpoint.java
+++ protocol/restconf-server/src/main/java/org/opendaylight/restconf/server/NettyEndpoint.java
@@ -11,11 +11,8 @@ import java.util.Map;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
-import org.opendaylight.netconf.transport.api.TransportChannel;
-import org.opendaylight.netconf.transport.api.TransportChannelListener;
 import org.opendaylight.netconf.transport.api.UnsupportedConfigurationException;
 import org.opendaylight.netconf.transport.http.HTTPServer;
-import org.opendaylight.netconf.transport.http.SseUtils;
 import org.opendaylight.netconf.transport.tcp.BootstrapFactory;
 import org.opendaylight.restconf.server.api.RestconfServer;
 import org.opendaylight.restconf.server.spi.RestconfStream;
@@ -23,36 +20,31 @@ import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Deactivate;
 import org.osgi.service.component.annotations.Reference;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 @Component(factory = NettyEndpoint.FACTORY_NAME, service = NettyEndpoint.class)
 public final class NettyEndpoint {
     public static final String FACTORY_NAME = "org.opendaylight.restconf.server.NettyEndpoint";
 
-    private static final Logger LOG = LoggerFactory.getLogger(NettyEndpoint.class);
     private static final String PROP_CONFIGURATION = ".configuration";
 
     private final HTTPServer httpServer;
 
     @Activate
-    public NettyEndpoint(@Reference final RestconfServer restconfService,
-            @Reference final PrincipalService principalService,
+    public NettyEndpoint(@Reference final RestconfServer server, @Reference final PrincipalService principalService,
             @Reference final RestconfStream.Registry streamRegistry, final Map<String, ?> props) {
-        this(restconfService, principalService, streamRegistry,
-            (NettyEndpointConfiguration) props.get(PROP_CONFIGURATION));
+        this(server, principalService, streamRegistry, (NettyEndpointConfiguration) props.get(PROP_CONFIGURATION));
     }
 
-    public NettyEndpoint(final RestconfServer restconfService, final PrincipalService principalService,
-            final RestconfStream.Registry streamRegistry, final NettyEndpointConfiguration config) {
-        final var bootstrapFactory = new BootstrapFactory(config.groupName(), config.groupThreads());
-        final var dispatcher = new RestconfRequestDispatcher(restconfService, principalService,
-            config.baseUri(), config.errorTagMapping(), config.defaultAcceptType(), config.prettyPrint());
-        final var overlayListener = buildSseOverlayListener(streamRegistry, config);
+    public NettyEndpoint(final RestconfServer server, final PrincipalService principalService,
+            final RestconfStream.Registry streamRegistry, final NettyEndpointConfiguration configuration) {
+        final var listener = new RestconfTransportChannelListener(streamRegistry, configuration);
+        final var dispatcher = new RestconfRequestDispatcher(server, principalService, configuration.baseUri(),
+            configuration.errorTagMapping(), configuration.defaultAcceptType(), configuration.prettyPrint());
 
+        final var bootstrapFactory = new BootstrapFactory(configuration.groupName(), configuration.groupThreads());
         try {
-            httpServer = HTTPServer.listen(overlayListener, bootstrapFactory.newServerBootstrap(),
-                config.transportConfiguration(), dispatcher, principalService).get();
+            httpServer = HTTPServer.listen(listener, bootstrapFactory.newServerBootstrap(),
+                configuration.transportConfiguration(), dispatcher, principalService).get();
         } catch (UnsupportedConfigurationException | ExecutionException | InterruptedException e) {
             throw new IllegalStateException("Could not start RESTCONF server", e);
         }
@@ -69,24 +61,5 @@ public final class NettyEndpoint {
 
     public static Map<String, ?> props(final NettyEndpointConfiguration configuration) {
         return Map.of(PROP_CONFIGURATION, configuration);
-    }
-
-    private static TransportChannelListener buildSseOverlayListener(final RestconfStream.Registry streamRegistry,
-            final NettyEndpointConfiguration configuration) {
-        return new TransportChannelListener() {
-            @Override
-            public void onTransportChannelEstablished(final TransportChannel channel) {
-                SseUtils.enableServerSse(channel.channel(),
-                    new RestconfStreamService(streamRegistry, configuration.baseUri(), configuration.errorTagMapping(),
-                        configuration.defaultAcceptType(), configuration.prettyPrint()),
-                    configuration.sseMaximumFragmentLength().toJava(),
-                    configuration.sseHeartbeatIntervalMillis().toJava());
-            }
-
-            @Override
-            public void onTransportChannelFailed(final Throwable cause) {
-                LOG.warn("Connection failed", cause);
-            }
-        };
     }
 }
