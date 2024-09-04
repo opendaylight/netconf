681cbaf9d2565468d5e970acbca17d5e35e48474
Ivan Hrasko <ivan.hrasko@pantheon.tech>
2024-09-04 14:30:25 +0300

    WIP: Add end-to-end tests for Netty restconf
    Add end-to-end tests for Netty based restconf implementation.
    
    Creates a Netty RESTCONF server with default controller models,
    which are accessed by test client.
    
    Client is sending requests for /data, /host-meta, /modules,
    /operations, /yang-library-version endpoints.
    
    It also tests subscription served by restconf-state/streams.
    
    FIXME: do we need to initialize datastore? It seems that its
    first time initialized with null model context.
    
    JIRA: NETCONF-1367
    Change-Id: Ibb9dc960cc03b1a7215cb8ff39e2fd2bd136db0e
    Signed-off-by: Ivan Hrasko <ivan.hrasko@pantheon.tech>
    Signed-off-by: Ruslan Kashapov <ruslan.kashapov@pantheon.tech>

---
+++ apps/restconf-it/src/test/java/org/opendaylight/restconf/server/netty/ModulesE2ETest.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright (c) 2024 PANTHEON.tech s.r.o. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.restconf.server.netty;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.opendaylight.yangtools.yang.common.YangConstants.RFC6020_YANG_MEDIA_TYPE;
+import static org.opendaylight.yangtools.yang.common.YangConstants.RFC6020_YIN_MEDIA_TYPE;
+
+import io.netty.handler.codec.http.HttpHeaderNames;
+import io.netty.handler.codec.http.HttpMethod;
+import io.netty.handler.codec.http.HttpResponseStatus;
+import java.nio.charset.StandardCharsets;
+import java.util.Set;
+import org.junit.jupiter.api.Test;
+
+class ModulesE2ETest extends AbstractE2ETest {
+    private static final String MODULE_URI = "/rests/modules/network-topology?revision=2013-10-21";
+
+    @Test
+    void options() throws Exception {
+        assertOptions(MODULE_URI, Set.of("GET", "HEAD", "OPTIONS"));
+    }
+
+    @Test
+    void head() throws Exception {
+        assertHead(MODULE_URI, RFC6020_YIN_MEDIA_TYPE);
+        assertHead(MODULE_URI, RFC6020_YANG_MEDIA_TYPE);
+    }
+
+    @Test
+    void readYin() throws Exception {
+        final var response = invokeRequest(HttpMethod.GET, MODULE_URI, RFC6020_YIN_MEDIA_TYPE);
+        assertEquals(HttpResponseStatus.OK, response.status());
+        assertEquals(RFC6020_YIN_MEDIA_TYPE, response.headers().get(HttpHeaderNames.CONTENT_TYPE));
+        final var content = response.content().toString(StandardCharsets.UTF_8);
+        // simplified content validation
+        assertTrue(content.contains(
+            "<module xmlns=\"urn:ietf:params:xml:ns:yang:yin:1\" name=\"network-topology\""));
+    }
+
+    @Test
+    void readYang() throws Exception {
+        final var response = invokeRequest(HttpMethod.GET, MODULE_URI, RFC6020_YANG_MEDIA_TYPE);
+        assertEquals(HttpResponseStatus.OK, response.status());
+        assertEquals(RFC6020_YANG_MEDIA_TYPE, response.headers().get(HttpHeaderNames.CONTENT_TYPE));
+        final var content = response.content().toString(StandardCharsets.UTF_8);
+        // simplified content validation
+        assertTrue(content.startsWith("module network-topology"));
+    }
+}
