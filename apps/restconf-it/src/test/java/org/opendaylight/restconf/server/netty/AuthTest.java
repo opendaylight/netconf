6d0e28255bb416d0669db82de0679583655aea67
Ivan Hrasko <ivan.hrasko@pantheon.tech>
2024-09-04 14:30:25 +0300

    End-to-end tests for Netty RESTCONF server
    Integration tests provided for Netty based RESTCONF server
    implementation. Instance of Netty endpoint with basic
    controller models is created then HTTP requests are performed
    using Netty based client from transport-http.
    
    Tests covering requests to /data (incl actions and yang-patch),
    /operations, /yang-library-version, /modules URIs plus
    /.well-known/host-meta{.json} URIs.
    
    Additional scenarios:
    - topology node mount using device simulator
    - data change notifications event stream
    - device notification event stream
    
    JIRA: NETCONF-1367
    Change-Id: Ibb9dc960cc03b1a7215cb8ff39e2fd2bd136db0e
    Signed-off-by: Ivan Hrasko <ivan.hrasko@pantheon.tech>
    Signed-off-by: Oleksandr Zharov <oleksandr.zharov@pantheon.tech>
    Signed-off-by: Matej Sramcik <matej.sramcik@pantheon.tech>
    Signed-off-by: Ruslan Kashapov <ruslan.kashapov@pantheon.tech>
    Signed-off-by: Yaroslav Lastivka <yaroslav.lastivka@pantheon.tech>

---
+++ apps/restconf-it/src/test/java/org/opendaylight/restconf/server/netty/AuthTest.java
@@ -0,0 +1,92 @@
+/*
+ * Copyright (c) 2024 PANTHEON.tech s.r.o. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.restconf.server.netty;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import io.netty.handler.codec.http.FullHttpResponse;
+import io.netty.handler.codec.http.HttpMethod;
+import io.netty.handler.codec.http.HttpResponseStatus;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import org.json.JSONObject;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+
+class AuthTest extends AbstractE2ETest {
+
+    private static List<String> targets() {
+        return List.of("/rests/data",
+            "/rests/operations",
+            "/rests/yang-library-version",
+            "/.well-known/host-meta",
+            "/rests/modules/network-topology?revision=2013-10-21");
+    }
+
+    @ParameterizedTest
+    @MethodSource("targets")
+    void authorized(final String uri) throws Exception {
+        final var response = invokeRequest(HttpMethod.GET, uri);
+        assertResponse(response, HttpResponseStatus.OK);
+    }
+
+    @ParameterizedTest
+    @MethodSource("targets")
+    void unauthorized(final String uri) throws Exception {
+        final var request = buildRequest(HttpMethod.GET, uri, APPLICATION_JSON, null, null);
+        final var response = invokeRequest(request, invalidClientStackGrouping);
+        assertResponse(response, HttpResponseStatus.UNAUTHORIZED);
+    }
+
+    @Test
+    void testStreamAuthorized() throws Exception {
+        final var response = invokeRequest(HttpMethod.GET,
+            "/rests/data/ietf-restconf-monitoring:restconf-state/streams/stream=" + createStream());
+        assertResponse(response, HttpResponseStatus.OK);
+    }
+
+    @Test
+    void testStreamUnauthorized() throws Exception {
+        final var stream = createStream();
+        final var request = buildRequest(HttpMethod.GET,
+            "/rests/data/ietf-restconf-monitoring:restconf-state/streams/stream=" + stream,
+            APPLICATION_JSON, null, null);
+        final var response = invokeRequest(request, invalidClientStackGrouping);
+        assertResponse(response, HttpResponseStatus.UNAUTHORIZED);
+    }
+
+    private String createStream() throws Exception {
+        final var response = invokeRequest(HttpMethod.POST,
+            "/rests/operations/sal-remote:create-data-change-event-subscription",
+            APPLICATION_JSON,
+            """
+                {
+                    "input": {
+                        "path": "/example-jukebox:jukebox/library/artist[name='artist']/album[name='album']",
+                        "sal-remote-augment:datastore": "CONFIGURATION",
+                        "sal-remote-augment:scope": "ONE"
+                    }
+                }
+                """);
+        assertEquals(HttpResponseStatus.OK, response.status());
+        return extractStreamNameJson(response.content().toString(StandardCharsets.UTF_8));
+    }
+
+    private static String extractStreamNameJson(final String content) {
+        final var json = new JSONObject(content);
+        return json.getJSONObject("sal-remote:output").getString("stream-name");
+    }
+
+    private static void assertResponse(final FullHttpResponse response, final HttpResponseStatus expectedStatus) {
+        assertNotNull(response);
+        assertEquals(expectedStatus, response.status());
+    }
+}
