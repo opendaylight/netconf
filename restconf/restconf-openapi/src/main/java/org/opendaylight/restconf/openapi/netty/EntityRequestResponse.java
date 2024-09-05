01830d02bb73f1adc09d380bddf5bbe89f989637
Ruslan Kashapov <ruslan.kashapov@pantheon.tech>
2024-09-05 10:06:43 +0300

    Netty endpoint for RESTCONF OpenApi
    Introduce OpenApiResourceProvider, which is registers to each
    NettyEndpoint's WebHostResourceProvider whiteboard.
    
    Each NettyEndpoint then drives drives instantiation of its own
    WebHostResourceInstance for each registered provider.
    
    JIRA: NETCONF-1370
    Change-Id: I4005f3bd1fe238672cfdee465474d625a628a561
    Signed-off-by: Ruslan Kashapov <ruslan.kashapov@pantheon.tech>
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>
    Signed-off-by: Ivan Hrasko <ivan.hrasko@pantheon.tech>

---
+++ restconf/restconf-openapi/src/main/java/org/opendaylight/restconf/openapi/netty/EntityRequestResponse.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright (c) 2024 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.restconf.openapi.netty;
+
+import static java.util.Objects.requireNonNull;
+
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.core.JsonGenerator;
+import io.netty.buffer.ByteBuf;
+import io.netty.handler.codec.http.DefaultFullHttpResponse;
+import io.netty.handler.codec.http.FullHttpResponse;
+import io.netty.handler.codec.http.HttpHeaderNames;
+import io.netty.handler.codec.http.HttpHeaderValues;
+import io.netty.handler.codec.http.HttpResponseStatus;
+import io.netty.handler.codec.http.HttpVersion;
+import java.io.IOException;
+import java.io.OutputStream;
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.opendaylight.netconf.transport.http.ByteStreamRequestResponse;
+import org.opendaylight.restconf.openapi.model.OpenApiEntity;
+
+@NonNullByDefault
+final class EntityRequestResponse extends ByteStreamRequestResponse {
+    private static final JsonFactory FACTORY = JsonFactory.builder().build();
+
+    private final OpenApiEntity entity;
+
+    EntityRequestResponse(final OpenApiEntity entity) {
+        super(HttpResponseStatus.OK, null);
+        this.entity = requireNonNull(entity);
+    }
+
+    @Override
+    protected FullHttpResponse toHttpResponse(final HttpVersion version, final ByteBuf content) {
+        final var response = new DefaultFullHttpResponse(version, status, content);
+        response.headers()
+            .add(HttpHeaderNames.CONTENT_TYPE, HttpHeaderValues.APPLICATION_JSON)
+            .add(HttpHeaderNames.CONTENT_LENGTH, content.readableBytes());
+        return response;
+    }
+
+    @Override
+    protected void writeBody(final OutputStream out) throws IOException {
+        final var generator = FACTORY.createGenerator(out);
+        entity.generate(generator);
+        generator.flush();
+    }
+}
---
+++ restconf/restconf-openapi/src/main/java/org/opendaylight/restconf/openapi/netty/OpenApiResourceInstance.java
@@ -0,0 +1,336 @@
+/*
+ * Copyright (c) 2024 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.restconf.openapi.netty;
+
+import static java.util.Objects.requireNonNull;
+
+import io.netty.handler.codec.http.DefaultHttpHeadersFactory;
+import io.netty.handler.codec.http.HttpHeaderNames;
+import io.netty.handler.codec.http.HttpHeaderValues;
+import io.netty.handler.codec.http.HttpHeaders;
+import io.netty.handler.codec.http.HttpResponseStatus;
+import io.netty.handler.codec.http.QueryStringDecoder;
+import java.io.IOException;
+import java.net.URI;
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.opendaylight.netconf.transport.http.EmptyRequestResponse;
+import org.opendaylight.netconf.transport.http.ExceptionRequestResponse;
+import org.opendaylight.netconf.transport.http.ImplementedMethod;
+import org.opendaylight.netconf.transport.http.LinkRelation;
+import org.opendaylight.netconf.transport.http.RequestResponse;
+import org.opendaylight.netconf.transport.http.SegmentPeeler;
+import org.opendaylight.netconf.transport.http.rfc6415.WebHostResourceInstance;
+import org.opendaylight.netconf.transport.http.rfc6415.XRD;
+import org.opendaylight.restconf.api.QueryParameters;
+import org.opendaylight.restconf.openapi.api.OpenApiService;
+import org.opendaylight.restconf.openapi.model.DocumentEntity;
+import org.opendaylight.restconf.openapi.model.MetadataEntity;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Main OpenAPI resource. Deals with dispatching HTTP requests to individual sub-resources as needed.
+ */
+@NonNullByDefault
+final class OpenApiResourceInstance extends WebHostResourceInstance {
+    private static final Logger LOG = LoggerFactory.getLogger(OpenApiResourceInstance.class);
+    private static final EmptyRequestResponse OPTIONS_ONLY_METHOD_NOT_ALLOWED;
+    private static final EmptyRequestResponse OPTIONS_ONLY_OK;
+
+    static {
+        final var headers = DefaultHttpHeadersFactory.headersFactory().newHeaders()
+            .set(HttpHeaderNames.ALLOW, "OPTIONS");
+        OPTIONS_ONLY_METHOD_NOT_ALLOWED = new EmptyRequestResponse(HttpResponseStatus.METHOD_NOT_ALLOWED, headers);
+        OPTIONS_ONLY_OK = new EmptyRequestResponse(HttpResponseStatus.OK, headers);
+    }
+
+    private static final EmptyRequestResponse GHO_METHOD_NOT_ALLOWED;
+    private static final EmptyRequestResponse GHO_OK;
+
+    static {
+        final var headers = DefaultHttpHeadersFactory.headersFactory().newEmptyHeaders()
+            .set(HttpHeaderNames.ALLOW, "GET, HEAD, OPTIONS");
+        GHO_METHOD_NOT_ALLOWED = new EmptyRequestResponse(HttpResponseStatus.METHOD_NOT_ALLOWED, headers);
+        GHO_OK = new EmptyRequestResponse(HttpResponseStatus.OK, headers);
+    }
+
+    private static final EmptyRequestResponse JSON_OK = new EmptyRequestResponse(HttpResponseStatus.OK,
+        DefaultHttpHeadersFactory.headersFactory().newEmptyHeaders()
+            .set(HttpHeaderNames.CONTENT_TYPE, HttpHeaderValues.APPLICATION_JSON));
+
+    private final OpenApiService service;
+
+    OpenApiResourceInstance(final String path, final OpenApiService service) {
+        super(path);
+        this.service = requireNonNull(service);
+    }
+
+    @Override
+    protected void removeRegistration() {
+        // No-op
+    }
+
+    @Override
+    public RequestResponse prepare(final ImplementedMethod method, final URI targetUri, final HttpHeaders headers,
+            final SegmentPeeler peeler, final XRD xrd) {
+        if (!peeler.hasNext()) {
+            return optionsOnlyResponse(method);
+        }
+
+        final var segment = peeler.next();
+        return switch (segment) {
+            case "api" -> api(method, targetUri, headers, peeler, xrd);
+            case "explorer" -> explorer(method, targetUri, headers, peeler);
+            case "ui" -> ui(method, peeler);
+            default -> EmptyRequestResponse.NOT_FOUND;
+        };
+    }
+
+    // the /api resource
+    private RequestResponse api(final ImplementedMethod method, final URI targetUri, final HttpHeaders headers,
+            final SegmentPeeler peeler, final XRD xrd) {
+        final var restconf = xrd.lookupLink(LinkRelation.RESTCONF);
+        if (restconf == null) {
+            return EmptyRequestResponse.NOT_FOUND;
+        }
+        if (!peeler.hasNext()) {
+            return optionsOnlyResponse(method);
+        }
+
+        final var first = peeler.next();
+        if (!first.equals("v3")) {
+            return EmptyRequestResponse.NOT_FOUND;
+        }
+
+        if (!peeler.hasNext()) {
+            return optionsOnlyResponse(method);
+        }
+
+        final var next = peeler.next();
+        return switch (next) {
+            case "mounts" -> peeler.hasNext() ? apiMounts(method, targetUri, headers, peeler) : switch (method) {
+                case GET -> new EntityRequestResponse(service.getListOfMounts());
+                case HEAD -> JSON_OK;
+                case OPTIONS -> GHO_OK;
+                default -> GHO_METHOD_NOT_ALLOWED;
+            };
+            case "single" -> single(method, targetUri, headers, peeler);
+            case "ui" -> new EmptyRequestResponse(HttpResponseStatus.SEE_OTHER,
+                DefaultHttpHeadersFactory.headersFactory().newEmptyHeaders()
+                    .set(HttpHeaderNames.LOCATION, "../../../explorer" + peeler.remaining()));
+            default -> peeler.hasNext() ? EmptyRequestResponse.NOT_FOUND : switch (method) {
+                case GET -> apiModule(targetUri, headers, next, true);
+                case HEAD -> apiModule(targetUri, headers, next, false);
+                case OPTIONS -> GHO_OK;
+                default -> GHO_METHOD_NOT_ALLOWED;
+            };
+        };
+    }
+
+    private RequestResponse apiModule(final URI targetUri, final HttpHeaders headers, final String module,
+            final boolean withContent) {
+        final var params = queryParams(targetUri);
+        final var revision = params.lookup("revision");
+        final var width = params.lookup("width", Integer::valueOf);
+        final var depth = params.lookup("depth", Integer::valueOf);
+
+        final DocumentEntity entity;
+        try {
+            entity = service.getDocByModule(module, revision, targetUri, width, depth);
+        } catch (IOException e) {
+            return new ExceptionRequestResponse(e);
+        }
+        return withContent ? new EntityRequestResponse(entity) : JSON_OK;
+    }
+
+    private RequestResponse apiMounts(final ImplementedMethod method, final URI targetUri, final HttpHeaders headers,
+            final SegmentPeeler peeler) {
+        final var instanceStr = peeler.next();
+        final long instance;
+        try {
+            instance = Long.parseLong(instanceStr);
+        } catch (NumberFormatException e) {
+            return new ExceptionRequestResponse(e);
+        }
+
+        if (!peeler.hasNext()) {
+            return switch (method) {
+                case GET -> apiMount(targetUri, headers, instance, true);
+                case HEAD -> apiMount(targetUri, headers, instance, false);
+                case OPTIONS -> GHO_OK;
+                default -> GHO_METHOD_NOT_ALLOWED;
+            };
+        }
+
+        final var next = peeler.next();
+        return peeler.hasNext() ? EmptyRequestResponse.NOT_FOUND : switch (next) {
+            case "meta" -> prepareMountsInstanceMeta(method, targetUri, headers, instance);
+            default -> prepareMountsInstanceModule(method, targetUri, headers, instance, next);
+        };
+    }
+
+    private RequestResponse apiMount(final URI targetUri, final HttpHeaders headers, final long instance,
+            final boolean withContent) {
+        final var params = queryParams(targetUri);
+        final var width = params.lookup("width", Integer::valueOf);
+        final var depth = params.lookup("depth", Integer::valueOf);
+        final var offset = params.lookup("offset", Integer::valueOf);
+        final var limit = params.lookup("limit", Integer::valueOf);
+
+        final DocumentEntity entity;
+        try {
+            entity = service.getMountDoc(instance, targetUri, width, depth, offset, limit);
+        } catch (IOException e) {
+            return new ExceptionRequestResponse(e);
+        }
+        return withContent ? new EntityRequestResponse(entity) : JSON_OK;
+    }
+
+    private RequestResponse prepareMountsInstanceMeta(final ImplementedMethod method, final URI targetUri,
+            final HttpHeaders headers, final long instance) {
+        return switch (method) {
+            case GET -> prepareMountsInstanceMetaGet(targetUri, headers, instance, true);
+            case HEAD -> prepareMountsInstanceMetaGet(targetUri, headers, instance, false);
+            case OPTIONS -> GHO_OK;
+            default -> GHO_METHOD_NOT_ALLOWED;
+        };
+    }
+
+    private RequestResponse prepareMountsInstanceMetaGet(final URI targetUri, final HttpHeaders headers,
+            final long instance, final boolean withContent) {
+        final var params = queryParams(targetUri);
+        final var offset = params.lookup("offset", Integer::valueOf);
+        final var limit = params.lookup("limit", Integer::valueOf);
+
+        final MetadataEntity entity;
+        try {
+            entity = service.getMountMeta(instance, offset, limit);
+        } catch (IOException e) {
+            return new ExceptionRequestResponse(e);
+        }
+        return withContent ? new EntityRequestResponse(entity) : JSON_OK;
+    }
+
+    private RequestResponse prepareMountsInstanceModule(final ImplementedMethod method, final URI targetUri,
+            final HttpHeaders headers, final long instance, final String module) {
+        return switch (method) {
+            case GET -> prepareMountsInstanceModuleGet(targetUri, headers, instance, module, true);
+            case HEAD -> prepareMountsInstanceModuleGet(targetUri, headers, instance, module, false);
+            case OPTIONS -> GHO_OK;
+            default -> GHO_METHOD_NOT_ALLOWED;
+        };
+    }
+
+    private RequestResponse prepareMountsInstanceModuleGet(final URI targetUri, final HttpHeaders headers,
+            final long instance, final String module, final boolean withContent) {
+        final var params = queryParams(targetUri);
+        final var revision = params.lookup("revision");
+        final var width = params.lookup("width", Integer::valueOf);
+        final var depth = params.lookup("depth", Integer::valueOf);
+
+        final DocumentEntity entity;
+        try {
+            entity = service.getMountDocByModule(instance, module, revision, targetUri, width, depth);
+        } catch (IOException e) {
+            return new ExceptionRequestResponse(e);
+        }
+        return withContent ? new EntityRequestResponse(entity) : JSON_OK;
+    }
+
+    private RequestResponse single(final ImplementedMethod method, final URI targetUri, final HttpHeaders headers,
+            final SegmentPeeler peeler) {
+        if (!peeler.hasNext()) {
+            return switch (method) {
+                case GET -> single(targetUri, headers, true);
+                case HEAD -> single(targetUri, headers, false);
+                case OPTIONS -> GHO_OK;
+                default -> GHO_METHOD_NOT_ALLOWED;
+            };
+        }
+        return !peeler.next().equals("meta") || peeler.hasNext() ? EmptyRequestResponse.NOT_FOUND : switch (method) {
+            case GET -> singleMeta(targetUri, headers, true);
+            case HEAD -> singleMeta(targetUri, headers, false);
+            case OPTIONS -> GHO_OK;
+            default -> GHO_METHOD_NOT_ALLOWED;
+        };
+    }
+
+    private RequestResponse single(final URI targetUri, final HttpHeaders headers, final boolean withContent) {
+        final var params = queryParams(targetUri);
+        final var width = params.lookup("width", Integer::valueOf);
+        final var depth = params.lookup("depth", Integer::valueOf);
+        final var offset = params.lookup("offset", Integer::valueOf);
+        final var limit = params.lookup("limit", Integer::valueOf);
+
+        final DocumentEntity entity;
+        try {
+            entity = service.getAllModulesDoc(targetUri, width, depth, offset, limit);
+        } catch (IOException e) {
+            return new ExceptionRequestResponse(e);
+        }
+        return withContent ? new EntityRequestResponse(entity) : JSON_OK;
+    }
+
+    private RequestResponse singleMeta(final URI targetUri, final HttpHeaders headers,
+            final boolean withContent) {
+        final var params = queryParams(targetUri);
+        final var offset = params.lookup("offset", Integer::valueOf);
+        final var limit = params.lookup("limit", Integer::valueOf);
+
+        final MetadataEntity entity;
+        try {
+            entity = service.getAllModulesMeta(offset, limit);
+        } catch (IOException e) {
+            return new ExceptionRequestResponse(e);
+        }
+        return withContent ? new EntityRequestResponse(entity) : JSON_OK;
+    }
+
+    // the /explorer resource
+    private static RequestResponse explorer(final ImplementedMethod method, final URI targetUri,
+            final HttpHeaders headers, final SegmentPeeler peeler) {
+        var requested = QueryStringDecoder.decodeComponent(peeler.remaining());
+        if (requested.isEmpty() || requested.equals("/")) {
+            requested = "/index.html";
+            LOG.debug("Adjusted request to {}", requested);
+        }
+
+        final var resourceName = "/explorer" + requested;
+        final var resource = OpenApiResourceInstance.class.getResource(resourceName);
+        if (resource == null) {
+            LOG.debug("Resource '{}' not found", resourceName);
+            return EmptyRequestResponse.NOT_FOUND;
+        }
+
+        LOG.debug("Found resource {} for requested {}", resource, requested);
+        return switch (method) {
+            case GET -> new URLRequestResponse(resource, true);
+            case HEAD -> new URLRequestResponse(resource, false);
+            case OPTIONS -> GHO_OK;
+            default -> GHO_METHOD_NOT_ALLOWED;
+        };
+    }
+
+    // the /ui resource.
+    private static RequestResponse ui(final ImplementedMethod method, final SegmentPeeler peeler) {
+        return switch (method) {
+            case GET, HEAD, OPTIONS -> new EmptyRequestResponse(HttpResponseStatus.SEE_OTHER,
+                DefaultHttpHeadersFactory.headersFactory().newEmptyHeaders()
+                    .set(HttpHeaderNames.LOCATION, "../explorer" + peeler.remaining()));
+            default -> GHO_METHOD_NOT_ALLOWED;
+        };
+    }
+
+    private static EmptyRequestResponse optionsOnlyResponse(final ImplementedMethod method) {
+        return method == ImplementedMethod.OPTIONS ? OPTIONS_ONLY_OK :  OPTIONS_ONLY_METHOD_NOT_ALLOWED;
+    }
+
+    private static QueryParameters queryParams(final URI targetUri) {
+        return QueryParameters.ofMultiValue(new QueryStringDecoder(targetUri).parameters());
+    }
+}
