d45836fca622b4ababae2dbf9ae224fafe4c74d0
Ruslan Kashapov <ruslan.kashapov@pantheon.tech>
2024-09-06 13:56:07 +0300

    Fix cleartext HTTP/2 upgrade
    HttpServerUpgradeHandler is a HttpObjectAggregator, therefore we need to
    pass a consistent maximum request size -- otherwise it will reject the
    first HTTP/1 request.
    
    The second part of the puzzle is intercepting the first request: for
    this we create AbstractServerHandler, manually triggering
    channelRead0() to handle it.
    
    JIRA: NETCONF-1378
    Change-Id: Ibeb85fe84e645411b4ac634649335f82189f0dba
    Signed-off-by: Ruslan Kashapov <ruslan.kashapov@pantheon.tech>
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ transport/transport-http/src/main/java/org/opendaylight/netconf/transport/http/AbstractServerHandler.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2024 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.netconf.transport.http;
+
+import com.google.common.annotations.VisibleForTesting;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.handler.codec.http.FullHttpRequest;
+import io.netty.handler.codec.http.HttpServerUpgradeHandler;
+import io.netty.handler.codec.http2.Http2CodecUtil;
+import io.netty.handler.codec.http2.HttpConversionUtil.ExtensionHeaderNames;
+
+/**
+ * Abstract base class for end-of-pipeline {@link HTTPServer} handlers. It is exposed for testing purposes and hence it
+ * is non-sealed. Were it not for tests, this class would be integrated directly into {@link HTTPServerSession}.
+ */
+@VisibleForTesting
+abstract class AbstractServerHandler extends SimpleChannelInboundHandler<FullHttpRequest> {
+    AbstractServerHandler() {
+        super(FullHttpRequest.class, false);
+    }
+
+    @Override
+    public final void userEventTriggered(final ChannelHandlerContext ctx, final Object event) throws Exception {
+        // We may be involved in a cleartext upgrade to HTTP/2, in which case we need to propagate the first event
+        // manually.
+        if (event instanceof HttpServerUpgradeHandler.UpgradeEvent upgrade) {
+            final var request = upgrade.upgradeRequest();
+            request.headers().setInt(ExtensionHeaderNames.STREAM_ID.text(), Http2CodecUtil.HTTP_UPGRADE_STREAM_ID);
+            channelRead0(ctx, request.retain());
+        }
+        super.userEventTriggered(ctx, event);
+    }
+}
--- transport/transport-http/src/test/java/org/opendaylight/netconf/transport/http/HttpClientServerTest.java
+++ transport/transport-http/src/test/java/org/opendaylight/netconf/transport/http/HttpClientServerTest.java
@@ -31,12 +31,17 @@ import static org.opendaylight.netconf.transport.http.TestUtils.generateX509Cert
 import static org.opendaylight.netconf.transport.http.TestUtils.invoke;
 
 import io.netty.channel.ChannelHandlerContext;
-import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.handler.codec.http.DefaultFullHttpRequest;
 import io.netty.handler.codec.http.DefaultFullHttpResponse;
 import io.netty.handler.codec.http.FullHttpRequest;
 import io.netty.handler.codec.http.HttpMethod;
+import java.net.Authenticator;
 import java.net.InetAddress;
+import java.net.PasswordAuthentication;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.nio.charset.StandardCharsets;
 import java.util.Map;
 import java.util.concurrent.Executors;
@@ -104,7 +109,7 @@ class HttpClientServerTest {
     void beforeEach() {
         doAnswer(inv -> {
             inv.<TransportChannel>getArgument(0).channel().pipeline()
-                .addLast(new SimpleChannelInboundHandler<>(FullHttpRequest.class) {
+                .addLast(new AbstractServerHandler() {
                     @Override
                     protected void channelRead0(final ChannelHandlerContext ctx, final FullHttpRequest msg) {
                         // return 200 response with a content built from request parameters
@@ -226,6 +231,50 @@ class HttpClientServerTest {
                 }
             } finally {
                 client.shutdown().get(2, TimeUnit.SECONDS);
+            }
+        } finally {
+            server.shutdown().get(2, TimeUnit.SECONDS);
+        }
+    }
+
+    @ParameterizedTest(name = "Java HttpClient compatibility check, Basic Auth: {0}")
+    @ValueSource(booleans = {false, true})
+    void cleartextUpgradeFlowCompatibility(final boolean withAuth) throws Exception {
+        // validate server cleartext protocol upgrade flow being compatible with java.net.HttpClient
+        final var localPort = freePort();
+        final var transport = withAuth
+            ? serverTransportTcp(localAddress, localPort, USER_HASHES_MAP)
+            : serverTransportTcp(localAddress, localPort);
+        doReturn(transport).when(serverConfig).getTransport();
+
+        final var uri = nextValue("URI");
+        final var payload = nextValue("PAYLOAD");
+        final var url = URI.create("http://%s:%d/%s".formatted(localAddress, localPort, uri));
+
+        final var server = HTTPServer.listen(serverTransportListener, bootstrapFactory.newServerBootstrap(),
+            serverConfig).get(2, TimeUnit.SECONDS);
+        try {
+            try (var client = HttpClient.newBuilder()
+                // authenticator is only used if only 401 returned
+                .authenticator(new Authenticator() {
+                    @Override
+                    protected PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication(USERNAME, PASSWORD.toCharArray());
+                    }
+                })
+                .build()) {
+
+                final var request = HttpRequest.newBuilder(url)
+                    .method("PATCH", HttpRequest.BodyPublishers.ofString(payload, StandardCharsets.UTF_8))
+                    .header("Content-Type", "text-plain")
+                    .header("Accept", "text-plain")
+                    .build();
+                final var response = client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
+                    .toCompletableFuture().get(2, TimeUnit.SECONDS);
+                assertNotNull(response);
+                assertEquals(200, response.statusCode());
+                final var expected = RESPONSE_TEMPLATE.formatted("PATCH", "/" + uri, payload);
+                assertEquals(expected, response.body());
             }
         } finally {
             server.shutdown().get(2, TimeUnit.SECONDS);
