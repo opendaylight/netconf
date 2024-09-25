6910745f7c1bfc992c392fc4e00bf22ed0a284d5
Robert Varga <robert.varga@pantheon.tech>
2024-09-25 18:39:57 +0200

    Reconstruct Target URI, part two
    Reconstructing Target URI from its usual origin-form is a tad more
    involved:
    
    1. we need access to the URI scheme being used, which we now have
       conveniently available from HTTPTransportChannel
    2. we need to ensure we have a Host header, that it contains only one
       value and that it is a valid authority
    
    We employ java.net.URI for most of the parsing, combining and error
    reporting functions. It also allows us to determine in the uri is in
    absolute-form, in which case we can pick it up as is.
    
    We use this URI to initialize QueryStringDecoder, so that its uri()
    method reports the complete thing.
    
    JIRA: NETCONF-1379
    Change-Id: I6641a613f2fd4c63332ae29b0a876ca32a325a03
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- protocol/restconf-server/src/test/java/org/opendaylight/restconf/server/RestconfSessionTest.java
+++ protocol/restconf-server/src/test/java/org/opendaylight/restconf/server/RestconfSessionTest.java
@@ -8,13 +8,17 @@
 package org.opendaylight.restconf.server;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import io.netty.buffer.Unpooled;
 import io.netty.handler.codec.http.DefaultHttpHeaders;
 import io.netty.handler.codec.http.HttpHeaderNames;
 import io.netty.handler.codec.http.HttpMethod;
 import io.netty.handler.codec.http.HttpResponseStatus;
+import io.netty.handler.codec.http.HttpScheme;
 import io.netty.handler.codec.http.HttpVersion;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -50,5 +54,38 @@ class RestconfSessionTest {
             Arguments.of(HttpMethod.PATCH),
             Arguments.of(HttpMethod.POST),
             Arguments.of(HttpMethod.PUT));
+    }
+
+    @ParameterizedTest
+    @MethodSource
+    void hostUriOfValid(final String expected, final HttpScheme scheme, final String host) throws Exception {
+        assertEquals(URI.create(expected), RestconfSession.hostUriOf(scheme, host));
+    }
+
+    private static List<Arguments> hostUriOfValid() {
+        return List.of(
+            Arguments.of("http://foo", HttpScheme.HTTP, "foo"),
+            Arguments.of("https://bar:1234", HttpScheme.HTTPS, "bar:1234"));
+    }
+
+    @Test
+    void hostUriOfInvalidPort() {
+        final var ex = assertThrows(URISyntaxException.class,
+            () -> RestconfSession.hostUriOf(HttpScheme.HTTP, "foo:abc"));
+        assertEquals("Illegal character in port number at index 11: http://foo:abc", ex.getMessage());
+    }
+
+    @Test
+    void hostUriOfInvalidHostname() {
+        final var ex = assertThrows(URISyntaxException.class,
+            () -> RestconfSession.hostUriOf(HttpScheme.HTTP, "--"));
+        assertEquals("Illegal character in hostname at index 7: http://--", ex.getMessage());
+    }
+
+    @Test
+    void hostUriOfWithUser() {
+        final var ex = assertThrows(URISyntaxException.class,
+            () -> RestconfSession.hostUriOf(HttpScheme.HTTP, "user@host"));
+        assertEquals("Illegal Host header: user@host", ex.getMessage());
     }
 }
