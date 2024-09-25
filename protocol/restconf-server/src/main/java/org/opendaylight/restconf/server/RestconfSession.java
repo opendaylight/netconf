b5d0eb663d4a7773e5ff6b36c33707e604951f05
Robert Varga <robert.varga@pantheon.tech>
2024-09-25 14:31:06 +0200

    Reconstruct Target URI, part one
    In order to correctly serve our requests, we need to properly
    reconstruct target URI, as per HTTP/1.1 spec.
    
    This patch deals with the simplest case, asterisk-form: it is only
    allowed for OPTIONS request and reports all methods implemented by
    the server. All other methods fails with either status 400.
    
    That necessitates knowledge as to what methods are requested, hence
    we have a statically-populated Set.
    
    JIRA: NETCONF-1379
    Change-Id: Id37044799460759d1066ad7a17524ee0c0284060
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- protocol/restconf-server/src/main/java/org/opendaylight/restconf/server/RestconfSession.java
+++ protocol/restconf-server/src/main/java/org/opendaylight/restconf/server/RestconfSession.java
@@ -9,14 +9,21 @@ package org.opendaylight.restconf.server;
 
 import static java.util.Objects.requireNonNull;
 
+import com.google.common.annotations.VisibleForTesting;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.handler.codec.http.DefaultFullHttpResponse;
 import io.netty.handler.codec.http.FullHttpRequest;
 import io.netty.handler.codec.http.FullHttpResponse;
+import io.netty.handler.codec.http.HttpHeaderNames;
+import io.netty.handler.codec.http.HttpMethod;
+import io.netty.handler.codec.http.HttpResponseStatus;
+import io.netty.handler.codec.http.HttpUtil;
 import io.netty.handler.codec.http.HttpVersion;
 import io.netty.handler.codec.http.QueryStringDecoder;
 import io.netty.handler.codec.http2.HttpConversionUtil.ExtensionHeaderNames;
 import io.netty.util.AsciiString;
+import java.util.Set;
 import org.opendaylight.restconf.server.api.TransportSession;
 
 /**
@@ -26,6 +33,11 @@ import org.opendaylight.restconf.server.api.TransportSession;
  */
 final class RestconfSession extends SimpleChannelInboundHandler<FullHttpRequest> implements TransportSession {
     private static final AsciiString STREAM_ID = ExtensionHeaderNames.STREAM_ID.text();
+
+    // Does NOT include CONNECT nor TRACE
+    private static final Set<HttpMethod> IMPLEMENTED_METHODS = Set.of(
+        HttpMethod.DELETE, HttpMethod.GET, HttpMethod.HEAD, HttpMethod.OPTIONS, HttpMethod.PATCH, HttpMethod.POST,
+        HttpMethod.PUT);
 
     private final RestconfRequestDispatcher dispatcher;
     private final WellKnownResources wellKnown;
@@ -41,17 +53,50 @@ final class RestconfSession extends SimpleChannelInboundHandler<FullHttpRequest>
         // non-null indicates HTTP/2 request, which we need to propagate to any response
         final var streamId = msg.headers().getInt(STREAM_ID);
         final var version = msg.protocolVersion();
-        final var decoder = new QueryStringDecoder(msg.uri());
+
+        // first things first:
+        // - check if we implement requested method
+        // - we do NOT implement CONNECT method, which is the only valid use of URIs in authority-form
+        final var method = msg.method();
+        if (!IMPLEMENTED_METHODS.contains(method)) {
+            msg.release();
+            respond(ctx, streamId, new DefaultFullHttpResponse(version, HttpResponseStatus.NOT_IMPLEMENTED));
+            return;
+        }
+
+        // next possibility: asterisk-form, as per https://www.rfc-editor.org/rfc/rfc9112#section-3.2.4
+        // it is only applicable to server-wide OPTIONS https://www.rfc-editor.org/rfc/rfc9110#section-9.3.7, which
+        // should result in Allow: listing the contents of IMPLEMENTED_METHODS
+        final var uri = msg.uri();
+        if (HttpUtil.isAsteriskForm(uri)) {
+            msg.release();
+            respond(ctx, streamId, asteriskRequest(version, method));
+            return;
+        }
+
+        // FIXME: finish Target URI reconstruction as per https://www.rfc-editor.org/rfc/rfc9112#section-3.3
+
+        final var decoder = new QueryStringDecoder(uri);
         final var path = decoder.path();
 
         if (path.startsWith("/.well-known/")) {
             // Well-known resources are immediately available and are trivial to service
-            respond(ctx, streamId, wellKnown.request(version, msg.method(), path.substring(13)));
             msg.release();
+            respond(ctx, streamId, wellKnown.request(version, method, path.substring(13)));
         } else {
             // Defer to dispatcher
             dispatchRequest(ctx, version, streamId, decoder, msg);
         }
+    }
+
+    @VisibleForTesting
+    static FullHttpResponse asteriskRequest(final HttpVersion version, final HttpMethod method) {
+        if (HttpMethod.OPTIONS.equals(method)) {
+            final var response = new DefaultFullHttpResponse(version, HttpResponseStatus.OK);
+            response.headers().set(HttpHeaderNames.ALLOW, "DELETE, GET, HEAD, OPTIONS, PATCH, POST, PUT");
+            return response;
+        }
+        return new DefaultFullHttpResponse(version, HttpResponseStatus.BAD_REQUEST);
     }
 
     private void dispatchRequest(final ChannelHandlerContext ctx, final HttpVersion version, final Integer streamId,
