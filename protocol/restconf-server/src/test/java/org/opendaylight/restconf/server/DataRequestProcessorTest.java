d407afc8b96a60d55552f10b3c7a957fe6221a64
Robert Varga <robert.varga@pantheon.tech>
2024-10-01 19:05:57 +0200

    WIP: Refactor RESTCONF request dispatch
    The act of servicing an HTTP request has four distinct phases:
    - request validation and binding, synchronous
    - request execution, asynchronous
    - result interpretation, synchronous
    - result streaming, asynchronous
    
    We have first two cobbled together in RestconfRequestDispatcher, while
    the last two are handled synchronously in ResponseUtils and the various
    RestconfRequest subclasses.
    
    This patch tackles the problem in RestconfRequestDispatcher, separating
    the two phases.
    
    We introduce a prepareRequest() method, whose sole raison d'etre is to
    synchronously process the contents of HttpRequest, i.e. method, URI and
    headers, so as to arrive at a specific request (and its parameters) that
    we want to be executing.
    
    The result is communicated via PreparedRequest, which is a sealed
    interface with two specializations:
    
    1. CompletedRequest, holding an immediate result -- typically an error
       response, or a response that is statically known (such as the answer
       to 'OPTIONS /restconf/operations HTTP/1.1')
    
    2. PendingRequest, represents has a number of specializations for the
       various requests we can execute asynchronously
    
    The former is handled by immediate conversion to a FullHttpResponse,
    which is then sent to the client.
    
    The latter is then fed any request body, if present and executed, with
    the results processed in the usual way.
    
    // FIXME: add more documentation
    // FIXME: deal with POST's input/output encoding distinction
    // FIXME: deal with ISEs
    
    JIRA: NETCONF-1379
    Change-Id: I58b89a389faac4a305881f425b5ec6cfac0d77f4
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- protocol/restconf-server/src/test/java/org/opendaylight/restconf/server/DataRequestProcessorTest.java
+++ protocol/restconf-server/src/test/java/org/opendaylight/restconf/server/DataRequestProcessorTest.java
@@ -104,8 +104,13 @@ class DataRequestProcessorTest extends AbstractRequestProcessorTest {
         assertResponseHeaders(response, Map.of(
             HttpHeaderNames.ALLOW, "GET, HEAD, OPTIONS, PATCH, POST, PUT",
             HttpHeaderNames.ACCEPT_PATCH, """
-                application/json, application/xml, application/yang-data+json, application/yang-data+xml, \
-                application/yang-patch+json, application/yang-patch+xml, text/xml"""));
+                application/yang-data+json, \
+                application/yang-data+xml, \
+                application/yang-patch+json, \
+                application/yang-patch+xml, \
+                application/json, \
+                application/xml, \
+                text/xml"""));
     }
 
     @Test
@@ -138,8 +143,13 @@ class DataRequestProcessorTest extends AbstractRequestProcessorTest {
         assertResponseHeaders(response, Map.of(
             HttpHeaderNames.ALLOW, "DELETE, GET, HEAD, OPTIONS, PATCH, POST, PUT",
             HttpHeaderNames.ACCEPT_PATCH, """
-                application/json, application/xml, application/yang-data+json, application/yang-data+xml, \
-                application/yang-patch+json, application/yang-patch+xml, text/xml"""));
+                application/yang-data+json, \
+                application/yang-data+xml, \
+                application/yang-patch+json, \
+                application/yang-patch+xml, \
+                application/json, \
+                application/xml, \
+                text/xml"""));
     }
 
     @Test
