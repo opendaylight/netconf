b97a6d6548d12019853ee79797442b1904a6c482
Robert Varga <robert.varga@pantheon.tech>
2024-10-01 19:05:57 +0200

    WIP: Refactor RESTCONF request dispatch
    The act of servicing an HTTP request has four distinct phases:
    - request validation and binding, synchronous
    - request execution, asynchronous
    - result interpretation, synchronous
    - result streaming, asynchronous
    
    We have first two cobbled together in RestconfRequestDispatcher, while
    the last two are handled synchronously in ResponseUtils and the various
    RestconfRequest subclasses.
    
    This patch tackles the problem in RestconfRequestDispatcher, separating
    the two phases.
    
    We introduce a prepareRequest() method, whose sole raison d'etre is to
    synchronously process the contents of HttpRequest, i.e. method, URI and
    headers, so as to arrive at a specific request (and its parameters) that
    we want to be executing.
    
    The result is communicated via PreparedRequest, which is a sealed
    interface with two specializations:
    
    1. CompletedRequest, holding an immediate result -- typically an error
       response, or a response that is statically known (such as the answer
       to 'OPTIONS /restconf/operations HTTP/1.1')
    2. PendingRequest, represents has a number of specializations for the
       various requests we can execute asynchronously
    
    The former is handled by immediate conversion to a FullHttpResponse,
    which is then sent to the client. The latter is then fed any request
    body, if present and executed, with the results processed in the usual
    way.
    
    // FIXME: add more documentation
    // FIXME: deal with POST's input/output encoding distinction
    
    JIRA: NETCONF-1379
    Change-Id: I58b89a389faac4a305881f425b5ec6cfac0d77f4
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ protocol/restconf-server/src/main/java/org/opendaylight/restconf/server/AbstractPendingRequest.java
@@ -0,0 +1,109 @@
+/*
+ * Copyright (c) 2024 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.restconf.server;
+
+import static java.util.Objects.requireNonNull;
+
+import com.google.common.base.MoreObjects.ToStringHelper;
+import io.netty.handler.codec.http.DefaultHttpHeadersFactory;
+import io.netty.handler.codec.http.HttpResponseStatus;
+import java.io.InputStream;
+import java.net.URI;
+import java.security.Principal;
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.eclipse.jdt.annotation.Nullable;
+import org.opendaylight.restconf.api.FormattableBody;
+import org.opendaylight.restconf.api.HttpStatusCode;
+import org.opendaylight.restconf.server.api.RestconfServer;
+
+/**
+ * An abstract implementation of {@link PendingRequest} contract for RESTCONF endpoint. This class and its subclasses
+ * act as an intermediary between the Netty pipeline and backend {@link RestconfServer}:
+ * <ul>
+ *   <li>when instructed to start execution, this class allocates the {@link NettyServerRequest} and passes it to the
+ *       concrete subclass, which is then responsible for passing the request, and whatever additional arguments, to the
+ *       correct {@link RestconfServer} method</li>
+ *   <li>upon {@link NettyServerRequest} success, this class calls down to the concrete subclass to translate the server
+ *       response to the appropriate HTTP {@link Response} and then notifies the {@link PendingRequestListener}</li>
+ *   <li>when {@link NettyServerRequest} fails, this class will wrap the failure in a {@link FormattableDataResponse},
+ *       and passes it to {@link PendingRequestListener}</li>
+ * </ul>
+ *
+ * @param <T> server response type
+ */
+@NonNullByDefault
+abstract class AbstractPendingRequest<T> extends PendingRequest<T> {
+    static final CompletedRequest NO_CONTENT = new DefaultCompletedRequest(HttpResponseStatus.NO_CONTENT);
+    static final DefaultHttpHeadersFactory HEADERS_FACTORY = DefaultHttpHeadersFactory.headersFactory();
+
+    final EndpointInvariants invariants;
+    final URI targetUri;
+
+    AbstractPendingRequest(final EndpointInvariants invariants, final URI targetUri) {
+        this.invariants = requireNonNull(invariants);
+        this.targetUri = requireNonNull(targetUri);
+    }
+
+    final RestconfServer server() {
+        return invariants.server();
+    }
+
+    /**
+     * Return the absolute URI pointing at the root API resource, as seen from the perspective of specified request.
+     *
+     * @param request a {@link NettyServerRequest}
+     * @return An absolute URI
+     */
+    final URI restconfURI() {
+        return targetUri.resolve(invariants.restconfPath());
+    }
+
+    @Override
+    @SuppressWarnings("checkstyle:illegalCatch")
+    final void execute(final PendingRequestListener listener, final @Nullable Principal principal,
+            final InputStream body) {
+        try {
+            execute(new NettyServerRequest<>(this, listener, principal), body);
+        } catch (RuntimeException e) {
+            listener.requestFailed(this, e);
+        }
+    }
+
+    /**
+     * Execute this request on the backend {@link RestconfServer}.
+     *
+     * @param request the {@link NettyServerRequest} to pass to the server method
+     * @param body request body
+     */
+    abstract void execute(NettyServerRequest<T> request, InputStream body);
+
+    final void onFailure(final PendingRequestListener listener, final NettyServerRequest<T> request,
+            final HttpStatusCode status, final FormattableBody body) {
+        listener.requestComplete(this, new FormattableDataResponse(HttpResponseStatus.valueOf(status.code()), null,
+            // FIXME: need to pick encoding
+            body, null, request.prettyPrint()));
+    }
+
+    final void onSuccess(final PendingRequestListener listener, final NettyServerRequest<T> request, final T result) {
+        listener.requestComplete(this, transformResult(request, result));
+    }
+
+    /**
+     * Transform a RestconfServer result to a {@link Response}.
+     *
+     * @param request {@link NettyServerRequest} handle
+     * @param result the result
+     * @return A {@link Response}
+     */
+    abstract Response transformResult(NettyServerRequest<?> request, T result);
+
+    @Override
+    protected ToStringHelper addToStringAttributes(final ToStringHelper helper) {
+        return helper.add("target", targetUri);
+    }
+}
---
+++ protocol/restconf-server/src/main/java/org/opendaylight/restconf/server/PendingDataPost.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2024 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.restconf.server;
+
+import static java.util.Objects.requireNonNull;
+
+import java.io.InputStream;
+import java.net.URI;
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.opendaylight.restconf.api.ApiPath;
+import org.opendaylight.restconf.server.api.DataPostBody;
+import org.opendaylight.restconf.server.api.DataPostResult;
+import org.opendaylight.restconf.server.api.JsonDataPostBody;
+import org.opendaylight.restconf.server.api.XmlDataPostBody;
+
+/**
+ * A POST request to a child of /data resource.
+ */
+@NonNullByDefault
+final class PendingDataPost extends AbstractPendingDataPost<DataPostResult, DataPostBody> {
+    private final ApiPath apiPath;
+
+    PendingDataPost(final EndpointInvariants invariants, final URI targetUri, final MessageEncoding encoding,
+            final ApiPath apiPath) {
+        super(invariants, targetUri, encoding);
+        this.apiPath = requireNonNull(apiPath);
+    }
+
+    @Override
+    void execute(final NettyServerRequest<DataPostResult> request, final DataPostBody body) {
+        server().dataPOST(request, apiPath, body);
+    }
+
+    @Override
+    DataPostBody wrapBody(final InputStream body) {
+        return switch (encoding) {
+            case JSON -> new JsonDataPostBody(body);
+            case XML -> new XmlDataPostBody(body);
+        };
+    }
+}
