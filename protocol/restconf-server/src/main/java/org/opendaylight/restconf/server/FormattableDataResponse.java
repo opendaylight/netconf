d7231115b7c267e72e06bec6033ade73fd09a016
Robert Varga <robert.varga@pantheon.tech>
2024-10-01 19:05:57 +0200

    WIP: Refactor RESTCONF request dispatch
    The act of servicing an HTTP request has four distinct phases:
    - request validation and binding, synchronous
    - request execution, asynchronous
    - result interpretation, synchronous
    - result streaming, asynchronous
    
    We have first two cobbled together in RestconfRequestDispatcher, while
    the last two are handled synchronously in ResponseUtils and the various
    RestconfRequest subclasses.
    
    This patch tackles the problem in RestconfRequestDispatcher, separating
    the two phases.
    
    We introduce a prepareRequest() method, whose sole raison d'etre is to
    synchronously process the contents of HttpRequest, i.e. method, URI and
    headers, so as to arrive at a specific request (and its parameters) that
    we want to be executing.
    
    The result is communicated via PreparedRequest, which is a sealed
    interface with two specializations:
    
    1. CompletedRequest, holding an immediate result -- typically an error
       response, or a response that is statically known (such as the answer
       to 'OPTIONS /restconf/operations HTTP/1.1')
    
    2. PendingRequest, represents has a number of specializations for the
       various requests we can execute asynchronously
    
    The former is handled by immediate conversion to a FullHttpResponse,
    which is then sent to the client.
    
    The latter is then fed any request body, if present and executed, with
    the results processed in the usual way.
    
    // FIXME: add more documentation
    // FIXME: deal with POST's input/output encoding distinction
    // FIXME: finish content type selection
    // FIXME: handle FormattableDataResponse
    // FIXME: needs more audit
    
    JIRA: NETCONF-1379
    Change-Id: I58b89a389faac4a305881f425b5ec6cfac0d77f4
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ protocol/restconf-server/src/main/java/org/opendaylight/restconf/server/FormattableDataResponse.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) 2024 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.restconf.server;
+
+import static java.util.Objects.requireNonNull;
+
+import io.netty.handler.codec.http.HttpHeaders;
+import io.netty.handler.codec.http.HttpResponseStatus;
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.eclipse.jdt.annotation.Nullable;
+import org.opendaylight.restconf.api.FormattableBody;
+import org.opendaylight.restconf.api.query.PrettyPrintParam;
+
+/**
+ * A {@link Response} containing a YANG Data as its content.
+ */
+@NonNullByDefault
+record FormattableDataResponse(
+        HttpResponseStatus status,
+        @Nullable HttpHeaders headers,
+        FormattableBody body,
+        MessageEncoding encoding,
+        PrettyPrintParam prettyPrint) implements Response {
+    FormattableDataResponse {
+        requireNonNull(status);
+        requireNonNull(body);
+        requireNonNull(encoding);
+        requireNonNull(prettyPrint);
+    }
+
+    FormattableDataResponse(final HttpHeaders headers, final FormattableBody body, final MessageEncoding encoding,
+            final PrettyPrintParam prettyPrint) {
+        this(HttpResponseStatus.OK, requireNonNull(headers), body, encoding, prettyPrint);
+    }
+
+    FormattableDataResponse(final FormattableBody body, final MessageEncoding encoding,
+            final PrettyPrintParam prettyPrint) {
+        this(HttpResponseStatus.OK, null, body, encoding, prettyPrint);
+    }
+}
