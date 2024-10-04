f935c8e172158cd6b8682a0c1472f1f716e0e2fc
Robert Varga <robert.varga@pantheon.tech>
2024-10-04 22:53:31 +0200

    Split out HTTPServerSession
    RestconfSession has a top-half, which is RESTCONF-specific, and a
    bottom-half, which is closely related to HTTPServer's channel setup.
    
    This splits out HTTPServerSession, so that it can be moved to
    transport-http and reused by other components if/when the need arises.
    
    JIRA: NETCONF-1379
    Change-Id: Iab96baab4b9469a6a5eaf2d3234cfdbb813a59a6
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- protocol/restconf-server/src/main/java/org/opendaylight/restconf/server/DataResource.java
+++ protocol/restconf-server/src/main/java/org/opendaylight/restconf/server/DataResource.java
@@ -16,6 +16,7 @@ import java.net.URI;
 import java.security.Principal;
 import org.eclipse.jdt.annotation.NonNullByDefault;
 import org.eclipse.jdt.annotation.Nullable;
+import org.opendaylight.netconf.transport.http.ImplementedMethod;
 import org.opendaylight.restconf.server.api.TransportSession;
 
 /**
