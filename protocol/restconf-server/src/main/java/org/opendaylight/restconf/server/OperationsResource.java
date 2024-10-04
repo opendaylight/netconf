471e0d1248e19f5da0edb7ff213efcff6d760147
Robert Varga <robert.varga@pantheon.tech>
2024-10-04 13:34:02 +0200

    RESTCONF request preparation
    RestconfRequestDispatcher's organization is now such that there are two
    three distinct parts to it:
    
    1. the 'RESTCONF API root resource' part, dealing request
    2. the preparation of subresources
    3. the Netty pipeline part, dealing with interface to RestconfSession
    
    In this patch we introduce AbstractResource to split out utilities and
    constants shared for the first two parts. It additionally defines the
    prepare() function, which we use from the last part.
    
    We split out {Data,Modules,Operations,YLV}Resource, so that each hosts
    only methods specific to them.
    
    JIRA: NETCONF-1379
    Change-Id: I0b42a3920146a30f22ce8bfdb3b5fd97693bfcdf
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ protocol/restconf-server/src/main/java/org/opendaylight/restconf/server/OperationsResource.java
@@ -0,0 +1,83 @@
+/*
+ * Copyright (c) 2024 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.restconf.server;
+
+import io.netty.handler.codec.http.HttpHeaders;
+import io.netty.handler.codec.http.HttpResponseStatus;
+import java.net.URI;
+import java.security.Principal;
+import org.eclipse.jdt.annotation.NonNull;
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.eclipse.jdt.annotation.Nullable;
+
+/**
+ * RESTCONF /operations resource, as defined in
+ * <a href="https://www.rfc-editor.org/rfc/rfc8040#section-3.3.2">RFC 8040, section 3.3.2</a>.
+ */
+@NonNullByDefault
+final class OperationsResource extends AbstractResource {
+    private static final CompletedRequest METHOD_NOT_ALLOWED_RPC =
+        new DefaultCompletedRequest(HttpResponseStatus.METHOD_NOT_ALLOWED, AbstractPendingOptions.HEADERS_RPC);
+
+    OperationsResource(final EndpointInvariants invariants) {
+        super(invariants);
+    }
+
+    @Override
+    PreparedRequest prepare(final ImplementedMethod method, final URI targetUri, final HttpHeaders headers,
+            final @Nullable Principal principal, final String path) {
+        return switch (method) {
+            case GET -> prepareOperationsGet(targetUri, headers, principal, path, true);
+            case HEAD -> prepareOperationsGet(targetUri, headers, principal, path, false);
+            case OPTIONS -> prepareOperationsOptions(targetUri, principal, path);
+            case POST -> prepareOperationsPost(targetUri, headers, principal, path);
+            default -> prepareOperationsDefault(targetUri, path);
+        };
+    }
+
+    private PreparedRequest prepareOperationsGet(final URI targetUri, final HttpHeaders headers,
+            final @Nullable Principal principal, final String path, final boolean withContent) {
+        final var encoding = chooseOutputEncoding(headers);
+        return encoding == null ? NOT_ACCEPTABLE_DATA : optionalApiPath(path,
+            apiPath -> new PendingOperationsGet(invariants, targetUri, principal, encoding, apiPath, withContent));
+    }
+
+    private PreparedRequest prepareOperationsOptions(final URI targetUri, final @Nullable Principal principal,
+            final String path) {
+        return path.isEmpty() ? AbstractPendingOptions.READ_ONLY
+            : requiredApiPath(path, apiPath -> new PendingOperationsOptions(invariants, targetUri, principal, apiPath));
+    }
+
+    // invoke rpc -> https://www.rfc-editor.org/rfc/rfc8040#section-4.4.2
+    private PreparedRequest prepareOperationsPost(final URI targetUri, final HttpHeaders headers,
+            final @Nullable Principal principal, final String path) {
+        final var accept = chooseOutputEncoding(headers);
+        return accept == null ? NOT_ACCEPTABLE_DATA : switch (chooseInputEncoding(headers)) {
+            case NOT_PRESENT ->
+                prepareOperationsPost(targetUri, principal, path, invariants.defaultEncoding(), accept);
+            case JSON -> prepareOperationsPost(targetUri, principal, path, MessageEncoding.JSON, accept);
+            case XML -> prepareOperationsPost(targetUri, principal, path, MessageEncoding.XML, accept);
+            case UNRECOGNIZED, UNSPECIFIED -> UNSUPPORTED_MEDIA_TYPE_DATA;
+        };
+    }
+
+    private PreparedRequest prepareOperationsPost(final URI targetUri, final @Nullable Principal principal,
+            final String path, final MessageEncoding content, final MessageEncoding accept) {
+        return optionalApiPath(path,
+            apiPath -> new PendingOperationsPost(invariants, targetUri, principal, content, accept, apiPath));
+    }
+
+    private static PreparedRequest prepareOperationsDefault(final URI targetUri, final String path) {
+        return path.isEmpty() ? METHOD_NOT_ALLOWED_READ_ONLY
+            // TODO: This is incomplete. We are always reporting 405 Method Not Allowed, but we can do better.
+            //       We should fire off an OPTIONS request for the apiPath and see if it exists: if it does not,
+            //       we should report a 404 Not Found instead.
+            : requiredApiPath(path, apiPath -> METHOD_NOT_ALLOWED_RPC);
+    }
+
+}
