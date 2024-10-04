f722a23b1b819f54e5946ba1b3c70c793b547056
Robert Varga <robert.varga@pantheon.tech>
2024-10-04 21:12:51 +0200

    Eliminate RestconfRequest
    The sole remaining user of this class is RestconfSession, which uses
    it just as an indirection.
    
    Fold RestconfRequest.execute() into RestconfSession.executeRequest(), so
    that we get all the things in one place.
    
    JIRA: NETCONF-1379
    Change-Id: I085d5c1e020d31ab1deb38c5496f1ace0bcd8ce7
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- protocol/restconf-server/src/main/java/org/opendaylight/restconf/server/RestconfSession.java
+++ protocol/restconf-server/src/main/java/org/opendaylight/restconf/server/RestconfSession.java
@@ -12,6 +12,8 @@ import static java.util.Objects.requireNonNull;
 import com.google.common.annotations.VisibleForTesting;
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.ByteBufInputStream;
+import io.netty.buffer.ByteBufOutputStream;
+import io.netty.buffer.ByteBufUtil;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.handler.codec.http.DefaultFullHttpResponse;
@@ -25,8 +27,10 @@ import io.netty.handler.codec.http.HttpUtil;
 import io.netty.handler.codec.http.HttpVersion;
 import io.netty.handler.codec.http2.HttpConversionUtil.ExtensionHeaderNames;
 import io.netty.util.AsciiString;
+import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 import java.util.Map;
 import java.util.function.Function;
@@ -193,12 +197,88 @@ final class RestconfSession extends SimpleChannelInboundHandler<FullHttpRequest>
             body = null;
         }
 
-        new RestconfRequest() {
+        pending.execute(new PendingRequestListener() {
             @Override
-            public void onSuccess(final FullHttpResponse response) {
+            public void requestFailed(final PendingRequest<?> request, final Exception cause) {
+                LOG.warn("Internal error while processing {}", request, cause);
+                final var response = new DefaultFullHttpResponse(version,
+                    HttpResponseStatus.INTERNAL_SERVER_ERROR);
+                final var content = response.content();
+                // Note: we are tempted to do a cause.toString() here, but we are dealing with unhandled badness here,
+                //       so we do not want to be too revealing -- hence a message is all the user gets.
+                ByteBufUtil.writeUtf8(content, cause.getMessage());
+                HttpUtil.setContentLength(response, content.readableBytes());
                 respond(ctx, streamId, response);
             }
-        }.execute(pending, version, body);
+
+            @Override
+            public void requestComplete(final PendingRequest<?> request, final Response reply) {
+                // FIXME: ServerRequests typically finish with a FormattableBody, which can contain a huge entity, which
+                //        we do *not* want to completely buffer to a FullHttpResponse.
+                final FullHttpResponse response;
+
+                switch (reply) {
+                    case CompletedRequest completed -> {
+                        response = completed.toHttpResponse(version);
+                    }
+
+                    // FIXME: these payloads use a synchronous dump of data into the socket. We cannot safely do that on
+                    //        the event loop, because a slow client would end up throttling our IO threads simply
+                    //        because of TCP window and similar queuing/backpressure things.
+                    //
+                    //        we really want to kick off a virtual thread to take care of that, i.e. doing its own
+                    //        synchronous write thing, talking to a short queue (SPSC?) of HttpObjects.
+                    //
+                    //        the event loop of each channel would be the consumer of that queue, picking them off as
+                    //        quickly as possible, but execting backpressure if the amount of pending stuff goes up.
+                    //
+                    //        as for the HttpObjects: this effectively means that the OutputStreams used in the below
+                    //        code should be replaced with entities which perform chunking:
+                    //        - buffer initial stuff, so that we produce a FullHttpResponse if the payload is below
+                    //          256KiB (or so), i.e. producing Content-Length header and dumping the thing in one go
+                    //        - otherwise emit just HttpResponse with Transfer-Enconding: chunked and continue sending
+                    //          out chunks (of reasonable size).
+                    //        - finish up with a LastHttpContent
+
+                    case CharSourceResponse charSource -> {
+                        response = new DefaultFullHttpResponse(version, HttpResponseStatus.OK);
+                        final var content = response.content();
+                        try (var os = new ByteBufOutputStream(content)) {
+                            charSource.source().asByteSource(StandardCharsets.UTF_8).copyTo(os);
+                        } catch (IOException e) {
+                            requestFailed(request, e);
+                            return;
+                        }
+
+                        response.headers()
+                            .set(HttpHeaderNames.CONTENT_TYPE, charSource.mediaType())
+                            .set(HttpHeaderNames.CONTENT_LENGTH, content.readableBytes());
+                    }
+                    case FormattableDataResponse formattable -> {
+                        response = new DefaultFullHttpResponse(version, formattable.status());
+                        final var content = response.content();
+
+                        try (var os = new ByteBufOutputStream(content)) {
+                            formattable.writeTo(os);
+                        } catch (IOException e) {
+                            requestFailed(request, e);
+                            return;
+                        }
+
+                        final var headers = response.headers();
+                        final var extra = formattable.headers();
+                        if (extra != null) {
+                            headers.set(extra);
+                        }
+                        headers
+                            .set(HttpHeaderNames.CONTENT_TYPE, formattable.encoding().dataMediaType())
+                            .set(HttpHeaderNames.CONTENT_LENGTH, content.readableBytes());
+                    }
+                }
+
+                respond(ctx, streamId, response);
+            }
+        }, body);
     }
 
     @VisibleForTesting
