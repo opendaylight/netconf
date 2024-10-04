c3ed98cff931258c1fc28f7c873d5976cd9eda03
Robert Varga <robert.varga@pantheon.tech>
2024-10-04 14:32:05 +0200

    Move RestconfRequestDispatcher processing
    The entire RestconfRequestDispatcher.dispatch() thing is going to be
    refactored. Move it to RestconfRequest, as it is completely independent
    from RestconfRequestDispatcher.
    
    JIRA: NETCONF-1379
    Change-Id: I6a7ea7768fa467002beecc38d806898f46cca55c
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- protocol/restconf-server/src/main/java/org/opendaylight/restconf/server/RestconfRequestDispatcher.java
+++ protocol/restconf-server/src/main/java/org/opendaylight/restconf/server/RestconfRequestDispatcher.java
@@ -9,20 +9,9 @@ package org.opendaylight.restconf.server;
 
 import static java.util.Objects.requireNonNull;
 
-import io.netty.buffer.ByteBufInputStream;
-import io.netty.buffer.ByteBufOutputStream;
-import io.netty.buffer.ByteBufUtil;
-import io.netty.handler.codec.http.DefaultFullHttpResponse;
 import io.netty.handler.codec.http.FullHttpRequest;
-import io.netty.handler.codec.http.FullHttpResponse;
-import io.netty.handler.codec.http.HttpHeaderNames;
 import io.netty.handler.codec.http.HttpHeaders;
-import io.netty.handler.codec.http.HttpResponseStatus;
-import io.netty.handler.codec.http.HttpUtil;
-import java.io.IOException;
-import java.io.InputStream;
 import java.net.URI;
-import java.nio.charset.StandardCharsets;
 import java.security.Principal;
 import java.util.List;
 import java.util.Map;
@@ -103,91 +92,8 @@ final class RestconfRequestDispatcher extends AbstractResource {
         switch (prepare(peeler, method, targetUri, request.headers(), principalService.acquirePrincipal(request))) {
             case CompletedRequest completed -> callback.onSuccess(completed.toHttpResponse(version));
             case PendingRequest<?> pending -> {
-                LOG.debug("Dispatching {} {}", request.method(), targetUri);
-
-                final var content = request.content();
-                pending.execute(new PendingRequestListener() {
-                    @Override
-                    public void requestFailed(final PendingRequest<?> request, final Exception cause) {
-                        LOG.warn("Internal error while processing {}", request, cause);
-                        final var response = new DefaultFullHttpResponse(version,
-                            HttpResponseStatus.INTERNAL_SERVER_ERROR);
-                        final var content = response.content();
-                        // Note: we are tempted to do a cause.toString() here, but we are dealing with unhandled badness
-                        //       here, so we do not want to be too revealing -- hence a message is all the user gets.
-                        ByteBufUtil.writeUtf8(content, cause.getMessage());
-                        HttpUtil.setContentLength(response, content.readableBytes());
-                        callback.onSuccess(response);
-                    }
-
-                    @Override
-                    public void requestComplete(final PendingRequest<?> request, final Response reply) {
-                        // FIXME: ServerRequests typically finish with a FormattableBody, which can contain a huge
-                        //        entity, which we do *not* want to completely buffer to a FullHttpResponse.
-                        final FullHttpResponse response;
-                        switch (reply) {
-                            case CompletedRequest completed -> {
-                                response = completed.toHttpResponse(version);
-                            }
-
-                            // FIXME: these payloads use a synchronous dump of data into the socket. We cannot safely
-                            //        do that on the event loop, because a slow client would end up throttling our IO
-                            //        threads simply because of TCP window and similar queuing/backpressure things.
-                            //
-                            //        we really want to kick off a virtual thread to take care of that, i.e. doing its
-                            //        own synchronous write thing, talking to a short queue (SPSC?) of HttpObjects.
-                            //
-                            //        the event loop of each channel would be the consumer of that queue, picking them
-                            //        off as quickly as possible, but execting backpressure if the amount of pending
-                            //        stuff goes up.
-                            //
-                            //        as for the HttpObjects: this effectively means that the OutputStreams used in the
-                            //        below code should be replaced with entities which perform chunking:
-                            //        - buffer initial stuff, so that we produce a FullHttpResponse if the payload is
-                            //          below 256KiB (or so), i.e. producing Content-Length header and dumping the thing
-                            //          in one go
-                            //        - otherwise emit just HttpResponse with Transfer-Enconding: chunked and continue
-                            //          sending out chunks (of reasonable size).
-                            //        - finish up with a LastHttpContent
-
-                            case CharSourceResponse charSource -> {
-                                response = new DefaultFullHttpResponse(version, HttpResponseStatus.OK);
-                                final var content = response.content();
-                                try (var os = new ByteBufOutputStream(content)) {
-                                    charSource.source().asByteSource(StandardCharsets.UTF_8).copyTo(os);
-                                } catch (IOException e) {
-                                    requestFailed(request, e);
-                                    return;
-                                }
-
-                                response.headers()
-                                    .set(HttpHeaderNames.CONTENT_TYPE, charSource.mediaType())
-                                    .set(HttpHeaderNames.CONTENT_LENGTH, content.readableBytes());
-                            }
-                            case FormattableDataResponse formattable -> {
-                                response = new DefaultFullHttpResponse(version, formattable.status());
-                                final var content = response.content();
-
-                                try (var os = new ByteBufOutputStream(content)) {
-                                    formattable.writeTo(os);
-                                } catch (IOException e) {
-                                    requestFailed(request, e);
-                                    return;
-                                }
-
-                                final var headers = response.headers();
-                                final var extra = formattable.headers();
-                                if (extra != null) {
-                                    headers.set(extra);
-                                }
-                                headers
-                                    .set(HttpHeaderNames.CONTENT_TYPE, formattable.encoding().dataMediaType())
-                                    .set(HttpHeaderNames.CONTENT_LENGTH, content.readableBytes());
-                            }
-                        }
-                        callback.onSuccess(response);
-                    }
-                }, content.readableBytes() == 0 ? InputStream.nullInputStream() : new ByteBufInputStream(content));
+                LOG.debug("Dispatching {} {}", method, targetUri);
+                callback.execute(pending, version, request.content());
             }
         }
     }
