b7777cba28601291cc053505d6d26dbbebaf78b1
Robert Varga <robert.varga@pantheon.tech>
2024-10-05 14:16:16 +0200

    Finish RestconfSession/PendingRequest integration
    We have all the bits and pieces concentrated in RestconfSession, now
    implement request tracking as outlined in the FIXMEs, completing the
    dispatch integration.
    
    JIRA: NETCONF-1379
    Change-Id: Icc5216b5496abda90cd2f1368f99134a07b09306
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- protocol/restconf-server/src/main/java/org/opendaylight/restconf/server/RestconfSession.java
+++ protocol/restconf-server/src/main/java/org/opendaylight/restconf/server/RestconfSession.java
@@ -22,9 +22,14 @@ import io.netty.handler.codec.http.HttpResponseStatus;
 import io.netty.handler.codec.http.HttpScheme;
 import io.netty.handler.codec.http.HttpUtil;
 import io.netty.handler.codec.http.HttpVersion;
+import io.netty.util.AsciiString;
 import java.io.IOException;
 import java.net.URI;
 import java.nio.charset.StandardCharsets;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.eclipse.jdt.annotation.Nullable;
 import org.opendaylight.netconf.transport.http.HTTPServerSession;
 import org.opendaylight.netconf.transport.http.ImplementedMethod;
 import org.opendaylight.restconf.server.api.TransportSession;
@@ -50,9 +55,22 @@ import org.slf4j.LoggerFactory;
 //        We support HTTP/1.1 -> HTTP/2 upgrade for the first request only -- hence we know before processing the first
 //        result which mode of operation is effective. We probably need to have two subclasses of this thing, with
 //        HTTP/1.1 and HTTP/2 specializations.
-final class RestconfSession extends HTTPServerSession implements TransportSession {
+final class RestconfSession extends HTTPServerSession implements TransportSession, PendingRequestListener {
+    @NonNullByDefault
+    private record RequestContext(ChannelHandlerContext ctx, HttpVersion version, @Nullable Integer streamId) {
+        RequestContext {
+            requireNonNull(ctx);
+            requireNonNull(version);
+        }
+
+        void respond(final FullHttpResponse response) {
+            HTTPServerSession.respond(ctx, streamId, response);
+        }
+    }
+
     private static final Logger LOG = LoggerFactory.getLogger(RestconfSession.class);
 
+    private final ConcurrentMap<PendingRequest<?>, RequestContext> executingRequests = new ConcurrentHashMap<>();
     private final EndpointRoot root;
 
     RestconfSession(final HttpScheme scheme, final EndpointRoot root) {
@@ -71,22 +89,12 @@ final class RestconfSession extends HTTPServerSession implements TransportSessio
             }
             case PendingRequest<?> pending -> {
                 LOG.debug("Dispatching {} {}", method, targetUri);
-                executeRequest(ctx, version, streamId, pending, body);
+                executeRequest(new RequestContext(ctx, version, streamId), pending, body);
             }
         }
     }
 
-    // FIXME: NETCONF-1379: second part of integration here:
-    //        - we will have PendingRequest<?>, which is the asynchronous invocation
-    //        - add a new field to track them:
-    //          ConcurrentMap<PendingRequest<?>, RequestContext> executingRequests;
-    //        - RequestContext is a DTO that holds streamId, ctx, msg (maybe) and perhaps some more state as needed
-    //        - this class implements PendingRequestListener:
-    //          - when request{Completed,Failed} is invoked, perform executingRequests.remove(req) to get
-    //            the corresponding RequestContext
-    //          - use that to call respond() with a formatted response (for now)
-    private static void executeRequest(final ChannelHandlerContext ctx, final HttpVersion version,
-            final Integer streamId, final PendingRequest<?> pending, final ByteBuf content) {
+    private void executeRequest(final RequestContext context, final PendingRequest<?> pending, final ByteBuf content) {
         // We are invoked with content's reference and need to make sure it gets released.
         final ByteBufInputStream body;
         if (content.isReadable()) {
@@ -96,87 +104,101 @@ final class RestconfSession extends HTTPServerSession implements TransportSessio
             body = null;
         }
 
-        pending.execute(new PendingRequestListener() {
-            @Override
-            public void requestFailed(final PendingRequest<?> request, final Exception cause) {
-                LOG.warn("Internal error while processing {}", request, cause);
-                final var response = new DefaultFullHttpResponse(version,
-                    HttpResponseStatus.INTERNAL_SERVER_ERROR);
-                final var content = response.content();
-                // Note: we are tempted to do a cause.toString() here, but we are dealing with unhandled badness here,
-                //       so we do not want to be too revealing -- hence a message is all the user gets.
-                ByteBufUtil.writeUtf8(content, cause.getMessage());
-                HttpUtil.setContentLength(response, content.readableBytes());
-                respond(ctx, streamId, response);
-            }
+        // Remember metadata about the request and then execute it
+        executingRequests.put(pending, context);
+        pending.execute(this, body);
+    }
 
-            @Override
-            public void requestComplete(final PendingRequest<?> request, final Response reply) {
-                // FIXME: ServerRequests typically finish with a FormattableBody, which can contain a huge entity, which
-                //        we do *not* want to completely buffer to a FullHttpResponse.
-                final FullHttpResponse response;
+    @Override
+    public void requestComplete(final PendingRequest<?> request, final Response response) {
+        final var context = executingRequests.remove(request);
+        if (context != null) {
+            context.respond(switch (response) {
+                case CompletedRequest completed -> completed.toHttpResponse(context.version);
+                case CharSourceResponse charSource -> formatResponse(charSource, context.version);
+                case FormattableDataResponse formattable -> formatResponse(formattable, context.version);
+            });
+        } else {
+            LOG.warn("Cannot pair request {}, not sending response {}", request, response, new Throwable());
+        }
+    }
 
-                switch (reply) {
-                    case CompletedRequest completed -> {
-                        response = completed.toHttpResponse(version);
-                    }
+    @Override
+    public void requestFailed(final PendingRequest<?> request, final Exception cause) {
+        LOG.warn("Internal error while processing {}", request, cause);
+        final var context = executingRequests.remove(request);
+        if (context != null) {
+            context.respond(formatException(cause, context.version));
+        } else {
+            LOG.warn("Cannot pair request, not sending response", new Throwable());
+        }
+    }
 
-                    // FIXME: these payloads use a synchronous dump of data into the socket. We cannot safely do that on
-                    //        the event loop, because a slow client would end up throttling our IO threads simply
-                    //        because of TCP window and similar queuing/backpressure things.
-                    //
-                    //        we really want to kick off a virtual thread to take care of that, i.e. doing its own
-                    //        synchronous write thing, talking to a short queue (SPSC?) of HttpObjects.
-                    //
-                    //        the event loop of each channel would be the consumer of that queue, picking them off as
-                    //        quickly as possible, but execting backpressure if the amount of pending stuff goes up.
-                    //
-                    //        as for the HttpObjects: this effectively means that the OutputStreams used in the below
-                    //        code should be replaced with entities which perform chunking:
-                    //        - buffer initial stuff, so that we produce a FullHttpResponse if the payload is below
-                    //          256KiB (or so), i.e. producing Content-Length header and dumping the thing in one go
-                    //        - otherwise emit just HttpResponse with Transfer-Enconding: chunked and continue sending
-                    //          out chunks (of reasonable size).
-                    //        - finish up with a LastHttpContent
+    @NonNullByDefault
+    private static FullHttpResponse formatException(final Exception cause, final HttpVersion version) {
+        final var response = new DefaultFullHttpResponse(version, HttpResponseStatus.INTERNAL_SERVER_ERROR);
+        final var content = response.content();
+        // Note: we are tempted to do a cause.toString() here, but we are dealing with unhandled badness here,
+        //       so we do not want to be too revealing -- hence a message is all the user gets.
+        ByteBufUtil.writeUtf8(content, cause.getMessage());
+        HttpUtil.setContentLength(response, content.readableBytes());
+        return response;
+    }
 
-                    case CharSourceResponse charSource -> {
-                        response = new DefaultFullHttpResponse(version, HttpResponseStatus.OK);
-                        final var content = response.content();
-                        try (var os = new ByteBufOutputStream(content)) {
-                            charSource.source().asByteSource(StandardCharsets.UTF_8).copyTo(os);
-                        } catch (IOException e) {
-                            requestFailed(request, e);
-                            return;
-                        }
+    // FIXME: below payloads use a synchronous dump of data into the socket. We cannot safely do that on the event loop,
+    //        because a slow client would end up throttling our IO threads simply because of TCP window and similar
+    //        queuing/backpressure things.
+    //
+    //        we really want to kick off a virtual thread to take care of that, i.e. doing its own synchronous write
+    //        thing, talking to a short queue (SPSC?) of HttpObjects.
+    //
+    //        the event loop of each channel would be the consumer of that queue, picking them off as quickly as
+    //        possible, but execting backpressure if the amount of pending stuff goes up.
+    //
+    //        as for the HttpObjects: this effectively means that the OutputStreams used in the below code should be
+    //        replaced with entities which perform chunking:
+    //        - buffer initial stuff, so that we produce a FullHttpResponse if the payload is below
+    //          256KiB (or so), i.e. producing Content-Length header and dumping the thing in one go
+    //        - otherwise emit just HttpResponse with Transfer-Enconding: chunked and continue sending
+    //          out chunks (of reasonable size).
+    //        - finish up with a LastHttpContent
 
-                        response.headers()
-                            .set(HttpHeaderNames.CONTENT_TYPE, charSource.mediaType())
-                            .set(HttpHeaderNames.CONTENT_LENGTH, content.readableBytes());
-                    }
-                    case FormattableDataResponse formattable -> {
-                        response = new DefaultFullHttpResponse(version, formattable.status());
-                        final var content = response.content();
+    @NonNullByDefault
+    private static FullHttpResponse formatResponse(final CharSourceResponse response, final HttpVersion version) {
+        final var httpResponse = new DefaultFullHttpResponse(version, HttpResponseStatus.OK);
+        try (var os = new ByteBufOutputStream(httpResponse.content())) {
+            response.source().asByteSource(StandardCharsets.UTF_8).copyTo(os);
+        } catch (IOException e) {
+            LOG.warn("IO error while converting stream body", e);
+            return formatException(e, version);
+        }
 
-                        try (var os = new ByteBufOutputStream(content)) {
-                            formattable.writeTo(os);
-                        } catch (IOException e) {
-                            requestFailed(request, e);
-                            return;
-                        }
+        return setContentHeaders(httpResponse, response.mediaType());
+    }
 
-                        final var headers = response.headers();
-                        final var extra = formattable.headers();
-                        if (extra != null) {
-                            headers.set(extra);
-                        }
-                        headers
-                            .set(HttpHeaderNames.CONTENT_TYPE, formattable.encoding().dataMediaType())
-                            .set(HttpHeaderNames.CONTENT_LENGTH, content.readableBytes());
-                    }
-                }
+    @NonNullByDefault
+    private static FullHttpResponse formatResponse(final FormattableDataResponse response, final HttpVersion version) {
+        final var httpResponse = new DefaultFullHttpResponse(version, response.status());
+        try (var os = new ByteBufOutputStream(httpResponse.content())) {
+            response.writeTo(os);
+        } catch (IOException e) {
+            LOG.warn("IO error while converting formattable body", e);
+            return formatException(e, version);
+        }
 
-                respond(ctx, streamId, response);
-            }
-        }, body);
+        final var extra = response.headers();
+        if (extra != null) {
+            httpResponse.headers().set(extra);
+        }
+
+        return setContentHeaders(httpResponse, response.encoding().dataMediaType());
+    }
+
+    @NonNullByDefault
+    private static FullHttpResponse setContentHeaders(final FullHttpResponse response, final AsciiString contentType) {
+        response.headers()
+            .set(HttpHeaderNames.CONTENT_TYPE, contentType)
+            .set(HttpHeaderNames.CONTENT_LENGTH, response.content().readableBytes());
+        return response;
     }
 }
