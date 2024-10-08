f1f1da4ff619834290a106f6555953311f6031d0
Oleksandr Zharov <oleksandr.zharov@pantheon.tech>
2024-10-08 22:43:13 +0200

    Java based tests for OpenApi over Netty
    Added tests for OpenApi over Netty using toaster
    model.
    
    JIRA: NETCONF-1371
    Change-Id: I8a9c9ecc20c6cc34d1d8809c22a85c16bf04f9db
    Signed-off-by: Oleksandr Zharov <oleksandr.zharov@pantheon.tech>
    (cherry picked from commit fa5295436756e2bf2db080832073b1c71c7c4c07)

---
+++ apps/restconf-it/src/test/java/org/opendaylight/restconf/openapi/ControllerDocumentTest.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright (c) 2024 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.restconf.openapi;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import io.netty.handler.codec.http.HttpMethod;
+import io.netty.handler.codec.http.HttpResponseStatus;
+import java.nio.charset.StandardCharsets;
+import java.util.stream.Stream;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.skyscreamer.jsonassert.JSONAssert;
+import org.skyscreamer.jsonassert.JSONCompareMode;
+
+class ControllerDocumentTest extends AbstractOpenApiTest {
+    @Test
+    void controllerAllDocTest() throws Exception {
+        final var expectedJson = getExpectedDoc("openapi-documents/controller-all.json");
+
+        final var response = invokeRequest(HttpMethod.GET, API_V3_PATH + "/single");
+        assertEquals(HttpResponseStatus.OK, response.status());
+
+        final var resultDoc = response.content().toString(StandardCharsets.UTF_8);
+        JSONAssert.assertEquals(fillPort(expectedJson, port), resultDoc, JSONCompareMode.NON_EXTENSIBLE);
+    }
+
+    /**
+     * Tests the swagger document that is result of the call to the '/toaster@revision' endpoint.
+     */
+    @ParameterizedTest
+    @MethodSource
+    void getDocByModuleTest(final String revision, final String jsonPath) throws Exception {
+        final var expectedJson = getExpectedDoc("openapi-documents/" + jsonPath);
+        final var uri = API_V3_PATH + "/" + TOASTER + "?revision=" + revision;
+
+        final var response = invokeRequest(HttpMethod.GET, uri);
+        assertEquals(HttpResponseStatus.OK, response.status());
+
+        final var resultDoc = response.content().toString(StandardCharsets.UTF_8);
+        JSONAssert.assertEquals(fillPort(expectedJson, port), resultDoc, JSONCompareMode.NON_EXTENSIBLE);
+    }
+
+    private static Stream<Arguments> getDocByModuleTest() {
+        // moduleName, revision, jsonPath
+        return Stream.of(
+            Arguments.of(TOASTER_REV, "controller-toaster.json"),
+            Arguments.of(TOASTER_OLD_REV, "controller-toaster-old.json")
+        );
+    }
+
+    private static String getExpectedDoc(final String jsonPath) throws Exception {
+        return MAPPER.writeValueAsString(MAPPER.readTree(
+            ControllerDocumentTest.class.getClassLoader().getResourceAsStream(jsonPath)));
+    }
+}
