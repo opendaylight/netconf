482a8b1ba3cd5fe695f44ebb7233357318fcb01b
Oleksandr Zharov <oleksandr.zharov@pantheon.tech>
2024-10-08 22:43:13 +0200

    Java based tests for OpenApi over Netty
    Added tests for OpenApi over Netty using toaster
    model.
    
    JIRA: NETCONF-1371
    Change-Id: I8a9c9ecc20c6cc34d1d8809c22a85c16bf04f9db
    Signed-off-by: Oleksandr Zharov <oleksandr.zharov@pantheon.tech>

---
+++ apps/restconf-it/src/test/java/org/opendaylight/restconf/openapi/NettyDocumentTest.java
@@ -0,0 +1,185 @@
+/*
+ * Copyright (c) 2024 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.restconf.openapi;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.timeout;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static shaded.org.eclipse.aether.repository.AuthenticationContext.PASSWORD;
+import static shaded.org.eclipse.aether.repository.AuthenticationContext.USERNAME;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.util.concurrent.FutureCallback;
+import io.netty.handler.codec.http.DefaultFullHttpRequest;
+import io.netty.handler.codec.http.FullHttpRequest;
+import io.netty.handler.codec.http.FullHttpResponse;
+import io.netty.handler.codec.http.HttpMethod;
+import io.netty.handler.codec.http.HttpVersion;
+import java.net.URI;
+import java.nio.charset.StandardCharsets;
+import java.util.stream.Stream;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.AuthenticationInfo;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authc.SimpleAuthenticationInfo;
+import org.apache.shiro.realm.AuthenticatingRealm;
+import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.opendaylight.mdsal.binding.dom.adapter.test.AbstractDataBrokerTest;
+import org.opendaylight.mdsal.dom.api.DOMMountPoint;
+import org.opendaylight.mdsal.dom.api.DOMMountPointService;
+import org.opendaylight.mdsal.dom.spi.FixedDOMSchemaService;
+import org.opendaylight.restconf.openapi.api.OpenApiService;
+import org.opendaylight.restconf.openapi.impl.MountPointOpenApiGeneratorRFC8040;
+import org.opendaylight.restconf.openapi.impl.OpenApiGeneratorRFC8040;
+import org.opendaylight.restconf.openapi.impl.OpenApiServiceImpl;
+import org.opendaylight.restconf.openapi.netty.OpenApiRequestDispatcher;
+import org.opendaylight.restconf.server.AAAShiroPrincipalService;
+import org.opendaylight.restconf.server.PrincipalService;
+import org.opendaylight.yangtools.yang.common.QName;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
+import org.opendaylight.yangtools.yang.test.util.YangParserTestUtils;
+import org.skyscreamer.jsonassert.JSONAssert;
+import org.skyscreamer.jsonassert.JSONCompareMode;
+
+@ExtendWith(MockitoExtension.class)
+public class NettyDocumentTest extends AbstractDataBrokerTest {
+    private static final String BASE_PATH = "/openapi";
+    private static final String API_V3_PATH = BASE_PATH + "/api/v3";
+    private static final String BASE_URL = "http://127.0.0.1:8184" + BASE_PATH;
+    private static final URI OPENAPI_BASE_URI = URI.create(BASE_URL);
+    private static final URI RESTCONF_SERVER_URI = URI.create("http://127.0.0.1:8182");
+    private static final ObjectMapper MAPPER = new ObjectMapper();
+    private static final YangInstanceIdentifier INSTANCE_ID = YangInstanceIdentifier.builder()
+        .node(QName.create("", "nodes"))
+        .node(QName.create("", "node"))
+        .nodeWithKey(QName.create("", "node"), QName.create("", "id"), "123").build();
+    private static final String TOASTER = "toaster";
+    private static final String TOASTER_REV = "2009-11-20";
+    /**
+     * Model toaster@2009-11-19 is used for test correct generating of openapi with models with same name and another
+     * revision date. We want to test that the same model is not duplicated and loaded just the newest version.
+     */
+    private static final String TOASTER_OLD_REV = "2009-11-19";
+
+    private OpenApiService openApiService;
+    private OpenApiRequestDispatcher dispatcher;
+    private PrincipalService principalService;
+
+    @Mock
+    private FutureCallback<FullHttpResponse> callback;
+    @Captor
+    private ArgumentCaptor<FullHttpResponse> responseCaptor;
+
+    protected void initializeClass(final String yangPath) {
+        // AAA services
+        final var securityManager = new DefaultWebSecurityManager(new AuthenticatingRealm() {
+            @Override
+            protected AuthenticationInfo doGetAuthenticationInfo(final AuthenticationToken token)
+                    throws AuthenticationException {
+                final var principal = ((String) token.getPrincipal());
+                final var credentials = new String(((char[]) token.getCredentials()));
+                if (USERNAME.equals(principal) && PASSWORD.equals(credentials)) {
+                    return new SimpleAuthenticationInfo(principal, credentials, "user");
+                }
+                return null;
+            }
+        });
+        principalService = new AAAShiroPrincipalService(securityManager);
+        final var context = YangParserTestUtils.parseYangResourceDirectory(yangPath);
+        final var schemaService = new FixedDOMSchemaService(context);
+
+        // TODO MountPoint without mocking
+        final var mountPoint = mock(DOMMountPoint.class);
+        when(mountPoint.getIdentifier()).thenReturn(INSTANCE_ID);
+//        when(mountPoint.getService(DOMSchemaService.class)).thenReturn(Optional.of(schemaService));
+
+        final var service = mock(DOMMountPointService.class);
+//        when(service.getMountPoint(INSTANCE_ID)).thenReturn(Optional.of(mountPoint));
+
+        final var mountPointRFC8040 = new MountPointOpenApiGeneratorRFC8040(schemaService, service, "rests");
+        final var openApiGeneratorRFC8040 = new OpenApiGeneratorRFC8040(schemaService, "rests");
+        mountPointRFC8040.getMountPointOpenApi().onMountPointCreated(mountPoint);
+        openApiService = new OpenApiServiceImpl(mountPointRFC8040, openApiGeneratorRFC8040);
+        dispatcher = new OpenApiRequestDispatcher(principalService, openApiService,
+                OPENAPI_BASE_URI, RESTCONF_SERVER_URI);
+    }
+
+    @BeforeEach
+    void beforeEach() {
+        initializeClass("/netty-documents/");
+    }
+
+    @Test
+    void controllerAllDocTest() throws Exception {
+        final var expectedJson = getExpectedDoc("netty-documents/controller-all.json");
+        final var response = dispatch(API_V3_PATH + "/single");
+        final var resultDoc = response.content().toString(StandardCharsets.UTF_8);
+        JSONAssert.assertEquals(expectedJson, resultDoc, JSONCompareMode.NON_EXTENSIBLE);
+    }
+
+    /**
+     * Tests the swagger document that is result of the call to the '/toaster@revision' endpoint.
+     */
+    @ParameterizedTest
+    @MethodSource
+    @Disabled
+    void getDocByModuleTest(final String revision, final String jsonPath) throws Exception {
+        final var expectedJson = getExpectedDoc("netty-documents/" + jsonPath);
+        var uri = API_V3_PATH + "/" + TOASTER + "(" + revision + ")";
+        final var response = dispatch(uri);
+        final var resultDoc = response.content().toString(StandardCharsets.UTF_8);
+        JSONAssert.assertEquals(expectedJson, resultDoc, JSONCompareMode.NON_EXTENSIBLE);
+    }
+
+    private static Stream<Arguments> getDocByModuleTest() {
+        // moduleName, revision, jsonPath
+        return Stream.of(
+            Arguments.of(TOASTER_REV, "controller-toaster.json"),
+            Arguments.of(TOASTER_OLD_REV, "controller-toaster-old.json")
+        );
+    }
+
+    @Test
+    @Disabled
+    void deviceAllDocTest() throws Exception {
+        final var expectedJson = getExpectedDoc("netty-documents/device-all.json");
+        final var response = dispatch(API_V3_PATH + "/mounts/1");
+        final var resultDoc = response.content().toString(StandardCharsets.UTF_8);
+        JSONAssert.assertEquals(expectedJson, resultDoc, JSONCompareMode.NON_EXTENSIBLE);
+    }
+
+    protected static String getExpectedDoc(final String jsonPath) throws Exception {
+        return MAPPER.writeValueAsString(MAPPER.readTree(
+            NettyDocumentTest.class.getClassLoader().getResourceAsStream(jsonPath)));
+    }
+
+    private FullHttpResponse dispatch(final String uri) {
+        return dispatch(new DefaultFullHttpRequest(HttpVersion.HTTP_1_1, HttpMethod.GET, uri));
+    }
+
+    private FullHttpResponse dispatch(final FullHttpRequest request) {
+        dispatcher.dispatch(request, callback);
+        verify(callback, timeout(1000)).onSuccess(responseCaptor.capture());
+        final var response = responseCaptor.getValue();
+        assertNotNull(response);
+        return response;
+    }
+}
