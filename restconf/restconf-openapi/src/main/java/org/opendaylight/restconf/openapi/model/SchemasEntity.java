843bf75fc887131c869ad2eeb0232e5f68125494
Robert Varga <robert.varga@pantheon.tech>
2024-10-09 14:18:00 +0200

    Do not buffer entities in SchemasEntity
    We have access to the generator -- hence we can immediately emit any
    entity we create.
    
    Change-Id: I0023b06cd7dc92c6317e4380dca5a43882538765
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- restconf/restconf-openapi/src/main/java/org/opendaylight/restconf/openapi/model/SchemasEntity.java
+++ restconf/restconf-openapi/src/main/java/org/opendaylight/restconf/openapi/model/SchemasEntity.java
@@ -12,7 +12,6 @@ import static org.opendaylight.restconf.openapi.util.RestDocgenUtil.widthList;
 
 import com.fasterxml.jackson.core.JsonGenerator;
 import java.io.IOException;
-import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
@@ -48,74 +47,72 @@ public final class SchemasEntity extends OpenApiEntity {
     }
 
     @Override
-    public void generate(final @NonNull JsonGenerator generator) throws IOException {
+    public void generate(final JsonGenerator generator) throws IOException {
         generator.writeObjectFieldStart("schemas");
 
         for (var module : modules) {
-            for (var schema : toComponents(module)) {
-                schema.generate(generator);
-            }
+            generateComponents(generator, module);
         }
 
         generator.writeEndObject();
     }
 
-    private ArrayDeque<SchemaEntity> toComponents(final Module module) {
-        final var result = new ArrayDeque<SchemaEntity>();
+    private void generateComponents(final JsonGenerator generator, final Module module) throws IOException {
         final var definitionNames = new DefinitionNames();
         final var stack = SchemaInferenceStack.of(modelContext);
         final var moduleName = module.getName();
         if (isForSingleModule) {
             definitionNames.addUnlinkedName(moduleName + "_module");
         }
+
         final var children = new ArrayList<DataSchemaNode>();
         for (final var rpc : module.getRpcs()) {
             stack.enterSchemaTree(rpc.getQName());
             final var rpcName = rpc.getQName().getLocalName();
+
             final var rpcInput = rpc.getInput();
             if (!rpcInput.getChildNodes().isEmpty()) {
-                final var input = new RpcSchemaEntity(rpcInput, moduleName + "_" + rpcName + INPUT_SUFFIX, null,
-                    OBJECT_TYPE, stack, moduleName, false, definitionNames, width, depth, 0);
-                result.add(input);
+                new RpcSchemaEntity(rpcInput, moduleName + "_" + rpcName + INPUT_SUFFIX, null, OBJECT_TYPE, stack,
+                    moduleName, false, definitionNames, width, depth, 0).generate(generator);
+
                 stack.enterSchemaTree(rpcInput.getQName());
                 for (final var child : rpcInput.getChildNodes()) {
                     if (!children.contains(child)) {
                         children.add(child);
-                        processDataAndActionNodes(child, moduleName, stack, definitionNames, result, moduleName, false,
-                            0);
+                        processDataAndActionNodes(generator, child, moduleName, stack, definitionNames, moduleName,
+                            false, 0);
                     }
                 }
                 stack.exit();
             }
+
             final var rpcOutput = rpc.getOutput();
             if (!rpcOutput.getChildNodes().isEmpty()) {
-                final var output = new RpcSchemaEntity(rpcOutput, moduleName + "_" + rpcName + OUTPUT_SUFFIX, null,
-                    OBJECT_TYPE, stack, moduleName, false, definitionNames, width, depth, 0);
-                result.add(output);
+                new RpcSchemaEntity(rpcOutput, moduleName + "_" + rpcName + OUTPUT_SUFFIX, null, OBJECT_TYPE, stack,
+                    moduleName, false, definitionNames, width, depth, 0).generate(generator);
+
                 stack.enterSchemaTree(rpcOutput.getQName());
                 for (final var child : rpcOutput.getChildNodes()) {
                     if (!children.contains(child)) {
                         children.add(child);
-                        processDataAndActionNodes(child, moduleName, stack, definitionNames, result, moduleName, false,
-                            0);
+                        processDataAndActionNodes(generator, child, moduleName, stack, definitionNames, moduleName,
+                            false, 0);
                     }
                 }
                 stack.exit();
             }
+
             stack.exit();
         }
 
-        final var childNodes = widthList(module, width);
-        for (final var childNode : childNodes) {
-            processDataAndActionNodes(childNode, moduleName, stack, definitionNames, result, moduleName, true, 0);
+        for (final var childNode : widthList(module, width)) {
+            processDataAndActionNodes(generator, childNode, moduleName, stack, definitionNames, moduleName, true, 0);
         }
-        return result;
     }
 
-    private void processDataAndActionNodes(final DataSchemaNode node, final String title,
-            final SchemaInferenceStack stack, final DefinitionNames definitionNames,
-            final ArrayDeque<SchemaEntity> result, final String parentName, final boolean isParentConfig,
-            final int nodeDepth) {
+    private void processDataAndActionNodes(final JsonGenerator generator, final DataSchemaNode node, final String title,
+            final SchemaInferenceStack stack, final DefinitionNames definitionNames, final String parentName,
+            final boolean isParentConfig, final int nodeDepth) throws IOException {
         if (depth > 0 && nodeDepth + 1 > depth) {
             return;
         }
@@ -125,16 +122,17 @@ public final class SchemasEntity extends OpenApiEntity {
                 // This means schema for this node is already processed
                 return;
             }
-            final var discriminator = definitionNames.pickDiscriminator(node, List.of(newTitle));
-            final var child = new NodeSchemaEntity(node, newTitle, discriminator, OBJECT_TYPE, stack, parentName,
-                isParentConfig, definitionNames, width, depth, nodeDepth + 1);
+            new NodeSchemaEntity(node, newTitle, definitionNames.pickDiscriminator(node, List.of(newTitle)),
+                OBJECT_TYPE, stack, parentName, isParentConfig, definitionNames, width, depth, nodeDepth + 1)
+                    .generate(generator);
+
             final var isConfig = node.isConfiguration() && isParentConfig;
-            result.add(child);
+
             stack.enterSchemaTree(node.getQName());
-            processActions(node, newTitle, stack, definitionNames, result, parentName, 0);
+            processActions(generator, node, newTitle, stack, definitionNames, parentName, 0);
             final var childNodes = widthList((DataNodeContainer) node, width);
             for (final var childNode : childNodes) {
-                processDataAndActionNodes(childNode, newTitle, stack, definitionNames, result, newTitle, isConfig,
+                processDataAndActionNodes(generator, childNode, newTitle, stack, definitionNames, newTitle, isConfig,
                     nodeDepth + 1);
             }
             stack.exit();
@@ -147,31 +145,31 @@ public final class SchemasEntity extends OpenApiEntity {
             stack.enterSchemaTree(caseNode.getQName());
             final var childNodes = widthList(caseNode, width);
             for (final var childNode : childNodes) {
-                processDataAndActionNodes(childNode, title, stack, definitionNames, result, parentName, isParentConfig,
-                    nodeDepth + 1);
+                processDataAndActionNodes(generator, childNode, title, stack, definitionNames, parentName,
+                    isParentConfig, nodeDepth + 1);
             }
             stack.exit(); // Exit the CaseSchemaNode context
             stack.exit(); // Exit the ChoiceSchemaNode context
         }
     }
 
-    private void processActions(final DataSchemaNode node, final String title, final SchemaInferenceStack stack,
-            final DefinitionNames definitionNames, final ArrayDeque<SchemaEntity> result, final String parentName,
-            final int nodeDepth) {
+    private void processActions(final JsonGenerator generator, final DataSchemaNode node, final String title,
+            final SchemaInferenceStack stack, final DefinitionNames definitionNames, final String parentName,
+            final int nodeDepth) throws IOException {
         for (final var actionDef : ((ActionNodeContainer) node).getActions()) {
             stack.enterSchemaTree(actionDef.getQName());
             final var actionName = actionDef.getQName().getLocalName();
+
             final var actionInput = actionDef.getInput();
             if (!actionInput.getChildNodes().isEmpty()) {
-                final var input = new RpcSchemaEntity(actionInput, title + "_" + actionName + INPUT_SUFFIX, null,
-                    OBJECT_TYPE, stack, parentName, false, definitionNames, width, depth, nodeDepth + 1);
-                result.add(input);
+                new RpcSchemaEntity(actionInput, title + "_" + actionName + INPUT_SUFFIX, null, OBJECT_TYPE, stack,
+                    parentName, false, definitionNames, width, depth, nodeDepth + 1).generate(generator);
             }
+
             final var actionOutput = actionDef.getOutput();
             if (!actionOutput.getChildNodes().isEmpty()) {
-                final var output = new RpcSchemaEntity(actionOutput, title + "_" + actionName + OUTPUT_SUFFIX, null,
-                    OBJECT_TYPE, stack, parentName, false, definitionNames, width, depth, nodeDepth + 1);
-                result.add(output);
+                new RpcSchemaEntity(actionOutput, title + "_" + actionName + OUTPUT_SUFFIX, null, OBJECT_TYPE, stack,
+                    parentName, false, definitionNames, width, depth, nodeDepth + 1).generate(generator);
             }
             stack.exit();
         }
