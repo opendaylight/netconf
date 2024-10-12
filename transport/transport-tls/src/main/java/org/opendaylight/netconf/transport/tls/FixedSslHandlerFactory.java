9d80c802bb62f4cea1320c1dc47227f404313c7c
Robert Varga <robert.varga@pantheon.tech>
2024-10-12 23:00:29 +0200

    Switch to using RFC9645 models
    ietf-tls-{client,common,server}.yang have been published in RFC9645 and
    are available from MD-SAL.
    
    Change-Id: If36b3c6070223fc81c34354bbcc6c4ea96289be2
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- transport/transport-tls/src/main/java/org/opendaylight/netconf/transport/tls/FixedSslHandlerFactory.java
+++ transport/transport-tls/src/main/java/org/opendaylight/netconf/transport/tls/FixedSslHandlerFactory.java
@@ -12,8 +12,8 @@ import static java.util.Objects.requireNonNull;
 import io.netty.handler.ssl.SslContext;
 import java.net.SocketAddress;
 import org.opendaylight.netconf.transport.api.UnsupportedConfigurationException;
-import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.tls.client.rev240208.TlsClientGrouping;
-import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.tls.server.rev240208.TlsServerGrouping;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.tls.client.rev241010.TlsClientGrouping;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.tls.server.rev241010.TlsServerGrouping;
 
 public final class FixedSslHandlerFactory extends SslHandlerFactory {
     private final SslContext sslContext;
