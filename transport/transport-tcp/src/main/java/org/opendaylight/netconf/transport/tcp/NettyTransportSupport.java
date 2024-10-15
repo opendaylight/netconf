daf0561b874ea94f351941d985ee59c408a9ee05
Robert Varga <robert.varga@pantheon.tech>
2024-10-15 18:23:11 +0200

    Move NettyImpl to transport-api
    We need to share this basic support between transport-tcp and
    transport-udp. Move it to a common place.
    
    Change-Id: Ie52a6365a9aae28f9509eeab6be260db3ac97be8
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- transport/transport-tcp/src/main/java/org/opendaylight/netconf/transport/tcp/NettyTransportSupport.java
+++ transport/transport-tcp/src/main/java/org/opendaylight/netconf/transport/tcp/NettyTransportSupport.java
@@ -14,14 +14,14 @@ import io.netty.bootstrap.Bootstrap;
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.channel.ChannelOption;
 import io.netty.channel.EventLoopGroup;
-import io.netty.channel.epoll.Epoll;
 import io.netty.channel.socket.DatagramChannel;
 import io.netty.channel.socket.ServerSocketChannel;
 import io.netty.channel.socket.SocketChannel;
 import org.eclipse.jdt.annotation.NonNullByDefault;
 import org.eclipse.jdt.annotation.Nullable;
+import org.opendaylight.netconf.transport.api.NettyImpl;
+import org.opendaylight.netconf.transport.api.NettyImpl.TcpKeepaliveOptions;
 import org.opendaylight.netconf.transport.api.UnsupportedConfigurationException;
-import org.opendaylight.netconf.transport.tcp.AbstractNettyImpl.TcpKeepaliveOptions;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.tcp.common.rev241010.tcp.common.grouping.Keepalives;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -33,7 +33,7 @@ import org.slf4j.LoggerFactory;
 @NonNullByDefault
 public final class NettyTransportSupport {
     private static final Logger LOG = LoggerFactory.getLogger(NettyTransportSupport.class);
-    private static final AbstractNettyImpl IMPL = Epoll.isAvailable() ? new EpollNettyImpl() : NioNettyImpl.INSTANCE;
+    private static final NettyImpl IMPL = NettyImpl.instance();
 
     static {
         LOG.info("Netty transport backed by {}", IMPL);
