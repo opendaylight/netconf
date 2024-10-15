bab4b09a97faf95f0161854f30ddcfd7534827ad
Robert Varga <robert.varga@pantheon.tech>
2024-10-15 18:10:27 +0200

    Disconnect AbstractNettyImpl from ietf-tcp-common
    The Netty implementation should not have direct bindings to a concrete
    model, so it can be reused between transport-{tcp,udp}.
    
    Change-Id: I447163b7db2d2c1bfccae9042c661e3c2d999bfb
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- transport/transport-tcp/src/main/java/org/opendaylight/netconf/transport/tcp/EpollNettyImpl.java
+++ transport/transport-tcp/src/main/java/org/opendaylight/netconf/transport/tcp/EpollNettyImpl.java
@@ -7,9 +7,6 @@
  */
 package org.opendaylight.netconf.transport.tcp;
 
-import io.netty.bootstrap.Bootstrap;
-import io.netty.bootstrap.ServerBootstrap;
-import io.netty.channel.ChannelOption;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.epoll.EpollChannelOption;
 import io.netty.channel.epoll.EpollDatagramChannel;
@@ -17,11 +14,14 @@ import io.netty.channel.epoll.EpollEventLoopGroup;
 import io.netty.channel.epoll.EpollServerSocketChannel;
 import io.netty.channel.epoll.EpollSocketChannel;
 import java.util.concurrent.ThreadFactory;
+import org.eclipse.jdt.annotation.NonNull;
 import org.eclipse.jdt.annotation.NonNullByDefault;
-import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.tcp.common.rev241010.tcp.common.grouping.Keepalives;
 
 @NonNullByDefault
 final class EpollNettyImpl extends AbstractNettyImpl {
+    private static final TcpKeepaliveOptions KEEPALIVE_OPTIONS = new TcpKeepaliveOptions(
+        EpollChannelOption.TCP_KEEPCNT, EpollChannelOption.TCP_KEEPIDLE, EpollChannelOption.TCP_KEEPINTVL);
+
     @Override
     Class<EpollDatagramChannel> datagramChannelClass() {
         return EpollDatagramChannel.class;
@@ -43,26 +43,8 @@ final class EpollNettyImpl extends AbstractNettyImpl {
     }
 
     @Override
-    boolean supportsKeepalives() {
-        return true;
-    }
-
-    @Override
-    void configureKeepalives(final Bootstrap bootstrap, final Keepalives keepalives) {
-        bootstrap
-            .option(ChannelOption.SO_KEEPALIVE, Boolean.TRUE)
-            .option(EpollChannelOption.TCP_KEEPIDLE, keepalives.requireIdleTime().toJava())
-            .option(EpollChannelOption.TCP_KEEPCNT, keepalives.requireMaxProbes().toJava())
-            .option(EpollChannelOption.TCP_KEEPINTVL, keepalives.requireProbeInterval().toJava());
-    }
-
-    @Override
-    void configureKeepalives(final ServerBootstrap bootstrap, final Keepalives keepalives) {
-        bootstrap
-            .childOption(ChannelOption.SO_KEEPALIVE, Boolean.TRUE)
-            .childOption(EpollChannelOption.TCP_KEEPIDLE, keepalives.requireIdleTime().toJava())
-            .childOption(EpollChannelOption.TCP_KEEPCNT, keepalives.requireMaxProbes().toJava())
-            .childOption(EpollChannelOption.TCP_KEEPINTVL, keepalives.requireProbeInterval().toJava());
+    @NonNull TcpKeepaliveOptions keepaliveOptions() {
+        return KEEPALIVE_OPTIONS;
     }
 
     @Override
