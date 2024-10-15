880655b21ca63e301a397a2e65d98b87c0cae9c0
Robert Varga <robert.varga@pantheon.tech>
2024-10-15 23:17:40 +0200

    WIP: Add UDPTransportStack
    // FIXME: finish this up
    
    Change-Id: Ife89ca46a7dbe7ff451fcfb350e4d20ad8b66497
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ transport/transport-udp/src/main/java/org/opendaylight/netconf/transport/udp/UDPTransportChannel.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2022 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.netconf.transport.udp;
+
+import io.netty.channel.Channel;
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.opendaylight.netconf.transport.api.TransportChannel;
+import org.opendaylight.netconf.transport.spi.AbstractUnderlayTransportChannel;
+
+/**
+ * A TCP {@link TransportChannel}.
+ */
+@NonNullByDefault
+public final class UDPTransportChannel extends AbstractUnderlayTransportChannel {
+    UDPTransportChannel(final Channel channel) {
+        super(channel);
+    }
+}
