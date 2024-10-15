d6b03f4837a890e868deabac260005c12c787c25
Robert Varga <robert.varga@pantheon.tech>
2024-10-15 23:36:18 +0200

    Introduce transport.spi
    We have a number of implementation-internal classes living in
    transport.api. Create transport.spi, so that implementation details are
    in a different package.
    
    Change-Id: I4701c632dc41f8202214068495711c640acd49a4
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- transport/transport-api/src/main/java/org/opendaylight/netconf/transport/spi/EpollNettyImpl.java
+++ transport/transport-api/src/main/java/org/opendaylight/netconf/transport/spi/EpollNettyImpl.java
@@ -5,7 +5,7 @@
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
-package org.opendaylight.netconf.transport.api;
+package org.opendaylight.netconf.transport.spi;
 
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.epoll.EpollChannelOption;
