ca09a978bb20830ecbb2d6037dd5c665244f0be4
Robert Varga <robert.varga@pantheon.tech>
2024-10-17 16:00:08 +0200

    Move socketAddressOf() methods
    These utility methods are universally useful, make sure they are
    available to all stacks.
    
    Change-Id: Id8b35230078f8cbfb0a864cb62353116ef76013a
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- transport/transport-tcp/src/main/java/org/opendaylight/netconf/transport/tcp/TCPTransportStack.java
+++ transport/transport-tcp/src/main/java/org/opendaylight/netconf/transport/tcp/TCPTransportStack.java
@@ -7,14 +7,9 @@
  */
 package org.opendaylight.netconf.transport.tcp;
 
-import java.net.InetSocketAddress;
 import org.opendaylight.netconf.transport.api.AbstractTransportStack;
 import org.opendaylight.netconf.transport.api.TransportChannelListener;
 import org.opendaylight.netconf.transport.api.TransportStack;
-import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev130715.Host;
-import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev130715.IetfInetUtil;
-import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev130715.IpAddress;
-import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev130715.PortNumber;
 
 /**
  * Base class for TCP-based {@link TransportStack}s.
@@ -23,19 +18,5 @@ public abstract sealed class TCPTransportStack extends AbstractTransportStack<TC
         permits TCPClient, TCPServer {
     TCPTransportStack(final TransportChannelListener<? super TCPTransportChannel> listener) {
         super(listener);
-    }
-
-    static final InetSocketAddress socketAddressOf(final Host host, final PortNumber port) {
-        final var addr = host.getIpAddress();
-        return addr != null ? socketAddressOf(addr, port)
-            : InetSocketAddress.createUnresolved(host.getDomainName().getValue(), port.getValue().toJava());
-    }
-
-    static final InetSocketAddress socketAddressOf(final IpAddress addr, final PortNumber port) {
-        final int portNum = port == null ? 0 : port.getValue().toJava();
-        if (addr == null) {
-            return port == null ? null : new InetSocketAddress(portNum);
-        }
-        return new InetSocketAddress(IetfInetUtil.inetAddressFor(addr), portNum);
     }
 }
