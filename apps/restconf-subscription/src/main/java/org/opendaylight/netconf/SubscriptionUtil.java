a3536e52f7179c3f346d3c9ea6548029e3cbc87e
Samuel Schneider <samuel.schneider@pantheon.tech>
2024-10-22 09:58:40 +0200

    WIP: Implement subscription RPCs
    JIRA: NETCONF-714
    Change-Id: I71357f48a554e6f43ad30d12c724184daaa63b60
    Signed-off-by: Samuel Schneider <samuel.schneider@pantheon.tech>

--- apps/restconf-subscription/src/main/java/org/opendaylight/netconf/SubscriptionUtil.java
+++ apps/restconf-subscription/src/main/java/org/opendaylight/netconf/SubscriptionUtil.java
@@ -8,8 +8,20 @@
 package org.opendaylight.netconf;
 
 import java.util.concurrent.atomic.AtomicLong;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.subscribed.notifications.rev190909.Subscriptions;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.subscribed.notifications.rev190909.subscriptions.Subscription;
+import org.opendaylight.yangtools.yang.common.QName;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
 
 public final class SubscriptionUtil {
+    public static final YangInstanceIdentifier SUBSCRIPTIONS = YangInstanceIdentifier.of(
+        YangInstanceIdentifier.NodeIdentifier.create(Subscriptions.QNAME),
+        YangInstanceIdentifier.NodeIdentifier.create(Subscription.QNAME));
+    public static final QName QNAME_ID = QName.create(Subscription.QNAME, "id");
+    public static final QName QNAME_STREAM = QName.create(Subscription.QNAME, "stream");
+    public static final QName QNAME_STRAM_FILTER = QName.create(Subscription.QNAME, "stream-filter-name");
+    public static final QName QNAME_STOP_TIME = QName.create(Subscription.QNAME, "stop-time");
+    public static final QName QNAME_ENCODING = QName.create(Subscription.QNAME, "encoding");
 
     private SubscriptionUtil() {
         // hidden on purpose
