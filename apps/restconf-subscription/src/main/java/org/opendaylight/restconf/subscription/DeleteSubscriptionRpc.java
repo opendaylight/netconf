361cd3b2354c41b72d06e0ddfe84f420bab24c25
Samuel Schneider <samuel.schneider@pantheon.tech>
2024-10-22 09:58:40 +0200

    Implement subscription RPCs
    Implement RPCs for managing RFC 8639 notification subscriptions.
    Create se service for managing subscription data in mdsal
    datastore.
    
    JIRA: NETCONF-714
    Change-Id: I71357f48a554e6f43ad30d12c724184daaa63b60
    Signed-off-by: Samuel Schneider <samuel.schneider@pantheon.tech>

---
+++ apps/restconf-subscription/src/main/java/org/opendaylight/restconf/subscription/DeleteSubscriptionRpc.java
@@ -0,0 +1,127 @@
+/*
+ * Copyright (c) 2024 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.restconf.subscription;
+
+import static java.util.Objects.requireNonNull;
+
+import com.google.common.util.concurrent.FutureCallback;
+import com.google.common.util.concurrent.MoreExecutors;
+import java.net.URI;
+import java.time.Instant;
+import java.util.NoSuchElementException;
+import javax.inject.Inject;
+import javax.inject.Singleton;
+import org.opendaylight.mdsal.common.api.CommitInfo;
+import org.opendaylight.restconf.notifications.mdsal.MdsalNotificationService;
+import org.opendaylight.restconf.notifications.mdsal.SubscriptionStateService;
+import org.opendaylight.restconf.server.api.ServerException;
+import org.opendaylight.restconf.server.api.ServerRequest;
+import org.opendaylight.restconf.server.spi.OperationInput;
+import org.opendaylight.restconf.server.spi.RpcImplementation;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.subscribed.notifications.rev190909.DeleteSubscription;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.subscribed.notifications.rev190909.DeleteSubscriptionInput;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.subscribed.notifications.rev190909.DeleteSubscriptionOutput;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.subscribed.notifications.rev190909.subscriptions.Subscription;
+import org.opendaylight.yangtools.yang.common.ErrorTag;
+import org.opendaylight.yangtools.yang.common.ErrorType;
+import org.opendaylight.yangtools.yang.common.QName;
+import org.opendaylight.yangtools.yang.common.Uint32;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.NodeIdentifier;
+import org.opendaylight.yangtools.yang.data.api.schema.ContainerNode;
+import org.opendaylight.yangtools.yang.data.spi.node.ImmutableNodes;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * RESTCONF implementation of {@link DeleteSubscription}.
+ */
+@Singleton
+@Component(service = RpcImplementation.class)
+public class DeleteSubscriptionRpc extends RpcImplementation {
+    private static final NodeIdentifier SUBSCRIPTION_ID =
+        NodeIdentifier.create(QName.create(DeleteSubscriptionInput.QNAME, "id").intern());
+
+    private static final Logger LOG = LoggerFactory.getLogger(DeleteSubscriptionRpc.class);
+
+    private final MdsalNotificationService mdsalService;
+    private final SubscriptionStateService subscriptionStateService;
+    private final SubscriptionStateMachine stateMachine;
+
+    @Inject
+    @Activate
+    public DeleteSubscriptionRpc(@Reference final MdsalNotificationService mdsalService,
+            @Reference final SubscriptionStateService subscriptionStateService,
+            @Reference final SubscriptionStateMachine stateMachine) {
+        super(DeleteSubscription.QNAME);
+        this.mdsalService = requireNonNull(mdsalService);
+        this.subscriptionStateService = requireNonNull(subscriptionStateService);
+        this.stateMachine = requireNonNull(stateMachine);
+    }
+
+    @Override
+    public void invoke(ServerRequest<ContainerNode> request, URI restconfURI, OperationInput input) {
+        final var body = input.input();
+        final Uint32 id;
+        try {
+            id = leaf(body, SUBSCRIPTION_ID, Uint32.class);
+        } catch (IllegalArgumentException e) {
+            request.completeWith(new ServerException(ErrorType.APPLICATION, ErrorTag.BAD_ELEMENT, e));
+            return;
+        }
+        if (id == null) {
+            request.completeWith(new ServerException(ErrorType.APPLICATION, ErrorTag.MISSING_ELEMENT,
+                "No ID specified."));
+            return;
+        }
+        try {
+            final var state = stateMachine.getSubscriptionState(id);
+            if (state != SubscriptionState.ACTIVE && state != SubscriptionState.SUSPENDED) {
+                request.completeWith(new ServerException(ErrorType.APPLICATION, ErrorTag.BAD_ELEMENT,
+                    "There is no active or suspended subscription with given ID."));
+                return;
+            }
+        } catch (NoSuchElementException e) {
+            request.completeWith(new ServerException(ErrorType.APPLICATION, ErrorTag.MISSING_ELEMENT,
+                "No subscription with given ID."));
+            return;
+        }
+
+        if (stateMachine.getSubscriptionSession(id) != request.session()) {
+            request.completeWith(new ServerException(ErrorType.APPLICATION, ErrorTag.BAD_ELEMENT,
+                "Subscription with given id does not exist on this session"));
+            return;
+        }
+        mdsalService.deleteSubscription(SubscriptionUtil.SUBSCRIPTIONS.node(YangInstanceIdentifier
+                .NodeIdentifierWithPredicates.of(Subscription.QNAME, SubscriptionUtil.QNAME_ID, id)))
+            .addCallback(new FutureCallback<CommitInfo>() {
+                @Override
+                public void onSuccess(CommitInfo result) {
+                    request.completeWith(ImmutableNodes.newContainerBuilder()
+                        .withNodeIdentifier(NodeIdentifier.create(DeleteSubscriptionOutput.QNAME))
+                        .build());
+                    stateMachine.moveTo(id, SubscriptionState.END);
+                    try {
+                        subscriptionStateService.subscriptionTerminated(Instant.now().toString(),
+                            id.longValue(), "subscription-delete");
+                    } catch (InterruptedException e) {
+                        LOG.warn("Could not send subscription terminated notification: {}", e.getMessage());
+                    }
+                }
+
+                @Override
+                public void onFailure(Throwable throwable) {
+                    request.completeWith(new ServerException(ErrorType.APPLICATION,
+                        ErrorTag.OPERATION_FAILED, throwable.getMessage()));
+                }
+            }, MoreExecutors.directExecutor());
+    }
+}
---
+++ apps/restconf-subscription/src/test/java/org/opendaylight/restconf/subscription/DeleteSubscriptionRpcTest.java
@@ -0,0 +1,131 @@
+/*
+ * Copyright (c) 2024 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.restconf.subscription;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.verify;
+
+import java.net.URI;
+import java.util.NoSuchElementException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.opendaylight.mdsal.common.api.CommitInfo;
+import org.opendaylight.mdsal.common.api.LogicalDatastoreType;
+import org.opendaylight.mdsal.dom.api.DOMDataBroker;
+import org.opendaylight.mdsal.dom.api.DOMDataTreeWriteTransaction;
+import org.opendaylight.restconf.notifications.mdsal.MdsalNotificationService;
+import org.opendaylight.restconf.notifications.mdsal.SubscriptionStateService;
+import org.opendaylight.restconf.server.api.DatabindPath;
+import org.opendaylight.restconf.server.api.ServerException;
+import org.opendaylight.restconf.server.api.TransportSession;
+import org.opendaylight.restconf.server.api.testlib.CompletingServerRequest;
+import org.opendaylight.restconf.server.spi.OperationInput;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.subscribed.notifications.rev190909.DeleteSubscriptionInput;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.subscribed.notifications.rev190909.DeleteSubscriptionOutput;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.subscribed.notifications.rev190909.subscriptions.Subscription;
+import org.opendaylight.yangtools.yang.common.Uint32;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
+import org.opendaylight.yangtools.yang.data.api.schema.ContainerNode;
+import org.opendaylight.yangtools.yang.data.spi.node.ImmutableNodes;
+
+@ExtendWith(MockitoExtension.class)
+public class DeleteSubscriptionRpcTest {
+    private static final URI RESTCONF_URI = URI.create("/restconf/");
+    private static final Uint32 ID = Uint32.valueOf(2147483648L);
+    private static final YangInstanceIdentifier.NodeIdentifierWithPredicates IDENTIFIER =
+        YangInstanceIdentifier.NodeIdentifierWithPredicates.of(Subscription.QNAME, SubscriptionUtil.QNAME_ID, ID);
+    private static final ContainerNode INPUT = ImmutableNodes.newContainerBuilder()
+        .withNodeIdentifier(YangInstanceIdentifier.NodeIdentifier.create(DeleteSubscriptionInput.QNAME))
+        .withChild(ImmutableNodes.leafNode(SubscriptionUtil.QNAME_ID, ID))
+        .build();
+
+    @Mock
+    private DOMDataBroker dataBroker;
+    @Mock
+    private SubscriptionStateService subscriptionStateService;
+    @Mock
+    private DatabindPath.Rpc operationPath;
+    @Mock
+    private DOMDataTreeWriteTransaction writeTx;
+    @Mock
+    private CompletingServerRequest<ContainerNode> request;
+    @Mock
+    private SubscriptionStateMachine stateMachine;
+    @Mock
+    private TransportSession session;
+    @Captor
+    private ArgumentCaptor<ServerException> response;
+
+    private MdsalNotificationService mdsalService;
+    private DeleteSubscriptionRpc rpc;
+
+    @BeforeEach
+    public void before() {
+        mdsalService = new MdsalNotificationService(dataBroker);
+        rpc = new DeleteSubscriptionRpc(mdsalService, subscriptionStateService, stateMachine);
+    }
+
+    @Test
+    void deleteSubscriptionTest() {
+        final var responseBuilder = ImmutableNodes.newContainerBuilder()
+            .withNodeIdentifier(YangInstanceIdentifier.NodeIdentifier.create(DeleteSubscriptionOutput.QNAME))
+            .build();
+
+        doReturn(writeTx).when(dataBroker).newWriteOnlyTransaction();
+        doReturn(CommitInfo.emptyFluentFuture()).when(writeTx).commit();
+        doReturn(session).when(request).session();
+        doReturn(session).when(stateMachine).getSubscriptionSession(ID);
+        doReturn(SubscriptionState.ACTIVE).when(stateMachine).getSubscriptionState(ID);
+
+        rpc.invoke(request, RESTCONF_URI, new OperationInput(operationPath, INPUT));
+        verify(writeTx).delete(eq(LogicalDatastoreType.OPERATIONAL),
+            eq(SubscriptionUtil.SUBSCRIPTIONS.node(IDENTIFIER)));
+        verify(request).completeWith(eq(responseBuilder));
+    }
+
+    @Test
+    void deleteSubscriptionWrongSessionTest() {
+        doReturn(session).when(request).session();
+        // return session different from request session
+        doReturn(null).when(stateMachine).getSubscriptionSession(ID);
+        doReturn(SubscriptionState.ACTIVE).when(stateMachine).getSubscriptionState(ID);
+
+        rpc.invoke(request, RESTCONF_URI, new OperationInput(operationPath, INPUT));
+        verify(request).completeWith(response.capture());
+        assertEquals("Subscription with given id does not exist on this session",
+            response.getValue().getMessage());
+    }
+
+    @Test
+    void deleteSubscriptionWrongIDTest() {
+        doThrow(new NoSuchElementException("No such subscription was registered.")).when(stateMachine)
+            .getSubscriptionState(ID);
+
+        rpc.invoke(request, RESTCONF_URI, new OperationInput(operationPath, INPUT));
+        verify(request).completeWith(response.capture());
+        assertEquals("No subscription with given ID.", response.getValue().getMessage());
+    }
+
+    @Test
+    void deleteSubscriptionAlreadyEndedTest() {
+        doReturn(SubscriptionState.END).when(stateMachine).getSubscriptionState(ID);
+
+        rpc.invoke(request, RESTCONF_URI, new OperationInput(operationPath, INPUT));
+        verify(request).completeWith(response.capture());
+        assertEquals("There is no active or suspended subscription with given ID.",
+            response.getValue().getMessage());
+    }
+}
