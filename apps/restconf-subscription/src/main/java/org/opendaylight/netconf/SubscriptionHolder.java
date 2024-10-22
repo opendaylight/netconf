f9d7e94db35aad527d7ed05662b72682e27a0452
Samuel Schneider <samuel.schneider@pantheon.tech>
2024-10-22 09:58:40 +0200

    WIP: Implement subscription RPCs
    JIRA: NETCONF-714
    Change-Id: I71357f48a554e6f43ad30d12c724184daaa63b60
    Signed-off-by: Samuel Schneider <samuel.schneider@pantheon.tech>

---
+++ apps/restconf-subscription/src/main/java/org/opendaylight/netconf/SubscriptionHolder.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2024 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.netconf;
+
+import org.opendaylight.restconf.notifications.mdsal.MdsalNotificationService;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.subscribed.notifications.rev190909.subscriptions.Subscription;
+import org.opendaylight.yangtools.concepts.Registration;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
+
+public record SubscriptionHolder(
+    Subscription subscription,
+    MdsalNotificationService mdsalNotificationService) implements Registration {
+
+    @Override
+    public void close() {
+        // TODO: subscription state change notification
+        mdsalNotificationService.deleteSubscription(SubscriptionUtil.SUBSCRIPTIONS.node(
+            YangInstanceIdentifier.NodeIdentifierWithPredicates.of(
+                Subscription.QNAME, SubscriptionUtil.QNAME_ID, subscription.getId().getValue().longValue())));
+    }
+}
