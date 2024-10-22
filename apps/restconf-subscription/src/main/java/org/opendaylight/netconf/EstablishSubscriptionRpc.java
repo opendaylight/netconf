cd0bbbe08db3e986c429d39c8bd42e566910d966
Samuel Schneider <samuel.schneider@pantheon.tech>
2024-10-22 09:58:40 +0200

    WIP: Implement subscription RPCs
    JIRA: NETCONF-714
    Change-Id: I71357f48a554e6f43ad30d12c724184daaa63b60
    Signed-off-by: Samuel Schneider <samuel.schneider@pantheon.tech>

---
+++ apps/restconf-subscription/src/main/java/org/opendaylight/netconf/EstablishSubscriptionRpc.java
@@ -0,0 +1,169 @@
+/*
+ * Copyright (c) 2024 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.netconf;
+
+import static java.util.Objects.requireNonNull;
+import static org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.NodeIdentifier;
+
+import com.google.common.util.concurrent.FutureCallback;
+import com.google.common.util.concurrent.MoreExecutors;
+import java.net.URI;
+import java.util.concurrent.atomic.AtomicLong;
+import javax.inject.Inject;
+import javax.inject.Singleton;
+import org.opendaylight.mdsal.common.api.CommitInfo;
+import org.opendaylight.restconf.notification.mdsal.MdsalNotificationaService;
+import org.opendaylight.restconf.server.api.ServerException;
+import org.opendaylight.restconf.server.api.ServerRequest;
+import org.opendaylight.restconf.server.spi.OperationInput;
+import org.opendaylight.restconf.server.spi.RestconfStream;
+import org.opendaylight.restconf.server.spi.RpcImplementation;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.subscribed.notifications.rev190909.Encoding;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.subscribed.notifications.rev190909.EstablishSubscription;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.subscribed.notifications.rev190909.EstablishSubscriptionInput;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.subscribed.notifications.rev190909.EstablishSubscriptionOutput;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.subscribed.notifications.rev190909.SubscriptionId;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.subscribed.notifications.rev190909.Subscriptions;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.subscribed.notifications.rev190909.subscription.policy.dynamic.Stream1Builder;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.subscribed.notifications.rev190909.subscription.policy.modifiable.target.StreamBuilder;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.subscribed.notifications.rev190909.subscription.policy.modifiable.target.stream.stream.filter.ByReferenceBuilder;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.subscribed.notifications.rev190909.subscriptions.Subscription;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.subscribed.notifications.rev190909.subscriptions.SubscriptionBuilder;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.yang.types.rev130715.DateAndTime;
+import org.opendaylight.yangtools.yang.common.ErrorTag;
+import org.opendaylight.yangtools.yang.common.ErrorType;
+import org.opendaylight.yangtools.yang.common.QName;
+import org.opendaylight.yangtools.yang.common.Uint32;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.NodeIdentifierWithPredicates;
+import org.opendaylight.yangtools.yang.data.api.schema.ContainerNode;
+import org.opendaylight.yangtools.yang.data.spi.node.ImmutableNodes;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * RESTCONF implementation of {@link EstablishSubscription}.
+ */
+@Singleton
+@Component(service = RpcImplementation.class)
+public class EstablishSubscriptionRpc extends RpcImplementation {
+    private static final NodeIdentifier SUBSCRIPTION_STREAM_FILTER_NAME =
+        NodeIdentifier.create(QName.create(EstablishSubscriptionInput.QNAME, "stream-filter-name").intern());
+    private static final NodeIdentifier SUBSCRIPTION_STREAM =
+        NodeIdentifier.create(QName.create(EstablishSubscriptionInput.QNAME, "stream").intern());
+    //private static final NodeIdentifier SUBSCRIPTION_STREAM_SUBTREE_FILTER =
+    //    NodeIdentifier.create(QName.create(EstablishSubscriptionInput.QNAME, "stream-subtree-filter").intern());
+    private static final NodeIdentifier SUBSCRIPTION_STOP_TIME =
+        NodeIdentifier.create(QName.create(EstablishSubscriptionInput.QNAME, "stop-time").intern());
+    private static final NodeIdentifier SUBSCRIPTION_ENCODING =
+        NodeIdentifier.create(QName.create(EstablishSubscriptionInput.QNAME, "encoding").intern());
+    private static final NodeIdentifier ESTABLISH_SUBSCRIPTION_OUTPUT =
+        NodeIdentifier.create(EstablishSubscriptionOutput.QNAME);
+    private static final NodeIdentifier OUTPUT_ID =
+        NodeIdentifier.create(QName.create(EstablishSubscriptionOutput.QNAME, "id").intern());
+    private static final YangInstanceIdentifier SUBSCRIPTIONS = YangInstanceIdentifier.of(
+        NodeIdentifier.create(Subscriptions.QNAME),
+        NodeIdentifier.create(Subscription.QNAME));
+
+    private static final QName QNAME_ID = QName.create(Subscription.QNAME, "id").intern();
+    private static final QName QNAME_STRAM_FILTER = QName.create(Subscription.QNAME, "stream-filter-name").intern();
+    private static final QName QNAME_STOP_TIME = QName.create(Subscription.QNAME, "stop-time").intern();
+    private static final QName QNAME_ENCODING = QName.create(Subscription.QNAME, "encoding").intern();
+
+    // Start subscription ID generation from the upper half of uint32 (2,147,483,648)
+    private static final long INITIAL_SUBSCRIPTION_ID = 2147483648L;
+
+    private final AtomicLong subscriptionIdCounter = new AtomicLong(INITIAL_SUBSCRIPTION_ID);
+    private final RestconfStream.Registry streamRegistry;
+    private final MdsalNotificationaService mdsalService;
+
+    @Inject
+    @Activate
+    public EstablishSubscriptionRpc(@Reference final RestconfStream.Registry streamRegistry,
+            @Reference final MdsalNotificationaService mdsalService) {
+        super(EstablishSubscription.QNAME);
+        this.streamRegistry = requireNonNull(streamRegistry);
+        this.mdsalService = requireNonNull(mdsalService);
+    }
+
+    @Override
+    public void invoke(final ServerRequest<ContainerNode> request, final URI restconfURI, final OperationInput input) {
+        final var body = input.input();
+        final var streamName = leaf(body, SUBSCRIPTION_STREAM, String.class);
+        final var streamFilter = leaf(body, SUBSCRIPTION_STREAM_FILTER_NAME, String.class);
+        //final var streamSubtree = leaf(body, SUBSCRIPTION_STREAM_SUBTREE_FILTER, String.class);
+        final var stopTime = leaf(body, SUBSCRIPTION_STOP_TIME, DateAndTime.class);
+        final var encoding = leaf(body, SUBSCRIPTION_ENCODING, Encoding.class);
+        final var session = request.session();
+
+        final var builder = new SubscriptionBuilder();
+        final var streamBuilder = new StreamBuilder();
+
+        if (streamName == null) {
+            request.completeWith(new ServerException(ErrorType.APPLICATION, ErrorTag.MISSING_ELEMENT,
+                "No stream specified"));
+            return;
+        }
+        final var stream = streamRegistry.lookupStream(streamName);
+        if (stream == null) {
+            request.completeWith(new ServerException(ErrorType.APPLICATION, ErrorTag.INVALID_VALUE,
+                "%s refers to an unknown stream", streamName));
+            return;
+        }
+        final var stream1Builder = new Stream1Builder();
+        stream1Builder.setStream(streamName);
+        streamBuilder.addAugmentation(stream1Builder.build());
+        if (streamFilter != null) {
+            final var byReferenceBuilder = new ByReferenceBuilder();
+            byReferenceBuilder.setStreamFilterName(streamFilter);
+            streamBuilder.setStreamFilter(byReferenceBuilder.build());
+        }
+        //if (streamSubtree != null) {
+            //TODO: parse anydata filter, rfc6241? https://www.rfc-editor.org/rfc/rfc8650#name-filter-example
+            // {@link StreamSubtreeFilter}.
+        //}
+        if (stopTime != null) {
+            builder.setStopTime(stopTime);
+        }
+        if (encoding != null) {
+            builder.setEncoding(encoding);
+        }
+        final var id =  Uint32.valueOf(SubscriptionUtil.generateSubscriptionId(subscriptionIdCounter));
+        builder.setId(new SubscriptionId(id));
+
+        final var subscription = new SubscriptionHolder(builder.build(), mdsalService);
+        session.registerResource(subscription);
+
+        final var node = ImmutableNodes.newMapEntryBuilder()
+            .withNodeIdentifier(NodeIdentifierWithPredicates.of(Subscription.QNAME, QNAME_ID, id))
+            .withChild(ImmutableNodes.leafNode(QNAME_ID, id))
+            .withChild(ImmutableNodes.leafNode(QNAME_STRAM_FILTER, streamFilter))
+            .withChild(ImmutableNodes.leafNode(QNAME_STOP_TIME, stopTime))
+            .withChild(ImmutableNodes.leafNode(QNAME_ENCODING, encoding))
+            .build();
+
+        mdsalService.writeSubscription(SUBSCRIPTIONS.node(node.name()), node)
+            .addCallback(new FutureCallback<CommitInfo>() {
+                @Override
+                public void onSuccess(CommitInfo result) {
+                    request.completeWith(ImmutableNodes.newContainerBuilder()
+                        .withNodeIdentifier(ESTABLISH_SUBSCRIPTION_OUTPUT)
+                        .withChild(ImmutableNodes.leafNode(OUTPUT_ID, id))
+                        //TODO: uri
+                        .build());
+                }
+
+                @Override
+                public void onFailure(Throwable throwable) {
+                    request.completeWith(new ServerException(ErrorType.APPLICATION, ErrorTag.OPERATION_FAILED,
+                        throwable.getMessage()));
+                }
+            }, MoreExecutors.directExecutor());
+    }
+}
---
+++ apps/restconf-subscription/src/main/java/org/opendaylight/netconf/KillSubscriptionRpc.java
@@ -0,0 +1,91 @@
+/*
+ * Copyright (c) 2024 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.netconf;
+
+import static java.util.Objects.requireNonNull;
+
+import com.google.common.util.concurrent.FutureCallback;
+import com.google.common.util.concurrent.MoreExecutors;
+import java.net.URI;
+import javax.inject.Inject;
+import javax.inject.Singleton;
+import org.opendaylight.mdsal.common.api.CommitInfo;
+import org.opendaylight.restconf.notification.mdsal.MdsalNotificationaService;
+import org.opendaylight.restconf.server.api.ServerException;
+import org.opendaylight.restconf.server.api.ServerRequest;
+import org.opendaylight.restconf.server.spi.OperationInput;
+import org.opendaylight.restconf.server.spi.RpcImplementation;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.subscribed.notifications.rev190909.KillSubscription;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.subscribed.notifications.rev190909.KillSubscriptionInput;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.subscribed.notifications.rev190909.Subscriptions;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.subscribed.notifications.rev190909.subscriptions.Subscription;
+import org.opendaylight.yangtools.yang.common.ErrorTag;
+import org.opendaylight.yangtools.yang.common.ErrorType;
+import org.opendaylight.yangtools.yang.common.QName;
+import org.opendaylight.yangtools.yang.common.Uint32;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
+import org.opendaylight.yangtools.yang.data.api.schema.ContainerNode;
+import org.opendaylight.yangtools.yang.data.spi.node.ImmutableNodes;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * RESTCONF implementation of {@link KillSubscription}.
+ */
+@Singleton
+@Component(service = RpcImplementation.class)
+public class KillSubscriptionRpc extends RpcImplementation {
+    private static final YangInstanceIdentifier.NodeIdentifier SUBSCRIPTION_ID =
+        YangInstanceIdentifier.NodeIdentifier.create(QName.create(KillSubscriptionInput.QNAME, "id").intern());
+    private static final YangInstanceIdentifier SUBSCRIPTIONS = YangInstanceIdentifier.of(
+        YangInstanceIdentifier.NodeIdentifier.create(Subscriptions.QNAME),
+        YangInstanceIdentifier.NodeIdentifier.create(Subscription.QNAME));
+    private static final QName QNAME_ID = QName.create(Subscription.QNAME, "id").intern();
+
+    private final MdsalNotificationaService mdsalService;
+
+    @Inject
+    @Activate
+    public KillSubscriptionRpc(@Reference final MdsalNotificationaService mdsalService) {
+        super(KillSubscription.QNAME);
+        this.mdsalService = requireNonNull(mdsalService);
+    }
+
+    @Override
+    public void invoke(ServerRequest<ContainerNode> request, URI restconfURI, OperationInput input) {
+        final var body = input.input();
+        final Uint32 id;
+        try {
+            id = leaf(body, SUBSCRIPTION_ID, Uint32.class);
+        } catch (IllegalArgumentException e) {
+            request.completeWith(new ServerException(ErrorType.APPLICATION, ErrorTag.BAD_ELEMENT, e));
+            return;
+        }
+        if (id == null) {
+            request.completeWith(new ServerException(ErrorType.APPLICATION, ErrorTag.MISSING_ELEMENT,
+                "No id specified"));
+            return;
+        }
+
+        mdsalService.deleteSubscription(SUBSCRIPTIONS.node(YangInstanceIdentifier
+                .NodeIdentifierWithPredicates.of(Subscription.QNAME, QNAME_ID, id)))
+            .addCallback(new FutureCallback<CommitInfo>() {
+                @Override
+                public void onSuccess(CommitInfo result) {
+                    request.completeWith(ImmutableNodes.newContainerBuilder().build());
+                }
+
+                @Override
+                public void onFailure(Throwable throwable) {
+                    request.completeWith(new ServerException(ErrorType.APPLICATION, ErrorTag.OPERATION_FAILED,
+                        throwable.getMessage()));
+                }
+            }, MoreExecutors.directExecutor());
+    }
+}
