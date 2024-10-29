5aeda0cc041f88fff1b22afd6f45e6c9dd8634bf
Matej Sramcik <matej.sramcik@pantheon.tech>
2024-10-29 14:11:31 +0100

    Implement subscription state change notifications
    JIRA: NETCONF-714
    Change-Id: I67dcaad79d64223b3d3770e8acb35db44d867da9
    Signed-off-by: Matej Sramcik <matej.sramcik@pantheon.tech>

---
+++ plugins/restconf-notifications-mdsal/src/main/java/org/opendaylight/restconf/notifications/mdsal/StateChangeNotifications.java
@@ -0,0 +1,116 @@
+/*
+ * Copyright (c) 2024 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.restconf.notifications.mdsal;
+
+import com.google.common.util.concurrent.ListenableFuture;
+import javax.inject.Inject;
+import org.opendaylight.mdsal.dom.api.DOMNotification;
+import org.opendaylight.mdsal.dom.api.DOMNotificationPublishService;
+import org.opendaylight.yang.svc.v1.urn.opendaylight.netconf.keystore.rev240708.YangModuleInfoImpl;
+import org.opendaylight.yangtools.yang.common.QName;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.NodeIdentifier;
+import org.opendaylight.yangtools.yang.data.spi.node.ImmutableNodes;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+@Component
+public class StateChangeNotifications {
+    private static final String COMPLETED = "subscription-completed";
+    private static final String RESUMED = "subscription-resumed";
+    private static final String MODIFIED = "subscription-modified";
+    private static final String TERMINATED = "subscription-terminated";
+    private static final String SUSPENDED = "subscription-suspended";
+    private static final String SUBSCRIBED_NOTIFICATION = "ietf-subscribed-notifications";
+    private static final QName EVENT_TIME = YangModuleInfoImpl.qnameOf("eventTime");
+    private static final QName FILTER = YangModuleInfoImpl.qnameOf("stream-xpath-filter");
+    private static final QName ID = YangModuleInfoImpl.qnameOf("id");
+    private static final QName NETCONF_NOTIFICATION = YangModuleInfoImpl.qnameOf("ietf-subscribed-notifications");
+    private static final QName NODE_IDENTIFIER = QName.create("ietf-restconf", "notification");
+    private static final QName URI = YangModuleInfoImpl.qnameOf("uri");
+
+    private final DOMNotificationPublishService service;
+
+    @Inject
+    @Activate
+    public StateChangeNotifications(@Reference final DOMNotificationPublishService publishService) {
+        this.service = publishService;
+    }
+
+
+    public ListenableFuture<?> subscriptionModified(String eventTime, Long id, String uri, String filter)
+        throws InterruptedException {
+        final var node = ImmutableNodes.newContainerBuilder()
+            .withNodeIdentifier(new NodeIdentifier(NODE_IDENTIFIER))
+            .withChild(ImmutableNodes.leafNode(EVENT_TIME, eventTime))
+            .withChild(ImmutableNodes.newContainerBuilder()
+                .withNodeIdentifier(new NodeIdentifier(QName.create(SUBSCRIBED_NOTIFICATION, MODIFIED)))
+                .withChild(ImmutableNodes.leafNode(ID, id))
+                .withChild(ImmutableNodes.leafNode(URI, uri))
+                .withChild(ImmutableNodes.leafNode(FILTER, filter))
+                .withChild(ImmutableNodes.newContainerBuilder()
+                    .withNodeIdentifier(new NodeIdentifier(YangModuleInfoImpl.qnameOf("stream")))
+                    .withChild(ImmutableNodes.leafNode(NETCONF_NOTIFICATION, "NETCONF"))
+                    .build())
+                .build())
+            .build();
+
+        DOMNotification notification = new RestconfNotification(node);
+        return service.putNotification(notification);
+    }
+
+
+    public ListenableFuture<?> subscriptionCompleted(String eventTime, Long id) throws InterruptedException {
+        return subscriptionCompletedResumed(eventTime, id, COMPLETED);
+    }
+
+    public ListenableFuture<?> subscriptionResumed(String eventTime, Long id) throws InterruptedException {
+        return subscriptionCompletedResumed(eventTime, id, RESUMED);
+    }
+
+    private ListenableFuture<?> subscriptionCompletedResumed(String eventTime, Long id, String notificationType)
+        throws InterruptedException {
+        final var node = ImmutableNodes.newContainerBuilder()
+            .withNodeIdentifier(new NodeIdentifier(NODE_IDENTIFIER))
+            .withChild(ImmutableNodes.leafNode(EVENT_TIME, eventTime))
+            .withChild(ImmutableNodes.newContainerBuilder()
+                .withNodeIdentifier(new NodeIdentifier(QName.create(SUBSCRIBED_NOTIFICATION, notificationType)))
+                .withChild(ImmutableNodes.leafNode(ID, id))
+                .build())
+            .build();
+
+        DOMNotification notification = new RestconfNotification(node);
+        return service.putNotification(notification);
+    }
+
+    public ListenableFuture<?> subscriptionTerminated(String eventTime, Long id, String errorId)
+        throws InterruptedException {
+        return subscriptionTerminatedSuspended(eventTime, id, errorId, TERMINATED);
+    }
+
+    public ListenableFuture<?> subscriptionSuspended(String eventTime, Long id, String errorId)
+        throws InterruptedException {
+        return subscriptionTerminatedSuspended(eventTime, id, errorId, SUSPENDED);
+    }
+
+    private ListenableFuture<?> subscriptionTerminatedSuspended(String eventTime, Long id, String errorId,
+        String notificationType) throws InterruptedException {
+        final var node = ImmutableNodes.newContainerBuilder()
+            .withNodeIdentifier(new NodeIdentifier(NODE_IDENTIFIER))
+            .withChild(ImmutableNodes.leafNode(EVENT_TIME, eventTime))
+            .withChild(ImmutableNodes.newContainerBuilder()
+                .withNodeIdentifier(new NodeIdentifier(QName.create(SUBSCRIBED_NOTIFICATION, notificationType)))
+                .withChild(ImmutableNodes.leafNode(ID, id))
+                .withChild(ImmutableNodes.leafNode(YangModuleInfoImpl.qnameOf("error-id"), errorId))
+                .build())
+            .build();
+
+        DOMNotification notification = new RestconfNotification(node);
+        return service.putNotification(notification);
+    }
+}
