cde10c840cc6987ad9353b167cf5bfd620d109ce
Ivan Hrasko <ivan.hrasko@pantheon.tech>
2024-11-04 14:54:42 +0100

    Add subscription resource
    Add web resource providing possibility to stream
    subscribed notifications over SSE at:
    <server>:<port>/subscriptions/<subscription-id>
    
    JIRA: NETCONF-714
    Change-Id: I8667266afae97e7efa6e08ea888a681d60eee769
    Signed-off-by: Ivan Hrasko <ivan.hrasko@pantheon.tech>
    Signed-off-by: Oleksandr Zharov <oleksandr.zharov@pantheon.tech>

---
+++ restconf/restconf-notifications/src/main/java/org/opendaylight/restconf/notifications/SubscriptionResourceInstance.java
@@ -0,0 +1,145 @@
+/*
+ * Copyright (c) 2024 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.restconf.notifications;
+
+import io.netty.channel.ChannelHandler;
+import io.netty.handler.codec.http.HttpHeaderNames;
+import io.netty.handler.codec.http.HttpHeaderValues;
+import io.netty.handler.codec.http.HttpHeaders;
+import io.netty.handler.codec.http.HttpResponseStatus;
+import io.netty.handler.codec.http.QueryStringDecoder;
+import io.netty.handler.codec.http.ReadOnlyHttpHeaders;
+import java.io.UnsupportedEncodingException;
+import java.net.URI;
+import javax.xml.xpath.XPathExpressionException;
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.eclipse.jdt.annotation.Nullable;
+import org.opendaylight.netconf.transport.http.EmptyResponse;
+import org.opendaylight.netconf.transport.http.EventStreamResponse;
+import org.opendaylight.netconf.transport.http.HeadersResponse;
+import org.opendaylight.netconf.transport.http.ImplementedMethod;
+import org.opendaylight.netconf.transport.http.Response;
+import org.opendaylight.netconf.transport.http.SegmentPeeler;
+import org.opendaylight.netconf.transport.http.rfc6415.WebHostResourceInstance;
+import org.opendaylight.netconf.transport.http.rfc6415.XRD;
+import org.opendaylight.restconf.api.QueryParameters;
+import org.opendaylight.restconf.server.ChannelSender;
+import org.opendaylight.restconf.server.api.EventStreamGetParams;
+import org.opendaylight.restconf.server.spi.RestconfStream;
+import org.opendaylight.restconf.subscription.SubscriptionStateMachine;
+import org.opendaylight.yangtools.concepts.Registration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * RESTCONF subscription resource. Deals with dispatching HTTP requests to individual sub-resources as needed.
+ */
+@NonNullByDefault
+final class SubscriptionResourceInstance extends WebHostResourceInstance {
+    private static final Logger LOG = LoggerFactory.getLogger(SubscriptionResourceInstance.class);
+    private static final HeadersResponse OPTIONS_ONLY_METHOD_NOT_ALLOWED;
+    private static final HeadersResponse OPTIONS_ONLY_OK;
+
+    private final SubscriptionStateMachine machine;
+    private final RestconfStream.Registry streamRegistry;
+    private final int sseMaximumFragmentLength;
+    private final int sseHeartbeatIntervalMillis;
+
+    static {
+        final var headers = new ReadOnlyHttpHeaders(true, HttpHeaderNames.ALLOW, "OPTIONS");
+        OPTIONS_ONLY_METHOD_NOT_ALLOWED = new HeadersResponse(HttpResponseStatus.METHOD_NOT_ALLOWED, headers);
+        OPTIONS_ONLY_OK = new HeadersResponse(HttpResponseStatus.OK, headers);
+    }
+
+    SubscriptionResourceInstance(final String path, final SubscriptionStateMachine machine,
+            final RestconfStream.Registry streamRegistry, final int sseMaximumFragmentLength,
+            final int sseHeartbeatIntervalMillis) {
+        super(path);
+        this.machine = machine;
+        this.streamRegistry = streamRegistry;
+        this.sseMaximumFragmentLength = sseMaximumFragmentLength;
+        this.sseHeartbeatIntervalMillis = sseHeartbeatIntervalMillis;
+    }
+
+    @Override
+    public Response prepare(final ChannelHandler channelHandler, final ImplementedMethod method, final URI targetUri,
+            final HttpHeaders headers, final SegmentPeeler peeler, final XRD xrd) {
+        if (!peeler.hasNext()) {
+            return optionsOnlyResponse(method);
+        }
+
+        final var subscriptionId = peeler.next();
+        return switch (method) {
+            case GET -> prepareEventStream(channelHandler, method, targetUri, headers, subscriptionId, xrd);
+            case HEAD, OPTIONS -> EmptyResponse.NOT_FOUND; // TODO
+            default -> EmptyResponse.NOT_FOUND;
+        };
+    }
+
+    private Response prepareEventStream(final ChannelHandler channelHandler, final ImplementedMethod method,
+            final URI targetUri, final HttpHeaders headers, final String subscriptionId, final XRD xrd) {
+        if (subscriptionId == null) {
+            LOG.debug("Subscription not found");
+            return EmptyResponse.NOT_FOUND;
+        }
+        if (!headers.contains(HttpHeaderNames.ACCEPT, HttpHeaderValues.TEXT_EVENT_STREAM, false)) {
+            return new EmptyResponse(HttpResponseStatus.NOT_ACCEPTABLE);
+        }
+
+        // FIXME do not hardcode stream name
+        final var stream = streamRegistry.lookupStream("NETCONF");
+        if (stream == null) {
+            LOG.debug("Stream '{}' not found", "NETCONF");
+            return EmptyResponse.NOT_FOUND;
+        }
+
+        final var decoder = new QueryStringDecoder(targetUri);
+        final EventStreamGetParams streamParams;
+        try {
+            streamParams = EventStreamGetParams.of(QueryParameters.ofMultiValue(decoder.parameters()));
+        } catch (IllegalArgumentException e) {
+            return new EmptyResponse(HttpResponseStatus.NOT_ACCEPTABLE);
+        }
+
+        final var sender = new ChannelSender(sseMaximumFragmentLength);
+        final var registration = registerSender(stream, RestconfStream.EncodingName.RFC8040_JSON, streamParams, sender);
+
+        if (registration == null) {
+            return EmptyResponse.NOT_FOUND;
+        }
+
+        sender.enable(registration);
+
+        return new EventStreamResponse(HttpResponseStatus.OK, sender, channelHandler, sseHeartbeatIntervalMillis);
+    }
+
+    private static @Nullable Registration registerSender(final RestconfStream<?> stream,
+            final RestconfStream.EncodingName encoding, final EventStreamGetParams params,
+            final RestconfStream.Sender sender) {
+        final Registration reg;
+        try {
+            reg = stream.addSubscriber(sender, encoding, params);
+        } catch (UnsupportedEncodingException | XPathExpressionException e) {
+            // FIXME: report an error
+            return null;
+        }
+        return reg;
+    }
+
+    @Override
+    protected void removeRegistration() {
+        // no-op
+    }
+
+    private static HeadersResponse optionsOnlyResponse(final ImplementedMethod method) {
+        return switch (method) {
+            case OPTIONS -> OPTIONS_ONLY_OK;
+            default -> OPTIONS_ONLY_METHOD_NOT_ALLOWED;
+        };
+    }
+}
