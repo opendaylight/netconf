e8c321a1354621e765363362f01518384bbdc263
Peter Suna <peter.suna@pantheon.tech>
2024-11-05 13:25:18 +0100

    Notify transport channel on session close
    Call notifyTransportChannelFailed method when SshClient
    or SshServer invokes sessionClosed.
    If ChannelInactive close the session and TransportChannel
    is not ready, then it is necessary to call
    notifyTransportChannelFailed which triggers the reconnection process.
    
    This needs to be handled here, because the NetconfClientSession
    lifecycle has not started yet. The NetconfClientSession is created
    in NetconfNodeHandler only when ClientTransportChannelListener is
    successful. If ClientTransportChannelListener is successful and session
    is closed, then ConnectingTask will never finish nor provide any
    additional information.
    
    JIRA: NETCONF-1423
    Change-Id: Ief230b2d28a747628488d0dba3700fbb552d163d
    Signed-off-by: Peter Suna <peter.suna@pantheon.tech>
    (cherry picked from commit 72a6440320fb5174ce9b9026311c01e25f7950b3)

---
+++ transport/transport-ssh/src/test/java/org/opendaylight/netconf/transport/ssh/AbstractClientServerTest.java
@@ -0,0 +1,82 @@
+/*
+ * Copyright (c) 2024 PANTHEON.tech s.r.o. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.netconf.transport.ssh;
+
+import static org.mockito.Mockito.when;
+
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicReference;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.opendaylight.netconf.transport.api.TransportChannelListener;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev130715.Host;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev130715.IetfInetUtil;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev130715.PortNumber;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.ssh.client.rev240208.SshClientGrouping;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.ssh.server.rev240208.SshServerGrouping;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.tcp.client.rev240208.TcpClientGrouping;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.tcp.server.rev240208.TcpServerGrouping;
+import org.opendaylight.yangtools.yang.common.Uint16;
+
+@ExtendWith(MockitoExtension.class)
+abstract class AbstractClientServerTest {
+    private static final String USER = "user";
+    private static final AtomicInteger COUNTER = new AtomicInteger(0);
+    private static final AtomicReference<String> USERNAME = new AtomicReference<>(USER);
+
+    static final String RSA = "RSA";
+    static final String PASSWORD = "pa$$w0rd";
+    static final String SUBSYSTEM = "subsystem";
+
+    @Mock
+    TcpClientGrouping tcpClientConfig;
+    @Mock
+    TcpServerGrouping tcpServerConfig;
+    @Mock
+    SshClientGrouping sshClientConfig;
+    @Mock
+    TransportChannelListener clientListener;
+    @Mock
+    SshServerGrouping sshServerConfig;
+    @Mock
+    TransportChannelListener serverListener;
+
+    @BeforeEach
+    void beforeEach() throws Exception {
+        // create temp socket to get available port for test
+        final var socket = new ServerSocket(0);
+        final var localAddress = IetfInetUtil.ipAddressFor(InetAddress.getLoopbackAddress());
+        final var localPort = new PortNumber(Uint16.valueOf(socket.getLocalPort()));
+        socket.close();
+
+        when(tcpServerConfig.getLocalAddress()).thenReturn(localAddress);
+        when(tcpServerConfig.requireLocalAddress()).thenCallRealMethod();
+        when(tcpServerConfig.getLocalPort()).thenReturn(localPort);
+        when(tcpServerConfig.requireLocalPort()).thenCallRealMethod();
+
+        when(tcpClientConfig.getRemoteAddress()).thenReturn(new Host(localAddress));
+        when(tcpClientConfig.requireRemoteAddress()).thenCallRealMethod();
+        when(tcpClientConfig.getRemotePort()).thenReturn(localPort);
+        when(tcpClientConfig.requireRemotePort()).thenCallRealMethod();
+    }
+
+    static String getUsername() {
+        return USERNAME.get();
+    }
+
+    /**
+     * Update username for next test.
+     */
+    static String getUsernameAndUpdate() {
+        return USERNAME.getAndSet(USER + COUNTER.incrementAndGet());
+    }
+}
