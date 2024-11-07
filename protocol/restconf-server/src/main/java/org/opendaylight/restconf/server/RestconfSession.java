4b7e63b308f6129d7292266f6d0103b111135050
Robert Varga <robert.varga@pantheon.tech>
2024-11-07 01:06:09 +0100

    Refactor RestconfSession.{close,registerResource}
    There is no need to have an explicit listener on Channel.closeFuture(),
    as we can just attach to channelInactive().
    
    This patch also refactor resource registration, as RPC invocations,
    which are expected to be the primary source of interactions, are
    expected to execute asynchronously.
    
    JIRA: NETCONF-1424
    Change-Id: I8c257f8cfd427e00b066651c463298c708782eca
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- protocol/restconf-server/src/main/java/org/opendaylight/restconf/server/RestconfSession.java
+++ protocol/restconf-server/src/main/java/org/opendaylight/restconf/server/RestconfSession.java
@@ -9,10 +9,18 @@ package org.opendaylight.restconf.server;
 
 import static java.util.Objects.requireNonNull;
 
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.http.HttpHeaders;
 import io.netty.handler.codec.http.HttpScheme;
+import java.lang.invoke.MethodHandles;
+import java.lang.invoke.VarHandle;
 import java.net.URI;
 import java.util.ArrayList;
+import java.util.List;
+import org.checkerframework.checker.lock.qual.Holding;
+import org.eclipse.jdt.annotation.NonNull;
+import org.eclipse.jdt.annotation.NonNullByDefault;
 import org.opendaylight.netconf.transport.http.HTTPServerSession;
 import org.opendaylight.netconf.transport.http.ImplementedMethod;
 import org.opendaylight.netconf.transport.http.PreparedRequest;
@@ -25,12 +33,75 @@ import org.opendaylight.yangtools.concepts.Registration;
  * connections.
  */
 final class RestconfSession extends HTTPServerSession implements TransportSession {
-    private final ArrayList<Registration> resources = new ArrayList<>();
+    /**
+     * Our resources state.
+     */
+    private sealed interface Resources {
+        // nothing else
+    }
+
+    /**
+     * The channel is inactive. We do not allow new resources to be registered.
+     */
+    private static final class InactiveResources implements Resources {
+        static final InactiveResources INSTANCE = new InactiveResources();
+    }
+
+    /**
+     * We have some resources that need to be cleaned up.
+     */
+    private record SomeResources(ArrayList<Registration> registrations) implements Resources {
+        SomeResources {
+            requireNonNull(registrations);
+        }
+    }
+
+    private static final VarHandle RESOURCES;
+
+    static {
+        try {
+            RESOURCES = MethodHandles.lookup().findVarHandle(RestconfSession.class, "resources", Resources.class);
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            throw new ExceptionInInitializerError(e);
+        }
+    }
+
     private final EndpointRoot root;
+
+    @SuppressFBWarnings(value = "UWF_UNWRITTEN_FIELD",
+        justification = "https://github.com/spotbugs/spotbugs/issues/2749")
+    private volatile Resources resources;
 
     RestconfSession(final HttpScheme scheme, final EndpointRoot root) {
         super(scheme);
         this.root = requireNonNull(root);
+    }
+
+    @Override
+    public void channelInactive(final ChannelHandlerContext ctx) throws Exception {
+        try {
+            super.channelInactive(ctx);
+        } finally {
+            cleanupResources();
+        }
+    }
+
+    private void cleanupResources() {
+        // Optimistic side-step of creating the object monitor
+        final var prev = (Resources) RESOURCES.compareAndExchange(this, null, InactiveResources.INSTANCE);
+        final var toRelease = switch (prev) {
+            case SomeResources some -> {
+                // synchronize with registerResource()
+                synchronized (this) {
+                    yield some.registrations;
+                }
+            }
+            // nothing to release
+            case null, default -> List.<Registration>of();
+        };
+
+        // callbacks without holding the lock
+        toRelease.forEach(Registration::close);
     }
 
     @Override
@@ -41,11 +112,40 @@ final class RestconfSession extends HTTPServerSession implements TransportSessio
 
     @Override
     public void registerResource(final Registration registration) {
-        resources.add(requireNonNull(registration));
+        final var reg = requireNonNull(registration);
+
+        // complicated because we want need to hold the lock during the entire inflate operation
+        final boolean release;
+        synchronized (this) {
+            release = switch (resources) {
+                case null -> registerFirst(reg);
+                case InactiveResources inactive -> true;
+                case SomeResources some -> doRegister(some, reg);
+            };
+        }
+
+        // Cannot register: close the registration without holding lock
+        if (release) {
+            reg.close();
+        }
     }
 
-    void close() {
-        resources.forEach(Registration::close);
-        resources.clear();
+    @Holding("this")
+    private boolean registerFirst(final @NonNull Registration reg) {
+        // Start off small, grow as needed
+        final var created = new SomeResources(new ArrayList<>(2));
+        final var witness = (Resources) RESOURCES.compareAndExchange(this, null, created);
+        return switch (witness) {
+            case null -> doRegister(created, reg);
+            case InactiveResources inactive -> true;
+            case SomeResources some -> doRegister(some, reg);
+        };
+    }
+
+    @Holding("this")
+    @NonNullByDefault
+    private static boolean doRegister(final SomeResources some, final Registration reg) {
+        some.registrations.add(reg);
+        return false;
     }
 }
