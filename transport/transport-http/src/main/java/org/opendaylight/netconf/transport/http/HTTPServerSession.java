d426a4cbd1575eb1394df42f141831d7c6809408
Robert Varga <robert.varga@pantheon.tech>
2024-11-10 20:16:25 +0100

    Split out ServerRequestExecutor
    This is a reusable piece, not directly tied to the pipeline. Split it
    out, making for a leader HTTPServerSession.
    
    JIRA: NETCONF-1424
    Change-Id: Ifc9e79ec32d91a2871a65e8b751267dec0cfbe67
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- transport/transport-http/src/main/java/org/opendaylight/netconf/transport/http/HTTPServerSession.java
+++ transport/transport-http/src/main/java/org/opendaylight/netconf/transport/http/HTTPServerSession.java
@@ -12,8 +12,6 @@ import static java.util.Objects.requireNonNull;
 import com.google.common.annotations.Beta;
 import com.google.common.annotations.VisibleForTesting;
 import io.netty.buffer.ByteBuf;
-import io.netty.buffer.ByteBufInputStream;
-import io.netty.buffer.ByteBufUtil;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.handler.codec.http.DefaultFullHttpResponse;
@@ -28,16 +26,10 @@ import io.netty.handler.codec.http.HttpUtil;
 import io.netty.handler.codec.http.HttpVersion;
 import io.netty.handler.codec.http2.HttpConversionUtil.ExtensionHeaderNames;
 import io.netty.util.AsciiString;
-import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.Arrays;
 import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ConcurrentMap;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.RejectedExecutionException;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 import org.eclipse.jdt.annotation.NonNull;
@@ -66,23 +58,7 @@ import org.slf4j.LoggerFactory;
  * snapshot -- which is the typical use case.
  */
 @Beta
-public abstract class HTTPServerSession extends SimpleChannelInboundHandler<FullHttpRequest>
-        implements PendingRequestListener {
-    /**
-     * Transport-level details about a {@link PendingRequest} execution.
-     *
-     * @param ctx the {@link ChannelHandlerContext} on which the request is occuring
-     * @param streamId the HTTP/2 stream ID, if present
-     * @param version HTTP version of the request
-     */
-    @NonNullByDefault
-    private record RequestContext(ChannelHandlerContext ctx, HttpVersion version, @Nullable Integer streamId) {
-        RequestContext {
-            requireNonNull(ctx);
-            requireNonNull(version);
-        }
-    }
-
+public abstract class HTTPServerSession extends SimpleChannelInboundHandler<FullHttpRequest> {
     private static final Logger LOG = LoggerFactory.getLogger(HTTPServerSession.class);
     private static final AsciiString STREAM_ID = ExtensionHeaderNames.STREAM_ID.text();
     private static final Map<HttpMethod, ImplementedMethod> ALL_METHODS = Arrays.stream(ImplementedMethod.values())
@@ -106,12 +82,11 @@ public abstract class HTTPServerSession extends SimpleChannelInboundHandler<Full
     // FIXME: this heavily depends on the object model and is tied to HTTPServer using aggregators, so perhaps we should
     //        reconsider the design
 
-    private final ConcurrentMap<PendingRequest<?>, RequestContext> executingRequests = new ConcurrentHashMap<>();
     private final HttpScheme scheme;
 
+
     // Only valid when the session is attached to a Channel
-    private ExecutorService reqExecutor;
-    private ExecutorService respExecutor;
+    private ServerRequestExecutor executor;
 
     protected HTTPServerSession(final HttpScheme scheme) {
         super(FullHttpRequest.class, false);
@@ -121,19 +96,7 @@ public abstract class HTTPServerSession extends SimpleChannelInboundHandler<Full
     @Override
     public final void handlerAdded(final ChannelHandlerContext ctx) {
         final var channel = ctx.channel();
-        final var remoteAddress = channel.remoteAddress().toString();
-        reqExecutor = Executors.newThreadPerTaskExecutor(Thread.ofVirtual()
-            .name(remoteAddress + "-http-server-req-", 0)
-            .inheritInheritableThreadLocals(false)
-            .uncaughtExceptionHandler(
-                (thread, exception) -> LOG.warn("Unhandled request-phase failure", exception))
-            .factory());
-        respExecutor = Executors.newThreadPerTaskExecutor(Thread.ofVirtual()
-            .name(remoteAddress + "-http-server-resp-", 0)
-            .inheritInheritableThreadLocals(false)
-            .uncaughtExceptionHandler(
-                (thread, exception) -> LOG.warn("Unhandled response-phase failure", exception))
-            .factory());
+        executor = new ServerRequestExecutor(channel.remoteAddress().toString());
         LOG.debug("Threadpools for {} started", channel);
     }
 
@@ -149,8 +112,7 @@ public abstract class HTTPServerSession extends SimpleChannelInboundHandler<Full
     }
 
     private void shutdownTreadpools(final ChannelHandlerContext ctx) {
-        reqExecutor.shutdown();
-        respExecutor.shutdown();
+        executor.shutdown();
         LOG.debug("Threadpools for {} shut down", ctx.channel());
     }
 
@@ -248,11 +210,11 @@ public abstract class HTTPServerSession extends SimpleChannelInboundHandler<Full
             case CompletedRequest completed -> {
                 msg.release();
                 LOG.debug("Immediate response to {} {}", method, targetUri);
-                respond(ctx, streamId, version, completed.asResponse());
+                executor.respond(ctx, streamId, version, completed.asResponse());
             }
             case PendingRequest<?> pending -> {
                 LOG.debug("Scheduling execution of {} {}", method, targetUri);
-                executeRequest(new RequestContext(ctx, version, streamId), pending, msg.content());
+                executor.executeRequest(ctx, version, streamId, pending, msg.content());
             }
         }
     }
@@ -302,106 +264,13 @@ public abstract class HTTPServerSession extends SimpleChannelInboundHandler<Full
     @NonNullByDefault
     protected abstract PreparedRequest prepareRequest(ImplementedMethod method, URI targetUri, HttpHeaders headers);
 
-    private void executeRequest(final RequestContext context, final PendingRequest<?> pending, final ByteBuf content) {
-        // We are invoked with content's reference and need to make sure it gets released.
-        if (content.isReadable()) {
-            executeRequest(context, pending, new ByteBufInputStream(content, true));
-        } else {
-            content.release();
-            executeRequest(context, pending, (ByteBufInputStream) null);
-        }
-    }
-
-    private void executeRequest(final RequestContext context, final PendingRequest<?> pending,
-            final ByteBufInputStream body) {
-        // Remember metadata about the request and then execute it
-        executingRequests.put(pending, context);
-        reqExecutor.execute(() -> pending.execute(this, body));
-    }
-
-    @Override
-    public void requestComplete(final PendingRequest<?> request, final Response response) {
-        final var req = executingRequests.remove(request);
-        if (req != null) {
-            respond(req.ctx, req.streamId, req.version, response);
-        } else {
-            LOG.warn("Cannot pair request {}, not sending response {}", request, response, new Throwable());
-        }
-    }
-
-    @Override
-    public void requestFailed(final PendingRequest<?> request, final Exception cause) {
-        LOG.warn("Internal error while processing {}", request, cause);
-        final var req = executingRequests.remove(request);
-        if (req != null) {
-            respond(req.ctx, req.streamId, formatException(cause, req.version()));
-        } else {
-            LOG.warn("Cannot pair request, not sending response", new Throwable());
-        }
-    }
-
     @NonNullByDefault
-    private void respond(final ChannelHandlerContext ctx, final @Nullable Integer streamId, final HttpVersion version,
-            final Response response) {
-        if (response instanceof ReadyResponse ready) {
-            respond(ctx, streamId, ready.toHttpResponse(version));
-            return;
-        }
-        try {
-            respExecutor.execute(() -> respond(ctx, streamId, formatResponse(response, ctx, version)));
-        } catch (RejectedExecutionException e) {
-            LOG.trace("Session shut down, dropping response {}", response, e);
-        }
-    }
-
-    @NonNullByDefault
-    private static void respond(final ChannelHandlerContext ctx, final @Nullable Integer streamId,
+    static final void respond(final ChannelHandlerContext ctx, final @Nullable Integer streamId,
             final FullHttpResponse response) {
         requireNonNull(response);
         if (streamId != null) {
             response.headers().setInt(STREAM_ID, streamId);
         }
         ctx.writeAndFlush(response);
-    }
-
-    // Hand-coded, as simple as possible
-    @NonNullByDefault
-    private static FullHttpResponse formatException(final Exception cause, final HttpVersion version) {
-        final var response = new DefaultFullHttpResponse(version, HttpResponseStatus.INTERNAL_SERVER_ERROR);
-        final var content = response.content();
-        // Note: we are tempted to do a cause.toString() here, but we are dealing with unhandled badness here,
-        //       so we do not want to be too revealing -- hence a message is all the user gets.
-        ByteBufUtil.writeUtf8(content, cause.getMessage());
-        HttpUtil.setContentLength(response, content.readableBytes());
-        return response;
-    }
-
-    // Executed on respExecutor, so it is okay to block
-    @NonNullByDefault
-    private static FullHttpResponse formatResponse(final Response response, final ChannelHandlerContext ctx,
-            final HttpVersion version) {
-        // FIXME: We are filling a full ByteBuf and producing a complete FullHttpResponse, which is not want we want.
-        //
-        //        We want to be emitting a series of write() requests into the queue, each of which is subject
-        //        to channel's flow control -- i.e. it effectively is a MPSC outbound queue.
-        //
-        //        We are using OutputStream below as the synchrous interface, but we really want our own, such that we
-        //        get the headers first and we invoke body streaming (if applicable and indicated by return).
-        //
-        //        In the streaming phase, we start with a HttpObjectSender initialized to the HttpResponse, as indicated
-        //        in previous step. It also implements OutputStream, which is the fasade we show to the user. As they
-        //        pump body data, we check for body size and events and:
-        //        - buffer initial stuff, so that we produce a FullHttpResponse if the payload is below
-        //          256KiB (or so), i.e. producing Content-Length header and dumping the thing in one go
-        //        - otherwise emit just HttpResponse with Transfer-Enconding: chunked and continue sending
-        //          out chunks (of reasonable size).
-        //        - finish up with a LastHttpContent when OutputStream.close() is called ... which might be problematic
-        //          w.r.t. failure cases, so it needs some figuring out
-        try {
-            return response.toHttpResponse(ctx.alloc(), version);
-        } catch (IOException e) {
-            LOG.warn("IO error while converting formatting response", e);
-            return formatException(e, version);
-        }
     }
 }
