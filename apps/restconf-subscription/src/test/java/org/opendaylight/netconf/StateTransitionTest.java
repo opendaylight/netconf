b8257cda30929fe025dca167c6ba4eff385db4d2
Oleksandr Zharov <oleksandr.zharov@pantheon.tech>
2024-11-14 13:04:18 +0100

    Implement simple state machine
    Added simple state machine to add some controle over
    subscriptions state transition.
    
    JIRA: NETCONF-714
    Change-Id: I23ba8a2b03104b3c7742b5cd59b33de8060daad0
    Signed-off-by: Oleksandr Zharov <oleksandr.zharov@pantheon.tech>

---
+++ apps/restconf-subscription/src/test/java/org/opendaylight/netconf/StateTransitionTest.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2024 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.netconf;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.opendaylight.restconf.server.api.TransportSession;
+import org.opendaylight.yangtools.yang.common.Uint32;
+
+@ExtendWith(MockitoExtension.class)
+public class StateTransitionTest {
+    @Mock
+    TransportSession session;
+
+    private SubscriptionStateMachine subscriptionStateMachine;
+
+    @Test
+    void transitionStateTest() {
+        // initializing state machine
+        subscriptionStateMachine = new SubscriptionStateMachine();
+        subscriptionStateMachine.registerSubscription(session, Uint32.ONE);
+
+        // Checking default stating state
+        var state = subscriptionStateMachine.getSubscriptionState(Uint32.ONE);
+        assertEquals(SubscriptionState.START, state);
+
+        // Checking legal state transition
+        subscriptionStateMachine.moveTo(Uint32.ONE, SubscriptionState.ACTIVE);
+        state = subscriptionStateMachine.getSubscriptionState(Uint32.ONE);
+        assertEquals(SubscriptionState.ACTIVE, state);
+
+        // Checking illegal state transition
+        assertThrows(IllegalStateException.class,
+            () -> subscriptionStateMachine.moveTo(Uint32.ONE, SubscriptionState.START));
+    }
+}
