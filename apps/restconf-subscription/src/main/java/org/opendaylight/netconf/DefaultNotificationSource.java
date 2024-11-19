970c2578de204544807ea088adb1ba2571d43621
Matej Sramcik <matej.sramcik@pantheon.tech>
2024-11-19 08:00:00 +0100

    [WIP] Expose NETCONF event stream for RESTCONF subscriptions
    Create default event stream called NETCONF for purposes of subscribed
    notifications.
    
    JIRA: NETCONF-1417
    Change-Id: I405789db2fa8011362e3bce34df2134b20720645
    Signed-off-by: Matej Sramcik <matej.sramcik@pantheon.tech>
    Signed-off-by: Ivan Hrasko <ivan.hrasko@pantheon.tech>
    Signed-off-by: Matej Sramcik <matej.sramcik@pantheon.tech>

---
+++ apps/restconf-subscription/src/main/java/org/opendaylight/netconf/DefaultNotificationSource.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright (c) 2024 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.netconf;
+
+import java.io.Closeable;
+import javax.inject.Inject;
+import org.opendaylight.mdsal.dom.api.DOMNotification;
+import org.opendaylight.mdsal.dom.api.DOMNotificationService;
+import org.opendaylight.mdsal.dom.api.DOMSchemaService;
+import org.opendaylight.restconf.mdsal.spi.AbstractNotificationSource;
+import org.opendaylight.restconf.mdsal.spi.NotificationSource;
+import org.opendaylight.restconf.server.spi.RestconfStream;
+import org.opendaylight.restconf.server.spi.RestconfStream.Sink;
+import org.opendaylight.yangtools.concepts.Registration;
+import org.opendaylight.yangtools.yang.model.api.EffectiveModelContext;
+import org.opendaylight.yangtools.yang.model.api.stmt.NotificationEffectiveStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.SchemaNodeIdentifier;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.service.component.annotations.Reference;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Default {@link RestconfStream} called "NETCONF" providing all controller's YANG notifications as described in
+ * <a href="https://www.rfc-editor.org/rfc/rfc8040#section-6.3.1">RFC 8040</a> for purposes of subscribed notifications
+ * defined in <a href="https://www.rfc-editor.org/rfc/rfc8639">RFC 8639</a>.
+ * <p>
+ * It automatically re-registers to current YANG notifications on controller's {@code EffectiveModelContext} change.
+ */
+// FIXME crate class ContextListener which listens on onModelContextUpdated and creates a new instance
+// FIXME of DefaultNotificationSource.
+// FIXME create a new class NetconfStream which uses ContextListener and writes NETCONF stream into mdsal.
+// FIXME Use RestconfSubscriptionsStreamRegistry utility.
+final class DefaultNotificationSource extends AbstractNotificationSource implements Closeable {
+    private static final Logger LOG = LoggerFactory.getLogger(DefaultNotificationSource.class);
+
+    private final DOMSchemaService schemaService;
+    private final DOMNotificationService notificationService;
+    private Sink<DOMNotification> sink;
+
+    private final EffectiveModelContext context;
+
+    DefaultNotificationSource(@Reference final DOMSchemaService schemaService,
+            @Reference final DOMNotificationService notificationService, EffectiveModelContext context) {
+        super(NotificationSource.ENCODINGS);
+        this.schemaService = schemaService;
+        this.notificationService = notificationService;
+        this.context = context;
+    }
+
+    @Override
+    protected Registration start(final Sink<DOMNotification> domNotificationSink) {
+        this.sink = domNotificationSink;
+        final var notifications = context.getModuleStatements().values().stream()
+            .flatMap(module -> module.streamEffectiveSubstatements(NotificationEffectiveStatement.class))
+            .map(notification -> SchemaNodeIdentifier.Absolute.of(notification.argument()))
+            .toList();
+        return notificationService.registerNotificationListener(new Listener(sink, () -> context), notifications);
+    }
+
+    @Override
+    public synchronized void close() {
+        sink.endOfStream();
+    }
+}
---
+++ plugins/restconf-notifications-mdsal/src/main/java/org/opendaylight/restconf/notifications/mdsal/RestconfSubscriptionsStreamRegistry.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright (c) 2024 PANTHEON.tech, s.r.o. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.restconf.notifications.mdsal;
+
+import static java.util.Objects.requireNonNull;
+
+import com.google.common.util.concurrent.ListenableFuture;
+import javax.inject.Inject;
+import javax.inject.Singleton;
+import org.opendaylight.mdsal.common.api.LogicalDatastoreType;
+import org.opendaylight.mdsal.dom.api.DOMDataBroker;
+import org.opendaylight.restconf.server.spi.AbstractRestconfStreamRegistry;
+import org.opendaylight.restconf.server.spi.RestconfStream;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.subscribed.notifications.rev190909.Streams;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.subscribed.notifications.rev190909.streams.Stream;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.NodeIdentifier;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.NodeIdentifierWithPredicates;
+import org.opendaylight.yangtools.yang.data.api.schema.MapEntryNode;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * This singleton class is responsible for creation, removal and searching for RFC 8639 {@link RestconfStream}s.
+ */
+@Singleton
+@Component(service = RestconfSubscriptionsStreamRegistry.class)
+public final class RestconfSubscriptionsStreamRegistry extends AbstractRestconfStreamRegistry {
+    private static final YangInstanceIdentifier RFC8639_STREAMS = YangInstanceIdentifier.of(
+        NodeIdentifier.create(Streams.QNAME),
+        NodeIdentifier.create(Stream.QNAME));
+
+    private final DOMDataBroker dataBroker;
+
+    @Inject
+    @Activate
+    public RestconfSubscriptionsStreamRegistry(@Reference final RestconfStream.LocationProvider locationProvider,
+            @Reference final DOMDataBroker dataBroker) {
+        super(locationProvider);
+        this.dataBroker = requireNonNull(dataBroker);
+    }
+
+    @Override
+    protected ListenableFuture<?> putStream(final MapEntryNode stream) {
+        // Now issue a put operation
+        final var tx = dataBroker.newWriteOnlyTransaction();
+        tx.put(LogicalDatastoreType.OPERATIONAL, RFC8639_STREAMS.node(stream.name()), stream);
+        return tx.commit();
+    }
+
+    @Override
+    protected ListenableFuture<?> deleteStream(final NodeIdentifierWithPredicates streamName) {
+        // Now issue a delete operation while the name is still protected by being associated in the map.
+        final var tx = dataBroker.newWriteOnlyTransaction();
+        tx.delete(LogicalDatastoreType.OPERATIONAL, RFC8639_STREAMS.node(streamName));
+        return tx.commit();
+    }
+}
