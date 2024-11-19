2a08d8dbfd6af8ee2d1787887fb49a9864ac34fa
Yaroslav Lastivka <yaroslav.lastivka@pantheon.tech>
2024-11-19 11:06:09 +0200

    Implement subtree filtering
    Added support for subtree filtering of NormalizedNode
    structures within the restconf-server-spi module
    
    JIRA: NETCONF-714
    Change-Id: I8fc246b5516582fb389a097e3366e867e71f7e84
    Signed-off-by: Yaroslav Lastivka <yaroslav.lastivka@pantheon.tech>

--- apps/restconf-subscription/src/main/java/org/opendaylight/restconf/subscription/EstablishSubscriptionRpc.java
+++ apps/restconf-subscription/src/main/java/org/opendaylight/restconf/subscription/EstablishSubscriptionRpc.java
@@ -65,6 +65,8 @@ public class EstablishSubscriptionRpc extends RpcImplementation {
             NodeIdentifier.create(QName.create(EstablishSubscriptionInput.QNAME, "target").intern());
     private static final NodeIdentifier SUBSCRIPTION_STREAM_FILTER =
         NodeIdentifier.create(QName.create(EstablishSubscriptionInput.QNAME, "stream-filter").intern());
+    private static final NodeIdentifier SUBSCRIPTION_STREAM_SUBTREE_FILTER =
+        NodeIdentifier.create(QName.create(EstablishSubscriptionInput.QNAME, "stream-subtree-filter").intern());
     private static final NodeIdentifier SUBSCRIPTION_STOP_TIME =
         NodeIdentifier.create(QName.create(EstablishSubscriptionInput.QNAME, "stop-time").intern());
     private static final NodeIdentifier SUBSCRIPTION_ENCODING =
@@ -172,8 +174,11 @@ public class EstablishSubscriptionRpc extends RpcImplementation {
                     streamFilterName));
                 nodeTargetBuilder.withChild(nodeFilterBuilder.build());
             }
-            //  TODO: parse anydata filter, rfc6241? https://www.rfc-editor.org/rfc/rfc8650#name-filter-example
-            //    {@link StreamSubtreeFilter}.
+
+/*            final var streamSubtree = leaf(streamFilter, SUBSCRIPTION_STREAM_SUBTREE_FILTER, String.class);
+            if (streamSubtree != null) {
+                SubtreeFilterRestconf.applyFilter(streamFilter.getChildByArg(SUBSCRIPTION_STREAM_SUBTREE_FILTER),);
+            }*/
         }
         nodeBuilder.withChild(nodeTargetBuilder.build());
 
