03c2b84efd3ede299f668fe1fb2cc351bd67a8bd
Matej Sramcik <matej.sramcik@pantheon.tech>
2024-11-19 08:00:00 +0100

    [WIP] Expose NETCONF event stream for RESTCONF subscriptions
    Create default event stream called NETCONF for purposes of subscribed
    notifications.
    
    JIRA: NETCONF-1417
    Change-Id: I405789db2fa8011362e3bce34df2134b20720645
    Signed-off-by: Matej Sramcik <matej.sramcik@pantheon.tech>
    Signed-off-by: Ivan Hrasko <ivan.hrasko@pantheon.tech>

---
+++ apps/restconf-subscription/src/main/java/org/opendaylight/netconf/NetconfStream.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright (c) 2024 PANTHEON.tech, s.r.o. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.netconf;
+
+import com.google.common.util.concurrent.FutureCallback;
+import com.google.common.util.concurrent.Futures;
+import com.google.common.util.concurrent.MoreExecutors;
+import java.net.URI;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import javax.inject.Inject;
+import javax.inject.Singleton;
+import org.eclipse.jdt.annotation.NonNull;
+import org.opendaylight.mdsal.dom.api.DOMDataBroker;
+import org.opendaylight.restconf.notifications.mdsal.RestconfSubscriptionsStreamRegistry;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.subscribed.notifications.rev190909.streams.Stream;
+import org.opendaylight.yangtools.yang.common.QName;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
+import org.opendaylight.yangtools.yang.data.api.schema.MapEntryNode;
+import org.opendaylight.yangtools.yang.data.spi.node.ImmutableNodes;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Singleton
+@Component
+public final class NetconfStream {
+    private static final Logger LOG = LoggerFactory.getLogger(NetconfStream.class);
+    private static final QName NAME_QNAME = QName.create(Stream.QNAME, "name").intern();
+    private static final QName DESCRIPTION_QNAME = QName.create(Stream.QNAME, "description").intern();
+    private static final QName REPLAY_LOG_CREATION_TIME_QNAME = QName.create(Stream.QNAME, "replay-log-creation-time");
+    private static final String NAME = "subscriptionStream";
+    private static final String DESCRIPTION = "stream description";
+
+    @Inject
+    @Activate
+    public NetconfStream(@Reference final DOMDataBroker dataBroker) {
+        final var restconfSubscriptionsStreamRegistry = new RestconfSubscriptionsStreamRegistry(
+            uri -> uri.resolve(URI.create(NAME)), dataBroker);
+
+        Futures.addCallback(restconfSubscriptionsStreamRegistry.addStream(streamEntry(NAME, DESCRIPTION)),
+            new FutureCallback<Object>() {
+                @Override
+                public void onSuccess(final Object result) {
+
+                }
+
+                @Override
+                public void onFailure(final Throwable cause) {
+
+                }
+            }, MoreExecutors.directExecutor());
+    }
+
+    private @NonNull MapEntryNode streamEntry(final String name, final String description) {
+        return ImmutableNodes.newMapEntryBuilder()
+            .withNodeIdentifier(YangInstanceIdentifier.NodeIdentifierWithPredicates.of(Stream.QNAME, NAME_QNAME, name))
+            .withChild(ImmutableNodes.leafNode(NAME_QNAME, name))
+            .withChild(ImmutableNodes.leafNode(DESCRIPTION_QNAME, description))
+            //FIXME: We don't use replays, but without this node we get error:
+            // "Node stream is missing mandatory descendant replay-log-creation-time"
+            .withChild(ImmutableNodes.leafNode(REPLAY_LOG_CREATION_TIME_QNAME,
+                Instant.now().truncatedTo(ChronoUnit.SECONDS).toString()))
+            .build();
+    }
+}
