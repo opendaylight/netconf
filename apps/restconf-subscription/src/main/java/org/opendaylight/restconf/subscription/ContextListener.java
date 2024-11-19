2cc531181dce4d126062f5ddeb13819f0fc38f42
Matej Sramcik <matej.sramcik@pantheon.tech>
2024-11-19 08:00:00 +0100

    Expose NETCONF event stream for subscriptions
    Create default event stream called NETCONF for purposes of
    subscribed notifications.
    
    JIRA: NETCONF-1417
    Change-Id: I405789db2fa8011362e3bce34df2134b20720645
    Signed-off-by: Matej Sramcik <matej.sramcik@pantheon.tech>
    Signed-off-by: Ivan Hrasko <ivan.hrasko@pantheon.tech>

---
+++ apps/restconf-subscription/src/main/java/org/opendaylight/restconf/subscription/ContextListener.java
@@ -0,0 +1,97 @@
+/*
+ * Copyright (c) 2024 PANTHEON.tech, s.r.o. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.restconf.subscription;
+
+import static java.util.Objects.requireNonNull;
+
+import com.google.common.util.concurrent.FutureCallback;
+import com.google.common.util.concurrent.Futures;
+import com.google.common.util.concurrent.MoreExecutors;
+import org.eclipse.jdt.annotation.NonNull;
+import org.opendaylight.mdsal.dom.api.DOMNotificationService;
+import org.opendaylight.mdsal.dom.api.DOMSchemaService;
+import org.opendaylight.restconf.server.spi.RestconfStream;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.subscribed.notifications.rev190909.streams.Stream;
+import org.opendaylight.yangtools.concepts.Registration;
+import org.opendaylight.yangtools.yang.common.QName;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
+import org.opendaylight.yangtools.yang.data.api.schema.MapEntryNode;
+import org.opendaylight.yangtools.yang.data.spi.node.ImmutableNodes;
+import org.opendaylight.yangtools.yang.model.api.EffectiveModelContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * This class recreates DefaultNotificationSource when model context is updated.
+ */
+final class ContextListener implements Registration {
+    private static final Logger LOG = LoggerFactory.getLogger(ContextListener.class);
+    private static final String NAME = "NETCONF";
+    private static final QName NAME_QNAME = QName.create(Stream.QNAME, "name").intern();
+    private static final QName DESCRIPTION_QNAME = QName.create(Stream.QNAME, "description").intern();
+    private static final String DESCRIPTION = "Stream for subscription state change notifications";
+
+    private final @NonNull DOMNotificationService notificationService;
+    private final @NonNull Registration registration;
+    private final RestconfStream.@NonNull Registry streamRegistry;
+
+    private DefaultNotificationSource notificationSource;
+
+    ContextListener(final DOMNotificationService notificationService, final DOMSchemaService schemaService,
+            final RestconfStream.Registry streamRegistry) {
+        this.notificationService = requireNonNull(notificationService);
+        this.streamRegistry = streamRegistry;
+        notificationSource = new DefaultNotificationSource(notificationService, schemaService.getGlobalContext());
+
+        addStream();
+        registration = schemaService.registerSchemaContextListener(this::onModelContextUpdated);
+    }
+
+    synchronized void onModelContextUpdated(final EffectiveModelContext context) {
+        if (notificationSource != null) {
+            notificationSource.close();
+        }
+        notificationSource = new DefaultNotificationSource(notificationService, context);
+
+        // FIXME after replace by #createStream use updated notificationSource for new stream
+        addStream();
+    }
+
+    public void addStream() {
+        // write NETCONF stream to datastore
+        // FIXME replace by correct override of #createStream
+        Futures.addCallback(streamRegistry.putStream(streamEntry()),
+            new FutureCallback<Object>() {
+                @Override
+                public void onSuccess(final Object result) {
+                    LOG.debug("Stream {} added", NAME);
+                }
+
+                @Override
+                public void onFailure(final Throwable cause) {
+                    LOG.error("Failed to add stream {}", NAME, cause);
+                }
+            }, MoreExecutors.directExecutor());
+    }
+
+    public static @NonNull MapEntryNode streamEntry() {
+        return ImmutableNodes.newMapEntryBuilder()
+            .withNodeIdentifier(YangInstanceIdentifier.NodeIdentifierWithPredicates.of(Stream.QNAME, NAME_QNAME, NAME))
+            .withChild(ImmutableNodes.leafNode(NAME_QNAME, NAME))
+            .withChild(ImmutableNodes.leafNode(DESCRIPTION_QNAME, DESCRIPTION))
+            .build();
+    }
+
+    @Override
+    public void close() {
+        registration.close();
+        if (notificationSource != null) {
+            notificationSource.close();
+        }
+    }
+}
