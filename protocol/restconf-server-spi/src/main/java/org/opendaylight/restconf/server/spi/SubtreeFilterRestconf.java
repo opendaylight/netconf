f556baa68efbafd9a1bff6d6b19bf6db47263854
Yaroslav Lastivka <yaroslav.lastivka@pantheon.tech>
2024-11-19 11:06:09 +0200

    Implement subtree filtering
    Added support for subtree filtering of NormalizedNode
    structures within the restconf-server-spi module
    
    JIRA: NETCONF-714
    Change-Id: I8fc246b5516582fb389a097e3366e867e71f7e84
    Signed-off-by: Yaroslav Lastivka <yaroslav.lastivka@pantheon.tech>

---
+++ protocol/restconf-server-spi/src/main/java/org/opendaylight/restconf/server/spi/SubtreeFilterRestconf.java
@@ -0,0 +1,82 @@
+/*
+ * Copyright (c) 2024 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.restconf.server.spi;
+
+import java.util.Objects;
+import org.opendaylight.yangtools.yang.data.api.schema.ContainerNode;
+import org.opendaylight.yangtools.yang.data.api.schema.LeafNode;
+import org.opendaylight.yangtools.yang.data.api.schema.MapNode;
+import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNode;
+import org.opendaylight.yangtools.yang.data.spi.node.ImmutableNodes;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * See <a href="http://tools.ietf.org/html/rfc6241#section-6">rfc6241</a> for details.
+ */
+public final class SubtreeFilterRestconf {
+    private static final Logger LOG = LoggerFactory.getLogger(SubtreeFilterRestconf.class);
+
+    private SubtreeFilterRestconf() {
+        // Hidden on purpose
+    }
+
+    public static NormalizedNode applyFilter(final NormalizedNode filterNode, final NormalizedNode dataNode) {
+        if (filterNode == null || dataNode == null) {
+            return null;
+        }
+
+        // Handle different node types
+        if (filterNode instanceof ContainerNode filterContainer && dataNode instanceof ContainerNode dataContainer) {
+            return filterContainerNode(filterContainer, dataContainer);
+        } else if (filterNode instanceof MapNode filterMap && dataNode instanceof MapNode dataMap) {
+            return filterMapNode(filterMap, dataMap);
+        } else if (filterNode instanceof LeafNode filterLeaf && dataNode instanceof LeafNode dataLeaf) {
+            return filterLeafNode(filterLeaf, dataLeaf);
+        }
+
+        // If node types do not match or cannot be handled
+        return null;
+    }
+
+    private static ContainerNode filterContainerNode(final ContainerNode filter, final ContainerNode data) {
+        final var builder = ImmutableNodes.newContainerBuilder(filter.getIdentifier());
+        for (var filterChild : filter.body().iterator()) {
+            var matchingChild = data.findChildByArg(filterChild.getIdentifier());
+            if (matchingChild.isPresent()) {
+                var filteredChild = applyFilter(filterChild, matchingChild.get());
+                if (filteredChild != null) {
+                    builder.withChild(filteredChild);
+                }
+            }
+        }
+        return builder.build();
+    }
+
+    private static MapNode filterMapNode(final MapNode filter, final MapNode data) {
+        final var builder = ImmutableNodes.mapNodeBuilder(filter.getIdentifier());
+        for (var filterEntry : filter.body().iterator()) {
+            var matchingEntry = data.findChildByArg(filterEntry.getIdentifier());
+            if (matchingEntry.isPresent()) {
+                var filteredEntry = applyFilter(filterEntry, matchingEntry.get());
+                if (filteredEntry != null) {
+                    builder.withChild(filteredEntry);
+                }
+            }
+        }
+        return builder.build();
+    }
+
+    private static LeafNode filterLeafNode(final LeafNode filter, final LeafNode data) {
+        // Perform a shallow comparison of the filter and data
+        if (Objects.equals(filter.body(), data.body())) {
+            return data;
+        }
+        return null;
+    }
+}
