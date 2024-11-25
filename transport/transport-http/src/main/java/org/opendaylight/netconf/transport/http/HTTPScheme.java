c13ac8affbfd3b1275f90ad407a2ab5735272ed5
Robert Varga <robert.varga@pantheon.tech>
2024-11-25 13:36:49 +0100

    Refactor HTTPScheme.initializeServerPipeline()
    The split nature of setup makes it quite unclear as to what is going on.
    
    This improves the code flow, as we now understand that channelHandler()
    is a HTTP2->HTTP1 thing -- hence we can allocate it only when needed.
    This leads to TLS-based-HTTP1 not allocating it at all.
    
    JIRA: NETCONF-1419
    Change-Id: Ib8998b648fac27536c2b7b64fac6818de1fb0374
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- transport/transport-http/src/main/java/org/opendaylight/netconf/transport/http/HTTPScheme.java
+++ transport/transport-http/src/main/java/org/opendaylight/netconf/transport/http/HTTPScheme.java
@@ -22,9 +22,9 @@ import io.netty.handler.codec.http2.CleartextHttp2ServerUpgradeHandler;
 import io.netty.handler.codec.http2.DefaultHttp2Connection;
 import io.netty.handler.codec.http2.DelegatingDecompressorFrameListener;
 import io.netty.handler.codec.http2.Http2CodecUtil;
-import io.netty.handler.codec.http2.Http2ConnectionHandler;
 import io.netty.handler.codec.http2.Http2FrameLogger;
 import io.netty.handler.codec.http2.Http2ServerUpgradeCodec;
+import io.netty.handler.codec.http2.HttpToHttp2ConnectionHandler;
 import io.netty.handler.codec.http2.HttpToHttp2ConnectionHandlerBuilder;
 import io.netty.handler.codec.http2.InboundHttp2ToHttpAdapterBuilder;
 import io.netty.handler.logging.LogLevel;
@@ -44,16 +44,20 @@ public enum HTTPScheme {
      * The <a href="https://www.rfc-editor.org/rfc/rfc9110#section-4.2.1">http scheme</a>.
      */
     HTTP(HttpScheme.HTTP) {
+        private static final Http2FrameLogger FRAME_LOGGER = new Http2FrameLogger(LogLevel.INFO, "Clear2To1");
+
         @Override
-        void initializeServerPipeline(final ChannelPipeline pipeline, final Http2ConnectionHandler connectionHandler) {
+        void initializeServerPipeline(final ChannelPipeline pipeline) {
             // Cleartext upgrade flow
             final var sourceCodec = new HttpServerCodec();
+            final var twoToOne = http2toHttp1(FRAME_LOGGER);
             pipeline
                 .addLast(new CleartextHttp2ServerUpgradeHandler(
                     sourceCodec,
                     new HttpServerUpgradeHandler(sourceCodec,
                         protocol -> AsciiString.contentEquals(Http2CodecUtil.HTTP_UPGRADE_PROTOCOL_NAME, protocol)
-                            ? new Http2ServerUpgradeCodec(connectionHandler) : null), connectionHandler))
+                            ? new Http2ServerUpgradeCodec(twoToOne) : null),
+                    twoToOne))
                 .addLast(new SimpleChannelInboundHandler<HttpMessage>() {
                     @Override
                     protected void channelRead0(final ChannelHandlerContext ctx, final HttpMessage request) {
@@ -85,26 +89,42 @@ public enum HTTPScheme {
      */
     HTTPS(HttpScheme.HTTPS) {
         @Override
-        void initializeServerPipeline(final ChannelPipeline pipeline, final Http2ConnectionHandler connectionHandler) {
-            // Application protocol negotiator over TLS
-            pipeline.addLast(new ApplicationProtocolNegotiationHandler(ApplicationProtocolNames.HTTP_1_1) {
-                @Override
-                protected void configurePipeline(final ChannelHandlerContext ctx, final String protocol) {
-                    switch (protocol) {
-                        case null -> throw new NullPointerException();
-                        case ApplicationProtocolNames.HTTP_1_1 -> ctx.pipeline()
-                            .addAfter(ctx.name(), null, new HttpObjectAggregator(HTTPServer.MAX_HTTP_CONTENT_LENGTH))
-                            .addAfter(ctx.name(), null, new HttpServerKeepAliveHandler())
-                            .replace(this, null, new HttpServerCodec());
-                        case ApplicationProtocolNames.HTTP_2 -> ctx.pipeline().replace(this, null, connectionHandler);
-                        default -> throw new IllegalStateException("unknown protocol: " + protocol);
-                    }
-                }
-            });
+        void initializeServerPipeline(final ChannelPipeline pipeline) {
+            pipeline.addLast(new AlpnHandler());
         }
     };
 
-    private static final Http2FrameLogger FRAME_LOGGER = new Http2FrameLogger(LogLevel.INFO, "Server");
+    /**
+     * Application-Level Protocol Negotiation-based channel pipeline configurator.
+     */
+    private static final class AlpnHandler extends ApplicationProtocolNegotiationHandler {
+        private static final Http2FrameLogger FRAME_LOGGER = new Http2FrameLogger(LogLevel.INFO, "Alpn2To1");
+
+        AlpnHandler() {
+            super(ApplicationProtocolNames.HTTP_1_1);
+        }
+
+        @Override
+        protected void configurePipeline(final ChannelHandlerContext ctx, final String protocol) {
+            switch (protocol) {
+                case null -> throw new NullPointerException();
+                case ApplicationProtocolNames.HTTP_1_1 -> configureHttp1(ctx);
+                case ApplicationProtocolNames.HTTP_2 -> configureHttp2(ctx);
+                default -> throw new IllegalStateException("unknown protocol: " + protocol);
+            }
+        }
+
+        private void configureHttp1(final ChannelHandlerContext ctx) {
+            ctx.pipeline()
+                .addAfter(ctx.name(), null, new HttpObjectAggregator(HTTPServer.MAX_HTTP_CONTENT_LENGTH))
+                .addAfter(ctx.name(), null, new HttpServerKeepAliveHandler())
+                .replace(this, null, new HttpServerCodec());
+        }
+
+        private void configureHttp2(final ChannelHandlerContext ctx) {
+            ctx.pipeline().replace(this, null, http2toHttp1(FRAME_LOGGER));
+        }
+    }
 
     private final @NonNull HttpScheme netty;
 
@@ -137,26 +157,25 @@ public enum HTTPScheme {
         return ret;
     }
 
-    final void initializeServerPipeline(final ChannelPipeline pipeline) {
-        // External HTTP 2 to internal HTTP 1.1 adapter handler
-        final var connection = new DefaultHttp2Connection(true);
-        final var frameListener = new InboundHttp2ToHttpAdapterBuilder(connection)
-            .maxContentLength(HTTPServer.MAX_HTTP_CONTENT_LENGTH)
-            .propagateSettings(true)
-            .build();
-
-        initializeServerPipeline(pipeline, new HttpToHttp2ConnectionHandlerBuilder()
-            .frameListener(new DelegatingDecompressorFrameListener(connection, frameListener))
-            .connection(connection)
-            .frameLogger(FRAME_LOGGER)
-            .gracefulShutdownTimeoutMillis(0L)
-            .build());
-    }
-
-    abstract void initializeServerPipeline(ChannelPipeline pipeline, Http2ConnectionHandler connectionHandler);
+    abstract void initializeServerPipeline(ChannelPipeline pipeline);
 
     @Override
     public String toString() {
         return netty.toString();
+    }
+
+    // External HTTP 2 to internal HTTP 1.1 adapter handler
+    private static HttpToHttp2ConnectionHandler http2toHttp1(final Http2FrameLogger frameLogger) {
+        final var connection = new DefaultHttp2Connection(true);
+        return new HttpToHttp2ConnectionHandlerBuilder()
+            .connection(connection)
+            .frameListener(
+                new DelegatingDecompressorFrameListener(connection, new InboundHttp2ToHttpAdapterBuilder(connection)
+                    .maxContentLength(HTTPServer.MAX_HTTP_CONTENT_LENGTH)
+                    .propagateSettings(true)
+                    .build()))
+            .frameLogger(frameLogger)
+            .gracefulShutdownTimeoutMillis(0L)
+            .build();
     }
 }
