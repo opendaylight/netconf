adeee7c3ce87568d91a807e0d786bcc7e8e8c345
Ivan Hrasko <ivan.hrasko@pantheon.tech>
2024-11-28 14:23:32 +0100

    [WIP] Add SubscribedEventStreamService
    Add dedicated implementation of EventStreamService for purposes
    of subscribed RESTCONF notifications (RFC 8639) which are using its
    own model and have no location requirement compared to RFC 8040
    streams.
    
    JIRA: NETCONF-714
    Change-Id: Ic6fd0943559dc051adcb77af178cdff4768eda0a
    Signed-off-by: Ivan Hrasko <ivan.hrasko@pantheon.tech>
    Signed-off-by: Matej Sramcik <matej.sramcik@pantheon.tech>

---
+++ apps/restconf-subscription/src/main/java/org/opendaylight/restconf/subscription/SubscribedEventStreamService.java
@@ -0,0 +1,130 @@
+/*
+ * Copyright (c) 2024 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.restconf.subscription;
+
+import static java.util.Objects.requireNonNull;
+
+import io.netty.handler.codec.http.QueryStringDecoder;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.NoSuchElementException;
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.opendaylight.netconf.transport.http.ErrorResponseException;
+import org.opendaylight.netconf.transport.http.EventStreamListener;
+import org.opendaylight.netconf.transport.http.EventStreamService;
+import org.opendaylight.restconf.api.query.PrettyPrintParam;
+import org.opendaylight.restconf.server.NettyMediaTypes;
+import org.opendaylight.restconf.server.PathParameters;
+import org.opendaylight.restconf.server.api.ServerError;
+import org.opendaylight.restconf.server.api.YangErrorsBody;
+import org.opendaylight.restconf.server.spi.ErrorTagMapping;
+import org.opendaylight.restconf.server.spi.RestconfStream;
+import org.opendaylight.yangtools.yang.common.ErrorTag;
+import org.opendaylight.yangtools.yang.common.ErrorType;
+import org.opendaylight.yangtools.yang.common.Uint32;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@NonNullByDefault
+public class SubscribedEventStreamService implements EventStreamService {
+    private static final Logger LOG = LoggerFactory.getLogger(SubscribedEventStreamService.class);
+
+    private static final String INVALID_STREAM_URI_ERROR = "Invalid stream URI";
+    private static final String MISSING_PARAMS_ERROR = "Both stream encoding and subscriptionId are required.";
+    private static final String UNKNOWN_STREAM_ERROR = "Requested stream does not exist";
+    private static final int ERROR_BUF_SIZE = 2048;
+
+
+    private final SubscriptionStateMachine machine;
+    private final String basePath;
+    private final ErrorTagMapping errorTagMapping;
+    private final RestconfStream.EncodingName defaultEncoding;
+    private final PrettyPrintParam defaultPrettyPrint;
+
+    public SubscribedEventStreamService(final SubscriptionStateMachine machine, final String basePath,
+            final ErrorTagMapping errorTagMapping, final RestconfStream.EncodingName defaultEncoding,
+            final PrettyPrintParam defaultPrettyPrint) {
+        this.machine = requireNonNull(machine);
+        this.basePath = basePath;
+        this.errorTagMapping = errorTagMapping;
+        this.defaultEncoding = defaultEncoding;
+        this.defaultPrettyPrint = defaultPrettyPrint;
+    }
+
+    @Override
+    public void startEventStream(final String requestUri, final EventStreamListener listener,
+            final StartCallback callback) {
+        LOG.info("Starting subscribed stream at: {}", requestUri);
+        // TODO implement this similar to RestconfStreamService using SubscriptionStateMachine
+        // FIXME how to verify we are on the correct transport session?
+        // if (machine.getSubscriptionSession(Uint32.valueOf(requestUri)) != null) {
+        //     callback.onStartFailure();
+        // }
+
+
+        final var decoder = new QueryStringDecoder(requestUri);
+
+        final var pathParams = PathParameters.from(decoder.path(), basePath);
+        if (!PathParameters.STREAMS.equals(pathParams.apiResource())) {
+            callback.onStartFailure(errorResponse(ErrorTag.DATA_MISSING, INVALID_STREAM_URI_ERROR, defaultEncoding));
+            return;
+        }
+        final var args = pathParams.childIdentifier().split("/", 2);
+        final var streamEncoding = encoding(args[0]);
+        final var streamName = args.length > 1 ? args[1] : null;
+        if (streamEncoding == null || streamName == null || streamName.isEmpty()) {
+            callback.onStartFailure(errorResponse(ErrorTag.BAD_ATTRIBUTE, MISSING_PARAMS_ERROR,
+                streamEncoding == null ? defaultEncoding : streamEncoding));
+            return;
+        }
+
+        try {
+            if (machine.getSubscriptionSession(Uint32.valueOf(requestUri)) != null){
+                callback.onStreamStarted();
+            } else {
+                callback.onStartFailure(errorResponse(ErrorTag.DATA_MISSING, UNKNOWN_STREAM_ERROR, streamEncoding));
+            }
+        } catch (IllegalArgumentException | NoSuchElementException e) {
+            callback.onStartFailure(errorResponse(ErrorTag.BAD_ATTRIBUTE, e.getMessage(), streamEncoding));
+        }
+
+    }
+
+    private static RestconfStream.EncodingName encoding(final String encodingName) {
+        try {
+            return new RestconfStream.EncodingName(encodingName);
+        } catch (IllegalArgumentException e) {
+            LOG.warn("Stream encoding name '{}' is invalid: {}. Ignored.", encodingName, e.getMessage());
+            return null;
+        }
+    }
+
+    private Exception errorResponse(final ErrorTag errorTag, final String errorMessage,
+        final RestconfStream.EncodingName encoding) {
+        final var yangErrorsBody =
+            new YangErrorsBody(List.of(new ServerError(ErrorType.PROTOCOL, errorTag, errorMessage)));
+        final var statusCode = errorTagMapping.statusOf(errorTag).code();
+        try (var out = new ByteArrayOutputStream(ERROR_BUF_SIZE)) {
+            if (RestconfStream.EncodingName.RFC8040_JSON.equals(encoding)) {
+                yangErrorsBody.formatToJSON(defaultPrettyPrint, out);
+                return new ErrorResponseException(statusCode, out.toString(StandardCharsets.UTF_8),
+                    NettyMediaTypes.APPLICATION_YANG_DATA_JSON);
+            } else {
+                yangErrorsBody.formatToXML(defaultPrettyPrint, out);
+                return new ErrorResponseException(statusCode, out.toString(StandardCharsets.UTF_8),
+                    NettyMediaTypes.APPLICATION_YANG_DATA_XML);
+            }
+        } catch (IOException e) {
+            LOG.error("Failure encoding error message", e);
+            // return as plain text
+            return new IllegalStateException(errorMessage);
+        }
+    }
+}
