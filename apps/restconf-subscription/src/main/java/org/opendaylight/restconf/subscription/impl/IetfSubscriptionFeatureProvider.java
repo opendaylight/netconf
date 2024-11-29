86d28e3f2367142fa4f828bc19b206a5266010dd
Oleksandr Zharov <oleksandr.zharov@pantheon.tech>
2024-11-29 19:17:19 +0100

    Expose supported features from RFC 8639
    This patch provides class that exposes supprorted featers
    from RFC 8639.
    
    JIRA: NETCONF-714
    Change-Id: Ia096f29c22c0591f476047fcc55ca562224b70a3
    Signed-off-by: Oleksandr Zharov <oleksandr.zharov@pantheon.tech>

---
+++ apps/restconf-subscription/src/main/java/org/opendaylight/restconf/subscription/impl/IetfSubscriptionFeatureProvider.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2024 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.restconf.subscription.impl;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.kohsuke.MetaInfServices;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.subscribed.notifications.rev190909.EncodeJson$F;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.subscribed.notifications.rev190909.EncodeXml$F;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.subscribed.notifications.rev190909.IetfSubscribedNotificationsData;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.subscribed.notifications.rev190909.Subtree;
+import org.opendaylight.yangtools.binding.YangFeature;
+import org.opendaylight.yangtools.binding.meta.YangFeatureProvider;
+import java.util.Set;
+
+/**
+ * Baseline features supported from {@code ietf-subscribed-notifications.yang}.
+ */
+@MetaInfServices
+@NonNullByDefault
+public class IetfSubscriptionFeatureProvider implements YangFeatureProvider<IetfSubscribedNotificationsData> {
+    @Override
+    public Class<IetfSubscribedNotificationsData> boundModule() {
+        return IetfSubscribedNotificationsData.class;
+    }
+
+    @Override
+    public Set<? extends YangFeature<?, IetfSubscribedNotificationsData>> supportedFeatures() {
+        return Set.of(EncodeJson$F.VALUE,
+            EncodeXml$F.VALUE,
+            Subtree.VALUE);
+    }
+}
