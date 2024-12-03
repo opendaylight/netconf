d0d954280558029d2b3ae324724c7dc98ea0ae62
Ivan Hrasko <ivan.hrasko@pantheon.tech>
2024-12-03 09:28:08 +0100

    Refactor AbstractRestconfStreamRegistry
    Refactor AbstractRestconfStreamRegistry in a way that createStream
    method is moved to MdsalRestconfStreamRegistry in order to allow us
    to add other overrides, for example for subscribed notifications.
    
    JIRA: NETCONF-714
    Change-Id: I5be4e7d51969f8aca024badd8173043dc1c5db68
    Signed-off-by: Ivan Hrasko <ivan.hrasko@pantheon.tech>
    Signed-off-by: Oleksandr Zharov <oleksandr.zharov@pantheon.tech>
    Signed-off-by: Samuel Schneider <samuel.schneider@pantheon.tech>
    Signed-off-by: Yaroslav Lastivka <yaroslav.lastivka@pantheon.tech>

--- protocol/restconf-server-spi/src/main/java/org/opendaylight/restconf/server/spi/AbstractRestconfStreamRegistry.java
+++ protocol/restconf-server-spi/src/main/java/org/opendaylight/restconf/server/spi/AbstractRestconfStreamRegistry.java
@@ -9,29 +9,17 @@ package org.opendaylight.restconf.server.spi;
 
 import static java.util.Objects.requireNonNull;
 
-import com.google.common.annotations.VisibleForTesting;
 import com.google.common.util.concurrent.FutureCallback;
 import com.google.common.util.concurrent.Futures;
 import com.google.common.util.concurrent.ListenableFuture;
 import com.google.common.util.concurrent.MoreExecutors;
-import java.net.URI;
-import java.util.Set;
-import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 import org.eclipse.jdt.annotation.NonNull;
 import org.eclipse.jdt.annotation.Nullable;
-import org.opendaylight.restconf.server.api.ServerException;
-import org.opendaylight.restconf.server.api.ServerRequest;
-import org.opendaylight.restconf.server.spi.RestconfStream.EncodingName;
-import org.opendaylight.restconf.server.spi.RestconfStream.Source;
-import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.restconf.monitoring.rev170126.restconf.state.streams.Stream;
-import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.restconf.monitoring.rev170126.restconf.state.streams.stream.Access;
 import org.opendaylight.yangtools.yang.common.QName;
-import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.NodeIdentifier;
 import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.NodeIdentifierWithPredicates;
 import org.opendaylight.yangtools.yang.data.api.schema.MapEntryNode;
-import org.opendaylight.yangtools.yang.data.spi.node.ImmutableNodes;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -41,20 +29,13 @@ import org.slf4j.LoggerFactory;
 public abstract class AbstractRestconfStreamRegistry implements RestconfStream.Registry {
     private static final Logger LOG = LoggerFactory.getLogger(AbstractRestconfStreamRegistry.class);
 
-    @VisibleForTesting
-    public static final QName NAME_QNAME =  QName.create(Stream.QNAME, "name").intern();
-    @VisibleForTesting
-    public static final QName DESCRIPTION_QNAME = QName.create(Stream.QNAME, "description").intern();
-    @VisibleForTesting
-    public static final QName ENCODING_QNAME =  QName.create(Stream.QNAME, "encoding").intern();
-    @VisibleForTesting
-    public static final QName LOCATION_QNAME =  QName.create(Stream.QNAME, "location").intern();
-
     private final ConcurrentMap<String, RestconfStream<?>> streams = new ConcurrentHashMap<>();
-    private final RestconfStream.LocationProvider locationProvider;
+    private final QName nameQname;
+    private final QName streamQname;
 
-    protected AbstractRestconfStreamRegistry(final RestconfStream.LocationProvider locationProvider) {
-        this.locationProvider = requireNonNull(locationProvider);
+    protected AbstractRestconfStreamRegistry(final QName streamQname) {
+        this.streamQname = requireNonNull(streamQname);
+        nameQname = QName.create(streamQname, "name").intern();
     }
 
     @Override
@@ -62,45 +43,16 @@ public abstract class AbstractRestconfStreamRegistry implements RestconfStream.R
         return streams.get(requireNonNull(name));
     }
 
-    @Override
-    public final <T> void createStream(final ServerRequest<RestconfStream<T>> request, final URI restconfURI,
-            final Source<T> source, final String description) {
-        final var baseStreamLocation = locationProvider.baseStreamLocation(restconfURI);
-        final var stream = allocateStream(source);
-        final var name = stream.name();
-        if (description.isBlank()) {
-            throw new IllegalArgumentException("Description must be descriptive");
-        }
-
-        Futures.addCallback(putStream(streamEntry(name, description, baseStreamLocation.toString(),
-            stream.encodings())), new FutureCallback<Object>() {
-                @Override
-                public void onSuccess(final Object result) {
-                    LOG.debug("Stream {} added", name);
-                    request.completeWith(stream);
-                }
-
-                @Override
-                public void onFailure(final Throwable cause) {
-                    LOG.debug("Failed to add stream {}", name, cause);
-                    streams.remove(name, stream);
-                    request.completeWith(new ServerException("Failed to allocate stream " + name, cause));
-                }
-            }, MoreExecutors.directExecutor());
+    protected RestconfStream<?> registerStream(final String name, final RestconfStream<?> stream) {
+        return streams.putIfAbsent(name, stream);
     }
 
-    private <T> RestconfStream<T> allocateStream(final Source<T> source) {
-        String name;
-        RestconfStream<T> stream;
-        do {
-            // Use Type 4 (random) UUID. While we could just use it as a plain string, be nice to observers and anchor
-            // it into UUID URN namespace as defined by RFC4122
-            name = "urn:uuid:" + UUID.randomUUID().toString();
-            stream = new RestconfStream<>(this, source, name);
-        } while (streams.putIfAbsent(name, stream) != null);
-
-        return stream;
+    protected void unregisterStream(final String name, final RestconfStream<?> stream) {
+        streams.remove(name, stream);
     }
+
+    // FIXME remake into protected
+    public abstract @NonNull ListenableFuture<?> putStream(@NonNull MapEntryNode stream);
 
     /**
      * Remove a particular stream and remove its entry from operational datastore.
@@ -115,7 +67,7 @@ public abstract class AbstractRestconfStreamRegistry implements RestconfStream.R
             return;
         }
 
-        Futures.addCallback(deleteStream(NodeIdentifierWithPredicates.of(Stream.QNAME, NAME_QNAME, name)),
+        Futures.addCallback(deleteStream(NodeIdentifierWithPredicates.of(streamQname, nameQname, name)),
             new FutureCallback<Object>() {
                 @Override
                 public void onSuccess(final Object result) {
@@ -132,27 +84,4 @@ public abstract class AbstractRestconfStreamRegistry implements RestconfStream.R
     }
 
     protected abstract @NonNull ListenableFuture<?> deleteStream(@NonNull NodeIdentifierWithPredicates streamName);
-
-    @VisibleForTesting
-    public static final @NonNull MapEntryNode streamEntry(final String name, final String description,
-            final String baseStreamLocation, final Set<EncodingName> encodings) {
-        final var accessBuilder = ImmutableNodes.newSystemMapBuilder()
-            .withNodeIdentifier(new NodeIdentifier(Access.QNAME));
-        for (var encoding : encodings) {
-            final var encodingName = encoding.name();
-            accessBuilder.withChild(ImmutableNodes.newMapEntryBuilder()
-                .withNodeIdentifier(NodeIdentifierWithPredicates.of(Access.QNAME, ENCODING_QNAME, encodingName))
-                .withChild(ImmutableNodes.leafNode(ENCODING_QNAME, encodingName))
-                .withChild(ImmutableNodes.leafNode(LOCATION_QNAME,
-                    baseStreamLocation + '/' + encodingName + '/' + name))
-                .build());
-        }
-
-        return ImmutableNodes.newMapEntryBuilder()
-            .withNodeIdentifier(NodeIdentifierWithPredicates.of(Stream.QNAME, NAME_QNAME, name))
-            .withChild(ImmutableNodes.leafNode(NAME_QNAME, name))
-            .withChild(ImmutableNodes.leafNode(DESCRIPTION_QNAME, description))
-            .withChild(accessBuilder.build())
-            .build();
-    }
 }
