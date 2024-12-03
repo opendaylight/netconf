d07e748284fc7e310c588f2afa1c27c9d84680bd
Ivan Hrasko <ivan.hrasko@pantheon.tech>
2024-12-03 09:28:08 +0100

    Refactor AbstractRestconfStreamRegistry
    Refactor AbstractRestconfStreamRegistry in a way that createStream
    method is moved to MdsalRestconfStreamRegistry in order to allow us
    to add other overrides, for example for subscribed notifications.
    
    JIRA: NETCONF-714
    Change-Id: I5be4e7d51969f8aca024badd8173043dc1c5db68
    Signed-off-by: Ivan Hrasko <ivan.hrasko@pantheon.tech>
    Signed-off-by: Oleksandr Zharov <oleksandr.zharov@pantheon.tech>

--- protocol/restconf-server-spi/src/main/java/org/opendaylight/restconf/server/spi/RestconfStream.java
+++ protocol/restconf-server-spi/src/main/java/org/opendaylight/restconf/server/spi/RestconfStream.java
@@ -173,7 +173,8 @@ public final class RestconfStream<T> {
             String description);
 
         //FIXME: Restore to be protected
-        @NonNull ListenableFuture<?> putStream(@NonNull MapEntryNode stream);
+        @NonNull
+        ListenableFuture<?> putStream(@NonNull MapEntryNode stream);
     }
 
     private static final Logger LOG = LoggerFactory.getLogger(RestconfStream.class);
@@ -219,7 +220,7 @@ public final class RestconfStream<T> {
 
     private Registration registration;
 
-    RestconfStream(final AbstractRestconfStreamRegistry registry, final Source<T> source, final String name) {
+    public RestconfStream(final AbstractRestconfStreamRegistry registry, final Source<T> source, final String name) {
         this.registry = requireNonNull(registry);
         this.source = requireNonNull(source);
         this.name = requireNonNull(name);
@@ -241,7 +242,7 @@ public final class RestconfStream<T> {
      * @return Supported encodings.
      */
     @SuppressWarnings("null")
-    @NonNull Set<EncodingName> encodings() {
+    public @NonNull Set<EncodingName> encodings() {
         return source.encodings.keySet();
     }
 
