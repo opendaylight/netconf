3e5b99369b2359dfe85c48eace400b7f7ed68fcb
Robert Varga <robert.varga@pantheon.tech>
2024-12-11 01:40:47 +0100

    WIP: Stream FiniteResponse
    Rather than collecting the entirety of a FiniteResponse, stream its
    contents to the underlying channel through a blocking process.
    
    Since a FiniteResponse does not have the response body ready, and the
    body can be huge, this patch introduces the mechanics to send the body
    in chunks.
    
    // FIXME: route to channel
    // FIXME: backpressure
    
    JIRA: NETCONF-1431
    Change-Id: If50d50af891fbbd9ca42b0486359a76b3b1a9494
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>
    Signed-off-by: Yaroslav Lastivka <yaroslav.lastivka@pantheon.tech>
    Signed-off-by: Matej Sramcik <matej.sramcik@pantheon.tech>

---
+++ transport/transport-http/src/main/java/org/opendaylight/netconf/transport/http/ResponseOutput.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright (c) 2024 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.netconf.transport.http;
+
+import static java.util.Objects.requireNonNull;
+
+import com.google.common.annotations.Beta;
+import com.google.common.base.MoreObjects;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.http.HttpResponseStatus;
+import io.netty.handler.codec.http.HttpVersion;
+import io.netty.handler.codec.http.ReadOnlyHttpHeaders;
+import io.netty.util.AsciiString;
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.eclipse.jdt.annotation.Nullable;
+
+/**
+ * Streaming interface for emitting the contents of a {@link FiniteResponse} to the session. It supports only a single
+ * operation, {@link #start(HttpResponseStatus, ReadOnlyHttpHeaders)}, from which the caller will get a
+ * {@link ResponseBodyOutputStream}, to which they can append the response body.
+ */
+@Beta
+@NonNullByDefault
+public final class ResponseOutput {
+    private final ChannelHandlerContext ctx;
+    @Nullable
+    private final Integer streamId;
+    private final HttpVersion version;
+
+    ResponseOutput(final ChannelHandlerContext ctx, final @Nullable Integer streamId, final HttpVersion version) {
+        this.ctx = requireNonNull(ctx);
+        this.streamId = streamId;
+        this.version = version;
+    }
+
+    public ResponseBodyOutputStream start(final HttpResponseStatus status) {
+        return start(status, null);
+    }
+
+    public ResponseBodyOutputStream start(final HttpResponseStatus status, final AsciiString name,
+            final CharSequence value) {
+        return start(status, new ReadOnlyHttpHeaders(HeadersResponse.VALIDATE_HEADERS, name, value));
+    }
+
+    public ResponseBodyOutputStream start(final HttpResponseStatus status,
+            final @Nullable ReadOnlyHttpHeaders headers) {
+        return new ResponseBodyOutputStream(ctx, status, headers != null ? headers : HeadersResponse.EMPTY_HEADERS,
+            streamId, version);
+    }
+
+    @Override
+    public String toString() {
+        return MoreObjects.toStringHelper(this).add("channel", ctx.channel()).toString();
+    }
+}
