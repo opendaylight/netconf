212d9fcb2abba9bc454cefda34614189039bc258
Yaroslav Lastivka <yaroslav.lastivka@pantheon.tech>
2025-01-20 17:10:14 +0200

    Create Subscribed-notifications tests
    Added tests for establishing, modifying, deleting, and killing subscriptions.
    Included validation tests for missing fields and invalid inputs.
    
    JIRA: NETCONF-714
    Change-Id: Ie4cc6d27f39dcca3f61bdd5e47613c29a56a6533
    Signed-off-by: Yaroslav Lastivka <yaroslav.lastivka@pantheon.tech>

--- apps/restconf-it/src/test/java/org/opendaylight/restconf/subscription/AbstractNotificationSubscriptionTest.java
+++ apps/restconf-it/src/test/java/org/opendaylight/restconf/subscription/AbstractNotificationSubscriptionTest.java
@@ -10,6 +10,7 @@ package org.opendaylight.restconf.subscription;
 import static org.awaitility.Awaitility.await;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.netty.buffer.Unpooled;
 import io.netty.handler.codec.http.DefaultFullHttpRequest;
 import io.netty.handler.codec.http.FullHttpRequest;
@@ -73,7 +74,9 @@ public class AbstractNotificationSubscriptionTest extends AbstractDataBrokerTest
     private static final String PASSWORD = "pa$$w0Rd";
     private static final String RESTCONF = "restconf";
 
+    protected static final ObjectMapper MAPPER = new ObjectMapper();
     protected static final String APPLICATION_JSON = "application/json";
+
 
     private static String localAddress;
     private static BootstrapFactory bootstrapFactory;
@@ -171,18 +174,14 @@ public class AbstractNotificationSubscriptionTest extends AbstractDataBrokerTest
         sshTransportStackFactory.close();
     }
 
-    protected FullHttpResponse invokeRequest(final HttpMethod method, final String uri) throws Exception {
-        return invokeRequest(buildRequest(method, uri, APPLICATION_JSON, null, null));
+    protected FullHttpResponse invokeRequest(final HttpMethod method, final String uri, final String acceptType)
+            throws Exception {
+        return invokeRequest(buildRequest(method, uri, null, null, acceptType));
     }
 
     protected FullHttpResponse invokeRequest(final HttpMethod method, final String uri, final String mediaType,
             final String content) throws Exception {
         return invokeRequest(buildRequest(method, uri, mediaType, content, null));
-    }
-
-    protected FullHttpResponse invokeRequest(final HttpMethod method, final String uri, final String mediaType,
-            final String acceptType, final String content) throws Exception {
-        return invokeRequest(buildRequest(method, uri, mediaType, content, acceptType));
     }
 
     protected FullHttpResponse invokeRequest(final FullHttpRequest request) throws Exception {
@@ -236,4 +235,39 @@ public class AbstractNotificationSubscriptionTest extends AbstractDataBrokerTest
             throw new AssertionError(e);
         }
     }
+
+    /**
+     * Utility method to extract subscription ID from response.
+     */
+    protected String extractSubscriptionId(final FullHttpResponse response) {
+        final var responseBody = response.content().toString(StandardCharsets.UTF_8);
+        final var subscriptionIdKey = "\"subscription-id\":";
+        final var startIndex = responseBody.indexOf(subscriptionIdKey) + subscriptionIdKey.length();
+        final var endIndex = responseBody.indexOf("}", startIndex);
+        return responseBody.substring(startIndex, endIndex).replaceAll("[^0-9]", "").trim();
+    }
+
+    /**
+     * Utility method to establish a subscription.
+     */
+    protected FullHttpResponse establishSubscription(final String stream, final String encoding) throws Exception {
+        final var input = String.format("""
+            {
+              "input": {
+                "stream": "%s",
+                "encoding": "%s"
+              }
+            }""", stream, encoding);
+
+        return invokeRequest(HttpMethod.POST,
+            "/restconf/operations/ietf-subscribed-notifications:establish-subscription",
+            APPLICATION_JSON, input);
+    }
+
+/*
+    protected String extractSubscriptionId(final FullHttpResponse response) throws Exception {
+        final var responseBody = response.content().toString(StandardCharsets.UTF_8);
+        final var jsonNode = MAPPER.readTree(responseBody);
+        return jsonNode.get("output").get("subscription-id").asText();
+    }*/
 }
