e648d33a29d2da128cee2587adbcaeb61cd7e3c6
Peter Suna <peter.suna@pantheon.tech>
2025-02-11 15:54:06 +0100

    Fix YANG patch repy on edit-config error
    When an edit-config operation failed, the result only showed
    a global error. This commit tracks the failed edit-config operations
    and updates the editCollection with correct PatchStatusEntity.
    
    JIRA: NETCONF-1197
    Change-Id: I6acc2b0e4ddf60eef1cc4102305a1e2529d9218b
    Signed-off-by: Peter Suna <peter.suna@pantheon.tech>

--- apps/restconf-nb/src/test/java/org/opendaylight/restconf/mdsal/spi/data/NetconfRestconfStrategyTest.java
+++ apps/restconf-nb/src/test/java/org/opendaylight/restconf/mdsal/spi/data/NetconfRestconfStrategyTest.java
@@ -62,6 +62,7 @@ import org.opendaylight.yangtools.yang.common.Empty;
 import org.opendaylight.yangtools.yang.common.ErrorSeverity;
 import org.opendaylight.yangtools.yang.common.ErrorTag;
 import org.opendaylight.yangtools.yang.common.ErrorType;
+import org.opendaylight.yangtools.yang.common.RpcResultBuilder;
 import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
 import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.NodeIdentifier;
 import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.NodeIdentifierWithPredicates;
@@ -420,6 +421,22 @@ final class NetconfRestconfStrategyTest extends AbstractRestconfStrategyTest {
             .create(LogicalDatastoreType.CONFIGURATION, PLAYER_IID, EMPTY_JUKEBOX, Optional.empty());
         doReturn(Futures.immediateFuture(new DefaultDOMRpcResult())).when(netconfService)
             .delete(LogicalDatastoreType.CONFIGURATION, CREATE_AND_DELETE_TARGET);
+        return jukeboxDataOperations();
+    }
+
+    @Override
+    RestconfStrategy testPatchWithDataExistExceptionStrategy() {
+        mockLockUnlock();
+        doReturn(Futures.immediateFuture(null)).when(netconfService).discardChanges();
+
+        final var rpcError = RpcResultBuilder.newError(ErrorType.PROTOCOL,
+            ErrorTag.DATA_EXISTS, "Data already exists", null, "", null);
+        doReturn(Futures.immediateFuture(new DefaultDOMRpcResult())).when(netconfService).merge(any(), any(),
+            any(), any());
+        doReturn(Futures.immediateFuture(new DefaultDOMRpcResult())).when(netconfService)
+            .replace(any(), any(), any(), any());
+        doReturn(Futures.immediateFuture(new DefaultDOMRpcResult(rpcError))).when(netconfService)
+            .create(LogicalDatastoreType.CONFIGURATION, PLAYER_IID, EMPTY_JUKEBOX, Optional.empty());
         return jukeboxDataOperations();
     }
 
--- plugins/restconf-mdsal-spi/src/main/java/org/opendaylight/restconf/mdsal/spi/data/NetconfRestconfTransaction.java
+++ plugins/restconf-mdsal-spi/src/main/java/org/opendaylight/restconf/mdsal/spi/data/NetconfRestconfTransaction.java
@@ -24,6 +24,7 @@ import java.util.Map;
 import java.util.Optional;
 import java.util.StringJoiner;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Supplier;
 import java.util.stream.Collectors;
 import org.eclipse.jdt.annotation.NonNull;
@@ -62,6 +63,7 @@ final class NetconfRestconfTransaction extends RestconfTransaction {
     private final NetconfDataTreeService netconfService;
     private final Map<YangInstanceIdentifier, Collection<? extends NormalizedNode>> readListCache =
         new ConcurrentHashMap<>();
+    private final AtomicInteger extraCallsCtr;
 
     private volatile boolean isLocked = false;
 
@@ -84,6 +86,8 @@ final class NetconfRestconfTransaction extends RestconfTransaction {
             }
         }, MoreExecutors.directExecutor());
         resultsFutures.add(lockResult);
+        // Create counter of additional features including lock feature.
+        extraCallsCtr = new AtomicInteger(1);
     }
 
     @Override
@@ -103,6 +107,8 @@ final class NetconfRestconfTransaction extends RestconfTransaction {
             } else {
                 items.forEach(item ->
                     enqueueOperation(() -> netconfService.delete(CONFIGURATION, path.node(item.name()))));
+                // If there is only one item, it is set to the same value, indicating no additional calls.
+                extraCallsCtr.set(items.size() - 1 + extraCallsCtr.get());
             }
         } else {
             enqueueOperation(() -> netconfService.delete(CONFIGURATION, path));
@@ -118,6 +124,8 @@ final class NetconfRestconfTransaction extends RestconfTransaction {
             } else {
                 items.forEach(item ->
                     enqueueOperation(() -> netconfService.remove(CONFIGURATION, path.node(item.name()))));
+                // If there is only one item, it is set to the same value, indicating no additional calls.
+                extraCallsCtr.set(items.size() - 1 + extraCallsCtr.get());
             }
         } else {
             enqueueOperation(() -> netconfService.remove(CONFIGURATION, path));
@@ -161,10 +169,13 @@ final class NetconfRestconfTransaction extends RestconfTransaction {
             final var emptySubTree = fromInstanceId(databind.modelContext(), path);
             merge(YangInstanceIdentifier.of(emptySubTree.name()), emptySubTree);
 
-            for (var child : ((NormalizedNodeContainer<?>) data).body()) {
+            final var bodyCollection = ((NormalizedNodeContainer<?>) data).body();
+            for (var child : bodyCollection) {
                 final var childPath = path.node(child.name());
                 enqueueOperation(() -> netconfService.create(CONFIGURATION, childPath, child, Optional.empty()));
             }
+            // The enqueueOperation call for every child is an additional call to the first merge method.
+            extraCallsCtr.set(bodyCollection.size() + extraCallsCtr.get());
         } else {
             enqueueOperation(() -> netconfService.create(CONFIGURATION, path, data, Optional.empty()));
         }
@@ -175,11 +186,13 @@ final class NetconfRestconfTransaction extends RestconfTransaction {
         if (data instanceof MapNode || data instanceof LeafSetNode) {
             final var emptySubTree = fromInstanceId(databind.modelContext(), path);
             merge(YangInstanceIdentifier.of(emptySubTree.name()), emptySubTree);
-
-            for (var child : ((NormalizedNodeContainer<?>) data).body()) {
+            final var bodyCollection = ((NormalizedNodeContainer<?>) data).body();
+            for (var child : bodyCollection) {
                 final var childPath = path.node(child.name());
                 enqueueOperation(() -> netconfService.replace(CONFIGURATION, childPath, child, Optional.empty()));
             }
+            // The enqueueOperation call for every child is an additional call to the first merge method.
+            extraCallsCtr.set(bodyCollection.size() + extraCallsCtr.get());
         } else {
             enqueueOperation(() -> netconfService.replace(CONFIGURATION, path, data, Optional.empty()));
         }
@@ -257,7 +270,8 @@ final class NetconfRestconfTransaction extends RestconfTransaction {
         final ListenableFuture<? extends DOMRpcResult> operationFuture;
         synchronized (resultsFutures) {
             // if we only have result for the lock operation ...
-            if (resultsFutures.size() == 1) {
+            final int currentSize = resultsFutures.size();
+            if (currentSize == 1) {
                 operationFuture = Futures.transformAsync(resultsFutures.get(0),
                     result -> {
                         // ... then add new operation to the chain if lock was successful
@@ -271,12 +285,14 @@ final class NetconfRestconfTransaction extends RestconfTransaction {
                     MoreExecutors.directExecutor());
             } else {
                 // ... otherwise just add operation to the execution chain
-                operationFuture = Futures.transformAsync(resultsFutures.get(resultsFutures.size() - 1),
+                operationFuture = Futures.transformAsync(resultsFutures.get(currentSize - 1),
                     result -> {
                         if (result != null && !allWarnings(result.errors())) {
                             // The edit-config operation failed. Stop execution and create a
-                            // TransactionEditConfigFailedException with a result error.
-                            final var editConfigFailedException = toEditConfigFailedException(result.errors());
+                            // TransactionEditConfigFailedException with the edit-config number and error.
+                            // Edit-config number subtracts extraCallsCtr to exclude additional created future.
+                            final var editConfigFailedException = toEditConfigFailedException(result.errors(),
+                                currentSize - extraCallsCtr.get());
                             return Futures.immediateFailedFuture(editConfigFailedException);
                         }
                         // If no errors continue in features execution.
@@ -296,10 +312,11 @@ final class NetconfRestconfTransaction extends RestconfTransaction {
     }
 
     private static TransactionEditConfigFailedException toEditConfigFailedException(
-            final Collection<? extends RpcError> errors) {
+            final Collection<? extends RpcError> errors,
+            final int transactionNumber) {
         final var netconfDocumentedException = getNetconfDocumentedException(errors);
         return new TransactionEditConfigFailedException("Netconf transaction edit-config failed",
-            netconfDocumentedException);
+            netconfDocumentedException, transactionNumber);
     }
 
     private static NetconfDocumentedException getNetconfDocumentedException(
