18e9e862e2ad3363b9e71d060a9747ec8b545548
Samuel Schneider <samuel.schneider@pantheon.tech>
2025-02-12 13:11:11 +0100

    WIP: Add test cases for filtering and counters
    JIRA: NETCONF-714
    Change-Id: I6c389bcf4c71880784de660fef7824c1bbf8fc57
    Signed-off-by: Samuel Schneider <samuel.schneider@pantheon.tech>

---
+++ apps/restconf-it/src/test/java/org/opendaylight/restconf/subscription/FilteringSubscriptionTest.java
@@ -0,0 +1,102 @@
+/*
+ * Copyright (c) 2025 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.restconf.subscription;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+import io.netty.handler.codec.http.FullHttpResponse;
+import io.netty.handler.codec.http.HttpMethod;
+import io.netty.handler.codec.http.HttpResponseStatus;
+import org.junit.jupiter.api.Test;
+import org.skyscreamer.jsonassert.JSONAssert;
+import org.skyscreamer.jsonassert.JSONCompareMode;
+
+public class FilteringSubscriptionTest extends AbstractNotificationSubscriptionTest {
+    /**
+     * Tests successful establish subscription RPC.
+     */
+    @Test
+    void filterNotificationReceivedTest() throws Exception {
+        final var response = establishFilteredSubscription(NETCONF_STREAM, ENCODE_XML, """
+            <toasterRestocked xmlns="http://netconfcentral.org/ns/toaster">
+              <amountOfBread/>
+            </toasterRestocked>
+            """);
+
+        assertEquals(HttpResponseStatus.OK, response.status());
+
+        startStreamClient();
+        final var eventListener = startSubscriptionStream("2147483648");
+
+        // invoke RPC
+
+        JSONAssert.assertEquals("""
+                data: {
+                  "ietf-restconf:notification": {
+                    "event-time":"2025-02-18T12:55:12.888818418+01:00",
+                    "toaster:toasterRestocked": {
+                      "amountOfBread":1
+                    }
+                  }
+                }""", eventListener.readNext(), JSONCompareMode.LENIENT);
+    }
+
+    @Test
+    void filterNotificationNotReceivedTest() throws Exception {
+        final var response = establishFilteredSubscription(NETCONF_STREAM, ENCODE_XML,
+            "<toasterOutOfBread xmlns=\"http://netconfcentral.org/ns/toaster\"/>");
+        assertEquals(HttpResponseStatus.OK, response.status());
+
+        startStreamClient();
+        final var eventListener = startSubscriptionStream("2147483648");
+
+        // invoke RPC
+
+        assertNull(eventListener.readNext());
+
+        // modify
+        final var modifyInput = """
+            {
+              "input": {
+                "id": 2147483648,
+                "stop-time": "2025-03-20T15:30:00Z"
+              }
+            }""";
+        final var response2 = invokeRequest(HttpMethod.POST, MODIFY_SUBSCRIPTION_URI, APPLICATION_JSON, modifyInput,
+            APPLICATION_JSON);
+        assertEquals(HttpResponseStatus.NO_CONTENT, response2.status());
+        JSONAssert.assertEquals("""
+                data: {
+                  "ietf-restconf:notification": {
+                    "event-time":"2025-02-18T12:55:12.888818418+01:00",
+                    "toaster:toasterRestocked": {
+                      "amountOfBread":1
+                    }
+                  }
+                }""", eventListener.readNext(), JSONCompareMode.LENIENT);
+    }
+
+    /**
+     * Utility method to establish a subscription.
+     */
+    protected FullHttpResponse establishFilteredSubscription(final String stream, final String encoding,
+            final String filter) throws Exception {
+        final var input = String.format("""
+             <establish-subscription xmlns="urn:ietf:params:xml:ns:yang:ietf-subscribed-notifications">
+               <stream>%s</stream>
+               <encoding>%s</encoding>
+               <stream-subtree-filter>%s</stream-subtree-filter>
+             </establish-subscription>
+             """, stream, encoding, filter);
+
+        return invokeRequestKeepClient(HttpMethod.POST,
+            "/restconf/operations/ietf-subscribed-notifications:establish-subscription",
+            APPLICATION_XML, input, APPLICATION_JSON);
+    }
+}
