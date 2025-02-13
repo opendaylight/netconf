47c57c8013a6fd2649662ca9f8c1f34d5c4c08df
Ivan Hrasko <ivan.hrasko@pantheon.tech>
2025-02-13 12:27:31 +0100

    Refactor ContextListener
    Remove no more used parts form ContextListener. The majority of logic
    has been moved to RestconfSubscriptionsStreamRegistry.
    
    Refactor nullness annotations as well.
    
    JIRA: NETCONF-714
    Change-Id: I5d980ae6e428f2ec0342ef05f7e0dff50678dca1
    Signed-off-by: Ivan Hrasko <ivan.hrasko@pantheon.tech>

--- apps/restconf-subscription/src/main/java/org/opendaylight/restconf/subscription/ContextListener.java
+++ apps/restconf-subscription/src/main/java/org/opendaylight/restconf/subscription/ContextListener.java
@@ -9,44 +9,34 @@ package org.opendaylight.restconf.subscription;
 
 import static java.util.Objects.requireNonNull;
 
-import org.eclipse.jdt.annotation.NonNull;
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.eclipse.jdt.annotation.Nullable;
 import org.opendaylight.mdsal.dom.api.DOMNotificationService;
 import org.opendaylight.mdsal.dom.api.DOMSchemaService;
 import org.opendaylight.restconf.server.spi.RestconfStream;
-import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.subscribed.notifications.rev190909.streams.Stream;
 import org.opendaylight.yangtools.concepts.Registration;
-import org.opendaylight.yangtools.yang.common.QName;
-import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
-import org.opendaylight.yangtools.yang.data.api.schema.MapEntryNode;
-import org.opendaylight.yangtools.yang.data.spi.node.ImmutableNodes;
 import org.opendaylight.yangtools.yang.model.api.EffectiveModelContext;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /**
  * This class recreates DefaultNotificationSource when model context is updated.
  */
+@NonNullByDefault
 final class ContextListener implements Registration {
-    private static final Logger LOG = LoggerFactory.getLogger(ContextListener.class);
-    private static final String NAME = "NETCONF";
-    private static final QName NAME_QNAME = QName.create(Stream.QNAME, "name").intern();
-    private static final QName DESCRIPTION_QNAME = QName.create(Stream.QNAME, "description").intern();
     private static final String DESCRIPTION = "Stream for subscription state change notifications";
 
-    private final @NonNull DOMNotificationService notificationService;
-    private final @NonNull Registration registration;
-    private final RestconfStream.@NonNull Registry streamRegistry;
+    private final DOMNotificationService notificationService;
+    private final RestconfStream.Registry streamRegistry;
+    private final Registration registration;
 
-    private DefaultNotificationSource notificationSource;
+    private @Nullable DefaultNotificationSource notificationSource;
 
     ContextListener(final DOMNotificationService notificationService, final DOMSchemaService schemaService,
             final RestconfStream.Registry streamRegistry) {
         this.notificationService = requireNonNull(notificationService);
-        this.streamRegistry = streamRegistry;
-        notificationSource = new DefaultNotificationSource(notificationService, schemaService.getGlobalContext());
-
-        streamRegistry.createStream(null, null, notificationSource, DESCRIPTION);
+        this.streamRegistry = requireNonNull(streamRegistry);
         registration = schemaService.registerSchemaContextListener(this::onModelContextUpdated);
+        notificationSource = new DefaultNotificationSource(notificationService, schemaService.getGlobalContext());
+        streamRegistry.createStream(null, null, notificationSource, DESCRIPTION);
     }
 
     synchronized void onModelContextUpdated(final EffectiveModelContext context) {
@@ -54,16 +44,7 @@ final class ContextListener implements Registration {
             notificationSource.close();
         }
         notificationSource = new DefaultNotificationSource(notificationService, context);
-
         streamRegistry.createStream(null, null, notificationSource, DESCRIPTION);
-    }
-
-    public static @NonNull MapEntryNode streamEntry() {
-        return ImmutableNodes.newMapEntryBuilder()
-            .withNodeIdentifier(YangInstanceIdentifier.NodeIdentifierWithPredicates.of(Stream.QNAME, NAME_QNAME, NAME))
-            .withChild(ImmutableNodes.leafNode(NAME_QNAME, NAME))
-            .withChild(ImmutableNodes.leafNode(DESCRIPTION_QNAME, DESCRIPTION))
-            .build();
     }
 
     @Override
