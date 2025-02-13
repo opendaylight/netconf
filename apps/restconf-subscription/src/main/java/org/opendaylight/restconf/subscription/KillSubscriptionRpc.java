7dc5812770a8210f404cabd4e2ee267b1c6a2b7d
Oleksandr Zharov <oleksandr.zharov@pantheon.tech>
2025-02-13 09:38:38 +0100

    Get subscription without dealing with exceptions
    Refactored get methods for subscription state and session
    to not throw exceptions if subscription wasn't found.
    
    JIRA: NETCONF-714
    Change-Id: I82fd63c41716a6729d2e8bb28ebe7476c13d0cfc
    Signed-off-by: Oleksandr Zharov <oleksandr.zharov@pantheon.tech>

--- apps/restconf-subscription/src/main/java/org/opendaylight/restconf/subscription/KillSubscriptionRpc.java
+++ apps/restconf-subscription/src/main/java/org/opendaylight/restconf/subscription/KillSubscriptionRpc.java
@@ -13,7 +13,6 @@ import com.google.common.util.concurrent.FutureCallback;
 import com.google.common.util.concurrent.MoreExecutors;
 import java.net.URI;
 import java.time.Instant;
-import java.util.NoSuchElementException;
 import javax.inject.Inject;
 import javax.inject.Singleton;
 import org.opendaylight.mdsal.common.api.CommitInfo;
@@ -81,16 +80,15 @@ public class KillSubscriptionRpc extends RpcImplementation {
                 "No id specified"));
             return;
         }
-        try {
-            final var state = stateMachine.getSubscriptionState(id);
-            if (state != SubscriptionState.ACTIVE && state != SubscriptionState.SUSPENDED) {
-                request.completeWith(new ServerException(ErrorType.APPLICATION, ErrorTag.BAD_ELEMENT,
-                    "There is no active or suspended subscription with given ID."));
-                return;
-            }
-        } catch (NoSuchElementException e) {
+        final var state = stateMachine.getSubscriptionState(id);
+        if (state == null) {
             request.completeWith(new ServerException(ErrorType.APPLICATION, ErrorTag.MISSING_ELEMENT,
                 "No subscription with given ID."));
+            return;
+        }
+        if (state != SubscriptionState.ACTIVE && state != SubscriptionState.SUSPENDED) {
+            request.completeWith(new ServerException(ErrorType.APPLICATION, ErrorTag.BAD_ELEMENT,
+                "There is no active or suspended subscription with given ID."));
             return;
         }
 
