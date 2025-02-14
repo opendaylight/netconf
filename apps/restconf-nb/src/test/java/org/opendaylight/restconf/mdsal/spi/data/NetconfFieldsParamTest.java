11c517032290111cf8fa7b5aed1c921403d08d68
Robert Varga <robert.varga@pantheon.tech>
2025-02-14 02:10:37 +0100

    Expose ServerStrategy from netconf-client-mdsal
    MdsalMountPointResolver operates primarily on DOMServerStrategy, without
    the need for other services.
    
    This patch rehosts NetconfRestconfStrategy, so that if forms the
    backbone of providing specialized implementations.
    
    JIRA: NETCONF-1352
    Change-Id: I550bee0679bc7e20b78247d8c1dfe2ce4ff893b4
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>
    Signed-off-by: Peter Suna <peter.suna@pantheon.tech>
    (cherry picked from commit 5958514502b0ae8a670f8bb90461bdb8c1bc83d1)

--- apps/restconf-nb/src/test/java/org/opendaylight/restconf/mdsal/spi/data/NetconfFieldsParamTest.java
+++ apps/restconf-nb/src/test/java/org/opendaylight/restconf/mdsal/spi/data/NetconfFieldsParamTest.java
@@ -15,6 +15,7 @@ import java.util.Set;
 import java.util.stream.Collectors;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.opendaylight.netconf.client.mdsal.spi.NetconfRestconfStrategy;
 import org.opendaylight.netconf.databind.RequestException;
 import org.opendaylight.restconf.api.query.FieldsParam;
 import org.opendaylight.yangtools.yang.common.QName;
