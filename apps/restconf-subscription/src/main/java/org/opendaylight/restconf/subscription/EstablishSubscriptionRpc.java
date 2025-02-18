24e28efb2bb5a324e405b3836fc48e1c7b2085b6
Robert Varga <robert.varga@pantheon.tech>
2025-02-18 18:25:24 +0100

    Document subscriptionIdCounter
    The counter has non-obvious initial value. Document why and add a FIXME
    to move the allocate elsewhere.
    
    JIRA: NETCONF-714
    Change-Id: If6427508ac708c1d4c8f8575d9e6ad059512a5e9
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- apps/restconf-subscription/src/main/java/org/opendaylight/restconf/subscription/EstablishSubscriptionRpc.java
+++ apps/restconf-subscription/src/main/java/org/opendaylight/restconf/subscription/EstablishSubscriptionRpc.java
@@ -13,7 +13,7 @@ import com.google.common.util.concurrent.FutureCallback;
 import com.google.common.util.concurrent.MoreExecutors;
 import java.net.URI;
 import java.util.concurrent.ExecutionException;
-import java.util.concurrent.atomic.AtomicLong;
+import java.util.concurrent.atomic.AtomicInteger;
 import javax.inject.Inject;
 import javax.inject.Singleton;
 import org.opendaylight.mdsal.common.api.CommitInfo;
@@ -74,10 +74,16 @@ public class EstablishSubscriptionRpc extends RpcImplementation {
     private static final NodeIdentifier OUTPUT_ID =
         NodeIdentifier.create(QName.create(EstablishSubscriptionOutput.QNAME, "id").intern());
 
-    // Start subscription ID generation from the upper half of uint32 (2,147,483,648)
-    private static final long INITIAL_SUBSCRIPTION_ID = 2147483648L;
-
-    private final AtomicLong subscriptionIdCounter = new AtomicLong(INITIAL_SUBSCRIPTION_ID);
+    // As per https://www.rfc-editor.org/rfc/rfc8639.html#section-6">RFC8639:
+    //
+    //    A best practice is to use the lower half of the "id"
+    //    object's integer space when that "id" is assigned by an external
+    //    entity (such as with a configured subscription).  This leaves the
+    //    upper half of the subscription integer space available to be
+    //    dynamically assigned by the publisher.
+    // FIXME: NETCONF-714: this should live in an actual service, not here, so that we can safely wrap without trampling
+    //                     on an existing subscription.
+    private final AtomicInteger subscriptionIdCounter = new AtomicInteger(Integer.MAX_VALUE);
     private final MdsalNotificationService mdsalService;
     private final SubscriptionStateService subscriptionStateService;
     private final SubscriptionStateMachine stateMachine;
@@ -111,7 +117,7 @@ public class EstablishSubscriptionRpc extends RpcImplementation {
             return;
         }
 
-        final var id = Uint32.valueOf(SubscriptionUtil.generateSubscriptionId(subscriptionIdCounter));
+        final var id = Uint32.fromIntBits(subscriptionIdCounter.incrementAndGet());
 
         final var subscriptionBuilder = new SubscriptionBuilder();
         subscriptionBuilder.setId(new SubscriptionId(id));
