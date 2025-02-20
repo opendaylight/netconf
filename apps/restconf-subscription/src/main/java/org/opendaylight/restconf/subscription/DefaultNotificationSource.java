74d532b550c8fc5a2d447ec959a6bb42a2d09d06
Oleksandr Zharov <oleksandr.zharov@pantheon.tech>
2025-02-20 13:32:49 +0100

    Apply subtree filtering to outgoing notifications
    TODO
    
    JIRA: NETCONF-1436
    Change-Id: I649ea1b2697110327263e32ae749a97ff0a71a37
    Signed-off-by: Oleksandr Zharov <oleksandr.zharov@pantheon.tech>

--- apps/restconf-subscription/src/main/java/org/opendaylight/restconf/subscription/DefaultNotificationSource.java
+++ apps/restconf-subscription/src/main/java/org/opendaylight/restconf/subscription/DefaultNotificationSource.java
@@ -10,13 +10,20 @@ package org.opendaylight.restconf.subscription;
 import static java.util.Objects.requireNonNull;
 
 import java.io.Closeable;
+import java.time.Instant;
+import java.util.function.Supplier;
 import org.eclipse.jdt.annotation.NonNull;
+import org.opendaylight.mdsal.dom.api.DOMEvent;
 import org.opendaylight.mdsal.dom.api.DOMNotification;
+import org.opendaylight.mdsal.dom.api.DOMNotificationListener;
 import org.opendaylight.mdsal.dom.api.DOMNotificationService;
+import org.opendaylight.netconf.common.mdsal.DOMNotificationEvent;
 import org.opendaylight.restconf.mdsal.spi.AbstractNotificationSource;
 import org.opendaylight.restconf.mdsal.spi.NotificationSource;
 import org.opendaylight.restconf.server.spi.RestconfStream.Sink;
+import org.opendaylight.restconf.server.spi.SubtreeFilterRestconf;
 import org.opendaylight.yangtools.concepts.Registration;
+import org.opendaylight.yangtools.yang.data.api.schema.ContainerNode;
 import org.opendaylight.yangtools.yang.model.api.EffectiveModelContext;
 import org.opendaylight.yangtools.yang.model.api.stmt.NotificationEffectiveStatement;
 import org.opendaylight.yangtools.yang.model.api.stmt.SchemaNodeIdentifier;
@@ -39,13 +46,38 @@ final class DefaultNotificationSource extends AbstractNotificationSource impleme
             .flatMap(module -> module.streamEffectiveSubstatements(NotificationEffectiveStatement.class))
             .map(notification -> SchemaNodeIdentifier.Absolute.of(notification.argument()))
             .toList();
-        return reg = notificationService.registerNotificationListener(new Listener(sink, () -> context), notifications);
+        return reg = notificationService.registerNotificationListener(new FilteredListener(sink, () -> context),
+            notifications);
     }
 
     @Override
     public synchronized void close() {
         if (reg != null) {
             reg.close();
+        }
+    }
+
+    private static final class FilteredListener implements DOMNotificationListener {
+        private final Sink<DOMNotification> sink;
+        private final Supplier<EffectiveModelContext> modelContext;
+
+        public FilteredListener(final Sink<DOMNotification> sink, final Supplier<EffectiveModelContext> modelContext) {
+            this.sink = requireNonNull(sink);
+            this.modelContext = requireNonNull(modelContext);
+        }
+
+        @Override
+        public void onNotification(final DOMNotification notification) {
+            // FIXME should be filter node instead on null
+            final var filteredNode = (ContainerNode) SubtreeFilterRestconf.applyFilter(null, notification.getBody());
+            if (filteredNode == null) {
+                return;
+            }
+            final var notificationEventInstant = notification instanceof DOMEvent domEvent ? domEvent.getEventInstant()
+                : Instant.now();
+            final var filteredNotification = new DOMNotificationEvent.Rfc6020(filteredNode,
+                notificationEventInstant);
+            sink.publish(modelContext.get(), filteredNotification, notificationEventInstant);
         }
     }
 }
