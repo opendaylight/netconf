87859095537286d177e9158715fcfe72155cff1b
Robert Varga <robert.varga@pantheon.tech>
2025-02-23 03:17:06 +0100

    Promote ErrorMessage to o.o.netconf.common
    This is a construct shared between both NETCONF and RESTCONF. Move it to
    a common place.
    
    Change-Id: Ifec1b31d2bb45c342ef021af44ff4fa20e3a03ff
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- protocol/netconf-common/src/main/java/org/opendaylight/netconf/common/ErrorMessage.java
+++ protocol/netconf-common/src/main/java/org/opendaylight/netconf/common/ErrorMessage.java
@@ -5,7 +5,7 @@
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
-package org.opendaylight.restconf.api;
+package org.opendaylight.netconf.common;
 
 import static java.util.Objects.requireNonNull;
 
