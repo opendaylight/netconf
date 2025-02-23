485560913d5e3496374ea7c6d6d186639358e479
Robert Varga <robert.varga@pantheon.tech>
2025-02-23 13:12:30 +0100

    Convert sal-remote-impl to JPMS module
    Make sure the model has an Automatic-Module-Name and convert the
    implementation to a JPMS module.
    
    Change-Id: I9c4cf503e3c046aea169f0d8aca5413b1ed8672b
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ apps/sal-remote-impl/src/main/java/module-info.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2024 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+module org.opendaylight.netconf.sal.remote.impl {
+    exports org.opendaylight.netconf.sal.remote.impl;
+
+    requires transitive org.opendaylight.restconf.mdsal.spi;
+    requires transitive org.opendaylight.restconf.server.api;
+    requires transitive org.opendaylight.restconf.server.spi;
+    requires transitive org.opendaylight.netconf.dom.api;
+    requires org.opendaylight.netconf.sal.remote;
+    requires org.opendaylight.yangtools.binding.spec;
+    requires org.opendaylight.yangtools.yang.data.spi;
+    requires org.slf4j;
+
+    // Annotation-only dependencies
+    requires static transitive javax.inject;
+    requires static transitive org.eclipse.jdt.annotation;
+    requires static org.osgi.service.component.annotations;
+}
