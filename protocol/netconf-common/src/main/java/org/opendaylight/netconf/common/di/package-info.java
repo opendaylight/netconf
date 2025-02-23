c83c46aea9ac641f1505dfe5d6b4caa662b23398
Robert Varga <robert.varga@pantheon.tech>
2025-02-23 05:46:32 +0100

    Clean up netconf-common
    Convert to bnd-parent and clean up exports.
    
    Change-Id: Idbe5713cfd180e5cc6e19476a5a104245ef17393
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- protocol/netconf-common/src/main/java/org/opendaylight/netconf/common/di/package-info.java
+++ protocol/netconf-common/src/main/java/org/opendaylight/netconf/common/di/package-info.java
@@ -1,12 +1,11 @@
 /*
- * Copyright (c) 2024 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ * Copyright (c) 2025 PANTHEON.tech, s.r.o. and others.  All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
 /**
- * Components shared between NETCONF client and NETCONF server.
+ * Components exposed for dependency injection.
  */
-@org.osgi.annotation.bundle.Export
-package org.opendaylight.netconf.common;
+package org.opendaylight.netconf.common.di;
