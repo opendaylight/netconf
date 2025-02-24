ff88905ebfe1c752081fddfa1fc301f52588f541
Robert Varga <robert.varga@pantheon.tech>
2025-02-24 20:50:59 +0100

    Improve DatabindContext reuse
    Carry the DatabindContext through discovery, so when there is nothing
    to do we just return it as is.
    
    JIRA: NETCONF-1352
    Change-Id: I5a9ac0254f88cc071f5bc29eb3b6aa08cda8d340
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- plugins/netconf-client-mdsal/src/main/java/org/opendaylight/netconf/client/mdsal/DeviceMountPointContext.java
+++ plugins/netconf-client-mdsal/src/main/java/org/opendaylight/netconf/client/mdsal/DeviceMountPointContext.java
@@ -12,26 +12,23 @@ import static java.util.Objects.requireNonNull;
 
 import com.google.common.collect.ImmutableMap;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.Map;
 import java.util.Optional;
 import org.eclipse.jdt.annotation.NonNull;
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.opendaylight.netconf.databind.DatabindContext;
 import org.opendaylight.yangtools.concepts.Immutable;
 import org.opendaylight.yangtools.rfc8528.model.api.SchemaMountConstants;
 import org.opendaylight.yangtools.yang.common.MountPointLabel;
 import org.opendaylight.yangtools.yang.common.QName;
-import org.opendaylight.yangtools.yang.common.QNameModule;
 import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.NodeIdentifier;
 import org.opendaylight.yangtools.yang.data.api.schema.ChoiceNode;
 import org.opendaylight.yangtools.yang.data.api.schema.ContainerNode;
-import org.opendaylight.yangtools.yang.data.api.schema.DataContainerChild;
 import org.opendaylight.yangtools.yang.data.api.schema.LeafNode;
-import org.opendaylight.yangtools.yang.data.api.schema.MapEntryNode;
 import org.opendaylight.yangtools.yang.data.api.schema.MapNode;
 import org.opendaylight.yangtools.yang.data.api.schema.MountPointContext;
 import org.opendaylight.yangtools.yang.data.api.schema.MountPointContextFactory;
 import org.opendaylight.yangtools.yang.model.api.EffectiveModelContext;
-import org.opendaylight.yangtools.yang.model.api.Module;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -64,30 +61,32 @@ final class DeviceMountPointContext implements Immutable, MountPointContext {
         this.mountPoints = ImmutableMap.copyOf(mountPoints);
     }
 
-    static MountPointContext create(final MountPointContext emptyContext, final ContainerNode mountData) {
-        final Optional<DataContainerChild> optMountPoint = mountData.findChildByArg(MOUNT_POINT);
+    @NonNullByDefault
+    static DatabindContext create(final DatabindContext emptyContext, final ContainerNode mountData) {
+        final var optMountPoint = mountData.findChildByArg(MOUNT_POINT);
         if (optMountPoint.isEmpty()) {
             LOG.debug("mount-point list not present in {}", mountData);
             return emptyContext;
         }
 
-        final EffectiveModelContext schemaContext = emptyContext.modelContext();
-        final DataContainerChild mountPoint = optMountPoint.orElseThrow();
+        final var modelContext = emptyContext.modelContext();
+        final var mountPoint = optMountPoint.orElseThrow();
         checkArgument(mountPoint instanceof MapNode, "mount-point list %s is not a MapNode", mountPoint);
 
-        final Map<MountPointLabel, NetconfMountPointContextFactory> mountPoints = new HashMap<>();
-        for (MapEntryNode entry : ((MapNode) mountPoint).body()) {
+        final var mountPoints = new HashMap<MountPointLabel, NetconfMountPointContextFactory>();
+        for (var entry : ((MapNode) mountPoint).body()) {
             final String moduleName = entry.findChildByArg(MODULE).map(mod -> {
                 checkArgument(mod instanceof LeafNode, "Unexpected module leaf %s", mod);
                 final Object value = mod.body();
                 checkArgument(value instanceof String, "Unexpected module leaf value %s", value);
                 return (String) value;
             }).orElseThrow(() -> new IllegalArgumentException("Mount module missing in " + entry));
-            final Iterator<? extends Module> it = schemaContext.findModules(moduleName).iterator();
-            checkArgument(it.hasNext(), "Failed to find a module named %s", moduleName);
-            final QNameModule module = it.next().getQNameModule();
 
-            final MountPointLabel mountId = new MountPointLabel(QName.create(module,
+            final var it = modelContext.findModules(moduleName).iterator();
+            checkArgument(it.hasNext(), "Failed to find a module named %s", moduleName);
+
+            final var module = it.next().getQNameModule();
+            final var mountId = new MountPointLabel(QName.create(module,
                 entry.findChildByArg(LABEL).map(lbl -> {
                     checkArgument(lbl instanceof LeafNode, "Unexpected label leaf %s", lbl);
                     final Object value = lbl.body();
@@ -95,21 +94,22 @@ final class DeviceMountPointContext implements Immutable, MountPointContext {
                     return (String) value;
                 }).orElseThrow(() -> new IllegalArgumentException("Mount module missing in " + entry))));
 
-            final DataContainerChild child = entry.findChildByArg(SCHEMA_REF).orElseThrow(
+            final var child = entry.findChildByArg(SCHEMA_REF).orElseThrow(
                 () -> new IllegalArgumentException("Missing schema-ref choice in " + entry));
-            checkArgument(child instanceof ChoiceNode, "Unexpected schema-ref choice %s", child);
-            final ChoiceNode schemaRef = (ChoiceNode) child;
+            if (!(child instanceof ChoiceNode schemaRef)) {
+                throw new IllegalArgumentException("Unexpected schema-ref choice " + child);
+            }
 
-            final Optional<DataContainerChild> maybeShared = schemaRef.findChildByArg(SHARED_SCHEMA);
+            final var maybeShared = schemaRef.findChildByArg(SHARED_SCHEMA);
             if (maybeShared.isEmpty()) {
                 LOG.debug("Ignoring non-shared mountpoint entry {}", entry);
                 continue;
             }
 
-            mountPoints.put(mountId, new NetconfMountPointContextFactory(schemaContext));
+            mountPoints.put(mountId, new NetconfMountPointContextFactory(modelContext));
         }
 
-        return new DeviceMountPointContext(schemaContext, mountPoints);
+        return DatabindContext.ofMountPoint(new DeviceMountPointContext(modelContext, mountPoints));
     }
 
     @Override
