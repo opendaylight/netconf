fce4749fdc55127387c3a3d6ebf6de5765a829ad
Robert Varga <robert.varga@pantheon.tech>
2025-02-24 16:57:51 +0100

    Clean up NormalizedDataUtil
    Encapsulate most methods, so we do not shortcut to them. These will be
    helpful when we integrate databind.
    
    Change-Id: I3bda313849ea5c7788a9d16773dd800acc4b7328
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- plugins/netconf-common-mdsal/src/main/java/org/opendaylight/netconf/common/mdsal/NormalizedDataUtil.java
+++ plugins/netconf-common-mdsal/src/main/java/org/opendaylight/netconf/common/mdsal/NormalizedDataUtil.java
@@ -9,23 +9,20 @@ package org.opendaylight.netconf.common.mdsal;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
 import java.util.stream.Collectors;
 import javax.xml.stream.XMLOutputFactory;
 import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.XMLStreamWriter;
 import javax.xml.transform.dom.DOMResult;
 import javax.xml.transform.dom.DOMSource;
+import org.eclipse.jdt.annotation.NonNullByDefault;
 import org.eclipse.jdt.annotation.Nullable;
 import org.opendaylight.netconf.api.NamespaceURN;
 import org.opendaylight.netconf.api.xml.XmlUtil;
-import org.opendaylight.yangtools.yang.common.QName;
 import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
 import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.NodeIdentifierWithPredicates;
-import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.PathArgument;
 import org.opendaylight.yangtools.yang.data.api.schema.MountPointContext;
 import org.opendaylight.yangtools.yang.data.api.schema.NormalizedMetadata;
 import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNode;
@@ -65,8 +62,7 @@ public final class NormalizedDataUtil {
         void initializeNamespace(XMLStreamWriter writer) throws XMLStreamException;
 
         static NamespaceSetter forFactory(final XMLOutputFactory xmlFactory) {
-            final String netconfNamespace = NamespaceURN.BASE;
-            final AnyXmlNamespaceContext namespaceContext = new AnyXmlNamespaceContext(Map.of("op", netconfNamespace));
+            final var namespaceContext = new AnyXmlNamespaceContext(Map.of("op", NamespaceURN.BASE));
 
             try {
                 final var testWriter = xmlFactory.createXMLStreamWriter(new DOMResult(XmlUtil.newDocument()));
@@ -74,7 +70,7 @@ public final class NormalizedDataUtil {
             } catch (final UnsupportedOperationException e) {
                 // This happens with JDK's DOM writer, which we may be using
                 LOG.debug("Unable to set namespace context, falling back to setPrefix()", e);
-                return writer -> writer.setPrefix("op", netconfNamespace);
+                return writer -> writer.setPrefix("op", NamespaceURN.BASE);
             } catch (XMLStreamException e) {
                 throw new ExceptionInInitializerError(e);
             }
@@ -89,16 +85,79 @@ public final class NormalizedDataUtil {
     public static final XMLOutputFactory XML_FACTORY;
 
     static {
-        final XMLOutputFactory f = XMLOutputFactory.newFactory();
+        final var factory = XMLOutputFactory.newFactory();
         // FIXME: not repairing namespaces is probably common, this should be availabe as common XML constant.
-        f.setProperty(XMLOutputFactory.IS_REPAIRING_NAMESPACES, false);
-        XML_FACTORY = f;
+        factory.setProperty(XMLOutputFactory.IS_REPAIRING_NAMESPACES, false);
+        XML_FACTORY = factory;
     }
 
+    // FIXME: this needs to be called when and why?
     private static final NamespaceSetter XML_NAMESPACE_SETTER = NamespaceSetter.forFactory(XML_FACTORY);
 
     private NormalizedDataUtil() {
         // No-op
+    }
+
+    @NonNullByDefault
+    private static void writeNode(final EffectiveModelContext modelContext, final NormalizedNode data,
+            final DOMResult result, final @Nullable Absolute parent) throws IOException, XMLStreamException {
+        final var xmlWriter = XML_FACTORY.createXMLStreamWriter(result);
+        try (var streamWriter = XMLStreamNormalizedNodeStreamWriter.create(xmlWriter, modelContext, parent);
+             var writer = NormalizedNodeWriter.forStreamWriter(streamWriter)) {
+            writer.write(data);
+            writer.flush();
+        } finally {
+            xmlWriter.close();
+        }
+    }
+
+    @NonNullByDefault
+    private static void writeNode(final EffectiveModelContext modelContext, final NormalizedNode data,
+            final NormalizedMetadata metadata, final DOMResult result, final @Nullable Absolute parent)
+                throws IOException, XMLStreamException {
+        final var xmlWriter = XML_FACTORY.createXMLStreamWriter(result);
+        XML_NAMESPACE_SETTER.initializeNamespace(xmlWriter);
+        try (var streamWriter = XMLStreamNormalizedNodeStreamWriter.create(xmlWriter, modelContext, parent);
+             var writer = NormalizedMetadataWriter.forStreamWriter(streamWriter)) {
+            writer.write(data, metadata);
+            writer.flush();
+        } finally {
+            xmlWriter.close();
+        }
+    }
+
+    @NonNullByDefault
+    private static void writePath(final EffectiveModelContext modelContext, final YangInstanceIdentifier path,
+            final DOMResult result, final @Nullable Absolute parent) throws IOException, XMLStreamException {
+        final var xmlWriter = XML_FACTORY.createXMLStreamWriter(result);
+        XML_NAMESPACE_SETTER.initializeNamespace(xmlWriter);
+        try (var streamWriter = XMLStreamNormalizedNodeStreamWriter.create(xmlWriter, modelContext, parent);
+             var writer = new EmptyListXmlWriter(streamWriter, xmlWriter)) {
+            final var it = path.getPathArguments().iterator();
+            final var first = it.next();
+            StreamingContext.fromSchemaAndQNameChecked(modelContext, first.getNodeType())
+                .streamToWriter(writer, first, it);
+        } finally {
+            xmlWriter.close();
+        }
+    }
+
+    @NonNullByDefault
+    private static void writePath(final EffectiveModelContext modelContext, final YangInstanceIdentifier path,
+            final NormalizedMetadata metadata, final DOMResult result, final @Nullable Absolute parent)
+                throws IOException, XMLStreamException {
+        final var xmlWriter = XML_FACTORY.createXMLStreamWriter(result);
+        XML_NAMESPACE_SETTER.initializeNamespace(xmlWriter);
+        try (var streamWriter = XMLStreamNormalizedNodeStreamWriter.create(xmlWriter, modelContext, parent);
+             var writer = new EmptyListXmlMetadataWriter(streamWriter, xmlWriter,
+                 streamWriter.extension(MetadataExtension.class), metadata)) {
+            final var it = path.getPathArguments().iterator();
+            final var first = it.next();
+            StreamingContext.fromSchemaAndQNameChecked(modelContext, first.getNodeType())
+                .streamToWriter(writer, first, it);
+        } finally {
+            xmlWriter.close();
+        }
     }
 
     /**
@@ -113,14 +172,7 @@ public final class NormalizedDataUtil {
      */
     public static void writeNormalizedNode(final NormalizedNode normalized, final DOMResult result,
             final EffectiveModelContext context, final @Nullable Absolute path) throws IOException, XMLStreamException {
-        final XMLStreamWriter xmlWriter = XML_FACTORY.createXMLStreamWriter(result);
-        try (var streamWriter = XMLStreamNormalizedNodeStreamWriter.create(xmlWriter, context, path);
-             var writer = NormalizedNodeWriter.forStreamWriter(streamWriter)) {
-            writer.write(normalized);
-            writer.flush();
-        } finally {
-            xmlWriter.close();
-        }
+        writeNode(context, normalized, result, path);
     }
 
     /**
@@ -137,19 +189,10 @@ public final class NormalizedDataUtil {
     public static void writeNormalizedNode(final NormalizedNode normalized, final @Nullable NormalizedMetadata metadata,
             final DOMResult result, final EffectiveModelContext context, final @Nullable Absolute path)
                 throws IOException, XMLStreamException {
-        if (metadata == null) {
-            writeNormalizedNode(normalized, result, context, path);
-            return;
-        }
-
-        final XMLStreamWriter xmlWriter = XML_FACTORY.createXMLStreamWriter(result);
-        XML_NAMESPACE_SETTER.initializeNamespace(xmlWriter);
-        try (var streamWriter = XMLStreamNormalizedNodeStreamWriter.create(xmlWriter, context, path);
-             var writer = NormalizedMetadataWriter.forStreamWriter(streamWriter)) {
-            writer.write(normalized, metadata);
-            writer.flush();
-        } finally {
-            xmlWriter.close();
+        if (metadata != null) {
+            writeNode(context, normalized, metadata, result, path);
+        } else {
+            writeNode(context, normalized, result, path);
         }
     }
 
@@ -165,16 +208,7 @@ public final class NormalizedDataUtil {
      */
     public static void writeNormalizedNode(final YangInstanceIdentifier query, final DOMResult result,
             final EffectiveModelContext context, final @Nullable Absolute path) throws IOException, XMLStreamException {
-        final XMLStreamWriter xmlWriter = XML_FACTORY.createXMLStreamWriter(result);
-        XML_NAMESPACE_SETTER.initializeNamespace(xmlWriter);
-        try (var streamWriter = XMLStreamNormalizedNodeStreamWriter.create(xmlWriter, context, path);
-             var writer = new EmptyListXmlWriter(streamWriter, xmlWriter)) {
-            final Iterator<PathArgument> it = query.getPathArguments().iterator();
-            final PathArgument first = it.next();
-            StreamingContext.fromSchemaAndQNameChecked(context, first.getNodeType()).streamToWriter(writer, first, it);
-        } finally {
-            xmlWriter.close();
-        }
+        writePath(context, query, result, path);
     }
 
     /**
@@ -192,21 +226,10 @@ public final class NormalizedDataUtil {
     public static void writeNormalizedNode(final YangInstanceIdentifier query,
             final @Nullable NormalizedMetadata metadata, final DOMResult result, final EffectiveModelContext context,
             final @Nullable Absolute path) throws IOException, XMLStreamException {
-        if (metadata == null) {
-            writeNormalizedNode(query, result, context, path);
-            return;
-        }
-
-        final XMLStreamWriter xmlWriter = XML_FACTORY.createXMLStreamWriter(result);
-        XML_NAMESPACE_SETTER.initializeNamespace(xmlWriter);
-        try (var streamWriter = XMLStreamNormalizedNodeStreamWriter.create(xmlWriter, context, path);
-             var writer = new EmptyListXmlMetadataWriter(streamWriter, xmlWriter,
-                 streamWriter.extension(MetadataExtension.class), metadata)) {
-            final Iterator<PathArgument> it = query.getPathArguments().iterator();
-            final PathArgument first = it.next();
-            StreamingContext.fromSchemaAndQNameChecked(context, first.getNodeType()).streamToWriter(writer, first, it);
-        } finally {
-            xmlWriter.close();
+        if (metadata != null) {
+            writePath(context, query, metadata, result, path);
+        } else {
+            writePath(context, query, result, path);
         }
     }
 
@@ -227,11 +250,11 @@ public final class NormalizedDataUtil {
             return;
         }
 
-        final XMLStreamWriter xmlWriter = XML_FACTORY.createXMLStreamWriter(result);
+        final var xmlWriter = XML_FACTORY.createXMLStreamWriter(result);
         try (var streamWriter = XMLStreamNormalizedNodeStreamWriter.create(xmlWriter, context, path);
              var writer = new EmptyListXmlWriter(streamWriter, xmlWriter)) {
-            final Iterator<PathArgument> it = query.getPathArguments().iterator();
-            final PathArgument first = it.next();
+            final var it = query.getPathArguments().iterator();
+            final var first = it.next();
             StreamingContext.fromSchemaAndQNameChecked(context, first.getNodeType()).streamToWriter(writer, first, it);
         } finally {
             xmlWriter.close();
@@ -259,14 +282,14 @@ public final class NormalizedDataUtil {
             return;
         }
 
-        final List<YangInstanceIdentifier> aggregatedFields = aggregateFields(fields);
-        final PathNode rootNode = constructPathArgumentTree(query, aggregatedFields);
+        final var aggregatedFields = aggregateFields(fields);
+        final var rootNode = constructPathArgumentTree(query, aggregatedFields);
 
-        final XMLStreamWriter xmlWriter = XML_FACTORY.createXMLStreamWriter(result);
+        final var xmlWriter = XML_FACTORY.createXMLStreamWriter(result);
         try {
             try (var streamWriter = XMLStreamNormalizedNodeStreamWriter.create(xmlWriter, context, path);
                  var writer = new EmptyListXmlWriter(streamWriter, xmlWriter)) {
-                final PathArgument first = rootNode.element();
+                final var first = rootNode.element();
                 StreamingContext.fromSchemaAndQNameChecked(context, first.getNodeType())
                         .streamToWriter(writer, first, rootNode);
             }
@@ -290,23 +313,23 @@ public final class NormalizedDataUtil {
     }
 
     private static void pathArgumentTreeToXmlStructure(final PathNode pathArgumentTree, final Element data) {
-        final PathArgument pathArg = pathArgumentTree.element();
+        final var pathArg = pathArgumentTree.element();
 
-        final QName nodeType = pathArg.getNodeType();
-        final String elementNamespace = nodeType.getNamespace().toString();
+        final var nodeType = pathArg.getNodeType();
+        final var elementNamespace = nodeType.getNamespace().toString();
 
         if (data.getElementsByTagNameNS(elementNamespace, nodeType.getLocalName()).getLength() != 0) {
             // element has already been written as list key
             return;
         }
 
-        final Element childElement = data.getOwnerDocument().createElementNS(elementNamespace, nodeType.getLocalName());
+        final var childElement = data.getOwnerDocument().createElementNS(elementNamespace, nodeType.getLocalName());
         data.appendChild(childElement);
         if (pathArg instanceof NodeIdentifierWithPredicates nip) {
             appendListKeyNodes(childElement, nip);
         }
-        for (final PathNode childrenNode : pathArgumentTree.children()) {
-            pathArgumentTreeToXmlStructure(childrenNode, childElement);
+        for (var childNode : pathArgumentTree.children()) {
+            pathArgumentTreeToXmlStructure(childNode, childElement);
         }
     }
 
@@ -317,9 +340,10 @@ public final class NormalizedDataUtil {
      * @param listEntryId   list entry identifier
      */
     public static void appendListKeyNodes(final Element parentElement, final NodeIdentifierWithPredicates listEntryId) {
-        for (Entry<QName, Object> key : listEntryId.entrySet()) {
-            final Element keyElement = parentElement.getOwnerDocument().createElementNS(
-                    key.getKey().getNamespace().toString(), key.getKey().getLocalName());
+        for (var key : listEntryId.entrySet()) {
+            final var qname = key.getKey();
+            final var keyElement = parentElement.getOwnerDocument()
+                .createElementNS(qname.getNamespace().toString(), qname.getLocalName());
             keyElement.setTextContent(key.getValue().toString());
             parentElement.appendChild(keyElement);
         }
@@ -334,10 +358,10 @@ public final class NormalizedDataUtil {
      */
     private static List<YangInstanceIdentifier> aggregateFields(final List<YangInstanceIdentifier> fields) {
         return fields.stream()
-                .filter(field -> fields.stream()
-                        .filter(fieldYiid -> !field.equals(fieldYiid))
-                        .noneMatch(fieldYiid -> fieldYiid.contains(field)))
-                .collect(Collectors.toList());
+            .filter(field -> fields.stream()
+                .filter(fieldYiid -> !field.equals(fieldYiid))
+                .noneMatch(fieldYiid -> fieldYiid.contains(field)))
+            .collect(Collectors.toList());
     }
 
     /**
@@ -351,17 +375,17 @@ public final class NormalizedDataUtil {
      */
     private static PathNode constructPathArgumentTree(final YangInstanceIdentifier query,
             final List<YangInstanceIdentifier> fields) {
-        final Iterator<PathArgument> queryIterator = query.getPathArguments().iterator();
-        final PathNode rootTreeNode = new PathNode(queryIterator.next());
+        final var queryIterator = query.getPathArguments().iterator();
+        final var rootTreeNode = new PathNode(queryIterator.next());
 
-        PathNode queryTreeNode = rootTreeNode;
+        var queryTreeNode = rootTreeNode;
         while (queryIterator.hasNext()) {
             queryTreeNode = queryTreeNode.ensureChild(queryIterator.next());
         }
 
-        for (final YangInstanceIdentifier field : fields) {
-            PathNode actualFieldTreeNode = queryTreeNode;
-            for (final PathArgument fieldPathArg : field.getPathArguments()) {
+        for (var field : fields) {
+            var actualFieldTreeNode = queryTreeNode;
+            for (var fieldPathArg : field.getPathArguments()) {
                 actualFieldTreeNode = actualFieldTreeNode.ensureChild(fieldPathArg);
             }
         }
@@ -370,10 +394,10 @@ public final class NormalizedDataUtil {
 
     public static NormalizationResultHolder transformDOMSourceToNormalizedNode(final MountPointContext mount,
             final DOMSource value) throws XMLStreamException, URISyntaxException, IOException, SAXException {
-        final NormalizationResultHolder resultHolder = new NormalizationResultHolder();
-        final NormalizedNodeStreamWriter writer = ImmutableNormalizedNodeStreamWriter.from(resultHolder);
-        try (XmlParserStream xmlParserStream = XmlParserStream.create(writer, new ProxyMountPointContext(mount))) {
-            xmlParserStream.traverse(value);
+        final var resultHolder = new NormalizationResultHolder();
+        final var writer = ImmutableNormalizedNodeStreamWriter.from(resultHolder);
+        try (var parser = XmlParserStream.create(writer, new ProxyMountPointContext(mount))) {
+            parser.traverse(value);
         }
         return resultHolder;
     }
