6664d11cdad2b37eba043622d32992c1240b6179
Samuel Schneider <samuel.schneider@pantheon.tech>
2025-03-06 17:19:58 +0100

    Adapt ModifySubscriptionRpc
    Adapt ModifySubscriptionRpc to changes in RestconfStream.
    Add possibility to modify filter in RestconfStream.Subscription
    and propagate this change in to operational datastore.
    
    Now we provide correct response to invoke of this rpc,
    in case of error correct error message, after filtering is finished
    correctly update datastore, instead of "Not implemented yet".
    
    JIRA: NETCONF-714
    Change-Id: If6487621d5a2ff378be7f9f9089351df75a50ff4
    Signed-off-by: Samuel Schneider <samuel.schneider@pantheon.tech>

--- apps/restconf-subscription/src/main/java/org/opendaylight/restconf/subscription/SubscriptionUtil.java
+++ apps/restconf-subscription/src/main/java/org/opendaylight/restconf/subscription/SubscriptionUtil.java
@@ -7,26 +7,35 @@
  */
 package org.opendaylight.restconf.subscription;
 
+import org.eclipse.jdt.annotation.Nullable;
+import org.opendaylight.restconf.server.spi.RestconfStream.SubscriptionFilter;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.subscribed.notifications.rev190909.EstablishSubscriptionInput;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.subscribed.notifications.rev190909.Filters;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.subscribed.notifications.rev190909.Streams;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.subscribed.notifications.rev190909.Subscriptions;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.subscribed.notifications.rev190909.filters.StreamFilter;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.subscribed.notifications.rev190909.stream.filter.elements.FilterSpec;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.subscribed.notifications.rev190909.stream.filter.elements.filter.spec.stream.subtree.filter.StreamSubtreeFilter;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.subscribed.notifications.rev190909.streams.Stream;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.subscribed.notifications.rev190909.subscriptions.Subscription;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.subscribed.notifications.rev190909.subscriptions.subscription.receivers.Receiver;
 import org.opendaylight.yangtools.yang.common.QName;
 import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.NodeIdentifier;
+import org.opendaylight.yangtools.yang.data.api.schema.AnydataNode;
+import org.opendaylight.yangtools.yang.data.api.schema.ChoiceNode;
+import org.opendaylight.yangtools.yang.data.api.schema.LeafNode;
 
 public final class SubscriptionUtil {
     public static final YangInstanceIdentifier SUBSCRIPTIONS = YangInstanceIdentifier.of(
-        YangInstanceIdentifier.NodeIdentifier.create(Subscriptions.QNAME),
-        YangInstanceIdentifier.NodeIdentifier.create(Subscription.QNAME));
+        NodeIdentifier.create(Subscriptions.QNAME),
+        NodeIdentifier.create(Subscription.QNAME));
     public static final YangInstanceIdentifier STREAMS = YangInstanceIdentifier.of(
-        YangInstanceIdentifier.NodeIdentifier.create(Streams.QNAME),
-        YangInstanceIdentifier.NodeIdentifier.create(Stream.QNAME));
+        NodeIdentifier.create(Streams.QNAME),
+        NodeIdentifier.create(Stream.QNAME));
     public static final YangInstanceIdentifier FILTERS = YangInstanceIdentifier.of(
-        YangInstanceIdentifier.NodeIdentifier.create(Filters.QNAME),
-        YangInstanceIdentifier.NodeIdentifier.create(StreamFilter.QNAME));
+        NodeIdentifier.create(Filters.QNAME),
+        NodeIdentifier.create(StreamFilter.QNAME));
     public static final QName QNAME_ID = QName.create(Subscription.QNAME, "id");
     public static final QName QNAME_STREAM = QName.create(Subscription.QNAME, "stream");
     public static final QName QNAME_STREAM_FILTER = QName.create(Subscription.QNAME, "stream-filter-name");
@@ -39,8 +48,35 @@ public final class SubscriptionUtil {
     public static final QName QNAME_SENT_EVENT_RECORDS = QName.create(Receiver.QNAME, "sent-event-records");
     public static final QName QNAME_EXCLUDED_EVENT_RECORDS = QName
         .create(Receiver.QNAME, "excluded-event-records");
+    private static final NodeIdentifier SUBSCRIPTION_STREAM_FILTER_NAME =
+        NodeIdentifier.create(QName.create(EstablishSubscriptionInput.QNAME, "stream-filter-name").intern());
 
     private SubscriptionUtil() {
         // hidden on purpose
+    }
+
+    static @Nullable SubscriptionFilter extractFilter(final ChoiceNode streamFilter) {
+        if (streamFilter.childByArg(SUBSCRIPTION_STREAM_FILTER_NAME) instanceof LeafNode<?> leafNode) {
+            if (leafNode.body() instanceof String filterName) {
+                return new SubscriptionFilter.Reference(filterName);
+            }
+            throw new IllegalArgumentException("Bad child " + leafNode.prettyTree());
+        }
+        final var filterSpec = (ChoiceNode) streamFilter.childByArg(new NodeIdentifier(FilterSpec.QNAME));
+        if (filterSpec == null) {
+            return null;
+        }
+        final var subtree = (AnydataNode<?>) filterSpec.childByArg(new NodeIdentifier(StreamSubtreeFilter.QNAME));
+        if (subtree != null) {
+            return new SubscriptionFilter.SubtreeDefinition(subtree);
+        }
+        if (filterSpec.childByArg(new NodeIdentifier(QName.create(FilterSpec.QNAME, "stream-xpath-filter")))
+            instanceof LeafNode<?> leafNode) {
+            if (leafNode.body() instanceof String xpath) {
+                return new SubscriptionFilter.XPathDefinition(xpath);
+            }
+            throw new IllegalArgumentException("Bad child " + leafNode.prettyTree());
+        }
+        return null;
     }
 }
