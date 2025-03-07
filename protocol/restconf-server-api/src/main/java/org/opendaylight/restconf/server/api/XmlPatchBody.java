bff07ac348d43c630bb25cfc27223efe9f45ac53
Peter Suna <peter.suna@pantheon.tech>
2025-03-07 17:26:58 +0100

    Handle RuntimeException in YangPatch
    The JsonPatchBody parses the provided JSON, and if any issue occurs
    during parsing, it throws a RuntimeException. This results in a 500
    error when users provide incorrect data.
    
    Fix this by properly handling RuntimeException in the JsonPatchBody
    class.
    
    JIRA: NETCONF-1438
    Change-Id: I1ebe8b8c18f6afe6867b11d3a08f27b388c2dfa3
    Signed-off-by: Peter Suna <peter.suna@pantheon.tech>

--- protocol/restconf-server-api/src/main/java/org/opendaylight/restconf/server/api/XmlPatchBody.java
+++ protocol/restconf-server-api/src/main/java/org/opendaylight/restconf/server/api/XmlPatchBody.java
@@ -62,7 +62,14 @@ public final class XmlPatchBody extends PatchBody {
             final Element element = (Element) editNodes.item(i);
             final String operation = requireNonNullValue(element.getElementsByTagName(OPERATION).item(0), OPERATION)
                 .getFirstChild().getNodeValue();
-            final Operation oper = Operation.ofName(operation);
+            final Operation oper;
+            try {
+                oper = Operation.ofName(operation);
+            } catch (IllegalArgumentException e) {
+                LOG.error("Provided operation type does not match", e);
+                throw new RequestException(ErrorType.APPLICATION, ErrorTag.MALFORMED_MESSAGE,
+                    "Operation value is incorrect: " + e.getMessage(), e);
+            }
             final String editId = requireNonNullValue(element.getElementsByTagName(EDIT_ID).item(0), EDIT_ID)
                 .getFirstChild().getNodeValue();
             final String target = requireNonNullValue(element.getElementsByTagName(TARGET).item(0), TARGET)
