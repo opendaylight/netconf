9eea6c16579eaf50572c35ff6808b93c72c6d80a
Peter Suna <peter.suna@pantheon.tech>
2025-03-07 19:09:47 +0100

    Add exception message for missing YangPatch nodes
    If any required schema node is missing, an NullPointerException
    is thrown without additional information.
    
    Use a custom requireNonNullValue check, which throws a checked
    RequestException and adds an exception message specifying
    the missing field.
    
    JIRA: NETCONF-1438
    Change-Id: I528a65f44d09ccf115533f73874c109cdf80c022
    Signed-off-by: Peter Suna <peter.suna@pantheon.tech>

--- protocol/restconf-server-api/src/main/java/org/opendaylight/restconf/server/api/PatchContext.java
+++ protocol/restconf-server-api/src/main/java/org/opendaylight/restconf/server/api/PatchContext.java
@@ -7,22 +7,39 @@
  */
 package org.opendaylight.restconf.server.api;
 
-import static java.util.Objects.requireNonNull;
+import static org.opendaylight.restconf.server.api.JsonPatchBody.requireNonNullValue;
 
 import com.google.common.annotations.Beta;
 import com.google.common.collect.ImmutableList;
 import java.util.List;
 import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.opendaylight.netconf.databind.RequestException;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.yang.patch.rev170222.yang.patch.YangPatch;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.yang.patch.rev170222.yang.patch.yang.patch.Edit;
+import org.opendaylight.yangtools.yang.common.QName;
 
 @Beta
 @NonNullByDefault
-public record PatchContext(String patchId, ImmutableList<PatchEntity> entities) {
-    public PatchContext {
-        requireNonNull(patchId);
-        requireNonNull(entities);
+public class PatchContext {
+    private static final QName PATCH_ID = QName.create(YangPatch.QNAME, "patch-id").intern();
+
+    private final String patchId;
+    private final ImmutableList<PatchEntity> entities;
+
+    public PatchContext(final String patchId, final ImmutableList<PatchEntity> entities) throws RequestException {
+        this.patchId = requireNonNullValue(patchId, PATCH_ID);
+        this.entities = requireNonNullValue(entities, Edit.QNAME);
     }
 
-    public PatchContext(final String patchId, final List<PatchEntity> entities) {
+    public PatchContext(final String patchId, final List<PatchEntity> entities) throws RequestException {
         this(patchId, ImmutableList.copyOf(entities));
+    }
+
+    public String patchId() {
+        return patchId;
+    }
+
+    public ImmutableList<PatchEntity> entities() {
+        return entities;
     }
 }
