ec8471bb8383cc40714924f325ce9f453eedc8da
Peter Suna <peter.suna@pantheon.tech>
2025-03-07 19:09:47 +0100

    Add exception message for missing YangPatch nodes
    If any required schema node is missing, an NullPointerException
    is thrown without additional information.
    
    Use a custom requireNonNullValue check, which throws a checked
    RequestException and adds an exception message specifying
    the missing field.
    
    JIRA: NETCONF-1438
    Change-Id: I528a65f44d09ccf115533f73874c109cdf80c022
    Signed-off-by: Peter Suna <peter.suna@pantheon.tech>

--- protocol/restconf-server-api/src/main/java/org/opendaylight/restconf/server/api/XmlPatchBody.java
+++ protocol/restconf-server-api/src/main/java/org/opendaylight/restconf/server/api/XmlPatchBody.java
@@ -54,15 +54,19 @@ public final class XmlPatchBody extends PatchBody {
     private static @NonNull PatchContext parse(final ResourceContext resource, final Document doc)
             throws IOException, RequestException, XMLStreamException, SAXException, URISyntaxException {
         final var entities = ImmutableList.<PatchEntity>builder();
-        final var patchId = doc.getElementsByTagName(PATCH_ID).item(0).getFirstChild().getNodeValue();
+        final var patchId = requireNonNullValue(doc.getElementsByTagName(PATCH_ID).item(0), PATCH_ID)
+            .getFirstChild().getNodeValue();
         final var editNodes = doc.getElementsByTagName(EDIT);
 
         for (int i = 0; i < editNodes.getLength(); i++) {
             final Element element = (Element) editNodes.item(i);
-            final String operation = element.getElementsByTagName(OPERATION).item(0).getFirstChild().getNodeValue();
+            final String operation = requireNonNullValue(element.getElementsByTagName(OPERATION).item(0), OPERATION)
+                .getFirstChild().getNodeValue();
             final Operation oper = Operation.ofName(operation);
-            final String editId = element.getElementsByTagName(EDIT_ID).item(0).getFirstChild().getNodeValue();
-            final String target = element.getElementsByTagName(TARGET).item(0).getFirstChild().getNodeValue();
+            final String editId = requireNonNullValue(element.getElementsByTagName(EDIT_ID).item(0), EDIT_ID)
+                .getFirstChild().getNodeValue();
+            final String target = requireNonNullValue(element.getElementsByTagName(TARGET).item(0), TARGET)
+                .getFirstChild().getNodeValue();
             final List<Element> values = readValueNodes(element, oper);
             final Element firstValueElement = values != null ? values.get(0) : null;
 
