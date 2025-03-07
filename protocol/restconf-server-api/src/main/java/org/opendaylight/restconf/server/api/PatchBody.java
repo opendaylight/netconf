aca8a3f724d31aa0e8c0ada2c71e57572eeeacee
Peter Suna <peter.suna@pantheon.tech>
2025-03-07 19:09:47 +0100

    Add exception message for missing YangPatch nodes
    If any required schema node is missing, an NullPointerException
    is thrown without additional information.
    
    Use a custom requireNonNullValue check, which throws a checked
    RequestException and adds an exception message specifying
    the missing field.
    
    JIRA: NETCONF-1438
    Change-Id: I528a65f44d09ccf115533f73874c109cdf80c022
    Signed-off-by: Peter Suna <peter.suna@pantheon.tech>

--- protocol/restconf-server-api/src/main/java/org/opendaylight/restconf/server/api/PatchBody.java
+++ protocol/restconf-server-api/src/main/java/org/opendaylight/restconf/server/api/PatchBody.java
@@ -100,4 +100,20 @@ public abstract sealed class PatchBody extends RequestBody permits JsonPatchBody
             case Delete, Move, Remove -> false;
         };
     }
+
+    /**
+     * Check if provided value is not null.
+     *
+     * @param value node value
+     * @param elementName element name
+     * @return provided value if it is not null, otherwise throws RequestException
+     * @throws RequestException if the value is null
+     */
+    static final <T> T requireNonNullValue(final T value, final String elementName) throws RequestException {
+        if (value == null) {
+            throw new RequestException(ErrorType.APPLICATION, ErrorTag.MISSING_ELEMENT,
+                "Missing required element '" + elementName + "'");
+        }
+        return value;
+    }
 }
