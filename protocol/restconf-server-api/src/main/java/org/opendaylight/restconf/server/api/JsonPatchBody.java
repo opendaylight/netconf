1813b2c8071d7d5a75d2ebd6e6cd19c3444f20db
Peter Suna <peter.suna@pantheon.tech>
2025-03-12 10:37:43 +0100

    Handle empty 'value' in YangPatch
    The 'value' node can not contain empty brackets. Handle
    this case, as it produces a IllegalStateException.
    
    JIRA: NETCONF-1438
    Change-Id: I4377c2db9050b8594c42992c1197d919a0ba4e5e
    Signed-off-by: Peter Suna <peter.suna@pantheon.tech>

--- protocol/restconf-server-api/src/main/java/org/opendaylight/restconf/server/api/JsonPatchBody.java
+++ protocol/restconf-server-api/src/main/java/org/opendaylight/restconf/server/api/JsonPatchBody.java
@@ -158,10 +158,7 @@ public final class JsonPatchBody extends PatchBody {
                     edit.setTargetSchemaNode(stack.toInference());
                 }
                 case "value" -> {
-                    if (edit.getData() != null || deferredValue != null) {
-                        throw new RequestException(ErrorType.APPLICATION, ErrorTag.MALFORMED_MESSAGE,
-                            "Multiple value entries found");
-                    }
+                    verifyCondition(edit.getData() == null && deferredValue == null, "Multiple value entries found");
 
                     if (edit.getTargetSchemaNode() == null) {
                         // save data defined in value node for next (later) processing, because target needs to be read
@@ -207,8 +204,9 @@ public final class JsonPatchBody extends PatchBody {
      * @param in JsonReader reader
      * @throws IOException if operation fails
      */
-    private static String readValueNode(final @NonNull JsonReader in) throws IOException {
+    private static String readValueNode(final @NonNull JsonReader in) throws IOException, RequestException {
         in.beginObject();
+        verifyCondition(in.peek() == JsonToken.NAME, "Empty 'value' element is not allowed");
         final var sb = new StringBuilder().append("{\"").append(in.nextName()).append("\":");
 
         switch (in.peek()) {
@@ -302,11 +300,11 @@ public final class JsonPatchBody extends PatchBody {
         final var writer = ImmutableNormalizedNodeStreamWriter.from(resultHolder);
         try {
             JsonParserStream.create(writer, codecs, targetSchemaNode).parse(in);
+            return resultHolder.getResult().data();
         } catch (IllegalArgumentException | IllegalStateException | JsonParseException e) {
             LOG.error("Failed to parse provided JSON data", e);
             throw new RequestException(ErrorType.APPLICATION, ErrorTag.MALFORMED_MESSAGE, e);
         }
-        return resultHolder.getResult().data();
     }
 
     /**
@@ -337,6 +335,19 @@ public final class JsonPatchBody extends PatchBody {
 
         throw new RequestException(ErrorType.APPLICATION, ErrorTag.MALFORMED_MESSAGE, "Provided 'operation' value "
             + operation + (requiresValue(operation) ? " requires " : " can not have ") + VALUE + " element");
+    }
+
+    /**
+     * If provided parameter condition is false, throw a {@link RequestException}.
+     *
+     * @param condition {@code boolean}
+     * @param message message thrown if condition is false
+     * @throws RequestException if condition is false
+     */
+    private static void verifyCondition(final boolean condition, final String message) throws RequestException {
+        if (!condition) {
+            throw new RequestException(ErrorType.APPLICATION, ErrorTag.MALFORMED_MESSAGE, message);
+        }
     }
 
     /**
