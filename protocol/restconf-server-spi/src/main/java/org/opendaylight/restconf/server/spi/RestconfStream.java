dc1187c8e621f16b9921e3b65d3d96681f91f4c9
Matej Sramcik <matej.sramcik@pantheon.tech>
2025-03-14 22:43:11 +0100

    WIP: Get stream info for subscription
    Just an initial patch,
    we need to access receiver state.
    
    JIRA: NETCONF-1448
    Change-Id: Ib19491f75bab411a3fc8811d34ff8f641f5c5d0a
    Signed-off-by: Matej Sramcik <matej.sramcik@pantheon.tech>

--- protocol/restconf-server-spi/src/main/java/org/opendaylight/restconf/server/spi/RestconfStream.java
+++ protocol/restconf-server-spi/src/main/java/org/opendaylight/restconf/server/spi/RestconfStream.java
@@ -20,6 +20,7 @@ import java.lang.invoke.MethodHandles;
 import java.lang.invoke.VarHandle;
 import java.net.URI;
 import java.time.Instant;
+import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.regex.Pattern;
@@ -30,6 +31,14 @@ import org.eclipse.jdt.annotation.Nullable;
 import org.opendaylight.netconf.databind.RequestException;
 import org.opendaylight.restconf.server.api.EventStreamGetParams;
 import org.opendaylight.restconf.server.api.ServerRequest;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.subscribed.notifications.rev190909.EncodeJson$I;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.subscribed.notifications.rev190909.EncodeXml$I;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.subscribed.notifications.rev190909.Encoding;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.subscribed.notifications.rev190909.SubscriptionId;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.subscribed.notifications.rev190909.subscriptions.SubscriptionBuilder;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.subscribed.notifications.rev190909.subscriptions.subscription.ReceiversBuilder;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.subscribed.notifications.rev190909.subscriptions.subscription.receivers.ReceiverBuilder;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.subscribed.notifications.rev190909.subscriptions.subscription.receivers.ReceiverKey;
 import org.opendaylight.yangtools.concepts.Registration;
 import org.opendaylight.yangtools.yang.common.Empty;
 import org.opendaylight.yangtools.yang.common.QName;
@@ -319,6 +328,28 @@ public final class RestconfStream<T> {
         @NonNullByDefault
         protected ToStringHelper addToStringAttributes(final ToStringHelper helper) {
             return helper.add("terminated", terminated);
+        }
+
+        public final org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.subscribed.notifications.rev190909.subscriptions.Subscription toOperational() {
+            Encoding encoding = null;
+            if (encoding().getLocalName().equals(EncodingName.RFC8040_XML.name())){
+                encoding = EncodeXml$I.VALUE;
+            } else if (encoding().getLocalName().equals(EncodingName.RFC8040_JSON.name())) {
+                encoding = EncodeJson$I.VALUE;
+            }
+            return new SubscriptionBuilder()
+                .setId(new SubscriptionId(id()))
+                .setEncoding(encoding)
+                .setReceivers(new ReceiversBuilder()
+                    .setReceiver(Map.of(new ReceiverKey(receiverName()), new ReceiverBuilder()
+                        .setName(receiverName())
+
+                        //FIXME: add fields
+                        .setState()
+                        .setSentEventRecords()
+                        .setExcludedEventRecords()
+                        .build())).build())
+                .build();
         }
     }
 
