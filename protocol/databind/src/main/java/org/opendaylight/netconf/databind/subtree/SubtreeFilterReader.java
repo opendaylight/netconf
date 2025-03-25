47d507942246b3eb15bedce378c67296ab03177a
Oleksandr Zharov <oleksandr.zharov@pantheon.tech>
2025-03-25 02:45:19 +0100

    Implement SubtreeFilterReader
    TODO
    
    JIRA: NETCONF-1446
    Change-Id: I4d4a05c818672c12ff42258b41d457eb06ec44f0
    Signed-off-by: Oleksandr Zharov <oleksandr.zharov@pantheon.tech>

--- protocol/databind/src/main/java/org/opendaylight/netconf/databind/subtree/SubtreeFilterReader.java
+++ protocol/databind/src/main/java/org/opendaylight/netconf/databind/subtree/SubtreeFilterReader.java
@@ -7,10 +7,13 @@
  */
 package org.opendaylight.netconf.databind.subtree;
 
+import javax.xml.stream.XMLStreamConstants;
 import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.XMLStreamReader;
 import org.eclipse.jdt.annotation.NonNullByDefault;
 import org.opendaylight.netconf.databind.DatabindContext;
+import org.opendaylight.netconf.databind.subtree.NamespaceSelection.Exact;
+import org.opendaylight.yangtools.yang.common.QName;
 
 @NonNullByDefault
 final class SubtreeFilterReader {
@@ -20,7 +23,69 @@ final class SubtreeFilterReader {
 
     static SubtreeFilter readSubtreeFilter(final XMLStreamReader reader, final DatabindContext databind)
             throws XMLStreamException {
-        // FIXME: implement this
-        throw new UnsupportedOperationException();
+        final var subtreeFilterBuilder = SubtreeFilter.builder(databind);
+        // Flag that will be flipped after we find start of the first element - filter
+        var isFilter = true;
+        // Only checking for start of filter element - all data we interested in is inside of it
+        while (reader.hasNext()) {
+            switch (reader.next()) {
+                case XMLStreamConstants.END_DOCUMENT: {
+                    // Build and return SubtreeFilter after reaching end of the document
+                    return subtreeFilterBuilder.build();
+                }
+                case XMLStreamConstants.START_ELEMENT: {
+                    if (isFilter) {
+                        // We inside filter element - skipping it because it has no use to us
+                        isFilter = false;
+                        break;
+                    }
+                    subtreeFilterBuilder.add(fillChildren(reader));
+                    break;
+                }
+//                case XMLStreamConstants.ATTRIBUTE: {
+//                    final var attrs = reader.getAttributeCount();
+//                    for (var i = 0; i < attrs; i++) {
+//                        final var attrName = reader.getAttributeName(i);
+//                        final var attrValue = reader.getAttributeValue(i);
+//                    }
+//                    break;
+//                }
+                default: {
+                    // ignore all other events
+                    break;
+                }
+            }
+        }
+        throw new XMLStreamException("Unexpected end of the document");
+    }
+
+    private static Sibling fillChildren(final XMLStreamReader reader)
+            throws XMLStreamException {
+        final var qName = reader.getName();
+        final var elementName = qName.getLocalPart();
+        final var elementNamespace = qName.getNamespaceURI();
+        while (reader.hasNext()) {
+            switch (reader.next()) {
+                case XMLStreamConstants.START_ELEMENT: {
+                    // TODO handle wildcard
+                    ContainmentNode.builder(new Exact(QName.create(elementNamespace, elementName)));
+                    fillChildren(reader);
+                    break;
+                }
+                case XMLStreamConstants.CHARACTERS: {
+                    // TODO handle content
+                    final var text = reader.getTextCharacters();
+                    break;
+                }
+                case XMLStreamConstants.END_ELEMENT: {
+                    return SelectionNode.builder(new Exact(QName.create(elementNamespace, elementName))).build();
+                }
+                default: {
+                    // ignore all other events
+                    break;
+                }
+            }
+        }
+        return null;
     }
 }
