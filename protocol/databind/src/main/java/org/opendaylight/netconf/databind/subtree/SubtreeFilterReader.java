a088587db1594b1b0388b0f9923e88d2de707c40
Oleksandr Zharov <oleksandr.zharov@pantheon.tech>
2025-03-25 02:45:19 +0100

    Implement SubtreeFilterReader
    Implemented SubtreeFilterReader method that processes data from
    XMLStreamReader and creates SubtreeFilter structure that
    corresponds to EffectiveModelContext.
    Added tests to cover RFC examples plus some extra for wildcard
    and multiple namespace cases.
    
    JIRA: NETCONF-1446
    Change-Id: I4d4a05c818672c12ff42258b41d457eb06ec44f0
    Signed-off-by: Oleksandr Zharov <oleksandr.zharov@pantheon.tech>

--- protocol/databind/src/main/java/org/opendaylight/netconf/databind/subtree/SubtreeFilterReader.java
+++ protocol/databind/src/main/java/org/opendaylight/netconf/databind/subtree/SubtreeFilterReader.java
@@ -7,10 +7,23 @@
  */
 package org.opendaylight.netconf.databind.subtree;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import javax.xml.stream.XMLStreamConstants;
 import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.XMLStreamReader;
 import org.eclipse.jdt.annotation.NonNullByDefault;
 import org.opendaylight.netconf.databind.DatabindContext;
+import org.opendaylight.netconf.databind.subtree.NamespaceSelection.Exact;
+import org.opendaylight.yangtools.yang.common.QName;
+import org.opendaylight.yangtools.yang.common.UnresolvedQName;
+import org.opendaylight.yangtools.yang.common.XMLNamespace;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.NodeIdentifier;
+import org.opendaylight.yangtools.yang.model.api.EffectiveModelContext;
+import org.opendaylight.yangtools.yang.model.api.stmt.ModuleEffectiveStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.SchemaTreeAwareEffectiveStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.SchemaTreeEffectiveStatement;
 
 @NonNullByDefault
 final class SubtreeFilterReader {
@@ -20,7 +33,246 @@ final class SubtreeFilterReader {
 
     static SubtreeFilter readSubtreeFilter(final XMLStreamReader reader, final DatabindContext databind)
             throws XMLStreamException {
-        // FIXME: implement this
-        throw new UnsupportedOperationException();
+        final var subtreeFilterBuilder = SubtreeFilter.builder(databind);
+        final var context = databind.modelContext();
+        while (reader.hasNext()) {
+            if (reader.next() == XMLStreamConstants.START_ELEMENT) {
+                break;
+            }
+        }
+        while (reader.hasNext()) {
+            if (reader.next() == XMLStreamConstants.START_ELEMENT) {
+                fillElement(reader, subtreeFilterBuilder, new ArrayList<>(), context, getAttributes(reader, context));
+                break;
+            }
+        }
+        return subtreeFilterBuilder.build();
+    }
+
+    // Goes over elements inside of filter node itself and fills SubtreeFilter builder with respective sibling types
+    private static void fillElement(final XMLStreamReader reader, final SiblingSetBuilder builder,
+            final List<javax.xml.namespace.QName> path, final EffectiveModelContext context,
+            final List<AttributeMatch> attributes) throws XMLStreamException {
+        final var elementName = reader.getName();
+        path.add(elementName);
+        // create NamespaceSelection based on provided info
+        final var namespace = getElementNamespace(context, path);
+        // Check what is next event after start of the element
+        while (reader.hasNext()) {
+            switch (reader.next()) {
+                case XMLStreamConstants.START_ELEMENT: {
+                    // return containment if there are other elements inside
+                    final var containmentBuilder = ContainmentNode.builder(namespace);
+                    // add first sibling since we already found it
+                    fillElement(reader, containmentBuilder, path, context, getAttributes(reader, context));
+                    // add rest of the siblings until we reach the end of the element
+                    while (reader.hasNext()) {
+                        final var event = reader.next();
+                        if (event == XMLStreamConstants.START_ELEMENT) {
+                            fillElement(reader, containmentBuilder, path, context, getAttributes(reader, context));
+                        } else if (event == XMLStreamConstants.END_ELEMENT) {
+                            if (elementName.equals(reader.getName())) {
+                                break;
+                            }
+                        }
+                    }
+                    // build containment and add it to parent siblings
+                    builder.addSibling(containmentBuilder.build());
+                    // remove last step from the path and return step above
+                    path.removeLast();
+                    return;
+                }
+                case XMLStreamConstants.CHARACTERS: {
+                    // return content if there are non whitespace characters
+                    final var text = reader.getText();
+                    if (!text.trim().isEmpty()) {
+                        builder.addSibling(new ContentMatchNode(namespace, text));
+                        path.removeLast();
+                        return;
+                    }
+                    break;
+                }
+                case XMLStreamConstants.END_ELEMENT: {
+                    // sibling still not assigned means it is just empty element - selection node
+                    final var selection = SelectionNode.builder(namespace);
+                    if (!attributes.isEmpty()) {
+                        attributes.forEach(selection::add);
+                    }
+                    builder.addSibling(selection.build());
+                    path.removeLast();
+                    return;
+                }
+                default: {
+                    // ignore all other events
+                    break;
+                }
+            }
+        }
+    }
+
+    private static List<AttributeMatch> getAttributes(final XMLStreamReader reader, final EffectiveModelContext context)
+            throws XMLStreamException {
+        final var childAttributes = new ArrayList<AttributeMatch>();
+        final var attrCount = reader.getAttributeCount();
+        for (var i = 0; i < attrCount; i++) {
+            final var attrName = reader.getAttributeName(i);
+            if (attrName.getNamespaceURI().isEmpty()) {
+                throw new XMLStreamException("Missing namespace for attribute " + attrName);
+            }
+            final var attrNamespace = attrName.getNamespaceURI();
+            // lookup module by namespace
+            final var it = context.findModuleStatements(XMLNamespace.of(attrNamespace)).iterator();
+            final Exact exactNamespace;
+            if (it.hasNext()) {
+                exactNamespace = new Exact(NodeIdentifier.create(QName.create(it.next().localQNameModule(),
+                    attrName.getLocalPart())));
+            } else {
+                throw new XMLStreamException("Failed to lookup module schema for namespace "
+                    + attrNamespace);
+            }
+            final var attrValue = reader.getAttributeValue(i);
+            childAttributes.add(new AttributeMatch(exactNamespace, attrValue));
+        }
+        return childAttributes;
+    }
+
+    private static NamespaceSelection getElementNamespace(final EffectiveModelContext context,
+            final List<javax.xml.namespace.QName> path)
+            throws XMLStreamException {
+        final var firstNode = path.getFirst();
+        final var firstNamespace = firstNode.getNamespaceURI();
+        final var modules = new ArrayList<ModuleEffectiveStatement>();
+        final var nodeQNames = new ArrayList<QName>();
+        // check path nodes to determine what we are dealing with - exact namespace, wildcard or mix
+        if (!firstNamespace.isEmpty()) {
+            // try to find exact module if we know namespace of the first node
+            final var found = context.findModuleStatements(XMLNamespace.of(firstNamespace)).iterator();
+            if (found.hasNext()) {
+                final var module = found.next();
+                // if last element has namespace then assume all elements above have too and if we found module then
+                // just create exact namespace selection directly and return it
+                final var lastNode = path.getLast();
+                if (!lastNode.getNamespaceURI().isEmpty()) {
+                    // go through schema tree using path elements to find node
+                    for (final var qname: path) {
+                        final var nodeNamespace = qname.getNamespaceURI();
+                        if (!nodeNamespace.isEmpty()) {
+                            // lookup module for every node in case filter has multiple namespaces
+                            final var nodeModule = context.findModuleStatements(XMLNamespace.of(nodeNamespace))
+                                .iterator();
+                            if (nodeModule.hasNext()) {
+                                nodeQNames.add(QName.create(nodeModule.next().localQNameModule(),
+                                    qname.getLocalPart()));
+                            } else {
+                                throw new XMLStreamException("Failed to lookup module schema for namespace "
+                                    + nodeNamespace);
+                            }
+                        } else {
+                            throw new XMLStreamException("Unexpected wildcard in the middle of the filter");
+                        }
+                    }
+                    final var foundNode = module.findSchemaTreeNode(nodeQNames)
+                        .orElseThrow(() -> new XMLStreamException("Failed to lookup node schema with name "
+                            + lastNode.getLocalPart()));
+                    return new Exact(NodeIdentifier.create(foundNode.argument()));
+                }
+                // if last element doesn't have namespace then we have a wildcard somewhere in the filter node but not
+                // at the start, so we can start creating namespace selection from module that was already found
+                modules.add(module);
+            } else {
+                throw new XMLStreamException("Failed to lookup module with namespace %s." + firstNamespace);
+            }
+        } else {
+            // in this case wildcard starts from the first element, so we need to check all modules and try to narrow
+            // search scope by finding every module that contain child node with same name as first node in path
+            for (final var module : context.getModuleStatements().values()) {
+                final var found = module.schemaTreeNodes().stream().anyMatch(schemaTreeEffectiveStatement -> firstNode
+                    .getLocalPart().equals(schemaTreeEffectiveStatement.getDeclared().argument().getLocalName()));
+                if (found) {
+                    modules.add(module);
+                }
+            }
+        }
+        // process wildcard for selected modules
+        for (final var module : modules) {
+            // if true then one or few top elements of the filter has specified namespace means they can be treated same
+            // as exact namespace selection
+            final var it = path.iterator();
+            // take first node and check if it has namespace
+            var qname = it.next();
+            while (it.hasNext()) {
+                final var nodeNamespace = qname.getNamespaceURI();
+                if (!nodeNamespace.isEmpty()) {
+                    // lookup module for every node in case filter has multiple namespaces
+                    final var nodeModule = context.findModuleStatements(XMLNamespace.of(nodeNamespace)).iterator();
+                    if (nodeModule.hasNext()) {
+                        nodeQNames.add(QName.create(nodeModule.next().localQNameModule(), qname.getLocalPart()));
+                    } else {
+                        throw new XMLStreamException("Failed to lookup module schema for namespace "
+                            + nodeNamespace);
+                    }
+                    qname = it.next();
+                } else {
+                    // no more filter elements that has specified namespace
+                    break;
+                }
+            }
+            final var firstWildcard = qname;
+            final List<SchemaTreeEffectiveStatement<?>> wildcardChildren;
+            // if there were some nodes with namespace specified then start search from that point
+            if (!nodeQNames.isEmpty()) {
+                // skip to the place where namespaces end and wildcard starts
+                final var found = module.findSchemaTreeNode(nodeQNames)
+                    .orElseThrow(() -> new XMLStreamException("Failed to lookup node schema with name "
+                        + nodeQNames.getLast().getLocalName()));
+                // find all children that has same name as wildcard
+                wildcardChildren = found instanceof SchemaTreeAwareEffectiveStatement<?,?> treeAware
+                    ? treeAware.schemaTreeNodes().stream().filter(child -> firstWildcard.getLocalPart().equals(child
+                        .getDeclared().argument().getLocalName())).toList() : null;
+            } else {
+                // in this case whole filter node is a wildcard so start from searching
+                wildcardChildren = module.schemaTreeNodes().stream().filter(schemaTreeEffectiveStatement ->
+                    firstWildcard.getLocalPart().equals(schemaTreeEffectiveStatement.getDeclared().argument()
+                        .getLocalName())).toList();
+            }
+            if (wildcardChildren == null || wildcardChildren.isEmpty()) {
+                throw new XMLStreamException("Failed to lookup node schema for " + firstWildcard.getLocalPart());
+            }
+            // process children recursively
+            final var unqualified = UnresolvedQName.Unqualified.of(path.getLast().getLocalPart());
+            final var qNames = processChildrenStatements(wildcardChildren, it);
+            if (qNames.isEmpty()) {
+                throw new XMLStreamException("Failed to identifiers for " + unqualified.getLocalName());
+            }
+            final var identifiers = qNames.stream().map(NodeIdentifier::create).toList();
+            return new NamespaceSelection.Wildcard(unqualified, identifiers);
+        }
+        throw new XMLStreamException("Failed to lookup any module with that corresponds to filter");
+    }
+
+    private static List<QName> processChildrenStatements(final List<SchemaTreeEffectiveStatement<?>> found,
+            final Iterator<javax.xml.namespace.QName> it) {
+        if (it.hasNext()) {
+            // if true then we have children after current node means it is too early to collect QNames
+            final var childName = it.next().getLocalPart();
+            final var qNames = new ArrayList<QName>();
+            for (final var stmt : found) {
+                final var children = stmt instanceof SchemaTreeAwareEffectiveStatement<?,?> treeAware
+                    ? treeAware.schemaTreeNodes().stream().filter(child -> childName.equals(child.getDeclared()
+                    .argument().getLocalName())).toList() : null;
+                if (children == null || children.isEmpty()) {
+                    continue;
+                }
+                qNames.addAll(processChildrenStatements(children, it));
+            }
+            return qNames;
+        } else {
+            // current node is the lase one - time to collect QNames and return them
+            final var qNames = new ArrayList<QName>();
+            for (final var stmt : found) {
+                qNames.add(stmt.getDeclared().argument());
+            }
+            return qNames;
+        }
     }
 }
