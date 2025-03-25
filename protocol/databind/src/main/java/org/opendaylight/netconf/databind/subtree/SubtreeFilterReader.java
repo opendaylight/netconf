70995a10b502db21245a5c292d18742a94ab1c81
Oleksandr Zharov <oleksandr.zharov@pantheon.tech>
2025-03-25 02:45:19 +0100

    Implement SubtreeFilterReader
    TODO
    
    JIRA: NETCONF-1446
    Change-Id: I4d4a05c818672c12ff42258b41d457eb06ec44f0
    Signed-off-by: Oleksandr Zharov <oleksandr.zharov@pantheon.tech>

--- protocol/databind/src/main/java/org/opendaylight/netconf/databind/subtree/SubtreeFilterReader.java
+++ protocol/databind/src/main/java/org/opendaylight/netconf/databind/subtree/SubtreeFilterReader.java
@@ -7,10 +7,18 @@
  */
 package org.opendaylight.netconf.databind.subtree;
 
+import java.io.IOException;
 import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.XMLStreamReader;
 import org.eclipse.jdt.annotation.NonNullByDefault;
 import org.opendaylight.netconf.databind.DatabindContext;
+import org.opendaylight.netconf.databind.subtree.NamespaceSelection.Exact;
+import org.opendaylight.yangtools.yang.data.api.schema.ContainerNode;
+import org.opendaylight.yangtools.yang.data.api.schema.DataContainerChild;
+import org.opendaylight.yangtools.yang.data.api.schema.LeafNode;
+import org.opendaylight.yangtools.yang.data.codec.xml.XmlParserStream;
+import org.opendaylight.yangtools.yang.data.impl.schema.ImmutableNormalizedNodeStreamWriter;
+import org.opendaylight.yangtools.yang.data.impl.schema.NormalizationResultHolder;
 
 @NonNullByDefault
 final class SubtreeFilterReader {
@@ -19,8 +27,43 @@ final class SubtreeFilterReader {
     }
 
     static SubtreeFilter readSubtreeFilter(final XMLStreamReader reader, final DatabindContext databind)
-            throws XMLStreamException {
-        // FIXME: implement this
-        throw new UnsupportedOperationException();
+            throws XMLStreamException, IOException {
+        final var builder = SubtreeFilter.builder(databind);
+
+        final var result = new NormalizationResultHolder();
+        final var streamWriter = ImmutableNormalizedNodeStreamWriter.from(result);
+        final var xmlParser = XmlParserStream.create(streamWriter, databind.modelContext());
+        xmlParser.parse(reader);
+        final var transformed = ((ContainerNode) result.getResult().data());
+        for (final var child : transformed.body()) {
+            builder.add(fillBuilder(child));
+        }
+
+        return builder.build();
+    }
+
+    private static Sibling fillBuilder(final DataContainerChild node) {
+        final var identifier = node.name();
+        if (node instanceof ContainerNode containerNode) {
+            // if container is empty - select whole container
+            if (containerNode.isEmpty()) {
+                return SelectionNode.builder(new Exact(identifier)).build();
+            }
+            // if container not empty - select container with only specified children
+            final var containment = ContainmentNode.builder(new Exact(identifier));
+            for (final var child : containerNode.body()) {
+                containment.add(fillBuilder(child));
+            }
+            return containment.build();
+        } else if (node instanceof LeafNode leaf) {
+            // if leaf is empty - select whole leaf
+            if (leaf.body().equals("")) {
+                return SelectionNode.builder(new Exact(identifier)).build();
+            }
+            // if leaf not empty - match content of the leaf
+            return new ContentMatchNode(new Exact(identifier), leaf.body());
+        }
+        // TODO choice
+        return null;
     }
 }
