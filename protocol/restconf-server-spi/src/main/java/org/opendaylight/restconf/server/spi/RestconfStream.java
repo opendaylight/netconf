93b3fe13453c07d1eada11741c914d2e11762409
Samuel Schneider <samuel.schneider@pantheon.tech>
2025-03-26 17:28:05 +0100

    Split RestconfStream
    Split RestconfStream into:
    LegacyRestconfStream which is closed when last subscriber
    is removed.
    RestconfStreamImpl which is NOT closed when last subscriber
    is removed.
    
    JIRA: NETCONF-714
    Change-Id: I40f12bb68baef8ce23f007df87297d524b08f787
    Signed-off-by: Samuel Schneider <samuel.schneider@pantheon.tech>

--- protocol/restconf-server-spi/src/main/java/org/opendaylight/restconf/server/spi/RestconfStream.java
+++ protocol/restconf-server-spi/src/main/java/org/opendaylight/restconf/server/spi/RestconfStream.java
@@ -46,7 +46,7 @@ import org.slf4j.LoggerFactory;
  *
  * @param <T> Type of processed events
  */
-public final class RestconfStream<T> {
+public abstract sealed class RestconfStream<T> permits LegacyRestconfStream, DefaultRestconfStream {
     /**
      * An opinionated view on what values we can produce for {@code Access.getEncoding()}. The name can only be composed
      * of one or more characters matching {@code [a-zA-Z]}.
@@ -576,8 +576,11 @@ public final class RestconfStream<T> {
     }
 
     /**
-     * Removes a {@link Subscriber}. If this was the last subscriber also shut down this stream and initiate its removal
-     * from global state.
+     * Removes a {@link Subscriber}. If this was the last subscriber we decide based on implementation what should
+     * happen with this stream.
+     * If it was created by {@link Registry#createLegacyStream} it will be shut down and its removal from global state
+     * should be initiated.
+     * If it was created by {@link Registry#createStream} we keep the stream running.
      *
      * @param subscriber The {@link Subscriber} to remove
      * @throws NullPointerException if {@code subscriber} is {@code null}
@@ -592,7 +595,7 @@ public final class RestconfStream<T> {
                 LOG.debug("Subscriber {} is removed", subscriber);
                 if (next == null) {
                     // We have lost the last subscriber, terminate.
-                    terminate();
+                    onLastSubscriber();
                 }
                 return;
             }
@@ -601,6 +604,8 @@ public final class RestconfStream<T> {
             observed = witness;
         }
     }
+
+    abstract void onLastSubscriber();
 
     private Subscribers<T> acquireSubscribers() {
         return (Subscribers<T>) SUBSCRIBERS_VH.getAcquire(this);
@@ -620,7 +625,7 @@ public final class RestconfStream<T> {
         }
     }
 
-    private void terminate() {
+    void terminate() {
         synchronized (this) {
             if (registration != null) {
                 registration.close();
