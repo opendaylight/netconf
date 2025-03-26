232294982274e641d97f80001ba48fd36d94f751
Samuel Schneider <samuel.schneider@pantheon.tech>
2025-03-26 17:28:05 +0100

    Split RestconfStream
    Split RestconfStream into:
    LegacyRestconfStream which is closed when last subscriber
    is removed.
    RestconfStreamImpl which is NOT closed when last subscriber
    is removed.
    
    JIRA: NETCONF-714
    Change-Id: I40f12bb68baef8ce23f007df87297d524b08f787
    Signed-off-by: Samuel Schneider <samuel.schneider@pantheon.tech>

--- protocol/restconf-server-spi/src/main/java/org/opendaylight/restconf/server/spi/AbstractRestconfStreamRegistry.java
+++ protocol/restconf-server-spi/src/main/java/org/opendaylight/restconf/server/spi/AbstractRestconfStreamRegistry.java
@@ -101,12 +101,25 @@ public abstract class AbstractRestconfStreamRegistry implements RestconfStream.R
 
     @Override
     public final <T> void createStream(final ServerRequest<RestconfStream<T>> request, final URI restconfURI,
-            final RestconfStream.Source<T> source, final String description) {
-        final var stream = allocateStream(source);
+            final Source<T> source, final String description) {
+        final var name = allocateStreamName();
+        registerStream(request, restconfURI, description, new RestconfStreamImpl<>(this, source, name));
+    }
+
+    @Override
+    public final <T> void createLegacyStream(final ServerRequest<RestconfStream<T>> request, final URI restconfURI,
+            final Source<T> source, final String description) {
+        final var name = allocateStreamName();
+        registerStream(request, restconfURI, description, new LegacyRestconfStream<>(this, source, name));
+    }
+
+    private <T> void registerStream(final ServerRequest<RestconfStream<T>> request, final URI restconfURI,
+            final String description, final RestconfStream<T> stream) {
         final var name = stream.name();
         if (description.isBlank()) {
             throw new IllegalArgumentException("Description must be descriptive");
         }
+        streams.put(name, stream);
 
         Futures.addCallback(putStream(stream, description, restconfURI), new FutureCallback<>() {
             @Override
@@ -125,14 +138,8 @@ public abstract class AbstractRestconfStreamRegistry implements RestconfStream.R
     }
 
     @Override
-    public <T> void createLegacyStream(final ServerRequest<RestconfStream<T>> request, final URI restconfURI,
-            final Source<T> source, final String description) {
-        createStream(request, restconfURI, source, description);
-    }
-
-    @Override
     public <T> void start(final Source<T> source) {
-        final var stream = new RestconfStream<>(this, source, DEFAULT_STREAM_NAME);
+        final var stream = new RestconfStreamImpl<>(this, source, DEFAULT_STREAM_NAME);
         streams.put(DEFAULT_STREAM_NAME, stream);
         Futures.addCallback(putStream(stream, DEFAULT_STREAM_DESCRIPTION, null), new FutureCallback<>() {
             @Override
@@ -148,17 +155,14 @@ public abstract class AbstractRestconfStreamRegistry implements RestconfStream.R
         }, MoreExecutors.directExecutor());
     }
 
-    private <T> RestconfStream<T> allocateStream(final Source<T> source) {
+    private String allocateStreamName() {
         String name;
-        RestconfStream<T> stream;
         do {
             // Use Type 4 (random) UUID. While we could just use it as a plain string, be nice to observers and anchor
             // it into UUID URN namespace as defined by RFC4122
-            name = "urn:uuid:" + UUID.randomUUID().toString();
-            stream = new RestconfStream<>(this, source, name);
-        } while (streams.putIfAbsent(name, stream) != null);
-
-        return stream;
+            name = "urn:uuid:" + UUID.randomUUID();
+        } while (streams.containsKey(name));
+        return name;
     }
 
     protected abstract @NonNull ListenableFuture<Void> putStream(@NonNull RestconfStream<?> stream,
