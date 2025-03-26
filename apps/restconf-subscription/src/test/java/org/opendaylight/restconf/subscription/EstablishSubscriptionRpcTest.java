510a6dc54f7787aa25aac5ad40df59ff72896691
Samuel Schneider <samuel.schneider@pantheon.tech>
2025-03-26 17:28:05 +0100

    Split RestconfStream
    Split RestconfStream into:
    LegacyRestconfStream which is closed when last subscriber
    is removed.
    RestconfStreamImpl which is NOT closed when last subscriber
    is removed.
    
    JIRA: NETCONF-714
    Change-Id: I40f12bb68baef8ce23f007df87297d524b08f787
    Signed-off-by: Samuel Schneider <samuel.schneider@pantheon.tech>

--- apps/restconf-subscription/src/test/java/org/opendaylight/restconf/subscription/EstablishSubscriptionRpcTest.java
+++ apps/restconf-subscription/src/test/java/org/opendaylight/restconf/subscription/EstablishSubscriptionRpcTest.java
@@ -32,6 +32,7 @@ import org.opendaylight.restconf.server.api.TransportSession;
 import org.opendaylight.restconf.server.api.testlib.CompletingServerRequest;
 import org.opendaylight.restconf.server.spi.OperationInput;
 import org.opendaylight.restconf.server.spi.RestconfStream;
+import org.opendaylight.restconf.server.spi.DefaultRestconfStream;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.subscribed.notifications.rev190909.EncodeJson$I;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.subscribed.notifications.rev190909.EstablishSubscriptionInput;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.subscribed.notifications.rev190909.EstablishSubscriptionOutput;
@@ -67,7 +68,7 @@ class EstablishSubscriptionRpcTest {
     @Mock
     private RestconfStream.Registry streamRegistry;
     @Mock
-    private RestconfStream<?> restconfStream;
+    private DefaultRestconfStream<?> restconfStream;
     @Captor
     private ArgumentCaptor<RequestException> response;
 
