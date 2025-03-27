1be469e57b953f00e2518bbeb522a7732a0c7174
Yaroslav Lastivka <yaroslav.lastivka@pantheon.tech>
2025-03-27 11:30:05 +0200

    Implement SubtreeMatcher
    TODO
    
    JIRA: NETCONF-1456
    Change-Id: Icc98c00aef663e9a55923af844451453318a8f69
    Signed-off-by: Yaroslav Lastivka <yaroslav.lastivka@pantheon.tech>

--- protocol/databind/src/main/java/org/opendaylight/netconf/databind/subtree/SubtreeMatcher.java
+++ protocol/databind/src/main/java/org/opendaylight/netconf/databind/subtree/SubtreeMatcher.java
@@ -10,9 +10,14 @@ package org.opendaylight.netconf.databind.subtree;
 import static java.util.Objects.requireNonNull;
 
 import com.google.common.base.MoreObjects;
+import java.util.Objects;
+import java.util.regex.Pattern;
 import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.eclipse.jdt.annotation.Nullable;
 import org.opendaylight.yangtools.concepts.Immutable;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
 import org.opendaylight.yangtools.yang.data.api.schema.ContainerNode;
+import org.opendaylight.yangtools.yang.data.api.schema.LeafNode;
 
 /**
  * A matcher of a {@link SubtreeFilter} with a {@link ContainerNode}.
@@ -28,8 +33,159 @@ public final class SubtreeMatcher implements Immutable {
     }
 
     public boolean matches() {
-        // FIXME: implement this
-        throw new UnsupportedOperationException();
+        // Check containment nodes recursively
+        for (final var containment : filter.containments()) {
+            if (!matchContainment(containment, data)) {
+                return false;
+            }
+        }
+
+        // Check content matches at the top level
+        for (final var contentMatch : filter.contentMatches()) {
+            if (!matchContent(contentMatch, data)) {
+                return false;
+            }
+        }
+
+        // Check selection nodes (which may enforce attribute matches)
+        for (final var selection : filter.selections()) {
+            if (!matchSelection(selection, data)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Recursively checks that a containment filter is satisfied in the given parent data node.
+     */
+    private boolean matchContainment(final ContainmentNode containment, final ContainerNode parent) {
+        // Find a child of the parent whose name matches the containment's selection.
+        final var child = findChild(parent, containment.selection());
+        if (child == null) {
+            return false;
+        }
+        // First, check any content match nodes defined in this containment.
+        for (final var cm : containment.contentMatches()) {
+            if (!matchContent(cm, child)) {
+                return false;
+            }
+        }
+        // Then check any nested containment nodes recursively.
+        for (final var nested : containment.containments()) {
+            if (!matchContainment(nested, child)) {
+                return false;
+            }
+        }
+        // Finally, check any selection nodes (attribute matches) defined in this containment.
+        for (final var selection : containment.selections()) {
+            if (!matchSelection(selection, child)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Checks that a content match node is satisfied in the given data node.
+     * If the expected value is a String, it is interpreted as a regular expression.
+     */
+    private boolean matchContent(final ContentMatchNode contentMatch, final ContainerNode node) {
+        final var leaf = findLeaf(node, contentMatch.selection());
+        if (leaf == null) {
+            return false;
+        }
+        final var actualValue = leaf.body();
+        final var expectedValue = contentMatch.value();
+        if (expectedValue instanceof String && actualValue != null) {
+            final var pattern = Pattern.compile((String) expectedValue);
+            return pattern.matcher(actualValue.toString()).matches();
+        } else {
+            return Objects.equals(expectedValue, actualValue);
+        }
+    }
+
+    /**
+     * Checks that a selection node (attribute match) is satisfied in the given data node.
+     * In this implementation attributes are simulated as child leaf nodes.
+     */
+    private boolean matchSelection(final SelectionNode selection, final ContainerNode node) {
+        // If no attribute matches are defined, the existence of the node suffices.
+        if (selection.attributeMatches().isEmpty()) {
+            return true;
+        }
+        for (AttributeMatch am : selection.attributeMatches()) {
+            final var actualAttr = getAttributeValue(node, am.selection());
+            final var expectedAttr = am.value();
+            if (expectedAttr instanceof String && actualAttr != null) {
+                final var pattern = Pattern.compile((String) expectedAttr);
+                if (!pattern.matcher(actualAttr.toString()).matches()) {
+                    return false;
+                }
+            } else if (!Objects.equals(expectedAttr, actualAttr)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Searches for a child of the given parent node whose name matches the provided NamespaceSelection.
+     * Here we assume that the selection is of type Exact.
+     */
+    private @Nullable ContainerNode findChild(final ContainerNode parent, final NamespaceSelection selection) {
+        if (!(selection instanceof NamespaceSelection.Exact exact)) {
+            return null;
+        }
+        final var expectedQName = exact.qname();
+        final YangInstanceIdentifier.NodeIdentifier expectedNodeId = YangInstanceIdentifier.NodeIdentifier
+            .create(expectedQName);
+        // First, check the parent's own identifier.
+        if (parent.name().equals(expectedNodeId)) {
+            return parent;
+        }
+        // Otherwise, check among the children.
+        for (var child : parent.body()) {
+            if (child instanceof ContainerNode cn && cn.name().equals(expectedNodeId)) {
+                return cn;
+            }
+            // Optionally, you might check for leaf nodes if the filter expects them.
+        }
+        return null;
+    }
+
+    /**
+     * Retrieves an attribute value from the given node.
+     * Attributes are simulated as leaf children with matching QName.
+     */
+    private @Nullable Object getAttributeValue(ContainerNode node, NamespaceSelection key) {
+        if (!(key instanceof NamespaceSelection.Exact exact)) {
+            return null;
+        }
+        final var attrQName = exact.qname();
+        final YangInstanceIdentifier.NodeIdentifier expectedId = YangInstanceIdentifier.NodeIdentifier
+            .create(attrQName);
+        for (var child : node.body()) {
+            if (child instanceof LeafNode<?> leaf && leaf.name().equals(expectedId)) {
+                return leaf.body();
+            }
+        }
+        return null;
+    }
+
+    private @Nullable LeafNode<?> findLeaf(final ContainerNode parent, final NamespaceSelection selection) {
+        if (!(selection instanceof NamespaceSelection.Exact exact)) {
+            return null;
+        }
+        final var expectedQName = exact.qname();
+        final YangInstanceIdentifier.NodeIdentifier expectedId = YangInstanceIdentifier.NodeIdentifier
+            .create(expectedQName);
+        for (var child : parent.body()) {
+            if (child instanceof LeafNode<?> leaf && leaf.name().equals(expectedId)) {
+                return leaf;
+            }
+        }
+        return null;
     }
 
     @Override
---
+++ protocol/databind/src/test/java/org/opendaylight/netconf/databind/subtree/SubtreeMatcherTest.java
@@ -0,0 +1,202 @@
+/*
+ * Copyright (c) 2025 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.netconf.databind.subtree;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.opendaylight.netconf.databind.DatabindContext;
+import org.opendaylight.yangtools.yang.common.QName;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.NodeIdentifier;
+import org.opendaylight.yangtools.yang.data.spi.node.ImmutableNodes;
+
+@ExtendWith(MockitoExtension.class)
+class SubtreeMatcherTest {
+    private static final String NAMESPACE = "http://example.com/schema/1.2/config";
+    private static final String NAMESPACE2 = "http://example.com/schema/1.2/config2";
+    private static final QName TOP_QNAME = QName.create(NAMESPACE, "top");
+    private static final QName INTERFACES_QNAME = QName.create(NAMESPACE, "interfaces");
+    private static final QName INTERFACE_QNAME = QName.create(NAMESPACE, "interface");
+    private static final QName IFNAME_QNAME = QName.create(NAMESPACE, "ifName");
+    private static final QName USERS_QNAME = QName.create(NAMESPACE, "users");
+    private static final QName USER_QNAME = QName.create(NAMESPACE, "user");
+    private static final QName NAME_QNAME = QName.create(NAMESPACE, "name");
+    private static final QName COMPANY_INFO_QNAME = QName.create(NAMESPACE, "company-info");
+
+    @Mock
+    private DatabindContext mockedContext;
+
+    @Test
+    void testSimpleSelectionMatch() {
+        final var filter = SubtreeFilter.builder(mockedContext)
+            .add(SelectionNode.builder(new NamespaceSelection.Exact(TOP_QNAME))
+                .build())
+            .build();
+
+        final var top = ImmutableNodes.newContainerBuilder()
+            .withNodeIdentifier(NodeIdentifier.create(TOP_QNAME))
+            .build();
+
+        final var matcher = new SubtreeMatcher(filter, top);
+        assertTrue(matcher.matches(), "Simple selection match should succeed");
+    }
+
+    @Test
+    void testNestedContainmentMatch() {
+        /* Filter structure:
+         * <top xmlns="http://example.com/schema/1.2/config">
+         *   <interfaces>
+         *     <interface>
+         *       <ifName>eth0</ifName>
+         *     </interface>
+         *   </interfaces>
+         * </top>
+         */
+        final var filter = SubtreeFilter.builder(mockedContext)
+            .add(ContainmentNode.builder(new NamespaceSelection.Exact(TOP_QNAME))
+                .add(ContainmentNode.builder(new NamespaceSelection.Exact(INTERFACES_QNAME))
+                    .add(ContainmentNode.builder(new NamespaceSelection.Exact(INTERFACE_QNAME))
+                        .add(new ContentMatchNode(new NamespaceSelection.Exact(IFNAME_QNAME), "eth0"))
+                        .build())
+                    .build())
+                .build())
+            .build();
+
+        // Build data tree: <top> → <interfaces> → <interface ifName="eth0"/>
+        final var ifNameLeaf = ImmutableNodes.leafNode(IFNAME_QNAME, "eth0");
+        final var interfaceNode = ImmutableNodes.newContainerBuilder()
+            .withNodeIdentifier(NodeIdentifier.create(INTERFACE_QNAME))
+            .addChild(ifNameLeaf)
+            .build();
+        final var interfacesNode = ImmutableNodes.newContainerBuilder()
+            .withNodeIdentifier(NodeIdentifier.create(INTERFACES_QNAME))
+            .addChild(interfaceNode)
+            .build();
+        final var top = ImmutableNodes.newContainerBuilder()
+            .withNodeIdentifier(NodeIdentifier.create(TOP_QNAME))
+            .addChild(interfacesNode)
+            .build();
+
+        final var matcher = new SubtreeMatcher(filter, top);
+        assertTrue(matcher.matches(), "Nested containment match should succeed");
+    }
+
+    @Test
+    void testContentMatch() {
+        /* Filter structure:
+         * <top xmlns="http://example.com/schema/1.2/config">
+         *   <users>
+         *     <user>
+         *       <name>fred</name>
+         *     </user>
+         *   </users>
+         * </top>
+         */
+        final var filter = SubtreeFilter.builder(mockedContext)
+            .add(ContainmentNode.builder(new NamespaceSelection.Exact(TOP_QNAME))
+                .add(ContainmentNode.builder(new NamespaceSelection.Exact(USERS_QNAME))
+                    .add(ContainmentNode.builder(new NamespaceSelection.Exact(USER_QNAME))
+                        .add(new ContentMatchNode(new NamespaceSelection.Exact(NAME_QNAME), "fred"))
+                        .build())
+                    .build())
+                .build())
+            .build();
+
+        // Build data tree:
+        // <top>
+        //   <users>
+        //     <user>
+        //       <name>fred</name>
+        //     </user>
+        //   </users>
+        // </top>
+        final var nameLeaf = ImmutableNodes.leafNode(NAME_QNAME, "fred");
+        final var user = ImmutableNodes.newContainerBuilder()
+            .withNodeIdentifier(NodeIdentifier.create(USER_QNAME))
+            .addChild(nameLeaf)
+            .build();
+        final var users = ImmutableNodes.newContainerBuilder()
+            .withNodeIdentifier(NodeIdentifier.create(USERS_QNAME))
+            .addChild(user)
+            .build();
+        final var top = ImmutableNodes.newContainerBuilder()
+            .withNodeIdentifier(NodeIdentifier.create(TOP_QNAME))
+            .addChild(users)
+            .build();
+
+        final var matcher = new SubtreeMatcher(filter, top);
+        assertTrue(matcher.matches(), "Content match should succeed");
+    }
+
+    @Test
+    void testAttributeMismatch() {
+        /* Filter structure:
+         * <item xmlns="http://example.com/schema/1.2/config" company-info="value"/>
+         */
+        final var filter = SubtreeFilter.builder(mockedContext)
+            .add(SelectionNode.builder(new NamespaceSelection.Exact(TOP_QNAME))
+                .add(new AttributeMatch(new NamespaceSelection.Exact(COMPANY_INFO_QNAME), "value"))
+                .build())
+            .build();
+
+        // Build data node <top> with simulated attribute as a leaf node "company-info" with value "wrong"
+        final var attrLeaf = ImmutableNodes.leafNode(COMPANY_INFO_QNAME, "wrong");
+        final var top = ImmutableNodes.newContainerBuilder()
+            .withNodeIdentifier(NodeIdentifier.create(TOP_QNAME))
+            .addChild(attrLeaf)
+            .build();
+
+        final var matcher = new SubtreeMatcher(filter, top);
+        assertFalse(matcher.matches(), "Attribute mismatch should fail");
+    }
+
+    @Test
+    void testNonMatchingContent() {
+        /* Filter structure:
+         * <top xmlns="http://example.com/schema/1.2/config">
+         *   <users>
+         *     <user>
+         *       <name>fred</name>
+         *     </user>
+         *   </users>
+         * </top>
+         */
+        final var filter = SubtreeFilter.builder(mockedContext)
+            .add(ContainmentNode.builder(new NamespaceSelection.Exact(TOP_QNAME))
+                .add(ContainmentNode.builder(new NamespaceSelection.Exact(USERS_QNAME))
+                    .add(ContainmentNode.builder(new NamespaceSelection.Exact(USER_QNAME))
+                        .add(new ContentMatchNode(new NamespaceSelection.Exact(NAME_QNAME), "fred"))
+                        .build())
+                    .build())
+                .build())
+            .build();
+
+        // Build data tree with non-matching content: <name>john</name> instead of "fred"
+        final var nameLeaf = ImmutableNodes.leafNode(NAME_QNAME, "john");
+        final var user = ImmutableNodes.newContainerBuilder()
+            .withNodeIdentifier(NodeIdentifier.create(USER_QNAME))
+            .addChild(nameLeaf)
+            .build();
+        final var users = ImmutableNodes.newContainerBuilder()
+            .withNodeIdentifier(NodeIdentifier.create(USERS_QNAME))
+            .addChild(user)
+            .build();
+        final var top = ImmutableNodes.newContainerBuilder()
+            .withNodeIdentifier(NodeIdentifier.create(TOP_QNAME))
+            .addChild(users)
+            .build();
+
+        final var matcher = new SubtreeMatcher(filter, top);
+        assertFalse(matcher.matches(), "Non-matching content should fail");
+    }
+}
+
