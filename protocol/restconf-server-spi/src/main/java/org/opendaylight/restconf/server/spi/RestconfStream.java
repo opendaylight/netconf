1f798a25b35a1b795be2392c9d91708f0177d490
Matej Sramcik <matej.sramcik@pantheon.tech>
2025-03-28 16:01:54 +0100

    [WIP] Introduce receiver class
    Introduction of RestconfStream.Receiver interface
    
    JIRA: NETCONF-1457
    Change-Id: I66eabf17fcc694a8ed444062c36d41e7226c7706
    Signed-off-by: Matej Sramcik <matej.sramcik@pantheon.tech>

--- protocol/restconf-server-spi/src/main/java/org/opendaylight/restconf/server/spi/RestconfStream.java
+++ protocol/restconf-server-spi/src/main/java/org/opendaylight/restconf/server/spi/RestconfStream.java
@@ -21,6 +21,7 @@ import java.lang.invoke.VarHandle;
 import java.net.URI;
 import java.time.Instant;
 import java.util.Set;
+import java.util.concurrent.atomic.AtomicLong;
 import java.util.regex.Pattern;
 import javax.xml.xpath.XPathExpressionException;
 import org.eclipse.jdt.annotation.NonNull;
@@ -363,6 +364,50 @@ public final class RestconfStream<T> {
             public XPathDefinition {
                 requireNonNull(xpath);
             }
+        }
+    }
+
+    public interface Receiver {
+        /**
+         * Increments the sent-event-records counter and writes the updated value to the MD-SAL datastore.
+         */
+        void updateSentEventRecord();
+
+        /**
+         * Increments the excluded-event-records counter and writes the updated value to the MD-SAL datastore.
+         */
+        void updateExcludedEventRecord();
+
+        /**
+         * Returns the {@code receiver state}.
+         */
+        State state();
+
+        /**
+         * Returns the {@code receiver name}.
+         */
+        String receiverName();
+
+        /**
+         * Returns the {@code subscription id}.
+         */
+        String subscriptionId();
+
+        void setState(final State newState);
+
+        /**
+         * Returns the {@code sent event counter}.
+         */
+        AtomicLong sentEventCounter();
+
+        /**
+         * Returns the {@code excluded event counter}.
+         */
+        AtomicLong excludedEventCounter();
+
+        enum State {
+            ACTIVE,
+            SUSPENDED,
         }
     }
 
