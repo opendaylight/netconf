afd57b55d81d147a7ab89f67e238200b1101acd7
Yaroslav Lastivka <yaroslav.lastivka@pantheon.tech>
2025-04-01 11:10:29 +0300

    Implement (databind) SubtreeFilterWriter
    JIRA: NETCONF-1455
    Change-Id: I064bda642b12f3eada96b8a9930c190befa3640b
    Signed-off-by: Yaroslav Lastivka <yaroslav.lastivka@pantheon.tech>

--- protocol/databind/src/main/java/org/opendaylight/netconf/databind/subtree/SubtreeFilterWriter.java
+++ protocol/databind/src/main/java/org/opendaylight/netconf/databind/subtree/SubtreeFilterWriter.java
@@ -13,12 +13,87 @@ import org.eclipse.jdt.annotation.NonNullByDefault;
 
 @NonNullByDefault
 final class SubtreeFilterWriter {
-    private SubtreeFilterWriter() {
+    SubtreeFilterWriter() {
         // Hidden on purpose
     }
 
     static void writeSubtreeFilter(final XMLStreamWriter writer, final SubtreeFilter filter) throws XMLStreamException {
-        // FIXME: implement this
-        throw new UnsupportedOperationException();
+        final var prefixes = Prefixes.of(filter);
+        // Write <filter type="subtree"> element
+        writer.writeStartElement("filter");
+        writer.writeAttribute("type", "subtree");
+
+        // Declare all namespaces
+        for (final var entry : prefixes.getNamespaceToPrefixMap().entrySet()) {
+            writer.setPrefix(entry.getValue(), entry.getKey());
+            writer.writeNamespace(entry.getValue(), entry.getKey());
+        }
+
+        writeSiblings(writer, prefixes, filter);
+
+        writer.writeEndElement(); // </filter>
     }
+
+    private static void writeSiblings(final XMLStreamWriter writer, final Prefixes prefixes, final SiblingSet siblings)
+            throws XMLStreamException {
+        for (final var content : siblings.contentMatches()) {
+            writeContentMatch(writer, prefixes, content);
+        }
+
+        for (final var selection : siblings.selections()) {
+            writeSelection(writer, prefixes, selection);
+        }
+
+        for (final var containment : siblings.containments()) {
+            writeContainment(writer, prefixes, containment);
+        }
+    }
+
+    private static void writeContentMatch(final XMLStreamWriter writer, final Prefixes prefixes,
+            final ContentMatchNode content) throws XMLStreamException {
+        switch (content.selection()) {
+            case NamespaceSelection.Exact(var qName) -> {
+                final var prefix = prefixes.lookUpPrefix(qName.getNamespace().toString());
+                writer.writeStartElement(prefix, qName.getLocalName(), qName.getNamespace().toString());
+            }
+            case NamespaceSelection.Wildcard(var name, var ignored) -> writer.writeStartElement(name.getLocalName());
+        }
+        writer.writeCharacters(String.valueOf(content.value()));
+        writer.writeEndElement();
+    }
+
+    private static void writeSelection(final XMLStreamWriter writer, final Prefixes prefixes,
+        final SelectionNode selection) throws XMLStreamException {
+        switch (selection.selection()) {
+            case NamespaceSelection.Exact(var qName) -> {
+                final var prefix = prefixes.lookUpPrefix(qName.getNamespace().toString());
+                writer.writeStartElement(prefix, qName.getLocalName(), qName.getNamespace().toString());
+            }
+            case NamespaceSelection.Wildcard(var name, var ignored) -> writer.writeStartElement(name.getLocalName());
+        }
+
+        for (var attr : selection.attributeMatches()) {
+            final var sel = attr.selection();
+            final var prefix = prefixes.lookUpPrefix(sel.qname().getNamespace().toString());
+            writer.writeAttribute(prefix, sel.qname().getNamespace().toString(), sel.qname().getLocalName(),
+                attr.value().toString());
+        }
+
+        writer.writeEndElement();
+    }
+
+    private static void writeContainment(final XMLStreamWriter writer, final Prefixes prefixes,
+        final ContainmentNode containment) throws XMLStreamException {
+        switch (containment.selection()) {
+            case NamespaceSelection.Exact(var qName) -> {
+                final var prefix = prefixes.lookUpPrefix(qName.getNamespace().toString());
+                writer.writeStartElement(prefix, qName.getLocalName(), qName.getNamespace().toString());
+            }
+            case NamespaceSelection.Wildcard(var qName, var ignored) -> writer.writeStartElement(qName.getLocalName());
+        }
+
+        writeSiblings(writer, prefixes, containment);
+        writer.writeEndElement();
+    }
+
 }
