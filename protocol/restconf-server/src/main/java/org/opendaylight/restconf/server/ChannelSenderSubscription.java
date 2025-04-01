f2fd1a7c158019820b930b77198ee91e1b13d918
Matej Sramcik <matej.sramcik@pantheon.tech>
2025-04-01 11:22:10 +0200

    Refactor receiverHolder class
    Refactored holder to implementation of receiver. Receiver is used in
    Subscription class instead of just receiverName so sentEventCounter
    and excludedEventCounter are accessible from subscription.
    
    JIRA: NETCONF-1457
    Change-Id: I41b4224aff598a7b958879c3e160499edfa08ade
    Signed-off-by: Matej Sramcik <matej.sramcik@pantheon.tech>
    Signed-off-by: Ivan Hrasko <ivan.hrasko@pantheon.tech>

--- protocol/restconf-server/src/main/java/org/opendaylight/restconf/server/ChannelSenderSubscription.java
+++ protocol/restconf-server/src/main/java/org/opendaylight/restconf/server/ChannelSenderSubscription.java
@@ -7,25 +7,28 @@
  */
 package org.opendaylight.restconf.server;
 
-import org.opendaylight.restconf.server.spi.ReceiverHolder;
+import java.util.List;
+import org.opendaylight.restconf.server.spi.RestconfStream;
 import org.opendaylight.restconf.server.spi.RestconfStream.Sender;
 
 /**
  * A {@link Sender} that manages subscription event streams over HTTP/1.
  */
 public final class ChannelSenderSubscription extends AbstractChannelSender {
-    private final ReceiverHolder receiver;
+    private final List<RestconfStream.Receiver> receivers;
 
-    public ChannelSenderSubscription(final int sseMaximumFragmentLength, final ReceiverHolder receiver) {
+    public ChannelSenderSubscription(final int sseMaximumFragmentLength, final List<RestconfStream.Receiver> receiver) {
         super(sseMaximumFragmentLength);
-        this.receiver = receiver;
+        this.receivers = receiver;
     }
 
     @Override
     public void sendDataMessage(final String data) {
-        if (!data.isEmpty() && super.getCtx() != null) {
-            super.sendDataMessage(data);
-            receiver.updateSentEventRecord();
+        for (final var receiver : receivers) {
+            if (!data.isEmpty() && super.getCtx() != null) {
+                super.sendDataMessage(data);
+                receiver.updateSentEventRecord();
+            }
         }
     }
 }
