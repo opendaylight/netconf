f1e364841b2504921ba73e05564223a8249ceda6
Matej Sramcik <matej.sramcik@pantheon.tech>
2025-04-01 11:22:10 +0200

    Refactor receiverHolder class
    Refactored holder to implementation of receiver. Receiver is used in
    Subscription class instead of just receiverName so sentEventCounter
    and excludedEventCounter are accessible from subscription.
    
    JIRA: NETCONF-1457
    Change-Id: I41b4224aff598a7b958879c3e160499edfa08ade
    Signed-off-by: Matej Sramcik <matej.sramcik@pantheon.tech>
    Signed-off-by: Ivan Hrasko <ivan.hrasko@pantheon.tech>

--- protocol/restconf-server-spi/src/main/java/org/opendaylight/restconf/server/spi/ForwardingRestconfStreamSubscription.java
+++ protocol/restconf-server-spi/src/main/java/org/opendaylight/restconf/server/spi/ForwardingRestconfStreamSubscription.java
@@ -10,6 +10,7 @@ package org.opendaylight.restconf.server.spi;
 import static java.util.Objects.requireNonNull;
 
 import com.google.common.base.MoreObjects.ToStringHelper;
+import java.util.List;
 import org.eclipse.jdt.annotation.NonNull;
 import org.opendaylight.restconf.server.api.TransportSession;
 import org.opendaylight.restconf.server.spi.RestconfStream.SubscriptionState;
@@ -33,8 +34,8 @@ public abstract non-sealed class ForwardingRestconfStreamSubscription<T extends 
     }
 
     @Override
-    public final String receiverName() {
-        return delegate.receiverName();
+    public List<RestconfStream.Receiver> receiver() {
+        return delegate.receiver();
     }
 
     @Override
---
+++ protocol/restconf-server-spi/src/main/java/org/opendaylight/restconf/server/spi/ForwardingSubscriptionReceiver.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright (c) 2025 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.restconf.server.spi;
+
+import static java.util.Objects.requireNonNull;
+
+import java.util.concurrent.atomic.AtomicLong;
+import org.eclipse.jdt.annotation.NonNull;
+import org.opendaylight.yangtools.yang.common.Uint32;
+
+public abstract non-sealed class ForwardingSubscriptionReceiver<T extends RestconfStream.Receiver>
+        implements RestconfStream.Receiver {
+    protected final @NonNull T delegate;
+
+    protected ForwardingSubscriptionReceiver(final T delegate) {
+        this.delegate = requireNonNull(delegate);
+    }
+
+    @Override
+    public void updateSentEventRecord(Uint32 id) {
+        delegate.updateSentEventRecord(id);
+    }
+
+    @Override
+    public void updateExcludedEventRecord(Uint32 id) {
+        delegate.updateExcludedEventRecord(id);
+    }
+
+    @Override
+    public String name() {
+        return delegate.name();
+    }
+
+    @Override
+    public RestconfStream.ReceiverState state() {
+        return delegate.state();
+    }
+
+    @Override
+    public AtomicLong sentEventRecords() {
+        return delegate.sentEventRecords();
+    }
+
+    @Override
+    public AtomicLong excludedEventRecords() {
+        return delegate.excludedEventRecords();
+    }
+}
