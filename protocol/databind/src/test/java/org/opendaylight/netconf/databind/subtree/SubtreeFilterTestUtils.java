c346a690ea6f7821b0d43a074f3d42a88c2d84c0
Yaroslav Lastivka <yaroslav.lastivka@pantheon.tech>
2025-04-01 11:10:29 +0300

    Implement (databind) SubtreeFilterWriter
    Add support for writing SubtreeFilter to XMLStreamWriter,
    including proper prefix handling and namespace declarations.
    Include unit tests covering exact and wildcard namespace selections.
    Implement a PrettyTree representation for SubtreeFilter that produces
    a human-readable, XML-style output with appropriate indentation and
    namespace binding information.
    
    JIRA: NETCONF-1455
    Change-Id: I064bda642b12f3eada96b8a9930c190befa3640b
    Signed-off-by: Yaroslav Lastivka <yaroslav.lastivka@pantheon.tech>
    Signed-off-by: Ivan Hrasko <ivan.hrasko@pantheon.tech>

---
+++ protocol/databind/src/test/java/org/opendaylight/netconf/databind/subtree/SubtreeFilterTestUtils.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2025 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.netconf.databind.subtree;
+
+import org.opendaylight.yangtools.yang.common.QName;
+
+abstract class SubtreeFilterTestUtils {
+    static final String NAMESPACE = "http://example.com/schema/1.2/config";
+    static final String NAMESPACE2 = "http://example.com/schema/1.2/config2";
+    static final QName TYPE_QNAME = QName.create(NAMESPACE, "type");
+    static final QName TOP_QNAME = QName.create(NAMESPACE, "top");
+    static final QName INTERFACES_QNAME = QName.create(NAMESPACE, "interfaces");
+    static final QName INTERFACE_QNAME = QName.create(NAMESPACE, "interface");
+    static final QName IFNAME_QNAME = QName.create(NAMESPACE, "ifName");
+    static final QName USERS_QNAME = QName.create(NAMESPACE, "users");
+    static final QName USER_QNAME = QName.create(NAMESPACE, "user");
+    static final QName NAME_QNAME = QName.create(NAMESPACE, "name");
+    static final QName COMPANY_INFO_QNAME = QName.create(NAMESPACE, "company-info");
+    static final String REVISION = "2025-03-31";
+    static final String REVISION2 = "2025-04-01";
+}
