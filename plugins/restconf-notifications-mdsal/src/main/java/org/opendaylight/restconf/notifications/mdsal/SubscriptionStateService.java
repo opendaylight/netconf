fb14e88a38072f1411c9de404756ed3537b63b03
Robert Varga <robert.varga@pantheon.tech>
2025-04-11 01:19:01 +0200

    Remove unneeded locals
    We are using a fluent builder, hence we can just pass the resulting
    object without using a local variable.
    
    Change-Id: Ia375a877d02f83cdc4e771612db3727a19ce47f1
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- plugins/restconf-notifications-mdsal/src/main/java/org/opendaylight/restconf/notifications/mdsal/SubscriptionStateService.java
+++ plugins/restconf-notifications-mdsal/src/main/java/org/opendaylight/restconf/notifications/mdsal/SubscriptionStateService.java
@@ -7,6 +7,8 @@
  */
 package org.opendaylight.restconf.notifications.mdsal;
 
+import static java.util.Objects.requireNonNull;
+
 import com.google.common.util.concurrent.ListenableFuture;
 import java.time.Instant;
 import javax.inject.Inject;
@@ -114,7 +116,7 @@ public class SubscriptionStateService {
     @Inject
     @Activate
     public SubscriptionStateService(@Reference final DOMNotificationPublishService publishService) {
-        this.publishService = publishService;
+        this.publishService = requireNonNull(publishService);
     }
 
     /**
@@ -211,11 +213,10 @@ public class SubscriptionStateService {
     private ListenableFuture<?> sendStateNotification(final Instant eventTime, final Uint32 id, final State state)
             throws InterruptedException {
         LOG.info("Publishing {} notification for ID: {}", state, id);
-        var node = ImmutableNodes.newContainerBuilder()
+        return sendNotification(eventTime, ImmutableNodes.newContainerBuilder()
             .withNodeIdentifier(state.nodeId)
             .withChild(ImmutableNodes.leafNode(ID_NODEID, id))
-            .build();
-        return sendNotification(eventTime, node);
+            .build());
     }
 
     /**
@@ -224,12 +225,11 @@ public class SubscriptionStateService {
     private ListenableFuture<?> sendErrorStateNotification(final Instant eventTime, final Uint32 id,
             final QName errorReason, final State state) throws InterruptedException {
         LOG.info("Publishing {} notification for ID: {} with error ID: {}", state, id, errorReason);
-        var node = ImmutableNodes.newContainerBuilder()
+        return sendNotification(eventTime, ImmutableNodes.newContainerBuilder()
             .withNodeIdentifier(state.nodeId)
             .withChild(ImmutableNodes.leafNode(ID_NODEID, id))
             .withChild(ImmutableNodes.leafNode(REASON_NODEID, errorReason))
-            .build();
-        return sendNotification(eventTime, node);
+            .build());
     }
 
     /**
@@ -237,7 +237,6 @@ public class SubscriptionStateService {
      */
     private ListenableFuture<?> sendNotification(final Instant eventTime, final ContainerNode node)
             throws InterruptedException {
-        final var notification = new DOMNotificationEvent.Rfc6020(node, eventTime);
-        return publishService.putNotification(notification);
+        return publishService.putNotification(new DOMNotificationEvent.Rfc6020(node, eventTime));
     }
 }
