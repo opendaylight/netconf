cec17bdf97ebfb21d8dfd16c07ad4c3b880b58f3
Robert Varga <robert.varga@pantheon.tech>
2025-05-05 20:45:18 +0200

    Fix format string
    RequestException operates on String.format() templates, not on Logger
    message strings.
    
    JIRA: NETCONF-714
    Change-Id: I0dd65137b91669541db87aceba27175557c1e3f9
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- plugins/restconf-server-mdsal/src/main/java/org/opendaylight/restconf/server/mdsal/MdsalRestconfStreamRegistry.java
+++ plugins/restconf-server-mdsal/src/main/java/org/opendaylight/restconf/server/mdsal/MdsalRestconfStreamRegistry.java
@@ -388,7 +388,7 @@ public final class MdsalRestconfStreamRegistry extends AbstractRestconfStreamReg
                 .createXMLStreamReader(new ByteArrayInputStream(writer.toString().getBytes(StandardCharsets.UTF_8))));
         } catch (IOException | XMLStreamException e) {
             LOG.error("Failed to parse anydata to subtree filter", e);
-            throw new RequestException("Failed to parse anydata filter {} to subtree filter", filter, e);
+            throw new RequestException("Failed to parse anydata filter %s to subtree filter", filter, e);
         }
         return new SubtreeEventStreamFilter(databindFilter);
     }
