0fed8f3719455c23b76979aaa9ee4ebec1b7b049
Robert Varga <robert.varga@pantheon.tech>
2025-05-06 01:26:36 +0200

    Fix updateReceiver()
    We have a bug here, as we are passing Subscription.QNAME as identifier
    of an individual receiver.
    
    Fix that and use a simple YangInstanceIdentifier.of(), taking advantage
    of the LeafNode.name()/path invariant.
    
    JIRA: NETCONF-1449
    Change-Id: I5364a9a7cf3bc103cd1bc717719076c52cf0f6b3
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- plugins/restconf-server-mdsal/src/main/java/org/opendaylight/restconf/server/mdsal/MdsalRestconfStreamRegistry.java
+++ plugins/restconf-server-mdsal/src/main/java/org/opendaylight/restconf/server/mdsal/MdsalRestconfStreamRegistry.java
@@ -282,34 +282,24 @@ public final class MdsalRestconfStreamRegistry extends AbstractRestconfStreamReg
     @Override
     public ListenableFuture<Void> updateReceiver(final ReceiverHolder receiver, final long counter,
             final ReceiverHolder.RecordType recordType) {
+        final var counterLeaf = switch (recordType) {
+            case SENT_EVENT_RECORDS ->
+                ImmutableNodes.leafNode(SENT_EVENT_RECORDS_NODEID, Uint64.valueOf(receiver.sentEventCounter().get()));
+            case EXCLUDED_EVENT_RECORDS ->
+                ImmutableNodes.leafNode(EXCLUDED_EVENT_RECORDS_NODEID, Uint64.valueOf(counter));
+        };
+
         // Now issue a merge operation
-        final var subscriptionId = receiver.subscriptionId();
-        final var sentEventIid = YangInstanceIdentifier.builder()
-            .node(SUBSCRIPTIONS_NODEID)
-            .node(SUBSCRIPTION_NODEID)
-            .node(NodeIdentifierWithPredicates.of(Subscription.QNAME, SubscriptionUtil.QNAME_ID,
-                Uint32.valueOf(subscriptionId)))
-            .node(RECEIVERS_NODEID)
-            .node(RECEIVER_NODEID)
-            .node(NodeIdentifierWithPredicates.of(Subscription.QNAME, SubscriptionUtil.QNAME_RECEIVER_NAME,
-                receiver.receiverName()));
-
-        final LeafNode<Uint64> counterValue;
-        switch (recordType) {
-            case SENT_EVENT_RECORDS -> {
-                sentEventIid.node(SENT_EVENT_RECORDS_NODEID);
-                counterValue = ImmutableNodes.leafNode(SENT_EVENT_RECORDS_NODEID,
-                    Uint64.valueOf(receiver.sentEventCounter().get()));
-            }
-            case EXCLUDED_EVENT_RECORDS -> {
-                sentEventIid.node(EXCLUDED_EVENT_RECORDS_NODEID);
-                counterValue = ImmutableNodes.leafNode(EXCLUDED_EVENT_RECORDS_NODEID, Uint64.valueOf(counter));
-            }
-            default -> throw new IllegalArgumentException("Unknown record type: " + recordType);
-        }
-
         final var tx = dataBroker.newWriteOnlyTransaction();
-        tx.merge(LogicalDatastoreType.OPERATIONAL, sentEventIid.build(), counterValue);
+        tx.merge(LogicalDatastoreType.OPERATIONAL, YangInstanceIdentifier.of(
+            SUBSCRIPTIONS_NODEID,
+            SUBSCRIPTION_NODEID,
+            NodeIdentifierWithPredicates.of(Subscription.QNAME,
+                SubscriptionUtil.QNAME_ID, Uint32.valueOf(receiver.subscriptionId())),
+            RECEIVERS_NODEID,
+            RECEIVER_NODEID,
+            NodeIdentifierWithPredicates.of(Receiver.QNAME, NAME_QNAME, receiver.receiverName()),
+            counterLeaf.name()), counterLeaf);
         return tx.commit().transform(unused -> null, MoreExecutors.directExecutor());
     }
 
