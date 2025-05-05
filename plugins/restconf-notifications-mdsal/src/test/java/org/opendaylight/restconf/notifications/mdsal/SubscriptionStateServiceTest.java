f545f48ef92c51b3c9d2ad0925e51ffa3f052431
Oleksandr Zharov <oleksandr.zharov@pantheon.tech>
2025-05-05 13:51:45 +0200

    Refactor subscription state service
    Changed SubscriptionStateService mathod to only return
    state change notification without publishing it.
    // FIXME having it as separete calss is too much - it
    can be intagrated in the subscription class.
    
    JIRA: NETCONF-1465
    Change-Id: I3ad32acec8ac79524e9b9afb5326bac5d3d2f4d6
    Signed-off-by: Oleksandr Zharov <oleksandr.zharov@pantheon.tech>

--- plugins/restconf-notifications-mdsal/src/test/java/org/opendaylight/restconf/notifications/mdsal/SubscriptionStateServiceTest.java
+++ plugins/restconf-notifications-mdsal/src/test/java/org/opendaylight/restconf/notifications/mdsal/SubscriptionStateServiceTest.java
@@ -9,25 +9,11 @@ package org.opendaylight.restconf.notifications.mdsal;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 
-import java.time.Instant;
-import java.time.LocalDateTime;
-import java.time.Month;
-import java.time.OffsetDateTime;
-import java.time.ZoneOffset;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.EnumSource;
-import org.mockito.ArgumentCaptor;
-import org.mockito.Captor;
-import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.opendaylight.mdsal.dom.api.DOMNotification;
-import org.opendaylight.mdsal.dom.api.DOMNotificationPublishService;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.subscribed.notifications.rev190909.EncodeXml$I;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.subscribed.notifications.rev190909.SubscriptionCompleted;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.subscribed.notifications.rev190909.SubscriptionModified;
@@ -35,15 +21,13 @@ import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.subscribed.
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.subscribed.notifications.rev190909.SubscriptionSuspended;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.subscribed.notifications.rev190909.SubscriptionTerminated;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.subscribed.notifications.rev190909.SubscriptionTerminatedReason;
-import org.opendaylight.yangtools.util.concurrent.FluentFutures;
 import org.opendaylight.yangtools.yang.common.QName;
 import org.opendaylight.yangtools.yang.common.Uint32;
 import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.NodeIdentifier;
+import org.opendaylight.yangtools.yang.data.api.schema.ContainerNode;
 
 @ExtendWith(MockitoExtension.class)
 class SubscriptionStateServiceTest {
-    private static final Instant EVENT_TIME =
-        OffsetDateTime.of(LocalDateTime.of(2024, Month.OCTOBER, 30, 12, 34, 56), ZoneOffset.UTC).toInstant();
     private static final Uint32 ID = Uint32.valueOf(2147483648L);
     private static final String STOP_TIME = "2024-10-30T12:34:56Z";
     private static final String STREAM_NAME = "NETCONF";
@@ -54,18 +38,6 @@ class SubscriptionStateServiceTest {
         org.opendaylight.yang.svc.v1.urn.ietf.params.xml.ns.yang.ietf.restconf.subscribed.notifications.rev191117
             .YangModuleInfoImpl.qnameOf("uri"));
 
-    @Mock
-    private DOMNotificationPublishService mockPublishService;
-    @Captor
-    private ArgumentCaptor<DOMNotification> captor;
-
-    private SubscriptionStateService notifications;
-
-    @BeforeEach
-    void setUp() {
-        notifications = new SubscriptionStateService(mockPublishService);
-    }
-
     enum SubscriptionType {
         MODIFIED, COMPLETED, RESUMED, TERMINATED, SUSPENDED
     }
@@ -73,50 +45,48 @@ class SubscriptionStateServiceTest {
     @ParameterizedTest
     @EnumSource(SubscriptionType.class)
     void testSubscriptionEvents(final SubscriptionType type) throws InterruptedException {
-        when(mockPublishService.putNotification(any())).thenReturn(FluentFutures.immediateNullFluentFuture());
+        final ContainerNode notificationNode;
 
         final QName eventQName = switch (type) {
             case MODIFIED -> {
-                notifications.subscriptionModified(EVENT_TIME, ID, STREAM_NAME, ENCODING, null, STOP_TIME, URI);
+                notificationNode = SubscriptionStateService.subscriptionModified(ID, STREAM_NAME, ENCODING, null,
+                    STOP_TIME, URI);
                 yield SubscriptionModified.QNAME;
             }
             case COMPLETED -> {
-                notifications.subscriptionCompleted(EVENT_TIME, ID);
+                notificationNode = SubscriptionStateService.subscriptionCompleted(ID);
                 yield SubscriptionCompleted.QNAME;
             }
             case RESUMED -> {
-                notifications.subscriptionResumed(EVENT_TIME, ID);
+                notificationNode = SubscriptionStateService.subscriptionResumed(ID);
                 yield SubscriptionResumed.QNAME;
             }
             case TERMINATED -> {
-                notifications.subscriptionTerminated(EVENT_TIME, ID, ERROR_REASON);
+                notificationNode = SubscriptionStateService.subscriptionTerminated(ID, ERROR_REASON);
                 yield SubscriptionTerminated.QNAME;
             }
             case SUSPENDED -> {
-                notifications.subscriptionSuspended(EVENT_TIME, ID, ERROR_REASON);
+                notificationNode = SubscriptionStateService.subscriptionSuspended(ID, ERROR_REASON);
                 yield SubscriptionSuspended.QNAME;
             }
         };
-
-        verify(mockPublishService).putNotification(captor.capture());
-        final var notification = captor.getValue().getBody();
-        assertNotNull(notification);
-        assertEquals(ID, notification.getChildByArg(
+        assertNotNull(notificationNode);
+        assertEquals(ID, notificationNode.getChildByArg(
             new NodeIdentifier(QName.create(eventQName, "id"))).body());
 
         switch (type) {
             case null -> throw new NullPointerException();
             case MODIFIED -> {
-                assertEquals(ENCODING, notification.getChildByArg(
+                assertEquals(ENCODING, notificationNode.getChildByArg(
                     new NodeIdentifier(QName.create(eventQName, "encoding"))).body());
-                assertEquals(STREAM_NAME, notification.getChildByArg(
+                assertEquals(STREAM_NAME, notificationNode.getChildByArg(
                     new NodeIdentifier(QName.create(eventQName, "stream"))).body());
-                assertEquals(STOP_TIME, notification.getChildByArg(
+                assertEquals(STOP_TIME, notificationNode.getChildByArg(
                     new NodeIdentifier(QName.create(eventQName, "stop-time"))).body());
-                assertEquals(URI, notification.getChildByArg(URI_NODEID).body());
+                assertEquals(URI, notificationNode.getChildByArg(URI_NODEID).body());
             }
             case SUSPENDED, TERMINATED ->
-                assertEquals(ERROR_REASON, notification.getChildByArg(
+                assertEquals(ERROR_REASON, notificationNode.getChildByArg(
                     new NodeIdentifier(QName.create(eventQName, "reason"))).body());
             case COMPLETED, RESUMED -> {
                 // Nothing else
