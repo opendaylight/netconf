b26a37563be9f870e43b42d9b6ca951f87416a6e
Robert Varga <robert.varga@pantheon.tech>
2025-05-07 22:45:12 +0200

    Add TransportSession.description()
    We need to generate reasonable names receiver names for dynamic
    subscriptions: it is the only indicator where that subscription is going
    after all.
    
    Introduce TransportSession.Description, returned from
    TransportSession.description(), which provides a reasonable amount of
    detail, including remote address.
    
    JIRA: NETCONF-1449
    Change-Id: I930368c734b4c320bfbb90250486bdecf4379ab9
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- protocol/restconf-server-api/src/main/java/org/opendaylight/restconf/server/api/TransportSession.java
+++ protocol/restconf-server-api/src/main/java/org/opendaylight/restconf/server/api/TransportSession.java
@@ -8,7 +8,9 @@
 package org.opendaylight.restconf.server.api;
 
 import com.google.common.annotations.Beta;
+import java.net.SocketAddress;
 import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.opendaylight.yangtools.concepts.Immutable;
 import org.opendaylight.yangtools.concepts.Registration;
 
 /**
@@ -18,6 +20,68 @@ import org.opendaylight.yangtools.concepts.Registration;
 @Beta
 @NonNullByDefault
 public interface TransportSession {
+    /**
+     * A description of a {@link TransportSession}.
+     *
+     * <p>Beware: strings returned by implementations of this contract are NOT part of their contract and SHOULD NOT be
+     * should be used by external tools. Notably an implementation can change the format of the returned string between
+     * any releases.
+     *
+     * <p>Implementations are encouraged to use well-known transport details and take semantic versioning into
+     * consideration when changing these formats.
+     *
+     * @apiNote This type uses strings on purpose. We could have used {@link SocketAddress} for
+     *          {@link #transportRemote()}, but explicitly do not. This interface's sole purpose is to provide a
+     *          description of the details, not the details themselves.
+     */
+    interface Description extends Immutable {
+        /**
+         * Returns a description of what message protocol the transport session is using. This could be something like
+         * {@code HTTP/0.9}, {@code NETCONF/1.1} or similar.
+         *
+         * @return a description of what message protocol the transport session is using
+         */
+        String messageLayer();
+
+        /**
+         * Returns a description of what transport protocol the transport session is using. This could be something like
+         * {@code TCP}, {@code TLSv3}, {@code SSHv2} or similar.
+         *
+         * @return a description of what protocol the transport session is using
+         */
+        String transportLayer();
+
+        /**
+         * Returns a description of where the transport session is from. This could be something like the remote TCP
+         * address and source port, or similar.
+         *
+         * @return a description of where the transport session is from
+         */
+        String transportRemote();
+
+        /**
+         * Returns a short, friendly string summarizing this description. The purpose of this string is to provide
+         * something an administrator can use to reason about where this session came from. Taking the example of a
+         * HTTPS session, this would include the remote address and source port, but not details about TLS encryption,
+         * authenticated user or anything of that kind.
+         *
+         * <p>Beware: provided default implementation is only a guideline. Implementations are free to use differently-
+         * formatted strings.
+         *
+         * @return a short, friendly description of this session
+         */
+        default String toFriendlyString() {
+            return messageLayer() + " over " + transportLayer() + " from " + transportRemote();
+        }
+    }
+
+    /**
+     * Return this session's {@link Description}.
+     *
+     * @return this session's {@link Description}
+     */
+    Description description();
+
     /**
      * Register resource to be closed on session end. Implementation should guarantee all registration are closed
      * when session ends.
