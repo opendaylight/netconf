7040ade3cd784e4b78a7e5e06781b3f35b790421
Matej Sramcik <matej.sramcik@pantheon.tech>
2025-05-12 13:51:31 +0200

    Stop subscription on stop-time reached
    ScheduledExecutorService is used to schedule task to stop the
    subscription with the earliest stop time and reschedule if earlier
    one is added or current is removed.
    
    JIRA: NETCONF-1466
    Change-Id: I5835f53762fa5eb51e228177b2fdbeba505c7eb5
    Signed-off-by: Matej Sramcik <matej.sramcik@pantheon.tech>

--- protocol/restconf-server-spi/src/main/java/org/opendaylight/restconf/server/spi/RestconfStream.java
+++ protocol/restconf-server-spi/src/main/java/org/opendaylight/restconf/server/spi/RestconfStream.java
@@ -333,6 +333,8 @@ public final class RestconfStream<T> {
         @NonNullByDefault
         protected abstract void terminateImpl(ServerRequest<Empty> request, QName reason);
 
+        abstract void stopTimeReached();
+
         @Override
         public final String toString() {
             return addToStringAttributes(MoreObjects.toStringHelper(this).omitNullValues()).toString();
