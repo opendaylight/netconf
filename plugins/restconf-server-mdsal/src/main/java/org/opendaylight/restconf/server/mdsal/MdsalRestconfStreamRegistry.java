84dc3dd6fe3c6910fc2176e80e2c1c50e12b0a05
Oleksandr Zharov <oleksandr.zharov@pantheon.tech>
2025-05-13 11:37:26 +0200

    Do not write into datastore on modify subscription
    Commented out datastore write part of modify subscription until
    it is supported by yangtools.
    
    Change-Id: I4a25fbbe5b95170c30f143165fcc3090e98482a7
    Signed-off-by: Oleksandr Zharov <oleksandr.zharov@pantheon.tech>

--- plugins/restconf-server-mdsal/src/main/java/org/opendaylight/restconf/server/mdsal/MdsalRestconfStreamRegistry.java
+++ plugins/restconf-server-mdsal/src/main/java/org/opendaylight/restconf/server/mdsal/MdsalRestconfStreamRegistry.java
@@ -293,20 +293,21 @@ public final class MdsalRestconfStreamRegistry extends AbstractRestconfStreamReg
     protected ListenableFuture<Void> modifySubscriptionFilter(final Uint32 subscriptionId,
             final RestconfStream.SubscriptionFilter filter) {
 
-        final var filterNode = switch (filter) {
-            case RestconfStream.SubscriptionFilter.Reference(var filterName) ->
-                ImmutableNodes.leafNode(STREAM_FILTER_NAME_NODEID, filterName);
-            case RestconfStream.SubscriptionFilter.SubtreeDefinition(var anydata) ->
-                ImmutableNodes.newChoiceBuilder()
-                    .withNodeIdentifier(FILTER_SPEC_NODEID)
-                    .withChild(ImmutableNodes.leafNode(STREAM_SUBTREE_FILTER_NODEID, anydata))
-                    .build();
-            case RestconfStream.SubscriptionFilter.XPathDefinition(final var xpath) ->
-                ImmutableNodes.newChoiceBuilder()
-                    .withNodeIdentifier(FILTER_SPEC_NODEID)
-                    .withChild(ImmutableNodes.leafNode(STREAM_XPATH_FILTER_NODEID, xpath))
-                    .build();
-        };
+        // FIXME YANGTOOLS-1670 enable when writing filter into datastore is supported
+//        final var filterNode = switch (filter) {
+//            case RestconfStream.SubscriptionFilter.Reference(var filterName) ->
+//                ImmutableNodes.leafNode(STREAM_FILTER_NAME_NODEID, filterName);
+//            case RestconfStream.SubscriptionFilter.SubtreeDefinition(var anydata) ->
+//                ImmutableNodes.newChoiceBuilder()
+//                    .withNodeIdentifier(FILTER_SPEC_NODEID)
+//                    .withChild(ImmutableNodes.leafNode(STREAM_SUBTREE_FILTER_NODEID, anydata))
+//                    .build();
+//            case RestconfStream.SubscriptionFilter.XPathDefinition(final var xpath) ->
+//                ImmutableNodes.newChoiceBuilder()
+//                    .withNodeIdentifier(FILTER_SPEC_NODEID)
+//                    .withChild(ImmutableNodes.leafNode(STREAM_XPATH_FILTER_NODEID, xpath))
+//                    .build();
+//        };
 
         final var pathArg = subscriptionArg(subscriptionId);
         final var tx = dataBroker.newWriteOnlyTransaction();
@@ -316,10 +317,10 @@ public final class MdsalRestconfStreamRegistry extends AbstractRestconfStreamReg
                 .withChild(ImmutableNodes.leafNode(ID_NODEID, subscriptionId))
                 .withChild(ImmutableNodes.newChoiceBuilder()
                     .withNodeIdentifier(TARGET_NODEID)
-                    .withChild(ImmutableNodes.newChoiceBuilder()
-                        .withNodeIdentifier(STREAM_FILTER_NODEID)
-                        .withChild(filterNode)
-                        .build())
+//                    .withChild(ImmutableNodes.newChoiceBuilder()
+//                        .withNodeIdentifier(STREAM_FILTER_NODEID)
+//                        .withChild(filterNode)
+//                        .build())
                     .build())
                 .build());
         return tx.commit().transform(info -> {
