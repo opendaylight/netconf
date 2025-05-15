48135a6518d3bd8315d9969c0183d8f0269c0351
Oleksandr Zharov <oleksandr.zharov@pantheon.tech>
2025-05-15 14:14:39 +0200

    Add OpenApi test over leafrefs
    Added E2E test that reproduces error when resolving leafref path
    to the node that was deviated as not-supported.
    
    JIRA: NETCONF-1440
    Change-Id: I3281ea16b81e65f3daaa9d3e10e0f2729acd381e
    Signed-off-by: Oleksandr Zharov <oleksandr.zharov@pantheon.tech>

---
+++ apps/restconf-it/src/test/java/org/opendaylight/restconf/it/openapi/LeafrefTest.java
@@ -0,0 +1,117 @@
+/*
+ * Copyright (c) 2025 PANTHEON.tech s.r.o. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.restconf.it.openapi;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.net.Authenticator;
+import java.net.PasswordAuthentication;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpTimeoutException;
+import java.nio.file.Path;
+import java.time.Duration;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.opendaylight.netconf.test.tool.NetconfDeviceSimulator;
+import org.opendaylight.netconf.test.tool.config.ConfigurationBuilder;
+import org.opendaylight.netconf.topology.impl.NetconfTopologyImpl;
+
+public class LeafrefTest extends AbstractOpenApiTest {
+    private NetconfDeviceSimulator deviceSimulator;
+    private NetconfTopologyImpl topologyService;
+    private int devicePort;
+
+    @BeforeEach
+    @Override
+    void beforeEach() throws Exception {
+        super.beforeEach();
+        // topology
+        topologyService = setupTopology();
+    }
+
+    @AfterEach
+    @Override
+    void afterEach() throws Exception {
+        if (deviceSimulator != null) {
+            deviceSimulator.close();
+            deviceSimulator = null;
+        }
+        if (topologyService != null) {
+            topologyService.close();
+            topologyService = null;
+        }
+        super.afterEach();
+    }
+
+    @Test
+    public void leafrefResolvedTest() throws Exception {
+        startDeviceSimulator("target/test-classes/leafref");
+        mountDeviceJson(devicePort);
+        final var client = HttpClient.newBuilder()
+            .authenticator(new Authenticator() {
+                @Override
+                protected PasswordAuthentication getPasswordAuthentication() {
+                    return new PasswordAuthentication(
+                            USERNAME,
+                            PASSWORD.toCharArray());
+                }
+            })
+            .build();
+
+        final var headers = client.send(HttpRequest.newBuilder()
+            .GET()
+            .uri(new URI("http://" + host + API_V3_PATH + "/mounts/1"))
+            .timeout(Duration.ofSeconds(10))
+            .build(), HttpResponse.BodyHandlers.discarding());
+        assertEquals(200, headers.statusCode());
+    }
+
+    @Test
+    public void leafrefNotResolvedTest() throws Exception {
+        startDeviceSimulator("target/test-classes/leafref-deviated");
+        mountDeviceJson(devicePort);
+        final var client = HttpClient.newBuilder()
+            .authenticator(new Authenticator() {
+                @Override
+                protected PasswordAuthentication getPasswordAuthentication() {
+                    return new PasswordAuthentication(
+                            USERNAME,
+                            PASSWORD.toCharArray());
+                }
+            })
+            .build();
+
+        try {
+            client.send(HttpRequest.newBuilder()
+                .GET()
+                .uri(new URI("http://" + host + API_V3_PATH + "/mounts/1"))
+                .timeout(Duration.ofSeconds(10))
+                .build(), HttpResponse.BodyHandlers.discarding());
+        } catch (HttpTimeoutException hte) {
+            assertEquals("request timed out", hte.getMessage());
+        }
+    }
+
+    private void startDeviceSimulator(final String path) {
+        devicePort = randomBindablePort();
+        final var configBuilder = new ConfigurationBuilder()
+                .setStartingPort(devicePort)
+                .setDeviceCount(1)
+                .setSsh(true)
+                .setAuthProvider((usr, pwd) -> DEVICE_USERNAME.equals(usr) && DEVICE_PASSWORD.equals(pwd))
+                .build();
+
+        configBuilder.setSchemasDir(Path.of(path).toFile());
+        deviceSimulator = new NetconfDeviceSimulator(configBuilder);
+        deviceSimulator.start();
+    }
+}
