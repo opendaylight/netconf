ac8ce50a43ed79e2c3c3117278ee3d76bd873c68
Oleksandr Zharov <oleksandr.zharov@pantheon.tech>
2025-05-22 22:25:26 +0200

    Remove SubscriptionStateService
    Removed SubscriptionStateService in favor of private state
    notifications sent by MdsalRestconfStreamRegistry.
    
    JIRA: NETCONF-1465
    Change-Id: Iaea14f9eb3e6e421dac51c35baf02b0b5dad1213
    Signed-off-by: Oleksandr Zharov <oleksandr.zharov@pantheon.tech>

--- apps/restconf-subscription/src/test/java/org/opendaylight/restconf/subscription/DeleteSubscriptionRpcTest.java
+++ apps/restconf-subscription/src/test/java/org/opendaylight/restconf/subscription/DeleteSubscriptionRpcTest.java
@@ -27,7 +27,6 @@ import org.opendaylight.mdsal.dom.api.DOMDataBroker;
 import org.opendaylight.mdsal.dom.api.DOMDataTreeWriteTransaction;
 import org.opendaylight.netconf.databind.DatabindPath;
 import org.opendaylight.netconf.databind.RequestException;
-import org.opendaylight.restconf.notifications.mdsal.SubscriptionStateService;
 import org.opendaylight.restconf.server.api.TransportSession;
 import org.opendaylight.restconf.server.api.testlib.CompletingServerRequest;
 import org.opendaylight.restconf.server.spi.AbstractRestconfStreamSubscription;
@@ -61,8 +60,6 @@ class DeleteSubscriptionRpcTest {
     @Mock
     private DOMDataBroker dataBroker;
     @Mock
-    private SubscriptionStateService subscriptionStateService;
-    @Mock
     private DatabindPath.Rpc operationPath;
     @Mock
     private DOMDataTreeWriteTransaction writeTx;
@@ -81,7 +78,7 @@ class DeleteSubscriptionRpcTest {
 
     @BeforeEach
     void before() {
-        rpc = new DeleteSubscriptionRpc(streamRegistry, subscriptionStateService);
+        rpc = new DeleteSubscriptionRpc(streamRegistry);
     }
 
     @Disabled
