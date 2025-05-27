d82e1e71bfdc311e5afb676b346c496e5e657904
Matej Sramcik <matej.sramcik@pantheon.tech>
2025-05-27 14:51:08 +0200

    Add Subscription.toOperational()
    Added toOperational method to subscription that returns instance of
    rfc8639 Subscription generated class.
    
    JIRA: NETCONF-1448
    Change-Id: Ib19491f75bab411a3fc8811d34ff8f641f5c5d0a
    Signed-off-by: Matej Sramcik <matej.sramcik@pantheon.tech>

--- protocol/restconf-server-spi/src/main/java/module-info.java
+++ protocol/restconf-server-spi/src/main/java/module-info.java
@@ -27,4 +27,5 @@ module org.opendaylight.restconf.server.spi {
     requires static transitive org.eclipse.jdt.annotation;
     requires static com.github.spotbugs.annotations;
     requires static org.osgi.annotation.bundle;
+    requires org.opendaylight.yangtools.binding.spec;
 }
