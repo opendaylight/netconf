d1d3d6ad0963af5154324044ccf118af1f52d4a2
Matej Sramcik <matej.sramcik@pantheon.tech>
2025-05-27 14:51:08 +0200

    Add Subscription.toOperational()
    Added toOperational method to subscription that returns instance of
    rfc8639 Subscription generated class.
    
    JIRA: NETCONF-1448
    Change-Id: Ib19491f75bab411a3fc8811d34ff8f641f5c5d0a
    Signed-off-by: Matej Sramcik <matej.sramcik@pantheon.tech>

--- protocol/restconf-server-spi/src/main/java/org/opendaylight/restconf/server/spi/AbstractRestconfStreamRegistry.java
+++ protocol/restconf-server-spi/src/main/java/org/opendaylight/restconf/server/spi/AbstractRestconfStreamRegistry.java
@@ -47,11 +47,19 @@ import org.opendaylight.restconf.server.spi.RestconfStream.SubscriptionState;
 import org.opendaylight.restconf.server.spi.Subscriber.Rfc8639Subscriber;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.subscribed.notifications.rev190909.EncodeJson$I;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.subscribed.notifications.rev190909.EncodeXml$I;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.subscribed.notifications.rev190909.Encoding;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.subscribed.notifications.rev190909.SubscriptionId;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.subscribed.notifications.rev190909.filters.StreamFilter;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.subscribed.notifications.rev190909.stream.filter.elements.filter.spec.StreamSubtreeFilter;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.subscribed.notifications.rev190909.stream.filter.elements.filter.spec.StreamXpathFilter;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.subscribed.notifications.rev190909.subscriptions.SubscriptionBuilder;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.subscribed.notifications.rev190909.subscriptions.subscription.ReceiversBuilder;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.subscribed.notifications.rev190909.subscriptions.subscription.receivers.Receiver;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.subscribed.notifications.rev190909.subscriptions.subscription.receivers.Receiver.State;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.subscribed.notifications.rev190909.subscriptions.subscription.receivers.ReceiverBuilder;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.yang.types.rev130715.DateAndTime;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.yang.types.rev130715.ZeroBasedCounter64;
+import org.opendaylight.yangtools.binding.util.BindingMap;
 import org.opendaylight.yangtools.concepts.AbstractRegistration;
 import org.opendaylight.yangtools.concepts.Registration;
 import org.opendaylight.yangtools.yang.common.Empty;
@@ -91,7 +99,7 @@ public abstract class AbstractRestconfStreamRegistry implements RestconfStream.R
      * Internal implementation
      * of a <a href="https://www.rfc-editor.org/rfc/rfc8639#section-2.4">dynamic subscription</a>.
      */
-    private final class DynSubscription extends AbstractRestconfStreamSubscription {
+    final class DynSubscription extends AbstractRestconfStreamSubscription {
         private final Set<Rfc8639Subscriber<?>> receivers = ConcurrentHashMap.newKeySet();
 
         private @Nullable EventStreamFilter filter;
@@ -279,6 +287,38 @@ public abstract class AbstractRestconfStreamRegistry implements RestconfStream.R
         @Override
         protected @Nullable EventStreamFilter filter() {
             return filter;
+        }
+
+        public org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.subscribed.notifications.rev190909
+                .subscriptions.Subscription toOperational() {
+
+            final Encoding encoding;
+            final var encodingName = encoding().getLocalName();
+
+            if (EncodeXml$I.QNAME.getLocalName().equals(encodingName)) {
+                encoding = EncodeXml$I.VALUE;
+            } else if (EncodeJson$I.QNAME.getLocalName().equals(encodingName)) {
+                encoding = EncodeJson$I.VALUE;
+            } else {
+                throw new IllegalArgumentException("Invalid encoding: " + encodingName);
+            }
+
+            final var subscriptionBuilder = new SubscriptionBuilder()
+                .setId(new SubscriptionId(id()))
+                .setEncoding(encoding)
+                .setReceivers(new ReceiversBuilder()
+                    .setReceiver(receivers.stream().map(receiver -> new ReceiverBuilder()
+                        .setName(receiver.receiverName())
+                        .setState(State.Active)
+                        .setSentEventRecords(new ZeroBasedCounter64(receiver.sentEventRecords()))
+                        .setExcludedEventRecords(new ZeroBasedCounter64(receiver.excludedEventRecords()))
+                        .build()).collect(BindingMap.toMap()))
+                    .build());
+
+            if (stopTime() != null) {
+                subscriptionBuilder.setStopTime(new DateAndTime(stopTime().toString()));
+            }
+            return subscriptionBuilder.build();
         }
     }
 
