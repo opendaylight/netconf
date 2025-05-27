d0954d8170d2d2aa2df95ada6dc4779d47c7ac70
Matej Sramcik <matej.sramcik@pantheon.tech>
2025-05-27 14:51:08 +0200

    Add Subscription.toOperational()
    Added toOperational method to subscription that returns instance of
    rfc8639 Subscription generated class.
    
    JIRA: NETCONF-1448
    Change-Id: Ib19491f75bab411a3fc8811d34ff8f641f5c5d0a
    Signed-off-by: Matej Sramcik <matej.sramcik@pantheon.tech>
    Signed-off-by: Ivan Hrasko <ivan.hrasko@pantheon.tech>

--- protocol/restconf-server-spi/src/main/java/org/opendaylight/restconf/server/spi/RestconfStream.java
+++ protocol/restconf-server-spi/src/main/java/org/opendaylight/restconf/server/spi/RestconfStream.java
@@ -20,6 +20,7 @@ import java.lang.invoke.MethodHandles;
 import java.lang.invoke.VarHandle;
 import java.net.URI;
 import java.time.Instant;
+import java.util.List;
 import java.util.Set;
 import java.util.regex.Pattern;
 import javax.xml.xpath.XPathExpressionException;
@@ -236,7 +237,6 @@ public final class RestconfStream<T> {
     /**
      * A handle to a RFC8639 subscription.
      */
-    // TODO: a .toOperational() should result in the equivalent MapEntryNode equivalent of a Binding Subscription
     @Beta
     public abstract static class Subscription {
         @SuppressFBWarnings(value = "UWF_UNWRITTEN_FIELD",
@@ -308,6 +308,13 @@ public final class RestconfStream<T> {
         public abstract @Nullable Instant stopTime();
 
         /**
+         * Returns the list of receivers names.
+         *
+         * @return the list of receivers
+         */
+        public abstract List<String> receiversNames();
+
+        /**
          * Add a new receiver with this subscription, forwarding events to the specified {@link Sender}.
          *
          * @param request a {@link ServerRequest} completing with a {@link Registration} of the receiver
@@ -340,6 +347,9 @@ public final class RestconfStream<T> {
         public final String toString() {
             return addToStringAttributes(MoreObjects.toStringHelper(this).omitNullValues()).toString();
         }
+
+        public abstract org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.subscribed.notifications.rev190909
+            .subscriptions.Subscription toOperational();
 
         @NonNullByDefault
         protected ToStringHelper addToStringAttributes(final ToStringHelper helper) {
