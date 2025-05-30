76704e85f24a9fb72c09d7c435a5e19d8c2ae16f
Oleksandr Zharov <oleksandr.zharov@pantheon.tech>
2025-05-30 19:58:32 +0200

    Expand filters tests with reading data
    Expanded IMDS filters tests with read operation and checking
    stored filters.
    
    JIRA: NETCONF-1467
    Change-Id: I58cfc5cce8d7cf98d6eb9156a28ca8ad35d407de
    Signed-off-by: Oleksandr Zharov <oleksandr.zharov@pantheon.tech>

--- apps/restconf-it/src/test/java/org/opendaylight/restconf/it/subscription/FilterWritingTest.java
+++ apps/restconf-it/src/test/java/org/opendaylight/restconf/it/subscription/FilterWritingTest.java
@@ -8,17 +8,28 @@
 package org.opendaylight.restconf.it.subscription;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import io.netty.handler.codec.http.HttpMethod;
 import io.netty.handler.codec.http.HttpResponseStatus;
+import java.nio.charset.StandardCharsets;
+import org.custommonkey.xmlunit.XMLUnit;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.opendaylight.restconf.api.MediaTypes;
+import org.skyscreamer.jsonassert.JSONAssert;
+import org.skyscreamer.jsonassert.JSONCompareMode;
 
 public class FilterWritingTest extends AbstractNotificationSubscriptionTest {
+    @BeforeAll
+    static void setUp() {
+        XMLUnit.setIgnoreWhitespace(true);
+    }
+
     @Disabled("FIXME fails to find toasterOutOfBread under stream-subtree-filter while POSTing JSON filter")
     @Test
-    void writeJsonSubtreeFilterTest() throws Exception {
+    void writeJsonReadJsonSubtreeFilterTest() throws Exception {
         // create filter
         final var postFilterResponse = invokeRequest(HttpMethod.POST,
             "/restconf/data/ietf-subscribed-notifications:filters",
@@ -32,12 +43,56 @@ public class FilterWritingTest extends AbstractNotificationSubscriptionTest {
                     }
                   }
                 }""", MediaTypes.APPLICATION_YANG_DATA_JSON);
-        // check response
         assertEquals(HttpResponseStatus.CREATED, postFilterResponse.status());
+
+        final var getFilterResponse = invokeRequest(HttpMethod.GET,
+            "/restconf/data/ietf-subscribed-notifications:filters/stream-filter=foo",
+            MediaTypes.APPLICATION_YANG_DATA_JSON, null, MediaTypes.APPLICATION_YANG_DATA_JSON);
+        final var result = getFilterResponse.content().toString(StandardCharsets.UTF_8);
+        assertEquals(HttpResponseStatus.OK, getFilterResponse.status());
+        final var expectedFilter = """
+            {
+              "ietf-subscribed-notifications:stream-filter":[{
+                "name":"foo"
+              }]
+            }""";
+        JSONAssert.assertEquals(expectedFilter, result, JSONCompareMode.LENIENT);
+    }
+
+    @Disabled("FIXME fails to find toasterOutOfBread under stream-subtree-filter while POSTing JSON filter")
+    @Test
+    void writeJsonReadXmlSubtreeFilterTest() throws Exception {
+        // create filter
+        final var postFilterResponse = invokeRequest(HttpMethod.POST,
+            "/restconf/data/ietf-subscribed-notifications:filters",
+            MediaTypes.APPLICATION_YANG_DATA_JSON,
+            """
+                {
+                  "stream-filter": {
+                    "name": "foo",
+                    "stream-subtree-filter": {
+                      "toaster:toasterOutOfBread": ""
+                    }
+                  }
+                }""", MediaTypes.APPLICATION_YANG_DATA_JSON);
+        assertEquals(HttpResponseStatus.CREATED, postFilterResponse.status());
+
+        // get filter
+        final var getFilterResponse = invokeRequest(HttpMethod.GET,
+            "/restconf/data/ietf-subscribed-notifications:filters/stream-filter=foo",
+            MediaTypes.APPLICATION_YANG_DATA_XML, null, MediaTypes.APPLICATION_YANG_DATA_XML);
+        final var result = getFilterResponse.content().toString(StandardCharsets.UTF_8);
+        assertEquals(HttpResponseStatus.OK, getFilterResponse.status());
+        final var expectedFilter = """
+            <stream-filter xmlns="urn:ietf:params:xml:ns:yang:ietf-subscribed-notifications">
+              <name>foo</name><stream-subtree-filter>
+              <toasterOutOfBread xmlns="http://netconfcentral.org/ns/toaster"></toasterOutOfBread>
+            </stream-subtree-filter></stream-filter>""";
+        assertTrue(XMLUnit.compareXML(expectedFilter, result).identical());
     }
 
     @Test
-    void writeXmlSubtreeFilterTest() throws Exception {
+    void writeXmlReadJsonSubtreeFilterTest() throws Exception {
         // create filter
         final var postFilterResponse = invokeRequest(HttpMethod.POST,
             "/restconf/data/ietf-subscribed-notifications:filters",
@@ -49,7 +104,48 @@ public class FilterWritingTest extends AbstractNotificationSubscriptionTest {
                   <toasterOutOfBread xmlns="http://netconfcentral.org/ns/toaster"/>
                  </stream-subtree-filter>
                 </stream-filter>""", MediaTypes.APPLICATION_YANG_DATA_JSON);
-        // check response
         assertEquals(HttpResponseStatus.CREATED, postFilterResponse.status());
+
+        final var getFilterResponse = invokeRequest(HttpMethod.GET,
+            "/restconf/data/ietf-subscribed-notifications:filters/stream-filter=foo",
+            MediaTypes.APPLICATION_YANG_DATA_JSON, null, MediaTypes.APPLICATION_YANG_DATA_JSON);
+        final var result = getFilterResponse.content().toString(StandardCharsets.UTF_8);
+        assertEquals(HttpResponseStatus.OK, getFilterResponse.status());
+        final var expectedFilter = """
+            {
+              "ietf-subscribed-notifications:stream-filter":[{
+                "name":"foo"
+              }]
+            }""";
+        JSONAssert.assertEquals(expectedFilter, result, JSONCompareMode.LENIENT);
+    }
+
+    @Test
+    void writeXmlReadXmlSubtreeFilterTest() throws Exception {
+        // create filter
+        final var postFilterResponse = invokeRequest(HttpMethod.POST,
+            "/restconf/data/ietf-subscribed-notifications:filters",
+            MediaTypes.APPLICATION_YANG_DATA_XML,
+            """
+                <stream-filter xmlns="urn:ietf:params:xml:ns:yang:ietf-subscribed-notifications">
+                 <name>foo</name>
+                 <stream-subtree-filter>
+                  <toasterOutOfBread xmlns="http://netconfcentral.org/ns/toaster"/>
+                 </stream-subtree-filter>
+                </stream-filter>""", MediaTypes.APPLICATION_YANG_DATA_JSON);
+        assertEquals(HttpResponseStatus.CREATED, postFilterResponse.status());
+
+        // get filter
+        final var getFilterResponse = invokeRequest(HttpMethod.GET,
+            "/restconf/data/ietf-subscribed-notifications:filters/stream-filter=foo",
+            MediaTypes.APPLICATION_YANG_DATA_XML, null, MediaTypes.APPLICATION_YANG_DATA_XML);
+        final var result = getFilterResponse.content().toString(StandardCharsets.UTF_8);
+        assertEquals(HttpResponseStatus.OK, getFilterResponse.status());
+        final var expectedFilter = """
+            <stream-filter xmlns="urn:ietf:params:xml:ns:yang:ietf-subscribed-notifications">
+              <name>foo</name><stream-subtree-filter>
+              <toasterOutOfBread xmlns="http://netconfcentral.org/ns/toaster"></toasterOutOfBread>
+            </stream-subtree-filter></stream-filter>""";
+        assertTrue(XMLUnit.compareXML(expectedFilter, result).identical());
     }
 }
