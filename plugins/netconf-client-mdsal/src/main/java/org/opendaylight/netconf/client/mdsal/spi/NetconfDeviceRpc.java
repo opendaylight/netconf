1392fa26f2b61000466171a603db20c19bac7f36
Yaroslav Lastivka <yaroslav.lastivka@pantheon.tech>
2025-06-12 10:54:41 +0300

    Support both 2011 and 2013 base-module revisions
    When a device advertises only the 2013-09-29 revision of
    urn:ietf:params:xml:ns:netconf:base:1.0, the connector failed on the
    second (schema-aware) GET/GET-CONFIG request because the hard-coded
    QNames still referenced the 2011-06-01 revision that is no longer part
    of the EffectiveModelContext.
    
    NetconfDeviceRpc#invokeNetconf now checks whether the
    QName’s module is present in the current schema if not,
    rewrites the request QName to the latest revision
    before dispatching it to NetconfDeviceDOMRpcService.
    
    JIRA: NETCONF-1474
    Change-Id: I244420595896d99770637c97980ab618e44bcc79
    Signed-off-by: Yaroslav Lastivka <yaroslav.lastivka@pantheon.tech>

--- plugins/netconf-client-mdsal/src/main/java/org/opendaylight/netconf/client/mdsal/spi/NetconfDeviceRpc.java
+++ plugins/netconf-client-mdsal/src/main/java/org/opendaylight/netconf/client/mdsal/spi/NetconfDeviceRpc.java
@@ -7,16 +7,36 @@
  */
 package org.opendaylight.netconf.client.mdsal.spi;
 
+import static java.util.Objects.requireNonNull;
+
+import com.google.common.util.concurrent.Futures;
 import com.google.common.util.concurrent.ListenableFuture;
+import com.google.common.util.concurrent.MoreExecutors;
+import java.util.Comparator;
+import java.util.Map;
+import java.util.function.Function;
+import javax.xml.transform.dom.DOMSource;
 import org.eclipse.jdt.annotation.NonNull;
 import org.opendaylight.mdsal.dom.api.DOMRpcResult;
 import org.opendaylight.mdsal.dom.api.DOMRpcService;
+import org.opendaylight.mdsal.dom.spi.DefaultDOMRpcResult;
 import org.opendaylight.netconf.client.mdsal.api.RemoteDeviceCommunicator;
 import org.opendaylight.netconf.client.mdsal.api.RemoteDeviceServices.Rpcs;
 import org.opendaylight.netconf.client.mdsal.api.RpcTransformer;
 import org.opendaylight.yangtools.yang.common.QName;
+import org.opendaylight.yangtools.yang.common.QNameModule;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
+import org.opendaylight.yangtools.yang.data.api.schema.AnyxmlNode;
+import org.opendaylight.yangtools.yang.data.api.schema.ChoiceNode;
 import org.opendaylight.yangtools.yang.data.api.schema.ContainerNode;
+import org.opendaylight.yangtools.yang.data.api.schema.DataContainerChild;
+import org.opendaylight.yangtools.yang.data.api.schema.LeafNode;
+import org.opendaylight.yangtools.yang.data.api.schema.MapEntryNode;
+import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNode;
+import org.opendaylight.yangtools.yang.data.spi.node.ImmutableNodes;
 import org.opendaylight.yangtools.yang.model.api.EffectiveModelContext;
+import org.opendaylight.yangtools.yang.model.api.Module;
+import org.opendaylight.yangtools.yang.model.api.stmt.ModuleEffectiveStatement;
 
 /**
  * Invokes RPC by sending netconf message via listener. Also transforms result from NetconfMessage to
@@ -24,15 +44,107 @@ import org.opendaylight.yangtools.yang.model.api.EffectiveModelContext;
  */
 public final class NetconfDeviceRpc implements Rpcs.Normalized {
     private final @NonNull NetconfDeviceDOMRpcService domRpcService;
+    private final @NonNull EffectiveModelContext modelContext;
+    private final Map<QNameModule, ModuleEffectiveStatement> moduleEffectiveStatementMap;
 
     public NetconfDeviceRpc(final EffectiveModelContext modelContext, final RemoteDeviceCommunicator communicator,
             final RpcTransformer<ContainerNode, DOMRpcResult> transformer) {
         domRpcService = new NetconfDeviceDOMRpcService(modelContext, communicator, transformer);
+        this.modelContext = requireNonNull(modelContext);
+        moduleEffectiveStatementMap = Map.copyOf(modelContext.getModuleStatements());
     }
 
+    /**
+     * {@inheritDoc}
+     *
+     * <p><b>Implementation note:</b> If the {@code EffectiveModelContext} lacks the exact
+     * module revision referenced by {@code type.getModule()} (e.g., NETCONF base 2011-06-01),
+     * this implementation resolves the latest available module for the same namespace
+     * (e.g., 2013-09-29), rewrites the outbound RPC {@code QName} and any NETCONF-base
+     * node identifiers inside {@code input} to that revision, invokes the RPC, and then
+     * rewrites the returned normalized output back to the caller’s original module revision.
+     *
+     * <p>If no module exists for the RPC’s namespace, an {@link IllegalStateException}
+     * is thrown with a descriptive message.
+     */
     @Override
     public ListenableFuture<? extends DOMRpcResult> invokeNetconf(final QName type, final ContainerNode input) {
-        return domRpcService().invokeRpc(type, input);
+        if (moduleEffectiveStatementMap.containsKey(type.getModule())) {
+            return domRpcService().invokeRpc(type, input);
+        }
+        final var module = modelContext.findModules(type.getModule().namespace()).stream()
+            .filter(m -> m.getRevision().isPresent())
+            .max(Comparator.comparing((Module m) -> m.getRevision().get()))
+            .orElseThrow(() -> new IllegalStateException("EffectiveModelContext does not contain model: "
+                + type.getModule()));
+        final var patchedModule = module.getQNameModule();
+        final var patchedType = QName.create(patchedModule, type.getLocalName());
+        final Function<QName, QName> toLatest = qn -> qn.getModule().equals(type.getModule())
+                ? QName.create(patchedModule, qn.getLocalName())
+                : qn;
+        final Function<QName, QName> toOriginal = qn -> qn.getModule().equals(patchedModule)
+                ? QName.create(type.getModule(), qn.getLocalName())
+                : qn;
+        final var patchedInput = (ContainerNode) rewriteNodeTree(input, toLatest);
+
+        final var future = domRpcService.invokeRpc(patchedType, patchedInput);
+        // Map result back if we patched
+        if (!patchedType.equals(type)) {
+            return Futures.transform(future, result -> {
+                final var value = result.value();
+                final var remapped = value != null ? (ContainerNode) rewriteNodeTree(value, toOriginal) : null;
+                return new DefaultDOMRpcResult(remapped, result.errors());
+            }, MoreExecutors.directExecutor());
+        }
+        return future;
+    }
+
+    private static NormalizedNode rewriteNodeTree(final NormalizedNode node, final Function<QName, QName> mapper) {
+        final var oldQName = node.name().getNodeType();
+        final var newQName = mapper.apply(oldQName);
+        final var newId = YangInstanceIdentifier.NodeIdentifier.create(newQName);
+        switch (node) {
+            case ContainerNode cont -> {
+                final var builder = ImmutableNodes.newContainerBuilder().withNodeIdentifier(newId);
+                cont.body().forEach(ch ->
+                    builder.withChild((DataContainerChild) rewriteNodeTree(ch, mapper)));
+                return builder.build();
+            }
+            case ChoiceNode choice -> {
+                final var builder = ImmutableNodes.newChoiceBuilder().withNodeIdentifier(newId);
+                choice.body().forEach(ch ->
+                    builder.withChild((DataContainerChild) rewriteNodeTree(ch, mapper)));
+                return builder.build();
+            }
+            case LeafNode<?> leaf -> {
+                if (leaf.name().getNodeType().equals(newQName)) {
+                    return node;
+                }
+                return ImmutableNodes.leafNode(newId, leaf.body());
+            }
+            case AnyxmlNode<?> any -> {
+                if (any.name().getNodeType().equals(newQName)) {
+                    return node;
+                }
+                return ImmutableNodes.newAnyxmlBuilder(DOMSource.class)
+                    .withNodeIdentifier(newId)
+                    .withValue((DOMSource) any.body())
+                    .build();
+            }
+            case MapEntryNode entry -> {
+                final var oldId = entry.name();
+                final var patchedId = oldId.getNodeType().equals(newQName)
+                    ? oldId
+                    : YangInstanceIdentifier.NodeIdentifierWithPredicates.of(newQName, oldId.asMap());
+                final var builder = ImmutableNodes.newMapEntryBuilder().withNodeIdentifier(patchedId);
+                entry.body().forEach(ch ->
+                    builder.withChild((DataContainerChild) rewriteNodeTree(ch, mapper)));
+                return builder.build();
+            }
+            default -> {
+                return node;
+            }
+        }
     }
 
     @Override
