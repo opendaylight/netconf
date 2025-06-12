4cd4c4f994b6b24060e80f4b0534cf359975c93f
Yaroslav Lastivka <yaroslav.lastivka@pantheon.tech>
2025-06-12 10:54:41 +0300

    Support both 2011 and 2013 base-module revisions
    When a device advertises only the 2013-09-29 revision of
    urn:ietf:params:xml:ns:netconf:base:1.0, the connector failed on the
    second (schema-aware) GET/GET-CONFIG request because the hard-coded
    QNames still referenced the 2011-06-01 revision that is no longer part
    of the EffectiveModelContext.
    
    This change remaps the SourceIdentifier used for
    EffectiveModelContext creation to tolerate the 2011/2013 revision
    mismatch and prefer the device-advertised revision where appropriate.
    Register a built-in ietf-netconf@2011-06-01 YANG into the device
    SharedSchemaRepository before schema resolution. We load the resource
    from the model bundle via YangModuleInfoImpl and register it as
    a local YangTextSource.
    
    JIRA: NETCONF-1474
    Change-Id: I244420595896d99770637c97980ab618e44bcc79
    Signed-off-by: Yaroslav Lastivka <yaroslav.lastivka@pantheon.tech>
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- plugins/netconf-client-mdsal/src/main/java/org/opendaylight/netconf/client/mdsal/impl/DefaultDeviceNetconfSchemaProvider.java
+++ plugins/netconf-client-mdsal/src/main/java/org/opendaylight/netconf/client/mdsal/impl/DefaultDeviceNetconfSchemaProvider.java
@@ -12,7 +12,10 @@ import static java.util.Objects.requireNonNull;
 import com.google.common.annotations.VisibleForTesting;
 import com.google.common.util.concurrent.Futures;
 import com.google.common.util.concurrent.ListenableFuture;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Executor;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 import java.util.stream.Collectors;
 import org.eclipse.jdt.annotation.NonNull;
 import org.opendaylight.netconf.client.mdsal.api.BaseNetconfSchema;
@@ -23,11 +26,17 @@ import org.opendaylight.netconf.client.mdsal.api.NetconfRpcService;
 import org.opendaylight.netconf.client.mdsal.api.NetconfSessionPreferences;
 import org.opendaylight.netconf.client.mdsal.api.RemoteDeviceId;
 import org.opendaylight.yangtools.concepts.Registration;
+import org.opendaylight.yangtools.yang.common.Revision;
+import org.opendaylight.yangtools.yang.model.api.source.SourceIdentifier;
+import org.opendaylight.yangtools.yang.model.api.source.YangTextSource;
 import org.opendaylight.yangtools.yang.model.repo.api.EffectiveModelContextFactory;
 import org.opendaylight.yangtools.yang.model.repo.api.SchemaContextFactoryConfiguration;
 import org.opendaylight.yangtools.yang.model.repo.api.SchemaRepository;
+import org.opendaylight.yangtools.yang.model.repo.spi.PotentialSchemaSource;
 import org.opendaylight.yangtools.yang.model.repo.spi.PotentialSchemaSource.Costs;
+import org.opendaylight.yangtools.yang.model.repo.spi.SchemaSourceProvider;
 import org.opendaylight.yangtools.yang.model.repo.spi.SchemaSourceRegistry;
+import org.opendaylight.yangtools.yang.model.spi.source.URLYangTextSource;
 import org.opendaylight.yangtools.yang.parser.repo.SharedSchemaRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -60,6 +69,7 @@ public final class DefaultDeviceNetconfSchemaProvider implements DeviceNetconfSc
     public ListenableFuture<DeviceNetconfSchema> deviceNetconfSchemaFor(final RemoteDeviceId deviceId,
             final NetconfSessionPreferences sessionPreferences, final NetconfRpcService deviceRpc,
             final BaseNetconfSchema baseSchema, final Executor processingExecutor) {
+        registerNetconfBase2011(registry, repository);
         // Acquire sources
         final var sourceResolverFuture = resolver.resolve(deviceId, sessionPreferences, deviceRpc,
             baseSchema.modelContext());
@@ -80,6 +90,44 @@ public final class DefaultDeviceNetconfSchemaProvider implements DeviceNetconfSc
 
             return future;
         }, processingExecutor);
+    }
+
+    private static void registerNetconfBase2011(final SchemaSourceRegistry registry, final SchemaRepository repo) {
+        final var id2011 = new SourceIdentifier("ietf-netconf", Revision.of("2011-06-01"));
+        if (hasTextSource(repo, id2011)) {
+            // already available (cached/registered)
+            return;
+        }
+
+        // anchor lookup on the module’s own class to cross the OSGi bundle boundary
+        final var anchor = org.opendaylight.yang.svc.v1.urn.ietf.params.xml.ns.netconf.base._1._0.rev110601
+            .YangModuleInfoImpl.class;
+        final var url = anchor.getResource("/META-INF/yang/ietf-netconf@2011-06-01.yang");
+        if (url == null) {
+            LOG.debug("Builtin YANG not found in {} bundle: {}", anchor.getPackageName(),
+                "ietf-netconf@2011-06-01.yang");
+            return;
+        }
+
+        final var source = new URLYangTextSource(url);
+
+        final SchemaSourceProvider<YangTextSource> provider = requested -> id2011.equals(requested)
+                ? Futures.immediateFuture(source)
+                : Futures.immediateFailedFuture(new IllegalArgumentException("Unsupported id " + requested));
+
+        registry.registerSchemaSource(provider, PotentialSchemaSource.create(id2011, YangTextSource.class,
+            PotentialSchemaSource.Costs.LOCAL_IO.getValue()));
+
+        LOG.info("Registered builtin ietf-netconf@2011-06-01 from {}", url);
+    }
+
+    private static boolean hasTextSource(final SchemaRepository repo, final SourceIdentifier id) {
+        try {
+            final var future = repo.getSchemaSource(id, YangTextSource.class);
+            return future.isDone() && !future.isCancelled() && future.get(1, TimeUnit.MILLISECONDS) != null;
+        } catch (InterruptedException | ExecutionException | TimeoutException e) {
+            return false;
+        }
     }
 
     @Deprecated
