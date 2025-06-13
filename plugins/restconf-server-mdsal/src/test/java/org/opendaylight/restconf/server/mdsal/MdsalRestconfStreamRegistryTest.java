c6138bd7083f0e5b6f306e00f8fdcdb2e60bfc27
Matej Sramcik <matej.sramcik@pantheon.tech>
2025-06-13 10:39:19 +0300

    Plug EffectiveModelContext into ARSR
    Added EffectiveModelContext to AbstractRestconfStreamRegistry via
    constructor and updateModelContext method.
    
    JIRA: NETCONF-1481
    Change-Id: I2d43a8087d73d5751a7411fab4aaa69c32eb0862
    Signed-off-by: Yaroslav Lastivka <yaroslav.lastivka@pantheon.tech>
    Signed-off-by: Matej Sramcik <matej.sramcik@pantheon.tech>
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- plugins/restconf-server-mdsal/src/test/java/org/opendaylight/restconf/server/mdsal/MdsalRestconfStreamRegistryTest.java
+++ plugins/restconf-server-mdsal/src/test/java/org/opendaylight/restconf/server/mdsal/MdsalRestconfStreamRegistryTest.java
@@ -9,6 +9,7 @@ package org.opendaylight.restconf.server.mdsal;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertSame;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.doReturn;
@@ -89,6 +90,8 @@ class MdsalRestconfStreamRegistryTest {
     private TransportSession.Description sessionDesc;
     @Mock
     private EffectiveModelContext effectiveModelContext;
+    @Mock
+    private EffectiveModelContext updatedModelContext;
 
     private MdsalRestconfStreamRegistry registry;
 
@@ -186,5 +189,16 @@ class MdsalRestconfStreamRegistryTest {
                 // Nothing else
             }
         }
+    }
+
+    /**
+     * Test that model context is present in AbstractRestconfStreamRegistry and is also modified when
+     * onModelContextUpdated is invoked.
+     */
+    @Test
+    void modelContextUpdateTest() {
+        assertSame(effectiveModelContext, registry.modelContext());
+        registry.onModelContextUpdated(updatedModelContext);
+        assertSame(updatedModelContext, registry.modelContext());
     }
 }
