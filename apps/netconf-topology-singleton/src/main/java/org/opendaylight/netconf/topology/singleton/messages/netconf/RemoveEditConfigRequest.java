97a668c1b7899efdc77f255b24417e27ceada6a0
Peter Suna <peter.suna@pantheon.tech>
2025-06-16 21:31:18 +0200

    Remove NetconfDataTreeService
    Replace NetconfDataTreeService with DataStoreService.
    
    FIXME: Split this patch.
    
    JIRA: NETCONF-1352
    Change-Id: I4c8a069ca3f50a62806e023938bcf99a2f5bb302
    Signed-off-by: Peter Suna <peter.suna@pantheon.tech>

--- apps/netconf-topology-singleton/src/main/java/org/opendaylight/netconf/topology/singleton/messages/netconf/RemoveEditConfigRequest.java
+++ apps/netconf-topology-singleton/src/main/java/org/opendaylight/netconf/topology/singleton/messages/netconf/RemoveEditConfigRequest.java
@@ -9,23 +9,16 @@ package org.opendaylight.netconf.topology.singleton.messages.netconf;
 
 import java.io.Serial;
 import java.io.Serializable;
-import org.opendaylight.mdsal.common.api.LogicalDatastoreType;
 import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
 
 public class RemoveEditConfigRequest implements Serializable {
     @Serial
     private static final long serialVersionUID = 1L;
 
-    private final LogicalDatastoreType store;
     private final YangInstanceIdentifier path;
 
-    public RemoveEditConfigRequest(final LogicalDatastoreType store, final YangInstanceIdentifier path) {
-        this.store = store;
+    public RemoveEditConfigRequest(final YangInstanceIdentifier path) {
         this.path = path;
-    }
-
-    public LogicalDatastoreType getStore() {
-        return store;
     }
 
     public YangInstanceIdentifier getPath() {
