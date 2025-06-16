b9ce4f2f1790efe873a0a8198af83d806a0cdc04
Peter Suna <peter.suna@pantheon.tech>
2025-06-16 21:31:18 +0200

    Replace NetconfDataTreeService
    We can simplify NetconfDataTreeService into DataStoreService,
    which no longer includes (lock, unlock, discardChanges, and
    getDeviceId) operations. These can now be handled by
    the DataStoreService implementation without forwarding these
    responsibilities to the user.
    Multiple "get" operations can be merged into one operation.
    
    Prepare new implementation of Candidate, Running and
    CandidateWithRunnning based on DataStoreService.
    
    JIRA: NETCONF-1352
    Change-Id: I4c8a069ca3f50a62806e023938bcf99a2f5bb302
    Signed-off-by: Peter Suna <peter.suna@pantheon.tech>

--- apps/netconf-topology-singleton/src/main/java/org/opendaylight/netconf/topology/singleton/impl/netconf/ActorProxyNetconfServiceFacade.java
+++ apps/netconf-topology-singleton/src/main/java/org/opendaylight/netconf/topology/singleton/impl/netconf/ActorProxyNetconfServiceFacade.java
@@ -7,9 +7,6 @@
  */
 package org.opendaylight.netconf.topology.singleton.impl.netconf;
 
-import static org.opendaylight.mdsal.common.api.LogicalDatastoreType.CONFIGURATION;
-import static org.opendaylight.mdsal.common.api.LogicalDatastoreType.OPERATIONAL;
-
 import com.google.common.util.concurrent.Futures;
 import com.google.common.util.concurrent.ListenableFuture;
 import com.google.common.util.concurrent.SettableFuture;
@@ -26,24 +23,18 @@ import org.opendaylight.mdsal.common.api.LogicalDatastoreType;
 import org.opendaylight.mdsal.common.api.ReadFailedException;
 import org.opendaylight.mdsal.dom.api.DOMRpcResult;
 import org.opendaylight.mdsal.dom.spi.DefaultDOMRpcResult;
-import org.opendaylight.netconf.api.EffectiveOperation;
 import org.opendaylight.netconf.client.mdsal.api.RemoteDeviceId;
 import org.opendaylight.netconf.topology.singleton.impl.utils.ClusteringRpcException;
 import org.opendaylight.netconf.topology.singleton.impl.utils.NetconfTopologyUtils;
 import org.opendaylight.netconf.topology.singleton.messages.NormalizedNodeMessage;
+import org.opendaylight.netconf.topology.singleton.messages.netconf.CancelChangesRequest;
 import org.opendaylight.netconf.topology.singleton.messages.netconf.CommitRequest;
 import org.opendaylight.netconf.topology.singleton.messages.netconf.CreateEditConfigRequest;
 import org.opendaylight.netconf.topology.singleton.messages.netconf.DeleteEditConfigRequest;
-import org.opendaylight.netconf.topology.singleton.messages.netconf.DiscardChangesRequest;
-import org.opendaylight.netconf.topology.singleton.messages.netconf.GetConfigRequest;
-import org.opendaylight.netconf.topology.singleton.messages.netconf.GetConfigWithFieldsRequest;
 import org.opendaylight.netconf.topology.singleton.messages.netconf.GetRequest;
-import org.opendaylight.netconf.topology.singleton.messages.netconf.GetWithFieldsRequest;
-import org.opendaylight.netconf.topology.singleton.messages.netconf.LockRequest;
 import org.opendaylight.netconf.topology.singleton.messages.netconf.MergeEditConfigRequest;
 import org.opendaylight.netconf.topology.singleton.messages.netconf.RemoveEditConfigRequest;
 import org.opendaylight.netconf.topology.singleton.messages.netconf.ReplaceEditConfigRequest;
-import org.opendaylight.netconf.topology.singleton.messages.netconf.UnlockRequest;
 import org.opendaylight.netconf.topology.singleton.messages.rpc.InvokeRpcMessageReply;
 import org.opendaylight.netconf.topology.singleton.messages.transactions.EmptyReadResponse;
 import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
@@ -71,146 +62,52 @@ public class ActorProxyNetconfServiceFacade implements ProxyNetconfServiceFacade
     }
 
     @Override
-    public ListenableFuture<DOMRpcResult> lock() {
-        LOG.debug("{}: Lock via actor {}", id, masterActor);
-        final SettableFuture<DOMRpcResult> lockResult = SettableFuture.create();
-        final Future<Object> future = Patterns.ask(masterActor, new LockRequest(), askTimeout);
-        future.onComplete(new OnComplete<>() {
-            @Override
-            public void onComplete(final Throwable failure, final Object response) {
-                if (failure != null) {
-                    lockResult.setException(failure);
-                } else if (response instanceof InvokeRpcMessageReply) {
-                    lockResult.set(mapInvokeRpcMessageReplyToDOMRpcResult((InvokeRpcMessageReply) response));
-                } else {
-                    lockResult.setException(new ClusteringRpcException("Lock operation returned unexpected type"));
-                    LOG.error("{}: Lock via actor {} returned unexpected type", id, masterActor);
-                }
-            }
-        }, executionContext);
-        return lockResult;
-    }
-
-    @Override
-    public ListenableFuture<DOMRpcResult> unlock() {
-        LOG.debug("{}: Unlock via actor {}", id, masterActor);
-        final SettableFuture<DOMRpcResult> unlockResult = SettableFuture.create();
-        final Future<Object> future = Patterns.ask(masterActor, new UnlockRequest(), askTimeout);
-        future.onComplete(new OnComplete<>() {
-            @Override
-            public void onComplete(final Throwable failure, final Object response) {
-                if (failure != null) {
-                    unlockResult.setException(failure);
-                } else if (response instanceof InvokeRpcMessageReply) {
-                    unlockResult.set(mapInvokeRpcMessageReplyToDOMRpcResult((InvokeRpcMessageReply) response));
-                } else {
-                    unlockResult.setException(new ClusteringRpcException("Unlock operation returned unexpected type"));
-                    LOG.error("{}: Unlock via actor {} returned unexpected type", id, masterActor);
-                }
-            }
-        }, executionContext);
-        return unlockResult;
-    }
-
-    @Override
-    public ListenableFuture<DOMRpcResult> discardChanges() {
-        LOG.debug("{}: Discard changes via actor {}", id, masterActor);
-        final SettableFuture<DOMRpcResult> discardChangesResult = SettableFuture.create();
-        final Future<Object> future = Patterns.ask(masterActor, new DiscardChangesRequest(), askTimeout);
-        future.onComplete(new OnComplete<>() {
-            @Override
-            public void onComplete(final Throwable failure, final Object response) {
-                if (failure != null) {
-                    discardChangesResult.setException(failure);
-                } else if (response instanceof InvokeRpcMessageReply) {
-                    discardChangesResult.set(mapInvokeRpcMessageReplyToDOMRpcResult((InvokeRpcMessageReply) response));
-                } else {
-                    discardChangesResult.setException(
-                        new ClusteringRpcException("Discard changes operation returned unexpected type"));
-                    LOG.error("{}: Discard changes  via actor {} returned unexpected type", id, masterActor);
-                }
-            }
-        }, executionContext);
-        return discardChangesResult;
-    }
-
-    @Override
-    public ListenableFuture<Optional<NormalizedNode>> get(final YangInstanceIdentifier path) {
-        LOG.debug("{}: Get {} {} via actor {}", id, OPERATIONAL, path, masterActor);
-        final Future<Object> future = Patterns.ask(masterActor, new GetRequest(path), askTimeout);
-        return read(future, OPERATIONAL, path);
-    }
-
-    @Override
-    public ListenableFuture<Optional<NormalizedNode>> get(final YangInstanceIdentifier path,
-            final List<YangInstanceIdentifier> fields) {
-        LOG.debug("{}: Get {} {} with fields {} via actor {}", id, OPERATIONAL, path, fields, masterActor);
-        final Future<Object> future = Patterns.ask(masterActor, new GetWithFieldsRequest(path, fields), askTimeout);
-        return read(future, OPERATIONAL, path);
-    }
-
-    @Override
-    public ListenableFuture<Optional<NormalizedNode>> getConfig(final YangInstanceIdentifier path) {
-        LOG.debug("{}: GetConfig {} {} via actor {}", id, CONFIGURATION, path, masterActor);
-        final Future<Object> future = Patterns.ask(masterActor, new GetConfigRequest(path), askTimeout);
-        return read(future, CONFIGURATION, path);
-    }
-
-    @Override
-    public ListenableFuture<Optional<NormalizedNode>> getConfig(final YangInstanceIdentifier path,
-            final List<YangInstanceIdentifier> fields) {
-        LOG.debug("{}: GetConfig {} {} with fields {} via actor {}", id, CONFIGURATION, path, fields, masterActor);
-        final Future<Object> future = Patterns.ask(masterActor,
-                new GetConfigWithFieldsRequest(path, fields), askTimeout);
-        return read(future, CONFIGURATION, path);
-    }
-
-    @Override
-    public ListenableFuture<? extends DOMRpcResult> merge(final LogicalDatastoreType store,
-            final YangInstanceIdentifier path, final NormalizedNode data,
-            final Optional<EffectiveOperation> defaultOperation) {
-        LOG.debug("{}: Merge {} {} via actor {}", id, store, path, masterActor);
-        masterActor.tell(new MergeEditConfigRequest(
-            store, new NormalizedNodeMessage(path, data), defaultOperation.orElse(null)), ActorRef.noSender());
-        return createResult();
-
-    }
-
-    @Override
-    public ListenableFuture<? extends DOMRpcResult> replace(final LogicalDatastoreType store,
-            final YangInstanceIdentifier path, final NormalizedNode data,
-            final Optional<EffectiveOperation> defaultOperation) {
-        LOG.debug("{}: Replace {} {} via actor {}", id, store, path, masterActor);
-
-        masterActor.tell(new ReplaceEditConfigRequest(
-            store, new NormalizedNodeMessage(path, data), defaultOperation.orElse(null)), ActorRef.noSender());
-        return createResult();
-    }
-
-    @Override
-    public ListenableFuture<? extends DOMRpcResult> create(final LogicalDatastoreType store,
-            final YangInstanceIdentifier path, final NormalizedNode data,
-            final Optional<EffectiveOperation> defaultOperation) {
-        LOG.debug("{}: Create {} {} via actor {}", id, store, path, masterActor);
+    public ListenableFuture<? extends DOMRpcResult> create(final YangInstanceIdentifier path,
+            final NormalizedNode data) {
+        LOG.debug("{}: Create {} via actor {}", id, path, masterActor);
         masterActor.tell(new CreateEditConfigRequest(
-            store, new NormalizedNodeMessage(path, data), defaultOperation.orElse(null)), ActorRef.noSender());
+            new NormalizedNodeMessage(path, data)), ActorRef.noSender());
         return createResult();
     }
 
     @Override
-    public ListenableFuture<? extends DOMRpcResult> delete(final LogicalDatastoreType store,
-            final YangInstanceIdentifier path) {
-        LOG.debug("{}: Delete {} {} via actor {}", id, store, path, masterActor);
-        masterActor.tell(new DeleteEditConfigRequest(store, path), ActorRef.noSender());
+    public ListenableFuture<? extends DOMRpcResult> delete(final YangInstanceIdentifier path) {
+        LOG.debug("{}: Delete {} via actor {}", id, path, masterActor);
+        masterActor.tell(new DeleteEditConfigRequest(path), ActorRef.noSender());
         return createResult();
     }
 
     @Override
-    public ListenableFuture<? extends DOMRpcResult> remove(final LogicalDatastoreType store,
-            final YangInstanceIdentifier path) {
-        LOG.debug("{}: Remove {} {} via actor {}", id, store, path, masterActor);
-        masterActor.tell(new RemoveEditConfigRequest(store, path), ActorRef.noSender());
+    public ListenableFuture<? extends DOMRpcResult> remove(final YangInstanceIdentifier path) {
+        LOG.debug("{}: Remove {} via actor {}", id, path, masterActor);
+        masterActor.tell(new RemoveEditConfigRequest(path), ActorRef.noSender());
         return createResult();
+    }
+
+    @Override
+    public ListenableFuture<? extends DOMRpcResult> merge(final YangInstanceIdentifier path,
+            final NormalizedNode data) {
+        LOG.debug("{}: Merge {} via actor {}", id, path, masterActor);
+        masterActor.tell(new MergeEditConfigRequest(
+            new NormalizedNodeMessage(path, data)), ActorRef.noSender());
+        return createResult();
+    }
+
+    @Override
+    public ListenableFuture<? extends DOMRpcResult> replace(final YangInstanceIdentifier path,
+            final NormalizedNode data) {
+        LOG.debug("{}: Replace {} via actor {}", id, path, masterActor);
+        masterActor.tell(new ReplaceEditConfigRequest(new NormalizedNodeMessage(path, data)),
+            ActorRef.noSender());
+        return createResult();
+    }
+
+    @Override
+    public ListenableFuture<Optional<NormalizedNode>> get(final LogicalDatastoreType store,
+            final YangInstanceIdentifier path, final List<YangInstanceIdentifier> fields) {
+        LOG.debug("{}: Get {} {} via actor {}", id, store, path, masterActor);
+        final Future<Object> future = Patterns.ask(masterActor, new GetRequest(store, path, fields), askTimeout);
+        return read(future, store, path);
     }
 
     @Override
@@ -237,19 +134,37 @@ public class ActorProxyNetconfServiceFacade implements ProxyNetconfServiceFacade
 
             private NetconfServiceFailedException newNetconfServiceFailedException(final Throwable failure) {
                 return new NetconfServiceFailedException(String.format("%s: Commit of operation failed",
-                    getDeviceId()), failure);
+                    id), failure);
             }
         }, executionContext);
         return settableFuture;
     }
 
     @Override
-    public Object getDeviceId() {
-        return id;
+    public ListenableFuture<? extends DOMRpcResult> cancel() {
+        LOG.debug("{}: Discard changes via actor {}", id, masterActor);
+        final SettableFuture<DOMRpcResult> cancelRequest = SettableFuture.create();
+        final Future<Object> future = Patterns.ask(masterActor, new CancelChangesRequest(), askTimeout);
+        future.onComplete(new OnComplete<>() {
+            @Override
+            public void onComplete(final Throwable failure, final Object response) {
+                if (failure != null) {
+                    cancelRequest.setException(failure);
+                } else if (response instanceof InvokeRpcMessageReply) {
+                    cancelRequest.set(mapInvokeRpcMessageReplyToDOMRpcResult((InvokeRpcMessageReply) response));
+                } else {
+                    cancelRequest.setException(
+                        new ClusteringRpcException("Discard changes operation returned unexpected type"));
+                    LOG.error("{}: Discard changes  via actor {} returned unexpected type", id, masterActor);
+                }
+            }
+        }, executionContext);
+        return cancelRequest;
     }
 
-    private SettableFuture<Optional<NormalizedNode>> read(final Future<Object> future, final LogicalDatastoreType store,
-                                                          final YangInstanceIdentifier path) {
+
+    private SettableFuture<Optional<NormalizedNode>> read(final Future<Object> future,
+            final LogicalDatastoreType store, final YangInstanceIdentifier path) {
         final SettableFuture<Optional<NormalizedNode>> settableFuture = SettableFuture.create();
         future.onComplete(new OnComplete<>() {
             @Override
