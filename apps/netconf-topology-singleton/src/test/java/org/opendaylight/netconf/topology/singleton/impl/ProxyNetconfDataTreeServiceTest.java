367150c1abe66f3d39017a09e70df8fc6e13b739
Peter Suna <peter.suna@pantheon.tech>
2025-06-16 21:31:18 +0200

    Replace NetconfDataTreeService
    We can simplify NetconfDataTreeService into DataStoreService,
    which no longer includes (lock, unlock, discardChanges, and
    getDeviceId) operations. These can now be handled by
    the DataStoreService implementation without forwarding these
    responsibilities to the user.
    Multiple "get" operations can be merged into one operation.
    
    Prepare new implementation of Candidate, Running and
    CandidateWithRunnning based on DataStoreService.
    
    JIRA: NETCONF-1352
    Change-Id: I4c8a069ca3f50a62806e023938bcf99a2f5bb302
    Signed-off-by: Peter Suna <peter.suna@pantheon.tech>

--- apps/netconf-topology-singleton/src/test/java/org/opendaylight/netconf/topology/singleton/impl/ProxyNetconfDataTreeServiceTest.java
+++ apps/netconf-topology-singleton/src/test/java/org/opendaylight/netconf/topology/singleton/impl/ProxyNetconfDataTreeServiceTest.java
@@ -7,17 +7,10 @@
  */
 package org.opendaylight.netconf.topology.singleton.impl;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.opendaylight.restconf.api.query.ContentParam.CONFIG;
 
-import com.google.common.util.concurrent.Futures;
-import com.google.common.util.concurrent.ListenableFuture;
-import com.google.common.util.concurrent.MoreExecutors;
 import java.net.InetSocketAddress;
 import java.util.List;
-import java.util.Optional;
 import java.util.concurrent.TimeUnit;
 import org.apache.pekko.actor.ActorSystem;
 import org.apache.pekko.actor.Status;
@@ -27,35 +20,26 @@ import org.apache.pekko.util.Timeout;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Test;
 import org.opendaylight.mdsal.common.api.LogicalDatastoreType;
-import org.opendaylight.mdsal.dom.api.DOMRpcResult;
 import org.opendaylight.netconf.client.mdsal.api.RemoteDeviceId;
 import org.opendaylight.netconf.topology.singleton.messages.netconf.CommitRequest;
 import org.opendaylight.netconf.topology.singleton.messages.netconf.CreateEditConfigRequest;
 import org.opendaylight.netconf.topology.singleton.messages.netconf.DeleteEditConfigRequest;
-import org.opendaylight.netconf.topology.singleton.messages.netconf.DiscardChangesRequest;
-import org.opendaylight.netconf.topology.singleton.messages.netconf.GetConfigRequest;
 import org.opendaylight.netconf.topology.singleton.messages.netconf.GetRequest;
-import org.opendaylight.netconf.topology.singleton.messages.netconf.LockRequest;
 import org.opendaylight.netconf.topology.singleton.messages.netconf.MergeEditConfigRequest;
 import org.opendaylight.netconf.topology.singleton.messages.netconf.NetconfDataTreeServiceRequest;
 import org.opendaylight.netconf.topology.singleton.messages.netconf.RemoveEditConfigRequest;
 import org.opendaylight.netconf.topology.singleton.messages.netconf.ReplaceEditConfigRequest;
-import org.opendaylight.netconf.topology.singleton.messages.netconf.UnlockRequest;
-import org.opendaylight.netconf.topology.singleton.messages.rpc.InvokeRpcMessageReply;
+import org.opendaylight.restconf.server.api.DataGetParams;
 import org.opendaylight.yangtools.yang.common.QName;
 import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
 import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.NodeIdentifier;
 import org.opendaylight.yangtools.yang.data.api.schema.ContainerNode;
 import org.opendaylight.yangtools.yang.data.spi.node.ImmutableNodes;
-import scala.concurrent.duration.Duration;
-import scala.concurrent.duration.FiniteDuration;
 
 class ProxyNetconfDataTreeServiceTest {
-    private static final FiniteDuration EXP_NO_MESSAGE_TIMEOUT = Duration.apply(300, TimeUnit.MILLISECONDS);
     private static final RemoteDeviceId DEVICE_ID =
         new RemoteDeviceId("dev1", InetSocketAddress.createUnresolved("localhost", 17830));
     private static final YangInstanceIdentifier PATH = YangInstanceIdentifier.of();
-    private static final LogicalDatastoreType STORE = LogicalDatastoreType.CONFIGURATION;
     private static final ContainerNode NODE = ImmutableNodes.newContainerBuilder()
         .withNodeIdentifier(new NodeIdentifier(QName.create("", "cont")))
         .build();
@@ -66,6 +50,7 @@ class ProxyNetconfDataTreeServiceTest {
     private final TestProbe masterActor = new TestProbe(system);
     private final ProxyNetconfDataTreeService proxy = new ProxyNetconfDataTreeService(DEVICE_ID, masterActor.ref(),
         system.dispatcher(), Timeout.apply(5, TimeUnit.SECONDS));
+    private final DataGetParams params = new DataGetParams(CONFIG, null, null, null);
 
     @AfterAll
     static void staticTearDown() {
@@ -73,47 +58,8 @@ class ProxyNetconfDataTreeServiceTest {
     }
 
     @Test
-    void testLock() {
-        lock();
-    }
-
-    @Test
-    void testUnlock() {
-        lock();
-        proxy.unlock();
-        masterActor.expectMsgClass(UnlockRequest.class);
-    }
-
-    @Test
-    void testUnlockWithoutLock() {
-        try {
-            proxy.unlock();
-            fail("Should throw IllegalStateException");
-        } catch (final IllegalStateException e) {
-            checkException(e);
-        }
-    }
-
-    @Test
-    void testDiscardChanges() {
-        lock();
-        proxy.discardChanges();
-        masterActor.expectMsgClass(DiscardChangesRequest.class);
-    }
-
-    @Test
-    void testDiscardChangesWithoutLock() {
-        try {
-            proxy.discardChanges();
-            fail("Should throw IllegalStateException");
-        } catch (final IllegalStateException e) {
-            checkException(e);
-        }
-    }
-
-    @Test
     void testGet() {
-        proxy.get(YangInstanceIdentifier.of());
+        proxy.get(LogicalDatastoreType.OPERATIONAL, PATH, List.of());
         masterActor.expectMsgClass(NetconfDataTreeServiceRequest.class);
         masterActor.reply(new Status.Success(masterActor.ref()));
         masterActor.expectMsgClass(GetRequest.class);
@@ -121,129 +67,57 @@ class ProxyNetconfDataTreeServiceTest {
 
     @Test
     void testGetConfig() {
-        proxy.getConfig(YangInstanceIdentifier.of());
+        proxy.get(LogicalDatastoreType.CONFIGURATION, PATH, List.of());
         masterActor.expectMsgClass(NetconfDataTreeServiceRequest.class);
         masterActor.reply(new Status.Success(masterActor.ref()));
-        masterActor.expectMsgClass(GetConfigRequest.class);
+        masterActor.expectMsgClass(GetRequest.class);
     }
 
     @Test
     void testMerge() {
-        lock();
-        proxy.merge(STORE, PATH, NODE, Optional.empty());
+        proxy.merge(PATH, NODE);
+        masterActor.expectMsgClass(NetconfDataTreeServiceRequest.class);
+        masterActor.reply(new Status.Success(masterActor.ref()));
         masterActor.expectMsgClass(MergeEditConfigRequest.class);
     }
 
     @Test
-    void testMergeWithoutLock() {
-        try {
-            proxy.merge(STORE, PATH, NODE, Optional.empty());
-            fail("Should throw IllegalStateException");
-        } catch (final IllegalStateException e) {
-            checkException(e);
-        }
-    }
-
-    @Test
     void testReplace() {
-        lock();
-        proxy.replace(STORE, PATH, NODE, Optional.empty());
+        proxy.replace(PATH, NODE);
+        masterActor.expectMsgClass(NetconfDataTreeServiceRequest.class);
+        masterActor.reply(new Status.Success(masterActor.ref()));
         masterActor.expectMsgClass(ReplaceEditConfigRequest.class);
     }
 
     @Test
-    void testReplaceWithoutLock() {
-        try {
-            proxy.replace(STORE, PATH, NODE, Optional.empty());
-            fail("Should throw IllegalStateException");
-        } catch (final IllegalStateException e) {
-            checkException(e);
-        }
-    }
-
-    @Test
     void testCreate() {
-        lock();
-        proxy.create(STORE, PATH, NODE, Optional.empty());
+        proxy.create(PATH, NODE);
+        masterActor.expectMsgClass(NetconfDataTreeServiceRequest.class);
+        masterActor.reply(new Status.Success(masterActor.ref()));
         masterActor.expectMsgClass(CreateEditConfigRequest.class);
     }
 
     @Test
-    void testCreateWithoutLock() {
-        try {
-            proxy.create(STORE, PATH, NODE, Optional.empty());
-            fail("Should throw IllegalStateException");
-        } catch (final IllegalStateException e) {
-            checkException(e);
-        }
-    }
-
-    @Test
     void testDelete() {
-        lock();
-        proxy.delete(STORE, PATH);
+        proxy.delete(PATH);
+        masterActor.expectMsgClass(NetconfDataTreeServiceRequest.class);
+        masterActor.reply(new Status.Success(masterActor.ref()));
         masterActor.expectMsgClass(DeleteEditConfigRequest.class);
     }
 
     @Test
-    void testDeleteWithoutLock() {
-        try {
-            proxy.delete(STORE, PATH);
-            fail("Should throw IllegalStateException");
-        } catch (final IllegalStateException e) {
-            checkException(e);
-        }
-    }
-
-    @Test
     void testRemove() {
-        lock();
-        proxy.remove(STORE, PATH);
+        proxy.remove(PATH);
+        masterActor.expectMsgClass(NetconfDataTreeServiceRequest.class);
+        masterActor.reply(new Status.Success(masterActor.ref()));
         masterActor.expectMsgClass(RemoveEditConfigRequest.class);
     }
 
     @Test
-    void testRemoveWithoutLock() {
-        try {
-            proxy.remove(STORE, PATH);
-            fail("Should throw IllegalStateException");
-        } catch (final IllegalStateException e) {
-            checkException(e);
-        }
-    }
-
-    @Test
     void testCommit() {
-        lock();
         proxy.commit();
-        masterActor.expectMsgClass(CommitRequest.class);
-    }
-
-    @Test
-    void testCommitWithoutLock() {
-        try {
-            proxy.commit();
-            fail("Should throw IllegalStateException");
-        } catch (final IllegalStateException e) {
-            checkException(e);
-        }
-    }
-
-    private void lock() {
-        final ListenableFuture<DOMRpcResult> lock = proxy.lock();
         masterActor.expectMsgClass(NetconfDataTreeServiceRequest.class);
         masterActor.reply(new Status.Success(masterActor.ref()));
-        masterActor.expectMsgClass(LockRequest.class);
-        masterActor.reply(new InvokeRpcMessageReply(null, List.of()));
-        Futures.whenAllComplete(lock).run(() -> {
-            assertTrue(lock.isDone());
-            assertNotNull(Futures.getUnchecked(lock));
-            }, MoreExecutors.directExecutor()
-        );
-    }
-
-    private void checkException(final IllegalStateException exception) {
-        assertEquals(String.format("%s: Device's datastore must be locked first", DEVICE_ID), exception.getMessage());
-        masterActor.expectNoMessage(EXP_NO_MESSAGE_TIMEOUT);
+        masterActor.expectMsgClass(CommitRequest.class);
     }
 }
---
+++ plugins/netconf-client-mdsal/src/main/java/org/opendaylight/netconf/client/mdsal/spi/Running.java
@@ -0,0 +1,149 @@
+/*
+ * Copyright (c) 2025 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.netconf.client.mdsal.spi;
+
+import static org.opendaylight.netconf.api.EffectiveOperation.CREATE;
+import static org.opendaylight.netconf.api.EffectiveOperation.DELETE;
+import static org.opendaylight.netconf.api.EffectiveOperation.MERGE;
+import static org.opendaylight.netconf.api.EffectiveOperation.REMOVE;
+import static org.opendaylight.netconf.api.EffectiveOperation.REPLACE;
+
+import com.google.common.util.concurrent.Futures;
+import com.google.common.util.concurrent.ListenableFuture;
+import java.util.Optional;
+import org.opendaylight.mdsal.dom.api.DOMRpcResult;
+import org.opendaylight.netconf.api.EffectiveOperation;
+import org.opendaylight.netconf.api.NetconfDocumentedException;
+import org.opendaylight.netconf.client.mdsal.api.RemoteDeviceId;
+import org.opendaylight.netconf.client.mdsal.impl.NetconfBaseOps;
+import org.opendaylight.netconf.client.mdsal.impl.NetconfRpcFutureCallback;
+import org.opendaylight.yangtools.yang.common.ErrorSeverity;
+import org.opendaylight.yangtools.yang.common.ErrorTag;
+import org.opendaylight.yangtools.yang.common.ErrorType;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
+import org.opendaylight.yangtools.yang.data.api.schema.ChoiceNode;
+import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNode;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * This class provides methods for manipulating and retrieving configuration and operational data on a NETCONF
+ *  running data store.
+ */
+final class Running extends AbstractDataStore {
+    private static final Logger LOG = LoggerFactory.getLogger(Running.class);
+
+    Running(final NetconfBaseOps netconfOps, final RemoteDeviceId id, final boolean rollbackSupport,
+            final boolean lockDatastore) {
+        super(netconfOps, id, rollbackSupport, lockDatastore);
+    }
+
+    @Override
+    public ListenableFuture<? extends DOMRpcResult> create(final YangInstanceIdentifier path,
+            final NormalizedNode data) {
+        return editConfig(CREATE, data, path);
+    }
+
+    @Override
+    public ListenableFuture<? extends DOMRpcResult> delete(final YangInstanceIdentifier path) {
+        return editConfig(DELETE, path);
+    }
+
+    @Override
+    public ListenableFuture<? extends DOMRpcResult> remove(final YangInstanceIdentifier path) {
+        return editConfig(REMOVE, path);
+    }
+
+    @Override
+    public ListenableFuture<? extends DOMRpcResult> merge(final YangInstanceIdentifier path,
+            final NormalizedNode data) {
+        return editConfig(MERGE, data, path);
+    }
+
+    @Override
+    public ListenableFuture<? extends DOMRpcResult> replace(final YangInstanceIdentifier path,
+            final NormalizedNode data) {
+        return editConfig(REPLACE, data, path);
+    }
+
+    @Override
+    public ListenableFuture<? extends DOMRpcResult> commit() {
+        LOG.trace("Execute commit on device {}", id);
+        return RPC_SUCCESS;
+    }
+
+    @Override
+    public ListenableFuture<? extends DOMRpcResult> cancel() {
+        LOG.trace("Execute cancel on device {}", id);
+        return unlock();
+    }
+
+    @Override
+    ListenableFuture<? extends DOMRpcResult> unlock() {
+        if (lockDatastore && lock.getAndSet(false)) {
+            LOG.trace("Execute unlock on device {}", id);
+            return netconfOps.unlockRunning(new NetconfRpcFutureCallback("Unlock running", id));
+        }
+        return RPC_SUCCESS;
+    }
+
+    @Override
+    ListenableFuture<? extends DOMRpcResult> lock() {
+        if (lockDatastore && !lock.getAndSet(true)) {
+            LOG.trace("Execute lock operation on device {}", id);
+            return netconfOps.lockRunning(new NetconfRpcFutureCallback("Lock running", id));
+        }
+        return RPC_SUCCESS;
+    }
+
+    private ListenableFuture<? extends DOMRpcResult> editConfig(final EffectiveOperation operation,
+            final NormalizedNode child, final YangInstanceIdentifier path) {
+        final ChoiceNode editStructure;
+        try {
+            editStructure = netconfOps.createEditConfigStructure(Optional.of(child), Optional.of(operation), path);
+        } catch (IllegalArgumentException | IllegalStateException cause) {
+            LOG.error("Failed to create edit-config structure node on device {} for child {} with path {}, with RPC"
+                    + " operation {}", id, child, path, operation);
+            final var documentedException = new NetconfDocumentedException(
+                "Failed to create edit-config structure node for RPC operation", cause, ErrorType.APPLICATION,
+                ErrorTag.OPERATION_FAILED, ErrorSeverity.ERROR);
+            return Futures.immediateFailedFuture(documentedException);
+        }
+        final var callback = new NetconfRpcFutureCallback("Edit running", id);
+        LOG.trace("Execute editConfig operation {} on device {} with data {} and path {}", id, operation, child, path);
+        // FIXME: Set default-operation" parameter based on called DataStoreService method.
+        //        https://www.rfc-editor.org/rfc/rfc6241#section-7.2
+        final var edit = addLockBeforeFuture(() -> netconfOps.editConfigRunning(callback, editStructure,
+            rollbackSupport));
+        final var editWithUnlock = addUnlockAfterFuture(edit);
+        return addCancelIfFails(editWithUnlock);
+    }
+
+    private ListenableFuture<? extends DOMRpcResult> editConfig(final EffectiveOperation operation,
+            final YangInstanceIdentifier path) {
+        final ChoiceNode editStructure;
+        try {
+            editStructure = netconfOps.createEditConfigStructure(Optional.empty(), Optional.of(operation), path);
+        } catch (IllegalArgumentException | IllegalStateException cause) {
+            LOG.error("Failed to create edit-config structure node with path {}, with RPC operation {}", path,
+                operation);
+            final var documentedException = new NetconfDocumentedException(
+                "Failed to create edit-config structure node for RPC operation", cause, ErrorType.APPLICATION,
+                ErrorTag.OPERATION_FAILED, ErrorSeverity.ERROR);
+            return Futures.immediateFailedFuture(documentedException);
+        }
+        final var callback = new NetconfRpcFutureCallback("Edit running", id);
+        LOG.trace("Execute editConfig operation {} on device {} with path {}", id, operation, path);
+        // FIXME: Set default-operation" parameter based on called DataStoreService method.
+        //        https://www.rfc-editor.org/rfc/rfc6241#section-7.2
+        final var edit = addLockBeforeFuture(() -> netconfOps.editConfigRunning(callback, editStructure,
+            rollbackSupport));
+        final var editWithUnlock = addUnlockAfterFuture(edit);
+        return addCancelIfFails(editWithUnlock);
+    }
+}
