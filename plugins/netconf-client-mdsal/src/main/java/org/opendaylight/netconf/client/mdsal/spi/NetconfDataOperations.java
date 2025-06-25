5eb555619246d8dd309de211d18c7b50989a343b
Peter Suna <peter.suna@pantheon.tech>
2025-06-25 14:27:30 +0200

    Create FieldsParamParser Helper class
    Translating FieldsParam into List<YangInstanceIdentifier> is separate
    logic which can be removed from NetconfDataOperation class.
    
    Separate this logic into FieldsParamParser class.
    
    JIRA: NETCONF-1352
    Change-Id: I9df50278042a4a7a6ba2d8e9c33c3b9ca2270558
    Signed-off-by: Peter Suna <peter.suna@pantheon.tech>

--- plugins/netconf-client-mdsal/src/main/java/org/opendaylight/netconf/client/mdsal/spi/NetconfDataOperations.java
+++ plugins/netconf-client-mdsal/src/main/java/org/opendaylight/netconf/client/mdsal/spi/NetconfDataOperations.java
@@ -8,11 +8,9 @@
 package org.opendaylight.netconf.client.mdsal.spi;
 
 import static com.google.common.base.Verify.verifyNotNull;
-import static java.util.Objects.requireNonNull;
 import static org.opendaylight.mdsal.common.api.LogicalDatastoreType.CONFIGURATION;
 import static org.opendaylight.yangtools.yang.data.impl.schema.ImmutableNodes.fromInstanceId;
 
-import com.google.common.annotations.VisibleForTesting;
 import com.google.common.util.concurrent.FutureCallback;
 import com.google.common.util.concurrent.Futures;
 import com.google.common.util.concurrent.ListenableFuture;
@@ -20,12 +18,9 @@ import com.google.common.util.concurrent.MoreExecutors;
 import com.google.common.util.concurrent.SettableFuture;
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.HashSet;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
-import java.util.Set;
 import java.util.StringJoiner;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.Executors;
@@ -46,7 +41,6 @@ import org.opendaylight.netconf.databind.RequestError;
 import org.opendaylight.netconf.databind.RequestException;
 import org.opendaylight.netconf.dom.api.NetconfDataTreeService;
 import org.opendaylight.restconf.api.ApiPath;
-import org.opendaylight.restconf.api.query.FieldsParam;
 import org.opendaylight.restconf.mdsal.spi.data.AsyncWithDefaultsProcessor;
 import org.opendaylight.restconf.mdsal.spi.data.RestconfStrategy;
 import org.opendaylight.restconf.server.api.CreateResourceResult;
@@ -70,8 +64,6 @@ import org.opendaylight.yangtools.yang.common.Empty;
 import org.opendaylight.yangtools.yang.common.ErrorSeverity;
 import org.opendaylight.yangtools.yang.common.ErrorTag;
 import org.opendaylight.yangtools.yang.common.ErrorType;
-import org.opendaylight.yangtools.yang.common.QName;
-import org.opendaylight.yangtools.yang.common.QNameModule;
 import org.opendaylight.yangtools.yang.common.RpcError;
 import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
 import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.NodeIdentifier;
@@ -81,8 +73,6 @@ import org.opendaylight.yangtools.yang.data.api.schema.LeafSetNode;
 import org.opendaylight.yangtools.yang.data.api.schema.MapNode;
 import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNode;
 import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNodeContainer;
-import org.opendaylight.yangtools.yang.data.util.DataSchemaContext;
-import org.opendaylight.yangtools.yang.model.api.EffectiveModelContext;
 import org.opendaylight.yangtools.yang.model.api.LeafListSchemaNode;
 import org.opendaylight.yangtools.yang.model.api.ListSchemaNode;
 import org.slf4j.Logger;
@@ -677,23 +667,24 @@ public class NetconfDataOperations extends AbstractServerDataOperations {
         //  type of data to read (config, state, all)
         final var content = params.content();
         // value of with-defaults parameter
-        final var fields = params.fields();
         final var processor = new AsyncWithDefaultsProcessor(path, params.withDefaults());
+        final var fieldsParser = new FieldsParamParser(path.inference().modelContext(), params.fields());
         return switch (content) {
             case ALL -> {
                 // PREPARE STATE DATA NODE
-                final var stateDataFuture = readDataViaTransaction(LogicalDatastoreType.OPERATIONAL, path, fields);
+                final var stateDataFuture = readDataViaTransaction(LogicalDatastoreType.OPERATIONAL, path,
+                    fieldsParser);
                 // PREPARE CONFIG DATA NODE
-                final var configDataFuture = readDataViaTransaction(CONFIGURATION, path, fields);
+                final var configDataFuture = readDataViaTransaction(CONFIGURATION, path, fieldsParser);
 
                 yield processor.all(configDataFuture, stateDataFuture);
             }
             case CONFIG -> {
-                final var configDataFuture = readDataViaTransaction(CONFIGURATION, path, fields);
+                final var configDataFuture = readDataViaTransaction(CONFIGURATION, path, fieldsParser);
                 yield processor.config(configDataFuture);
             }
             case NONCONFIG -> processor.nonConfig(
-                readDataViaTransaction(LogicalDatastoreType.OPERATIONAL, path, fields));
+                readDataViaTransaction(LogicalDatastoreType.OPERATIONAL, path, fieldsParser));
         };
     }
 
@@ -703,22 +694,15 @@ public class NetconfDataOperations extends AbstractServerDataOperations {
      *
      * @param store                 datastore type
      * @param path                  parent path to selected fields
-     * @param fields                paths to selected subtrees which should be read, relative to the parent path
+     * @param fieldsParser          FieldsParser prepared to read from starting node
      * @return {@link NormalizedNode}
      * @throws RequestException when an error occurs
      */
     private ListenableFuture<Optional<NormalizedNode>> readDataViaTransaction(final @NonNull LogicalDatastoreType store,
-            final @NonNull Data path, final FieldsParam fields) throws RequestException {
+            final @NonNull Data path, final FieldsParamParser fieldsParser) throws RequestException {
         // Paths to selected subtrees which should be read, relative to the parent path
-        final List<YangInstanceIdentifier> fieldPaths;
-        if (fields != null) {
-            final var tmp = fieldsParamToPaths(path.inference().modelContext(), path.schema(), fields);
-            fieldPaths = tmp.isEmpty() ? null : tmp;
-        } else {
-            fieldPaths = null;
-        }
-
-        if (fieldPaths != null) {
+        final var fieldPaths = fieldsParser.toPaths(path.schema());
+        if (!fieldPaths.isEmpty()) {
             return read(store, path, fieldPaths);
         }
         return read(store, path);
@@ -827,145 +811,6 @@ public class NetconfDataOperations extends AbstractServerDataOperations {
         return ret;
     }
 
-    /**
-     * Translate a {@link FieldsParam} to a list of child node paths saved in lists, suitable for use with
-     * {@link NetconfDataTreeService}.
-     *
-     * <p>Fields parser that stores a set of all the leaf {@link LinkedPathElement}s specified in {@link FieldsParam}.
-     * Using {@link LinkedPathElement} it is possible to create a chain of path arguments and build complete paths
-     * since this element contains identifiers of intermediary mixin nodes and also linked to its parent
-     * {@link LinkedPathElement}.
-     *
-     * <p>Example: field 'a(b/c;d/e)' ('e' is place under choice node 'x') is parsed into following levels:
-     * <pre>
-     *   - './a' +- 'a/b' - 'b/c'
-     *           |
-     *           +- 'a/d' - 'd/x/e'
-     * </pre>
-     *
-     *
-     * @param modelContext EffectiveModelContext
-     * @param startNode Root DataSchemaNode
-     * @param input input value of fields parameter
-     * @return {@link List} of {@link YangInstanceIdentifier} that are relative to the last {@link PathArgument}
-     *         of provided {@code identifier}
-     * @throws RequestException when an error occurs
-     */
-    @VisibleForTesting
-    public static @NonNull List<YangInstanceIdentifier> fieldsParamToPaths(
-            final @NonNull EffectiveModelContext modelContext, final @NonNull DataSchemaContext startNode,
-            final @NonNull FieldsParam input) throws RequestException {
-        final var parsed = new HashSet<LinkedPathElement>();
-        processSelectors(parsed, modelContext, startNode.dataSchemaNode().getQName().getModule(),
-            new LinkedPathElement(null, List.of(), startNode), input.nodeSelectors());
-        return parsed.stream().map(NetconfDataOperations::buildPath).toList();
-    }
-
-    private static void processSelectors(final Set<LinkedPathElement> parsed, final EffectiveModelContext context,
-            final QNameModule startNamespace, final LinkedPathElement startPathElement,
-            final List<FieldsParam.NodeSelector> selectors) throws RequestException {
-        for (var selector : selectors) {
-            var pathElement = startPathElement;
-            var namespace = startNamespace;
-
-            // Note: path is guaranteed to have at least one step
-            final var it = selector.path().iterator();
-            do {
-                final var step = it.next();
-                final var module = step.module();
-                if (module != null) {
-                    // FIXME: this is not defensive enough, as we can fail to find the module
-                    namespace = context.findModules(module).iterator().next().getQNameModule();
-                }
-
-                // add parsed path element linked to its parent
-                pathElement = addChildPathElement(pathElement, step.identifier().bindTo(namespace));
-            } while (it.hasNext());
-
-            final var subs = selector.subSelectors();
-            if (!subs.isEmpty()) {
-                processSelectors(parsed, context, namespace, pathElement, subs);
-            } else {
-                parsed.add(pathElement);
-            }
-        }
-    }
-
-    private static LinkedPathElement addChildPathElement(final LinkedPathElement currentElement,
-            final QName childQName) throws RequestException {
-        final var collectedMixinNodes = new ArrayList<PathArgument>();
-
-        final var currentNode = currentElement.targetNode;
-        var actualContextNode = childByQName(currentNode, childQName);
-        if (actualContextNode == null) {
-            actualContextNode = resolveMixinNode(currentNode, currentNode.getPathStep().getNodeType());
-            actualContextNode = childByQName(actualContextNode, childQName);
-        }
-
-        while (actualContextNode instanceof DataSchemaContext.PathMixin) {
-            final var actualDataSchemaNode = actualContextNode.dataSchemaNode();
-            if (actualDataSchemaNode instanceof ListSchemaNode listSchema && listSchema.getKeyDefinition().isEmpty()) {
-                // we need just a single node identifier from list in the path IFF it is an unkeyed list, otherwise
-                // we need both (which is the default case)
-                actualContextNode = childByQName(actualContextNode, childQName);
-            } else if (actualDataSchemaNode instanceof LeafListSchemaNode) {
-                // NodeWithValue is unusable - stop parsing
-                break;
-            } else {
-                collectedMixinNodes.add(actualContextNode.getPathStep());
-                actualContextNode = childByQName(actualContextNode, childQName);
-            }
-        }
-
-        if (actualContextNode == null) {
-            throw new RequestException(ErrorType.PROTOCOL, ErrorTag.INVALID_VALUE, "Child %s node missing in %s",
-                childQName.getLocalName(), currentNode.getPathStep().getNodeType().getLocalName());
-        }
-
-        return new LinkedPathElement(currentElement, collectedMixinNodes, actualContextNode);
-    }
-
-    private static @Nullable DataSchemaContext childByQName(final DataSchemaContext parent, final QName qname) {
-        return parent instanceof DataSchemaContext.Composite composite ? composite.childByQName(qname) : null;
-    }
-
-    private static YangInstanceIdentifier buildPath(final LinkedPathElement lastPathElement) {
-        var pathElement = lastPathElement;
-        final var path = new LinkedList<PathArgument>();
-        do {
-            path.addFirst(contextPathArgument(pathElement.targetNode));
-            path.addAll(0, pathElement.mixinNodesToTarget);
-            pathElement = pathElement.parentPathElement;
-        } while (pathElement.parentPathElement != null);
-
-        return YangInstanceIdentifier.of(path);
-    }
-
-    private static @NonNull PathArgument contextPathArgument(final DataSchemaContext context) {
-        final var arg = context.pathStep();
-        if (arg != null) {
-            return arg;
-        }
-
-        final var schema = context.dataSchemaNode();
-        if (schema instanceof ListSchemaNode listSchema && !listSchema.getKeyDefinition().isEmpty()) {
-            return NodeIdentifierWithPredicates.of(listSchema.getQName());
-        }
-        if (schema instanceof LeafListSchemaNode leafListSchema) {
-            return new YangInstanceIdentifier.NodeWithValue<>(leafListSchema.getQName(), Empty.value());
-        }
-        throw new UnsupportedOperationException("Unsupported schema " + schema);
-    }
-
-    private static DataSchemaContext resolveMixinNode(final DataSchemaContext node,
-            final @NonNull QName qualifiedName) {
-        DataSchemaContext currentNode = node;
-        while (currentNode != null && currentNode instanceof DataSchemaContext.PathMixin currentMixin) {
-            currentNode = currentMixin.childByQName(qualifiedName);
-        }
-        return currentNode;
-    }
-
     private static ListenableFuture<? extends DOMRpcResult> chainPatchEditTransaction(
         final ListenableFuture<? extends DOMRpcResult> futureChain,
         final Supplier<ListenableFuture<? extends DOMRpcResult>> nextFuture,
@@ -1028,28 +873,6 @@ public class NetconfDataOperations extends AbstractServerDataOperations {
         } else {
             throw new RequestException(ErrorType.PROTOCOL, ErrorTag.BAD_ELEMENT,
                 "Insert parameter can be used only with list or leaf-list");
-        }
-    }
-
-    /**
-     * {@link DataSchemaContext} of data element grouped with identifiers of leading mixin nodes and previous path
-     * element.<br>
-     *  - identifiers of mixin nodes on the path to the target node - required for construction of full valid
-     *    DOM paths,<br>
-     *  - {@link LinkedPathElement} of the previous non-mixin node - required to successfully create a chain
-     *    of {@link PathArgument}s
-     *
-     * @param parentPathElement     parent path element
-     * @param mixinNodesToTarget    identifiers of mixin nodes on the path to the target node
-     * @param targetNode            target non-mixin node
-     */
-    private record LinkedPathElement(
-        @Nullable LinkedPathElement parentPathElement,
-        @NonNull List<PathArgument> mixinNodesToTarget,
-        @NonNull DataSchemaContext targetNode) {
-        LinkedPathElement {
-            requireNonNull(mixinNodesToTarget);
-            requireNonNull(targetNode);
         }
     }
 }
