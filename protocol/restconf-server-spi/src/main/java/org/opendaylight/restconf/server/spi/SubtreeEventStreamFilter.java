41fde8846970081cb0faa978b0ececd87e91103b
Matej Sramcik <matej.sramcik@pantheon.tech>
2025-06-26 13:59:02 +0300

    Reconcile EventFilter and EventStreamFilter
    SubtreeEventStreamFilter now extends EventFilter and all filter
    implement #matches method. Subscriber now holds only EventFilter<T>.
    Path and body extraction is now done in SubtreeEventStreamFilter.
    
    JIRA: NETCONF-1479
    Change-Id: Ie76aeb437c943f0929bef67f19df3c7d66ba50da
    Signed-off-by: Yaroslav Lastivka <yaroslav.lastivka@pantheon.tech>
    Signed-off-by: Matej Sramcik <matej.sramcik@pantheon.tech>

--- protocol/restconf-server-spi/src/main/java/org/opendaylight/restconf/server/spi/SubtreeEventStreamFilter.java
+++ protocol/restconf-server-spi/src/main/java/org/opendaylight/restconf/server/spi/SubtreeEventStreamFilter.java
@@ -9,12 +9,20 @@ package org.opendaylight.restconf.server.spi;
 
 import static org.opendaylight.netconf.databind.subtree.SubtreeMatcher.permitsPath;
 
+import com.google.common.base.MoreObjects;
+import java.util.List;
+import org.opendaylight.mdsal.dom.api.DOMNotification;
+import org.opendaylight.netconf.common.mdsal.DOMNotificationEvent;
 import org.opendaylight.netconf.databind.subtree.SubtreeFilter;
 import org.opendaylight.netconf.databind.subtree.SubtreeMatcher;
 import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
 import org.opendaylight.yangtools.yang.data.api.schema.ContainerNode;
+import org.opendaylight.yangtools.yang.data.spi.node.ImmutableNodes;
+import org.opendaylight.yangtools.yang.data.tree.api.DataTreeCandidate;
+import org.opendaylight.yangtools.yang.model.api.EffectiveModelContext;
 
-public final class SubtreeEventStreamFilter implements AbstractRestconfStreamRegistry.EventStreamFilter {
+public final class SubtreeEventStreamFilter<T> extends EventFilter<T>
+        implements AbstractRestconfStreamRegistry.EventStreamFilter {
     private final SubtreeFilter filter;
 
     public SubtreeEventStreamFilter(final SubtreeFilter filter) {
@@ -24,5 +32,53 @@ public final class SubtreeEventStreamFilter implements AbstractRestconfStreamReg
     @Override
     public boolean test(final YangInstanceIdentifier path, final ContainerNode body) {
         return permitsPath(filter, path) && new SubtreeMatcher(filter, body, path).matches();
+    }
+
+    @Override
+    boolean matches(final EffectiveModelContext modelContext, final T event) {
+        final var path = extractPath(event);
+        final var body  = extractBody(event);
+        return path != null && body != null && test(path, body);
+    }
+
+    private static <T> YangInstanceIdentifier extractPath(final T event) {
+        return switch (event) {
+            case DOMNotificationEvent.Rfc7950 n7950 -> n7950.path();
+            case DOMNotificationEvent.Rfc6020 ignored -> YangInstanceIdentifier.of();
+            case DOMNotification ignored -> YangInstanceIdentifier.of();
+            case List<?> list when !list.isEmpty() && list.getFirst() instanceof DataTreeCandidate ->
+                YangInstanceIdentifier.of();
+            default -> null;
+        };
+    }
+
+
+    @SuppressWarnings("unchecked")
+    private static <T> ContainerNode extractBody(T event) {
+        return switch (event) {
+            case DOMNotificationEvent.Rfc7950 n7950 -> n7950.getBody();
+            case DOMNotificationEvent.Rfc6020 n6020 -> n6020.getBody();
+            case DOMNotification notification -> notification.getBody();
+            case List<?> list when !list.isEmpty() && list.getFirst() instanceof DataTreeCandidate candidates ->
+                parseDataTreeCandidates((List<DataTreeCandidate>) candidates);
+            default -> null;
+        };
+    }
+
+    private static ContainerNode parseDataTreeCandidates(final List<DataTreeCandidate> candidates) {
+        final var firstArg = candidates.getFirst().getRootPath().getLastPathArgument();
+        final var rootId = firstArg instanceof YangInstanceIdentifier.NodeIdentifier nip ? nip :
+            YangInstanceIdentifier.NodeIdentifier.create(firstArg.getNodeType());
+        final var builder = ImmutableNodes.newContainerBuilder().withNodeIdentifier(rootId);
+
+        for (final DataTreeCandidate cand : candidates) {
+            builder.withChild((ContainerNode) cand.getRootNode().dataAfter());
+        }
+        return builder.build();
+    }
+
+    @Override
+    protected MoreObjects.ToStringHelper addToStringAttributes(final MoreObjects.ToStringHelper helper) {
+        return helper;
     }
 }
