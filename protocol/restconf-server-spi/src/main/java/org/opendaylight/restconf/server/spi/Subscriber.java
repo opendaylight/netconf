dfe6ba52c9ef867680fad37a99a95bf96f55b592
Matej Sramcik <matej.sramcik@pantheon.tech>
2025-07-16 13:59:41 +0200

    Add State as parameter of Subscriber
    Replaced hardcoded state by Subscriber parameter that can be modified.
    
    JIRA: NETCONF-1482
    Change-Id: I110c3be0db74e4316f0824e8bac7650b447178ed
    Signed-off-by: Matej Sramcik <matej.sramcik@pantheon.tech>

--- protocol/restconf-server-spi/src/main/java/org/opendaylight/restconf/server/spi/Subscriber.java
+++ protocol/restconf-server-spi/src/main/java/org/opendaylight/restconf/server/spi/Subscriber.java
@@ -16,6 +16,7 @@ import java.lang.invoke.VarHandle;
 import org.eclipse.jdt.annotation.NonNullByDefault;
 import org.eclipse.jdt.annotation.Nullable;
 import org.opendaylight.restconf.server.spi.RestconfStream.Sender;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.subscribed.notifications.rev190909.subscriptions.subscription.receivers.Receiver;
 import org.opendaylight.yangtools.concepts.AbstractRegistration;
 import org.opendaylight.yangtools.yang.common.Uint64;
 
@@ -73,12 +74,14 @@ abstract sealed class Subscriber<T> extends AbstractRegistration {
         private long excludedEventRecords;
         @SuppressFBWarnings(value = "UUF_UNUSED_FIELD")
         private long sentEventRecords;
+        private Receiver.State receiverState;
 
         Rfc8639Subscriber(final RestconfStream<T> stream, final Sender sender, final EventFormatter<T> formatter,
                 final EventFilter<T> filter, final String receiverName,
                 @Nullable final EventStreamFilter eventStreamFilter) {
             super(stream, sender, formatter, filter);
             this.receiverName = requireNonNull(receiverName);
+            this.receiverState = Receiver.State.Active;
             this.eventStreamFilter = eventStreamFilter;
         }
 
@@ -96,6 +99,10 @@ abstract sealed class Subscriber<T> extends AbstractRegistration {
 
         String receiverName() {
             return receiverName;
+        }
+
+        Receiver.State receiverState() {
+            return receiverState;
         }
 
         Uint64 excludedEventRecords() {
@@ -116,6 +123,10 @@ abstract sealed class Subscriber<T> extends AbstractRegistration {
 
         public void setEventStreamFilter(final EventStreamFilter newEventStreamFilter) {
             this.eventStreamFilter = newEventStreamFilter;
+        }
+
+        public void setReceiverState(final Receiver.State newState) {
+            receiverState = newState;
         }
     }
 
