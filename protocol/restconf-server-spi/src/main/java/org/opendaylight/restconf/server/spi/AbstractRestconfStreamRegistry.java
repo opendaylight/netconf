192bdc7b66af3a003c62dabf7352da2c7312909b
Matej Sramcik <matej.sramcik@pantheon.tech>
2025-07-28 17:28:39 +0200

    Add test for subscription suspension
    Add test to verify functionality of suspending and resuming
    subscriptions.
    
    JIRA: NETCONF-1482
    Change-Id: Ic36ad8e03536dc768343ab9121208c1d1a2f1da7
    Signed-off-by: Matej Sramcik <matej.sramcik@pantheon.tech>

--- protocol/restconf-server-spi/src/main/java/org/opendaylight/restconf/server/spi/AbstractRestconfStreamRegistry.java
+++ protocol/restconf-server-spi/src/main/java/org/opendaylight/restconf/server/spi/AbstractRestconfStreamRegistry.java
@@ -288,7 +288,7 @@ public abstract class AbstractRestconfStreamRegistry implements RestconfStream.R
 
 
         @Override
-        void suspendSubscription(final QName reason) {
+        public void suspendSubscription(final QName reason) {
             Futures.addCallback(updateReceiverState(State.Suspended), new FutureCallback<>() {
                 @Override
                 public void onSuccess(Void result) {
@@ -304,7 +304,7 @@ public abstract class AbstractRestconfStreamRegistry implements RestconfStream.R
         }
 
         @Override
-        void resumeSubscription() {
+        public void resumeSubscription() {
             Futures.addCallback(updateReceiverState(State.Active), new FutureCallback<>() {
                 @Override
                 public void onSuccess(Void result) {
