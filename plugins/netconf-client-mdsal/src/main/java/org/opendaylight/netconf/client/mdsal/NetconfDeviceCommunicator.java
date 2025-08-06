2e360c232ab84d277cf7503bb9e8c74c624238de
Peter Suna <peter.suna@pantheon.tech>
2025-08-06 11:21:02 +0200

    Add fairness policy into NetconfDeviceCommunicator
    When multiple threads request access to a sessionLock, permission
    is granted randomly and not in the order of who requested it first.
    
    JIRA: NETCONF-1499
    Change-Id: I5fb6621abcf867038ec975f5d41e68f015e9b349
    Signed-off-by: Peter Suna <peter.suna@pantheon.tech>

--- plugins/netconf-client-mdsal/src/main/java/org/opendaylight/netconf/client/mdsal/NetconfDeviceCommunicator.java
+++ plugins/netconf-client-mdsal/src/main/java/org/opendaylight/netconf/client/mdsal/NetconfDeviceCommunicator.java
@@ -72,7 +72,7 @@ public class NetconfDeviceCommunicator implements NetconfClientSessionListener, 
     protected final RemoteDevice<NetconfDeviceCommunicator> remoteDevice;
     private final @Nullable UserPreferences overrideNetconfCapabilities;
     protected final RemoteDeviceId id;
-    private final Lock sessionLock = new ReentrantLock();
+    private final Lock sessionLock = new ReentrantLock(true);
 
     private final Semaphore semaphore;
     private final int concurentRpcMsgs;
