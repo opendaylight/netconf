ae895bda9ad71cdbc727a58f1defc1d5dd881d44
Peter Suna <peter.suna@pantheon.tech>
2025-08-15 11:47:54 +0200

    Use request timeout for all netconf device RPCs
    The MonitoringSchemaSourceProvider uses direct NetconfDeviceRpc,
    which does not check for user specified time-out on request to device.
    This can lead to hanging requests in case of device failure.
    
    Similar issue happends when keepalive-delay is set to 0. Since
    the KeepaliveSalFacade is the only instance which sets
    the default-request-timeout-millis for RPC requests,
    Disabling it also disables request time-out.
    
    This commit introduces the RpcsTimeoutAndRecoveryHandler for handling
    the RPC requests. This handler provides a decorator method for
    the Rpcs instance. This decorator adds a user-defined
    requestTimeoutMillis to the RPCs. In case of an RPCs failure or
    expired timeout, it closes the NetconfDeviceCommunicator,
    which will initiate a device reconnection.
    
    Preparing the RpcsTimeoutAndRecoveryHandler in NetconfNodeHandler
    allows this logic to be separated from KeepaliveSalFacade and
    applied easily by decorating NetconfDeviceRpc when it is needed.
    
    JIRA: NETCONF-1501
    Change-Id: I28ee9d3ecd3f4e62418fd347064c72688a519dce
    Signed-off-by: Peter Suna <peter.suna@pantheon.tech>

---
+++ plugins/netconf-client-mdsal/src/main/java/org/opendaylight/netconf/client/mdsal/api/RpcsHandler.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2025 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.netconf.client.mdsal.api;
+
+import org.opendaylight.netconf.client.mdsal.api.RemoteDeviceServices.Rpcs;
+
+/**
+ * Handle RPCs, adding required logic to RPCs such a request time-out.
+ */
+public interface RpcsHandler {
+
+    /**
+     * Wrap the given Rpcs service with the required logic.
+     *
+     *  @param service {@link Rpcs}
+     */
+    Rpcs decorateRpcs(Rpcs service);
+}
