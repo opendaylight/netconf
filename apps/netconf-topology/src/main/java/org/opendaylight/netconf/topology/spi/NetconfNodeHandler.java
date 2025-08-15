24a25d39c33de610558b7be5c476f0aa81843024
Peter Suna <peter.suna@pantheon.tech>
2025-08-15 11:47:54 +0200

    Use request timeout for all netconf device RPCs
    The MonitoringSchemaSourceProvider uses direct NetconfDeviceRpc,
    which does not check for user specified time-out on request to device.
    This can lead to hanging request issues in case of device failure.
    
    This commit introduces the InitialRpcsHandler for handling the initial
    request. This handler provides a decorator method for the Rpcs instance.
    This decorator adds a user-defined timeout to the RPCs. In case of
    an RPCs failure or timeout, it closes the NetconfDeviceCommunicator,
    which will initiate a device reconnection.
    
    Additionally, the InitialRpcTimeoutHandler provides
    the RemoteDeviceHandler instance, which removes duplicity during
    the initialization process.
    
    JIRA: NETCONF-1501
    Change-Id: I28ee9d3ecd3f4e62418fd347064c72688a519dce
    Signed-off-by: Peter Suna <peter.suna@pantheon.tech>

--- apps/netconf-topology/src/main/java/org/opendaylight/netconf/topology/spi/NetconfNodeHandler.java
+++ apps/netconf-topology/src/main/java/org/opendaylight/netconf/topology/spi/NetconfNodeHandler.java
@@ -31,6 +31,7 @@ import org.opendaylight.netconf.client.mdsal.LibrarySchemaSourceProvider;
 import org.opendaylight.netconf.client.mdsal.NetconfDeviceBuilder;
 import org.opendaylight.netconf.client.mdsal.NetconfDeviceCommunicator;
 import org.opendaylight.netconf.client.mdsal.NetconfDeviceSchema;
+import org.opendaylight.netconf.client.mdsal.RpcsTimeoutAndRecoveryHandler;
 import org.opendaylight.netconf.client.mdsal.SchemalessNetconfDevice;
 import org.opendaylight.netconf.client.mdsal.api.BaseNetconfSchemaProvider;
 import org.opendaylight.netconf.client.mdsal.api.DeviceActionFactory;
@@ -161,16 +162,17 @@ public final class NetconfNodeHandler extends AbstractRegistration implements Re
         // The facade we are going it present to NetconfDevice
         RemoteDeviceHandler salFacade;
         final KeepaliveSalFacade keepAliveFacade;
-        final long keepaliveDelay = node.requireKeepaliveDelay().toJava();
+        final var keepaliveDelay = node.requireKeepaliveDelay().toJava();
         if (keepaliveDelay > 0) {
             LOG.info("Adding keepalive facade, for device {}", nodeId);
-            salFacade = keepAliveFacade = new KeepaliveSalFacade(deviceId, this, timer, keepaliveDelay,
-                node.requireDefaultRequestTimeoutMillis().toJava());
+            salFacade = keepAliveFacade = new KeepaliveSalFacade(deviceId, this, timer, keepaliveDelay);
         } else {
             salFacade = this;
             keepAliveFacade = null;
         }
 
+        final var defaultRequestTimeout = node.requireDefaultRequestTimeoutMillis().toJava();
+        final var rpcsHandler = new RpcsTimeoutAndRecoveryHandler(deviceId, timer, defaultRequestTimeout);
         final RemoteDevice<NetconfDeviceCommunicator> device;
         if (node.requireSchemaless()) {
             device = new SchemalessNetconfDevice(baseSchemaProvider, deviceId, salFacade);
@@ -184,6 +186,7 @@ public final class NetconfNodeHandler extends AbstractRegistration implements Re
                 .setProcessingExecutor(schemaAssembler.executor())
                 .setId(deviceId)
                 .setSalFacade(salFacade)
+                .setRpcsHandler(rpcsHandler)
                 .setDeviceActionFactory(deviceActionFactory)
                 .build();
             yanglibRegistrations = registerDeviceSchemaSources(deviceId, node, resources);
@@ -196,7 +199,7 @@ public final class NetconfNodeHandler extends AbstractRegistration implements Re
 
         communicator = new NetconfDeviceCommunicator(deviceId, device, rpcMessageLimit,
             NetconfNodeUtils.extractUserCapabilities(node));
-
+        rpcsHandler.setListener(communicator);
         if (keepAliveFacade != null) {
             keepAliveFacade.setListener(communicator);
         }
@@ -282,7 +285,9 @@ public final class NetconfNodeHandler extends AbstractRegistration implements Re
         synchronized (this) {
             attempts = 0;
         }
-        delegate.onDeviceConnected(deviceSchema, sessionPreferences, services);
+
+        delegate.onDeviceConnected(deviceSchema, sessionPreferences, new RemoteDeviceServices(services.rpcs(),
+            services.actions()));
     }
 
     @Override
