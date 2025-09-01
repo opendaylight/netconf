2741b3dbb2f1b0224d51392a5e6bfc8d14aa0d41
Ivan Hrasko <ivan.hrasko@pantheon.tech>
2025-09-01 12:39:08 +0200

    Bump mina-sshd to 2.16.0
    https://github.com/apache/mina-sshd/releases/tag/sshd-2.16.0
    
    Remove workaround from TransportIoService to assign channelGroup since
    that is now done by NettyIoService constructor.
    See also:
    https://github.com/apache/mina-sshd/issues/663
    
    Change-Id: Ib6f3e29695cbe5f1661b46a88a1c77b48f962799
    Signed-off-by: Ivan Hrasko <ivan.hrasko@pantheon.tech>

--- transport/transport-ssh/src/main/java/org/opendaylight/netconf/transport/ssh/TransportIoService.java
+++ transport/transport-ssh/src/main/java/org/opendaylight/netconf/transport/ssh/TransportIoService.java
@@ -15,16 +15,7 @@ import org.opendaylight.netconf.shaded.sshd.netty.NettyIoServiceFactory;
 
 final class TransportIoService extends NettyIoService {
     TransportIoService(final FactoryManager factoryManager, final IoHandler handler) {
-        super((NettyIoServiceFactory) factoryManager.getIoServiceFactory(), handler);
-
-        // Required to keep things working, but does not need to be functional at all.
-        //
-        // Under normal circumstances, this group is used to track all open channels, so that we can close them when
-        // we are shutdown.
-        //
-        // We do not need to do that, as we track the underlying transport instead, hence any channels tracked here will
-        // disappear when we shut down the underlay.
-        channelGroup = CompatChannelGroup.INSTANCE;
+        super((NettyIoServiceFactory) factoryManager.getIoServiceFactory(), handler, "transport-io-session-channels");
     }
 
     TransportIoSession createSession(final SocketAddress acceptanceAddress) {
