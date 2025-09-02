195a377964d1f0c34bbe96fa8b324fa8e85ab3a2
Matej Sramcik <matej.sramcik@pantheon.tech>
2025-09-02 11:34:10 +0200

    Fix pipelining test
    Removed delayed request processing, which was unreliable and caused
    sporadic test failures.
    Switched to a manual approach: dispatch requests via channelRead0() and
    complete them with notifyRequestFinished().
    
    JIRA: NETCONF-1496
    Change-Id: I0d1f4bf62e613375861f09b7eed01bd96bec8c32
    Signed-off-by: Matej Sramcik <matej.sramcik@pantheon.tech>

--- protocol/restconf-server/src/test/java/org/opendaylight/restconf/server/AbstractRequestProcessorTest.java
+++ protocol/restconf-server/src/test/java/org/opendaylight/restconf/server/AbstractRequestProcessorTest.java
@@ -14,7 +14,6 @@ import static org.mockito.Mockito.atLeastOnce;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.timeout;
 import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 import static org.opendaylight.restconf.server.TestUtils.ERROR_TAG_MAPPING;
 
 import io.netty.buffer.UnpooledByteBufAllocator;
@@ -27,7 +26,6 @@ import io.netty.handler.codec.http.DefaultLastHttpContent;
 import io.netty.handler.codec.http.FullHttpRequest;
 import io.netty.handler.codec.http.FullHttpResponse;
 import io.netty.handler.codec.http.HttpObject;
-import io.netty.util.concurrent.EventExecutor;
 import io.netty.util.concurrent.ImmediateEventExecutor;
 import java.net.InetSocketAddress;
 import java.net.URI;
@@ -54,8 +52,7 @@ import org.opendaylight.restconf.server.spi.RestconfStream;
 
 @ExtendWith(MockitoExtension.class)
 class AbstractRequestProcessorTest {
-    private static final PrettyPrintParam PRETTY_PRINT = PrettyPrintParam.FALSE;
-
+    protected static final PrettyPrintParam PRETTY_PRINT = PrettyPrintParam.FALSE;
     protected static final String BASE_PATH = "/rests";
     protected static final String HOST = "somehost:1234";
     protected static final URI BASE_URI = URI.create("http://" + HOST + BASE_PATH);
@@ -66,6 +63,7 @@ class AbstractRequestProcessorTest {
     protected static final TestEncoding DEFAULT_ENCODING = TestEncoding.JSON;
     protected static final String XML_CONTENT = "xml-content";
     protected static final String JSON_CONTENT = "json-content";
+    protected static final WellKnownResources WELL_KNOWN = new WellKnownResources(BASE_PATH);
 
     protected static final ApiPath API_PATH;
     protected static final ApiPath NEW_API_PATH;
@@ -80,8 +78,6 @@ class AbstractRequestProcessorTest {
             throw new ExceptionInInitializerError(e);
         }
     }
-
-    private static final WellKnownResources WELL_KNOWN = new WellKnownResources(BASE_PATH);
 
     @Mock
     protected RestconfServer server;
@@ -121,10 +117,6 @@ class AbstractRequestProcessorTest {
 
     void mockSession() {
         doReturn(session).when(pipeline).get(HTTPServerSession.class);
-    }
-
-    void mockExecutor(final EventExecutor executor) {
-        when(ctx.executor()).thenReturn(executor);
     }
 
     void writableResponseWriter() {
@@ -171,9 +163,5 @@ class AbstractRequestProcessorTest {
             Arguments.of(TestEncoding.JSON, JSON_CONTENT),
             Arguments.of(TestEncoding.XML, XML_CONTENT)
         );
-    }
-
-    int blockedRequestsSize() {
-        return session.blockedRequestsSize();
     }
 }
