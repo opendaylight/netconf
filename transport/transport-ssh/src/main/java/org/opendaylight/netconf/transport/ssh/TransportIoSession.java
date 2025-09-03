b6e9e8c6dc28bf62beffbbbd3e1faa2c4f855a75
Ivan Hrasko <ivan.hrasko@pantheon.tech>
2025-09-03 09:27:16 +0200

    Reimplement TransportIoSession
    If we do not want to use NettyIoService we have problems to invoke
    NettyIoSession constructor in ODL's TransportIoSession -
    service is a parameter for session.
    
    The purpose of TransportIoSession is to provide us with
    NettyIoSession's Adapter which is required in SSHTransportStack#
    onUnderlayChannelEstablished.
    
    This adapter is ChannelInboundHandlerAdapter implementation operating
    with IOHandler which operates on IoSession.
    
    According to SSHTransportStack docs Adapter is used to route traffic
    from socket to TransportSshServer/Client.
    
    Options:
    0. Assert that we need an implementation of IoSession.
    1. Let TransportIoSession be child of NettyIoSession and override
    methods using NettyIoService (channelActive and channelInactive).
    2. Let TransportIoSession implements IoSession (requires more work) and
    reimplement NettyIoSession.Adapter as well.
    
    Choosen option is #1.
    
    JIRA: NETCONF-1504
    Change-Id: I41490a287587a4fcacdbf6fd3f93fb6d53158829
    Signed-off-by: Ivan Hrasko <ivan.hrasko@pantheon.tech>

--- transport/transport-ssh/src/main/java/org/opendaylight/netconf/transport/ssh/TransportIoSession.java
+++ transport/transport-ssh/src/main/java/org/opendaylight/netconf/transport/ssh/TransportIoSession.java
@@ -11,10 +11,12 @@ import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandler;
 import java.net.SocketAddress;
 import org.opendaylight.netconf.shaded.sshd.common.io.IoHandler;
+import org.opendaylight.netconf.shaded.sshd.common.session.helpers.MissingAttachedSessionException;
+import org.opendaylight.netconf.shaded.sshd.netty.NettyIoService;
 import org.opendaylight.netconf.shaded.sshd.netty.NettyIoSession;
 
 final class TransportIoSession extends NettyIoSession {
-    TransportIoSession(final TransportIoService service, final IoHandler handler,
+    TransportIoSession(final NettyIoService service, final IoHandler handler,
             final SocketAddress acceptanceAddress) {
         super(service, handler, acceptanceAddress);
     }
@@ -23,11 +25,68 @@ final class TransportIoSession extends NettyIoSession {
         return adapter;
     }
 
+    @SuppressWarnings("checkstyle:IllegalCatch")
+    @Override
+    protected void channelActive(final ChannelHandlerContext ctx) throws Exception {
+        context = ctx;
+        final var channel = ctx.channel();
+        prev = context.newPromise().setSuccess();
+        remoteAddr = channel.remoteAddress();
+        // If handler.sessionCreated() propagates an exception, we'll have a NettyIoSession without SSH session. We'll
+        // propagate the exception, exceptionCaught will be called, which won't find an SSH session to handle the
+        // exception and propagate a MissingAttachedSessionException. However, Netty will swallow and log exceptions
+        // propagated out of exceptionCaught. This will lead to follow-up exceptions.
+        //
+        // We have to close the NettyIoSession in this case.
+        final var connectFuture = channel.attr(NettyIoService.CONNECT_FUTURE_KEY);
+        final var future = connectFuture.get();
+        try {
+            handler.sessionCreated(this);
+            if (future != null) {
+                future.setSession(this);
+                if (future.getSession() != this) {
+                    close(true);
+                }
+            }
+        } catch (Throwable e) {
+            warn("channelActive(session={}): could not create SSH session ({}); closing",
+                this, e.getClass().getName(), e);
+            try {
+                if (future != null) {
+                    future.setException(e);
+                }
+            } finally {
+                close(true);
+            }
+        }
+    }
+
     @Override
     protected void channelInactive(final ChannelHandlerContext ctx) throws Exception {
+        try {
+            handler.sessionClosed(this);
+        } catch (MissingAttachedSessionException e) {
+            // handler.sessionClosed() is supposed to close the attached SSH session. If there isn't one,
+            // we don't care anymore at this point.
+            if (log.isTraceEnabled()) {
+                log.trace("channelInactive(session={}): caught {}", this, e.getClass().getName(), e);
+            }
+        } finally {
+            final var channel = ctx.channel();
+            final var connectFuture = channel.attr(NettyIoService.CONNECT_FUTURE_KEY);
+            final var future = connectFuture.get();
+            if (future != null) {
+                // If the future wasn't fulfilled already cancel it.
+                final var cancellation = future.cancel();
+                if (cancellation != null) {
+                    cancellation.setCanceled();
+                }
+            }
+            context = null;
+        }
+
         // adapter is not propagating fireChannelInactive() down the the pipeline, but instead loops here. Once we have
         // cleaned up, propagate fireChannelInactive() outselves.
-        super.channelInactive(ctx);
         ctx.fireChannelInactive();
     }
 }
