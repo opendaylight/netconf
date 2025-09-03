0d81347d33216c0abf5a6c79f4cb5bda06189b1f
Ivan Hrasko <ivan.hrasko@pantheon.tech>
2025-09-03 09:27:16 +0200

    Reimplement TransportSession
    If we do not want to use NettyIoService we have problems to invoke
    NettyIoSession constructor in ODL's TransportIoSession -
    service is a parameter for session.
    
    The purpose of TransportIoSession is to provide us with
    NettyIoSession's Adapter which is required in SSHTransportStack#
    onUnderlayChannelEstablished.
    
    This adapter is ChannelInboundHandlerAdapter implementation operating
    with IOHandler which operates on IoSession.
    
    According to SSHTransportStack docs Adapter is used to route traffic
    from socket to TransportSshServer/Client.
    
    Options:
    0. Assert that we need an implementation of IoSession.
    1. Let TransportIoSession be child of NettyIoSession and override
    methods using NettyIoService (channelActive and channelInactive).
    2. Let TransportIoSession implements IoSession (requires more work) and
    reimplement NettyIoSession.Adapter as well.
    
    Change-Id: I41490a287587a4fcacdbf6fd3f93fb6d53158829
    Signed-off-by: Ivan Hrasko <ivan.hrasko@pantheon.tech>

--- transport/transport-ssh/src/main/java/org/opendaylight/netconf/transport/ssh/TransportIoService.java
+++ transport/transport-ssh/src/main/java/org/opendaylight/netconf/transport/ssh/TransportIoService.java
@@ -7,34 +7,16 @@
  */
 package org.opendaylight.netconf.transport.ssh;
 
-import io.netty.channel.Channel;
-import java.net.SocketAddress;
-import java.util.concurrent.CancellationException;
-import org.opendaylight.netconf.shaded.sshd.common.FactoryManager;
 import org.opendaylight.netconf.shaded.sshd.common.io.IoHandler;
-import org.opendaylight.netconf.shaded.sshd.netty.NettyIoService;
-import org.opendaylight.netconf.shaded.sshd.netty.NettyIoServiceFactory;
 
-final class TransportIoService extends NettyIoService {
-    TransportIoService(final FactoryManager factoryManager, final IoHandler handler) {
-        super((NettyIoServiceFactory) factoryManager.getIoServiceFactory(), handler, "sshd-transport-channels");
+final class TransportIoService {
+    private final IoHandler handler;
+
+    TransportIoService(final IoHandler handler) {
+        this.handler = handler;
     }
 
-    @Override
-    protected void registerChannel(final Channel channel) throws CancellationException {
-        // Required to keep things working, but does not need to be functional at all.
-        //
-        // Under normal circumstances, NettyIoService's group is used to track all open channels, so that we can close
-        // them when we are shutdown.
-        //
-        // We do not need to do that, as we track the underlying transport instead, hence any channels tracked here will
-        // disappear when we shut down the underlay.
-        // Thus, we even do not need to check NettyIoService.noMoreSessions property.
-
-        // no-op
-    }
-
-    TransportIoSession createSession(final SocketAddress acceptanceAddress) {
-        return new TransportIoSession(this, handler, acceptanceAddress);
+    TransportIoSession createSession() {
+        return new TransportIoSession(handler);
     }
 }
