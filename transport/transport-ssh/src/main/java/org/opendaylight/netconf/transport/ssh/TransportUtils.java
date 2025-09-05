8e43adb656ab6fa410f26bc48612c6f8dbb1a65c
Robert Varga <robert.varga@pantheon.tech>
2025-09-05 23:19:25 +0200

    Add TransportSubsystem
    This factors out TransportUtils.ChannelInactive and attachUnderlay() is
    supports into a separate separate interface, TransportSubsystem.
    
    It is then implemented by Transport{Client,Server}Subsystem, who call a
    common default method, attachToUnderlay() and implement
    subsystemChannelInactive().
    
    The end result is more direct attachment, saving one level of lambda
    indirection. Since both implementations are known, we can also properly
    specialize the throws declaration.
    
    JIRA: NETCONF-1510
    Change-Id: I8adc0ac557120ff88c0c04bfc0ad9cd36195f2ae
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- transport/transport-ssh/src/main/java/org/opendaylight/netconf/transport/ssh/TransportUtils.java
+++ transport/transport-ssh/src/main/java/org/opendaylight/netconf/transport/ssh/TransportUtils.java
@@ -7,15 +7,10 @@
  */
 package org.opendaylight.netconf.transport.ssh;
 
-import com.google.common.base.VerifyException;
 import com.google.common.collect.ImmutableList;
-import io.netty.channel.ChannelHandlerContext;
 import java.util.List;
 import java.util.Map;
 import org.eclipse.jdt.annotation.NonNull;
-import org.opendaylight.netconf.shaded.sshd.common.channel.ChannelAsyncOutputStream;
-import org.opendaylight.netconf.shaded.sshd.common.io.IoOutputStream;
-import org.opendaylight.netconf.transport.api.TransportChannel;
 import org.opendaylight.netconf.transport.api.UnsupportedConfigurationException;
 
 final class TransportUtils {
@@ -34,32 +29,5 @@ final class TransportUtils {
             builder.add(mapped);
         }
         return builder.build();
-    }
-
-    @FunctionalInterface
-    interface ChannelInactive {
-
-        void onChannelInactive() throws Exception;
-    }
-
-    static ChannelHandlerContext attachUnderlay(final IoOutputStream out, final TransportChannel underlay,
-            final ChannelInactive inactive) {
-        if (!(out instanceof ChannelAsyncOutputStream asyncOut)) {
-            throw new VerifyException("Unexpected output " + out);
-        }
-
-        // Note that there may be multiple handlers already present on the channel, hence we are attaching last, but
-        // from the logical perspective we are the head handlers.
-        final var pipeline = underlay.channel().pipeline();
-
-        // outbound packet handler, i.e. moving bytes from the channel into SSHD's pipeline
-        pipeline.addLast(new OutboundChannelHandler(asyncOut));
-
-        // invoke requested action on channel termination
-        underlay.channel().closeFuture().addListener(future -> inactive.onChannelInactive());
-
-        // last handler context is used as entry point to direct inbound packets (captured by SSH adapter)
-        // back to same channel pipeline
-        return pipeline.lastContext();
     }
 }
