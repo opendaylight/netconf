a92adabcb092e45621fb7f1b47891b986683d52f
Robert Varga <robert.varga@pantheon.tech>
2025-09-05 22:06:56 +0200

    WIP: Split out EncryptionAlgorightms
    Change-Id: If5b014007dd7cfa136e25370f66a822513071c1c
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ transport/transport-ssh/src/main/java/org/opendaylight/netconf/transport/ssh/EncryptionAlgorithms.java
@@ -0,0 +1,135 @@
+/*
+ * Copyright (c) 2025 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.netconf.transport.ssh;
+
+import com.google.common.annotations.VisibleForTesting;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import org.eclipse.jdt.annotation.NonNull;
+import org.eclipse.jdt.annotation.Nullable;
+import org.opendaylight.netconf.shaded.sshd.common.BaseBuilder;
+import org.opendaylight.netconf.shaded.sshd.common.NamedFactory;
+import org.opendaylight.netconf.shaded.sshd.common.cipher.BuiltinCiphers;
+import org.opendaylight.netconf.shaded.sshd.common.cipher.Cipher;
+import org.opendaylight.netconf.shaded.sshd.common.cipher.CipherFactory;
+import org.opendaylight.netconf.transport.api.UnsupportedConfigurationException;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.iana.ssh.encryption.algs.rev241016.SshEncryptionAlgorithm;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.ssh.common.rev241010.transport.params.grouping.Encryption;
+
+/**
+ * Mapping of supported encryption algorithms, mostly as maintained by IANA in
+ * <a href="https://www.iana.org/assignments/ssh-parameters/ssh-parameters.xhtml">Encryption Algorithm Names</a>.
+ */
+final class EncryptionAlgorithms {
+    @VisibleForTesting
+    static final Map<
+        org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.ssh.common.rev241010.SshEncryptionAlgorithm,
+        CipherFactory> BY_YANG = Map.ofEntries(
+            // Keep the same order as in iana-ssh-encryption-algs.yang
+
+            // FIXME: audit commented-out algorithms missing in BuiltinCiphers or provide justification for exclusion
+            // FIXME: update based on https://www.rfc-editor.org/rfc/rfc8758
+
+            // required in https://www.rfc-editor.org/rfc/rfc4253#section-6.3
+            entry(SshEncryptionAlgorithm._3desCbc, BuiltinCiphers.tripledescbc),
+            // optional in https://www.rfc-editor.org/rfc/rfc4253#section-6.3
+            entry(SshEncryptionAlgorithm.BlowfishCbc, BuiltinCiphers.blowfishcbc),
+
+            // defined in https://www.rfc-editor.org/rfc/rfc4253#section-6.3
+            // SshEncryptionAlgorithm.Twofish256Cbc
+            // SshEncryptionAlgorithm.TwofishCbc
+            // SshEncryptionAlgorithm.Twofish192Cbc
+            // SshEncryptionAlgorithm.Twofish128Cbc
+
+            // optional in https://www.rfc-editor.org/rfc/rfc4253#section-6.3
+            entry(SshEncryptionAlgorithm.Aes256Cbc, BuiltinCiphers.aes256cbc),
+            // optional in https://www.rfc-editor.org/rfc/rfc4253#section-6.3
+            entry(SshEncryptionAlgorithm.Aes192Cbc, BuiltinCiphers.aes192cbc),
+            // recommended in https://www.rfc-editor.org/rfc/rfc4253#section-6.3
+            entry(SshEncryptionAlgorithm.Aes128Cbc, BuiltinCiphers.aes128cbc),
+
+            // defined in https://www.rfc-editor.org/rfc/rfc4253#section-6.3
+            // SshEncryptionAlgorithm.Serpent256Cbc
+            // SshEncryptionAlgorithm.Serpent192Cbc
+            // SshEncryptionAlgorithm.Serpent128Cbc
+            // SshEncryptionAlgorithm.Arcfour
+            // SshEncryptionAlgorithm.IdeaCbc
+            // SshEncryptionAlgorithm.Cast128Cbc
+
+            // not recommended in https://www.rfc-editor.org/rfc/rfc4253#section-6.3
+            // FIXME: consistency: we DO provide this, but DO NOT provide SshMacAlgorithm.None
+            entry(SshEncryptionAlgorithm.None, BuiltinCiphers.none),
+
+            // SshEncryptionAlgorithm.DesCbc is HISTORIC and hence not implemented
+
+            // defined in https://www.rfc-editor.org/rfc/rfc4345#section-4
+            entry(SshEncryptionAlgorithm.Arcfour128, BuiltinCiphers.arcfour128),
+            entry(SshEncryptionAlgorithm.Arcfour256, BuiltinCiphers.arcfour256),
+
+            // recommended in https://www.rfc-editor.org/rfc/rfc4344.html#section-4
+            entry(SshEncryptionAlgorithm.Aes128Ctr, BuiltinCiphers.aes128ctr),
+            // recommended in https://www.rfc-editor.org/rfc/rfc4344.html#section-4
+            entry(SshEncryptionAlgorithm.Aes192Ctr, BuiltinCiphers.aes192ctr),
+            // recommended in https://www.rfc-editor.org/rfc/rfc4344.html#section-4
+            entry(SshEncryptionAlgorithm.Aes256Ctr, BuiltinCiphers.aes256ctr),
+
+            // defined in https://www.rfc-editor.org/rfc/rfc4344.html#section-4
+            // SshEncryptionAlgorithm._3desCtr
+            // SshEncryptionAlgorithm.BlowfishCtr
+            // SshEncryptionAlgorithm.Twofish128Ctr
+            // SshEncryptionAlgorithm.Twofish192Ctr
+            // SshEncryptionAlgorithm.Twofish256Ctr
+            // SshEncryptionAlgorithm.Serpent128Ctr
+            // SshEncryptionAlgorithm.Serpent192Ctr
+            // SshEncryptionAlgorithm.Serpent256Ctr
+            // SshEncryptionAlgorithm.IdeaCtr
+            // SshEncryptionAlgorithm.Cast128Ctr
+
+            // defined in https://www.rfc-editor.org/rfc/rfc5647
+            // negotiated as per https://www.ietf.org/archive/id/draft-miller-sshm-aes-gcm-00.html
+            entry(SshEncryptionAlgorithm.AEADAES128GCM, BuiltinCiphers.aes128gcm),
+            entry(SshEncryptionAlgorithm.AEADAES256GCM, BuiltinCiphers.aes256gcm)
+
+            // defined in https://datatracker.ietf.org/doc/draft-josefsson-ssh-chacha20-poly1305-openssh/01/
+            // FIXME: does this equal to BuiltinCiphers.cc20p1305_openssh? we need to read up on the drafts to
+            //        determine that
+            // SshEncryptionAlgorithm.Chacha20Poly1305
+            );
+
+    private static final @NonNull List<NamedFactory<Cipher>> DEFAULT_FACTORIES =
+        List.copyOf(BaseBuilder.DEFAULT_CIPHERS_PREFERENCE);
+
+    private EncryptionAlgorithms() {
+        // Hidden on purpose
+    }
+
+    static @NonNull List<NamedFactory<Cipher>> factoriesFor(final @Nullable Encryption encryption)
+            throws UnsupportedConfigurationException {
+        if (encryption != null) {
+            final var encAlg = encryption.getEncryptionAlg();
+            if (encAlg != null && !encAlg.isEmpty()) {
+                return TransportUtils.mapValues(BY_YANG, encAlg, "Unsupported Encryption algorithm %s");
+            }
+        }
+        return DEFAULT_FACTORIES;
+    }
+
+    private static Entry<
+            org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.ssh.common.rev241010.SshEncryptionAlgorithm,
+            CipherFactory> entry(final SshEncryptionAlgorithm alg, final CipherFactory factory) {
+        return Map.entry(keyOf(alg), factory);
+    }
+
+    @VisibleForTesting
+    static org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.ssh.common.rev241010
+            .SshEncryptionAlgorithm keyOf(final SshEncryptionAlgorithm alg) {
+        return new org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.ssh.common.rev241010
+            .SshEncryptionAlgorithm(alg);
+    }
+}
