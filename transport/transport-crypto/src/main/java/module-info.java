b0c940ea8baba5a039a29374f7678e7ed4b782a8
Robert Varga <robert.varga@pantheon.tech>
2025-09-08 14:34:19 +0200

    Add transport-crypto module
    This starts the process of unifying crypto handling by introducing a
    commmon CertificateSupport class.
    
    Unlike the transport-ssh version of this method, we use just the default
    provider, not necessarily BouncyCastle.
    
    JIRA: NETCONF-1506
    Change-Id: I5f2b586be955a3583f7558307ecf6383631232bc
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>
    (cherry picked from commit 3283a5a9c8335685e2070ab3b8179bbd1b3232e7)

---
+++ transport/transport-crypto/src/main/java/module-info.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2025 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+/**
+ * Support for {@code ietf-crypto-types.yang}.
+ */
+module org.opendaylight.netconf.transport.crypto {
+    exports org.opendaylight.netconf.transport.crypto;
+
+    requires transitive org.opendaylight.netconf.transport.api;
+    requires transitive org.opendaylight.yang.gen.ietf.crypto.types.rfc9640;
+
+    requires com.google.common;
+    requires org.bouncycastle.provider;
+    requires org.opendaylight.yangtools.binding.spec;
+    requires org.slf4j;
+
+    // Annotation-only dependencies
+    requires static transitive org.eclipse.jdt.annotation;
+    requires static java.management;
+    requires static org.osgi.annotation.bundle;
+}
