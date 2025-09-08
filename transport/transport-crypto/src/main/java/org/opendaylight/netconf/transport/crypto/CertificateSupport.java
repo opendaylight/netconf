70e3a9bda77e67c5366046c4c02beb01f96a0160
Robert Varga <robert.varga@pantheon.tech>
2025-09-08 14:34:19 +0200

    Add transport-crypto module
    This starts the process of unifying crypto handling by introducing a
    commmon CertificateSupport class.
    
    This is inherited from the transport-ssh version of KeyUtils, which is
    using BouncyCastle. The reason for that is guarded by a test, which does
    not explain much :(
    
    In any case, we improve on the previous incarnation by caching the
    Provider directly, side-stepping the lookup-by-String.
    
    JIRA: NETCONF-1506
    Change-Id: I5f2b586be955a3583f7558307ecf6383631232bc
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ transport/transport-crypto/src/main/java/org/opendaylight/netconf/transport/crypto/CertificateSupport.java
@@ -0,0 +1,98 @@
+/*
+ * Copyright (c) 2025 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.netconf.transport.crypto;
+
+import com.google.common.annotations.Beta;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.security.Provider;
+import java.security.Security;
+import java.security.cert.Certificate;
+import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
+import java.util.Base64;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.opendaylight.netconf.transport.api.UnsupportedConfigurationException;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.crypto.types.rev241010.EndEntityCertCms;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.crypto.types.rev241010.TrustAnchorCertCms;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.crypto.types.rev241010.X509;
+
+/**
+ * Support for certificates in their various forms. This class is considered a Beta-level API for internal use. It can
+ * change incompatibly between minor releases.
+ *
+ * @since 10.0.1
+ */
+@Beta
+@NonNullByDefault
+public final class CertificateSupport {
+    private static final Provider BCPROV;
+
+    static {
+        final var existing = Security.getProvider( BouncyCastleProvider.PROVIDER_NAME);
+        BCPROV = existing != null ? existing : new BouncyCastleProvider();
+    }
+
+    private CertificateSupport() {
+        // Hidden on purpose
+    }
+
+    /**
+     * Parse an X.509 certificate.
+     *
+     * @param certificate the certificate
+     * @return A {@link Certificate}
+     * @throws UnsupportedConfigurationException when the certificate cannot be parsed
+     */
+    public static Certificate parseCertificate(final EndEntityCertCms certificate)
+            throws UnsupportedConfigurationException {
+        return parseX509Certificate(certificate.getValue());
+    }
+
+    /**
+     * Parse an X.509 certificate.
+     *
+     * @param certificate the certificate
+     * @return A {@link Certificate}
+     * @throws UnsupportedConfigurationException when the certificate cannot be parsed
+     */
+    public static Certificate parseCertificate(final TrustAnchorCertCms certificate)
+            throws UnsupportedConfigurationException {
+        return parseX509Certificate(certificate.getValue());
+    }
+
+    /**
+     * Parse an X.509 certificate.
+     *
+     * @param certificate the certificate
+     * @return A {@link Certificate}
+     * @throws UnsupportedConfigurationException when the certificate cannot be parsed
+     */
+    public static Certificate parseCertificate(final X509 certificate)
+            throws UnsupportedConfigurationException {
+        return parseX509Certificate(certificate.getValue());
+    }
+
+    private static Certificate parseX509Certificate(final byte[] certificateBytes)
+            throws UnsupportedConfigurationException {
+        final CertificateFactory factory;
+        try {
+            factory = CertificateFactory.getInstance("X.509", BCPROV);
+        } catch (CertificateException e) {
+            throw new UnsupportedConfigurationException("X.509 certificates are not supported", e);
+        }
+
+        try (var in = new ByteArrayInputStream(certificateBytes)) {
+            return factory.generateCertificate(in);
+        } catch (CertificateException | IOException e) {
+            throw new UnsupportedConfigurationException(
+                "Cannot parse certificate " + Base64.getEncoder().encodeToString(certificateBytes), e);
+        }
+    }
+}
