dba15f976003e8ea10368f98d7002f81cfeeb51c
Ivan Hrasko <ivan.hrasko@pantheon.tech>
2025-09-17 10:04:58 +0200

    Rework TransportIoService and TransportIoSession
    We do not need a full implementations of neither
    TransportIoService nor TransportIoSession.
    
    NettyIoService's channelGroup is used to track all open channels,
    so that we can close them when we are shutdown.
    
    We do not need to do that, as we track the underlying transport
    instead, hence any channels tracked here will disappear when we shut
    down the underlay.
    
    Rework TransportIoService to be just an intermediate step to create
    sessions. Use simple counter for creating session ids.
    
    Rework TransportIoSession to be an partial implementation of IoSession,
    adapt NettyIoSession.Adapter implementation to provide suitable inbound
    handler.
    
    JIRA: NETCONF-1504
    Change-Id: I026779eb3c2be7234b50bf94405fa071d8a446ba
    Signed-off-by: Ivan Hrasko <ivan.hrasko@pantheon.tech>

--- transport/transport-ssh/src/main/java/org/opendaylight/netconf/transport/ssh/TransportIoSession.java
+++ transport/transport-ssh/src/main/java/org/opendaylight/netconf/transport/ssh/TransportIoSession.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023 PANTHEON.tech s.r.o. All rights reserved.
+ * Copyright (c) 2025 PANTHEON.tech, s.r.o. and others.  All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
@@ -7,27 +7,243 @@
  */
 package org.opendaylight.netconf.transport.ssh;
 
-import io.netty.channel.ChannelHandlerContext;
-import io.netty.channel.ChannelInboundHandler;
-import java.net.SocketAddress;
-import org.opendaylight.netconf.shaded.sshd.common.io.IoHandler;
-import org.opendaylight.netconf.shaded.sshd.netty.NettyIoSession;
+import static java.util.Objects.requireNonNull;
 
-final class TransportIoSession extends NettyIoSession {
-    TransportIoSession(final TransportIoService service, final IoHandler handler,
-            final SocketAddress acceptanceAddress) {
-        super(service, handler, acceptanceAddress);
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.group.ChannelGroup;
+import io.netty.channel.socket.DuplexChannel;
+import java.io.IOException;
+import java.net.SocketAddress;
+import java.nio.channels.ClosedChannelException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicBoolean;
+import org.eclipse.jdt.annotation.NonNull;
+import org.eclipse.jdt.annotation.Nullable;
+import org.opendaylight.netconf.shaded.sshd.common.io.AbstractIoWriteFuture;
+import org.opendaylight.netconf.shaded.sshd.common.io.IoHandler;
+import org.opendaylight.netconf.shaded.sshd.common.io.IoService;
+import org.opendaylight.netconf.shaded.sshd.common.io.IoSession;
+import org.opendaylight.netconf.shaded.sshd.common.io.IoWriteFuture;
+import org.opendaylight.netconf.shaded.sshd.common.session.helpers.MissingAttachedSessionException;
+import org.opendaylight.netconf.shaded.sshd.common.util.buffer.Buffer;
+import org.opendaylight.netconf.shaded.sshd.common.util.closeable.AbstractCloseable;
+import org.opendaylight.netconf.shaded.sshd.netty.NettyIoService;
+import org.opendaylight.netconf.shaded.sshd.netty.NettyIoSession;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * {@link IoSession} implementation based on {@link NettyIoSession} without usage of {@link NettyIoService}.
+ *
+ * <p>Without @link @NettyIoService} we are not creating {@link ChannelGroup}s for each session which is unwanted
+ * in our case.
+ */
+final class TransportIoSession extends AbstractCloseable implements IoSession {
+    private static final Logger LOG = LoggerFactory.getLogger(TransportIoSession.class);
+
+    private final Map<Object, Object> attributes = new HashMap<>();
+    private final AtomicBoolean readSuspended = new AtomicBoolean();
+
+    private final @NonNull IoHandler handler;
+    private final @NonNull SocketAddress acceptanceAddress;
+    private final long id;
+
+    private @Nullable ChannelHandlerContext context;
+    private @Nullable SocketAddress remoteAddr;
+    private @Nullable ChannelFuture prev;
+
+    TransportIoSession(final @NonNull IoHandler handler, final @NonNull SocketAddress acceptanceAddress,
+            final long id) {
+        this.handler = requireNonNull(handler);
+        this.acceptanceAddress = requireNonNull(acceptanceAddress);
+        this.id = id;
     }
 
-    ChannelInboundHandler handler() {
-        return adapter;
+    ChannelHandler createAdapter() {
+        return new OdlFlowControlHandler(handler, this);
+    }
+
+    @SuppressWarnings("checkstyle:illegalCatch")
+    void channelActive(final ChannelHandlerContext ctx) {
+        context = ctx;
+        final var channel = context.channel();
+        prev = context.newPromise().setSuccess();
+        remoteAddr = channel.remoteAddress();
+        final var connectFuture = channel.attr(NettyIoService.CONNECT_FUTURE_KEY);
+        final var future = connectFuture.get();
+        try {
+            handler.sessionCreated(this);
+            if (future != null) {
+                future.setSession(this);
+                if (future.getSession() != this) {
+                    close(true);
+                }
+            }
+        } catch (final Throwable e) {
+            LOG.warn("channelActive(session={}): could not create SSH session ({}); closing",
+                this, e.getClass().getName(), e);
+            try {
+                if (future != null) {
+                    future.setException(e);
+                }
+            } finally {
+                close(true);
+            }
+        }
+    }
+
+    void channelInactive(final ChannelHandlerContext ctx) throws Exception {
+        try {
+            handler.sessionClosed(this);
+        } catch (final MissingAttachedSessionException e) {
+            LOG.trace("channelInactive(session={}): caught {}", this, e.getClass().getName(), e);
+        } finally {
+            final var channel = ctx.channel();
+            final var connectFuture = channel.attr(NettyIoService.CONNECT_FUTURE_KEY);
+            final var future = connectFuture.get();
+            if (future != null) {
+                final var cancellation = future.cancel();
+                if (cancellation != null) {
+                    cancellation.setCanceled();
+                }
+            }
+            context = null;
+
+            // fix for NETCONF-1200
+            ctx.fireChannelInactive();
+        }
     }
 
     @Override
-    protected void channelInactive(final ChannelHandlerContext ctx) throws Exception {
-        // adapter is not propagating fireChannelInactive() down the the pipeline, but instead loops here. Once we have
-        // cleaned up, propagate fireChannelInactive() outselves.
-        super.channelInactive(ctx);
-        ctx.fireChannelInactive();
+    public long getId() {
+        return id;
+    }
+
+    @Override
+    public SocketAddress getAcceptanceAddress() {
+        return acceptanceAddress;
+    }
+
+    @Override
+    public Object getAttribute(final Object key) {
+        synchronized (attributes) {
+            return attributes.get(key);
+        }
+    }
+
+    @Override
+    public Object setAttribute(final Object key, final Object value) {
+        synchronized (attributes) {
+            return attributes.put(key, value);
+        }
+    }
+
+    @Override
+    public Object setAttributeIfAbsent(final Object key, final Object value) {
+        synchronized (attributes) {
+            return attributes.putIfAbsent(key, value);
+        }
+    }
+
+    @Override
+    public Object removeAttribute(final Object key) {
+        synchronized (attributes) {
+            return attributes.remove(key);
+        }
+    }
+
+    @Override
+    public IoWriteFuture writeBuffer(final Buffer buffer) throws IOException {
+        final int bufLen = buffer.available();
+        final var buf = Unpooled.buffer(bufLen);
+        buf.writeBytes(buffer.array(), buffer.rpos(), bufLen);
+        final var msg = new DefaultIoWriteFuture(getRemoteAddress(), null);
+        final var ctx = context;
+        if (ctx == null) {
+            msg.setValue(new ClosedChannelException());
+            return msg;
+        }
+        final var next = ctx.newPromise();
+        prev.addListener(whatever -> {
+            ChannelHandlerContext localCtx = context;
+            if (localCtx != null) {
+                localCtx.writeAndFlush(buf, next);
+            } else {
+                msg.setValue(new ClosedChannelException());
+                next.cancel(true);
+            }
+        });
+        prev = next;
+        next.addListener(fut -> {
+            if (fut.isSuccess()) {
+                msg.setValue(Boolean.TRUE);
+            } else {
+                msg.setValue(fut.cause());
+            }
+        });
+        return msg;
+    }
+
+    private static class DefaultIoWriteFuture extends AbstractIoWriteFuture {
+        DefaultIoWriteFuture(final Object id, final @Nullable Object lock) {
+            super(id, lock);
+        }
+    }
+
+    @Override
+    public IoService getService() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void suspendRead() {
+        if (!readSuspended.getAndSet(true)) {
+            final var ctx = context;
+            if (ctx != null) {
+                final var ch = ctx.channel();
+                ch.config().setAutoRead(false);
+            }
+        }
+    }
+
+    @Override
+    public void resumeRead() {
+        if (readSuspended.getAndSet(false)) {
+            final var ctx = context;
+            if (ctx != null) {
+                final var ch = ctx.channel();
+                ch.config().setAutoRead(true);
+            }
+        }
+    }
+
+    @Override
+    public void shutdownOutputStream() throws IOException {
+        final var ctx = context;
+        if (ctx == null) {
+            return;
+        }
+        final var ch = ctx.channel();
+        if (ch instanceof DuplexChannel) {
+            ((DuplexChannel) ch).shutdownOutput();
+        } else {
+            LOG.debug("shutdownOutputStream({}) channel is not DuplexChannel: {}", this,
+                (ch == null) ? null : ch.getClass().getSimpleName());
+        }
+    }
+
+    @Override
+    public SocketAddress getRemoteAddress() {
+        return remoteAddr;
+    }
+
+    @Override
+    public SocketAddress getLocalAddress() {
+        final var channel = (context == null) ? null : context.channel();
+        return (channel == null) ? null : channel.localAddress();
     }
 }
