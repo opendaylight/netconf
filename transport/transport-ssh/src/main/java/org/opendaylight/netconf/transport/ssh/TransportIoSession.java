59708912ab72f3a39a55ba0c3a8e39bd81318d29
Ivan Hrasko <ivan.hrasko@pantheon.tech>
2025-09-17 10:04:58 +0200

    Rework TransportIoService and TransportIoSession
    Since we do not need a full implementations of neither
    TransportIoService nor TransportIoSession.
    
    Rework TransportIoService to be just an intermediate step to create
    sessions. Use simple counter for creating session ids.
    
    Rework TransportIoSession to be an partial implementation of IoSession,
    adapt NettyIoSession.Adapter implementation to provide suitable inbound
    handler.
    
    JIRA: NETCONF-1504
    Change-Id: I026779eb3c2be7234b50bf94405fa071d8a446ba
    Signed-off-by: Ivan Hrasko <ivan.hrasko@pantheon.tech>

--- transport/transport-ssh/src/main/java/org/opendaylight/netconf/transport/ssh/TransportIoSession.java
+++ transport/transport-ssh/src/main/java/org/opendaylight/netconf/transport/ssh/TransportIoSession.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023 PANTHEON.tech s.r.o. All rights reserved.
+ * Copyright (c) 2025 PANTHEON.tech, s.r.o. and others.  All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
@@ -7,27 +7,123 @@
  */
 package org.opendaylight.netconf.transport.ssh;
 
-import io.netty.channel.ChannelHandlerContext;
-import io.netty.channel.ChannelInboundHandler;
-import java.net.SocketAddress;
-import org.opendaylight.netconf.shaded.sshd.common.io.IoHandler;
-import org.opendaylight.netconf.shaded.sshd.netty.NettyIoSession;
+import static java.util.Objects.requireNonNull;
 
-final class TransportIoSession extends NettyIoSession {
-    TransportIoSession(final TransportIoService service, final IoHandler handler,
-            final SocketAddress acceptanceAddress) {
-        super(service, handler, acceptanceAddress);
+import io.netty.channel.ChannelInboundHandler;
+import java.io.IOException;
+import java.net.SocketAddress;
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.opendaylight.netconf.shaded.sshd.common.future.CloseFuture;
+import org.opendaylight.netconf.shaded.sshd.common.future.SshFutureListener;
+import org.opendaylight.netconf.shaded.sshd.common.io.IoHandler;
+import org.opendaylight.netconf.shaded.sshd.common.io.IoService;
+import org.opendaylight.netconf.shaded.sshd.common.io.IoSession;
+import org.opendaylight.netconf.shaded.sshd.common.io.IoWriteFuture;
+import org.opendaylight.netconf.shaded.sshd.common.util.buffer.Buffer;
+
+@NonNullByDefault
+final class TransportIoSession implements IoSession {
+    private final IoHandler handler;
+    private final SocketAddress acceptanceAddress;
+    private final long id;
+
+    TransportIoSession(final IoHandler handler, final SocketAddress acceptanceAddress, final long id) {
+        this.handler = requireNonNull(handler);
+        this.acceptanceAddress = requireNonNull(acceptanceAddress);
+        this.id = id;
     }
 
     ChannelInboundHandler handler() {
-        return adapter;
+        return new OdlFlowControlHandler(handler, this);
     }
 
     @Override
-    protected void channelInactive(final ChannelHandlerContext ctx) throws Exception {
-        // adapter is not propagating fireChannelInactive() down the the pipeline, but instead loops here. Once we have
-        // cleaned up, propagate fireChannelInactive() outselves.
-        super.channelInactive(ctx);
-        ctx.fireChannelInactive();
+    public long getId() {
+        return id;
+    }
+
+    @Override
+    public SocketAddress getAcceptanceAddress() {
+        return acceptanceAddress;
+    }
+
+    @Override
+    public Object getAttribute(final Object key) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Object setAttribute(final Object key, final Object value) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Object setAttributeIfAbsent(final Object key, final Object value) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Object removeAttribute(final Object key) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public IoWriteFuture writeBuffer(final Buffer buffer) throws IOException {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public CloseFuture close(final boolean immediately) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void addCloseFutureListener(final SshFutureListener<CloseFuture> listener) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void removeCloseFutureListener(final SshFutureListener<CloseFuture> listener) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean isClosed() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean isClosing() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public IoService getService() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void shutdownOutputStream() throws IOException {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void suspendRead() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void resumeRead() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public SocketAddress getRemoteAddress() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public SocketAddress getLocalAddress() {
+        throw new UnsupportedOperationException();
     }
 }
