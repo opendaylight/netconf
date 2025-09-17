a727122abd6c643d324706e8f7c3fbeeee015a32
Ivan Hrasko <ivan.hrasko@pantheon.tech>
2025-09-17 10:04:58 +0200

    Rework TransportIoService and TransportIoSession
    Since we do not need a full implementations of neither
    TransportIoService nor TransportIoSession.
    
    Rework TransportIoService to be just an intermediate step to create
    sessions. Use simple counter for creating session ids.
    
    Rework TransportIoSession to be an partial implementation of IoSession,
    adapt NettyIoSession.Adapter implementation to provide suitable inbound
    handler.
    
    JIRA: NETCONF-1504
    Change-Id: I026779eb3c2be7234b50bf94405fa071d8a446ba
    Signed-off-by: Ivan Hrasko <ivan.hrasko@pantheon.tech>

--- transport/transport-ssh/src/main/java/org/opendaylight/netconf/transport/ssh/SSHTransportStack.java
+++ transport/transport-ssh/src/main/java/org/opendaylight/netconf/transport/ssh/SSHTransportStack.java
@@ -35,9 +35,6 @@ import org.slf4j.LoggerFactory;
  * An SSH {@link TransportStack}. Instances of this class are built indirectly. The setup of the Netty channel is quite
  * weird. We start off with whatever the underlay sets up.
  *
- * <p>We then add {@link TransportIoSession#handler()}, which routes data between the socket and
- * {@link TransportSshClient} (or {@link TransportSshServer}) -- forming the "bottom half" of the channel.
- *
  * <p>The "upper half" of the channel is formed once the corresponding SSH subsystem is established, via
  * {@link TransportClientSubsystem}, which installs a {@link OutboundChannelHandler}. These two work together:
  * <ul>
@@ -134,7 +131,7 @@ public abstract sealed class SSHTransportStack extends AbstractOverlayTransportS
     SSHTransportStack(final TransportChannelListener<? super SSHTransportChannel> listener,
             final FactoryManager factoryManager, final IoHandler handler) {
         super(listener);
-        ioService = new TransportIoService(factoryManager, handler);
+        ioService = new TransportIoService(handler);
         factoryManager.addSessionListener(new Listener());
     }
 
--- transport/transport-ssh/src/main/java/org/opendaylight/netconf/transport/ssh/TransportIoSession.java
+++ transport/transport-ssh/src/main/java/org/opendaylight/netconf/transport/ssh/TransportIoSession.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023 PANTHEON.tech s.r.o. All rights reserved.
+ * Copyright (c) 2025 PANTHEON.tech, s.r.o. and others.  All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
@@ -7,27 +7,139 @@
  */
 package org.opendaylight.netconf.transport.ssh;
 
+import static java.util.Objects.requireNonNull;
+
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandler;
+import java.io.IOException;
 import java.net.SocketAddress;
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.opendaylight.netconf.shaded.sshd.common.future.CloseFuture;
+import org.opendaylight.netconf.shaded.sshd.common.future.SshFutureListener;
 import org.opendaylight.netconf.shaded.sshd.common.io.IoHandler;
-import org.opendaylight.netconf.shaded.sshd.netty.NettyIoSession;
+import org.opendaylight.netconf.shaded.sshd.common.io.IoService;
+import org.opendaylight.netconf.shaded.sshd.common.io.IoSession;
+import org.opendaylight.netconf.shaded.sshd.common.io.IoWriteFuture;
+import org.opendaylight.netconf.shaded.sshd.common.util.buffer.Buffer;
+import org.opendaylight.netconf.shaded.sshd.common.util.closeable.AbstractCloseable;
+import org.opendaylight.netconf.shaded.sshd.netty.NettyIoService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-final class TransportIoSession extends NettyIoSession {
-    TransportIoSession(final TransportIoService service, final IoHandler handler,
-            final SocketAddress acceptanceAddress) {
-        super(service, handler, acceptanceAddress);
+@NonNullByDefault
+final class TransportIoSession extends AbstractCloseable implements IoSession {
+    private static final Logger LOG = LoggerFactory.getLogger(TransportIoSession.class);
+
+    private final IoHandler handler;
+    private final SocketAddress acceptanceAddress;
+    private final long id;
+
+    TransportIoSession(final IoHandler handler, final SocketAddress acceptanceAddress, final long id) {
+        this.handler = requireNonNull(handler);
+        this.acceptanceAddress = requireNonNull(acceptanceAddress);
+        this.id = id;
     }
 
     ChannelInboundHandler handler() {
-        return adapter;
+        return new OdlFlowControlHandler(handler, this);
     }
 
     @Override
-    protected void channelInactive(final ChannelHandlerContext ctx) throws Exception {
-        // adapter is not propagating fireChannelInactive() down the the pipeline, but instead loops here. Once we have
-        // cleaned up, propagate fireChannelInactive() outselves.
-        super.channelInactive(ctx);
-        ctx.fireChannelInactive();
+    public long getId() {
+        return id;
+    }
+
+    @Override
+    public SocketAddress getAcceptanceAddress() {
+        return acceptanceAddress;
+    }
+
+    @Override
+    public Object getAttribute(final Object key) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Object setAttribute(final Object key, final Object value) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Object setAttributeIfAbsent(final Object key, final Object value) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Object removeAttribute(final Object key) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public IoWriteFuture writeBuffer(final Buffer buffer) throws IOException {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void addCloseFutureListener(final SshFutureListener<CloseFuture> listener) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void removeCloseFutureListener(final SshFutureListener<CloseFuture> listener) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public IoService getService() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void shutdownOutputStream() throws IOException {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void suspendRead() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void resumeRead() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public SocketAddress getRemoteAddress() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public SocketAddress getLocalAddress() {
+        throw new UnsupportedOperationException();
+    }
+
+    public void channelActive(final ChannelHandlerContext ctx) {
+        final var connectFuture = ctx.channel().attr(NettyIoService.CONNECT_FUTURE_KEY);
+        final var future = connectFuture.get();
+        try {
+            handler.sessionCreated(this);
+            if (future != null) {
+                future.setSession(this);
+                if (future.getSession() != this) {
+                    close(true);
+                }
+            }
+        } catch (Throwable e) {
+            LOG.warn("channelActive(session={}): could not create SSH session ({}); closing",
+                this, e.getClass().getName(), e);
+            try {
+                if (future != null) {
+                    future.setException(e);
+                }
+            } finally {
+                close(true);
+            }
+        }
     }
 }
