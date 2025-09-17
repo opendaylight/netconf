ab6f5513a7ba1311ed6043d67d936c569df26102
Robert Varga <robert.varga@pantheon.tech>
2025-09-17 10:44:00 +0200

    Check for null EffecticeModelContext earlier
    If we do not have an EffectiveModelContext we should just return without
    looking at other parameters.
    
    We add some nullness annotations to ensure mismatches are flagged and
    check for YangInstanceIdentifier/EffectiveModelContext.
    
    JIRA: NETCONF-1494
    Change-Id: Iab333a85eefe0faee4b32411cc74c6159b0458f0
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>
    (cherry picked from commit 3dd46433ff92068bef1d8d6c93316e4180411d26)
    (cherry picked from commit fd546199edeb58ccac36e402ca1e86a13f7b49b1)

--- restconf/restconf-openapi/src/main/java/org/opendaylight/restconf/openapi/mountpoints/MountPointOpenApi.java
+++ restconf/restconf-openapi/src/main/java/org/opendaylight/restconf/openapi/mountpoints/MountPointOpenApi.java
@@ -12,12 +12,15 @@ import static org.opendaylight.restconf.openapi.impl.BaseYangOpenApiGenerator.SE
 
 import java.io.IOException;
 import java.net.URI;
+import java.util.Comparator;
 import java.util.HashMap;
-import java.util.Map;
-import java.util.Map.Entry;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.ConcurrentSkipListMap;
 import java.util.concurrent.atomic.AtomicLong;
+import org.eclipse.jdt.annotation.NonNull;
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.eclipse.jdt.annotation.Nullable;
 import org.opendaylight.mdsal.dom.api.DOMMountPoint;
 import org.opendaylight.mdsal.dom.api.DOMMountPointListener;
 import org.opendaylight.mdsal.dom.api.DOMMountPointService;
@@ -29,9 +32,7 @@ import org.opendaylight.restconf.openapi.model.MountPointsEntity;
 import org.opendaylight.yangtools.concepts.Registration;
 import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
 import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.NodeIdentifierWithPredicates;
-import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.PathArgument;
 import org.opendaylight.yangtools.yang.model.api.EffectiveModelContext;
-import org.opendaylight.yangtools.yang.model.api.Module;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -40,13 +41,16 @@ public class MountPointOpenApi implements DOMMountPointListener, AutoCloseable {
     private static final String DATASTORES_REVISION = "-";
     private static final String DATASTORES_LABEL = "Datastores";
 
+    private static final Comparator<YangInstanceIdentifier> COMPARATOR =
+        // FIXME: do not use toString()
+        (o1, o2) -> o1.toString().compareToIgnoreCase(o2.toString());
+
+    private final ConcurrentMap<YangInstanceIdentifier, Long> mountIdToLong = new ConcurrentSkipListMap<>(COMPARATOR);
+    private final ConcurrentHashMap<Long, YangInstanceIdentifier> longToMountId = new ConcurrentHashMap<>();
+    private final AtomicLong idKey = new AtomicLong();
     private final DOMSchemaService globalSchema;
     private final DOMMountPointService mountService;
     private final BaseYangOpenApiGenerator openApiGenerator;
-    private final Map<YangInstanceIdentifier, Long> instanceIdToLongId =
-            new ConcurrentSkipListMap<>((o1, o2) -> o1.toString().compareToIgnoreCase(o2.toString()));
-    private final Map<Long, YangInstanceIdentifier> longIdToInstanceId = new ConcurrentHashMap<>();
-    private final AtomicLong idKey = new AtomicLong(0);
 
     private Registration registration;
 
@@ -69,37 +73,41 @@ public class MountPointOpenApi implements DOMMountPointListener, AutoCloseable {
     }
 
     public MountPointsEntity getInstanceIdentifiers() {
-        final Map<String, Long> urlToId = new HashMap<>();
-        final EffectiveModelContext modelContext = globalSchema.getGlobalContext();
-        for (final Entry<YangInstanceIdentifier, Long> entry : instanceIdToLongId.entrySet()) {
-            final String modName = findModuleName(entry.getKey(), modelContext);
+        final var urlToId = new HashMap<String, Long>();
+        final var modelContext = globalSchema.getGlobalContext();
+        for (var entry : mountIdToLong.entrySet()) {
+            final var modName = findModuleName(entry.getKey(), modelContext);
             urlToId.put(generateUrlPrefixFromInstanceID(entry.getKey(), modName), entry.getValue());
         }
         return new MountPointsEntity(urlToId);
     }
 
-    private static String findModuleName(final YangInstanceIdentifier id, final EffectiveModelContext modelContext) {
-        final PathArgument rootQName = id.getPathArguments().iterator().next();
-        for (final Module mod : modelContext.getModules()) {
-            if (mod.findDataChildByName(rootQName.getNodeType()).isPresent()) {
+    private static @Nullable String findModuleName(final @NonNull YangInstanceIdentifier id,
+            final @NonNull EffectiveModelContext modelContext) {
+        final var rootQName = id.getPathArguments().iterator().next();
+        for (var mod : modelContext.getModules()) {
+            if (mod.dataChildByName(rootQName.getNodeType()) != null) {
                 return mod.getName();
             }
         }
         return null;
     }
 
-    private String getYangMountUrl(final YangInstanceIdentifier key) {
-        final String modName = findModuleName(key, globalSchema.getGlobalContext());
-        return generateUrlPrefixFromInstanceID(key, modName) + "yang-ext:mount";
+    @NonNullByDefault
+    private String getYangMountUrl(final YangInstanceIdentifier mountId) {
+        return generateUrlPrefixFromInstanceID(mountId, findModuleName(mountId, globalSchema.getGlobalContext()))
+            + "yang-ext:mount";
     }
 
-    private static String generateUrlPrefixFromInstanceID(final YangInstanceIdentifier key, final String moduleName) {
+    @NonNullByDefault
+    private static String generateUrlPrefixFromInstanceID(final YangInstanceIdentifier mountId,
+            final @Nullable String moduleName) {
         final StringBuilder builder = new StringBuilder();
         builder.append("/");
         if (moduleName != null) {
             builder.append(moduleName).append(':');
         }
-        for (var arg : key.getPathArguments()) {
+        for (var arg : mountId.getPathArguments()) {
             final String name = arg.getNodeType().getLocalName();
             if (arg instanceof NodeIdentifierWithPredicates nodeId) {
                 for (var entry : nodeId.entrySet()) {
@@ -112,8 +120,8 @@ public class MountPointOpenApi implements DOMMountPointListener, AutoCloseable {
         return builder.toString();
     }
 
-    private EffectiveModelContext getModelContext(final YangInstanceIdentifier id) {
-        return id == null ? null : mountService.getMountPoint(id)
+    private @Nullable EffectiveModelContext modelContextFor(final @NonNull YangInstanceIdentifier moundId) {
+        return mountService.getMountPoint(moundId)
             .flatMap(mountPoint -> mountPoint.getService(DOMSchemaService.class))
             .map(DOMSchemaService::getGlobalContext)
             .orElse(null);
@@ -121,14 +129,17 @@ public class MountPointOpenApi implements DOMMountPointListener, AutoCloseable {
 
     public DocumentEntity getMountPointApi(final URI uri, final long id, final String module, final String revision,
             final int width, final int depth) throws IOException  {
-        final YangInstanceIdentifier iid = longIdToInstanceId.get(id);
-        final EffectiveModelContext modelContext = getModelContext(iid);
-        final String urlPrefix = getYangMountUrl(iid);
-        final String deviceName = extractDeviceName(iid);
-
+        final var mountId = longToMountId.get(id);
+        if (mountId == null) {
+            return null;
+        }
+        final var modelContext = modelContextFor(mountId);
         if (modelContext == null) {
             return null;
         }
+
+        final String urlPrefix = getYangMountUrl(mountId);
+        final String deviceName = extractDeviceName(mountId);
 
         if (DATASTORES_LABEL.equals(module) && DATASTORES_REVISION.equals(revision)) {
             return generateDataStoreOpenApi(modelContext, uri, urlPrefix, deviceName, width, depth);
@@ -139,17 +150,20 @@ public class MountPointOpenApi implements DOMMountPointListener, AutoCloseable {
 
     public DocumentEntity getMountPointApi(final URI uri, final long id, final int width, final int depth,
             final int offset, final int limit) throws IOException {
-        final var iid = longIdToInstanceId.get(id);
-        final var context = getModelContext(iid);
-        final var urlPrefix = getYangMountUrl(iid);
-        final var deviceName = extractDeviceName(iid);
-
-        if (context == null) {
+        final var mountId = longToMountId.get(id);
+        if (mountId == null) {
+            return null;
+        }
+        final var modelContext = modelContextFor(mountId);
+        if (modelContext == null) {
             return null;
         }
 
+        final var urlPrefix = getYangMountUrl(mountId);
+        final var deviceName = extractDeviceName(mountId);
+
         final boolean includeDataStore = limit == 0 && offset == 0;
-        final var modulesWithoutDuplications = BaseYangOpenApiGenerator.getModulesWithoutDuplications(context);
+        final var modulesWithoutDuplications = BaseYangOpenApiGenerator.getModulesWithoutDuplications(modelContext);
         final var portionOfModules = BaseYangOpenApiGenerator.getModelsSublist(modulesWithoutDuplications,
             offset, limit);
         final var schema = openApiGenerator.createSchemaFromUri(uri);
@@ -157,25 +171,27 @@ public class MountPointOpenApi implements DOMMountPointListener, AutoCloseable {
         final var title = deviceName + " modules of RESTCONF";
         final var url = schema + "://" + host + "/";
         final var basePath = openApiGenerator.getBasePath();
-        return new DocumentEntity(context, title, url, SECURITY, deviceName, urlPrefix, false, includeDataStore,
+        return new DocumentEntity(modelContext, title, url, SECURITY, deviceName, urlPrefix, false, includeDataStore,
             portionOfModules, basePath, width, depth);
     }
 
     public MetadataEntity getMountPointApiMeta(final long id, final int offset, final int limit) throws IOException {
-        final var iid = longIdToInstanceId.get(id);
-        final var context = getModelContext(iid);
-
-        if (context == null) {
+        final var mountId = longToMountId.get(id);
+        if (mountId == null) {
+            return null;
+        }
+        final var modelContext = modelContextFor(mountId);
+        if (modelContext == null) {
             return null;
         }
 
-        final var modulesWithoutDuplications = BaseYangOpenApiGenerator.getModulesWithoutDuplications(context);
+        final var modulesWithoutDuplications = BaseYangOpenApiGenerator.getModulesWithoutDuplications(modelContext);
         return new MetadataEntity(offset, limit, modulesWithoutDuplications.size(),
             BaseYangOpenApiGenerator.configModulesList(modulesWithoutDuplications).size());
     }
 
-    private static String extractDeviceName(final YangInstanceIdentifier iid) {
-        return ((NodeIdentifierWithPredicates.Singleton) iid.getLastPathArgument()).values().getFirst().toString();
+    private static String extractDeviceName(final YangInstanceIdentifier mountId) {
+        return ((NodeIdentifierWithPredicates.Singleton) mountId.getLastPathArgument()).values().getFirst().toString();
     }
 
     private DocumentEntity generateDataStoreOpenApi(final EffectiveModelContext modelContext, final URI uri,
@@ -191,17 +207,19 @@ public class MountPointOpenApi implements DOMMountPointListener, AutoCloseable {
 
     @Override
     public void onMountPointCreated(final DOMMountPoint mountPoint) {
-        final var path = mountPoint.getIdentifier();
-        LOG.debug("Mount point {} created", path);
+        final var mountId = mountPoint.getIdentifier();
+        LOG.debug("Mount point {} created", mountId);
         final Long idLong = idKey.incrementAndGet();
-        instanceIdToLongId.put(path, idLong);
-        longIdToInstanceId.put(idLong, path);
+        mountIdToLong.put(mountId, idLong);
+        longToMountId.put(idLong, mountId);
     }
 
     @Override
     public void onMountPointRemoved(final YangInstanceIdentifier path) {
         LOG.debug("Mount point {} removed", path);
-        final Long id = instanceIdToLongId.remove(path);
-        longIdToInstanceId.remove(id);
+        final var id = mountIdToLong.remove(path);
+        if (id != null) {
+            longToMountId.remove(id, path);
+        }
     }
 }
