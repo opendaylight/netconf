87c171efefeee0456c493498d642bcd80360b1a8
Robert Varga <robert.varga@pantheon.tech>
2025-09-17 04:41:19 +0200

    WIP: State management?
    Change-Id: I2c31d51ae4e3ad30b9c95dc402cbd0095c9722bd
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- protocol/restconf-server-spi/src/main/java/org/opendaylight/restconf/server/spi/AbstractRestconfStreamSubscription.java
+++ protocol/restconf-server-spi/src/main/java/org/opendaylight/restconf/server/spi/AbstractRestconfStreamSubscription.java
@@ -12,11 +12,13 @@ import static java.util.Objects.requireNonNull;
 import com.google.common.base.MoreObjects.ToStringHelper;
 import java.time.Instant;
 import org.eclipse.jdt.annotation.NonNull;
+import org.eclipse.jdt.annotation.NonNullByDefault;
 import org.eclipse.jdt.annotation.Nullable;
 import org.opendaylight.restconf.server.api.TransportSession;
 import org.opendaylight.restconf.server.spi.AbstractRestconfStreamRegistry.EventStreamFilter;
 import org.opendaylight.restconf.server.spi.RestconfStream.EncodingName;
 import org.opendaylight.restconf.server.spi.RestconfStream.SubscriptionState;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.subscribed.notifications.rev190909.subscriptions.subscription.receivers.Receiver.State;
 import org.opendaylight.yangtools.yang.common.QName;
 import org.opendaylight.yangtools.yang.common.Uint32;
 
@@ -24,6 +26,81 @@ import org.opendaylight.yangtools.yang.common.Uint32;
  * Abstract base class for {@link RestconfStream.Subscription}s.
  */
 public abstract class AbstractRestconfStreamSubscription extends RestconfStream.Subscription {
+    @NonNullByDefault
+    private sealed interface CurrentState {
+
+        default @Nullable State state() {
+            return switch (subscriptionState()) {
+                case ACTIVE -> State.Active;
+                case SUSPENDED -> State.Suspended;
+                case END -> null;
+            };
+        }
+
+        SubscriptionState subscriptionState();
+
+        @Nullable Instant stopTime();
+    }
+
+    @NonNullByDefault
+    private static final class Ended implements CurrentState {
+        static final Ended INSTANCE = new Ended();
+
+        private Ended() {
+            // Hidden on purpose
+        }
+
+        @Override
+        public @Nullable Instant stopTime() {
+            return null;
+        }
+
+        @Override
+        public SubscriptionState subscriptionState() {
+            return SubscriptionState.END;
+        }
+    }
+
+    @NonNullByDefault
+    private record WithStopTime(SubscriptionState subscriptionState, Instant stopTime) implements CurrentState {
+        WithStopTime {
+            requireNonNull(subscriptionState);
+            requireNonNull(stopTime);
+        }
+
+        public SubscriptionState subscriptionState() {
+            return subscriptionState;
+        }
+    }
+
+    @NonNullByDefault
+    private enum WithoutStopTime implements CurrentState {
+        ACTIVE(State.Active),
+        SUSPENDED(State.Suspended);
+
+        private State receiverState;
+
+        WithoutStopTime(State receiverState) {
+            this.receiverState = requireNonNull(receiverState);
+        }
+
+        @Override
+        public @NonNull State state() {
+            return receiverState;
+        }
+
+        @Override
+        public @Nullable Instant stopTime() {
+            return null;
+        }
+
+        @Override
+        public SubcriptionState subscriptionState() {
+            // TODO Auto-generated method stub
+            return null;
+        }
+    }
+
     private final @NonNull Uint32 id;
     private final @NonNull QName encoding;
     private final @NonNull EncodingName encodingName;
@@ -31,8 +108,7 @@ public abstract class AbstractRestconfStreamSubscription extends RestconfStream.
     private final @NonNull String receiverName;
     private final @NonNull TransportSession session;
 
-    private @NonNull SubscriptionState state = SubscriptionState.ACTIVE;
-    private @Nullable Instant stopTime;
+    private @NonNull CurrentState currentState;
 
     protected AbstractRestconfStreamSubscription(final Uint32 id, final QName encoding, final EncodingName encodingName,
             final String streamName, final String receiverName, final TransportSession session,
@@ -43,7 +119,8 @@ public abstract class AbstractRestconfStreamSubscription extends RestconfStream.
         this.session = requireNonNull(session);
         this.streamName = requireNonNull(streamName);
         this.receiverName = requireNonNull(receiverName);
-        this.stopTime = stopTime;
+
+        currentState = stopTime == null ? WithoutStopTime.ACTIVE : new WithStopTime(State.Active, stopTime);
     }
 
     @Override
@@ -68,21 +145,21 @@ public abstract class AbstractRestconfStreamSubscription extends RestconfStream.
 
     @Override
     public final SubscriptionState state() {
-        return state;
+        return currentState.subscriptionState();
     }
 
     @Override
     public @Nullable Instant stopTime() {
-        return stopTime;
+        return currentState.stopTime();
     }
 
     @Override
     public void setState(final SubscriptionState newState) {
-        if (state.canMoveTo(newState)) {
-            state = newState;
-        } else {
+        final var state = state();
+        if (!state.canMoveTo(newState)) {
             throw new IllegalStateException("Cannot transition from " + state + " to " + newState);
         }
+        this.state = newState;
     }
 
     @Override
