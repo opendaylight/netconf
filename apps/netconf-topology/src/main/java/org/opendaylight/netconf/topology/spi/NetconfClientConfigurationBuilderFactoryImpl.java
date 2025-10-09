2ddd2bb307857de8049c6a779cd2b5d0141055d3
Samuel Schneider <samuel.schneider@pantheon.tech>
2025-10-09 17:43:17 +0200

    WIP: Make SSH transport configurable
    Add configuration for ciphers, macs, key exchange and
    host keys algorithms to netconf-topology.
    
    Default configuration being empty string which
    results in default options of mina-sshd client being
    used.
    
    The possible values for these configurations can be
    found in yang enum defined by RFC9644.
    And should be separated by ',' for multiple options.
    
    TODO:
      - verify current approach
      - add new revision
      - is it possible to use RFC9644 types instead of string?
    
    JIRA: NETCONF-1502
    Change-Id: Ic5208602cccb7618026e63ca37abc1e05e08702a
    Signed-off-by: Samuel Schneider <samuel.schneider@pantheon.tech>

--- apps/netconf-topology/src/main/java/org/opendaylight/netconf/topology/spi/NetconfClientConfigurationBuilderFactoryImpl.java
+++ apps/netconf-topology/src/main/java/org/opendaylight/netconf/topology/spi/NetconfClientConfigurationBuilderFactoryImpl.java
@@ -11,9 +11,12 @@ import static java.util.Objects.requireNonNull;
 
 import java.nio.charset.StandardCharsets;
 import java.security.GeneralSecurityException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Pattern;
 import javax.inject.Inject;
 import javax.inject.Singleton;
+import org.eclipse.jdt.annotation.NonNull;
 import org.opendaylight.aaa.encrypt.AAAEncryptionService;
 import org.opendaylight.netconf.client.conf.NetconfClientConfiguration.NetconfClientProtocol;
 import org.opendaylight.netconf.client.conf.NetconfClientConfigurationBuilder;
@@ -29,7 +32,17 @@ import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.netconf.cli
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.netconf.client.rev240814.netconf.client.initiate.stack.grouping.transport.ssh.ssh.TcpClientParametersBuilder;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.ssh.client.rev241010.ssh.client.grouping.ClientIdentity;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.ssh.client.rev241010.ssh.client.grouping.ClientIdentityBuilder;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.ssh.client.rev241010.ssh.client.grouping.TransportParams;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.ssh.client.rev241010.ssh.client.grouping.TransportParamsBuilder;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.ssh.client.rev241010.ssh.client.grouping.client.identity.PasswordBuilder;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.ssh.common.rev241010.SshEncryptionAlgorithm;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.ssh.common.rev241010.SshKeyExchangeAlgorithm;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.ssh.common.rev241010.SshMacAlgorithm;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.ssh.common.rev241010.SshPublicKeyAlgorithm;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.ssh.common.rev241010.transport.params.grouping.EncryptionBuilder;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.ssh.common.rev241010.transport.params.grouping.HostKeyBuilder;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.ssh.common.rev241010.transport.params.grouping.KeyExchangeBuilder;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.ssh.common.rev241010.transport.params.grouping.MacBuilder;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.netconf.device.rev241009.connection.parameters.Protocol.Name;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.netconf.device.rev241009.credentials.Credentials;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.netconf.device.rev241009.credentials.credentials.KeyAuth;
@@ -47,6 +60,8 @@ import org.osgi.service.component.annotations.Reference;
 @Component
 @Singleton
 public final class NetconfClientConfigurationBuilderFactoryImpl implements NetconfClientConfigurationBuilderFactory {
+    private static final Pattern COMMA_WITH_WHITESPACE = Pattern.compile(",\\s*");
+
     private final SslContextFactoryProvider sslContextFactoryProvider;
     private final AAAEncryptionService encryptionService;
     private final CredentialProvider credentialProvider;
@@ -71,7 +86,7 @@ public final class NetconfClientConfigurationBuilderFactoryImpl implements Netco
             builder.withProtocol(NetconfClientProtocol.TCP);
         } else if (protocol == null || protocol.getName() == Name.SSH) {
             builder.withProtocol(NetconfClientProtocol.SSH);
-            setSshParametersFromCredentials(builder, node.getCredentials());
+            setSshParametersFromCredentials(builder, node.getCredentials(), node);
         } else if (protocol.getName() == Name.TLS) {
             final var contextFactory = sslContextFactoryProvider.getSslContextFactory(protocol.getSpecification());
             final var sslContext = protocol.getKeyId() == null
@@ -97,7 +112,7 @@ public final class NetconfClientConfigurationBuilderFactoryImpl implements Netco
     }
 
     private void setSshParametersFromCredentials(final NetconfClientConfigurationBuilder confBuilder,
-            final Credentials credentials) {
+            final Credentials credentials, final NetconfNode node) {
         final var sshParamsBuilder = new SshClientParametersBuilder();
         if (credentials instanceof LoginPwUnencrypted unencrypted) {
             final var loginPassword = unencrypted.getLoginPasswordUnencrypted();
@@ -139,6 +154,7 @@ public final class NetconfClientConfigurationBuilderFactoryImpl implements Netco
         } else {
             throw new IllegalArgumentException("Unsupported credential type: " + credentials.getClass());
         }
+        sshParamsBuilder.setTransportParams(parseTransportParams(node));
         confBuilder.withSshParameters(sshParamsBuilder.build());
     }
 
@@ -151,5 +167,55 @@ public final class NetconfClientConfigurationBuilderFactoryImpl implements Netco
                     .build())
                 .build())
             .build();
+    }
+
+    private @NonNull TransportParams parseTransportParams(final NetconfNode node) {
+        // KEX
+        final var kexRaw = node.requireKeyExchange();
+        final var kex = kexRaw.isEmpty() ? new String[0] : COMMA_WITH_WHITESPACE.split(kexRaw);
+        final var kexAlg = new ArrayList<SshKeyExchangeAlgorithm>();
+        final var kexBuilder = new KeyExchangeBuilder();
+        for (var alg : kex) {
+            kexAlg.add(new SshKeyExchangeAlgorithm(org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.iana.ssh
+                .key.exchange.algs.rev241016.SshKeyExchangeAlgorithm.ofName(alg)));
+        }
+        final var transportParamBuilder = new TransportParamsBuilder();
+        transportParamBuilder.setKeyExchange(kexBuilder.setKeyExchangeAlg(kexAlg).build());
+
+        //MACS
+        final var macsRaw = node.requireMacs();
+        final var macs = macsRaw.isEmpty() ? new String[0] : COMMA_WITH_WHITESPACE.split(macsRaw);
+        final var macAlg = new ArrayList<SshMacAlgorithm>();
+        final var macBuilder = new MacBuilder();
+        for (var alg : macs) {
+            macAlg.add(new SshMacAlgorithm(org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.iana.ssh
+                .mac.algs.rev241016.SshMacAlgorithm.ofName(alg)));
+        }
+        transportParamBuilder.setMac(macBuilder.setMacAlg(macAlg).build());
+
+        //ENCRYPTION (ciphers)
+        final var encryptionsRaw = node.requireEncryption();
+        final var encryptions = encryptionsRaw.isEmpty() ? new String[0] : COMMA_WITH_WHITESPACE.split(encryptionsRaw);
+        final var encryptionAlg = new ArrayList<SshEncryptionAlgorithm>();
+        final var encryptionBuilder = new EncryptionBuilder();
+        for (var alg : encryptions) {
+            encryptionAlg.add(new SshEncryptionAlgorithm(org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.iana
+                .ssh.encryption.algs.rev241016.SshEncryptionAlgorithm.ofName(alg)));
+        }
+        transportParamBuilder.setEncryption(encryptionBuilder.setEncryptionAlg(encryptionAlg).build());
+
+
+        // HOST KEYS (signatures)
+        final var hostKeysRaw = node.requireHostKeys();
+        final var hostKeys = hostKeysRaw.isEmpty() ? new String[0] : COMMA_WITH_WHITESPACE.split(hostKeysRaw);
+        final var hostKeysAlg = new ArrayList<SshPublicKeyAlgorithm>();
+        final var hostKeyBuilder = new HostKeyBuilder();
+        for (var alg : hostKeys) {
+            hostKeysAlg.add(new SshPublicKeyAlgorithm(org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.iana.ssh
+                ._public.key.algs.rev241016.SshPublicKeyAlgorithm.ofName(alg)));
+        }
+        transportParamBuilder.setHostKey(hostKeyBuilder.setHostKeyAlg(hostKeysAlg).build());
+
+        return transportParamBuilder.build();
     }
 }
