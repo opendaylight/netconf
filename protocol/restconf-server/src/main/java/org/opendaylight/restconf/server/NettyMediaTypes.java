015264f4db732fafd72c3e1fc67988af8d906dbd
Robert Varga <robert.varga@pantheon.tech>
2025-10-10 22:57:26 +0200

    Hide NettyMediaTypes
    This class is only accessed from within restconf-server. Do not expose
    them to the outside world and annotate the constants as @NonNull.
    
    Change-Id: I78142acc3e2dfd0a42bf2c9fa0794def63351cdf
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- protocol/restconf-server/src/main/java/org/opendaylight/restconf/server/NettyMediaTypes.java
+++ protocol/restconf-server/src/main/java/org/opendaylight/restconf/server/NettyMediaTypes.java
@@ -8,52 +8,50 @@
 package org.opendaylight.restconf.server;
 
 import io.netty.util.AsciiString;
+import org.eclipse.jdt.annotation.NonNullByDefault;
 import org.opendaylight.restconf.api.MediaTypes;
 import org.opendaylight.yangtools.yang.common.YangConstants;
 
-public final class NettyMediaTypes {
+@NonNullByDefault
+final class NettyMediaTypes {
     /**
      * A {@link AsciiString} constant representing {@value MediaTypes#APPLICATION_YANG_DATA_XML} media type.
      *
      * @see <a href="https://www.rfc-editor.org/rfc/rfc8040#section-11.3.1">RFC8040, section 11.3.1</a>
      */
-    public static final AsciiString APPLICATION_YANG_DATA_XML =
-        AsciiString.cached(MediaTypes.APPLICATION_YANG_DATA_XML);
+    static final AsciiString APPLICATION_YANG_DATA_XML = AsciiString.cached(MediaTypes.APPLICATION_YANG_DATA_XML);
     /**
      * A {@link AsciiString} constant representing {@value MediaTypes#APPLICATION_YANG_DATA_JSON} media type.
      *
      * @see <a href="https://www.rfc-editor.org/rfc/rfc8040#section-11.3.2">RFC8040, section 11.3.2</a>
      */
-    public static final AsciiString APPLICATION_YANG_DATA_JSON =
-        AsciiString.cached(MediaTypes.APPLICATION_YANG_DATA_JSON);
+    static final AsciiString APPLICATION_YANG_DATA_JSON = AsciiString.cached(MediaTypes.APPLICATION_YANG_DATA_JSON);
     /**
      * A {@link AsciiString} constant representing {@value MediaTypes#APPLICATION_YANG_PATCH_XML} media type.
      *
      * @see <a href="https://www.rfc-editor.org/rfc/rfc8072#section-4.2.1">RFC8072, section 4.2.1</a>
      */
-    public static final AsciiString APPLICATION_YANG_PATCH_XML =
-        AsciiString.cached(MediaTypes.APPLICATION_YANG_PATCH_XML);
+    static final AsciiString APPLICATION_YANG_PATCH_XML = AsciiString.cached(MediaTypes.APPLICATION_YANG_PATCH_XML);
     /**
      * A {@link AsciiString} constant representing {@value MediaTypes#APPLICATION_YANG_PATCH_JSON} media type.
      *
      * @see <a href="https://www.rfc-editor.org/rfc/rfc8072#section-4.2.1">RFC8072, section 4.2.2</a>
      */
-    public static final AsciiString APPLICATION_YANG_PATCH_JSON =
-        AsciiString.cached(MediaTypes.APPLICATION_YANG_PATCH_JSON);
+    static final AsciiString APPLICATION_YANG_PATCH_JSON = AsciiString.cached(MediaTypes.APPLICATION_YANG_PATCH_JSON);
     /**
      * A {@link AsciiString} constant representing {@value YangConstants#RFC6020_YANG_MEDIA_TYPE} media type.
      *
      * @see <a href="https://www.rfc-editor.org/rfc/rfc6020#section-14.1">RFC6020, section 14.1</a>
      */
-    public static final AsciiString APPLICATION_YANG = AsciiString.cached(YangConstants.RFC6020_YANG_MEDIA_TYPE);
+    static final AsciiString APPLICATION_YANG = AsciiString.cached(YangConstants.RFC6020_YANG_MEDIA_TYPE);
     /**
      * A {@link AsciiString} constant representing {@value YangConstants#RFC6020_YIN_MEDIA_TYPE} media type.
      *
      * @see <a href="https://www.rfc-editor.org/rfc/rfc6020#section-14.2">RFC6020, section 14.2</a>
      */
-    public static final AsciiString APPLICATION_YIN_XML = AsciiString.cached(YangConstants.RFC6020_YIN_MEDIA_TYPE);
+    static final AsciiString APPLICATION_YIN_XML = AsciiString.cached(YangConstants.RFC6020_YIN_MEDIA_TYPE);
 
-    public static final AsciiString TEXT_XML = AsciiString.cached("text/xml");
+    static final AsciiString TEXT_XML = AsciiString.cached("text/xml");
 
     private NettyMediaTypes() {
         // hidden on purpose
