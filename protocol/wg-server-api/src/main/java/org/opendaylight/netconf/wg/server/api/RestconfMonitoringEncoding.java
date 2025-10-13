4a35ea7c49574aa42b88695c1f0195b15a85a641
Robert Varga <robert.varga@pantheon.tech>
2025-10-13 22:02:57 +0200

    Split out wg-server-api
    There is a server model underlying NETCONF and RESTCONF, which we define
    here to encapsulate the shared bits where ietf-restconf-monitoring is
    concerned: each stream has an encoding, nominally "json" or "xml".
    
    JIRA: NETCONF-1524
    Change-Id: I3d44d534ed87a1ddabdbfd8642372f2af446c518
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- protocol/wg-server-api/src/main/java/org/opendaylight/netconf/wg/server/api/RestconfMonitoringEncoding.java
+++ protocol/wg-server-api/src/main/java/org/opendaylight/netconf/wg/server/api/RestconfMonitoringEncoding.java
@@ -5,36 +5,37 @@
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
-package org.opendaylight.restconf.server.api;
+package org.opendaylight.netconf.wg.server.api;
 
 import java.util.regex.Pattern;
 import org.eclipse.jdt.annotation.NonNullByDefault;
 
 /**
- * An opinionated view on what values we can produce for {@code leaf encoding}. The name can only be composed
- * of one or more characters matching {@code [a-zA-Z]}.
+ * An opinionated view on what values we can produce for {@code leaf encoding} in the context of
+ * {@code ietf-restconf-monitoring.yang}. The name can only be composed of one or more characters matching
+ * {@code [a-zA-Z]}.
  *
  * @param value Encoding name, as visible via the stream's {@code access} list's {@code encoding} leaf
  */
 @NonNullByDefault
-public record MonitoringEncoding(String value) {
+public record RestconfMonitoringEncoding(String value) {
     private static final Pattern PATTERN = Pattern.compile("[a-zA-Z]+");
 
     /**
      * Well-known JSON encoding defined by RFC8040's {@code ietf-restconf-monitoring.yang} as {@code json}.
      */
-    public static final MonitoringEncoding JSON = new MonitoringEncoding("json");
+    public static final RestconfMonitoringEncoding JSON = new RestconfMonitoringEncoding("json");
     /**
      * Well-known XML encoding defined by RFC8040's {@code ietf-restconf-monitoring.yang} as {@code xml}.
      */
-    public static final MonitoringEncoding XML = new MonitoringEncoding("xml");
+    public static final RestconfMonitoringEncoding XML = new RestconfMonitoringEncoding("xml");
 
     /**
      * Default constructor.
      *
      * @param value the encoding name
      */
-    public MonitoringEncoding {
+    public RestconfMonitoringEncoding {
         if (!PATTERN.matcher(value).matches()) {
             throw new IllegalArgumentException("name must match " + PATTERN);
         }
@@ -44,14 +45,14 @@ public record MonitoringEncoding(String value) {
      * Factory method, taking into consideration well-known values.
      *
      * @param value the encoding name
-     * @return A {@link MonitoringEncoding}
+     * @return A {@link RestconfMonitoringEncoding}
      * @throws IllegalArgumentException if the {@code name} is not a valid encoding name
      */
-    public static MonitoringEncoding of(String value) {
+    public static RestconfMonitoringEncoding of(final String value) {
         return switch (value) {
             case "json" -> JSON;
             case "xml" -> XML;
-            default -> new MonitoringEncoding(value);
+            default -> new RestconfMonitoringEncoding(value);
         };
     }
 }
