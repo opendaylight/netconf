000aa347f5ade0564051776c713e79bcae48ab1d
Yaroslav Lastivka <yaroslav.lastivka@pantheon.tech>
2025-10-14 18:23:16 +0300

    Implement ietf-netconf model in test devices
    Provide a minimal ietf-netconf.yang implementation in mock NETCONF
    devices so they advertise and serve the base model consistently during
    tests.
    
    JIRA: NETCONF-1474
    Change-Id: If211fcba85d87cedd956c82be2b30c536d930ba2
    Signed-off-by: Yaroslav Lastivka <yaroslav.lastivka@pantheon.tech>
    (cherry picked from commit f94f68166c52485307b4822ed550ac27f5f3549a)

--- plugins/netconf-client-mdsal/src/test/java/org/opendaylight/netconf/client/mdsal/AbstractTestModelTest.java
+++ plugins/netconf-client-mdsal/src/test/java/org/opendaylight/netconf/client/mdsal/AbstractTestModelTest.java
@@ -11,13 +11,62 @@ import com.google.common.util.concurrent.Futures;
 import com.google.common.util.concurrent.ListenableFuture;
 import org.eclipse.jdt.annotation.NonNullByDefault;
 import org.opendaylight.netconf.databind.DatabindContext;
+import org.opendaylight.yangtools.yang.common.Revision;
 import org.opendaylight.yangtools.yang.model.api.EffectiveModelContext;
+import org.opendaylight.yangtools.yang.model.api.source.SourceIdentifier;
+import org.opendaylight.yangtools.yang.model.spi.source.StringYangTextSource;
+import org.opendaylight.yangtools.yang.model.spi.source.URLYangTextSource;
+import org.opendaylight.yangtools.yang.parser.api.YangParserConfiguration;
 import org.opendaylight.yangtools.yang.test.util.YangParserTestUtils;
 
 @NonNullByDefault
 public abstract class AbstractTestModelTest extends AbstractBaseSchemasTest {
+<<<<<<< HEAD   (7fddc7 Introduce SchemaSetup.Result)
     protected static final EffectiveModelContext TEST_MODEL =
         YangParserTestUtils.parseYangResource("/schemas/test-module.yang");
+=======
+    protected static final EffectiveModelContext TEST_MODEL = YangParserTestUtils.parseYangSources(
+        YangParserConfiguration.DEFAULT, null, new StringYangTextSource(
+            new SourceIdentifier("test-module", Revision.of("2013-07-22")),
+            """
+                module test-module {
+                  yang-version 1;
+                  namespace "test:namespace";
+                  prefix "tt";
+
+                  description "Types for testing";
+
+                  revision "2013-07-22";
+
+                  container c {
+                    leaf a {
+                      type string;
+                    }
+
+                    leaf b {
+                      type string;
+                    }
+
+                    container d {
+                      leaf x {
+                        type boolean;
+                      }
+                    }
+                  }
+
+                  container e {
+                    leaf z {
+                      type uint8;
+                    }
+                  }
+                }""", "test-module"),
+        new URLYangTextSource(AbstractTestModelTest.class.getResource(
+            "/META-INF/yang/ietf-netconf@2011-06-01.yang")),
+        new URLYangTextSource(AbstractTestModelTest.class.getResource(
+            "/META-INF/yang/ietf-inet-types@2013-07-15.yang"))
+    );
+
+>>>>>>> CHANGE (f94f68 Implement ietf-netconf model in test devices)
     protected static final DatabindContext TEST_DATABIND = DatabindContext.ofModel(TEST_MODEL);
     protected static final ListenableFuture<EffectiveModelContext> TEST_MODEL_FUTURE =
         Futures.immediateFuture(TEST_MODEL);
