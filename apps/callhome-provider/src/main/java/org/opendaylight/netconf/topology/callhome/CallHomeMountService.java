1eb4e2fbfc8fb8ebce539c3e8251985f2901809f
Matej Sramcik <matej.sramcik@pantheon.tech>
2025-10-20 09:48:14 +0200

    Improve CallHomeMountService dispatch
    Modernize DataObjectModification dispatch by using its sealed hierarchy
    and eliminating switch statement as all cases leaded to same result.
    
    This eliminates the need for some mocking in
    CallHomeMountServiceTest, which is removed as well.
    
    JIRA: NETCONF-1530
    Change-Id: I1c7726d06fbbb290ce9f1a166bbf60e44a2e2bcd
    Signed-off-by: Matej Sramcik <matej.sramcik@pantheon.tech>

--- apps/callhome-provider/src/main/java/org/opendaylight/netconf/topology/callhome/CallHomeMountService.java
+++ apps/callhome-provider/src/main/java/org/opendaylight/netconf/topology/callhome/CallHomeMountService.java
@@ -338,19 +338,9 @@ public final class CallHomeMountService implements AutoCloseable {
     @VisibleForTesting
     void onAllowedDevicesChanged(final List<DataTreeModification<Device>> changes) {
         for (final var change : changes) {
-            final var rootNode = change.getRootNode();
-            switch (rootNode.modificationType()) {
-                case DELETE:
-                case WRITE:
-                case SUBTREE_MODIFIED:
-                    final var deletedDevice = rootNode.dataBefore();
-                    if (deletedDevice != null) {
-                        final var uniqueId = deletedDevice.getUniqueId();
-                        topology.disableNode(new NodeId(uniqueId));
-                    }
-                    break;
-                default:
-                    break;
+            final var deletedDevice = change.getRootNode().dataBefore();
+            if (deletedDevice != null) {
+                topology.disableNode(new NodeId(deletedDevice.getUniqueId()));
             }
         }
     }
