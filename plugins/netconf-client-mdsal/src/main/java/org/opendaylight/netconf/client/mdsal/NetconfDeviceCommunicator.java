63b6e73bd698a1e0da8072eb1b49ab427ddd3eb5
Peter Suna <peter.suna@pantheon.tech>
2025-10-24 09:32:57 +0200

    Print netconf message in debug LOG
    If a request to the device times out, we restart the connection.
    This jumps to the case where we receive a message but have an empty
    request poll. We will log that we received such a message and
    ignore it, including the decoded whole message.
    
    When this happens with a get-schema request, we print the yang models
    in the karaf logs, which makes them unclear and unnecessarily large
    due to the yang models size.
    
    Instead, show the message-id in the warning and print the message only
    in debug mode.
    
    JIRA: NETCONF-1501
    Change-Id: I180b8334c3dede8a12c5608196a9d876e530ac2e
    Signed-off-by: Peter Suna <peter.suna@pantheon.tech>

--- plugins/netconf-client-mdsal/src/main/java/org/opendaylight/netconf/client/mdsal/NetconfDeviceCommunicator.java
+++ plugins/netconf-client-mdsal/src/main/java/org/opendaylight/netconf/client/mdsal/NetconfDeviceCommunicator.java
@@ -278,7 +278,11 @@ public class NetconfDeviceCommunicator implements NetconfClientSessionListener, 
         if (request == null) {
             // No matching request, bail out
             if (LOG.isWarnEnabled()) {
-                LOG.warn("{}: Ignoring unsolicited message {}", id, msgToS(message));
+                LOG.warn("{}: Ignoring unsolicited message with id {} ", id, message.getDocument().getDocumentElement()
+                    .getAttribute(XmlNetconfConstants.MESSAGE_ID));
+            }
+            if (LOG.isDebugEnabled()) {
+                LOG.debug("{}: Received unsolicited message {}", id, msgToS(message));
             }
             return;
         }
