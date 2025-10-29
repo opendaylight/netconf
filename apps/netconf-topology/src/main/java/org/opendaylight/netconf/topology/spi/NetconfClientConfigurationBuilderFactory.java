d1ef45f75ff1f37295044b91404161cd211648d8
Samuel Schneider <samuel.schneider@pantheon.tech>
2025-10-29 14:05:19 +0100

    Make topology SSH transport configurable
    Add default topology SSH transport configurable.
    Add configuration for ciphers, macs, key exchange and
    host keys algorithms to netconf topology. This config
    serves as default for nodes and can be overridden by
    specific netconf node configuration.
    
    JIRA: NETCONF-1502
    Change-Id: Ie16220d3a54fb2b5355abca2e2db21e765d5b970
    Signed-off-by: Samuel Schneider <samuel.schneider@pantheon.tech>

--- apps/netconf-topology/src/main/java/org/opendaylight/netconf/topology/spi/NetconfClientConfigurationBuilderFactory.java
+++ apps/netconf-topology/src/main/java/org/opendaylight/netconf/topology/spi/NetconfClientConfigurationBuilderFactory.java
@@ -10,6 +10,7 @@ package org.opendaylight.netconf.topology.spi;
 import org.eclipse.jdt.annotation.NonNull;
 import org.opendaylight.netconf.client.conf.NetconfClientConfigurationBuilder;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.netconf.node.topology.rev251028.netconf.node.augment.NetconfNode;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netconf.node.topology.rev251028.network.topology.topology.topology.types.topology.netconf.SshTransportTopologyParameters;
 import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.NodeId;
 
 /**
@@ -24,5 +25,5 @@ public interface NetconfClientConfigurationBuilderFactory {
      * @return An initialized {@link NetconfClientConfigurationBuilder}
      */
     @NonNull NetconfClientConfigurationBuilder createClientConfigurationBuilder(@NonNull NodeId nodeId,
-        @NonNull NetconfNode node);
+        @NonNull NetconfNode node, SshTransportTopologyParameters sshParams);
 }
