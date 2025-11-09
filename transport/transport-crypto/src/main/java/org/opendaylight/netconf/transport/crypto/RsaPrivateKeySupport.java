50d01139b3751a47f3d520cccb57a06c070bb704
Robert Varga <robert.varga@pantheon.tech>
2025-11-10 00:19:52 +0100

    WIP: Split out supports
    This adds support for modularized take on what we can do.
    
    // FIXME: figure more stuff out:
    //        - the X.509 story is weird and not we normally do
    //        - we really depend on BouncyCastle, so how about we roll with
    //          it and do not involve JCE at all?
    
    // FIXME: overall this is too ambitious and should be done separately
    
    JIRA: NETCONF-1506
    Change-Id: Id4129fcb2106e6a7c1a1ac0a666cfd2f26b058c2
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ transport/transport-crypto/src/main/java/org/opendaylight/netconf/transport/crypto/RsaPrivateKeySupport.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2025 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.netconf.transport.crypto;
+
+import java.security.KeyFactory;
+import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.PKCS8EncodedKeySpec;
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.opendaylight.netconf.transport.api.UnsupportedConfigurationException;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.crypto.types.rev241010.RsaPrivateKeyFormat;
+
+/**
+ * {@link PrivateKeySupport} for {@link RsaPrivateKeyFormat}.
+ */
+// FIXME: turn this into a component once we have Dagger
+@NonNullByDefault
+final class RsaPrivateKeySupport implements PrivateKeySupport<RsaPrivateKeyFormat> {
+    RsaPrivateKeySupport() {
+        // Nothing else
+    }
+
+    @Override
+    public RsaPrivateKeyFormat format() {
+        return RsaPrivateKeyFormat.VALUE;
+    }
+
+    @Override
+    public PrivateKey createKey(byte[] bytes) throws UnsupportedConfigurationException {
+        try {
+            return KeyFactory.getInstance("RSA").generatePrivate(new PKCS8EncodedKeySpec(bytes));
+        } catch (InvalidKeySpecException e) {
+            throw new UnsupportedConfigurationException("Invalid RSAPrivateKey", e);
+        } catch (NoSuchAlgorithmException e) {
+            throw new UnsupportedConfigurationException("RSAPrivateKey is not supported", e);
+        }
+    }
+}
