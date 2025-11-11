1d70774f89d5a61f6a4851323378e614e002855d
Robert Varga <robert.varga@pantheon.tech>
2025-11-11 17:02:26 +0100

    Unify KeyUtils.validateKeyPair()
    The KeyPair validation needs to live in KeyPairParser. Move it there,
    eliminating code duplication. Also drop a FIXME, as we should be able
    to check the correspondence.
    
    JIRA: NETCONF-1506
    Change-Id: Ie35361f3a498f2ea4c6897fd11b9fc7b731099c2
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- transport/transport-ssh/src/main/java/org/opendaylight/netconf/transport/ssh/KeyUtils.java
+++ transport/transport-ssh/src/main/java/org/opendaylight/netconf/transport/ssh/KeyUtils.java
@@ -7,18 +7,10 @@
  */
 package org.opendaylight.netconf.transport.ssh;
 
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
-import java.security.PrivateKey;
 import java.security.PublicKey;
 import java.security.Security;
-import java.security.Signature;
-import java.security.SignatureException;
 import java.security.cert.Certificate;
-import java.security.interfaces.ECPrivateKey;
-import java.security.interfaces.RSAPrivateKey;
 import java.util.Objects;
-import java.util.concurrent.ThreadLocalRandom;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.opendaylight.netconf.transport.api.UnsupportedConfigurationException;
 
@@ -29,36 +21,6 @@ final class KeyUtils {
 
     private KeyUtils() {
         // utility class
-    }
-
-    static void validateKeyPair(final PublicKey publicKey, final PrivateKey privateKey)
-            throws UnsupportedConfigurationException {
-        final String signAlgorithm;
-        if (privateKey instanceof RSAPrivateKey) {
-            signAlgorithm = "SHA256withRSA";
-        } else if (privateKey instanceof ECPrivateKey) {
-            signAlgorithm = "SHA256withECDSA";
-        } else {
-            throw new UnsupportedConfigurationException("Unsupported key type " + privateKey);
-        }
-        try {
-            // test data
-            final byte[] original = new byte[1024];
-            ThreadLocalRandom.current().nextBytes(original);
-            // sign using the private key
-            final Signature signature = Signature.getInstance(signAlgorithm);
-            signature.initSign(privateKey);
-            signature.update(original);
-            final byte[] signed = signature.sign();
-            // verify using the public key
-            signature.initVerify(publicKey);
-            signature.update(original);
-            if (!signature.verify(signed)) {
-                throw new UnsupportedConfigurationException("Private key mismatches Public key");
-            }
-        } catch (NoSuchAlgorithmException | InvalidKeyException | SignatureException e) {
-            throw new UnsupportedConfigurationException("Key pair validation failed", e);
-        }
     }
 
     static void validatePublicKey(final PublicKey publicKey, final Certificate certificate)
