76f84ab34956714a0bedc0c87774323a444afba8
Matej Sramcik <matej.sramcik@pantheon.tech>
2025-12-09 14:32:33 +0100

    Expose used SSH transport parameters
    Introduced SSHNegotiatedAlgListener to capture negotiated transport
    parameters after session establishment.
    Updated SSHClient to notify the listener once algorithms are negotiated
    and add them to datastore when device is successfully connected.
    
    JIRA: NETCONF-1534
    Change-Id: Id54a28d5be164405e4f7a417d8bd26680c2e91d1
    Signed-off-by: Matej Sramcik <matej.sramcik@pantheon.tech>

---
+++ transport/transport-ssh/src/main/java/org/opendaylight/netconf/transport/ssh/SSHNegotiatedAlgListener.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2026 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.netconf.transport.ssh;
+
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.ssh.common.rev241010.SshEncryptionAlgorithm;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.ssh.common.rev241010.SshKeyExchangeAlgorithm;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.ssh.common.rev241010.SshMacAlgorithm;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.ssh.common.rev241010.SshPublicKeyAlgorithm;
+
+/**
+ * SSH connection algorithms listener.
+ */
+public interface SSHNegotiatedAlgListener {
+    /**
+     * Invoked when SSH connection algorithms are negotiated.
+     *
+     * @param kexAlgorithm key exchange algorithm
+     * @param hostKey server host key algorithm
+     * @param encryption encryption algorithm
+     * @param mac mac algorithm
+     */
+    void onAlgorithmsNegotiated(SshKeyExchangeAlgorithm kexAlgorithm, SshPublicKeyAlgorithm hostKey,
+        SshEncryptionAlgorithm encryption, SshMacAlgorithm mac);
+}
