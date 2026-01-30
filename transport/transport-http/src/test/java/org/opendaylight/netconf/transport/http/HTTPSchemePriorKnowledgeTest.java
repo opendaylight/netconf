f1f7fbab06d76963f91f3b135d1396113bb14f8f
Yaroslav Lastivka <yaroslav.lastivka@pantheon.tech>
2026-01-30 10:28:13 +0200

    Add HTTP/2 prior-knowledge support
    Integrate HTTP/2 cleartext prior-knowledge with the
    existing RESTCONF Netty server pipeline.
    Use CleartextHttp2ServerUpgradeHandler for both h2c upgrade and
    prior-knowledge, relying on it to install Http2FrameCodec. Extend
    CleartextUpgradeHandler to handle PriorKnowledgeUpgradeEvent by adding
    Http2MultiplexHandler after the frame codec and firing the existing
    HTTPServerPipelineSetup.HTTP_2 event so ConcurrentRestconfSession is
    created for HTTP/2 as well.
    
    JIRA: NETCONF-1557
    Change-Id: Ib0af96865052eb440f84095dff8b3120cd0f3b14
    Signed-off-by: Yaroslav Lastivka <yaroslav.lastivka@pantheon.tech>

---
+++ transport/transport-http/src/test/java/org/opendaylight/netconf/transport/http/HTTPSchemePriorKnowledgeTest.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright (c) 2026 PANTHEON.tech, s.r.o. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.netconf.transport.http;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import io.netty.buffer.Unpooled;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.channel.embedded.EmbeddedChannel;
+import io.netty.handler.codec.http.HttpObjectAggregator;
+import io.netty.handler.codec.http2.Http2StreamFrameToHttpObjectCodec;
+import io.netty.util.CharsetUtil;
+import java.util.ArrayList;
+import org.junit.jupiter.api.Test;
+import org.opendaylight.yangtools.yang.common.Uint32;
+
+class HTTPSchemePriorKnowledgeTest {
+    @Test
+    void http2PriorKnowledgeCleartext() {
+        final var events = new ArrayList<>();
+        final var eventCatcher = new SimpleChannelInboundHandler<>(false) {
+            @Override
+            protected void channelRead0(final ChannelHandlerContext ctx, final Object msg) {
+                // ignore
+            }
+
+            @Override
+            public void userEventTriggered(final ChannelHandlerContext ctx, final Object evt) {
+                events.add(evt);
+            }
+        };
+
+        final var dummy = new ChannelInboundHandlerAdapter();
+        final var channel = new EmbeddedChannel(dummy, eventCatcher);
+
+        HTTPScheme.HTTP.initializeServerPipeline(channel.pipeline().context(dummy), new ChannelInitializer<>() {
+            @Override
+            protected void initChannel(final Channel ch) {
+                ch.pipeline().addLast(new Http2StreamFrameToHttpObjectCodec(true));
+                ch.pipeline().addLast(new HttpObjectAggregator(HTTPServer.MAX_HTTP_CONTENT_LENGTH));
+            }
+        }, Uint32.valueOf(16384));
+
+        final var preface = Unpooled.copiedBuffer("PRI * HTTP/2.0\r\n\r\nSM\r\n\r\n", CharsetUtil.US_ASCII);
+        channel.writeInbound(preface);
+        channel.runPendingTasks();
+
+        assertTrue(events.contains(HTTPServerPipelineSetup.HTTP_2),
+            "Expected HTTP/2 setup event for prior-knowledge preface");
+        assertNotNull(channel.pipeline().get("h2-multiplexer"));
+        channel.finishAndReleaseAll();
+    }
+}
