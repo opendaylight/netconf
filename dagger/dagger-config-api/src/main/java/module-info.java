f17b5281da21242986d4f3b8c1ba1d5dff96f6bd
Peter Suna <peter.suna@pantheon.tech>
2026-02-10 17:10:27 +0100

    Add non-OSGi configuration loader
    Add configuration loader for Dagger applications. Implement springboot
    configuration loader with spring-boot-configuration-processor.
    
    The spring-boot-configuration-processor plugin allows to generate
    configuration metadata file from annotated DTO configuration file.
    
    JIRA: NETCONF-1569
    Change-Id: I9815963d1bf95438aad31da51228cb5facdcf008
    Signed-off-by: Peter Suna <peter.suna@pantheon.tech>

---
+++ dagger/dagger-config-api/src/main/java/module-info.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2026 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+/**
+ * Configuration loader module for components that are not initialized or managed by the OSGi.
+ */
+module org.opendaylight.netconf.dagger.config.api {
+    exports org.opendaylight.netconf.dagger.config.api;
+
+    requires static org.eclipse.jdt.annotation;
+}
