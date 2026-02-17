c83c259c8c640e7a88f965a5a3ee3a285a6b9dd2
Anil Belur <abelur@linuxfoundation.org>
2026-02-17 21:59:17 +1000

    fix: apply pre-commit formatting to source files
    Apply trailing whitespace and tab removal fixes to Java source files,
    YANG modules, XML resources, and configuration files.
    
    Changes: trailing whitespace cleanup, mixed line ending fixes, tab
    removal.
    
    Change-Id: Ia0461ce0ecaa1ff947f7e99496f5785793c444e5
    Signed-off-by: Anil Belur <abelur@linuxfoundation.org>

--- plugins/netconf-server-mdsal/src/main/java/org/opendaylight/netconf/server/mdsal/DOMDataTransactionValidator.java
+++ plugins/netconf-server-mdsal/src/main/java/org/opendaylight/netconf/server/mdsal/DOMDataTransactionValidator.java
@@ -60,4 +60,3 @@ public interface DOMDataTransactionValidator extends Extension {
         }
     }
 }
-
--- usecase/netconf-testtool/src/main/java/org/opendaylight/netconf/test/tool/monitoring/NetconfMonitoringOperationServiceFactory.java
+++ usecase/netconf-testtool/src/main/java/org/opendaylight/netconf/test/tool/monitoring/NetconfMonitoringOperationServiceFactory.java
@@ -46,4 +46,3 @@ public class NetconfMonitoringOperationServiceFactory implements NetconfOperatio
         // No-op
     }
 }
-
