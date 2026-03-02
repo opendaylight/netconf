425cdb9a7c7e42c09bff57346f6d6b26c7c033d9
Matej Sramcik <matej.sramcik@pantheon.tech>
2026-03-02 16:08:15 +0100

    Cover HTTP/3 implementation with integration tests
    Introduce HTTP/3 Netty-based client.
    Add HTTP/3 variants of existing integration tests.
    
    JIRA: NETCONF-1540
    Change-Id: I5b27344f51d1fd08979ec3c32a191d20058b0e31
    Signed-off-by: Matej Sramcik <matej.sramcik@pantheon.tech>

--- apps/restconf-it/src/test/java/org/opendaylight/restconf/it/server/AbstractE2ETest.java
+++ apps/restconf-it/src/test/java/org/opendaylight/restconf/it/server/AbstractE2ETest.java
@@ -114,6 +114,9 @@ import org.xmlunit.diff.DefaultNodeMatcher;
 import org.xmlunit.diff.ElementSelectors;
 
 public abstract class AbstractE2ETest extends AbstractDataBrokerTest {
+    static final JSONParserConfiguration JSON_PARSER_CONFIGURATION =
+        new JSONParserConfiguration().withStrictMode();
+
     private static final Logger LOG = LoggerFactory.getLogger(AbstractE2ETest.class);
     private static final Uint32 CHUNK_SIZE = Uint32.valueOf(256 * 1024);
     private static final Uint32 FRAME_SIZE = Uint32.valueOf(16 * 1024);
@@ -123,8 +126,6 @@ public abstract class AbstractE2ETest extends AbstractDataBrokerTest {
     private static final Uint64 HTTP3_INITIAL_MAX_STREAM_DATA_BIDIRECTIONAL_REMOTE = Uint64.valueOf(256L * 1024);
     private static final Uint32 HTTP3_INITIAL_MAX_STREAMS_BIDIRECTIONAL = Uint32.valueOf(100);
 
-    protected static final JSONParserConfiguration JSON_PARSER_CONFIGURATION =
-        new JSONParserConfiguration().withStrictMode();
     protected static final Map<String, String> NS_CONTEXT = Map.of("r", "urn:ietf:params:xml:ns:yang:ietf-restconf");
     protected static final Splitter COMMA_SPLITTER = Splitter.on(',').trimResults().omitEmptyStrings();
     protected static final ErrorTagMapping ERROR_TAG_MAPPING = ErrorTagMapping.RFC8040;
@@ -136,16 +137,19 @@ public abstract class AbstractE2ETest extends AbstractDataBrokerTest {
     protected static String localAddress;
     protected static BootstrapFactory bootstrapFactory;
     protected static SSHTransportStackFactory sshTransportStackFactory;
-    protected HttpClientStackGrouping clientStackGrouping;
     protected HttpClientStackGrouping invalidClientStackGrouping;
     protected DOMMountPointService domMountPointService;
     protected RpcProviderService rpcProviderService;
     protected ActionProviderService actionProviderService;
     protected String host;
 
+    protected int port;
+
     protected volatile EventStreamService clientStreamService;
     protected volatile EventStreamService.StreamControl streamControl;
 
+    private HttpServerStackGrouping serverStackGrouping;
+    private HttpClientStackGrouping clientStackGrouping;
     private DOMRpcRouter domRpcRouter;
     private SimpleNettyEndpoint endpoint;
     private DOMNotificationRouter domNotificationRouter;
@@ -161,10 +165,10 @@ public abstract class AbstractE2ETest extends AbstractDataBrokerTest {
     @BeforeEach
     protected void beforeEach() throws Exception {
         // transport configuration
-        final var port = randomBindablePort();
+        port = randomBindablePort();
         host = localAddress + ":" + port;
-        final var serverTransport = ConfigUtils.serverTransportTcp(localAddress, port);
-        final var serverStackGrouping = new HttpServerStackGrouping() {
+        final var serverTransport = createTransport();
+        serverStackGrouping = new HttpServerStackGrouping() {
             @Override
             public Class<? extends HttpServerStackGrouping> implementedInterface() {
                 return HttpServerStackGrouping.class;
@@ -184,7 +188,7 @@ public abstract class AbstractE2ETest extends AbstractDataBrokerTest {
         final var securityManager = new DefaultWebSecurityManager(new AuthenticatingRealm() {
             @Override
             protected AuthenticationInfo doGetAuthenticationInfo(final AuthenticationToken token)
-                throws AuthenticationException {
+                    throws AuthenticationException {
                 final var principal = (String) token.getPrincipal();
                 final var credentials = new String((char[]) token.getCredentials());
                 if (USERNAME.equals(principal) && PASSWORD.equals(credentials)) {
@@ -234,11 +238,7 @@ public abstract class AbstractE2ETest extends AbstractDataBrokerTest {
             rpcImplementations);
 
         // Netty endpoint
-        final var configuration = new NettyEndpointConfiguration(
-            ERROR_TAG_MAPPING, PrettyPrintParam.FALSE, Uint16.ZERO, Uint32.valueOf(1000),
-            "rests", MessageEncoding.JSON, serverStackGrouping, CHUNK_SIZE, FRAME_SIZE, ALT_SVC_HEADER,
-            HTTP3_ALT_SVC_MAX_AGE_SECONDS, HTTP3_INITIAL_MAX_DATA, HTTP3_INITIAL_MAX_STREAM_DATA_BIDIRECTIONAL_REMOTE,
-            HTTP3_INITIAL_MAX_STREAMS_BIDIRECTIONAL);
+        final var configuration = createEndpointConfiguration();
         endpoint = new SimpleNettyEndpoint(server, principalService, streamRegistry, bootstrapFactory, configuration);
     }
 
@@ -257,6 +257,15 @@ public abstract class AbstractE2ETest extends AbstractDataBrokerTest {
     }
 
     /**
+     * Return the serverStackGrouping.
+     *
+     * @return the serverStackGrouping
+     */
+    protected final HttpServerStackGrouping serverStackGrouping() {
+        return serverStackGrouping;
+    }
+
+    /**
      * Find a local port which has a good chance of not failing {@code bind()} due to a conflict.
      *
      * @return a local port
@@ -267,6 +276,18 @@ public abstract class AbstractE2ETest extends AbstractDataBrokerTest {
         } catch (IOException e) {
             throw new AssertionError(e);
         }
+    }
+
+    protected Transport createTransport() {
+        return ConfigUtils.serverTransportTcp(localAddress, port);
+    }
+
+    protected NettyEndpointConfiguration createEndpointConfiguration() {
+        return new NettyEndpointConfiguration(
+            ERROR_TAG_MAPPING, PrettyPrintParam.FALSE, Uint16.ZERO, Uint32.valueOf(1000),
+            "rests", MessageEncoding.JSON, serverStackGrouping, CHUNK_SIZE, FRAME_SIZE, ALT_SVC_HEADER,
+            HTTP3_ALT_SVC_MAX_AGE_SECONDS, HTTP3_INITIAL_MAX_DATA, HTTP3_INITIAL_MAX_STREAM_DATA_BIDIRECTIONAL_REMOTE,
+            HTTP3_INITIAL_MAX_STREAMS_BIDIRECTIONAL);
     }
 
     protected FullHttpResponse invokeRequest(final HttpMethod method, final String uri) throws Exception {
@@ -492,7 +513,7 @@ public abstract class AbstractE2ETest extends AbstractDataBrokerTest {
         return eventListener;
     }
 
-    static final class ExampleActionImpl implements ExampleAction {
+    public static final class ExampleActionImpl implements ExampleAction {
         @Override
         public ListenableFuture<RpcResult<ExampleActionOutput>> invoke(final DataObjectIdentifier<Root> path,
                 final ExampleActionInput input) {
