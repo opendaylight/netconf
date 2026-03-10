2d74652397091991ea93b96b9417130fe2810cfe
Peter Suna <peter.suna@pantheon.tech>
2026-03-10 17:33:53 +0100

    Daggerize DOMDataBroker
    This patch introduces Dagger modules to handle the wiring and
    lifecycle of the DOMDataBroker service.
    
    Key changes:
    * Added `DOMDataBrokerModule` to provide the `DOMDataBroker` service.
    * Added `DataBrokerCommitExecutorConfig` to handle configuration
      mapping.
    * Wired up `SpecialExecutors` for data broker future callbacks and
      registered them for proper teardown.
    * Added transaction tests to `MdsalDomBrokerTest` using the sample
      toaster model to verify read and write operations.
    
    JIRA: NETCONF-1571
    Change-Id: Ifdd519d5b620a34e5c1e55a2dc4584bf4025a882
    Signed-off-by: Peter Suna <peter.suna@pantheon.tech>

--- usecase/netconf-topology-1node-transient/src/test/java/org/opendaylight/netconf/dagger/mdsal/MdsalDomBrokerTestFactory.java
+++ usecase/netconf-topology-1node-transient/src/test/java/org/opendaylight/netconf/dagger/mdsal/MdsalDomBrokerTestFactory.java
@@ -10,12 +10,14 @@ package org.opendaylight.netconf.dagger.mdsal;
 import dagger.Component;
 import jakarta.inject.Singleton;
 import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.opendaylight.mdsal.binding.api.DataBroker;
 import org.opendaylight.mdsal.binding.api.NotificationPublishService;
 import org.opendaylight.mdsal.dom.api.DOMActionService;
 import org.opendaylight.mdsal.dom.broker.DOMNotificationRouter;
 import org.opendaylight.mdsal.dom.broker.DOMRpcRouter;
 import org.opendaylight.mdsal.eos.binding.api.EntityOwnershipService;
 import org.opendaylight.mdsal.singleton.api.ClusterSingletonServiceProvider;
+import org.opendaylight.netconf.dagger.controller.DOMDataBrokerModule;
 import org.opendaylight.netconf.dagger.mdsal.MdsalQualifiers.SchemaServiceContext;
 import org.opendaylight.netconf.dagger.springboot.config.SpringbootConfigLoaderModule;
 import org.opendaylight.odlparent.dagger.AutoCloseableComponent;
@@ -30,6 +32,8 @@ import org.opendaylight.yangtools.yang.model.api.EffectiveModelContext;
     MdsalEosBindingAdapterModule.class,
     MdsalEosDomSimpleModule.class,
     MdsalSingletonImplModule.class,
+    DOMDataBrokerModule.class,
+    InMemoryDataStoreModule.class,
     SpringbootConfigLoaderModule.class,
     ResourceSupportModule.class
 })
@@ -48,4 +52,6 @@ public interface MdsalDomBrokerTestFactory extends AutoCloseableComponent {
     ClusterSingletonServiceProvider clusterSingletonServiceProvider();
 
     EntityOwnershipService entityOwnershipService();
+
+    DataBroker dataBroker();
 }
