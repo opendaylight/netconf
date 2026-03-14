38fa63afa77a3d520e103f22fc5819de8c41a1c1
Robert Varga <robert.varga@pantheon.tech>
2026-03-14 22:07:36 +0100

    Split out HTTPServerOver{Tcp,Tls}
    ConfigUtils is quite overloaded, this patch creates
    HTTPServerOverTcp and HTTPServerOverTls static utility classes
    to hold factory methods for instantiation.
    
    Change-Id: I735ecd7d29aa0e7a1e7c9bda9acc238cfe005346
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ transport/transport-http/src/main/java/org/opendaylight/netconf/transport/http/HTTPServerOverTcp.java
@@ -0,0 +1,113 @@
+/*
+ * Copyright (c) 2026 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.netconf.transport.http;
+
+import static java.util.Objects.requireNonNull;
+
+import com.google.common.annotations.Beta;
+import java.util.Map;
+import org.eclipse.jdt.annotation.NonNull;
+import org.eclipse.jdt.annotation.Nullable;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.iana.crypt.hash.rev140806.CryptHash;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.http.server.rev251111.http.server.grouping.ClientAuthentication;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.http.server.rev251111.http.server.grouping.ClientAuthenticationBuilder;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.http.server.rev251111.http.server.grouping.client.authentication.users.user.auth.type.basic.basic.PasswordBuilder;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.http.server.rev251111.http.server.listen.stack.grouping.transport.HttpOverTcp;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.http.server.rev251111.http.server.listen.stack.grouping.transport.HttpOverTcpBuilder;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.http.server.rev251111.http.server.listen.stack.grouping.transport.http.over.tcp.http.over.tcp.HttpServerParameters;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.http.server.rev251111.http.server.listen.stack.grouping.transport.http.over.tcp.http.over.tcp.HttpServerParametersBuilder;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.http.server.rev251111.http.server.listen.stack.grouping.transport.http.over.tcp.http.over.tcp.TcpServerParameters;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.http.server.rev251111.http.server.listen.stack.grouping.transport.http.over.tcp.http.over.tcp.TcpServerParametersBuilder;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev130715.IetfInetUtil;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev130715.PortNumber;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.tcp.server.rev241010.tcp.server.grouping.LocalBindBuilder;
+import org.opendaylight.yangtools.binding.util.BindingMap;
+import org.opendaylight.yangtools.yang.common.Uint16;
+
+/**
+ * Utility class for creating {@link HttpOverTcp} configurations for {@link HTTPServer}.
+ */
+@Beta
+public final class HTTPServerOverTcp {
+    private HTTPServerOverTcp() {
+        // Hidden on purpose
+    }
+
+    /**
+     * Builds transport configuration for {@link HTTPServer} using TCP transport underlay with no authorization.
+     *
+     * @param host local address
+     * @param port local port
+     * @return transport configuration
+     */
+    public static @NonNull HttpOverTcp of(final @NonNull String host, final int port) {
+        return of(host, port, null);
+    }
+
+    /**
+     * Builds transport configuration for {@link HTTPServer} using TCP transport underlay with Basic Authorization.
+     *
+     * @param host local address
+     * @param port local port
+     * @param userCryptHashMap user credentials map for Basic Authorization where key is username and value is a
+     *      {@link CryptHash} value for user password
+     * @return transport configuration
+     */
+    public static @NonNull HttpOverTcp of(final @NonNull String host, final int port,
+            final @Nullable Map<String, String> userCryptHashMap) {
+        return of(
+            new TcpServerParametersBuilder()
+                .setLocalBind(BindingMap.of(new LocalBindBuilder()
+                    .setLocalAddress(IetfInetUtil.ipAddressFor(requireNonNull(host)))
+                    .setLocalPort(new PortNumber(Uint16.valueOf(port)))
+                    .build()))
+                .build(),
+            new HttpServerParametersBuilder().setClientAuthentication(clientAuthentication(userCryptHashMap)).build());
+    }
+
+    /**
+     * Builds transport configuration for {@link HTTPServer} using TCP transport underlay.
+     *
+     * @param tcpParams TCP layer configuration
+     * @param httpParams HTTP layer configuration
+     * @return transport configuration
+     */
+    static @NonNull HttpOverTcp of(final @NonNull TcpServerParameters tcpParams,
+            final @Nullable HttpServerParameters httpParams) {
+        return new HttpOverTcpBuilder()
+            .setHttpOverTcp(new org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.http.server.rev251111
+                .http.server.listen.stack.grouping.transport.http.over.tcp.HttpOverTcpBuilder()
+                .setTcpServerParameters(tcpParams)
+                .setHttpServerParameters(httpParams)
+                .build())
+            .build();
+    }
+
+    static @Nullable ClientAuthentication clientAuthentication(final @Nullable Map<String, String> userCryptHashMap) {
+        if (userCryptHashMap == null || userCryptHashMap.isEmpty()) {
+            return null;
+        }
+        final var userMap = userCryptHashMap.entrySet().stream()
+            .map(entry -> new org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.http.server.rev251111
+                .http.server.grouping.client.authentication.users.UserBuilder()
+                .setUserId(entry.getKey())
+                .setAuthType(
+                    new org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.http.server.rev251111
+                        .http.server.grouping.client.authentication.users.user.auth.type.BasicBuilder().setBasic(
+                            new org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.http.server.rev251111
+                                .http.server.grouping.client.authentication.users.user.auth.type.basic.BasicBuilder()
+                                .setUsername(entry.getKey())
+                                .setPassword(new PasswordBuilder()
+                                    .setHashedPassword(new CryptHash(entry.getValue())).build()).build()
+                    ).build()).build())
+            .collect(BindingMap.toOrderedMap());
+        return new ClientAuthenticationBuilder()
+            .setUsers(new org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.http.server.rev251111
+                .http.server.grouping.client.authentication.UsersBuilder().setUser(userMap).build()).build();
+    }
+}
