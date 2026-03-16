f99151a42e06556a49f88b69adea88c23044fb64
Peter Suna <peter.suna@pantheon.tech>
2026-03-16 09:29:04 +0100

    Daggerize insecure AAA components
    This patch introduces Dagger modules to handle the wiring and
    lifecycle of the basic AAA components required for insecure connections
    by Netconf and Restconf.
    
    Key changes:
    * Added `InsecureAAAModule` as an aggregator for the basic AAA
      services.
    * Added `JettyWebServerModule` and `JerseyServletSupportModule` to
      provide the web server and servlet support.
    * Added `CustomFilterAdapterConfigurationModule` to provide the
      default filter adapter configuration.
    * Added `NoEncryptionServiceModule` and `NoWebContextSecurerModule`
      to provide insecure dummy implementations for testing purposes.
    * Added AAAShiroWebEnvironmentModule to provide the WebEnvironment.
    
    JIRA: NETCONF-1572
    Change-Id: I4ef37c560ee5d46077ba86fdcce916a6868ee79b
    Signed-off-by: Peter Suna <peter.suna@pantheon.tech>

---
+++ usecase/netconf-topology-1node-transient/src/main/java/org/opendaylight/netconf/dagger/aaa/JettyWebServerModule.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2026 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.netconf.dagger.aaa;
+
+import com.google.errorprone.annotations.DoNotMock;
+import dagger.Module;
+import dagger.Provides;
+import jakarta.inject.Singleton;
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.opendaylight.aaa.web.WebServer;
+import org.opendaylight.aaa.web.jetty.JettyWebServer;
+import org.opendaylight.odlparent.dagger.ResourceSupport;
+
+/**
+ * A Dagger module providing Jetty {@link WebServer}.
+ */
+@Module
+@DoNotMock
+@NonNullByDefault
+public interface JettyWebServerModule {
+
+    @Provides
+    @Singleton
+    static WebServer jettyWebServer(final ResourceSupport resourceSupport) {
+        final var jettyWebServer = new JettyWebServer();
+        resourceSupport.register(jettyWebServer::stop);
+        return jettyWebServer;
+    }
+}
