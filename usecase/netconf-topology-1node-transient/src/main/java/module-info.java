87313df5f8836708d996927059c2ba4982fd8139
Peter Suna <peter.suna@pantheon.tech>
2026-03-16 09:29:04 +0100

    Daggerize insecure AAA components
    This patch introduces Dagger modules to handle the wiring and
    lifecycle of the basic AAA components required for insecure connections
    by Netconf and Restconf.
    
    Key changes:
    * Added `InsecureAAAModule` as an aggregator for the basic AAA
      services.
    * Added `JettyWebServerModule` and `JerseyServletSupportModule` to
      provide the web server and servlet support.
    * Added `CustomFilterAdapterConfigurationModule` to provide the
      default filter adapter configuration.
    * Added `NoEncryptionServiceModule` and `NoWebContextSecurerModule`
      to provide insecure dummy implementations for testing purposes.
    * Added AAAShiroWebEnvironmentModule to provide the WebEnvironment.
    
    JIRA: NETCONF-1572
    Change-Id: I4ef37c560ee5d46077ba86fdcce916a6868ee79b
    Signed-off-by: Peter Suna <peter.suna@pantheon.tech>

--- usecase/netconf-topology-1node-transient/src/main/java/module-info.java
+++ usecase/netconf-topology-1node-transient/src/main/java/module-info.java
@@ -9,6 +9,7 @@
  * Dagger prototype for the RNC standalone transient applications, built from daggerized ODL components.
  */
 module org.opendaylight.netconf.dagger {
+    exports org.opendaylight.netconf.dagger.aaa;
     exports org.opendaylight.netconf.dagger.controller;
     exports org.opendaylight.netconf.dagger.mdsal;
     exports org.opendaylight.netconf.dagger.springboot.config;
@@ -40,14 +41,28 @@ module org.opendaylight.netconf.dagger {
     requires transitive org.opendaylight.yangtools.yang.parser.rfc7950;
     requires transitive org.opendaylight.yangtools.yang.xpath.impl;
     requires transitive spring.boot;
+    requires aaa.authn.api;
+    requires aaa.cert;
+    requires aaa.encrypt.service;
+    requires aaa.filterchain;
+    requires aaa.password.service.api;
+    requires aaa.password.service.impl;
+    requires aaa.shiro;
+    requires aaa.tokenauthrealm;
+    requires javax.servlet.api;
     requires mdsal.dom.inmemory.datastore;
     requires org.opendaylight.mdsal.eos.dom.api;
     requires org.opendaylight.mdsal.eos.dom.simple;
     requires org.opendaylight.mdsal.singleton.impl;
+    requires repackaged.shiro;
     requires sal.distributed.datastore;
+    requires servlet.api;
+    requires servlet.jersey2;
     requires spring.beans;
     requires spring.context;
     requires spring.core;
+    requires web.api;
+    requires web.jetty.impl;
 
     requires static transitive com.google.errorprone.annotations;
     requires static jakarta.inject;
