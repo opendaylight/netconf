2a5c27acdf5d95c80c4f04cab94ef05295d5c518
Yaroslav Lastivka <yaroslav.lastivka@pantheon.tech>
2026-03-18 17:21:16 +0200

    Add HTTP write buffer watermarks to RFC8040 config
    Expose Netty write buffer low/high watermark settings in the
    RFC8040 OSGi configuration so outbound backpressure can be tuned.
    These settings are intended for use by RESTCONF HTTP/1.1, HTTP/2
    and HTTP/3 server transports.
    
    JIRA: NETCONF-1586
    Change-Id: Ia3135d257f1c1307faad7e46945d454fc04dce38
    Signed-off-by: Yaroslav Lastivka <yaroslav.lastivka@pantheon.tech>

--- protocol/restconf-server/src/main/java/org/opendaylight/restconf/server/Http3ServerBootstrap.java
+++ protocol/restconf-server/src/main/java/org/opendaylight/restconf/server/Http3ServerBootstrap.java
@@ -12,6 +12,7 @@ import static java.util.Objects.requireNonNull;
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.EventLoopGroup;
+import io.netty.channel.WriteBufferWaterMark;
 import io.netty.handler.codec.http.HttpObjectAggregator;
 import io.netty.handler.codec.http3.Http3;
 import io.netty.handler.codec.http3.Http3FrameToHttpObjectCodec;
@@ -48,7 +49,7 @@ final class Http3ServerBootstrap implements AutoCloseable {
 
     static Http3ServerBootstrap start(final String bindAddress, final int bindPort,
             final X509Certificate certificate, final PrivateKey privateKey, final EndpointRoot root,
-            final Uint32 chunkSize, final Uint64 initialMaxData,
+            final Uint32 chunkSize, final WriteBufferWaterMark writeBufferWaterMark, final Uint64 initialMaxData,
             final Uint64 initialMaxStreamDataBidirectionalRemote, final Uint32 initialMaxStreamsBidirectional)
             throws SSLException {
         final var sslContext = QuicSslContextBuilder.forServer(privateKey, null, certificate)
@@ -58,6 +59,7 @@ final class Http3ServerBootstrap implements AutoCloseable {
         final var streamInitializer = new ChannelInitializer<QuicStreamChannel>() {
             @Override
             protected void initChannel(final QuicStreamChannel stream) {
+                stream.config().setWriteBufferWaterMark(writeBufferWaterMark);
                 final var pipeline = stream.pipeline();
                 pipeline.addLast("h3-stream-log", new LoggingHandler(LogLevel.DEBUG));
                 pipeline.addLast(new Http3FrameToHttpObjectCodec(true));
