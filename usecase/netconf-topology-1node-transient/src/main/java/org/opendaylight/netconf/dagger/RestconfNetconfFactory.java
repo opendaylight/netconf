281f24ebca327f059a616b6dcdd97604c5eb2e2e
Peter Suna <peter.suna@pantheon.tech>
2026-04-14 13:24:51 +0200

    Add RNC transient Dagger application
    Introduce the RNC standalone Dagger application and component to wire
    the application modules. Temporarily skip SpotBugs to bypass false
    positive URF_UNREAD_FIELD on Dagger-generated classes.
    
    JIRA: NETCONF-1575
    Change-Id: I7f50b757e8b193cf4a45ce8d10b99c51483ff4aa
    Signed-off-by: Peter Suna <peter.suna@pantheon.tech>

---
+++ usecase/netconf-topology-1node-transient/src/main/java/org/opendaylight/netconf/dagger/RestconfNetconfFactory.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2026 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.netconf.dagger;
+
+import dagger.Component;
+import jakarta.inject.Singleton;
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.opendaylight.netconf.dagger.aaa.InsecureAAAModule;
+import org.opendaylight.netconf.dagger.controller.DOMDataBrokerModule;
+import org.opendaylight.netconf.dagger.mdsal.InMemoryDataStoreModule;
+import org.opendaylight.netconf.dagger.mdsal.MdsalBindingDomAdapterModule;
+import org.opendaylight.netconf.dagger.mdsal.MdsalDomBrokerModule;
+import org.opendaylight.netconf.dagger.mdsal.MdsalEosBindingAdapterModule;
+import org.opendaylight.netconf.dagger.mdsal.MdsalEosDomSimpleModule;
+import org.opendaylight.netconf.dagger.mdsal.MdsalSingletonImplModule;
+import org.opendaylight.netconf.dagger.netconf.NetconfTopologyModule;
+import org.opendaylight.netconf.dagger.netconf.RestconfNettyEndpointModule;
+import org.opendaylight.netconf.dagger.springboot.config.SpringbootConfigLoaderModule;
+import org.opendaylight.netconf.topology.impl.NetconfTopologyImpl;
+import org.opendaylight.odlparent.dagger.AutoCloseableComponent;
+import org.opendaylight.odlparent.dagger.ResourceSupportModule;
+import org.opendaylight.restconf.server.NettyEndpoint;
+
+@Singleton
+@NonNullByDefault
+@Component(modules = {
+    MdsalDomBrokerModule.class,
+    MdsalBindingDomAdapterModule.class,
+    MdsalEosBindingAdapterModule.class,
+    MdsalEosDomSimpleModule.class,
+    MdsalSingletonImplModule.class,
+    DOMDataBrokerModule.class,
+    InMemoryDataStoreModule.class,
+    InsecureAAAModule.class,
+    RestconfNettyEndpointModule.class,
+    NetconfTopologyModule.class,
+    SpringbootConfigLoaderModule.class,
+    ResourceSupportModule.class
+})
+public interface RestconfNetconfFactory extends AutoCloseableComponent {
+
+    NettyEndpoint nettyEndpoint();
+
+    NetconfTopologyImpl netconfTopologyImpl();
+}
