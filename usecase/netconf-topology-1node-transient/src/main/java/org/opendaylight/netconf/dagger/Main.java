e43149e6f6554eda5750df0b12d1920a73459966
Peter Suna <peter.suna@pantheon.tech>
2026-04-14 13:24:51 +0200

    Add RNC transient Dagger application
    Introduce the RNC standalone Dagger application and component to wire
    the application modules. Temporarily skip SpotBugs to bypass false
    positive URF_UNREAD_FIELD on Dagger-generated classes.
    
    JIRA: NETCONF-1575
    Change-Id: I7f50b757e8b193cf4a45ce8d10b99c51483ff4aa
    Signed-off-by: Peter Suna <peter.suna@pantheon.tech>

---
+++ usecase/netconf-topology-1node-transient/src/main/java/org/opendaylight/netconf/dagger/Main.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2026 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.netconf.dagger;
+
+public class Main {
+    private final RestconfNetconfFactory factory;
+
+    public static void main(final String[] args) {
+        final var main = new Main(DaggerRestconfNetconfFactory.create());
+        main.startApp();
+    }
+
+    public Main(final RestconfNetconfFactory factory) {
+        this.factory = factory;
+    }
+
+    public void startApp() {
+        Runtime.getRuntime().addShutdownHook(new Thread(this::shutdown));
+        factory.nettyEndpoint();
+        factory.netconfTopologyImpl();
+    }
+
+    public void shutdown() {
+        this.factory.close();
+    }
+}
