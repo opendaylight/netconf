a69d8caae391502d39ed430badceb4a34ce32663
Yaroslav Lastivka <yaroslav.lastivka@pantheon.tech>
2026-04-15 21:00:59 +0300

    Split restconf-nb from deprecated JAX-RS
    Move the deprecated JAX-RS northbound wiring out of
    odl-restconf-common and odl-restconf-nb into
    the new odl-restconf-nb-jaxrs feature and the new restconf-nb-jaxrs
    bundle.
    
    Keep odl-restconf-nb on the Netty stack and move the Jetty and
    restconf-server-jaxrs dependencies to odl-restconf-nb-jaxrs.
    
    Add a Netty RestconfStream LocationProvider so the Netty-only
    northbound still satisfies stream registry wiring after
    restconf-server-jaxrs is removed.
    
    JIRA: NETCONF-1606
    Change-Id: Iee8f1e42bc36f12d61ba0cf325dcc850bdf99650
    Signed-off-by: Yaroslav Lastivka <yaroslav.lastivka@pantheon.tech>

---
+++ apps/restconf-nb-jaxrs/src/main/java/org/opendaylight/restconf/nb/jaxrs/OSGiNorthboundJaxrs.java
@@ -0,0 +1,113 @@
+/*
+ * Copyright (c) 2026 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.restconf.nb.jaxrs;
+
+import static java.util.Objects.requireNonNull;
+
+import java.util.Map;
+import org.opendaylight.restconf.api.query.PrettyPrintParam;
+import org.opendaylight.restconf.server.jaxrs.JaxRsEndpoint;
+import org.opendaylight.restconf.server.jaxrs.JaxRsEndpointConfiguration;
+import org.opendaylight.restconf.server.spi.EndpointConfiguration;
+import org.opendaylight.restconf.server.spi.ErrorTagMapping;
+import org.opendaylight.yangtools.yang.common.Uint16;
+import org.opendaylight.yangtools.yang.common.Uint32;
+import org.osgi.framework.FrameworkUtil;
+import org.osgi.service.component.ComponentFactory;
+import org.osgi.service.component.ComponentInstance;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.service.component.annotations.Modified;
+import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.metatype.annotations.AttributeDefinition;
+import org.osgi.service.metatype.annotations.Designate;
+import org.osgi.service.metatype.annotations.ObjectClassDefinition;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Component(service = { }, configurationPid = "org.opendaylight.restconf.nb.rfc8040")
+@Designate(ocd = OSGiNorthboundJaxrs.Configuration.class)
+public final class OSGiNorthboundJaxrs {
+    private static final Logger LOG = LoggerFactory.getLogger(OSGiNorthboundJaxrs.class);
+
+    @ObjectClassDefinition
+    public @interface Configuration {
+        @AttributeDefinition(
+            name = "default pretty-print",
+            description = "Control the default value of the '" + PrettyPrintParam.uriName + "' query parameter.")
+        boolean pretty$_$print() default false;
+
+        @AttributeDefinition(
+            name = "Report 404 on data-missing",
+            description = """
+                Control the HTTP status code reporting of conditions corresponding to "data-missing". When this is set
+                to true, the server will violate RFC8040 and report "404" instead of "409".""")
+        boolean data$_$missing$_$is$_$404() default false;
+
+        @AttributeDefinition(min = "0", max = "" + EndpointConfiguration.SSE_MAXIMUM_FRAGMENT_LENGTH_MAX)
+        int maximum$_$fragment$_$length() default 0;
+
+        @AttributeDefinition(min = "0")
+        int heartbeat$_$interval() default 10000;
+
+        @AttributeDefinition(name = "{+restconf}", description = """
+            The value of RFC8040 {+restconf} URI template, pointing to the root resource. Must not end with '/'.""")
+        String restconf() default "rests";
+
+        @AttributeDefinition(min = "1")
+        String ping$_$executor$_$name$_$prefix() default JaxRsEndpointConfiguration.DEFAULT_NAME_PREFIX;
+
+        // FIXME: this is a misnomer: it specifies the core pool size, i.e. minimum thread count, the maximum is set to
+        //        Integer.MAX_VALUE, which is not what we want
+        @AttributeDefinition(min = "0")
+        int max$_$thread$_$count() default JaxRsEndpointConfiguration.DEFAULT_CORE_POOL_SIZE;
+    }
+
+    private final ComponentFactory<JaxRsEndpoint> jaxrsFactory;
+
+    private ComponentInstance<JaxRsEndpoint> jaxrs;
+    private Map<String, ?> jaxrsProps;
+
+    @Activate
+    public OSGiNorthboundJaxrs(@Reference(target = "(component.factory=" + JaxRsEndpoint.FACTORY_NAME + ")")
+            final ComponentFactory<JaxRsEndpoint> jaxrsFactory, final Configuration configuration) {
+        this.jaxrsFactory = requireNonNull(jaxrsFactory);
+
+        jaxrsProps = newJaxrsProps(configuration);
+        jaxrs = jaxrsFactory.newInstance(FrameworkUtil.asDictionary(jaxrsProps));
+        LOG.info("Deprecated JAX-RS RESTCONF Northbound started");
+    }
+
+    @Modified
+    void modified(final Configuration configuration) {
+        final var newJaxRsProps = newJaxrsProps(configuration);
+        if (!newJaxRsProps.equals(jaxrsProps)) {
+            jaxrs.dispose();
+            jaxrsProps = newJaxRsProps;
+            jaxrs = jaxrsFactory.newInstance(FrameworkUtil.asDictionary(jaxrsProps));
+            LOG.debug("JAX-RS Northbound restarted with {}", jaxrsProps);
+        }
+    }
+
+    @Deactivate
+    void deactivate() {
+        jaxrs.dispose();
+        jaxrs = null;
+        LOG.info("JAX-RS RESTCONF Northbound stopped");
+    }
+
+    private static Map<String, ?> newJaxrsProps(final Configuration configuration) {
+        return JaxRsEndpoint.props(new JaxRsEndpointConfiguration(
+            configuration.data$_$missing$_$is$_$404() ? ErrorTagMapping.ERRATA_5565 : ErrorTagMapping.RFC8040,
+            PrettyPrintParam.of(configuration.pretty$_$print()),
+            Uint16.valueOf(configuration.maximum$_$fragment$_$length()),
+            Uint32.valueOf(configuration.heartbeat$_$interval()), configuration.restconf(),
+            configuration.ping$_$executor$_$name$_$prefix(), configuration.max$_$thread$_$count()));
+    }
+}
