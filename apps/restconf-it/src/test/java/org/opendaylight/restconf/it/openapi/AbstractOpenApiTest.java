3b68b748ea84b08a8d2c4173aa47570d3cc70c94
Yaroslav Lastivka <yaroslav.lastivka@pantheon.tech>
2026-04-21 20:07:30 +0300

    Use HTTPServerOverQuic for HTTP/3 config
    Build the RESTCONF HTTP/3 transport via HTTPServerOverQuic and carry the
    resulting HttpOverQuic configuration through NettyEndpointConfiguration
    instead of passing separate bind/TLS/QUIC values.
    
    Add the new HTTPServerOverQuic helper in transport-http, mirroring the
    existing HTTPServerOverTcp and HTTPServerOverTls builders for the HTTP/3
    transport case.
    
    Update the local Http3ServerBootstrap path to consume HttpOverQuic and
    read bind, TLS and QuicUnderHttp augmentation parameters from the model.
    Adjust RESTCONF server dependencies for the restored bootstrap imports.
    
    JIRA: NETCONF-1602
    Change-Id: I45e9b0fb57439267115a0f6d9813d256f20b6179
    Signed-off-by: Yaroslav Lastivka <yaroslav.lastivka@pantheon.tech>
    (cherry picked from commit 363f5d36bf6c6e991564eb2d8114290da3c6aa08)

--- apps/restconf-it/src/test/java/org/opendaylight/restconf/it/openapi/AbstractOpenApiTest.java
+++ apps/restconf-it/src/test/java/org/opendaylight/restconf/it/openapi/AbstractOpenApiTest.java
@@ -97,7 +97,6 @@ import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.http.server
 import org.opendaylight.yangtools.binding.data.codec.impl.di.DefaultBindingDOMCodecServices;
 import org.opendaylight.yangtools.yang.common.Uint16;
 import org.opendaylight.yangtools.yang.common.Uint32;
-import org.opendaylight.yangtools.yang.common.Uint64;
 import org.opendaylight.yangtools.yang.parser.impl.DefaultYangParserFactory;
 import org.opendaylight.yangtools.yang.test.util.YangParserTestUtils;
 import org.skyscreamer.jsonassert.JSONAssert;
@@ -117,9 +116,6 @@ public class AbstractOpenApiTest extends AbstractDataBrokerTest {
     private static final Uint32 FRAME_SIZE = Uint32.valueOf(16 * 1024);
     private static final String ALT_SVC_HEADER = "h3=\":8443\"; ma=3600";
     private static final Uint32 HTTP3_ALT_SVC_MAX_AGE_SECONDS = Uint32.valueOf(3600);
-    private static final Uint64 HTTP3_INITIAL_MAX_DATA = Uint64.valueOf(4L * 1024 * 1024);
-    private static final Uint64 HTTP3_INITIAL_MAX_STREAM_DATA_BIDIRECTIONAL_REMOTE = Uint64.valueOf(256L * 1024);
-    private static final Uint32 HTTP3_INITIAL_MAX_STREAMS_BIDIRECTIONAL = Uint32.valueOf(100);
     private static final Uint32 WRITE_BUFFER_LOW_WATER_MARK = Uint32.valueOf(32 * 1024);
     private static final Uint32 WRITE_BUFFER_HIGH_WATER_MARK = Uint32.valueOf(64 * 1024);
 
@@ -285,8 +281,7 @@ public class AbstractOpenApiTest extends AbstractDataBrokerTest {
         return new NettyEndpointConfiguration(
             ERROR_TAG_MAPPING, PrettyPrintParam.FALSE, Uint16.ZERO, Uint32.valueOf(1000),
             RESTS, MessageEncoding.JSON, serverStackGrouping, CHUNK_SIZE, FRAME_SIZE, WRITE_BUFFER_LOW_WATER_MARK,
-            WRITE_BUFFER_HIGH_WATER_MARK, ALT_SVC_HEADER, HTTP3_ALT_SVC_MAX_AGE_SECONDS, HTTP3_INITIAL_MAX_DATA,
-            HTTP3_INITIAL_MAX_STREAM_DATA_BIDIRECTIONAL_REMOTE, HTTP3_INITIAL_MAX_STREAMS_BIDIRECTIONAL);
+            WRITE_BUFFER_HIGH_WATER_MARK, ALT_SVC_HEADER, HTTP3_ALT_SVC_MAX_AGE_SECONDS);
     }
 
     protected FullHttpResponse invokeRequest(final HttpMethod method, final String uri) throws Exception {
