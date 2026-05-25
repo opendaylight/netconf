0729732713af833bea374d6dd040d0b7be57ab1b
Martin Balaz <martin.balaz@pantheon.tech>
2026-05-25 11:35:20 +0200

    WIP: MDSAL pytests
    Change-Id: If99b7283dfd20f3f2a910223ee3a7e72c43ae1ae
    Signed-off-by: Martin Balaz <martin.balaz@pantheon.tech>

--- tests/libraries/variables.py
+++ tests/libraries/variables.py
@@ -43,6 +43,9 @@ class Variables(BaseSettings):
     CALLHOME_WHITELIST: str = (
         "restconf/data/odl-netconf-callhome-server:netconf-callhome-server/allowed-devices"
     )
+    REQUEST_COUNT: int = 16384
+
+    CALLHOME_WHITELIST: str = "restconf/data/odl-netconf-callhome-server:netconf-callhome-server/allowed-devices"
     NETCONF_KEYSTORE_DATA_URL: str = "restconf/data/netconf-keystore:keystore"
 
     MODULES_API: str = (
