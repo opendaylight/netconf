264a455023b82d3c3048b270915e24706ba9d2a5
Matej Sramcik <matej.sramcik@pantheon.tech>
2026-06-05 15:31:05 +0200

    Refactor netconf-netty-util into netconf-common
    The legacy netconf-netty-util module holds common pieces
    for netconf-client and netconf-server. We already have established
    a new protocol/netconf-common module, this patch migrates the
    remaining netty-util components into it.
    
    As part of this migration:
    - Contents are moved from netconf-netty-util to protocol/netconf-common
    - The legacy netconf-netty-util module is completely removed
    
    JIRA: NETCONF-1191
    Change-Id: I54444083d622ac2e8c68bc8596d7658242d22dd8
    Signed-off-by: Matej Sramcik <matej.sramcik@pantheon.tech>

---
+++ protocol/netconf-common/src/test/java/org/opendaylight/netconf/common/TestingNetconfSession.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.netconf.common;
+
+import io.netty.channel.Channel;
+import org.opendaylight.netconf.api.NetconfSessionListener;
+import org.opendaylight.netconf.codec.MessageDecoder;
+import org.opendaylight.netconf.codec.MessageWriter;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.netconf.base._1._0.rev110601.SessionIdType;
+
+class TestingNetconfSession
+        extends AbstractNetconfExiSession<TestingNetconfSession, NetconfSessionListener<TestingNetconfSession>> {
+    TestingNetconfSession(final NetconfSessionListener<TestingNetconfSession> sessionListener,
+                          final Channel channel, final SessionIdType sessionId) {
+        super(sessionListener, channel, sessionId);
+    }
+
+    @Override
+    protected TestingNetconfSession thisInstance() {
+        return this;
+    }
+
+    @Override
+    protected void addExiHandlers(final MessageDecoder decoder, final MessageWriter encoder) {
+        // No-op
+    }
+
+    @Override
+    public void stopExiCommunication() {
+        // No-op
+    }
+}
