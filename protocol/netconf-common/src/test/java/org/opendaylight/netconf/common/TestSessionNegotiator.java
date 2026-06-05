4b24cb0159714e05aae312233a632a6171415a56
Matej Sramcik <matej.sramcik@pantheon.tech>
2026-06-05 15:31:05 +0200

    WIP:Refactor netconf-netty-util
    Refactor netconf-netty-util into netconf common.
    
    JIRA: NETCONF-1191
    Change-Id: I54444083d622ac2e8c68bc8596d7658242d22dd8
    Signed-off-by: Matej Sramcik <matej.sramcik@pantheon.tech>

---
+++ protocol/netconf-common/src/test/java/org/opendaylight/netconf/common/TestSessionNegotiator.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2018 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.netconf.common;
+
+import io.netty.channel.Channel;
+import io.netty.util.concurrent.Promise;
+import org.opendaylight.netconf.api.NetconfSessionListener;
+import org.opendaylight.netconf.api.messages.HelloMessage;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.netconf.base._1._0.rev110601.SessionIdType;
+import org.opendaylight.yangtools.yang.common.Uint32;
+
+final class TestSessionNegotiator
+        extends NetconfSessionNegotiator<TestingNetconfSession, NetconfSessionListener<TestingNetconfSession>> {
+    TestSessionNegotiator(final HelloMessage hello, final Promise<TestingNetconfSession> promise,
+            final Channel channel, final NetconfTimer timer,
+            final NetconfSessionListener<TestingNetconfSession> sessionListener, final long connectionTimeoutMillis) {
+        super(hello, promise, channel, timer, sessionListener, connectionTimeoutMillis, 16384);
+    }
+
+    @Override
+    protected TestingNetconfSession getSession(final NetconfSessionListener<TestingNetconfSession> sessionListener,
+            final Channel channel, final HelloMessage message) {
+        return new TestingNetconfSession(sessionListener, channel, new SessionIdType(Uint32.ONE));
+    }
+
+    @Override
+    protected void handleMessage(final HelloMessage netconfHelloMessage) {
+        // No-op
+    }
+}
