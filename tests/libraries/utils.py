89f0026920c8c6492836f4b88aa9b12302e22e2b
Martin Balaz <martin.balaz@pantheon.tech>
2026-06-10 10:19:28 +0200

    WIP: enhace bug link logging in report
    Change-Id: I062ef9ac6d5be6fe44732e6df54e534caedc0a15
    Signed-off-by: Martin Balaz <martin.balaz@pantheon.tech>

--- tests/libraries/utils.py
+++ tests/libraries/utils.py
@@ -117,8 +117,8 @@ def report_known_bug_on_failure(bug_id: str):
     try:
         yield
     except AssertionError as e:
-        url = f"https://jira.opendaylight.org/browse/{bug_id}"
-        allure.dynamic.link(url, name=f"Related Bug {bug_id}")
+        url = f"https://lf-opendaylight.atlassian.net/issues/?jql=%22External%20issue%20ID%22~%22{bug_id}%22"
+        allure.dynamic.issue(url, name=f"This test fails due to {bug_id}")
         error_msg = (
             f"\nThis test failed due to a previously reported bug: {url}\n"
             f"Original error: {str(e)}"
