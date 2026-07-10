90e60561333f95f8bf8a8f4d70cb3a53fe61ee37
Martin Balaz <martin.balaz@pantheon.tech>
2026-07-10 12:57:46 +0200

    Pytest: Seperate workspace setup fixture
    Create common workspace setup fixture to be used for both single node
    and three node cluster setups, and make the per-suite/per-test
    karaf logging fixtures apply automatically so suites don't need to
    expectly define this fixture.
    
    JIRA: NETCONF-1649
    Change-Id: I830cae27562d79cecae364a7478ba627d4268959
    Signed-off-by: Martin Balaz <martin.balaz@pantheon.tech>

--- tests/suites/notifications/test_notifications.py
+++ tests/suites/notifications/test_notifications.py
@@ -46,8 +46,6 @@ log = logging.getLogger(__name__)
 @pytest.mark.single_device
 @pytest.mark.standalone
 @pytest.mark.usefixtures("odl_standalone")
-@pytest.mark.usefixtures("log_test_suite_start_end_to_karaf")
-@pytest.mark.usefixtures("log_test_case_start_end_to_karaf")
 @pytest.mark.usefixtures("teardown_kill_all_running_ssereceiver_processes")
 @pytest.mark.run(order=SuiteOrder.NOTIFICATIONS)
 class TestNotifications:
