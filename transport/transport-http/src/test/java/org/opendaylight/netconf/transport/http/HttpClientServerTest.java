90254dc5dae860b98c5497eedf9dc199b857c612
Robert Varga <robert.varga@pantheon.tech>
2026-08-27 16:37:26 +0200

    Fix buffer leak in TestRequest
    The InputStream needs to be closed when consumed, so the underlying
    buffer is released.
    
    JIRA: NETCONF-1625
    Change-Id: I7b6b3f11e6343a086b0e1d90f8a6089e1ac8c0fd
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>
    (cherry picked from commit 740d80ba5dc64856a677600c5ccc185c7aa5fd2d)
    (cherry picked from commit 40d74d48a21a78136864ed111954adac51ccad65)

--- transport/transport-http/src/test/java/org/opendaylight/netconf/transport/http/HttpClientServerTest.java
+++ transport/transport-http/src/test/java/org/opendaylight/netconf/transport/http/HttpClientServerTest.java
@@ -18,6 +18,7 @@ import static io.netty.handler.codec.http.HttpResponseStatus.OK;
 import static io.netty.handler.codec.http.HttpVersion.HTTP_1_1;
 import static java.util.Objects.requireNonNull;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -102,6 +103,8 @@ class HttpClientServerTest {
                 } catch (IOException e) {
                     listener.requestFailed(this, e);
                     return;
+                } finally {
+                    assertDoesNotThrow(body::close);
                 }
             } else {
                 payload = "";
