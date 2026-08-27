5a6594a50cf6be9c97e7676d60d4c51ca6641907
Robert Varga <robert.varga@pantheon.tech>
2026-08-27 16:47:33 +0200

    Improve transport channel acquisition
    Use Invocation.getParameter(int, Class) to assert correct object.
    
    Change-Id: Id9e7eea487000029370f055aad817bb835f20c52
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>
    (cherry picked from commit fa1b693618294732dfb762c17cad80b86e70dccd)
    (cherry picked from commit 16e031cc1a13c7b74c796de8ebea524430ec2518)

--- transport/transport-http/src/test/java/org/opendaylight/netconf/transport/http/HttpClientServerTest.java
+++ transport/transport-http/src/test/java/org/opendaylight/netconf/transport/http/HttpClientServerTest.java
@@ -169,7 +169,7 @@ class HttpClientServerTest {
     void beforeEach() {
         // TODO: this looks like a spy() on a real implementation
         doAnswer(inv -> {
-            final var channel = inv.<HTTPTransportChannel>getArgument(0);
+            final var channel = inv.getArgument(0, HTTPTransportChannel.class);
             channel.channel().pipeline().addLast(new HTTPServerSessionBootstrap(channel.scheme(),
                     Uint32.valueOf(16384)) {
                 @Override
